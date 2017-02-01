fbde169f218ca1e1bbc08d50e1712d27ab8be848
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-02-01 14:26:38 +0100

    Covered basic tx actions

--- trader/options/util.go
+++ trader/options/util.go
@@ -18,15 +18,20 @@ func (a Accountant) SetAccount(addr []byte, acc *types.Account) {
 	state.SetAccount(a.store, addr, acc)
 }
 
+func (a Accountant) GetOrCreateAccount(addr []byte) *types.Account {
+	acct := state.GetAccount(a.store, addr)
+	if acct == nil {
+		acct = &types.Account{}
+	}
+	return acct
+}
+
 func (a Accountant) Refund(ctx types.CallContext) {
 	a.Pay(ctx.CallerAddress, ctx.Coins)
 }
 
 func (a Accountant) Pay(addr []byte, coins types.Coins) {
-	acct := a.GetAccount(addr)
-	if acct == nil {
-		acct = &types.Account{}
-	}
+	acct := a.GetOrCreateAccount(addr)
 	acct.Balance = acct.Balance.Plus(coins)
 	a.SetAccount(addr, acct)
 }

b135d8469335c950dd605149160d5c1c47105de0
rigelrozanski <rigel.rozanski@gmail.com>
2017-02-13 01:57:21 -0500

    doc edits
    go basics doc update
    
    exposed comment

--- docs/guide/deployment.md
+++ docs/guide/deployment.md
@@ -1,7 +1,9 @@
 ## Deployment
 
-Up until this point, we have only been testing the code as a stand-alone abci app, which is nice for developing, but it is no blockchain.  Just a blockchain-ready application.
+Up until this point, we have only been testing the code as a stand-alone abci app, 
+which is nice for developing, but it is no blockchain.  Just a blockchain-ready application.
 
-This section will demonstrate how to launch your basecoin-based application along with a tendermint testnet and initialize the genesis block for fun and profit.
+This section will demonstrate how to launch your basecoin-based application along 
+with a tendermint testnet and initialize the genesis block for fun and profit.
 
 **TODO** Maybe we link to a blog post for this???
--- docs/guide/src/example-plugin/cmd.go
+++ docs/guide/src/example-plugin/cmd.go
@@ -8,17 +8,24 @@ import (
 	"github.com/tendermint/basecoin/types"
 )
 
+//Called during CLI initialization
 func init() {
+
+	//Register a plugin specific CLI command as a subcommand of the tx command
 	commands.RegisterTxSubcommand(ExamplePluginTxCmd)
+
+	//Register the example with basecoin at start
 	commands.RegisterStartPlugin("example-plugin", func() types.Plugin { return NewExamplePlugin() })
 }
 
 var (
+	//CLI Flags
 	ExampleFlag = cli.BoolFlag{
 		Name:  "valid",
 		Usage: "Set this to make the transaction valid",
 	}
 
+	//CLI Plugin Commands
 	ExamplePluginTxCmd = cli.Command{
 		Name:  "example",
 		Usage: "Create, sign, and broadcast a transaction to the example plugin",
@@ -29,8 +36,17 @@ var (
 	}
 )
 
+//Send a transaction
 func cmdExamplePluginTx(c *cli.Context) error {
+	//Retrieve any flag results
 	exampleFlag := c.Bool("valid")
+
+	//Create a transaction object with flag results
 	exampleTx := ExamplePluginTx{exampleFlag}
-	return commands.AppTx(c, "example-plugin", wire.BinaryBytes(exampleTx))
+
+	//Encode transaction bytes
+	exampleTxBytes := wire.BinaryBytes(exampleTx)
+
+	//Send the transaction and return any errors
+	return commands.AppTx(c, "example-plugin", exampleTxBytes)
 }

3a1313ab7d0868f9c410ab423909bdbc45ad4a7f
Ethan Buchman <ethan@coinculture.info>
2017-03-22 01:18:56 -0400

    secp256k1: use compressed pubkey, bitcoin-style address

--- priv_key.go
+++ priv_key.go
@@ -160,9 +160,9 @@ func (privKey PrivKeySecp256k1) Sign(msg []byte) Signature {
 
 func (privKey PrivKeySecp256k1) PubKey() PubKey {
 	_, pub__ := secp256k1.PrivKeyFromBytes(secp256k1.S256(), privKey[:])
-	pub := [64]byte{}
-	copy(pub[:], pub__.SerializeUncompressed()[1:])
-	return PubKeySecp256k1(pub)
+	var pub PubKeySecp256k1
+	copy(pub[:], pub__.SerializeCompressed())
+	return pub
 }
 
 func (privKey PrivKeySecp256k1) Equals(other PrivKey) bool {
---
+++ pub_key_test.go
@@ -0,0 +1,41 @@
+package crypto
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/stretchr/testify/assert"
+)
+
+type keyData struct {
+	priv string
+	pub  string
+	addr string
+}
+
+var secpDataTable = []keyData{
+	{
+		priv: "a96e62ed3955e65be32703f12d87b6b5cf26039ecfa948dc5107a495418e5330",
+		pub:  "02950e1cdfcb133d6024109fd489f734eeb4502418e538c28481f22bce276f248c",
+		addr: "1CKZ9Nx4zgds8tU7nJHotKSDr4a9bYJCa3",
+	},
+}
+
+func TestPubKeySecp256k1Address(t *testing.T) {
+	for _, d := range secpDataTable {
+		privB, _ := hex.DecodeString(d.priv)
+		pubB, _ := hex.DecodeString(d.pub)
+		addrB, _, _ := base58.CheckDecode(d.addr)
+
+		var priv PrivKeySecp256k1
+		copy(priv[:], privB)
+
+		pubT := priv.PubKey().(PubKeySecp256k1)
+		pub := pubT[:]
+		addr := priv.PubKey().Address()
+
+		assert.Equal(t, pub, pubB, "Expected pub keys to match")
+		assert.Equal(t, addr, addrB, "Expected addresses to match")
+	}
+}

ed9b5e840f456a01082d4b5930269de48716e114
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-06-15 20:56:59 +0200

    Support other binaries

--- tests/cli/basictx.sh
+++ tests/cli/basictx.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# these are two globals to control all scripts (can use eg. counter instead)
+SERVER_EXE=basecoin
+CLIENT_EXE=basecli
 
 oneTimeSetUp() {
+  # these are passed in as args
   BASE_DIR=$HOME/.basecoin_test_basictx
   CHAIN_ID=my-test-chain
 
   rm -rf $BASE_DIR 2>/dev/null
   mkdir -p $BASE_DIR
 
-  ACCOUNTS=(jae ethan bucky rigel igor)
-  RICH=${ACCOUNTS[0]}
-  POOR=${ACCOUNTS[1]}
 
-  # set up client
+  # set up client - make sure you use the proper prefix if you set
+  # a custom CLIENT_EXE
   export BC_HOME=${BASE_DIR}/client
   prepareClient
 
@@ -31,7 +33,7 @@ oneTimeSetUp() {
 oneTimeTearDown() {
   echo
   echo
-  echo "stopping basecoin test server..."
+  echo "stopping $SERVER_EXE test server..."
   kill -9 $PID_SERVER >/dev/null 2>&1
   sleep 1
 }
@@ -40,13 +42,13 @@ test00GetAccount() {
   SENDER=$(getAddr $RICH)
   RECV=$(getAddr $POOR)
 
-  assertFalse "requires arg" "basecli query account"
-  ACCT=$(basecli query account $SENDER)
+  assertFalse "requires arg" "${CLIENT_EXE} query account"
+  ACCT=$(${CLIENT_EXE} query account $SENDER)
   assertTrue "must have proper genesis account" $?
   assertEquals "no tx" "0" $(echo $ACCT | jq .data.sequence)
   assertEquals "has money" "9007199254740992" $(echo $ACCT | jq .data.coins[0].amount)
 
-  ACCT2=$(basecli query account $RECV)
+  ACCT2=$(${CLIENT_EXE} query account $RECV)
   assertFalse "has no genesis account" $?
 }
 
@@ -54,10 +56,10 @@ test01SendTx() {
   SENDER=$(getAddr $RICH)
   RECV=$(getAddr $POOR)
 
-  assertFalse "missing dest" "basecli tx send --amount=992mycoin --sequence=1 2>/dev/null"
-  assertFalse "bad password" "echo foo | basecli tx send --amount=992mycoin --sequence=1 --to=$RECV --name=$RICH 2>/dev/null"
+  assertFalse "missing dest" "${CLIENT_EXE} tx send --amount=992mycoin --sequence=1 2>/dev/null"
+  assertFalse "bad password" "echo foo | ${CLIENT_EXE} tx send --amount=992mycoin --sequence=1 --to=$RECV --name=$RICH 2>/dev/null"
   # we have to remove the password request from stdout, to just get the json
-  RES=$(echo qwertyuiop | basecli tx send --amount=992mycoin --sequence=1 --to=$RECV --name=$RICH 2>/dev/null | tail -n +2)
+  RES=$(echo qwertyuiop | ${CLIENT_EXE} tx send --amount=992mycoin --sequence=1 --to=$RECV --name=$RICH 2>/dev/null | tail -n +2)
   assertTrue "sent tx" $?
   HASH=$(echo $RES | jq .hash | tr -d \")
   TX_HEIGHT=$(echo $RES | jq .height)
@@ -65,19 +67,19 @@ test01SendTx() {
   assertEquals "good deliver" "0" $(echo $RES | jq .deliver_tx.code)
 
   # make sure sender goes down
-  ACCT=$(basecli query account $SENDER)
+  ACCT=$(${CLIENT_EXE} query account $SENDER)
   assertTrue "must have genesis account" $?
   assertEquals "one tx" "1" $(echo $ACCT | jq .data.sequence)
   assertEquals "has money" "9007199254740000" $(echo $ACCT | jq .data.coins[0].amount)
 
   # make sure recipient goes up
-  ACCT2=$(basecli query account $RECV)
+  ACCT2=$(${CLIENT_EXE} query account $RECV)
   assertTrue "must have new account" $?
   assertEquals "no tx" "0" $(echo $ACCT2 | jq .data.sequence)
   assertEquals "has money" "992" $(echo $ACCT2 | jq .data.coins[0].amount)
 
   # make sure tx is indexed
-  TX=$(basecli query tx $HASH)
+  TX=$(${CLIENT_EXE} query tx $HASH)
   assertTrue "found tx" $?
   assertEquals "proper height" $TX_HEIGHT $(echo $TX | jq .height)
   assertEquals "type=send" '"send"' $(echo $TX | jq .data.type)

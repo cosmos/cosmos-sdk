6bc5fa387635f965dbeab6ee22bae696d66e6c11
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-08-07 00:00:34 +0200

    Add eyes and eyescli main commands
    Note how the all framework commands can be reused with a bit of configurations.
    And one can add the custom query and tx commands.

---
+++ cmd/eyes/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"github.com/tendermint/tmlibs/cli"
+
+	"github.com/tendermint/basecoin"
+	"github.com/tendermint/basecoin/cmd/basecoin/commands"
+	"github.com/tendermint/basecoin/modules/base"
+	"github.com/tendermint/basecoin/modules/etc"
+	"github.com/tendermint/basecoin/stack"
+)
+
+// BuildApp constructs the stack we want to use for this app
+func BuildApp() basecoin.Handler {
+	return stack.New(
+		base.Logger{},
+		stack.Recovery{},
+	).
+		// We do this to demo real usage, also embeds it under it's own namespace
+		Dispatch(
+			stack.WrapHandler(etc.NewHandler()),
+		)
+}
+
+func main() {
+	rt := commands.RootCmd
+	rt.Short = "eyes"
+	rt.Long = "A demo app to show key-value store with proofs over abci"
+
+	commands.Handler = BuildApp()
+
+	rt.AddCommand(
+		// out own init command to not require argument
+		InitCmd,
+		commands.StartCmd,
+		//commands.RelayCmd,
+		commands.UnsafeResetAllCmd,
+		commands.VersionCmd,
+	)
+
+	cmd := cli.PrepareMainCmd(rt, "EYE", os.ExpandEnv("$HOME/.eyes"))
+	cmd.Execute()
+}

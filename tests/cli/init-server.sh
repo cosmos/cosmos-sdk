bfd4ce96bbe9d3428479440a0399c619e10cd300
Rigel Rozanski <rigelrozanski@Rigels-MacBook-Air.local>
2017-08-30 19:25:35 -0400

    added init option flag and tests

---
+++ tests/cli/init-server.sh
@@ -0,0 +1,23 @@
+" Press ? for help
+
+.. (up a dir)
+/
+▸ Applications/
+▸ bin/
+▸ cores/
+▸ dev/
+▸ etc/ -> /private/etc/
+▸ home/
+▸ Library/
+▸ net/
+▸ Network/
+▸ opt/
+▸ private/
+▸ sbin/
+▸ System/
+▸ tmp/ -> /private/tmp/
+▸ Users/
+▸ usr/
+▸ var/ -> /private/var/
+▸ Volumes/
+  installer.failurerequests [RO]

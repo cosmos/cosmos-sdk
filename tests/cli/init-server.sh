ab8505d1f6ecfd4ec5fd14affc5deebf52466951
Rigel Rozanski <rigelrozanski@Rigels-MacBook-Air.local>
2017-08-30 19:54:55 -0400

    init options tests

--- tests/cli/init-server.sh
+++ tests/cli/init-server.sh
@@ -1,23 +1,31 @@
-" Press ? for help
+#!/bin/bash
 
-.. (up a dir)
-/
-▸ Applications/
-▸ bin/
-▸ cores/
-▸ dev/
-▸ etc/ -> /private/etc/
-▸ home/
-▸ Library/
-▸ net/
-▸ Network/
-▸ opt/
-▸ private/
-▸ sbin/
-▸ System/
-▸ tmp/ -> /private/tmp/
-▸ Users/
-▸ usr/
-▸ var/ -> /private/var/
-▸ Volumes/
-  installer.failurerequests [RO]
+CLIENT_EXE=basecli
+SERVER_EXE=basecoin
+
+test01initOption() {
+    BASE=~/.bc_init_test
+    rm -rf "$BASE"
+    mkdir -p "$BASE"
+
+    SERVER="${BASE}/server"
+    GENESIS_FILE=${SERVER}/genesis.json
+    HEX="deadbeef1234deadbeef1234deadbeef1234aaaa"
+
+    ${SERVER_EXE} init ${HEX} --home="$SERVER" --option=app1/key1/val1 --option=app2/key2/val2 >/dev/null
+    if ! assertTrue "line=${LINENO}" $?; then return 1; fi
+
+    OPTION1KEY=$(cat ${GENESIS_FILE} | jq '.app_options.plugin_options[2]')
+    OPTION1VAL=$(cat ${GENESIS_FILE} | jq '.app_options.plugin_options[3]')
+    OPTION2KEY=$(cat ${GENESIS_FILE} | jq '.app_options.plugin_options[4]')
+    OPTION2VAL=$(cat ${GENESIS_FILE} | jq '.app_options.plugin_options[5]')
+
+    assertEquals "line=${LINENO}" '"app1/key1"' $OPTION1KEY
+    assertEquals "line=${LINENO}" '"val1"' $OPTION1VAL
+    assertEquals "line=${LINENO}" '"app2/key2"' $OPTION2KEY
+    assertEquals "line=${LINENO}" '"val2"' $OPTION2VAL
+}
+
+# load and run these tests with shunit2!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #get this files directory
+. $DIR/shunit2

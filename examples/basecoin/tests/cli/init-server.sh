b11536c57931119a8aba9bb7e8d625f6ac537cbd
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-09-08 20:51:14 +0200

    Move the tests into basecoin examples

--- examples/basecoin/tests/cli/init-server.sh
+++ examples/basecoin/tests/cli/init-server.sh
@@ -38,6 +38,8 @@ oneTimeTearDown() {
 }
 
 # load and run these tests with shunit2!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #get this files directory
 CLI_DIR=$GOPATH/src/github.com/cosmos/cosmos-sdk/tests/cli
+
 . $CLI_DIR/common.sh
 . $CLI_DIR/shunit2

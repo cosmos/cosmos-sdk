1f151d1232b4971aa68ebbda15ee0d7aa45c89bd
Ethan Buchman <ethan@coinculture.info>
2017-10-02 14:40:42 -0400

    update for tendermint 0.11

--- examples/eyes/cmd/eyes/init.go
+++ examples/eyes/cmd/eyes/init.go
@@ -46,7 +46,7 @@ func getGenesisJSON(chainID string) string {
   "genesis_time": "0001-01-01T00:00:00.000Z",
   "validators": [
     {
-      "amount": 10,
+      "power": 10,
       "name": "",
       "pub_key": {
         "type": "ed25519",

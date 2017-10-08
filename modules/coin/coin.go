80ebfe9c0e60105f2e52e6597ef5343003a38fdc
rigelrozanski <rigel.rozanski@gmail.com>
2017-10-07 22:44:16 -0400

    remove IsValidNonNegative

--- modules/coin/coin.go
+++ modules/coin/coin.go
@@ -127,11 +127,6 @@ func (coins Coins) IsValid() bool {
 	}
 }
 
-// IsValidNonnegative tests is coins IsValid and IsNonnegative
-func (coins Coins) IsValidNonnegative() bool {
-	return coins.IsValid() && coins.IsNonnegative()
-}
-
 // Plus combines to sets of coins
 //
 // TODO: handle empty coins!

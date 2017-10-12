098d17711aa2053b9abe30a29e9d0febd7c6f2a1
Ethan Buchman <ethan@coinculture.info>
2017-10-11 23:44:40 -0400

    fix test

--- version/version.go
+++ version/version.go
@@ -1,6 +1,10 @@
 //nolint
 package version
 
+// when updating these,
+// remember to also update examples/basecoin/tests/cli/rpc.sh
+// ... ugh ....
+
 const Maj = "0"
 const Min = "7"
 const Fix = "1"

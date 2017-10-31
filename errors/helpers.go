561d77cae09fca37f66a396b2db90e3a7e5664aa
Jae Kwon <jae@tendermint.com>
2017-10-31 17:58:12 -0500

    remove errors module

--- errors/helpers.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-// CheckErr is the type of all the check functions here
-type CheckErr func(error) bool
-
-// NoErr is useful for test cases when you want to fulfil the CheckErr type
-func NoErr(err error) bool {
-	return err == nil
-}

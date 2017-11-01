a2583e278334c35b170352e467a741166474b6da
Jae Kwon <jae@tendermint.com>
2017-11-01 16:38:11 -0500

    Draft of suggested changes

--- keys/keybase.go
+++ keys/keybase.go
@@ -4,47 +4,49 @@ import (
 	"strings"
 
 	crypto "github.com/tendermint/go-crypto"
-	keys "github.com/tendermint/go-crypto/keys"
+	dbm "github.com/tendermint/tmlibs/db"
 )
 
-// Manager combines encyption and storage implementation to provide
+// XXX Lets use go-crypto/bcrypt and ascii encoding directly in here without
+// further wrappers around a store or DB.
+// Copy functions from: https://github.com/tendermint/mintkey/blob/master/cmd/mintkey/common.go
+//
+// dbKeybase combines encyption and storage implementation to provide
 // a full-featured key manager
-type Manager struct {
-	es    encryptedStorage
-	codec keys.Codec
+type dbKeybase struct {
+	db    dbm.DB
+	codec Codec
 }
 
-func New(coder Encoder, store keys.Storage, codec keys.Codec) Manager {
-	return Manager{
+func New(db dbm.DB, codec Codec) dbKeybase {
+	return dbKeybase{
 		es: encryptedStorage{
+			db:    db,
 			coder: coder,
-			store: store,
 		},
 		codec: codec,
 	}
 }
 
-// assert Manager satisfies keys.Signer and keys.Manager interfaces
-var _ keys.Signer = Manager{}
-var _ keys.Manager = Manager{}
+var _ Keybase = dbKeybase{}
 
 // Create adds a new key to the storage engine, returning error if
 // another key already stored under this name
 //
 // algo must be a supported go-crypto algorithm: ed25519, secp256k1
-func (s Manager) Create(name, passphrase, algo string) (keys.Info, string, error) {
+func (kb dbKeybase) Create(name, passphrase, algo string) (Info, string, error) {
 	// 128-bits are the all the randomness we can make use of
 	secret := crypto.CRandBytes(16)
 	gen := getGenerator(algo)
 
 	key, err := gen.Generate(secret)
 	if err != nil {
-		return keys.Info{}, "", err
+		return Info{}, "", err
 	}
 
-	err = s.es.Put(name, passphrase, key)
+	err = kb.es.Put(name, passphrase, key)
 	if err != nil {
-		return keys.Info{}, "", err
+		return Info{}, "", err
 	}
 
 	// we append the type byte to the serialized secret to help with recovery
@@ -52,7 +54,7 @@ func (s Manager) Create(name, passphrase, algo string) (keys.Info, string, error
 	typ := key.Bytes()[0]
 	secret = append(secret, typ)
 
-	seed, err := s.codec.BytesToWords(secret)
+	seed, err := kb.codec.BytesToWords(secret)
 	phrase := strings.Join(seed, " ")
 	return info(name, key), phrase, err
 }
@@ -63,11 +65,11 @@ func (s Manager) Create(name, passphrase, algo string) (keys.Info, string, error
 // it under name, protected by passphrase.
 //
 // Result similar to New(), except it doesn't return the seed again...
-func (s Manager) Recover(name, passphrase, seedphrase string) (keys.Info, error) {
+func (s dbKeybase) Recover(name, passphrase, seedphrase string) (Info, error) {
 	words := strings.Split(strings.TrimSpace(seedphrase), " ")
-	secret, err := s.codec.WordsToBytes(words)
+	secret, err := kb.codec.WordsToBytes(words)
 	if err != nil {
-		return keys.Info{}, err
+		return Info{}, err
 	}
 
 	// secret is comprised of the actual secret with the type appended
@@ -78,24 +80,24 @@ func (s Manager) Recover(name, passphrase, seedphrase string) (keys.Info, error)
 	gen := getGeneratorByType(typ)
 	key, err := gen.Generate(secret)
 	if err != nil {
-		return keys.Info{}, err
+		return Info{}, err
 	}
 
 	// d00d, it worked!  create the bugger....
-	err = s.es.Put(name, passphrase, key)
+	err = kb.es.Put(name, passphrase, key)
 	return info(name, key), err
 }
 
 // List loads the keys from the storage and enforces alphabetical order
-func (s Manager) List() (keys.Infos, error) {
-	res, err := s.es.List()
+func (s dbKeybase) List() (Infos, error) {
+	res, err := kb.es.List()
 	res.Sort()
 	return res, err
 }
 
 // Get returns the public information about one key
-func (s Manager) Get(name string) (keys.Info, error) {
-	_, info, err := s.es.store.Get(name)
+func (s dbKeybase) Get(name string) (Info, error) {
+	_, info, err := kb.es.store.Get(name)
 	return info, err
 }
 
@@ -103,8 +105,8 @@ func (s Manager) Get(name string) (keys.Info, error) {
 // this public key
 //
 // If no key for this name, or the passphrase doesn't match, returns an error
-func (s Manager) Sign(name, passphrase string, tx keys.Signable) error {
-	key, _, err := s.es.Get(name, passphrase)
+func (s dbKeybase) Sign(name, passphrase string, msg []byte) error {
+	key, _, err := kb.es.Get(name, passphrase)
 	if err != nil {
 		return err
 	}
@@ -115,55 +117,55 @@ func (s Manager) Sign(name, passphrase string, tx keys.Signable) error {
 
 // Export decodes the private key with the current password, encodes
 // it with a secure one-time password and generates a sequence that can be
-// Imported by another Manager
+// Imported by another dbKeybase
 //
 // This is designed to copy from one device to another, or provide backups
 // during version updates.
-func (s Manager) Export(name, oldpass, transferpass string) ([]byte, error) {
-	key, _, err := s.es.Get(name, oldpass)
+func (s dbKeybase) Export(name, oldpass, transferpass string) ([]byte, error) {
+	key, _, err := kb.es.Get(name, oldpass)
 	if err != nil {
 		return nil, err
 	}
 
-	res, err := s.es.coder.Encrypt(key, transferpass)
+	res, err := kb.es.coder.Encrypt(key, transferpass)
 	return res, err
 }
 
 // Import accepts bytes generated by Export along with the same transferpass
 // If they are valid, it stores the password under the given name with the
 // new passphrase.
-func (s Manager) Import(name, newpass, transferpass string, data []byte) error {
-	key, err := s.es.coder.Decrypt(data, transferpass)
+func (s dbKeybase) Import(name, newpass, transferpass string, data []byte) error {
+	key, err := kb.es.coder.Decrypt(data, transferpass)
 	if err != nil {
 		return err
 	}
 
-	return s.es.Put(name, newpass, key)
+	return kb.es.Put(name, newpass, key)
 }
 
 // Delete removes key forever, but we must present the
 // proper passphrase before deleting it (for security)
-func (s Manager) Delete(name, passphrase string) error {
+func (s dbKeybase) Delete(name, passphrase string) error {
 	// verify we have the proper password before deleting
-	_, _, err := s.es.Get(name, passphrase)
+	_, _, err := kb.es.Get(name, passphrase)
 	if err != nil {
 		return err
 	}
-	return s.es.Delete(name)
+	return kb.es.Delete(name)
 }
 
 // Update changes the passphrase with which a already stored key is encoded.
 //
 // oldpass must be the current passphrase used for encoding, newpass will be
 // the only valid passphrase from this time forward
-func (s Manager) Update(name, oldpass, newpass string) error {
-	key, _, err := s.es.Get(name, oldpass)
+func (s dbKeybase) Update(name, oldpass, newpass string) error {
+	key, _, err := kb.es.Get(name, oldpass)
 	if err != nil {
 		return err
 	}
 
 	// we must delete first, as Putting over an existing name returns an error
-	s.Delete(name, oldpass)
+	kb.Delete(name, oldpass)
 
-	return s.es.Put(name, newpass, key)
+	return kb.es.Put(name, newpass, key)
 }

8da0f3f38ab815a05dcfce995ca355a9c13ac8af
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-11-22 16:07:10 +0100

    Updated to latest develop, removed websocket from proxy due to changes

--- client/wrapper.go
+++ client/wrapper.go
@@ -1,10 +1,7 @@
 package client
 
 import (
-	"fmt"
-
 	"github.com/tendermint/go-wire/data"
-	"github.com/tendermint/tmlibs/events"
 
 	"github.com/tendermint/tendermint/certifiers"
 	certclient "github.com/tendermint/tendermint/certifiers/client"
@@ -28,11 +25,12 @@ type Wrapper struct {
 // If it is wrapping an HTTP rpcclient, it will also wrap the websocket interface
 func SecureClient(c rpcclient.Client, cert *certifiers.Inquiring) Wrapper {
 	wrap := Wrapper{c, cert}
+	// TODO: no longer possible as no more such interface exposed....
 	// if we wrap http client, then we can swap out the event switch to filter
-	if hc, ok := c.(*rpcclient.HTTP); ok {
-		evt := hc.WSEvents.EventSwitch
-		hc.WSEvents.EventSwitch = WrappedSwitch{evt, wrap}
-	}
+	// if hc, ok := c.(*rpcclient.HTTP); ok {
+	// 	evt := hc.WSEvents.EventSwitch
+	// 	hc.WSEvents.EventSwitch = WrappedSwitch{evt, wrap}
+	// }
 	return wrap
 }
 
@@ -127,44 +125,44 @@ func (w Wrapper) Commit(height *int) (*ctypes.ResultCommit, error) {
 	return r, err
 }
 
-// WrappedSwitch creates a websocket connection that auto-verifies any info
-// coming through before passing it along.
-//
-// Since the verification takes 1-2 rpc calls, this is obviously only for
-// relatively low-throughput situations that can tolerate a bit extra latency
-type WrappedSwitch struct {
-	types.EventSwitch
-	client rpcclient.Client
-}
+// // WrappedSwitch creates a websocket connection that auto-verifies any info
+// // coming through before passing it along.
+// //
+// // Since the verification takes 1-2 rpc calls, this is obviously only for
+// // relatively low-throughput situations that can tolerate a bit extra latency
+// type WrappedSwitch struct {
+// 	types.EventSwitch
+// 	client rpcclient.Client
+// }
 
-// FireEvent verifies any block or header returned from the eventswitch
-func (s WrappedSwitch) FireEvent(event string, data events.EventData) {
-	tm, ok := data.(types.TMEventData)
-	if !ok {
-		fmt.Printf("bad type %#v\n", data)
-		return
-	}
+// // FireEvent verifies any block or header returned from the eventswitch
+// func (s WrappedSwitch) FireEvent(event string, data events.EventData) {
+// 	tm, ok := data.(types.TMEventData)
+// 	if !ok {
+// 		fmt.Printf("bad type %#v\n", data)
+// 		return
+// 	}
 
-	// check to validate it if possible, and drop if not valid
-	switch t := tm.Unwrap().(type) {
-	case types.EventDataNewBlockHeader:
-		err := verifyHeader(s.client, t.Header)
-		if err != nil {
-			fmt.Printf("Invalid header: %#v\n", err)
-			return
-		}
-	case types.EventDataNewBlock:
-		err := verifyBlock(s.client, t.Block)
-		if err != nil {
-			fmt.Printf("Invalid block: %#v\n", err)
-			return
-		}
-		// TODO: can we verify tx as well? anything else
-	}
+// 	// check to validate it if possible, and drop if not valid
+// 	switch t := tm.Unwrap().(type) {
+// 	case types.EventDataNewBlockHeader:
+// 		err := verifyHeader(s.client, t.Header)
+// 		if err != nil {
+// 			fmt.Printf("Invalid header: %#v\n", err)
+// 			return
+// 		}
+// 	case types.EventDataNewBlock:
+// 		err := verifyBlock(s.client, t.Block)
+// 		if err != nil {
+// 			fmt.Printf("Invalid block: %#v\n", err)
+// 			return
+// 		}
+// 		// TODO: can we verify tx as well? anything else
+// 	}
 
-	// looks good, we fire it
-	s.EventSwitch.FireEvent(event, data)
-}
+// 	// looks good, we fire it
+// 	s.EventSwitch.FireEvent(event, data)
+// }
 
 func verifyHeader(c rpcclient.Client, head *types.Header) error {
 	// get a checkpoint to verify from

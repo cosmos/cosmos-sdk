d03810a6816e07c0f8bf13daa68bfa014ecd399b
Jae Kwon <jae@tendermint.com>
2018-01-15 19:22:12 -0800

    Add tools dir

---
+++ tools/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	// Include dependencies here so glide picks them up
+	// and installs sub-dependencies.
+	_ "github.com/rigelrozanski/shelldown/cmd/shelldown"
+)
+
+func main() {}

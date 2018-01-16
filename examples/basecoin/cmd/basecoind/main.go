f1cea5edde76b33ea7f13d665c4c804f5a1761bd
Jae Kwon <jae@tendermint.com>
2018-01-16 11:21:07 -0800

    Fix tools/main.go; Add cmd/basecoind to make make pass

---
+++ examples/basecoin/cmd/basecoind/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("TODO: move examples/basecoin/main.go here and refactor")
+}
--- tools/main.go
+++ tools/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	// Include dependencies here so glide picks them up
 	// and installs sub-dependencies.
-	_ "github.com/rigelrozanski/shelldown/cmd/shelldown"
+
+	// TODO: Ideally this gets auto-imported on glide update.
+	// Any way to make that happen?
+	_ "github.com/rigelrozanski/common"
 )
 
 func main() {}

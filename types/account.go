9ad0cebbf624ca7439cbbf19c56b72533e2f101e
Michael Sofaer <github@mike.sofaer.net>
2018-01-20 08:09:04 -0800

    Add errors on setters to match BaseAccount implementation

--- types/account.go
+++ types/account.go
@@ -17,7 +17,7 @@ type Account interface {
 	SetSequence(int64) error
 
 	GetCoins() Coins
-	SetCoins(Coins)
+	SetCoins(Coins) error
 
 	Get(key interface{}) (value interface{}, err error)
 	Set(key interface{}, value interface{}) error
@@ -27,7 +27,7 @@ type Account interface {
 type AccountStore interface {
 	NewAccountWithAddress(ctx Context, addr crypto.Address) Account
 	GetAccount(ctx Context, addr crypto.Address) Account
-	SetAccount(ctx Context, acc Account)
+	SetAccount(ctx Context, acc Account) error
 }
 
 // new(AccountStoreKey) is a capabilities key.

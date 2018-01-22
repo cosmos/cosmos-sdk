f7b8d6eba08a31f7af2ea753f25d0e8957c2889f
Jae Kwon <jae@tendermint.com>
2018-01-22 06:26:18 -0800

    Split dummy file

--- examples/dummy/main.go
+++ examples/dummy/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 
 	"github.com/tendermint/abci/server"
-	crypto "github.com/tendermint/go-crypto"
 	cmn "github.com/tendermint/tmlibs/common"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
@@ -50,68 +48,6 @@ func main() {
 		srv.Stop()
 	})
 	return
-}
-
-type dummyTx struct {
-	key   []byte
-	value []byte
-
-	bytes []byte
-}
-
-func (tx dummyTx) Get(key interface{}) (value interface{}) {
-	switch k := key.(type) {
-	case string:
-		switch k {
-		case "key":
-			return tx.key
-		case "value":
-			return tx.value
-		}
-	}
-	return nil
-}
-
-func (tx dummyTx) Type() string {
-	return "dummy"
-}
-
-func (tx dummyTx) GetSignBytes() []byte {
-	return tx.bytes
-}
-
-// Should the app be calling this? Or only handlers?
-func (tx dummyTx) ValidateBasic() error {
-	return nil
-}
-
-func (tx dummyTx) GetSigners() []crypto.Address {
-	return nil
-}
-
-func (tx dummyTx) GetSignatures() []sdk.StdSignature {
-	return nil
-}
-
-func (tx dummyTx) GetFeePayer() crypto.Address {
-	return nil
-}
-
-func decodeTx(txBytes []byte) (sdk.Tx, error) {
-	var tx sdk.Tx
-
-	split := bytes.Split(txBytes, []byte("="))
-	if len(split) == 1 {
-		k := split[0]
-		tx = dummyTx{k, k, txBytes}
-	} else if len(split) == 2 {
-		k, v := split[0], split[1]
-		tx = dummyTx{k, v, txBytes}
-	} else {
-		return nil, fmt.Errorf("too many =")
-	}
-
-	return tx, nil
 }
 
 func DummyHandler(storeKey sdk.StoreKey) sdk.Handler {

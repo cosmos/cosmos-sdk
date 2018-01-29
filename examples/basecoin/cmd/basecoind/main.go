a1e8cc77a22f4d9649110e81f727654983a9a65a
Adrian Brink <adrian@brink-holdings.com>
2018-01-29 10:26:04 +0100

    Implement basic version cmd for basecoind

--- examples/basecoin/cmd/basecoind/main.go
+++ examples/basecoin/cmd/basecoind/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/tendermint/tmlibs/cli"
+
+	"github.com/cosmos/cosmos-sdk/examples/basecoin/version"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "basecoind",
+	Short: "The basecoin daemon.",
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version info for basecoind.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version.Version)
+	},
+}
+
 func main() {
-	fmt.Println("TODO: move examples/basecoin/main.go here and refactor")
+	rootCmd.AddCommand(
+		versionCmd,
+	)
+
+	cmd := cli.PrepareMainCmd(rootCmd, "BC", os.ExpandEnv("$HOME/.basecoind"))
+	cmd.Execute()
 }

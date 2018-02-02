3baea456946917589057a47219421ab676a19a84
Zach Ramsay <zach.ramsay@gmail.com>
2018-02-02 22:03:03 +0000

    remove deprecated 'publish/' directory

--- publish/publish.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-
-# Get the version from the environment, or try to figure it out.
-if [ -z $VERSION ]; then
-    VERSION=$(awk -F\" '/Version =/ { print $2; exit }' < version/version.go)
-fi
-aws s3 cp --recursive build/dist s3://tendermint/binaries/basecoin/v${VERSION} --acl public-read

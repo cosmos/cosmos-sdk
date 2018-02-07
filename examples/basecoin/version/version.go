5e799e3b9910f25bef4da089971e9de17f07afe9
Adrian Brink <adrian@brink-holdings.com>
2018-02-07 13:54:33 +0100

    Fix Makefiles

--- examples/basecoin/version/version.go
+++ examples/basecoin/version/version.go
@@ -1,16 +1,11 @@
-//nolint
 package version
-
-// when updating these,
-// remember to also update examples/basecoin/tests/cli/rpc.sh
-// TODO improve
 
 const Maj = "0"
 const Min = "7"
-const Fix = "1"
+const Fix = "0"
 
 var (
-	Version = "0.7.1"
+	Version = "0.7.0"
 
 	GitCommit string
 )

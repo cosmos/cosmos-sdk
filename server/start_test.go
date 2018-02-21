1f31fbeea89275ea5b75ba65bb3de2ad240b715f
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-02-21 16:04:54 +0100

    Start writing test scaffold to test server

---
+++ server/start_test.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"testing"
+)
+
+func TestStart(t *testing.T) {
+	// app, cleanup, err := mock.SetupApp()
+	// if cleanup != nil {
+	// 	defer cleanup()
+	// }
+
+	// require.NoError(t, err)
+	// // TODO: init and start
+}

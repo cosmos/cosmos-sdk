ee31db32638e1c49075a2195d81d368218bb82ec
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-02-22 16:17:19 +0100

    Very rough import of old keys logic

---
+++ client/keys/get.go
@@ -0,0 +1,47 @@
+// Copyright © 2017 Ethan Frey
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package keys
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get [name]",
+	Short: "Get details of one key",
+	Long:  `Return public details of one local key.`,
+	RunE:  runGetCmd,
+}
+
+func runGetCmd(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 || len(args[0]) == 0 {
+		return errors.New("You must provide a name for the key")
+	}
+	name := args[0]
+
+	kb, err := GetKeyBase()
+	if err != nil {
+		return err
+	}
+
+	info, err := kb.Get(name)
+	if err == nil {
+		printInfo(info)
+	}
+	return err
+}

f4f9c431ea2b058b7fc78c1636e205cbf0d2049d
rigelrozanski <rigel.rozanski@gmail.com>
2018-02-28 23:35:38 +0000

    bank registers crypto

--- x/bank/wire.go
+++ x/bank/wire.go
@@ -1,6 +1,7 @@
 package bank
 
 import (
+	crypto "github.com/tendermint/go-crypto"
 	"github.com/tendermint/go-wire"
 )
 
@@ -8,4 +9,5 @@ func RegisterWire(cdc *wire.Codec) {
 	// TODO include option to always include prefix bytes.
 	cdc.RegisterConcrete(SendMsg{}, "cosmos-sdk/SendMsg", nil)
 	cdc.RegisterConcrete(IssueMsg{}, "cosmos-sdk/IssueMsg", nil)
+	crypto.RegisterWire(cdc) // Register crypto.[PubKey,PrivKey,Signature] types.
 }

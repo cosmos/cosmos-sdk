105d16782fbad475f5b85a32ef78909000cab3fa
rigelrozanski <rigel.rozanski@gmail.com>
2018-03-17 19:18:04 +0100

    gettin' stakin' tests up to snuff

--- types/account.go
+++ types/account.go
@@ -1,12 +1,27 @@
 package types
 
 import (
+	"encoding/hex"
+	"errors"
+
 	crypto "github.com/tendermint/go-crypto"
 	cmn "github.com/tendermint/tmlibs/common"
 )
 
 // Address in go-crypto style
 type Address = cmn.HexBytes
+
+// create an Address from a string
+func GetAddress(address string) (addr Address, err error) {
+	if len(address) == 0 {
+		return addr, errors.New("must use provide address")
+	}
+	bz, err := hex.DecodeString(address)
+	if err != nil {
+		return nil, err
+	}
+	return Address(bz), nil
+}
 
 // Account is a standard account using a sequence number for replay protection
 // and a pubkey for authentication.

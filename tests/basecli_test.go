3a53bc90e1ae771f542c941763f5bc93543f4fc1
Zach Ramsay <zach.ramsay@gmail.com>
2018-03-21 11:31:45 +0800

    go tests: basics working

--- tests/basecli_test.go
+++ tests/basecli_test.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	//"strings"
 	"testing"
 	"time"
 
@@ -15,8 +14,6 @@ import (
 
 // Tests assume the `basecoind` and `basecli` binaries
 // have been built and are located in `./build`
-
-// TODO remove test dirs if tests are successful
 
 var (
 	gopath = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "cosmos", "cosmos-sdk")
@@ -35,9 +32,13 @@ var (
 	bob      = ACCOUNTS[1]
 	charlie  = ACCOUNTS[2]
 	igor     = ACCOUNTS[3]
+
+	password = "some-random-password-doesnt-matter"
 )
 
 func TestMain(m *testing.M) {
+	// start by cleaning up because test dirs get left
+	// behind if tests are manually exited
 	cleanUp()
 
 	m.Run()
@@ -45,78 +46,72 @@ func TestMain(m *testing.M) {
 	cleanUp()
 }
 
-func TestInitBaseCoin(t *testing.T) {
-
+func TestInitBasecoin(t *testing.T) {
 	var err error
 
-	password := "some-random-password"
 	usePassword := exec.Command("echo", password)
 
 	initBasecoind := exec.Command(basecoindPath, "init", "--home", basecoindDir)
 
 	initBasecoind.Stdin, err = usePassword.StdoutPipe()
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	initBasecoind.Stdout = os.Stdout
 
-	if err := initBasecoind.Start(); err != nil {
-		t.Error(err)
-	}
-	if err := usePassword.Run(); err != nil {
-		t.Error(err)
-	}
-	if err := initBasecoind.Wait(); err != nil {
-		t.Error(err)
-	}
+	err = initBasecoind.Start()
+	assert.Nil(t, err)
 
-	if err := makeKeys(); err != nil {
-		t.Error(err)
-	}
+	err = usePassword.Run()
+	assert.Nil(t, err)
+
+	err = initBasecoind.Wait()
+	assert.Nil(t, err)
+
+	// left here as a sanity test
+	makeKeys(t)
 }
 
-func makeKeys() error {
+// identical to above test but doesn't make keys
+// and returns the address as string
+func initBasecoinServer(t *testing.T) string {
 	var err error
-	for _, acc := range ACCOUNTS {
-		pass := exec.Command("echo", "1234567890")
-		makeKeys := exec.Command(basecliPath, "keys", "add", acc, "--home", basecliDir)
 
-		makeKeys.Stdin, err = pass.StdoutPipe()
-		if err != nil {
-			return err
-		}
+	usePassword := exec.Command("echo", password)
 
-		makeKeys.Stdout = os.Stdout
-		if err := makeKeys.Start(); err != nil {
-			return err
-		}
-		if err := pass.Run(); err != nil {
-			return err
-		}
+	initBasecoind := exec.Command(basecoindPath, "init", "--home", basecoindDir)
 
-		if err := makeKeys.Wait(); err != nil {
-			return err
-		}
-	}
+	initBasecoind.Stdin, err = usePassword.StdoutPipe()
+	assert.Nil(t, err)
 
-	return nil
+	initBasecoind.Stdout = os.Stdout
+
+	err = initBasecoind.Start()
+	assert.Nil(t, err)
+
+	err = usePassword.Run()
+	assert.Nil(t, err)
+
+	err = initBasecoind.Wait()
+	assert.Nil(t, err)
+
+	// TODO get the address!
+
+	return ""
+
 }
 
-// these are in the original bash tests
-func TestBaseCliRecover(t *testing.T) {}
-func TestBaseCliShow(t *testing.T)    {}
-
+// TODO see https://github.com/cosmos/cosmos-sdk/issues/674
 func _TestSendCoins(t *testing.T) {
-	if err := startServer(); err != nil {
-		t.Error(err)
-	}
+	var err error
+
+	validatorAddress := initBasecoinServer(t)
+
+	err = startServer()
+	assert.Nil(t, err)
 
 	// send some coins
-	// [zr] where dafuq do I get a FROM (oh, use --name)
-
 	sendTo := fmt.Sprintf("--to=%s", bob)
-	sendFrom := fmt.Sprintf("--from=%s", alice)
+	sendFrom := fmt.Sprintf("--from=%s", validatorAddress)
 
 	cmdOut, err := exec.Command(basecliPath, "send", sendTo, "--amount=1000mycoin", sendFrom, "--seq=0").Output()
 	if err != nil {
@@ -127,7 +122,33 @@ func _TestSendCoins(t *testing.T) {
 
 }
 
-// expects TestInitBaseCoin to have been run
+func makeKeys(t *testing.T) {
+	var err error
+	for _, acc := range ACCOUNTS {
+		pass := exec.Command("echo", password)
+		makeKeys := exec.Command(basecliPath, "keys", "add", acc, "--home", basecliDir)
+
+		makeKeys.Stdin, err = pass.StdoutPipe()
+		assert.Nil(t, err)
+
+		makeKeys.Stdout = os.Stdout
+		err = makeKeys.Start()
+		assert.Nil(t, err)
+
+		err = pass.Run()
+		assert.Nil(t, err)
+
+		err = makeKeys.Wait()
+		assert.Nil(t, err)
+	}
+
+}
+
+// these are in the original bash tests
+func TestBaseCliRecover(t *testing.T) {}
+func TestBaseCliShow(t *testing.T)    {}
+
+// expects initBasecoinServer to have been run
 func startServer() error {
 	// straight outta https://nathanleclaire.com/blog/2014/12/29/shelled-out-commands-in-golang/
 	cmdName := basecoindPath

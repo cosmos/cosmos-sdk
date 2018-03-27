408127a7cb73be667f0f27fce9b09193ff60bf8f
rigelrozanski <rigel.rozanski@gmail.com>
2018-03-27 21:48:41 +0200

    sprintf and fix error codes

--- examples/basecoin/x/cool/errors.go
+++ examples/basecoin/x/cool/errors.go
@@ -1,15 +1,17 @@
 package cool
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 const (
-	// Cool module reserves error 300-399 lawl
-	CodeIncorrectCoolAnswer sdk.CodeType = 300
+	// Cool module reserves error 400-499 lawl
+	CodeIncorrectCoolAnswer sdk.CodeType = 400
 )
 
 // ErrIncorrectCoolAnswer - Error returned upon an incorrect guess
 func ErrIncorrectCoolAnswer(answer string) sdk.Error {
-	return sdk.NewError(CodeIncorrectCoolAnswer, "Incorrect cool answer")
+	return sdk.NewError(CodeIncorrectCoolAnswer, fmt.Sprintf("Incorrect cool answer: %v", answer))
 }

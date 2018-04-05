231e58aad50d97724591ebf4d9544d305a317825
Sunny Aggarwal <sunnya97@gmail.com>
2018-04-05 19:10:25 +0200

    in progress

--- x/gov/msgs.go
+++ x/gov/msgs.go
@@ -8,9 +8,9 @@ import (
 )
 
 //-----------------------------------------------------------
-// SubmitProposalMsg
+// MsgSubmitProposal
 
-type SubmitProposalMsg struct {
+type MsgSubmitProposal struct {
 	Title          string      //  Title of the proposal
 	Description    string      //  Description of the proposal
 	ProposalType   string      //  Type of proposal. Initial set {PlainTextProposal, SoftwareUpgradeProposal}
@@ -18,8 +18,8 @@ type SubmitProposalMsg struct {
 	InitialDeposit sdk.Coins   //  Initial deposit paid by sender. Must be strictly positive.
 }
 
-func NewSubmitProposalMsg(title string, description string, proposalType string, proposer sdk.Address, initialDeposit sdk.Coins) SubmitProposalMsg {
-	return SubmitProposalMsg{
+func NewMsgSubmitProposal(title string, description string, proposalType string, proposer sdk.Address, initialDeposit sdk.Coins) MsgSubmitProposal {
+	return MsgSubmitProposal{
 		Title:          title,
 		Description:    description,
 		ProposalType:   proposalType,
@@ -29,10 +29,10 @@ func NewSubmitProposalMsg(title string, description string, proposalType string,
 }
 
 // Implements Msg.
-func (msg SubmitProposalMsg) Type() string { return "gov" }
+func (msg MsgSubmitProposal) Type() string { return "gov" }
 
 // Implements Msg.
-func (msg SubmitProposalMsg) ValidateBasic() sdk.Error {
+func (msg MsgSubmitProposal) ValidateBasic() sdk.Error {
 
 	if len(msg.Title) == 0 {
 		return ErrInvalidTitle(msg.Title) // TODO: Proper Error
@@ -55,17 +55,17 @@ func (msg SubmitProposalMsg) ValidateBasic() sdk.Error {
 	return nil
 }
 
-func (msg SubmitProposalMsg) String() string {
-	return fmt.Sprintf("SubmitProposalMsg{%v, %v, %v, %v}", msg.Title, msg.Description, msg.ProposalType, msg.InitialDeposit)
+func (msg MsgSubmitProposal) String() string {
+	return fmt.Sprintf("MsgSubmitProposal{%v, %v, %v, %v}", msg.Title, msg.Description, msg.ProposalType, msg.InitialDeposit)
 }
 
 // Implements Msg.
-func (msg SubmitProposalMsg) Get(key interface{}) (value interface{}) {
+func (msg MsgSubmitProposal) Get(key interface{}) (value interface{}) {
 	return nil
 }
 
 // Implements Msg.
-func (msg SubmitProposalMsg) GetSignBytes() []byte {
+func (msg MsgSubmitProposal) GetSignBytes() []byte {
 	b, err := json.Marshal(msg) // XXX: ensure some canonical form
 	if err != nil {
 		panic(err)
@@ -74,21 +74,21 @@ func (msg SubmitProposalMsg) GetSignBytes() []byte {
 }
 
 // Implements Msg.
-func (msg SubmitProposalMsg) GetSigners() []sdk.Address {
+func (msg MsgSubmitProposal) GetSigners() []sdk.Address {
 	return []sdk.Address{msg.Proposer}
 }
 
 //-----------------------------------------------------------
-// DepositMsg
+// MsgDeposit
 
-type DepositMsg struct {
+type MsgDeposit struct {
 	ProposalID int64       `json:"proposal_id"` // ID of the proposal
 	Depositer  sdk.Address `json:"depositer"`   // Address of the depositer
 	Amount     sdk.Coins   `json:"amount"`      // Coins to add to the proposal's deposit
 }
 
-func NewDepositMsgMsg(proposalID int64, depositer sdk.Address, amount sdk.Coins) DepositMsg {
-	return DepositMsg{
+func NewMsgDeposit(proposalID int64, depositer sdk.Address, amount sdk.Coins) MsgDeposit {
+	return MsgDeposit{
 		ProposalID: proposalID,
 		Depositer:  depositer,
 		Amount:     amount,
@@ -96,10 +96,10 @@ func NewDepositMsgMsg(proposalID int64, depositer sdk.Address, amount sdk.Coins)
 }
 
 // Implements Msg.
-func (msg DepositMsg) Type() string { return "gov" }
+func (msg MsgDeposit) Type() string { return "gov" }
 
 // Implements Msg.
-func (msg DepositMsg) ValidateBasic() sdk.Error {
+func (msg MsgDeposit) ValidateBasic() sdk.Error {
 	if len(msg.Depositer) == 0 {
 		return sdk.ErrInvalidAddress(msg.Depositer.String())
 	}
@@ -112,17 +112,17 @@ func (msg DepositMsg) ValidateBasic() sdk.Error {
 	return nil
 }
 
-func (msg DepositMsg) String() string {
-	return fmt.Sprintf("DepositMsg{%v=>%v: %v}", msg.Depositer, msg.ProposalID, msg.Amount)
+func (msg MsgDeposit) String() string {
+	return fmt.Sprintf("MsgDeposit{%v=>%v: %v}", msg.Depositer, msg.ProposalID, msg.Amount)
 }
 
 // Implements Msg.
-func (msg DepositMsg) Get(key interface{}) (value interface{}) {
+func (msg MsgDeposit) Get(key interface{}) (value interface{}) {
 	return nil
 }
 
 // Implements Msg.
-func (msg DepositMsg) GetSignBytes() []byte {
+func (msg MsgDeposit) GetSignBytes() []byte {
 	b, err := json.Marshal(msg) // XXX: ensure some canonical form
 	if err != nil {
 		panic(err)
@@ -131,21 +131,21 @@ func (msg DepositMsg) GetSignBytes() []byte {
 }
 
 // Implements Msg.
-func (msg DepositMsg) GetSigners() []sdk.Address {
+func (msg MsgDeposit) GetSigners() []sdk.Address {
 	return []sdk.Address{msg.Depositer}
 }
 
 //-----------------------------------------------------------
-// VoteMsg
+// MsgVote
 
-type VoteMsg struct {
+type MsgVote struct {
 	Voter      sdk.Address //  address of the voter
 	ProposalID int64       //  proposalID of the proposal
 	Option     string      //  option from OptionSet chosen by the voter
 }
 
-func NewVoteMsg(voter sdk.Address, proposalID int64, option string) VoteMsg {
-	return VoteMsg{
+func NewMsgVote(voter sdk.Address, proposalID int64, option string) MsgVote {
+	return MsgVote{
 		Voter:      voter,
 		ProposalID: proposalID,
 		Option:     option,
@@ -153,10 +153,10 @@ func NewVoteMsg(voter sdk.Address, proposalID int64, option string) VoteMsg {
 }
 
 // Implements Msg.
-func (msg VoteMsg) Type() string { return "gov" }
+func (msg MsgVote) Type() string { return "gov" }
 
 // Implements Msg.
-func (msg VoteMsg) ValidateBasic() sdk.Error {
+func (msg MsgVote) ValidateBasic() sdk.Error {
 
 	if len(msg.Voter) == 0 {
 		return sdk.ErrInvalidAddress(msg.Voter.String())
@@ -167,17 +167,17 @@ func (msg VoteMsg) ValidateBasic() sdk.Error {
 	return nil
 }
 
-func (msg VoteMsg) String() string {
-	return fmt.Sprintf("VoteMsg{%v - %v}", msg.ProposalID, msg.Option)
+func (msg MsgVote) String() string {
+	return fmt.Sprintf("MsgVote{%v - %v}", msg.ProposalID, msg.Option)
 }
 
 // Implements Msg.
-func (msg VoteMsg) Get(key interface{}) (value interface{}) {
+func (msg MsgVote) Get(key interface{}) (value interface{}) {
 	return nil
 }
 
 // Implements Msg.
-func (msg VoteMsg) GetSignBytes() []byte {
+func (msg MsgVote) GetSignBytes() []byte {
 	b, err := json.Marshal(msg) // XXX: ensure some canonical form
 	if err != nil {
 		panic(err)
@@ -186,6 +186,6 @@ func (msg VoteMsg) GetSignBytes() []byte {
 }
 
 // Implements Msg.
-func (msg VoteMsg) GetSigners() []sdk.Address {
+func (msg MsgVote) GetSigners() []sdk.Address {
 	return []sdk.Address{msg.Voter}
 }

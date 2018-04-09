e52a00ad1967380b90a8cad8fb18017d86af0385
Sunny Aggarwal <sunnya97@gmail.com>
2018-04-09 15:02:03 +0200

    asdf

---
+++ x/bank/keeper_test.go
@@ -0,0 +1,51 @@
+package simplestake
+
+import (
+	"fmt"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	abci "github.com/tendermint/abci/types"
+	crypto "github.com/tendermint/go-crypto"
+	dbm "github.com/tendermint/tmlibs/db"
+
+	"github.com/cosmos/cosmos-sdk/store"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+)
+
+func setupMultiStore() (sdk.MultiStore, *sdk.KVStoreKey) {
+	db := dbm.NewMemDB()
+	authKey := sdk.NewKVStoreKey("authkey")
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(authKey, sdk.StoreTypeIAVL, db)
+	ms.LoadLatestVersion()
+	return ms, authKey
+}
+
+func TestCoinKeeper(t *testing.T) {
+	ms, authKey := setupMultiStore()
+
+	ctx := sdk.NewContext(ms, abci.Header{}, false, nil)
+	stakeKeeper := NewKeeper(capKey, bank.NewCoinKeeper(nil))
+	addr := sdk.Address([]byte("some-address"))
+
+	bi := stakeKeeper.getBondInfo(ctx, addr)
+	assert.Equal(t, bi, bondInfo{})
+
+	privKey := crypto.GenPrivKeyEd25519()
+
+	bi = bondInfo{
+		PubKey: privKey.PubKey(),
+		Power:  int64(10),
+	}
+	fmt.Printf("Pubkey: %v\n", privKey.PubKey())
+	stakeKeeper.setBondInfo(ctx, addr, bi)
+
+	savedBi := stakeKeeper.getBondInfo(ctx, addr)
+	assert.NotNil(t, savedBi)
+	fmt.Printf("Bond Info: %v\n", savedBi)
+	assert.Equal(t, int64(10), savedBi.Power)
+}

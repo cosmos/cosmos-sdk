69448f90e5f796146ce343241431bc5b96015e17
Sunny Aggarwal <sunnya97@gmail.com>
2018-04-10 01:32:04 +0200

    added just FF byte

--- types/store_test.go
+++ types/store_test.go
@@ -18,6 +18,7 @@ func TestPrefixEndBytes(t *testing.T) {
 		{[]byte{byte(55), byte(200), byte(255)}, []byte{byte(55), byte(201)}},
 		{[]byte{byte(55), byte(255), byte(255)}, []byte{byte(56)}},
 		{[]byte{byte(255), byte(255), byte(255)}, nil},
+		{[]byte{byte(255)}, nil},
 		{nil, nil},
 	}
 

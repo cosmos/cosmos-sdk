f275034830134a142149892049344e5c4a48b21a
Ethan Buchman <ethan@coinculture.info>
2018-04-09 20:40:16 +0300

    gaiacli is just basecli

--- cmd/gaiacli/main.go
+++ cmd/gaiacli/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"errors"
 	"os"
 
 	"github.com/spf13/cobra"
 
 	"github.com/tendermint/tmlibs/cli"
 
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/client/lcd"
+	"github.com/cosmos/cosmos-sdk/client/rpc"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+
 	"github.com/cosmos/cosmos-sdk/version"
+	authcmd "github.com/cosmos/cosmos-sdk/x/auth/commands"
+	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/commands"
+	ibccmd "github.com/cosmos/cosmos-sdk/x/ibc/commands"
+	simplestakingcmd "github.com/cosmos/cosmos-sdk/x/simplestake/commands"
+
+	"github.com/cosmos/cosmos-sdk/examples/basecoin/app"
+	"github.com/cosmos/cosmos-sdk/examples/basecoin/types"
 )
 
-const (
-	flagTo     = "to"
-	flagAmount = "amount"
-	flagFee    = "fee"
-)
+// TODO: distinguish from basecli
 
 // rootCmd is the entry point for this binary
 var (
@@ -23,55 +31,58 @@ var (
 		Use:   "gaiacli",
 		Short: "Gaia light-client",
 	}
-
-	lineBreak = &cobra.Command{Run: func(*cobra.Command, []string) {}}
-
-	getAccountCmd = &cobra.Command{
-		Use:   "account <address>",
-		Short: "Query account balance",
-		RunE:  todoNotImplemented,
-	}
 )
-
-func todoNotImplemented(_ *cobra.Command, _ []string) error {
-	return errors.New("TODO: Command not yet implemented")
-}
-
-func postSendCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "send",
-		Short: "Create and sign a send tx",
-		RunE:  todoNotImplemented,
-	}
-	cmd.Flags().String(flagTo, "", "Address to send coins")
-	cmd.Flags().String(flagAmount, "", "Amount of coins to send")
-	cmd.Flags().String(flagFee, "", "Fee to pay along with transaction")
-	return cmd
-}
 
 func main() {
 	// disable sorting
 	cobra.EnableCommandSorting = false
 
-	// generic client commands
-	AddClientCommands(rootCmd)
-	// query commands (custom to binary)
+	// get the codec
+	cdc := app.MakeCodec()
+
+	// TODO: setup keybase, viper object, etc. to be passed into
+	// the below functions and eliminate global vars, like we do
+	// with the cdc
+
+	// add standard rpc, and tx commands
+	rpc.AddCommands(rootCmd)
+	rootCmd.AddCommand(client.LineBreak)
+	tx.AddCommands(rootCmd, cdc)
+	rootCmd.AddCommand(client.LineBreak)
+
+	// add query/post commands (custom to binary)
 	rootCmd.AddCommand(
-		GetCommands(getAccountCmd)...)
-	// post tx commands (custom to binary)
+		client.GetCommands(
+			authcmd.GetAccountCmd("main", cdc, types.GetAccountDecoder(cdc)),
+		)...)
 	rootCmd.AddCommand(
-		PostCommands(postSendCommand())...)
+		client.PostCommands(
+			bankcmd.SendTxCmd(cdc),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			ibccmd.IBCTransferCmd(cdc),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			ibccmd.IBCRelayCmd(cdc),
+			simplestakingcmd.BondTxCmd(cdc),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			simplestakingcmd.UnbondTxCmd(cdc),
+		)...)
 
 	// add proxy, version and key info
 	rootCmd.AddCommand(
-		lineBreak,
-		serveCommand(),
-		KeyCommands(),
-		lineBreak,
+		client.LineBreak,
+		lcd.ServeCommand(cdc),
+		keys.Commands(),
+		client.LineBreak,
 		version.VersionCmd,
 	)
 
 	// prepare and add flags
-	executor := cli.PrepareBaseCmd(rootCmd, "GA", os.ExpandEnv("$HOME/.gaiacli"))
+	executor := cli.PrepareMainCmd(rootCmd, "BC", os.ExpandEnv("$HOME/.gaiacli"))
 	executor.Execute()
 }

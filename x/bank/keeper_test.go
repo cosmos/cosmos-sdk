c7e47f2a76c3571c6bca81b5ed709bcc15920e37
Sunny Aggarwal <sunnya97@gmail.com>
2018-04-09 20:21:26 +0200

    working

--- x/bank/keeper_test.go
+++ x/bank/keeper_test.go
@@ -1,18 +1,18 @@
-package simplestake
+package bank
 
 import (
-	"fmt"
-
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	abci "github.com/tendermint/abci/types"
-	crypto "github.com/tendermint/go-crypto"
 	dbm "github.com/tendermint/tmlibs/db"
 
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	oldwire "github.com/tendermint/go-wire"
+
+	"github.com/cosmos/cosmos-sdk/x/auth"
 )
 
 func setupMultiStore() (sdk.MultiStore, *sdk.KVStoreKey) {
@@ -27,37 +27,191 @@ func setupMultiStore() (sdk.MultiStore, *sdk.KVStoreKey) {
 func TestCoinKeeper(t *testing.T) {
 	ms, authKey := setupMultiStore()
 
+	// wire registration while we're at it ... TODO
+	var _ = oldwire.RegisterInterface(
+		struct{ sdk.Account }{},
+		oldwire.ConcreteType{&auth.BaseAccount{}, 0x1},
+	)
+
 	ctx := sdk.NewContext(ms, abci.Header{}, false, nil)
-	accountMapper := auth.NewMapper(authKey, &auth.BaseAccount{})
+	accountMapper := auth.NewAccountMapperSealed(authKey, &auth.BaseAccount{})
 	coinKeeper := NewCoinKeeper(accountMapper)
-	accountMapper.SetAccount(accountMapper.NewAccountWithAddress())
 
-	assert.Equal(t, sdk.Coins{}, coinKeeper, ...)
+	addr := sdk.Address([]byte("addr1"))
+	addr2 := sdk.Address([]byte("addr2"))
+	addr3 := sdk.Address([]byte("addr3"))
+	acc := accountMapper.NewAccountWithAddress(ctx, addr)
 
+	// Test GetCoins/SetCoins
+	accountMapper.SetAccount(ctx, acc)
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{}))
 
-	addr := sdk.Address([]byte("some-address"))
+	coinKeeper.SetCoins(ctx, addr, sdk.Coins{{"foocoin", 10}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
 
-	bi := stakeKeeper.getBondInfo(ctx, addr)
-	assert.Equal(t, bi, bondInfo{})
+	// Test HasCoins
+	assert.True(t, coinKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, coinKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 5}}))
+	assert.False(t, coinKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 15}}))
+	assert.False(t, coinKeeper.HasCoins(ctx, addr, sdk.Coins{{"barcoin", 5}}))
 
-	privKey := crypto.GenPrivKeyEd25519()
+	// Test AddCoins
+	coinKeeper.AddCoins(ctx, addr, sdk.Coins{{"foocoin", 15}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 25}}))
 
-	bi = bondInfo{
-		PubKey: privKey.PubKey(),
-		Power:  int64(10),
+	coinKeeper.AddCoins(ctx, addr, sdk.Coins{{"barcoin", 15}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 15}, {"foocoin", 25}}))
+
+	// Test SubtractCoins
+	coinKeeper.SubtractCoins(ctx, addr, sdk.Coins{{"foocoin", 10}})
+	coinKeeper.SubtractCoins(ctx, addr, sdk.Coins{{"barcoin", 5}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 10}, {"foocoin", 15}}))
+
+	_, err := coinKeeper.SubtractCoins(ctx, addr, sdk.Coins{{"barcoin", 11}})
+	assert.Implements(t, (*sdk.Error)(nil), err)
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 10}, {"foocoin", 15}}))
+
+	coinKeeper.SubtractCoins(ctx, addr, sdk.Coins{{"barcoin", 10}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 15}}))
+	assert.False(t, coinKeeper.HasCoins(ctx, addr, sdk.Coins{{"barcoin", 1}}))
+
+	// Test SendCoins
+	coinKeeper.SendCoins(ctx, addr, addr2, sdk.Coins{{"foocoin", 5}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, coinKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"foocoin", 5}}))
+
+	err2 := coinKeeper.SendCoins(ctx, addr, addr2, sdk.Coins{{"foocoin", 50}})
+	assert.Implements(t, (*sdk.Error)(nil), err2)
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, coinKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"foocoin", 5}}))
+
+	coinKeeper.AddCoins(ctx, addr, sdk.Coins{{"barcoin", 30}})
+	coinKeeper.SendCoins(ctx, addr, addr2, sdk.Coins{{"barcoin", 10}, {"foocoin", 5}})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 20}, {"foocoin", 5}}))
+	assert.True(t, coinKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"barcoin", 10}, {"foocoin", 10}}))
+
+	// Test InputOutputCoins
+	input1 := NewInput(addr2, sdk.Coins{{"foocoin", 2}})
+	output1 := NewOutput(addr, sdk.Coins{{"foocoin", 2}})
+	coinKeeper.InputOutputCoins(ctx, []Input{input1}, []Output{output1})
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 20}, {"foocoin", 7}}))
+	assert.True(t, coinKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"barcoin", 10}, {"foocoin", 8}}))
+
+	inputs := []Input{
+		NewInput(addr, sdk.Coins{{"foocoin", 3}}),
+		NewInput(addr2, sdk.Coins{{"barcoin", 3}, {"foocoin", 2}}),
 	}
-	fmt.Printf("Pubkey: %v\n", privKey.PubKey())
-	stakeKeeper.setBondInfo(ctx, addr, bi)
 
-	savedBi := stakeKeeper.getBondInfo(ctx, addr)
-	assert.NotNil(t, savedBi)
-	fmt.Printf("Bond Info: %v\n", savedBi)
-	assert.Equal(t, int64(10), savedBi.Power)
+	outputs := []Output{
+		NewOutput(addr, sdk.Coins{{"barcoin", 1}}),
+		NewOutput(addr3, sdk.Coins{{"barcoin", 2}, {"foocoin", 5}}),
+	}
+	coinKeeper.InputOutputCoins(ctx, inputs, outputs)
+	assert.True(t, coinKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 21}, {"foocoin", 4}}))
+	assert.True(t, coinKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"barcoin", 7}, {"foocoin", 6}}))
+	assert.True(t, coinKeeper.GetCoins(ctx, addr3).IsEqual(sdk.Coins{{"barcoin", 2}, {"foocoin", 5}}))
+
 }
 
-func GenerateKeys() (crypto.PrivKey, crypto.PubKey, sdk.Address) {
-	privKey := crypto.GenPrivKeyEd25519()
-	pubKey := privKey.PubKey()
-	addr := pubKey.Address()
-	return (privKey, pubKey, addr)
+func TestSendKeeper(t *testing.T) {
+	ms, authKey := setupMultiStore()
+
+	// wire registration while we're at it ... TODO
+	var _ = oldwire.RegisterInterface(
+		struct{ sdk.Account }{},
+		oldwire.ConcreteType{&auth.BaseAccount{}, 0x1},
+	)
+
+	ctx := sdk.NewContext(ms, abci.Header{}, false, nil)
+	accountMapper := auth.NewAccountMapperSealed(authKey, &auth.BaseAccount{})
+	coinKeeper := NewCoinKeeper(accountMapper)
+	sendKeeper := NewSendKeeper(accountMapper)
+
+	addr := sdk.Address([]byte("addr1"))
+	addr2 := sdk.Address([]byte("addr2"))
+	addr3 := sdk.Address([]byte("addr3"))
+	acc := accountMapper.NewAccountWithAddress(ctx, addr)
+
+	// Test GetCoins/SetCoins
+	accountMapper.SetAccount(ctx, acc)
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{}))
+
+	coinKeeper.SetCoins(ctx, addr, sdk.Coins{{"foocoin", 10}})
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
+
+	// Test HasCoins
+	assert.True(t, sendKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, sendKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 5}}))
+	assert.False(t, sendKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 15}}))
+	assert.False(t, sendKeeper.HasCoins(ctx, addr, sdk.Coins{{"barcoin", 5}}))
+
+	coinKeeper.SetCoins(ctx, addr, sdk.Coins{{"foocoin", 15}})
+
+	// Test SendCoins
+	sendKeeper.SendCoins(ctx, addr, addr2, sdk.Coins{{"foocoin", 5}})
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, sendKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"foocoin", 5}}))
+
+	err2 := sendKeeper.SendCoins(ctx, addr, addr2, sdk.Coins{{"foocoin", 50}})
+	assert.Implements(t, (*sdk.Error)(nil), err2)
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, sendKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"foocoin", 5}}))
+
+	coinKeeper.AddCoins(ctx, addr, sdk.Coins{{"barcoin", 30}})
+	sendKeeper.SendCoins(ctx, addr, addr2, sdk.Coins{{"barcoin", 10}, {"foocoin", 5}})
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 20}, {"foocoin", 5}}))
+	assert.True(t, sendKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"barcoin", 10}, {"foocoin", 10}}))
+
+	// Test InputOutputCoins
+	input1 := NewInput(addr2, sdk.Coins{{"foocoin", 2}})
+	output1 := NewOutput(addr, sdk.Coins{{"foocoin", 2}})
+	sendKeeper.InputOutputCoins(ctx, []Input{input1}, []Output{output1})
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 20}, {"foocoin", 7}}))
+	assert.True(t, sendKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"barcoin", 10}, {"foocoin", 8}}))
+
+	inputs := []Input{
+		NewInput(addr, sdk.Coins{{"foocoin", 3}}),
+		NewInput(addr2, sdk.Coins{{"barcoin", 3}, {"foocoin", 2}}),
+	}
+
+	outputs := []Output{
+		NewOutput(addr, sdk.Coins{{"barcoin", 1}}),
+		NewOutput(addr3, sdk.Coins{{"barcoin", 2}, {"foocoin", 5}}),
+	}
+	sendKeeper.InputOutputCoins(ctx, inputs, outputs)
+	assert.True(t, sendKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"barcoin", 21}, {"foocoin", 4}}))
+	assert.True(t, sendKeeper.GetCoins(ctx, addr2).IsEqual(sdk.Coins{{"barcoin", 7}, {"foocoin", 6}}))
+	assert.True(t, sendKeeper.GetCoins(ctx, addr3).IsEqual(sdk.Coins{{"barcoin", 2}, {"foocoin", 5}}))
+
+}
+
+func TestViewKeeper(t *testing.T) {
+	ms, authKey := setupMultiStore()
+
+	// wire registration while we're at it ... TODO
+	var _ = oldwire.RegisterInterface(
+		struct{ sdk.Account }{},
+		oldwire.ConcreteType{&auth.BaseAccount{}, 0x1},
+	)
+
+	ctx := sdk.NewContext(ms, abci.Header{}, false, nil)
+	accountMapper := auth.NewAccountMapperSealed(authKey, &auth.BaseAccount{})
+	coinKeeper := NewCoinKeeper(accountMapper)
+	viewKeeper := NewViewKeeper(accountMapper)
+
+	addr := sdk.Address([]byte("addr1"))
+	acc := accountMapper.NewAccountWithAddress(ctx, addr)
+
+	// Test GetCoins/SetCoins
+	accountMapper.SetAccount(ctx, acc)
+	assert.True(t, viewKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{}))
+
+	coinKeeper.SetCoins(ctx, addr, sdk.Coins{{"foocoin", 10}})
+	assert.True(t, viewKeeper.GetCoins(ctx, addr).IsEqual(sdk.Coins{{"foocoin", 10}}))
+
+	// Test HasCoins
+	assert.True(t, viewKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 10}}))
+	assert.True(t, viewKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 5}}))
+	assert.False(t, viewKeeper.HasCoins(ctx, addr, sdk.Coins{{"foocoin", 15}}))
+	assert.False(t, viewKeeper.HasCoins(ctx, addr, sdk.Coins{{"barcoin", 5}}))
 }

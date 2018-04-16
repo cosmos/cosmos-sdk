b9e52f4a5019b5e99f1a611eacba85debd198bae
Christopher Goes <cwgoes@protonmail.ch>
2018-04-16 15:12:20 +0200

    Add RegisterOrPanic testcase

--- types/codespacer_test.go
+++ types/codespacer_test.go
@@ -32,3 +32,16 @@ func TestRegisterNext(t *testing.T) {
 	codespacer.RegisterNext(MaximumCodespace - 1)
 	codespacer.RegisterNext(MaximumCodespace - 1)
 }
+
+func TestRegisterOrPanic(t *testing.T) {
+	codespacer := NewCodespacer()
+	// unregistered, allow
+	code1 := codespacer.RegisterNext(CodespaceType(2))
+	require.Equal(t, code1, CodespaceType(2))
+	// panic on duplicate
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "Did not panic on duplicate codespace")
+	}()
+	codespacer.RegisterOrPanic(CodespaceType(2))
+}

c2636c3c6b95cc314e6a7ddc079daf236a9a1a5a
Ethan Buchman <ethan@coinculture.info>
2018-05-22 13:04:42 -0400

    tmhash: add Sum function

--- tmhash/hash.go
+++ tmhash/hash.go
@@ -34,8 +34,15 @@ func (h sha256trunc) BlockSize() int {
 	return h.sha256.BlockSize()
 }
 
+// New returns a new hash.Hash.
 func New() hash.Hash {
 	return sha256trunc{
 		sha256: sha256.New(),
 	}
+}
+
+// Sum returns the first 20 bytes of SHA256 of the bz.
+func Sum(bz []byte) []byte {
+	hash := sha256.Sum256(bz)
+	return hash[:Size]
 }

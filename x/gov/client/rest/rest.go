258fc3dd57ebeded4d7a6d77ef769114674428f9
zhangzhiqiang <745124335@aa.com>
2018-05-29 19:00:47 +0800

    add query proposal command

--- x/gov/client/rest/rest.go
+++ x/gov/client/rest/rest.go
@@ -9,6 +9,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/tendermint/go-crypto/keys"
 	"net/http"
+	"github.com/pkg/errors"
+	"strconv"
 )
 
 // RegisterRoutes - Central function to define routes that get registered by the main application
@@ -16,6 +18,7 @@ func RegisterRoutes(ctx context.CoreContext, r *mux.Router, cdc *wire.Codec, kb 
 	r.HandleFunc("/gov/proposal", postProposalHandlerFn(cdc, kb, ctx)).Methods("POST")
 	r.HandleFunc("/gov/deposit", depositHandlerFn(cdc, kb, ctx)).Methods("POST")
 	r.HandleFunc("/gov/vote", voteHandlerFn(cdc, kb, ctx)).Methods("POST")
+	r.HandleFunc("/gov/{proposalId}/proposal", queryProposalHandlerFn(gov.MsgType,cdc, kb, ctx)).Methods("GET")
 }
 
 func postProposalHandlerFn(cdc *wire.Codec, kb keys.Keybase, ctx context.CoreContext) http.HandlerFunc {
@@ -98,5 +101,45 @@ func voteHandlerFn(cdc *wire.Codec, kb keys.Keybase, ctx context.CoreContext) ht
 		msg := gov.NewMsgVote(voter, req.ProposalID, req.Option)
 		// sign
 		signAndBuild(w, ctx, req.BaseReq, msg, cdc)
+	}
+}
+func queryProposalHandlerFn(storeName string,cdc *wire.Codec, kb keys.Keybase, ctx context.CoreContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		proposalId := vars["proposalId"]
+
+		if len(proposalId) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			err := errors.Errorf("proposalId required but not specified")
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		id, err := strconv.ParseInt(proposalId, 10, 64)
+		if err != nil {
+			err := errors.Errorf("proposalID [%s] is not positive", proposalId)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		ctx := context.NewCoreContextFromViper()
+
+		key, _ := cdc.MarshalBinary(id)
+		res, err := ctx.Query(key, storeName)
+		if len(res) == 0 || err != nil {
+			err := errors.Errorf("proposalID [%d] is not existed", proposalId)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		proposal := new(gov.Proposal)
+		cdc.MustUnmarshalBinary(res, proposal)
+		output, err := wire.MarshalJSONIndent(cdc, proposal)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(output)
 	}
 }

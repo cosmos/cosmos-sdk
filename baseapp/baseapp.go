b6ab3b66b5e4ecf66f7833205d6bb529faa85c8f
Dev Ojha <ValarDragon@users.noreply.github.com>
2018-06-25 11:23:17 -0700

    Merge PR #1358: Fix typos and gofmt files
    * Fix typos
    * gofmt -s files
    * Add mispellings and gofmt checks to circle CI
    * circleci: Install misspell in the linting step

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -179,7 +179,7 @@ func (app *BaseApp) LastCommitID() sdk.CommitID {
 	return app.cms.LastCommitID()
 }
 
-// the last commited block height
+// the last committed block height
 func (app *BaseApp) LastBlockHeight() int64 {
 	return app.cms.LastCommitID().Version
 }

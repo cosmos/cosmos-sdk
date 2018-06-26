a8de328ef6cd3de0101c2af42d7db4c47126763c
ValarDragon <dojha12@gmail.com>
2018-06-25 18:25:26 -0700

    types/rat: Fix overflowing in printing
    This now uses the underlying golang big.rat's string function,
    instead of casting to num and den which are int64s.
    
    Closes #1258

--- types/rational.go
+++ types/rational.go
@@ -115,7 +115,7 @@ func (r Rat) Mul(r2 Rat) Rat    { return Rat{*new(big.Rat).Mul(&(r.Rat), &(r2.Ra
 func (r Rat) Quo(r2 Rat) Rat    { return Rat{*new(big.Rat).Quo(&(r.Rat), &(r2.Rat))} } // Quo - quotient
 func (r Rat) Add(r2 Rat) Rat    { return Rat{*new(big.Rat).Add(&(r.Rat), &(r2.Rat))} } // Add - addition
 func (r Rat) Sub(r2 Rat) Rat    { return Rat{*new(big.Rat).Sub(&(r.Rat), &(r2.Rat))} } // Sub - subtraction
-func (r Rat) String() string    { return fmt.Sprintf("%v/%v", r.Num(), r.Denom()) }
+func (r Rat) String() string    { return r.Rat.String() }
 
 var (
 	zero  = big.NewInt(0)

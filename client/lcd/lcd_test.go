3d2834ac4c065d641c1fe1c88156f0cfcb7c56b4
Christopher Goes <cwgoes@pluranimity.org>
2018-06-30 05:34:55 +0200

    Merge PR #1278: Slashing v2
    Implement semifinal Gaia slashing spec (#1263), less #1348, #1378, and #1440 which are TBD.

--- client/lcd/lcd_test.go
+++ client/lcd/lcd_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/gov"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
 	"github.com/cosmos/cosmos-sdk/x/stake"
 	stakerest "github.com/cosmos/cosmos-sdk/x/stake/client/rest"
 )
@@ -521,6 +522,19 @@ func TestVote(t *testing.T) {
 	require.Equal(t, gov.VoteOptionToString(gov.OptionYes), vote.Option)
 }
 
+func TestUnrevoke(t *testing.T) {
+	_, password := "test", "1234567890"
+	addr, _ := CreateAddr(t, "test", password, GetKB(t))
+	cleanup, pks, port := InitializeTestLCD(t, 1, []sdk.Address{addr})
+	defer cleanup()
+
+	signingInfo := getSigningInfo(t, port, pks[0].Address())
+	tests.WaitForHeight(4, port)
+	require.Equal(t, true, signingInfo.IndexOffset > 0)
+	require.Equal(t, int64(0), signingInfo.JailedUntil)
+	require.Equal(t, true, signingInfo.SignedBlocksCounter > 0)
+}
+
 func TestProposalsQuery(t *testing.T) {
 	name, password1 := "test", "1234567890"
 	name2, password2 := "test2", "1234567890"
@@ -677,6 +691,16 @@ func doIBCTransfer(t *testing.T, port, seed, name, password string, addr sdk.Add
 	require.Nil(t, err)
 
 	return resultTx
+}
+
+func getSigningInfo(t *testing.T, port string, validatorAddr sdk.Address) slashing.ValidatorSigningInfo {
+	validatorAddrBech := sdk.MustBech32ifyVal(validatorAddr)
+	res, body := Request(t, port, "GET", "/slashing/signing_info/"+validatorAddrBech, nil)
+	require.Equal(t, http.StatusOK, res.StatusCode, body)
+	var signingInfo slashing.ValidatorSigningInfo
+	err := cdc.UnmarshalJSON([]byte(body), &signingInfo)
+	require.Nil(t, err)
+	return signingInfo
 }
 
 func getDelegation(t *testing.T, port string, delegatorAddr, validatorAddr sdk.Address) stake.Delegation {

7e3576657317d95a28f56e46698c277a4ddd9eb3
ValarDragon <dojha12@gmail.com>
2018-07-04 09:24:15 -0700

    Export TestAndRunTx

--- x/bank/app_test.go
+++ x/bank/app_test.go
@@ -1,13 +1,10 @@
 package bank
 
 import (
-	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"math/big"
 	"math/rand"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -94,106 +91,11 @@ func TestBankWithRandomMessages(t *testing.T) {
 
 	mapp.RandomizedTesting(
 		t,
-		[]mock.TestAndRunTx{randSingleSendTx},
+		[]mock.TestAndRunTx{TestAndRunSingleInputMsgSend},
 		[]mock.RandSetup{setup},
 		[]mock.Invariant{ModuleInvariants, mock.AuthInvariant},
 		100, 30, 30,
 	)
-}
-
-// Send a random "Send" Transaction from two already existing accounts
-func randSingleSendTx(t *testing.T, r *rand.Rand, app *mock.App, ctx sdk.Context, keys []crypto.PrivKey, log string) (action string, err sdk.Error) {
-	fromKey := keys[r.Intn(len(keys))]
-	fromAddr := fromKey.PubKey().Address()
-	toKey := keys[r.Intn(len(keys))]
-	// Disallow sending money to yourself
-	for {
-		if !fromKey.Equals(toKey) {
-			break
-		}
-		toKey = keys[r.Intn(len(keys))]
-	}
-	toAddr := toKey.PubKey().Address()
-	initFromAcc := app.AccountMapper.GetAccount(ctx, fromAddr)
-	initFromCoins := initFromAcc.GetCoins()
-
-	denomIndex := r.Intn(len(initFromCoins))
-	amt, goErr := randPositiveInt(r, initFromCoins[denomIndex].Amount)
-	if goErr != nil {
-		return "skipping bank send due to account having no coins of denomination " + initFromCoins[denomIndex].Denom, nil
-	}
-
-	action = fmt.Sprintf("%s is sending %s %s to %s",
-		fromAddr.String(),
-		amt.String(),
-		initFromCoins[denomIndex].Denom,
-		toAddr.String(),
-	)
-	log = fmt.Sprintf("%s\n%s", log, action)
-
-	coins := sdk.Coins{{initFromCoins[denomIndex].Denom, amt}}
-	var msg = MsgSend{
-		Inputs:  []Input{NewInput(initFromAcc.GetAddress(), coins)},
-		Outputs: []Output{NewOutput(toAddr, coins)},
-	}
-	sendAndVerifyMsgSend(t, app, msg, ctx, log, []crypto.PrivKey{fromKey})
-
-	return action, nil
-}
-
-// Note this fails if there are repeated inputs or outputs
-func sendAndVerifyMsgSend(t *testing.T, app *mock.App, msg MsgSend, ctx sdk.Context, log string, privkeys []crypto.PrivKey) {
-	initialInputAddrCoins := make([]sdk.Coins, len(msg.Inputs))
-	initialOutputAddrCoins := make([]sdk.Coins, len(msg.Outputs))
-	AccountNumbers := make([]int64, len(msg.Inputs))
-	SequenceNumbers := make([]int64, len(msg.Inputs))
-
-	for i := 0; i < len(msg.Inputs); i++ {
-		acc := app.AccountMapper.GetAccount(ctx, msg.Inputs[i].Address)
-		AccountNumbers[i] = acc.GetAccountNumber()
-		SequenceNumbers[i] = acc.GetSequence()
-		initialInputAddrCoins[i] = acc.GetCoins()
-	}
-	for i := 0; i < len(msg.Outputs); i++ {
-		acc := app.AccountMapper.GetAccount(ctx, msg.Outputs[i].Address)
-		initialOutputAddrCoins[i] = acc.GetCoins()
-	}
-	tx := mock.GenTx([]sdk.Msg{msg},
-		AccountNumbers,
-		SequenceNumbers,
-		privkeys...)
-	res := app.Deliver(tx)
-	if !res.IsOK() {
-		// TODO: Do this in a more 'canonical' way
-		fmt.Println(res)
-		fmt.Println(log)
-		t.FailNow()
-	}
-
-	for i := 0; i < len(msg.Inputs); i++ {
-		terminalInputCoins := app.AccountMapper.GetAccount(ctx, msg.Inputs[i].Address).GetCoins()
-		require.Equal(t,
-			initialInputAddrCoins[i].Minus(msg.Inputs[i].Coins),
-			terminalInputCoins,
-			fmt.Sprintf("Input #%d had an incorrect amount of coins\n%s", i, log),
-		)
-	}
-	for i := 0; i < len(msg.Outputs); i++ {
-		terminalOutputCoins := app.AccountMapper.GetAccount(ctx, msg.Outputs[i].Address).GetCoins()
-		require.Equal(t,
-			initialOutputAddrCoins[i].Plus(msg.Outputs[i].Coins),
-			terminalOutputCoins,
-			fmt.Sprintf("Output #%d had an incorrect amount of coins\n%s", i, log),
-		)
-	}
-}
-
-func randPositiveInt(r *rand.Rand, max sdk.Int) (sdk.Int, error) {
-	if !max.GT(sdk.OneInt()) {
-		return sdk.Int{}, errors.New("max too small")
-	}
-	max = max.Sub(sdk.OneInt())
-	return sdk.NewIntFromBigInt(new(big.Int).Rand(r, max.BigInt())).Add(sdk.OneInt()), nil
 }
 
 func TestMsgSendWithAccounts(t *testing.T) {

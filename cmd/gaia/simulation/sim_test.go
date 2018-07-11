f9f326cefb18cb0fa9689121a8bc6890616ffe18
Christopher Goes <cwgoes@pluranimity.org>
2018-07-12 00:14:37 +0200

    Move files

---
+++ cmd/gaia/simulation/sim_test.go
@@ -0,0 +1,11 @@
+package simulation
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFullGaiaSimulation(t *testing.T) {
+	require.True(t, true, "should not happen")
+}

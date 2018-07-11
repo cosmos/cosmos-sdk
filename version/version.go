7b2ede2467d5d778bca5d2a9f645c2bc16ef4fcd
Ethan Buchman <ethan@coinculture.info>
2018-07-11 11:06:17 -0400

    changelog and version

--- version/version.go
+++ version/version.go
@@ -3,9 +3,9 @@ package version
 
 const Maj = "0"
 const Min = "20"
-const Fix = "0"
+const Fix = "1"
 
-const Version = "0.20.0"
+const Version = "0.20.1"
 
 // GitCommit set by build flags
 var GitCommit = ""

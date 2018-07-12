a0ab1d61964519a9a94c2c62ac80a0e04606cdeb
rigelrozanski <rigel.rozanski@gmail.com>
2018-07-12 17:22:21 -0400

    move inflation to pool type

--- x/stake/types/pool.go
+++ x/stake/types/pool.go
@@ -75,3 +75,44 @@ func (p Pool) removeBondedTokens(bondedTokens sdk.Rat) Pool {
 	}
 	return p
 }
+
+//_______________________________________________________________________
+// Inflation
+
+const precision = 100000000000     // increased to this precision for accuracy
+var hrsPerYrRat = sdk.NewRat(8766) // as defined by a julian year of 365.25 days
+
+// process provisions for an hour period
+func (p Pool) ProcessProvisions(params Params) Pool {
+	p.Inflation = p.NextInflation(params)
+	provisions := p.Inflation.Mul(p.TokenSupply()).Quo(hrsPerYrRat)
+
+	// TODO add to the fees provisions
+	p.LooseTokens = p.LooseTokens.Add(provisions)
+	return p
+}
+
+// get the next inflation rate for the hour
+func (p Pool) NextInflation(params Params) (inflation sdk.Rat) {
+
+	// The target annual inflation rate is recalculated for each previsions cycle. The
+	// inflation is also subject to a rate change (positive or negative) depending on
+	// the distance from the desired ratio (67%). The maximum rate change possible is
+	// defined to be 13% per year, however the annual inflation is capped as between
+	// 7% and 20%.
+
+	// (1 - bondedRatio/GoalBonded) * InflationRateChange
+	inflationRateChangePerYear := sdk.OneRat().Sub(p.BondedRatio().Quo(params.GoalBonded)).Mul(params.InflationRateChange)
+	inflationRateChange := inflationRateChangePerYear.Quo(hrsPerYrRat)
+
+	// increase the new annual inflation for this next cycle
+	inflation = p.Inflation.Add(inflationRateChange)
+	if inflation.GT(params.InflationMax) {
+		inflation = params.InflationMax
+	}
+	if inflation.LT(params.InflationMin) {
+		inflation = params.InflationMin
+	}
+
+	return inflation.Round(precision)
+}

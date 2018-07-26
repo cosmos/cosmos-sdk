4f56572c19df1a604e1929e06ac334f6de2a924a
Christopher Goes <cwgoes@pluranimity.org>
2018-07-27 01:08:55 +0200

    Tendermint 0.22.7; bump patch version

--- version/version.go
+++ version/version.go
@@ -3,9 +3,9 @@ package version
 
 const Maj = "0"
 const Min = "23"
-const Fix = "0"
+const Fix = "1"
 
-const Version = "0.23.0"
+const Version = "0.23.1"
 
 // GitCommit set by build flags
 var GitCommit = ""

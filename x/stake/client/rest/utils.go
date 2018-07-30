3a26b4681e387126acafcd56c7b9a626d4b34e17
Federico Kunze <fekunze@berkeley.edu>
2018-07-30 18:53:59 +0200

    Revert prev commit

--- x/stake/client/rest/utils.go
+++ x/stake/client/rest/utils.go
@@ -5,11 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
-	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/stake/types"
-	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/libs/common"
-	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
 // Contains checks if the a given query contains one of the tx types
@@ -20,51 +16,6 @@ func contains(stringSlice []string, txType string) bool {
 		}
 	}
 	return false
-}
-
-func formatTxResults(cdc *wire.Codec, res []*ctypes.ResultTx) ([]txInfo, error) {
-	var err error
-	out := make([]txInfo, len(res))
-	for i := range res {
-		out[i], err = formatTxResult(cdc, res[i])
-		if err != nil {
-			return nil, err
-		}
-	}
-	return out, nil
-}
-
-func formatTxResult(cdc *wire.Codec, res *ctypes.ResultTx) (txInfo, error) {
-	// TODO: verify the proof if requested
-	tx, err := parseTx(cdc, res.Tx)
-	if err != nil {
-		return txInfo{}, err
-	}
-
-	info := txInfo{
-		Hash:   res.Hash,
-		Height: res.Height,
-		Tx:     tx,
-		Result: res.TxResult,
-	}
-	return info, nil
-}
-
-// txInfo is used to prepare info to display
-type txInfo struct {
-	Hash   common.HexBytes        `json:"hash"`
-	Height int64                  `json:"height"`
-	Tx     sdk.Tx                 `json:"tx"`
-	Result abci.ResponseDeliverTx `json:"result"`
-}
-
-func parseTx(cdc *wire.Codec, txBytes []byte) (sdk.Tx, error) {
-	var tx auth.StdTx
-	err := cdc.UnmarshalBinary(txBytes, &tx)
-	if err != nil {
-		return nil, err
-	}
-	return tx, nil
 }
 
 // get all Validators

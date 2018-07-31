bf28f568ad460348f1a7cc7cc0c185d13d0e28a4
Christopher Goes <cwgoes@pluranimity.org>
2018-07-31 02:01:03 +0200

    Tiny fix after merge

--- x/gov/endblocker_test.go
+++ x/gov/endblocker_test.go
@@ -183,7 +183,7 @@ func TestSlashing(t *testing.T) {
 	val1Initial := keeper.ds.GetValidatorSet().Validator(ctx, addrs[1]).GetPower().Quo(initTotalPower)
 	val2Initial := keeper.ds.GetValidatorSet().Validator(ctx, addrs[2]).GetPower().Quo(initTotalPower)
 
-	newProposalMsg := NewMsgSubmitProposal("Test", "test", ProposalTypeText, addrs[0], sdk.Coins{sdk.NewCoin("steak", 15)})
+	newProposalMsg := NewMsgSubmitProposal("Test", "test", ProposalTypeText, addrs[0], sdk.Coins{sdk.NewInt64Coin("steak", 15)})
 
 	res := govHandler(ctx, newProposalMsg)
 	require.True(t, res.IsOK())

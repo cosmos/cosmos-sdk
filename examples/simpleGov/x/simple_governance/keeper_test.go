425961869ef446b09265660a395d556a45fff67b
Federico Kunze <fekunze@berkeley.edu>
2018-08-07 16:54:21 +0200

    Tests fail, only gets last proposal

--- examples/simpleGov/x/simple_governance/keeper_test.go
+++ examples/simpleGov/x/simple_governance/keeper_test.go
@@ -3,7 +3,6 @@ package simpleGovernance
 import (
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
@@ -22,7 +21,20 @@ func TestSimpleGovKeeper(t *testing.T) {
 	// assert.NotNil(t, err)
 
 	// create proposals
-	proposal := k.NewProposal(ctx, titles[0], descriptions[0])
+	var proposals []Proposal
+	for i := 0; i < len(titles); i++ {
+		proposal := k.NewProposal(ctx, titles[i], descriptions[i])
+		proposals = append(proposals, proposal)
+	}
+
+	// Case 1: valid request
+	for idx := 0; idx < len(titles); idx++ {
+		res, err := k.GetProposal(ctx, proposals[idx].ID)
+		assert.NotEqual(t, Proposal{}, res)
+		assert.Nil(t, err)
+		assert.Equal(t, proposals[idx], res)
+	}
+
 	// TODO Error when defined proposal 2 is defined here
 	// proposal2 := k.NewProposal(ctx, titles[1], descriptions[1])
 
@@ -32,17 +44,10 @@ func TestSimpleGovKeeper(t *testing.T) {
 
 	// ––––––– Test GetProposal –––––––
 
-	// Case 1: valid request
-	res, err := k.GetProposal(ctx, proposal.ID)
-	assert.NotEqual(t, Proposal{}, res)
-	assert.Nil(t, err)
-	assert.Equal(t, proposal, res)
-
-	proposal2 := k.NewProposal(ctx, titles[1], descriptions[1])
-
 	// Case 2: invalid proposalID
-	res, err = k.GetProposal(ctx, 4)
+	res, err := k.GetProposal(ctx, int64(len(titles)+10))
 	assert.NotNil(t, err)
+	assert.Equal(t, Proposal{}, res)
 
 	k.SetVote(ctx, 1, addrs[2], options[1])
 
@@ -72,73 +77,73 @@ func TestSimpleGovKeeper(t *testing.T) {
 	//             KEEPER READ
 	// –––––––––––––––––––––––––––––––––––––––
 
-	simpleGovKey := sdk.NewKVStoreKey("simpleGov")
-	keeperRead := NewKeeperRead(simpleGovKey, k.ck, k.sm, DefaultCodespace)
-
-	// ––––––– Test GetProposal –––––––
-
-	// Case 1: valid request
-	res, err = keeperRead.GetProposal(ctx, proposal.ID)
-	assert.NotNil(t, res)
-	assert.Nil(t, err)
-	assert.Equal(t, proposal, res)
-
-	// Case 2: invalid proposalID
-	res, err = keeperRead.GetProposal(ctx, 10)
-	assert.NotNil(t, err)
-
-	// ––––––– Test SetProposal –––––––
-
-	err = keeperRead.SetProposal(ctx, proposal2) // Error Unauthorized
-	assert.NotNil(t, err)
-
-	// ––––––– Test GetVote –––––––
-
-	// Case 1: existing proposal, valid voter
-	option, err = keeperRead.GetVote(ctx, 1, addrs[2])
-	assert.Equal(t, options[1], option)
-	assert.Nil(t, err)
-
-	// Case 2: existing proposal, invalid voter
-	option, err = keeperRead.GetVote(ctx, 1, addrs[3]) // existing proposal, invalid voter
-	assert.Equal(t, "", option)
-	assert.NotNil(t, err)
-
-	// Case 3: invalid proposal, valid voter
-	option, err = keeperRead.GetVote(ctx, 2, addrs[2])
-	assert.Equal(t, "", option)
-	assert.NotNil(t, err)
-
-	// Case 4: invalid proposal, invalid voter
-	option, err = keeperRead.GetVote(ctx, 2, addrs[3])
-	assert.Equal(t, "", option)
-	assert.NotNil(t, err)
+	// simpleGovKey := sdk.NewKVStoreKey("simpleGov")
+	// keeperRead := NewKeeperRead(simpleGovKey, k.ck, k.sm, DefaultCodespace)
+	//
+	// // ––––––– Test GetProposal –––––––
+	//
+	// // Case 1: valid request
+	// res, err = keeperRead.GetProposal(ctx, proposal.ID)
+	// assert.NotNil(t, res)
+	// assert.Nil(t, err)
+	// assert.Equal(t, proposal, res)
+	//
+	// // Case 2: invalid proposalID
+	// res, err = keeperRead.GetProposal(ctx, 10)
+	// assert.NotNil(t, err)
+	//
+	// // ––––––– Test SetProposal –––––––
+	//
+	// err = keeperRead.SetProposal(ctx, proposal2) // Error Unauthorized
+	// assert.NotNil(t, err)
+	//
+	// // ––––––– Test GetVote –––––––
+	//
+	// // Case 1: existing proposal, valid voter
+	// option, err = keeperRead.GetVote(ctx, 1, addrs[2])
+	// assert.Equal(t, options[1], option)
+	// assert.Nil(t, err)
+	//
+	// // Case 2: existing proposal, invalid voter
+	// option, err = keeperRead.GetVote(ctx, 1, addrs[3]) // existing proposal, invalid voter
+	// assert.Equal(t, "", option)
+	// assert.NotNil(t, err)
+	//
+	// // Case 3: invalid proposal, valid voter
+	// option, err = keeperRead.GetVote(ctx, 2, addrs[2])
+	// assert.Equal(t, "", option)
+	// assert.NotNil(t, err)
+	//
+	// // Case 4: invalid proposal, invalid voter
+	// option, err = keeperRead.GetVote(ctx, 2, addrs[3])
+	// assert.Equal(t, "", option)
+	// assert.NotNil(t, err)
 
 	// –––––––––––––––––––––––––––––––––––––––
 	//            PROPOSAL QUEUE
 	// –––––––––––––––––––––––––––––––––––––––
 
-	err = k.ProposalQueuePush(ctx, 1) // ProposalQueue not set
-	assert.NotNil(t, err)
-
-	k.setProposalQueue(ctx, ProposalQueue{})
-
-	err = k.ProposalQueuePush(ctx, 1)
-	assert.Nil(t, err)
-
-	res, err = k.ProposalQueueHead(ctx) // Gets first proposal
-	assert.NotNil(t, res)
-	assert.Nil(t, err)
-	assert.Equal(t, proposal, res)
-
-	res, err = k.ProposalQueuePop(ctx) // Pops first proposal
-	assert.Nil(t, err)
-	assert.Equal(t, proposal, res)
-
-	res, err = k.ProposalQueuePop(ctx) // Empty queue --> error
-	assert.NotNil(t, err)
-	assert.Equal(t, proposal, Proposal{})
-
-	res, err = k.ProposalQueueHead(ctx) // Empty queue --> error
-	assert.NotNil(t, err)
+	// err = k.ProposalQueuePush(ctx, 1) // ProposalQueue not set
+	// assert.NotNil(t, err)
+	//
+	// k.setProposalQueue(ctx, ProposalQueue{})
+	//
+	// err = k.ProposalQueuePush(ctx, 1)
+	// assert.Nil(t, err)
+	//
+	// res, err = k.ProposalQueueHead(ctx) // Gets first proposal
+	// assert.NotNil(t, res)
+	// assert.Nil(t, err)
+	// assert.Equal(t, proposal, res)
+	//
+	// res, err = k.ProposalQueuePop(ctx) // Pops first proposal
+	// assert.Nil(t, err)
+	// assert.Equal(t, proposal, res)
+	//
+	// res, err = k.ProposalQueuePop(ctx) // Empty queue --> error
+	// assert.NotNil(t, err)
+	// assert.Equal(t, proposal, Proposal{})
+	//
+	// res, err = k.ProposalQueueHead(ctx) // Empty queue --> error
+	// assert.NotNil(t, err)
 }

7ef8ee8e6f9f8e23255c7cb470c95dd32300c291
Aleksandr Bezobchuk <alex@tendermint.com>
2018-08-17 15:33:30 -0400

    Fix broken invariant of bonded validator power decrease

--- x/stake/keeper/validator.go
+++ x/stake/keeper/validator.go
@@ -387,6 +387,7 @@ func (k Keeper) UpdateBondedValidators(
 	ctx sdk.Context, affectedValidator types.Validator) (
 	updatedVal types.Validator, updated bool) {
 
+	pool := k.GetPool(ctx)
 	store := ctx.KVStore(k.storeKey)
 
 	oldCliffValidatorAddr := k.GetCliffValidator(ctx)
@@ -452,21 +453,32 @@ func (k Keeper) UpdateBondedValidators(
 	// swap the cliff validator for a new validator if the affected validator
 	// was bonded
 	if newValidatorBonded {
-		// unbond the cliff validator
 		if oldCliffValidatorAddr != nil {
-			cliffVal, found := k.GetValidator(ctx, oldCliffValidatorAddr)
+			oldCliffVal, found := k.GetValidator(ctx, oldCliffValidatorAddr)
 			if !found {
 				panic(fmt.Sprintf("validator record not found for address: %v\n", oldCliffValidatorAddr))
 			}
 
-			k.unbondValidator(ctx, cliffVal)
+			affectedValRank := GetValidatorsByPowerIndexKey(affectedValidator, pool)
+			valRank := GetValidatorsByPowerIndexKey(validator, pool)
+
+			if bytes.Compare(valRank, affectedValRank) < 0 {
+				// unbond the old cliff validator iff the affected validator was
+				// newly bonded and has greater power
+				k.unbondValidator(ctx, oldCliffVal)
+			} else {
+				// otherwise unbond the affected validator, which must have been
+				// kicked out
+				affectedValidator = k.unbondValidator(ctx, affectedValidator)
+			}
 		}
 
-		// bond the new validator
 		validator = k.bondValidator(ctx, validatorToBond)
 		if bytes.Equal(validator.Owner, affectedValidator.Owner) {
 			return validator, true
 		}
+
+		return affectedValidator, true
 	}
 
 	return types.Validator{}, false

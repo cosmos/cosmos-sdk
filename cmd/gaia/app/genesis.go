7e9eec9221984b5bbd1ede4c4b342a44b2153411
HaoyangLiu <lhy.0318@163.com>
2018-08-25 11:10:00 +0800

    Change delegation amount from int64 to sdk.Int

--- cmd/gaia/app/genesis.go
+++ cmd/gaia/app/genesis.go
@@ -199,7 +199,7 @@ func GaiaAppGenState(cdc *wire.Codec, appGenTxs []json.RawMessage) (genesisState
 
 			// add some new shares to the validator
 			var issuedDelShares sdk.Dec
-			validator, stakeData.Pool, issuedDelShares = validator.AddTokensFromDel(stakeData.Pool, freeFermionVal)
+			validator, stakeData.Pool, issuedDelShares = validator.AddTokensFromDel(stakeData.Pool, sdk.NewInt(freeFermionVal))
 			stakeData.Validators = append(stakeData.Validators, validator)
 
 			// create the self-delegation from the issuedDelShares

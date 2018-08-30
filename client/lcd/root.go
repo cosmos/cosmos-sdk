5473771105b5db36c093a6a6de649240cd40e3bb
HaoyangLiu <lhy.0318@163.com>
2018-08-30 15:08:10 +0800

    IRISHUB-238 :add trust option for rest-server command

--- client/lcd/root.go
+++ client/lcd/root.go
@@ -69,6 +69,7 @@ func ServeCommand(cdc *wire.Codec) *cobra.Command {
 	cmd.Flags().String(client.FlagChainID, "", "The chain ID to connect to")
 	cmd.Flags().String(client.FlagNode, "tcp://localhost:26657", "Address of the node to connect to")
 	cmd.Flags().Int(flagMaxOpenConnections, 1000, "The number of maximum open connections")
+	cmd.Flags().Bool(client.FlagTrustNode, false, "Whether trust connected full node")
 
 	return cmd
 }

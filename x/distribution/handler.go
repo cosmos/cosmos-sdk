9a2aee87910c08f32ae71a3cf3ab46c205d6d275
rigelrozanski <rigel.rozanski@gmail.com>
2018-09-03 20:46:33 -0400

    working

---
+++ x/distribution/handler.go
@@ -0,0 +1,8 @@
+package stake
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// burn burn burn
+func BurnFeeHandler(ctx sdk.Context, _ sdk.Tx, collectedFees sdk.Coins) {}
---
+++ x/distribution/keeper/key.go
@@ -0,0 +1,29 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// keys/key-prefixes
+var (
+	GlobalKey            = []byte{0x00} // key for global distribution state
+	ValidatorDistInfoKey = []byte{0x01} // prefix for each key to a validator distribution
+	DelegatorDistInfoKey = []byte{0x02} // prefix for each key to a delegation distribution
+)
+
+// gets the key for the validator distribution info from address
+// VALUE: distribution/types.ValidatorDistInfo
+func GetValidatorDistInfoKey(operatorAddr sdk.ValAddress) []byte {
+	return append(ValidatorDistInfoKey, operatorAddr.Bytes()...)
+}
+
+// gets the key for delegator distribution for a validator
+// VALUE: distribution/types.DelegatorDistInfo
+func GetDelegationDistInfoKey(delAddr sdk.AccAddress, valOperatorAddr sdk.ValAddress) []byte {
+	return append(GetDelegationDistInfosKey(delAddr), valAddr.Bytes()...)
+}
+
+// gets the prefix for a delegator's distributions across all validators
+func GetDelegationDistInfosKey(delAddr sdk.AccAddress) []byte {
+	return append(DelegatorDistInfoKey, delAddr.Bytes()...)
+}

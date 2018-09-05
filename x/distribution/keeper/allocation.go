e2b5b9b73b1287119a6f66895a2936b38fe0802e
rigelrozanski <rigel.rozanski@gmail.com>
2018-09-05 18:46:09 -0400

    ...

--- x/distribution/keeper/allocation.go
+++ x/distribution/keeper/allocation.go
@@ -3,16 +3,16 @@ package keeper
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
 // XXX TODO
-func (k Keeper) AllocateFees(ctx sdk.Context, feesCollected sdk.Coins, proposerAddr ValidatorDistribution,
-	sumPowerPrecommitValidators, totalBondedTokens, communityTax,
-	proposerCommissionRate sdk.Dec) {
+func (k Keeper) AllocateFees(ctx sdk.Context, feesCollected sdk.Coins, proposerAddr sdk.ConsAddrs,
+	sumPowerPrecommitValidators, totalBondedTokens, communityTax, proposerCommissionRate sdk.Dec) {
 
 	feePool := k.GetFeePool()
-	proposerOpAddr := Stake.Get
-	proposer := GetFeeDistribution()
+	validator := k.stakeKeeper.GetValidatorFromConsAddr(ctx, 
+	proposerOpAddr := Stake.GetValidator
+	proposer := k.GetFeeDistribution(ctx, proposerOpAddr)
 
 	feesCollectedDec = MakeDecCoins(feesCollected)
-	proposerReward = feesCollectedDec * (0.01 + 0.04*sumPowerPrecommitValidators/totalBondedTokens)
+	proposerReward = feesCollectedDec.Mul(sdk.NewDecWithPrec(1, 2) + sdk.NewDecWithPrec(1, 2).Mul(sumPowerPrecommitValidators)/totalBondedTokens)
 
 	commission = proposerReward * proposerCommissionRate
 	proposer.PoolCommission += commission

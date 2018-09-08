67b760ec95474c79af8a006be2dacbf22826e3e1
Alessio Treglia <alessio@tendermint.com>
2018-09-08 10:14:27 +0100

    Read from STDIN if '-' is supplied as filename as per UNIX popular convention

--- x/bank/client/cli/broadcast.go
+++ x/bank/client/cli/broadcast.go
@@ -16,7 +16,8 @@ func GetBroadcastCommand(codec *amino.Codec) *cobra.Command {
 		Use:   "broadcast <file>",
 		Short: "Broadcast transactions generated offline",
 		Long: `Broadcast transactions created with the --generate-only flag and signed with the sign command.
-Read a transaction from <file> and broadcast it to a node.`,
+Read a transaction from <file> and broadcast it to a node. If you supply a dash (-) argument
+in place of an input filename, the command reads from standard input.`,
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			cliCtx := context.NewCLIContext().WithCodec(codec).WithLogger(os.Stdout)
@@ -36,7 +37,12 @@ Read a transaction from <file> and broadcast it to a node.`,
 
 func readAndUnmarshalStdTx(cdc *amino.Codec, filename string) (stdTx auth.StdTx, err error) {
 	var bytes []byte
-	if bytes, err = ioutil.ReadFile(filename); err != nil {
+	if filename == "-" {
+		bytes, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		bytes, err = ioutil.ReadFile(filename)
+	}
+	if err != nil {
 		return
 	}
 	if err = cdc.UnmarshalJSON(bytes, &stdTx); err != nil {

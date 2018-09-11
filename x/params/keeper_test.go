63427362cd973b016f6355fe25af98f418a96413
mossid <torecursedivine@gmail.com>
2018-09-11 16:19:48 +0900

    string -> []byte in keeper_test.go

--- x/params/keeper_test.go
+++ x/params/keeper_test.go
@@ -39,13 +39,13 @@ func TestKeeper(t *testing.T) {
 		key   Key
 		param int64
 	}{
-		{NewKey("key1"), 10},
-		{NewKey("key2"), 55},
-		{NewKey("key3"), 182},
-		{NewKey("key4"), 17582},
-		{NewKey("key5"), 2768554},
-		{NewKey("space1", "key1"), 1157279},
-		{NewKey("space1", "key2"), 9058701},
+		{NewKey([]byte("key1")), 10},
+		{NewKey([]byte("key2")), 55},
+		{NewKey([]byte("key3")), 182},
+		{NewKey([]byte("key4")), 17582},
+		{NewKey([]byte("key5")), 2768554},
+		{NewKey([]byte("space1"), []byte("key1")), 1157279},
+		{NewKey([]byte("space1"), []byte("key2")), 9058701},
 	}
 
 	skey := sdk.NewKVStoreKey("test")
@@ -96,14 +96,14 @@ func TestGet(t *testing.T) {
 		zero  interface{}
 		ptr   interface{}
 	}{
-		{NewKey("string"), "test", "", new(string)},
-		{NewKey("bool"), true, false, new(bool)},
-		{NewKey("int16"), int16(1), int16(0), new(int16)},
-		{NewKey("int32"), int32(1), int32(0), new(int32)},
-		{NewKey("int64"), int64(1), int64(0), new(int64)},
-		{NewKey("uint16"), uint16(1), uint16(0), new(uint16)},
-		{NewKey("uint32"), uint32(1), uint32(0), new(uint32)},
-		{NewKey("uint64"), uint64(1), uint64(0), new(uint64)},
+		{NewKey([]byte("string")), "test", "", new(string)},
+		{NewKey([]byte("bool")), true, false, new(bool)},
+		{NewKey([]byte("int16")), int16(1), int16(0), new(int16)},
+		{NewKey([]byte("int32")), int32(1), int32(0), new(int32)},
+		{NewKey([]byte("int64")), int64(1), int64(0), new(int64)},
+		{NewKey([]byte("uint16")), uint16(1), uint16(0), new(uint16)},
+		{NewKey([]byte("uint32")), uint32(1), uint32(0), new(uint32)},
+		{NewKey([]byte("uint64")), uint64(1), uint64(0), new(uint64)},
 		/*
 			{NewKey("int"), sdk.NewInt(1), *new(sdk.Int), new(sdk.Int)},
 			{NewKey("uint"), sdk.NewUint(1), *new(sdk.Uint), new(sdk.Uint)},
@@ -116,9 +116,9 @@ func TestGet(t *testing.T) {
 	}
 
 	for _, kv := range kvs {
-		require.NotPanics(t, func() { space.GetIfExists(ctx, NewKey("invalid"), kv.ptr) })
+		require.NotPanics(t, func() { space.GetIfExists(ctx, NewKey([]byte("invalid")), kv.ptr) })
 		require.Equal(t, kv.zero, reflect.ValueOf(kv.ptr).Elem().Interface())
-		require.Panics(t, func() { space.Get(ctx, NewKey("invalid"), kv.ptr) })
+		require.Panics(t, func() { space.Get(ctx, NewKey([]byte("invalid")), kv.ptr) })
 		require.Equal(t, kv.zero, reflect.ValueOf(kv.ptr).Elem().Interface())
 
 		require.NotPanics(t, func() { space.GetIfExists(ctx, kv.key, kv.ptr) })
@@ -126,7 +126,7 @@ func TestGet(t *testing.T) {
 		require.NotPanics(t, func() { space.Get(ctx, kv.key, kv.ptr) })
 		require.Equal(t, kv.param, reflect.ValueOf(kv.ptr).Elem().Interface())
 
-		require.Panics(t, func() { space.Get(ctx, NewKey("invalid"), kv.ptr) })
+		require.Panics(t, func() { space.Get(ctx, NewKey([]byte("invalid")), kv.ptr) })
 		require.Equal(t, kv.param, reflect.ValueOf(kv.ptr).Elem().Interface())
 
 		require.Panics(t, func() { space.Get(ctx, kv.key, nil) })

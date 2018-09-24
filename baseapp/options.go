f45728c02b13b9baa5a87b781eaa34282ce843b2
ValarDragon <dojha12@gmail.com>
2018-09-24 10:09:52 -0700

    add newlines

--- baseapp/options.go
+++ baseapp/options.go
@@ -1,3 +1,4 @@
+// nolint: golint
 package baseapp
 
 import (
@@ -38,70 +39,80 @@ func SetMinimumFees(minFees string) func(*BaseApp) {
 	return func(bap *BaseApp) { bap.SetMinimumFees(fees) }
 }
 
-// nolint - Setter functions
 func (app *BaseApp) SetName(name string) {
 	if app.sealed {
 		panic("SetName() on sealed BaseApp")
 	}
 	app.name = name
 }
+
 func (app *BaseApp) SetDB(db dbm.DB) {
 	if app.sealed {
 		panic("SetDB() on sealed BaseApp")
 	}
 	app.db = db
 }
+
 func (app *BaseApp) SetCMS(cms store.CommitMultiStore) {
 	if app.sealed {
 		panic("SetEndBlocker() on sealed BaseApp")
 	}
 	app.cms = cms
 }
+
 func (app *BaseApp) SetInitChainer(initChainer sdk.InitChainer) {
 	if app.sealed {
 		panic("SetInitChainer() on sealed BaseApp")
 	}
 	app.initChainer = initChainer
 }
+
 func (app *BaseApp) SetBeginBlocker(beginBlocker sdk.BeginBlocker) {
 	if app.sealed {
 		panic("SetBeginBlocker() on sealed BaseApp")
 	}
 	app.beginBlocker = beginBlocker
 }
+
 func (app *BaseApp) SetEndBlocker(endBlocker sdk.EndBlocker) {
 	if app.sealed {
 		panic("SetEndBlocker() on sealed BaseApp")
 	}
 	app.endBlocker = endBlocker
 }
+
 func (app *BaseApp) SetAnteHandler(ah sdk.AnteHandler) {
 	if app.sealed {
 		panic("SetAnteHandler() on sealed BaseApp")
 	}
 	app.anteHandler = ah
 }
+
 func (app *BaseApp) SetAddrPeerFilter(pf sdk.PeerFilter) {
 	if app.sealed {
 		panic("SetAddrPeerFilter() on sealed BaseApp")
 	}
 	app.addrPeerFilter = pf
 }
+
 func (app *BaseApp) SetPubKeyPeerFilter(pf sdk.PeerFilter) {
 	if app.sealed {
 		panic("SetPubKeyPeerFilter() on sealed BaseApp")
 	}
 	app.pubkeyPeerFilter = pf
 }
+
 func (app *BaseApp) Router() Router {
 	if app.sealed {
 		panic("Router() on sealed BaseApp")
 	}
 	return app.router
 }
+
 func (app *BaseApp) QueryRouter() QueryRouter {
 	return app.queryRouter
 }
+
 func (app *BaseApp) Seal()          { app.sealed = true }
 func (app *BaseApp) IsSealed() bool { return app.sealed }
 func (app *BaseApp) enforceSeal() {

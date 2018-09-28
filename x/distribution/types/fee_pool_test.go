16f5d69329fef9714c98de23f74585d81082e76b
rigelrozanski <rigel.rozanski@gmail.com>
2018-09-28 19:32:42 -0400

    types fee pool test

--- x/distribution/types/fee_pool_test.go
+++ x/distribution/types/fee_pool_test.go
@@ -1,6 +1,19 @@
 package types
 
-import "testing"
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
 
 func TestTotalAccumUpdate(t *testing.T) {
+
+	ta := NewTotalAccum(0)
+
+	ta.Update(5, sdk.NewDec(3))
+	require.True(DecEq(t, sdk.NewDec(15), ta.Accum))
+
+	ta.Update(8, sdk.NewDec(2))
+	require.True(DecEq(t, sdk.NewDec(21), ta.Accum))
 }

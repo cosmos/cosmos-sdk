142f42ecc79359a8eb43f6dbdaa4e0923e3c4a79
rigelrozanski <rigel.rozanski@gmail.com>
2018-10-08 20:14:03 -0400

    ...

--- x/distribution/keeper/delegation_test.go
+++ x/distribution/keeper/delegation_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestWithdrawDelegationReward(t *testing.T) {
 
+	keeper.WithdrawDelegationReward(ctx, delAddr1, valAddr1)
 }
 
 func TestWithdrawDelegationRewardsAll(t *testing.T) {

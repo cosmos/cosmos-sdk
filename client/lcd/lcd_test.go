1ee8deed2bcee90973729b8ff58e3e3787088eed
Ethan Buchman <ethan@coinculture.info>
2018-10-17 13:37:58 -0400

    Merge PR #2090: Improve crypto/keys and add `keys mnemonic` and `keys new` commands
    * crypto/keys/hd: use btcec to remove dep on tendermint
    
    * crypto/keys/bcrypt: improve comment about fork
    
    * crypto/keys/bip39 -> crypto/keys/bip39/fundraiser
    
    * crypto/keys/bip39: bring in fork of tyler-smith
    
    * crypto/keys/hd: update dep
    
    * crypto/keys: update deps
    
    * crypto/keys: move mintkey.go into new crypto/keys/mintkey
    
    * crypto/keys/hd: NewParamsFromPath
    
    * crypto/keys: keybase.Derive takes a bip39 passphrase too
    
    * crypto/keys/hd: BIP44Params.DerivationPath
    
    * gaiacli keys: add commands new and mnemonic
    
    * fix lints
    
    * minor fixes from review
    
    * update Gopkg.toml
    
    * add tendermint fork of golang.org/x/crypto
    * pin some transitive deps
    
    * crypto/keys/bcrypt: remove
    
    * remove in favour of fork of golang.org/x/crypto/bcrypt at github.com/tendermint/crypto/bcrypt
    
    * crypto/keys/bip39: remove completely
    
    * use fork cosmos/go-bip39 instead
    
    * Gopkg.toml: dont use master
    
    * Pull in changes from my PR
    
    * fixes from review
    
    * enforce min len for --unsafe-entropy
    
    * lint fix
    
    * feedback from review
    
    * fix dep

--- client/lcd/lcd_test.go
+++ client/lcd/lcd_test.go
@@ -10,20 +10,20 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cosmos/cosmos-sdk/client/rpc"
-	"github.com/cosmos/cosmos-sdk/client/tx"
-	p2p "github.com/tendermint/tendermint/p2p"
-
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	cryptoKeys "github.com/cosmos/cosmos-sdk/crypto/keys"
+	p2p "github.com/tendermint/tendermint/p2p"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 
 	client "github.com/cosmos/cosmos-sdk/client"
 	keys "github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/client/rpc"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/codec"
+	cryptoKeys "github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/mintkey"
 	tests "github.com/cosmos/cosmos-sdk/tests"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	version "github.com/cosmos/cosmos-sdk/version"
@@ -35,7 +35,7 @@ import (
 )
 
 func init() {
-	cryptoKeys.BcryptSecurityParameter = 1
+	mintkey.BcryptSecurityParameter = 1
 	version.Version = os.Getenv("VERSION")
 }
 

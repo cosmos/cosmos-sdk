5270da65886c3e1be070353579168864e18d40a0
HaoyangLiu <lhy.0318@163.com>
2018-10-19 14:46:17 +0800

    fix ci test failure

--- x/gov/client/utils.go
+++ x/gov/client/utils.go
@@ -4,45 +4,53 @@ package client
 func NormalizeVoteOption(option string) string {
 	switch option {
 	case "yes":
+	case "Yes":
 		return "Yes"
 	case "abstain":
+	case "Abstain":
 		return "Abstain"
 	case "no":
+	case "No":
 		return "No"
 	case "no_with_veto":
+	case "NoWithVeto":
 		return "NoWithVeto"
-	default:
-		return ""
 	}
+	return ""
 }
 
 //NormalizeProposalType - normalize user specified proposal type
 func NormalizeProposalType(proposalType string) string {
 	switch proposalType {
 	case "text":
+	case "Text":
 		return "Text"
 	case "parameter_change":
+	case "ParameterChange":
 		return "ParameterChange"
 	case "software_upgrade":
+	case "SoftwareUpgrade":
 		return "SoftwareUpgrade"
-	default:
-		return ""
 	}
+	return ""
 }
 
 //NormalizeProposalStatus - normalize user specified proposal status
 func NormalizeProposalStatus(status string) string {
 	switch status {
 	case "deposit_period":
+	case "DepositPeriod":
 		return "DepositPeriod"
 	case "voting_period":
+	case "VotingPeriod":
 		return "VotingPeriod"
 	case "passed":
+	case "Passed":
 		return "Passed"
 	case "rejected":
+	case "Rejected":
 		return "Rejected"
-	default:
-		return ""
 	}
+	return ""
 }
 

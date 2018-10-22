90fb578fb6528160fe994fb83e64dbfa94f0c2b1
mossid <torecursedivine@gmail.com>
2018-10-22 11:57:12 +0200

    fix ante

--- cmd/gaia/app/app.go
+++ cmd/gaia/app/app.go
@@ -230,7 +230,8 @@ func (app *GaiaApp) NewAnteHandler() sdk.AnteHandler {
 		if abort {
 			return
 		}
-		return circuitante(newctx, tx, simulate)
+		newctx, _, abort = circuitante(newctx, tx, simulate)
+		return
 	}
 }
 

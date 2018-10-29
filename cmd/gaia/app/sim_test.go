0d8325b859e67fac8476d0c74c4f68afb4a9b752
Dev Ojha <ValarDragon@users.noreply.github.com>
2018-10-29 16:10:39 -0700

    Merge PR #2623: Speedup simulator by switching to goleveldb
    Due to requiring app.Commit() at the moment, golevel db is significantly faster than a memdb

--- cmd/gaia/app/sim_test.go
+++ cmd/gaia/app/sim_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"testing"
@@ -141,7 +142,7 @@ func BenchmarkFullGaiaSimulation(b *testing.B) {
 	var logger log.Logger
 	logger = log.NewNopLogger()
 	var db dbm.DB
-	dir := os.TempDir()
+	dir, _ := ioutil.TempDir("", "goleveldb-gaia-sim")
 	db, _ = dbm.NewGoLevelDB("Simulation", dir)
 	defer func() {
 		db.Close()
@@ -183,7 +184,13 @@ func TestFullGaiaSimulation(t *testing.T) {
 	} else {
 		logger = log.NewNopLogger()
 	}
-	db := dbm.NewMemDB()
+	var db dbm.DB
+	dir, _ := ioutil.TempDir("", "goleveldb-gaia-sim")
+	db, _ = dbm.NewGoLevelDB("Simulation", dir)
+	defer func() {
+		db.Close()
+		os.RemoveAll(dir)
+	}()
 	app := NewGaiaApp(logger, db, nil)
 	require.Equal(t, "GaiaApp", app.Name())
 
@@ -198,7 +205,11 @@ func TestFullGaiaSimulation(t *testing.T) {
 		commit,
 	)
 	if commit {
-		fmt.Println("Database Size", db.Stats()["database.size"])
+		// for memdb:
+		// fmt.Println("Database Size", db.Stats()["database.size"])
+		fmt.Println("GoLevelDB Stats")
+		fmt.Println(db.Stats()["leveldb.stats"])
+		fmt.Println("GoLevelDB cached block size", db.Stats()["leveldb.cachedblock"])
 	}
 	require.Nil(t, err)
 }

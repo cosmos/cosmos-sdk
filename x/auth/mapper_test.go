4b338afd7fd8a9396132cf72b80c07e278e26caa
yutianwu <wzxingbupt@gmail.com>
2018-11-02 10:24:09 +0800

    add benchmarks without coins

--- x/auth/mapper_test.go
+++ x/auth/mapper_test.go
@@ -111,12 +111,40 @@ func BenchmarkAccountMapperGetAccountFound(b *testing.B) {
 		arr := []byte{byte((i & 0xFF0000) >> 16), byte((i & 0xFF00) >> 8), byte(i & 0xFF)}
 		addr := sdk.AccAddress(arr)
 		acc := mapper.NewAccountWithAddress(ctx, addr)
-		acc.SetCoins(sdk.Coins{sdk.NewCoin("LTC", sdk.NewInt(1000)),
-			sdk.NewCoin("BTC", sdk.NewInt(1000)),
-			sdk.NewCoin("ETH", sdk.NewInt(1000)),
-			sdk.NewCoin("XRP", sdk.NewInt(1000)),
-			sdk.NewCoin("BCH", sdk.NewInt(1000)),
-			sdk.NewCoin("EOS", sdk.NewInt(1000))})
+		mapper.SetAccount(ctx, acc)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		arr := []byte{byte((i & 0xFF0000) >> 16), byte((i & 0xFF00) >> 8), byte(i & 0xFF)}
+		mapper.GetAccount(ctx, sdk.AccAddress(arr))
+	}
+}
+
+func BenchmarkAccountMapperGetAccountFoundWithCoins(b *testing.B) {
+	ms, capKey, _ := setupMultiStore()
+	cdc := codec.New()
+	RegisterBaseAccount(cdc)
+
+	// make context and mapper
+	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
+	mapper := NewAccountKeeper(cdc, capKey, ProtoBaseAccount)
+
+	coins := sdk.Coins{
+		sdk.NewCoin("LTC", sdk.NewInt(1000)),
+		sdk.NewCoin("BTC", sdk.NewInt(1000)),
+		sdk.NewCoin("ETH", sdk.NewInt(1000)),
+		sdk.NewCoin("XRP", sdk.NewInt(1000)),
+		sdk.NewCoin("BCH", sdk.NewInt(1000)),
+		sdk.NewCoin("EOS", sdk.NewInt(1000)),
+	}
+
+	// assumes b.N < 2**24
+	for i := 0; i < b.N; i++ {
+		arr := []byte{byte((i & 0xFF0000) >> 16), byte((i & 0xFF00) >> 8), byte(i & 0xFF)}
+		addr := sdk.AccAddress(arr)
+		acc := mapper.NewAccountWithAddress(ctx, addr)
+		acc.SetCoins(coins)
 		mapper.SetAccount(ctx, acc)
 	}
 
@@ -142,12 +170,35 @@ func BenchmarkAccountMapperSetAccount(b *testing.B) {
 		arr := []byte{byte((i & 0xFF0000) >> 16), byte((i & 0xFF00) >> 8), byte(i & 0xFF)}
 		addr := sdk.AccAddress(arr)
 		acc := mapper.NewAccountWithAddress(ctx, addr)
-		acc.SetCoins(sdk.Coins{sdk.NewCoin("LTC", sdk.NewInt(1000)),
-			sdk.NewCoin("BTC", sdk.NewInt(1000)),
-			sdk.NewCoin("ETH", sdk.NewInt(1000)),
-			sdk.NewCoin("XRP", sdk.NewInt(1000)),
-			sdk.NewCoin("BCH", sdk.NewInt(1000)),
-			sdk.NewCoin("EOS", sdk.NewInt(1000))})
+		mapper.SetAccount(ctx, acc)
+	}
+}
+
+func BenchmarkAccountMapperSetAccountWithCoins(b *testing.B) {
+	ms, capKey, _ := setupMultiStore()
+	cdc := codec.New()
+	RegisterBaseAccount(cdc)
+
+	// make context and mapper
+	ctx := sdk.NewContext(ms, abci.Header{}, false, log.NewNopLogger())
+	mapper := NewAccountKeeper(cdc, capKey, ProtoBaseAccount)
+
+	coins := sdk.Coins{
+		sdk.NewCoin("LTC", sdk.NewInt(1000)),
+		sdk.NewCoin("BTC", sdk.NewInt(1000)),
+		sdk.NewCoin("ETH", sdk.NewInt(1000)),
+		sdk.NewCoin("XRP", sdk.NewInt(1000)),
+		sdk.NewCoin("BCH", sdk.NewInt(1000)),
+		sdk.NewCoin("EOS", sdk.NewInt(1000)),
+	}
+
+	b.ResetTimer()
+	// assumes b.N < 2**24
+	for i := 0; i < b.N; i++ {
+		arr := []byte{byte((i & 0xFF0000) >> 16), byte((i & 0xFF00) >> 8), byte(i & 0xFF)}
+		addr := sdk.AccAddress(arr)
+		acc := mapper.NewAccountWithAddress(ctx, addr)
+		acc.SetCoins(coins)
 		mapper.SetAccount(ctx, acc)
 	}
 }

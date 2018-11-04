8a732fca237580a35874369515fcf830c661ec9f
Aleksandr Bezobchuk <alex@tendermint.com>
2018-11-04 16:01:12 -0500

    Fix key path creation

--- client/context/query.go
+++ client/context/query.go
@@ -218,9 +218,9 @@ func (ctx CLIContext) verifyProof(queryPath string, resp abci.ResponseQuery) err
 		return err
 	}
 
-	kp := new(merkle.KeyPath)
-	kp.AppendKey([]byte(storeName), merkle.KeyEncodingURL)
-	kp.AppendKey(resp.Key, merkle.KeyEncodingURL)
+	kp := merkle.KeyPath{}
+	kp = kp.AppendKey([]byte(storeName), merkle.KeyEncodingURL)
+	kp = kp.AppendKey(resp.Key, merkle.KeyEncodingURL)
 
 	err = prt.VerifyValue(resp.Proof, commit.Header.AppHash, kp.String(), resp.Value)
 	if err != nil {

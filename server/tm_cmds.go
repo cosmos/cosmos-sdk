b66d98757d2b97bbbff6431aaa1876dc4db2e003
Aleksandr Bezobchuk <alex@tendermint.com>
2018-11-09 07:40:26 -0500

    Use consensus address bech32 prefix

--- server/tm_cmds.go
+++ server/tm_cmds.go
@@ -67,13 +67,13 @@ func ShowAddressCmd(ctx *Context) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cfg := ctx.Config
 			privValidator := pvm.LoadOrGenFilePV(cfg.PrivValidatorFile())
-			valAddr := (sdk.ValAddress)(privValidator.Address)
+			valConsAddr := (sdk.ConsAddress)(privValidator.Address)
 
 			if viper.GetBool(client.FlagJson) {
-				return printlnJSON(valAddr)
+				return printlnJSON(valConsAddr)
 			}
 
-			fmt.Println(valAddr.String())
+			fmt.Println(valConsAddr.String())
 			return nil
 		},
 	}

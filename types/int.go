e67f179e011b438275dbc08af0c94ffaa61c6522
Aleksandr Bezobchuk <alex@tendermint.com>
2018-11-13 16:06:31 -0500

    DRY-up uint overflow logic

--- types/int.go
+++ types/int.go
@@ -321,11 +321,11 @@ func NewUint(n uint64) Uint {
 
 // NewUintFromBigUint constructs Uint from big.Uint
 func NewUintFromBigInt(i *big.Int) Uint {
-	// Check overflow
-	if i.Sign() == -1 || i.Sign() == 1 && i.BitLen() > 256 {
+	res := Uint{i}
+	if UintOverflow(res) {
 		panic("Uint overflow")
 	}
-	return Uint{i}
+	return res
 }
 
 // NewUintFromString constructs Uint from string
@@ -352,11 +352,12 @@ func NewUintWithDecimal(n uint64, dec int) Uint {
 	i := new(big.Int)
 	i.Mul(new(big.Int).SetUint64(n), exp)
 
-	// Check overflow
-	if i.Sign() == -1 || i.Sign() == 1 && i.BitLen() > 256 {
+	res := Uint{i}
+	if UintOverflow(res) {
 		panic("NewUintWithDecimal() out of bound")
 	}
-	return Uint{i}
+
+	return res
 }
 
 // ZeroUint returns Uint value with zero
@@ -407,8 +408,7 @@ func (i Uint) LT(i2 Uint) bool {
 // Add adds Uint from another
 func (i Uint) Add(i2 Uint) (res Uint) {
 	res = Uint{add(i.i, i2.i)}
-	// Check overflow
-	if res.Sign() == -1 || res.Sign() == 1 && res.i.BitLen() > 256 {
+	if UintOverflow(res) {
 		panic("Uint overflow")
 	}
 	return
@@ -422,8 +422,7 @@ func (i Uint) AddRaw(i2 uint64) Uint {
 // Sub subtracts Uint from another
 func (i Uint) Sub(i2 Uint) (res Uint) {
 	res = Uint{sub(i.i, i2.i)}
-	// Check overflow
-	if res.Sign() == -1 || res.Sign() == 1 && res.i.BitLen() > 256 {
+	if UintOverflow(res) {
 		panic("Uint overflow")
 	}
 	return
@@ -436,15 +435,15 @@ func (i Uint) SubRaw(i2 uint64) Uint {
 
 // Mul multiples two Uints
 func (i Uint) Mul(i2 Uint) (res Uint) {
-	// Check overflow
 	if i.i.BitLen()+i2.i.BitLen()-1 > 256 {
 		panic("Uint overflow")
 	}
+
 	res = Uint{mul(i.i, i2.i)}
-	// Check overflow
-	if res.Sign() == -1 || res.Sign() == 1 && res.i.BitLen() > 256 {
+	if UintOverflow(res) {
 		panic("Uint overflow")
 	}
+
 	return
 }
 
@@ -528,6 +527,12 @@ func (i *Uint) UnmarshalJSON(bz []byte) error {
 }
 
 //__________________________________________________________________________
+
+// UintOverflow returns true if a given unsigned integer overflows and false
+// otherwise.
+func UintOverflow(x Uint) bool {
+	return x.i.Sign() == -1 || x.i.Sign() == 1 && x.i.BitLen() > 256
+}
 
 // intended to be used with require/assert:  require.True(IntEq(...))
 func IntEq(t *testing.T, exp, got Int) (*testing.T, bool, string, string, string) {

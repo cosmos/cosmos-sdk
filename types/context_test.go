85bf273f79b8b9b061af2a7438ee92b29389cec1
Aleksandr Bezobchuk <alex@tendermint.com>
2018-11-14 08:58:21 -0500

    Update context tests

--- types/context_test.go
+++ types/context_test.go
@@ -162,7 +162,7 @@ func TestContextWithCustom(t *testing.T) {
 	logger := NewMockLogger()
 	voteinfos := []abci.VoteInfo{{}}
 	meter := types.NewGasMeter(10000)
-	minFees := types.Coins{types.NewInt64Coin("feeCoin", 1)}
+	minFees := types.Coins{types.NewUInt64Coin("feeCoin", 1)}
 
 	ctx = types.NewContext(nil, header, ischeck, logger)
 	require.Equal(t, header, ctx.BlockHeader())
@@ -181,5 +181,5 @@ func TestContextWithCustom(t *testing.T) {
 	require.Equal(t, logger, ctx.Logger())
 	require.Equal(t, voteinfos, ctx.VoteInfos())
 	require.Equal(t, meter, ctx.GasMeter())
-	require.Equal(t, minFees, types.Coins{types.NewInt64Coin("feeCoin", 1)})
+	require.Equal(t, minFees, types.Coins{types.NewUInt64Coin("feeCoin", 1)})
 }

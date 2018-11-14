492e9f0313d1b3abb338e8750b97bed95932ba96
Christopher Goes <cwgoes@pluranimity.org>
2018-11-14 16:43:05 +0100

    Minor cleanup

--- cmd/gaia/app/invariants.go
+++ cmd/gaia/app/invariants.go
@@ -2,31 +2,33 @@ package app
 
 import (
 	"fmt"
+	"time"
 
 	banksim "github.com/cosmos/cosmos-sdk/x/bank/simulation"
 	distrsim "github.com/cosmos/cosmos-sdk/x/distribution/simulation"
-	govsim "github.com/cosmos/cosmos-sdk/x/gov/simulation"
 	"github.com/cosmos/cosmos-sdk/x/mock/simulation"
-	slashingsim "github.com/cosmos/cosmos-sdk/x/slashing/simulation"
 	stakesim "github.com/cosmos/cosmos-sdk/x/stake/simulation"
 )
 
 func (app *GaiaApp) runtimeInvariants() []simulation.Invariant {
 	return []simulation.Invariant{
 		banksim.NonnegativeBalanceInvariant(app.accountKeeper),
-		govsim.AllInvariants(),
-		distrsim.AllInvariants(app.distrKeeper, app.stakeKeeper),
-		stakesim.AllInvariants(app.bankKeeper, app.stakeKeeper,
+		distrsim.ValAccumInvariants(app.distrKeeper, app.stakeKeeper),
+		stakesim.SupplyInvariants(app.bankKeeper, app.stakeKeeper,
 			app.feeCollectionKeeper, app.distrKeeper, app.accountKeeper),
-		slashingsim.AllInvariants(),
+		stakesim.PositivePowerInvariant(app.stakeKeeper),
 	}
 }
 
 func (app *GaiaApp) assertRuntimeInvariants() {
 	invariants := app.runtimeInvariants()
+	start := time.Now()
 	for _, inv := range invariants {
 		if err := inv(app.BaseApp); err != nil {
 			panic(fmt.Errorf("invariant broken: %s", err))
 		}
 	}
+	end := time.Now()
+	diff := end.Sub(start)
+	app.BaseApp.Logger.With("module", "invariants").Info("Asserted all invariants", "duration", diff)
 }

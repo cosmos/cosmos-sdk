715548340b619a568bba0165ba79b3bd49b65020
Christopher Goes <cwgoes@pluranimity.org>
2018-11-16 13:41:27 +0100

    Remove offset-by-one, must be some other issue

--- cmd/gaia/app/export.go
+++ cmd/gaia/app/export.go
@@ -19,7 +19,7 @@ import (
 // export the state of gaia for a genesis file
 func (app *GaiaApp) ExportAppStateAndValidators(forZeroHeight bool) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
 	// as if they could withdraw from the start of the next block
-	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight() + 1})
+	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
 
 	// prepare for fresh start at zero height
 	if forZeroHeight {

9f9f3ce91c4ddfda98d76bacfa835b27a419ec7e
Christopher Goes <cwgoes@pluranimity.org>
2018-11-19 16:08:06 +0100

    Remove all bank.MsgIssue code

--- x/bank/codec.go
+++ x/bank/codec.go
@@ -7,7 +7,6 @@ import (
 // Register concrete types on codec codec
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgSend{}, "cosmos-sdk/Send", nil)
-	cdc.RegisterConcrete(MsgIssue{}, "cosmos-sdk/Issue", nil)
 }
 
 var msgCdc = codec.New()

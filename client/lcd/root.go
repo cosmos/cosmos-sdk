155c55345691fe48f3712b0dbf3e90bac6174867
Aleksandr Bezobchuk <alex@tendermint.com>
2018-11-27 16:03:53 -0500

    Refactor Gaia Lite bootstrapping

--- client/lcd/root.go
+++ client/lcd/root.go
@@ -145,15 +145,19 @@ func (rs *RestServer) Start(listenAddr string, sslHosts string,
 	return nil
 }
 
-// ServeCommand will generate a long-running rest server
-// (aka Light Client Daemon) that exposes functionality similar
-// to the cli, but over rest
-func (rs *RestServer) ServeCommand() *cobra.Command {
+// ServeCommand will start a Gaia Lite REST service as a blocking process. It
+// takes a codec to create a RestServer object and a function to register all
+// necessary routes.
+func ServeCommand(cdc *codec.Codec, registerRoutesFn func(*RestServer)) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "rest-server",
 		Short: "Start LCD (light-client daemon), a local REST server",
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			rs := NewRestServer(cdc)
+
 			rs.setKeybase(nil)
+			registerRoutesFn(rs)
+
 			// Start the rest server and return error if one exists
 			err = rs.Start(
 				viper.GetString(client.FlagListenAddr),

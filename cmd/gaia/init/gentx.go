058a6878dee772b871d1ea68da4062197ebb506e
Jack Zampolin <jack.zampolin@gmail.com>
2018-12-03 08:52:23 -0800

    Change to bondDenom from the stake section in genesis

--- cmd/gaia/init/gentx.go
+++ cmd/gaia/init/gentx.go
@@ -108,8 +108,8 @@ following delegation and commission default parameters:
 				// Ensure that account is in genesis
 				if acc.Address.Equals(key.GetAddress()) {
 					// Ensure account contains enough funds of default bond denom
-					if acc.Coins.AmountOf(stakeTypes.DefaultBondDenom).
-						LT(coins.AmountOf(stakeTypes.DefaultBondDenom)) {
+					if acc.Coins.AmountOf(genesisState.StakeData.Params.BondDenom).
+						LT(coins.AmountOf(genesisState.StakeData.Params.BondDenom)) {
 						return fmt.Errorf(
 							"Account %s is in genesis, but the only has %s available to stake, not %s",
 							key.GetAddress(), acc.Coins, amount,

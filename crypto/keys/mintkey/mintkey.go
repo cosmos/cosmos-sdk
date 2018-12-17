69e086435937192ad04857051a9448b51474ad58
qiuyingzi <qiuyingzilv@gmail.com>
2018-12-17 10:43:38 +0800

    Modify to new crypto

--- crypto/keys/mintkey/mintkey.go
+++ crypto/keys/mintkey/mintkey.go
@@ -75,6 +75,8 @@ func unarmorBytes(armorStr, blockType string) (bz []byte, err error) {
 	if err != nil {
 		return
 	}
+	fmt.Println(armorStr)
+	fmt.Println(bType)
 	if bType != blockType {
 		err = fmt.Errorf("Unrecognized armor type %q, expected: %q", bType, blockType)
 		return
@@ -104,8 +106,7 @@ func EncryptArmorPrivKey(privKey crypto.PrivKey, passphrase string) string {
 // generated salt and the xsalsa20 cipher. returns the salt and the
 // encrypted priv key.
 func encryptPrivKey(privKey crypto.PrivKey, passphrase string) (saltBytes []byte, encBytes []byte) {
-	saltBytes = crypto.CRandBytes(16)
-	key, err := bcrypt.GenerateFromPassword(saltBytes, []byte(passphrase), BcryptSecurityParameter)
+	key, err := bcrypt.GenerateFromPassword([]byte(passphrase), BcryptSecurityParameter)
 	if err != nil {
 		cmn.Exit("Error generating bcrypt key from passphrase: " + err.Error())
 	}
@@ -139,7 +140,7 @@ func UnarmorDecryptPrivKey(armorStr string, passphrase string) (crypto.PrivKey, 
 }
 
 func decryptPrivKey(saltBytes []byte, encBytes []byte, passphrase string) (privKey crypto.PrivKey, err error) {
-	key, err := bcrypt.GenerateFromPassword(saltBytes, []byte(passphrase), BcryptSecurityParameter)
+	key, err := bcrypt.GenerateFromPassword([]byte(passphrase), BcryptSecurityParameter)
 	if err != nil {
 		cmn.Exit("Error generating bcrypt key from passphrase: " + err.Error())
 	}

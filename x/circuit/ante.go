3748724e3b60c69fb1d2f68b05382b66790fbd99
mossid <torecursedivine@gmail.com>
2019-01-05 08:19:22 +0100

    add accessors to the keeper

--- x/circuit/ante.go
+++ x/circuit/ante.go
@@ -7,14 +7,8 @@ import (
 func NewAnteHandler(k Keeper) sdk.AnteHandler {
 	return func(ctx sdk.Context, tx sdk.Tx, simulate bool) (sdk.Context, sdk.Result, bool) {
 		for _, msg := range tx.GetMsgs() {
-			var breaker bool
-			k.space.GetWithSubkeyIfExists(ctx, MsgRouteKey, []byte(msg.Route()), &breaker)
-			if breaker {
-				return ctx, sdk.ErrUnauthorized("msg route circuit breaked").Result(), true
-			}
-			k.space.GetWithSubkeyIfExists(ctx, MsgTypeKey, []byte(msg.Type()), &breaker)
-			if breaker {
-				return ctx, sdk.ErrUnauthorized("msg type circuit breaked").Result(), true
+			if k.CheckMsgBreak(ctx, msg) {
+				return ctx, sdk.ErrUnauthorized("msg circuit break").Result(), true
 			}
 		}
 		return ctx, sdk.Result{}, false
--- x/circuit/keeper.go
+++ x/circuit/keeper.go
@@ -1,6 +1,7 @@
 package circuit
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/params"
 )
 
@@ -12,4 +13,29 @@ func NewKeeper(space params.Subspace) Keeper {
 	return Keeper{
 		space: space.WithKeyTable(ParamKeyTable()),
 	}
+}
+
+func (k Keeper) BreakRoute(ctx sdk.Context, msg sdk.Msg) {
+	k.space.SetWithSubkey(ctx, MsgRouteKey, []byte(msg.Route()), true)
+}
+
+func (k Keeper) BreakType(ctx sdk.Context, msg sdk.Msg) {
+	k.space.SetWithSubkey(ctx, MsgTypeKey, []byte(msg.Type()), true)
+}
+
+func (k Keeper) RecoverRoute(ctx sdk.Context, msg sdk.Msg) {
+	k.space.SetWithSubkey(ctx, MsgRouteKey, []byte(msg.Route()), false)
+}
+
+func (k Keeper) RecoverType(ctx sdk.Context, msg sdk.Msg) {
+	k.space.SetWithSubkey(ctx, MsgTypeKey, []byte(msg.Type()), false)
+}
+
+func (k Keeper) CheckMsgBreak(ctx sdk.Context, msg sdk.Msg) (brk bool) {
+	k.space.GetWithSubkeyIfExists(ctx, MsgRouteKey, []byte(msg.Route()), &brk)
+	if brk {
+		return
+	}
+	k.space.GetWithSubkeyIfExists(ctx, MsgTypeKey, []byte(msg.Type()), &brk)
+	return
 }

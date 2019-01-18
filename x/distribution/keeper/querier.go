e285cbe075f1a2c33a83fbfce900d85768c9a92c
Christopher Goes <cwgoes@pluranimity.org>
2019-01-18 11:20:21 +0100

    Parameter queries

--- x/distribution/keeper/querier.go
+++ x/distribution/keeper/querier.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -12,6 +14,11 @@ const (
 	QueryValidatorCommission = "validator_commission"
 	QueryValidatorSlashes    = "validator_slashes"
 	QueryDelegationInfo      = "delegation_info"
+
+	ParamCommunityTax        = "community_tax"
+	ParamBaseProposerReward  = "base_proposer_reward"
+	ParamBonusProposerReward = "bonus_proposer_reward"
+	ParamWithdrawAddrEnabled = "withdraw_addr_enabled"
 )
 
 func NewQuerier(k Keeper) sdk.Querier {
@@ -34,11 +41,34 @@ func NewQuerier(k Keeper) sdk.Querier {
 }
 
 func queryParams(ctx sdk.Context, path []string, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	bz, err := codec.MarshalJSONIndent(k.cdc, k.GetParams(ctx))
-	if err != nil {
-		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
+	switch path[0] {
+	case ParamCommunityTax:
+		bz, err := codec.MarshalJSONIndent(k.cdc, k.GetCommunityTax(ctx))
+		if err != nil {
+			return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
+		}
+		return bz, nil
+	case ParamBaseProposerReward:
+		bz, err := codec.MarshalJSONIndent(k.cdc, k.GetBaseProposerReward(ctx))
+		if err != nil {
+			return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
+		}
+		return bz, nil
+	case ParamBonusProposerReward:
+		bz, err := codec.MarshalJSONIndent(k.cdc, k.GetBonusProposerReward(ctx))
+		if err != nil {
+			return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
+		}
+		return bz, nil
+	case ParamWithdrawAddrEnabled:
+		bz, err := codec.MarshalJSONIndent(k.cdc, k.GetWithdrawAddrEnabled(ctx))
+		if err != nil {
+			return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
+		}
+		return bz, nil
+	default:
+		return nil, sdk.ErrUnknownRequest(fmt.Sprintf("%s is not a valid query request path", req.Path))
 	}
-	return bz, nil
 }
 
 func queryOutstandingRewards(ctx sdk.Context, path []string, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {

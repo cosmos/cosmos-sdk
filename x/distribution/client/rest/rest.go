869fa1a2512a469c222fcccdf3a26cc95b68d20c
Alessio Treglia <alessio@tendermint.com>
2019-01-30 11:32:26 -0800

    REST skeleton

---
+++ x/distribution/client/rest/rest.go
@@ -0,0 +1,11 @@
+package rest
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/gorilla/mux"
+)
+
+func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
+
+}

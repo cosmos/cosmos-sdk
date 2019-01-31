c565a6ed79fab58fa86133c2c215146b55fa6b77
Alessio Treglia <alessio@tendermint.com>
2019-01-30 19:13:36 -0800

    Implement GET /distribution/delegators/{delegatorAddr}/rewards

--- x/distribution/client/rest/query.go
+++ x/distribution/client/rest/query.go
@@ -7,6 +7,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/utils"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	distr "github.com/cosmos/cosmos-sdk/x/distribution"
 	"github.com/cosmos/cosmos-sdk/x/distribution/client/cli"
 	"github.com/gorilla/mux"
 )
@@ -14,17 +16,53 @@ import (
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router,
 	cdc *codec.Codec, queryRoute string) {
 
-	// Get the current staking parameter values
+	// Get delegator's rewards
+	r.HandleFunc(
+		"/distribution/delegators/{delegatorAddr}/rewards",
+		delegatorRewardsHandlerFn(cliCtx, cdc, queryRoute),
+	).Methods("GET")
+
+	// Get the current distribution parameter values
 	r.HandleFunc(
 		"/distribution/parameters",
 		paramsHandlerFn(cliCtx, cdc, queryRoute),
 	).Methods("GET")
 
-	// Get the current staking parameter values
+	// Get the current distribution pool
 	r.HandleFunc(
 		"/distribution/pool",
 		poolHandlerFn(cliCtx, cdc, queryRoute),
 	).Methods("GET")
+}
+
+// HTTP request handler to query delegators rewards
+func delegatorRewardsHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec,
+	queryRoute string) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// query for rewards from a particular delegator
+		delegatorAddr, err := sdk.AccAddressFromBech32(mux.Vars(r)["delegatorAddr"])
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		params := distr.NewQueryDelegationRewardsParams(delegatorAddr, nil)
+		bz, err := cdc.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		route := fmt.Sprintf("custom/%s/delegation_rewards", queryRoute)
+		res, err := cliCtx.QueryWithData(route, bz)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
+	}
 }
 
 // HTTP request handler to query the distribution params values

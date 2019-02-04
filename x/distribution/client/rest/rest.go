be16e5cdbeeff2bcad2319e59cba6863161ca788
Alessio Treglia <alessio@tendermint.com>
2019-02-04 09:02:16 -0800

    Implement POST /delegators/{delegatorAddr}/rewards/{validatorAddr}

--- x/distribution/client/rest/rest.go
+++ x/distribution/client/rest/rest.go
@@ -9,4 +9,5 @@ import (
 // RegisterRoutes register distribution REST routes.
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, queryRoute string) {
 	registerQueryRoutes(cliCtx, r, cdc, queryRoute)
+	registerTxRoutes(cliCtx, r, cdc)
 }
---
+++ x/distribution/client/rest/tx.go
@@ -0,0 +1,94 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/utils"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router,
+	cdc *codec.Codec) {
+
+	// Withdraw delegation rewards
+	r.HandleFunc(
+		"/distribution/delegators/{delegatorAddr}/rewards/{validatorAddr}",
+		withdrawDelegationRewardsHandlerFn(cdc, cliCtx),
+	).Methods("POST")
+
+}
+
+type withdrawDelegationRewardsReq struct {
+	BaseReq utils.BaseReq `json:"base_req"`
+}
+
+func withdrawDelegationRewardsHandlerFn(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req withdrawDelegationRewardsReq
+
+		if err := utils.ReadRESTReq(w, r, cdc, &req); err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		delAddr, abort := checkDelegatorAddressVar(w, r)
+		if abort {
+			return
+		}
+
+		valAddr, abort := checkValidatorAddressVar(w, r)
+		if abort {
+			return
+		}
+
+		// derive the from account address and name from the Keybase
+		fromAddress, fromName, err := context.GetFromFields(req.BaseReq.From)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		cliCtx = cliCtx.WithFromName(fromName).WithFromAddress(fromAddress)
+
+		msg := types.NewMsgWithdrawDelegatorReward(delAddr, valAddr)
+		if err := msg.ValidateBasic(); err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		if req.BaseReq.GenerateOnly {
+			utils.WriteGenerateStdTxResponse(w, cdc, req.BaseReq, []sdk.Msg{msg})
+			return
+		}
+
+		utils.CompleteAndBroadcastTxREST(w, r, cliCtx, req.BaseReq, []sdk.Msg{msg}, cdc)
+	}
+}
+
+func checkDelegatorAddressVar(w http.ResponseWriter, r *http.Request) (sdk.AccAddress, bool) {
+	addr, err := sdk.AccAddressFromBech32(mux.Vars(r)["delegatorAddr"])
+	if err != nil {
+		utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+		return nil, true
+	}
+	return addr, false
+}
+
+func checkValidatorAddressVar(w http.ResponseWriter, r *http.Request) (sdk.ValAddress, bool) {
+	addr, err := sdk.ValAddressFromBech32(mux.Vars(r)["validatorAddr"])
+	if err != nil {
+		utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+		return nil, true
+	}
+	return addr, false
+}

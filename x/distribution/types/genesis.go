70e588c891d29e4df84ff62641e51188fbd03740
Christopher Goes <cwgoes@pluranimity.org>
2019-02-05 19:55:56 +0100

    Fix math mistake (thanks @mossid)

--- x/distribution/types/genesis.go
+++ x/distribution/types/genesis.go
@@ -89,7 +89,7 @@ func DefaultGenesisState() GenesisState {
 	return GenesisState{
 		FeePool:                         InitialFeePool(),
 		CommunityTax:                    sdk.NewDecWithPrec(2, 2),         // 2%
-		ProposerReward:                  sdk.NewDec(3).Quo(sdk.NewDec(7)), // 3/7, exact, see https://github.com/cosmos/cosmos-sdk/pull/3494
+		ProposerReward:                  sdk.NewDec(3).Quo(sdk.NewDec(4)), // 3/4, exact, see https://github.com/cosmos/cosmos-sdk/pull/3494
 		WithdrawAddrEnabled:             true,
 		DelegatorWithdrawInfos:          []DelegatorWithdrawInfo{},
 		PreviousProposer:                nil,

7d2777fe34633b2876ea199f380125be95c804c6
Aleksandr Bezobchuk <alex@tendermint.com>
2019-02-05 13:35:36 -0800

    remove RunForever

--- baseapp/helpers.go
+++ baseapp/helpers.go
@@ -1,10 +1,6 @@
 package baseapp
 
 import (
-	"github.com/tendermint/tendermint/abci/server"
-	abci "github.com/tendermint/tendermint/abci/types"
-	cmn "github.com/tendermint/tendermint/libs/common"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -21,29 +17,4 @@ func (app *BaseApp) Simulate(tx sdk.Tx) (result sdk.Result) {
 // nolint
 func (app *BaseApp) Deliver(tx sdk.Tx) (result sdk.Result) {
 	return app.runTx(runTxModeDeliver, nil, tx)
-}
-
-// RunForever creates and starts an ABCI server and allows it to run
-// indefinitely listening for interrupt signals.
-func RunForever(app abci.Application) {
-	srv, err := server.NewServer("0.0.0.0:26658", "socket", app)
-	if err != nil {
-		cmn.Exit(err.Error())
-		return
-	}
-
-	err = srv.Start()
-	if err != nil {
-		cmn.Exit(err.Error())
-		return
-	}
-
-	// wait forever
-	cmn.TrapSignal(func() {
-		// cleanup
-		err := srv.Stop()
-		if err != nil {
-			cmn.Exit(err.Error())
-		}
-	})
 }

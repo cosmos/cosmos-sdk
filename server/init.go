cde15dd60334222858a9776746c7a8be79d1be73
Alessio Treglia <alessio@tendermint.com>
2019-02-07 14:09:32 -0800

    Code cleanup, small coverage boost

--- server/init.go
+++ server/init.go
@@ -1,101 +1,21 @@
 package server
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-
-	"github.com/tendermint/tendermint/crypto"
-	dbm "github.com/tendermint/tendermint/libs/db"
-	"github.com/tendermint/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
 
-	tmtypes "github.com/tendermint/tendermint/types"
-
 	clkeys "github.com/cosmos/cosmos-sdk/client/keys"
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
-
-// SimpleGenTx is a simple genesis tx
-type SimpleGenTx struct {
-	Addr sdk.AccAddress `json:"addr"`
-}
-
-//_____________________________________________________________________
-
-// Generate a genesis transaction
-func SimpleAppGenTx(cdc *codec.Codec, pk crypto.PubKey) (
-	appGenTx, cliPrint json.RawMessage, validator types.GenesisValidator, err error) {
-	var addr sdk.AccAddress
-	var secret string
-	addr, secret, err = GenerateCoinKey()
-	if err != nil {
-		return
-	}
-	var bz []byte
-	simpleGenTx := SimpleGenTx{Addr: addr}
-	bz, err = cdc.MarshalJSON(simpleGenTx)
-	if err != nil {
-		return
-	}
-	appGenTx = json.RawMessage(bz)
-	mm := map[string]string{"secret": secret}
-	bz, err = cdc.MarshalJSON(mm)
-	if err != nil {
-		return
-	}
-	cliPrint = json.RawMessage(bz)
-	validator = tmtypes.GenesisValidator{
-		PubKey: pk,
-		Power:  10,
-	}
-	return
-}
-
-// create the genesis app state
-func SimpleAppGenState(cdc *codec.Codec, genDoc tmtypes.GenesisDoc, appGenTxs []json.RawMessage) (
-	appState json.RawMessage, err error) {
-
-	if len(appGenTxs) != 1 {
-		err = errors.New("must provide a single genesis transaction")
-		return
-	}
-
-	var tx SimpleGenTx
-	err = cdc.UnmarshalJSON(appGenTxs[0], &tx)
-	if err != nil {
-		return
-	}
-
-	appState = json.RawMessage(fmt.Sprintf(`{
-  "accounts": [{
-    "address": "%s",
-    "coins": [
-      {
-        "denom": "mycoin",
-        "amount": "9007199254740992"
-      }
-    ]
-  }]
-}`, tx.Addr))
-	return
-}
-
-//___________________________________________________________________________________________
 
 // GenerateCoinKey returns the address of a public key, along with the secret
 // phrase to recover the private key.
 func GenerateCoinKey() (sdk.AccAddress, string, error) {
 
-	// construct an in-memory key store
-	keybase := keys.New(
-		dbm.NewMemDB(),
-	)
-
 	// generate a private key, with recovery phrase
-	info, secret, err := keybase.CreateMnemonic("name", keys.English, "pass", keys.Secp256k1)
+	info, secret, err := clkeys.NewInMemoryKeyBase().CreateMnemonic(
+		"name", keys.English, "pass", keys.Secp256k1)
 	if err != nil {
 		return sdk.AccAddress([]byte{}), "", err
 	}
@@ -105,7 +25,8 @@ func GenerateCoinKey() (sdk.AccAddress, string, error) {
 
 // GenerateSaveCoinKey returns the address of a public key, along with the secret
 // phrase to recover the private key.
-func GenerateSaveCoinKey(clientRoot, keyName, keyPass string, overwrite bool) (sdk.AccAddress, string, error) {
+func GenerateSaveCoinKey(clientRoot, keyName, keyPass string,
+	overwrite bool) (sdk.AccAddress, string, error) {
 
 	// get the keystore from the client
 	keybase, err := clkeys.NewKeyBaseFromDir(clientRoot)
@@ -127,6 +48,6 @@ func GenerateSaveCoinKey(clientRoot, keyName, keyPass string, overwrite bool) (s
 	if err != nil {
 		return sdk.AccAddress([]byte{}), "", err
 	}
-	addr := info.GetPubKey().Address()
-	return sdk.AccAddress(addr), secret, nil
+
+	return sdk.AccAddress(info.GetPubKey().Address()), secret, nil
 }

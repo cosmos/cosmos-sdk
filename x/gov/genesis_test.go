01a0fd42842734388c11789a06ee61910291f1fb
rigelrozanski <rigel.rozanski@gmail.com>
2019-02-12 07:42:27 -0500

    ...

--- x/gov/genesis_test.go
+++ x/gov/genesis_test.go
@@ -3,7 +3,6 @@ package gov
 import (
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/stretchr/testify/require"
 
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -77,7 +76,7 @@ func TestImportExportQueues(t *testing.T) {
 	require.True(t, keeper.GetProposal(ctx, proposalID1).GetStatus() == StatusDepositPeriod)
 	require.True(t, keeper.GetProposal(ctx, proposalID2).GetStatus() == StatusVotingPeriod)
 
-	genAccs := auth.GetAllAccounts(mapp.AccountKeeper, ctx)
+	genAccs := mapp.AccountKeeper.GetAllAccounts(ctx)
 
 	// Export the state and import it into a new Mock App
 	genState := ExportGenesis(ctx, keeper)

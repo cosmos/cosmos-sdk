6ab40d893e397e9e334b68c83893e0605d6d9b08
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-02-20 10:51:58 -0500

    Update tx encoding endpoint

--- client/tx/root.go
+++ client/tx/root.go
@@ -12,5 +12,5 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) 
 	r.HandleFunc("/txs/{hash}", QueryTxRequestHandlerFn(cdc, cliCtx)).Methods("GET")
 	r.HandleFunc("/txs", SearchTxRequestHandlerFn(cliCtx, cdc)).Methods("GET")
 	r.HandleFunc("/txs", BroadcastTxRequest(cliCtx, cdc)).Methods("POST")
-	r.HandleFunc("/tx/encode", EncodeTxRequestHandlerFn(cdc, cliCtx)).Methods("POST")
+	r.HandleFunc("/txs/encode", EncodeTxRequestHandlerFn(cdc, cliCtx)).Methods("POST")
 }

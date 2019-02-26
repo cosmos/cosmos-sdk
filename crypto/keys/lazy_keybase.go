4bb720bd4d85014009138606dabc49e7c18d9b35
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-02-25 19:54:40 -0500

    Revert back to NewGoLevelDB

--- crypto/keys/lazy_keybase.go
+++ crypto/keys/lazy_keybase.go
@@ -2,8 +2,6 @@ package keys
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/tendermint/tendermint/crypto"
 	cmn "github.com/tendermint/tendermint/libs/common"
@@ -30,7 +28,7 @@ func New(name, dir string) Keybase {
 }
 
 func (lkb lazyKeybase) List() ([]Info, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -40,7 +38,7 @@ func (lkb lazyKeybase) List() ([]Info, error) {
 }
 
 func (lkb lazyKeybase) Get(name string) (Info, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -50,7 +48,7 @@ func (lkb lazyKeybase) Get(name string) (Info, error) {
 }
 
 func (lkb lazyKeybase) GetByAddress(address types.AccAddress) (Info, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -60,7 +58,7 @@ func (lkb lazyKeybase) GetByAddress(address types.AccAddress) (Info, error) {
 }
 
 func (lkb lazyKeybase) Delete(name, passphrase string, skipPass bool) error {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
@@ -70,7 +68,7 @@ func (lkb lazyKeybase) Delete(name, passphrase string, skipPass bool) error {
 }
 
 func (lkb lazyKeybase) Sign(name, passphrase string, msg []byte) ([]byte, crypto.PubKey, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -80,7 +78,7 @@ func (lkb lazyKeybase) Sign(name, passphrase string, msg []byte) ([]byte, crypto
 }
 
 func (lkb lazyKeybase) CreateMnemonic(name string, language Language, passwd string, algo SigningAlgo) (info Info, seed string, err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, "", err
 	}
@@ -90,7 +88,7 @@ func (lkb lazyKeybase) CreateMnemonic(name string, language Language, passwd str
 }
 
 func (lkb lazyKeybase) CreateAccount(name, mnemonic, bip39Passwd, encryptPasswd string, account uint32, index uint32) (Info, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -100,7 +98,7 @@ func (lkb lazyKeybase) CreateAccount(name, mnemonic, bip39Passwd, encryptPasswd 
 }
 
 func (lkb lazyKeybase) Derive(name, mnemonic, bip39Passwd, encryptPasswd string, params hd.BIP44Params) (Info, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -110,7 +108,7 @@ func (lkb lazyKeybase) Derive(name, mnemonic, bip39Passwd, encryptPasswd string,
 }
 
 func (lkb lazyKeybase) CreateLedger(name string, algo SigningAlgo, account uint32, index uint32) (info Info, err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -120,7 +118,7 @@ func (lkb lazyKeybase) CreateLedger(name string, algo SigningAlgo, account uint3
 }
 
 func (lkb lazyKeybase) CreateOffline(name string, pubkey crypto.PubKey) (info Info, err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -130,7 +128,7 @@ func (lkb lazyKeybase) CreateOffline(name string, pubkey crypto.PubKey) (info In
 }
 
 func (lkb lazyKeybase) Update(name, oldpass string, getNewpass func() (string, error)) error {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
@@ -140,7 +138,7 @@ func (lkb lazyKeybase) Update(name, oldpass string, getNewpass func() (string, e
 }
 
 func (lkb lazyKeybase) Import(name string, armor string) (err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
@@ -150,7 +148,7 @@ func (lkb lazyKeybase) Import(name string, armor string) (err error) {
 }
 
 func (lkb lazyKeybase) ImportPubKey(name string, armor string) (err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
@@ -160,7 +158,7 @@ func (lkb lazyKeybase) ImportPubKey(name string, armor string) (err error) {
 }
 
 func (lkb lazyKeybase) Export(name string) (armor string, err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return "", err
 	}
@@ -170,7 +168,7 @@ func (lkb lazyKeybase) Export(name string) (armor string, err error) {
 }
 
 func (lkb lazyKeybase) ExportPubKey(name string) (armor string, err error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return "", err
 	}
@@ -180,7 +178,7 @@ func (lkb lazyKeybase) ExportPubKey(name string) (armor string, err error) {
 }
 
 func (lkb lazyKeybase) ExportPrivateKeyObject(name string, passphrase string) (crypto.PrivKey, error) {
-	db, err := lkb.newGoLevelDB()
+	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
@@ -190,34 +188,3 @@ func (lkb lazyKeybase) ExportPrivateKeyObject(name string, passphrase string) (c
 }
 
 func (lkb lazyKeybase) CloseDB() {}
-
-// newGoLevelDB returns a new reference to a GoLevelDB instance with the
-// appropriate file permissions overridden. An error is returned if the DB
-// cannot be opened or if updating the file permissions fails. In the latter
-// case, the DB is closed. If no error is returned, it is up to the caller to
-// close the DB connection handle.
-func (lkb lazyKeybase) newGoLevelDB() (*dbm.GoLevelDB, error) {
-	db, err := dbm.NewGoLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-
-	// if err := chmodR(lkb.dir, 0700); err != nil {
-	// 	db.Close()
-	// 	return nil, err
-	// }
-
-	return db, nil
-}
-
-// chmodR recursively invokes chmod with a given permissions os.FileMode on
-// path and all of its contents. An error is returned if walking the directory
-// path fails or any chmod call fails.
-func chmodR(path string, mode os.FileMode) error {
-	return filepath.Walk(path, func(name string, _ os.FileInfo, err error) error {
-		if err == nil {
-			err = os.Chmod(name, mode)
-		}
-		return err
-	})
-}

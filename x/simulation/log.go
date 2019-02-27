93b64a6aa8e7ba24921ab0ebb222e26032cc27af
rigelrozanski <rigel.rozanski@gmail.com>
2019-02-27 01:56:15 -0500

    logger overhaul
    int

--- x/simulation/log.go
+++ x/simulation/log.go
@@ -4,72 +4,98 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"strings"
 	"time"
 )
 
-// Builds a function to add logs for this particular block
-func addLogMessage(testingmode bool,
-	blockLogBuilders []*strings.Builder, height int) func(string) {
+// log writter
+type LogWriter interface {
+	AddEntry(OperationEntry)
+	PrintLogs()
+}
 
+// LogWriter - return a dummy or standard log writer given the testingmode
+func NewLogWriter(testingmode bool) LogWriter {
 	if !testingmode {
-		return func(_ string) {}
+		return &DummyLogWriter{}
 	}
+	return &StandardLogWriter{}
+}
 
-	blockLogBuilders[height] = &strings.Builder{}
-	return func(x string) {
-		(*blockLogBuilders[height]).WriteString(x)
-		(*blockLogBuilders[height]).WriteString("\n")
+// log writter
+type StandardLogWriter struct {
+	OpEntries []OperationEntry `json:"op_entries"`
+}
+
+// add an entry to the log writter
+func (lw *StandardLogWriter) AddEntry(opEntry OperationEntry) {
+	lw.OpEntries = append(lw.OpEntries, opEntry)
+}
+
+// PrintLogs - print the logs to a simulation file
+func (lw *StandardLogWriter) PrintLogs() {
+	f := createLogFile()
+	for i := 0; i < len(lw.OpEntries); i++ {
+		writeEntry := fmt.Sprintf("%s\n", (lw.OpEntries[i]).MustMarshal())
+		_, err := f.WriteString(writeEntry)
+		if err != nil {
+			panic("Failed to write logs to file")
+		}
 	}
 }
 
-// Creates a function to print out the logs
-func logPrinter(testingmode bool, logs []*strings.Builder) func() {
-	if !testingmode {
-		return func() {}
+//// Builds a function to append logs
+//func getLogWriter(testingmode bool, opEntries []*OperationEntry) func(OperationEntry) {
+
+//if !testingmode {
+//return func(_ OperationEntry) {}
+//}
+
+//return func(opEntry OperationEntry) {
+//opEntries = append(opEntries, opEntry)
+//}
+//}
+
+//// Creates a function to print out the logs
+//func logPrinter(testingmode bool, opEntries []*OperationEntry) func() {
+//if !testingmode {
+//return func() {}
+//}
+
+//return func() {
+//f := createLogFile()
+//_, _ = f.WriteString(fmt.Sprintf("debug opEntries: %v\n", opEntries))
+//for i := 0; i < len(opEntries); i++ {
+//writeEntry := fmt.Sprintf("%s\n", (*opEntries[i]).MustMarshal())
+//_, err := f.WriteString(writeEntry)
+//if err != nil {
+//panic("Failed to write logs to file")
+//}
+//}
+//}
+//}
+
+func createLogFile() *os.File {
+	var f *os.File
+	fileName := fmt.Sprintf("%s.log", time.Now().Format("2006-01-02_15:04:05"))
+
+	folderPath := os.ExpandEnv("$HOME/.gaiad/simulations")
+	filePath := path.Join(folderPath, fileName)
+
+	err := os.MkdirAll(folderPath, os.ModePerm)
+	if err != nil {
+		panic(err)
 	}
-
-	return func() {
-		numLoggers := 0
-		for i := 0; i < len(logs); i++ {
-			// We're passed the last created block
-			if logs[i] == nil {
-				numLoggers = i
-				break
-			}
-		}
-
-		var f *os.File
-		if numLoggers > 10 {
-			fileName := fmt.Sprintf("%s.txt", time.Now().Format("2006-01-02_15:04:05"))
-
-			folderPath := os.ExpandEnv("$HOME/.gaiad/simulations")
-			filePath := path.Join(folderPath, fileName)
-			fmt.Printf("Too many logs to display, instead writing to %s\n", filePath)
-
-			err := os.MkdirAll(folderPath, os.ModePerm)
-			if err != nil {
-				panic(err)
-			}
-			f, _ = os.Create(filePath)
-		}
-
-		for i := 0; i < numLoggers; i++ {
-			if f == nil {
-				fmt.Printf("Begin block %d\n", i+1)
-				fmt.Println((*logs[i]).String())
-				continue
-			}
-
-			_, err := f.WriteString(fmt.Sprintf("Begin block %d\n", i+1))
-			if err != nil {
-				panic("Failed to write logs to file")
-			}
-
-			_, err = f.WriteString((*logs[i]).String())
-			if err != nil {
-				panic("Failed to write logs to file")
-			}
-		}
-	}
+	f, _ = os.Create(filePath)
+	fmt.Printf("Logs to writing to %s\n", filePath)
+	return f
 }
+
+//_____________________
+// dummy log writter
+type DummyLogWriter struct{}
+
+// do nothing
+func (lw *DummyLogWriter) AddEntry(_ OperationEntry) {}
+
+// do nothing
+func (lw *DummyLogWriter) PrintLogs() {}

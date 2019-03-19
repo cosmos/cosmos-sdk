d050daffa91298dfe93db63b2ee3bf3cbaede006
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-03-19 09:12:16 -0700

    Add comments to unit tests

--- x/staking/types/commission_test.go
+++ x/staking/types/commission_test.go
@@ -14,12 +14,19 @@ func TestCommissionValidate(t *testing.T) {
 		input     Commission
 		expectErr bool
 	}{
+		// invalid commission; max rate < 0%
 		{NewCommission(sdk.ZeroDec(), sdk.MustNewDecFromStr("-1.00"), sdk.ZeroDec()), true},
+		// invalid commission; max rate > 100%
 		{NewCommission(sdk.ZeroDec(), sdk.MustNewDecFromStr("2.00"), sdk.ZeroDec()), true},
+		// invalid commission; rate < 0%
 		{NewCommission(sdk.MustNewDecFromStr("-1.00"), sdk.ZeroDec(), sdk.ZeroDec()), true},
+		// invalid commission; rate > max rate
 		{NewCommission(sdk.MustNewDecFromStr("0.75"), sdk.MustNewDecFromStr("0.50"), sdk.ZeroDec()), true},
+		// invalid commission; max change rate < 0%
 		{NewCommission(sdk.OneDec(), sdk.OneDec(), sdk.MustNewDecFromStr("-1.00")), true},
+		// invalid commission; max change rate > max rate
 		{NewCommission(sdk.OneDec(), sdk.MustNewDecFromStr("0.75"), sdk.MustNewDecFromStr("0.90")), true},
+		// valid commission
 		{NewCommission(sdk.MustNewDecFromStr("0.20"), sdk.OneDec(), sdk.MustNewDecFromStr("0.10")), false},
 	}
 
@@ -40,11 +47,17 @@ func TestCommissionValidateNewRate(t *testing.T) {
 		blockTime time.Time
 		expectErr bool
 	}{
+		// invalid new commission rate; last update < 24h ago
 		{c1, sdk.MustNewDecFromStr("0.50"), now, true},
+		// invalid new commission rate; new rate < 0%
 		{c1, sdk.MustNewDecFromStr("-1.00"), now.Add(48 * time.Hour), true},
+		// invalid new commission rate; new rate > max rate
 		{c1, sdk.MustNewDecFromStr("0.90"), now.Add(48 * time.Hour), true},
+		// invalid new commission rate; new rate > max change rate
 		{c1, sdk.MustNewDecFromStr("0.60"), now.Add(48 * time.Hour), true},
+		// valid commission
 		{c1, sdk.MustNewDecFromStr("0.50"), now.Add(48 * time.Hour), false},
+		// valid commission
 		{c1, sdk.MustNewDecFromStr("0.10"), now.Add(48 * time.Hour), false},
 	}
 

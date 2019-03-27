4fda18de00946c349714ff43d833a1855df1c452
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-03-27 13:12:30 +0100

    test ParseHTTPArgs
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- types/rest/rest_test.go
+++ types/rest/rest_test.go
@@ -55,3 +55,72 @@ func TestBaseReqValidateBasic(t *testing.T) {
 		})
 	}
 }
+
+func TestParseHTTPArgs(t *testing.T) {
+	req0, err := http.NewRequest("", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req1, err := http.NewRequest("", "/?limit=5", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req2, err := http.NewRequest("", "/?page=5", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req3, err := http.NewRequest("", "/?page=5&limit=5", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	reqE1, err := http.NewRequest("", "/?page=-1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	reqE2, err := http.NewRequest("", "/?limit=-1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req4, err := http.NewRequest("", "/?foo=faa", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = req4.ParseForm()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name  string
+		req   *http.Request
+		w     http.ResponseWriter
+		tags  []string
+		page  int
+		limit int
+		err   bool
+	}{
+		{"no params", req0, httptest.NewRecorder(), []string{}, DefaultPage, DefaultLimit, false},
+		{"Limit", req1, httptest.NewRecorder(), []string{}, DefaultPage, 5, false},
+		{"Page", req2, httptest.NewRecorder(), []string{}, 5, DefaultLimit, false},
+		{"Page and limit", req3, httptest.NewRecorder(), []string{}, 5, 5, false},
+
+		{"error page 0", reqE1, httptest.NewRecorder(), []string{}, DefaultPage, DefaultLimit, true},
+		{"error limit 0", reqE2, httptest.NewRecorder(), []string{}, DefaultPage, DefaultLimit, true},
+
+		{"tags", req4, httptest.NewRecorder(), []string{"foo='faa'"}, DefaultPage, DefaultLimit, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tags, page, limit, err := ParseHTTPArgs(tt.req)
+			if tt.err {
+				require.NotNil(t, err)
+			} else {
+				require.Nil(t, err)
+				require.Equal(t, tt.tags, tags)
+				require.Equal(t, tt.page, page)
+				require.Equal(t, tt.limit, limit)
+			}
+		})
+	}
+}

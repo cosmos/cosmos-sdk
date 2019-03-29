cbcec4953e053ffd136fa31c814e2ef0aeb39994
Aaron Craelius <aaron@regen.network>
2019-03-28 22:16:54 -0400

    Convert Keeper to an interface and return underlying struct as pointer
    Because a cache is being kept on the keeper itself currently, it must
    get passed around as a pointer so that the cache remains consistent.

--- x/upgrade/keeper.go
+++ x/upgrade/keeper.go
@@ -8,7 +8,33 @@ import (
 )
 
 // Keeper of the upgrade module
-type Keeper struct {
+type Keeper interface {
+	// ScheduleUpgrade schedules an upgrade based on the specified plan
+	ScheduleUpgrade(ctx sdk.Context, plan Plan) sdk.Error
+
+	// SetUpgradeHandler sets an UpgradeHandler for the upgrade specified by name. This handler will be called when the upgrade
+	// with this name is applied. In order for an upgrade with the given name to proceed, a handler for this upgrade
+	// must be set even if it is a no-op function.
+	SetUpgradeHandler(name string, upgradeHandler Handler)
+
+	// ClearUpgradePlan clears any schedule upgrade
+	ClearUpgradePlan(ctx sdk.Context)
+
+	// GetUpgradePlan returns the currently scheduled Plan if any, setting havePlan to true if there is a scheduled
+	// upgrade or false if there is none
+	GetUpgradePlan(ctx sdk.Context) (plan Plan, havePlan bool)
+
+	// SetDoShutdowner sets a custom shutdown function for the upgrade module. This shutdown
+	// function will be called during the BeginBlock method when an upgrade is required
+	// instead of panic'ing which is the default behavior
+	SetDoShutdowner(doShutdowner func(ctx sdk.Context, plan Plan))
+
+	// BeginBlocker should be called inside the BeginBlocker method of any app using the upgrade module. Scheduled upgrade
+	// plans are cached in memory so the overhead of this method is trivial.
+	BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock)
+}
+
+type keeper struct {
 	storeKey        sdk.StoreKey
 	cdc             *codec.Codec
 	doShutdowner    func(sdk.Context, Plan)
@@ -25,22 +51,18 @@ const (
 
 // NewKeeper constructs an upgrade keeper
 func NewKeeper(storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
-	return Keeper{
+	return &keeper{
 		storeKey:        storeKey,
 		cdc:             cdc,
 		upgradeHandlers: map[string]Handler{},
 	}
 }
 
-// SetUpgradeHandler sets an UpgradeHandler for the upgrade specified by name. This handler will be called when the upgrade
-// with this name is applied. In order for an upgrade with the given name to proceed, a handler for this upgrade
-// must be set even if it is a no-op function.
-func (keeper *Keeper) SetUpgradeHandler(name string, upgradeHandler Handler) {
+func (keeper *keeper) SetUpgradeHandler(name string, upgradeHandler Handler) {
 	keeper.upgradeHandlers[name] = upgradeHandler
 }
 
-// ScheduleUpgrade schedules an upgrade based on the specified plan
-func (keeper *Keeper) ScheduleUpgrade(ctx sdk.Context, plan Plan) sdk.Error {
+func (keeper *keeper) ScheduleUpgrade(ctx sdk.Context, plan Plan) sdk.Error {
 	err := plan.ValidateBasic()
 	if err != nil {
 		return err
@@ -67,8 +89,7 @@ func (keeper *Keeper) ScheduleUpgrade(ctx sdk.Context, plan Plan) sdk.Error {
 	return nil
 }
 
-// ClearUpgradePlan clears any schedule upgrade
-func (keeper *Keeper) ClearUpgradePlan(ctx sdk.Context) {
+func (keeper *keeper) ClearUpgradePlan(ctx sdk.Context) {
 	store := ctx.KVStore(keeper.storeKey)
 	keeper.haveCache = false
 	store.Delete([]byte(PlanKey))
@@ -83,9 +104,7 @@ func (plan Plan) ValidateBasic() sdk.Error {
 	return nil
 }
 
-// GetUpgradePlan returns the currently scheduled Plan if any, setting havePlan to true if there is a scheduled
-// upgrade or false if there is none
-func (keeper *Keeper) GetUpgradePlan(ctx sdk.Context) (plan Plan, havePlan bool) {
+func (keeper *keeper) GetUpgradePlan(ctx sdk.Context) (plan Plan, havePlan bool) {
 	store := ctx.KVStore(keeper.storeKey)
 	bz := store.Get([]byte(PlanKey))
 	if bz == nil {
@@ -95,10 +114,7 @@ func (keeper *Keeper) GetUpgradePlan(ctx sdk.Context) (plan Plan, havePlan bool)
 	return plan, true
 }
 
-// SetDoShutdowner sets a custom shutdown function for the upgrade module. This shutdown
-// function will be called during the BeginBlock method when an upgrade is required
-// instead of panic'ing which is the default behavior
-func (keeper *Keeper) SetDoShutdowner(doShutdowner func(ctx sdk.Context, plan Plan)) {
+func (keeper *keeper) SetDoShutdowner(doShutdowner func(ctx sdk.Context, plan Plan)) {
 	keeper.doShutdowner = doShutdowner
 }
 
@@ -106,9 +122,7 @@ func upgradeDoneKey(name string) []byte {
 	return []byte(fmt.Sprintf("done/%s", name))
 }
 
-// BeginBlocker should be called inside the BeginBlocker method of any app using the upgrade module. Scheduled upgrade
-// plans are cached in memory so the overhead of this method is trivial.
-func (keeper *Keeper) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) {
+func (keeper *keeper) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) {
 	blockTime := ctx.BlockHeader().Time
 	blockHeight := ctx.BlockHeight()
 

49448d0b50ab9ce8ee3f89184d930361427d7729
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-03-31 14:24:38 -0400

    Implement initial minting querier

---
+++ x/mint/querier.go
@@ -0,0 +1,38 @@
+package mint
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// Query endpoints supported by the minting querier
+const (
+	QueryParameters = "parameters"
+)
+
+// NewQuerier returns a minting Querier handler.
+func NewQuerier(k Keeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, _ abci.RequestQuery) ([]byte, sdk.Error) {
+		switch path[0] {
+		case QueryParameters:
+			return queryParams(ctx, k)
+
+		default:
+			return nil, sdk.ErrUnknownRequest(fmt.Sprintf("unknown auth query endpoint: %s", path[0]))
+		}
+	}
+}
+
+func queryParams(ctx sdk.Context, k Keeper) ([]byte, sdk.Error) {
+	params := k.GetParams(ctx)
+
+	res, err := codec.MarshalJSONIndent(k.cdc, params)
+	if err != nil {
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("failed to marshal JSON", err.Error()))
+	}
+
+	return res, nil
+}

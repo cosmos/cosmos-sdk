c11124a9ac8828434ecc35203aeac102940615e8
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-03-31 14:25:00 -0400

    Minor cleanup

--- x/mint/minter.go
+++ x/mint/minter.go
@@ -73,7 +73,7 @@ func (m Minter) NextInflationRate(params Params, bondedRatio sdk.Dec) sdk.Dec {
 
 // NextAnnualProvisions returns the annual provisions based on current total
 // supply and inflation rate.
-func (m Minter) NextAnnualProvisions(params Params, totalSupply sdk.Int) sdk.Dec {
+func (m Minter) NextAnnualProvisions(_ Params, totalSupply sdk.Int) sdk.Dec {
 	return m.Inflation.MulInt(totalSupply)
 }
 

334b10bc01e2193f8a5d36a9c9744adc24bffa2d
Federico Kunze <federico.kunze94@gmail.com>
2019-04-08 10:50:49 +0200

    add missing tag action for governance

--- x/gov/tags/tags.go
+++ x/gov/tags/tags.go
@@ -1,11 +1,16 @@
 package tags
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // Governance tags
 var (
 	ActionProposalDropped  = "proposal-dropped"
 	ActionProposalPassed   = "proposal-passed"
 	ActionProposalRejected = "proposal-rejected"
 
+	Action            = sdk.TagAction
 	Proposer          = "proposer"
 	ProposalID        = "proposal-id"
 	VotingPeriodStart = "voting-period-start"

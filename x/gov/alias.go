b336398a6cdc208ccfbaf25bf006279edb3819c4
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-04-18 16:55:34 -0400

    Update gov aliases

--- x/gov/alias.go
+++ x/gov/alias.go
@@ -3,7 +3,41 @@ package gov
 
 import "github.com/cosmos/cosmos-sdk/x/gov/types"
 
+const (
+	StatusNil           = types.StatusNil
+	StatusDepositPeriod = types.StatusDepositPeriod
+	StatusVotingPeriod  = types.StatusVotingPeriod
+	StatusPassed        = types.StatusPassed
+	StatusRejected      = types.StatusRejected
+)
+
 type (
-	Content = types.Content
-	Handler = types.Handler
+	Content  = types.Content
+	Handler  = types.Handler
+	Proposal = types.Proposal
+
+	Deposit    = types.Deposit
+	Vote       = types.Vote
+	VoteOption = types.VoteOption
+
+	ProposalStatus = types.ProposalStatus
+)
+
+var (
+	ErrUnknownProposal         = types.ErrUnknownProposal
+	ErrInactiveProposal        = types.ErrInactiveProposal
+	ErrAlreadyActiveProposal   = types.ErrAlreadyActiveProposal
+	ErrAlreadyFinishedProposal = types.ErrAlreadyFinishedProposal
+	ErrAddressNotStaked        = types.ErrAddressNotStaked
+	ErrInvalidTitle            = types.ErrInvalidTitle
+	ErrInvalidDescription      = types.ErrInvalidDescription
+	ErrInvalidProposalType     = types.ErrInvalidProposalType
+	ErrInvalidVote             = types.ErrInvalidVote
+	ErrInvalidGenesis          = types.ErrInvalidGenesis
+	ErrNoProposalHandlerExists = types.ErrNoProposalHandlerExists
+
+	NewProposal = types.NewProposal
+
+	ValidVoteOption     = types.ValidVoteOption
+	ValidProposalStatus = types.ValidProposalStatus
 )

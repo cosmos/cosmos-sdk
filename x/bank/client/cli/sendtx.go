93e8f467fedcc99028974431dedbd111e8037c57
Alessio Treglia <quadrispro@ubuntu.com>
2019-04-18 19:48:52 +0100

    Turn --from into a positional argument in gaiacli tx send (#4146)
    Closes: #4142

--- x/bank/client/cli/sendtx.go
+++ x/bank/client/cli/sendtx.go
@@ -20,12 +20,12 @@ const (
 // SendTxCmd will create a send tx and sign it with the given key.
 func SendTxCmd(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "send [to_address] [amount]",
+		Use:   "send [from_key_or_address] [to_address] [amount]",
 		Short: "Create and sign a send tx",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			txBldr := authtxb.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
-			cliCtx := context.NewCLIContext().
+			cliCtx := context.NewCLIContextWithFrom(args[0]).
 				WithCodec(cdc).
 				WithAccountDecoder(cdc)
 
@@ -33,27 +33,24 @@ func SendTxCmd(cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			to, err := sdk.AccAddressFromBech32(args[0])
+			to, err := sdk.AccAddressFromBech32(args[1])
 			if err != nil {
 				return err
 			}
 
 			// parse coins trying to be sent
-			coins, err := sdk.ParseCoins(args[1])
+			coins, err := sdk.ParseCoins(args[2])
 			if err != nil {
 				return err
 			}
 
-			from := cliCtx.GetFromAddress()
-
 			// build and sign the transaction, then broadcast to Tendermint
-			msg := bank.NewMsgSend(from, to, coins)
+			msg := bank.NewMsgSend(cliCtx.GetFromAddress(), to, coins)
 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg}, false)
 		},
 	}
 
 	cmd = client.PostCommands(cmd)[0]
-	cmd.MarkFlagRequired(client.FlagFrom)
 
 	return cmd
 }

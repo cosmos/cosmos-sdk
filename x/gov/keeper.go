cc5022fb63965ba401c5021477f57b553556b3f6
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-04-18 15:28:08 -0400

    Move keys to types package

--- x/gov/keeper.go
+++ x/gov/keeper.go
@@ -11,23 +11,6 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 )
 
-const (
-	// ModuleKey is the name of the module
-	ModuleName = "gov"
-
-	// StoreKey is the store key string for gov
-	StoreKey = ModuleName
-
-	// RouterKey is the message route for gov
-	RouterKey = ModuleName
-
-	// QuerierRoute is the querier route for gov
-	QuerierRoute = ModuleName
-
-	// Parameter store default namestore
-	DefaultParamspace = ModuleName
-)
-
 // Parameter store key
 var (
 	ParamStoreKeyDepositParams = []byte("depositparams")
---
+++ x/gov/types/keys.go
@@ -0,0 +1,18 @@
+package types
+
+const (
+	// ModuleKey is the name of the module
+	ModuleName = "gov"
+
+	// StoreKey is the store key string for gov
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for gov
+	RouterKey = ModuleName
+
+	// QuerierRoute is the querier route for gov
+	QuerierRoute = ModuleName
+
+	// Parameter store default namestore
+	DefaultParamspace = ModuleName
+)

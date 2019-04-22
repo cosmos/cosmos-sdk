9caf75a4261386e4ebb4aa036bafd8c60b8b40f9
wind <xing.guo@okcoin.com>
2019-04-22 16:54:34 +0800

    modify coin marshal

--- types/coin.go
+++ types/coin.go
@@ -51,6 +51,31 @@ func (coin Coin) String() string {
 	return fmt.Sprintf("%v%v", coin.Amount, coin.Denom)
 }
 
+// MarshalJSON marshals the coin
+func (coin Coin) MarshalJSON() ([]byte, error) {
+	type Alias Coin
+	return json.Marshal(&struct{
+		Denom string `json:"denom"`
+		Amount Dec `json:"amount"`
+	}{
+		coin.Denom,
+		NewDecFromIntWithPrec(coin.Amount, Precision),
+	})
+}
+
+func (coin *Coin) UnmarshalJSON(data []byte) error {
+	c := &struct{
+		Denom string `json:"denom"`
+		Amount Dec `json:"amount"`
+	}{}
+	if err := json.Unmarshal(data, c); err != nil {
+		return err
+	}
+	coin.Denom = c.Denom
+	coin.Amount = c.Amount.RoundInt()
+	return nil
+}
+
 // IsZero returns if this represents no money
 func (coin Coin) IsZero() bool {
 	return coin.Amount.IsZero()

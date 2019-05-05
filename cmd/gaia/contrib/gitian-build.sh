48d4deb81bc44ae2815eea9586ea6adc95c58e3c
Alessio Treglia <quadrispro@ubuntu.com>
2019-05-05 13:17:07 +0100

    Tested with linux 386

--- cmd/gaia/contrib/gitian-build.sh
+++ cmd/gaia/contrib/gitian-build.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 f_main() {
   local l_workdir \
-    l_gitian \
     l_sdk \
     l_commit \
     l_platform
@@ -17,16 +16,18 @@ f_main() {
 
   l_workdir="$(pwd)/gitian-build-$(date +%Y-%m-%d-%H%M%S)"
   mkdir ${l_workdir}/
-  echo "Work directory: ${l_workdir}, log: ${l_workdir}/log" >&2
-  pushd ${l_workdir}
-  git clone https://github.com/devrandom/gitian-builder >${l_workdir}/log
-  l_gitian="$(pwd)/gitian-builder"
+  echo "Download gitian" >&2
+  git clone https://github.com/devrandom/gitian-builder ${l_workdir}
 
-  f_prep_docker_image "${l_gitian}" >${l_workdir}/log
-  f_download_go "${l_gitian}" >${l_workdir}/log
+  echo "Prepare gitian-target docker image" >&2
+  f_prep_docker_image "${l_workdir}" >${l_workdir}/log
 
-  f_build "${l_gitian}" "${l_sdk}" "${l_commit}" "${l_platform}" >${l_workdir}/log
-  echo "You may find the result in ${l_gitian}/result" >&2
+  echo "Download Go" >&2
+  f_download_go "${l_workdir}" >${l_workdir}/log
+
+  echo "Start the build" >&2
+  f_build "${l_workdir}" "${l_sdk}" "${l_commit}" "${l_platform}" >${l_workdir}/log
+  echo "You may find the result in ${l_workdir}/result" >&2
   return 0
 }
 
@@ -64,5 +65,23 @@ f_build() {
   bin/gbuild $l_descriptor --commit cosmos-sdk=$l_commit
   libexec/stop-target || echo "warning: couldn't stop target" >&2
 }
+
+f_help() {
+  cat >&2 <<EOF
+Usage: $(basename $0) [-h] GOOS GIT_REPO
+Launch a gitian build from the local clone of cosmos-sdk available at GIT_REPO.
+
+  Options:
+   -h        Display this help and exit
+EOF
+}
+
+while getopts ":d:hnv" opt; do
+  case "${opt}" in
+    h)  f_help ; exit 0 ;;
+  esac
+done
+
+shift "$((OPTIND-1))"
 
 f_main $1 $2

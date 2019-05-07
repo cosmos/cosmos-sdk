acfaf3bfa362ea7a1f0dbed4af625dd458334828
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-05-07 12:51:33 -0400

    Add querySigningInfo to the slashing querier w/ params

--- x/slashing/querier.go
+++ x/slashing/querier.go
@@ -1,6 +1,8 @@
 package slashing
 
 import (
+	"fmt"
+
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -9,7 +11,8 @@ import (
 
 // Query endpoints supported by the slashing querier
 const (
-	QueryParameters = "parameters"
+	QueryParameters   = "parameters"
+	QuerySigningInfos = "signingInfos"
 )
 
 // NewQuerier creates a new querier for slashing clients.
@@ -18,6 +21,10 @@ func NewQuerier(k Keeper, cdc *codec.Codec) sdk.Querier {
 		switch path[0] {
 		case QueryParameters:
 			return queryParams(ctx, cdc, k)
+
+		case QuerySigningInfos:
+			return querySigningInfo(ctx, cdc, req, k)
+
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown staking query endpoint")
 		}
@@ -30,6 +37,58 @@ func queryParams(ctx sdk.Context, cdc *codec.Codec, k Keeper) ([]byte, sdk.Error
 	res, err := codec.MarshalJSONIndent(cdc, params)
 	if err != nil {
 		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("failed to marshal JSON", err.Error()))
+	}
+
+	return res, nil
+}
+
+// QuerySigningInfosParams defines the params for the following queries:
+// - 'custom/slashing/signingInfos'
+type QuerySigningInfosParams struct {
+	Page, Limit int
+}
+
+func NewQuerySigningInfosParams(page, limit int) QuerySigningInfosParams {
+	return QuerySigningInfosParams{page, limit}
+}
+
+func querySigningInfo(ctx sdk.Context, cdc *codec.Codec, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
+	var params QuerySigningInfosParams
+
+	err := cdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
+	}
+
+	if params.Limit == 0 {
+		// set the default limit to max bonded if no limit was provided
+		params.Limit = int(k.validatorSet.MaxValidators(ctx))
+	}
+
+	var signingInfos []ValidatorSigningInfo
+
+	k.IterateValidatorSigningInfos(ctx, func(consAddr sdk.ConsAddress, info ValidatorSigningInfo) (stop bool) {
+		signingInfos = append(signingInfos, info)
+		return false
+	})
+
+	// get pagination bounds
+	start := (params.Page - 1) * params.Limit
+	end := params.Limit + start
+	if end >= len(signingInfos) {
+		end = len(signingInfos)
+	}
+
+	if start >= len(signingInfos) {
+		// page is out of bounds
+		signingInfos = []ValidatorSigningInfo{}
+	} else {
+		signingInfos = signingInfos[start:end]
+	}
+
+	res, err := codec.MarshalJSONIndent(cdc, signingInfos)
+	if err != nil {
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("failed to JSON marshal result: %s", err.Error()))
 	}
 
 	return res, nil

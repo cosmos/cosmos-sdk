8948d80c95269898cf46da291d565de9803e108f
rigelrozanski <rigel.rozanski@gmail.com>
2019-05-07 21:06:07 -0400

    BasicGaiaApp -> ModuleBasics

--- cmd/gaia/app/genesis.go
+++ cmd/gaia/app/genesis.go
@@ -15,5 +15,5 @@ type GenesisState map[string]json.RawMessage
 
 // NewDefaultGenesisState generates the default state for gaia.
 func NewDefaultGenesisState() GenesisState {
-	return BasicGaiaApp.DefaultGenesis()
+	return ModuleBasics.DefaultGenesis()
 }
--- cmd/gaia/cmd/gaiad/main.go
+++ cmd/gaia/cmd/gaiad/main.go
@@ -47,12 +47,12 @@ func main() {
 		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
 	}
 
-	rootCmd.AddCommand(genutilcli.InitCmd(ctx, cdc, app.BasicGaiaApp))
-	rootCmd.AddCommand(genutilcli.CollectGenTxsCmd(ctx, cdc, app.BasicGaiaApp, genaccounts.AppModuleBasic{}))
-	rootCmd.AddCommand(genutilcli.GenTxCmd(ctx, cdc, app.BasicGaiaApp, genaccounts.AppModuleBasic{}, app.DefaultNodeHome, app.DefaultCLIHome))
-	rootCmd.AddCommand(genutilcli.ValidateGenesisCmd(ctx, cdc, app.BasicGaiaApp))
+	rootCmd.AddCommand(genutilcli.InitCmd(ctx, cdc, app.ModuleBasics))
+	rootCmd.AddCommand(genutilcli.CollectGenTxsCmd(ctx, cdc, app.ModuleBasics, genaccounts.AppModuleBasic{}))
+	rootCmd.AddCommand(genutilcli.GenTxCmd(ctx, cdc, app.ModuleBasics, genaccounts.AppModuleBasic{}, app.DefaultNodeHome, app.DefaultCLIHome))
+	rootCmd.AddCommand(genutilcli.ValidateGenesisCmd(ctx, cdc, app.ModuleBasics))
 	rootCmd.AddCommand(genaccscli.AddGenesisAccountCmd(ctx, cdc))
-	rootCmd.AddCommand(testnet.InitFilesCmd(ctx, cdc, app.BasicGaiaApp, genaccounts.AppModuleBasic{}))
+	rootCmd.AddCommand(testnet.InitFilesCmd(ctx, cdc, app.ModuleBasics, genaccounts.AppModuleBasic{}))
 	rootCmd.AddCommand(client.NewCompletionCmd(rootCmd, true))
 	rootCmd.AddCommand(replayCmd())
 

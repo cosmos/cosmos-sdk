925070ae381f2f1b7c135e0dfbac2b003bbc5366
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2019-05-09 13:38:15 -0400

    Merge PR #4292: Fix Signing Infos Endpoint

--- x/slashing/signing_info.go
+++ x/slashing/signing_info.go
@@ -92,18 +92,22 @@ func (k Keeper) clearValidatorMissedBlockBitArray(ctx sdk.Context, address sdk.C
 
 // Signing info for a validator
 type ValidatorSigningInfo struct {
-	StartHeight         int64     `json:"start_height"`          // height at which validator was first a candidate OR was unjailed
-	IndexOffset         int64     `json:"index_offset"`          // index offset into signed block bit array
-	JailedUntil         time.Time `json:"jailed_until"`          // timestamp validator cannot be unjailed until
-	Tombstoned          bool      `json:"tombstoned"`            // whether or not a validator has been tombstoned (killed out of validator set)
-	MissedBlocksCounter int64     `json:"missed_blocks_counter"` // missed blocks counter (to avoid scanning the array every time)
+	Address             sdk.ConsAddress `json:"address"`               // validator consensus address
+	StartHeight         int64           `json:"start_height"`          // height at which validator was first a candidate OR was unjailed
+	IndexOffset         int64           `json:"index_offset"`          // index offset into signed block bit array
+	JailedUntil         time.Time       `json:"jailed_until"`          // timestamp validator cannot be unjailed until
+	Tombstoned          bool            `json:"tombstoned"`            // whether or not a validator has been tombstoned (killed out of validator set)
+	MissedBlocksCounter int64           `json:"missed_blocks_counter"` // missed blocks counter (to avoid scanning the array every time)
 }
 
 // Construct a new `ValidatorSigningInfo` struct
-func NewValidatorSigningInfo(startHeight, indexOffset int64, jailedUntil time.Time,
-	tombstoned bool, missedBlocksCounter int64) ValidatorSigningInfo {
+func NewValidatorSigningInfo(
+	condAddr sdk.ConsAddress, startHeight, indexOffset int64,
+	jailedUntil time.Time, tombstoned bool, missedBlocksCounter int64,
+) ValidatorSigningInfo {
 
 	return ValidatorSigningInfo{
+		Address:             condAddr,
 		StartHeight:         startHeight,
 		IndexOffset:         indexOffset,
 		JailedUntil:         jailedUntil,
@@ -114,11 +118,13 @@ func NewValidatorSigningInfo(startHeight, indexOffset int64, jailedUntil time.Ti
 
 // Return human readable signing info
 func (i ValidatorSigningInfo) String() string {
-	return fmt.Sprintf(`Start Height:          %d
-Index Offset:          %d
-Jailed Until:          %v
-Tombstoned:            %t
-Missed Blocks Counter: %d`,
-		i.StartHeight, i.IndexOffset, i.JailedUntil,
+	return fmt.Sprintf(`Validator Signing Info:
+  Address:               %s
+  Start Height:          %d
+  Index Offset:          %d
+  Jailed Until:          %v
+  Tombstoned:            %t
+  Missed Blocks Counter: %d`,
+		i.Address, i.StartHeight, i.IndexOffset, i.JailedUntil,
 		i.Tombstoned, i.MissedBlocksCounter)
 }

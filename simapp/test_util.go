b2ee1bbc57b98f23f92ccb21be8c9261a225bcaa
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-05-17 11:18:28 -0400

    Update test_util.go

--- simapp/test_util.go
+++ simapp/test_util.go
@@ -11,11 +11,12 @@ import (
 	dbm "github.com/tendermint/tendermint/libs/db"
 )
 
-// used for debugging by gaia/cmd/gaiadebug
-// NOTE to not use this function with non-test code
+// NewSimAppUNSAFE is used for debugging purposes only.
+//
+// NOTE: to not use this function with non-test code
 func NewSimAppUNSAFE(logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest bool,
 	invCheckPeriod uint, baseAppOptions ...func(*bam.BaseApp),
-) (gapp *GaiaApp, keyMain, keyStaking *sdk.KVStoreKey, stakingKeeper staking.Keeper) {
+) (gapp *SimApp, keyMain, keyStaking *sdk.KVStoreKey, stakingKeeper staking.Keeper) {
 
 	gapp = NewSimApp(logger, db, traceStore, loadLatest, invCheckPeriod, baseAppOptions...)
 	return gapp, gapp.keyMain, gapp.keyStaking, gapp.stakingKeeper

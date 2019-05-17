9c6a279d14964433b5746dd3168c9ad6be59b347
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-05-17 21:08:30 +0200

    script in decent writing
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- scripts/get_node.sh
+++ scripts/get_node.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-NODE=v11.15.0
+VERSION=v11.15.0
+NODE_FULL=node-${VERSION}-linux-x64
 
 mkdir -p ~/.local/bin
 mkdir -p ~/.local/node
-wget http://nodejs.org/dist/${NODE}/node-${NODE}-linux-x64.tar.gz -O ~/.local/node/${NODE}.tar.gz
-tar -xzf ~/.local/node/${NODE}.tar.gz -C ~/.local/node/
-ln -s ~/.local/node/node-${NODE}-linux-x64/bin/node ~/.local/bin/node
-ln -s ~/.local/node/node-${NODE}-linux-x64/bin/npm ~/.local/bin/npm
+wget http://nodejs.org/dist/${VERSION}/${NODE_FULL}.tar.gz -O ~/.local/node/${NODE_FULL}.tar.gz
+tar -xzf ~/.local/node/${NODE_FULL}.tar.gz -C ~/.local/node/
+ln -s ~/.local/node/${NODE_FULL}/bin/node ~/.local/bin/node
+ln -s ~/.local/node/${NODE_FULL}/bin/npm ~/.local/bin/npm
 export PATH=~/.local/bin:$PATH
 npm i -g dredd@11.0.1
-ln -s ~/.local/node/node-${NODE}-linux-x64/bin/dredd ~/.local/bin/dredd
-
+ln -s ~/.local/node/${NODE_FULL}/bin/dredd ~/.local/bin/dredd

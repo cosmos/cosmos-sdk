9bf8b01de3296c6ee72f014472bcbf88151234ea
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-05-17 11:33:05 -0400

    Update errors.go

--- client/context/errors.go
+++ client/context/errors.go
@@ -17,6 +17,6 @@ Are you sure there has been a transaction involving it?`, addr)
 // height can't be verified. The reason is that the base checkpoint of the certifier is
 // newer than the given height
 func ErrVerifyCommit(height int64) error {
-	return fmt.Errorf(`The height of base truststore in gaia-lite is higher than height %d. 
+	return fmt.Errorf(`The height of base truststore in the light client is higher than height %d. 
 Can't verify blockchain proof at this height. Please set --trust-node to true and try again`, height)
 }

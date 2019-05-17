e83c1b669de4b56196d94533efdd73433ce567b1
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-05-17 20:20:59 +0200

    fix cci env
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- scripts/get_node.sh
+++ scripts/get_node.sh
@@ -8,7 +8,9 @@ wget http://nodejs.org/dist/${NODE}/node-${NODE}-linux-x64.tar.gz -O ~/.local/no
 tar -xzf ~/.local/node/${NODE}.tar.gz -C ~/.local/node/
 ln -s ~/.local/node/node-${NODE}-linux-x64/bin/node ~/.local/bin/node
 ln -s ~/.local/node/node-${NODE}-linux-x64/bin/npm ~/.local/bin/npm
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
-~/.local/bin/npm i -g dredd@11.0.1
+#echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+#source ~/.bashrc
+export PATH=$HOME/.local/bin:$PATH
+npm i -g dredd@11.0.1
+#~/.local/bin/npm i -g dredd@11.0.1
 ln -s ~/.local/node/node-${NODE}-linux-x64/bin/dredd ~/.local/bin/dredd

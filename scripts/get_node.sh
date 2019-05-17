b8eca6f0e24b67e6aa62a5c942e4e8d6c208cdf5
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-05-17 18:16:30 +0200

    just absolute paths
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- scripts/get_node.sh
+++ scripts/get_node.sh
@@ -2,15 +2,11 @@
 
 NODE=v11.15.0
 
-cd ~
-mkdir -p .local/bin
-mkdir -p .local/node
-cd .local/node
-curl -O http://nodejs.org/dist/${NODE}/node-${NODE}-linux-x64.tar.gz
-tar -xzf node-${NODE}-linux-x64.tar.gz
-ln -s node-${NODE}-linux-x64 latest
-cd ../bin
-ln -s ../node/latest/bin/node
-ln -s ../node/latest/bin/npm
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/node
+wget http://nodejs.org/dist/${NODE}/node-${NODE}-linux-x64.tar.gz -O ~/.local/node/${NODE}.tar.gz
+tar -xzf ~/.local/node/${NODE}.tar.gz -C ~/.local/node/
+ln -s ~/.local/node/node-${NODE}-linux-x64/bin/node ~/.local/bin/node
+ln -s ~/.local/node/node-${NODE}-linux-x64/bin/npm ~/.local/bin/npm
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc

49ba726ccd29e7e9781be42cfd03cc27e5de17e8
Christopher Goes <cwgoes@pluranimity.org>
2019-05-20 15:17:50 +0200

    Bugfix

--- x/distribution/keeper/proposal_handler.go
+++ x/distribution/keeper/proposal_handler.go
@@ -9,10 +9,11 @@ import (
 
 func HandleCommunityPoolSpendProposal(ctx sdk.Context, k Keeper, p types.CommunityPoolSpendProposal) sdk.Error {
 	feePool := k.GetFeePool(ctx)
-	feePool.CommunityPool = feePool.CommunityPool.Sub(sdk.NewDecCoins(p.Amount))
-	if feePool.CommunityPool.IsAnyNegative() {
+	newPool, negative := feePool.CommunityPool.SafeSub(sdk.NewDecCoins(p.Amount))
+	if negative {
 		return types.ErrBadDistribution(k.codespace)
 	}
+	feePool.CommunityPool = newPool
 	k.SetFeePool(ctx, feePool)
 	_, err := k.bankKeeper.AddCoins(ctx, p.Recipient, p.Amount)
 	if err != nil {

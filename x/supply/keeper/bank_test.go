668b64d82e4bcc5dfd1bd55cd303523e2490138f
Federico Kunze <federico.kunze94@gmail.com>
2019-05-24 17:56:39 +0200

    supply keeper tests

---
+++ x/supply/keeper/bank_test.go
@@ -0,0 +1,114 @@
+package keeper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/supply/types"
+)
+
+const (
+	foo = "foo"
+	bar = "bar"
+	minter = "minter"
+	base = "baseAcc"
+
+	initialPower = int64(100)
+)
+
+var (
+	fooAcc = types.NewPoolHolderAccount(foo)
+	barAcc = types.NewPoolHolderAccount(bar)
+	minterAcc = types.NewPoolMinterAccount(minter)
+
+	initTokens = sdk.TokensFromTendermintPower(initialPower)
+	initCoins = sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, initTokens))
+)
+
+func TestSendCoins(t *testing.T) {
+	nAccs := int64(4)
+	ctx, ak, keeper := createTestInput(t, false, initialPower, nAccs)
+
+	baseAcc := ak.NewAccountWithAddress(ctx, sdk.AccAddress(crypto.AddressHash([]byte("baseAcc"))))
+
+	err := fooAcc.SetCoins(initCoins)
+	require.NoError(t, err)
+
+	keeper.SetPoolAccount(ctx, fooAcc)
+	keeper.SetPoolAccount(ctx, barAcc)
+	ak.SetAccount(ctx, baseAcc)
+
+	err = keeper.SendCoinsPoolToPool(ctx, "", bar, initCoins)
+	require.Error(t, err)
+
+	err = keeper.SendCoinsPoolToPool(ctx, foo, "", initCoins)
+	require.Error(t, err)
+
+	err = keeper.SendCoinsPoolToAccount(ctx, "", baseAcc.GetAddress(), initCoins)
+	require.Error(t, err)
+
+	err = keeper.SendCoinsPoolToAccount(ctx, foo, baseAcc.GetAddress(), initCoins.Add(initCoins))
+	require.Error(t, err)
+
+	err = keeper.SendCoinsPoolToPool(ctx, foo, bar, initCoins)
+	require.NoError(t, err)
+	require.Equal(t, sdk.Coins(nil), keeper.GetCoinsByName(ctx, foo))
+	require.Equal(t, initCoins, keeper.GetCoinsByName(ctx, bar))
+
+	err = keeper.SendCoinsPoolToAccount(ctx, bar, baseAcc.GetAddress(), initCoins)
+	require.NoError(t, err)
+	require.Equal(t, sdk.Coins(nil), keeper.GetCoinsByName(ctx, bar))
+	require.Equal(t, initCoins, keeper.GetCoins(ctx, baseAcc.GetAddress()))
+
+	err = keeper.SendCoinsAccountToPool(ctx, baseAcc.GetAddress(), foo, initCoins)
+	require.NoError(t, err)
+	require.Equal(t, sdk.Coins(nil), keeper.GetCoins(ctx, baseAcc.GetAddress()))
+	require.Equal(t, initCoins, keeper.GetCoinsByName(ctx, foo))
+}
+
+func TestMintCoins(t *testing.T) {
+	nAccs := int64(4)
+	ctx, _, keeper := createTestInput(t, false, initialPower, nAccs)
+
+	keeper.SetPoolAccount(ctx, fooAcc)
+	keeper.SetPoolAccount(ctx, minterAcc)
+
+	initialSupply := keeper.GetSupply(ctx)
+
+	err := keeper.MintCoins(ctx, "", initCoins)
+	require.Error(t, err)
+
+	err = keeper.MintCoins(ctx, minter, initCoins)
+	require.NoError(t, err)
+	require.Equal(t, initCoins, keeper.GetCoinsByName(ctx, minter))
+	require.Equal(t, initialSupply.Total.Add(initCoins), keeper.GetSupply(ctx).Total)
+
+	require.Panics(t,  func() { keeper.MintCoins(ctx, foo, initCoins) })
+}
+
+func TestBurnCoins(t *testing.T) {
+	nAccs := int64(4)
+	ctx, _, keeper := createTestInput(t, false, initialPower, nAccs)
+
+	err := fooAcc.SetCoins(initCoins)
+	require.NoError(t, err)
+	keeper.SetPoolAccount(ctx, fooAcc)
+
+	initialSupply := keeper.GetSupply(ctx)
+	initialSupply.Inflate(initCoins)
+	keeper.SetSupply(ctx, initialSupply)
+
+	err = keeper.BurnCoins(ctx, "", initCoins)
+	require.Error(t, err)
+
+	err = keeper.BurnCoins(ctx, foo, initialSupply.Total)
+	require.Error(t, err)
+
+	err = keeper.BurnCoins(ctx, foo, initCoins)
+	require.NoError(t, err)
+	require.Equal(t, sdk.Coins(nil), keeper.GetCoinsByName(ctx, foo))
+	require.Equal(t, initialSupply.Total.Sub(initCoins), keeper.GetSupply(ctx).Total)
+}

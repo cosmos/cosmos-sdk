1db10b00332b12e2a7f26af54796499c02dc4bf7
Frank Yang <f.yang@hashgard.com>
2019-05-30 20:46:38 +0800

    Configurable Bip44 CoinType & HdPath for SDK users (#4300)
    Closes: #4144

--- crypto/keys/lazy_keybase_test.go
+++ crypto/keys/lazy_keybase_test.go
@@ -335,7 +335,7 @@ func TestLazySeedPhrase(t *testing.T) {
 	require.NotNil(t, err)
 
 	// let us re-create it from the mnemonic-phrase
-	params := *hd.NewFundraiserParams(0, 0)
+	params := *hd.NewFundraiserParams(0, sdk.CoinType, 0)
 	newInfo, err := kb.Derive(n2, mnemonic, DefaultBIP39Passphrase, p2, params)
 	require.NoError(t, err)
 	require.Equal(t, n2, newInfo.GetName())

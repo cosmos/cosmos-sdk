02b69f02309dc609a62eeb1fc2b87cd0654deb29
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2019-06-03 16:44:15 +0200

    Apply suggestions from code review
    Co-Authored-By: Alexander Bezobchuk <alexanderbez@users.noreply.github.com>

--- x/gov/types/keys.go
+++ x/gov/types/keys.go
@@ -112,7 +112,7 @@ func VoteKey(proposalID uint64, voterAddr sdk.AccAddress) []byte {
 // SplitProposalKey split the proposal key and returns the proposal id
 func SplitProposalKey(key []byte) (proposalID uint64) {
 	if len(key[1:]) != 8 {
-		panic(fmt.Sprintf("unexpected key length (%d ≠ 8)", len(key)))
+		panic(fmt.Sprintf("unexpected key length (%d ≠ 8)", len(key[1:])))
 	}
 
 	return binary.LittleEndian.Uint64(key[1:])
@@ -143,7 +143,7 @@ func SplitKeyVote(key []byte) (proposalID uint64, voterAddr sdk.AccAddress) {
 func splitKeyWithTime(key []byte) (proposalID uint64, endTime time.Time) {
 	lenTime := len(sdk.FormatTimeBytes(time.Now()))
 	if len(key[1:]) != 8+lenTime {
-		panic(fmt.Sprintf("unexpected key length (%d ≠ %d)", len(key), lenTime+8))
+		panic(fmt.Sprintf("unexpected key length (%d ≠ %d)", len(key[1:]), lenTime+8))
 	}
 
 	endTime, err := sdk.ParseTimeBytes(key[1 : 1+lenTime])

6706ad70af0ea0cc72123262d2a3b7869d31bc91
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-06-05 13:32:43 -0400

    Fix doc.go

--- simapp/doc.go
+++ simapp/doc.go
@@ -1,7 +1,10 @@
 /*
-# Simulation App
+Package simapp implements a full fledged Cosmos SDK application used for executing
+simulation test suites.
 
-The `SimApp` defines an application used for running extensive simulation
+Simulation App
+
+The SimApp defines an application used for running extensive simulation
 testing suites. It contains all core modules, including governance, staking,
 slashing, and distribution.
 
@@ -11,13 +14,15 @@ checking period, and a seed which is used as a source of pseudo-randomness.
 
 In addition to the various inputs, simulation runs mainly in three modes:
 
-* Completely random where the initial state, module parameters and simulation
+1.) Completely random where the initial state, module parameters and simulation
 parameters are pseudo-randomly generated.
-* From a genesis file where the initial state and the module parameters are defined.
+
+2.) From a genesis file where the initial state and the module parameters are defined.
 This mode is helpful for running simulations on a known state such as a live
 network export where a new (mostly likely breaking) version of the application
 needs to be tested.
-* From a params file where the initial state is pseudo-randomly generated but the
+
+3.) From a params file where the initial state is pseudo-randomly generated but the
 module and simulation parameters can be provided manually. This allows for a more
 controlled and deterministic simulation setup while allowing the state space to
 still be pseudo-randomly simulated.
@@ -25,7 +30,7 @@ still be pseudo-randomly simulated.
 The simulation test suite also supports testing determinism and import/export
 functionality.
 
-## Randomness
+Randomness
 
 Currently, simulation uses a single seed (integer) as a source for a PRNG by
 which all random operations are executed from. Any call to the PRNG changes all
@@ -39,53 +44,47 @@ the simulation suite is expected to support a series of PRNGs that can be used
 uniquely per module and simulation component so that they will not effect each
 others state execution outcome.
 
-## Usage
+Usage
 
 To execute a completely pseudo-random simulation:
 
-```shell
-$ go test -mod=readonly github.com/cosmos/cosmos-sdk/simapp \
-  -run=TestFullAppSimulation \
-  -SimulationEnabled=true \
-  -SimulationNumBlocks=100 \
-  -SimulationBlockSize=200 \
-  -SimulationCommit=true \
-  -SimulationSeed=99 \
-  -SimulationPeriod=5 \
-  -v -timeout 24h
-```
+ $ go test -mod=readonly github.com/cosmos/cosmos-sdk/simapp \
+	-run=TestFullAppSimulation \
+	-SimulationEnabled=true \
+	-SimulationNumBlocks=100 \
+	-SimulationBlockSize=200 \
+	-SimulationCommit=true \
+	-SimulationSeed=99 \
+	-SimulationPeriod=5 \
+	-v -timeout 24h
 
 To execute simulation from a genesis file:
 
-```shell
-$ go test -mod=readonly github.com/cosmos/cosmos-sdk/simapp \
-  -run=TestFullAppSimulation \
-  -SimulationEnabled=true \
-  -SimulationNumBlocks=100 \
-  -SimulationBlockSize=200 \
-  -SimulationCommit=true \
-  -SimulationSeed=99 \
-  -SimulationPeriod=5 \
-  -SimulationGenesis=/path/to/genesis.json \
-  -v -timeout 24h
-```
+ $ go test -mod=readonly github.com/cosmos/cosmos-sdk/simapp \
+ 	-run=TestFullAppSimulation \
+ 	-SimulationEnabled=true \
+ 	-SimulationNumBlocks=100 \
+ 	-SimulationBlockSize=200 \
+ 	-SimulationCommit=true \
+ 	-SimulationSeed=99 \
+ 	-SimulationPeriod=5 \
+ 	-SimulationGenesis=/path/to/genesis.json \
+ 	-v -timeout 24h
 
 To execute simulation from a params file:
 
-```shell
-$ go test -mod=readonly github.com/cosmos/cosmos-sdk/simapp \
-  -run=TestFullAppSimulation \
-  -SimulationEnabled=true \
-  -SimulationNumBlocks=100 \
-  -SimulationBlockSize=200 \
-  -SimulationCommit=true \
-  -SimulationSeed=99 \
-  -SimulationPeriod=5 \
-  -SimulationParams=/path/to/params.json \
-  -v -timeout 24h
-```
+ $ go test -mod=readonly github.com/cosmos/cosmos-sdk/simapp \
+	-run=TestFullAppSimulation \
+	-SimulationEnabled=true \
+	-SimulationNumBlocks=100 \
+	-SimulationBlockSize=200 \
+	-SimulationCommit=true \
+	-SimulationSeed=99 \
+	-SimulationPeriod=5 \
+	-SimulationParams=/path/to/params.json \
+	-v -timeout 24h
 
-## Params
+Params
 
 Params that are provided to simulation from a JSON file are used to used to set
 both module parameters and simulation parameters. See `sim_test.go` for the full

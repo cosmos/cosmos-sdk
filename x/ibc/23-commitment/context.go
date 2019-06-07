89dde5626200766481535b9320dcd5c0ea6467d0
mossid <torecursedivine@gmail.com>
2019-06-07 23:46:16 +0200

    add commitment

---
+++ x/ibc/23-commitment/context.go
@@ -0,0 +1,17 @@
+package commitment
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// TODO: define Context type which embeds sdk.Context and ensures the existence of RemoteKVStore
+
+type ContextKeyRemoteKVStore struct{}
+
+func WithStore(ctx sdk.Context, store Store) sdk.Context {
+	return ctx.WithValue(ContextKeyRemoteKVStore{}, store)
+}
+
+func GetStore(ctx sdk.Context) sdk.KVStore {
+	return ctx.Value(ContextKeyRemoteKVStore{}).(sdk.KVStore)
+}

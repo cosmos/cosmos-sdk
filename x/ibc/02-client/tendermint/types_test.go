e7dde7881baf8bef52661b3260b03f50019ed363
mossid <torecursedivine@gmail.com>
2019-06-13 19:08:13 +0200

    add test

--- x/ibc/02-client/tendermint/types_test.go
+++ x/ibc/02-client/tendermint/types_test.go
@@ -1,11 +1,13 @@
 package tendermint
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
 	dbm "github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/libs/log"
 	tmtypes "github.com/tendermint/tendermint/types"
@@ -14,6 +16,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	stypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/cosmos-sdk/x/ibc/23-commitment"
+	"github.com/cosmos/cosmos-sdk/x/ibc/23-commitment/merkle"
 )
 
 const chainid = "testchain"
@@ -33,7 +38,8 @@ func defaultComponents() (sdk.StoreKey, sdk.Context, stypes.CommitMultiStore, *c
 }
 
 type node struct {
-	valset MockValidators
+	prevvalset MockValidators
+	valset     MockValidators
 
 	cms   sdk.CommitMultiStore
 	store sdk.KVStore
@@ -60,7 +66,7 @@ func (node *node) last() tmtypes.SignedHeader {
 
 func (node *node) Commit() tmtypes.SignedHeader {
 	valsethash := node.valset.ValidatorSet().Hash()
-	nextvalset := node.valset.Mutate(false)
+	nextvalset := node.valset.Mutate()
 	nextvalsethash := nextvalset.ValidatorSet().Hash()
 	commitid := node.cms.Commit()
 
@@ -78,13 +84,11 @@ func (node *node) Commit() tmtypes.SignedHeader {
 
 	commit := node.valset.Sign(header)
 
+	node.prevvalset = node.valset
+	node.valset = nextvalset
 	node.commits = append(node.commits, commit)
 
 	return commit
-}
-
-func (node *node) Set(key, value string) {
-	node.store.Set(append([]byte{0x00}, []byte(key)...), []byte(value))
 }
 
 type Verifier struct {
@@ -102,10 +106,11 @@ func NewVerifier(header tmtypes.SignedHeader, nextvalset MockValidators) *Verifi
 	}
 }
 
-func (v *Verifier) Validate(header tmtypes.SignedHeader, nextvalset MockValidators) error {
+func (v *Verifier) Validate(header tmtypes.SignedHeader, valset, nextvalset MockValidators) error {
 	newcs, err := v.ConsensusState.Validate(
 		Header{
 			SignedHeader:     header,
+			ValidatorSet:     valset.ValidatorSet(),
 			NextValidatorSet: nextvalset.ValidatorSet(),
 		},
 	)
@@ -117,15 +122,96 @@ func (v *Verifier) Validate(header tmtypes.SignedHeader, nextvalset MockValidato
 	return nil
 }
 
-func TestUpdate(t *testing.T) {
+func testUpdate(t *testing.T, interval int, ok bool) {
 	node := NewNode(NewMockValidators(100, 10))
 
 	node.Commit()
 
 	verifier := NewVerifier(node.last(), node.valset)
 
-	header := node.Commit()
+	for i := 0; i < 100; i++ {
+		header := node.Commit()
 
-	err := verifier.Validate(header, node.valset)
-	require.NoError(t, err)
+		if i%interval == 0 {
+			err := verifier.Validate(header, node.prevvalset, node.valset)
+			if ok {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+		}
+	}
+}
+
+func TestEveryBlockUpdate(t *testing.T) {
+	testUpdate(t, 1, true)
+}
+
+func TestEvenBlockUpdate(t *testing.T) {
+	testUpdate(t, 2, true)
+}
+
+func TestSixthBlockUpdate(t *testing.T) {
+	testUpdate(t, 6, true)
+}
+
+/*
+// This should fail, since the amount of mutation is so large
+// Commented out because it sometimes success
+func TestTenthBlockUpdate(t *testing.T) {
+	testUpdate(t, 10, false)
+}
+*/
+
+func key(str []byte) []byte {
+	return append([]byte{0x00}, str...)
+}
+
+func (node *node) query(t *testing.T, k []byte) ([]byte, commitment.Proof) {
+	qres := node.cms.(stypes.Queryable).Query(abci.RequestQuery{Path: "/ibc/key", Data: key(k), Prove: true})
+	require.Equal(t, uint32(0), qres.Code, qres.Log)
+	proof := merkle.Proof{
+		Key:   []byte(k),
+		Proof: qres.Proof,
+	}
+	return qres.Value, proof
+}
+
+func (node *node) Set(k, value []byte) {
+	node.store.Set(key(k), value)
+}
+
+func testProof(t *testing.T) {
+	node := NewNode(NewMockValidators(100, 10))
+
+	node.Commit()
+
+	kvps := cmn.KVPairs{}
+	for h := 0; h < 20; h++ {
+		for i := 0; i < 100; i++ {
+			k := make([]byte, 32)
+			v := make([]byte, 32)
+			rand.Read(k)
+			rand.Read(v)
+			kvps = append(kvps, cmn.KVPair{Key: k, Value: v})
+			node.Set(k, v)
+		}
+		header := node.Commit()
+		proofs := []commitment.Proof{}
+		for _, kvp := range kvps {
+			v, p := node.query(t, []byte(kvp.Key))
+			require.Equal(t, kvp.Value, v)
+			proofs = append(proofs, p)
+		}
+		cstore, err := commitment.NewStore([]byte(header.AppHash), proofs)
+		require.NoError(t, err)
+
+		for _, kvp := range kvps {
+			require.True(t, cstore.Prove(kvp.Key, kvp.Value))
+		}
+	}
+}
+
+func TestProofs(t *testing.T) {
+	testProof(t)
 }

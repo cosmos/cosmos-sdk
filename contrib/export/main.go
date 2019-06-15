36bc1116ee5912b6b8b587903bb4fc7517edc257
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-06-15 16:08:34 +0200

    rename flags, print genesis instead of outputting into file
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- contrib/export/main.go
+++ contrib/export/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	extypes "github.com/cosmos/cosmos-sdk/contrib/export/types"
 	"github.com/cosmos/cosmos-sdk/contrib/export/v036"
-	"github.com/cosmos/cosmos-sdk/x/genutil"
 	"github.com/tendermint/tendermint/types"
 	"log"
 	"os"
@@ -29,14 +28,13 @@ func init() {
 
 	// this flag seems unnecessary, we can reintriduce it once we support multiple versions migration at once
 	//flag.StringVar(&source, "s", "034", "SDK version that exported the genesis")
-	flag.StringVar(&target, "v", "036", "Goal SDK version that will import the genesis")
-	flag.StringVar(&importGenesis, "g", "genesis.json", "Source genesis file")
-	flag.StringVar(&exportGenesis, "o", "genesis.json", "Output genesis file")
+	flag.StringVar(&target, "target", "036", "Goal SDK version that will import the genesis")
+	flag.StringVar(&importGenesis, "genesis", "genesis.json", "Source genesis file")
 
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(),
-			`Usage: %s [-t 036] [-g genesis.json] [-o genesis.json]
-Migrate the source genesis into the target version
+			`Usage: %s [-target 036] [-genesis genesis.json]
+Migrate the source genesis into the target version and export it as standard output
 
 `, filepath.Base(os.Args[0]))
 		flag.PrintDefaults()
@@ -55,6 +53,7 @@ func main() {
 	// We could add an invariant test for genesis, conversion should be identical to exporting the target genesis once imported
 
 	cdc := codec.New()
+	codec.RegisterCrypto(cdc)
 	// Dump here an example genesis for CCI to test import from old types, and export to new ones
 	genDoc, err := types.GenesisDocFromFile(importGenesis)
 	if err != nil {
@@ -69,8 +68,7 @@ func main() {
 	newGenState := migrationMap[target](initialState, cdc)
 
 	genDoc.AppState = cdc.MustMarshalJSON(newGenState)
-	// Keep dumping updated genesis to test import of a new genesis directly
-	if err = genutil.ExportGenesisFile(genDoc, exportGenesis); err != nil {
-		panic(err)
-	}
+
+	out, err := cdc.MarshalJSONIndent(genDoc, "", "  ")
+	fmt.Println(string(out))
 }

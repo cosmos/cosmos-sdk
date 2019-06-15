31291991310e85fd25910b7adf64aca09cc99c61
okwme <billy.rennekamp@gmail.com>
2019-06-15 11:14:16 +0000

    add back cli

--- x/nft/client/cli/tx.go
+++ x/nft/client/cli/tx.go
@@ -28,9 +28,21 @@ func GetCmdTransferNFT(cdc *codec.Codec) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
 			txBldr := authtypes.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			sender, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			recipient, err := sdk.AccAddressFromBech32(args[1])
+			if err != nil {
+				return err
+			}
+
+			denom := args[2]
 			tokenID := args[3]
 
-			msg := types.NewMsgTransferNFT(sdk.AccAddress(args[0]), sdk.AccAddress(args[1]), args[2], tokenID)
+			msg := types.NewMsgTransferNFT(sender, recipient, denom, tokenID)
 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 		},
 	}
@@ -65,4 +77,60 @@ func GetCmdEditNFTMetadata(cdc *codec.Codec) *cobra.Command {
 	cmd.Flags().String(flagImage, "", "Image path")
 	cmd.Flags().String(flagTokenURI, "", "Extra properties available for querying")
 	return cmd
+}
+
+
+// GetCmdMintNFT is the CLI command for a MintNFT transaction
+func GetCmdMintNFT(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "mint [recipient] [denom] [tokenID]",
+		Short: "mints a token of some denom with some tokenID to some recipient",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
+			txBldr := authtypes.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			denom := args[1]
+			tokenID := args[2]
+
+			recipient, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			name := viper.GetString(flagName)
+			description := viper.GetString(flagDescription)
+			image := viper.GetString(flagImage)
+			tokenURI := viper.GetString(flagTokenURI)
+
+			msg := types.NewMsgMintNFT(cliCtx.GetFromAddress(), recipient, denom, tokenID, name, description, image, tokenURI)
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+
+	cmd.Flags().String(flagName, "", "Name/title of nft")
+	cmd.Flags().String(flagDescription, "", "Description of nft")
+	cmd.Flags().String(flagImage, "", "Image uri of nft")
+	cmd.Flags().String(flagTokenURI, "", "URI for supplemental off-chain metadata (should return a JSON object)")
+
+	return cmd
+}
+
+// GetCmdBurnNFT is the CLI command for sending a BurnNFT transaction
+func GetCmdBurnNFT(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "burn [denom] [tokenID]",
+		Short: "burn a token of some denom with some tokenID",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
+			txBldr := authtypes.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			denom := args[0]
+			tokenID := args[1]
+
+			msg := types.NewMsgBurnNFT(cliCtx.GetFromAddress(), tokenID, denom)
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
 }

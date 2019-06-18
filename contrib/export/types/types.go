329556355e9a051a34ab21d6274585f104c68c12
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-06-18 16:02:10 +0200

    Update contrib/export/types/types.go
    Co-Authored-By: Alexander Bezobchuk <alexanderbez@users.noreply.github.com>

--- contrib/export/types/types.go
+++ contrib/export/types/types.go
@@ -9,5 +9,4 @@ type (
 	AppMap            map[string]json.RawMessage
 	MigrationCallback func(AppMap, *codec.Codec) AppMap
 	MigrationMap      map[string]MigrationCallback // It maps a version to a function migrate to the previous one to this.
-	// We can expand this type to include the previous version too
 )

b5da6d0eeeed909a080f955607e82980afd7a2c5
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-06-19 14:40:15 +0200

    cleanup comments
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- contrib/export/types/types.go
+++ contrib/export/types/types.go
@@ -6,7 +6,10 @@ import (
 )
 
 type (
-	AppMap            map[string]json.RawMessage
+	// AppMap map modules names with their json raw representation
+	AppMap map[string]json.RawMessage
+	// MigrationCallback converts a genesis map from the previous version to the targeted one
 	MigrationCallback func(AppMap, *codec.Codec) AppMap
-	MigrationMap      map[string]MigrationCallback // It maps a version to a function migrate to the previous one to this.
+	// MigrationMap defines a mapping from a version to a MigrationCallback
+	MigrationMap map[string]MigrationCallback
 )

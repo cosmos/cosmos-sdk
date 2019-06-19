8f5d0b58b9fd0e6cedd9723840d5b268af2cb32c
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-06-19 18:27:44 -0400

    Remove print response flag

--- client/flags/flags.go
+++ client/flags/flags.go
@@ -45,7 +45,6 @@ const (
 	FlagFees               = "fees"
 	FlagGasPrices          = "gas-prices"
 	FlagBroadcastMode      = "broadcast-mode"
-	FlagPrintResponse      = "print-response"
 	FlagDryRun             = "dry-run"
 	FlagGenerateOnly       = "generate-only"
 	FlagIndentResponse     = "indent"
@@ -96,7 +95,6 @@ func PostCommands(cmds ...*cobra.Command) []*cobra.Command {
 		c.Flags().Bool(FlagUseLedger, false, "Use a connected Ledger device")
 		c.Flags().Float64(FlagGasAdjustment, DefaultGasAdjustment, "adjustment factor to be multiplied against the estimate returned by the tx simulation; if the gas limit is set manually this flag is ignored ")
 		c.Flags().StringP(FlagBroadcastMode, "b", BroadcastSync, "Transaction broadcasting mode (sync|async|block)")
-		c.Flags().Bool(FlagPrintResponse, true, "return tx response (only works with async = false)")
 		c.Flags().Bool(FlagTrustNode, true, "Trust connected full node (don't verify proofs for responses)")
 		c.Flags().Bool(FlagDryRun, false, "ignore the --gas flag and perform a simulation of a transaction, but don't broadcast it")
 		c.Flags().Bool(FlagGenerateOnly, false, "Build an unsigned transaction and write it to STDOUT (when enabled, the local Keybase is not accessible and the node operates offline)")

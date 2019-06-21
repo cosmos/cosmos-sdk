e354f93ad74e23a1e50f5551a448fc43954acf26
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-06-21 15:57:16 +0200

    functions helps, but still simulation fails
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- contrib/export/v036/gov/types.go
+++ contrib/export/v036/gov/types.go
@@ -1,6 +1,8 @@
 package gov
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"time"
@@ -32,7 +34,6 @@ type Vote struct {
 }
 
 type Votes []Vote
-type VoteOption byte
 
 // Param around deposits for governance
 type DepositParams struct {
@@ -65,14 +66,20 @@ type Proposal struct {
 }
 
 type Proposals []Proposal
+type ProposalQueue []uint64
 
-type (
-	// ProposalQueue
-	ProposalQueue []uint64
+type TallyParams struct {
+	Quorum    sdk.Dec `json:"quorum,omitempty"`    //  Minimum percentage of total stake needed to vote for a result to be considered valid
+	Threshold sdk.Dec `json:"threshold,omitempty"` //  Minimum proportion of Yes votes for proposal to pass. Initial value: 0.5
+	Veto      sdk.Dec `json:"veto,omitempty"`      //  Minimum value of Veto votes to Total votes ratio for proposal to be vetoed. Initial value: 1/3
+}
 
-	// ProposalStatus is a type alias that represents a proposal status as a byte
-	ProposalStatus byte
-)
+// ----------------------------------------------------------------------------
+// ProposalStatus
+// ----------------------------------------------------------------------------
+
+// ProposalStatus is a type alias that represents a proposal status as a byte
+type ProposalStatus byte
 
 //nolint
 const (
@@ -84,10 +91,98 @@ const (
 	StatusFailed        ProposalStatus = 0x05
 )
 
-type TallyParams struct {
-	Quorum    sdk.Dec `json:"quorum,omitempty"`    //  Minimum percentage of total stake needed to vote for a result to be considered valid
-	Threshold sdk.Dec `json:"threshold,omitempty"` //  Minimum proportion of Yes votes for proposal to pass. Initial value: 0.5
-	Veto      sdk.Dec `json:"veto,omitempty"`      //  Minimum value of Veto votes to Total votes ratio for proposal to be vetoed. Initial value: 1/3
+// ProposalStatusToString turns a string into a ProposalStatus
+func ProposalStatusFromString(str string) (ProposalStatus, error) {
+	switch str {
+	case "DepositPeriod":
+		return StatusDepositPeriod, nil
+
+	case "VotingPeriod":
+		return StatusVotingPeriod, nil
+
+	case "Passed":
+		return StatusPassed, nil
+
+	case "Rejected":
+		return StatusRejected, nil
+
+	case "Failed":
+		return StatusFailed, nil
+
+	case "":
+		return StatusNil, nil
+
+	default:
+		return ProposalStatus(0xff), fmt.Errorf("'%s' is not a valid proposal status", str)
+	}
+}
+
+// Marshal needed for protobuf compatibility
+func (status ProposalStatus) Marshal() ([]byte, error) {
+	return []byte{byte(status)}, nil
+}
+
+// Unmarshal needed for protobuf compatibility
+func (status *ProposalStatus) Unmarshal(data []byte) error {
+	*status = ProposalStatus(data[0])
+	return nil
+}
+
+// Marshals to JSON using string
+func (status ProposalStatus) MarshalJSON() ([]byte, error) {
+	return json.Marshal(status.String())
+}
+
+// Unmarshals from JSON assuming Bech32 encoding
+func (status *ProposalStatus) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	bz2, err := ProposalStatusFromString(s)
+	if err != nil {
+		return err
+	}
+
+	*status = bz2
+	return nil
+}
+
+// String implements the Stringer interface.
+func (status ProposalStatus) String() string {
+	switch status {
+	case StatusDepositPeriod:
+		return "DepositPeriod"
+
+	case StatusVotingPeriod:
+		return "VotingPeriod"
+
+	case StatusPassed:
+		return "Passed"
+
+	case StatusRejected:
+		return "Rejected"
+
+	case StatusFailed:
+		return "Failed"
+
+	default:
+		return ""
+	}
+}
+
+// Format implements the fmt.Formatter interface.
+// nolint: errcheck
+func (status ProposalStatus) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 's':
+		s.Write([]byte(status.String()))
+	default:
+		// TODO: Do this conversion more directly
+		s.Write([]byte(fmt.Sprintf("%v", byte(status))))
+	}
 }
 
 type VotingParams struct {
@@ -100,6 +195,106 @@ type TallyResult struct {
 	No         sdk.Int `json:"no"`
 	NoWithVeto sdk.Int `json:"no_with_veto"`
 }
+
+// ----------------------------------------------------------------------------
+// VoteOption
+// ----------------------------------------------------------------------------
+
+type VoteOption byte
+
+// Vote options
+const (
+	OptionEmpty      VoteOption = 0x00
+	OptionYes        VoteOption = 0x01
+	OptionAbstain    VoteOption = 0x02
+	OptionNo         VoteOption = 0x03
+	OptionNoWithVeto VoteOption = 0x04
+)
+
+// VoteOptionFromString returns a VoteOption from a string. It returns an error
+// if the string is invalid.
+func VoteOptionFromString(str string) (VoteOption, error) {
+	switch str {
+	case "Yes":
+		return OptionYes, nil
+
+	case "Abstain":
+		return OptionAbstain, nil
+
+	case "No":
+		return OptionNo, nil
+
+	case "NoWithVeto":
+		return OptionNoWithVeto, nil
+
+	default:
+		return VoteOption(0xff), fmt.Errorf("'%s' is not a valid vote option", str)
+	}
+}
+
+// Marshal needed for protobuf compatibility.
+func (vo VoteOption) Marshal() ([]byte, error) {
+	return []byte{byte(vo)}, nil
+}
+
+// Unmarshal needed for protobuf compatibility.
+func (vo *VoteOption) Unmarshal(data []byte) error {
+	*vo = VoteOption(data[0])
+	return nil
+}
+
+// Marshals to JSON using string.
+func (vo VoteOption) MarshalJSON() ([]byte, error) {
+	return json.Marshal(vo.String())
+}
+
+// UnmarshalJSON decodes from JSON assuming Bech32 encoding.
+func (vo *VoteOption) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	bz2, err := VoteOptionFromString(s)
+	if err != nil {
+		return err
+	}
+
+	*vo = bz2
+	return nil
+}
+
+// String implements the Stringer interface.
+func (vo VoteOption) String() string {
+	switch vo {
+	case OptionYes:
+		return "Yes"
+	case OptionAbstain:
+		return "Abstain"
+	case OptionNo:
+		return "No"
+	case OptionNoWithVeto:
+		return "NoWithVeto"
+	default:
+		return ""
+	}
+}
+
+// Format implements the fmt.Formatter interface.
+// nolint: errcheck
+func (vo VoteOption) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 's':
+		s.Write([]byte(vo.String()))
+	default:
+		s.Write([]byte(fmt.Sprintf("%v", byte(vo))))
+	}
+}
+
+// ----------------------------------------------------------------------------
+// Codec
+// ----------------------------------------------------------------------------
 
 func RegisterCodec(codec *codec.Codec) {
 	codec.RegisterInterface((*Content)(nil), nil)

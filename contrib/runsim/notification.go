9ba839fab1f8b1897d553694665976f69e86c3ed
mircea-c <mircea@tendermint.com>
2019-06-25 17:57:24 -0400

    commenting out some more unused functions

--- contrib/runsim/notification.go
+++ contrib/runsim/notification.go
@@ -1,42 +1,36 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/google/go-github/v26/github"
 	"github.com/nlopes/slack"
+	"log"
 )
 
-type GithubPayload struct {
-	Issue struct {
-		Number int `json:"number"`
-		Pull   struct {
-			Url string `json:"url,omitempty"`
-		} `json:"pull_request,omitempty"`
-	} `json:"issue"`
-
-	Comment struct {
-		Body string `json:"body"`
-	} `json:"comment"`
-
-	Repository struct {
-		Name  string `json:"name"`
-		Owner struct {
-			Login string `json:"login"`
-		} `json:"owner"`
-	} `json:"repository"`
-}
-
-type PullRequestDetails struct {
-	Head struct {
-		Ref string `json:"ref"`
-		Sha string `json:"sha"`
-	} `json:"head"`
-}
+//type GithubPayload struct {
+//	Issue struct {
+//		Number int `json:"number"`
+//		Pull   struct {
+//			Url string `json:"url,omitempty"`
+//		} `json:"pull_request,omitempty"`
+//	} `json:"issue"`
+//
+//	Comment struct {
+//		Body string `json:"body"`
+//	} `json:"comment"`
+//
+//	Repository struct {
+//		Name  string `json:"name"`
+//		Owner struct {
+//			Login string `json:"login"`
+//		} `json:"owner"`
+//	} `json:"repository"`
+//}
+//
+//type PullRequestDetails struct {
+//	Head struct {
+//		Ref string `json:"ref"`
+//		Sha string `json:"sha"`
+//	} `json:"head"`
+//}
 
 func slackMessage(token string, channel string, threadTS *string, message string) {
 	client := slack.New(token)
@@ -54,55 +48,55 @@ func slackMessage(token string, channel string, threadTS *string, message string
 
 }
 
-func createCheckRun(client *github.Client, payload GithubPayload, pr PullRequestDetails) error {
-	var opt github.CreateCheckRunOptions
-	opt.Name = "Test Check"
-	opt.HeadBranch = pr.Head.Ref
-	opt.HeadSHA = pr.Head.Sha
-
-	checkRUn, resp, err := client.Checks.CreateCheckRun(context.Background(), payload.Repository.Owner.Login, payload.Repository.Name, opt)
-	log.Printf("%v", resp)
-	log.Printf("%v", checkRUn)
-	if err != nil {
-		log.Printf("ERROR: CreateCheckRun: %v", err.Error())
-		return err
-	}
-	return err
-}
-
-func getPrDetails(prUrl string) (*PullRequestDetails, error) {
-	request, err := http.Get(prUrl)
-	if err != nil {
-		return nil, err
-	}
-
-	var details PullRequestDetails
-	if err := json.NewDecoder(request.Body).Decode(&details); err != nil {
-		return nil, err
-	}
-
-	return &details, nil
-}
-
-func updateCheckRun(client *github.Client, payload GithubPayload, pr PullRequestDetails) error {
-	status := "completed"
-	conclusion := "success"
-	var opt github.UpdateCheckRunOptions
-	opt.Name = "Test Check"
-	opt.Status = &status
-	opt.Conclusion = &conclusion
-	ts := github.Timestamp{Time: time.Now()}
-	opt.CompletedAt = &ts
-
-	updatedCheck, resp, err := client.Checks.UpdateCheckRun(context.Background(), payload.Repository.Owner.Login, payload.Repository.Name, 136693316, opt)
-	log.Printf("%v", updatedCheck)
-	log.Printf("%v", resp)
-	if err != nil {
-		log.Printf("ERROR: UpdateCheckRun: %v", err.Error())
-		return err
-	}
-	return nil
-}
+//func createCheckRun(client *github.Client, payload GithubPayload, pr PullRequestDetails) error {
+//	var opt github.CreateCheckRunOptions
+//	opt.Name = "Test Check"
+//	opt.HeadBranch = pr.Head.Ref
+//	opt.HeadSHA = pr.Head.Sha
+//
+//	checkRUn, resp, err := client.Checks.CreateCheckRun(context.Background(), payload.Repository.Owner.Login, payload.Repository.Name, opt)
+//	log.Printf("%v", resp)
+//	log.Printf("%v", checkRUn)
+//	if err != nil {
+//		log.Printf("ERROR: CreateCheckRun: %v", err.Error())
+//		return err
+//	}
+//	return err
+//}
+//
+//func getPrDetails(prUrl string) (*PullRequestDetails, error) {
+//	request, err := http.Get(prUrl)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	var details PullRequestDetails
+//	if err := json.NewDecoder(request.Body).Decode(&details); err != nil {
+//		return nil, err
+//	}
+//
+//	return &details, nil
+//}
+//
+//func updateCheckRun(client *github.Client, payload GithubPayload, pr PullRequestDetails) error {
+//	status := "completed"
+//	conclusion := "success"
+//	var opt github.UpdateCheckRunOptions
+//	opt.Name = "Test Check"
+//	opt.Status = &status
+//	opt.Conclusion = &conclusion
+//	ts := github.Timestamp{Time: time.Now()}
+//	opt.CompletedAt = &ts
+//
+//	updatedCheck, resp, err := client.Checks.UpdateCheckRun(context.Background(), payload.Repository.Owner.Login, payload.Repository.Name, 136693316, opt)
+//	log.Printf("%v", updatedCheck)
+//	log.Printf("%v", resp)
+//	if err != nil {
+//		log.Printf("ERROR: UpdateCheckRun: %v", err.Error())
+//		return err
+//	}
+//	return nil
+//}
 
 //func githubCheckHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 //	response := events.APIGatewayProxyResponse{StatusCode: 200}

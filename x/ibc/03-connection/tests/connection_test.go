c0ca538099463aeeffce5cac534c6811eff58266
mossid <torecursedivine@gmail.com>
2019-06-26 19:09:32 +0200

    add test in progress

--- x/ibc/03-connection/tests/connection_test.go
+++ x/ibc/03-connection/tests/connection_test.go
@@ -8,15 +8,26 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 
 	"github.com/cosmos/cosmos-sdk/x/ibc/02-client"
+	tmclient "github.com/cosmos/cosmos-sdk/x/ibc/02-client/tendermint"
 	"github.com/cosmos/cosmos-sdk/x/ibc/02-client/tendermint/tests"
+	"github.com/cosmos/cosmos-sdk/x/ibc/23-commitment"
+	"github.com/cosmos/cosmos-sdk/x/ibc/23-commitment/merkle"
 )
 
 func registerCodec(cdc *codec.Codec) {
 	client.RegisterCodec(cdc)
+	tmclient.RegisterCodec(cdc)
+	commitment.RegisterCodec(cdc)
+	merkle.RegisterCodec(cdc)
 }
 
 func TestHandshake(t *testing.T) {
 	cdc := codec.New()
+	registerCodec(cdc)
 
-	node := NewNode(tendermint.NewMockValidators(10), tendermint.NewMockValidators(10))
+	node := NewNode(tendermint.NewMockValidators(10), tendermint.NewMockValidators(10), cdc)
+	node.Advance(t)
+	node.Advance(t)
+	node.Advance(t)
+	node.Advance(t)
 }

e48fd1c2bd9eee01986028b0476e73716554321a
mossid <torecursedivine@gmail.com>
2019-06-26 15:19:42 +0200

    cli in progress

--- x/ibc/02-client/cli.go
+++ x/ibc/02-client/cli.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
 
 	"github.com/cosmos/cosmos-sdk/x/ibc/23-commitment/merkle"
 )
@@ -11,6 +12,7 @@ type CLIObject struct {
 	ID                string
 	ConsensusStateKey []byte
 	FrozenKey         []byte
+	Cdc               *codec.Codec
 }
 
 func (obj Object) CLI() CLIObject {
@@ -21,6 +23,34 @@ func (obj Object) CLI() CLIObject {
 	}
 }
 
-func (obj CLIObject) ConsensusState(ctx context.CLIContext, root merkle.Root) (res ConsensusState, proof merkle..Proof) {
-	val, proof, _, err := ctx.QueryProof(obj.ConsensusStateKey)
+func query(ctx context.CLIContext, root merkle.Root, key []byte) ([]byte, merkle.Proof, error) {
+	resp, err := ctx.QueryABCI(root.RequestQuery(key))
+	if err != nil {
+		return nil, merkle.Proof{}, err
+	}
+	proof := merkle.Proof{
+		Key:   key,
+		Proof: resp.Proof,
+	}
+	return resp.Value, proof, nil
+
+}
+
+func (obj CLIObject) ConsensusState(ctx context.CLIContext, root merkle.Root) (res ConsensusState, proof merkle.Proof, err error) {
+	val, proof, err := query(ctx, root, obj.ConsensusStateKey)
+	obj.Cdc.MustUnmarshalBinaryBare(val, &res)
+	return
+}
+
+func (obj CLIObject) Frozen(ctx context.CLIContext, root merkle.Root) (res bool, proof merkle.Proof, err error) {
+	val, tmproof, _, err := ctx.QueryProof(obj.FrozenKey, "ibc") // TODO
+	if err != nil {
+		return
+	}
+	proof = merkle.Proof{
+		Key:   obj.FrozenKey,
+		Proof: tmproof,
+	}
+	obj.Cdc.MustUnmarshalBinaryBare(val, &res)
+	return
 }

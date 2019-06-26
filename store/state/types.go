092cd746161d8d9c877081cf80b93769666a2c5f
mossid <torecursedivine@gmail.com>
2019-06-26 15:19:42 +0200

    cli in progress

--- store/state/types.go
+++ store/state/types.go
@@ -1,8 +1,13 @@
 package state
 
 import (
+	"github.com/tendermint/tendermint/crypto/merkle"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type KVStore = sdk.KVStore
 type Context = sdk.Context
+type CLIContext = context.CLIContext
+type Proof = merkle.Proof

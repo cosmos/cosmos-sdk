aba1f649ad34d41dbb32573034081b968f5359a4
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2019-07-02 09:00:46 -0400

    Merge PR #4658: Generalize Querier REST Pagination

---
+++ client/utils_test.go
@@ -0,0 +1,61 @@
+package client_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/client"
+)
+
+func TestPaginate(t *testing.T) {
+	testCases := []struct {
+		name                           string
+		numObjs, page, limit, defLimit int
+		expectedStart, expectedEnd     int
+	}{
+		{
+			"all objects in a single page",
+			100, 1, 100, 100,
+			0, 100,
+		},
+		{
+			"page one of three",
+			75, 1, 25, 100,
+			0, 25,
+		},
+		{
+			"page two of three",
+			75, 2, 25, 100,
+			25, 50,
+		},
+		{
+			"page three of three",
+			75, 3, 25, 100,
+			50, 75,
+		},
+		{
+			"end is greater than total number of objects",
+			75, 2, 50, 100,
+			50, 75,
+		},
+		{
+			"invalid start page",
+			75, 4, 25, 100,
+			-1, -1,
+		},
+		{
+			"invalid zero start page",
+			75, 0, 25, 100,
+			-1, -1,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			start, end := client.Paginate(tc.numObjs, tc.page, tc.limit, tc.defLimit)
+			require.Equal(t, tc.expectedStart, start, "invalid result; test case #%d", i)
+			require.Equal(t, tc.expectedEnd, end, "invalid result; test case #%d", i)
+		})
+	}
+}

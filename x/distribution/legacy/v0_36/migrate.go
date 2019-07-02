13261a14a6351bc599a9d67a3730c2c8f8405ecc
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-07-02 17:17:31 -0400

    More migration logic

--- x/distribution/legacy/v0_36/migrate.go
+++ x/distribution/legacy/v0_36/migrate.go
@@ -5,8 +5,34 @@ import (
 )
 
 func Migrate(oldGenState v034distr.GenesisState) GenesisState {
+	feePool := FeePool{CommunityPool: oldGenState.FeePool.CommunityPool}
+
+	dwifos := make([]DelegatorWithdrawInfo, len(oldGenState.DelegatorWithdrawInfos))
+	for i, info := range oldGenState.DelegatorWithdrawInfos {
+		dwifos[i] = DelegatorWithdrawInfo{
+			DelegatorAddress: info.DelegatorAddress,
+			WithdrawAddress:  info.WithdrawAddress,
+		}
+	}
+
+	outRewards := make([]ValidatorOutstandingRewardsRecord, len(oldGenState.OutstandingRewards))
+	for i, rew := range oldGenState.OutstandingRewards {
+		outRewards[i] = ValidatorOutstandingRewardsRecord{
+			ValidatorAddress:   rew.ValidatorAddress,
+			OutstandingRewards: rew.OutstandingRewards,
+		}
+	}
+
+	accumComm := make([]ValidatorAccumulatedCommissionRecord, len(oldGenState.ValidatorAccumulatedCommissions))
+	for i, comm := range oldGenState.ValidatorAccumulatedCommissions {
+		accumComm[i] = ValidatorAccumulatedCommissionRecord{
+			ValidatorAddress: comm.ValidatorAddress,
+			Accumulated:      comm.Accumulated,
+		}
+	}
+
 	// migrate slash events which now have the period included
-	slashEvents := make([]ValidatorSlashEventRecord, len(oldGenState.ValidatorSlashEvents), len(oldGenState.ValidatorSlashEvents))
+	slashEvents := make([]ValidatorSlashEventRecord, len(oldGenState.ValidatorSlashEvents))
 	for i, se := range oldGenState.ValidatorSlashEvents {
 		slashEvents[i] = ValidatorSlashEventRecord{
 			ValidatorAddress: se.ValidatorAddress,
@@ -20,9 +46,8 @@ func Migrate(oldGenState v034distr.GenesisState) GenesisState {
 	}
 
 	return NewGenesisState(
-		oldGenState.FeePool, oldGenState.CommunityTax, oldGenState.BaseProposerReward, oldGenState.BonusProposerReward,
-		oldGenState.WithdrawAddrEnabled, oldGenState.DelegatorWithdrawInfos, oldGenState.PreviousProposer,
-		oldGenState.OutstandingRewards, oldGenState.ValidatorAccumulatedCommissions,
+		feePool, oldGenState.CommunityTax, oldGenState.BaseProposerReward, oldGenState.BonusProposerReward,
+		oldGenState.WithdrawAddrEnabled, dwifos, oldGenState.PreviousProposer, outRewards, accumComm,
 		oldGenState.ValidatorHistoricalRewards, oldGenState.ValidatorCurrentRewards,
 		oldGenState.DelegatorStartingInfos, slashEvents,
 	)

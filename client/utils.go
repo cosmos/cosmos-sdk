d8dfaacb4ea3d0c28baa982d77de3b18b3b8bb13
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-07-01 21:28:40 -0400

    Generalize querier pagination

---
+++ client/utils.go
@@ -0,0 +1,25 @@
+package client
+
+// Paginate returns the correct starting and ending index for a paginated query,
+// given that client provides a desired page and limit of objects and the handler
+// provides the total number of objects. If the start page is invalid, non-positive
+// values are returned signaling the request is invalid.
+func Paginate(numObjs, page, limit, defLimit int) (start, end int) {
+	if limit == 0 {
+		limit = defLimit
+	}
+
+	start = (page - 1) * limit
+	end = limit + start
+
+	if end >= numObjs {
+		end = numObjs
+	}
+
+	if start >= numObjs {
+		// page is out of bounds
+		return -1, -1
+	}
+
+	return start, end
+}

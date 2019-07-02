b6ad781862728816daa86350ef2ad6c72c8e5d45
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-07-02 17:06:51 -0400

    Start on distribution Migrate

--- x/distribution/legacy/v0_36/migrate.go
+++ x/distribution/legacy/v0_36/migrate.go
@@ -5,5 +5,25 @@ import (
 )
 
 func Migrate(oldGenState v034distr.GenesisState) GenesisState {
+	// migrate slash events which now have the period included
+	slashEvents := make([]ValidatorSlashEventRecord, len(oldGenState.ValidatorSlashEvents), len(oldGenState.ValidatorSlashEvents))
+	for i, se := range oldGenState.ValidatorSlashEvents {
+		slashEvents[i] = ValidatorSlashEventRecord{
+			ValidatorAddress: se.ValidatorAddress,
+			Height:           se.Height,
+			Period:           se.Event.ValidatorPeriod,
+			Event: ValidatorSlashEvent{
+				ValidatorPeriod: se.Event.ValidatorPeriod,
+				Fraction:        se.Event.Fraction,
+			},
+		}
+	}
 
+	return NewGenesisState(
+		oldGenState.FeePool, oldGenState.CommunityTax, oldGenState.BaseProposerReward, oldGenState.BonusProposerReward,
+		oldGenState.WithdrawAddrEnabled, oldGenState.DelegatorWithdrawInfos, oldGenState.PreviousProposer,
+		oldGenState.OutstandingRewards, oldGenState.ValidatorAccumulatedCommissions,
+		oldGenState.ValidatorHistoricalRewards, oldGenState.ValidatorCurrentRewards,
+		oldGenState.DelegatorStartingInfos, slashEvents,
+	)
 }

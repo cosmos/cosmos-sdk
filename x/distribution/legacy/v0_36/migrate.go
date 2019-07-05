be367fb435dd79bd3972a0393de1bc6c4986bd54
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-07-05 12:11:35 -0400

    Minor cleanup

--- x/distribution/legacy/v0_36/migrate.go
+++ x/distribution/legacy/v0_36/migrate.go
@@ -8,7 +8,6 @@ import (
 // genesis state. All entries are identical except for validator slashing events
 // which now include the period.
 func Migrate(oldGenState v034distr.GenesisState) GenesisState {
-
 	// migrate slash events which now have the period included
 	slashEvents := make([]ValidatorSlashEventRecord, len(oldGenState.ValidatorSlashEvents))
 	for i, se := range oldGenState.ValidatorSlashEvents {
@@ -23,7 +22,9 @@ func Migrate(oldGenState v034distr.GenesisState) GenesisState {
 	return NewGenesisState(
 		oldGenState.FeePool, oldGenState.CommunityTax, oldGenState.BaseProposerReward,
 		oldGenState.BonusProposerReward, oldGenState.WithdrawAddrEnabled,
-		oldGenState.DelegatorWithdrawInfos, oldGenState.PreviousProposer, oldGenState.OutstandingRewards, oldGenState.ValidatorAccumulatedCommissions,
-		oldGenState.ValidatorHistoricalRewards, oldGenState.ValidatorCurrentRewards, oldGenState.DelegatorStartingInfos, slashEvents,
+		oldGenState.DelegatorWithdrawInfos, oldGenState.PreviousProposer,
+		oldGenState.OutstandingRewards, oldGenState.ValidatorAccumulatedCommissions,
+		oldGenState.ValidatorHistoricalRewards, oldGenState.ValidatorCurrentRewards,
+		oldGenState.DelegatorStartingInfos, slashEvents,
 	)
 }

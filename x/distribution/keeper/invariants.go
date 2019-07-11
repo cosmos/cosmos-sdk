41767a416e6f0543b359d9b2a47ebae9ff6a1355
Colin Axner <colinaxner@berkeley.edu>
2019-07-11 12:56:14 -0700

    update distribution invariant formatting

--- x/distribution/keeper/invariants.go
+++ x/distribution/keeper/invariants.go
@@ -149,7 +149,9 @@ func ModuleAccountInvariant(k Keeper) sdk.Invariant {
 		macc := k.GetDistributionAccount(ctx)
 
 		broken := !macc.GetCoins().IsEqual(expectedInt)
-		return sdk.FormatInvariant(types.ModuleName, "ModuleAccount coins", fmt.Sprintf("\texpected ModuleAccount coins: %s\n"+
-			"\tdistribution ModuleAccount coins : %s\n", expectedInt, macc.GetCoins()), broken)
+		return sdk.FormatInvariant(types.ModuleName, "ModuleAccount coins",
+			fmt.Sprintf("\texpected ModuleAccount coins:      %s\n"+
+				"\tdistribution ModuleAccount coins : %s\n",
+				expectedInt, macc.GetCoins()), broken)
 	}
 }

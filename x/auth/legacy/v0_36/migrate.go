ba6a55d65bb3368cd4ab72fb81c91328cbadc4d1
Federico Kunze <federico.kunze94@gmail.com>
2019-07-11 22:48:46 +0200

    fix test

--- x/auth/legacy/v0_36/migrate.go
+++ x/auth/legacy/v0_36/migrate.go
@@ -7,8 +7,8 @@ import (
 )
 
 // Migrate accepts exported genesis state from v0.34 and migrates it to v0.36
-// genesis state. All entries are identical except for validator slashing events
-// which now include the period.
+// genesis state. This migration removes the CollectedFees coins from the old
+// FeeCollectorKeeper.
 func Migrate(oldGenState v034auth.GenesisState) GenesisState {
 	return NewGenesisState(oldGenState.Params)
 }

a42b0452acf9a926fd9764689efb1e048b3dad49
Aayushi Jain <asjain237@gmail.com>
2019-07-12 12:53:55 -0400

    Added query.go that includes the cli commands for querying.

---
+++ x/coinswap/client/cli/query.go
@@ -0,0 +1,93 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/cosmos/cosmos-sdk/x/coinswap/internal/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	coinswapQueryCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the coinswap module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+	coinswapQueryCmd.AddCommand(client.GetCommands(
+		GetCmdQueryLiquidity(queryRoute, cdc),
+		GetCmdQueryParams(queryRoute, cdc))...)
+
+	return coinswapQueryCmd
+}
+
+// GetCmdQueryLiquidity implements the liquidity query command
+func GetCmdQueryLiquidity(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "liquidity",
+		Short: "Query the current liquidity values",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values for the liquidity stored in the reserve pool.
+			
+Example:
+$ %s query coinswap liquidity
+`,
+				version.ClientName,
+			),
+		),
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			route := fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryLiquidity)
+			bz, _, err := cliCtx.QueryWithData(route, nil)
+			if err != nil {
+				return err
+			}
+
+			var liquidity sdk.Coin
+			cdc.MustUnmarshalJSON(bz, &liquidity)
+			return cliCtx.PrintOutput(liquidity)
+		},
+	}
+}
+
+// GetCmdQueryParams implements the params query command
+func GetCmdQueryParams(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "params",
+		Short: "Query the parameters involved in the coinswap process",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query all the parameters for the coinswap process.
+			
+Example:
+$ %s query coinswap params
+`,
+				version.ClientName,
+			),
+		),
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			route := fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryParameters)
+			bz, _, err := cliCtx.QueryWithData(route, nil)
+			if err != nil {
+				return err
+			}
+
+			var params types.Params
+			cdc.MustUnmarshalJSON(bz, &params)
+			return cliCtx.PrintOutput(params)
+		},
+	}
+}

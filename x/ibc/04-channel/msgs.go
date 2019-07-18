7ecd36016199acdfe16f96e2f620e7b9b03eb36f
mossid <torecursedivine@gmail.com>
2019-07-19 00:56:10 +0900

    fix golangci

--- x/ibc/04-channel/msgs.go
+++ x/ibc/04-channel/msgs.go
@@ -6,6 +6,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/ibc/23-commitment"
 )
 
+const Route = "ibc"
+
 type MsgOpenInit struct {
 	ConnectionID       string
 	ChannelID          string
@@ -18,7 +20,7 @@ type MsgOpenInit struct {
 var _ sdk.Msg = MsgOpenInit{}
 
 func (msg MsgOpenInit) Route() string {
-	return "ibc"
+	return Route
 }
 
 func (msg MsgOpenInit) Type() string {
@@ -51,11 +53,11 @@ type MsgOpenTry struct {
 var _ sdk.Msg = MsgOpenTry{}
 
 func (msg MsgOpenTry) Route() string {
-	return "ibc"
+	return Route
 }
 
 func (msg MsgOpenTry) Type() string {
-	return "open-init"
+	return "open-try"
 }
 
 func (msg MsgOpenTry) ValidateBasic() sdk.Error {
@@ -82,11 +84,11 @@ type MsgOpenAck struct {
 var _ sdk.Msg = MsgOpenAck{}
 
 func (msg MsgOpenAck) Route() string {
-	return "ibc"
+	return Route
 }
 
 func (msg MsgOpenAck) Type() string {
-	return "open-init"
+	return "open-ack"
 }
 
 func (msg MsgOpenAck) ValidateBasic() sdk.Error {
@@ -112,11 +114,11 @@ type MsgOpenConfirm struct {
 var _ sdk.Msg = MsgOpenConfirm{}
 
 func (msg MsgOpenConfirm) Route() string {
-	return "ibc"
+	return Route
 }
 
 func (msg MsgOpenConfirm) Type() string {
-	return "open-init"
+	return "open-confirm"
 }
 
 func (msg MsgOpenConfirm) ValidateBasic() sdk.Error {

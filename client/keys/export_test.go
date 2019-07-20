c21bd0c01f510ce67cbbb6112dc8d2ae8329add4
poldsam <kpoldsam@gmail.com>
2019-07-20 14:09:31 -0700

    Fixing export_test

--- client/keys/export_test.go
+++ client/keys/export_test.go
@@ -38,12 +38,17 @@ func Test_runExportCmd(t *testing.T) {
 	}
 
 	if runningOnServer {
-		mockIn.Reset("testpass1\ntestpass1\ntestpass1\n")
+		mockIn.Reset("testpass1\ntestpass1\n")
 	}
 	_, err := kb.CreateAccount("keyname1", tests.TestMnemonic, "", "123456789", 0, 0)
 	assert.NoError(t, err)
 
-	mockIn.Reset("123456789\n123456789\n")
+	if runningOnServer {
+		mockIn.Reset("testpass1\ntestpass1\ntestpass1\n")
+	} else {
+		mockIn.Reset("123456789\n123456789\n")
+
+	}
 	// Now enter password
 	assert.NoError(t, runExportCmd(exportKeyCommand, []string{"keyname1"}))
 }

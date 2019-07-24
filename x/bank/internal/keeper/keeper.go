87bf29c0a65182473496a06701ec6b6210053619
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2019-07-24 15:50:34 -0700

    Merge PR #4772: Add New Events Attributes

--- x/bank/internal/keeper/keeper.go
+++ x/bank/internal/keeper/keeper.go
@@ -210,7 +210,6 @@ func (keeper BaseSendKeeper) InputOutputCoins(ctx sdk.Context, inputs []types.In
 
 // SendCoins moves coins from one account to another
 func (keeper BaseSendKeeper) SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) sdk.Error {
-
 	_, err := keeper.SubtractCoins(ctx, fromAddr, amt)
 	if err != nil {
 		return err
@@ -225,6 +224,7 @@ func (keeper BaseSendKeeper) SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress,
 		sdk.NewEvent(
 			types.EventTypeTransfer,
 			sdk.NewAttribute(types.AttributeKeyRecipient, toAddr.String()),
+			sdk.NewAttribute(sdk.AttributeKeyAmount, amt.String()),
 		),
 		sdk.NewEvent(
 			sdk.EventTypeMessage,

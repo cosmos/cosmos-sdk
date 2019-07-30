361aa42a93a4290766a54c38bf7959a1de3aee23
mossid <torecursedivine@gmail.com>
2019-07-31 04:01:46 +0900

    add godoc

--- x/ibc/23-commitment/merkle/merkle.go
+++ x/ibc/23-commitment/merkle/merkle.go
@@ -16,25 +16,35 @@ const merkleKind = "merkle"
 // Applied on SDK-based IBC implementation
 var _ commitment.Root = Root{}
 
+// Root is Merkle root hash
 type Root struct {
 	Hash []byte
 }
 
+// NewRoot constructs a new Root
 func NewRoot(hash []byte) Root {
-	return Root{hash}
+	return Root{
+		Hash: hash,
+	}
 }
 
+// Implements commitment.Root
 func (Root) CommitmentKind() string {
 	return merkleKind
 }
 
 var _ commitment.Path = Path{}
 
+// Path is merkle path prefixed to the key.
+// The constructed key from the Path and the key will be append(Path.KeyPath, append(Path.KeyPrefix, key...))
 type Path struct {
-	KeyPath   [][]byte
+	// KeyPath is the list of keys prepended before the prefixed key
+	KeyPath [][]byte
+	// KeyPrefix is a byte slice prefixed before the key
 	KeyPrefix []byte
 }
 
+// NewPath() constructs new Path
 func NewPath(keypath [][]byte, keyprefix []byte) Path {
 	return Path{
 		KeyPath:   keypath,
@@ -42,25 +52,30 @@ func NewPath(keypath [][]byte, keyprefix []byte) Path {
 	}
 }
 
+// Implements commitment.Path
 func (Path) CommitmentKind() string {
 	return merkleKind
 }
 
 var _ commitment.Proof = Proof{}
 
+// Proof is Merkle proof with the key information.
 type Proof struct {
 	Proof *merkle.Proof
 	Key   []byte
 }
 
+// Implements commitment.Proof
 func (Proof) CommitmentKind() string {
 	return merkleKind
 }
 
+// Returns the key of the proof
 func (proof Proof) GetKey() []byte {
 	return proof.Key
 }
 
+// Verify() proves the proof against the given root, path, and value.
 func (proof Proof) Verify(croot commitment.Root, cpath commitment.Path, value []byte) error {
 	root, ok := croot.(Root)
 	if !ok {
@@ -78,7 +93,7 @@ func (proof Proof) Verify(croot commitment.Root, cpath commitment.Path, value []
 	}
 	keypath = keypath.AppendKey(append(path.KeyPrefix, proof.Key...), merkle.KeyEncodingHex)
 
-	// TODO: Hard coded for now, proof runtime should be extensible for other proof types
+	// TODO: hard coded for now, should be extensible
 	runtime := rootmulti.DefaultProofRuntime()
 
 	if value != nil {

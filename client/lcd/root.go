cosmos/cosmos-sdk

commit 99df748dda8c8390a5b3b8b1b8d53bd829282fd3

Merge PR #4285: Update REST Client to Support Custom R/W Timeouts

client/lcd/root.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rakyll/statik/fs"
@@ -45,20 +46,22 @@ func NewRestServer(cdc *codec.Codec) *RestServer {
 		Mux:    r,
 		CliCtx: cliCtx,
 		Cdc:    cdc,
-
-		log: logger,
+		log:    logger,
 	}
 }
 
 // Start starts the rest server
-func (rs *RestServer) Start(listenAddr string, maxOpen int) (err error) {
+func (rs *RestServer) Start(listenAddr string, maxOpen int, readTimeout, writeTimeout uint) (err error) {
 	server.TrapSignal(func() {
 		err := rs.listener.Close()
 		rs.log.Error("error closing listener", "err", err)
 	})
 
-	cfg := rpcserver.DefaultConfig()
-	cfg.MaxOpenConnections = maxOpen
+	cfg := &rpcserver.Config{
+		MaxOpenConnections: maxOpen,
+		ReadTimeout:        time.Duration(readTimeout) * time.Second,
+		WriteTimeout:       time.Duration(writeTimeout) * time.Second,
+	}
 
 	rs.listener, err = rpcserver.Listen(listenAddr, cfg)
 	if err != nil {
@@ -87,8 +90,12 @@ func ServeCommand(cdc *codec.Codec, registerRoutesFn func(*RestServer)) *cobra.C
 			registerRoutesFn(rs)
 
 			// Start the rest server and return error if one exists
-			err = rs.Start(viper.GetString(client.FlagListenAddr),
-				viper.GetInt(client.FlagMaxOpenConnections))
+			err = rs.Start(
+				viper.GetString(client.FlagListenAddr),
+				viper.GetInt(client.FlagMaxOpenConnections),
+				uint(viper.GetInt(client.FlagRPCReadTimeout)),
+				uint(viper.GetInt(client.FlagRPCWriteTimeout)),
+			)
 
 			return err
 		},

.pending/breaking/sdk/4263-RestServer-Star
@@ -1,0 +1,1 @@
+#4263 RestServer#Start now takes read and write timeout arguments.

.pending/features/sdk/4263-Add---read-time
@@ -1,0 +1,2 @@
+#4263 Add `--read-timeout` and `--write-timeout` args to the `rest-server` command
+to support custom RPC R/W timeouts.

client/flags.go
@@ -49,6 +49,8 @@ const (
 	FlagListenAddr         = "laddr"
 	FlagCORS               = "cors"
 	FlagMaxOpenConnections = "max-open"
+	FlagRPCReadTimeout     = "read-timeout"
+	FlagRPCWriteTimeout    = "write-timeout"
 	FlagOutputDocument     = "output-document" // inspired by wget -O
 	FlagSkipConfirmation   = "yes"
 )
@@ -116,7 +118,9 @@ func RegisterRestServerFlags(cmd *cobra.Command) *cobra.Command {
 	cmd = GetCommands(cmd)[0]
 	cmd.Flags().String(FlagListenAddr, "tcp://localhost:1317", "The address for the server to listen on")
 	cmd.Flags().String(FlagCORS, "", "Set the domains that can make CORS requests (* for all)")
-	cmd.Flags().Int(FlagMaxOpenConnections, 1000, "The number of maximum open connections")
+	cmd.Flags().Uint(FlagMaxOpenConnections, 1000, "The number of maximum open connections")
+	cmd.Flags().Uint(FlagRPCReadTimeout, 10, "The RPC read timeout (in seconds)")
+	cmd.Flags().Uint(FlagRPCWriteTimeout, 10, "The RPC write timeout (in seconds)")
 
 	return cmd
 }

commit 7eedc9568b0e8b08f9f815f42595163a0230a451

Rely on rpcserver.DefaultConfig() to inherit default config values
This fixes a small regression inadvertently introduced
in 99df748dda8c8 that caused the REST server to return
"http: request body too large" on POST requests handling.

client/lcd/root.go
@@ -54,11 +54,10 @@ func (rs *RestServer) Start(listenAddr string, maxOpen int, readTimeout, writeTi
 		rs.log.Error("error closing listener", "err", err)
 	})
 
-	cfg := &rpcserver.Config{
-		MaxOpenConnections: maxOpen,
-		ReadTimeout:        time.Duration(readTimeout) * time.Second,
-		WriteTimeout:       time.Duration(writeTimeout) * time.Second,
-	}
+	cfg := rpcserver.DefaultConfig()
+	cfg.MaxOpenConnections = maxOpen
+	cfg.ReadTimeout = time.Duration(readTimeout) * time.Second
+	cfg.WriteTimeout = time.Duration(writeTimeout) * time.Second
 
 	rs.listener, err = rpcserver.Listen(listenAddr, cfg)
 	if err != nil {
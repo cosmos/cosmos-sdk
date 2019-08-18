ee8e8c769293fef16fb381f9e478839b7d4bdeff
Marko Baricevic <marbar3778@yahoo.com>
2019-08-18 23:01:22 +0200

    add deleted tests

--- x/staking/keeper/validator_test.go
+++ x/staking/keeper/validator_test.go
@@ -292,6 +292,146 @@ func TestValidatorBasics(t *testing.T) {
 	require.False(t, found)
 }
 
+// test how the validators are sorted, tests GetBondedValidatorsByPower
+// func GetValidatorSortingUnmixed(t *testing.T) {
+// 	ctx, _, keeper, _ := CreateTestInput(t, false, 1000)
+
+// 	// initialize some validators into the state
+// 	amts := []int64{0, 100, 1, 400, 200}
+// 	n := len(amts)
+// 	var validators [5]types.Validator
+// 	for i, amt := range amts {
+// 		validators[i] = types.NewValidator(sdk.ValAddress(Addrs[i]), PKs[i], types.Description{})
+// 		validators[i].Status = sdk.Bonded
+// 		validators[i].Tokens = sdk.NewInt(amt)
+// 		validators[i].DelegatorShares = sdk.NewDec(amt)
+// 		TestingUpdateValidator(keeper, ctx, validators[i], true)
+// 	}
+
+// 	// first make sure everything made it in to the gotValidator group
+// 	resValidators := keeper.GetBondedValidatorsByPower(ctx)
+// 	assert.Equal(t, n, len(resValidators))
+// 	assert.Equal(t, sdk.NewInt(400), resValidators[0].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(200), resValidators[1].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(100), resValidators[2].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(1), resValidators[3].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(0), resValidators[4].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, validators[3].OperatorAddress, resValidators[0].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[4].OperatorAddress, resValidators[1].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[1].OperatorAddress, resValidators[2].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[2].OperatorAddress, resValidators[3].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[0].OperatorAddress, resValidators[4].OperatorAddress, "%v", resValidators)
+
+// 	// test a basic increase in voting power
+// 	validators[3].Tokens = sdk.NewInt(500)
+// 	TestingUpdateValidator(keeper, ctx, validators[3], true)
+// 	resValidators = keeper.GetBondedValidatorsByPower(ctx)
+// 	require.Equal(t, len(resValidators), n)
+// 	assert.True(ValEq(t, validators[3], resValidators[0]))
+
+// 	// test a decrease in voting power
+// 	validators[3].Tokens = sdk.NewInt(300)
+// 	TestingUpdateValidator(keeper, ctx, validators[3], true)
+// 	resValidators = keeper.GetBondedValidatorsByPower(ctx)
+// 	require.Equal(t, len(resValidators), n)
+// 	assert.True(ValEq(t, validators[3], resValidators[0]))
+// 	assert.True(ValEq(t, validators[4], resValidators[1]))
+
+// 	// test equal voting power, different age
+// 	validators[3].Tokens = sdk.NewInt(200)
+// 	ctx = ctx.WithBlockHeight(10)
+// 	TestingUpdateValidator(keeper, ctx, validators[3], true)
+// 	resValidators = keeper.GetBondedValidatorsByPower(ctx)
+// 	require.Equal(t, len(resValidators), n)
+// 	assert.True(ValEq(t, validators[3], resValidators[0]))
+// 	assert.True(ValEq(t, validators[4], resValidators[1]))
+
+// 	// no change in voting power - no change in sort
+// 	ctx = ctx.WithBlockHeight(20)
+// 	TestingUpdateValidator(keeper, ctx, validators[4], true)
+// 	resValidators = keeper.GetBondedValidatorsByPower(ctx)
+// 	require.Equal(t, len(resValidators), n)
+// 	assert.True(ValEq(t, validators[3], resValidators[0]))
+// 	assert.True(ValEq(t, validators[4], resValidators[1]))
+
+// 	// change in voting power of both validators, both still in v-set, no age change
+// 	validators[3].Tokens = sdk.NewInt(300)
+// 	validators[4].Tokens = sdk.NewInt(300)
+// 	TestingUpdateValidator(keeper, ctx, validators[3], true)
+// 	resValidators = keeper.GetBondedValidatorsByPower(ctx)
+// 	require.Equal(t, len(resValidators), n)
+// 	ctx = ctx.WithBlockHeight(30)
+// 	TestingUpdateValidator(keeper, ctx, validators[4], true)
+// 	resValidators = keeper.GetBondedValidatorsByPower(ctx)
+// 	require.Equal(t, len(resValidators), n, "%v", resValidators)
+// 	assert.True(ValEq(t, validators[3], resValidators[0]))
+// 	assert.True(ValEq(t, validators[4], resValidators[1]))
+// }
+
+// func GetValidatorSortingMixed(t *testing.T) {
+// 	ctx, _, keeper, _ := CreateTestInput(t, false, 1000)
+
+// 	// now 2 max resValidators
+// 	params := keeper.GetParams(ctx)
+// 	params.MaxValidators = 2
+// 	keeper.SetParams(ctx, params)
+
+// 	// initialize some validators into the state
+// 	amts := []int64{0, 100, 1, 400, 200}
+
+// 	n := len(amts)
+// 	var validators [5]types.Validator
+// 	for i, amt := range amts {
+// 		validators[i] = types.NewValidator(sdk.ValAddress(Addrs[i]), PKs[i], types.Description{})
+// 		validators[i].DelegatorShares = sdk.NewDec(amt)
+// 	}
+
+// 	validators[0].Status = sdk.Bonded
+// 	validators[1].Status = sdk.Bonded
+// 	validators[2].Status = sdk.Bonded
+// 	validators[0].Tokens = sdk.NewInt(amts[0])
+// 	validators[1].Tokens = sdk.NewInt(amts[1])
+// 	validators[2].Tokens = sdk.NewInt(amts[2])
+
+// 	validators[3].Status = sdk.Bonded
+// 	validators[4].Status = sdk.Bonded
+// 	validators[3].Tokens = sdk.NewInt(amts[3])
+// 	validators[4].Tokens = sdk.NewInt(amts[4])
+
+// 	for i := range amts {
+// 		TestingUpdateValidator(keeper, ctx, validators[i], true)
+// 	}
+// 	val0, found := keeper.GetValidator(ctx, sdk.ValAddress(sdk.ValAddress(PKs[0].Address().Bytes())))
+// 	require.True(t, found)
+// 	val1, found := keeper.GetValidator(ctx, sdk.ValAddress(Addrs[1]))
+// 	require.True(t, found)
+// 	val2, found := keeper.GetValidator(ctx, sdk.ValAddress(Addrs[2]))
+// 	require.True(t, found)
+// 	val3, found := keeper.GetValidator(ctx, sdk.ValAddress(Addrs[3]))
+// 	require.True(t, found)
+// 	val4, found := keeper.GetValidator(ctx, sdk.ValAddress(Addrs[4]))
+// 	require.True(t, found)
+// 	require.Equal(t, sdk.Unbonded, val0.Status)
+// 	require.Equal(t, sdk.Unbonded, val1.Status)
+// 	require.Equal(t, sdk.Unbonded, val2.Status)
+// 	require.Equal(t, sdk.Bonded, val3.Status)
+// 	require.Equal(t, sdk.Bonded, val4.Status)
+
+// 	// first make sure everything made it in to the gotValidator group
+// 	resValidators := keeper.GetBondedValidatorsByPower(ctx)
+// 	assert.Equal(t, n, len(resValidators))
+// 	assert.Equal(t, sdk.NewInt(400), resValidators[0].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(200), resValidators[1].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(100), resValidators[2].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(1), resValidators[3].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, sdk.NewInt(0), resValidators[4].BondedTokens(), "%v", resValidators)
+// 	assert.Equal(t, validators[3].OperatorAddress, resValidators[0].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[4].OperatorAddress, resValidators[1].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[1].OperatorAddress, resValidators[2].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[2].OperatorAddress, resValidators[3].OperatorAddress, "%v", resValidators)
+// 	assert.Equal(t, validators[0].OperatorAddress, resValidators[4].OperatorAddress, "%v", resValidators)
+// }
+
 // TODO separate out into multiple tests
 func TestGetValidatorsEdgeCases(t *testing.T) {
 	ctx, _, keeper, _ := CreateTestInput(t, false, 1000)

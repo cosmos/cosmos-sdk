93a85263ecf547b03871b5a082cb9fe886d679db
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-08-21 10:47:07 +0200

    Used type wrappers instead of re defining the whole type while converting the responses

--- client/rpc/block_converter.go
+++ client/rpc/block_converter.go
@@ -1,13 +1,11 @@
 package rpc
 
 import (
-	"time"
+	"encoding/json"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	cmn "github.com/tendermint/tendermint/libs/common"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/tendermint/tendermint/types"
-	"github.com/tendermint/tendermint/version"
 )
 
 type ResultBlock struct {
@@ -20,32 +18,22 @@ type BlockMeta struct {
 	Header  Header        `json:"header"`   // The block's Header
 }
 
+// Header defines a wrapper around Tendermint's Header type overriding various fields.
 type Header struct {
-	// basic block info
-	Version  version.Consensus `json:"version"`
-	ChainID  string            `json:"chain_id"`
-	Height   int64             `json:"height"`
-	Time     time.Time         `json:"time"`
-	NumTxs   int64             `json:"num_txs"`
-	TotalTxs int64             `json:"total_txs"`
+	// embed original type
+	types.Header
 
-	// prev block info
-	LastBlockID types.BlockID `json:"last_block_id"`
+	// override fields from original type
+	ProposerAddress sdk.ValAddress `json:"proposer_address"`
+}
 
-	// hashes of block data
-	LastCommitHash cmn.HexBytes `json:"last_commit_hash"` // commit from validators from the last block
-	DataHash       cmn.HexBytes `json:"data_hash"`        // transactions
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (h Header) MarshalJSON() ([]byte, error) {
+	type headerJSON Header
+	_h := headerJSON(h)
 
-	// hashes from the app output from the prev block
-	ValidatorsHash     cmn.HexBytes `json:"validators_hash"`      // validators for the current block
-	NextValidatorsHash cmn.HexBytes `json:"next_validators_hash"` // validators for the next block
-	ConsensusHash      cmn.HexBytes `json:"consensus_hash"`       // consensus params for current block
-	AppHash            cmn.HexBytes `json:"app_hash"`             // state after txs from the previous block
-	LastResultsHash    cmn.HexBytes `json:"last_results_hash"`    // root hash of all results from the txs from the previous block
-
-	// consensus info
-	EvidenceHash    cmn.HexBytes   `json:"evidence_hash"`    // evidence included in the block
-	ProposerAddress sdk.ValAddress `json:"proposer_address"` // original proposer of the block
+	return json.Marshal(_h)
 }
 
 type Block struct {
@@ -61,39 +49,24 @@ type Commit struct {
 }
 
 type CommitSig struct {
-	Type             types.SignedMsgType `json:"type"`
-	Height           int64               `json:"height"`
-	Round            int                 `json:"round"`
-	BlockID          types.BlockID       `json:"block_id"` // zero if vote is nil.
-	Timestamp        time.Time           `json:"timestamp"`
-	ValidatorAddress sdk.ValAddress      `json:"validator_address"`
-	ValidatorIndex   int                 `json:"validator_index"`
-	Signature        []byte              `json:"signature"`
+	types.CommitSig
+	ValidatorAddress sdk.ValAddress `json:"validator_address"`
+}
+
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (c CommitSig) MarshalJSON() ([]byte, error) {
+	type headerJSON CommitSig
+	_h := headerJSON(c)
+
+	return json.Marshal(_h)
 }
 
 func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 
 	// header
 	header := Header{
-		Version:  res.BlockMeta.Header.Version,
-		ChainID:  res.BlockMeta.Header.ChainID,
-		Height:   res.BlockMeta.Header.Height,
-		Time:     res.BlockMeta.Header.Time,
-		NumTxs:   res.BlockMeta.Header.NumTxs,
-		TotalTxs: res.BlockMeta.Header.TotalTxs,
-
-		LastBlockID: res.BlockMeta.Header.LastBlockID,
-
-		LastCommitHash: res.BlockMeta.Header.LastCommitHash,
-		DataHash:       res.BlockMeta.Header.DataHash,
-
-		ValidatorsHash:     res.BlockMeta.Header.ValidatorsHash,
-		NextValidatorsHash: res.BlockMeta.Header.NextValidatorsHash,
-		ConsensusHash:      res.BlockMeta.Header.ConsensusHash,
-		AppHash:            res.BlockMeta.Header.AppHash,
-		LastResultsHash:    res.BlockMeta.Header.LastResultsHash,
-
-		EvidenceHash:    res.BlockMeta.Header.EvidenceHash,
+		Header:          res.BlockMeta.Header,
 		ProposerAddress: sdk.ValAddress(res.BlockMeta.Header.ProposerAddress),
 	}
 
@@ -129,14 +102,8 @@ func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 func convertPreCommits(preCommits []*types.CommitSig) (sigs []CommitSig) {
 	for _, commit := range preCommits {
 		sig := CommitSig{
-			Type:             commit.Type,
-			Height:           commit.Height,
-			Round:            commit.Round,
-			BlockID:          commit.BlockID,
-			Timestamp:        commit.Timestamp,
+			CommitSig:        *commit,
 			ValidatorAddress: sdk.ValAddress(commit.ValidatorAddress),
-			ValidatorIndex:   commit.ValidatorIndex,
-			Signature:        commit.Signature,
 		}
 
 		sigs = append(sigs, sig)

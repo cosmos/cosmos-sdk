909faa0dd4a89396adcc47524b02ada2a447f380
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-08-26 08:02:10 +0200

    Unified how different types are treated (encapsulation) and added missing comments

--- client/rpc/block_converter.go
+++ client/rpc/block_converter.go
@@ -8,14 +8,37 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
+// ResultBlock defines a wrapper around the standard ResultBlock type overriding various fields.
+// nolint: structtag
 type ResultBlock struct {
+	ctypes.ResultBlock
 	BlockMeta BlockMeta `json:"block_meta"`
 	Block     Block     `json:"block"`
 }
 
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (r ResultBlock) MarshalJSON() ([]byte, error) {
+	type resultBlockJSON ResultBlock
+	_h := resultBlockJSON(r)
+
+	return json.Marshal(_h)
+}
+
+// BlockMeta defines a wrapper around Tendermint's BlockMeta type overriding various fields.
+// nolint: structtag
 type BlockMeta struct {
-	BlockID types.BlockID `json:"block_id"` // the block hash and partsethash
-	Header  Header        `json:"header"`   // The block's Header
+	types.BlockMeta
+	Header Header `json:"header"` // The block's Header
+}
+
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (b BlockMeta) MarshalJSON() ([]byte, error) {
+	type blockMetaJSON BlockMeta
+	_h := blockMetaJSON(b)
+
+	return json.Marshal(_h)
 }
 
 // Header defines a wrapper around Tendermint's Header type overriding various fields.
@@ -37,18 +60,40 @@ func (h Header) MarshalJSON() ([]byte, error) {
 	return json.Marshal(_h)
 }
 
+// Block defines a wrapper around Tendermint's Block type overriding various fields.
+// nolint: structtag
 type Block struct {
+	types.Block
 	Header     `json:"header"`
-	types.Data `json:"data"`
-	Evidence   types.EvidenceData `json:"evidence"`
-	LastCommit Commit             `json:"last_commit"`
+	LastCommit Commit `json:"last_commit"`
 }
 
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (b Block) MarshalJSON() ([]byte, error) {
+	type blockJSON Block
+	_h := blockJSON(b)
+
+	return json.Marshal(_h)
+}
+
+// Commit defines a wrapper around Tendermint's Commit type overriding various fields.
+// nolint: structtag
 type Commit struct {
-	BlockID    types.BlockID `json:"block_id"`
-	Precommits []CommitSig   `json:"precommits"`
+	types.Commit
+	Precommits []CommitSig `json:"precommits"`
 }
 
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (c Commit) MarshalJSON() ([]byte, error) {
+	type commitJSON Commit
+	_h := commitJSON(c)
+
+	return json.Marshal(_h)
+}
+
+// CommitSig defines a wrapper around Tendermint's CommitSig type overriding various fields.
 // nolint: structtag
 type CommitSig struct {
 	types.CommitSig
@@ -58,12 +103,14 @@ type CommitSig struct {
 // MarshalJSON implements the json.Marshaler interface. We do this because Amino
 // does not respect the JSON stdlib embedding semantics.
 func (c CommitSig) MarshalJSON() ([]byte, error) {
-	type headerJSON CommitSig
-	_h := headerJSON(c)
+	type commitSigJSON CommitSig
+	_h := commitSigJSON(c)
 
 	return json.Marshal(_h)
 }
 
+// ConvertBlockResult allows to convert the given standard ResultBlock into a new ResultBlock having all the
+// validator addresses as Bech32 strings instead of HEX ones.
 func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 
 	header := Header{
@@ -72,16 +119,16 @@ func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 	}
 
 	return ResultBlock{
+		ResultBlock: *res,
 		BlockMeta: BlockMeta{
-			BlockID: res.BlockMeta.BlockID,
-			Header:  header,
+			BlockMeta: *res.BlockMeta,
+			Header:    header,
 		},
 		Block: Block{
-			Header:   header,
-			Data:     res.Block.Data,
-			Evidence: res.Block.Evidence,
+			Block:  *res.Block,
+			Header: header,
 			LastCommit: Commit{
-				BlockID:    res.Block.LastCommit.BlockID,
+				Commit:     *res.Block.LastCommit,
 				Precommits: convertPreCommits(res.Block.LastCommit.Precommits),
 			},
 		},

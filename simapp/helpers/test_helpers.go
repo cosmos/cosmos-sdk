0d33e53c886ab94632c125f592ccf4314364d6a6
Federico Kunze <federico.kunze94@gmail.com>
2019-08-29 16:27:12 +0200

    increase gas x10

--- simapp/helpers/test_helpers.go
+++ simapp/helpers/test_helpers.go
@@ -19,7 +19,7 @@ const SimAppChainID = "simulation-app"
 func GenTx(msgs []sdk.Msg, feeAmt sdk.Coins, chainID string, accnums []uint64, seq []uint64, priv ...crypto.PrivKey) auth.StdTx {
 	fee := auth.StdFee{
 		Amount: feeAmt,
-		Gas:    100000, // TODO: this should be a param
+		Gas:    1000000, // TODO: this should be a param
 	}
 
 	sigs := make([]auth.StdSignature, len(priv))

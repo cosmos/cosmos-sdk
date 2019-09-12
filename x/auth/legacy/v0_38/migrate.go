1198733ff537de33be013c4fe78cf0f469eed20e
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-09-12 12:22:03 -0400

    Update godoc

--- x/auth/legacy/v0_38/migrate.go
+++ x/auth/legacy/v0_38/migrate.go
@@ -6,9 +6,8 @@ import (
 	v036auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v0_36"
 )
 
-// Migrate accepts exported genesis state from v0.34 and migrates it to v0.36
-// genesis state. This migration removes the CollectedFees coins from the old
-// FeeCollectorKeeper.
+// Migrate accepts exported genesis state from v0.34 and migrates it to v0.38
+// genesis state.
 func Migrate(oldGenState v036auth.GenesisState, accounts json.RawMessage) GenesisState {
 	return NewGenesisState(oldGenState.Params, accounts)
 }

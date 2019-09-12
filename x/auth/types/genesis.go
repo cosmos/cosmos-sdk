ed6366679c38e9d43aca901e1d67a7fc7efe435e
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2019-09-12 22:22:25 +0300

    Merge PR #5017: ADR 011: Generalize Genesis Accounts Implementation

--- x/auth/types/genesis.go
+++ x/auth/types/genesis.go
@@ -2,40 +2,71 @@ package types
 
 import (
 	"fmt"
+	"sort"
+
+	"github.com/cosmos/cosmos-sdk/x/auth/exported"
 )
 
 // GenesisState - all auth state that must be provided at genesis
 type GenesisState struct {
-	Params Params `json:"params" yaml:"params"`
+	Params   Params                    `json:"params" yaml:"params"`
+	Accounts []exported.GenesisAccount `json:"accounts" yaml:"accounts"`
 }
 
 // NewGenesisState - Create a new genesis state
-func NewGenesisState(params Params) GenesisState {
-	return GenesisState{params}
+func NewGenesisState(params Params, accounts []exported.GenesisAccount) GenesisState {
+	return GenesisState{
+		Params:   params,
+		Accounts: accounts,
+	}
 }
 
 // DefaultGenesisState - Return a default genesis state
 func DefaultGenesisState() GenesisState {
-	return NewGenesisState(DefaultParams())
+	return NewGenesisState(DefaultParams(), []exported.GenesisAccount{})
 }
 
 // ValidateGenesis performs basic validation of auth genesis data returning an
 // error for any failed validation criteria.
 func ValidateGenesis(data GenesisState) error {
-	if data.Params.TxSigLimit == 0 {
-		return fmt.Errorf("invalid tx signature limit: %d", data.Params.TxSigLimit)
+	if err := data.Params.Validate(); err != nil {
+		return err
 	}
-	if data.Params.SigVerifyCostED25519 == 0 {
-		return fmt.Errorf("invalid ED25519 signature verification cost: %d", data.Params.SigVerifyCostED25519)
+
+	return validateGenAccounts(data.Accounts)
+}
+
+// Sanitize sorts accounts and coin sets.
+func Sanitize(genAccs []exported.GenesisAccount) []exported.GenesisAccount {
+	sort.Slice(genAccs, func(i, j int) bool {
+		return genAccs[i].GetAccountNumber() < genAccs[j].GetAccountNumber()
+	})
+
+	for _, acc := range genAccs {
+		if err := acc.SetCoins(acc.GetCoins().Sort()); err != nil {
+			panic(err)
+		}
 	}
-	if data.Params.SigVerifyCostSecp256k1 == 0 {
-		return fmt.Errorf("invalid SECK256k1 signature verification cost: %d", data.Params.SigVerifyCostSecp256k1)
-	}
-	if data.Params.MaxMemoCharacters == 0 {
-		return fmt.Errorf("invalid max memo characters: %d", data.Params.MaxMemoCharacters)
-	}
-	if data.Params.TxSizeCostPerByte == 0 {
-		return fmt.Errorf("invalid tx size cost per byte: %d", data.Params.TxSizeCostPerByte)
+
+	return genAccs
+}
+
+func validateGenAccounts(accounts []exported.GenesisAccount) error {
+	addrMap := make(map[string]bool, len(accounts))
+	for _, acc := range accounts {
+
+		// check for duplicated accounts
+		addrStr := acc.GetAddress().String()
+		if _, ok := addrMap[addrStr]; ok {
+			return fmt.Errorf("duplicate account found in genesis state; address: %s", addrStr)
+		}
+
+		addrMap[addrStr] = true
+
+		// check account specific validation
+		if err := acc.Validate(); err != nil {
+			return fmt.Errorf("invalid account found in genesis state; address: %s, error: %s", addrStr, err.Error())
+		}
 	}
 	return nil
 }

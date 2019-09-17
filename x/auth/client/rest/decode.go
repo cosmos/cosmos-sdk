4c29424548d619e7294a723bcca0f53ef976654f
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-09-17 16:50:09 -0400

    Remove dup/invalid decode cmd

--- x/auth/client/rest/decode.go
+++ x/auth/client/rest/decode.go
@@ -5,13 +5,9 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-
-	"github.com/spf13/cobra"
-	amino "github.com/tendermint/go-amino"
 )
 
 type (
@@ -59,44 +55,4 @@ func DecodeTxRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 		response := DecodeResp(stdTx)
 		rest.PostProcessResponse(w, cliCtx, response)
 	}
-}
-
-// txDecodeRespStr implements a simple Stringer wrapper for a decoded tx.
-type txDecodeRespTx authtypes.StdTx
-
-func (tx txDecodeRespTx) String() string {
-	return tx.String()
-}
-
-// GetDecodeCommand returns the decode command to take Amino-serialized bytes and turn it into
-// a JSONified transaction
-func GetDecodeCommand(codec *amino.Codec) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "decode [amino-byte-string]",
-		Short: "Decode an amino-encoded transaction string",
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			cliCtx := context.NewCLIContext().WithCodec(codec)
-
-			txBytesBase64 := args[0]
-
-			txBytes, err := base64.StdEncoding.DecodeString(txBytesBase64)
-			if err != nil {
-				return err
-			}
-
-			var stdTx authtypes.StdTx
-			err = cliCtx.Codec.UnmarshalBinaryLengthPrefixed(txBytes, &stdTx)
-			if err != nil {
-				return err
-			}
-
-			response := txDecodeRespTx(stdTx)
-			_ = cliCtx.PrintOutput(response)
-
-			return nil
-		},
-	}
-
-	return client.PostCommands(cmd)[0]
 }

1e64e92e625029fbc7bd8e6de55cdfc3ef63e9f6
mossid <torecursedivine@gmail.com>
2019-09-18 17:51:40 +0200

    revise querier interface to work both on cli & store

--- store/state/types.go
+++ store/state/types.go
@@ -4,7 +4,9 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/crypto/merkle"
 
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
+	stypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -14,5 +16,46 @@ type Proof = merkle.Proof
 type Codec = codec.Codec
 
 type ABCIQuerier interface {
-	QueryABCI(req abci.RequestQuery) (abci.ResponseQuery, error)
+	Query(storeName string, key []byte) (abci.ResponseQuery, error)
+}
+
+var _ ABCIQuerier = CLIQuerier{}
+
+type CLIQuerier struct {
+	ctx context.CLIContext
+}
+
+func NewCLIQuerier(ctx context.CLIContext) CLIQuerier {
+	return CLIQuerier{ctx}
+}
+
+func (q CLIQuerier) Query(storeName string, key []byte) (abci.ResponseQuery, error) {
+	req := abci.RequestQuery{
+		Path:  "/store/" + storeName + "/key",
+		Data:  key,
+		Prove: true,
+	}
+
+	return q.ctx.QueryABCI(req)
+}
+
+var _ ABCIQuerier = StoreQuerier{}
+
+type StoreQuerier struct {
+	store stypes.Queryable
+}
+
+func NewStoreQuerier(store stypes.Queryable) StoreQuerier {
+	return StoreQuerier{store}
+}
+
+func (q StoreQuerier) Query(storeName string, key []byte) (abci.ResponseQuery, error) {
+	req := abci.RequestQuery{
+		Path:  "/" + storeName + "/key",
+		Data:  key,
+		Prove: true,
+	}
+
+	return q.store.Query(req), nil
+
 }

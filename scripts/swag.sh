4dd2bbfb3920a44ab5262c48ee3163187284fe7d
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-09-18 11:22:14 -0400

    Fix script

--- scripts/swag.sh
+++ scripts/swag.sh
@@ -11,7 +11,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Generate Swagger documentation from source and verify annotations have not
+# changed by checking if the swagger.yaml config is dirty or not. Since the
+# 'swag init' command does not currently support ignoring timestamps, the doc.go
+# file will always be modified so we must prevent that from always becoming dirty
+# by replacing it with the original after the command is executed.
+cp client/rest/docs/docs.go client/rest/docs/docs_bak.go
 ${swag_cmd} init -g client/rest/root.go --output client/rest/docs
+mv client/rest/docs/docs_bak.go client/rest/docs/docs.go
 
 if (($(git status --porcelain 2>/dev/null | grep 'client/rest/docs/swagger.yaml' | wc -l) > 0)); then
   echo -e "${RED}Swagger docs are out of sync!!!${NC}"

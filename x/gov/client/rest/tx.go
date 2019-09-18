cb477e2dd07ddecd45a5090d36780d684dd2945f
Jack Zampolin <jack.zampolin@gmail.com>
2019-09-18 07:04:57 -0700

    Merge PR #5059: Swagger Auto Gen - Gov

--- x/gov/client/rest/tx.go
+++ x/gov/client/rest/tx.go
@@ -25,6 +25,18 @@ func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router, phs []ProposalRE
 	r.HandleFunc(fmt.Sprintf("/gov/proposals/{%s}/votes", RestProposalID), voteHandlerFn(cliCtx)).Methods("POST")
 }
 
+// postProposalHandlerFn implements a proposal generation handler that
+// is responsible for constructing a properly formatted proposal for signing.
+//
+// @Summary Generate an unsigned proposal transaction
+// @Description Generate a proposal transaction that is ready for signing.
+// @Tags governance
+// @Accept  json
+// @Produce  json
+// @Param tx body rest.PostProposalReq true "The data required to construct a proposal message. The proposal_type can be text | parameter_change"
+// @Success 200 {object} types.StdTx
+// @Failure 400 {object} rest.ErrorResponse "Returned if the request is invalid"
+// @Router /gov/proposals [post]
 func postProposalHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req PostProposalReq
@@ -50,6 +62,19 @@ func postProposalHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	}
 }
 
+// depositHandlerFn implements a deposit generation handler that
+// is responsible for constructing a properly formatted deposit for signing.
+//
+// @Summary Generate an unsigned deposit transaction
+// @Description Generate a deposit transaction that is ready for signing.
+// @Tags governance
+// @Accept  json
+// @Produce  json
+// @Param proposalID path int true "The ID of the governance proposal to deposit to"
+// @Param tx body rest.DepositReq true "The data required to construct a deposit message"
+// @Success 200 {object} types.StdTx
+// @Failure 400 {object} rest.ErrorResponse "Returned if the request is invalid"
+// @Router /gov/proposals/{proposalID}/deposits [post]
 func depositHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -86,6 +111,19 @@ func depositHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	}
 }
 
+// voteHandlerFn implements a governance vote generation handler that
+// is responsible for constructing a properly formatted governance vote for signing.
+//
+// @Summary Generate an unsigned vote transaction
+// @Description Generate a vote transaction that is ready for signing.
+// @Tags governance
+// @Accept  json
+// @Produce  json
+// @Param proposalID path int true "The ID of the governance proposal to vote for"
+// @Param tx body rest.VoteReq true "The data required to construct a vote message"
+// @Success 200 {object} types.StdTx
+// @Failure 400 {object} rest.ErrorResponse "Returned if the request is invalid"
+// @Router /gov/proposals/{proposalID}/votes [post]
 func voteHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)

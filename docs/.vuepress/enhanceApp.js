720a4b8a774e2dd356fcc9502d463f92e96b6808
Denis Fadeev <denis@fadeev.org>
2019-09-20 08:38:24 +0500

    removed axios

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,13 +1,5 @@
-import axios from "axios";
-import Vue from "vue";
 import pageComponents from "@internal/page-components";
 import TmTooltip from "tm-tooltip";
-
-Vue.use({
-  install(Vue) {
-    Vue.prototype.$axios = axios.create();
-  }
-});
 
 export default ({ Vue }) => {
   Vue.component("def", TmTooltip);

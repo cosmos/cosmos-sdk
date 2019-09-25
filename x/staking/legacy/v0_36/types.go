906dcdb733e079097a6abb03448374d3c4a1ed83
Kwun Yeung <kwun@forbole.com>
2019-09-25 23:46:36 +0800

    Merge PR  #5099: Fix x/staking Migrate Bug

--- x/staking/legacy/v0_36/types.go
+++ x/staking/legacy/v0_36/types.go
@@ -5,10 +5,10 @@ package v0_36
 import (
 	"time"
 
-	"github.com/tendermint/tendermint/crypto"
-
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v034staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v0_34"
+	"github.com/tendermint/tendermint/crypto"
 )
 
 const (
@@ -30,6 +30,20 @@ type (
 	Validator struct {
 		OperatorAddress         sdk.ValAddress          `json:"operator_address" yaml:"operator_address"`
 		ConsPubKey              crypto.PubKey           `json:"consensus_pubkey" yaml:"consensus_pubkey"`
+		Jailed                  bool                    `json:"jailed" yaml:"jailed"`
+		Status                  sdk.BondStatus          `json:"status" yaml:"status"`
+		Tokens                  sdk.Int                 `json:"tokens" yaml:"tokens"`
+		DelegatorShares         sdk.Dec                 `json:"delegator_shares" yaml:"delegator_shares"`
+		Description             v034staking.Description `json:"description" yaml:"description"`
+		UnbondingHeight         int64                   `json:"unbonding_height" yaml:"unbonding_height"`
+		UnbondingCompletionTime time.Time               `json:"unbonding_time" yaml:"unbonding_time"`
+		Commission              Commission              `json:"commission" yaml:"commission"`
+		MinSelfDelegation       sdk.Int                 `json:"min_self_delegation" yaml:"min_self_delegation"`
+	}
+
+	bechValidator struct {
+		OperatorAddress         sdk.ValAddress          `json:"operator_address" yaml:"operator_address"`
+		ConsPubKey              string                  `json:"consensus_pubkey" yaml:"consensus_pubkey"`
 		Jailed                  bool                    `json:"jailed" yaml:"jailed"`
 		Status                  sdk.BondStatus          `json:"status" yaml:"status"`
 		Tokens                  sdk.Int                 `json:"tokens" yaml:"tokens"`
@@ -71,4 +85,50 @@ func NewGenesisState(
 		Redelegations:        reds,
 		Exported:             exported,
 	}
+}
+
+func (v Validator) MarshalJSON() ([]byte, error) {
+	bechConsPubKey, err := sdk.Bech32ifyConsPub(v.ConsPubKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return codec.Cdc.MarshalJSON(bechValidator{
+		OperatorAddress:         v.OperatorAddress,
+		ConsPubKey:              bechConsPubKey,
+		Jailed:                  v.Jailed,
+		Status:                  v.Status,
+		Tokens:                  v.Tokens,
+		DelegatorShares:         v.DelegatorShares,
+		Description:             v.Description,
+		UnbondingHeight:         v.UnbondingHeight,
+		UnbondingCompletionTime: v.UnbondingCompletionTime,
+		MinSelfDelegation:       v.MinSelfDelegation,
+		Commission:              v.Commission,
+	})
+}
+
+func (v *Validator) UnmarshalJSON(data []byte) error {
+	bv := &bechValidator{}
+	if err := codec.Cdc.UnmarshalJSON(data, bv); err != nil {
+		return err
+	}
+	consPubKey, err := sdk.GetConsPubKeyBech32(bv.ConsPubKey)
+	if err != nil {
+		return err
+	}
+	*v = Validator{
+		OperatorAddress:         bv.OperatorAddress,
+		ConsPubKey:              consPubKey,
+		Jailed:                  bv.Jailed,
+		Tokens:                  bv.Tokens,
+		Status:                  bv.Status,
+		DelegatorShares:         bv.DelegatorShares,
+		Description:             bv.Description,
+		UnbondingHeight:         bv.UnbondingHeight,
+		UnbondingCompletionTime: bv.UnbondingCompletionTime,
+		Commission:              bv.Commission,
+		MinSelfDelegation:       bv.MinSelfDelegation,
+	}
+	return nil
 }

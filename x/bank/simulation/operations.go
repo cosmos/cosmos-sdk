7c3314ef57b5bcc620de9606dff9e9cd5d9a6663
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-10-01 17:14:29 -0400

    x/bank/simulation/operations.go linting

--- x/bank/simulation/operations.go
+++ x/bank/simulation/operations.go
@@ -22,6 +22,7 @@ func SimulateMsgSend(ak types.AccountKeeper, bk keeper.Keeper) simulation.Operat
 		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context,
 		accs []simulation.Account, chainID string,
 	) (simulation.OperationMsg, []simulation.FutureOperation, error) {
+
 		if !bk.GetSendEnabled(ctx) {
 			return simulation.NoOpMsg(types.ModuleName), nil, nil
 		}
@@ -47,8 +48,11 @@ func SimulateMsgSend(ak types.AccountKeeper, bk keeper.Keeper) simulation.Operat
 }
 
 // sendMsgSend sends a transaction with a MsgSend from a provided random account.
-func sendMsgSend(r *rand.Rand, app *baseapp.BaseApp, ak types.AccountKeeper,
-	msg types.MsgSend, ctx sdk.Context, chainID string, privkeys []crypto.PrivKey) (err error) {
+func sendMsgSend(
+	r *rand.Rand, app *baseapp.BaseApp, ak types.AccountKeeper,
+	msg types.MsgSend, ctx sdk.Context, chainID string, privkeys []crypto.PrivKey,
+) (err error) {
+
 	account := ak.GetAccount(ctx, msg.FromAddress)
 	coins := account.SpendableCoins(ctx.BlockTime())
 
@@ -86,6 +90,7 @@ func SimulateSingleInputMsgMultiSend(ak types.AccountKeeper, bk keeper.Keeper) s
 		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context,
 		accs []simulation.Account, chainID string,
 	) (simulation.OperationMsg, []simulation.FutureOperation, error) {
+
 		if !bk.GetSendEnabled(ctx) {
 			return simulation.NoOpMsg(types.ModuleName), nil, nil
 		}
@@ -115,8 +120,11 @@ func SimulateSingleInputMsgMultiSend(ak types.AccountKeeper, bk keeper.Keeper) s
 
 // sendMsgMultiSend sends a transaction with a MsgMultiSend from a provided random
 // account.
-func sendMsgMultiSend(r *rand.Rand, app *baseapp.BaseApp, ak types.AccountKeeper,
-	msg types.MsgMultiSend, ctx sdk.Context, chainID string, privkeys []crypto.PrivKey) (err error) {
+func sendMsgMultiSend(
+	r *rand.Rand, app *baseapp.BaseApp, ak types.AccountKeeper,
+	msg types.MsgMultiSend, ctx sdk.Context, chainID string, privkeys []crypto.PrivKey,
+) (err error) {
+
 	initialInputAddrCoins := make([]sdk.Coins, len(msg.Inputs))
 	initialOutputAddrCoins := make([]sdk.Coins, len(msg.Outputs))
 	accountNumbers := make([]uint64, len(msg.Inputs))
@@ -166,8 +174,10 @@ func sendMsgMultiSend(r *rand.Rand, app *baseapp.BaseApp, ak types.AccountKeeper
 
 // randomSendFields returns the sender and recipient simulation accounts as well
 // as the transferred amount.
-func randomSendFields(r *rand.Rand, ctx sdk.Context, accs []simulation.Account,
-	ak types.AccountKeeper) (simulation.Account, simulation.Account, sdk.Coins, bool, error) {
+func randomSendFields(
+	r *rand.Rand, ctx sdk.Context, accs []simulation.Account, ak types.AccountKeeper,
+) (simulation.Account, simulation.Account, sdk.Coins, bool, error) {
+
 	simAccount, _ := simulation.RandomAcc(r, accs)
 	toSimAcc, _ := simulation.RandomAcc(r, accs)
 

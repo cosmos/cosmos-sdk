0fd1234335efbdded572edcee485138a59b8fea7
Aditya Sripal <adityasripal@gmail.com>
2019-10-08 09:53:55 -0700

    add order comments

--- x/auth/ante/ante.go
+++ x/auth/ante/ante.go
@@ -11,12 +11,12 @@ import (
 // signer.
 func NewAnteHandler(ak keeper.AccountKeeper, supplyKeeper types.SupplyKeeper, sigGasConsumer SignatureVerificationGasConsumer) sdk.AnteHandler {
 	return sdk.ChainAnteDecorators(
-		NewSetUpContextDecorator(), // outermost AnteDecorator
+		NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
 		NewMempoolFeeDecorator(),
 		NewValidateBasicDecorator(),
 		NewValidateMemoDecorator(ak),
 		NewConsumeGasForTxSizeDecorator(ak),
-		NewSetPubKeyDecorator(ak),
+		NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
 		NewValidateSigCountDecorator(ak),
 		NewDeductFeeDecorator(ak, supplyKeeper),
 		NewSigGasConsumeDecorator(ak, sigGasConsumer),

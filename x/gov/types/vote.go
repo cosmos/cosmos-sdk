6a06701e302bdeaad9c4837efe52a3324511e90b
Marko Baricevic <marbar3778@yahoo.com>
2019-10-14 15:18:07 +0200

    another try v2

--- x/gov/types/vote.go
+++ x/gov/types/vote.go
@@ -143,7 +143,7 @@ func (vo VoteOption) String() string {
 }
 
 // Format implements the fmt.Formatter interface.
-// nolint: errcheck
+// nolint:errcheck
 func (vo VoteOption) Format(s fmt.State, verb rune) {
 	switch verb {
 	case 's':

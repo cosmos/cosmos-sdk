8be3af78e5c3a0d3b5d564d1d75a29e63d3a5e99
Federico Kunze <federico.kunze94@gmail.com>
2019-10-16 15:56:22 +0200

    rename pkg

--- x/ibc/client/cli/cli.go
+++ x/ibc/client/cli/cli.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
-	ics02 "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
+	ibcclient "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
 	"github.com/cosmos/cosmos-sdk/x/ibc/types"
 )
 
@@ -20,7 +20,7 @@ func GetTxCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 	}
 
 	ibcTxCmd.AddCommand(
-		ics02.GetTxCmd(cdc, storeKey),
+		ibcclient.GetTxCmd(cdc, storeKey),
 	)
 	return ibcTxCmd
 }
@@ -37,7 +37,7 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	}
 
 	ibcQueryCmd.AddCommand(
-		ics02.GetQueryCmd(cdc, queryRoute),
+		ibcclient.GetQueryCmd(cdc, queryRoute),
 	)
 	return ibcQueryCmd
 }
--- x/ibc/keeper/keeper.go
+++ x/ibc/keeper/keeper.go
@@ -3,17 +3,17 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	ics02 "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
+	client "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
 )
 
 // Keeper defines each ICS keeper for IBC
 type Keeper struct {
-	ClientKeeper ics02.Keeper
+	ClientKeeper client.Keeper
 }
 
 // NewKeeper creates a new ibc Keeper
 func NewKeeper(cdc *codec.Codec, key sdk.StoreKey, codespace sdk.CodespaceType) Keeper {
 	return Keeper{
-		ClientKeeper: ics02.NewKeeper(cdc, key, codespace),
+		ClientKeeper: client.NewKeeper(cdc, key, codespace),
 	}
 }

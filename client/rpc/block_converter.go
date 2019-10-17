b5207747f3cf92979da2d88e0abd9bf0c59e6499
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-10-17 09:41:44 +0200

    Fixed the missing of some fields

--- client/rpc/block_converter.go
+++ client/rpc/block_converter.go
@@ -6,6 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/tendermint/tendermint/types"
+	"github.com/tendermint/tendermint/version"
 )
 
 // ResultBlock represents a single block with metadata
@@ -27,6 +28,11 @@ type Header struct {
 	types.Header
 
 	// override fields from original type
+	Version         Consensus      `json:"version"`
+	Height          int64          `json:"height,string"`    // Override int64 fields do to json.Marshal not converting them to string
+	NumTxs          int64          `json:"num_txs,string"`   // Override int64 fields do to json.Marshal not converting them to string
+	TotalTxs        int64          `json:"total_txs,string"` // Override int64 fields do to json.Marshal not converting them to string
+	LastBlockID     BlockID        `json:"last_block_id"`
 	ProposerAddress sdk.ValAddress `json:"proposer_address"`
 }
 
@@ -35,37 +41,112 @@ type Header struct {
 func (h Header) MarshalJSON() ([]byte, error) {
 	type headerJSON Header
 	_h := headerJSON(h)
+	return json.Marshal(_h)
+}
 
+// BlockID defines a wrapper around Tendermint's BlockID type overriding various fields.
+// nolint: structtag
+type BlockID struct {
+	// embed original type
+	types.BlockID
+
+	// override fields from original type
+	PartsHeader PartSetHeader `json:"parts"`
+}
+
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (b BlockID) MarshalJSON() ([]byte, error) {
+	type blockIDJson BlockID
+	_h := blockIDJson(b)
+	return json.Marshal(_h)
+}
+
+// PartSetHeader defines a wrapper around Tendermint's PartSetHeader type overriding various fields.
+// nolint: structtag
+type PartSetHeader struct {
+	// embed original type
+	types.PartSetHeader
+
+	// override fields from original type
+	Total int `json:"total,string"`
+}
+
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (b PartSetHeader) MarshalJSON() ([]byte, error) {
+	type partSetHeadJson PartSetHeader
+	_h := partSetHeadJson(b)
+	return json.Marshal(_h)
+}
+
+// Consensus defines a wrapper around Tendermint's Consensus type overriding various fields.
+// nolint: structtag
+type Consensus struct {
+	// embed original type
+	version.Consensus
+
+	// override fields from original type
+	App   uint64 `json:"app,string"`   // Override uint64 fields do to json.Marshal not converting them to string
+	Block uint64 `json:"block,string"` // Override uint64 fields do to json.Marshal not converting them to string
+}
+
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (b Consensus) MarshalJSON() ([]byte, error) {
+	type consensusJson Consensus
+	_h := consensusJson(b)
 	return json.Marshal(_h)
 }
 
 // Block defines the atomic unit of a Tendermint blockchain.
 type Block struct {
-	Header     `json:"header"`
-	types.Data `json:"data"`
-	Evidence   types.EvidenceData `json:"evidence"`
-	LastCommit Commit             `json:"last_commit"`
+	// embed original type
+	types.Block
+
+	// override fields
+	Header     Header `json:"header"`
+	LastCommit Commit `json:"last_commit"`
 }
 
-// Commit contains the evidence that a block was committed by a set of validators.
-// NOTE: Commit is empty for height 1, but never nil.
+// MarshalJSON implements the json.Marshaler interface. We do this because Amino
+// does not respect the JSON stdlib embedding semantics.
+func (b Block) MarshalJSON() ([]byte, error) {
+	type blockJSON Block
+	_b := blockJSON(b)
+	return json.Marshal(_b)
+}
+
+// Commit defines a wrapper around Tendermint's Commit type overriding various fields.
+// nolint: structtag.
 type Commit struct {
-	BlockID    types.BlockID `json:"block_id"`
-	Precommits []CommitSig   `json:"precommits"`
+	// embed original type
+	*types.Commit
+
+	// override fields
+	BlockID    BlockID     `json:"block_id"`
+	Precommits []CommitSig `json:"precommits"`
 }
 
 // CommitSig defines a wrapper around Tendermint's CommitSig type overriding various fields.
 // nolint: structtag
 type CommitSig struct {
+	// embed original type
 	types.CommitSig
+
+	// override fields
+	Height           int64          `json:"height,string"` // Override int64 fields do to json.Marshal not converting them to string
+	Round            int            `json:"round,string"`  // Override int fields do to json.Marshal not converting them to string
+	BlockID          BlockID        `json:"block_id"`
 	ValidatorAddress sdk.ValAddress `json:"validator_address"`
+	ValidatorIndex   int            `json:"validator_index,string"` // Override int fields do to json.Marshal not converting them to string
 }
 
 // MarshalJSON implements the json.Marshaler interface. We do this because Amino
 // does not respect the JSON stdlib embedding semantics.
 func (c CommitSig) MarshalJSON() ([]byte, error) {
-	type headerJSON CommitSig
-	_h := headerJSON(c)
+	type commitSigJson CommitSig
+	_h := commitSigJson(c)
 
 	return json.Marshal(_h)
 }
@@ -75,7 +156,24 @@ func (c CommitSig) MarshalJSON() ([]byte, error) {
 func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 
 	header := Header{
-		Header:          res.BlockMeta.Header,
+		Header: res.BlockMeta.Header,
+
+		Version: Consensus{
+			Consensus: res.BlockMeta.Header.Version,
+			App:       res.BlockMeta.Header.Version.App.Uint64(),
+			Block:     res.BlockMeta.Header.Version.Block.Uint64(),
+		},
+		Height:   res.BlockMeta.Header.Height,
+		NumTxs:   res.BlockMeta.Header.NumTxs,
+		TotalTxs: res.BlockMeta.Header.TotalTxs,
+		LastBlockID: BlockID{
+			BlockID: res.BlockMeta.Header.LastBlockID,
+			PartsHeader: PartSetHeader{
+				PartSetHeader: res.BlockMeta.Header.LastBlockID.PartsHeader,
+				Total:         res.BlockMeta.Header.LastBlockID.PartsHeader.Total,
+			},
+		},
+
 		ProposerAddress: sdk.ValAddress(res.BlockMeta.Header.ProposerAddress),
 	}
 
@@ -85,11 +183,16 @@ func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 			Header:  header,
 		},
 		Block: Block{
-			Header:   header,
-			Data:     res.Block.Data,
-			Evidence: res.Block.Evidence,
+			Header: header,
 			LastCommit: Commit{
-				BlockID:    res.Block.LastCommit.BlockID,
+				Commit: res.Block.LastCommit,
+				BlockID: BlockID{
+					BlockID: res.Block.LastCommit.BlockID,
+					PartsHeader: PartSetHeader{
+						PartSetHeader: res.Block.LastCommit.BlockID.PartsHeader,
+						Total:         res.Block.LastCommit.BlockID.PartsHeader.Total,
+					},
+				},
 				Precommits: convertPreCommits(res.Block.LastCommit.Precommits),
 			},
 		},
@@ -99,8 +202,18 @@ func ConvertBlockResult(res *ctypes.ResultBlock) (blockResult ResultBlock) {
 func convertPreCommits(preCommits []*types.CommitSig) (sigs []CommitSig) {
 	for _, commit := range preCommits {
 		sig := CommitSig{
-			CommitSig:        *commit,
+			CommitSig: *commit,
+			Height:    commit.Height,
+			Round:     commit.Round,
+			BlockID: BlockID{
+				BlockID: commit.BlockID,
+				PartsHeader: PartSetHeader{
+					PartSetHeader: commit.BlockID.PartsHeader,
+					Total:         commit.BlockID.PartsHeader.Total,
+				},
+			},
 			ValidatorAddress: sdk.ValAddress(commit.ValidatorAddress),
+			ValidatorIndex:   commit.ValidatorIndex,
 		}
 
 		sigs = append(sigs, sig)

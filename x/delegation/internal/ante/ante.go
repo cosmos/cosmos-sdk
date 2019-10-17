e476c030dc1c5b0a180a69abe124124a76e3c976
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-17 13:19:57 +0200

    Build full ante handler stack and test it

---
+++ x/delegation/internal/ante/ante.go
@@ -0,0 +1,27 @@
+package ante
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authAnte "github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authKeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authTypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/delegation/internal/keeper"
+)
+
+// NewAnteHandler is just like auth.NewAnteHandler, except we use the DeductDelegatedFeeDecorator
+// in order to allow payment of fees via a delegation.
+func NewAnteHandler(ak authKeeper.AccountKeeper, supplyKeeper authTypes.SupplyKeeper, dk keeper.Keeper, sigGasConsumer authAnte.SignatureVerificationGasConsumer) sdk.AnteHandler {
+	return sdk.ChainAnteDecorators(
+		authAnte.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		authAnte.NewMempoolFeeDecorator(),
+		authAnte.NewValidateBasicDecorator(),
+		authAnte.NewValidateMemoDecorator(ak),
+		authAnte.NewConsumeGasForTxSizeDecorator(ak),
+		authAnte.NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
+		authAnte.NewValidateSigCountDecorator(ak),
+		NewDeductDelegatedFeeDecorator(ak, supplyKeeper, dk),
+		authAnte.NewSigGasConsumeDecorator(ak, sigGasConsumer),
+		authAnte.NewSigVerificationDecorator(ak),
+		authAnte.NewIncrementSequenceDecorator(ak), // innermost AnteDecorator
+	)
+}
--- x/delegation/internal/ante/fee_test.go
+++ x/delegation/internal/ante/fee_test.go
@@ -19,8 +19,13 @@ import (
 func TestDeductFeesNoDelegation(t *testing.T) {
 	// setup
 	app, ctx := createTestApp(true)
+
+	// this just tests our handler
 	dfd := ante.NewDeductDelegatedFeeDecorator(app.AccountKeeper, app.SupplyKeeper, app.DelegationKeeper)
-	antehandler := sdk.ChainAnteDecorators(dfd)
+	ourAnteHandler := sdk.ChainAnteDecorators(dfd)
+
+	// this tests the whole stack
+	anteHandlerStack := ante.NewAnteHandler(app.AccountKeeper, app.SupplyKeeper, app.DelegationKeeper, SigGasNoConsumer)
 
 	// keys and addresses
 	priv1, _, addr1 := authtypes.KeyTestPubAddr()
@@ -72,65 +77,145 @@ func TestDeductFeesNoDelegation(t *testing.T) {
 		signerKey  crypto.PrivKey
 		signer     sdk.AccAddress
 		feeAccount sdk.AccAddress
+		handler    sdk.AnteHandler
 		fee        int64
 		valid      bool
 	}{
-		"paying with low funds": {
+		"paying with low funds (only ours)": {
 			signerKey: priv1,
 			signer:    addr1,
 			fee:       50,
+			handler:   ourAnteHandler,
 			valid:     false,
 		},
-		"paying with good funds": {
+		"paying with good funds (only ours)": {
 			signerKey: priv2,
 			signer:    addr2,
 			fee:       50,
+			handler:   ourAnteHandler,
 			valid:     true,
 		},
-		"paying with no account": {
+		"paying with no account (only ours)": {
 			signerKey: priv3,
 			signer:    addr3,
 			fee:       1,
+			handler:   ourAnteHandler,
 			valid:     false,
 		},
-		"no fee with real account": {
+		"no fee with real account (only ours)": {
 			signerKey: priv1,
 			signer:    addr1,
 			fee:       0,
+			handler:   ourAnteHandler,
 			valid:     true,
 		},
-		"no fee with no account": {
+		"no fee with no account (only ours)": {
 			signerKey: priv4,
 			signer:    addr4,
 			fee:       0,
+			handler:   ourAnteHandler,
 			valid:     true,
 		},
-		"valid delegation": {
+		"valid delegation (only ours)": {
 			signerKey:  priv3,
 			signer:     addr3,
 			feeAccount: addr2,
 			fee:        50,
+			handler:    ourAnteHandler,
 			valid:      true,
 		},
-		"no delegation": {
+		"no delegation (only ours)": {
 			signerKey:  priv3,
 			signer:     addr3,
 			feeAccount: addr1,
 			fee:        2,
+			handler:    ourAnteHandler,
 			valid:      false,
 		},
-		"allowance smaller than requested fee": {
+		"allowance smaller than requested fee (only ours)": {
 			signerKey:  priv4,
 			signer:     addr4,
 			feeAccount: addr2,
 			fee:        50,
+			handler:    ourAnteHandler,
 			valid:      false,
 		},
-		"granter cannot cover allowed delegation": {
+		"granter cannot cover allowed delegation (only ours)": {
 			signerKey:  priv4,
 			signer:     addr4,
 			feeAccount: addr1,
 			fee:        50,
+			handler:    ourAnteHandler,
+			valid:      false,
+		},
+
+		"paying with low funds (whole stack)": {
+			signerKey: priv1,
+			signer:    addr1,
+			fee:       50,
+			handler:   anteHandlerStack,
+			valid:     false,
+		},
+		"paying with good funds (whole stack)": {
+			signerKey: priv2,
+			signer:    addr2,
+			fee:       50,
+			handler:   anteHandlerStack,
+			valid:     true,
+		},
+		"paying with no account (whole stack)": {
+			signerKey: priv3,
+			signer:    addr3,
+			fee:       1,
+			handler:   anteHandlerStack,
+			valid:     false,
+		},
+		"no fee with real account (whole stack)": {
+			signerKey: priv1,
+			signer:    addr1,
+			fee:       0,
+			handler:   anteHandlerStack,
+			valid:     true,
+		},
+		// TODO: cannot pay zero fees if account doesn't exist (is this good?)
+		// "no fee with no account (whole stack)": {
+		// 	signerKey: priv4,
+		// 	signer:    addr4,
+		// 	fee:       0,
+		// 	handler:   anteHandlerStack,
+		// 	valid:     true,
+		// },
+		// TODO: cannot delegate fees if account doesn't exist (this must change for delegation)
+		// "valid delegation (whole stack)": {
+		// 	signerKey:  priv3,
+		// 	signer:     addr3,
+		// 	feeAccount: addr2,
+		// 	fee:        50,
+		// 	handler:    anteHandlerStack,
+		// 	valid:      true,
+		// },
+		"no delegation (whole stack)": {
+			signerKey:  priv3,
+			signer:     addr3,
+			feeAccount: addr1,
+			fee:        2,
+			handler:    anteHandlerStack,
+			valid:      false,
+		},
+		"allowance smaller than requested fee (whole stack)": {
+			signerKey:  priv4,
+			signer:     addr4,
+			feeAccount: addr2,
+			fee:        50,
+			handler:    anteHandlerStack,
+			valid:      false,
+		},
+		"granter cannot cover allowed delegation (whole stack)": {
+			signerKey:  priv4,
+			signer:     addr4,
+			feeAccount: addr1,
+			fee:        50,
+			handler:    anteHandlerStack,
 			valid:      false,
 		},
 	}
@@ -143,7 +228,7 @@ func TestDeductFeesNoDelegation(t *testing.T) {
 			privs, accNums, seqs := []crypto.PrivKey{tc.signerKey}, []uint64{0}, []uint64{0}
 			tx := authtypes.NewTestTxWithFeeAccount(ctx, msgs, privs, accNums, seqs, fee, tc.feeAccount)
 
-			_, err := antehandler(ctx, tx, false)
+			_, err := tc.handler(ctx, tx, false)
 			if tc.valid {
 				require.NoError(t, err)
 			} else {
@@ -160,4 +245,9 @@ func createTestApp(isCheckTx bool) (*simapp.SimApp, sdk.Context) {
 	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
 
 	return app, ctx
+}
+
+// don't cosume any gas
+func SigGasNoConsumer(meter sdk.GasMeter, sig []byte, pubkey crypto.PubKey, params authtypes.Params) error {
+	return nil
 }

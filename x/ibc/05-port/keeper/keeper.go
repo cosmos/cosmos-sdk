cabd902f9c7620891acf1cb570881c0212a14237
Christopher Goes <cwgoes@pluranimity.org>
2019-10-21 12:10:50 +0200

    Refactor to static bind

--- x/ibc/05-port/keeper/keeper.go
+++ x/ibc/05-port/keeper/keeper.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/ibc/05-port/exported"
 	"github.com/cosmos/cosmos-sdk/x/ibc/05-port/types"
 )
 
@@ -16,6 +14,7 @@ type Keeper struct {
 	cdc       *codec.Codec
 	codespace sdk.CodespaceType
 	prefix    []byte // prefix bytes for accessing the store
+	ports     map[sdk.CapabilityKey]string
 }
 
 // NewKeeper creates a new IBC connection Keeper instance
@@ -28,92 +27,18 @@ func NewKeeper(cdc *codec.Codec, key sdk.StoreKey, codespace sdk.CodespaceType) 
 	}
 }
 
-// GetPort returns a port with a particular identifier
-func (k Keeper) GetPort(ctx sdk.Context, portID string) (sdk.CapabilityKey, bool) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), k.prefix)
-	bz := store.Get(types.KeyPort(portID))
-	if bz == nil {
-		return nil, false
-	}
-
-	var capabilityKey sdk.CapabilityKey
-	k.cdc.MustUnmarshalBinaryLengthPrefixed(bz, &capabilityKey)
-	return capabilityKey, true
-}
-
-// SetPort sets a port to the store
-func (k Keeper) SetPort(ctx sdk.Context, portID string, capabilityKey sdk.CapabilityKey) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), k.prefix)
-	bz := k.cdc.MustMarshalBinaryLengthPrefixed(capabilityKey)
-	store.Set(types.KeyPort(portID), bz)
-}
-
-// delete a port ID key from the store
-func (k Keeper) deletePort(ctx sdk.Context, portID string) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), k.prefix)
-	store.Delete(types.KeyPort(portID))
-}
-
-// BindPort binds to an unallocated port, failing if the port has already been
-// allocated.
-func (k Keeper) BindPort(ctx sdk.Context, portID string, generateFn exported.Generate) sdk.Error {
+// BindPort binds to a port, returning the capability key which must be given to a module
+func (k Keeper) BindPort(portID string) sdk.CapabilityKey {
 	if !types.ValidatePortID(portID) {
-		return types.ErrInvalidPortID(k.codespace)
+		panic(fmt.Sprintf("invalid port id: %s", types.ErrInvalidPortID(k.codespace)))
 	}
 
-	_, found := k.GetPort(ctx, portID)
-	if found {
-		return types.ErrPortExists(k.codespace, portID)
-	}
-	key := generateFn()
-	k.SetPort(ctx, portID, key)
-	return nil
+	key := sdk.NewKVStoreKey(portID)
+	k.ports[key] = portID
+	return key
 }
 
-// TransferPort allows an existing port (i.e capability key) to be transfered
-// to another module and thus stored under another path.
-//
-// NOTE: not neccessary if the host state machine supports object-capabilities,
-// since the port reference is a bearer capability.
-func (k Keeper) TransferPort(
-	ctx sdk.Context,
-	portID string,
-	authenticateFn exported.Authenticate,
-	generateFn exported.Generate,
-) sdk.Error {
-	port, found := k.GetPort(ctx, portID)
-	if !found {
-		return types.ErrPortNotFound(k.codespace, portID)
-	}
-
-	if !authenticateFn(port) {
-		return types.ErrPortNotAuthenticated(k.codespace)
-	}
-
-	key := generateFn()
-	k.SetPort(ctx, portID, key)
-	return nil
-}
-
-// ReleasePort allows a module to release a port such that other modules may
-// then bind to it.
-//
-// WARNING: releasing a port will allow other modules to bind to that port and
-// possibly intercept incoming channel opening handshakes. Modules should release
-// ports only when doing so is safe.
-func (k Keeper) ReleasePort(
-	ctx sdk.Context,
-	portID string,
-	authenticateFn exported.Authenticate) sdk.Error {
-	port, found := k.GetPort(ctx, portID)
-	if !found {
-		return types.ErrPortNotFound(k.codespace, portID)
-	}
-
-	if !authenticateFn(port) {
-		return types.ErrPortNotAuthenticated(k.codespace)
-	}
-
-	k.deletePort(ctx, portID)
-	return nil
+// Authenticate authenticates a key against a port ID
+func (k Keeper) Authenticate(key sdk.CapabilityKey, portID string) bool {
+	return k.ports[key] == portID
 }

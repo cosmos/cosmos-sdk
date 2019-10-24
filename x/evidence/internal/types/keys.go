45db95ef67bcd4878f1b071e571d33ab440a0d8e
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-10-24 15:40:32 -0400

    add module constants

---
+++ x/evidence/internal/types/keys.go
@@ -0,0 +1,18 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "evidence"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// RouterKey defines the module's message routing key
+	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	QuerierRoute = ModuleName
+
+	// DefaultParamspace defines the module's default paramspace name
+	DefaultParamspace = ModuleName
+)

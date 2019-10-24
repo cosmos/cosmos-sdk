e8851a63c4805b1e86e4eb182178042c574848ac
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-10-24 16:30:23 -0400

    update alias'

--- x/evidence/alias.go
+++ x/evidence/alias.go
@@ -1,1 +1,25 @@
 package evidence
+
+import (
+	"github.com/cosmos/cosmos-sdk/x/evidence/internal/types"
+)
+
+const (
+	ModuleName        = types.ModuleName
+	StoreKey          = types.StoreKey
+	RouterKey         = types.RouterKey
+	QuerierRoute      = types.QuerierRoute
+	DefaultParamspace = types.DefaultParamspace
+)
+
+var (
+	RegisterCodec             = types.RegisterCodec
+	RegisterEvidenceTypeCodec = types.RegisterEvidenceTypeCodec
+	ModuleCdc                 = types.ModuleCdc
+)
+
+type (
+	Evidence = types.Evidence
+	Handler  = types.Handler
+	Router   = types.Router
+)

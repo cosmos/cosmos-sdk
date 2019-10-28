a593d67b5683d27cfad113b9395dab062b0f6b4c
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-10-28 13:33:36 -0400

    add ErrNoEvidenceExists

--- x/evidence/internal/types/errors.go
+++ x/evidence/internal/types/errors.go
@@ -13,9 +13,10 @@ const (
 
 	CodeNoEvidenceHandlerExists sdk.CodeType = 1
 	CodeInvalidEvidence         sdk.CodeType = 2
+	CodeNoEvidenceExists        sdk.CodeType = 3
 )
 
-// ErrNoEvidenceHandlerExists returns a typed error for an invalid evidence
+// ErrNoEvidenceHandlerExists returns a typed ABCI error for an invalid evidence
 // handler route.
 func ErrNoEvidenceHandlerExists(codespace sdk.CodespaceType, route string) error {
 	return sdkerrors.New(
@@ -25,11 +26,21 @@ func ErrNoEvidenceHandlerExists(codespace sdk.CodespaceType, route string) error
 	)
 }
 
-// ErrInvalidEvidence returns a typed error for invalid evidence.
+// ErrInvalidEvidence returns a typed ABCI error for invalid evidence.
 func ErrInvalidEvidence(codespace sdk.CodespaceType, err error) error {
 	return sdkerrors.New(
 		string(codespace),
 		uint32(CodeInvalidEvidence),
 		fmt.Sprintf("invalid evidence: %s", err),
+	)
+}
+
+// ErrNoEvidenceExists returns a typed ABCI error for Evidence that does not exist
+// for a given hash.
+func ErrNoEvidenceExists(codespace sdk.CodespaceType, hash string) error {
+	return sdkerrors.New(
+		string(codespace),
+		uint32(CodeNoEvidenceExists),
+		fmt.Sprintf("evidence with hash %s does not exist", hash),
 	)
 }

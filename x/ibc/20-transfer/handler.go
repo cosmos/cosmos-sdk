b772c8e6dec276aeab16e447080de050b77c204a
Federico Kunze <federico.kunze94@gmail.com>
2019-10-30 16:10:21 +0100

    add event to MsgRecvPacket

--- x/ibc/20-transfer/handler.go
+++ x/ibc/20-transfer/handler.go
@@ -5,16 +5,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/ibc/20-transfer/types"
 )
 
-// handleMsgRecvPacket defines the sdk.Handler for MsgRecvPacket
-func HandleMsgRecvPacket(ctx sdk.Context, k Keeper, msg MsgRecvPacket) (res sdk.Result) {
-	err := k.ReceivePacket(ctx, msg.Packet, msg.Proofs[0], msg.Height)
-	if err != nil {
-		return sdk.ResultFromError(err)
-	}
-
-	return sdk.Result{Events: ctx.EventManager().Events()}
-}
-
 // HandleMsgTransfer defines the sdk.Handler for MsgTransfer
 func HandleMsgTransfer(ctx sdk.Context, k Keeper, msg MsgTransfer) (res sdk.Result) {
 	err := k.SendTransfer(ctx, msg.SourcePort, msg.SourceChannel, msg.Amount, msg.Sender, msg.Receiver, msg.Source)
@@ -28,7 +18,26 @@ func HandleMsgTransfer(ctx sdk.Context, k Keeper, msg MsgTransfer) (res sdk.Resu
 			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
 			sdk.NewAttribute(sdk.AttributeKeySender, msg.Sender.String()),
 			sdk.NewAttribute(types.AttributeKeyReceiver, msg.Receiver.String()),
-		))
+		),
+	)
+
+	return sdk.Result{Events: ctx.EventManager().Events()}
+}
+
+// HandleMsgRecvPacket defines the sdk.Handler for MsgRecvPacket
+func HandleMsgRecvPacket(ctx sdk.Context, k Keeper, msg MsgRecvPacket) (res sdk.Result) {
+	err := k.ReceivePacket(ctx, msg.Packet, msg.Proofs[0], msg.Height)
+	if err != nil {
+		return sdk.ResultFromError(err)
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Signer.String()),
+		),
+	)
 
 	return sdk.Result{Events: ctx.EventManager().Events()}
 }

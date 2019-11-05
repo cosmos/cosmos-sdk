024486c10fcf1c1c0c8af3ecf47122d2366b20c6
Federico Kunze <federico.kunze94@gmail.com>
2019-11-05 14:13:19 +0100

    upstream changes

--- x/ibc/keeper/querier.go
+++ x/ibc/keeper/querier.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"fmt"
+
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,16 +14,27 @@ import (
 func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
 		switch path[0] {
-		case client.QueryClientState:
-			return client.QuerierClientState(ctx, req, k.ClientKeeper)
-		case client.QueryConsensusState:
-			return client.QuerierConsensusState(ctx, req, k.ClientKeeper)
-		case client.QueryVerifiedRoot:
-			return client.QuerierVerifiedRoot(ctx, req, k.ClientKeeper)
-		case connection.QueryConnection:
-			return connection.QuerierConnection(ctx, req, k.ConnectionKeeper)
-		case connection.QueryClientConnections:
-			return connection.QuerierClientConnections(ctx, req, k.ConnectionKeeper)
+		case client.SubModuleName:
+			switch path[1] {
+			case client.QueryClientState:
+				return client.QuerierClientState(ctx, req, k.ClientKeeper)
+			case client.QueryConsensusState:
+				return client.QuerierConsensusState(ctx, req, k.ClientKeeper)
+			case client.QueryVerifiedRoot:
+				return client.QuerierVerifiedRoot(ctx, req, k.ClientKeeper)
+			default:
+				return nil, sdk.ErrUnknownRequest(fmt.Sprintf("unknown IBC %s query endpoint", client.SubModuleName))
+			}
+
+		case connection.SubModuleName:
+			switch path[1] {
+			case connection.QueryConnection:
+				return connection.QuerierConnection(ctx, req, k.ConnectionKeeper)
+			case connection.QueryClientConnections:
+				return connection.QuerierClientConnections(ctx, req, k.ConnectionKeeper)
+			default:
+				return nil, sdk.ErrUnknownRequest(fmt.Sprintf("unknown IBC %s query endpoint", connection.SubModuleName))
+			}
 
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown IBC query endpoint")

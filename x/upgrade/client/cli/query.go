d8a9c42fcf46e72de014b9481c97c39b122ed13d
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-11-05 12:26:03 +0100

    Return header for query applied-height

--- x/upgrade/client/cli/query.go
+++ x/upgrade/client/cli/query.go
@@ -40,17 +40,13 @@ func GetPlanCmd(storeName string, cdc *codec.Codec) *cobra.Command {
 	}
 }
 
-type appliedHeight struct {
-	Applied int64 `json:"applied" yaml:"applied"`
-}
-
 // GetAppliedHeightCmd returns the height at which a completed upgrade was applied
 func GetAppliedHeightCmd(storeName string, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "applied-height [upgrade-name]",
-		Short: "height at which a completed upgrade was applied",
-		Long: "If upgrade-name was previously executed on the chain, this returns the height at which it was applied.\n" +
-			"This helps a client determine which binary was valid over a given range of blocks.",
+		Use:   "applied [upgrade-name]",
+		Short: "block header for height at which a completed upgrade was applied",
+		Long: "If upgrade-name was previously executed on the chain, this returns the header for the block at which it was applied.\n" +
+			"This helps a client determine which binary was valid over a given range of blocks, as well as more context to understand past migrations.",
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
@@ -73,9 +69,28 @@ func GetAppliedHeightCmd(storeName string, cdc *codec.Codec) *cobra.Command {
 			if len(res) != 8 {
 				return fmt.Errorf("unknown format for applied-upgrade")
 			}
-
 			applied := int64(binary.BigEndian.Uint64(res))
-			return cliCtx.PrintOutput(appliedHeight{applied})
+
+			// we got the height, now let's return the headers
+			node, err := cliCtx.GetNode()
+			if err != nil {
+				return err
+			}
+			headers, err := node.BlockchainInfo(applied, applied)
+			if err != nil {
+				return err
+			}
+			if len(headers.BlockMetas) == 0 {
+				return fmt.Errorf("No headers returned for height %d", applied)
+			}
+
+			// always output json as Header is unreable in toml ([]byte is a long list of numbers)
+			bz, err = cdc.MarshalJSONIndent(headers.BlockMetas[0], "", "  ")
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(bz))
+			return nil
 		},
 	}
 }

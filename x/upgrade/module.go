9542709069f75ed1a504db775701edc46cfc3909
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-11-05 15:31:40 +0100

    Use viper.GetBool instead of cast

--- x/upgrade/module.go
+++ x/upgrade/module.go
@@ -127,7 +127,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 //
 // CONTRACT: this is registered in BeginBlocker *before* all other modules' BeginBlock functions
 func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
-	skipUpgrade := viper.Get(FlagUnsafeSkipUpgrade).(bool)
+	skipUpgrade := viper.GetBool(FlagUnsafeSkipUpgrade)
 	BeginBlocker(am.keeper, ctx, req, skipUpgrade)
 }
 

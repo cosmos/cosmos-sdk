1427288c3d1a3aa221add1166d4ee6aaca05858f
Denis Fadeev <denis@fadeev.org>
2019-11-07 17:58:41 -0500

    testing netlify

--- docs/pre.sh
+++ docs/pre.sh
@@ -5,6 +5,6 @@ mkdir -p modules
 for D in ../x/*; do
   if [ -d "${D}" ]; then
     rm -rf "modules/$(echo $D | awk -F/ '{print $NF}')"
-    mkdir -p "modules/$(echo $D | awk -F/ '{print $NF}')" && cp -r $D/spec/ "$_"
+    mkdir -p "modules/$(echo $D | awk -F/ '{print $NF}')" && cp -r $D/spec/ "$_/"
   fi
 done

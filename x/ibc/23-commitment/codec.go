c617d8966b74287725bd0d512aaf7311fdc08d86
Jack Zampolin <jack.zampolin@gmail.com>
2019-11-12 10:34:14 -0800

    Fix codec issue in ics23

--- x/ibc/23-commitment/codec.go
+++ x/ibc/23-commitment/codec.go
@@ -4,6 +4,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
+var SubModuleCdc = codec.New()
+
 // RegisterCodec registers types declared in this package
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterInterface((*RootI)(nil), nil)
@@ -15,4 +17,8 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(Prefix{}, "ibc/commitment/merkle/Prefix", nil)
 	cdc.RegisterConcrete(Path{}, "ibc/commitment/merkle/Path", nil)
 	cdc.RegisterConcrete(Proof{}, "ibc/commitment/merkle/Proof", nil)
+}
+
+func init() {
+	RegisterCodec(SubModuleCdc)
 }

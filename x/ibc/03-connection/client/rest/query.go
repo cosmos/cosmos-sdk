15c5fa9f51e16764f43233a777328b6e802c6f6f
chengwenxi <vincent.ch.cn@gmail.com>
2019-11-12 15:41:26 +0800

    add rest endpoints for ibc connection

---
+++ x/ibc/03-connection/client/rest/query.go
@@ -0,0 +1,99 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/ibc/03-connection/types"
+	"github.com/gorilla/mux"
+)
+
+func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, queryRoute string) {
+	r.HandleFunc(fmt.Sprintf("/ibc/connection/connections/{%s}", RestConnectionID), queryConnectionHandlerFn(cliCtx, queryRoute)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/ibc/connection/connections/clients/{%s}/connections", RestClientID), queryClientConnectionsHandlerFn(cliCtx, queryRoute)).Methods("GET")
+}
+
+func queryConnectionHandlerFn(cliCtx context.CLIContext, queryRoute string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		connectionID := vars[RestConnectionID]
+
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		// return proof if the prove query param is set to true
+		proveStr := r.FormValue("prove")
+		prove := false
+		if strings.ToLower(strings.TrimSpace(proveStr)) == "true" {
+			prove = true
+		}
+
+		bz, err := cliCtx.Codec.MarshalJSON(types.NewQueryConnectionParams(connectionID))
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		req := abci.RequestQuery{
+			Path:  fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryConnection),
+			Data:  bz,
+			Prove: prove,
+		}
+
+		res, err := cliCtx.QueryABCI(req)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		cliCtx = cliCtx.WithHeight(res.Height)
+		rest.PostProcessResponse(w, cliCtx, res)
+	}
+}
+
+func queryClientConnectionsHandlerFn(cliCtx context.CLIContext, queryRoute string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		clientID := vars[RestClientID]
+
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		// return proof if the prove query param is set to true
+		proveStr := r.FormValue("prove")
+		prove := false
+		if strings.ToLower(strings.TrimSpace(proveStr)) == "true" {
+			prove = true
+		}
+
+		bz, err := cliCtx.Codec.MarshalJSON(types.NewQueryClientConnectionsParams(clientID))
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		req := abci.RequestQuery{
+			Path:  fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryClientConnections),
+			Data:  bz,
+			Prove: prove,
+		}
+
+		res, err := cliCtx.QueryABCI(req)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		cliCtx = cliCtx.WithHeight(res.Height)
+		rest.PostProcessResponse(w, cliCtx, res)
+	}
+}
---
+++ x/ibc/03-connection/client/rest/tx.go
@@ -0,0 +1,158 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/cosmos/cosmos-sdk/x/ibc/03-connection/types"
+	"github.com/gorilla/mux"
+)
+
+func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc("/ibc/connection/connection/open-init", connectionOpenInitHandlerFn(cliCtx)).Methods("POST")
+	r.HandleFunc("/ibc/connection/connection/open-try", connectionOpenTryHandlerFn(cliCtx)).Methods("POST")
+	r.HandleFunc("/ibc/connection/connections/{connection-id}/open-ack", connectionOpenAckHandlerFn(cliCtx)).Methods("POST")
+	r.HandleFunc("/ibc/connection/connections/{connection-id}/open-confirm", connectionOpenConfirmHandlerFn(cliCtx)).Methods("POST")
+}
+
+func connectionOpenInitHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req ConnectionOpenInitReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgConnectionOpenInit(
+			req.ConnectionID, req.ClientID, req.CounterpartyConnectionID,
+			req.CounterpartyClientID, req.CounterpartyPrefix, fromAddr,
+		)
+
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+	}
+}
+
+func connectionOpenTryHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req ConnectionOpenTryReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgConnectionOpenTry(
+			req.ConnectionID, req.ClientID, req.CounterpartyConnectionID,
+			req.CounterpartyClientID, req.CounterpartyPrefix, req.CounterpartyVersions,
+			req.ProofInit, req.ProofConsensus, req.ProofHeight,
+			req.ConsensusHeight, fromAddr,
+		)
+
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+	}
+}
+
+func connectionOpenAckHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		connectionID := vars[RestConnectionID]
+
+		var req ConnectionOpenAckReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgConnectionOpenAck(
+			connectionID, req.ProofTry, req.ProofConsensus, req.ProofHeight,
+			req.ConsensusHeight, req.Version, fromAddr,
+		)
+
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+	}
+}
+
+func connectionOpenConfirmHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		connectionID := vars[RestConnectionID]
+
+		var req ConnectionOpenConfirmReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgConnectionOpenConfirm(
+			connectionID, req.ProofAck, req.ProofHeight, fromAddr,
+		)
+
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+	}
+}

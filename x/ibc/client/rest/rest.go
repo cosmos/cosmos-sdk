8329ec95734f5b7230e3d2072fa9ffddb8660bd6
Segue <huoda.china@163.com>
2019-11-15 15:42:12 +0800

    use sub module name instead of icsxx

--- x/ibc/client/rest/rest.go
+++ x/ibc/client/rest/rest.go
@@ -4,16 +4,16 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
-	ics02 "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
-	ics03 "github.com/cosmos/cosmos-sdk/x/ibc/03-connection"
-	ics04 "github.com/cosmos/cosmos-sdk/x/ibc/04-channel"
-	ics20 "github.com/cosmos/cosmos-sdk/x/ibc/20-transfer"
+	client "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
+	connection "github.com/cosmos/cosmos-sdk/x/ibc/03-connection"
+	channel "github.com/cosmos/cosmos-sdk/x/ibc/04-channel"
+	transfer "github.com/cosmos/cosmos-sdk/x/ibc/20-transfer"
 )
 
 // RegisterRoutes - Central function to define routes that get registered by the main application
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, queryRoute string) {
-	ics02.RegisterRESTRoutes(cliCtx, r, queryRoute)
-	ics03.RegisterRESTRoutes(cliCtx, r, queryRoute)
-	ics04.RegisterRESTRoutes(cliCtx, r, queryRoute)
-	ics20.RegisterRESTRoutes(cliCtx, r)
+	client.RegisterRESTRoutes(cliCtx, r, queryRoute)
+	connection.RegisterRESTRoutes(cliCtx, r, queryRoute)
+	channel.RegisterRESTRoutes(cliCtx, r, queryRoute)
+	transfer.RegisterRESTRoutes(cliCtx, r)
 }

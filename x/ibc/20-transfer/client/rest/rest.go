9c726dd93445831bcb7df42925ff87241e2f99e6
Segue <huoda.china@163.com>
2019-11-15 11:03:55 +0800

    fix receive packet

--- x/ibc/20-transfer/client/rest/rest.go
+++ x/ibc/20-transfer/client/rest/rest.go
@@ -6,6 +6,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
+	channelexported "github.com/cosmos/cosmos-sdk/x/ibc/04-channel/exported"
+	commitment "github.com/cosmos/cosmos-sdk/x/ibc/23-commitment"
 )
 
 const (
@@ -27,24 +29,10 @@ type TransferTxReq struct {
 	Source   bool           `json:"source" yaml:"source"`
 }
 
-/*
 // RecvPacketReq defines the properties of a receive packet request's body.
 type RecvPacketReq struct {
 	BaseReq rest.BaseReq            `json:"base_req" yaml:"base_req"`
 	Packet  channelexported.PacketI `json:"packet" yaml:"packet"`
 	Proofs  []commitment.Proof      `json:"proofs" yaml:"proofs"`
 	Height  uint64                  `json:"height" yaml:"height"`
-}
-*/
-
-// RecvPacketReq defines the properties of a receive packet request's body.
-type RecvPacketReq struct {
-	BaseReq         rest.BaseReq `json:"base_req" yaml:"base_req"`
-	SourceNode      string       `json:"source_node" yaml:"source_node"`
-	Sequence        string       `json:"sequence" yaml:"sequence"`
-	Timeout         string       `json:"timeout" yaml:"timeout"`
-	SourceChainID   string       `json:"source_chain_id" yaml:"source_chain_id"`
-	SourcePortID    string       `json:"source_port_id" yaml:"source_port_id"`
-	SourceChannelID string       `json:"source_channel_id" yaml:"source_channel_id"`
-	ClientID        string       `json:"client_id" yaml:"client_id"`
 }
--- x/ibc/20-transfer/client/rest/tx.go
+++ x/ibc/20-transfer/client/rest/tx.go
@@ -3,22 +3,14 @@ package rest
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/client/keys"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
-	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-	clientutils "github.com/cosmos/cosmos-sdk/x/ibc/02-client/client/utils"
-	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/02-client/types"
-	channelutils "github.com/cosmos/cosmos-sdk/x/ibc/04-channel/client/utils"
 	"github.com/cosmos/cosmos-sdk/x/ibc/20-transfer/types"
-	commitment "github.com/cosmos/cosmos-sdk/x/ibc/23-commitment"
 )
 
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
@@ -67,7 +59,6 @@ func transferHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	}
 }
 
-/*
 func recvPacketHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req RecvPacketReq
@@ -91,98 +82,6 @@ func recvPacketHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 			req.Packet,
 			req.Proofs,
 			req.Height,
-			fromAddr,
-		)
-
-		if err := msg.ValidateBasic(); err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
-	}
-}
-*/
-
-func recvPacketHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cliCtx.Codec))
-		cliCtx := context.NewCLIContext().WithCodec(cliCtx.Codec).WithBroadcastMode(flags.BroadcastBlock)
-
-		var req RecvPacketReq
-		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
-			return
-		}
-
-		req.BaseReq = req.BaseReq.Sanitize()
-		if !req.BaseReq.ValidateBasic(w) {
-			return
-		}
-
-		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		sourceNode := req.SourceNode
-		sourceChainID := req.SourceChainID
-
-		cliCtx2 := context.NewCLIContextIBC(req.BaseReq.From, sourceChainID, sourceNode).
-			WithCodec(cliCtx.Codec).
-			WithBroadcastMode(flags.BroadcastBlock)
-
-		header, err := clientutils.GetTendermintHeader(cliCtx2)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		clientid := req.ClientID
-		sourcePort := req.SourcePortID
-		sourceChannel := req.SourceChannelID
-
-		passphrase, err := keys.GetPassphrase(req.BaseReq.From)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		msgUpdateClient := clienttypes.NewMsgUpdateClient(clientid, header, fromAddr)
-		if err := msgUpdateClient.ValidateBasic(); err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		res, err := utils.CompleteAndBroadcastTx(txBldr, cliCtx, []sdk.Msg{msgUpdateClient}, passphrase)
-		if err != nil || !res.IsOK() {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		sequence, err := strconv.ParseUint(req.Sequence, 10, 64)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		timeout, err := strconv.ParseUint(req.Timeout, 10, 64)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		packetRes, err := channelutils.QueryPacket(cliCtx2.WithHeight(header.Height-1), sourcePort, sourceChannel, sequence, timeout, "ibc")
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		// create the message
-		msg := types.NewMsgRecvPacket(
-			packetRes.Packet,
-			[]commitment.Proof{packetRes.Proof},
-			packetRes.ProofHeight,
 			fromAddr,
 		)
 

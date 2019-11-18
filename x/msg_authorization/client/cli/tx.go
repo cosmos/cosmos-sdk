429e77d7f30f6a7d3179d912b04eee9179fd4dc8
saren <sahithnarahari@gmail.com>
2019-11-19 03:52:58 +0530

    Added code skeleton for msg authorization

---
+++ x/msg_authorization/client/cli/tx.go
@@ -0,0 +1,36 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/spf13/cobra"
+	"github.com/cosmos/cosmos-sdk/x/msg_authorization/internal/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	AuthorizationTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Authorization transactions subcommands",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	AuthorizationTxCmd.AddCommand(client.PostCommands(
+		GetCmdGrantCapability(cdc),
+		GetCmdRevokeCapability(cdc),
+	)...)
+
+	return AuthorizationTxCmd
+}
+
+func GetCmdGrantCapability(cdc *codec.Codec) *cobra.Command {
+	//TODO
+	return nil
+}
+
+func GetCmdRevokeCapability(cdc *codec.Codec) *cobra.Command {
+	//TODO
+	return nil
+}
---
+++ x/msg_authorization/exported/keeper.go
@@ -0,0 +1,23 @@
+package exported
+
+import (
+	"time"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+)
+
+type Keeper interface {
+	//DispatchActions executes the provided messages via capability grants from the message signer to the grantee
+	DispatchActions(ctx sdk.Context, grantee sdk.AccAddress, msgs []sdk.Msg) sdk.Result
+
+	// Grants the provided capability to the grantee on the granter's account with the provided expiration time
+	// If there is an existing capability grant for the same sdk.Msg type, this grant overwrites that.
+	Grant(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, capability Capability, expiration time.Time)
+
+	//Revokes any capability for the provided message type granted to the grantee by the granter.
+	Revoke(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg)
+
+	//Returns any Capability (or nil), with the expiration time,
+	// granted to the grantee by the granter for the provided msg type.
+	GetCapability(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg) (cap Capability, expiration time.Time)
+}

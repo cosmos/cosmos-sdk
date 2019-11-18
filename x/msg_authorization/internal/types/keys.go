ec6d38767619f9ac6cc52a5dfa2fb679e2ec1efc
saren <sahithnarahari@gmail.com>
2019-11-19 03:52:58 +0530

    Added code skeleton for msg authorization

---
+++ x/msg_authorization/internal/types/keys.go
@@ -0,0 +1,15 @@
+package types
+
+const (
+	// ModuleName is the module name constant used in many places
+	ModuleName = "msg_authorization"
+
+	// StoreKey is the store key string for msg_authorization
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for msg_authorization
+	RouterKey = ModuleName
+
+	// QuerierRoute is the querier route for msg_authorization
+	QuerierRoute = ModuleName
+)

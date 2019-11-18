d113c3ecf68f61374a78c797327f364f85485470
sahith-narahari <sahithnarahari@gmail.com>
2019-11-19 05:28:52 +0530

    Implemented sdk.msg methods

--- x/msg_authorization/exported/keeper.go
+++ x/msg_authorization/exported/keeper.go
@@ -1,9 +1,9 @@
 package exported
 
 import (
-	"time"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
+	"github.com/cosmos/cosmos-sdk/x/msg_authorization/internal/types"
+	"time"
 )
 
 type Keeper interface {
@@ -12,12 +12,12 @@ type Keeper interface {
 
 	// Grants the provided capability to the grantee on the granter's account with the provided expiration time
 	// If there is an existing capability grant for the same sdk.Msg type, this grant overwrites that.
-	Grant(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, capability Capability, expiration time.Time)
+	Grant(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, capability types.Capability, expiration time.Time)
 
 	//Revokes any capability for the provided message type granted to the grantee by the granter.
 	Revoke(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg)
 
 	//Returns any Capability (or nil), with the expiration time,
 	// granted to the grantee by the granter for the provided msg type.
-	GetCapability(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg) (cap Capability, expiration time.Time)
+	GetCapability(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg) (cap types.Capability, expiration time.Time)
 }
--- x/msg_authorization/handler.go
+++ x/msg_authorization/handler.go
@@ -1,1 +1,15 @@
 package msg_authorization
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func NewHandler(k Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		switch msg := msg.(type) {
+		//TODO
+		}
+	}
+}

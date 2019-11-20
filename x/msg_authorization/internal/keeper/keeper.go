1fa819b584a93a10015551b10a9d51fa8569bc06
sahith-narahari <sahithnarahari@gmail.com>
2019-11-21 02:23:17 +0530

    Added base for internal keeper

--- x/msg_authorization/internal/keeper/keeper.go
+++ x/msg_authorization/internal/keeper/keeper.go
@@ -3,6 +3,8 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/msg_authorization/internal/types"
+	"time"
 )
 
 type Keeper struct {
@@ -20,4 +22,20 @@ func NewKeeper(storeKey sdk.StoreKey, cdc *codec.Codec, router sdk.Router) Keepe
 	}
 }
 
-//TODO implement all keeper methods
+func (k Keeper) DispatchActions(ctx sdk.Context, grantee sdk.AccAddress, msgs []sdk.Msg) sdk.Result {
+	//TODO
+	return sdk.Result{}
+}
+
+func (k Keeper) Grant(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, capability types.Capability, expiration time.Time) {
+	//TODO
+}
+
+func (k Keeper) Revoke(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg)  {
+	//TODO
+}
+
+func (k Keeper) GetCapability(ctx sdk.Context, grantee sdk.AccAddress, granter sdk.AccAddress, msgType sdk.Msg) (cap types.Capability, expiration time.Time)  {
+	//TODO
+	return nil, time.Now()
+}

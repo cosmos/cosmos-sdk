22faf51e8191ab64372698b529df23358db40603
Aditya Sripal <adityasripal@gmail.com>
2019-11-22 14:13:29 -0800

    cleanup evidence test

--- x/ibc/02-client/types/tendermint/evidence_test.go
+++ x/ibc/02-client/types/tendermint/evidence_test.go
@@ -13,10 +13,13 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 	altPrivVal := tmtypes.NewMockPV()
 	altVal := tmtypes.NewValidator(altPrivVal.GetPubKey(), 4)
 
-	altValSet := tmtypes.NewValidatorSet(append(suite.valSet.Validators, altVal))
-	wrongValSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{altVal})
+	// Create bothValSet with both suite validator and altVal
+	bothValSet := tmtypes.NewValidatorSet(append(suite.valSet.Validators, altVal))
+	// Create alternative validator set with only altVal
+	altValSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{altVal})
 
 	signers := []tmtypes.PrivValidator{suite.privVal}
+	// Create signer array and ensure it is in same order as bothValSet
 	var bothSigners []tmtypes.PrivValidator
 	if bytes.Compare(altPrivVal.GetPubKey().Address(), suite.privVal.GetPubKey().Address()) == -1 {
 		bothSigners = []tmtypes.PrivValidator{altPrivVal, suite.privVal}
@@ -36,7 +39,7 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 			"valid evidence",
 			Evidence{
 				Header1: suite.header,
-				Header2: MakeHeader("gaia", 4, suite.valSet, altValSet, signers),
+				Header2: MakeHeader("gaia", 4, suite.valSet, bothValSet, signers),
 				ChainID: "gaia",
 			},
 			func(ev *Evidence) {},
@@ -46,7 +49,7 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 			"wrong chainID on header1",
 			Evidence{
 				Header1: suite.header,
-				Header2: MakeHeader("ethermint", 4, suite.valSet, altValSet, signers),
+				Header2: MakeHeader("ethermint", 4, suite.valSet, bothValSet, signers),
 				ChainID: "ethermint",
 			},
 			func(ev *Evidence) {},
@@ -56,7 +59,7 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 			"wrong chainID on header2",
 			Evidence{
 				Header1: suite.header,
-				Header2: MakeHeader("ethermint", 4, suite.valSet, altValSet, signers),
+				Header2: MakeHeader("ethermint", 4, suite.valSet, bothValSet, signers),
 				ChainID: "gaia",
 			},
 			func(ev *Evidence) {},
@@ -66,17 +69,7 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 			"mismatched heights",
 			Evidence{
 				Header1: suite.header,
-				Header2: MakeHeader("gaia", 6, suite.valSet, altValSet, signers),
-				ChainID: "gaia",
-			},
-			func(ev *Evidence) {},
-			true,
-		},
-		{
-			"mismatched heights",
-			Evidence{
-				Header1: suite.header,
-				Header2: MakeHeader("gaia", 6, suite.valSet, altValSet, signers),
+				Header2: MakeHeader("gaia", 6, suite.valSet, bothValSet, signers),
 				ChainID: "gaia",
 			},
 			func(ev *Evidence) {},
@@ -96,11 +89,12 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 			"header doesn't have 2/3 majority",
 			Evidence{
 				Header1: suite.header,
-				Header2: MakeHeader("gaia", 4, altValSet, altValSet, bothSigners),
+				Header2: MakeHeader("gaia", 4, bothValSet, bothValSet, bothSigners),
 				ChainID: "gaia",
 			},
 			func(ev *Evidence) {
-				wrongVoteSet := tmtypes.NewVoteSet("gaia", ev.Header2.Height, 1, tmtypes.PrecommitType, wrongValSet)
+				// voteSet contains only altVal which is less than 2/3 of total power (4/14)
+				wrongVoteSet := tmtypes.NewVoteSet("gaia", ev.Header2.Height, 1, tmtypes.PrecommitType, altValSet)
 				var err error
 				ev.Header2.Commit, err = tmtypes.MakeCommit(ev.Header2.Commit.BlockID, ev.Header2.Height, ev.Header2.Commit.Round(), wrongVoteSet, altSigners)
 				if err != nil {
@@ -113,7 +107,7 @@ func (suite *TendermintTestSuite) TestValidateBasic() {
 			"validators sign off on wrong commit",
 			Evidence{
 				Header1: suite.header,
-				Header2: MakeHeader("gaia", 4, altValSet, altValSet, bothSigners),
+				Header2: MakeHeader("gaia", 4, bothValSet, bothValSet, bothSigners),
 				ChainID: "gaia",
 			},
 			func(ev *Evidence) {

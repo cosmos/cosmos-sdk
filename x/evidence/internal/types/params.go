ee1de08de31909e8c1d222045e6dd288ec41e83a
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-11-27 13:12:18 -0500

    Move DoubleSignJailEndTime to the evidence module

--- x/evidence/internal/types/params.go
+++ x/evidence/internal/types/params.go
@@ -17,6 +17,10 @@ const (
 // Parameter store keys
 var (
 	KeyMaxEvidenceAge = []byte("MaxEvidenceAge")
+
+	// The Double Sign Jail period ends at Max Time supported by Amino
+	// (Dec 31, 9999 - 23:59:59 GMT).
+	DoubleSignJailEndTime = time.Unix(253402300799, 0)
 )
 
 // Params defines the total set of parameters for the evidence module
--- x/slashing/internal/types/params.go
+++ x/slashing/internal/types/params.go
@@ -15,9 +15,7 @@ const (
 	DefaultDowntimeJailDuration = 60 * 10 * time.Second
 )
 
-// The Double Sign Jail period ends at Max Time supported by Amino (Dec 31, 9999 - 23:59:59 GMT)
 var (
-	DoubleSignJailEndTime          = time.Unix(253402300799, 0)
 	DefaultMinSignedPerWindow      = sdk.NewDecWithPrec(5, 1)
 	DefaultSlashFractionDoubleSign = sdk.NewDec(1).Quo(sdk.NewDec(20))
 	DefaultSlashFractionDowntime   = sdk.NewDec(1).Quo(sdk.NewDec(100))

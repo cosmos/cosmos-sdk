debecf31344cc206ed9fa7386d23b5c01780113c
Sahith Reddy Narahari <sahith@vitwit.com>
2019-12-04 21:40:08 +0530

    Added keeper implementations

--- x/msg_authorization/exported/keeper.go
+++ x/msg_authorization/exported/keeper.go
@@ -1,9 +1,10 @@
 package exported
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/msg_authorization/internal/types"
-	"time"
 )
 
 type Keeper interface {
---
+++ x/msg_authorization/internal/types/send_capability.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+type SendCapability struct {
+	// SpendLimit specifies the maximum amount of tokens that can be spent
+	// by this capability and will be updated as tokens are spent. If it is
+	// empty, there is no spend limit and any amount of coins can be spent.
+	SpendLimit sdk.Coins
+}
+
+func (capability SendCapability) MsgType() sdk.Msg {
+	return bank.MsgSend{}
+}
+
+func (capability SendCapability) Accept(msg sdk.Msg, block abci.Header) (allow bool, updated Capability, delete bool) {
+	switch msg := msg.(type) {
+	case bank.MsgSend:
+		limitLeft, isNegative := capability.SpendLimit.SafeSub(msg.Amount)
+		if isNegative {
+			return false, nil, false
+		}
+		if limitLeft.IsZero() {
+			return true, nil, true
+		}
+		return true, SendCapability{SpendLimit: limitLeft}, false
+	}
+	return false, nil, false
+}

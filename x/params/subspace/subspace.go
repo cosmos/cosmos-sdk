3b062e2093fd5d50e3a506f3c17905c4537622d4
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-12-04 19:40:40 -0500

    Call ValidatorFn in SetParamSet per pair

--- x/params/subspace/subspace.go
+++ x/params/subspace/subspace.go
@@ -204,6 +204,11 @@ func (s Subspace) SetParamSet(ctx sdk.Context, ps ParamSet) {
 		// since SetStruct is meant to be used in InitGenesis
 		// so this method will not be called frequently
 		v := reflect.Indirect(reflect.ValueOf(pair.Value)).Interface()
+
+		if err := pair.ValidatorFn(v); err != nil {
+			panic(fmt.Sprintf("value from ParamSetPair is invalid: %s", err))
+		}
+
 		s.Set(ctx, pair.Key, v)
 	}
 }

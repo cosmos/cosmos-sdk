9f03b57fe3ce223d6233a47e555536e671be27c9
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2019-12-10 11:48:57 -0500

    Merge PR #5359: Params Validation

--- x/params/types/proposal_test.go
+++ x/params/types/proposal_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestParameterChangeProposal(t *testing.T) {
 	pc1 := NewParamChange("sub", "foo", "baz")
-	pc2 := NewParamChangeWithSubkey("sub", "bar", "cat", "dog")
+	pc2 := NewParamChange("sub", "bar", "cat")
 	pcp := NewParameterChangeProposal("test title", "test description", []ParamChange{pc1, pc2})
 
 	require.Equal(t, "test title", pcp.GetTitle())
@@ -17,15 +17,11 @@ func TestParameterChangeProposal(t *testing.T) {
 	require.Equal(t, ProposalTypeChange, pcp.ProposalType())
 	require.Nil(t, pcp.ValidateBasic())
 
-	pc3 := NewParamChangeWithSubkey("", "bar", "cat", "dog")
+	pc3 := NewParamChange("", "bar", "cat")
 	pcp = NewParameterChangeProposal("test title", "test description", []ParamChange{pc3})
 	require.Error(t, pcp.ValidateBasic())
 
-	pc4 := NewParamChangeWithSubkey("sub", "", "cat", "dog")
+	pc4 := NewParamChange("sub", "", "cat")
 	pcp = NewParameterChangeProposal("test title", "test description", []ParamChange{pc4})
-	require.Error(t, pcp.ValidateBasic())
-
-	pc5 := NewParamChangeWithSubkey("sub", "foo", "cat", "")
-	pcp = NewParameterChangeProposal("test title", "test description", []ParamChange{pc5})
 	require.Error(t, pcp.ValidateBasic())
 }

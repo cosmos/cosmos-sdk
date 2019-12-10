5d091800bb4a68335eea4f8489183033ece16f99
sahith-narahari <sahith@vitwit.com>
2019-12-10 13:16:55 +0530

    Added more test cases for keeper

--- x/msg_authorization/internal/keeper/keeper_test.go
+++ x/msg_authorization/internal/keeper/keeper_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -29,24 +28,48 @@ func (s *TestSuite) SetupTest() {
 
 }
 
-func (s *TestSuite) TestKeeper(t *testing.T) {
+func (s *TestSuite) TestKeeper() {
 	err := s.bankKeeper.SetCoins(s.ctx, granterAddr, sdk.NewCoins(sdk.NewInt64Coin("steak", 10000)))
-	require.Nil(t, err)
-	require.True(t, s.bankKeeper.GetCoins(s.ctx, granterAddr).IsEqual(sdk.NewCoins(sdk.NewInt64Coin("steak", 10000))))
+	s.Require().Nil(err)
+	s.Require().True(s.bankKeeper.GetCoins(s.ctx, granterAddr).IsEqual(sdk.NewCoins(sdk.NewInt64Coin("steak", 10000))))
 
-	t.Log("Verify that no capability returns nil")
+	s.T().Log("Verify that no capability returns nil")
 	capability, _ := s.keeper.GetCapability(s.ctx, granteeAddr, granterAddr, bank.MsgSend{})
 	s.Require().Nil(capability)
 	//require.Nil(t, expiration)
 	now := s.ctx.BlockHeader().Time
-	require.NotNil(t, now)
+	s.Require().NotNil(now)
 
 	newCoins := sdk.NewCoins(sdk.NewInt64Coin("steak", 100))
+	s.T().Log("Verify if expired capability is rejected")
 	s.keeper.Grant(s.ctx, granterAddr, granteeAddr, types.SendCapability{SpendLimit: newCoins}, now.Add(-1*time.Hour))
-	capability, expiration := s.keeper.GetCapability(s.ctx, granteeAddr, granterAddr, bank.MsgSend{})
-	require.NotNil(t, capability)
-	require.NotNil(t, expiration)
-	//TODO add more cases
+	capability, _ = s.keeper.GetCapability(s.ctx, granteeAddr, granterAddr, bank.MsgSend{})
+	s.Require().Nil(capability)
+
+	s.T().Log("Verify if capability is accepted")
+	s.keeper.Grant(s.ctx, granteeAddr, granterAddr, types.SendCapability{SpendLimit: newCoins}, now.Add(time.Hour))
+	capability, _ = s.keeper.GetCapability(s.ctx, granteeAddr, granterAddr, bank.MsgSend{})
+	s.Require().NotNil(capability)
+	s.Require().Equal(capability.MsgType(), bank.MsgSend{})
+
+	s.T().Log("Verify fetching capability with wrong msg type fails")
+	capability, _ = s.keeper.GetCapability(s.ctx, granteeAddr, granterAddr, bank.MsgMultiSend{})
+	s.Require().Nil(capability)
+
+	s.T().Log("Verify fetching capability with wrong grantee fails")
+	capability, _ = s.keeper.GetCapability(s.ctx, recepientAddr, granterAddr, bank.MsgMultiSend{})
+	s.Require().Nil(capability)
+
+	s.T().Log("Verify revoke fails with wrong information")
+	s.keeper.Revoke(s.ctx, recepientAddr, granterAddr, bank.MsgSend{})
+	capability, _ = s.keeper.GetCapability(s.ctx, recepientAddr, granterAddr, bank.MsgSend{})
+	s.Require().Nil(capability)
+
+	s.T().Log("Verify revoke executes with correct information")
+	s.keeper.Revoke(s.ctx, recepientAddr, granterAddr, bank.MsgSend{})
+	capability, _ = s.keeper.GetCapability(s.ctx, granteeAddr, granterAddr, bank.MsgSend{})
+	s.Require().NotNil(capability)
+
 }
 
 func TestTestSuite(t *testing.T) {

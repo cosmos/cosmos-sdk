47b2974502ab5003d4cab8eb0e007cd6d47fa71b
sahith-narahari <sahith@vitwit.com>
2019-12-10 13:16:55 +0530

    Added more test cases for keeper

--- x/msg_authorization/internal/types/codec.go
+++ x/msg_authorization/internal/types/codec.go
@@ -10,4 +10,8 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgGrantAuthorization{}, "cosmos-sdk/GrantAuthorization", nil)
 	cdc.RegisterConcrete(MsgRevokeAuthorization{}, "cosmos-sdk/RevokeAuthorization", nil)
 	cdc.RegisterConcrete(MsgExecDelegated{}, "cosmos-sdk/ExecDelegated", nil)
+	cdc.RegisterConcrete(SendCapability{}, "cosmos-sdk/SendCapability", nil)
+	cdc.RegisterConcrete(CapabilityGrant{}, "cosmos-sdk/CapabilityGrant", nil)
+
+	cdc.RegisterInterface((*Capability)(nil), nil)
 }

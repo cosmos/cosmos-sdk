908fd2ba78504e3a72b99653ae8e8ef7e40486c8
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2019-12-17 13:59:16 -0500

    Merge PR #5417: Update distribution module doc

---
+++ x/distribution/doc.go
@@ -0,0 +1,6 @@
+/*
+Package distribution implements a Cosmos SDK module, that provides an implementation
+of the F1 fee distribution algorithm. It handles reward tracking, allocation, and
+distribution. Please refer to the specification under /spec for further information.
+*/
+package distribution

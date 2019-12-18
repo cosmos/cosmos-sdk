264260ae9034d05d637f7d8d38d62869605dec27
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-12-17 22:27:27 -0500

    Fix checks in runMsgs

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -647,7 +647,7 @@ func (app *BaseApp) runMsgs(ctx sdk.Context, msgs []sdk.Msg, mode runTxMode) (*s
 	// NOTE: GasWanted is determined by the AnteHandler and GasUsed by the GasMeter.
 	for i, msg := range msgs {
 		// skip actual execution for (Re)CheckTx mode
-		if mode != runTxModeCheck && mode != runTxModeReCheck {
+		if mode == runTxModeCheck || mode == runTxModeReCheck {
 			break
 		}
 

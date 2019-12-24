5188c3565ba6c70238eddd8a5c84a0bed73bd1ee
Dmitry Shulyak <yashulyak@gmail.com>
2019-12-24 19:04:36 +0200

    Merge PR #5445: Mock rpcclient in tests for votes pagination

--- x/gov/client/cli/query.go
+++ x/gov/client/cli/query.go
@@ -14,7 +14,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	gcutils "github.com/cosmos/cosmos-sdk/x/gov/client/utils"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
@@ -286,7 +285,7 @@ $ %[1]s query gov votes 1 --page=2 --limit=100
 
 			propStatus := proposal.Status
 			if !(propStatus == types.StatusVotingPeriod || propStatus == types.StatusDepositPeriod) {
-				res, err = gcutils.QueryVotesByTxQuery(cliCtx, params, utils.QueryTxsByEvents)
+				res, err = gcutils.QueryVotesByTxQuery(cliCtx, params)
 			} else {
 				res, _, err = cliCtx.QueryWithData(fmt.Sprintf("custom/%s/votes", queryRoute), bz)
 			}

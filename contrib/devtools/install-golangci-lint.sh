3f6fd6de1d56f14b4c13ca6c8813baa31e8b1ae2
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-12-30 11:00:28 -0500

    GOLANGCI_LINT_HASHSUM

--- contrib/devtools/install-golangci-lint.sh
+++ contrib/devtools/install-golangci-lint.sh
@@ -22,10 +22,10 @@ get_latest_release() {
 
 VERSION="$(get_latest_release golangci/golangci-lint)"
 
-echo "Downloading golangci-lint ${VERSION} installer ..." >&2
+echo "Downloading golangci-lint ${VERSION} installer..." >&2
 "${CURL}" -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/${VERSION}/install.sh" > "${installer}"
 
-echo "Checking hashsum ..." >&2
+echo "Checking hashsum..." >&2
 [ "${HASHSUM}" = "$(f_sha256 ${installer})" ]
 chmod +x "${installer}"
 

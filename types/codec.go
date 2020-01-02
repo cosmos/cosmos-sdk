b990a1084c41e89cf72f03aae9c8e1d6dee13c47
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-01-02 10:36:17 -0500

    remove old codec.go

--- types/codec.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "github.com/cosmos/cosmos-sdk/codec"
-
-// Register the sdk message type
-func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterInterface((*Msg)(nil), nil)
-	cdc.RegisterInterface((*Tx)(nil), nil)
-}

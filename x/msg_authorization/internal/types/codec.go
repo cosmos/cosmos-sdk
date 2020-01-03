743d0436243f3c69c41bcf2acee4ee04f88cd6be
Sahith Reddy Narahari <sahith@vitwit.com>
2020-01-03 17:56:51 +0530

    Removed comments in codec

--- x/msg_authorization/internal/types/codec.go
+++ x/msg_authorization/internal/types/codec.go
@@ -2,20 +2,22 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/msg_authorization/exported"
 )
 
 var ModuleCdc = codec.New()
 
-//func RegisterCodec(cdc *codec.Codec) {
-//	cdc.RegisterInterface((*exported.Authorization)(nil), nil)
-//	cdc.RegisterConcrete(MsgGrantAuthorization{}, "cosmos-sdk/GrantAuthorization", nil)
-//	cdc.RegisterConcrete(MsgRevokeAuthorization{}, "cosmos-sdk/RevokeAuthorization", nil)
-//	cdc.RegisterConcrete(MsgExecDelegated{}, "cosmos-sdk/ExecDelegated", nil)
-//	cdc.RegisterConcrete(SendAuthorization{}, "cosmos-sdk/SendAuthorization", nil)
-//	cdc.RegisterConcrete(AuthorizationGrant{}, "cosmos-sdk/AuthorizationGrant", nil)
-//	cdc.RegisterConcrete(GenericAuthorization{}, "cosmos-sdk/GenericAuthorization", nil)
-//}
-//
-//func init() {
-//	RegisterCodec(ModuleCdc)
-//}
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgGrantAuthorization{}, "cosmos-sdk/GrantAuthorization", nil)
+	cdc.RegisterConcrete(MsgRevokeAuthorization{}, "cosmos-sdk/RevokeAuthorization", nil)
+	cdc.RegisterConcrete(MsgExecDelegated{}, "cosmos-sdk/ExecDelegated", nil)
+	cdc.RegisterConcrete(SendAuthorization{}, "cosmos-sdk/SendAuthorization", nil)
+	cdc.RegisterConcrete(AuthorizationGrant{}, "cosmos-sdk/AuthorizationGrant", nil)
+	cdc.RegisterConcrete(GenericAuthorization{}, "cosmos-sdk/GenericAuthorization", nil)
+
+	cdc.RegisterInterface((*exported.Authorization)(nil), nil)
+}
+
+func init() {
+	RegisterCodec(ModuleCdc)
+}

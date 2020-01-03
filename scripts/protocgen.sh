974003bd367f979da0bb462c30882023fe64f4f1
Aaron Craelius <aaron@regen.network>
2020-01-03 18:43:43 -0500

    WIP on gov proto file

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 
 proto_dirs=$(find . -path ./vendor -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  protoc -I. \
-  --gocosmos_out=plugins=interfacetype,paths=source_relative:. \
-  $(find "${dir}" -name '*.proto')
+  protoc -I=. \
+  --gocosmos_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,plugins=interfacetype,paths=source_relative:. $(find "${dir}" -name '*.proto')
 done
--- x/upgrade/internal/types/plan.go
+++ x/upgrade/internal/types/plan.go
@@ -2,34 +2,13 @@ package types
 
 import (
 	"fmt"
+	"github.com/gogo/protobuf/types"
 	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
-
-// Plan specifies information about a planned upgrade and when it should occur
-type Plan struct {
-	// Sets the name for the upgrade. This name will be used by the upgraded version of the software to apply any
-	// special "on-upgrade" commands during the first BeginBlock method after the upgrade is applied. It is also used
-	// to detect whether a software version can handle a given upgrade. If no upgrade handler with this name has been
-	// set in the software, it will be assumed that the software is out-of-date when the upgrade Time or Height
-	// is reached and the software will exit.
-	Name string `json:"name,omitempty"`
-
-	// The time after which the upgrade must be performed.
-	// Leave set to its zero value to use a pre-defined Height instead.
-	Time time.Time `json:"time,omitempty"`
-
-	// The height at which the upgrade must be performed.
-	// Only used if Time is not set.
-	Height int64 `json:"height,omitempty"`
-
-	// Any application specific upgrade info to be included on-chain
-	// such as a git commit that validators could automatically upgrade to
-	Info string `json:"info,omitempty"`
-}
 
 func (p Plan) String() string {
 	due := p.DueAt()
@@ -40,18 +19,30 @@ func (p Plan) String() string {
   Info: %s`, p.Name, dueUp, p.Info)
 }
 
+func (p Plan) GetGoTime() time.Time {
+	if p.Time == nil {
+		return time.Time{}
+	}
+	t, err := types.TimestampFromProto(p.Time)
+	if err != nil {
+		panic(err)
+	}
+	return t;
+}
+
 // ValidateBasic does basic validation of a Plan
 func (p Plan) ValidateBasic() error {
+	t := p.GetGoTime()
 	if len(p.Name) == 0 {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "name cannot be empty")
 	}
 	if p.Height < 0 {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "height cannot be negative")
 	}
-	if p.Time.IsZero() && p.Height == 0 {
+	if t.IsZero() && p.Height == 0 {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "must set either time or height")
 	}
-	if !p.Time.IsZero() && p.Height != 0 {
+	if !t.IsZero() && p.Height != 0 {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "cannot set both time and height")
 	}
 
@@ -60,8 +51,9 @@ func (p Plan) ValidateBasic() error {
 
 // ShouldExecute returns true if the Plan is ready to execute given the current context
 func (p Plan) ShouldExecute(ctx sdk.Context) bool {
-	if !p.Time.IsZero() {
-		return !ctx.BlockTime().Before(p.Time)
+	t := p.GetGoTime()
+	if !t.IsZero() {
+		return !ctx.BlockTime().Before(t)
 	}
 	if p.Height > 0 {
 		return p.Height <= ctx.BlockHeight()
@@ -71,8 +63,9 @@ func (p Plan) ShouldExecute(ctx sdk.Context) bool {
 
 // DueAt is a string representation of when this plan is due to be executed
 func (p Plan) DueAt() string {
-	if !p.Time.IsZero() {
-		return fmt.Sprintf("time: %s", p.Time.UTC().Format(time.RFC3339))
+	t := p.GetGoTime()
+	if !t.IsZero() {
+		return fmt.Sprintf("time: %s", t.UTC().Format(time.RFC3339))
 	}
 	return fmt.Sprintf("height: %d", p.Height)
 }

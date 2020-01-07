9aee39d831c86614d89e9302f02cb1ad2e959c4b
mossid <torecursedivine@gmail.com>
2020-01-08 00:58:30 +0900

    mv recvpacket rest from 20 -> 04

--- x/ibc/20-transfer/client/rest/rest.go
+++ x/ibc/20-transfer/client/rest/rest.go
@@ -6,8 +6,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
-	channelexported "github.com/cosmos/cosmos-sdk/x/ibc/04-channel/exported"
-	commitment "github.com/cosmos/cosmos-sdk/x/ibc/23-commitment"
 )
 
 const (
@@ -27,12 +25,4 @@ type TransferTxReq struct {
 	Amount   sdk.Coins      `json:"amount" yaml:"amount"`
 	Receiver sdk.AccAddress `json:"receiver" yaml:"receiver"`
 	Source   bool           `json:"source" yaml:"source"`
-}
-
-// RecvPacketReq defines the properties of a receive packet request's body.
-type RecvPacketReq struct {
-	BaseReq rest.BaseReq            `json:"base_req" yaml:"base_req"`
-	Packet  channelexported.PacketI `json:"packet" yaml:"packet"`
-	Proofs  []commitment.Proof      `json:"proofs" yaml:"proofs"`
-	Height  uint64                  `json:"height" yaml:"height"`
 }

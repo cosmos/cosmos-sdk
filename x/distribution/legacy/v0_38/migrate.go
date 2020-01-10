b2d573a2b20193179fff3953c46346054374458a
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-01-10 13:27:16 -0500

    Add v0.38 genesis migration

---
+++ x/distribution/legacy/v0_38/migrate.go
@@ -0,0 +1,27 @@
+package v0_38
+
+// DONTCOVER
+// nolint
+
+import (
+	v036distr "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v0_36"
+)
+
+// Migrate accepts exported genesis state from v0.36 or v0.37 and migrates it to
+// v0.38 genesis state. All entries are identical except for parameters.
+func Migrate(oldGenState v036distr.GenesisState) GenesisState {
+	params := Params{
+		CommunityTax:        oldGenState.CommunityTax,
+		BaseProposerReward:  oldGenState.BaseProposerReward,
+		BonusProposerReward: oldGenState.BonusProposerReward,
+		WithdrawAddrEnabled: oldGenState.WithdrawAddrEnabled,
+	}
+
+	return NewGenesisState(
+		params, oldGenState.FeePool,
+		oldGenState.DelegatorWithdrawInfos, oldGenState.PreviousProposer,
+		oldGenState.OutstandingRewards, oldGenState.ValidatorAccumulatedCommissions,
+		oldGenState.ValidatorHistoricalRewards, oldGenState.ValidatorCurrentRewards,
+		oldGenState.DelegatorStartingInfos, oldGenState.ValidatorSlashEvents,
+	)
+}

d54baf9670221bf89a33aed94a8d6ef66ca55214
Christopher Goes <cwgoes@pluranimity.org>
2020-01-23 16:04:54 +0100

    Move to packet_test.go

--- x/ibc/04-channel/types/msgs_test.go
+++ x/ibc/04-channel/types/msgs_test.go
@@ -548,28 +548,3 @@ func (suite *MsgTestSuite) TestMsgAcknowledgement() {
 		}
 	}
 }
-
-func (suite *MsgTestSuite) TestPacketValidateBasic() {
-	testCases := []struct {
-		packet  Packet
-		expPass bool
-		errMsg  string
-	}{
-		{NewPacket(validPacketT{}, 1, portid, chanid, cpportid, cpchanid), true, ""},
-		{NewPacket(validPacketT{}, 0, portid, chanid, cpportid, cpchanid), false, "invalid sequence"},
-		{NewPacket(validPacketT{}, 1, invalidPort, chanid, cpportid, cpchanid), false, "invalid source port"},
-		{NewPacket(validPacketT{}, 1, portid, invalidChannel, cpportid, cpchanid), false, "invalid source channel"},
-		{NewPacket(validPacketT{}, 1, portid, chanid, invalidPort, cpchanid), false, "invalid destination port"},
-		{NewPacket(validPacketT{}, 1, portid, chanid, cpportid, invalidChannel), false, "invalid destination channel"},
-		{NewPacket(invalidPacketT{}, 1, portid, chanid, cpportid, cpchanid), false, "invalid packet data"},
-	}
-
-	for i, tc := range testCases {
-		err := tc.packet.ValidateBasic()
-		if tc.expPass {
-			suite.Require().NoError(err, "Msg %d failed: %s", i, tc.errMsg)
-		} else {
-			suite.Require().Error(err, "Invalid Msg %d passed: %s", i, tc.errMsg)
-		}
-	}
-}

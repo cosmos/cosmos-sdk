0a521bf033ed57b6dab0911931607e7df57e5602
Jack Zampolin <jack.zampolin@gmail.com>
2020-01-27 16:54:42 -0800

    one working testcase and mocked packet types

--- x/ibc/04-channel/keeper/packet_test.go
+++ x/ibc/04-channel/keeper/packet_test.go
@@ -1,6 +1,15 @@
 package keeper_test
 
+import (
+	"fmt"
+	connectionexported "github.com/cosmos/cosmos-sdk/x/ibc/03-connection/exported"
+	"github.com/cosmos/cosmos-sdk/x/ibc/04-channel/exported"
+	"github.com/cosmos/cosmos-sdk/x/ibc/04-channel/types"
+)
+
 func (suite *KeeperTestSuite) TestSendPacket() {
+	var counterparty types.Counterparty
+	var packet exported.PacketI
 	// Packet passes/fails validate basic packet
 	// Channel found/not found
 	// Channel closed/not CLOSED
@@ -13,6 +22,33 @@ func (suite *KeeperTestSuite) TestSendPacket() {
 	// Next sequence found/not found
 	// if packet.GetSequence() != nextSequenceSend {}
 	// Success
+	testCases := []testCase{
+		{"success", func() {
+			counterparty = types.NewCounterparty(testPort2, testChannel2)
+			suite.createClient(testClientID1)
+			suite.createConnection(testConnectionID1, testConnectionID2, testClientID1, testClientID2, connectionexported.INIT)
+			suite.createChannel(testPort1, testChannel1, testPort2, testChannel2, exported.OPEN, exported.ORDERED, testConnectionID1)
+			seq, _ := suite.app.IBCKeeper.ChannelKeeper.GetNextSequenceSend(suite.ctx, testPort1, testChannel1)
+			packet = types.NewPacket(mockSuccessPacket{}, seq+1, testPort1, testChannel1, counterparty.GetPortID(), counterparty.GetChannelID())
+			suite.app.IBCKeeper.ChannelKeeper.SetNextSequenceSend(suite.ctx, testPort1, testChannel1, 1)
+			suite.updateClient()
+		}, true},
+	}
+	for i, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s, %d/%d tests", tc.msg, i, len(testCases)), func() {
+			var err error
+			suite.SetupTest() // reset
+			tc.malleate()
+			if tc.expPass {
+				err = suite.app.IBCKeeper.ChannelKeeper.SendPacket(suite.ctx, packet)
+				suite.Require().NoError(err)
+			} else {
+				err = suite.app.IBCKeeper.ChannelKeeper.SendPacket(suite.ctx, packet)
+				suite.Require().Error(err)
+			}
+		})
+	}
+
 }
 
 func (suite *KeeperTestSuite) TestRecvPacket() {
@@ -71,3 +107,45 @@ func (suite *KeeperTestSuite) TestCleanupPacket() {
 	// Success/fail on verify UNORDERED packet commitment
 	// Invalid ordering packet failure
 }
+
+type mockSuccessPacket struct{}
+
+// GetBytes returns the serialised packet data (without timeout)
+func (mp mockSuccessPacket) GetBytes() []byte { return []byte("THIS IS A SUCCESS PACKET") }
+
+// GetTimeoutHeight returns the timeout height defined specifically for each packet data type instance
+func (mp mockSuccessPacket) GetTimeoutHeight() uint64 { return 10000 }
+
+// ValidateBasic validates basic properties of the packet data, implements sdk.Msg
+func (mp mockSuccessPacket) ValidateBasic() error { return nil }
+
+// Type returns human readable identifier, implements sdk.Msg
+func (mp mockSuccessPacket) Type() string { return "mock/packet/success" }
+
+type mockFailPacket struct{}
+
+// GetBytes returns the serialised packet data (without timeout)
+func (mp mockFailPacket) GetBytes() []byte { return []byte("THIS IS A FAILURE PACKET") }
+
+// GetTimeoutHeight returns the timeout height defined specifically for each packet data type instance
+func (mp mockFailPacket) GetTimeoutHeight() uint64 { return 10000 }
+
+// ValidateBasic validates basic properties of the packet data, implements sdk.Msg
+func (mp mockFailPacket) ValidateBasic() error { return fmt.Errorf("Packet failed validate basic") }
+
+// Type returns human readable identifier, implements sdk.Msg
+func (mp mockFailPacket) Type() string { return "mock/packet/failure" }
+
+type mockTimeoutPacket struct{}
+
+// GetBytes returns the serialised packet data (without timeout)
+func (mp mockTimeoutPacket) GetBytes() []byte { return []byte("THIS IS A TIMEOUT PACKET") }
+
+// GetTimeoutHeight returns the timeout height defined specifically for each packet data type instance
+func (mp mockTimeoutPacket) GetTimeoutHeight() uint64 { return 10 }
+
+// ValidateBasic validates basic properties of the packet data, implements sdk.Msg
+func (mp mockTimeoutPacket) ValidateBasic() error { return nil }
+
+// Type returns human readable identifier, implements sdk.Msg
+func (mp mockTimeoutPacket) Type() string { return "mock/packet/timeout" }

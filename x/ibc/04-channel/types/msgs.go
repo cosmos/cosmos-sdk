4f947b8f047f4b90d9d3d20e1914f899464fd974
Christopher Goes <cwgoes@pluranimity.org>
2020-02-03 13:52:29 +0100

    Validate acknowledgement field by length

--- x/ibc/04-channel/types/msgs.go
+++ x/ibc/04-channel/types/msgs.go
@@ -534,6 +534,9 @@ func (msg MsgAcknowledgement) ValidateBasic() error {
 	if err := msg.Proof.ValidateBasic(); err != nil {
 		return sdkerrors.Wrap(err, "proof ack cannot be nil")
 	}
+	if len(msg.Acknowledgement.GetBytes()) > 100 {
+		return sdkerrors.Wrap(ErrAcknowledgementTooLong, "acknowledgement cannot exceed 100 bytes")
+	}
 	if msg.ProofHeight == 0 {
 		return sdkerrors.Wrap(ibctypes.ErrInvalidHeight, "proof height must be > 0")
 	}

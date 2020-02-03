c268a9e539766aa1e704f31ab54f1dccfede4706
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-02-03 14:40:50 -0500

    Remove PubKey proto type

--- types/types.pb.go
+++ types/types.pb.go
@@ -4,15 +4,12 @@
 package types
 
 import (
-	bytes "bytes"
 	fmt "fmt"
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
 	math "math"
 	math_bits "math/bits"
-	reflect "reflect"
-	strings "strings"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -122,115 +119,15 @@ func (m *DecCoin) GetDenom() string {
 	return ""
 }
 
-// PublicKey defines a type to represent raw public key bytes.
-type PublicKey struct {
-	// Types that are valid to be assigned to Pub:
-	//	*PublicKey_Ed25519
-	//	*PublicKey_Secp256K1
-	//	*PublicKey_Sr25519
-	Pub isPublicKey_Pub `protobuf_oneof:"pub"`
-}
-
-func (m *PublicKey) Reset()      { *m = PublicKey{} }
-func (*PublicKey) ProtoMessage() {}
-func (*PublicKey) Descriptor() ([]byte, []int) {
-	return fileDescriptor_2c0f90c600ad7e2e, []int{2}
-}
-func (m *PublicKey) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *PublicKey) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_PublicKey.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *PublicKey) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PublicKey.Merge(m, src)
-}
-func (m *PublicKey) XXX_Size() int {
-	return m.Size()
-}
-func (m *PublicKey) XXX_DiscardUnknown() {
-	xxx_messageInfo_PublicKey.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_PublicKey proto.InternalMessageInfo
-
-type isPublicKey_Pub interface {
-	isPublicKey_Pub()
-	Equal(interface{}) bool
-	MarshalTo([]byte) (int, error)
-	Size() int
-}
-
-type PublicKey_Ed25519 struct {
-	Ed25519 []byte `protobuf:"bytes,1,opt,name=ed25519,proto3,oneof" json:"ed25519,omitempty"`
-}
-type PublicKey_Secp256K1 struct {
-	Secp256K1 []byte `protobuf:"bytes,2,opt,name=secp256k1,proto3,oneof" json:"secp256k1,omitempty"`
-}
-type PublicKey_Sr25519 struct {
-	Sr25519 []byte `protobuf:"bytes,3,opt,name=sr25519,proto3,oneof" json:"sr25519,omitempty"`
-}
-
-func (*PublicKey_Ed25519) isPublicKey_Pub()   {}
-func (*PublicKey_Secp256K1) isPublicKey_Pub() {}
-func (*PublicKey_Sr25519) isPublicKey_Pub()   {}
-
-func (m *PublicKey) GetPub() isPublicKey_Pub {
-	if m != nil {
-		return m.Pub
-	}
-	return nil
-}
-
-func (m *PublicKey) GetEd25519() []byte {
-	if x, ok := m.GetPub().(*PublicKey_Ed25519); ok {
-		return x.Ed25519
-	}
-	return nil
-}
-
-func (m *PublicKey) GetSecp256K1() []byte {
-	if x, ok := m.GetPub().(*PublicKey_Secp256K1); ok {
-		return x.Secp256K1
-	}
-	return nil
-}
-
-func (m *PublicKey) GetSr25519() []byte {
-	if x, ok := m.GetPub().(*PublicKey_Sr25519); ok {
-		return x.Sr25519
-	}
-	return nil
-}
-
-// XXX_OneofWrappers is for the internal use of the proto package.
-func (*PublicKey) XXX_OneofWrappers() []interface{} {
-	return []interface{}{
-		(*PublicKey_Ed25519)(nil),
-		(*PublicKey_Secp256K1)(nil),
-		(*PublicKey_Sr25519)(nil),
-	}
-}
-
 func init() {
 	proto.RegisterType((*Coin)(nil), "cosmos_sdk.v1.Coin")
 	proto.RegisterType((*DecCoin)(nil), "cosmos_sdk.v1.DecCoin")
-	proto.RegisterType((*PublicKey)(nil), "cosmos_sdk.v1.PublicKey")
 }
 
 func init() { proto.RegisterFile("types/types.proto", fileDescriptor_2c0f90c600ad7e2e) }
 
 var fileDescriptor_2c0f90c600ad7e2e = []byte{
-	// 293 bytes of a gzipped FileDescriptorProto
+	// 214 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2c, 0xa9, 0x2c, 0x48,
 	0x2d, 0xd6, 0x07, 0x93, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xbc, 0xc9, 0xf9, 0xc5, 0xb9,
 	0xf9, 0xc5, 0xf1, 0xc5, 0x29, 0xd9, 0x7a, 0x65, 0x86, 0x52, 0x6a, 0x25, 0x19, 0x99, 0x45, 0x29,
@@ -239,121 +136,14 @@ var fileDescriptor_2c0f90c600ad7e2e = []byte{
 	0x5e, 0x7e, 0xae, 0x04, 0xa3, 0x02, 0xa3, 0x06, 0x67, 0x10, 0x84, 0x23, 0xa4, 0xcc, 0xc5, 0x96,
 	0x98, 0x9b, 0x5f, 0x9a, 0x57, 0x22, 0xc1, 0x04, 0x12, 0x76, 0xe2, 0x3e, 0x71, 0x4f, 0x9e, 0xe1,
 	0xd6, 0x3d, 0x79, 0x66, 0xcf, 0xbc, 0x92, 0x20, 0xa8, 0x94, 0x92, 0x0b, 0x17, 0xbb, 0x4b, 0x6a,
-	0x32, 0x39, 0xa6, 0xb8, 0xa4, 0x26, 0xc3, 0x4d, 0x29, 0xe1, 0xe2, 0x0c, 0x28, 0x4d, 0xca, 0xc9,
-	0x4c, 0xf6, 0x4e, 0xad, 0x14, 0x92, 0xe2, 0x62, 0x4f, 0x4d, 0x31, 0x32, 0x35, 0x35, 0xb4, 0x04,
-	0x9b, 0xc4, 0xe3, 0xc1, 0x10, 0x04, 0x13, 0x10, 0x92, 0xe3, 0xe2, 0x2c, 0x4e, 0x4d, 0x2e, 0x30,
-	0x32, 0x35, 0xcb, 0x36, 0x04, 0x1b, 0x08, 0x92, 0x45, 0x08, 0x81, 0xf4, 0x16, 0x17, 0x41, 0xf4,
-	0x32, 0xc3, 0xf4, 0x42, 0x05, 0xac, 0x38, 0x5e, 0x2c, 0x90, 0x67, 0x6c, 0xb8, 0xa3, 0xc0, 0xe8,
-	0xc4, 0xca, 0xc5, 0x5c, 0x50, 0x9a, 0xe4, 0xe4, 0x72, 0xe3, 0xa1, 0x1c, 0x43, 0xc3, 0x23, 0x39,
-	0x86, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x71, 0xc2, 0x63,
-	0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18, 0x6e, 0x3c, 0x96, 0x63, 0x88, 0x52, 0x4a, 0xcf, 0x2c, 0xc9,
-	0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0x87, 0x04, 0x31, 0x94, 0xd2, 0x2d, 0x4e, 0xc9, 0x86,
-	0xc4, 0x40, 0x12, 0x1b, 0x38, 0x2c, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x83, 0x31, 0xbe,
-	0xbf, 0x97, 0x01, 0x00, 0x00,
+	0x32, 0x39, 0xa6, 0xb8, 0xa4, 0x26, 0xc3, 0x4c, 0x71, 0x72, 0xb9, 0xf1, 0x50, 0x8e, 0xa1, 0xe1,
+	0x91, 0x1c, 0xc3, 0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38,
+	0xe1, 0xb1, 0x1c, 0xc3, 0x85, 0xc7, 0x72, 0x0c, 0x37, 0x1e, 0xcb, 0x31, 0x44, 0x29, 0xa5, 0x67,
+	0x96, 0x64, 0x94, 0x26, 0xe9, 0x25, 0xe7, 0xe7, 0xea, 0x43, 0x3c, 0x0b, 0xa5, 0x74, 0x8b, 0x53,
+	0xb2, 0x21, 0x61, 0x91, 0xc4, 0x06, 0xf6, 0x95, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x8d, 0xc6,
+	0x8c, 0x7d, 0x21, 0x01, 0x00, 0x00,
 }
 
-func (this *PublicKey) Equal(that interface{}) bool {
-	if that == nil {
-		return this == nil
-	}
-
-	that1, ok := that.(*PublicKey)
-	if !ok {
-		that2, ok := that.(PublicKey)
-		if ok {
-			that1 = &that2
-		} else {
-			return false
-		}
-	}
-	if that1 == nil {
-		return this == nil
-	} else if this == nil {
-		return false
-	}
-	if that1.Pub == nil {
-		if this.Pub != nil {
-			return false
-		}
-	} else if this.Pub == nil {
-		return false
-	} else if !this.Pub.Equal(that1.Pub) {
-		return false
-	}
-	return true
-}
-func (this *PublicKey_Ed25519) Equal(that interface{}) bool {
-	if that == nil {
-		return this == nil
-	}
-
-	that1, ok := that.(*PublicKey_Ed25519)
-	if !ok {
-		that2, ok := that.(PublicKey_Ed25519)
-		if ok {
-			that1 = &that2
-		} else {
-			return false
-		}
-	}
-	if that1 == nil {
-		return this == nil
-	} else if this == nil {
-		return false
-	}
-	if !bytes.Equal(this.Ed25519, that1.Ed25519) {
-		return false
-	}
-	return true
-}
-func (this *PublicKey_Secp256K1) Equal(that interface{}) bool {
-	if that == nil {
-		return this == nil
-	}
-
-	that1, ok := that.(*PublicKey_Secp256K1)
-	if !ok {
-		that2, ok := that.(PublicKey_Secp256K1)
-		if ok {
-			that1 = &that2
-		} else {
-			return false
-		}
-	}
-	if that1 == nil {
-		return this == nil
-	} else if this == nil {
-		return false
-	}
-	if !bytes.Equal(this.Secp256K1, that1.Secp256K1) {
-		return false
-	}
-	return true
-}
-func (this *PublicKey_Sr25519) Equal(that interface{}) bool {
-	if that == nil {
-		return this == nil
-	}
-
-	that1, ok := that.(*PublicKey_Sr25519)
-	if !ok {
-		that2, ok := that.(PublicKey_Sr25519)
-		if ok {
-			that1 = &that2
-		} else {
-			return false
-		}
-	}
-	if that1 == nil {
-		return this == nil
-	} else if this == nil {
-		return false
-	}
-	if !bytes.Equal(this.Sr25519, that1.Sr25519) {
-		return false
-	}
-	return true
-}
 func (m *Coin) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -434,86 +224,6 @@ func (m *DecCoin) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *PublicKey) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *PublicKey) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *PublicKey) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.Pub != nil {
-		{
-			size := m.Pub.Size()
-			i -= size
-			if _, err := m.Pub.MarshalTo(dAtA[i:]); err != nil {
-				return 0, err
-			}
-		}
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *PublicKey_Ed25519) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *PublicKey_Ed25519) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	if m.Ed25519 != nil {
-		i -= len(m.Ed25519)
-		copy(dAtA[i:], m.Ed25519)
-		i = encodeVarintTypes(dAtA, i, uint64(len(m.Ed25519)))
-		i--
-		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-func (m *PublicKey_Secp256K1) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *PublicKey_Secp256K1) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	if m.Secp256K1 != nil {
-		i -= len(m.Secp256K1)
-		copy(dAtA[i:], m.Secp256K1)
-		i = encodeVarintTypes(dAtA, i, uint64(len(m.Secp256K1)))
-		i--
-		dAtA[i] = 0x12
-	}
-	return len(dAtA) - i, nil
-}
-func (m *PublicKey_Sr25519) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *PublicKey_Sr25519) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	if m.Sr25519 != nil {
-		i -= len(m.Sr25519)
-		copy(dAtA[i:], m.Sr25519)
-		i = encodeVarintTypes(dAtA, i, uint64(len(m.Sr25519)))
-		i--
-		dAtA[i] = 0x1a
-	}
-	return len(dAtA) - i, nil
-}
 func encodeVarintTypes(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTypes(v)
 	base := offset
@@ -555,108 +265,11 @@ func (m *DecCoin) Size() (n int) {
 	return n
 }
 
-func (m *PublicKey) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Pub != nil {
-		n += m.Pub.Size()
-	}
-	return n
-}
-
-func (m *PublicKey_Ed25519) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Ed25519 != nil {
-		l = len(m.Ed25519)
-		n += 1 + l + sovTypes(uint64(l))
-	}
-	return n
-}
-func (m *PublicKey_Secp256K1) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Secp256K1 != nil {
-		l = len(m.Secp256K1)
-		n += 1 + l + sovTypes(uint64(l))
-	}
-	return n
-}
-func (m *PublicKey_Sr25519) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Sr25519 != nil {
-		l = len(m.Sr25519)
-		n += 1 + l + sovTypes(uint64(l))
-	}
-	return n
-}
-
 func sovTypes(x uint64) (n int) {
 	return (math_bits.Len64(x|1) + 6) / 7
 }
 func sozTypes(x uint64) (n int) {
 	return sovTypes(uint64((x << 1) ^ uint64((int64(x) >> 63))))
-}
-func (this *PublicKey) String() string {
-	if this == nil {
-		return "nil"
-	}
-	s := strings.Join([]string{`&PublicKey{`,
-		`Pub:` + fmt.Sprintf("%v", this.Pub) + `,`,
-		`}`,
-	}, "")
-	return s
-}
-func (this *PublicKey_Ed25519) String() string {
-	if this == nil {
-		return "nil"
-	}
-	s := strings.Join([]string{`&PublicKey_Ed25519{`,
-		`Ed25519:` + fmt.Sprintf("%v", this.Ed25519) + `,`,
-		`}`,
-	}, "")
-	return s
-}
-func (this *PublicKey_Secp256K1) String() string {
-	if this == nil {
-		return "nil"
-	}
-	s := strings.Join([]string{`&PublicKey_Secp256K1{`,
-		`Secp256K1:` + fmt.Sprintf("%v", this.Secp256K1) + `,`,
-		`}`,
-	}, "")
-	return s
-}
-func (this *PublicKey_Sr25519) String() string {
-	if this == nil {
-		return "nil"
-	}
-	s := strings.Join([]string{`&PublicKey_Sr25519{`,
-		`Sr25519:` + fmt.Sprintf("%v", this.Sr25519) + `,`,
-		`}`,
-	}, "")
-	return s
-}
-func valueToStringTypes(v interface{}) string {
-	rv := reflect.ValueOf(v)
-	if rv.IsNil() {
-		return "nil"
-	}
-	pv := reflect.Indirect(rv).Interface()
-	return fmt.Sprintf("*%v", pv)
 }
 func (m *Coin) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
@@ -871,158 +484,6 @@ func (m *DecCoin) Unmarshal(dAtA []byte) error {
 			if err := m.Amount.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipTypes(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthTypes
-			}
-			if (iNdEx + skippy) < 0 {
-				return ErrInvalidLengthTypes
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *PublicKey) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowTypes
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: PublicKey: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: PublicKey: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Ed25519", wireType)
-			}
-			var byteLen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowTypes
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				byteLen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if byteLen < 0 {
-				return ErrInvalidLengthTypes
-			}
-			postIndex := iNdEx + byteLen
-			if postIndex < 0 {
-				return ErrInvalidLengthTypes
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			v := make([]byte, postIndex-iNdEx)
-			copy(v, dAtA[iNdEx:postIndex])
-			m.Pub = &PublicKey_Ed25519{v}
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Secp256K1", wireType)
-			}
-			var byteLen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowTypes
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				byteLen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if byteLen < 0 {
-				return ErrInvalidLengthTypes
-			}
-			postIndex := iNdEx + byteLen
-			if postIndex < 0 {
-				return ErrInvalidLengthTypes
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			v := make([]byte, postIndex-iNdEx)
-			copy(v, dAtA[iNdEx:postIndex])
-			m.Pub = &PublicKey_Secp256K1{v}
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Sr25519", wireType)
-			}
-			var byteLen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowTypes
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				byteLen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if byteLen < 0 {
-				return ErrInvalidLengthTypes
-			}
-			postIndex := iNdEx + byteLen
-			if postIndex < 0 {
-				return ErrInvalidLengthTypes
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			v := make([]byte, postIndex-iNdEx)
-			copy(v, dAtA[iNdEx:postIndex])
-			m.Pub = &PublicKey_Sr25519{v}
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex

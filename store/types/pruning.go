e8676aba17cca4ccfd13149da4424f9a02392792
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-02-06 10:49:44 -0500

    Linting and cleanup

--- store/types/pruning.go
+++ store/types/pruning.go
@@ -1,48 +1,66 @@
 package types
 
-// PruningOptions defines the specific pruning strategy every store in a multi-store will use
-// when committing state, where keepEvery determines which committed heights are flushed
-// to disk and snapshotEvery determines which of these heights are kept after pruning.
-// Note, that the invariant keepEvery % snapshotEvery = 0 must hold
-type PruningOptions struct {
-	KeepEvery     int64
-	SnapshotEvery int64
-}
-
-// Checks if PruningOptions is valid
-// SnapshotEvery is a multiple of KeepEvery
-func (po PruningOptions) IsValid() bool {
-	// Must flush at positive block interval
-	if po.KeepEvery <= 0 {
-		return false
-	}
-	// SnapshotEvery must be a multiple of KeepEvery
-	return po.SnapshotEvery%po.KeepEvery == 0
-}
-
-func (po PruningOptions) FlushVersion(ver int64) bool {
-	return po.KeepEvery != 0 && ver%po.KeepEvery == 0
-}
-
-func (po PruningOptions) SnapshotVersion(ver int64) bool {
-	return po.SnapshotEvery != 0 && ver%po.SnapshotEvery == 0
-}
-
-// default pruning strategies
 var (
-	// PruneEverything means all saved states will be deleted, storing only the current state
+	// PruneEverything defines a pruning strategy where all committed states will
+	// be deleted, persisting only the current state.
 	PruneEverything = PruningOptions{
 		KeepEvery:     1,
 		SnapshotEvery: 0,
 	}
-	// PruneNothing means all historic states will be saved, nothing will be deleted
+
+	// PruneNothing defines a pruning strategy where all committed states will be
+	// kept on disk, i.e. no states will be pruned.
 	PruneNothing = PruningOptions{
 		KeepEvery:     1,
 		SnapshotEvery: 1,
 	}
-	// PruneSyncable means only those states not needed for state syncing will be deleted (flush every 100 + Snapshot every 10000th)
+
+	// PruneSyncable defines a pruning strategy where only those states not needed
+	// for state syncing will be pruned. It flushes every 100th state to disk and
+	// keeps every 10000th.
 	PruneSyncable = PruningOptions{
 		KeepEvery:     100,
 		SnapshotEvery: 10000,
 	}
 )
+
+// PruningOptions defines the specific pruning strategy every store in a multi-store
+// will use when committing state, where keepEvery determines which committed
+// heights are flushed to disk and snapshotEvery determines which of these heights
+// are kept after pruning.
+type PruningOptions struct {
+	KeepEvery     int64
+	SnapshotEvery int64
+}
+
+// IsValid verifies if the pruning options are valid. It returns false if invalid
+// and true otherwise. Pruning options are considered valid iff:
+//
+// - KeepEvery > 0
+// - SnapshotEvery >= 0
+// - SnapshotEvery % KeepEvery = 0
+func (po PruningOptions) IsValid() bool {
+	// must flush at positive block interval
+	if po.KeepEvery <= 0 {
+		return false
+	}
+
+	// cannot snapshot negative intervals
+	if po.SnapshotEvery < 0 {
+		return false
+	}
+
+	return po.SnapshotEvery%po.KeepEvery == 0
+}
+
+// FlushVersion returns a boolean signaling if the provided version/height should
+// be flushed to disk.
+func (po PruningOptions) FlushVersion(ver int64) bool {
+	return po.KeepEvery != 0 && ver%po.KeepEvery == 0
+}
+
+// SnapshotVersion returns a boolean signaling if the provided version/height
+// should be snapshotted (kept on disk).
+func (po PruningOptions) SnapshotVersion(ver int64) bool {
+	return po.SnapshotEvery != 0 && ver%po.SnapshotEvery == 0
+}

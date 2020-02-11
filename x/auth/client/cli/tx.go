769e57f52926991d60b17f9e695686c286c7aa80
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-02-11 16:37:43 -0500

    Update client logic to use pkg-level codec

--- x/auth/client/cli/tx.go
+++ x/auth/client/cli/tx.go
@@ -8,6 +8,15 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
+// Codec defines the CLI codec to be used for use with the AccountRetriever.
+// Client must be sure to set this to their respective codec that implements the
+// AuthCodec interface and must be the same codec that passed to the x/auth
+// module.
+//
+// TODO:/XXX: Using a package-level global isn't ideal and we should consider
+// refactoring the module manager to allow passing in the correct module codec.
+var Codec types.AuthCodec = types.ModuleCdc
+
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	txCmd := &cobra.Command{

45f516e3897e489c9b9161cb715daf320b4e62f9
Marko Baricevic <marbar3778@yahoo.com>
2020-02-12 15:42:28 +0100

    decoder test

--- x/mint/simulation/decoder.go
+++ x/mint/simulation/decoder.go
@@ -11,7 +11,7 @@ import (
 )
 
 // DecodeStore unmarshals the KVPair's Value to the corresponding mint type
-func DecodeStore(cdc *codec.Codec, kvA, kvB tmkv.Pair) string {
+func DecodeStore(cdc codec.Marshaler, kvA, kvB tmkv.Pair) string {
 	switch {
 	case bytes.Equal(kvA.Key, types.MinterKey):
 		var minterA, minterB types.Minter

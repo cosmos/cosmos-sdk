a63de303b8005a9143e696d8918bb4101d605a2c
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2020-02-18 14:43:00 +0100

    Merge PR #5665: Bump Module ABCI Error Codes

--- x/params/types/errors.go
+++ x/params/types/errors.go
@@ -6,10 +6,10 @@ import (
 
 // x/params module sentinel errors
 var (
-	ErrUnknownSubspace  = sdkerrors.Register(ModuleName, 1, "unknown subspace")
-	ErrSettingParameter = sdkerrors.Register(ModuleName, 2, "failed to set parameter")
-	ErrEmptyChanges     = sdkerrors.Register(ModuleName, 3, "submitted parameter changes are empty")
-	ErrEmptySubspace    = sdkerrors.Register(ModuleName, 4, "parameter subspace is empty")
-	ErrEmptyKey         = sdkerrors.Register(ModuleName, 5, "parameter key is empty")
-	ErrEmptyValue       = sdkerrors.Register(ModuleName, 6, "parameter value is empty")
+	ErrUnknownSubspace  = sdkerrors.Register(ModuleName, 2, "unknown subspace")
+	ErrSettingParameter = sdkerrors.Register(ModuleName, 3, "failed to set parameter")
+	ErrEmptyChanges     = sdkerrors.Register(ModuleName, 4, "submitted parameter changes are empty")
+	ErrEmptySubspace    = sdkerrors.Register(ModuleName, 5, "parameter subspace is empty")
+	ErrEmptyKey         = sdkerrors.Register(ModuleName, 6, "parameter key is empty")
+	ErrEmptyValue       = sdkerrors.Register(ModuleName, 7, "parameter value is empty")
 )

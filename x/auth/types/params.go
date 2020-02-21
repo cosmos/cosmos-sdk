4a6278b94bf1a8f53e188721682c0ea50d783637
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-02-21 12:15:28 +0100

    Linting

--- x/auth/types/params.go
+++ x/auth/types/params.go
@@ -35,6 +35,7 @@ var _ paramtypes.ParamSet = &Params{}
 func NewParams(
 	maxMemoCharacters, txSigLimit, txSizeCostPerByte, sigVerifyCostED25519, sigVerifyCostSecp256k1 uint64,
 ) Params {
+
 	return Params{
 		MaxMemoCharacters:      maxMemoCharacters,
 		TxSigLimit:             txSigLimit,

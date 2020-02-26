6ef884f2f56bb78ded9ad360edfd5a43574f25ad
Federico Kunze <federico.kunze94@gmail.com>
2020-02-26 12:46:09 -0300

    proto: start ICS07 proto files

--- x/ibc/07-tendermint/types/codec.go
+++ x/ibc/07-tendermint/types/codec.go
@@ -4,10 +4,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
-// SubModuleCdc defines the IBC tendermint client codec.
-var SubModuleCdc *codec.Codec
-
-// RegisterCodec registers the Tendermint types
+// RegisterCodec registers the necessary x/ibc/07-tendermint interfaces and concrete types
+// on the provided Amino codec. These types are used for Amino JSON serialization.
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(ClientState{}, "ibc/client/tendermint/ClientState", nil)
 	cdc.RegisterConcrete(ConsensusState{}, "ibc/client/tendermint/ConsensusState", nil)
@@ -16,11 +14,22 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgCreateClient{}, "ibc/client/MsgCreateClient", nil)
 	cdc.RegisterConcrete(MsgUpdateClient{}, "ibc/client/MsgUpdateClient", nil)
 	cdc.RegisterConcrete(MsgSubmitClientMisbehaviour{}, "ibc/client/MsgSubmitClientMisbehaviour", nil)
-
-	SetSubModuleCodec(cdc)
 }
 
-// SetSubModuleCodec sets the ibc tendermint client codec
-func SetSubModuleCodec(cdc *codec.Codec) {
-	SubModuleCdc = cdc
+var (
+	amino = codec.New()
+
+	// SubModuleCdc references the global x/ibc/07-tendermint module codec. Note, the codec
+	// should ONLY be used in certain instances of tests and for JSON encoding as Amino
+	// is still used for that purpose.
+	//
+	// The actual codec used for serialization should be provided to x/ibc/07-tendermint and
+	// defined at the application level.
+	SubModuleCdc = codec.NewHybridCodec(amino)
+)
+
+func init() {
+	RegisterCodec(amino)
+	codec.RegisterCrypto(amino)
+	amino.Seal()
 }

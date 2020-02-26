8a38c3e2bd78ecbd798f2ef365a813858de5654b
Federico Kunze <federico.kunze94@gmail.com>
2020-02-26 17:35:21 -0300

    proto: various ICS proto definitions

--- x/ibc/02-client/types/codec.go
+++ x/ibc/02-client/types/codec.go
@@ -5,10 +5,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/ibc/02-client/exported"
 )
 
-// SubModuleCdc defines the IBC client codec.
-var SubModuleCdc *codec.Codec
-
-// RegisterCodec registers the IBC client interfaces and types
+// RegisterCodec registers the necessary x/ibc/02-client interfaces and concrete types
+// on the provided Amino codec. These types are used for Amino JSON serialization.
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterInterface((*exported.ClientState)(nil), nil)
 	cdc.RegisterInterface((*exported.MsgCreateClient)(nil), nil)
@@ -16,10 +14,22 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterInterface((*exported.ConsensusState)(nil), nil)
 	cdc.RegisterInterface((*exported.Header)(nil), nil)
 	cdc.RegisterInterface((*exported.Misbehaviour)(nil), nil)
-
-	SetSubModuleCodec(cdc)
 }
 
-func SetSubModuleCodec(cdc *codec.Codec) {
-	SubModuleCdc = cdc
+var (
+	amino = codec.New()
+
+	// SubModuleCdc references the global x/ibc/02-client module codec. Note, the codec
+	// should ONLY be used in certain instances of tests and for JSON encoding as Amino
+	// is still used for that purpose.
+	//
+	// The actual codec used for serialization should be provided to x/ibc/02-client and
+	// defined at the application level.
+	SubModuleCdc = codec.NewHybridCodec(amino)
+)
+
+func init() {
+	RegisterCodec(amino)
+	codec.RegisterCrypto(amino)
+	amino.Seal()
 }
--- x/ibc/23-commitment/codec.go
+++ x/ibc/23-commitment/codec.go
@@ -4,9 +4,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
-var SubModuleCdc *codec.Codec
-
-// RegisterCodec registers types declared in this package
+// RegisterCodec registers the necessary x/ibc/23-commitment interfaces and concrete types
+// on the provided Amino codec. These types are used for Amino JSON serialization.
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterInterface((*RootI)(nil), nil)
 	cdc.RegisterInterface((*PrefixI)(nil), nil)
@@ -17,10 +16,21 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(Prefix{}, "ibc/commitment/merkle/Prefix", nil)
 	cdc.RegisterConcrete(Path{}, "ibc/commitment/merkle/Path", nil)
 	cdc.RegisterConcrete(Proof{}, "ibc/commitment/merkle/Proof", nil)
-
-	SetSubModuleCodec(cdc)
 }
 
-func SetSubModuleCodec(cdc *codec.Codec) {
-	SubModuleCdc = cdc
+var (
+	amino = codec.New()
+
+	// SubModuleCdc references the global x/ibc/23-commitmentl module codec. Note, the codec should
+	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
+	// still used for that purpose.
+	//
+	// The actual codec used for serialization should be provided to x/ibc/23-commitmentl and
+	// defined at the application level.
+	SubModuleCdc = codec.NewHybridCodec(amino)
+)
+
+func init() {
+	RegisterCodec(amino)
+	amino.Seal()
 }

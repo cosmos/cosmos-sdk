cfb3819183b01e7c209ef5e9ea405b0a45cd85cb
Jonathan Gimeno <jgimeno@gmail.com>
2020-02-27 13:40:01 +0100

    add function to get pruning options from flags

---
+++ server/pruning.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"github.com/cosmos/cosmos-sdk/store"
+	"github.com/spf13/viper"
+)
+
+func GetPruningOptionsFromFlags() store.PruningOptions {
+	if viper.IsSet(flagPruning) {
+		return store.NewPruningOptionsFromString(viper.GetString(flagPruning))
+	}
+
+	if viper.IsSet(flagPruningKeepEvery) && viper.IsSet(flagPruningSnapshotEvery) {
+		return store.PruningOptions{
+			KeepEvery:     viper.GetInt64(flagPruningKeepEvery),
+			SnapshotEvery: viper.GetInt64(flagPruningSnapshotEvery),
+		}
+	}
+
+	return store.PruneSyncable
+}

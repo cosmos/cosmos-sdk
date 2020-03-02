e8af2b48ee5970e690c6e560fc706c2f0e30ef3e
Jonathan Gimeno <jgimeno@gmail.com>
2020-03-02 13:26:55 +0100

    migrate keeper test to simapp

--- x/distribution/keeper/common_test.go
+++ x/distribution/keeper/common_test.go
@@ -3,6 +3,8 @@ package keeper_test
 import (
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/cosmos/cosmos-sdk/x/supply"
 )
 
 var (
@@ -15,4 +17,6 @@ var (
 	valConsAddr1 = sdk.ConsAddress(valConsPk1.Address())
 	valConsAddr2 = sdk.ConsAddress(valConsPk2.Address())
 	valConsAddr3 = sdk.ConsAddress(valConsPk3.Address())
+
+	distrAcc = supply.NewEmptyModuleAccount(types.ModuleName)
 )

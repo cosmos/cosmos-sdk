e009fc3080f21d7107daac79f3159c3ab7cbd67e
Jonathan Gimeno <jgimeno@gmail.com>
2020-03-17 19:49:31 +0100

    add flag for cli context

--- client/context/context_test.go
+++ client/context/context_test.go
@@ -3,13 +3,26 @@ package context
 import (
 	"testing"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/client/flags"
 )
 
 func TestCLIContext_WithOffline(t *testing.T) {
-	ctx := NewCLIContext()
-	require.False(t, ctx.Offline)
+	viper.Set(flags.FlagOffline, true)
+	viper.Set(flags.FlagNode, "tcp://localhost:26657")
 
-	ctx = ctx.WithOffline(true)
+	ctx := NewCLIContext()
 	require.True(t, ctx.Offline)
+	require.Nil(t, ctx.Client)
+
+	viper.Reset()
+
+	viper.Set(flags.FlagOffline, false)
+	viper.Set(flags.FlagNode, "tcp://localhost:26657")
+
+	ctx = NewCLIContext()
+	require.False(t, ctx.Offline)
+	require.NotNil(t, ctx.Client)
 }

191df595d2145f33ea3718826e8343f96f043c34
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-03-19 15:41:12 -0400

    Add codec

---
+++ x/capability/types/codec.go
@@ -0,0 +1,25 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// ModuleCdc defines the capability module's codec. The codec is not sealed as to
+// allow other modules to register their concrete Capability types.
+var ModuleCdc = codec.New()
+
+// RegisterCodec registers all the necessary types and interfaces for the
+// capability module.
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterInterface((*Capability)(nil), nil)
+}
+
+// RegisterCapabilityTypeCodec registers an external concrete Capability type
+// defined in another module for the internal ModuleCdc.
+func RegisterCapabilityTypeCodec(o interface{}, name string) {
+	ModuleCdc.RegisterConcrete(o, name, nil)
+}
+
+func init() {
+	RegisterCodec(ModuleCdc)
+}

b6548b0892a30031287ee1ed502281c1dca55860
Marko Baricevic <marbar3778@yahoo.com>
2020-03-19 09:51:11 +0100

    remore require error

--- x/params/types/subspace_test.go
+++ x/params/types/subspace_test.go
@@ -112,8 +112,7 @@ func (suite *SubspaceTestSuite) TestModified() {
 
 func (suite *SubspaceTestSuite) TestUpdate() {
 	suite.Require().Panics(func() {
-		err := suite.ss.Update(suite.ctx, []byte("invalid_key"), nil)
-		suite.Require().NoError(err)
+		suite.ss.Update(suite.ctx, []byte("invalid_key"), nil) // nolint:errcheck
 	})
 
 	t := time.Hour * 48

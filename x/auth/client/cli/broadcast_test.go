9df8a94ce9c7123d1e2b23b48a2749fa48572209
Jonathan Gimeno <jgimeno@gmail.com>
2020-03-19 01:35:57 +0100

    test broadcast

--- x/auth/client/cli/broadcast_test.go
+++ x/auth/client/cli/broadcast_test.go
@@ -1,18 +1,19 @@
 package cli
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
-
-	"github.com/stretchr/testify/require"
-
 	"github.com/spf13/viper"
-
+	"github.com/stretchr/testify/require"
 	"github.com/tendermint/go-amino"
+
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/tests"
 )
 
-func TestGetBroadcastCommand_CannotBePerformedWhenOfflineFlag(t *testing.T) {
+func TestGetBroadcastCommand_OfflineFlag(t *testing.T) {
 	codec := amino.NewCodec()
 	cmd := GetBroadcastCommand(codec)
 
@@ -20,4 +21,25 @@ func TestGetBroadcastCommand_CannotBePerformedWhenOfflineFlag(t *testing.T) {
 
 	err := cmd.RunE(nil, []string{})
 	require.EqualError(t, err, "cannot broadcast tx during offline mode")
+}
+
+func TestGetBroadcastCommand_WithoutOfflineFlag(t *testing.T) {
+	codec := amino.NewCodec()
+	cmd := GetBroadcastCommand(codec)
+
+	viper.Set(flags.FlagOffline, false)
+
+	testDir, cleanFunc := tests.NewTestCaseDir(t)
+	t.Cleanup(cleanFunc)
+
+	// Create new file with tx
+	txContents := []byte("{\"type\":\"cosmos-sdk/StdTx\",\"value\":{\"msg\":[{\"type\":\"cosmos-sdk/MsgSend\",\"value\":{\"from_address\":\"cosmos1cxlt8kznps92fwu3j6npahx4mjfutydyene2qw\",\"to_address\":\"cosmos1wc8mpr8m3sy3ap3j7fsgqfzx36um05pystems4\",\"amount\":[{\"denom\":\"stake\",\"amount\":\"10000\"}]}}],\"fee\":{\"amount\":[],\"gas\":\"200000\"},\"signatures\":null,\"memo\":\"\"}}")
+	txFileName := fmt.Sprintf("%s/tx.json", testDir)
+	err := ioutil.WriteFile(txFileName, txContents, 0644)
+	require.NoError(t, err)
+
+	err = cmd.RunE(cmd, []string{txFileName})
+
+	// We test it tries to broadcast but we set unsupported tx to get the error.
+	require.EqualError(t, err, "unsupported return type ; supported types: sync, async, block")
 }

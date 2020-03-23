7af823eddebe6acbaa71ee4a64f58085edbc2aab
Jonathan Gimeno <jgimeno@gmail.com>
2020-03-23 12:13:50 +0100

    Update x/gov/simulation/params.go
    Co-Authored-By: Marko <marbar3778@yahoo.com>

--- x/gov/simulation/params.go
+++ x/gov/simulation/params.go
@@ -8,7 +8,6 @@ import (
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/x/simulation"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"

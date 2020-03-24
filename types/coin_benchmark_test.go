1040ca01b606f256b9d48830356be1f1c5282816
phuoc <phuocdongxoai@gmail.com>
2020-03-24 09:32:53 +0700

    fix more invalid coin names

--- types/coin_benchmark_test.go
+++ types/coin_benchmark_test.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -12,10 +13,10 @@ func BenchmarkCoinsAdditionIntersect(b *testing.B) {
 			coinsB := Coins(make([]Coin, numCoinsB))
 
 			for i := 0; i < numCoinsA; i++ {
-				coinsA[i] = NewCoin("COINZ_"+string(i), NewInt(int64(i)))
+				coinsA[i] = NewCoin("coinz"+strconv.Itoa(i), NewInt(int64(i)))
 			}
 			for i := 0; i < numCoinsB; i++ {
-				coinsB[i] = NewCoin("COINZ_"+string(i), NewInt(int64(i)))
+				coinsB[i] = NewCoin("coinz"+strconv.Itoa(i), NewInt(int64(i)))
 			}
 
 			b.ResetTimer()
@@ -41,10 +42,10 @@ func BenchmarkCoinsAdditionNoIntersect(b *testing.B) {
 			coinsB := Coins(make([]Coin, numCoinsB))
 
 			for i := 0; i < numCoinsA; i++ {
-				coinsA[i] = NewCoin("COINZ_"+string(numCoinsB+i), NewInt(int64(i)))
+				coinsA[i] = NewCoin("coinz"+strconv.Itoa(i), NewInt(int64(i)))
 			}
 			for i := 0; i < numCoinsB; i++ {
-				coinsB[i] = NewCoin("COINZ_"+string(i), NewInt(int64(i)))
+				coinsB[i] = NewCoin("coinz"+strconv.Itoa(i), NewInt(int64(i)))
 			}
 
 			b.ResetTimer()

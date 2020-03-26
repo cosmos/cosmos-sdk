f58970d9ef5c0a48e8f2b8dece41a29bbadf9a23
Alessio Treglia <alessio@tendermint.com>
2020-03-26 11:40:29 +0100

    New keyring interface
    WIP

--- crypto/keyring/keyring.go
+++ crypto/keyring/keyring.go
@@ -14,6 +14,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/tendermint/crypto/bcrypt"
+	"github.com/tendermint/tendermint/crypto"
 	tmcrypto "github.com/tendermint/tendermint/crypto"
 	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
 
@@ -38,6 +39,69 @@ const (
 )
 
 var _ Keybase = keyringKeybase{}
+
+// Keyring exposes operations on a generic keystore
+type Keyring interface {
+
+	// List all keys.
+	List() ([]Info, error)
+
+	// Key and KeyByAddress return keys by uid and address respectively.
+	Key(uid string) (Info, error)
+	KeyByAddress(address types.Address) (Info, error)
+
+	// Delete and DeleteByAddress remove keys.
+	Delete(uid string) error
+	DeleteByAddress(address types.Address) error
+
+	// NewMnemonic generates a new mnemonic, derives a hierarchical deterministic
+	// key from that, and persists it to storage. Returns the generated mnemonic and the key
+	// Info. It returns an error if it fails to generate a key for the given algo type, or if
+	// another key is already stored under the same name.
+	NewMnemonic(uid string, language Language, algo SigningAlgo) (Info, string, error)
+
+	// NewAccount converts a mnemonic to a private key and BIP-39 HD Path  and persists it.
+	NewAccount(uid, mnemonic, bip39Passwd, hdPath string, algo SigningAlgo) (Info, error)
+
+	// SaveLedgerKey retrieves a public key reference from a Ledger device and persists it.
+	SaveLedgerKey(uid string, algo SigningAlgo, hrp string, account, index uint32) (Info, error)
+
+	// SavePubKey stores a public key and returns the persisted Info structure.
+	SavePubKey(uid string, pubkey crypto.PubKey, algo SigningAlgo) (Info, error)
+
+	// SaveMultisig stores, stores, and returns a new multsig (offline) key reference
+	SaveMultisig(uid string, pubkey crypto.PubKey) (Info, error)
+
+	// SupportedAlgos returns a list of signing algorithms supported by the keybase
+	SupportedAlgos() []SigningAlgo
+
+	// SupportedAlgosLedger returns a list of signing algorithms supported by the keybase's ledger integration
+	SupportedAlgosLedger() []SigningAlgo
+}
+
+// Signer is implemented by key stores that want to provide signing capabilities.
+type Signer interface {
+	// Sign and SignByAddress sign byte messages with a user key.
+	Sign(uid string, msg []byte) ([]byte, crypto.PubKey, error)
+	SignByAddress(address types.Address, msg []byte) ([]byte, crypto.PubKey, error)
+}
+
+// Importer is implemented by key stores that support import of public and private keys.
+type Importer interface {
+	ImportPrivKey(uid, armor, passphrase string) error
+	ImportPubKey(uid string, armor string) error
+}
+
+// Exporter is implemented by key stores that support export of public and private keys.
+type Exporter interface {
+	// Export public key
+	ExportPubKeyArmor(uid string) (string, error)
+	ExportPubKeyArmorByAddress(address types.Address) (string, error)
+	// ExportPrivKey returns a private key in ASCII armored format.
+	// It returns an error if the key does not exist or a wrong encryption passphrase is supplied.
+	ExportPrivKeyArmor(uid, encryptPassphrase string) (armor string, err error)
+	ExportPrivKeyArmorByAddress(address types.Address, encryptPassphrase string) (armor string, err error)
+}
 
 // keyringKeybase implements the Keybase interface by using the Keyring library
 // for account key persistence.

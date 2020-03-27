32699c59854c0388bed2b15081c34919e3e68fe7
Sahith Reddy Narahari <sahith@vitwit.com>
2020-03-27 21:21:53 +0530

    Added proto compatible x/slashing clients

--- x/slashing/client/rest/tx.go
+++ x/slashing/client/rest/tx.go
@@ -7,22 +7,80 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	authclient "github.com/cosmos/cosmos-sdk/x/auth/client"
 	"github.com/cosmos/cosmos-sdk/x/slashing/types"
 )
 
-func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc(
-		"/slashing/validators/{validatorAddr}/unjail",
-		unjailRequestHandlerFn(cliCtx),
-	).Methods("POST")
+func registerTxHandlers(ctx context.CLIContext, m codec.Marshaler, txg tx.Generator, r *mux.Router) {
+	r.HandleFunc("/slashing/validators/{validatorAddr}/unjail", NewUnjailRequestHandlerFn(ctx, m, txg)).Methods("POST")
 }
 
 // Unjail TX body
 type UnjailReq struct {
 	BaseReq rest.BaseReq `json:"base_req" yaml:"base_req"`
+}
+
+// NewUnjailRequestHandlerFn returns an HTTP REST handler for creating a MsgUnjail
+// transaction.
+func NewUnjailRequestHandlerFn(ctx context.CLIContext, m codec.Marshaler, txg tx.Generator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx = ctx.WithMarshaler(m)
+
+		vars := mux.Vars(r)
+		bech32Validator := vars["validatorAddr"]
+
+		var req UnjailReq
+		if !rest.ReadRESTReq(w, r, ctx.Marshaler, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		valAddr, err := sdk.ValAddressFromBech32(bech32Validator)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if !bytes.Equal(fromAddr, valAddr) {
+			rest.WriteErrorResponse(w, http.StatusUnauthorized, "must use own validator address")
+			return
+		}
+
+		msg := types.NewMsgUnjail(valAddr)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		tx.WriteGeneratedTxResponse(ctx, w, txg, req.BaseReq, msg)
+	}
+}
+
+// ---------------------------------------------------------------------------
+// Deprecated
+//
+// TODO: Remove once client-side Protobuf migration has been completed.
+// ---------------------------------------------------------------------------
+// ref: https://github.com/cosmos/cosmos-sdk/issues/5864
+func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc(
+		"/slashing/validators/{validatorAddr}/unjail",
+		unjailRequestHandlerFn(cliCtx),
+	).Methods("POST")
 }
 
 func unjailRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {

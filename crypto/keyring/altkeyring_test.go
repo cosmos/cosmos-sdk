1c736f1cbbf39eeb5196b6043ffca166a668ff77
Jonathan Gimeno <jgimeno@gmail.com>
2020-03-30 12:59:21 +0200

    add signature into keyring

--- crypto/keyring/altkeyring_test.go
+++ crypto/keyring/altkeyring_test.go
@@ -230,6 +230,25 @@ func TestAltKeyring_SaveMultisig(t *testing.T) {
 	require.Len(t, list, 3)
 }
 
+func TestAltKeyring_Sign(t *testing.T) {
+	dir, clean := tests.NewTestCaseDir(t)
+	t.Cleanup(clean)
+
+	keyring, err := NewAltKeyring(t.Name(), BackendTest, dir, nil)
+	require.NoError(t, err)
+
+	uid := "jack"
+	_, _, err = keyring.NewMnemonic(uid, English, AltSecp256k1)
+	require.NoError(t, err)
+
+	msg := []byte("some message")
+
+	sign, key, err := keyring.Sign(uid, msg)
+	require.NoError(t, err)
+
+	require.True(t, key.VerifyBytes(msg, sign))
+}
+
 func requireEqualInfo(t *testing.T, key Info, mnemonic Info) {
 	require.Equal(t, key.GetName(), mnemonic.GetName())
 	require.Equal(t, key.GetAddress(), mnemonic.GetAddress())

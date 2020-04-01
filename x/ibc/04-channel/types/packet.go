842d73dcad12e9a7ac0ebc4ff066cb6b7119135d
Michael FIG <mfig@agoric.com>
2020-04-01 12:44:50 -0600

    chore: remove unnecessary "encoding/binary" import

--- x/ibc/04-channel/types/packet.go
+++ x/ibc/04-channel/types/packet.go
@@ -1,14 +1,12 @@
 package types
 
 import (
-	"encoding/binary"
-
 	"github.com/tendermint/tendermint/crypto/tmhash"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/ibc/04-channel/exported"
 	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // CommitPacket return the hash of commitment bytes

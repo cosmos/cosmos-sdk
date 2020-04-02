613cdd8de6e0bdfbbd1750bf9eb52245ccd5f05d
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-04-02 13:23:58 -0400

    Linting

--- x/capability/types/store.go
+++ x/capability/types/store.go
@@ -16,41 +16,41 @@ func NewCapabilityStore() CapabilityStore {
 // GetCapability returns a Capability by module and name tuple. If no Capability
 // exists, nil will be returned.
 func (cs CapabilityStore) GetCapability(module, name string) Capability {
-	key := string(RevCapabilityKey(module, name))
-	return cs.revMemStore[key]
+	key := RevCapabilityKey(module, name)
+	return cs.revMemStore[string(key)]
 }
 
 // GetCapabilityName returns a Capability name by module and Capability tuple. If
 // no Capability name exists for the given tuple, an empty string is returned.
 func (cs CapabilityStore) GetCapabilityName(module string, cap Capability) string {
-	key := string(FwdCapabilityKey(module, cap))
-	return cs.fwdMemStore[key]
+	key := FwdCapabilityKey(module, cap)
+	return cs.fwdMemStore[string(key)]
 }
 
 // SetCapability sets the reverse mapping between the module and capability name
 // and the capability in the in-memory store.
 func (cs CapabilityStore) SetCapability(module, name string, cap Capability) {
-	key := string(RevCapabilityKey(module, name))
-	cs.revMemStore[key] = cap
+	key := RevCapabilityKey(module, name)
+	cs.revMemStore[string(key)] = cap
 }
 
 // SetCapabilityName sets the forward mapping between the module and capability
 // tuple and the capability name in the in-memory store.
 func (cs CapabilityStore) SetCapabilityName(module, name string, cap Capability) {
-	key := string(FwdCapabilityKey(module, cap))
-	cs.fwdMemStore[key] = name
+	key := FwdCapabilityKey(module, cap)
+	cs.fwdMemStore[string(key)] = name
 }
 
 // DeleteCapability removes the reverse mapping between the module and capability
 // name and the capability in the in-memory store.
 func (cs CapabilityStore) DeleteCapability(module, name string) {
-	key := string(RevCapabilityKey(module, name))
-	delete(cs.revMemStore, key)
+	key := RevCapabilityKey(module, name)
+	delete(cs.revMemStore, string(key))
 }
 
 // DeleteCapabilityName removes the forward mapping between the module and capability
 // tuple and the capability name in the in-memory store.
 func (cs CapabilityStore) DeleteCapabilityName(module string, cap Capability) {
-	key := string(FwdCapabilityKey(module, cap))
-	delete(cs.fwdMemStore, key)
+	key := FwdCapabilityKey(module, cap)
+	delete(cs.fwdMemStore, string(key))
 }

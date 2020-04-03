7f78e61b93a522989b603e30871b4e9e9a779402
SaReN <sahith@vitwit.com>
2020-04-04 01:36:37 +0530

    Merge PR #5915: Tx Client Migration: x/staking

--- x/staking/client/rest/rest.go
+++ x/staking/client/rest/rest.go
@@ -4,7 +4,14 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/cosmos/cosmos-sdk/codec"
 )
+
+func RegisterHandlers(cliCtx context.CLIContext, m codec.Marshaler, txg tx.Generator, r *mux.Router) {
+	registerQueryRoutes(cliCtx, r)
+	registerTxHandlers(cliCtx, m, txg, r)
+}
 
 // RegisterRoutes registers staking-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {

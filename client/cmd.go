9cfd8a1c5c5dc72eb7cd3625a56be6468606d771
Colin Axner <colinaxner@berkeley.edu>
2020-04-03 13:21:32 -0700

    lint fix and updated tests

--- client/cmd.go
+++ client/cmd.go
@@ -20,7 +20,8 @@ func ValidateCmd(cmd *cobra.Command, args []string) error {
 		}
 
 		// check if the current arg is a flag
-		if len(arg) > 0 && (arg[0] == '-') {
+		switch {
+		case len(arg) > 0 && (arg[0] == '-'):
 			// the next arg should be skipped if the current arg is a
 			// flag and does not use "=" to assign the flag's value
 			if !strings.Contains(arg, "=") {
@@ -28,23 +29,29 @@ func ValidateCmd(cmd *cobra.Command, args []string) error {
 			} else {
 				skipNext = false
 			}
-		} else if skipNext {
+		case skipNext:
 			// skip current arg
 			skipNext = false
-		} else if unknownCmd == "" {
+		case unknownCmd == "":
 			// unknown command found
+			// continue searching for help flag
 			unknownCmd = arg
 		}
 	}
 
-	err := fmt.Sprintf("unknown command \"%s\" for \"%s\"", unknownCmd, cmd.CalledAs())
+	// return the help screen if no unknown command is found
+	if unknownCmd != "" {
+		err := fmt.Sprintf("unknown command \"%s\" for \"%s\"", unknownCmd, cmd.CalledAs())
 
-	// build suggestions for unknown argument
-	if suggestions := cmd.SuggestionsFor(unknownCmd); len(suggestions) > 0 {
-		err += "\n\nDid you mean this?\n"
-		for _, s := range suggestions {
-			err += fmt.Sprintf("\t%v\n", s)
+		// build suggestions for unknown argument
+		if suggestions := cmd.SuggestionsFor(unknownCmd); len(suggestions) > 0 {
+			err += "\n\nDid you mean this?\n"
+			for _, s := range suggestions {
+				err += fmt.Sprintf("\t%v\n", s)
+			}
 		}
+		return errors.New(err)
 	}
-	return errors.New(err)
+
+	return cmd.Help()
 }

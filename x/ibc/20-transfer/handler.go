df5badaf4c2bffde370d7cdee95ff9c1d15e7577
Christopher Goes <cwgoes@pluranimity.org>
2020-04-06 19:06:59 +0200

    Merge PR #5939: Unmarshal packets as JSON

--- x/ibc/20-transfer/handler.go
+++ x/ibc/20-transfer/handler.go
@@ -16,13 +16,13 @@ func NewHandler(k Keeper) sdk.Handler {
 			return handleMsgTransfer(ctx, k, msg)
 		case channeltypes.MsgPacket:
 			var data FungibleTokenPacketData
-			if err := types.ModuleCdc.UnmarshalBinaryBare(msg.GetData(), &data); err != nil {
+			if err := types.ModuleCdc.UnmarshalJSON(msg.GetData(), &data); err != nil {
 				return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet data: %s", err.Error())
 			}
 			return handlePacketDataTransfer(ctx, k, msg, data)
 		case channeltypes.MsgTimeout:
 			var data FungibleTokenPacketData
-			if err := types.ModuleCdc.UnmarshalBinaryBare(msg.GetData(), &data); err != nil {
+			if err := types.ModuleCdc.UnmarshalJSON(msg.GetData(), &data); err != nil {
 				return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet data: %s", err.Error())
 			}
 			return handleTimeoutDataTransfer(ctx, k, msg, data)

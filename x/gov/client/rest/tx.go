ecf248a1bb105219354293582be00125f3ff0944
Aaron Craelius <aaronc@users.noreply.github.com>
2020-04-07 13:18:42 -0400

    Add protobuf x/gov REST tx handlers

--- x/gov/client/rest/tx.go
+++ x/gov/client/rest/tx.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -14,6 +15,119 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
+func registerTxHandlers(cliCtx context.CLIContext, m types.Codec, txg tx.Generator, r *mux.Router, phs []ProposalRESTHandler) {
+	propSubRtr := r.PathPrefix("/gov/proposals").Subrouter()
+	for _, ph := range phs {
+		propSubRtr.HandleFunc(fmt.Sprintf("/%s", ph.SubRoute), ph.Handler).Methods("POST")
+	}
+
+	cliCtx = cliCtx.WithMarshaler(m)
+	r.HandleFunc("/gov/proposals", newPostProposalHandlerFn(cliCtx, m, txg)).Methods("POST")
+	r.HandleFunc(fmt.Sprintf("/gov/proposals/{%s}/deposits", RestProposalID), newDepositHandlerFn(cliCtx, txg)).Methods("POST")
+	r.HandleFunc(fmt.Sprintf("/gov/proposals/{%s}/votes", RestProposalID), newVoteHandlerFn(cliCtx, txg)).Methods("POST")
+}
+
+func newPostProposalHandlerFn(cliCtx context.CLIContext, m types.Codec, txg tx.Generator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req PostProposalReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		proposalType := gcutils.NormalizeProposalType(req.ProposalType)
+		content := types.ContentFromProposalType(req.Title, req.Description, proposalType)
+
+		msg, err := m.NewMsgSubmitProposal(content, req.InitialDeposit, req.Proposer)
+		if rest.CheckBadRequestError(w, err) {
+			return
+		}
+		if rest.CheckBadRequestError(w, msg.ValidateBasic()) {
+			return
+		}
+
+		tx.WriteGeneratedTxResponse(cliCtx, w, txg, req.BaseReq, msg)
+	}
+}
+
+func newDepositHandlerFn(cliCtx context.CLIContext, txg tx.Generator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		strProposalID := vars[RestProposalID]
+
+		if len(strProposalID) == 0 {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "proposalId required but not specified")
+			return
+		}
+
+		proposalID, ok := rest.ParseUint64OrReturnBadRequest(w, strProposalID)
+		if !ok {
+			return
+		}
+
+		var req DepositReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgDeposit(req.Depositor, proposalID, req.Amount)
+		if rest.CheckBadRequestError(w, msg.ValidateBasic()) {
+			return
+		}
+
+		tx.WriteGeneratedTxResponse(cliCtx, w, txg, req.BaseReq, msg)
+	}
+}
+
+func newVoteHandlerFn(cliCtx context.CLIContext, txg tx.Generator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		strProposalID := vars[RestProposalID]
+
+		if len(strProposalID) == 0 {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "proposalId required but not specified")
+			return
+		}
+
+		proposalID, ok := rest.ParseUint64OrReturnBadRequest(w, strProposalID)
+		if !ok {
+			return
+		}
+
+		var req VoteReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+
+		voteOption, err := types.VoteOptionFromString(gcutils.NormalizeVoteOption(req.Option))
+		if rest.CheckBadRequestError(w, err) {
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgVote(req.Voter, proposalID, voteOption)
+		if rest.CheckBadRequestError(w, msg.ValidateBasic()) {
+			return
+		}
+
+		tx.WriteGeneratedTxResponse(cliCtx, w, txg, req.BaseReq, msg)
+	}
+}
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router, phs []ProposalRESTHandler) {
 	propSubRtr := r.PathPrefix("/gov/proposals").Subrouter()
 	for _, ph := range phs {

890f6786b9c309035f362e36658c1827260d2754
Alessio Treglia <alessio@tendermint.com>
2020-04-14 10:38:51 +0200

    don't use viper.IsSet()

--- server/pruning.go
+++ server/pruning.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 
 	"github.com/cosmos/cosmos-sdk/store"
@@ -9,17 +11,23 @@ import (
 // GetPruningOptionsFromFlags parses start command flags and returns the correct PruningOptions.
 // flagPruning prevails over flagPruningKeepEvery and flagPruningSnapshotEvery.
 // Default option is PruneSyncable.
-func GetPruningOptionsFromFlags() store.PruningOptions {
-	if viper.IsSet(flagPruning) {
-		return store.NewPruningOptionsFromString(viper.GetString(flagPruning))
-	}
+func GetPruningOptionsFromFlags() (store.PruningOptions, error) {
+	strategy := viper.GetString(flagPruning)
+	switch strategy {
+	case "syncable", "nothing", "everything":
+		return store.NewPruningOptionsFromString(viper.GetString(flagPruning)), nil
 
-	if viper.IsSet(flagPruningKeepEvery) && viper.IsSet(flagPruningSnapshotEvery) {
-		return store.PruningOptions{
+	case "custom":
+		opts := store.PruningOptions{
 			KeepEvery:     viper.GetInt64(flagPruningKeepEvery),
 			SnapshotEvery: viper.GetInt64(flagPruningSnapshotEvery),
 		}
-	}
+		if !opts.IsValid() {
+			return opts, fmt.Errorf("invalid granular options")
+		}
+		return opts, nil
 
-	return store.PruneSyncable
+	default:
+		return store.PruningOptions{}, fmt.Errorf("unknown pruning strategy %s", strategy)
+	}
 }

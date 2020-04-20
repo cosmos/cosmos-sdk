2b98aff91db9025a17cb996917c151f066cd6be1
Jonathan Gimeno <jgimeno@gmail.com>
2020-04-21 01:25:29 +0200

    mispelled back

--- client/cmd_test.go
+++ client/cmd_test.go
@@ -37,7 +37,7 @@ func TestValidateCmd(t *testing.T) {
 		args    []string
 		wantErr bool
 	}{
-		{"misspelled command", []string{"commission"}, true}, // nolint: misspell
+		{"misspelled command", []string{"comission"}, true}, // nolint: misspell
 		{"no command provided", []string{}, false},
 		{"help flag", []string{"commission", "--help"}, false},       // nolint: misspell
 		{"shorthand help flag", []string{"commission", "-h"}, false}, // nolint: misspell

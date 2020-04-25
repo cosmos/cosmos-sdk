e21f8a27ca8e9638d99e530e6bf32dd669068d9c
Aaron Craelius <aaron@regen.network>
2020-04-24 21:45:18 -0400

    Add Any value cache

--- types/any.go
+++ types/any.go
@@ -6,6 +6,44 @@ import (
 	"reflect"
 )
 
+type Any struct {
+	// A URL/resource name that uniquely identifies the type of the serialized
+	// protocol buffer message. This string must contain at least
+	// one "/" character. The last segment of the URL's path must represent
+	// the fully qualified name of the type (as in
+	// `path/google.protobuf.Duration`). The name should be in a canonical form
+	// (e.g., leading "." is not accepted).
+	//
+	// In practice, teams usually precompile into the binary all types that they
+	// expect it to use in the context of Any. However, for URLs which use the
+	// scheme `http`, `https`, or no scheme, one can optionally set up a type
+	// server that maps type URLs to message definitions as follows:
+	//
+	// * If no scheme is provided, `https` is assumed.
+	// * An HTTP GET on the URL must yield a [google.protobuf.Type][]
+	//   value in binary format, or produce an error.
+	// * Applications are allowed to cache lookup results based on the
+	//   URL, or have them precompiled into a binary to avoid any
+	//   lookup. Therefore, binary compatibility needs to be preserved
+	//   on changes to types. (Use versioned type names to manage
+	//   breaking changes.)
+	//
+	// Note: this functionality is not currently available in the official
+	// protobuf release, and it is not used for type URLs beginning with
+	// type.googleapis.com.
+	//
+	// Schemes other than `http`, `https` (or the empty scheme) might be
+	// used with implementation specific semantics.
+	//
+	TypeUrl string `protobuf:"bytes,1,opt,name=type_url,json=typeUrl,proto3" json:"type_url,omitempty"`
+	// Must be a valid serialized protocol buffer of the above specified type.
+	Value                []byte   `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+	cachedValue          interface{}
+}
+
 type interfaceMap = map[string]reflect.Type
 
 type interfaceContext struct {
@@ -51,6 +89,7 @@ func (any *Any) Pack(x proto.Message) error {
 		return err
 	}
 	any.Value = bz
+	any.cachedValue = x
 	return nil
 }
 
@@ -59,7 +98,15 @@ func (ctx *interfaceContext) UnpackAny(any *Any, iface interface{}) error {
 	if rv.Kind() != reflect.Ptr {
 		return fmt.Errorf("UnpackAny expects a pointer")
 	}
-	imap, found := ctx.interfaceImpls[rv.Elem().Type()]
+	rt := rv.Elem().Type()
+	cachedValue := any.cachedValue
+	if cachedValue != nil {
+		if reflect.TypeOf(cachedValue).AssignableTo(rt) {
+			rv.Elem().Set(reflect.ValueOf(cachedValue))
+			return nil
+		}
+	}
+	imap, found := ctx.interfaceImpls[rt]
 	if !found {
 		return fmt.Errorf("no registered implementations of interface type %T", iface)
 	}
@@ -76,6 +123,7 @@ func (ctx *interfaceContext) UnpackAny(any *Any, iface interface{}) error {
 		return err
 	}
 	rv.Elem().Set(reflect.ValueOf(msg))
+	any.cachedValue = msg
 	return nil
 }
 
--- types/any_test.go
+++ types/any_test.go
@@ -16,12 +16,23 @@ func TestAny_Pack(t *testing.T) {
 	ctx.RegisterImplementation((*TestI)(nil), &testdata.Dog{})
 	ctx.RegisterImplementation((*TestI)(nil), &testdata.Cat{})
 
+	// without cache
 	spot := &testdata.Dog{Name: "Spot"}
 	any := Any{}
 	err := any.Pack(spot)
 	require.NoError(t, err)
 
+	any.cachedValue = nil
+
 	var test TestI
+	err = ctx.UnpackAny(&any, &test)
+	require.NoError(t, err)
+	require.Equal(t, spot, test)
+
+	// with cache
+	err = any.Pack(spot)
+	require.Equal(t, spot, any.cachedValue)
+	require.NoError(t, err)
 	err = ctx.UnpackAny(&any, &test)
 	require.NoError(t, err)
 	require.Equal(t, spot, test)

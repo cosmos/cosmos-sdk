9b51908597b3775b274d4562de808f8a217bc04c
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-04-27 14:04:26 -0400

    ibc/02-client: import export GenesisState (#6073)
    * ibc/02-client: import export GenesisState
    
    * client validation
    
    * genesis validation
    
    * ibc genesis tests
    
    * GetAllConsensusStates test
    
    * fix non-determinism
    
    * lint
    
    * fix test

--- x/ibc/genesis.go
+++ x/ibc/genesis.go
@@ -2,17 +2,20 @@ package ibc
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	client "github.com/cosmos/cosmos-sdk/x/ibc/02-client"
 	connection "github.com/cosmos/cosmos-sdk/x/ibc/03-connection"
 )
 
 // GenesisState defines the ibc module's genesis state.
 type GenesisState struct {
+	ClientGenesis     client.GenesisState     `json:"client_genesis" yaml:"client_genesis"`
 	ConnectionGenesis connection.GenesisState `json:"connection_genesis" yaml:"connection_genesis"`
 }
 
 // DefaultGenesisState returns the ibc module's default genesis state.
 func DefaultGenesisState() GenesisState {
 	return GenesisState{
+		ClientGenesis:     client.DefaultGenesisState(),
 		ConnectionGenesis: connection.DefaultGenesisState(),
 	}
 }
@@ -20,18 +23,24 @@ func DefaultGenesisState() GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+	if err := gs.ClientGenesis.Validate(); err != nil {
+		return err
+	}
+
 	return gs.ConnectionGenesis.Validate()
 }
 
-// InitGenesis initializes the ibc connection submodule's state from a provided genesis
+// InitGenesis initializes the ibc state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k Keeper, gs GenesisState) {
+	client.InitGenesis(ctx, k.ClientKeeper, gs.ClientGenesis)
 	connection.InitGenesis(ctx, k.ConnectionKeeper, gs.ConnectionGenesis)
 }
 
-// ExportGenesis returns the ibc connection submodule's exported genesis.
+// ExportGenesis returns the ibc exported genesis.
 func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
 	return GenesisState{
+		ClientGenesis:     client.ExportGenesis(ctx, k.ClientKeeper),
 		ConnectionGenesis: connection.ExportGenesis(ctx, k.ConnectionKeeper),
 	}
 }

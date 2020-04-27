0114086c1408809b40a773e218ee5e934fc05d57
Aaron Craelius <aaron@regen.network>
2020-04-27 13:37:55 -0400

    Fix linting issues

--- std/tx_test.go
+++ std/tx_test.go
@@ -1,24 +1,24 @@
 package std_test
 
 import (
-	std2 "github.com/cosmos/cosmos-sdk/std"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	std "github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 )
 
 func TestTransaction(t *testing.T) {
-	f := std2.NewStdFee(100, sdk.NewCoins(sdk.NewInt64Coin("stake", 50)))
+	f := std.NewStdFee(100, sdk.NewCoins(sdk.NewInt64Coin("stake", 50)))
 	m := "hello world"
 	acc1 := sdk.AccAddress("from")
 	acc2 := sdk.AccAddress("to")
 	msg1 := bank.NewMsgSend(acc1, acc2, sdk.NewCoins(sdk.NewInt64Coin("stake", 100000)))
 	sdkMsgs := []sdk.Msg{&msg1}
 
-	tx, err := std2.NewTransaction(f, m, sdkMsgs)
+	tx, err := std.NewTransaction(f, m, sdkMsgs)
 	require.NoError(t, err)
 	require.NotNil(t, tx)
 	require.Equal(t, tx.GetMsgs(), sdkMsgs)

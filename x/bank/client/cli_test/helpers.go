68a14336d70d5328823d5d06341ef484ddf3c473
sahith-narahari <sahithnarahari@gmail.com>
2020-04-30 03:12:34 +0530

    Moved supply test from mint to bank

--- x/bank/client/cli_test/helpers.go
+++ x/bank/client/cli_test/helpers.go
@@ -48,3 +48,26 @@ func QueryBalances(f *helpers.Fixtures, address sdk.AccAddress, flags ...string)
 
 	return balances
 }
+
+// QueryTotalSupply returns the total supply of coins
+func QueryTotalSupply(f *helpers.Fixtures, flags ...string) (totalSupply sdk.Coins) {
+	cmd := fmt.Sprintf("%s query bank total %s", f.SimcliBinary, f.Flags())
+	res, errStr := tests.ExecuteT(f.T, cmd, "")
+	require.Empty(f.T, errStr)
+
+	err := f.Cdc.UnmarshalJSON([]byte(res), &totalSupply)
+	require.NoError(f.T, err)
+	return totalSupply
+}
+
+// QueryTotalSupplyOf returns the total supply of a given coin denom
+func QueryTotalSupplyOf(f *helpers.Fixtures, denom string, flags ...string) sdk.Int {
+	cmd := fmt.Sprintf("%s query bank total %s %s", f.SimcliBinary, denom, f.Flags())
+	res, errStr := tests.ExecuteT(f.T, cmd, "")
+	require.Empty(f.T, errStr)
+
+	var supplyOf sdk.Int
+	err := f.Cdc.UnmarshalJSON([]byte(res), &supplyOf)
+	require.NoError(f.T, err)
+	return supplyOf
+}

e6dc755ab3ac1d1f809f28ee87ceef116b2f7fcd
Federico Kunze <federico.kunze94@gmail.com>
2020-05-07 17:45:04 -0400

    remove localhost client

--- x/ibc/client/cli/cli.go
+++ x/ibc/client/cli/cli.go
@@ -10,7 +10,6 @@ import (
 	connection "github.com/cosmos/cosmos-sdk/x/ibc/03-connection"
 	channel "github.com/cosmos/cosmos-sdk/x/ibc/04-channel"
 	tmclient "github.com/cosmos/cosmos-sdk/x/ibc/07-tendermint/client/cli"
-	localhostclient "github.com/cosmos/cosmos-sdk/x/ibc/09-localhost/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/ibc/types"
 )
 
@@ -26,7 +25,6 @@ func GetTxCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 
 	ibcTxCmd.AddCommand(flags.PostCommands(
 		tmclient.GetTxCmd(cdc, storeKey),
-		localhostclient.GetTxCmd(cdc, storeKey),
 		connection.GetTxCmd(cdc, storeKey),
 		channel.GetTxCmd(cdc, storeKey),
 	)...)

99433f7e8bd0a28eb7eeafcdce41652d05560785
Marko Baricevic <marbar3778@yahoo.com>
2020-05-12 17:55:30 +0200

    change to fmt.Errorf

--- types/bech32/bech32.go
+++ types/bech32/bech32.go
@@ -1,15 +1,16 @@
 package bech32
 
 import (
+	"fmt"
+
 	"github.com/btcsuite/btcutil/bech32"
-	"github.com/pkg/errors"
 )
 
 //ConvertAndEncode converts from a base64 encoded byte string to base32 encoded byte string and then to bech32
 func ConvertAndEncode(hrp string, data []byte) (string, error) {
 	converted, err := bech32.ConvertBits(data, 8, 5, true)
 	if err != nil {
-		return "", errors.Wrap(err, "encoding bech32 failed")
+		return "", fmt.Errorf("encoding bech32 failed: %w", err)
 	}
 
 	return bech32.Encode(hrp, converted)
@@ -19,12 +20,12 @@ func ConvertAndEncode(hrp string, data []byte) (string, error) {
 func DecodeAndConvert(bech string) (string, []byte, error) {
 	hrp, data, err := bech32.Decode(bech)
 	if err != nil {
-		return "", nil, errors.Wrap(err, "decoding bech32 failed")
+		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
 	}
 
 	converted, err := bech32.ConvertBits(data, 5, 8, false)
 	if err != nil {
-		return "", nil, errors.Wrap(err, "decoding bech32 failed")
+		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
 	}
 
 	return hrp, converted, nil

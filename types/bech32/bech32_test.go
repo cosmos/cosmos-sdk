8ce6e18ff0004c9189bcf52a0a6ffddadbe02183
Marko Baricevic <marbar3778@yahoo.com>
2020-05-12 18:20:33 +0200

    remove err cheks

--- types/bech32/bech32_test.go
+++ types/bech32/bech32_test.go
@@ -14,14 +14,10 @@ func TestEncodeAndDecode(t *testing.T) {
 	ss := "shasum"
 
 	bech, err := bech32.ConvertAndEncode(ss, sum[:])
-	if err != nil {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 
 	hrp, data, err := bech32.DecodeAndConvert(bech)
-	if err != nil {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 
 	require.Equal(t, hrp, ss, "Invalid hrp")
 	require.True(t, bytes.Equal(data, sum[:]), "Invalid decode")

c9ec0ff0870fd2a3220869b7b94a412a29ec8017
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-05-14 18:47:11 -0400

    x/ibc: default genesis state for sims (#6158)
    * x/ibc: default genesis state for sims
    
    * add ibc and transfer module to simulations
    
    * minor update
    
    * remove TODO
    
    * add validation to transfer genesis
    
    * fix transfer init genesis
    
    * address @alexanderbez comments

--- x/ibc/20-transfer/types/genesis.go
+++ x/ibc/20-transfer/types/genesis.go
@@ -1,13 +1,24 @@
 package types
 
+import (
+	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
+)
+
 // GenesisState is currently only used to ensure that the InitGenesis gets run
 // by the module manager
 type GenesisState struct {
-	PortID string `json:"portid" yaml:"portid"`
+	PortID string `json:"port_id" yaml:"port_id"`
 }
 
-func DefaultGenesis() GenesisState {
+// DefaultGenesisState returns a GenesisState with "transfer" as the default PortID.
+func DefaultGenesisState() GenesisState {
 	return GenesisState{
 		PortID: PortID,
 	}
+}
+
+// Validate performs basic genesis state validation returning an error upon any
+// failure.
+func (gs GenesisState) Validate() error {
+	return host.PortIdentifierValidator(gs.PortID)
 }

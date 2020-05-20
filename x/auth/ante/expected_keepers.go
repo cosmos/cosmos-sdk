bf8809ef9840b4f5369887a38d8345e2380a567f
SaReN <sahithnarahari@gmail.com>
2020-05-21 00:51:00 +0530

    Update x/auth to use Any (#6165)
    * Migrate keeper codec to use marshaler
    
    * Migrate AccountI to types
    
    * Did go imports
    
    * Fix tests for x/auth
    
    * Cleanup std/codec
    
    * Sort imports
    
    * Fix legacy codec
    
    * Add godoc for RegisterInterfaces
    
    * Add RegisterInterfaces to std
    
    * Fix typo
    
    * Fixed merge changes
    
    * Eliminate vesting import in auth
    
    * Fix lint issues
    
    * Fix tests
    
    * Addressed comments
    
    * Rename interfaces in RegisterInterfaces
    
    * Removed codec.proto from std
    
    * Minor code cleanup
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>

--- x/auth/ante/expected_keepers.go
+++ x/auth/ante/expected_keepers.go
@@ -2,7 +2,6 @@ package ante
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/exported"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
@@ -10,7 +9,7 @@ import (
 // Interface provides support to use non-sdk AccountKeeper for AnteHandler's decorators.
 type AccountKeeper interface {
 	GetParams(ctx sdk.Context) (params types.Params)
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) exported.Account
-	SetAccount(ctx sdk.Context, acc exported.Account)
+	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
+	SetAccount(ctx sdk.Context, acc types.AccountI)
 	GetModuleAddress(moduleName string) sdk.AccAddress
 }

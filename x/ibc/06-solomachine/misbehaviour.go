d459d57b6bc0f5f4465cef4c567e9145e1792036
Colin Axner <colinaxner@berkeley.edu>
2020-05-21 14:50:48 -0700

    add misbehaviour tests

--- x/ibc/06-solomachine/misbehaviour.go
+++ x/ibc/06-solomachine/misbehaviour.go
@@ -3,6 +3,7 @@ package solomachine
 import (
 	"bytes"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	clientexported "github.com/cosmos/cosmos-sdk/x/ibc/02-client/exported"
 	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/02-client/types"
@@ -12,7 +13,7 @@ import (
 // public key signed over two different messages with the same sequence.
 func CheckMisbehaviourAndUpdateState(
 	clientState clientexported.ClientState,
-	consensusState clientexported.ConsensusState,
+	_ clientexported.ConsensusState,
 	misbehaviour clientexported.Misbehaviour,
 ) (clientexported.ClientState, error) {
 	// cast the interface to specific types before checking for misbehaviour
@@ -25,12 +26,12 @@ func CheckMisbehaviourAndUpdateState(
 		return nil, sdkerrors.Wrapf(clienttypes.ErrInvalidEvidence, "client is already frozen")
 	}
 
-	smEvidence, ok := misbehaviour.(Evidence)
+	evidence, ok := misbehaviour.(Evidence)
 	if !ok {
 		return nil, sdkerrors.Wrap(clienttypes.ErrInvalidClientType, "evidence type is not solo machine")
 	}
 
-	if err := checkMisbehaviour(smClientState, smEvidence); err != nil {
+	if err := checkMisbehaviour(smClientState, evidence); err != nil {
 		return nil, sdkerrors.Wrap(clienttypes.ErrInvalidEvidence, err.Error())
 	}
 
@@ -48,14 +49,18 @@ func checkMisbehaviour(clientState ClientState, evidence Evidence) error {
 		return sdkerrors.Wrap(clienttypes.ErrInvalidEvidence, "evidence signatures have identical data messages")
 	}
 
+	data := append(sdk.Uint64ToBigEndian(evidence.Sequence), evidence.SignatureOne.Data...)
+
 	// check first signature
-	if pubKey.VerifyBytes(evidence.SignatureOne.Data, evidence.SignatureOne.Signature) {
-		return sdkerrors.Wrap(clienttypes.ErrInvalidEvidence, "evidence signature one not signed by currently registered public key")
+	if !pubKey.VerifyBytes(data, evidence.SignatureOne.Signature) {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidEvidence, "evidence signature one failed to be verified by currently registered public key")
 	}
 
+	data = append(sdk.Uint64ToBigEndian(evidence.Sequence), evidence.SignatureTwo.Data...)
+
 	// check second signature
-	if pubKey.VerifyBytes(evidence.SignatureTwo.Data, evidence.SignatureTwo.Signature) {
-		return sdkerrors.Wrap(clienttypes.ErrInvalidEvidence, "evidence signature two not signed by currently registered public key")
+	if !pubKey.VerifyBytes(data, evidence.SignatureTwo.Signature) {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidEvidence, "evidence signature two failed to be verified by currently registered public key")
 	}
 
 	return nil
--- x/ibc/06-solomachine/misbehaviour_test.go
+++ x/ibc/06-solomachine/misbehaviour_test.go
@@ -1,1 +1,145 @@
 package solomachine_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	clientexported "github.com/cosmos/cosmos-sdk/x/ibc/02-client/exported"
+	solomachine "github.com/cosmos/cosmos-sdk/x/ibc/06-solomachine"
+	ibctmtypes "github.com/cosmos/cosmos-sdk/x/ibc/07-tendermint/types"
+)
+
+func (suite *SoloMachineTestSuite) TestCheckMisbehaviourAndUpdateState() {
+	var (
+		clientState clientexported.ClientState
+		evidence    clientexported.Misbehaviour
+	)
+
+	testCases := []struct {
+		name    string
+		setup   func()
+		expPass bool
+	}{
+		{
+			"valid misbehaviour evidence",
+			func() {
+				clientState = suite.ClientState()
+				evidence = suite.Evidence()
+			},
+			true,
+		},
+		{
+			"wrong client state type",
+			func() {
+				clientState = ibctmtypes.ClientState{}
+				evidence = suite.Evidence()
+			},
+			false,
+		},
+		{
+			"invalid evidence type",
+			func() {
+				clientState = suite.ClientState()
+				evidence = ibctmtypes.Evidence{}
+			},
+			false,
+		},
+		{
+			"equal data in signatures",
+			func() {
+				clientState = suite.ClientState()
+
+				// store in tmp var before assigning to interface type
+				ev := suite.Evidence()
+				ev.SignatureOne.Data = ev.SignatureTwo.Data
+				evidence = ev
+			},
+			false,
+		},
+		{
+			"invalid first signature",
+			func() {
+				clientState = suite.ClientState()
+
+				// store in tmp var before assigning to interface type
+				ev := suite.Evidence()
+
+				msg := []byte("DATA ONE")
+				data := append(sdk.Uint64ToBigEndian(suite.sequence+1), msg...)
+				sig, err := suite.privKey.Sign(data)
+				suite.Require().NoError(err)
+
+				ev.SignatureOne.Signature = sig
+				ev.SignatureOne.Data = msg
+				evidence = ev
+			},
+			false,
+		},
+		{
+			"invalid second signature",
+			func() {
+				clientState = suite.ClientState()
+
+				// store in tmp var before assigning to interface type
+				ev := suite.Evidence()
+
+				msg := []byte("DATA TWO")
+				data := append(sdk.Uint64ToBigEndian(suite.sequence+1), msg...)
+				sig, err := suite.privKey.Sign(data)
+				suite.Require().NoError(err)
+
+				ev.SignatureTwo.Signature = sig
+				ev.SignatureTwo.Data = msg
+				evidence = ev
+			},
+			false,
+		},
+		{
+			"signatures sign over different sequence",
+			func() {
+				clientState = suite.ClientState()
+
+				// store in tmp var before assigning to interface type
+				ev := suite.Evidence()
+
+				// Signature One
+				msg := []byte("DATA ONE")
+				// sequence used is plus 1
+				data := append(sdk.Uint64ToBigEndian(suite.sequence+1), msg...)
+				sig, err := suite.privKey.Sign(data)
+				suite.Require().NoError(err)
+
+				ev.SignatureOne.Signature = sig
+				ev.SignatureOne.Data = msg
+
+				// Signature Two
+				msg = []byte("DATA TWO")
+				// sequence used is minus 1
+				data = append(sdk.Uint64ToBigEndian(suite.sequence-1), msg...)
+				sig, err = suite.privKey.Sign(data)
+				suite.Require().NoError(err)
+
+				ev.SignatureTwo.Signature = sig
+				ev.SignatureTwo.Data = msg
+
+				evidence = ev
+
+			},
+			false,
+		},
+	}
+
+	for i, tc := range testCases {
+		tc := tc
+		// setup test
+		tc.setup()
+
+		clientState, err := solomachine.CheckMisbehaviourAndUpdateState(clientState, suite.ConsensusState(), evidence)
+
+		if tc.expPass {
+			suite.Require().NoError(err, "valid test case %d failed: %s", i, tc.name)
+			suite.Require().True(clientState.IsFrozen(), "valid test case %d did not freeze the client: %s", i, tc.name)
+		} else {
+			suite.Require().Error(err, "invalid test case %d passed: %s", i, tc.name)
+			suite.Require().Nil(clientState, "invalid test case %d returned non-nil client state: %s", i, tc.name)
+		}
+	}
+}

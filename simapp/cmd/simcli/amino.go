f089efb235847fd87e5e174bea257fd90aa440d6
Aaron Craelius <aaron@regen.network>
2020-05-26 15:59:35 -0400

    Fix amino build

--- simapp/cmd/simcli/amino.go
+++ simapp/cmd/simcli/amino.go
@@ -4,14 +4,16 @@ package main
 
 import (
 	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 func MakeTxCLIContext() context.CLIContext {
 	cliCtx := context.CLIContext{}
+	aminoCdc := codec.NewAminoCodec(encodingConfig.Amino)
 	return cliCtx.
-		WithJSONMarshaler(appCodec).
-		WithTxGenerator(MakeTxGenerator()).
-		WithAccountRetriever(types.NewAccountRetriever(appCodec)).
-		WithCodec(cdc)
+		WithJSONMarshaler(aminoCdc).
+		WithTxGenerator(encodingConfig.TxGenerator).
+		WithAccountRetriever(types.NewAccountRetriever(encodingConfig.Marshaler)).
+		WithCodec(encodingConfig.Amino)
 }

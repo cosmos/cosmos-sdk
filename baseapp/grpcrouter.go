8cca6bcf228c3999d96ce3f695cf0f2bdee1d768
Aaron Craelius <aaron@regen.network>
2020-06-02 16:04:52 -0400

    Add GRPCRouter

---
+++ baseapp/grpcrouter.go
@@ -0,0 +1,108 @@
+package baseapp
+
+import (
+	gocontext "context"
+	"fmt"
+
+	gogogrpc "github.com/gogo/protobuf/grpc"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/encoding"
+	"google.golang.org/grpc/encoding/proto"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var protoCodec = encoding.GetCodec(proto.Name)
+
+type GRPCRouter struct {
+	routes map[string]GRPCHandler
+}
+
+var _ gogogrpc.Server
+
+func NewGRPCRouter() *GRPCRouter {
+	return &GRPCRouter{
+		routes: map[string]GRPCHandler{},
+	}
+}
+
+type GRPCHandler = func(ctx sdk.Context, req abci.RequestQuery) (abci.ResponseQuery, error)
+
+// Route returns the Querier for a given query route path.
+func (qrt *GRPCRouter) Route(path string) GRPCHandler {
+	handler, found := qrt.routes[path]
+	if !found {
+		return nil
+	}
+	return handler
+}
+
+// RegisterService implements the grpc Server.RegisterService method
+func (qrt *GRPCRouter) RegisterService(sd *grpc.ServiceDesc, handler interface{}) {
+	// adds a top-level querier based on the GRPC service name
+	for _, method := range sd.Methods {
+		fqName := fmt.Sprintf("/%s/%s", sd.ServiceName, method.MethodName)
+		methodHandler := method.Handler
+
+		qrt.routes[fqName] = func(ctx sdk.Context, req abci.RequestQuery) (abci.ResponseQuery, error) {
+			res, err := methodHandler(handler, sdk.WrapSDKContext(ctx), func(i interface{}) error {
+				return protoCodec.Unmarshal(req.Data, i)
+			}, nil)
+			if err != nil {
+				return abci.ResponseQuery{}, err
+			}
+
+			resBytes, err := protoCodec.Marshal(res)
+			if err != nil {
+				return abci.ResponseQuery{}, err
+			}
+
+			return abci.ResponseQuery{
+				Height: req.Height,
+				Value:  resBytes,
+			}, nil
+		}
+	}
+}
+
+// QueryServiceTestHelper provides a helper for making grpc query service
+// rpc calls in unit tests. It implements both the grpc Server and ClientConn
+// interfaces needed to register a query service server and create a query
+// service client.
+type QueryServiceTestHelper struct {
+	*GRPCRouter
+	ctx sdk.Context
+}
+
+// NewQueryServerTestHelper creates a new QueryServiceTestHelper that wraps
+// the provided sdk.Context
+func NewQueryServerTestHelper(ctx sdk.Context) *QueryServiceTestHelper {
+	return &QueryServiceTestHelper{GRPCRouter: NewGRPCRouter(), ctx: ctx}
+}
+
+// Invoke implements the grpc ClientConn.Invoke method
+func (q *QueryServiceTestHelper) Invoke(_ gocontext.Context, method string, args, reply interface{}, _ ...grpc.CallOption) error {
+	querier := q.Route(method)
+	if querier == nil {
+		return fmt.Errorf("handler not found for %s", method)
+	}
+	reqBz, err := protoCodec.Marshal(args)
+	if err != nil {
+		return err
+	}
+	res, err := querier(q.ctx, abci.RequestQuery{Data: reqBz})
+
+	if err != nil {
+		return err
+	}
+	return protoCodec.Unmarshal(res.Value, reply)
+}
+
+// NewStream implements the grpc ClientConn.NewStream method
+func (q *QueryServiceTestHelper) NewStream(gocontext.Context, *grpc.StreamDesc, string, ...grpc.CallOption) (grpc.ClientStream, error) {
+	return nil, fmt.Errorf("not supported")
+}
+
+var _ gogogrpc.Server = &QueryServiceTestHelper{}
+var _ gogogrpc.ClientConn = &QueryServiceTestHelper{}
--- go.mod
+++ go.mod
@@ -32,9 +32,10 @@ require (
 	github.com/tendermint/iavl v0.13.3
 	github.com/tendermint/tendermint v0.33.5
 	github.com/tendermint/tm-db v0.5.1
+	google.golang.org/grpc v1.28.1
 	gopkg.in/yaml.v2 v2.3.0
 )
 
-replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alpha.regen.1
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alpha.regen.4
 
 go 1.14

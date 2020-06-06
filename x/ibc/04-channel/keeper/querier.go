b9b578d6dea504ba371b002fc345491e67b4d5c0
colin axner <25233464+colin-axner@users.noreply.github.com>
2020-06-06 16:25:45 -0700

    x/ibc: querying support for unordered channels (#6294)
    * pause
    
    * add querying for packet commitments and unrelayed acks
    
    * add get all packet commitments test
    
    * add query channels test
    
    * add query connection channels test
    
    * add test for query packet commitments
    
    * fix key path and add unrelayed acks test
    
    * fix part of lint
    
    * add unrelayed packetsends func
    
    * fix godoc
    
    * add nolint directive
    
    * fix lint
    
    Co-authored-by: Federico Kunze <31522760+fedekunze@users.noreply.github.com>

--- x/ibc/04-channel/keeper/querier.go
+++ x/ibc/04-channel/keeper/querier.go
@@ -66,3 +66,79 @@ func QuerierConnectionChannels(ctx sdk.Context, req abci.RequestQuery, k Keeper)
 
 	return res, nil
 }
+
+// QuerierPacketCommitments defines the sdk.Querier to query all packet commitments on a
+// specified channel.
+func QuerierPacketCommitments(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	var params types.QueryPacketCommitmentsParams
+
+	if err := k.cdc.UnmarshalJSON(req.Data, &params); err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+
+	packetCommitments := k.GetAllPacketCommitmentsAtChannel(ctx, params.PortID, params.ChannelID)
+	sequences := make([]uint64, 0, len(packetCommitments))
+
+	for _, pc := range packetCommitments {
+		sequences = append(sequences, pc.Sequence)
+	}
+
+	start, end := client.Paginate(len(sequences), params.Page, params.Limit, 100)
+	if start < 0 || end < 0 {
+		sequences = []uint64{}
+	} else {
+		sequences = sequences[start:end]
+	}
+
+	res, err := codec.MarshalJSONIndent(k.cdc, sequences)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+// QuerierUnrelayedAcknowledgements defines the sdk.Querier to query all unrelayed
+// acknowledgements for a specified channel end.
+func QuerierUnrelayedAcknowledgements(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	return queryUnrelayedPackets(ctx, req, k, true)
+}
+
+// QuerierUnrelayedPacketSends defines the sdk.Querier to query all unrelayed packets for a
+// specified channel end.
+func QuerierUnrelayedPacketSends(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	return queryUnrelayedPackets(ctx, req, k, false)
+}
+
+// helper function to query for unrelayed packets as specified by the isForAcks boolean. If
+// set to true it will return unrelayed acknowledgements otherwise it will return unrelayed
+// packet sends.
+func queryUnrelayedPackets(ctx sdk.Context, req abci.RequestQuery, k Keeper, isForAcks bool) ([]byte, error) {
+	var params types.QueryUnrelayedPacketsParams
+
+	if err := k.cdc.UnmarshalJSON(req.Data, &params); err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+
+	var unrelayedPackets []uint64
+	for _, seq := range params.Sequences {
+		if _, found := k.GetPacketAcknowledgement(ctx, params.PortID, params.ChannelID, seq); found == isForAcks {
+			unrelayedPackets = append(unrelayedPackets, seq)
+		}
+	}
+
+	start, end := client.Paginate(len(unrelayedPackets), params.Page, params.Limit, 100)
+	if start < 0 || end < 0 {
+		unrelayedPackets = []uint64{}
+	} else {
+		unrelayedPackets = unrelayedPackets[start:end]
+	}
+
+	res, err := codec.MarshalJSONIndent(k.cdc, unrelayedPackets)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+
+}

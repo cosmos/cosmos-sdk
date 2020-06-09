e66f3f55601f8d375658ff8dab7819170817f78e
Federico Kunze <federico.kunze94@gmail.com>
2020-06-09 15:59:05 +0200

    fixes

--- x/ibc/02-client/simulation/decoder.go
+++ x/ibc/02-client/simulation/decoder.go
@@ -8,32 +8,29 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/ibc/02-client/exported"
-	"github.com/cosmos/cosmos-sdk/x/ibc/02-client/types"
 	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
 )
 
 // NewDecodeStore returns a decoder function closure that unmarshals the KVPair's
 // Value to the corresponding client type.
-func NewDecodeStore(cdc codec.Marshaler) func(kvA, kvB tmkv.Pair) string {
-	return func(kvA, kvB tmkv.Pair) string {
-		switch {
-		case bytes.Equal(kvA.Key, host.KeyClientStorePrefix) && bytes.HasSuffix(kvA.Key, host.KeyClientState()):
-			var clientStateA, clientStateB exported.ClientState
-			cdc.MustUnmarshalBinaryBare(kvA.Value, &clientStateA)
-			cdc.MustUnmarshalBinaryBare(kvB.Value, &clientStateB)
-			return fmt.Sprintf("ClientState A: %v\nClientState B:%v", clientStateA, clientStateB)
+func NewDecodeStore(cdc *codec.Codec, kvA, kvB tmkv.Pair) (string, bool) {
+	switch {
+	case bytes.Equal(kvA.Key, host.KeyClientStorePrefix) && bytes.HasSuffix(kvA.Key, host.KeyClientState()):
+		var clientStateA, clientStateB exported.ClientState
+		cdc.MustUnmarshalBinaryBare(kvA.Value, &clientStateA)
+		cdc.MustUnmarshalBinaryBare(kvB.Value, &clientStateB)
+		return fmt.Sprintf("ClientState A: %v\nClientState B:%v", clientStateA, clientStateB), true
 
-		case bytes.Equal(kvA.Key, host.KeyClientStorePrefix) && bytes.HasSuffix(kvA.Key, host.KeyClientType()):
-			return fmt.Sprintf("Client type A: %v\nClient type B:%v", string(kvA.Value), string(kvB.Value))
+	case bytes.Equal(kvA.Key, host.KeyClientStorePrefix) && bytes.HasSuffix(kvA.Key, host.KeyClientType()):
+		return fmt.Sprintf("Client type A: %v\nClient type B:%v", string(kvA.Value), string(kvB.Value)), true
 
-		case bytes.Equal(kvA.Key, host.KeyClientStorePrefix) && bytes.Contains(kvA.Key, []byte("consensusState")):
-			var consensusStateA, consensusStateB exported.ConsensusState
-			cdc.MustUnmarshalBinaryBare(kvA.Value, &consensusStateA)
-			cdc.MustUnmarshalBinaryBare(kvB.Value, &consensusStateB)
-			return fmt.Sprintf("ConsensusState A: %v\nConsensusState B:%v", consensusStateA, consensusStateB)
+	case bytes.Equal(kvA.Key, host.KeyClientStorePrefix) && bytes.Contains(kvA.Key, []byte("consensusState")):
+		var consensusStateA, consensusStateB exported.ConsensusState
+		cdc.MustUnmarshalBinaryBare(kvA.Value, &consensusStateA)
+		cdc.MustUnmarshalBinaryBare(kvB.Value, &consensusStateB)
+		return fmt.Sprintf("ConsensusState A: %v\nConsensusState B:%v", consensusStateA, consensusStateB), true
 
-		default:
-			panic(fmt.Sprintf("invalid %s %s key prefix: %s", host.ModuleName, types.SubModuleName, string(kvA.Key)))
-		}
+	default:
+		return "", false
 	}
 }
--- x/ibc/04-channel/simulation/decoder.go
+++ x/ibc/04-channel/simulation/decoder.go
@@ -8,44 +8,42 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/04-channel/types"
+	"github.com/cosmos/cosmos-sdk/x/ibc/04-channel/types"
 	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
 )
 
 // NewDecodeStore returns a decoder function closure that unmarshals the KVPair's
 // Value to the corresponding channel type.
-func NewDecodeStore(cdc codec.Marshaler) func(kvA, kvB tmkv.Pair) string {
-	return func(kvA, kvB tmkv.Pair) string {
-		switch {
-		case bytes.HasPrefix(kvA.Key, []byte(host.KeyChannelPrefix)):
-			var channelA, channelB types.Channel
-			cdc.MustUnmarshalBinaryBare(kvA.Value, &channelA)
-			cdc.MustUnmarshalBinaryBare(kvB.Value, &channelB)
-			return fmt.Sprintf("%v\n%v", channelA, channelB)
+func NewDecodeStore(cdc codec.Marshaler, kvA, kvB tmkv.Pair) (string, bool) {
+	switch {
+	case bytes.HasPrefix(kvA.Key, []byte(host.KeyChannelPrefix)):
+		var channelA, channelB types.Channel
+		cdc.MustUnmarshalBinaryBare(kvA.Value, &channelA)
+		cdc.MustUnmarshalBinaryBare(kvB.Value, &channelB)
+		return fmt.Sprintf("%v\n%v", channelA, channelB), true
 
-		case bytes.HasPrefix(kvA.Key, []byte(host.KeyNextSeqSendPrefix)):
-			seqA := sdk.BigEndianToUint64(kvA.Value)
-			seqB := sdk.BigEndianToUint64(kvB.Value)
-			return fmt.Sprintf("NextSeqSend A: %d\nNextSeqSend B: %d\n", seqA, seqB)
+	case bytes.HasPrefix(kvA.Key, []byte(host.KeyNextSeqSendPrefix)):
+		seqA := sdk.BigEndianToUint64(kvA.Value)
+		seqB := sdk.BigEndianToUint64(kvB.Value)
+		return fmt.Sprintf("NextSeqSend A: %d\nNextSeqSend B: %d\n", seqA, seqB), true
 
-		case bytes.HasPrefix(kvA.Key, []byte(host.KeyNextSeqRecvPrefix)):
-			seqA := sdk.BigEndianToUint64(kvA.Value)
-			seqB := sdk.BigEndianToUint64(kvB.Value)
-			return fmt.Sprintf("NextSeqRecv A: %d\nNextSeqRecv B: %d\n", seqA, seqB)
+	case bytes.HasPrefix(kvA.Key, []byte(host.KeyNextSeqRecvPrefix)):
+		seqA := sdk.BigEndianToUint64(kvA.Value)
+		seqB := sdk.BigEndianToUint64(kvB.Value)
+		return fmt.Sprintf("NextSeqRecv A: %d\nNextSeqRecv B: %d\n", seqA, seqB), true
 
-		case bytes.HasPrefix(kvA.Key, []byte(host.KeyNextSeqAckPrefix)):
-			seqA := sdk.BigEndianToUint64(kvA.Value)
-			seqB := sdk.BigEndianToUint64(kvB.Value)
-			return fmt.Sprintf("NextSeqAck A: %d\nNextSeqAck B: %d\n", seqA, seqB)
+	case bytes.HasPrefix(kvA.Key, []byte(host.KeyNextSeqAckPrefix)):
+		seqA := sdk.BigEndianToUint64(kvA.Value)
+		seqB := sdk.BigEndianToUint64(kvB.Value)
+		return fmt.Sprintf("NextSeqAck A: %d\nNextSeqAck B: %d\n", seqA, seqB), true
 
-		case bytes.HasPrefix(kvA.Key, []byte(host.KeyPacketCommitmentPrefix)):
-			return fmt.Sprintf("CommitmentHash A: %X\nCommitmentHash B: %X", kvA.Value, kvB.Value)
+	case bytes.HasPrefix(kvA.Key, []byte(host.KeyPacketCommitmentPrefix)):
+		return fmt.Sprintf("CommitmentHash A: %X\nCommitmentHash B: %X", kvA.Value, kvB.Value), true
 
-		case bytes.HasPrefix(kvA.Key, []byte(host.KeyPacketAckPrefix)):
-			return fmt.Sprintf("AckHash A: %X\nAckHash B: %X", kvA.Value, kvB.Value)
+	case bytes.HasPrefix(kvA.Key, []byte(host.KeyPacketAckPrefix)):
+		return fmt.Sprintf("AckHash A: %X\nAckHash B: %X", kvA.Value, kvB.Value), true
 
-		default:
-			panic(fmt.Sprintf("invalid %s %s key prefix: %s", host.ModuleName, types.SubModuleName, string(kvA.Key)))
-		}
+	default:
+		return "", false
 	}
 }

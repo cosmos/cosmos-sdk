bdcd4ec14b34640c9cbba8ea86adaaba1fccd2e5
Aaron Craelius <aaronc@users.noreply.github.com>
2020-06-09 15:17:01 -0400

    godocs

--- types/tx/signing/signature_data.go
+++ types/tx/signing/signature_data.go
@@ -4,17 +4,29 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/types"
 )
 
+// SignatureData represents either a *SingleSignatureData or *MultiSignatureData.
+// It is a convenience type that is easier to use in buiness logic than the encoded
+// protobuf ModeInfo's and raw signatures.
 type SignatureData interface {
 	isSignatureData()
 }
 
+// SingleSignatureData represents the signature and SignMode of a single (non-multisig) signer
 type SingleSignatureData struct {
-	SignMode  SignMode
+	// SignMode represents the SignMode of the signature
+	SignMode SignMode
+
+	// SignMode represents the SignMode of the signature
 	Signature []byte
 }
 
+// SingleSignatureData represents the nested SignatureData of a multisig signature
 type MultiSignatureData struct {
-	BitArray   *types.CompactBitArray
+	// BitArray is a compact way of indicating which signers from the multisig key
+	// have signed
+	BitArray *types.CompactBitArray
+
+	// Signatures is the nested SignatureData's for each signer
 	Signatures []SignatureData
 }
 

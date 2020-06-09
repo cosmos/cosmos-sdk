68f6185a36d11d49656671b0c065b02c7439b83f
Aaron Craelius <aaronc@users.noreply.github.com>
2020-06-09 13:09:55 -0400

    revert changes

--- client/tx_generator.go
+++ client/tx_generator.go
@@ -1,8 +1,9 @@
 package client
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	types "github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/tendermint/tendermint/crypto"
+
+	"github.com/cosmos/cosmos-sdk/types"
 )
 
 type (
@@ -10,13 +11,22 @@ type (
 	// application-defined concrete transaction type. The type returned must
 	// implement TxBuilder.
 	TxGenerator interface {
-		NewTxBuilder() TxBuilder
-		// WrapTxBuilder wraps an existing tx in a TxBuilder or returns an error
-		WrapTxBuilder(tx sdk.Tx) (TxBuilder, error)
-		TxEncoder() sdk.TxEncoder
-		TxDecoder() sdk.TxDecoder
-		TxJSONEncoder() sdk.TxEncoder
-		TxJSONDecoder() sdk.TxDecoder
+		NewTx() TxBuilder
+		NewFee() Fee
+		NewSignature() Signature
+		MarshalTx(tx types.Tx) ([]byte, error)
+	}
+
+	Fee interface {
+		types.Fee
+		SetGas(uint64)
+		SetAmount(types.Coins)
+	}
+
+	Signature interface {
+		types.Signature
+		SetPubKey(crypto.PubKey) error
+		SetSignature([]byte)
 	}
 
 	// TxBuilder defines an interface which an application-defined concrete transaction
@@ -24,12 +34,18 @@ type (
 	// signatures, and provide canonical bytes to sign over. The transaction must
 	// also know how to encode itself.
 	TxBuilder interface {
-		GetTx() types.SigTx
+		GetTx() types.Tx
 
-		SetMsgs(msgs ...sdk.Msg) error
-		SetSignatures(signatures ...types.SignatureV2) error
-		SetMemo(memo string)
-		SetFee(amount sdk.Coins)
-		SetGasLimit(limit uint64)
+		SetMsgs(...types.Msg) error
+		GetSignatures() []types.Signature
+		SetSignatures(...Signature) error
+		GetFee() types.Fee
+		SetFee(Fee) error
+		GetMemo() string
+		SetMemo(string)
+
+		// CanonicalSignBytes returns the canonical sign bytes to sign over, given a
+		// chain ID, along with an account and sequence number.
+		CanonicalSignBytes(cid string, num, seq uint64) ([]byte, error)
 	}
 )

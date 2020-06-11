97ece9849e8c5ba6768988ddcad8b38f1d8eeee5
atheesh <atheesh@vitwit.com>
2020-06-11 19:26:12 +0530

    WIP: added test for direct mode handler

--- types/tx/tx_wrapper_test.go
+++ types/tx/tx_wrapper_test.go
@@ -1,6 +1,13 @@
 package tx
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/std"
+	types "github.com/cosmos/cosmos-sdk/types/tx"
+)
 
 func TestTxWrapper(t *testing.T) {
 	// TODO:
@@ -10,4 +17,18 @@ func TestTxWrapper(t *testing.T) {
 	//   - verify that calling the SetBody results in the correct GetBodyBytes
 	//   - verify that calling the SetAuthInfo results in the correct GetAuthInfoBytes and GetPubKeys
 	//   - verify no nil panics
+
+	tx := NewTxWrapper(codec.New(), std.DefaultPublicKeyCodec{})
+
+	txBody := TxBody{}
+	tx.SetBody(&txBody)
+	memo := "memo"
+	protoTx, ok := tx.(types.ProtoTx)
+
+	if !ok {
+		return nil, fmt.Errorf("can only get direct sign bytes for a ProtoTx, got %T", tx)
+	}
+
+	bodyBz := protoTx.GetBodyBytes()
+	// facing import cycle error
 }
--- x/auth/signing/direct/direct_test.go
+++ x/auth/signing/direct/direct_test.go
@@ -1,8 +1,71 @@
 package direct
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/std"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	types "github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto"
+)
 
 func TestDirectModeHandler(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, abci.Header{})
+
+	var directModeHandler DirectModeHandler
+
+	require.Equal(t, directModeHandler.DefaultMode(), types.SignMode_SIGN_MODE_DIRECT)
+	require.Len(t, directModeHandler.Modes(), 1)
+
+	signingData := signing.SignerData{
+		ChainID:         "test-chain",
+		AccountNumber:   1,
+		AccountSequence: 1,
+	}
+
+	priv, _, addr := authtypes.KeyTestPubAddr()
+
+	msgs := []sdk.Msg{authtypes.NewTestMsg(addr)}
+	fee := authtypes.NewTestStdFee()
+	privs, accNums, seqs := []crypto.PrivKey{priv}, []uint64{0}, []uint64{0}
+	_ = authtypes.NewTestTx(ctx, msgs, privs, accNums, seqs, fee)
+	// signBytes, err := directModeHandler.GetSignBytes(types.SignMode_SIGN_MODE_DIRECT, signingData, tx)
+
+	// fmt.Println("signBytes, err", string(signBytes), err)
+
+	txWrapper := types.NewTxWrapper(&codec.ProtoCodec{}, std.DefaultPublicKeyCodec{})
+
+	// setbody throwing error, since it is using any type
+	txWrapper.SetBody(&types.TxBody{
+		Messages: msgs,
+	})
+
+	// SetAuthInfo throwing error, since it is using any type
+	// txWrapper.SetAuthInfo(&types.AuthInfo{
+	// 	SignerInfos: &[]types.SignerInfo{
+	// 		PublicKey: priv.PubKey(),
+	// 		ModeInfo: &types.ModeInfo{
+	// 			Sum: &types.ModeInfo_Single_{
+	// 				Single: &types.ModeInfo_Single{Mode: types.SignMode_SIGN_MODE_DIRECT},
+	// 			},
+	// 		},
+	// 	},
+	// 	Fee: fee,
+	// })
+
+	signBytes, err := directModeHandler.GetSignBytes(types.SignMode_SIGN_MODE_DIRECT, signingData, txWrapper)
+
+	fmt.Println("signBytes, err", string(signBytes), err)
+
+	require.NoError(t, err)
 	// TODO:
 	// - verify DefaultMode and Modes
 	// - verify GetSignBytes using a test transaction vs manually generating sign bytes by marshaling SignDoc

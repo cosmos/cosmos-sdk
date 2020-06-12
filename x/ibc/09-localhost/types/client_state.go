35d55cb29bf155802ff84b6a4d46806231825365
Jonathan Gimeno <jgimeno@gmail.com>
2020-06-12 16:09:19 +0200

    run make format on project

--- x/ibc/09-localhost/types/client_state.go
+++ x/ibc/09-localhost/types/client_state.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	ics23 "github.com/confio/ics23/go"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"

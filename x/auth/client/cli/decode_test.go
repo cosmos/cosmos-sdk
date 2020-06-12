16d1118696533c5039a6bac08ef4d15a4710c306
atheesh <atheesh@vitwit.com>
2020-06-12 19:20:15 +0530

    added txEncoder at bytes

--- x/auth/client/cli/decode_test.go
+++ x/auth/client/cli/decode_test.go
@@ -1,13 +1,14 @@
 package cli
 
 import (
-	"encoding/base64"
-	"encoding/hex"
-	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	"github.com/cosmos/cosmos-sdk/tests"
+	authclient "github.com/cosmos/cosmos-sdk/x/auth/client"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 )
@@ -20,16 +21,21 @@ func TestGetCommandDecode(t *testing.T) {
 
 	viper.Set("hex", false)
 
+	testDir, cleanFunc := tests.NewTestCaseDir(t)
+	t.Cleanup(cleanFunc)
+
 	txContents := []byte("{\"type\":\"cosmos-sdk/StdTx\",\"value\":{\"msg\":[{\"type\":\"cosmos-sdk/MsgSend\",\"value\":{\"from_address\":\"cosmos1cxlt8kznps92fwu3j6npahx4mjfutydyene2qw\",\"to_address\":\"cosmos1wc8mpr8m3sy3ap3j7fsgqfzx36um05pystems4\",\"amount\":[{\"denom\":\"stake\",\"amount\":\"10000\"}]}}],\"fee\":{\"amount\":[],\"gas\":\"200000\"},\"signatures\":null,\"memo\":\"\"}}")
-	hexStr := hex.EncodeToString(txContents)
+	txFileName := filepath.Join(testDir, "tx.json")
 
-	txBytesBase64 := base64.StdEncoding.EncodeToString(txContents)
+	err := ioutil.WriteFile(txFileName, txContents, 0644)
+	require.NoError(t, err)
 
-	fmt.Println("err", hexStr)
-	fmt.Println("hex", string(txBytesBase64))
+	stdTx, err := authclient.ReadTxFromFile(clientCtx, txFileName)
+	require.NoError(t, err)
 
-	err := cmd.RunE(cmd, []string{hexStr})
-	fmt.Println("err", err)
+	txBytes, err := clientCtx.TxGenerator.TxEncoder()(stdTx)
+	require.NoError(t, err)
+	err = cmd.RunE(cmd, []string{string(txBytes)})
 
 	require.NoError(t, err)
 }

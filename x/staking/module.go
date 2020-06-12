390148062912dd74d4b127607c75373fe653d700
DauTT <dautruongtan@gmail.com>
2020-06-12 14:01:22 +0200

    Add missing end curly parenthesis

--- x/staking/module.go
+++ x/staking/module.go
@@ -137,7 +137,8 @@ func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
 // Route returns the message routing key for the staking module.
 func (am AppModule) Route() sdk.Route {
 	return sdk.NewRoute(types.RouterKey, NewHandler(am.keeper))
-  
+}
+
 // QuerierRoute returns the staking module's querier route name.
 func (AppModule) QuerierRoute() string {
 	return types.QuerierRoute

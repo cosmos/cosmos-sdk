b878734077a692357094f2619ba0b2c3db76f696
atheeshp <59333759+atheeshp@users.noreply.github.com>
2020-06-16 00:58:18 +0530

    6391 - lint issues and code coverage (#6414)
    * fixed lint issue of "txEncodeRespStr"
    
    * added tests for encode.go
    
    * WIP: added tests for decode.go
    
    * added txEncoder at bytes
    
    * updated decode test
    
    * updated txBytes to use TxEncoder in decoder test
    
    * added a require after TxEncoder
    
    * removed file save
    
    * debug decode command
    
    * fixed decode tests
    
    * added decode cli
    
    * updated encode and decode in a single file
    
    * separated decode test from encode test
    
    * review changes
    
    * removed register interface
    
    * review change

---
+++ x/auth/client/cli/encode_test.go
@@ -0,0 +1,66 @@
+package cli
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	"github.com/cosmos/cosmos-sdk/tests"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetCommandEncode(t *testing.T) {
+	clientCtx := client.Context{}
+	clientCtx = clientCtx.WithTxGenerator(simappparams.MakeEncodingConfig().TxGenerator)
+
+	cmd := GetEncodeCommand(clientCtx)
+	encodingConfig := simappparams.MakeEncodingConfig()
+	authtypes.RegisterCodec(encodingConfig.Amino)
+	sdk.RegisterCodec(encodingConfig.Amino)
+
+	txGen := encodingConfig.TxGenerator
+
+	// Build a test transaction
+	fee := authtypes.NewStdFee(50000, sdk.Coins{sdk.NewInt64Coin("atom", 150)})
+	stdTx := authtypes.NewStdTx([]sdk.Msg{}, fee, []authtypes.StdSignature{}, "foomemo")
+	JSONEncoded, err := txGen.TxJSONEncoder()(stdTx)
+	require.NoError(t, err)
+
+	txFile, cleanup := tests.WriteToNewTempFile(t, string(JSONEncoded))
+	txFileName := txFile.Name()
+	t.Cleanup(cleanup)
+
+	err = cmd.RunE(cmd, []string{txFileName})
+	require.NoError(t, err)
+}
+
+func TestGetCommandDecode(t *testing.T) {
+	clientCtx := client.Context{}
+	clientCtx = clientCtx.WithTxGenerator(simappparams.MakeEncodingConfig().TxGenerator)
+
+	cmd := GetDecodeCommand(clientCtx)
+
+	encodingConfig := simappparams.MakeEncodingConfig()
+	sdk.RegisterCodec(encodingConfig.Amino)
+
+	txGen := encodingConfig.TxGenerator
+	clientCtx = clientCtx.WithTxGenerator(txGen)
+
+	// Build a test transaction
+	fee := authtypes.NewStdFee(50000, sdk.Coins{sdk.NewInt64Coin("atom", 150)})
+	stdTx := authtypes.NewStdTx([]sdk.Msg{}, fee, []authtypes.StdSignature{}, "foomemo")
+
+	// Encode transaction
+	txBytes, err := clientCtx.TxGenerator.TxEncoder()(stdTx)
+	require.NoError(t, err)
+
+	// Convert the transaction into base64 encoded string
+	base64Encoded := base64.StdEncoding.EncodeToString(txBytes)
+
+	// Execute the command
+	err = runDecodeTxString(clientCtx)(cmd, []string{base64Encoded})
+	require.NoError(t, err)
+}

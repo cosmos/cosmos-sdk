f7fe1b3949b5c9c80eb3a34b3a001e855c91c7ee
anilCSE <anil@vitwit.com>
2020-06-16 22:31:44 +0530

    Add Sign tests

--- client/tx/tx_test.go
+++ client/tx/tx_test.go
@@ -4,6 +4,10 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/tests"
+
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 
 	"github.com/stretchr/testify/require"
@@ -108,4 +112,38 @@ func TestBuildUnsignedTx(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, tx)
 	require.Empty(t, tx.GetTx().(ante.SigVerifiableTx).GetSignatures())
+}
+
+func TestSign(t *testing.T) {
+	dir, clean := tests.NewTestCaseDir(t)
+	t.Cleanup(clean)
+
+	path := hd.CreateHDPath(118, 0, 0).String()
+	kr, err := keyring.New(t.Name(), "test", dir, nil)
+	require.NoError(t, err)
+
+	var from = "test_sign"
+
+	_, seed, err := kr.NewMnemonic(from, keyring.English, path, hd.Secp256k1)
+	require.NoError(t, err)
+	require.NoError(t, kr.Delete(from))
+
+	info, err := kr.NewAccount(from, seed, "", path, hd.Secp256k1)
+	require.NoError(t, err)
+
+	txf := tx.Factory{}.
+		WithKeybase(kr).
+		WithTxGenerator(NewTestTxGenerator()).
+		WithAccountNumber(50).
+		WithSequence(23).
+		WithFees("50stake").
+		WithMemo("memo").
+		WithChainID("test-chain")
+
+	msg := banktypes.NewMsgSend(info.GetAddress(), sdk.AccAddress("to"), nil)
+	txn, err := tx.BuildUnsignedTx(txf, msg)
+	require.NoError(t, err)
+
+	err = tx.Sign(txf, from, txn)
+	require.NoError(t, err)
 }

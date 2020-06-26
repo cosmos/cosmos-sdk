836ab64bf0c7ed50dc511f9b58f72ec9b8872963
sahith-narahari <sahithnarahari@gmail.com>
2020-06-26 11:39:07 +0530

    Add reference issue

--- x/auth/ante/ante_test.go
+++ x/auth/ante/ante_test.go
@@ -600,7 +600,8 @@ func generatePubKeysAndSignatures(n int, msg []byte, _ bool) (pubkeys []crypto.P
 		var privkey crypto.PrivKey
 		privkey = secp256k1.GenPrivKey()
 
-		// TODO: also generate ed25519 keys as below when ed25519 keys are actually supported,
+		// TODO: also generate ed25519 keys as below when ed25519 keys are
+		//  actually supported, https://github.com/cosmos/cosmos-sdk/issues/4789
 		// for now this fails:
 		//if rand.Int63()%2 == 0 {
 		//	privkey = ed25519.GenPrivKey()

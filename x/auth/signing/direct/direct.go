a74165bd03dde13da9e336a5198a0dcd9cfed7b9
Aaron Craelius <aaronc@users.noreply.github.com>
2020-06-30 10:59:55 -0400

    cleanup

--- x/auth/signing/direct/direct.go
+++ x/auth/signing/direct/direct.go
@@ -10,23 +10,32 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/signing"
 )
 
+// ProtoTx defines an interface which protobuf transactions must implement for
+// signature verification via SignModeDirect
 type ProtoTx interface {
+	// GetBodyBytes returns the raw serialized bytes for TxBody
 	GetBodyBytes() []byte
+
+	// GetBodyBytes returns the raw serialized bytes for AuthInfo
 	GetAuthInfoBytes() []byte
 }
 
+// ModeHandler defines the SIGN_MODE_DIRECT SignModeHandler
 type ModeHandler struct{}
 
+var _ signing.SignModeHandler = ModeHandler{}
+
+// DefaultMode implements SignModeHandler.DefaultMode
 func (h ModeHandler) DefaultMode() signingtypes.SignMode {
 	return signingtypes.SignMode_SIGN_MODE_DIRECT
 }
 
-var _ signing.SignModeHandler = ModeHandler{}
-
+// Modes implements SignModeHandler.Modes
 func (ModeHandler) Modes() []signingtypes.SignMode {
 	return []signingtypes.SignMode{signingtypes.SignMode_SIGN_MODE_DIRECT}
 }
 
+// GetSignBytes implements SignModeHandler.GetSignBytes
 func (ModeHandler) GetSignBytes(mode signingtypes.SignMode, data signing.SignerData, tx sdk.Tx) ([]byte, error) {
 	if mode != signingtypes.SignMode_SIGN_MODE_DIRECT {
 		return nil, fmt.Errorf("expected %s, got %s", signingtypes.SignMode_SIGN_MODE_DIRECT, mode)
@@ -43,10 +52,12 @@ func (ModeHandler) GetSignBytes(mode signingtypes.SignMode, data signing.SignerD
 	return SignBytes(bodyBz, authInfoBz, data.ChainID, data.AccountNumber, data.AccountSequence)
 }
 
-func SignBytes(bodyBz, authInfoBz []byte, chainID string, accnum, sequence uint64) ([]byte, error) {
+// SignBytes returns the SIGN_MODE_DIRECT sign bytes for the provided TxBody bytes, AuthInfo bytes, chain ID,
+// account number and sequence.
+func SignBytes(bodyBytes, authInfoBytes []byte, chainID string, accnum, sequence uint64) ([]byte, error) {
 	signDoc := types.SignDoc{
-		BodyBytes:       bodyBz,
-		AuthInfoBytes:   authInfoBz,
+		BodyBytes:       bodyBytes,
+		AuthInfoBytes:   authInfoBytes,
 		ChainId:         chainID,
 		AccountNumber:   accnum,
 		AccountSequence: sequence,

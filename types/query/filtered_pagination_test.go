33fdcc980b9e272f6f01c91256fe74d53a56c7e9
sahith-narahari <sahithnarahari@gmail.com>
2020-06-30 17:35:23 +0530

    Add example for filtered pagination

--- types/query/filtered_pagination_test.go
+++ types/query/filtered_pagination_test.go
@@ -23,11 +23,10 @@ func TestFilteredPaginations(t *testing.T) {
 		balances = append(balances, sdk.NewInt64Coin(denom, 100))
 	}
 
-	balances = append(balances, sdk.NewInt64Coin("test1", 250))
-	balances = append(balances, sdk.NewInt64Coin("test2", 250))
-	balances = append(balances, sdk.NewInt64Coin("test3", 250))
-	balances = append(balances, sdk.NewInt64Coin("test4", 250))
-
+	for i := 0; i < 5; i++ {
+		denom := fmt.Sprintf("test%ddenom", i)
+		balances = append(balances, sdk.NewInt64Coin(denom, 250))
+	}
 	addr1 := sdk.AccAddress([]byte("addr1"))
 	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
 	app.AccountKeeper.SetAccount(ctx, acc1)
@@ -57,8 +56,61 @@ func TestFilteredPaginations(t *testing.T) {
 
 		return false, nil
 	})
-	t.Log(balResult)
 	require.NoError(t, err)
 	require.NotNil(t, res)
 	require.Equal(t, 4, len(balResult))
+}
+
+func ExampleFilteredPaginate() {
+	app, ctx, appCodec := setupTest()
+
+	var balances sdk.Coins
+
+	for i := 0; i < numBalances; i++ {
+		denom := fmt.Sprintf("foo%ddenom", i)
+		balances = append(balances, sdk.NewInt64Coin(denom, 100))
+	}
+
+	for i := 0; i < 5; i++ {
+		denom := fmt.Sprintf("test%ddenom", i)
+		balances = append(balances, sdk.NewInt64Coin(denom, 250))
+	}
+	addr1 := sdk.AccAddress([]byte("addr1"))
+	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
+	app.AccountKeeper.SetAccount(ctx, acc1)
+	err := app.BankKeeper.SetBalances(ctx, addr1, balances)
+	if err != nil { // should return no error
+		fmt.Println(err)
+	}
+
+	pageReq := &query.PageRequest{Key: nil, Limit: 1, CountTotal: true}
+	store := ctx.KVStore(app.GetKey(authtypes.StoreKey))
+	balancesStore := prefix.NewStore(store, types.BalancesPrefix)
+	accountStore := prefix.NewStore(balancesStore, addr1.Bytes())
+
+	var balResult sdk.Coins
+	res, err := query.FilteredPaginate(accountStore, pageReq, func(key []byte, value []byte, accumulate bool) (bool, error) {
+		var bal sdk.Coin
+		err := appCodec.UnmarshalBinaryBare(value, &bal)
+		if err != nil {
+			return false, err
+		}
+
+		if bal.Amount.Int64() > int64(100) {
+			if accumulate {
+				balResult = append(balResult, bal)
+			}
+
+			return true, nil
+		}
+
+		return false, nil
+	})
+
+	if err != nil { // should return no error
+		fmt.Println(err)
+	}
+	fmt.Println(&types.QueryAllBalancesResponse{Balances: balResult, Res: res})
+	// Output:
+	// balances:<denom:"test1denom" amount:"250" > res:<next_key:"test0denom" total:5 >
 }

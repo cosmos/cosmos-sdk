99ea4b759a1af927ae90064907cfa0e78abf9023
Aaron Craelius <aaron@regen.network>
2020-07-01 11:26:29 -0400

    Fix testdata proto-gen (#6568)

--- client/context_test.go
+++ client/context_test.go
@@ -109,7 +109,7 @@ func TestContext_PrintOutput(t *testing.T) {
 	err = ctx.PrintOutput(hasAnimal)
 	require.NoError(t, err)
 	require.Equal(t,
-		`{"animal":{"@type":"/cosmos_sdk.codec.v1.Dog","size":"big","name":"Spot"},"x":"10"}
+		`{"animal":{"@type":"/testdata.Dog","size":"big","name":"Spot"},"x":"10"}
 `, string(buf.Bytes()))
 
 	// yaml
@@ -120,7 +120,7 @@ func TestContext_PrintOutput(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t,
 		`animal:
-  '@type': /cosmos_sdk.codec.v1.Dog
+  '@type': /testdata.Dog
   name: Spot
   size: big
 x: "10"

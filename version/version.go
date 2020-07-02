052db8c72974e3f049ce5baa29f280f2a0fe435a
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2020-07-02 09:03:46 -0400

    Apply suggestions from code review
    Co-authored-by: Federico Kunze <31522760+fedekunze@users.noreply.github.com>

--- version/version.go
+++ version/version.go
@@ -10,8 +10,7 @@
 // can be passed as build flags as shown in the following example:
 //
 //  go build -X github.com/cosmos/cosmos-sdk/version.Name=gaia \
-//   -X github.com/cosmos/cosmos-sdk/version.ServerName=gaiad \
-//   -X github.com/cosmos/cosmos-sdk/version.AppName=gaiacli \
+//   -X github.com/cosmos/cosmos-sdk/version.AppName=gaiad \
 //   -X github.com/cosmos/cosmos-sdk/version.Version=1.0 \
 //   -X github.com/cosmos/cosmos-sdk/version.Commit=f0f7b7dab7e36c20b757cebce0e8f4fc5b95de60 \
 //   -X "github.com/cosmos/cosmos-sdk/version.BuildTags=linux darwin amd64"

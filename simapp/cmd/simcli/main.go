3fbd8a236d8989d28b3a3b317385712514f10491
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2020-07-02 08:56:41 -0400

    Update simapp/cmd/simcli/main.go
    Co-authored-by: Federico Kunze <31522760+fedekunze@users.noreply.github.com>

--- simapp/cmd/simcli/main.go
+++ simapp/cmd/simcli/main.go
@@ -111,7 +111,7 @@ func txCmd() *cobra.Command {
 		Short:                      "Transactions subcommands",
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
-		PreRunE: func(cmd *cobra.Command, args []string) error {
+		PreRunE: func(cmd *cobra.Command, _ []string) error {
 			return client.SetCmdClientContextHandler(initClientCtx, cmd)
 		},
 		RunE: client.ValidateCmd,

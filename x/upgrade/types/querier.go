4459201591a50e5f07200e4c3a8d754bfe2c42e7
Amaury Martiny <amaury.martiny@protonmail.com>
2020-07-04 19:30:17 +0200

    x/upgrade gRPC query service (#6590)
    * Add .proto and make proto-gen
    
    * Correct proto types
    
    * Update pseudo tests
    
    * Update tests
    
    * line break
    
    * Fix tests after merge
    
    * Update proto/cosmos/upgrade/query.proto
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>
    
    * Update proto/cosmos/upgrade/query.proto
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>
    
    * Update proto/cosmos/upgrade/query.proto
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>
    
    * Update proto/cosmos/upgrade/query.proto
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>

--- x/upgrade/types/querier.go
+++ x/upgrade/types/querier.go
@@ -6,6 +6,16 @@ const (
 	QueryApplied = "applied"
 )
 
+// NewQueryCurrentPlanRequest creates a new instance of QueryCurrentPlanRequest.
+func NewQueryCurrentPlanRequest() *QueryCurrentPlanRequest {
+	return &QueryCurrentPlanRequest{}
+}
+
+// NewQueryAppliedPlanRequest creates a new instance of QueryAppliedPlanRequest.
+func NewQueryAppliedPlanRequest(name string) *QueryAppliedPlanRequest {
+	return &QueryAppliedPlanRequest{Name: name}
+}
+
 // QueryAppliedParams is passed as data with QueryApplied
 type QueryAppliedParams struct {
 	Name string

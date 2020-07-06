f35e3b2c02a3d4566dcd99fa1df045c8c9cf5892
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-07-06 21:35:35 +0200

    x/ibc: gRPC query service (#6466)
    * x/ibc: gRPC query service
    
    * fixes
    
    * connection updates
    
    * register channel query service
    
    * update clients
    
    * change proofs to bytes
    
    * implement additional channel grpc queries
    
    * unrelayed packets query
    
    * query.proto files
    
    * move next sequence recv query to channel client
    
    * update REST
    
    * wip test
    
    * add missing cli queries
    
    * install the right tools
    
    * fixes
    
    * build
    
    * lint
    
    * use gRPC when query proof is not requested
    
    * connection gRPC tests
    
    * IBC query server interface
    
    * more gRPC channel tests
    
    * pagination tests
    
    * connection use query server
    
    * connection pagination tests
    
    * channel pagination tests
    
    * typo
    
    * remove buf
    
    * Update x/ibc/03-connection/keeper/grpc_query_test.go
    
    Co-authored-by: colin axner <25233464+colin-axner@users.noreply.github.com>
    
    * address comments from review
    
    * fix tests
    
    * unrelayed packet sequences flag
    
    * remove ClientsConnections query
    
    * fix
    
    Co-authored-by: colin axner <25233464+colin-axner@users.noreply.github.com>

--- x/ibc-transfer/client/cli/cli.go
+++ x/ibc-transfer/client/cli/cli.go
@@ -7,23 +7,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 )
 
-// GetQueryCmd returns the query commands for IBC fungible token transfer
-func GetQueryCmd(clientCtx client.Context) *cobra.Command {
-	ics20TransferQueryCmd := &cobra.Command{
-		Use:                        "ibc-transfer",
-		Short:                      "IBC fungible token transfer query subcommands",
-		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
-	}
-
-	ics20TransferQueryCmd.AddCommand(flags.GetCommands(
-		GetCmdQueryNextSequence(clientCtx),
-	)...)
-
-	return ics20TransferQueryCmd
-}
-
 // NewTxCmd returns the transaction commands for IBC fungible token transfer
 func NewTxCmd(clientCtx client.Context) *cobra.Command {
 	ics20TransferTxCmd := &cobra.Command{
--- x/ibc/04-channel/client/cli/query.go
+++ x/ibc/04-channel/client/cli/query.go
@@ -1,17 +1,60 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/cosmos/cosmos-sdk/x/ibc/04-channel/client/utils"
 	"github.com/cosmos/cosmos-sdk/x/ibc/04-channel/types"
 	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
 )
+
+const flagSequences = "sequences"
+
+// GetCmdQueryChannels defines the command to query all the channels ends
+// that this chain mantains.
+func GetCmdQueryChannels(clientCtx client.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "channels",
+		Short:   "Query all channels",
+		Long:    "Query all channels from a chain",
+		Example: fmt.Sprintf("%s query %s %s channels", version.ClientName, host.ModuleName, types.SubModuleName),
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx = clientCtx.Init()
+			queryClient := types.NewQueryClient(clientCtx)
+
+			offset, _ := cmd.Flags().GetInt(flags.FlagPage)
+			limit, _ := cmd.Flags().GetInt(flags.FlagLimit)
+
+			req := &types.QueryChannelsRequest{
+				Req: &query.PageRequest{
+					Offset: uint64(offset),
+					Limit:  uint64(limit),
+				},
+			}
+
+			res, err := queryClient.Channels(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			clientCtx = clientCtx.WithHeight(res.Height)
+			return clientCtx.PrintOutput(res)
+		},
+	}
+	cmd.Flags().Int(flags.FlagPage, 1, "pagination page of light clients to to query for")
+	cmd.Flags().Int(flags.FlagLimit, 100, "pagination limit of light clients to query for")
+
+	return cmd
+}
 
 // GetCmdQueryChannel defines the command to query a channel end
 func GetCmdQueryChannel(clientCtx client.Context) *cobra.Command {
@@ -28,7 +71,10 @@ func GetCmdQueryChannel(clientCtx client.Context) *cobra.Command {
 
 			portID := args[0]
 			channelID := args[1]
-			prove, _ := cmd.Flags().GetBool(flags.FlagProve)
+			prove, err := cmd.Flags().GetBool(flags.FlagProve)
+			if err != nil {
+				return err
+			}
 
 			channelRes, err := utils.QueryChannel(clientCtx, portID, channelID, prove)
 			if err != nil {
@@ -40,6 +86,44 @@ func GetCmdQueryChannel(clientCtx client.Context) *cobra.Command {
 		},
 	}
 	cmd.Flags().Bool(flags.FlagProve, true, "show proofs for the query results")
+	return cmd
+}
+
+// GetCmdQueryConnectionChannels defines the command to query all the channels associated with a
+// connection
+func GetCmdQueryConnectionChannels(clientCtx client.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "connections [connection-id]",
+		Short:   "Query all channels associated with a connection",
+		Long:    "Query all channels associated with a connection",
+		Example: fmt.Sprintf("%s query %s %s connections [connection-id]", version.ClientName, host.ModuleName, types.SubModuleName),
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx = clientCtx.Init()
+			queryClient := types.NewQueryClient(clientCtx)
+
+			offset, _ := cmd.Flags().GetInt(flags.FlagPage)
+			limit, _ := cmd.Flags().GetInt(flags.FlagLimit)
+
+			req := &types.QueryConnectionChannelsRequest{
+				Connection: args[0],
+				Req: &query.PageRequest{
+					Offset: uint64(offset),
+					Limit:  uint64(limit),
+				},
+			}
+
+			res, err := queryClient.ConnectionChannels(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			clientCtx = clientCtx.WithHeight(res.Height)
+			return clientCtx.PrintOutput(res)
+		},
+	}
+	cmd.Flags().Int(flags.FlagPage, 1, "pagination page of light clients to to query for")
+	cmd.Flags().Int(flags.FlagLimit, 100, "pagination limit of light clients to query for")
 
 	return cmd
 }
@@ -52,7 +136,7 @@ func GetCmdQueryChannelClientState(clientCtx client.Context) *cobra.Command {
 		Long:    "Query the client state associated with a channel, by providing its port and channel identifiers.",
 		Example: fmt.Sprintf("%s query ibc channel client-state [port-id] [channel-id]", version.ClientName),
 		Args:    cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(_ *cobra.Command, args []string) error {
 			clientCtx = clientCtx.Init()
 
 			portID := args[0]
@@ -67,5 +151,171 @@ func GetCmdQueryChannelClientState(clientCtx client.Context) *cobra.Command {
 			return clientCtx.PrintOutput(clientStateRes)
 		},
 	}
+	return cmd
+}
+
+// GetCmdQueryPacketCommitments defines the command to query all packet commitments associated with
+// a channel
+func GetCmdQueryPacketCommitments(clientCtx client.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "packet-commitments [port-id] [channel-id]",
+		Short:   "Query all packet commitments associated with a channel",
+		Long:    "Query all packet commitments associated with a channel",
+		Example: fmt.Sprintf("%s query %s %s packet-commitments [port-id] [channel-id]", version.ClientName, host.ModuleName, types.SubModuleName),
+		Args:    cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx = clientCtx.Init()
+			queryClient := types.NewQueryClient(clientCtx)
+
+			offset, _ := cmd.Flags().GetInt(flags.FlagPage)
+			limit, _ := cmd.Flags().GetInt(flags.FlagLimit)
+
+			req := &types.QueryPacketCommitmentsRequest{
+				PortID:    args[0],
+				ChannelID: args[1],
+				Req: &query.PageRequest{
+					Offset: uint64(offset),
+					Limit:  uint64(limit),
+				},
+			}
+
+			res, err := queryClient.PacketCommitments(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			clientCtx = clientCtx.WithHeight(res.Height)
+			return clientCtx.PrintOutput(res)
+		},
+	}
+	cmd.Flags().Int(flags.FlagPage, 1, "pagination page of light clients to to query for")
+	cmd.Flags().Int(flags.FlagLimit, 100, "pagination limit of light clients to query for")
+
+	return cmd
+}
+
+// GetCmdQueryPacketCommitment defines the command to query a channel end
+func GetCmdQueryPacketCommitment(clientCtx client.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "packet-commitment [port-id] [channel-id] [sequence]",
+		Short: "Query a packet commitment",
+		Long:  "Query a packet commitment",
+		Example: fmt.Sprintf(
+			"%s query %s %s end [port-id] [channel-id]", version.ClientName, host.ModuleName, types.SubModuleName,
+		),
+		Args: cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx = clientCtx.Init()
+
+			portID := args[0]
+			channelID := args[1]
+			prove, err := cmd.Flags().GetBool(flags.FlagProve)
+			if err != nil {
+				return err
+			}
+
+			seq, err := strconv.ParseUint(args[2], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			res, err := utils.QueryPacketCommitment(clientCtx, portID, channelID, seq, prove)
+			if err != nil {
+				return err
+			}
+
+			clientCtx = clientCtx.WithHeight(int64(res.ProofHeight))
+			return clientCtx.PrintOutput(res)
+		},
+	}
+	cmd.Flags().Bool(flags.FlagProve, true, "show proofs for the query results")
+	return cmd
+}
+
+// GetCmdQueryUnrelayedPackets defines the command to query all the unrelayed packets.
+func GetCmdQueryUnrelayedPackets(clientCtx client.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "unrelayed-packets [port-id] [channel-id]",
+		Short: "Query all the unrelayed packets associated with a channel",
+		Long: `It indicates if a packet, given a list of packet commitment sequences, is unrelayed.
+An unrelayed packet corresponds to:
+
+- Unrelayed packet commitments: when no acknowledgement exists for the given sequence.
+- Unrelayed packet acknowledgements: when an acknowledgement exists and a packet commitment also exists.`,
+		Example: fmt.Sprintf("%s query %s %s unrelayed-packets [port-id] [channel-id] --sequences=1,2,3", version.ClientName, host.ModuleName, types.SubModuleName),
+		Args:    cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx = clientCtx.Init()
+			queryClient := types.NewQueryClient(clientCtx)
+
+			seqSlice, err := cmd.Flags().GetInt64Slice(flagSequences)
+			if err != nil {
+				return err
+			}
+
+			seqs := make([]uint64, len(seqSlice))
+			for i := range seqSlice {
+				seqs[i] = uint64(seqSlice[i])
+			}
+
+			offset, _ := cmd.Flags().GetInt(flags.FlagPage)
+			limit, _ := cmd.Flags().GetInt(flags.FlagLimit)
+
+			req := &types.QueryUnrelayedPacketsRequest{
+				PortID:    args[0],
+				ChannelID: args[1],
+				Sequences: seqs,
+				Req: &query.PageRequest{
+					Offset: uint64(offset),
+					Limit:  uint64(limit),
+				},
+			}
+
+			res, err := queryClient.UnrelayedPackets(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			clientCtx = clientCtx.WithHeight(res.Height)
+			return clientCtx.PrintOutput(res)
+		},
+	}
+	cmd.Flags().Int64Slice(flagSequences, []int64{}, "comma separated list of packet sequence numbers")
+	cmd.Flags().Int(flags.FlagPage, 1, "pagination page of light clients to to query for")
+	cmd.Flags().Int(flags.FlagLimit, 100, "pagination limit of light clients to query for")
+
+	return cmd
+}
+
+// GetCmdQueryNextSequenceReceive defines the command to query a next receive sequence for a given channel
+func GetCmdQueryNextSequenceReceive(clientCtx client.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "next-sequence-receive [port-id] [channel-id]",
+		Short: "Query a next receive sequence",
+		Long:  "Query the next receive sequence for a given channel",
+		Example: fmt.Sprintf(
+			"%s query %s %s next-sequence-receive [port-id] [channel-id]", version.ClientName, host.ModuleName, types.SubModuleName,
+		),
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx = clientCtx.Init()
+
+			portID := args[0]
+			channelID := args[1]
+			prove, err := cmd.Flags().GetBool(flags.FlagProve)
+			if err != nil {
+				return err
+			}
+
+			sequenceRes, err := utils.QueryNextSequenceReceive(clientCtx, portID, channelID, prove)
+			if err != nil {
+				return err
+			}
+
+			clientCtx = clientCtx.WithHeight(int64(sequenceRes.ProofHeight))
+			return clientCtx.PrintOutput(sequenceRes)
+		},
+	}
+	cmd.Flags().Bool(flags.FlagProve, true, "show proofs for the query results")
 	return cmd
 }

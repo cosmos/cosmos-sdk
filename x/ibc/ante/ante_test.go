39ffee4a09b0bba0a910a60be0894a3934090f0c
Colin Axner <colinaxner@berkeley.edu>
2020-07-06 16:25:25 +0200

    add acknowledge test

--- x/ibc/ante/ante_test.go
+++ x/ibc/ante/ante_test.go
@@ -166,6 +166,155 @@ func (suite *HandlerTestSuite) TestHandleRecvPacket() {
 	}
 }
 
+// tests the ante handler acknowledging a packet on ordered and unordered
+// channels. It verifies that no state changes occur as the deleting of packet
+// commitments from state should occur in the 'AcknowledgementExecuted'
+// function. It test high level properties like ordering and basic sanity
+// checks. More rigorous testing of 'AcknowledgePacket' can be found in
+// the 04-channel/keeper/packet_test.go.
+func (suite *HandlerTestSuite) TestHandleAcknowledgePacket() {
+	var (
+		packet channeltypes.Packet
+	)
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{"success: ORDERED", func() {
+			clientA, clientB, connA, connB := suite.coordinator.SetupClientConnections(suite.chainA, suite.chainB, clientexported.Tendermint)
+			channelA, channelB := suite.coordinator.CreateChannel(suite.chainA, suite.chainB, connA, connB, channeltypes.ORDERED)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+
+			err = suite.coordinator.PacketExecuted(suite.chainB, suite.chainA, packet, clientA)
+			suite.Require().NoError(err)
+		}, true},
+		{"success: UNORDERED", func() {
+			clientA, clientB, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+
+			err = suite.coordinator.PacketExecuted(suite.chainB, suite.chainA, packet, clientA)
+			suite.Require().NoError(err)
+		}, true},
+		{"success: UNORDERED acknowledge out of order packet", func() {
+			// setup uses an UNORDERED channel
+			clientA, clientB, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+
+			// attempts to acknowledge ack with sequence 10 without acknowledging ack with sequence 1 (removing packet commitment)
+			for i := uint64(1); i < 10; i++ {
+				packet = channeltypes.NewPacket(ibctesting.TestHash, i, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+				err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+				suite.Require().NoError(err)
+
+				err = suite.coordinator.PacketExecuted(suite.chainB, suite.chainA, packet, clientA)
+				suite.Require().NoError(err)
+
+			}
+		}, true},
+		{"failure: ORDERED acknowledge out of order packet", func() {
+			clientA, clientB, connA, connB := suite.coordinator.SetupClientConnections(suite.chainA, suite.chainB, clientexported.Tendermint)
+			channelA, channelB := suite.coordinator.CreateChannel(suite.chainA, suite.chainB, connA, connB, channeltypes.ORDERED)
+
+			// attempts to acknowledge ack with sequence 10 without acknowledging ack with sequence 1 (removing packet commitment
+			for i := uint64(1); i < 10; i++ {
+				packet = channeltypes.NewPacket(ibctesting.TestHash, i, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+				err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+				suite.Require().NoError(err)
+
+				err = suite.coordinator.PacketExecuted(suite.chainB, suite.chainA, packet, clientA)
+				suite.Require().NoError(err)
+			}
+		}, false},
+		{"channel does not exist", func() {
+			// any non-nil value of packet is valid
+			suite.Require().NotNil(packet)
+		}, false},
+		{"packet not received", func() {
+			_, clientB, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+		}, false},
+		{"ORDERED: packet already acknowledged (replay)", func() {
+			clientA, clientB, connA, connB := suite.coordinator.SetupClientConnections(suite.chainA, suite.chainB, clientexported.Tendermint)
+			channelA, channelB := suite.coordinator.CreateChannel(suite.chainA, suite.chainB, connA, connB, channeltypes.ORDERED)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+
+			err = suite.coordinator.PacketExecuted(suite.chainB, suite.chainA, packet, clientA)
+			suite.Require().NoError(err)
+
+			err = suite.coordinator.AcknowledgementExecuted(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+		}, false},
+		{"UNORDERED: packet already received (replay)", func() {
+			clientA, clientB, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+
+			err = suite.coordinator.PacketExecuted(suite.chainB, suite.chainA, packet, clientA)
+			suite.Require().NoError(err)
+
+			err = suite.coordinator.AcknowledgementExecuted(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+		}, false},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+
+			handler := sdk.ChainAnteDecorators(ante.NewProofVerificationDecorator(
+				suite.chainA.App.IBCKeeper.ClientKeeper,
+				suite.chainA.App.IBCKeeper.ChannelKeeper,
+			))
+
+			tc.malleate()
+
+			packetKey := host.KeyPacketAcknowledgement(packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+			proof, proofHeight := suite.chainB.QueryProof(packetKey)
+
+			ack := ibctesting.TestHash
+
+			msg := channeltypes.NewMsgAcknowledgement(packet, ack, proof, proofHeight, suite.chainA.SenderAccount.GetAddress())
+
+			// ante-handle RecvPacket
+			_, err := handler(suite.chainA.GetContext(), newTx(msg), false)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				// replay should return same result since there is no state changes
+				_, err := handler(suite.chainA.GetContext(), newTx(msg), false)
+				suite.Require().NoError(err)
+
+				// verify packet commitment was not deleted
+				has := suite.chainA.App.IBCKeeper.ChannelKeeper.HasPacketCommitment(suite.chainA.GetContext(), packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+				suite.Require().True(has)
+
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
 func TestHandlerTestSuite(t *testing.T) {
 	suite.Run(t, new(HandlerTestSuite))
 }

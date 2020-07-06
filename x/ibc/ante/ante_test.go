7f5a6626e2aff6902506369d061b9cb10a366e30
Colin Axner <colinaxner@berkeley.edu>
2020-07-06 16:44:18 +0200

    add timeout tests

--- x/ibc/ante/ante_test.go
+++ x/ibc/ante/ante_test.go
@@ -315,6 +315,126 @@ func (suite *HandlerTestSuite) TestHandleAcknowledgePacket() {
 	}
 }
 
+// tests the ante handler timing out a packet on ordered and unordered channels.
+// It verifies that no state changes occur as the deleting of packet
+// commitments from state should occur in the 'TimeoutExecuted' function. It
+// tests high level properties like ordering and basic sanity
+// checks. More rigorous testing of 'TimeoutPacket' can be found in the
+// 04-channel/keeper/timeout_test.go.
+func (suite *HandlerTestSuite) TestHandleTimeoutPacket() {
+	var (
+		packet    channeltypes.Packet
+		packetKey []byte
+	)
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{"success: ORDERED", func() {
+			clientA, clientB, connA, connB := suite.coordinator.SetupClientConnections(suite.chainA, suite.chainB, clientexported.Tendermint)
+			channelA, channelB := suite.coordinator.CreateChannel(suite.chainA, suite.chainB, connA, connB, channeltypes.ORDERED)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, uint64(suite.chainB.GetContext().BlockHeight()), uint64(suite.chainB.GetContext().BlockTime().UnixNano()))
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+
+			// need to update chainA client to prove missing ack
+			suite.coordinator.UpdateClient(suite.chainA, suite.chainB, clientA, clientexported.Tendermint)
+
+			packetKey = host.KeyNextSequenceRecv(packet.GetDestPort(), packet.GetDestChannel())
+		}, true},
+		{"success: UNORDERED", func() {
+			clientA, clientB, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, uint64(suite.chainB.GetContext().BlockHeight()), uint64(suite.chainB.GetContext().BlockTime().UnixNano()))
+			err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+			suite.Require().NoError(err)
+
+			// need to update chainA client to prove missing ack
+			suite.coordinator.UpdateClient(suite.chainA, suite.chainB, clientA, clientexported.Tendermint)
+			packetKey = host.KeyPacketAcknowledgement(packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+		}, true},
+		{"success: UNORDERED timeout out of order packet", func() {
+			// setup uses an UNORDERED channel
+			clientA, clientB, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+
+			// attempts to timeout packet with sequence 10 without timing out packet with sequence 1
+			for i := uint64(1); i < 10; i++ {
+				packet = channeltypes.NewPacket(ibctesting.TestHash, i, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, uint64(suite.chainB.GetContext().BlockHeight()), 0)
+
+				err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+				suite.Require().NoError(err)
+			}
+			// need to update chainA client to prove missing ack
+			suite.coordinator.UpdateClient(suite.chainA, suite.chainB, clientA, clientexported.Tendermint)
+			packetKey = host.KeyPacketAcknowledgement(packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+
+		}, true},
+		{"success: ORDERED timeout out of order packet", func() {
+			clientA, clientB, connA, connB := suite.coordinator.SetupClientConnections(suite.chainA, suite.chainB, clientexported.Tendermint)
+			channelA, channelB := suite.coordinator.CreateChannel(suite.chainA, suite.chainB, connA, connB, channeltypes.ORDERED)
+
+			// attempts to timeout packet with sequence 10 without timing out packet with sequence 1
+			for i := uint64(1); i < 10; i++ {
+				packet = channeltypes.NewPacket(ibctesting.TestHash, i, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, uint64(suite.chainB.GetContext().BlockHeight()), 0)
+
+				err := suite.coordinator.SendPacket(suite.chainA, suite.chainB, packet, clientB)
+				suite.Require().NoError(err)
+			}
+			// need to update chainA client to prove missing ack
+			suite.coordinator.UpdateClient(suite.chainA, suite.chainB, clientA, clientexported.Tendermint)
+			packetKey = host.KeyNextSequenceRecv(packet.GetDestPort(), packet.GetDestChannel())
+
+		}, true},
+		{"channel does not exist", func() {
+			// any non-nil value of packet is valid
+			suite.Require().NotNil(packet)
+
+			packetKey = host.KeyNextSequenceRecv(packet.GetDestPort(), packet.GetDestChannel())
+		}, false},
+		{"UNORDERED: packet not sent", func() {
+			_, _, _, _, channelA, channelB := suite.coordinator.Setup(suite.chainA, suite.chainB)
+			packet = channeltypes.NewPacket(ibctesting.TestHash, 1, channelA.PortID, channelA.ID, channelB.PortID, channelB.ID, timeoutHeight, 0)
+			packetKey = host.KeyPacketAcknowledgement(packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+		}, false},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+
+			handler := sdk.ChainAnteDecorators(ante.NewProofVerificationDecorator(
+				suite.chainA.App.IBCKeeper.ClientKeeper,
+				suite.chainA.App.IBCKeeper.ChannelKeeper,
+			))
+
+			tc.malleate()
+
+			proof, proofHeight := suite.chainB.QueryProof(packetKey)
+
+			msg := channeltypes.NewMsgTimeout(packet, 1, proof, proofHeight, suite.chainA.SenderAccount.GetAddress())
+
+			// ante-handle RecvPacket
+			_, err := handler(suite.chainA.GetContext(), newTx(msg), false)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				// replay should return same result since there is no state changes
+				_, err := handler(suite.chainA.GetContext(), newTx(msg), false)
+				suite.Require().NoError(err)
+
+				// verify packet commitment was not deleted
+				has := suite.chainA.App.IBCKeeper.ChannelKeeper.HasPacketCommitment(suite.chainA.GetContext(), packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+				suite.Require().True(has)
+
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
 func TestHandlerTestSuite(t *testing.T) {
 	suite.Run(t, new(HandlerTestSuite))
 }

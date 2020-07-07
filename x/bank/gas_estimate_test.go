fc0b3a8f667d12f3db51818a13a4631321a44d92
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-07-07 22:10:24 +0200

    Use lower-level simapp api, get reasonable simulation gas result

--- x/bank/gas_estimate_test.go
+++ x/bank/gas_estimate_test.go
@@ -3,6 +3,7 @@ package bank_test
 import (
 	"fmt"
 	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/simapp/helpers"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	authexported "github.com/cosmos/cosmos-sdk/x/auth/exported"
@@ -46,13 +47,45 @@ func TestSendGasEstimates(t *testing.T) {
 	sendMsg := types.NewMsgSend(addr1, addr2, send)
 	header := abci.Header{Height: app.LastBlockHeight() + 1}
 
-	gas, res, err := simapp.SignCheckDeliver(t, app.Codec(), app.BaseApp, header, []sdk.Msg{sendMsg}, []uint64{acct.GetAccountNumber()}, []uint64{acct.GetSequence()}, true, true, priv1)
+	// this will build proper tx
+	buildTx := func(expectedGas uint64) sdk.Tx {
+		return helpers.GenTx(
+			[]sdk.Msg{sendMsg},
+			sdk.Coins{sdk.NewInt64Coin(sdk.DefaultBondDenom, 0)},
+			expectedGas,
+			"",
+			[]uint64{acct.GetAccountNumber()},
+			[]uint64{acct.GetSequence()},
+			priv1,
+		)
+	}
+
+	// run simulation
+	tx := buildTx(200_000)
+	txBytes, err := app.Codec().MarshalBinaryLengthPrefixed(tx)
+	require.NoError(t, err)
+	simGas, res, err := app.Simulate(txBytes, tx)
 	require.NoError(t, err)
 	require.NotNil(t, res)
+	fmt.Printf("(SIM) wanted: %d / used: %d\n", simGas.GasWanted, simGas.GasUsed)
+
+	// deliver the tx with the gas returned from simulate (plus 10%)
+	tx = buildTx(simGas.GasUsed + simGas.GasUsed / 10)
+	app.BeginBlock(abci.RequestBeginBlock{Header: header})
+	gas, res, err := app.Deliver(tx)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	app.EndBlock(abci.RequestEndBlock{})
+	app.Commit()
+
 	// We have wanted 1 million, used ~53k
 	fmt.Printf("wanted: %d / used: %d\n", gas.GasWanted, gas.GasUsed)
 	// sanity checks for reasonable gas values
 	assert.Less(t, uint64(40000), gas.GasUsed)
 	assert.Less(t, gas.GasUsed, uint64(80000))
 }
+
+
+/// these pieces are taken from simapp to give us a bit more control
+
 

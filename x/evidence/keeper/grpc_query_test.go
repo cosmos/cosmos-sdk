85d524c3106d091eb1622c9ffd0a3366978ec6e0
blushi <marie.gauthier63@gmail.com>
2020-07-09 09:43:25 +0200

    Use table tests

--- x/evidence/keeper/grpc_query_test.go
+++ x/evidence/keeper/grpc_query_test.go
@@ -1,9 +1,9 @@
 package keeper_test
 
 import (
-	gocontext "context"
+	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/cosmos-sdk/x/evidence/exported"
 	"github.com/cosmos/cosmos-sdk/x/evidence/types"
@@ -12,67 +12,132 @@ import (
 )
 
 func (suite *KeeperTestSuite) TestQueryEvidence() {
-	app, ctx := suite.app, suite.ctx
+	var (
+		req      *types.QueryEvidenceRequest
+		evidence []exported.Evidence
+	)
 
-	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
-	types.RegisterQueryServer(queryHelper, app.EvidenceKeeper)
-	queryClient := types.NewQueryClient(queryHelper)
+	testCases := []struct {
+		msg       string
+		malleate  func()
+		expPass   bool
+		posttests func(res *types.QueryEvidenceResponse)
+	}{
+		{
+			"empty request",
+			func() {
+				req = &types.QueryEvidenceRequest{}
+			},
+			false,
+			func(res *types.QueryEvidenceResponse) {},
+		},
+		{
+			"invalid request with empty evidence hash",
+			func() {
+				req = &types.QueryEvidenceRequest{EvidenceHash: tmbytes.HexBytes{}}
+			},
+			false,
+			func(res *types.QueryEvidenceResponse) {},
+		},
+		{
+			"success",
+			func() {
+				numEvidence := 100
+				evidence = suite.populateEvidence(suite.ctx, numEvidence)
+				req = types.NewQueryEvidenceRequest(evidence[0].Hash())
+			},
+			true,
+			func(res *types.QueryEvidenceResponse) {
+				var evi exported.Evidence
+				err := suite.app.InterfaceRegistry().UnpackAny(res.Evidence, &evi)
+				suite.Require().NoError(err)
+				suite.Require().NotNil(evi)
+				suite.Require().Equal(evi, evidence[0])
+			},
+		},
+	}
 
-	_, err := queryClient.Evidence(gocontext.Background(), &types.QueryEvidenceRequest{})
-	suite.Require().Error(err)
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest()
 
-	_, err = queryClient.Evidence(gocontext.Background(), &types.QueryEvidenceRequest{EvidenceHash: tmbytes.HexBytes{}})
-	suite.Require().Error(err)
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.ctx)
 
-	numEvidence := 100
-	evidence := suite.populateEvidence(ctx, numEvidence)
+			res, err := suite.queryClient.Evidence(ctx, req)
 
-	req := types.NewQueryEvidenceRequest(evidence[0].Hash())
-	res, err := queryClient.Evidence(gocontext.Background(), req)
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res)
-	suite.Require().NotNil(res.Evidence)
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Nil(res)
+			}
 
-	var evi exported.Evidence
-	err = app.InterfaceRegistry().UnpackAny(res.Evidence, &evi)
-	suite.Require().NoError(err)
-	suite.Require().NotNil(evi)
-	suite.Require().Equal(evi, evidence[0])
+			tc.posttests(res)
+		})
+	}
 }
 
 func (suite *KeeperTestSuite) TestQueryAllEvidence() {
-	app, ctx := suite.app, suite.ctx
+	var (
+		req *types.QueryAllEvidenceRequest
+	)
 
-	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
-	types.RegisterQueryServer(queryHelper, app.EvidenceKeeper)
-	queryClient := types.NewQueryClient(queryHelper)
-
-	res, err := queryClient.AllEvidence(gocontext.Background(), &types.QueryAllEvidenceRequest{})
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res)
-	suite.Require().Empty(res.Evidence)
-
-	numEvidence := 100
-	_ = suite.populateEvidence(ctx, numEvidence)
-	pageReq := &query.PageRequest{
-		Key:        nil,
-		Limit:      50,
-		CountTotal: false,
+	testCases := []struct {
+		msg       string
+		malleate  func()
+		expPass   bool
+		posttests func(res *types.QueryAllEvidenceResponse)
+	}{
+		{
+			"success without evidence",
+			func() {
+				req = &types.QueryAllEvidenceRequest{}
+			},
+			true,
+			func(res *types.QueryAllEvidenceResponse) {
+				suite.Require().Empty(res.Evidence)
+			},
+		},
+		{
+			"success",
+			func() {
+				numEvidence := 100
+				_ = suite.populateEvidence(suite.ctx, numEvidence)
+				pageReq := &query.PageRequest{
+					Key:        nil,
+					Limit:      50,
+					CountTotal: false,
+				}
+				req = types.NewQueryAllEvidenceRequest(pageReq)
+			},
+			true,
+			func(res *types.QueryAllEvidenceResponse) {
+				suite.Equal(len(res.Evidence), 50)
+				suite.NotNil(res.Res.NextKey)
+			},
+		},
 	}
-	req := types.NewQueryAllEvidenceRequest(pageReq)
-	res, err = queryClient.AllEvidence(gocontext.Background(), req)
-	suite.Require().NoError(err)
-	suite.Require().NotNil(res)
-	suite.Equal(len(res.Evidence), 50)
-	suite.NotNil(res.Res.NextKey)
 
-	pageReq = &query.PageRequest{
-		Key:        res.Res.NextKey,
-		Limit:      50,
-		CountTotal: true,
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest()
+
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.ctx)
+
+			res, err := suite.queryClient.AllEvidence(ctx, req)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Nil(res)
+			}
+
+			tc.posttests(res)
+		})
 	}
-	req = types.NewQueryAllEvidenceRequest(pageReq)
-	res, err = queryClient.AllEvidence(gocontext.Background(), req)
-	suite.Equal(len(res.Evidence), 50)
-	suite.Nil(res.Res.NextKey)
 }

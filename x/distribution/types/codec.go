232e720b138ad0889b142b13c0367a226e05a199
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-07-10 14:36:34 +0200

    launchpad: register MsgFundCommunityPool to distribution codec (#6675)
    Closes: #6210

--- x/distribution/types/codec.go
+++ x/distribution/types/codec.go
@@ -4,12 +4,13 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
-// Register concrete types on codec codec
+// RegisterCodec concrete distribution types on amino codec
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgWithdrawDelegatorReward{}, "cosmos-sdk/MsgWithdrawDelegationReward", nil)
 	cdc.RegisterConcrete(MsgWithdrawValidatorCommission{}, "cosmos-sdk/MsgWithdrawValidatorCommission", nil)
 	cdc.RegisterConcrete(MsgSetWithdrawAddress{}, "cosmos-sdk/MsgModifyWithdrawAddress", nil)
 	cdc.RegisterConcrete(CommunityPoolSpendProposal{}, "cosmos-sdk/CommunityPoolSpendProposal", nil)
+	cdc.RegisterConcrete(MsgFundCommunityPool{}, "cosmos-sdk/MsgFundCommunityPool", nil)
 }
 
 // ModuleCdc is a generic sealed codec to be used throughout module

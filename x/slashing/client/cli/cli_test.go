f318ff806605777ab6f1b6791003e58d82a736d3
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-07-13 14:07:35 -0400

    tests: TestGetCmdQueryParams

--- x/slashing/client/cli/cli_test.go
+++ x/slashing/client/cli/cli_test.go
@@ -3,10 +3,11 @@ package cli_test
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/suite"
+	tmcli "github.com/tendermint/tendermint/libs/cli"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
@@ -14,7 +15,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/slashing/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/slashing/types"
 )
 
 type IntegrationTestSuite struct {
@@ -53,29 +53,31 @@ func (s *IntegrationTestSuite) TestGetCmdQuerySigningInfo() {
 	s.Require().NoError(err)
 
 	testCases := []struct {
-		name      string
-		args      []string
-		expectErr bool
-		respType  fmt.Stringer
-		expected  fmt.Stringer
+		name           string
+		args           []string
+		expectErr      bool
+		expectedOutput string
 	}{
-		{"invalid address", []string{"foo"}, true, nil, nil},
+		{"invalid address", []string{"foo"}, true, ``},
 		{
-			"valid address",
+			"valid address (default output)",
 			[]string{
 				valConsPubKey,
 				fmt.Sprintf("--%s=1", flags.FlagHeight),
 			},
 			false,
-			&types.ValidatorSigningInfo{},
-			types.ValidatorSigningInfo{
-				Address:             sdk.ConsAddress(val.PubKey.Address()),
-				StartHeight:         0,
-				IndexOffset:         0,
-				JailedUntil:         time.Unix(0, 0).UTC(),
-				Tombstoned:          false,
-				MissedBlocksCounter: 0,
+			fmt.Sprintf(`{"address":"%s","jailed_until":"1970-01-01T00:00:00Z"}`, sdk.ConsAddress(val.PubKey.Address())),
+		},
+		{
+			"valid address (text output)",
+			[]string{
+				valConsPubKey,
+				fmt.Sprintf("--%s=text", tmcli.OutputFlag),
+				fmt.Sprintf("--%s=1", flags.FlagHeight),
 			},
+			false,
+			fmt.Sprintf(`address: %s
+jailed_until: "1970-01-01T00:00:00Z"`, sdk.ConsAddress(val.PubKey.Address())),
 		},
 	}
 
@@ -99,9 +101,53 @@ func (s *IntegrationTestSuite) TestGetCmdQuerySigningInfo() {
 				s.Require().Error(err)
 			} else {
 				s.Require().NoError(err)
-				s.Require().NoError(clientCtx.JSONMarshaler.UnmarshalJSON(out.Bytes(), tc.respType))
-				s.Require().Equal(tc.expected.String(), tc.respType.String())
+				s.Require().Equal(tc.expectedOutput, strings.TrimSpace(out.String()))
 			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestGetCmdQueryParams() {
+	val := s.network.Validators[0]
+
+	testCases := []struct {
+		name           string
+		args           []string
+		expectedOutput string
+	}{
+		{
+			"default output",
+			[]string{},
+			`{"json:\"signed_blocks_window\"":"100","json:\"min_signed_per_window\"":"0.500000000000000000","json:\"downtime_jail_duration\"":"600000000000","json:\"slash_fraction_double_sign\"":"0.050000000000000000","json:\"slash_fraction_downtime\"":"0.010000000000000000"}`,
+		},
+		{
+			"text output",
+			[]string{fmt.Sprintf("--%s=text", tmcli.OutputFlag)},
+			`json:"downtime_jail_duration": "600000000000"
+json:"min_signed_per_window": "0.500000000000000000"
+json:"signed_blocks_window": "100"
+json:"slash_fraction_double_sign": "0.050000000000000000"
+json:"slash_fraction_downtime": "0.010000000000000000"`,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			cmd := cli.GetCmdQueryParams()
+			_, out := testutil.ApplyMockIO(cmd)
+
+			clientCtx := val.ClientCtx.WithOutput(out)
+
+			ctx := context.Background()
+			ctx = context.WithValue(ctx, client.ClientContextKey, &clientCtx)
+
+			out.Reset()
+			cmd.SetArgs(tc.args)
+
+			s.Require().NoError(cmd.ExecuteContext(ctx))
+			s.Require().Equal(tc.expectedOutput, strings.TrimSpace(out.String()))
 		})
 	}
 }

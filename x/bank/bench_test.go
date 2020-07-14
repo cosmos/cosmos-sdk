6393ec68821a52019b71eb535c8e568e637c712e
sahith-narahari <sahithnarahari@gmail.com>
2020-07-14 22:56:50 +0530

    fix tests

--- x/bank/bench_test.go
+++ x/bank/bench_test.go
@@ -35,7 +35,7 @@ func BenchmarkOneBankSendTxPerBlock(b *testing.B) {
 	txGen := simappparams.MakeEncodingConfig().TxGenerator
 
 	// Precompute all txs
-	txs, err := simapp.GenSequenceOfTxs([]sdk.Msg{sendMsg1}, txGen, []uint64{0}, []uint64{uint64(0)}, b.N, priv1)
+	txs, err := simapp.GenSequenceOfTxs(txGen, []sdk.Msg{sendMsg1}, []uint64{0}, []uint64{uint64(0)}, b.N, priv1)
 	require.NoError(b, err)
 	b.ResetTimer()
 
@@ -77,7 +77,7 @@ func BenchmarkOneBankMultiSendTxPerBlock(b *testing.B) {
 	txGen := simappparams.MakeEncodingConfig().TxGenerator
 
 	// Precompute all txs
-	txs, err := simapp.GenSequenceOfTxs([]sdk.Msg{multiSendMsg1}, txGen, []uint64{0}, []uint64{uint64(0)}, b.N, priv1)
+	txs, err := simapp.GenSequenceOfTxs(txGen, []sdk.Msg{multiSendMsg1}, []uint64{0}, []uint64{uint64(0)}, b.N, priv1)
 	require.NoError(b, err)
 	b.ResetTimer()
 

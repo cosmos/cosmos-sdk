0cba060703e514827a2b336eb0fd90d051c32bf7
okwme <billy.rennekamp@gmail.com>
2020-07-15 17:47:37 +0200

    regex includes emoji and old denom logic

--- types/coin.go
+++ types/coin.go
@@ -595,21 +595,22 @@ func (coins Coins) Sort() Coins {
 
 var (
 	// Denominations can be 3 ~ 16 characters long.
-	reDnmString = `[a-z][a-z0-9]{2,15}`
-	reAmt       = `[[:digit:]]+`
-	reDecAmt    = `[[:digit:]]*\.[[:digit:]]+`
-	reSpc       = `[[:space:]]*`
-	reDnm       = regexp.MustCompile(fmt.Sprintf(`^%s$`, reDnmString))
-	reCoin      = regexp.MustCompile(fmt.Sprintf(`^(%s)%s(%s)$`, reAmt, reSpc, reDnmString))
-	reDecCoin   = regexp.MustCompile(fmt.Sprintf(`^(%s)%s(%s)$`, reDecAmt, reSpc, reDnmString))
+	reDnmString = `[a-z][a-z0-9]{2,15}|[\x{0080}-\x{02AF}|\x{0300}-\x{03FF}|\x{0600}-\x{06FF}|\x{0C00}-\x{0C7F}|\x{1DC0}-\x{1DFF}|\x{1E00}-\x{1EFF}|\x{2000}-\x{209F}|\x{20D0}-\x{214F}|\x{2190}-\x{23FF}|\x{2460}-\x{25FF}|\x{2600}-\x{27EF}|\x{2900}-\x{29FF}|\x{2B00}-\x{2BFF}|\x{2C60}-\x{2C7F}|\x{2E00}-\x{2E7F}|\x{3000}-\x{303F}|\x{A490}-\x{A4CF}|\x{E000}-\x{F8FF}|\x{FE00}-\x{FE0F}|\x{FE30}-\x{FE4F}|\x{1F000}-\x{1F02F}|\x{1F0A0}-\x{1F0FF}|\x{1F100}-\x{1F64F}|\x{1F680}-\x{1F6FF}|\x{1F910}-\x{1F96B}|\x{1F980}-\x{1F9E0}|\x{1F93C}-\x{1F93E}|\x{1f975}]`
+	// reDnmString   = `[a-z][a-z0-9]{2,15}`
+	reAmt     = `[[:digit:]]+`
+	reDecAmt  = `[[:digit:]]*\.[[:digit:]]+`
+	reSpc     = `[[:space:]]*`
+	reDnm     = regexp.MustCompile(fmt.Sprintf(`^%s$`, reDnmString))
+	reCoin    = regexp.MustCompile(fmt.Sprintf(`^(%s)%s(%s)$`, reAmt, reSpc, reDnmString))
+	reDecCoin = regexp.MustCompile(fmt.Sprintf(`^(%s)%s(%s)$`, reDecAmt, reSpc, reDnmString))
 )
 
 // ValidateDenom validates a denomination string returning an error if it is
 // invalid.
 func ValidateDenom(denom string) error {
-	// if !reDnm.MatchString(denom) {
-	// 	return fmt.Errorf("invalid denom: %s", denom)
-	// }
+	if !reDnm.MatchString(denom) {
+		return fmt.Errorf("invalid denom: %s", denom)
+	}
 	return nil
 }
 

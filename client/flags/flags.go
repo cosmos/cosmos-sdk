08d254864b8b9a00ff1f50f46d17b0a4c9bf6070
atheesh <atheesh@vitwit.com>
2020-07-17 16:36:27 +0530

    review change

--- client/flags/flags.go
+++ client/flags/flags.go
@@ -129,7 +129,7 @@ func AddTxFlagsToCmd(cmd *cobra.Command) {
 // AddPaginationFlagsToCmd adds common paginations flags to command
 func AddPaginationFlagsToCmd(cmd *cobra.Command, query string) {
 	cmd.Flags().String(FlagPageKey, "", fmt.Sprintf("pagination page-key of %s to query for", query))
-	cmd.Flags().Uint64(FlagOffset, 1, fmt.Sprintf("pagination offset of %s to query for", query))
+	cmd.Flags().Uint64(FlagOffset, 0, fmt.Sprintf("pagination offset of %s to query for", query))
 	cmd.Flags().Uint64(FlagLimit, 100, fmt.Sprintf("pagination limit of %s to query for", query))
 	cmd.Flags().Bool(FlagCountTotal, false, fmt.Sprintf("total number of records in %s to query for", query))
 }

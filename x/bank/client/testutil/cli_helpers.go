0ccc48d2a3a34ca3ad585f5fa466e5e87fd7c7f5
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2020-07-21 09:54:07 -0400

    CLI/Tests: Remove Fixtures (#6799)
    * remove fixtures
    
    * setup tests
    
    * update x/mint
    
    * cli: update x/staking commands
    
    * tests: convert x/staking CLI tests
    
    * tests: fix x/auth CLI tests
    
    * cli updates
    
    * fix buiild
    
    * fix build
    
    * Update x/gov/client/cli/cli_test.go
    
    Co-authored-by: Amaury Martiny <amaury.martiny@protonmail.com>
    
    * remove GenerateOrBroadcastTx
    
    * move TestCLIQueryConn
    
    Co-authored-by: Federico Kunze <31522760+fedekunze@users.noreply.github.com>
    Co-authored-by: Amaury Martiny <amaury.martiny@protonmail.com>

--- x/bank/client/testutil/cli_helpers.go
+++ x/bank/client/testutil/cli_helpers.go
@@ -7,13 +7,7 @@ import (
 
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/cosmos/cosmos-sdk/client"
-	clientkeys "github.com/cosmos/cosmos-sdk/client/keys"
-	"github.com/cosmos/cosmos-sdk/tests"
-	"github.com/cosmos/cosmos-sdk/tests/cli"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankcli "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
 )
 
@@ -29,29 +23,4 @@ func QueryBalancesExec(clientCtx client.Context, address fmt.Stringer, extraArgs
 	args = append(args, extraArgs...)
 
 	return clitestutil.ExecTestCLICmd(clientCtx, bankcli.GetBalancesCmd(), args)
-}
-
-// ----------------------------------------------------------------------------
-// TODO: REMOVE ALL FUNCTIONS BELOW.
-//
-// REF: https://github.com/cosmos/cosmos-sdk/issues/6571
-// ----------------------------------------------------------------------------
-
-// TxSend is simcli tx send
-func TxSend(f *cli.Fixtures, from string, to sdk.AccAddress, amount sdk.Coin, flags ...string) (bool, string, string) {
-	cmd := fmt.Sprintf("%s tx send --keyring-backend=test %s %s %s %v", f.SimdBinary, from, to, amount, f.Flags())
-	return cli.ExecuteWriteRetStdStreams(f.T, cli.AddFlags(cmd, flags), clientkeys.DefaultKeyPass)
-}
-
-// QueryBalances executes the bank query balances command for a given address and
-// flag set.
-func QueryBalances(f *cli.Fixtures, address sdk.AccAddress, flags ...string) sdk.Coins {
-	cmd := fmt.Sprintf("%s query bank balances %s %v", f.SimdBinary, address, f.Flags())
-	out, _ := tests.ExecuteT(f.T, cli.AddFlags(cmd, flags), "")
-
-	var balances sdk.Coins
-
-	require.NoError(f.T, f.Cdc.UnmarshalJSON([]byte(out), &balances), "out %v\n", out)
-
-	return balances
 }

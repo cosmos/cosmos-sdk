aaa1ac7baa81219339e3cf4ad9e90c8d45fbdf8f
Aaron Craelius <aaronc@users.noreply.github.com>
2020-07-23 12:02:02 -0400

    Add response type cache

--- server/grpc/proxy.go
+++ server/grpc/proxy.go
@@ -2,14 +2,16 @@ package grpc
 
 import (
 	"context"
+	"reflect"
 
-	"github.com/cosmos/cosmos-sdk/client"
 	gogogrpc "github.com/gogo/protobuf/grpc"
 	"github.com/gogo/protobuf/proto"
 	"github.com/tendermint/tendermint/abci/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/cosmos/cosmos-sdk/client"
 )
 
 type proxyServer struct {
@@ -49,6 +51,8 @@ func (proxy *proxyServer) RegisterService(desc *grpc.ServiceDesc, handler interf
 // ABCIQueryProxyInterceptor implements a query interceptor that intercepts
 // queries and sends them to the ABCI.
 func ABCIQueryProxyInterceptor(clientCtx client.Context) grpc.UnaryServerInterceptor {
+	resTypeCache := map[string]reflect.Type{}
+
 	return func(_ context.Context, req interface{}, info *grpc.UnaryServerInfo, _ grpc.UnaryHandler) (resp interface{}, err error) {
 		msg, ok := req.(proto.Message)
 		if !ok {
@@ -65,6 +69,12 @@ func ABCIQueryProxyInterceptor(clientCtx client.Context) grpc.UnaryServerInterce
 			Path: info.FullMethod,
 		}
 
+		resType, ok := resTypeCache[info.FullMethod]
+		if !ok {
+			// TODO figure out response type and cache
+			// NOTE: this is a bit hacky but...
+		}
+
 		abciRes, err := clientCtx.QueryABCI(abciReq)
 		if err != nil {
 			return nil, err
@@ -74,7 +84,7 @@ func ABCIQueryProxyInterceptor(clientCtx client.Context) grpc.UnaryServerInterce
 			return nil, status.Errorf(codes.Internal, abciRes.Log)
 		}
 
-		respMsg, ok := resp.(proto.Message)
+		respMsg, ok := reflect.New(resType).Interface().(proto.Message)
 		if !ok {
 			return nil, status.Errorf(codes.Internal, "unable to proto marshal")
 		}

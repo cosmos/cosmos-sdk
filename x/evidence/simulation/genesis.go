db1361350038914de2f3037cc280b0c13b8029a5
blushi <marie.gauthier63@gmail.com>
2020-07-29 17:03:37 +0200

    Keep []Evidence as return type in GenEvidences

--- x/evidence/simulation/genesis.go
+++ x/evidence/simulation/genesis.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/evidence/exported"
 	"github.com/cosmos/cosmos-sdk/x/evidence/types"
 )
 
@@ -18,20 +18,20 @@ import (
 const evidence = "evidence"
 
 // GenEvidences returns an empty slice of evidences.
-func GenEvidences(_ *rand.Rand, _ []simtypes.Account) []*codectypes.Any {
-	return []*codectypes.Any{}
+func GenEvidences(_ *rand.Rand, _ []simtypes.Account) []exported.Evidence {
+	return []exported.Evidence{}
 }
 
 // RandomizedGenState generates a random GenesisState for evidence
 func RandomizedGenState(simState *module.SimulationState) {
-	var ev []*codectypes.Any
+	var ev []exported.Evidence
 
 	simState.AppParams.GetOrGenerate(
 		simState.Cdc, evidence, &ev, simState.Rand,
 		func(r *rand.Rand) { ev = GenEvidences(r, simState.Accounts) },
 	)
 
-	evidenceGenesis := types.GenesisState{Evidence: ev}
+	evidenceGenesis := types.NewGenesisState(ev)
 
 	fmt.Printf("Selected randomly generated %s parameters:\n%s\n", types.ModuleName, codec.MustMarshalJSONIndent(simState.Cdc, evidenceGenesis))
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(evidenceGenesis)

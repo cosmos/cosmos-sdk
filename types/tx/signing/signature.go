72ebafeecadba67a39d3ebe1df8a24d85409ea18
SaReN <sahithnarahari@gmail.com>
2020-07-30 04:03:42 +0530

    Update auth cli commands (#6717)
    * add utils
    
    * update sign cmd
    
    * update multisign cmd
    
    * update sign batch cmd
    
    * update genutil cmd
    
    * add wrap tx builder
    
    * update gentx cli
    
    * update validate sigs cmd
    
    * fix lint
    
    * add flag reader to cli
    
    * update marshaler for batchscan
    
    * add register query server
    
    * update to master
    
    * remove depricated methods
    
    * fix keyring issue
    
    * update wraptx
    
    * Fix batch scan
    
    * fix register interfaces issue
    
    * update printOutput
    
    * Update Validate Sigs test
    
    * WIP on signature JSON
    
    * Cleanup
    
    * Cleanup
    
    * Test fixes
    
    * Test fixes
    
    * Fixes
    
    * WIP on tests
    
    * Fix gov tests
    
    * fix lint
    
    * fix MultiSign tests
    
    * fix tests
    
    * refactor tests
    
    * Cleanup
    
    * Address review comments
    
    * Update encode
    
    * Test fix
    
    * Rename SignData func
    
    * Fix lint
    
    * Fix lint
    
    * Revert ReadTxCommandFlags
    
    Co-authored-by: Aaron Craelius <aaron@regen.network>
    Co-authored-by: Aaron Craelius <aaronc@users.noreply.github.com>

--- types/tx/signing/signature.go
+++ types/tx/signing/signature.go
@@ -1,6 +1,10 @@
 package signing
 
-import "github.com/tendermint/tendermint/crypto"
+import (
+	"fmt"
+
+	"github.com/tendermint/tendermint/crypto"
+)
 
 // SignatureV2 is a convenience type that is easier to use in application logic
 // than the protobuf SignerInfo's and raw signature bytes. It goes beyond the
@@ -14,4 +18,65 @@ type SignatureV2 struct {
 	// Data is the actual data of the signature which includes SignMode's and
 	// the signatures themselves for either single or multi-signatures.
 	Data SignatureData
+}
+
+// SignatureDataToProto converts a SignatureData to SignatureDescriptor_Data.
+// SignatureDescriptor_Data is considered an encoding type whereas SignatureData is used for
+// business logic.
+func SignatureDataToProto(data SignatureData) *SignatureDescriptor_Data {
+	switch data := data.(type) {
+	case *SingleSignatureData:
+		return &SignatureDescriptor_Data{
+			Sum: &SignatureDescriptor_Data_Single_{
+				Single: &SignatureDescriptor_Data_Single{
+					Mode:      data.SignMode,
+					Signature: data.Signature,
+				},
+			},
+		}
+	case *MultiSignatureData:
+		descDatas := make([]*SignatureDescriptor_Data, len(data.Signatures))
+
+		for j, d := range data.Signatures {
+			descDatas[j] = SignatureDataToProto(d)
+		}
+
+		return &SignatureDescriptor_Data{
+			Sum: &SignatureDescriptor_Data_Multi_{
+				Multi: &SignatureDescriptor_Data_Multi{
+					Bitarray:   data.BitArray,
+					Signatures: descDatas,
+				},
+			},
+		}
+	default:
+		panic(fmt.Errorf("unexpected case %+v", data))
+	}
+}
+
+// SignatureDataFromProto converts a SignatureDescriptor_Data to SignatureData.
+// SignatureDescriptor_Data is considered an encoding type whereas SignatureData is used for
+// business logic.
+func SignatureDataFromProto(descData *SignatureDescriptor_Data) SignatureData {
+	switch descData := descData.Sum.(type) {
+	case *SignatureDescriptor_Data_Single_:
+		return &SingleSignatureData{
+			SignMode:  descData.Single.Mode,
+			Signature: descData.Single.Signature,
+		}
+	case *SignatureDescriptor_Data_Multi_:
+		multi := descData.Multi
+		datas := make([]SignatureData, len(multi.Signatures))
+
+		for j, d := range multi.Signatures {
+			datas[j] = SignatureDataFromProto(d)
+		}
+
+		return &MultiSignatureData{
+			BitArray:   multi.Bitarray,
+			Signatures: datas,
+		}
+	default:
+		panic(fmt.Errorf("unexpected case %+v", descData))
+	}
 }

9a943bb36052fbad669386174b93788f15d6f6d2
atheesh <atheesh@vitwit.com>
2020-08-02 19:42:46 +0530

    grpc for bank module

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -10,11 +10,28 @@ for dir in $proto_dirs; do
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
+
+  # command to generate gRPC gateway
+  protoc \
+  -I "proto" \
+  -I "third_party/proto" \
+  --grpc-gateway_out=logtostderr=true:. \
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
+
+  # TODO
+  # command to generate swagger doc
+  # protoc \
+  # -I "proto" \
+  # -I "third_party/proto" \
+  # --swagger_out=logtostderr=true:. \
+  # $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
 
 # generate codec/testdata proto code
 protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil/testdata/proto.proto
+
+# protoc -I "proto" -I "third_party/proto" -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. proto/cosmos/bank/query.proto
 
 # move proto files to the right places
 cp -r github.com/cosmos/cosmos-sdk/* ./

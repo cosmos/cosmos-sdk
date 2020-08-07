7806e6cdee92abf5c67afdd226dabbbb86f3b19d
Federico Kunze <federico.kunze94@gmail.com>
2020-08-07 10:26:01 +0200

    unmarshaler interface

--- x/ibc/02-client/simulation/decoder.go
+++ x/ibc/02-client/simulation/decoder.go
@@ -5,25 +5,31 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/types/kv"
-	"github.com/cosmos/cosmos-sdk/x/ibc/02-client/keeper"
+	"github.com/cosmos/cosmos-sdk/x/ibc/02-client/exported"
 	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
 )
 
+// ClientUnmarshaler defines an interface for unmarshaling ICS02 interfaces.
+type ClientUnmarshaler interface {
+	MustUnmarshalClientState([]byte) exported.ClientState
+	MustUnmarshalConsensusState([]byte) exported.ConsensusState
+}
+
 // NewDecodeStore returns a decoder function closure that unmarshals the KVPair's
 // Value to the corresponding client type.
-func NewDecodeStore(k keeper.Keeper, kvA, kvB kv.Pair) (string, bool) {
+func NewDecodeStore(cdc ClientUnmarshaler, kvA, kvB kv.Pair) (string, bool) {
 	switch {
 	case bytes.HasPrefix(kvA.Key, host.KeyClientStorePrefix) && bytes.HasSuffix(kvA.Key, host.KeyClientState()):
-		clientStateA := k.MustUnmarshalClientState(kvA.Value)
-		clientStateB := k.MustUnmarshalClientState(kvB.Value)
+		clientStateA := cdc.MustUnmarshalClientState(kvA.Value)
+		clientStateB := cdc.MustUnmarshalClientState(kvB.Value)
 		return fmt.Sprintf("ClientState A: %v\nClientState B: %v", clientStateA, clientStateB), true
 
 	case bytes.HasPrefix(kvA.Key, host.KeyClientStorePrefix) && bytes.HasSuffix(kvA.Key, host.KeyClientType()):
 		return fmt.Sprintf("Client type A: %s\nClient type B: %s", string(kvA.Value), string(kvB.Value)), true
 
 	case bytes.HasPrefix(kvA.Key, host.KeyClientStorePrefix) && bytes.Contains(kvA.Key, []byte("consensusState")):
-		consensusStateA := k.MustUnmarshalConsensusState(kvA.Value)
-		consensusStateB := k.MustUnmarshalConsensusState(kvB.Value)
+		consensusStateA := cdc.MustUnmarshalConsensusState(kvA.Value)
+		consensusStateB := cdc.MustUnmarshalConsensusState(kvB.Value)
 		return fmt.Sprintf("ConsensusState A: %v\nConsensusState B: %v", consensusStateA, consensusStateB), true
 
 	default:

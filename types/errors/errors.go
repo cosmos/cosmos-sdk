d4715daf73edb3597235a9f9d64bb19d8ab3b6f2
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-08-07 17:59:30 -0400

    update godoc

--- types/errors/errors.go
+++ types/errors/errors.go
@@ -106,7 +106,8 @@ var (
 	// ErrInvalidType defines an error an invalid type.
 	ErrInvalidType = Register(RootCodespace, 29, "invalid type")
 
-	// ErrTxTimeoutHeight defines an error for when a tx timesout due to it's height
+	// ErrTxTimeoutHeight defines an error for when a tx is rejected out due to an
+	// explicitly set timeout height.
 	ErrTxTimeoutHeight = Register(RootCodespace, 30, "tx timeout height")
 
 	// ErrPanic is only set when we recover from a panic, so we know to

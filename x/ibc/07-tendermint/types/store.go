088060ec2366086c4b7bf96690157c6f7c426476
Colin Axner <colinaxner@berkeley.edu>
2020-08-11 14:53:26 +0200

    move con state retrieval to verify funcs

---
+++ x/ibc/07-tendermint/types/store.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	clientexported "github.com/cosmos/cosmos-sdk/x/ibc/02-client/exported"
+	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/02-client/types"
+	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
+)
+
+// GetConsensusState retrieves the consensus state from the client prefixed
+// store. An error is returned if the consensus state does not exist.
+func GetConsensusState(store sdk.KVStore, cdc codec.BinaryMarshaler, height uint64) (*ConsensusState, error) {
+	bz := store.Get(host.KeyConsensusState(height))
+	if bz == nil {
+		return nil, sdkerrors.Wrapf(
+			clienttypes.ErrConsensusStateNotFound,
+			"consensus state does not exist for height %d", height,
+		)
+	}
+
+	var consensusStateI clientexported.ConsensusState
+	if err := codec.UnmarshalAny(cdc, &consensusStateI, bz); err != nil {
+		return nil, err
+	}
+
+	consensusState, ok := consensusStateI.(*ConsensusState)
+	if !ok {
+		return nil, sdkerrors.Wrapf(
+			clienttypes.ErrInvalidConsensus,
+			"invalid consensus type %T, expected %T", consensusState, &ConsensusState{},
+		)
+	}
+
+	return consensusState, nil
+}

c121a62eabc30b585bdf29198750edfef5ed95ff
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-08-11 09:44:32 -0400

    auth: Update AccountRetriever

--- x/auth/types/account_retriever.go
+++ x/auth/types/account_retriever.go
@@ -1,67 +1,65 @@
 package types
 
 import (
-	"fmt"
+	context "context"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // AccountRetriever defines the properties of a type that can be used to
 // retrieve accounts.
 type AccountRetriever struct {
-	codec codec.JSONMarshaler
+	ir types.InterfaceRegistry
 }
 
-// NewAccountRetriever initialises a new AccountRetriever instance.
-func NewAccountRetriever(codec codec.JSONMarshaler) AccountRetriever {
-	return AccountRetriever{codec: codec}
+// NewAccountRetriever initializes a new AccountRetriever instance.
+func NewAccountRetriever(ir types.InterfaceRegistry) AccountRetriever {
+	return AccountRetriever{ir: ir}
 }
 
 // GetAccount queries for an account given an address and a block height. An
 // error is returned if the query or decoding fails.
-func (ar AccountRetriever) GetAccount(querier client.NodeQuerier, addr sdk.AccAddress) (AccountI, error) {
-	account, _, err := ar.GetAccountWithHeight(querier, addr)
+func (ar AccountRetriever) GetAccount(clientCtx client.Context, addr sdk.AccAddress) (AccountI, error) {
+	account, _, err := ar.GetAccountWithHeight(clientCtx, addr)
 	return account, err
 }
 
 // GetAccountWithHeight queries for an account given an address. Returns the
 // height of the query with the account. An error is returned if the query
 // or decoding fails.
-func (ar AccountRetriever) GetAccountWithHeight(querier client.NodeQuerier, addr sdk.AccAddress) (AccountI, int64, error) {
-	bs, err := ar.codec.MarshalJSON(QueryAccountRequest{Address: addr})
+func (ar AccountRetriever) GetAccountWithHeight(clientCtx client.Context, addr sdk.AccAddress) (AccountI, int64, error) {
+	queryClient := NewQueryClient(clientCtx)
+	res, err := queryClient.Account(context.Background(), &QueryAccountRequest{Address: addr})
 	if err != nil {
 		return nil, 0, err
 	}
 
-	bz, height, err := querier.QueryWithData(fmt.Sprintf("custom/%s/%s", QuerierRoute, QueryAccount), bs)
-	if err != nil {
-		return nil, height, err
+	var acc AccountI
+	if err := ar.ir.UnpackAny(res.Account, &acc); err != nil {
+		return nil, 0, err
 	}
 
-	var account AccountI
-	if err := ar.codec.UnmarshalJSON(bz, &account); err != nil {
-		return nil, height, err
-	}
-
-	return account, height, nil
+	return acc, 0, nil
 }
 
 // EnsureExists returns an error if no account exists for the given address else nil.
-func (ar AccountRetriever) EnsureExists(querier client.NodeQuerier, addr sdk.AccAddress) error {
-	if _, err := ar.GetAccount(querier, addr); err != nil {
+func (ar AccountRetriever) EnsureExists(clientCtx client.Context, addr sdk.AccAddress) error {
+	if _, err := ar.GetAccount(clientCtx, addr); err != nil {
 		return err
 	}
+
 	return nil
 }
 
 // GetAccountNumberSequence returns sequence and account number for the given address.
 // It returns an error if the account couldn't be retrieved from the state.
-func (ar AccountRetriever) GetAccountNumberSequence(nodeQuerier client.NodeQuerier, addr sdk.AccAddress) (uint64, uint64, error) {
-	acc, err := ar.GetAccount(nodeQuerier, addr)
+func (ar AccountRetriever) GetAccountNumberSequence(clientCtx client.Context, addr sdk.AccAddress) (uint64, uint64, error) {
+	acc, err := ar.GetAccount(clientCtx, addr)
 	if err != nil {
 		return 0, 0, err
 	}
+
 	return acc.GetAccountNumber(), acc.GetSequence(), nil
 }

0eb56f05f078506f8d5052b3938b91b913beb8c9
sahith-narahari <sahithnarahari@gmail.com>
2020-08-12 23:48:49 +0530

    Fix lint

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -63,8 +63,8 @@ func NewSimApp(val Validator) servertypes.Application {
 // Config defines the necessary configuration used to bootstrap and start an
 // in-process local testing network.
 type Config struct {
-	Codec            codec.Marshaler
-	LegacyAmino      *codec.LegacyAmino
+	Codec             codec.Marshaler
+	LegacyAmino       *codec.LegacyAmino
 	InterfaceRegistry codectypes.InterfaceRegistry
 
 	TxConfig         client.TxConfig
@@ -92,25 +92,25 @@ func DefaultConfig() Config {
 	encCfg := simapp.MakeEncodingConfig()
 
 	return Config{
-		Codec:            encCfg.Marshaler,
-		TxConfig:         encCfg.TxConfig,
-		LegacyAmino:      encCfg.Amino,
+		Codec:             encCfg.Marshaler,
+		TxConfig:          encCfg.TxConfig,
+		LegacyAmino:       encCfg.Amino,
 		InterfaceRegistry: encCfg.InterfaceRegistry,
-		AccountRetriever: authtypes.NewAccountRetriever(encCfg.Marshaler),
-		AppConstructor:   NewSimApp,
-		GenesisState:     simapp.ModuleBasics.DefaultGenesis(encCfg.Marshaler),
-		TimeoutCommit:    2 * time.Second,
-		ChainID:          "chain-" + tmrand.NewRand().Str(6),
-		NumValidators:    4,
-		BondDenom:        sdk.DefaultBondDenom,
-		MinGasPrices:     fmt.Sprintf("0.000006%s", sdk.DefaultBondDenom),
-		AccountTokens:    sdk.TokensFromConsensusPower(1000),
-		StakingTokens:    sdk.TokensFromConsensusPower(500),
-		BondedTokens:     sdk.TokensFromConsensusPower(100),
-		PruningStrategy:  storetypes.PruningOptionNothing,
-		CleanupDir:       true,
-		SigningAlgo:      string(hd.Secp256k1Type),
-		KeyringOptions:   []keyring.Option{},
+		AccountRetriever:  authtypes.NewAccountRetriever(encCfg.Marshaler),
+		AppConstructor:    NewSimApp,
+		GenesisState:      simapp.ModuleBasics.DefaultGenesis(encCfg.Marshaler),
+		TimeoutCommit:     2 * time.Second,
+		ChainID:           "chain-" + tmrand.NewRand().Str(6),
+		NumValidators:     4,
+		BondDenom:         sdk.DefaultBondDenom,
+		MinGasPrices:      fmt.Sprintf("0.000006%s", sdk.DefaultBondDenom),
+		AccountTokens:     sdk.TokensFromConsensusPower(1000),
+		StakingTokens:     sdk.TokensFromConsensusPower(500),
+		BondedTokens:      sdk.TokensFromConsensusPower(100),
+		PruningStrategy:   storetypes.PruningOptionNothing,
+		CleanupDir:        true,
+		SigningAlgo:       string(hd.Secp256k1Type),
+		KeyringOptions:    []keyring.Option{},
 	}
 }
 

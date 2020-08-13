15c70e5cd8d788cd28c3057b48c93dbfdcab1810
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-08-13 09:46:18 -0400

    address reviews

--- client/account_retriever.go
+++ client/account_retriever.go
@@ -9,13 +9,3 @@ type AccountRetriever interface {
 	EnsureExists(clientCtx Context, addr types.AccAddress) error
 	GetAccountNumberSequence(clientCtx Context, addr types.AccAddress) (accNum uint64, accSeq uint64, err error)
 }
-
-// NodeQuerier is an interface that is satisfied by types that provide the QueryWithData method
-type NodeQuerier interface {
-	// QueryWithData performs a query to a Tendermint node with the provided path
-	// and a data payload. It returns the result and height of the query upon success
-	// or an error if the query fails.
-	QueryWithData(path string, data []byte) ([]byte, int64, error)
-}
-
-var _ NodeQuerier = Context{}

aa9f6ad5d7fd028e3e3fbff08d819d7c78521030
atheesh <atheesh@vitwit.com>
2020-08-14 15:14:58 +0530

    review changes

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -10,12 +10,8 @@ for dir in $proto_dirs; do
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
-done
 
-# TODO update the command to include ./proto/ibc too and add the following commands in above loop
-proto_dirs=$(find ./proto/cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  # command to generate gRPC gateway
+  # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
   protoc \
   -I "proto" \
   -I "third_party/proto" \

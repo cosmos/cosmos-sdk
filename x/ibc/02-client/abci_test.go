09feb5d7c14559467cb03d3609401f3eca8fd66c
Federico Kunze <federico.kunze94@gmail.com>
2020-08-14 19:08:55 +0200

    fix test

--- x/ibc/02-client/abci_test.go
+++ x/ibc/02-client/abci_test.go
@@ -5,11 +5,6 @@ import (
 
 	ibctesting "github.com/cosmos/cosmos-sdk/x/ibc/testing"
 	"github.com/stretchr/testify/suite"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/simapp"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type ClientTestSuite struct {

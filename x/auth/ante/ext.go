a3dee03d0b84b1d4dc30ce4b7fbb62a5777cfbdf
Aaron Craelius <aaronc@users.noreply.github.com>
2020-08-14 13:11:37 -0400

    Docs

--- x/auth/ante/ext.go
+++ x/auth/ante/ext.go
@@ -11,18 +11,20 @@ type HasExtensionOptionsTx interface {
 	GetNonCriticalExtensionOptions() []*codectypes.Any
 }
 
-// RejectExtensionOptionsDecorator is a AnteDecorator that rejects all extension
+// RejectExtensionOptionsDecorator is an AnteDecorator that rejects all extension
 // options which can optionally be included in protobuf transactions. Users that
 // need extension options should create a custom AnteHandler chain that handles
 // needed extension options properly and rejects unknown ones.
 type RejectExtensionOptionsDecorator struct{}
 
+// NewRejectExtensionOptionsDecorator creates a new RejectExtensionOptionsDecorator
 func NewRejectExtensionOptionsDecorator() RejectExtensionOptionsDecorator {
 	return RejectExtensionOptionsDecorator{}
 }
 
 var _ types.AnteDecorator = RejectExtensionOptionsDecorator{}
 
+// AnteHandle implements the AnteDecorator.AnteHandle method
 func (r RejectExtensionOptionsDecorator) AnteHandle(ctx types.Context, tx types.Tx, simulate bool, next types.AnteHandler) (newCtx types.Context, err error) {
 	if hasExtOptsTx, ok := tx.(HasExtensionOptionsTx); ok {
 		if len(hasExtOptsTx.GetExtensionOptions()) != 0 {

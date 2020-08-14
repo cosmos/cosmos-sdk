6914e053826ae6fef49e3f4660c0540ad8afa6db
Federico Kunze <federico.kunze94@gmail.com>
2020-08-14 11:37:17 +0200

    address a few comments from review

--- x/ibc-transfer/keeper/genesis_test.go
+++ x/ibc-transfer/keeper/genesis_test.go
@@ -7,23 +7,31 @@ import (
 )
 
 func (suite *KeeperTestSuite) TestGenesis() {
-	var trace string
+	var (
+		traceStr string
+		traces   types.Traces
+	)
+
 	for i := 0; i < 5; i++ {
 		prefix := fmt.Sprintf("transfer/channelToChain%d", i)
 		if i == 0 {
-			trace = prefix
+			traceStr = prefix
 		} else {
-			trace = prefix + trace
+			traceStr = prefix + "/" + traceStr
 		}
 
 		denomTrace := types.DenomTrace{
 			BaseDenom: "uatom",
-			Trace:     trace,
+			Trace:     traceStr,
 		}
+		traces = append(types.Traces{denomTrace}, traces...)
 		suite.chainA.App.TransferKeeper.SetDenomTrace(suite.chainA.GetContext(), denomTrace)
 	}
 
 	genesis := suite.chainA.App.TransferKeeper.ExportGenesis(suite.chainA.GetContext())
+
+	suite.Require().Equal(types.PortID, genesis.PortId)
+	suite.Require().Equal(traces, genesis.DenomTraces)
 
 	suite.Require().NotPanics(func() {
 		suite.chainA.App.TransferKeeper.InitGenesis(suite.chainA.GetContext(), *genesis)

42297414193b03754522ec206a742409b7770d3d
puneet2019 <59960662+puneet2019@users.noreply.github.com>
2020-08-14 08:48:41 +0530

    fixed interactive mode key add.
    key addition of interactive mode failed while being asked to generate new seed.

--- client/keys/add.go
+++ client/keys/add.go
@@ -226,18 +226,21 @@ func RunAddCmd(cmd *cobra.Command, args []string, kb keyring.Keyring, inBuf *buf
 	var mnemonic, bip39Passphrase string
 
 	recover, _ := cmd.Flags().GetBool(flagRecover)
-	if interactive || recover {
-		bip39Message := "Enter your bip39 mnemonic"
-		if !recover {
-			bip39Message = "Enter your bip39 mnemonic, or hit enter to generate one."
-		}
-
+	if recover {
 		mnemonic, err = input.GetString(bip39Message, inBuf)
 		if err != nil {
 			return err
 		}
-
 		if !bip39.IsMnemonicValid(mnemonic) {
+			return errors.New("invalid mnemonic")
+		}
+	} else if interactive {
+		bip39Message = "Enter your bip39 mnemonic, or hit enter to generate one."
+		mnemonic, err = input.GetString(bip39Message, inBuf)
+		if err != nil {
+			return err
+		}
+		if !bip39.IsMnemonicValid(mnemonic) && mnemonic != "" {
 			return errors.New("invalid mnemonic")
 		}
 	}

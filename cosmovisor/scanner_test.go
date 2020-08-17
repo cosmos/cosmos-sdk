07a9a7ac761f4d346790a1fdc80c5e3c1e7b5190
Jonathan Gimeno <jgimeno@gmail.com>
2020-08-17 18:07:06 +0200

    remove unused fields in update info 2

--- cosmovisor/scanner_test.go
+++ cosmovisor/scanner_test.go
@@ -20,24 +20,21 @@ func TestWaitForInfo(t *testing.T) {
 		"match height with no info": {
 			write: []string{"first line\n", `UPGRADE "myname" NEEDED at height: 123: `, "\nnext line\n"},
 			expectUpgrade: &UpgradeInfo{
-				Name:   "myname",
-				Height: 123,
-				Info:   "",
+				Name: "myname",
+				Info: "",
 			},
 		},
 		"match height with info": {
 			write: []string{"first line\n", `UPGRADE "take2" NEEDED at height: 123:   DownloadData here!`, "\nnext line\n"},
 			expectUpgrade: &UpgradeInfo{
-				Name:   "take2",
-				Height: 123,
-				Info:   "DownloadData",
+				Name: "take2",
+				Info: "DownloadData",
 			},
 		},
 		"match time with no info": {
 			write: []string{"first line\n", `UPGRADE "timer" NEEDED at time: 2020-04-01T11:22:33Z:   `, "\nnext line\n"},
 			expectUpgrade: &UpgradeInfo{
 				Name: "timer",
-				Time: "2020-04-01T11:22:33Z",
 				Info: "",
 			},
 		},
@@ -45,16 +42,14 @@ func TestWaitForInfo(t *testing.T) {
 			write: []string{"first line\n", `UPGRADE "april" NEEDED at time: 2020-04-01T11:22:33Z: https://april.foo.rs/hahaha  `, "\nnext line\n"},
 			expectUpgrade: &UpgradeInfo{
 				Name: "april",
-				Time: "2020-04-01T11:22:33Z",
 				Info: "https://april.foo.rs/hahaha",
 			},
 		},
 		"chunks": {
 			write: []string{"first l", "ine\nERROR 2020-02-03T11:22:33Z: UPGRADE ", `"split" NEEDED at height: `, "789:   {\"foo\":123} asgsdg", "  \n LOG: next line"},
 			expectUpgrade: &UpgradeInfo{
-				Name:   "split",
-				Height: 789,
-				Info:   `{"foo":123}`,
+				Name: "split",
+				Info: `{"foo":123}`,
 			},
 		},
 	}

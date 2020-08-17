24bc89183efec81563c6f48cffdec5758eb16acc
SaReN <sahithnarahari@gmail.com>
2020-08-18 03:02:16 +0530

    Fix add-genesis-account in simd (#7069)
    * Update genaccounts to use encoding/json
    
    * Fix add genesis accounts
    
    * Add test for genaccounts
    
    * Code cleanup

--- x/genutil/client/cli/gentx_test.go
+++ x/genutil/client/cli/gentx_test.go
@@ -1,4 +1,4 @@
-package cli
+package cli_test
 
 import (
 	"context"
@@ -17,6 +17,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
@@ -51,7 +52,7 @@ func (s *IntegrationTestSuite) TestGenTxCmd() {
 	dir, clean := testutil.NewTestCaseDir(s.T())
 	defer clean()
 
-	cmd := GenTxCmd(
+	cmd := cli.GenTxCmd(
 		simapp.ModuleBasics,
 		val.ClientCtx.TxConfig, banktypes.GenesisBalancesIterator{}, val.ClientCtx.HomeDir)
 
@@ -63,7 +64,7 @@ func (s *IntegrationTestSuite) TestGenTxCmd() {
 
 	genTxFile := filepath.Join(dir, "myTx")
 	cmd.SetArgs([]string{
-		fmt.Sprintf("--%s=%s", flagChainID, s.network.Config.ChainID),
+		fmt.Sprintf("--%s=%s", flags.FlagChainID, s.network.Config.ChainID),
 		fmt.Sprintf("--%s=%s", flags.FlagOutputDocument, genTxFile),
 		val.Moniker,
 	})

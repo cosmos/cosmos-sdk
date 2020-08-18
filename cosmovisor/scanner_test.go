40e7baf5bed4e69c611c06d92235f41b7c079127
Jonathan Gimeno <jgimeno@gmail.com>
2020-08-18 15:23:07 +0200

    simplify tests

--- cosmovisor/scanner_test.go
+++ cosmovisor/scanner_test.go
@@ -17,39 +17,18 @@ func TestWaitForInfo(t *testing.T) {
 		"no match": {
 			write: []string{"some", "random\ninfo\n"},
 		},
-		"match height with no info": {
+		"match name with no info": {
 			write: []string{"first line\n", `UPGRADE "myname" NEEDED at height: 123: `, "\nnext line\n"},
 			expectUpgrade: &UpgradeInfo{
 				Name: "myname",
 				Info: "",
 			},
 		},
-		"match height with info": {
+		"match name with info": {
 			write: []string{"first line\n", `UPGRADE "take2" NEEDED at height: 123:   DownloadData here!`, "\nnext line\n"},
 			expectUpgrade: &UpgradeInfo{
 				Name: "take2",
 				Info: "DownloadData",
-			},
-		},
-		"match time with no info": {
-			write: []string{"first line\n", `UPGRADE "timer" NEEDED at time: 2020-04-01T11:22:33Z:   `, "\nnext line\n"},
-			expectUpgrade: &UpgradeInfo{
-				Name: "timer",
-				Info: "",
-			},
-		},
-		"match time with info": {
-			write: []string{"first line\n", `UPGRADE "april" NEEDED at time: 2020-04-01T11:22:33Z: https://april.foo.rs/hahaha  `, "\nnext line\n"},
-			expectUpgrade: &UpgradeInfo{
-				Name: "april",
-				Info: "https://april.foo.rs/hahaha",
-			},
-		},
-		"chunks": {
-			write: []string{"first l", "ine\nERROR 2020-02-03T11:22:33Z: UPGRADE ", `"split" NEEDED at height: `, "789:   {\"foo\":123} asgsdg", "  \n LOG: next line"},
-			expectUpgrade: &UpgradeInfo{
-				Name: "split",
-				Info: `{"foo":123}`,
 			},
 		},
 	}

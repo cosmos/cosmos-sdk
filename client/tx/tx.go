b4da972bcc8ddca5c360c92cdaa091f0a536a5ea
blushi <marie.gauthier63@gmail.com>
2020-08-19 10:43:57 +0200

    Use TxConfig.TxJSONEncoder

--- client/tx/tx.go
+++ client/tx/tx.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	"github.com/gogo/protobuf/jsonpb"
-	"github.com/gogo/protobuf/proto"
 	"github.com/spf13/pflag"
 	"github.com/tendermint/tendermint/crypto"
 
@@ -101,12 +100,7 @@ func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
 	}
 
 	if !clientCtx.SkipConfirm {
-		msg, ok := tx.(proto.Message)
-		if !ok {
-			return fmt.Errorf("can't proto marshal %T", tx)
-		}
-
-		out, err := clientCtx.JSONMarshaler.MarshalJSON(msg)
+		out, err := clientCtx.TxConfig.TxJSONEncoder()(tx.GetTx())
 		if err != nil {
 			return err
 		}
@@ -114,7 +108,7 @@ func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
 		_, _ = fmt.Fprintf(os.Stderr, "%s\n\n", out)
 
 		buf := bufio.NewReader(os.Stdin)
-		ok, err = input.GetConfirmation("confirm transaction before signing and broadcasting", buf, os.Stderr)
+		ok, err := input.GetConfirmation("confirm transaction before signing and broadcasting", buf, os.Stderr)
 
 		if err != nil || !ok {
 			_, _ = fmt.Fprintf(os.Stderr, "%s\n", "cancelled transaction")

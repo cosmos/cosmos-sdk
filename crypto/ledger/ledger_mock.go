dccf1ef803edf75df1e24340050b7d26ce3fe8ee
sahith-narahari <sahithnarahari@gmail.com>
2020-08-20 00:25:20 +0530

    Update ledger_mock

--- crypto/ledger/ledger_mock.go
+++ crypto/ledger/ledger_mock.go
@@ -14,6 +14,7 @@ import (
 	bip39 "github.com/cosmos/go-bip39"
 
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	csecp256k1 "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	"github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -76,7 +77,7 @@ func (mock LedgerSECP256K1Mock) GetAddressPubKeySECP256K1(derivationPath []uint3
 		return nil, "", fmt.Errorf("error parsing public key: %v", err)
 	}
 
-	compressedPublicKey := make(tmsecp256k1.PubKey, tmsecp256k1.PubKeySize)
+	compressedPublicKey := make(csecp256k1.PubKey, csecp256k1.PubKeySize)
 	copy(compressedPublicKey, cmp.SerializeCompressed())
 
 	// Generate the bech32 addr using existing tmcrypto/etc.

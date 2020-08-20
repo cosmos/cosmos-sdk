2395231cfa5ba7fe9355ca51d9a9ddf3e9d100da
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-08-20 09:19:16 +0200

    Update x/ibc/02-client/keeper/keeper_test.go

--- x/ibc/02-client/keeper/keeper_test.go
+++ x/ibc/02-client/keeper/keeper_test.go
@@ -175,7 +175,7 @@ func (suite *KeeperTestSuite) TestValidateSelfClient() {
 		if tc.expPass {
 			suite.Require().NoError(err, "expected valid client for case: %s", tc.name)
 		} else {
-			suite.Require().Error(err, "expected invalid client for case: %s, ValidatClient passed", tc.name)
+			suite.Require().Error(err, "expected invalid client for case: %s", tc.name)
 		}
 	}
 }

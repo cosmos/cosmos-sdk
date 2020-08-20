da96310d571e38d4962bbaf093a7b9d8de44df70
atheesh <atheesh@vitwit.com>
2020-08-20 18:46:56 +0530

    fix tests

--- tests/mocks/types_module_module.go
+++ tests/mocks/types_module_module.go
@@ -15,6 +15,7 @@ import (
 	grpc "github.com/gogo/protobuf/grpc"
 	gomock "github.com/golang/mock/gomock"
 	mux "github.com/gorilla/mux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	cobra "github.com/spf13/cobra"
 	types1 "github.com/tendermint/tendermint/abci/types"
 )
@@ -118,6 +119,18 @@ func (m *MockAppModuleBasic) RegisterRESTRoutes(arg0 client.Context, arg1 *mux.R
 func (mr *MockAppModuleBasicMockRecorder) RegisterRESTRoutes(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RegisterRESTRoutes", reflect.TypeOf((*MockAppModuleBasic)(nil).RegisterRESTRoutes), arg0, arg1)
+}
+
+// RegisterGRPCRoutes mocks base method
+func (m *MockAppModuleBasic) RegisterGRPCRoutes(arg0 client.Context, arg1 *runtime.ServeMux) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "RegisterGRPCRoutes", arg0, arg1)
+}
+
+// RegisterGRPCRoutes indicates an expected call of RegisterGRPCRoutes
+func (mr *MockAppModuleBasicMockRecorder) RegisterGRPCRoutes(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RegisterGRPCRoutes", reflect.TypeOf((*MockAppModuleBasic)(nil).RegisterGRPCRoutes), arg0, arg1)
 }
 
 // GetTxCmd mocks base method

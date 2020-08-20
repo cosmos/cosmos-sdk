9ec6c225383d0c4df3d719e4b6b582051880a93c
Federico Kunze <federico.kunze94@gmail.com>
2020-08-20 17:43:29 +0200

    params and genesis

---
+++ x/ibc-transfer/simulation/params.go
@@ -0,0 +1,32 @@
+package simulation
+
+// DONTCOVER
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/ibc-transfer/types"
+)
+
+// ParamChanges defines the parameters that can be modified by param change proposals
+// on the simulation
+func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
+	return []simtypes.ParamChange{
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeySendEnabled),
+			func(r *rand.Rand) string {
+				sendEnabled := RadomEnabled(r)
+				return fmt.Sprintf("%s", types.ModuleCdc.MustMarshalJSON(sendEnabled))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyReceiveEnabled),
+			func(r *rand.Rand) string {
+				receiveEnabled := RadomEnabled(r)
+				return fmt.Sprintf("%s", types.ModuleCdc.MustMarshalJSON(receiveEnabled))
+			},
+		),
+	}
+}

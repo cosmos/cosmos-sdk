dd1a560f59fcce7d1e51f5f681136742a7137a8f
atheesh <atheesh@vitwit.com>
2020-08-20 16:34:33 +0530

    updated tests

--- x/bank/client/rest/query_test.go
+++ x/bank/client/rest/query_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 type IntegrationTestSuite struct {
@@ -130,7 +131,6 @@ func (s *IntegrationTestSuite) TestTotalSupplyHandlerFn() {
 	}
 }
 
-// TODO move these Gateway tests into a separate file
 func (s *IntegrationTestSuite) TestTotalSupplyGRPCHandler() {
 	val := s.network.Validators[0]
 	baseURL := val.APIAddress
@@ -143,27 +143,30 @@ func (s *IntegrationTestSuite) TestTotalSupplyGRPCHandler() {
 	}{
 		{
 			"test GRPC total supply",
-			fmt.Sprintf("%s/cosmos/bank/v1beta1/supply?height=1", baseURL),
-			&sdk.Coins{},
-			sdk.NewCoins(
-				sdk.NewCoin(fmt.Sprintf("%stoken", val.Moniker), s.cfg.AccountTokens),
-				sdk.NewCoin(s.cfg.BondDenom, s.cfg.StakingTokens.Add(sdk.NewInt(10))),
-			),
+			fmt.Sprintf("%s/cosmos/bank/v1beta1/supply", baseURL),
+			&types.QueryTotalSupplyResponse{},
+			&types.QueryTotalSupplyResponse{
+				Supply: sdk.NewCoins(
+					sdk.NewCoin(fmt.Sprintf("%stoken", val.Moniker), s.cfg.AccountTokens),
+					sdk.NewCoin(s.cfg.BondDenom, s.cfg.StakingTokens.Add(sdk.NewInt(40))),
+				),
+			},
 		},
 		{
 			"GRPC total supply of a specific denom",
-			fmt.Sprintf("%s/cosmos/bank/v1beta1/supply/%s?height=1", baseURL, s.cfg.BondDenom),
-			&sdk.Coins{},
-			sdk.NewCoins(
-				sdk.NewCoin(fmt.Sprintf("%stoken", val.Moniker), s.cfg.AccountTokens),
-				sdk.NewCoin(s.cfg.BondDenom, s.cfg.StakingTokens.Add(sdk.NewInt(10))),
-			),
+			fmt.Sprintf("%s/cosmos/bank/v1beta1/supply/%s", baseURL, s.cfg.BondDenom),
+			&types.QuerySupplyOfResponse{},
+			&types.QuerySupplyOfResponse{
+				Amount: sdk.NewCoin(s.cfg.BondDenom, s.cfg.StakingTokens.Add(sdk.NewInt(40))),
+			},
 		},
 		{
 			"GRPC total supply of a bogus denom",
-			fmt.Sprintf("%s/cosmos/bank/v1beta1/supply/foobar?height=1", baseURL),
-			&sdk.Coin{},
-			sdk.NewCoin("foobar", sdk.ZeroInt()),
+			fmt.Sprintf("%s/cosmos/bank/v1beta1/supply/foobar", baseURL),
+			&types.QuerySupplyOfResponse{},
+			&types.QuerySupplyOfResponse{
+				Amount: sdk.NewCoin("foobar", sdk.ZeroInt()),
+			},
 		},
 	}
 
@@ -171,17 +174,10 @@ func (s *IntegrationTestSuite) TestTotalSupplyGRPCHandler() {
 		tc := tc
 		s.Run(tc.name, func() {
 			resp, err := rest.GetRequest(tc.url)
-			// debug
-			fmt.Println("url", tc.url)
-			fmt.Println("resp", string(resp))
 			s.Require().NoError(err)
 
-			//bz, err := rest.ParseResponseWithHeight(val.ClientCtx.LegacyAmino, resp)
-			//
-			//fmt.Println("bz", bz)
-			//s.Require().NoError(err)
-			//s.Require().NoError(val.ClientCtx.LegacyAmino.UnmarshalJSON(resp, tc.respType))
-			s.Require().Equal(tc.expected.String(), string(resp))
+			s.Require().NoError(val.ClientCtx.LegacyAmino.UnmarshalJSON(resp, tc.respType))
+			s.Require().Equal(tc.expected.String(), tc.respType.String())
 		})
 	}
 }

1d66082b94a8f8520fb00c0a8bc998bd28c20925
Federico Kunze <federico.kunze94@gmail.com>
2020-08-20 15:43:50 +0200

    final gRPC tests

--- x/ibc/02-client/keeper/grpc_query_test.go
+++ x/ibc/02-client/keeper/grpc_query_test.go
@@ -13,6 +13,70 @@ import (
 	commitmenttypes "github.com/cosmos/cosmos-sdk/x/ibc/23-commitment/types"
 )
 
+func (suite *KeeperTestSuite) TestQueryClientState() {
+	var (
+		req            *types.QueryClientStateRequest
+		expClientState *codectypes.Any
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{"invalid clientID",
+			func() {
+				req = &types.QueryClientStateRequest{}
+			},
+			false,
+		},
+		{"client not found",
+			func() {
+				req = &types.QueryClientStateRequest{
+					ClientId: testClientID,
+				}
+			},
+			false,
+		},
+		{
+			"success",
+			func() {
+				clientState := ibctmtypes.NewClientState(testChainID, ibctmtypes.DefaultTrustLevel, trustingPeriod, ubdPeriod, maxClockDrift, 0, commitmenttypes.GetSDKSpecs())
+				suite.keeper.SetClientState(suite.ctx, testClientID, clientState)
+
+				var err error
+				expClientState, err = types.PackClientState(clientState)
+				suite.Require().NoError(err)
+
+				req = &types.QueryClientStateRequest{
+					ClientId: testClientID,
+				}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			res, err := suite.queryClient.ClientState(ctx, req)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(res)
+
+				expClientState.ClearCachedValue()
+				suite.Require().Equal(expClientState, res.ClientState)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
 func (suite *KeeperTestSuite) TestQueryClientStates() {
 	var (
 		req             *types.QueryClientStatesRequest
@@ -83,9 +147,111 @@ func (suite *KeeperTestSuite) TestQueryClientStates() {
 				for i := range expClientStates {
 					suite.Require().Equal(expClientStates[i].ClientId, res.ClientStates[i].ClientId)
 					suite.Require().NotNil(res.ClientStates[i].ClientState)
-					// can't use suite.Require().Equal because of cached value mismatch
-					suite.Require().True(res.ClientStates[i].ClientState.Equal(expClientStates[i].ClientState))
+					expClientStates[i].ClientState.ClearCachedValue()
+					suite.Require().Equal(expClientStates[i].ClientState, res.ClientStates[i].ClientState)
 				}
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestQueryConsensusState() {
+	var (
+		req               *types.QueryConsensusStateRequest
+		expConsensusState *codectypes.Any
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"invalid clientID",
+			func() {
+				req = &types.QueryConsensusStateRequest{}
+			},
+			false,
+		},
+		{
+			"invalid height",
+			func() {
+				req = &types.QueryConsensusStateRequest{
+					ClientId:     testClientID,
+					Height:       0,
+					LatestHeight: false,
+				}
+			},
+			false,
+		},
+		{
+			"consensus state not found",
+			func() {
+				req = &types.QueryConsensusStateRequest{
+					ClientId:     testClientID,
+					LatestHeight: true,
+				}
+			},
+			false,
+		},
+		{
+			"success latest height",
+			func() {
+				clientState := ibctmtypes.NewClientState(testChainID, ibctmtypes.DefaultTrustLevel, trustingPeriod, ubdPeriod, maxClockDrift, testClientHeight, commitmenttypes.GetSDKSpecs())
+				cs := ibctmtypes.NewConsensusState(
+					suite.consensusState.Timestamp, commitmenttypes.NewMerkleRoot([]byte("hash1")), suite.consensusState.GetHeight(), nil,
+				)
+				suite.keeper.SetClientState(suite.ctx, testClientID, clientState)
+				suite.keeper.SetClientConsensusState(suite.ctx, testClientID, suite.consensusState.GetHeight(), cs)
+
+				var err error
+				expConsensusState, err = types.PackConsensusState(cs)
+				suite.Require().NoError(err)
+
+				req = &types.QueryConsensusStateRequest{
+					ClientId:     testClientID,
+					LatestHeight: true,
+				}
+			},
+			true,
+		},
+		{
+			"success with height",
+			func() {
+				cs := ibctmtypes.NewConsensusState(
+					suite.consensusState.Timestamp, commitmenttypes.NewMerkleRoot([]byte("hash1")), suite.consensusState.GetHeight(), nil,
+				)
+				suite.keeper.SetClientConsensusState(suite.ctx, testClientID, testClientHeight, cs)
+
+				var err error
+				expConsensusState, err = types.PackConsensusState(cs)
+				suite.Require().NoError(err)
+
+				req = &types.QueryConsensusStateRequest{
+					ClientId: testClientID,
+					Height:   testClientHeight,
+				}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			res, err := suite.queryClient.ConsensusState(ctx, req)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(res)
+
+				expConsensusState.ClearCachedValue()
+				suite.Require().Equal(expConsensusState, res.ConsensusState)
 			} else {
 				suite.Require().Error(err)
 			}
@@ -180,8 +346,8 @@ func (suite *KeeperTestSuite) TestQueryConsensusStates() {
 				suite.Require().Equal(len(expConsensusStates), len(res.ConsensusStates))
 				for i := range expConsensusStates {
 					suite.Require().NotNil(res.ConsensusStates[i])
-					// can't use suite.Require().Equal because of cached value mismatch
-					suite.Require().True(res.ConsensusStates[i].Equal(expConsensusStates[i]))
+					expConsensusStates[i].ClearCachedValue()
+					suite.Require().Equal(expConsensusStates[i], res.ConsensusStates[i])
 				}
 			} else {
 				suite.Require().Error(err)

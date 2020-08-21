c1ce287a6453d1bcb1b4277b5a8b850630198cb9
anilCSE <anil@vitwit.com>
2020-08-22 01:09:29 +0530

    remove files

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 proto_files=''
+query_files=''
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
@@ -22,15 +23,32 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 
   query_file=$(find "${dir}" -maxdepth 1 -name 'query.proto')
   if [[ ! -z "$query_file" ]]; then
-    protoc  \
-    -I "proto" \
-    -I "third_party/proto" \
-    "$query_file" \
-    --swagger_out=logtostderr=true,fqn_for_swagger_name=true,allow_merge=true,merge_file_name=apidoc:.
+    query_files=${query_files}" ${query_file}"
+#    protoc  \
+#    -I "proto" \
+#    -I "third_party/proto" \
+#    "$query_file" \
+#    --go_out=plugins=grpc:pkg \
+#    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:.
   fi
+
+
 
   proto_files=${proto_files}" ${dir:2}/*.proto"
 done
+
+protoc  \
+    -I "proto" \
+    -I "third_party/proto" \
+    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
+    --go_out=plugins=grpc:pkg $(find ./ -name 'query.proto')
+
+#echo $query_files
+#protoc  \
+#-I "proto" \
+#-I "third_party/proto" \
+#$query_files \
+#--swagger_out=logtostderr=true,include_package_in_tags=true,fqn_for_swagger_name=true,allow_merge=true:.
 
 # generate codec/testdata proto code
 protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
@@ -38,10 +56,6 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil
 
 # move proto files to the right places
 cp -r github.com/cosmos/cosmos-sdk/* ./
-cp -r cosmos/* ./x/
-cp -r ibc/* ./x/ibc/
 rm -rf github.com
-rm -rf cosmos
-rm -rf ibc
 
-swagger mixin ./x/auth/**/query.swagger.json ./x/**/**/*swagger.json
+#swagger mixin ./x/auth/**/query.swagger.json ./x/**/**/*swagger.json

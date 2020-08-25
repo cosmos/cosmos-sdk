3b5d0f2625b0bc79da4196e02051ccd27c5ab7a7
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-08-25 10:38:33 +0200

    Update x/capability/module.go

--- x/capability/module.go
+++ x/capability/module.go
@@ -69,7 +69,7 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONMarshaler, config client.TxE
 	return genState.Validate()
 }
 
-// RegisterRESTRoutes registers the gRPC Gateway routes for the capability module.
+// RegisterRESTRoutes registers the capability module's REST service handlers.
 func (a AppModuleBasic) RegisterRESTRoutes(_ client.Context, _ *mux.Router) {}
 
 // RegisterGRPCRoutes registers the gRPC Gateway routes for the capability module.

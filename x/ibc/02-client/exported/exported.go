cde1aea98166f1e0500c38f227afe2469bcf78a5
Aditya Sripal <adityasripal@gmail.com>
2020-08-25 18:58:52 -0400

    start using Height in 02-client

--- x/ibc/02-client/exported/exported.go
+++ x/ibc/02-client/exported/exported.go
@@ -2,7 +2,6 @@ package exported
 
 import (
 	"encoding/json"
-	"fmt"
 
 	ics23 "github.com/confio/ics23/go"
 
@@ -173,6 +172,16 @@ type MsgUpdateClient interface {
 	GetHeader() Header
 }
 
+// Height is a wrapper interface over clienttypes.Height
+// all clients must use the concrete implementation in types
+type Height interface {
+	IsZero() bool
+	LT(Height) bool
+	EQ(Height) bool
+	GT(Height) bool
+	Valid() bool
+}
+
 // ClientType defines the type of the consensus algorithm
 type ClientType byte
 
@@ -234,90 +243,4 @@ func ClientTypeFromString(clientType string) ClientType {
 	default:
 		return 0
 	}
-}
-
-// Height is a monotonically increasing data type
-// that can be compared against another Height for the purposes of updating and freezing clients
-//
-// Normally the EpochHeight is incremented at each height while keeping epoch number the same
-// However some consensus algorithms may choose to reset the height in certain conditions
-// e.g. hard forks, state-machine breaking changes
-// In these cases, the epoch number is incremented so that height continues to be monitonically increasing
-// even as the EpochHeight gets reset
-type Height struct {
-	EpochNumber uint64 `json:"epoch_number" yaml:"epoch_number"` // The current epoch of the chain
-	EpochHeight uint64 `json:"epoch_height" yaml:"epoch_height"` // The height of the chain at the given epoch
-}
-
-// NewHeight creates a new client Height instance from the epoch values.
-func NewHeight(epochNumber, epochHeight uint64) Height {
-	return Height{
-		EpochNumber: epochNumber,
-		EpochHeight: epochHeight,
-	}
-}
-
-/// Compare implements a method to compare two heights. When comparing two heights a, b
-// we can call a.Compare(b) which will return
-// -1 if a < b
-// 0  if a = b
-// 1  if a > b
-//
-// It first compares based on epoch numbers, whichever has the higher epoch number is the higher height
-// If epoch number is the same, then the epoch height is compared
-func (h Height) Compare(other Height) int64 {
-	var cmp int64
-	if h.EpochNumber != other.EpochNumber {
-		cmp = int64(h.EpochNumber) - int64(other.EpochNumber)
-	} else {
-		cmp = int64(h.EpochHeight) - int64(other.EpochHeight)
-	}
-	if cmp < 0 {
-		return -1
-	} else if cmp > 0 {
-		return 1
-	}
-	return 0
-}
-
-// LT Helper comparison function returns true if h < other
-func (h Height) LT(other Height) bool {
-	return h.Compare(other) == -1
-}
-
-// GT Helper comparison function returns true if h > other
-func (h Height) GT(other Height) bool {
-	return h.Compare(other) == 1
-}
-
-// EQ Helper comparison function returns true if h == other
-func (h Height) EQ(other Height) bool {
-	return h.Compare(other) == 0
-}
-
-// Decrement will return a decremented height from the given height. If this is not possible,
-// an error is returned
-// Decrement will return a new height with the EpochHeight decremented
-// If the EpochHeight is already at lowest value (1), then false success flag is returend
-func (h Height) Decrement() (decremented Height, success bool) {
-	if h.EpochHeight <= 1 {
-		return Height{}, false
-	}
-	return NewHeight(h.EpochNumber, h.EpochHeight-1), true
-}
-
-// Valid returns true if height is valid, false otherwise
-// Returns false if EpochHeight is 0
-func (h Height) Valid() bool {
-	return h.EpochHeight != 0
-}
-
-// String implements the fmt.Stringer interface
-func (h Height) String() string {
-	return fmt.Sprintf("epoch-%d_height-%d", h.EpochNumber, h.EpochHeight)
-}
-
-// IsZero returns true if height epoch and epoch-height are both 0
-func (h Height) IsZero() bool {
-	return h.EpochNumber == 0 && h.EpochHeight == 0
 }
---
+++ x/ibc/02-client/types/height_test.go
@@ -0,0 +1,62 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompareHeights(t *testing.T) {
+	testCases := []struct {
+		name        string
+		height1     Height
+		height2     Height
+		compareSign int64
+	}{
+		{"epoch number 1 is lesser", NewHeight(1, 3), NewHeight(3, 4), -1},
+		{"epoch number 1 is greater", NewHeight(7, 5), NewHeight(4, 5), 1},
+		{"epoch height 1 is lesser", NewHeight(3, 4), NewHeight(3, 9), -1},
+		{"epoch height 1 is greater", NewHeight(3, 8), NewHeight(3, 3), 1},
+		{"height is equal", NewHeight(4, 4), NewHeight(4, 4), 0},
+	}
+
+	for i, tc := range testCases {
+		compare := tc.height1.Compare(tc.height2)
+
+		switch tc.compareSign {
+		case -1:
+			require.True(t, compare == -1, "case %d: %s should return negative value on comparison, got: %d",
+				i, tc.name, compare)
+		case 0:
+			require.True(t, compare == 0, "case %d: %s should return zero on comparison, got: %d",
+				i, tc.name, compare)
+		case 1:
+			require.True(t, compare == 1, "case %d: %s should return positive value on comparison, got: %d",
+				i, tc.name, compare)
+		}
+	}
+}
+
+func TestDecrement(t *testing.T) {
+	validDecrement := NewHeight(3, 3)
+	expected := NewHeight(3, 2)
+
+	actual, success := validDecrement.Decrement()
+	require.Equal(t, expected, actual, "decrementing %s did not return expected height: %s. got %s",
+		validDecrement.String(), expected.String(), actual.String())
+	require.True(t, success, "decrement failed unexpectedly")
+
+	invalidDecrement := NewHeight(3, 1)
+	actual, success = invalidDecrement.Decrement()
+
+	require.Equal(t, Height{}, actual, "invalid decrement returned non-zero height: %s", actual.String())
+	require.False(t, success, "invalid decrement passed")
+}
+
+func TestValid(t *testing.T) {
+	valid := NewHeight(0, 2)
+	require.True(t, valid.Valid(), "valid height did not return true on Valid()")
+
+	invalid := NewHeight(2, 0)
+	require.False(t, invalid.Valid(), "invalid height returned true on Valid()")
+}

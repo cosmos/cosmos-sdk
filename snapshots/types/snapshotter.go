59ed84a5cb820e957ee07b1038d2e01e18f69f2e
Erik Grinaker <erik@interchain.berlin>
2020-08-28 17:13:18 +0200

    comment tweaks

--- snapshots/types/snapshotter.go
+++ snapshots/types/snapshotter.go
@@ -2,13 +2,13 @@ package types
 
 import "io"
 
-// Snapshotter is something that can take and restore snapshots, consisting of streamed binary
+// Snapshotter is something that can create and restore snapshots, consisting of streamed binary
 // chunks - all of which must be read from the channel and closed. If an unsupported format is
 // given, it must return ErrUnknownFormat (possibly wrapped with fmt.Errorf).
 type Snapshotter interface {
-	// Snapshot takes a state snapshot.
+	// Snapshot creates a state snapshot, returning a channel of snapshot chunk readers.
 	Snapshot(height uint64, format uint32) (<-chan io.ReadCloser, error)
 
-	// Restore restores a state snapshot.
+	// Restore restores a state snapshot, taking snapshot chunk readers as input.
 	Restore(height uint64, format uint32, chunks <-chan io.ReadCloser) error
 }

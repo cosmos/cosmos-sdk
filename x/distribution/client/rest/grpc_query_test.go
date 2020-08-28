c95895668c521740cbfcea4fbc7163711bc83b95
atheesh <atheesh@vitwit.com>
2020-08-28 22:27:12 +0530

    added tests for x/distr grpc tests

--- x/distribution/client/rest/grpc_query_test.go
+++ x/distribution/client/rest/grpc_query_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 	"github.com/gogo/protobuf/proto"
@@ -70,7 +71,8 @@ func (s *IntegrationTestSuite) TestQueryOutstandingRewardsGRPC() {
 	val := s.network.Validators[0]
 	baseURL := val.APIAddress
 
-	rewards, _ := sdk.ParseDecCoins("19.6stake")
+	rewards, err := sdk.ParseDecCoins("19.6stake")
+	s.Require().NoError(err)
 
 	testCases := []struct {
 		name     string
@@ -109,6 +111,371 @@ func (s *IntegrationTestSuite) TestQueryOutstandingRewardsGRPC() {
 	for _, tc := range testCases {
 		tc := tc
 		resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+		s.Run(tc.name, func() {
+			if tc.expErr {
+				s.Require().Error(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+			} else {
+				s.Require().NoError(err)
+				s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+				s.Require().Equal(tc.expected.String(), tc.respType.String())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryValidatorCommissionGRPC() {
+	val := s.network.Validators[0]
+	baseURL := val.APIAddress
+
+	commission, err := sdk.ParseDecCoins("9.8stake")
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name     string
+		url      string
+		headers  map[string]string
+		expErr   bool
+		respType proto.Message
+		expected proto.Message
+	}{
+		{
+			"gRPC request params with wrong validator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/validators/%s/commission", baseURL, "wrongAddress"),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "1",
+			},
+			false,
+			&types.QueryValidatorCommissionResponse{},
+			&types.QueryValidatorCommissionResponse{},
+		},
+		{
+			"gRPC request params valid address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/validators/%s/commission", baseURL, base64.URLEncoding.EncodeToString(val.ValAddress)),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "2",
+			},
+			false,
+			&types.QueryValidatorCommissionResponse{},
+			&types.QueryValidatorCommissionResponse{
+				Commission: types.ValidatorAccumulatedCommission{
+					Commission: commission,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+		s.Run(tc.name, func() {
+			if tc.expErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err)
+				s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+				s.Require().Equal(tc.expected.String(), tc.respType.String())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQuerySlashesGRPC() {
+	val := s.network.Validators[0]
+	baseURL := val.APIAddress
+	valAddressBase64 := base64.URLEncoding.EncodeToString(val.Address)
+
+	testCases := []struct {
+		name     string
+		url      string
+		headers  map[string]string
+		expErr   bool
+		respType proto.Message
+		expected proto.Message
+	}{
+		{
+			"invalid validator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/validators/%s/slashes", baseURL, ""),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "4",
+			},
+			true,
+			&types.QueryValidatorSlashesResponse{},
+			nil,
+		},
+		{
+			"invalid start height",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/validators/%s/slashes?starting_height=%s&ending_height=%s", baseURL, valAddressBase64, "-1", "3"),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "4",
+			},
+			true,
+			&types.QueryValidatorSlashesResponse{},
+			nil,
+		},
+		{
+			"invalid start height",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/validators/%s/slashes?starting_height=%s&ending_height=%s", baseURL, valAddressBase64, "1", "-3"),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "4",
+			},
+			true,
+			&types.QueryValidatorSlashesResponse{},
+			nil,
+		},
+		{
+			"valid request get slashes",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/validators/%s/slashes?starting_height=%s&ending_height=%s", baseURL, valAddressBase64, "1", "3"),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "4",
+			},
+			false,
+			&types.QueryValidatorSlashesResponse{},
+			&types.QueryValidatorSlashesResponse{
+				Pagination: &query.PageResponse{},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+
+		s.Run(tc.name, func() {
+			if tc.expErr {
+				s.Require().Error(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+			} else {
+				s.Require().NoError(err)
+				s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+				s.Require().Equal(tc.expected.String(), tc.respType.String())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryDelegatorRewardsGRPC() {
+	val := s.network.Validators[0]
+	baseUrl := val.APIAddress
+
+	delAddrBase64 := base64.URLEncoding.EncodeToString(val.Address)
+	valAddrBase64 := base64.URLEncoding.EncodeToString(val.ValAddress)
+
+	rewards, err := sdk.ParseDecCoins("9.8stake")
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name     string
+		url      string
+		headers  map[string]string
+		expErr   bool
+		respType proto.Message
+		expected proto.Message
+	}{
+		{
+			"wrong delegator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/rewards", baseUrl, "wrongDelegatorAddress"),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "2",
+			},
+			true,
+			&types.QueryDelegationTotalRewardsResponse{},
+			nil,
+		},
+		{
+			"wrong delegator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/rewards", baseUrl, delAddrBase64),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "2",
+			},
+			false,
+			&types.QueryDelegationTotalRewardsResponse{},
+			&types.QueryDelegationTotalRewardsResponse{
+				Rewards: []types.DelegationDelegatorReward{
+					types.NewDelegationDelegatorReward(val.ValAddress, rewards),
+				},
+				Total: rewards,
+			},
+		},
+		{
+			"wrong validator address(specific validator rewards)",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/rewards/%s", baseUrl, delAddrBase64, "wrongValAddress"),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "2",
+			},
+			true,
+			&types.QueryDelegationTotalRewardsResponse{},
+			nil,
+		},
+		{
+			"valid request(specific validator rewards)",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/rewards/%s", baseUrl, delAddrBase64, valAddrBase64),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "2",
+			},
+			false,
+			&types.QueryDelegationRewardsResponse{},
+			&types.QueryDelegationRewardsResponse{
+				Rewards: rewards,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+
+		s.Run(tc.name, func() {
+			if tc.expErr {
+				s.Require().Error(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+			} else {
+				s.Require().NoError(err)
+				s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+				s.Require().Equal(tc.expected.String(), tc.respType.String())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryDelegatorValidatorsGRPC() {
+	val := s.network.Validators[0]
+	baseUrl := val.APIAddress
+
+	delAddrBase64 := base64.URLEncoding.EncodeToString(val.Address)
+
+	testCases := []struct {
+		name     string
+		url      string
+		expErr   bool
+		respType proto.Message
+		expected proto.Message
+	}{
+		{
+			"empty delegator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/validators", baseUrl, ""),
+			true,
+			&types.QueryDelegatorValidatorsResponse{},
+			nil,
+		},
+		{
+			"wrong delegator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/validators", baseUrl, "wrongDelegatorAddress"),
+			true,
+			&types.QueryDelegatorValidatorsResponse{},
+			nil,
+		},
+		{
+			"valid request",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/validators", baseUrl, delAddrBase64),
+			false,
+			&types.QueryDelegatorValidatorsResponse{},
+			&types.QueryDelegatorValidatorsResponse{
+				Validators: []sdk.ValAddress{val.ValAddress},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		resp, err := rest.GetRequest(tc.url)
+
+		s.Run(tc.name, func() {
+			if tc.expErr {
+				s.Require().Error(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+			} else {
+				s.Require().NoError(err)
+				s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+				s.Require().Equal(tc.expected.String(), tc.respType.String())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryWithdrawAddressGRPC() {
+	val := s.network.Validators[0]
+	baseUrl := val.APIAddress
+
+	delAddrBase64 := base64.URLEncoding.EncodeToString(val.Address)
+
+	testCases := []struct {
+		name     string
+		url      string
+		expErr   bool
+		respType proto.Message
+		expected proto.Message
+	}{
+		{
+			"empty delegator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/withdraw_address", baseUrl, ""),
+			true,
+			&types.QueryDelegatorWithdrawAddressResponse{},
+			nil,
+		},
+		{
+			"wrong delegator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/withdraw_address", baseUrl, "wrongDelegatorAddress"),
+			true,
+			&types.QueryDelegatorWithdrawAddressResponse{},
+			nil,
+		},
+		{
+			"valid request",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/delegators/%s/withdraw_address", baseUrl, delAddrBase64),
+			false,
+			&types.QueryDelegatorWithdrawAddressResponse{},
+			&types.QueryDelegatorWithdrawAddressResponse{
+				WithdrawAddress: val.Address,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		resp, err := rest.GetRequest(tc.url)
+
+		s.Run(tc.name, func() {
+			if tc.expErr {
+				s.Require().Error(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+			} else {
+				s.Require().NoError(err)
+				s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(resp, tc.respType))
+				s.Require().Equal(tc.expected.String(), tc.respType.String())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryValidatorCommunityPoolGRPC() {
+	val := s.network.Validators[0]
+	baseURL := val.APIAddress
+
+	communityPool, err := sdk.ParseDecCoins("0.4stake")
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name     string
+		url      string
+		headers  map[string]string
+		expErr   bool
+		respType proto.Message
+		expected proto.Message
+	}{
+		{
+			"gRPC request params with wrong validator address",
+			fmt.Sprintf("%s/cosmos/distribution/v1beta1/community_pool", baseURL),
+			map[string]string{
+				grpctypes.GRPCBlockHeightHeader: "2",
+			},
+			false,
+			&types.QueryCommunityPoolResponse{},
+			&types.QueryCommunityPoolResponse{
+				Pool: communityPool,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+
 		s.Run(tc.name, func() {
 			if tc.expErr {
 				s.Require().Error(err)

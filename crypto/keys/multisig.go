a4f6dd6640b1aaa025c5454f240f3838bc95a174
blushi <marie.gauthier63@gmail.com>
2020-09-02 13:01:13 +0200

    Add tests for MultisigThresholdPubKey

--- crypto/keys/multisig.go
+++ crypto/keys/multisig.go
@@ -1,6 +1,8 @@
 package keys
 
 import (
+	fmt "fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 
 	"github.com/cosmos/cosmos-sdk/codec/types"
@@ -16,9 +18,8 @@ var cdc = codec.NewProtoCodec(types.NewInterfaceRegistry())
 var _ multisig.PubKey = &MultisigThresholdPubKey{}
 
 // Address implements crypto.PubKey Address method
-// TODO
 func (m *MultisigThresholdPubKey) Address() crypto.Address {
-	return nil
+	return tmcrypto.AddressHash(m.Bytes())
 }
 
 // Bytes returns the proto encoded version of the MultisigThresholdPubKey
@@ -27,55 +28,72 @@ func (m *MultisigThresholdPubKey) Bytes() []byte {
 }
 
 // VerifyMultisignature implements the multisig.PubKey VerifyMultisignature method
-// TODO
 func (m *MultisigThresholdPubKey) VerifyMultisignature(getSignBytes multisig.GetSignBytesFunc, sig *signing.MultiSignatureData) error {
+	bitarray := sig.BitArray
+	sigs := sig.Signatures
+	size := bitarray.Count()
+	pubKeys := m.GetPubKeys()
+	// ensure bit array is the correct size
+	if len(pubKeys) != size {
+		return fmt.Errorf("bit array size is incorrect %d", len(pubKeys))
+	}
+	// ensure size of signature list
+	if len(sigs) < int(m.K) || len(sigs) > size {
+		return fmt.Errorf("signature size is incorrect %d", len(sigs))
+	}
+	// ensure at least k signatures are set
+	if bitarray.NumTrueBitsBefore(size) < int(m.K) {
+		return fmt.Errorf("minimum number of signatures not set, have %d, expected %d", bitarray.NumTrueBitsBefore(size), int(m.K))
+	}
+	// index in the list of signatures which we are concerned with.
+	sigIndex := 0
+	for i := 0; i < size; i++ {
+		if bitarray.GetIndex(i) {
+			si := sig.Signatures[sigIndex]
+			switch si := si.(type) {
+			case *signing.SingleSignatureData:
+				msg, err := getSignBytes(si.SignMode)
+				if err != nil {
+					return err
+				}
+				if !pubKeys[i].VerifySignature(msg, si.Signature) {
+					return err
+				}
+			case *signing.MultiSignatureData:
+				nestedMultisigPk, ok := pubKeys[i].(multisig.PubKey)
+				if !ok {
+					return fmt.Errorf("unable to parse pubkey of index %d", i)
+				}
+				if err := nestedMultisigPk.VerifyMultisignature(getSignBytes, si); err != nil {
+					return err
+				}
+			default:
+				return fmt.Errorf("improper signature data type for index %d", sigIndex)
+			}
+			sigIndex++
+		}
+	}
 	return nil
 }
 
 // VerifySignature implements crypto.PubKey VerifySignature method
 func (m *MultisigThresholdPubKey) VerifySignature(msg []byte, sig []byte) bool {
-	// var sigData signing.SignatureDescriptor_Data_Multi // is this expected?
-	// err := cdc.UnmarshalBinaryBare(sig, &sigData)
-	// if err != nil {
-	// 	return false
-	// }
-	// size := sigData.Bitarray.Count()
-	// // ensure bit array is the correct size
-	// if len(m.PubKeys) != size {
-	// 	return false
-	// }
-	// // ensure size of signature list
-	// if len(sigData.Signatures) < int(m.K) || len(sigData.Signatures) > size {
-	// 	return false
-	// }
-	// // ensure at least k signatures are set
-	// if sigData.Bitarray.NumTrueBitsBefore(size) < int(m.K) {
-	// 	return false
-	// }
-	// // index in the list of signatures which we are concerned with.
-	// sigIndex := 0
-	// for i := 0; i < size; i++ {
-	// 	if sigData.Bitarray.GetIndex(i) {
-	// 		pk, ok := m.PubKeys[i].GetCachedValue().(tmcrypto.PubKey)
-	// 		if !ok {
-	// 			return false
-	// 		}
-	// 		if !pk.VerifySignature(msg, sigData.Signatures[sigIndex]) { // TODO fix
-	// 			return false
-	// 		}
-	// 		sigIndex++
-	// 	}
-	// }
-	return true
+	// TODO
+	// which type is expected in sig?
+	return false
 }
 
 // GetPubKeys implements the PubKey.GetPubKeys method
 func (m *MultisigThresholdPubKey) GetPubKeys() []crypto.PubKey {
-	pubKeys := make([]crypto.PubKey, len(m.PubKeys))
-	for i := 0; i < len(m.PubKeys); i++ {
-		pubKeys[i] = m.PubKeys[i].GetCachedValue().(tmcrypto.PubKey)
+	if m != nil {
+		pubKeys := make([]crypto.PubKey, len(m.PubKeys))
+		for i := 0; i < len(m.PubKeys); i++ {
+			pubKeys[i] = m.PubKeys[i].GetCachedValue().(tmcrypto.PubKey)
+		}
+		return pubKeys
 	}
-	return pubKeys
+
+	return nil
 }
 
 // Equals returns true if m and other both have the same number of keys, and
--- crypto/keys/multisig_test.go
+++ crypto/keys/multisig_test.go
@@ -1,3 +1,115 @@
 package keys_test
 
-// TODO
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto"
+	keys "github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/cosmos-sdk/crypto/types/multisig"
+	proto "github.com/gogo/protobuf/proto"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddress(t *testing.T) {
+	msg := []byte{1, 2, 3, 4}
+	pubKeys, _ := generatePubKeysAndSignatures(5, msg)
+	anyPubKeys, err := packPubKeys(pubKeys)
+	require.NoError(t, err)
+	multisigKey := &keys.MultisigThresholdPubKey{K: 2, PubKeys: anyPubKeys}
+
+	require.Len(t, multisigKey.Address().Bytes(), 20)
+}
+
+func TestEquals(t *testing.T) {
+	pubKey1 := secp256k1.GenPrivKey().PubKey()
+	pubKey2 := secp256k1.GenPrivKey().PubKey()
+
+	pbPubKey1 := &keys.Secp256K1PubKey{Key: pubKey1.(secp256k1.PubKey)}
+	pbPubKey2 := &keys.Secp256K1PubKey{Key: pubKey2.(secp256k1.PubKey)}
+	anyPubKeys, err := packPubKeys([]crypto.PubKey{pbPubKey1, pbPubKey2})
+	require.NoError(t, err)
+	multisigKey := keys.MultisigThresholdPubKey{K: 1, PubKeys: anyPubKeys}
+
+	otherPubKeys, err := packPubKeys([]crypto.PubKey{pbPubKey1, &multisigKey})
+	require.NoError(t, err)
+	otherMultisigKey := keys.MultisigThresholdPubKey{K: 1, PubKeys: otherPubKeys}
+
+	testCases := []struct {
+		msg      string
+		other    crypto.PubKey
+		expectEq bool
+	}{
+		{
+			"equals with proto pub key",
+			&keys.MultisigThresholdPubKey{K: 1, PubKeys: anyPubKeys},
+			true,
+		},
+		{
+			"different threshold",
+			&keys.MultisigThresholdPubKey{K: 2, PubKeys: anyPubKeys},
+			false,
+		},
+		{
+			"different pub keys length",
+			&keys.MultisigThresholdPubKey{K: 1, PubKeys: []*types.Any{anyPubKeys[0]}},
+			false,
+		},
+		{
+			"different pub keys",
+			&otherMultisigKey,
+			false,
+		},
+		{
+			"different types",
+			secp256k1.GenPrivKey().PubKey(),
+			false,
+		},
+		{
+			"equals with amino pub key",
+			multisig.NewPubKeyMultisigThreshold(1, []crypto.PubKey{pubKey1, pubKey2}),
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.msg, func(t *testing.T) {
+			eq := multisigKey.Equals(tc.other)
+			require.Equal(t, eq, tc.expectEq)
+		})
+	}
+}
+
+func TestVerifyMultisignature(t *testing.T) {
+	// TODO
+}
+
+func generatePubKeysAndSignatures(n int, msg []byte) (pubKeys []crypto.PubKey, signatures []signing.SignatureData) {
+	pubKeys = make([]crypto.PubKey, n)
+	signatures = make([]signing.SignatureData, n)
+
+	for i := 0; i < n; i++ {
+		privkey := &keys.Secp256K1PrivKey{Key: secp256k1.GenPrivKey()}
+		pubKeys[i] = privkey.PubKey()
+
+		sig, _ := privkey.Sign(msg)
+		signatures[i] = &signing.SingleSignatureData{Signature: sig}
+	}
+	return
+}
+
+func packPubKeys(pubKeys []crypto.PubKey) ([]*types.Any, error) {
+	anyPubKeys := make([]*types.Any, len(pubKeys))
+
+	for i := 0; i < len(pubKeys); i++ {
+		any, err := types.NewAnyWithValue(pubKeys[i].(proto.Message))
+		if err != nil {
+			return nil, err
+		}
+		anyPubKeys[i] = any
+	}
+	return anyPubKeys, nil
+}

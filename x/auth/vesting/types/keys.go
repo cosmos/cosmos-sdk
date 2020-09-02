060adee44b574fcac680e3eee235a43bb6416e6a
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-09-02 12:01:16 -0700

    implement vesting app module

--- x/auth/vesting/types/keys.go
+++ x/auth/vesting/types/keys.go
@@ -1,7 +1,12 @@
 package types
 
-// vesting key types
 const (
-	ModuleName             = "vesting"
+	// ModuleName defines the module's name.
+	ModuleName = "vesting"
+
+	// AttributeValueCategory is an alias for the message event value.
 	AttributeValueCategory = ModuleName
+
+	// RouterKey defines the module's message routing key
+	RouterKey = ModuleName
 )

5dc43f4e604353055f3cf9f8433814d35692afc3
aleem1314 <aleem.md789@gmail.com>
2020-09-07 11:03:34 +0530

    Fix tests

--- x/distribution/keeper/grpc_query_test.go
+++ x/distribution/keeper/grpc_query_test.go
@@ -128,7 +128,7 @@ func (suite *KeeperTestSuite) TestGRPCValidatorOutstandingRewards() {
 		}, {
 			"valid request",
 			func() {
-				req = &types.QueryValidatorOutstandingRewardsRequest{ValidatorAddress: valAddrs[0]}
+				req = &types.QueryValidatorOutstandingRewardsRequest{ValidatorAddress: valAddrs[0].String()}
 			},
 			true,
 		},
@@ -175,7 +175,7 @@ func (suite *KeeperTestSuite) TestGRPCValidatorCommission() {
 		{
 			"valid request",
 			func() {
-				req = &types.QueryValidatorCommissionRequest{ValidatorAddress: valAddrs[0]}
+				req = &types.QueryValidatorCommissionRequest{ValidatorAddress: valAddrs[0].String()}
 			},
 			true,
 		},
@@ -385,8 +385,8 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"empty delegator request",
 			func() {
 				req = &types.QueryDelegationRewardsRequest{
-					DelegatorAddress: nil,
-					ValidatorAddress: valAddrs[0],
+					DelegatorAddress: "",
+					ValidatorAddress: valAddrs[0].String(),
 				}
 			},
 			false,
@@ -395,8 +395,8 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"empty validator request",
 			func() {
 				req = &types.QueryDelegationRewardsRequest{
-					DelegatorAddress: addrs[1],
-					ValidatorAddress: nil,
+					DelegatorAddress: addrs[1].String(),
+					ValidatorAddress: "",
 				}
 			},
 			false,
@@ -405,8 +405,8 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"request with wrong delegator and validator",
 			func() {
 				req = &types.QueryDelegationRewardsRequest{
-					DelegatorAddress: addrs[1],
-					ValidatorAddress: valAddrs[1],
+					DelegatorAddress: addrs[1].String(),
+					ValidatorAddress: valAddrs[1].String(),
 				}
 			},
 			false,
@@ -415,8 +415,8 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"valid request",
 			func() {
 				req = &types.QueryDelegationRewardsRequest{
-					DelegatorAddress: addrs[0],
-					ValidatorAddress: valAddrs[0],
+					DelegatorAddress: addrs[0].String(),
+					ValidatorAddress: valAddrs[0].String(),
 				}
 
 				expRes = &types.QueryDelegationRewardsResponse{
@@ -465,7 +465,7 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"valid total delegation rewards",
 			func() {
 				totalRewardsReq = &types.QueryDelegationTotalRewardsRequest{
-					DelegatorAddress: addrs[0],
+					DelegatorAddress: addrs[0].String(),
 				}
 
 				expectedDelReward := types.NewDelegationDelegatorReward(valAddrs[0],
@@ -519,7 +519,7 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"request no delegations address",
 			func() {
 				delegatorValidatorsReq = &types.QueryDelegatorValidatorsRequest{
-					DelegatorAddress: addrs[1],
+					DelegatorAddress: addrs[1].String(),
 				}
 
 				expDelegatorValidatorsRes = &types.QueryDelegatorValidatorsResponse{}
@@ -530,10 +530,14 @@ func (suite *KeeperTestSuite) TestGRPCDelegationRewards() {
 			"valid request",
 			func() {
 				delegatorValidatorsReq = &types.QueryDelegatorValidatorsRequest{
-					DelegatorAddress: addrs[0],
+					DelegatorAddress: addrs[0].String(),
 				}
 				expDelegatorValidatorsRes = &types.QueryDelegatorValidatorsResponse{
-					Validators: valAddrs[:1],
+					Validators: func() []string {
+						addrs := make([]string, 2)
+						addrs = append(addrs, valAddrs[0].String(), valAddrs[1].String())
+						return addrs
+					}(),
 				}
 			},
 			true,
@@ -580,7 +584,7 @@ func (suite *KeeperTestSuite) TestGRPCDelegatorWithdrawAddress() {
 		{
 			"valid request",
 			func() {
-				req = &types.QueryDelegatorWithdrawAddressRequest{DelegatorAddress: addrs[0]}
+				req = &types.QueryDelegatorWithdrawAddressRequest{DelegatorAddress: addrs[0].String()}
 			},
 			true,
 		},

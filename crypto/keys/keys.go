b9ab6c95ee32097e8f9cf10e5aadb6c5bcdfa6a4
blushi <marie.gauthier63@gmail.com>
2020-09-07 09:16:49 +0200

    Address review comments

--- crypto/keys/keys.go
+++ crypto/keys/keys.go
@@ -10,51 +10,6 @@ import (
 	proto "github.com/gogo/protobuf/proto"
 )
 
-var (
-	_ crypto.PubKey  = &Secp256K1PubKey{}
-	_ crypto.PrivKey = &Secp256K1PrivKey{}
-)
-
-func (m *Secp256K1PubKey) Address() crypto.Address {
-	return m.Key.Address()
-}
-
-func (m *Secp256K1PubKey) Bytes() []byte {
-	return m.Key.Bytes()
-}
-
-func (m *Secp256K1PubKey) VerifySignature(msg []byte, sig []byte) bool {
-	return m.Key.VerifySignature(msg, sig)
-}
-
-func (m *Secp256K1PubKey) Equals(key crypto.PubKey) bool {
-	return m.Key.Equals(key)
-}
-
-func (m *Secp256K1PubKey) Type() string {
-	return m.Key.Type()
-}
-
-func (m *Secp256K1PrivKey) Bytes() []byte {
-	return m.Key.Bytes()
-}
-
-func (m *Secp256K1PrivKey) Sign(msg []byte) ([]byte, error) {
-	return m.Key.Sign(msg)
-}
-
-func (m *Secp256K1PrivKey) PubKey() crypto.PubKey {
-	return &Secp256K1PubKey{Key: m.Key.PubKey().(secp256k1.PubKey)}
-}
-
-func (m *Secp256K1PrivKey) Equals(key crypto.PrivKey) bool {
-	return m.Key.Equals(key)
-}
-
-func (m *Secp256K1PrivKey) Type() string {
-	return m.Key.Type()
-}
-
 // ProtoPubKeyToAminoPubKey converts the proto.Message into a crypto.PubKey
 func ProtoPubKeyToAminoPubKey(msg proto.Message) (crypto.PubKey, error) {
 	switch msg := msg.(type) {
---
+++ crypto/keys/secp256k1_test.go
@@ -0,0 +1,149 @@
+package keys_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	crypto "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	tmcrypto "github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/sr25519"
+)
+
+func TestPubKeyEquals(t *testing.T) {
+	secp256K1PubKey := secp256k1.GenPrivKey().PubKey().(secp256k1.PubKey)
+	secp256K1PbPubKey := &keys.Secp256K1PubKey{Key: secp256K1PubKey}
+
+	testCases := []struct {
+		msg      string
+		pubKey   crypto.PubKey
+		other    crypto.PubKey
+		expectEq bool
+	}{
+		{
+			"secp256k1 pb different bytes",
+			secp256K1PbPubKey,
+			&keys.Secp256K1PubKey{
+				Key: secp256k1.GenPrivKey().PubKey().(secp256k1.PubKey),
+			},
+			false,
+		},
+		{
+			"secp256k1 pb equals",
+			secp256K1PbPubKey,
+			&keys.Secp256K1PubKey{
+				Key: secp256K1PubKey,
+			},
+			true,
+		},
+		{
+			"secp256k1 different types",
+			secp256K1PbPubKey,
+			sr25519.GenPrivKey().PubKey(),
+			false,
+		},
+		{
+			"secp256k1 different bytes",
+			secp256K1PbPubKey,
+			secp256k1.GenPrivKey().PubKey(),
+			false,
+		},
+		{
+			"secp256k1 equals",
+			secp256K1PbPubKey,
+			secp256K1PubKey,
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.msg, func(t *testing.T) {
+			eq := tc.pubKey.Equals(tc.other)
+			require.Equal(t, eq, tc.expectEq)
+		})
+	}
+}
+
+func TestPrivKeyEquals(t *testing.T) {
+	secp256K1PrivKey := secp256k1.GenPrivKey()
+	secp256K1PbPrivKey := &keys.Secp256K1PrivKey{Key: secp256K1PrivKey}
+
+	testCases := []struct {
+		msg      string
+		privKey  crypto.PrivKey
+		other    crypto.PrivKey
+		expectEq bool
+	}{
+		{
+			"secp256k1 pb different bytes",
+			secp256K1PbPrivKey,
+			&keys.Secp256K1PrivKey{
+				Key: secp256k1.GenPrivKey(),
+			},
+			false,
+		},
+		{
+			"secp256k1 pb equals",
+			secp256K1PbPrivKey,
+			&keys.Secp256K1PrivKey{
+				Key: secp256K1PrivKey,
+			},
+			true,
+		},
+		{
+			"secp256k1 different types",
+			secp256K1PbPrivKey,
+			sr25519.GenPrivKey(),
+			false,
+		},
+		{
+			"secp256k1 different bytes",
+			secp256K1PbPrivKey,
+			secp256k1.GenPrivKey(),
+			false,
+		},
+		{
+			"secp256k1 equals",
+			secp256K1PbPrivKey,
+			secp256K1PrivKey,
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.msg, func(t *testing.T) {
+			eq := tc.privKey.Equals(tc.other)
+			require.Equal(t, eq, tc.expectEq)
+		})
+	}
+}
+
+func TestSignAndVerifySignature(t *testing.T) {
+	testCases := []struct {
+		msg     string
+		privKey crypto.PrivKey
+	}{
+		{
+			"secp256k1",
+			&keys.Secp256K1PrivKey{Key: secp256k1.GenPrivKey()},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.msg, func(t *testing.T) {
+			pubKey := tc.privKey.PubKey()
+			msg := tmcrypto.CRandBytes(128)
+			sig, err := tc.privKey.Sign(msg)
+			require.Nil(t, err)
+
+			assert.True(t, pubKey.VerifySignature(msg, sig))
+
+			sig[7] ^= byte(0x01)
+
+			assert.False(t, pubKey.VerifySignature(msg, sig))
+		})
+	}
+
+}

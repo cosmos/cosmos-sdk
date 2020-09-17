b9986375649e668ac0b554cb60e17573e0874c0d
Colin Axner <colinaxner@berkeley.edu>
2020-09-17 16:16:17 +0200

    Update QueryTendermint to take in tendermint height
    Update QueryTendermint to subtract the provided height by one to query at the IAVL height.

--- x/ibc/client/query.go
+++ x/ibc/client/query.go
@@ -12,16 +12,28 @@ import (
 	host "github.com/cosmos/cosmos-sdk/x/ibc/24-host"
 )
 
-// QueryTendermintProof performs an ABCI query with the given key and returns the value
-// of the query, the proto encoded merkle proof for the query and the height
-// at which the proof will succeed on a tendermint verifier (one above the
-// returned IAVL version height).
+// QueryTendermintProof performs an ABCI query with the given key and returns
+// the value of the query, the proto encoded merkle proof, and the height at
+// which the proof will succeed on a tendermint verifier (one above the
+// returned IAVL version height). The desired tendermint height to perform
+// the query should be set in the client context. The query will be performed
+// at one below this height (at the IAVL version) in order to obtain the
+// correct merkle proof.
 // Issue: https://github.com/cosmos/cosmos-sdk/issues/6567
 func QueryTendermintProof(clientCtx client.Context, key []byte) ([]byte, []byte, clienttypes.Height, error) {
+	height := clientCtx.Height
+
+	// Use the IAVL height if a valid tendermint height is passed in.
+	// ABCI queries at zero or negative height may have other side affects.
+	if clientCtx.Height > 0 {
+		height--
+	}
+
 	req := abci.RequestQuery{
-		Path:  fmt.Sprintf("store/%s/key", host.StoreKey),
-		Data:  key,
-		Prove: true,
+		Path:   fmt.Sprintf("store/%s/key", host.StoreKey),
+		Height: height,
+		Data:   key,
+		Prove:  true,
 	}
 
 	res, err := clientCtx.QueryABCI(req)

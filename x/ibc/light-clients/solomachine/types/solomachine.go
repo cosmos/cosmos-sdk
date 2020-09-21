00f6a67953b14c9a654b457cfc84c8251ffac399
Amaury Martiny <amaury.martiny@protonmail.com>
2020-09-21 11:21:51 +0200

    Add tests for solomachine unpack interfaces

--- x/ibc/light-clients/solomachine/types/solomachine.go
+++ x/ibc/light-clients/solomachine/types/solomachine.go
@@ -10,21 +10,21 @@ import (
 var _, _, _, _ codectypes.UnpackInterfacesMessage = &ClientState{}, &ConsensusState{}, &Header{}, &HeaderData{}
 
 // UnpackInterfaces implements the UnpackInterfaceMessages.UnpackInterfaces method
-func (cs *ClientState) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
+func (cs ClientState) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
 	return cs.ConsensusState.UnpackInterfaces(unpacker)
 }
 
 // UnpackInterfaces implements the UnpackInterfaceMessages.UnpackInterfaces method
-func (cs *ConsensusState) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
+func (cs ConsensusState) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
 	return unpacker.UnpackAny(cs.PublicKey, new(crypto.PubKey))
 }
 
 // UnpackInterfaces implements the UnpackInterfaceMessages.UnpackInterfaces method
-func (h *Header) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
+func (h Header) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
 	return unpacker.UnpackAny(h.NewPublicKey, new(crypto.PubKey))
 }
 
 // UnpackInterfaces implements the UnpackInterfaceMessages.UnpackInterfaces method
-func (hd *HeaderData) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
+func (hd HeaderData) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
 	return unpacker.UnpackAny(hd.NewPubKey, new(crypto.PubKey))
 }

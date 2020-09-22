afbaec07f8961763833d595a7e94470e6ce9cf0b
Amaury Martiny <amaury.martiny@protonmail.com>
2020-09-22 15:00:18 +0200

    Sort import

--- client/debug/main.go
+++ client/debug/main.go
@@ -7,11 +7,11 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/crypto"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 )

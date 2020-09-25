2633331092438d18d0835582ac34a0759ad548e6
Amaury Martiny <amaury.martiny@protonmail.com>
2020-09-25 10:39:00 +0200

    deterministic tests help

--- x/auth/legacy/v0_40/migrate_test.go
+++ x/auth/legacy/v0_40/migrate_test.go
@@ -1,7 +1,6 @@
 package v040_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -25,12 +24,14 @@ func TestMigrate(t *testing.T) {
 		WithJSONMarshaler(encodingConfig.Marshaler)
 
 	coins := sdk.NewCoins(sdk.NewInt64Coin("stake", 50))
-	addr1, _ := sdk.AccAddressFromBech32("cosmos1xxkueklal9vejv9unqu80w9vptyepfa95pd53u")
-	acc1 := v039auth.NewBaseAccount(addr1, coins, secp256k1.GenPrivKey().PubKey(), 1, 0)
+	pk1 := secp256k1.GenPrivKeyFromSecret([]byte("acc1")).PubKey()
+	addr1 := sdk.AccAddress(pk1.Address())
+	acc1 := v039auth.NewBaseAccount(addr1, coins, pk1, 1, 0)
 
-	addr2, _ := sdk.AccAddressFromBech32("cosmos15v50ymp6n5dn73erkqtmq0u8adpl8d3ujv2e74")
-	vaac := v039auth.NewContinuousVestingAccountRaw(
-		v039auth.NewBaseVestingAccount(v039auth.NewBaseAccount(addr2, coins, secp256k1.GenPrivKey().PubKey(), 1, 0), coins, nil, nil, 3160620846),
+	pk2 := secp256k1.GenPrivKeyFromSecret([]byte("acc2")).PubKey()
+	addr2 := sdk.AccAddress(pk1.Address())
+	acc2 := v039auth.NewContinuousVestingAccountRaw(
+		v039auth.NewBaseVestingAccount(v039auth.NewBaseAccount(addr2, coins, pk2, 1, 0), coins, nil, nil, 3160620846),
 		1580309972,
 	)
 
@@ -42,13 +43,12 @@ func TestMigrate(t *testing.T) {
 			SigVerifyCostED25519:   10,
 			SigVerifyCostSecp256k1: 10,
 		},
-		Accounts: v038auth.GenesisAccounts{acc1, vaac},
+		Accounts: v038auth.GenesisAccounts{acc1, acc2},
 	}
 
 	migrated := v040auth.Migrate(gs)
-	expected := `{"params":{"max_memo_characters":"10","tx_sig_limit":"10","tx_size_cost_per_byte":"10","sig_verify_cost_ed25519":"10","sig_verify_cost_secp256k1":"10"},"accounts":[{"@type":"/cosmos.auth.v1beta1.BaseAccount","address":"cosmos1xxkueklal9vejv9unqu80w9vptyepfa95pd53u","pub_key":{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"A6F1ZrVNtYoOow+b4WfUKgv4FqotgS/1gPlG23d69ER9"},"account_number":"1","sequence":"0"},{"@type":"/cosmos.vesting.v1beta1.ContinuousVestingAccount","base_vesting_account":{"base_account":{"address":"cosmos15v50ymp6n5dn73erkqtmq0u8adpl8d3ujv2e74","pub_key":{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"A9cjpzj9EGI+klNDXM5eH68hKjEygnUY5hktk74OnSri"},"account_number":"1","sequence":"0"},"original_vesting":[{"denom":"stake","amount":"50"}],"delegated_free":[],"delegated_vesting":[],"end_time":"3160620846"},"start_time":"3160620846"}]}`
+	expected := `{"params":{"max_memo_characters":"10","tx_sig_limit":"10","tx_size_cost_per_byte":"10","sig_verify_cost_ed25519":"10","sig_verify_cost_secp256k1":"10"},"accounts":[{"@type":"/cosmos.auth.v1beta1.BaseAccount","address":"cosmos13syh7de9xndv9wmklccpfvc0d8dcyvay4s6z6l","pub_key":{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"A8oWyJkohwy8XZ0Df92jFMBTtTPMvYJplYIrlEHTKPYk"},"account_number":"1","sequence":"0"},{"@type":"/cosmos.vesting.v1beta1.ContinuousVestingAccount","base_vesting_account":{"base_account":{"address":"cosmos13syh7de9xndv9wmklccpfvc0d8dcyvay4s6z6l","pub_key":{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"AruDygh5HprMOpHOEato85dLgAsybMJVyxBGUa3KuWCr"},"account_number":"1","sequence":"0"},"original_vesting":[{"denom":"stake","amount":"50"}],"delegated_free":[],"delegated_vesting":[],"end_time":"3160620846"},"start_time":"3160620846"}]}`
 
-	fmt.Printf("migrated=%+v\n", migrated.Accounts)
 	bz, err := clientCtx.JSONMarshaler.MarshalJSON(migrated)
 	require.NoError(t, err)
 	require.Equal(t, expected, string(bz))

ddaa3c5176bc79dcee12361027a6fef4f30f8dfd
Alessio Treglia <alessio@tendermint.com>
2020-09-29 13:29:10 +0200

    types: tests -> test suites migration (#7405)

--- types/tx_msg_test.go
+++ types/tx_msg_test.go
@@ -9,23 +9,23 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-type msgTestSuite struct {
+type testMsgSuite struct {
 	suite.Suite
 }
 
 func TestMsgTestSuite(t *testing.T) {
-	suite.Run(t, new(msgTestSuite))
+	suite.Run(t, new(testMsgSuite))
 }
 
-func (s *msgTestSuite) TestTestMsg() {
-	addr := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+func (s *testMsgSuite) TestMsg() {
+	addr := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
 	accAddr := sdk.AccAddress(addr)
 
 	msg := testdata.NewTestMsg(accAddr)
 	s.Require().NotNil(msg)
+	s.Require().Equal([]sdk.AccAddress{accAddr}, msg.GetSigners())
 	s.Require().Equal("TestMsg", msg.Route())
 	s.Require().Equal("Test message", msg.Type())
 	s.Require().Nil(msg.ValidateBasic())
 	s.Require().NotPanics(func() { msg.GetSignBytes() })
-	s.Require().Equal([]sdk.AccAddress{accAddr}, msg.GetSigners())
 }

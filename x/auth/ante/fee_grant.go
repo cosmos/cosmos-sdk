efe59e027eb96c644a8872f0c4c8f3101e547e84
Aaron Craelius <aaronc@users.noreply.github.com>
2020-09-30 10:55:47 -0400

    WIP on adding Fee granter field

---
+++ x/auth/ante/fee_grant.go
@@ -0,0 +1,27 @@
+package ante
+
+import (
+	"github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// DeductGrantedFeeDecorator is a placeholder AnteDecorator for fee grants for
+// when that functionality is enabled. Currently it simpy rejects transactions
+// which have the Fee.granter field set.
+type DeductGrantedFeeDecorator struct{}
+
+// NewDeductGrantedFeeDecorator returns a new DeductGrantedFeeDecorator.
+func NewDeductGrantedFeeDecorator() DeductGrantedFeeDecorator {
+	return DeductGrantedFeeDecorator{}
+}
+
+var _ types.AnteDecorator = DeductGrantedFeeDecorator{}
+
+func (d DeductGrantedFeeDecorator) AnteHandle(ctx types.Context, tx types.Tx, simulate bool, next types.AnteHandler) (newCtx types.Context, err error) {
+	feeTx, ok := tx.(types.FeeTx)
+	if ok && feeTx.FeeGranter() != nil {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fee grants are not supported")
+	}
+
+	return next(ctx, tx, simulate)
+}

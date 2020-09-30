1fa367ea7728a91bb91ba406439e364e03557f28
antstalepresh <stalepresh121@outlook.com>
2020-09-30 21:54:43 +1000

    Modify change pub key message type to use generalize pub key instead of secp256k1 only support

--- x/auth/pubkey/types/codec.go
+++ x/auth/pubkey/types/codec.go
@@ -3,6 +3,7 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -23,5 +24,7 @@ var amino = codec.NewLegacyAmino()
 
 func init() {
 	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
+
 	amino.Seal()
 }

45f11aef84d0f8b6a2d3865bcf84f012fd3f6999
Federico Kunze <federico.kunze94@gmail.com>
2020-10-02 12:04:17 +0200

    LGTM alerts audit

--- x/simulation/mock_tendermint.go
+++ x/simulation/mock_tendermint.go
@@ -99,9 +99,9 @@ func updateValidators(
 
 			event("end_block", "validator_updates", "kicked")
 			delete(current, str)
-		} else if mVal, ok := current[str]; ok {
+		} else if _, ok := current[str]; ok {
 			// validator already exists
-			mVal.val = update
+			// mVal.val = update
 			event("end_block", "validator_updates", "updated")
 
 		} else {

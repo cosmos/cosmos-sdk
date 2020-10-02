5469ad45336bdd61f3e59223b6c590c0d91ea13a
Aaron Craelius <aaronc@users.noreply.github.com>
2020-10-02 16:19:44 -0400

    Ideas

---
+++ types/module/v1/module.go
@@ -0,0 +1,44 @@
+package v1
+
+import (
+	"context"
+
+	grpc2 "google.golang.org/grpc"
+
+	"github.com/gogo/protobuf/grpc"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type Module interface {
+	Init(Configurator)
+}
+
+type Configurator interface {
+	StoreKey() StoreKey
+
+	BinaryMarshaler() codec.BinaryMarshaler
+	InterfaceRegistry() types.InterfaceRegistry
+
+	MsgServer() grpc.Server
+	QueryServer() grpc.Server
+	HooksServer() grpc.Server
+
+	RequireMsgServices(...interface{})
+	RequireQueryServices(...interface{})
+}
+
+type StoreKey interface {
+	ModuleKey
+
+	KVStore(context.Context) sdk.KVStore
+	TransientStore(context.Context) sdk.KVStore
+}
+
+type ModuleKey interface {
+	grpc2.ClientConnInterface
+
+	DerivedKey(path string) ModuleKey
+}

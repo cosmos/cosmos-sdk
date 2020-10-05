b9ba9cd7cc03d5ddb81ad45988ddadd8b4e93e57
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-05 13:27:40 +0200

    Remove init

--- std/codec.go
+++ std/codec.go
@@ -1,9 +1,6 @@
 package std
 
 import (
-	tmjson "github.com/tendermint/tendermint/libs/json"
-	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
@@ -11,12 +8,6 @@ import (
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	vesting "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 )
-
-// Temporary to solve https://github.com/cosmos/cosmos-sdk/pull/7442
-func init() {
-	tmjson.RegisterType((*tmcrypto.PublicKey)(nil), "tendermint.crypto.PublicKey")
-	tmjson.RegisterType((*tmcrypto.PublicKey_Ed25519)(nil), "tendermint.crypto.PublicKey_Ed25519")
-}
 
 // RegisterLegacyAminoCodec registers types with the Amino codec.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {

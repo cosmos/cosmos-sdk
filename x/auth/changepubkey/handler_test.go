cf9c859855433633e7f2b4a40f19d1fe8a85bd92
antstalepresh <stalepresh121@outlook.com>
2020-10-08 01:31:08 +1000

    fix unit test issue relate to length of address bytes

--- x/auth/changepubkey/handler_test.go
+++ x/auth/changepubkey/handler_test.go
@@ -31,14 +31,18 @@ func (suite *HandlerTestSuite) SetupTest() {
 func (suite *HandlerTestSuite) TestMsgChangePubKey() {
 	ctx := suite.app.BaseApp.NewContext(false, tmproto.Header{Height: suite.app.LastBlockHeight() + 1})
 
-	addr1 := sdk.AccAddress([]byte("addr1"))
+	addr1 := sdk.AccAddress([]byte("any----------address"))
 	acc1 := suite.app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
 	suite.app.AccountKeeper.SetAccount(ctx, acc1)
 	suite.Require().NoError(suite.app.BankKeeper.SetBalances(ctx, addr1, sdk.NewCoins(sdk.NewInt64Coin("stake", 1000))))
 
-	addr2 := sdk.AccAddress([]byte("addr2"))
+	addr2 := sdk.AccAddress([]byte("some---------address"))
 	acc2 := suite.app.AccountKeeper.NewAccountWithAddress(ctx, addr2)
+	suite.Require().NotNil(acc2)
+	suite.Require().Equal(addr2, acc2.GetAddress())
+
 	suite.app.AccountKeeper.SetAccount(ctx, acc2)
+	suite.Require().NotNil(suite.app.AccountKeeper.GetAccount(ctx, addr2))
 	suite.Require().NoError(suite.app.BankKeeper.SetBalances(ctx, addr2, sdk.NewCoins(sdk.NewInt64Coin("stake", 10000))))
 
 	privKey := secp256k1.GenPrivKeyFromSecret([]byte("mySecret"))

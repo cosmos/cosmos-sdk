5004acaaa65125564e9a745014f57f6ec1fd7e62
antstalepresh <stalepresh121@outlook.com>
2020-10-13 19:42:27 +1000

    update gas limit for only staking test

--- x/staking/client/testutil/test_helpers.go
+++ x/staking/client/testutil/test_helpers.go
@@ -15,6 +15,7 @@ var commonArgs = []string{
 	fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 	fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
 	fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10))).String()),
+	fmt.Sprintf("--gas=%d", 210000),
 }
 
 // MsgRedelegateExec creates a redelegate message.

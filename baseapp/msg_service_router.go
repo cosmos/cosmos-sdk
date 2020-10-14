c306dffb64ce5cd40ee78a056ea83b8340731a77
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-14 15:17:33 +0200

    Update baseapp/msg_service_router.go
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- baseapp/msg_service_router.go
+++ baseapp/msg_service_router.go
@@ -58,7 +58,7 @@ func (msr *MsgServiceRouter) RegisterService(sd *grpc.ServiceDesc, handler inter
 			if !ok {
 				// We panic here because there is no other alternative and the app cannot be initialized correctly
 				// this should only happen if there is a problem with code generation in which case the app won't
-				// work correctly anyway
+				// work correctly anyway.
 				panic(fmt.Errorf("can't register request type %T for service method %s", i, fqMethod))
 			}
 

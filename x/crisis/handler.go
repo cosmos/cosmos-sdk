90d1bd53601dca6beaa3da332b21dc0f4e8e8384
atheesh <atheesh@vitwit.com>
2020-10-14 19:45:39 +0530

    fix lint

--- x/crisis/handler.go
+++ x/crisis/handler.go
@@ -3,14 +3,13 @@ package crisis
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/cosmos/cosmos-sdk/x/crisis/keeper"
 	"github.com/cosmos/cosmos-sdk/x/crisis/types"
 )
 
 // RouterKey
 const RouterKey = types.ModuleName
 
-func NewHandler(k keeper.Keeper) sdk.Handler {
+func NewHandler(k types.MsgServer) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 

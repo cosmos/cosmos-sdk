1294fadc06ddba964d39ecf3c273bda09e60e9fd
Robert Zaremba <robert@zaremba.ch>
2020-10-15 22:44:18 +0200

    order imports

--- baseapp/msg_service_router.go
+++ baseapp/msg_service_router.go
@@ -4,15 +4,13 @@ import (
 	"context"
 	"fmt"
 
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
-	"github.com/gogo/protobuf/proto"
-
 	gogogrpc "github.com/gogo/protobuf/grpc"
+	"github.com/gogo/protobuf/proto"
 	"google.golang.org/grpc"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // MsgServiceRouter routes fully-qualified Msg service methods to their handler.

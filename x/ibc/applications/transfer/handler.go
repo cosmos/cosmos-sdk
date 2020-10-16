4a9b99100d6f60abc00a8fdf1ae5ace1817d1f4d
atheesh <atheesh@vitwit.com>
2020-10-16 21:50:15 +0530

    lint

--- x/ibc/applications/transfer/handler.go
+++ x/ibc/applications/transfer/handler.go
@@ -3,12 +3,11 @@ package transfer
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/keeper"
 	"github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
 )
 
 // NewHandler returns sdk.Handler for IBC token transfer module messages
-func NewHandler(k keeper.Keeper) sdk.Handler {
+func NewHandler(k types.MsgServer) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 

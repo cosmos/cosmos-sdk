8cd8466d233dd7878a9016b73e844fd6de777b53
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-26 18:33:23 +0100

    Add comment about panic

--- baseapp/msg_service_router.go
+++ baseapp/msg_service_router.go
@@ -46,7 +46,10 @@ func (msr *MsgServiceRouter) RegisterService(sd *grpc.ServiceDesc, handler inter
 		methodHandler := method.Handler
 
 		// Check that the service Msg fully-qualified method name has already
-		// been registered (via RegisterInterfaces).
+		// been registered (via RegisterInterfaces). If the user registers the
+		// services without registering the service Msg types, there might be
+		// some unexpected behavior down the road. So we prefer to panic here
+		// (at startup).
 		serviceMsg, err := msr.interfaceRegistry.Resolve(fqMethod)
 		if err != nil || serviceMsg == nil {
 			panic(

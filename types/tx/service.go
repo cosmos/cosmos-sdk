a488465145b487bbe33e3fb50ee1e7871ff8b353
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-27 18:52:32 +0100

    Fix lint

--- types/tx/service.go
+++ types/tx/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/hex"
 
+	gogogrpc "github.com/gogo/protobuf/grpc"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -117,6 +118,6 @@ func TxBuilderToProtoTx(txBuilder client.TxBuilder) (*Tx, error) { // nolint
 
 // RegisterGRPCGatewayRoutes mounts the tx service's GRPC-gateway routes on the
 // given Mux.
-func RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	RegisterServiceHandlerClient(context.Background(), mux, NewServiceClient(clientCtx))
+func RegisterGRPCGatewayRoutes(clientConn gogogrpc.ClientConn, mux *runtime.ServeMux) {
+	RegisterServiceHandlerClient(context.Background(), mux, NewServiceClient(clientConn))
 }

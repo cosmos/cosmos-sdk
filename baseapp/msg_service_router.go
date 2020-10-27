817c0aecdb802a3ca1f0a93f308afab662c16ff0
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-27 10:53:57 +0100

    Update baseapp/msg_service_router.go
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>

--- baseapp/msg_service_router.go
+++ baseapp/msg_service_router.go
@@ -47,7 +47,7 @@ func (msr *MsgServiceRouter) RegisterService(sd *grpc.ServiceDesc, handler inter
 
 		// Check that the service Msg fully-qualified method name has already
 		// been registered (via RegisterInterfaces). If the user registers a
-		// services without registering the service Msg types, there might be
+		// service without registering according service Msg type, there might be
 		// some unexpected behavior down the road. Since we can't return an error (`Server.RegisterService` interface restriction) we panic.
 		// (at startup).
 		serviceMsg, err := msr.interfaceRegistry.Resolve(fqMethod)

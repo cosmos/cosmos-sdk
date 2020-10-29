e1c7d0218da15e75a6569c9048a553eee0b21e85
Cory <cjlevinson@gmail.com>
2020-10-29 04:37:46 -0700

    Add Deprecation headers for legacy rest endpoints (#7686)
    * add deprecation headers for legacy rest endpoints
    
    * add deprecation headers for missing tx routes
    
    * rm handler-level deprecation headers
    
    * switch to middleware Route.Use method for setting deprecation Headers
    
    * set deprecation headers using subrouter
    
    * cleanup gofmt
    
    * goimports
    
    * Update client/rest/rest.go
    
    * update deprecation headers to be set on each module individually
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- x/upgrade/client/rest/rest.go
+++ x/upgrade/client/rest/rest.go
@@ -1,13 +1,15 @@
 package rest
 
 import (
+	"github.com/cosmos/cosmos-sdk/client/rest"
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client"
 )
 
 // RegisterRoutes registers REST routes for the upgrade module under the path specified by routeName.
-func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
+func RegisterRoutes(clientCtx client.Context, rtr *mux.Router) {
+	r := rest.WithHTTPDeprecationHeaders(rtr)
 	registerQueryRoutes(clientCtx, r)
 	registerTxHandlers(clientCtx, r)
 }

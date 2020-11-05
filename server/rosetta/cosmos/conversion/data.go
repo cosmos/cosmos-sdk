8b26b8e2aee756d38cfce8660cabdd1f094a696b
frojdidymylja <frojdi.dymylja@gmail.com>
2020-11-05 18:55:43 +0100

    add: detect transaction operations from msgs

--- server/rosetta/cosmos/conversion/data.go
+++ server/rosetta/cosmos/conversion/data.go
@@ -5,8 +5,11 @@ import (
 	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/cosmos/cosmos-sdk/server/rosetta"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	tmcoretypes "github.com/tendermint/tendermint/rpc/core/types"
 	tmtypes "github.com/tendermint/tendermint/types"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -35,9 +38,10 @@ func CoinsToBalance(coins []sdk.Coin) []*types.Amount {
 func ResultTxSearchToTransaction(txs []*rosetta.SdkTxWithHash) []*types.Transaction {
 	converted := make([]*types.Transaction, len(txs))
 	for i, tx := range txs {
+		//hasError := tx.Code > 0
 		converted[i] = &types.Transaction{
 			TransactionIdentifier: &types.TransactionIdentifier{Hash: tx.HexHash},
-			Operations:            SdkTxToOperations(tx.Tx),
+			Operations:            SdkTxToOperations(tx.Tx, false, false),
 			Metadata:              nil,
 		}
 	}
@@ -45,30 +49,8 @@ func ResultTxSearchToTransaction(txs []*rosetta.SdkTxWithHash) []*types.Transact
 }
 
 // SdkTxResponseToOperations converts a tx response to operations
-func SdkTxToOperations(tx sdk.Tx) []*types.Operation {
-	msgs := tx.GetMsgs()
-	ops := make([]*types.Operation, len(msgs))
-	for i, msg := range msgs {
-		// TODO: assert if balance change op?
-		ops[i] = &types.Operation{
-			OperationIdentifier: &types.OperationIdentifier{
-				Index:        int64(i),
-				NetworkIndex: nil,
-			},
-			RelatedOperations: nil,
-			Type:              msg.Type(),
-			Status:            "",
-			Account: &types.AccountIdentifier{
-				Address:    "",
-				SubAccount: nil,
-				Metadata:   nil,
-			},
-			Amount:     nil,
-			CoinChange: nil,
-			Metadata:   nil,
-		}
-	}
-	return ops
+func SdkTxToOperations(tx sdk.Tx, hasError, withoutStatus bool) []*types.Operation {
+	return toOperations(tx.GetMsgs(), hasError, withoutStatus)
 }
 
 // TendermintTxsToTxIdentifiers converts a tendermint raw transaction into a rosetta tx identifier
@@ -86,4 +68,96 @@ func TendermintBlockToBlockIdentifier(block *tmcoretypes.ResultBlock) *types.Blo
 		Index: block.Block.Height,
 		Hash:  block.Block.Hash().String(),
 	}
+}
+
+func toOperations(msgs []sdk.Msg, hasError bool, withoutStatus bool) (operations []*types.Operation) {
+	for i, msg := range msgs {
+		x := msg.Type()
+		switch x {
+		case "send":
+			newMsg := msg.(*banktypes.MsgSend)
+			fromAddress := newMsg.FromAddress
+			toAddress := newMsg.ToAddress
+			amounts := newMsg.Amount
+			if len(amounts) == 0 {
+				continue
+			}
+			coin := amounts[0]
+			sendOp := func(account, amount string, index int) *types.Operation {
+				status := rosetta.StatusSuccess
+				if hasError {
+					status = rosetta.StatusReverted
+				}
+				if withoutStatus {
+					status = ""
+				}
+				return &types.Operation{
+					OperationIdentifier: &types.OperationIdentifier{
+						Index: int64(index),
+					},
+					Type:   rosetta.OperationTransfer,
+					Status: status,
+					Account: &types.AccountIdentifier{
+						Address: account,
+					},
+					Amount: &types.Amount{
+						Value: amount,
+						Currency: &types.Currency{
+							Symbol: coin.Denom,
+						},
+					},
+				}
+			}
+			operations = append(operations,
+				sendOp(fromAddress, "-"+coin.Amount.String(), i),
+				sendOp(toAddress, coin.Amount.String(), i+1),
+			)
+		}
+	}
+	return
+}
+
+func GetMsgDataFromOperations(ops []*types.Operation) (sdk.Msg, error) {
+	op := ops[0]
+	switch op.Type {
+	case rosetta.OperationTransfer:
+		return getTransferTxDataFromOperations(ops)
+	}
+
+	return nil, fmt.Errorf("unable to iterate operations")
+}
+
+// getTransferTxDataFromOperations extracts the from and to addresses from a list of operations.
+// We assume that it comes formated in the correct way. And that the balance of the sender is the same
+// as the receiver operations.
+func getTransferTxDataFromOperations(ops []*types.Operation) (sdk.Msg, error) {
+	var (
+		from, to sdk.AccAddress
+		sendAmt  sdk.Coin
+		err      error
+	)
+
+	for _, op := range ops {
+		if strings.HasPrefix(op.Amount.Value, "-") {
+			from, err = sdk.AccAddressFromBech32(op.Account.Address)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			to, err = sdk.AccAddressFromBech32(op.Account.Address)
+			if err != nil {
+				return nil, err
+			}
+
+			amount, err := strconv.ParseInt(op.Amount.Value, 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid amount")
+			}
+
+			sendAmt = sdk.NewCoin(op.Amount.Currency.Symbol, sdk.NewInt(amount))
+		}
+	}
+
+	msg := banktypes.NewMsgSend(from, to, sdk.NewCoins(sendAmt))
+	return msg, nil
 }
--- server/rosetta/services/data.go
+++ server/rosetta/services/data.go
@@ -102,7 +102,7 @@ func (sn SingleNetwork) BlockTransaction(ctx context.Context, request *types.Blo
 	return &types.BlockTransactionResponse{
 		Transaction: &types.Transaction{
 			TransactionIdentifier: &types.TransactionIdentifier{Hash: request.TransactionIdentifier.Hash},
-			Operations:            conversion.SdkTxToOperations(tx),
+			Operations:            conversion.SdkTxToOperations(tx, false, false),
 			Metadata:              nil,
 		},
 	}, nil
@@ -126,7 +126,7 @@ func (sn SingleNetwork) MempoolTransaction(ctx context.Context, request *types.M
 	return &types.MempoolTransactionResponse{
 		Transaction: &types.Transaction{
 			TransactionIdentifier: &types.TransactionIdentifier{Hash: request.TransactionIdentifier.Hash},
-			Operations:            conversion.SdkTxToOperations(tx),
+			Operations:            conversion.SdkTxToOperations(tx, false, false),
 			Metadata:              nil,
 		},
 	}, nil

4684bf03589fdd10d0d2feef10b193d5b2a4c543
Amaury Martiny <amaury.martiny@protonmail.com>
2020-11-06 11:53:02 +0100

    Remove useless function

--- crypto/codec/tm.go
+++ crypto/codec/tm.go
@@ -3,14 +3,13 @@ package codec
 import (
 	tmcrypto "github.com/tendermint/tendermint/crypto"
 	tmed25519 "github.com/tendermint/tendermint/crypto/ed25519"
-	tmprotocrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// FromTmPubKey converts TM's tmcrypto.PubKey toour own PubKey.
+// FromTmPubKey converts TM's tmcrypto.PubKey to our own PubKey.
 func FromTmPubKey(tmPk tmcrypto.PubKey) (cryptotypes.PubKey, error) {
 	switch tmPk := tmPk.(type) {
 	case tmed25519.PubKey:
@@ -28,20 +27,5 @@ func ToTmPubKey(pk cryptotypes.PubKey) (tmcrypto.PubKey, error) {
 		return tmed25519.PubKey(pk.Key), nil
 	default:
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot convert %v to Tendermint public key", pk)
-	}
-
-}
-
-// ToTmPublicKey converts our own PubKey to TM's tmprotocrypto.PublicKey.
-func ToTmPublicKey(pk cryptotypes.PubKey) (tmprotocrypto.PublicKey, error) {
-	switch pk := pk.(type) {
-	case *ed25519.PubKey:
-		return tmprotocrypto.PublicKey{
-			Sum: &tmprotocrypto.PublicKey_Ed25519{
-				Ed25519: pk.Key,
-			},
-		}, nil
-	default:
-		return tmprotocrypto.PublicKey{}, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot convert %v to Tendermint public key", pk)
 	}
 }

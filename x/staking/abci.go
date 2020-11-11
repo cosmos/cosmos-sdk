85e9f22ad94c7f4fd582ad8b73a878d33a68029b
antstalepresh <stalepresh121@outlook.com>
2020-11-12 03:43:56 +1000

    validator set epoching

--- x/staking/abci.go
+++ x/staking/abci.go
@@ -14,14 +14,17 @@ import (
 // BeginBlocker will persist the current header and validator set as a historical entry
 // and prune the oldest entry based on the HistoricalEntries parameter
 func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
-	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
-
-	k.TrackHistoricalInfo(ctx)
+	if ctx.BlockHeight()%10 == 1 {
+		defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
+		k.TrackHistoricalInfo(ctx)
+	}
 }
 
 // Called every block, update validator set
 func EndBlocker(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
-	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyEndBlocker)
-
-	return k.BlockValidatorUpdates(ctx)
+	if ctx.BlockHeight()%10 == 0 {
+		defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyEndBlocker)
+		return k.BlockValidatorUpdates(ctx)
+	}
+	return []abci.ValidatorUpdate{}
 }

2237274ef31d111773809be0a2cab77828b3df29
Marko <marbar3778@yahoo.com>
2020-11-11 21:36:54 +0100

    Update scripts/protocgen.sh
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,7 +3,6 @@
 set -eo pipefail
 
 protoc_gen_gocosmos() {
-
   if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
     echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
     return 1

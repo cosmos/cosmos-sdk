6e4d099dc00847702f0ba8e1bf914b510eaf392c
Colin Axner <colinaxner@berkeley.edu>
2020-11-19 11:00:09 +0100

    use ADR 028 AddressHash construction

--- x/ibc/applications/transfer/types/keys.go
+++ x/ibc/applications/transfer/types/keys.go
@@ -1,9 +1,8 @@
 package types
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"github.com/tendermint/tendermint/crypto"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -44,14 +43,17 @@ var (
 )
 
 // GetEscrowAddress returns the escrow address for the specified channel.
-// The escrow address uses a prefix to prevent address collsions with Public Key addresses.
-//
-// CONTRACT: this assumes that there's only one bank bridge module that owns the
-// port associated with the channel ID so that the address created is actually
-// unique.
+// The escrow address follows the format as outlined in ADR 028:
+// https://github.com/cosmos/cosmos-sdk/blob/master/docs/architecture/adr-028-public-key-addresses.md
 func GetEscrowAddress(portID, channelID string) sdk.AccAddress {
-	return append(
-		[]byte(EscrowPrefix),
-		crypto.AddressHash([]byte(fmt.Sprintf("%s/%s", portID, channelID)))...,
-	)
+	// a slash is used to create domain separation between port and channel identifiers to
+	// prevent address  collisions between escrow addresses created for different channels
+	contents := fmt.Sprintf("%s/%s", portID, channelID)
+
+	// ADR 028 AddressHash construction
+	preImage := []byte(EscrowPrefix)
+	preImage = append(preImage, 0)
+	preImage = append(preImage, contents...)
+	hash := sha256.Sum256(preImage)
+	return hash[:20]
 }

6d10064e8cc0424b34e9226e3054fce5a19cc89f
Aditya Sripal <adityasripal@gmail.com>
2020-11-23 17:04:38 +0000

    improve tm genesis test

--- x/ibc/light-clients/07-tendermint/types/genesis_test.go
+++ x/ibc/light-clients/07-tendermint/types/genesis_test.go
@@ -19,18 +19,20 @@ func (suite *TendermintTestSuite) TestExportMetadata() {
 	clientStore := suite.chainA.App.IBCKeeper.ClientKeeper.ClientStore(suite.chainA.GetContext(), "clientA")
 
 	// set some processed times
-	timestamp := uint64(time.Now().UnixNano())
-	timestampBz := sdk.Uint64ToBigEndian(timestamp)
-	types.SetProcessedTime(clientStore, clienttypes.NewHeight(0, 1), timestamp)
-	types.SetProcessedTime(clientStore, clienttypes.NewHeight(0, 2), timestamp)
+	timestamp1 := uint64(time.Now().UnixNano())
+	timestamp2 := uint64(time.Now().Add(time.Minute).UnixNano())
+	timestampBz1 := sdk.Uint64ToBigEndian(timestamp1)
+	timestampBz2 := sdk.Uint64ToBigEndian(timestamp2)
+	types.SetProcessedTime(clientStore, clienttypes.NewHeight(0, 1), timestamp1)
+	types.SetProcessedTime(clientStore, clienttypes.NewHeight(0, 2), timestamp2)
 
 	gm = clientState.ExportMetadata(suite.chainA.App.IBCKeeper.ClientKeeper.ClientStore(suite.chainA.GetContext(), "clientA"))
 	suite.Require().NotNil(gm, "client with metadata returned nil exported metadata")
 	suite.Require().Len(gm, 2, "exported metadata has unexpected length")
 
 	suite.Require().Equal(types.ProcessedTimeKey(clienttypes.NewHeight(0, 1)), gm[0].GetKey(), "metadata has unexpected key")
-	suite.Require().Equal(timestampBz, gm[0].GetValue(), "metadata has unexpected value")
+	suite.Require().Equal(timestampBz1, gm[0].GetValue(), "metadata has unexpected value")
 
 	suite.Require().Equal(types.ProcessedTimeKey(clienttypes.NewHeight(0, 2)), gm[1].GetKey(), "metadata has unexpected key")
-	suite.Require().Equal(timestampBz, gm[0].GetValue(), "metadata has unexpected value")
+	suite.Require().Equal(timestampBz2, gm[1].GetValue(), "metadata has unexpected value")
 }

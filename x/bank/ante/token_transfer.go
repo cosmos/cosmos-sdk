0128d318f88cef5897293fa9616564424c7c1aff
secret <secret@bianjie.ai>
2020-11-24 14:14:49 +0800

    add multisend validation

--- x/bank/ante/token_transfer.go
+++ x/bank/ante/token_transfer.go
@@ -35,7 +35,10 @@ func (vtd ValidateTokenTransferDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx,
 					}
 
 				case *types.MsgMultiSend:
-					// TODO
+					err := vtd.validateMsgMultiSend(ctx, msg)
+					if err != nil {
+						return ctx, err
+					}
 				}
 			}
 		}
@@ -44,6 +47,7 @@ func (vtd ValidateTokenTransferDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx,
 	return next(ctx, tx, simulate)
 }
 
+// validateMsgSend validates the MsgSend msg
 func (vtd ValidateTokenTransferDecorator) validateMsgSend(ctx sdk.Context, msg *types.MsgSend) error {
 	for _, coin := range msg.Amount {
 		ownerAddr, err := vtd.tokenKeeper.GetOwner(ctx, coin.Denom)
@@ -63,7 +67,63 @@ func (vtd ValidateTokenTransferDecorator) validateMsgSend(ctx sdk.Context, msg *
 	return nil
 }
 
+// validateMsgSend validates the MsgMultiSend msg
 func (vtd ValidateTokenTransferDecorator) validateMsgMultiSend(ctx sdk.Context, msg *types.MsgMultiSend) error {
-	// TODO
+	inputMap := getInputMap(msg.Inputs)
+	outputMap := getOutputMap(msg.Outputs)
+
+	for denom, addresses := range inputMap {
+		ownerAddr, err := vtd.tokenKeeper.GetOwner(ctx, denom)
+		if err == nil {
+			owner := ownerAddr.String()
+
+			if !owned(owner, addresses) && !owned(owner, outputMap[denom]) {
+				return sdkerrors.Wrapf(
+					types.ErrUnauthorizedTransfer,
+					"either the sender or recipient must be the owner %s for token %s",
+					owner, denom,
+				)
+			}
+		}
+	}
+
 	return nil
+}
+
+// owned returns false if any address is not the owner of the denom among the given non-empty addresses
+// True otherwise
+func owned(owner string, addresses []string) bool {
+	for _, addr := range addresses {
+		if addr != owner {
+			return false
+		}
+	}
+
+	return true
+}
+
+// getInputMap maps input denoms to addresses
+func getInputMap(inputs []types.Input) map[string][]string {
+	inputMap := make(map[string][]string)
+
+	for _, input := range inputs {
+		for _, coin := range input.Coins {
+			inputMap[coin.Denom] = append(inputMap[coin.Denom], input.Address)
+		}
+	}
+
+	return inputMap
+}
+
+// getOutputMap maps output denoms to addresses
+func getOutputMap(outputs []types.Output) map[string][]string {
+	outputMap := make(map[string][]string)
+
+	for _, output := range outputs {
+		for _, coin := range output.Coins {
+			outputMap[coin.Denom] = append(outputMap[coin.Denom], output.Address)
+		}
+	}
+
+	return outputMap
 }

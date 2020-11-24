7151b25a8dc49988f3a185cd7a70ecd37fb5b088
Robert Zaremba <robert@zaremba.ch>
2020-11-24 21:52:26 +0100

    remove AssertMsg

--- internal/protocdc/marshal.go
+++ internal/protocdc/marshal.go
@@ -13,19 +13,9 @@ import (
 // This function should be used only with concrete types. For interface serialization
 // you need to wrap the interface into Any or generally use MarshalIfcJSON.
 func MarshalJSON(msg interface{}, resolver jsonpb.AnyResolver) ([]byte, error) {
-	msgProto, err := AssertMsg(msg)
-	if err != nil {
-		return nil, err
-	}
-	return codec.ProtoMarshalJSON(msgProto, resolver)
-}
-
-// AssertMsg casts i to a proto.Message. Returns an error if it's not possible.
-// TODO remove
-func AssertMsg(i interface{}) (proto.Message, error) {
-	pm, ok := i.(proto.Message)
+	msgProto, ok := i.(proto.Message)
 	if !ok {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "Expecting protobuf Message type, got %T", i)
 	}
-	return pm, nil
+	return codec.ProtoMarshalJSON(msgProto, resolver)
 }

e424be4b324833bc24cfea26c6a5831da2badc03
atheesh <atheesh@vitwit.com>
2020-12-11 13:38:20 +0530

    Unused types and funcs

--- x/feegrant/types/test_common.go
+++ x/feegrant/types/test_common.go
@@ -2,26 +2,26 @@
 
 package types
 
-import (
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+// import (
+// 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
-)
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
+// )
 
-func NewTestTx(ctx sdk.Context, msgs []sdk.Msg, privs []cryptotypes.PrivKey, accNums []uint64, seqs []uint64, fee GrantedFee) sdk.Tx {
-	sigs := make([]legacytx.StdSignature, len(privs))
-	for i, priv := range privs {
-		signBytes := StdSignBytes(ctx.ChainID(), accNums[i], seqs[i], fee, msgs, "")
+// func NewTestTx(ctx sdk.Context, msgs []sdk.Msg, privs []cryptotypes.PrivKey, accNums []uint64, seqs []uint64, fee GrantedFee) sdk.Tx {
+// 	sigs := make([]legacytx.StdSignature, len(privs))
+// 	for i, priv := range privs {
+// 		signBytes := StdSignBytes(ctx.ChainID(), accNums[i], seqs[i], fee, msgs, "")
 
-		sig, err := priv.Sign(signBytes)
-		if err != nil {
-			panic(err)
-		}
+// 		sig, err := priv.Sign(signBytes)
+// 		if err != nil {
+// 			panic(err)
+// 		}
 
-		sigs[i] = legacytx.StdSignature{PubKey: priv.PubKey(), Signature: sig}
-	}
+// 		sigs[i] = legacytx.StdSignature{PubKey: priv.PubKey(), Signature: sig}
+// 	}
 
-	tx := NewFeeGrantTx(msgs, fee, sigs, "")
-	return tx
-}
+// 	tx := NewFeeGrantTx(msgs, fee, sigs, "")
+// 	return tx
+// }

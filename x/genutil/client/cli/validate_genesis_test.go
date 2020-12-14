faa8c13ab82060f53309462f7ce30c64c82d4b70
Amaury Martiny <amaury.martiny@protonmail.com>
2020-12-14 14:12:48 +0100

    Better indentation

--- x/genutil/client/cli/validate_genesis_test.go
+++ x/genutil/client/cli/validate_genesis_test.go
@@ -9,21 +9,21 @@ import (
 // An example exported genesis file from a 0.37 chain. Note that evidence
 // parameters only contains `max_age`.
 var v037Exported = `{
-		"app_hash": "",
-		"app_state": {},
-		"chain_id": "test",
-		"consensus_params": {
-		  "block": {
-			"max_bytes": "22020096",
-			"max_gas": "-1",
-			"time_iota_ms": "1000"
-		  },
-		  "evidence": { "max_age": "100000" },
-		  "validator": { "pub_key_types": ["ed25519"] }
+	"app_hash": "",
+	"app_state": {},
+	"chain_id": "test",
+	"consensus_params": {
+		"block": {
+		"max_bytes": "22020096",
+		"max_gas": "-1",
+		"time_iota_ms": "1000"
 		},
-		"genesis_time": "2020-09-29T20:16:29.172362037Z",
-		"validators": []
-	  }`
+		"evidence": { "max_age": "100000" },
+		"validator": { "pub_key_types": ["ed25519"] }
+	},
+	"genesis_time": "2020-09-29T20:16:29.172362037Z",
+	"validators": []
+}`
 
 func (s *IntegrationTestSuite) TestValidateGenesis_FromV037() {
 	val0 := s.network.Validators[0]

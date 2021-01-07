e13e6034f3d62aa8408486cb0f7cab104edf6924
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2021-01-07 13:29:44 +0100

    add proto message to Header and Misbehaviour

--- x/ibc/core/exported/client.go
+++ x/ibc/core/exported/client.go
@@ -178,6 +178,8 @@ type ConsensusState interface {
 
 // Misbehaviour defines counterparty misbehaviour for a specific consensus type
 type Misbehaviour interface {
+    proto.Message
+
 	ClientType() string
 	GetClientID() string
 	ValidateBasic() error
@@ -188,6 +190,8 @@ type Misbehaviour interface {
 
 // Header is the consensus state update information
 type Header interface {
+    proto.Message
+    
 	ClientType() string
 	GetHeight() Height
 	ValidateBasic() error

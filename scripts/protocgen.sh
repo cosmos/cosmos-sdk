a93de2462fbde5002eb03593a60e195799a30366
Marko Baricevic <marbar3778@yahoo.com>
2021-01-18 15:55:43 +0100

    merge grpc_gateway into gocosmos_out

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -20,12 +20,6 @@ for dir in $proto_dirs; do
   -I "third_party/proto" \
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-  # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-  buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
   --grpc-gateway_out=logtostderr=true:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 

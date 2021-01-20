64cfb4653c960ec353254191514f5b6cc38fd33e
Amaury Martiny <amaury.martiny@protonmail.com>
2021-01-20 15:24:57 +0100

    Fix merge master

--- x/bank/keeper/grpc_query.go
+++ x/bank/keeper/grpc_query.go
@@ -6,6 +6,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/cosmos-sdk/x/bank/types"

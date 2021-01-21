57f5e965708fe9acdd3e96cb5fd586f383e17824
SaReN <sahithnarahari@gmail.com>
2021-01-21 15:03:02 +0530

    Rosetta API implementation (#7695)
    Ref: #7492
    
    Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>
    Co-authored-by: Alessio Treglia <alessio@tendermint.com>
    Co-authored-by: Frojdi Dymylja <33157909+fdymylja@users.noreply.github.com>
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>
    Co-authored-by: Federico Kunze <31522760+fedekunze@users.noreply.github.com>

---
+++ contrib/rosetta/configuration/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+addr="abcd"
+
+send_tx() {
+  echo '12345678' | simd tx bank send $addr "$1" "$2"
+}
+
+detect_account() {
+  line=$1
+}
+
+wait_for_rosetta() {
+  timeout 30 sh -c 'until nc -z $0 $1; do sleep 1; done' rosetta 8080
+}
+
+echo "waiting for rosetta instance to be up"
+wait_for_rosetta
+
+echo "checking data API"
+rosetta-cli check:data --configuration-file ./config/rosetta.json
+
+echo "checking construction API"
+rosetta-cli check:construction --configuration-file ./config/rosetta.json
+
+echo "checking staking API"
+rosetta-cli check:construction --configuration-file ./config/staking.json

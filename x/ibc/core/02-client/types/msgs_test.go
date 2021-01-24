8b08673420fca1e7c5db6fa393172ad6a3a8e3f7
Federico Kunze <federico.kunze94@gmail.com>
2021-01-24 11:31:47 -0300

    wip msg tests

--- x/ibc/core/02-client/types/msgs_test.go
+++ x/ibc/core/02-client/types/msgs_test.go
@@ -1,6 +1,7 @@
 package types_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -32,6 +33,24 @@ func (suite *TypesTestSuite) SetupTest() {
 
 func TestTypesTestSuite(t *testing.T) {
 	suite.Run(t, new(TypesTestSuite))
+}
+
+func (suite *TypesTestSuite) TestMsgCreateClientGetSignBytes() {
+	soloMachine := ibctesting.NewSolomachine(suite.T(), suite.chainA.Codec, "solomachine", "", 2)
+	msg, err := types.NewMsgCreateClient(soloMachine.ClientState(), soloMachine.ConsensusState(), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res := msg.GetSignBytes()
+
+	expected := `{"type":"cosmos-sdk/MsgCreateClient","value":{"client_state":{"consensus_state":{"public_key":{"public_keys":["AibT/cJYde0lnMNJ/hb90Gsg9e5sXqB+aGOfC69Cl4VC","ArRUuKZ4p6+aIgKEuEsYSx4QHOqpNE9vstk2KzZH6Ig5"],"threshold":2},"timestamp":"10"},"sequence":"1"},"consensus_state":{"public_key":{"public_keys":["AibT/cJYde0lnMNJ/hb90Gsg9e5sXqB+aGOfC69Cl4VC","ArRUuKZ4p6+aIgKEuEsYSx4QHOqpNE9vstk2KzZH6Ig5"],"threshold":2},"timestamp":"10"},"signer":"cosmos1eulvtawa5ynj6qlxcv9ys7myahlyshhvl9vmlf"}}`
+	suite.Require().Equal(expected, string(res))
+
+	tendermintClient := ibctmtypes.NewClientState(suite.chainA.ChainID, ibctesting.DefaultTrustLevel, ibctesting.TrustingPeriod, ibctesting.UnbondingPeriod, ibctesting.MaxClockDrift, clientHeight, commitmenttypes.GetSDKSpecs(), ibctesting.UpgradePath, false, false)
+	msg, err = types.NewMsgCreateClient(tendermintClient, suite.chainA.CurrentTMClientHeader().ConsensusState(), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res = msg.GetSignBytes()
+
+	expected = `{"type":"cosmos-sdk/MsgCreateClient","value":{"client_state":{"chain_id":"testchain0","frozen_height":{},"latest_height":{"revision_height":"10"},"max_clock_drift":"10000000000","proof_specs":[{"inner_spec":{"child_order":[0,1],"child_size":33,"hash":1,"max_prefix_length":12,"min_prefix_length":4},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}},{"inner_spec":{"child_order":[0,1],"child_size":32,"hash":1,"max_prefix_length":1,"min_prefix_length":1},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}}],"trust_level":{"denominator":"3","numerator":"1"},"trusting_period":"1209600000000000","unbonding_period":"1814400000000000","upgrade_path":["upgrade","upgradedIBCState"]},"consensus_state":{"next_validators_hash":"018119586C484CA72377680E19A8E848811DF312246D7A575ED030E2655B7BF7","root":{"hash":"5/1HBckYk1urdEnNfIduqeKbWzizaggikXRZ2wetyJM="},"timestamp":"2020-01-02T00:00:00Z"},"signer":"cosmos1rashze72a4sm6003nvn7u740kxulhyftawv0fk"}}`
+	suite.Require().Equal(expected, string(res))
 }
 
 // tests that different clients within MsgCreateClient can be marshaled
@@ -240,6 +259,23 @@ func (suite *TypesTestSuite) TestMarshalMsgUpdateClient() {
 	}
 }
 
+func (suite *TypesTestSuite) TestMsgUpdateClientGetSignBytes() {
+	soloMachine := ibctesting.NewSolomachine(suite.T(), suite.chainA.Codec, "solomachine", "", 2)
+	msg, err := types.NewMsgUpdateClient(soloMachine.ClientID, soloMachine.CreateHeader(), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res := msg.GetSignBytes()
+
+	expected := fmt.Sprintf(`{"type":"cosmos-sdk/MsgUpdateClient","value":{"client_id":"solomachine","header":{"new_public_key":{"public_keys":["AzFlPey75R/Qj/bgzN2lyRUECssP6sxMRUj5p4npKL/p","A0mbyQaw2T+fvtar1baQkss2/TOCXCAxGK51UCPOu6ga"],"threshold":2},"sequence":"1","signature":"EpMBCgUIAhIBwBJECkISQOu8lGSWlSNiGpQYYpRMvlKG6NP19LRC37DCEXa6f0NNKU9VvA0EMwApAwhxagjnS87tRt8hZhn6UGQcVaNOR20SRApCEkDbgirlEJpPdYi6ZKdMkS36eoflXLmlsKhXYnRy/DGq1mWsgqYPiIjxNF7jw3WLxhbDPtfHBE/E239hEYRCjtjA","timestamp":"10"},"signer":"%s"}}`, suite.chainA.SenderAccount.GetAddress())
+	// suite.Require().Equal(expected, string(res))
+
+	msg, err = types.NewMsgUpdateClient("tendermint", suite.chainA.CurrentTMClientHeader(), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res = msg.GetSignBytes()
+
+	expected = `{"type":"cosmos-sdk/MsgCreateClient","value":{"client_state":{"chain_id":"testchain0","frozen_height":{},"latest_height":{"revision_height":"10"},"max_clock_drift":"10000000000","proof_specs":[{"inner_spec":{"child_order":[0,1],"child_size":33,"hash":1,"max_prefix_length":12,"min_prefix_length":4},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}},{"inner_spec":{"child_order":[0,1],"child_size":32,"hash":1,"max_prefix_length":1,"min_prefix_length":1},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}}],"trust_level":{"denominator":"3","numerator":"1"},"trusting_period":"1209600000000000","unbonding_period":"1814400000000000","upgrade_path":["upgrade","upgradedIBCState"]},"consensus_state":{"next_validators_hash":"018119586C484CA72377680E19A8E848811DF312246D7A575ED030E2655B7BF7","root":{"hash":"5/1HBckYk1urdEnNfIduqeKbWzizaggikXRZ2wetyJM="},"timestamp":"2020-01-02T00:00:00Z"},"signer":"cosmos1rashze72a4sm6003nvn7u740kxulhyftawv0fk"}}`
+	suite.Require().Equal(expected, string(res)) // FIXME: this panics
+}
+
 func (suite *TypesTestSuite) TestMsgUpdateClient_ValidateBasic() {
 	var (
 		msg = &types.MsgUpdateClient{}
@@ -375,6 +411,25 @@ func (suite *TypesTestSuite) TestMarshalMsgUpgradeClient() {
 			suite.Require().NoError(err)
 		})
 	}
+}
+
+func (suite *TypesTestSuite) TestMsgUpgradeClientGetSignBytes() {
+	soloMachine := ibctesting.NewSolomachine(suite.T(), suite.chainA.Codec, "solomachine", "", 1)
+	msg, err := types.NewMsgUpgradeClient("clientid", soloMachine.ClientState(), soloMachine.ConsensusState(), []byte("proofUpgradeClient"), []byte("proofUpgradeConsState"), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res := msg.GetSignBytes()
+
+	expected := fmt.Sprintf(`{"type":"cosmos-sdk/MsgUpgradeClient","value":{"client_id":"clientid","client_state":{"consensus_state":{"public_key":"AwrpSEaM4tX2+JlSU5F1KJeEUhuZvD1yAnW8P64HmTFV","timestamp":"10"},"sequence":"1"},"consensus_state":{"public_key":"AwrpSEaM4tX2+JlSU5F1KJeEUhuZvD1yAnW8P64HmTFV","timestamp":"10"},"proof_upgrade_client":"cHJvb2ZVcGdyYWRlQ2xpZW50","proof_upgrade_consensus_state":"cHJvb2ZVcGdyYWRlQ29uc1N0YXRl","signer":"%s"}}`, suite.chainA.SenderAccount.GetAddress())
+	suite.Require().Equal(expected, string(res))
+
+	tendermintClient := ibctmtypes.NewClientState(suite.chainA.ChainID, ibctesting.DefaultTrustLevel, ibctesting.TrustingPeriod, ibctesting.UnbondingPeriod, ibctesting.MaxClockDrift, clientHeight, commitmenttypes.GetSDKSpecs(), ibctesting.UpgradePath, false, false)
+	tendermintConsState := &ibctmtypes.ConsensusState{NextValidatorsHash: []byte("nextValsHash")}
+	msg, err = types.NewMsgUpgradeClient("clientid", tendermintClient, tendermintConsState, []byte("proofUpgradeClient"), []byte("proofUpgradeConsState"), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res = msg.GetSignBytes()
+
+	expected = fmt.Sprintf(`{"type":"cosmos-sdk/MsgUpgradeClient","value":{"client_id":"clientid","client_state":{"chain_id":"testchain0","frozen_height":{},"latest_height":{"revision_height":"10"},"max_clock_drift":"10000000000","proof_specs":[{"inner_spec":{"child_order":[0,1],"child_size":33,"hash":1,"max_prefix_length":12,"min_prefix_length":4},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}},{"inner_spec":{"child_order":[0,1],"child_size":32,"hash":1,"max_prefix_length":1,"min_prefix_length":1},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}}],"trust_level":{"denominator":"3","numerator":"1"},"trusting_period":"1209600000000000","unbonding_period":"1814400000000000","upgrade_path":["upgrade","upgradedIBCState"]},"consensus_state":{"next_validators_hash":"6E65787456616C7348617368","root":{},"timestamp":"0001-01-01T00:00:00Z"},"proof_upgrade_client":"cHJvb2ZVcGdyYWRlQ2xpZW50","proof_upgrade_consensus_state":"cHJvb2ZVcGdyYWRlQ29uc1N0YXRl","signer":"%s"}}`, suite.chainA.SenderAccount.GetAddress())
+	suite.Require().Equal(expected, string(res))
 }
 
 func (suite *TypesTestSuite) TestMsgUpgradeClient_ValidateBasic() {
@@ -523,6 +578,29 @@ func (suite *TypesTestSuite) TestMarshalMsgSubmitMisbehaviour() {
 			suite.Require().True(proto.Equal(msg, newMsg))
 		})
 	}
+}
+
+func (suite *TypesTestSuite) TestMsgSubmitMisbehaviourGetSignBytes() {
+	soloMachine := ibctesting.NewSolomachine(suite.T(), suite.chainA.Codec, "solomachine", "", 2)
+	msg, err := types.NewMsgSubmitMisbehaviour(soloMachine.ClientID, soloMachine.CreateMisbehaviour(), suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res := msg.GetSignBytes()
+
+	expected := fmt.Sprintf(`{"type":"cosmos-sdk/MsgSubmitMisbehaviour","value":{"client_id":"solomachine","misbehaviour":{"client_id":"solomachine","sequence":"1","signature_one":{"data":"CikvaWJjL2NsaWVudHMlMkZjb3VudGVycGFydHklMkZjbGllbnRTdGF0ZRL7AQosL2liYy5saWdodGNsaWVudHMuc29sb21hY2hpbmUudjEuQ2xpZW50U3RhdGUSygEIARrFAQrAAQopL2Nvc21vcy5jcnlwdG8ubXVsdGlzaWcuTGVnYWN5QW1pbm9QdWJLZXkSkgEIAhJGCh8vY29zbW9zLmNyeXB0by5zZWNwMjU2azEuUHViS2V5EiMKIQPIWp26D7PQ7IftuP64ni0UcdoNxhMbm0NVleSTQzeW+xJGCh8vY29zbW9zLmNyeXB0by5zZWNwMjU2azEuUHViS2V5EiMKIQNK6n93W1oFBLaqwl/NScKvVcbDDuz/C/PYxOwMLgT/KhgK","data_type":1,"signature":"EpMBCgUIAhIBwBJECkISQNPIQU2h3tY5TfqbuUrx2zbkvj2DJG9NGUdt2UYRKEEWfZExGncI/za3EcFDAIoFG6I2u3Et/GKDvO1zmdMdwOUSRApCEkA87Vm/QeVJWY0DHvU+64uJLJQ76lGv8Xb1O7pCmOg1oEITeqrhjbIhZ5qviupoOUOxKQzdANfOBgxdEXWswsfR","timestamp":"10"},"signature_two":{"data":"CjMvaWJjL2NsaWVudHMlMkZjb3VudGVycGFydHklMkZjb25zZW5zdXNTdGF0ZXMlMkYwLTES+QEKLy9pYmMubGlnaHRjbGllbnRzLnNvbG9tYWNoaW5lLnYxLkNvbnNlbnN1c1N0YXRlEsUBCsABCikvY29zbW9zLmNyeXB0by5tdWx0aXNpZy5MZWdhY3lBbWlub1B1YktleRKSAQgCEkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohA8hanboPs9Dsh+24/rieLRRx2g3GExubQ1WV5JNDN5b7EkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohA0rqf3dbWgUEtqrCX81Jwq9VxsMO7P8L89jE7AwuBP8qGAo=","data_type":2,"signature":"EpMBCgUIAhIBwBJECkISQPuhj97kVBX79lp6a+iuq4MQu1hZiY8eAWfVWYtT0qyka+2+75GWsknhsC2ehdx+cTYhmGD701e4QS6Qcy3BJtwSRApCEkDuzVqNeRtnqKza3v5ciCLeACPSnz8W5UWKDzjbRuLDCXHN5rod8dE7hxBYWD5FT+w/9URs2EAfMdJ4pm4parS4","timestamp":"11"}},"signer":"%s"}}`, suite.chainA.SenderAccount.GetAddress())
+	// suite.Require().Equal(expected, string(res))
+
+	height := types.NewHeight(0, uint64(suite.chainA.CurrentHeader.Height))
+	heightMinus1 := types.NewHeight(0, uint64(suite.chainA.CurrentHeader.Height)-1)
+	header1 := suite.chainA.CreateTMClientHeader(suite.chainA.ChainID, int64(height.RevisionHeight), heightMinus1, suite.chainA.CurrentHeader.Time, suite.chainA.Vals, suite.chainA.Vals, suite.chainA.Signers)
+	header2 := suite.chainA.CreateTMClientHeader(suite.chainA.ChainID, int64(height.RevisionHeight), heightMinus1, suite.chainA.CurrentHeader.Time.Add(time.Minute), suite.chainA.Vals, suite.chainA.Vals, suite.chainA.Signers)
+
+	misbehaviour := ibctmtypes.NewMisbehaviour("tendermint", header1, header2)
+	msg, err = types.NewMsgSubmitMisbehaviour("tendermint", misbehaviour, suite.chainA.SenderAccount.GetAddress())
+	suite.Require().NoError(err)
+	res = msg.GetSignBytes()
+
+	expected = fmt.Sprintf(`{"type":"cosmos-sdk/MsgSubmitMisbehaviour","value":{"client_id":"clientid","client_state":{"chain_id":"testchain0","frozen_height":{},"latest_height":{"revision_height":"10"},"max_clock_drift":"10000000000","proof_specs":[{"inner_spec":{"child_order":[0,1],"child_size":33,"hash":1,"max_prefix_length":12,"min_prefix_length":4},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}},{"inner_spec":{"child_order":[0,1],"child_size":32,"hash":1,"max_prefix_length":1,"min_prefix_length":1},"leaf_spec":{"hash":1,"length":1,"prefix":"AA==","prehash_value":1}}],"trust_level":{"denominator":"3","numerator":"1"},"trusting_period":"1209600000000000","unbonding_period":"1814400000000000","upgrade_path":["upgrade","upgradedIBCState"]},"consensus_state":{"next_validators_hash":"6E65787456616C7348617368","root":{},"timestamp":"0001-01-01T00:00:00Z"},"proof_upgrade_client":"cHJvb2ZVcGdyYWRlQ2xpZW50","proof_upgrade_consensus_state":"cHJvb2ZVcGdyYWRlQ29uc1N0YXRl","signer":"%s"}}`, suite.chainA.SenderAccount.GetAddress())
+	suite.Require().Equal(expected, string(res))
 }
 
 func (suite *TypesTestSuite) TestMsgSubmitMisbehaviour_ValidateBasic() {

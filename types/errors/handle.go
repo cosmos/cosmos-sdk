90a0a397f303b3473e02f4bdbd42e1cc9b42e10f
Robert Zaremba <robert@zaremba.ch>
2021-01-27 13:20:43 +0100

    fix errors/handle

--- types/errors/handle.go
+++ types/errors/handle.go
@@ -5,8 +5,8 @@ import "fmt"
 // Panic panics on error
 // Should be only used with interface methods, which require return error, but the
 // error is always nil
-func Panic(err) {
+func Panic(err error) {
 	if err != nil {
-		panic(fmt.Errorf("Logic error - this should never happen. %w", err))
+		panic(fmt.Errorf("logic error - this should never happen. %w", err))
 	}
 }

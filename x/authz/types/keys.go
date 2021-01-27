e4c222bdbbe869553616e468e3a493adbd8aa17d
Amaury Martiny <amaury.martiny@protonmail.com>
2021-01-27 12:01:36 +0100

    Update comment

--- x/authz/types/keys.go
+++ x/authz/types/keys.go
@@ -40,7 +40,7 @@ func GetAuthorizationStoreKey(grantee sdk.AccAddress, granter sdk.AccAddress, ms
 // ExtractAddressesFromGrantKey - split granter & grantee address from the authorization key
 func ExtractAddressesFromGrantKey(key []byte) (granterAddr, granteeAddr sdk.AccAddress) {
 	// key if of format:
-	// 0x01<granterAddressLen (1 Byte)><granterAddress_Bytes><granteeAddressLen (1 Byte)><granteeAddress_Bytes><msgType_Bytes>: Grant
+	// 0x01<granterAddressLen (1 Byte)><granterAddress_Bytes><granteeAddressLen (1 Byte)><granteeAddress_Bytes><msgType_Bytes>
 	granterAddrLen := key[1] // remove prefix key
 	granterAddr = sdk.AccAddress(key[2 : 2+granterAddrLen])
 	granteeAddrLen := int(key[2+granterAddrLen])

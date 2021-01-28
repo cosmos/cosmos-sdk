e1ba81c1e0a274dd7ea30def291bda505868f2e1
Amaury Martiny <amaury.martiny@protonmail.com>
2021-01-28 12:37:32 +0100

    Fix test build

--- types/address_test.go
+++ types/address_test.go
@@ -528,32 +528,3 @@ func (s *addressTestSuite) TestGetFromBech32() {
 	s.Require().Error(err)
 	s.Require().Equal("invalid Bech32 prefix; expected x, got cosmos", err.Error())
 }
-
-func (s *addressTestSuite) TestLengthPrefixedAddressStoreKey() {
-	addr10byte := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	addr20byte := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-	addr256byte := make([]byte, 256)
-
-	tests := []struct {
-		name        string
-		addr        []byte
-		expStoreKey []byte
-		expErr      bool
-	}{
-		{"10-byte address", addr10byte, append([]byte{byte(10)}, addr10byte...), false},
-		{"20-byte address", addr20byte, append([]byte{byte(20)}, addr20byte...), false},
-		{"256-byte address (too long)", addr256byte, nil, true},
-	}
-	for _, tt := range tests {
-		tt := tt
-		s.T().Run(tt.name, func(t *testing.T) {
-			storeKey, err := types.LengthPrefixedAddressStoreKey(tt.addr)
-			if tt.expErr {
-				s.Require().Error(err)
-			} else {
-				s.Require().NoError(err)
-				s.Require().Equal(tt.expStoreKey, storeKey)
-			}
-		})
-	}
-}

f7ecbcb09430518e1779d0913bf0fa155337b47a
Ian Norden <iannordenn@gmail.com>
2021-02-08 17:01:43 -0600

    hook and streaming service interfaces

---
+++ types/streaming.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	"sync"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/cosmos/cosmos-sdk/store/types"
+)
+
+// Hook interface used to hook into the ABCI message processing of the BaseApp
+type Hook interface {
+	// update the streaming service with the latest BeginBlock messages
+	ListenBeginBlock(ctx Context, req abci.RequestBeginBlock, res abci.ResponseBeginBlock)
+	// update the steaming service with the latest EndBlock messages
+	ListenEndBlock(ctx Context, req abci.RequestEndBlock, res abci.ResponseEndBlock)
+	// update the steaming service with the latest DeliverTx messages
+	ListenDeliverTx(ctx Context, req abci.RequestDeliverTx, res abci.ResponseDeliverTx)
+}
+
+// StreamingService interface for registering WriteListeners with the BaseApp and updating the service with the ABCI messages using the hooks
+type StreamingService interface {
+	// streaming service loop, awaits kv pairs and writes them to some destination stream or file
+	Stream(wg *sync.WaitGroup, quitChan <-chan struct{})
+	// returns the streaming service's listeners for the BaseApp to register
+	Listeners() map[StoreKey][]types.WriteListener
+	// interface for hooking into the ABCI messages from inside the BaseApp
+	Hook
+}

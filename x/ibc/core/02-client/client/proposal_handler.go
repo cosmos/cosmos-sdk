7b281f46fddeeb70081b88c719ce88763d3e5cc4
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2021-02-26 17:04:24 +0100

    construct empty rest handler

--- x/ibc/core/02-client/client/proposal_handler.go
+++ x/ibc/core/02-client/client/proposal_handler.go
@@ -1,11 +1,17 @@
 package client
 
 import (
+	"github.com/cosmos/cosmos-sdk/client"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
+	"github.com/cosmos/cosmos-sdk/x/gov/client/rest"
 	"github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/client/cli"
 )
 
 var (
-	UpdateClientProposalHandler = govclient.NewProposalHandler(cli.NewCmdSubmitUpdateClientProposal, nil)
-	UpgradeProposalHandler      = govclient.NewProposalHandler(cli.NewCmdSubmitUpgradeProposal, nil)
+	UpdateClientProposalHandler = govclient.NewProposalHandler(cli.NewCmdSubmitUpdateClientProposal, emptyRestHandler)
+	UpgradeProposalHandler      = govclient.NewProposalHandler(cli.NewCmdSubmitUpgradeProposal, emptyRestHandler)
 )
+
+func emptyRestHandler(client.Context) rest.ProposalRESTHandler {
+	return rest.ProposalRESTHandler{}
+}

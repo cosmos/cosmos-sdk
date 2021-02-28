c9d5cb3995e17fe43f6cba44a9dbaa7234f84e9f
Cuong Manh Le <cuong@orijtech.com>
2021-02-28 10:21:16 +0700

    Testing bencher

--- types/abci.go
+++ types/abci.go
@@ -19,3 +19,5 @@ type EndBlocker func(ctx Context, req abci.RequestEndBlock) abci.ResponseEndBloc
 
 // PeerFilter responds to p2p filtering queries from Tendermint
 type PeerFilter func(info string) abci.ResponseQuery
+
+type dummy struct{}

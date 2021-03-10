b75009f52ae1b1aa3417757c70d367dc92e78e18
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-03-10 15:15:01 +0100

    Add comment

--- x/auth/legacy/v036/types.go
+++ x/auth/legacy/v036/types.go
@@ -1,9 +1,7 @@
 // DONTCOVER
 package v036
 
-import (
-	v034auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v034"
-)
+import v034auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v034"
 
 const (
 	ModuleName = "auth"

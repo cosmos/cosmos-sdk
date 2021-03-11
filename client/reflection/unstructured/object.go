2fc08cab79a4b1efcfaf9f00c4da3aa677c903fe
Frojdi Dymylja <frojdi.dymylja@gmail.com>
2021-03-12 00:25:20 +0100

    add: maximum recursion support

--- client/reflection/unstructured/object.go
+++ client/reflection/unstructured/object.go
@@ -27,38 +27,19 @@ func (o Map) Marshal(md protoreflect.MessageDescriptor) (*dynamicpb.Message, err
 		if fd == nil {
 			return nil, fmt.Errorf("descriptor %s does not contain field named %s", md.FullName(), fieldName)
 		}
-		switch {
-		case fd.IsList():
-			value := dyn.NewField(fd)
-			listValue := value.List()
-			err := fillList(interfaceValue, listValue, fd, md)
-			if err != nil {
-				return nil, err
-			}
-			dyn.Set(fd, value)
-		case fd.IsMap():
-			rawField := dyn.NewField(fd)
-			pMap := rawField.Map()
-			err := fillMap(interfaceValue, pMap, fd, md)
-			if err != nil {
-				return nil, err
-			}
-			dyn.Set(fd, rawField)
-		default:
-			pv, err := interfaceToProtoValue(interfaceValue, fd, md)
-			if err != nil {
-				return nil, err
-			}
-			// set the field
-			dyn.Set(fd, pv)
+		pv, err := interfaceToProtoValue(dyn, interfaceValue, fd, md)
+		if err != nil {
+			return nil, err
 		}
+		// set the field
+		dyn.Set(fd, pv)
 	}
 
 	return dyn, nil
 }
 
 // TODO: indirect
-func fillMap(v interface{}, pMap protoreflect.Map, fd protoreflect.FieldDescriptor, md protoreflect.MessageDescriptor) error {
+func fillMap(dyn *dynamicpb.Message, v interface{}, pMap protoreflect.Map, fd protoreflect.FieldDescriptor, md protoreflect.MessageDescriptor) error {
 	rv := reflect.ValueOf(v)
 	if rv.Kind() != reflect.Map {
 		return errTypeMismatch(md, fd, v)
@@ -74,11 +55,11 @@ func fillMap(v interface{}, pMap protoreflect.Map, fd protoreflect.FieldDescript
 		v := mapIter.Value().Interface()
 
 		// cast k and v to kDesc and vDesc
-		kValue, err := interfaceToProtoValue(k, keyDesc, md)
+		kValue, err := interfaceToProtoValue(dyn, k, keyDesc, md)
 		if err != nil {
 			return fmt.Errorf("unable to set map key for map field descriptor %s: %w", fd, err)
 		}
-		vValue, err := interfaceToProtoValue(v, valueDesc, md)
+		vValue, err := interfaceToProtoValue(dyn, v, valueDesc, md)
 		if err != nil {
 			return fmt.Errorf("unable to set map value for map field descriptor %s: %w", fd, err)
 		}
@@ -90,14 +71,14 @@ func fillMap(v interface{}, pMap protoreflect.Map, fd protoreflect.FieldDescript
 }
 
 // TODO: indirect
-func fillList(v interface{}, list protoreflect.List, fd protoreflect.FieldDescriptor, md protoreflect.MessageDescriptor) error {
+func fillList(dyn *dynamicpb.Message, v interface{}, list protoreflect.List, fd protoreflect.FieldDescriptor, md protoreflect.MessageDescriptor) error {
 	rv := reflect.ValueOf(v)
 	if rv.Kind() != reflect.Slice {
 		return errTypeMismatch(md, fd, v)
 	}
 	for i := 0; i < rv.Len(); i++ {
 		iv := rv.Index(i).Interface()
-		v, err := interfaceToProtoValue(iv, fd, md)
+		v, err := interfaceToProtoValue(dyn, iv, fd, md)
 		if err != nil {
 			return err
 		}
@@ -106,91 +87,110 @@ func fillList(v interface{}, list protoreflect.List, fd protoreflect.FieldDescri
 	return nil
 }
 
-func interfaceToProtoValue(interfaceValue interface{}, fieldDesc protoreflect.FieldDescriptor, desc protoreflect.MessageDescriptor) (protoreflect.Value, error) {
-	switch fieldDesc.Kind() {
-	// bool
-	case protoreflect.BoolKind:
-		v, err := cast.ToBoolE(interfaceValue)
+func interfaceToProtoValue(dyn *dynamicpb.Message, interfaceValue interface{}, fd protoreflect.FieldDescriptor, md protoreflect.MessageDescriptor) (protoreflect.Value, error) {
+	// handle list or map
+	if fd.IsList() {
+		value := dyn.NewField(fd)
+		listValue := value.List()
+		err := fillList(dyn, interfaceValue, listValue, fd, md)
 		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
+			return protoreflect.Value{}, err
 		}
-		return protoreflect.ValueOfBool(v), nil
-	// enum
-	case protoreflect.EnumKind:
-		v, err := cast.ToInt32E(interfaceValue)
+		return value, nil
+	} else if fd.IsMap() {
+		value := dyn.NewField(fd)
+		mapValue := value.Map()
+		err := fillMap(dyn, interfaceValue, mapValue, fd, md)
 		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
+			return protoreflect.Value{}, err
 		}
-		return protoreflect.ValueOfEnum((protoreflect.EnumNumber)(v)), nil
-	// int32
-	case protoreflect.Int32Kind:
-		v, err := cast.ToInt32E(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
+		return value, nil
+	} else { // handle normal
+		switch fd.Kind() {
+		// bool
+		case protoreflect.BoolKind:
+			v, err := cast.ToBoolE(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfBool(v), nil
+		// enum
+		case protoreflect.EnumKind:
+			v, err := cast.ToInt32E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfEnum((protoreflect.EnumNumber)(v)), nil
+		// int32
+		case protoreflect.Int32Kind:
+			v, err := cast.ToInt32E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfInt32(v), nil
+		// uint32
+		case protoreflect.Uint32Kind:
+			v, err := cast.ToUint32E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfUint32(v), nil
+		// int64
+		case protoreflect.Int64Kind:
+			v, err := cast.ToInt64E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfInt64(v), nil
+		// uint64
+		case protoreflect.Uint64Kind:
+			v, err := cast.ToUint64E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfUint64(v), nil
+		// float
+		case protoreflect.FloatKind:
+			v, err := cast.ToFloat32E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfFloat32(v), nil
+		// handle double
+		case protoreflect.DoubleKind:
+			v, err := cast.ToFloat64E(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfFloat64(v), nil
+		// string
+		case protoreflect.StringKind:
+			v, err := cast.ToStringE(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfString(v), nil
+		// bytes
+		case protoreflect.BytesKind:
+			v, err := castToBytes(interfaceValue)
+			if err != nil {
+				return protoreflect.Value{}, errTypeMismatch(md, fd, interfaceValue)
+			}
+			return protoreflect.ValueOfBytes(v), nil
+		// handle message kind
+		case protoreflect.MessageKind:
+			ob, ok := interfaceValue.(Object)
+			if !ok {
+				return protoreflect.Value{}, fmt.Errorf("descriptor %s expected %s kind at %s which should be castable to unstructured.Object, got: %T", md, protoreflect.MessageKind, fd, interfaceValue)
+			}
+			dpb, err := ob.Marshal(fd.Message())
+			if err != nil {
+				return protoreflect.Value{}, fmt.Errorf("descriptor %s failed to marshal Message for field descriptor %s: %w", md, fd, err)
+			}
+			return protoreflect.ValueOfMessage(dpb), nil
+		default:
+			return protoreflect.Value{}, fmt.Errorf("descriptor %s field %s unsupported type: %s", md.FullName(), fd.FullName(), fd.Kind().String())
 		}
-		return protoreflect.ValueOfInt32(v), nil
-	// uint32
-	case protoreflect.Uint32Kind:
-		v, err := cast.ToUint32E(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfUint32(v), nil
-	// int64
-	case protoreflect.Int64Kind:
-		v, err := cast.ToInt64E(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfInt64(v), nil
-	// uint64
-	case protoreflect.Uint64Kind:
-		v, err := cast.ToUint64E(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfUint64(v), nil
-	// float
-	case protoreflect.FloatKind:
-		v, err := cast.ToFloat32E(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfFloat32(v), nil
-	// handle double
-	case protoreflect.DoubleKind:
-		v, err := cast.ToFloat64E(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfFloat64(v), nil
-	// string
-	case protoreflect.StringKind:
-		v, err := cast.ToStringE(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfString(v), nil
-	// bytes
-	case protoreflect.BytesKind:
-		v, err := castToBytes(interfaceValue)
-		if err != nil {
-			return protoreflect.Value{}, errTypeMismatch(desc, fieldDesc, interfaceValue)
-		}
-		return protoreflect.ValueOfBytes(v), nil
-	// handle message kind
-	case protoreflect.MessageKind:
-		ob, ok := interfaceValue.(Object)
-		if !ok {
-			return protoreflect.Value{}, fmt.Errorf("descriptor %s expected %s kind at %s which should be castable to unstructured.Object, got: %T", desc, protoreflect.MessageKind, fieldDesc, interfaceValue)
-		}
-		dpb, err := ob.Marshal(fieldDesc.Message())
-		if err != nil {
-			return protoreflect.Value{}, fmt.Errorf("descriptor %s failed to marshal Message for field descriptor %s: %w", desc, fieldDesc, err)
-		}
-		return protoreflect.ValueOfMessage(dpb), nil
-	default:
-		return protoreflect.Value{}, fmt.Errorf("descriptor %s field %s unsupported type: %s", desc.FullName(), fieldDesc.FullName(), fieldDesc.Kind().String())
 	}
 }
 

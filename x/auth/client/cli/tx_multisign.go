623b4625f93767f03cb82f5878266ed9558a1e16
Alessio Treglia <alessio@tendermint.com>
2021-03-15 12:18:46 +0000

    add --output-document to multisign-batch (#8873)
    Closes: #8870
    
    Co-authored-by: SaReN <sahithnarahari@gmail.com>
    (cherry picked from commit 5f71e93b1e0f9a7d9617f5ea88f5c18b73361a7b)

--- x/auth/client/cli/tx_multisign.go
+++ x/auth/client/cli/tx_multisign.go
@@ -215,6 +215,7 @@ The SIGN_MODE_DIRECT sign mode is not supported.'
 		flagMultisig, "",
 		"Address of the multisig account that the transaction signs on behalf of",
 	)
+	cmd.Flags().String(flags.FlagOutputDocument, "", "The document is written to the given file instead of STDOUT")
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
@@ -274,6 +275,15 @@ func makeBatchMultisignCmd() func(cmd *cobra.Command, args []string) error {
 
 			txFactory = txFactory.WithAccountNumber(accnum).WithSequence(seq)
 		}
+
+		// prepare output document
+		closeFunc, err := setOutputFile(cmd)
+		if err != nil {
+			return err
+		}
+
+		defer closeFunc()
+		clientCtx.WithOutput(cmd.OutOrStdout())
 
 		for i := 0; scanner.Scan(); i++ {
 			txBldr, err := txCfg.WrapTxBuilder(scanner.Tx())
@@ -348,7 +358,7 @@ func makeBatchMultisignCmd() func(cmd *cobra.Command, args []string) error {
 			txFactory = txFactory.WithSequence(sequence)
 		}
 
-		return nil
+		return scanner.UnmarshalErr()
 	}
 }
 

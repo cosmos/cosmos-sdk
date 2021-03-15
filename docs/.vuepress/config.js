eb09ee10c484a8951ab1ab321c56f7d815b64718
Cyrus Goh <hello@lovincyrus.com>
2021-03-15 16:32:07 -0700

    docs: update dropdown v0.41 → v0.42 (#8888)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,8 +43,8 @@ module.exports = {
         "key": "v0.39"
       },
       {
-        "label": "v0.41",
-        "key": "v0.41"
+        "label": "v0.42",
+        "key": "v0.42"
       },
       {
         "label": "master",

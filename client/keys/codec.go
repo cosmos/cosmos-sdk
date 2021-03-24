b0b8f59e07d02cda98f00c7f4f40268f9794f586
Robert Zaremba <robert@zaremba.ch>
2021-03-24 15:19:23 +0100

    add link to github issue

--- client/keys/codec.go
+++ client/keys/codec.go
@@ -5,7 +5,7 @@ import (
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 )
 
-// TODO: remove this file #8047
+// TODO: remove this file https://github.com/cosmos/cosmos-sdk/issues/8047
 
 // KeysCdc defines codec to be used with key operations
 var KeysCdc *codec.LegacyAmino

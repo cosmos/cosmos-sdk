e81ee9a1611c63a0fa4e995e29e6320f0f841ea9
aleem1314 <aleem@vitwit.com>
2021-04-15 17:47:54 +0530

    fix tests

--- x/authz/simulation/operations_test.go
+++ x/authz/simulation/operations_test.go
@@ -120,7 +120,7 @@ func (suite *SimTestSuite) TestSimulateGrantAuthorization() {
 
 func (suite *SimTestSuite) TestSimulateRevokeAuthorization() {
 	// setup 3 accounts
-	s := rand.NewSource(1)
+	s := rand.NewSource(2)
 	r := rand.New(s)
 	accounts := suite.getTestingAccounts(r, 3)
 
@@ -159,7 +159,7 @@ func (suite *SimTestSuite) TestSimulateRevokeAuthorization() {
 
 func (suite *SimTestSuite) TestSimulateExecAuthorization() {
 	// setup 3 accounts
-	s := rand.NewSource(1)
+	s := rand.NewSource(2)
 	r := rand.New(s)
 	accounts := suite.getTestingAccounts(r, 3)
 

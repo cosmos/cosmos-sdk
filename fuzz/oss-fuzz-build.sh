eae2409c9adea33eb0547866241a240576c93ea9
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-04-20 13:51:31 +0700

    fuzz: make all fuzz packages works with oss-fuzz (#9143)

--- fuzz/oss-fuzz-build.sh
+++ fuzz/oss-fuzz-build.sh
@@ -2,4 +2,14 @@
 
 export FUZZ_ROOT="github.com/cosmos/cosmos-sdk"
 
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/crypto/hd/deriveprivatekeyforpath Fuzz fuzz_crypto_hd_deriveprivatekeyforpath fuzz
+
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/parsecoin Fuzz fuzz_types_parsecoin fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/parsedeccoin Fuzz fuzz_types_parsedeccoin fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/parsetimebytes Fuzz fuzz_types_parsetimebytes fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/verifyaddressformat Fuzz fuzz_types_verifyaddressformat fuzz
 compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/setstring Fuzz fuzz_types_dec_setstring fuzz
+
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/unknownproto Fuzz fuzz_unknownproto fuzz
+
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/x/bank/types/addressfrombalancesstore Fuzz fuzz_x_bank_types_addressfrombalancesstore fuzz

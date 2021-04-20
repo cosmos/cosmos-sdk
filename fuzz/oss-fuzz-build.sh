c573fb8574552d8b7a4668a2071b775e525830fd
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-04-20 23:25:25 +0700

    fuzz: fix wrong path for fuzz/types packages (#9147)

--- fuzz/oss-fuzz-build.sh
+++ fuzz/oss-fuzz-build.sh
@@ -4,10 +4,10 @@ export FUZZ_ROOT="github.com/cosmos/cosmos-sdk"
 
 compile_go_fuzzer "$FUZZ_ROOT"/fuzz/crypto/hd/deriveprivatekeyforpath Fuzz fuzz_crypto_hd_deriveprivatekeyforpath fuzz
 
-compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/parsecoin Fuzz fuzz_types_parsecoin fuzz
-compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/parsedeccoin Fuzz fuzz_types_parsedeccoin fuzz
-compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/parsetimebytes Fuzz fuzz_types_parsetimebytes fuzz
-compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/verifyaddressformat Fuzz fuzz_types_verifyaddressformat fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/parsecoin Fuzz fuzz_types_parsecoin fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/parsedeccoin Fuzz fuzz_types_parsedeccoin fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/parsetimebytes Fuzz fuzz_types_parsetimebytes fuzz
+compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/verifyaddressformat Fuzz fuzz_types_verifyaddressformat fuzz
 compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/setstring Fuzz fuzz_types_dec_setstring fuzz
 
 compile_go_fuzzer "$FUZZ_ROOT"/fuzz/unknownproto Fuzz fuzz_unknownproto fuzz

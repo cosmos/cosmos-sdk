5cb430147f2477f1fd65e8de95cc3bd0b643d1b4
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-04-21 13:01:53 +0200

    Fix codec registration

--- x/feegrant/types/codec.go
+++ x/feegrant/types/codec.go
@@ -2,11 +2,17 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 // RegisterInterfaces registers the interfaces types with the interface registry
 func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgGrantFeeAllowance{},
+		&MsgRevokeFeeAllowance{},
+	)
+
 	registry.RegisterInterface(
 		"cosmos.feegrant.v1beta1.FeeAllowanceI",
 		(*FeeAllowanceI)(nil),

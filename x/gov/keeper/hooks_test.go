7e77ac11badaefe033bf8301744291508b003df0
Sunny Aggarwal <sunnya97@protonmail.ch>
2021-04-20 20:15:05 -0400

    add more tests

--- x/gov/keeper/hooks_test.go
+++ x/gov/keeper/hooks_test.go
@@ -2,12 +2,14 @@ package keeper_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/gov"
 	"github.com/cosmos/cosmos-sdk/x/gov/keeper"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
@@ -41,6 +43,10 @@ func (h *MockGovHooksReceiver) AfterProposalActive(ctx sdk.Context, proposalID u
 
 func TestHooks(t *testing.T) {
 	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	minDeposit := app.GovKeeper.GetDepositParams(ctx).MinDeposit
+	addrs := simapp.AddTestAddrs(app, ctx, 1, minDeposit[0].Amount)
 
 	govHooksReceiver := MockGovHooksReceiver{}
 
@@ -50,11 +56,39 @@ func TestHooks(t *testing.T) {
 		),
 	)
 
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+	require.False(t, govHooksReceiver.AfterProposalSubmissionValid)
+	require.False(t, govHooksReceiver.AfterProposalDepositValid)
+	require.False(t, govHooksReceiver.AfterProposalVoteValid)
+	require.False(t, govHooksReceiver.AfterProposalInactiveValid)
+	require.False(t, govHooksReceiver.AfterProposalActiveValid)
 
 	tp := TestProposal
 	_, err := app.GovKeeper.SubmitProposal(ctx, tp)
 	require.NoError(t, err)
-
 	require.True(t, govHooksReceiver.AfterProposalSubmissionValid)
+
+	newHeader := ctx.BlockHeader()
+	newHeader.Time = ctx.BlockHeader().Time.Add(app.GovKeeper.GetDepositParams(ctx).MaxDepositPeriod).Add(time.Duration(1) * time.Second)
+	ctx = ctx.WithBlockHeader(newHeader)
+	gov.EndBlocker(ctx, app.GovKeeper)
+
+	require.True(t, govHooksReceiver.AfterProposalInactiveValid)
+
+	p2, err := app.GovKeeper.SubmitProposal(ctx, tp)
+	require.NoError(t, err)
+
+	activated, err := app.GovKeeper.AddDeposit(ctx, p2.ProposalId, addrs[0], minDeposit)
+	require.True(t, activated)
+	require.NoError(t, err)
+	require.True(t, govHooksReceiver.AfterProposalDepositValid)
+
+	err = app.GovKeeper.AddVote(ctx, p2.ProposalId, addrs[0], types.NewNonSplitVoteOption(types.OptionYes))
+	require.NoError(t, err)
+	require.True(t, govHooksReceiver.AfterProposalVoteValid)
+
+	newHeader = ctx.BlockHeader()
+	newHeader.Time = ctx.BlockHeader().Time.Add(app.GovKeeper.GetVotingParams(ctx).VotingPeriod).Add(time.Duration(1) * time.Second)
+	ctx = ctx.WithBlockHeader(newHeader)
+	gov.EndBlocker(ctx, app.GovKeeper)
+	require.True(t, govHooksReceiver.AfterProposalActiveValid)
 }

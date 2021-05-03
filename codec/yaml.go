a8fca99bb8c34a59033d0d7a0fa870e96e860b4d
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-05-03 16:15:42 +0200

    Fix lint

--- codec/yaml.go
+++ codec/yaml.go
@@ -10,10 +10,10 @@ import (
 // MarshalYAML marshals toPrint using JSONCodec to leverage specialized MarshalJSON methods
 // (usually related to serialize data with protobuf or amin depending on a configuration).
 // This involves additional roundtrip through JSON.
-func MarshalYAML(JSONCodec JSONCodec, toPrint proto.Message) ([]byte, error) {
+func MarshalYAML(cdc JSONCodec, toPrint proto.Message) ([]byte, error) {
 	// We are OK with the performance hit of the additional JSON roundtip. MarshalYAML is not
 	// used in any critical parts of the system.
-	bz, err := JSONCodec.MarshalJSON(toPrint)
+	bz, err := cdc.MarshalJSON(toPrint)
 	if err != nil {
 		return nil, err
 	}

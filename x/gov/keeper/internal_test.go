c990606ac1b39e4e65d67a8f52f6033e8faa122d
Amaury <1293565+amaurym@users.noreply.github.com>
2021-05-06 16:33:30 +0200

    Update x/gov/keeper/internal_test.go

--- x/gov/keeper/internal_test.go
+++ x/gov/keeper/internal_test.go
@@ -2,7 +2,7 @@ package keeper
 
 import "github.com/cosmos/cosmos-sdk/x/gov/types"
 
-// UnsafeSetHooks updates the gov keepers hooks, overriding any potential
+// UnsafeSetHooks updates the gov keeper's hooks, overriding any potential
 // pre-existing hooks.
 // WARNING: this function should only be used in tests.
 func UnsafeSetHooks(k *Keeper, h types.GovHooks) {

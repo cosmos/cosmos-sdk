a669d27907dc30ac8f3bff9836f435575a4dc402
Levi Cook <levicook@gmail.com>
2021-05-06 15:48:20 -0600

    poolx: rm REST plumbing

--- x/poolx/client/rest/rest.go
+++ x/poolx/client/rest/rest.go
@@ -17,38 +17,10 @@ const (
 
 // RegisterRoutes registers poolx-related REST handlers to a router
 func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
-	r = clientrest.WithHTTPDeprecationHeaders(r)
-
-	registerQueryRoutes(clientCtx, r)
-	registerTxHandlers(clientCtx, r)
 }
 
 func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
-	// Get the current poolx parameter values
-	r.HandleFunc(
-		"/poolx/parameters",
-		paramsHandlerFn(clientCtx),
-	).Methods("GET")
 }
 
 func registerTxHandlers(clientCtx client.Context, r *mux.Router) {
-}
-
-// HTTP request handler to query the distribution params values
-func paramsHandlerFn(clientCtx client.Context) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		clientCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, clientCtx, r)
-		if !ok {
-			return
-		}
-
-		route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QueryParams)
-		res, height, err := clientCtx.QueryWithData(route, nil)
-		if rest.CheckInternalServerError(w, err) {
-			return
-		}
-
-		clientCtx = clientCtx.WithHeight(height)
-		rest.PostProcessResponse(w, clientCtx, res)
-	}
 }

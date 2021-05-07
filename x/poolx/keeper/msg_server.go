777656d760d92703e359cf683a23ccae9212446b
Levi Cook <levicook@gmail.com>
2021-05-07 10:50:21 -0600

    poolx: msg handler scaffolding ++

--- x/poolx/keeper/msg_server.go
+++ x/poolx/keeper/msg_server.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/poolx/types"
 )
 
@@ -16,10 +17,36 @@ type msgServer struct {
 	Keeper
 }
 
-func (msgServer) CreatePool(context.Context, *types.MsgCreatePool) (*types.MsgCreatePoolResponse, error) {
-	return nil, nil // TODO(levi) implement
+func (msgServer) CreatePool(c context.Context, _ *types.MsgCreatePool) (*types.MsgCreatePoolResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+	_ = ctx
+
+	// TODO(levi) delegate work to the keeper -- ie really create the pool
+
+	// TODO(levi) emit events (& learn conventions)
+	// ctx.EventManager().EmitEvent(
+	// 	sdk.NewEvent(
+	// 		sdk.EventTypeMessage,
+	// 		// ???
+	// 	),
+	// )
+
+	return &types.MsgCreatePoolResponse{}, nil
 }
 
-func (msgServer) FundPool(context.Context, *types.MsgFundPool) (*types.MsgFundPoolResponse, error) {
-	return nil, nil // TODO(levi) implement
+func (msgServer) FundPool(c context.Context, _ *types.MsgFundPool) (*types.MsgFundPoolResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+	_ = ctx
+
+	// TODO(levi) delegate work to the keeper -- ie really fund the pool
+
+	// TODO(levi) emit events (& learn conventions)
+	// ctx.EventManager().EmitEvent(
+	// 	sdk.NewEvent(
+	// 		sdk.EventTypeMessage,
+	// 		// ???
+	// 	),
+	// )
+
+	return &types.MsgFundPoolResponse{}, nil
 }

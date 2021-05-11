f6aaa6be25269873f27aedb6335fd17b4542db2b
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-05-11 11:03:55 +0200

    Add comment

--- x/auth/legacy/v038/types.go
+++ x/auth/legacy/v038/types.go
@@ -1,3 +1,6 @@
+// Package v038 is used for legacy migration scripts. Actual migration scripts
+// for v038 have been removed, but the v039->v042 migration script still
+// references types from this file, so we're keeping it for now.
 package v038
 
 // DONTCOVER

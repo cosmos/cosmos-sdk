fea37bec20d212a0a8de38a132ef1be09838965d
Andrea Giacobino <no.andrea@gmail.com>
2021-05-14 17:32:52 +0200

    chore: fix format

--- server/rosetta/lib/errors/errors_test.go
+++ server/rosetta/lib/errors/errors_test.go
@@ -9,26 +9,25 @@ func TestRegisterError(t *testing.T) {
 	var error *Error
 	// this is the number of errors registered by default in errors.go
 	registeredErrorsCount := 16
-	assert.Equal(t, len(registry.list()), registeredErrorsCount )
+	assert.Equal(t, len(registry.list()), registeredErrorsCount)
 	assert.ElementsMatch(t, registry.list(), ListErrors())
 	// add a new Error
 	error = RegisterError(69, "nice!", false, "nice!")
 	assert.NotNil(t, error)
 	// now we have a new error
 	registeredErrorsCount++
-	assert.Equal(t, len(ListErrors()), registeredErrorsCount )
+	assert.Equal(t, len(ListErrors()), registeredErrorsCount)
 	// re-register an error should not change anything
 	error = RegisterError(69, "nice!", false, "nice!")
-	assert.Equal(t, len(ListErrors()), registeredErrorsCount )
+	assert.Equal(t, len(ListErrors()), registeredErrorsCount)
 
 	// test sealing
 	assert.Equal(t, registry.sealed, false)
 	errors := SealAndListErrors()
 	assert.Equal(t, registry.sealed, true)
-	assert.Equal(t, len(errors), registeredErrorsCount )
+	assert.Equal(t, len(errors), registeredErrorsCount)
 	// add a new error on a sealed registry
 	error = RegisterError(1024, "bytes", false, "bytes")
 	assert.NotNil(t, error)
-
 
 }

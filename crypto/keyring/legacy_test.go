205924394297a2d420527e09f43a7d4c55f34872
Andrei Ivasko <cyberbono3@gmail.com>
2021-05-15 13:37:34 +0300

    review legacy_test.go

--- crypto/keyring/legacy_test.go
+++ crypto/keyring/legacy_test.go
@@ -8,23 +8,26 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/simapp"
 )
 
 func TestNewLegacyKeyBase(t *testing.T) {
 	dir := t.TempDir()
+	encCfg := simapp.MakeTestEncodingConfig()
 
-	kb, err := keyring.NewLegacy("keybasename", dir)
+	kb, err := keyring.NewLegacy("keybasename", dir, encCfg.Marshaler)
 	require.NoError(t, err)
 	require.NoError(t, kb.Close())
 }
 
 func TestLegacyKeybase(t *testing.T) {
 	dir := t.TempDir()
+	encCfg := simapp.MakeTestEncodingConfig()
 
 	// Backup testdata
 	require.NoError(t, copy.Copy("testdata", dir))
 
-	kb, err := keyring.NewLegacy("keys", filepath.Join(dir, "keys"))
+	kb, err := keyring.NewLegacy("keys", filepath.Join(dir, "keys"), encCfg.Marshaler)
 	require.NoError(t, err)
 	t.Cleanup(func() { kb.Close() })
 

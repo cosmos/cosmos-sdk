e4cae70a9cc37ab392d97b25c1f085a3e9744dfd
ryanchrypto <12519942+ryanchrypto@users.noreply.github.com>
2021-05-19 14:30:32 -0700

    use chain subdirectories

--- simapp/simd/cmd/testnet.go
+++ simapp/simd/cmd/testnet.go
@@ -206,18 +206,25 @@ func InitTestnetFiles(
 		genFiles    []string
 	)
 
+	baseDir := fmt.Sprintf("%s/%s", outputDir, chainID)
+	if _, err := os.Stat(baseDir); !os.IsNotExist(err) {
+		return fmt.Errorf(
+			"Testnests directory already exists for chain-id '%s': %s, please remove or select a new --chain-id",
+			chainID, baseDir)
+	}
+
 	inBuf := bufio.NewReader(cmd.InOrStdin())
 	// generate private keys, node IDs, and initial transactions
 	for i := 0; i < numValidators; i++ {
 		nodeDirName := fmt.Sprintf("%s%d", nodeDirPrefix, i)
-		nodeDir := filepath.Join(outputDir, nodeDirName, nodeDaemonHome)
-		gentxsDir := filepath.Join(outputDir, "gentxs")
+		nodeDir := filepath.Join(baseDir, nodeDirName, nodeDaemonHome)
+		gentxsDir := filepath.Join(baseDir, "gentxs")
 
 		nodeConfig.SetRoot(nodeDir)
 		nodeConfig.RPC.ListenAddress = "tcp://0.0.0.0:26657"
 
 		if err := os.MkdirAll(filepath.Join(nodeDir, "config"), nodeDirPerm); err != nil {
-			_ = os.RemoveAll(outputDir)
+			_ = os.RemoveAll(baseDir)
 			return err
 		}
 
@@ -225,13 +232,13 @@ func InitTestnetFiles(
 
 		ip, err := getIP(i, startingIPAddress)
 		if err != nil {
-			_ = os.RemoveAll(outputDir)
+			_ = os.RemoveAll(baseDir)
 			return err
 		}
 
 		nodeIDs[i], valPubKeys[i], err = genutil.InitializeNodeValidatorFiles(nodeConfig)
 		if err != nil {
-			_ = os.RemoveAll(outputDir)
+			_ = os.RemoveAll(baseDir)
 			return err
 		}
 
@@ -251,7 +258,7 @@ func InitTestnetFiles(
 
 		addr, secret, err := server.GenerateSaveCoinKey(kb, nodeDirName, true, algo)
 		if err != nil {
-			_ = os.RemoveAll(outputDir)
+			_ = os.RemoveAll(baseDir)
 			return err
 		}
 
@@ -326,7 +333,7 @@ func InitTestnetFiles(
 
 	err := collectGenFiles(
 		clientCtx, nodeConfig, chainID, nodeIDs, valPubKeys, numValidators,
-		outputDir, nodeDirPrefix, nodeDaemonHome, genBalIterator,
+		baseDir, nodeDirPrefix, nodeDaemonHome, genBalIterator,
 	)
 	if err != nil {
 		return err

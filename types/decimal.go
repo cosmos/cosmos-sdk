fdd99a0b2d4ee0b6bff1cedcb76097626a8a239a
Techno Freak <freak12techno@gmail.com>
2021-05-24 01:02:58 +0300

    chore: add godoc

--- types/decimal.go
+++ types/decimal.go
@@ -474,10 +474,14 @@ func (d Dec) String() string {
 	return string(bzStr)
 }
 
+// Returns the float64 representation of a Dec.
+// Will return the error if the conversion failed.
 func (d Dec) Float64() (float64, error) {
 	return strconv.ParseFloat(d.String(), 64)
 }
 
+// Returns the float64 representation of a Dec.
+// Would panic if the conversion failed.
 func (d Dec) MustFloat64() float64 {
 	if value, err := strconv.ParseFloat(d.String(), 64); err != nil {
 		panic(err)

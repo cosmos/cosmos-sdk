777c85ab18117d8876306b982ec5e0b477ddf5d3
Aaron Craelius <aaronc@users.noreply.github.com>
2021-05-26 21:52:45 -0400

    add plugin support

--- core/module/app/plugins.go
+++ core/module/app/plugins.go
@@ -5,9 +5,13 @@ import "google.golang.org/grpc"
 type HasPlugins interface {
 	Module
 
-	RegisterPlugins(registrar grpc.ServiceRegistrar)
+	RegisterPlugins(registrar PluginRegistrar)
 }
 
 type PluginRegistrar interface {
-	RegisterPluginService(desc *grpc.ServiceDesc, impl interface{})
+	RegisterPluginService(paramType interface{}, desc *grpc.ServiceDesc, impl interface{})
+}
+
+func PluginClient(pluginParam interface{}) grpc.ClientConnInterface {
+	panic("TODO")
 }

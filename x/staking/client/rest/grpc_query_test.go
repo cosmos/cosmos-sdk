67762fe8bf0070c06cf33322f2d468cc14ecf811
likhita-809 <likhita@vitwit.com>
2021-05-29 19:37:39 +0530

    remove response code tests for staking delegations

--- x/staking/client/rest/grpc_query_test.go
+++ x/staking/client/rest/grpc_query_test.go
@@ -4,16 +4,12 @@ package rest_test
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/crypto/hd"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -412,35 +408,6 @@ func (s *IntegrationTestSuite) TestQueryUnbondingDelegationGRPC() {
 			}
 		})
 	}
-}
-
-func (s *IntegrationTestSuite) TestQueryDelegationsResponseCode() {
-	val := s.network.Validators[0]
-
-	// Create new account in the keyring.
-	info, _, err := val.ClientCtx.Keyring.NewMnemonic("test", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
-	s.Require().NoError(err)
-	newAddr := sdk.AccAddress(info.GetPubKey().Address())
-
-	s.T().Log("expect 404 error for address without delegations")
-	res, statusCode, err := getRequest(fmt.Sprintf("%s/cosmos/staking/v1beta1/delegations/%s", val.APIAddress, newAddr.String()))
-	s.Require().NoError(err)
-	s.Require().Contains(string(res), "\"code\": 5")
-	s.Require().Equal(404, statusCode)
-}
-
-func getRequest(url string) ([]byte, int, error) {
-	res, err := http.Get(url) // nolint:gosec
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, res.StatusCode, err
-	}
-
-	if err = res.Body.Close(); err != nil {
-		return nil, res.StatusCode, err
-	}
-
-	return body, res.StatusCode, nil
 }
 
 func (s *IntegrationTestSuite) TestQueryDelegatorDelegationsGRPC() {

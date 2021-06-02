e2f01845a3ebf40ba386edc21dbdad48c3c181d4
Aaron Craelius <aaronc@users.noreply.github.com>
2021-06-02 16:51:24 -0400

    update simapp2

--- simapp/v2/app.go
+++ simapp/v2/app.go
@@ -3,9 +3,14 @@ package main
 import (
 	_ "embed"
 
+	"github.com/cosmos/cosmos-sdk/app"
 	"github.com/cosmos/cosmos-sdk/app/cli"
 
-	"github.com/cosmos/cosmos-sdk/app"
+	// Load Modules
+	_ "github.com/cosmos/cosmos-sdk/x/auth/module"
+	_ "github.com/cosmos/cosmos-sdk/x/bank/module"
+	_ "github.com/cosmos/cosmos-sdk/x/gov/module"
+	_ "github.com/cosmos/cosmos-sdk/x/params/module"
 )
 
 //go:embed app.yaml

860157e396a0a2d9995f1350ec462a11dea9e717
Aaron Craelius <aaronc@users.noreply.github.com>
2021-06-02 18:39:19 -0400

    add docs

--- container/container.go
+++ container/container.go
@@ -13,6 +13,18 @@ func (c *Container) Provide(fn interface{}) error {
 	panic("implement me")
 }
 
+func (c *Container) ProvideWithScope(fn interface{}, scope Scope) error {
+	panic("implement me")
+}
+
 func (c *Container) Invoke(fn interface{}) error {
+	panic("TODO")
+}
+
+// InitializeAll attempts to initialize all registered providers in the container.
+// It returns an error if a provider returns an error. If a given provider has
+// dependencies which cannot be resolved, an error is not returned and instead
+// that provider is not called.
+func (c *Container) InitializeAll() error {
 	panic("TODO")
 }

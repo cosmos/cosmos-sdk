ff977ddb6909f6b0daf30947804ceddb779475ad
Robert Zaremba <robert@zaremba.ch>
2021-06-10 23:58:31 +0200

    added a note that EncodingConfig.Marshaler will be renamed to Codec

--- simapp/params/encoding.go
+++ simapp/params/encoding.go
@@ -10,7 +10,7 @@ import (
 // This is provided for compatibility between protobuf and amino implementations.
 type EncodingConfig struct {
 	InterfaceRegistry types.InterfaceRegistry
-	// Deprecated: this field wil lbe renamed to Codec
+	// NOTE: this field wil lbe renamed to Codec
 	Marshaler codec.Codec
 	TxConfig  client.TxConfig
 	Amino     *codec.LegacyAmino

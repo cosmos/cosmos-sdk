e86272dc6d7118b8cecea5f3d45d2142eb29800d
Zaki Manian <zaki@manian.org>
2021-06-20 21:42:09 -0700

    update the help message

--- x/auth/vesting/client/cli/tx.go
+++ x/auth/vesting/client/cli/tx.go
@@ -102,8 +102,7 @@ func NewMsgCreatePeriodicVestingAccountCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "create-periodic-vesting-account [to_address] [periods_json_file]",
 		Short: "Create a new vesting account funded with an allocation of tokens.",
-		Long: `Create a new vesting account funded with an allocation of tokens. This takes a destination address and 
-		a period json file. 
+		Long: `A sequence of coins and period length in seconds. Periods are sequential, in that the duration of of a period only starts at the end of the previous period. The duration of the first period starts upon account creation. For instance, the following periods.json file shows 20 "test" coins vesting 30 days apart from each other.
 		Where periods.json contains:
 
 		An array of coin strings and unix epoch times for coins to vest

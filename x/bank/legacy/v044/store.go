7312e5b400ab6b800188b2ec3285c37a216c4029
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-06-30 11:25:53 -0700

    x/bank: add v0.44 legacy migration

---
+++ x/bank/legacy/v044/store.go
@@ -0,0 +1,51 @@
+package v044
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+// MigrateStore performs in-place store migrations from v0.43 to v0.44. The
+// migration includes:
+//
+// - Add an additional reverse index from denomination to address.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+	return addDenomReverseIndex(store, cdc)
+}
+
+func addDenomReverseIndex(store sdk.KVStore, cdc codec.BinaryCodec) error {
+	oldBalancesStore := prefix.NewStore(store, types.BalancesPrefix)
+
+	oldBalancesIter := oldBalancesStore.Iterator(nil, nil)
+	defer oldBalancesIter.Close()
+
+	denomPrefixStores := make(map[string]prefix.Store) // memoize prefix stores
+
+	for ; oldBalancesIter.Valid(); oldBalancesIter.Next() {
+		var balance sdk.Coin
+		if err := cdc.Unmarshal(oldBalancesIter.Value(), &balance); err != nil {
+			return err
+		}
+
+		addr, err := types.AddressFromBalancesStore(oldBalancesIter.Key())
+		if err != nil {
+			return err
+		}
+
+		denomPrefixStore, ok := denomPrefixStores[balance.Denom]
+		if !ok {
+			denomPrefixStore = prefix.NewStore(store, types.CreateDenomPrefix(balance.Denom))
+			denomPrefixStores[balance.Denom] = denomPrefixStore
+		}
+
+		// Store a reverse index from denomination to account address with a
+		// sentinel value.
+		denomPrefixStore.Set(address.MustLengthPrefix(addr), []byte{0})
+	}
+
+	return nil
+}
---
+++ x/bank/legacy/v044/store_test.go
@@ -0,0 +1,1 @@
+package v044_test

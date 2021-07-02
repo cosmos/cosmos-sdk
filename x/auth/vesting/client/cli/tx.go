073851d9004e4bdbdf978c0307ec4eec1b29d0f8
Zaki Manian <zaki@manian.org>
2021-07-01 22:53:03 -0700

    Add starttime to the vesting input file

--- x/auth/vesting/client/cli/tx.go
+++ x/auth/vesting/client/cli/tx.go
@@ -91,6 +91,11 @@ timestamp.`,
 	return cmd
 }
 
+type VestingData struct {
+	StartTime int64
+	Periods   []InputPeriod
+}
+
 type InputPeriod struct {
 	Coins  string `json:"coins"`
 	Length int64  `json:"length_seconds"`
@@ -100,23 +105,24 @@ type InputPeriod struct {
 // MsgCreateVestingAccount transaction.
 func NewMsgCreatePeriodicVestingAccountCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "create-periodic-vesting-account [start_time_unix] [to_address] [periods_json_file]",
+		Use:   "create-periodic-vesting-account [to_address] [periods_json_file]",
 		Short: "Create a new vesting account funded with an allocation of tokens.",
 		Long: `A sequence of coins and period length in seconds. Periods are sequential, in that the duration of of a period only starts at the end of the previous period. The duration of the first period starts upon account creation. For instance, the following periods.json file shows 20 "test" coins vesting 30 days apart from each other.
 		Where periods.json contains:
 
 		An array of coin strings and unix epoch times for coins to vest
-
-[
-{
+{ "start_time": 1625204910,
+"period":[
+ {
   "coins": "10test",
   "length_seconds":2592000 //30 days
  },
  {
 	"coins": "10test",
 	"length_seconds":2592000 //30 days
-   },
+ },
 ]
+	}
 		`,
 		Args: cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -125,31 +131,26 @@ func NewMsgCreatePeriodicVestingAccountCmd() *cobra.Command {
 				return err
 			}
 
-			startTime, err := strconv.ParseInt(args[0], 10, 64)
-
-			if err != nil {
-				return err
-			}
-			toAddr, err := sdk.AccAddressFromBech32(args[1])
+			toAddr, err := sdk.AccAddressFromBech32(args[0])
 			if err != nil {
 				return err
 			}
 
-			contents, err := ioutil.ReadFile(args[2])
+			contents, err := ioutil.ReadFile(args[1])
 			if err != nil {
 				return err
 			}
 
-			var inputPeriods []InputPeriod
+			var vestingData VestingData
 
-			err = json.Unmarshal(contents, &inputPeriods)
+			err = json.Unmarshal(contents, &vestingData)
 			if err != nil {
 				return err
 			}
 
 			var periods []types.Period
 
-			for i, p := range inputPeriods {
+			for i, p := range vestingData.Periods {
 
 				amount, err := sdk.ParseCoinsNormalized(p.Coins)
 				if err != nil {

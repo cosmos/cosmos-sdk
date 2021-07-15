4a04673cb4343da48bd66be37d728c4ffa6ec33c
huangyi <huang@crypto.com>
2021-07-15 18:27:03 +0800

    start GRPCWebServer in goroutine
    so it don't block other code from executing.
    
    Closes #9703
    
    Update server/grpc/grpc_web.go
    
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
    
    add changelog entry
    
    Update server/grpc/grpc_web.go
    
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>
    
    change server start time to 3 seconds
    
    and refactored to share a constant variable

--- testutil/network/util.go
+++ testutil/network/util.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server/api"
 	servergrpc "github.com/cosmos/cosmos-sdk/server/grpc"
+	srvtypes "github.com/cosmos/cosmos-sdk/server/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
@@ -90,7 +91,7 @@ func startInProcess(cfg Config, val *Validator) error {
 		select {
 		case err := <-errCh:
 			return err
-		case <-time.After(5 * time.Second): // assume server started successfully
+		case <-time.After(srvtypes.ServerStartTime): // assume server started successfully
 		}
 
 		val.api = apiSrv
@@ -117,7 +118,7 @@ func startInProcess(cfg Config, val *Validator) error {
 			select {
 			case err := <-errCh1:
 				return err
-			case <-time.After(5 * time.Second): // assume server started successfully
+			case <-time.After(srvtypes.ServerStartTime): // assume server started successfully
 			}
 
 		}

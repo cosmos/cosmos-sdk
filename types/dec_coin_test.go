5dbc08cccef439a1e746b899696c2f981060927b
Spoorthi Satheesha <spoorthi_s@ymail.com>
2021-07-21 09:28:59 +0200

    Added test for DecCoins IsLT method

--- types/dec_coin_test.go
+++ types/dec_coin_test.go
@@ -676,3 +676,45 @@ func (s *decCoinTestSuite) TestDecCoins_IsAllPositive() {
 		}
 	}
 }
+
+func (s *decCoinTestSuite) TestDecCoins_IsLT() {
+	testCases := []struct {
+		coin           sdk.DecCoin
+		otherCoin      sdk.DecCoin
+		expectedResult bool
+		expectedPanic  bool
+		msg            string
+	}{
+
+		// Same Denom - Less than other coin
+		{sdk.DecCoin{testDenom1, sdk.NewDec(3)}, sdk.DecCoin{testDenom1, sdk.NewDec(19)}, true, false, "DecCoin amount lesser than other coin. Should be true."},
+
+		// Same Denom - Greater than other coin
+		{sdk.DecCoin{testDenom1, sdk.NewDec(343340)}, sdk.DecCoin{testDenom1, sdk.NewDec(14)}, false, false, "DecCoin amount greater than other coin. Should be false."},
+
+		// Same Denom - Same as other coin
+		{sdk.DecCoin{testDenom1, sdk.NewDec(20)}, sdk.DecCoin{testDenom1, sdk.NewDec(20)}, false, false, "DecCoin amount same as other coin. Should be false."},
+
+		// Different Denom - Less than other coin
+		{sdk.DecCoin{testDenom1, sdk.NewDec(3)}, sdk.DecCoin{testDenom2, sdk.NewDec(19)}, true, true, "DecCoin denom different than other coin. Should panic."},
+
+		// Different Denom - Greater than other coin
+		{sdk.DecCoin{testDenom1, sdk.NewDec(343340)}, sdk.DecCoin{testDenom2, sdk.NewDec(14)}, true, true, "DecCoin denom different than other coin. Should panic."},
+
+		// Different Denom - Same as other coin
+		{sdk.DecCoin{testDenom1, sdk.NewDec(20)}, sdk.DecCoin{testDenom2, sdk.NewDec(20)}, true, true, "DecCoin denom different than other coin. Should panic."},
+	}
+
+	for i, tc := range testCases {
+		if tc.expectedPanic {
+			s.Require().Panics(func() { tc.coin.IsLT(tc.otherCoin) }, "Test case #%d: %s", i, tc.msg)
+		} else {
+			res := tc.coin.IsLT(tc.otherCoin)
+			if tc.expectedResult {
+				s.Require().True(res, "Test case #%d: %s", i, tc.msg)
+			} else {
+				s.Require().False(res, "Test case #%d: %s", i, tc.msg)
+			}
+		}
+	}
+}

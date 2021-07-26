4d3544ac82d4853a54f0a47192d119f33278f5ca
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-07-26 12:40:31 +0200

    refactor: Improve txRaw decoder code

--- x/auth/tx/decoder.go
+++ x/auth/tx/decoder.go
@@ -16,7 +16,7 @@ import (
 func DefaultTxDecoder(cdc codec.ProtoCodecMarshaler) sdk.TxDecoder {
 	return func(txBytes []byte) (sdk.Tx, error) {
 		// Make sure txBytes follow ADR-027.
-		err := rejectNonADR027(txBytes)
+		err := rejectNonADR027Tx(txBytes)
 		if err != nil {
 			return nil, sdkerrors.Wrap(sdkerrors.ErrTxDecode, err.Error())
 		}
@@ -90,13 +90,13 @@ func DefaultJSONTxDecoder(cdc codec.ProtoCodecMarshaler) sdk.TxDecoder {
 	}
 }
 
-// rejectNonADR027 rejects txBytes that do not follow ADR-027. This function
+// rejectNonADR027Tx rejects txBytes that do not follow ADR-027. This function
 // only checks that:
 // - field numbers are in ascending order (1, 2, and potentially multiple 3s),
-// - and varints as as short as possible.
-// All other ADR-027 edge cases (e.g. TxRaw fields having default values) will
-// not happen with TxRaw.
-func rejectNonADR027(txBytes []byte) error {
+// - and varints are as short as possible.
+// All other ADR-027 edge cases (e.g. TxRaw fields having default values) are
+// not applicable with TxRaw.
+func rejectNonADR027Tx(txBytes []byte) error {
 	// Make sure all fields are ordered in ascending order with this variable.
 	prevTagNum := protowire.Number(0)
 
@@ -105,17 +105,21 @@ func rejectNonADR027(txBytes []byte) error {
 		if m < 0 {
 			return fmt.Errorf("invalid length; %w", protowire.ParseError(m))
 		}
+		// TxRaw only has bytes fields.
 		if wireType != protowire.BytesType {
 			return fmt.Errorf("expected %d wire type, got %d", protowire.VarintType, wireType)
 		}
+		// Make sure fields are ordered in ascending order.
 		if tagNum < prevTagNum {
 			return fmt.Errorf("txRaw must follow ADR-027, got tagNum %d after tagNum %d", tagNum, prevTagNum)
 		}
 		prevTagNum = tagNum
 
 		// All 3 fields of TxRaw have wireType == 2, so their next component
-		// is a varint.
-		// We make sure that the varint is as short as possible.
+		// is a varint, so we can safely call ConsumeVarint here.
+		// We make sure that this varint is as short as possible.
+		//
+		// Byte structure: <varint of bytes lenght><bytes sequence>
 		lengthPrefix, m := protowire.ConsumeVarint(txBytes[m:])
 		if m < 0 {
 			return fmt.Errorf("invalid length; %w", protowire.ParseError(m))
@@ -143,21 +147,21 @@ func varintMinLength(n uint64) int {
 	// Note: 1<<N == 2**N.
 	case n < 1<<7:
 		return 1
-	case n < 1<<14:
+	case n < 1<<7*2:
 		return 2
-	case n < 1<<21:
+	case n < 1<<7*3:
 		return 3
-	case n < 1<<28:
+	case n < 1<<7*4:
 		return 4
-	case n < 1<<35:
+	case n < 1<<7*5:
 		return 5
-	case n < 1<<42:
+	case n < 1<<7*6:
 		return 6
-	case n < 1<<49:
+	case n < 1<<7*7:
 		return 7
-	case n < 1<<56:
+	case n < 1<<7*8:
 		return 8
-	case n < 1<<63:
+	case n < 1<<7*9:
 		return 9
 	default:
 		return 10

3836af96076dd435d14976839fbc8c1aa98da54d
Michael FIG <mfig@agoric.com>
2021-07-27 14:57:41 -0600

    fix: don't crash EndBlock with a nil ctx.EventManager()

--- types/module/module.go
+++ types/module/module.go
@@ -756,7 +756,9 @@ func (m *Manager) BeginBlock(ctx sdk.Context) (sdk.BeginBlock, error) {
 // child context with an event manager to aggregate events emitted from all
 // modules.
 func (m *Manager) EndBlock(ctx sdk.Context, req abci.RequestEndBlock) abci.ResponseEndBlock {
-	ctx = ctx.WithEventManager(sdk.NewEventManagerWithHistory(ctx.EventManager().GetABCIEventHistory()))
+	if em := ctx.EventManager(); em != nil {
+		ctx = ctx.WithEventManager(sdk.NewEventManagerWithHistory(em.GetABCIEventHistory()))
+	}
 	validatorUpdates := []abci.ValidatorUpdate{}
 
 	for _, moduleName := range m.OrderEndBlockers {

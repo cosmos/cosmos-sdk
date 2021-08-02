9c73350ec0ec2e68014a81ffded1ffe37e91d4ef
Andrei Ivasko <cyberbono3@gmail.com>
2021-08-02 19:12:13 +0300

    fix output test

--- crypto/keyring/output_test.go
+++ crypto/keyring/output_test.go
@@ -1,4 +1,4 @@
-package keyring_test
+package keyring
 
 import (
 	"fmt"
@@ -6,7 +6,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	kmultisig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	"github.com/cosmos/cosmos-sdk/crypto/types"
@@ -18,16 +17,16 @@ func TestBech32KeysOutput(t *testing.T) {
 	tmpKey := sk.PubKey()
 	multisigPk := kmultisig.NewLegacyAminoPubKey(1, []types.PubKey{tmpKey})
 
-	k, err := keyring.NewRecord("multisig", multisigPk, keyring.NewMultiRecordItem(keyring.NewMultiRecord()))
+	k, err := NewRecord("multisig", multisigPk, NewMultiRecordItem(NewMultiRecord()))
 	require.NotNil(t, k)
 	require.NoError(t, err)
 	pubKey, err := k.GetPubKey()
 	require.NoError(t, err)
 	accAddr := sdk.AccAddress(pubKey.Address())
-	expectedOutput, err := keyring.NewKeyOutput(k.Name, k.GetType(), accAddr, multisigPk)
+	expectedOutput, err := NewKeyOutput(k.Name, k.GetType(), accAddr, multisigPk)
 	require.NoError(t, err)
 
-	out, err := keyring.MkAccKeyOutput(k)
+	out, err := MkAccKeyOutput(k)
 	require.NoError(t, err)
 	require.Equal(t, expectedOutput, out)
 	require.Equal(t, "{Name:multisig Type:multi Address:cosmos1nf8lf6n4wa43rzmdzwe6hkrnw5guekhqt595cw PubKey:{\"@type\":\"/cosmos.crypto.multisig.LegacyAminoPubKey\",\"threshold\":1,\"public_keys\":[{\"@type\":\"/cosmos.crypto.secp256k1.PubKey\",\"key\":\"AurroA7jvfPd1AadmmOvWM2rJSwipXfRf8yD6pLbA2DJ\"}]} Mnemonic:}", fmt.Sprintf("%+v", out))

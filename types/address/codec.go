366e58923bb321557196bc26f5959a799c6d6d55
Andrei Ivasko <cyberbono3@gmail.com>
2021-08-04 15:28:17 +0300

    update codec comments

--- types/address/codec.go
+++ types/address/codec.go
@@ -1,8 +1,8 @@
 package address
 
 type Codec interface {
-	// addressStringToBytes encodes text to bytes
+	// ConvertAddressStringToBytes encodes text to bytes
 	ConvertAddressStringToBytes(text string) ([]byte, error)
-	// addressBytesToString encodes bytes to text
+	// ConvertAddressBytesToString encodes bytes to text
 	ConvertAddressBytesToString(bz []byte) (string, error)
 }

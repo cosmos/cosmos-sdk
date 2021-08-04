1a552d7f5e054bed33816a62efc15036e2d22a43
Andrei Ivasko <cyberbono3@gmail.com>
2021-08-04 15:25:01 +0300

    remove unnecessary code

--- x/auth/keeper/grpc_query_test.go
+++ x/auth/keeper/grpc_query_test.go
@@ -9,11 +9,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
-func (suite *KeeperTestSuite) TestGRPCQueryModuleAccounts() {
-	var (
-		req *types.QueryModuleAccountsRequest
-	)
-
 func (suite *KeeperTestSuite) TestGRPCQueryAccounts() {
 	var (
 		req *types.QueryAccountsRequest

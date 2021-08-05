1cdf9ab91a43adad760fa7e5785d37009d049347
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2021-08-05 16:18:57 +0200

    add tx fee event

--- x/auth/ante/fee.go
+++ x/auth/ante/fee.go
@@ -117,6 +117,12 @@ func (dfd DeductFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bo
 		}
 	}
 
+	var events sdk.Events
+	events = append(events, sdk.NewEvent(sdk.EventTypeTx,
+		sdk.NewAttribute(sdk.AttributeKeyFee, feeTx.GetFee().String()),
+	))
+	ctx.EventManager().EmitEvents(events)
+
 	return next(ctx, tx, simulate)
 }
 

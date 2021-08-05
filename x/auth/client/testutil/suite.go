cb7e57a53cd4305fdfe33d2b06039f3d00a7f49c
likhita-809 <likhita@vitwit.com>
2021-08-05 16:30:03 +0530

    fix failing test

--- x/auth/client/testutil/suite.go
+++ x/auth/client/testutil/suite.go
@@ -128,11 +128,11 @@ func (s *IntegrationTestSuite) TestCLISignBatch() {
 
 	// sign-batch file - offline and sequence is set but account-number is not set
 	_, err = TxSignBatchExec(val.ClientCtx, val.Address, outputFile.Name(), fmt.Sprintf("--%s=%s --%s=%s", flags.FlagChainID, val.ClientCtx.ChainID, flags.FlagSequence, "1"), "--offline")
-	s.Require().EqualError(err, "required flag(s) \"account-number\" not set")
+	s.Require().EqualError(err, "required flag(s) \"account-number\", \"sequence\" not set")
 
 	// sign-batch file - offline and account-number is set but sequence is not set
 	_, err = TxSignBatchExec(val.ClientCtx, val.Address, outputFile.Name(), fmt.Sprintf("--%s=%s --%s=%s", flags.FlagChainID, val.ClientCtx.ChainID, flags.FlagAccountNumber, "1"), "--offline")
-	s.Require().EqualError(err, "required flag(s) \"sequence\" not set")
+	s.Require().EqualError(err, "required flag(s) \"account-number\", \"sequence\" not set")
 
 	// sign-batch file - sequence and account-number are set when offline is false
 	res, err := TxSignBatchExec(val.ClientCtx, val.Address, outputFile.Name(), fmt.Sprintf("--%s=%s --%s=%s --%s=%s", flags.FlagChainID, val.ClientCtx.ChainID, flags.FlagSequence, "1", flags.FlagAccountNumber, "1"))

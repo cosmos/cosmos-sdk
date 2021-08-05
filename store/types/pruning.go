a4765ea53bc42f52357d9ec9294b1a0dd419efd7
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-08-05 09:36:25 -0400

    store: godoc++

--- store/types/pruning.go
+++ store/types/pruning.go
@@ -11,9 +11,11 @@ const (
 )
 
 var (
-	// PruneDefault defines a pruning strategy where the last 100 heights are kept
-	// in addition to every 100th and where to-be pruned heights are pruned at
-	// every 10th height.
+	// PruneDefault defines a pruning strategy where the last 9072000 heights are
+	// kept in addition to every 100th and where to-be pruned heights are pruned
+	// at every 10th height. The last 9072000 heights are kept assuming the typical
+	// block time is 5s and typical unbonding period is 21 days. If these values
+	// do not match the applications' requirements, use the "custom" option.
 	PruneDefault = NewPruningOptions(9072000, 100, 10)
 
 	// PruneEverything defines a pruning strategy where all committed heights are

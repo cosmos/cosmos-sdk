ef525f998b4ce4ab35f8c5d9250a7347ed31ea7a
aleem1314 <aleem@vitwit.com>
2021-08-15 22:13:58 +0530

    fix tests

--- x/auth/vesting/types/vesting_account_test.go
+++ x/auth/vesting/types/vesting_account_test.go
@@ -31,9 +31,9 @@ type VestingAccountTestSuite struct {
 
 func (s *VestingAccountTestSuite) SetupTest() {
 	checkTx := false
-	app := simapp.Setup(s.T(), checkTx)
+	s.app = simapp.Setup(s.T(), checkTx)
 
-	s.ctx = app.BaseApp.NewContext(checkTx, tmproto.Header{Height: 1})
+	s.ctx = s.app.BaseApp.NewContext(checkTx, tmproto.Header{Height: 1})
 }
 
 func TestGetVestedCoinsContVestingAcc(t *testing.T) {

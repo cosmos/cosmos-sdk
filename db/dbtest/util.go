c4b45539b24893a146f315433f8d9c1c13cafb61
Roy Crihfield <roy@manteia.ltd>
2021-08-17 12:44:27 +0800

    Add prefixed db read/writers

---
+++ db/dbtest/util.go
@@ -0,0 +1,54 @@
+package dbtest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	dbm "github.com/cosmos/cosmos-sdk/db"
+)
+
+func AssertNext(t *testing.T, itr dbm.Iterator, expected bool) {
+	t.Helper()
+	require.Equal(t, expected, itr.Next())
+}
+
+func AssertDomain(t *testing.T, itr dbm.Iterator, start, end []byte) {
+	t.Helper()
+	ds, de := itr.Domain()
+	assert.Equal(t, start, ds, "checkDomain domain start incorrect")
+	assert.Equal(t, end, de, "checkDomain domain end incorrect")
+}
+
+func AssertItem(t *testing.T, itr dbm.Iterator, key []byte, value []byte) {
+	t.Helper()
+	v := itr.Value()
+	k := itr.Key()
+	assert.Exactly(t, key, k)
+	assert.Exactly(t, value, v)
+}
+
+func AssertInvalid(t *testing.T, itr dbm.Iterator) {
+	t.Helper()
+	AssertNext(t, itr, false)
+	AssertKeyPanics(t, itr)
+	AssertValuePanics(t, itr)
+}
+
+func AssertKeyPanics(t *testing.T, itr dbm.Iterator) {
+	t.Helper()
+	assert.Panics(t, func() { itr.Key() }, "checkKeyPanics expected panic but didn't")
+}
+
+func AssertValue(t *testing.T, db dbm.DBReader, key []byte, valueWanted []byte) {
+	t.Helper()
+	valueGot, err := db.Get(key)
+	assert.NoError(t, err)
+	assert.Equal(t, valueWanted, valueGot)
+}
+
+func AssertValuePanics(t *testing.T, itr dbm.Iterator) {
+	t.Helper()
+	assert.Panics(t, func() { itr.Value() })
+}

52a83528d7bd048142a22ee5c075ca8b13295b00
atheesh <atheesh@vitwit.com>
2021-08-18 15:12:07 +0530

    add signer index

--- client/tx/tx.go
+++ client/tx/tx.go
@@ -201,11 +201,28 @@ func Sign(txf Factory, name string, txBuilder client.TxBuilder, overwriteSig boo
 	if err != nil {
 		return err
 	}
+
 	pubKey := key.GetPubKey()
+	tx1 := txBuilder.GetTx()
+
+	pubkeys, err := tx1.GetPubKeys()
+	if err != nil {
+		return err
+	}
+
+	signerIndex := 0
+	for i, p := range pubkeys {
+		if p.Equals(pubKey) {
+			signerIndex = i
+			break
+		}
+	}
+
 	signerData := authsigning.SignerData{
 		ChainID:       txf.chainID,
 		AccountNumber: txf.accountNumber,
 		Sequence:      txf.sequence,
+		SignerIndex:   signerIndex,
 	}
 
 	// For SIGN_MODE_DIRECT, calling SetSignatures calls setSignerInfos on

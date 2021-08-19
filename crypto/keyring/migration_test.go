dbdcbf6199a74449000e19f4e00ad968d8b753e5
Andrei Ivasko <cyberbono3@gmail.com>
2021-08-19 18:28:02 +0300

    add migration test suite in migration_test.go

--- crypto/keyring/migration_test.go
+++ crypto/keyring/migration_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/99designs/keyring"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
@@ -17,6 +17,242 @@ import (
 )
 
 const n1 = "cosmos"
+
+
+type MigrationTestSuite struct {
+	suite.Suite
+
+	kb Keyring
+	priv cryptotypes.PrivKey
+	pub cryptotypes.PubKey
+	ks keystore
+
+}
+
+func (s *MigrationTestSuite) SetupSuite() {
+	dir := s.T().TempDir()
+	mockIn := strings.NewReader("")
+	cdc := getCodec()
+
+	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
+	s.Require().NoError(err)
+
+	ks, ok := kb.(keystore)
+	s.Require().True(ok)
+
+	s.kb = kb
+	s.ks = ks
+
+	s.priv = cryptotypes.PrivKey(secp256k1.GenPrivKey())
+	s.pub = s.priv.PubKey()
+
+}
+
+func (s *MigrationTestSuite) TestMigrateLegacyLocalKey() {
+
+	legacyLocalInfo := newLegacyLocalInfo(n1, s.pub, string(legacy.Cdc.MustMarshal(s.priv)), hd.Secp256k1.Name())
+	serializedLegacyLocalInfo := MarshalInfo(legacyLocalInfo)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyLocalInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	_, migrated, err := s.ks.migrate(n1)
+	s.Require().True(migrated)
+	s.Require().NoError(err)
+}
+
+
+func (s *MigrationTestSuite) TestMigrateLegacyLedgerKey() {
+
+	account, coinType, index := uint32(118), uint32(0), uint32(0)
+	hdPath := hd.NewFundraiserParams(account, coinType, index)
+	legacyLedgerInfo := newLegacyLedgerInfo(n1, s.pub, *hdPath, hd.Secp256k1.Name())
+	serializedLegacyLedgerInfo := MarshalInfo(legacyLedgerInfo)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyLedgerInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	_, migrated, err := s.ks.migrate(n1)
+	s.Require().True(migrated)
+	s.Require().NoError(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateLegacyOfflineKey() {
+
+
+	legacyOfflineInfo := newLegacyOfflineInfo(n1, s.pub, hd.Secp256k1.Name())
+	serializedLegacyOfflineInfo := MarshalInfo(legacyOfflineInfo)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyOfflineInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	_, migrated, err := s.ks.migrate(n1)
+	s.Require().True(migrated)
+	s.Require().NoError(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateLegacyMultiKey(t *testing.T) {
+
+	multi := multisig.NewLegacyAminoPubKey(
+		1, []cryptotypes.PubKey{
+			s.pub,
+		},
+	)
+
+	LegacyMultiInfo, err := NewLegacyMultiInfo(n1, multi)
+	s.Require().NoError(err)
+	serializedLegacyMultiInfo := MarshalInfo(LegacyMultiInfo)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyMultiInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	_, migrated, err := s.ks.migrate(n1)
+	s.Require().True(migrated)
+	s.Require().NoError(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateLocalRecord() {
+	
+	k1, err := NewLocalRecord("test record", s.priv, s.pub)
+	s.Require().NoError(err)
+
+	serializedRecord, err := s.ks.protoMarshalRecord(k1)
+	s.Require().NoError(err)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedRecord,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	k2, migrated, err := s.ks.migrate(n1)
+	s.Require().Equal(k2.Name, k1.Name)
+	
+	pub, err := k2.GetPubKey()
+	s.Require().NoError(err)
+	s.Require().Equal(pub, s.pub)
+
+	priv, err := extractPrivKeyFromRecord(k2)
+	s.Require().NoError(err)
+	s.Require().Equal(priv, s.priv)
+
+	s.Require().False(migrated)
+	s.Require().NoError(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateOneRandomItemError() {
+
+	randomBytes := []byte("abckd0s03l")
+
+	errItem := keyring.Item{
+		Key:         n1,
+		Data:        randomBytes,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(errItem))
+
+	_, migrated, err := s.ks.migrate(n1)
+	s.Require().False(migrated)
+	s.Require().Error(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateAllLegacyMultiOffline() {
+	
+	multi := multisig.NewLegacyAminoPubKey(
+		1, []cryptotypes.PubKey{
+			s.pub,
+		},
+	)
+
+	LegacyMultiInfo, err := NewLegacyMultiInfo(n1, multi)
+	s.Require().NoError(err)
+	serializedLegacyMultiInfo := MarshalInfo(LegacyMultiInfo)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyMultiInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	legacyOfflineInfo := newLegacyOfflineInfo(n1, s.pub, hd.Secp256k1.Name())
+	serializedLegacyOfflineInfo := MarshalInfo(legacyOfflineInfo)
+
+	item = keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyOfflineInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	migrated, err := s.kb.MigrateAll()
+	s.Require().True(migrated)
+	s.Require().NoError(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateAllNoItem() {
+	migrated, err := s.kb.MigrateAll()
+	s.Require().False(migrated)
+	s.Require().NoError(err)
+}
+
+func (s *MigrationTestSuite) TestMigrateErrUnknownItemKey() {
+
+	legacyOfflineInfo := newLegacyOfflineInfo(n1, s.pub, hd.Secp256k1.Name())
+	serializedLegacyOfflineInfo := MarshalInfo(legacyOfflineInfo)
+
+	item := keyring.Item{
+		Key:         n1,
+		Data:        serializedLegacyOfflineInfo,
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	incorrectItemKey := n1 + "1"
+	_, migrated, err := s.ks.migrate(incorrectItemKey)
+	s.Require().False(migrated)
+	s.Require().EqualError(err, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, incorrectItemKey).Error())
+}
+
+func (s *MigrationTestSuite) TestMigrateErrEmptyItemData(t *testing.T) {
+	item := keyring.Item{
+		Key:         n1,
+		Data:        []byte{},
+		Description: "SDK kerying version",
+	}
+
+	s.Require().NoError(s.ks.SetItem(item))
+
+	_, migrated, err := s.ks.migrate(n1)
+	s.Require().False(migrated)
+	s.Require().EqualError(err, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, n1).Error())
+}
 
 // newLegacyLocalInfo creates a new legacyLocalInfo instance
 func newLegacyLocalInfo(name string, pub cryptotypes.PubKey, privArmor string, algo hd.PubKeyType) LegacyInfo {
@@ -45,313 +281,4 @@ func newLegacyOfflineInfo(name string, pub cryptotypes.PubKey, algo hd.PubKeyTyp
 		PubKey: pub,
 		Algo:   algo,
 	}
-}
-
-func TestMigrateLegacyLocalKey(t *testing.T) {
-	//saves legacyLocalInfo to keyring
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	privKey := cryptotypes.PrivKey(priv)
-	pub := priv.PubKey()
-
-	legacyLocalInfo := newLegacyLocalInfo(n1, pub, string(legacy.Cdc.MustMarshal(privKey)), hd.Secp256k1.Name())
-	serializedLegacyLocalInfo := MarshalInfo(legacyLocalInfo)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyLocalInfo,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	_, migrated, err := ks.migrate(n1)
-	require.True(migrated)
-	require.NoError(err)
-}
-
-// test pass!
-// go test -tags='cgo ledger norace' github.com/cosmos-sdk/crypto
-
-func TestMigrateLegacyLedgerKey(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	pub := priv.PubKey()
-
-	account, coinType, index := uint32(118), uint32(0), uint32(0)
-	hdPath := hd.NewFundraiserParams(account, coinType, index)
-	legacyLedgerInfo := newLegacyLedgerInfo(n1, pub, *hdPath, hd.Secp256k1.Name())
-	serializedLegacyLedgerInfo := MarshalInfo(legacyLedgerInfo)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyLedgerInfo,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	_, migrated, err := ks.migrate(n1)
-	require.True(migrated)
-	require.NoError(err)
-}
-
-func TestMigrateLegacyOfflineKey(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	pub := priv.PubKey()
-
-	legacyOfflineInfo := newLegacyOfflineInfo(n1, pub, hd.Secp256k1.Name())
-	serializedLegacyOfflineInfo := MarshalInfo(legacyOfflineInfo)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyOfflineInfo,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	_, migrated, err := ks.migrate(n1)
-	require.True(migrated)
-	require.NoError(err)
-}
-
-func TestMigrateLegacyMultiKey(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	multi := multisig.NewLegacyAminoPubKey(
-		1, []cryptotypes.PubKey{
-			priv.PubKey(),
-		},
-	)
-	LegacyMultiInfo, err := NewLegacyMultiInfo(n1, multi)
-	require.NoError(err)
-	serializedLegacyMultiInfo := MarshalInfo(LegacyMultiInfo)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyMultiInfo,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	_, migrated, err := ks.migrate(n1)
-	require.True(migrated)
-	require.NoError(err)
-}
-
-func TestMigrateLocalRecord(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	privKey := cryptotypes.PrivKey(priv)
-	pub := priv.PubKey()
-
-	k, err := NewLocalRecord("test record", privKey, pub)
-	require.NoError(err)
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-
-	serializedRecord, err := ks.protoMarshalRecord(k)
-	require.NoError(err)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedRecord,
-		Description: "SDK kerying version",
-	}
-
-	require.NoError(ks.SetItem(item))
-
-	_, migrated, err := ks.migrate(n1)
-	require.False(migrated)
-	require.NoError(err)
-}
-
-func TestMigrateOneRandomItemError(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	randomBytes := []byte("abckd0s03l")
-
-	errItem := keyring.Item{
-		Key:         n1,
-		Data:        randomBytes,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(errItem))
-
-	_, migrated, err := ks.migrate(n1)
-	require.False(migrated)
-	require.Error(err)
-}
-
-func TestMigrateAllMultiOffline(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	multi := multisig.NewLegacyAminoPubKey(
-		1, []cryptotypes.PubKey{
-			priv.PubKey(),
-		},
-	)
-	LegacyMultiInfo, err := NewLegacyMultiInfo(n1, multi)
-	require.NoError(err)
-	serializedLegacyMultiInfo := MarshalInfo(LegacyMultiInfo)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyMultiInfo,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	priv = secp256k1.GenPrivKey()
-	pub := priv.PubKey()
-
-	legacyOfflineInfo := newLegacyOfflineInfo(n1, pub, hd.Secp256k1.Name())
-	serializedLegacyOfflineInfo := MarshalInfo(legacyOfflineInfo)
-
-	item = keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyOfflineInfo,
-		Description: "SDK kerying version",
-	}
-
-	require.NoError(ks.SetItem(item))
-
-	migrated, err := kb.MigrateAll()
-	require.True(migrated)
-	require.NoError(err)
-}
-
-func TestMigrateAllNoItem(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	migrated, err := kb.MigrateAll()
-	require.False(migrated)
-	require.NoError(err)
-}
-
-func TestMigrateErrUnknownItemKey(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	priv := secp256k1.GenPrivKey()
-	pub := priv.PubKey()
-
-	legacyOfflineInfo := newLegacyOfflineInfo(n1, pub, hd.Secp256k1.Name())
-	serializedLegacyOfflineInfo := MarshalInfo(legacyOfflineInfo)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        serializedLegacyOfflineInfo,
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	incorrectItemKey := n1 + "1"
-	_, migrated, err := ks.migrate(incorrectItemKey)
-	require.False(migrated)
-	require.EqualError(err, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, incorrectItemKey).Error())
-}
-
-func TestMigrateErrEmptyItemData(t *testing.T) {
-	dir := t.TempDir()
-	mockIn := strings.NewReader("")
-	cdc := getCodec()
-
-	require := require.New(t)
-	kb, err := New(n1, BackendTest, dir, mockIn, cdc)
-	require.NoError(err)
-
-	item := keyring.Item{
-		Key:         n1,
-		Data:        []byte{},
-		Description: "SDK kerying version",
-	}
-
-	ks, ok := kb.(keystore)
-	require.True(ok)
-	require.NoError(ks.SetItem(item))
-
-	_, migrated, err := ks.migrate(n1)
-	require.False(migrated)
-	require.EqualError(err, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, n1).Error())
 }

599fadf49fb4c4faa52eb1d1ba49d6528460de89
Andrei Ivasko <cyberbono3@gmail.com>
2021-08-24 15:37:10 +0300

    second draft

--- types/valuerenderer/value_renderer.go
+++ types/valuerenderer/value_renderer.go
@@ -11,7 +11,9 @@ type ValueRenderer interface {
 	Parse(s string) (interface{}, error)
 }
 
-type defaultValueRenderer struct {}
+type defaultValueRenderer struct {
+	denomQuerier func(string) banktypes.Metadata //convert DenomUnits to Display units
+}
 
 var _ ValueRenderer = &defaultValueRenderer{}
 
@@ -20,14 +22,21 @@ func (d defaultValueRenderer) Format(i interface{}) (string, error) {
 	  // Format(x interface{}) {
 		switch i.(type) {
 		case Int:
-			// 1000000000000 => 1,000,000,000,000
-
-		case Coins:
-			// "1000000000uregen" => "1regen"
-			c, ok := i.(Coins)
-
+			// 1000000000000 => "1,000,000,000,000"
+			// prefer number operations divide
+			// https://stackoverflow.com/questions/13020308/how-to-fmt-printf-an-integer-with-thousands-comma
+			// 1, there is no methods to convert Int to string
+			// 2. use string builder and go backwards and take every 3 characters
+			//  000,000,000,000,1  => reverse the string
+			// 3.reverse the string at the end
+		case Coin:  //convert Coin.Denom to Display.Denom
+			// "1000000uregen" => "1regen"
+		    // query denom.metadata from state
+			//  we concatanate fields Denom(choose Display.Denom) and Amount
+			// for Amount use the same algo then in case Int
 
 		case Dec:
+			// 
 
 		default:
 			return "",fmt.Errorf("incorrect type")
@@ -47,15 +56,15 @@ func (d defaultValueRenderer) Parse(s string) (interface{}, error) {
 
 	if !strings.HasSuffix(s, denom) {
 		// TODO handle this case  "1,000,000" -> Uint 10000000
-		result = ""
+		var sb strings.Builder
 		for _, s := range strings.Split(s, ",") {
 			// check if s does consist only from digits
-			result += s
+			sb.WriteString(s)
 		}// or use strings.Join
 
 		// check if result does consist only from digits
 		// make int from result and return it
-		return NewUintFromString(result), nil // test if panics
+		return NewUintFromString(sb.String()), nil // test if panics
 	}
 
 	// TODO handle this case "1,000,000regen" -> Coin
@@ -65,18 +74,16 @@ func (d defaultValueRenderer) Parse(s string) (interface{}, error) {
 	
 	// remove all commas from "1,000,000" in amount Str => "1000000"
 	// TODO consider to use standalone func for that cause this code block is repeated
-	validAmountStr = ""
-	for _, str := range strings.Split(amountStr, ",") {
-		validAmountStr += str
+	var sb strings.Builder
+	for _, s := range strings.Split(amountStr, ",") {
+		sb.WriteString(s)
 	}
 
-	// convert amount "1000000" =< 1000000 int64 
-	i, err := strconv.ParseInt(validAmountStr, 10, 64)
-	if err != nil {
-		return nil, err
+	i, ok := NewIntFromString(sb.String())
+	if !ok {
+		return nil, fmt.Errorf("unable to construct Int from str")
 	}
 
 	return NewInt64Coin(denomStr, i), nil
 	
-
 } 

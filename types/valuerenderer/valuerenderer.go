4231817c157762cd1f854d478e10bd58024e729c
Andrei Ivasko <cyberbono3@gmail.com>
2021-08-26 17:44:54 +0300

    WIP

--- types/valuerenderer/valuerenderer.go
+++ types/valuerenderer/valuerenderer.go
@@ -2,12 +2,12 @@ package valuerenderer
 
 import (
 	"errors"
-	//"strings"
-	// strconv
-
+//	"strings"
+//	"strconv"
+//	"unicode"
 
 	"github.com/cosmos/cosmos-sdk/types"
-
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
@@ -18,33 +18,20 @@ type ValueRenderer interface {
 	Parse(string) (interface{}, error)
 }
 
-type DefaultValueRenderer struct {}
-	//denomQuerier func(string) banktypes.Metadata //convert DenomUnits to Display units
-
+// create default value rreenderer in CLI and then get context from CLI 
+type DefaultValueRenderer struct {
+	// /string is denom that user sents
+	denomQuerier func(string) banktypes.Metadata// define in test only //convert DenomUnits to Display units 
+	//queryClient banktypes.QueryClient
+}
+func NewDefaultValueRenderer() {}
 
 var _ ValueRenderer = &DefaultValueRenderer{}
 
-/*
-	case Coin:  //convert Coin.Denom to Display.Denom
-		"1000000uregen" => "1regen"
-		query denom.metadata from state
-		 we concatanate fields Denom(choose Display.Denom) and Amount
-		for Amount use the same algo then in case Int
 
-	case Dec:
-*/
 func (d DefaultValueRenderer) Format(x interface{}) (string, error) { 
 	switch x.(type) {
 		case types.Int:
-			/*
-			1000000000000 => "1,000,000,000,000"
-			prefer number operations divide
-			https://stackoverflow.com/questions/13020308/how-to-fmt-printf-an-integer-with-thousands-comma
-			1, there is no methods to convert Int to string
-			2. use string builder and go backwards and take every 3 characters
-			000,000,000,000,1  => reverse the string
-			3.reverse the string at the end
-			*/
 			i, ok := x.(types.Int)
 			if !ok {
 				return "", errors.New("unable to cast interface{} to Int")
@@ -52,52 +39,47 @@ func (d DefaultValueRenderer) Format(x interface{}) (string, error) {
 			
 			p := message.NewPrinter(language.English)
 			return p.Sprintf("%d",i.Int64()),nil
-				
+		case types.Coin:
+			/*
+
+				case Coin:  //convert Coin.Denom to Display.Denom
+			"1000000uregen" => "1regen"
+			    
+			query denom.metadata from state
+		 	we concatanate fields Denom(choose Display.Denom) and Amount
+			for Amount use the same algo then in case Int
+
+			*/
+			coin, ok := x.(types.Coin)
+			if !ok {
+				return "", errors.New("unable to cast interface{} to Coin")
+			}
+		
+			metadata := d.denomQuerier(coin.Denom)
+			for _, denom := range metadata.DenomUnits { // find exponent that matches coin.Denom  {
+				// 23000000 mregen   =>  "regen"
+				if denom.Denom == metadata.Display {
+
+					//TODO
+					// find exponent that matches Display 
+					// substract 2 exponents
+				}
+			}
+			
+
+			// som real lifec examplke 23000 uregen   -> regen in tests
+
+
+			return "", nil
+		// TODO case Dec	
 		default: 
 			return "good", nil
 	}
 }
 
-func (d DefaultValueRenderer) Parse(s string) (interface{}, error) {return "h",nil}
-
 // TODO only 2 cases possible?
 // "1,000,000regen" -> Coin
-// "1,000,000" -> uint
-/*
-func (d defaultValueRenderer) Parse(s string) (interface{}, error) {
-	// "1,000,000regen" -> Coin we have to sepearate denom and amount"regen"
+// "1,000,000" -> Uint
 
-	if !strings.HasSuffix(s, denom) {
-		// TODO handle this case  "1,000,000" -> Uint 10000000
-		var sb strings.Builder
-		for _, s := range strings.Split(s, ",") {
-			// check if s does consist only from digits
-			sb.WriteString(s)
-		}// or use strings.Join
-
-		// check if result does consist only from digits
-		// make int from result and return it
-		return NewUintFromString(sb.String()), nil // test if panics
-	}
-
-	// TODO handle this case "1,000,000regen" -> Coin
-	index := strings.Index(s, denom)
-	// "1,000,000", "regen"
-	amountStr, denomStr := strings.Join(s[:index], ""), strings.Join(s[index:], "")
-	
-	// remove all commas from "1,000,000" in amount Str => "1000000"
-	// TODO consider to use standalone func for that cause this code block is repeated
-	var sb strings.Builder
-	for _, s := range strings.Split(amountStr, ",") {
-		sb.WriteString(s)
-	}
-
-	i, ok := NewIntFromString(sb.String())
-	if !ok {
-		return nil, fmt.Errorf("unable to construct Int from str")
-	}
-
-	return NewInt64Coin(denomStr, i), nil
-	
-} 
-*/
+func (d DefaultValueRenderer) Parse(s string) (interface{}, error) { return nil,nil }
+// See TestParseString
--- types/valuerenderer/valuerenderer_test.go
+++ types/valuerenderer/valuerenderer_test.go
@@ -1,7 +1,13 @@
 package valuerenderer_test
 
 import (
+	"regexp"
+	"strings"
 	"testing"
+	"strconv"
+	"errors"
+	"unicode"
+
 
 	"github.com/stretchr/testify/require"
 
@@ -32,3 +38,79 @@ func TestFormatInt(t *testing.T) {
 	require.Equal(t, s, "1,000,000,000")
 }
 */
+
+
+func TestParseString(t *testing.T) {
+   re := regexp.MustCompile(`\d+[mu]?regen`)
+
+   tt := []struct {
+	   str string
+	   denomExp bool
+	   errExp bool
+   }{
+	   {"10regen", true, false},
+	   {"1,000,000", false, false},
+	   {"323,000,000", false, false},
+	   {"1mregen", true, false},
+	   {"500uregen", true, false},
+	   {"500,000,000regen", true, false},
+	   {"500,000,000regen", false, true},
+	   {"1,500,000,000regen", true, false},
+	   {"394,382,328uregen", true, false},
+   }
+
+   for _, tc := range tt {
+	   t.Run(tc.str, func(t *testing.T) {
+			s := strings.ReplaceAll(tc.str, ",", "")
+			if tc.denomExp {
+				// make sure it matches regexp to make up a Coin
+				require.True(t, re.MatchString(s))
+				c, err := coinFromString(s)
+				require.NoError(t, err)
+				require.NotNil(t, c)
+			} else {
+				// convert to Uint
+				i64, err := strconv.ParseUint(s, 10, 64)
+				if tc.errExp {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
+					require.NotNil(t, types.NewUint(i64))
+				}
+			} 
+	   })
+   }
+}
+
+func coinFromString(s string) (types.Coin, error) {
+	index := len(s) -1
+	for i := len(s)-1; i >= 0; i--{
+		if unicode.IsLetter(rune(s[i])) {
+			continue
+		}
+
+		index = i
+		break
+	}
+
+	if index == len(s)-1 {
+		return types.Coin{}, errors.New("no denom has been found")
+	}
+    
+	denom := s[index+1:]
+	amountStr := s[:index+1]
+	// convert to int64 to make up Coin later
+	amountInt, ok := types.NewIntFromString(amountStr)
+	if !ok {
+		return types.Coin{}, errors.New("unable convert amoountStr to int64")
+	}
+
+	return types.NewCoin(denom, amountInt), nil
+}
+
+
+
+
+
+
+

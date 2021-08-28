177a6d041f157e5287fb8094bd1d7ba8ee47d725
ValarDragon <dojha12@gmail.com>
2021-08-28 16:51:49 -0400

    Update Spec

--- x/auth/keeper/keeper.go
+++ x/auth/keeper/keeper.go
@@ -25,11 +25,11 @@ type AccountKeeperI interface {
 	// Return a new account with the next account number. Does not save the new account to the store.
 	NewAccount(sdk.Context, types.AccountI) types.AccountI
 
-	// Retrieve an account from the store.
-	GetAccount(sdk.Context, sdk.AccAddress) types.AccountI
-
 	// Check if an account exists in the store.
 	HasAccount(sdk.Context, sdk.AccAddress) bool
+
+	// Retrieve an account from the store.
+	GetAccount(sdk.Context, sdk.AccAddress) types.AccountI
 
 	// Set an account in the store.
 	SetAccount(sdk.Context, types.AccountI)

356f91847f0744014cfd299cc9ea1a8e17bf6c14
Jeeyong Um <conr2d@gmail.com>
2021-09-10 19:21:35 +0900

    fix: Fix lint errors in types/bech32

--- types/bech32/bech32.go
+++ types/bech32/bech32.go
@@ -304,7 +304,7 @@ func ConvertBits(data []byte, fromBits, toBits uint8, pad bool) ([]byte, error) 
 	for _, b := range data {
 
 		// Discard unused bits.
-		b = b << (8 - fromBits)
+		b <<= (8 - fromBits)
 
 		// How many bits remaining to extract from the input data.
 		remFromBits := fromBits
@@ -325,7 +325,7 @@ func ConvertBits(data []byte, fromBits, toBits uint8, pad bool) ([]byte, error) 
 
 			// Discard the bits we just extracted and get ready for
 			// next iteration.
-			b = b << toExtract
+			b <<= toExtract
 			remFromBits -= toExtract
 			filledBits += toExtract
 
@@ -341,7 +341,7 @@ func ConvertBits(data []byte, fromBits, toBits uint8, pad bool) ([]byte, error) 
 
 	// We pad any unfinished group if specified.
 	if pad && filledBits > 0 {
-		nextByte = nextByte << (toBits - filledBits)
+		nextByte <<= (toBits - filledBits)
 		regrouped = append(regrouped, nextByte)
 		filledBits = 0
 		nextByte = 0

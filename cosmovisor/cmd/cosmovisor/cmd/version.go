2c435898dbcd75300be32cb0f97c475365c5e361
Spoorthi <9302666+spoo-bar@users.noreply.github.com>
2021-09-13 10:05:07 +0530

    typo fix

--- cosmovisor/cmd/cosmovisor/cmd/version.go
+++ cosmovisor/cmd/cosmovisor/cmd/version.go
@@ -14,10 +14,10 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Cosmovisor and the application",
+	Short: "Prints the version number of Cosmovisor and the application",
 	Long:  `Prints the version number of Comovisor and the underlying application`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		fmt.Println("Cosmovisor version : ", Version)
+		fmt.Println("Cosmovisor Version: ", Version)
 	},
 }

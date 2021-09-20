aa7e5298bb6e5785d5853a0a51991b95e1246f45
Andrei Ivasko <cyberbono3@gmail.com>
2021-09-20 14:57:50 +0300

    Update scripts/protocgen.sh
    Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -18,7 +18,7 @@ for dir in $proto_dirs; do
   buf protoc \
     -I "proto" \
     -I "third_party/proto" \
-        --gocosmos_out=plugins=grpc,\
+    --gocosmos_out=plugins=grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
     --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')

4853a2459db39601b263de9da267ff2583322f53
Aaron Craelius <aaronc@users.noreply.github.com>
2021-09-23 11:12:56 -0400

    address review comments

--- container/struct_args.go
+++ container/struct_args.go
@@ -6,13 +6,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-// StructArgs is a type which can be embedded in another struct to alert the
-// container that the fields of the struct are dependency inputs/outputs. That
-// is, the container will not look to resolve a value with StructArgs embedded
-// directly, but will instead use the struct's fields to resolve or populate
-// dependencies. Types with embedded StructArgs can be used in both the input
-// and output parameter positions.
-
 // In can be embedded in another struct to inform the container that the
 // fields of the struct should be treated as dependency inputs.
 // This allows a struct to be used to specify dependencies rather than

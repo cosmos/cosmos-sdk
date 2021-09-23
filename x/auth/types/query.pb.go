d572a7273d285ac7733282e01f87ca94e9facedc
lukerhoads <pumpkineater661@gmail.com>
2021-09-23 08:39:22 -0500

    Resolve merge conflicts

--- x/auth/types/query.pb.go
+++ x/auth/types/query.pb.go
@@ -16,6 +16,9 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	io "io"
+	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.

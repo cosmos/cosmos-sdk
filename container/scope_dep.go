b3db69738477a657dbe2346d7aa09fb34f66d850
Aaron Craelius <aaron@regen.network>
2021-09-23 11:12:02 -0400

    Apply suggestions from code review
    Co-authored-by: Marie Gauthier <marie.gauthier63@gmail.com>

--- container/scope_dep.go
+++ container/scope_dep.go
@@ -5,7 +5,7 @@ import (
 )
 
 type scopeDepProvider struct {
-	ctr            *ProviderDescriptor
+	provider            *ProviderDescriptor
 	calledForScope map[Scope]bool
 	valueMap       map[Scope][]reflect.Value
 }

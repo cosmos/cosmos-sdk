deccdfae3ca712ffdb9c7a95e381857618cc21d9
likhita-809 <likhita@vitwit.com>
2021-09-28 12:10:44 +0530

    charge gas for key length

--- store/gaskv/store.go
+++ store/gaskv/store.go
@@ -50,6 +50,7 @@ func (gs *Store) Set(key []byte, value []byte) {
 	types.AssertValidValue(value)
 	gs.gasMeter.ConsumeGas(gs.gasConfig.WriteCostFlat, types.GasWriteCostFlatDesc)
 	// TODO overflow-safe math?
+	gs.gasMeter.ConsumeGas(gs.gasConfig.WriteCostPerByte*types.Gas(len(key)), types.GasWritePerByteDesc)
 	gs.gasMeter.ConsumeGas(gs.gasConfig.WriteCostPerByte*types.Gas(len(value)), types.GasWritePerByteDesc)
 	gs.parent.Set(key, value)
 }
@@ -173,8 +174,10 @@ func (gi *gasIterator) Error() error {
 // based on the current value's length.
 func (gi *gasIterator) consumeSeekGas() {
 	if gi.Valid() {
+		key := gi.Key()
 		value := gi.Value()
 
+		gi.gasMeter.ConsumeGas(gi.gasConfig.ReadCostPerByte*types.Gas(len(key)), types.GasValuePerByteDesc)
 		gi.gasMeter.ConsumeGas(gi.gasConfig.ReadCostPerByte*types.Gas(len(value)), types.GasValuePerByteDesc)
 	}
 	gi.gasMeter.ConsumeGas(gi.gasConfig.IterNextCostFlat, types.GasIterNextCostFlatDesc)

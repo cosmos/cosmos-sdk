a29673c2c256dbee964e4b5d577c726287e3fd10
Robert Zaremba <robert@zaremba.ch>
2021-09-28 17:13:09 +0200

    comment update

--- x/bank/types/key.go
+++ x/bank/types/key.go
@@ -60,9 +60,10 @@ func CreateAccountBalancesPrefix(addr []byte) []byte {
 // CreateDenomAddressPrefix creates a prefix for a reverse index of denomination
 // to account balance for that denomination.
 func CreateDenomAddressPrefix(denom string) []byte {
+	// we add a "zero" byte at the end - null byte terminator, to allow prefix denom prefix
+	// scan. Setting it is not needed (key[last] = 0) - because this is the default.
 	key := make([]byte, len(DenomAddressPrefix)+len(denom)+1)
 	copy(key, DenomAddressPrefix)
 	copy(key[len(DenomAddressPrefix):], denom)
-	// key[last] = 0 - not needed, because this is the default
 	return key
 }

6018aec39f24929fd561f44e4bcaaaed607ace5f
tycho garen <garen@tychoish.com>
2021-10-04 15:55:23 -0400

    avoid braodcasttxcommit

--- client/broadcast.go
+++ client/broadcast.go
@@ -21,14 +21,11 @@ import (
 // defined.
 func (ctx Context) BroadcastTx(txBytes []byte) (res *sdk.TxResponse, err error) {
 	switch ctx.BroadcastMode {
-	case flags.BroadcastSync:
+	case flags.BroadcastSync, flags.BroadcastBlock:
 		res, err = ctx.BroadcastTxSync(txBytes)
 
 	case flags.BroadcastAsync:
 		res, err = ctx.BroadcastTxAsync(txBytes)
-
-	case flags.BroadcastBlock:
-		res, err = ctx.BroadcastTxCommit(txBytes)
 
 	default:
 		return nil, fmt.Errorf("unsupported return type %s; supported types: sync, async, block", ctx.BroadcastMode)

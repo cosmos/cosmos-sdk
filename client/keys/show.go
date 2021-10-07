565685476edc22a5f253f82fdc040f5bec615184
likhita-809 <likhita@vitwit.com>
2021-10-07 16:59:08 +0530

    migrate key names correctly

--- client/keys/show.go
+++ client/keys/show.go
@@ -3,6 +3,7 @@ package keys
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/libs/cli"
@@ -163,6 +164,9 @@ func runShowCmd(cmd *cobra.Command, args []string) (err error) {
 }
 
 func fetchKey(kb keyring.Keyring, keyref string) (*keyring.Record, error) {
+	if !strings.HasSuffix(keyref, ".info") {
+		keyref = keyref + ".info"
+	}
 	// firstly check if the keyref is a key name of a key registered in a keyring.
 	k, err := kb.Key(keyref)
 	// if the key is not there or if we have a problem with a keyring itself then we move to a

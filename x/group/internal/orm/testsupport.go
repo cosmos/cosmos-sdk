560662e579092f85b0476f3f7fc0da985ec0ceb8
blushi <marie.gauthier63@gmail.com>
2021-10-08 12:39:12 +0200

    Fix tests

--- x/group/internal/orm/testsupport.go
+++ x/group/internal/orm/testsupport.go
@@ -3,6 +3,7 @@ package orm
 import (
 	"github.com/cosmos/cosmos-sdk/store"
 	"github.com/cosmos/cosmos-sdk/store/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	dbm "github.com/tendermint/tm-db"
 )
@@ -20,11 +21,11 @@ func NewMockContext() *MockContext {
 	}
 }
 
-func (m MockContext) KVStore(key sdk.StoreKey) sdk.KVStore {
+func (m MockContext) KVStore(key storetypes.StoreKey) sdk.KVStore {
 	if s := m.store.GetCommitKVStore(key); s != nil {
 		return s
 	}
-	m.store.MountStoreWithDB(key, sdk.StoreTypeIAVL, m.db)
+	m.store.MountStoreWithDB(key, storetypes.StoreTypeIAVL, m.db)
 	if err := m.store.LoadLatestVersion(); err != nil {
 		panic(err)
 	}

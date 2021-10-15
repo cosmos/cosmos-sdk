b646d588b7538fe2a711fb1c56e03c9d6135ebd8
Daniel Wedul <dwedul@figure.com>
2021-10-15 13:16:10 -0600

    [10286]: Finish up unit tests on download.go.

--- x/upgrade/types/planinfo/downloader_test.go
+++ x/upgrade/types/planinfo/downloader_test.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"io/fs"
+	"io"
 	"os"
 	"path/filepath"
 	"testing"
@@ -18,16 +18,13 @@ type DownloaderTestSuite struct {
 	suite.Suite
 
 	// Home is a temporary directory for use in these tests.
-	// It will have src/ and dst/ directories.
-	// src/ is for things to "download"
-	// dst/ is where things get downloaded to.
+	// It will have a src/ for things to download.
 	Home string
 }
 
 func (s *DownloaderTestSuite) SetupTest() {
 	s.Home = s.T().TempDir()
 	s.Assert().NoError(os.MkdirAll(filepath.Join(s.Home, "src"), 0777), "creating src/ dir")
-	s.Assert().NoError(os.MkdirAll(filepath.Join(s.Home, "dst"), 0777), "creating dst/ dir")
 	s.T().Logf("Home: [%s]", s.Home)
 }
 
@@ -112,6 +109,24 @@ func (s DownloaderTestSuite) saveSrcTestFile(f *TestFile) string {
 	return fullName
 }
 
+// requireFileExistsAndIsExecutable requires that the given file exists and is executable.
+func requireFileExistsAndIsExecutable(t *testing.T, path string) {
+	info, err := os.Stat(path)
+	require.NoError(t, err, "stat error")
+	perm := info.Mode().Perm()
+	// Checks if at least one executable bit is set (user, group, or other)
+	isExe := perm&0111 != 0
+	require.True(t, isExe, "is executable: permissions = %s", perm)
+}
+
+// requireFileEquals requires that the contents of the file at the given path
+// is equal to the contents of the given TestFile.
+func requireFileEquals(t *testing.T, path string, tf *TestFile) {
+	file, err := os.ReadFile(path)
+	require.NoError(t, err, "reading file")
+	require.Equal(t, string(tf.Contents), string(file), "file contents")
+}
+
 func (s *DownloaderTestSuite) TestDownloadUpgrade() {
 	justAFile := NewTestFile("just-a-file", "#!/usr/bin\necho 'I am just a file'\n")
 	someFileName := "some-file"
@@ -125,30 +140,79 @@ func (s *DownloaderTestSuite) TestDownloadUpgrade() {
 		_, tName := filepath.Split(testName)
 		return s.Home + "/dst/" + tName
 	}
-	
-	s.T().Run("url does not exist returns error", func(t *testing.T) {
+	makeFileURL := func(t *testing.T, path string) string {
+		f, err := os.Open(path)
+		require.NoError(t, err, "opening file")
+		defer f.Close()
+		hasher := sha256.New()
+		_, err = io.Copy(hasher, f)
+		require.NoError(t, err, "copying file to hasher")
+		return fmt.Sprintf("file://%s?checksum=sha256:%x", path, hasher.Sum(nil))
+	}
+
+	s.T().Run("url does not exist", func(t *testing.T) {
 		dstRoot := getDstDir(t.Name())
-		url := "file:///never-gonna-be-a-thing"
+		url := "file:///never/gonna/be/a/thing.zip?checksum=sha256:2c22e34510bd1d4ad2343cdc54f7165bccf30caef73f39af7dd1db2795a3da48"
 		err := DownloadUpgrade(dstRoot, url, "nothing")
 		require.Error(t, err)
-		assert.Contains(t, err.Error(), "could not download reference link")
+		assert.Contains(t, err.Error(), "no such file or directory")
 	})
-	
-	s.T().Run("url returns single file", func(t *testing.T) {
+
+	s.T().Run("url does not have checksum", func(t *testing.T) {
 		dstRoot := getDstDir(t.Name())
 		url := "file://" + justAFilePath
 		err := DownloadUpgrade(dstRoot, url, justAFile.Name)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "missing checksum query parameter")
 	})
-	// TODO: Unit tests on DownloadUpgrade(dstRoot, url, daemonName)
-	// Tests:
-	// * Url does not exist: Error.
-	// * Url returns a file: No error.
-	// * Url returns an archive with a bin/ dir with the file: No Error.
-	// * Url returns an archive with just the expected file: No Error.
-	// * Url returns an archive without the expected file: Error.
-	// For each that isn't supposed to return an error, make sure:
-	// * The expected file exists.
-	// * The expected file is executable.
+
+	s.T().Run("url has incorrect checksum", func(t *testing.T) {
+		dstRoot := getDstDir(t.Name())
+		badChecksum := "2c22e34510bd1d4ad2343cdc54f7165bccf30caef73f39af7dd1db2795a3da48"
+		url := "file://" + justAFilePath + "?checksum=sha256:" + badChecksum
+		err := DownloadUpgrade(dstRoot, url, justAFile.Name)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "Checksums did not match")
+		assert.Contains(t, err.Error(), "Expected: " + badChecksum)
+	})
+
+	s.T().Run("url returns single file", func(t *testing.T) {
+		dstRoot := getDstDir(t.Name())
+		url := makeFileURL(t, justAFilePath)
+		err := DownloadUpgrade(dstRoot, url, justAFile.Name)
+		require.NoError(t, err)
+		expectedFile := filepath.Join(dstRoot, "bin", justAFile.Name)
+		requireFileExistsAndIsExecutable(t, expectedFile)
+		requireFileEquals(t, expectedFile, justAFile)
+	})
+
+	s.T().Run("url returns archive with file in bin", func(t *testing.T) {
+		dstRoot := getDstDir(t.Name())
+		url := makeFileURL(t, someFileInBinZip)
+		err := DownloadUpgrade(dstRoot, url, someFileName)
+		require.NoError(t, err)
+		expectedFile := filepath.Join(dstRoot, "bin", someFileName)
+		requireFileExistsAndIsExecutable(t, expectedFile)
+		requireFileEquals(t, expectedFile, someFileInBin)
+	})
+
+	s.T().Run("url returns archive with just expected file", func(t *testing.T) {
+		dstRoot := getDstDir(t.Name())
+		url := makeFileURL(t, justAFileZip)
+		err := DownloadUpgrade(dstRoot, url, justAFile.Name)
+		require.NoError(t, err)
+		expectedFile := filepath.Join(dstRoot, "bin", justAFile.Name)
+		requireFileExistsAndIsExecutable(t, expectedFile)
+		requireFileEquals(t, expectedFile, justAFile)
+	})
+
+	s.T().Run("url returns archive without expected file", func(t *testing.T) {
+		dstRoot := getDstDir(t.Name())
+		url := makeFileURL(t, allFilesZip)
+		err := DownloadUpgrade(dstRoot, url, "not-expected")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "result does not contain a bin/not-expected or not-expected file")
+	})
 }
 
 func (s *DownloaderTestSuite) TestEnsureBinary() {
@@ -157,13 +221,13 @@ func (s *DownloaderTestSuite) TestEnsureBinary() {
 	isExeName := s.saveSrcTestFile(NewTestFile("is-exe.sh", "#!/bin/bash\necho 'executing'\n"))
 	s.Require().NoError(os.Chmod(isExeName, 0777), "chmod error isExeName")
 
-	s.T().Run("file does not exist gives error", func(t *testing.T) {
+	s.T().Run("file does not exist", func(t *testing.T) {
 		name := filepath.Join(s.Home, "does-not-exist.txt")
 		actual := EnsureBinary(name)
 		require.Error(t, actual)
 	})
 
-	s.T().Run("file is a directory gives error", func(t *testing.T) {
+	s.T().Run("file is a directory", func(t *testing.T) {
 		name := filepath.Join(s.Home, "src")
 		actual := EnsureBinary(name)
 		require.EqualError(t, actual, fmt.Sprintf("%s is not a regular file", "src"))
@@ -173,20 +237,14 @@ func (s *DownloaderTestSuite) TestEnsureBinary() {
 		name := nonExeName
 		actual := EnsureBinary(name)
 		require.NoError(t, actual, "EnsureBinary error")
-		info, err := os.Stat(name)
-		require.NoError(t, err, "stat error")
-		actualExec := info.Mode().Perm() & 0001
-		require.Equal(t, fs.FileMode(1), actualExec, "permissions")
+		requireFileExistsAndIsExecutable(t, name)
 	})
 
-	s.T().Run("file is already executable no error", func(t *testing.T) {
+	s.T().Run("file is already executable", func(t *testing.T) {
 		name := isExeName
 		actual := EnsureBinary(name)
 		require.NoError(t, actual, "EnsureBinary error")
-		info, err := os.Stat(name)
-		require.NoError(t, err, "stat error")
-		actualExec := info.Mode().Perm() & 0001
-		require.Equal(t, fs.FileMode(1), actualExec, "permissions")
+		requireFileExistsAndIsExecutable(t, name)
 	})
 }
 
@@ -195,30 +253,32 @@ func (s *DownloaderTestSuite) TestDownloadPlanInfoFromURL() {
 	planFile := NewTestFile("plan-info.json", planContents)
 	planPath := s.saveSrcTestFile(planFile)
 	planChecksum := fmt.Sprintf("%x", sha256.Sum256(planFile.Contents))
-	emptyPlanPath := s.saveSrcTestFile(NewTestFile("empty-plan-info.json", ""))
+	emptyFile := NewTestFile("empty-plan-info.json", "")
+	emptyPlanPath := s.saveSrcTestFile(emptyFile)
+	emptyChecksum := fmt.Sprintf("%x", sha256.Sum256(emptyFile.Contents))
 
-	s.T().Run("url does not exist returns error", func(t *testing.T) {
-		url := "file:///never-gonna-be-a-thing"
+	s.T().Run("url does not exist", func(t *testing.T) {
+		url := "file:///never-gonna-be-a-thing?checksum=sha256:2c22e34510bd1d4ad2343cdc54f7165bccf30caef73f39af7dd1db2795a3da48"
 		_, err := DownloadPlanInfoFromURL(url)
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "could not download reference link")
 	})
 
-	s.T().Run("without checksum returns content", func(t *testing.T) {
+	s.T().Run("without checksum", func(t *testing.T) {
 		url := "file://" + planPath
-		actual, err := DownloadPlanInfoFromURL(url)
-		require.NoError(t, err)
-		require.Equal(t, planContents, actual)
+		_, err := DownloadPlanInfoFromURL(url)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "missing checksum query parameter")
 	})
 
-	s.T().Run("with correct checksum returns content", func(t *testing.T) {
+	s.T().Run("with correct checksum", func(t *testing.T) {
 		url := "file://" + planPath + "?checksum=sha256:" + planChecksum
 		actual, err := DownloadPlanInfoFromURL(url)
 		require.NoError(t, err)
 		require.Equal(t, planContents, actual)
 	})
 
-	s.T().Run("with incorrect checksum returns error", func(t *testing.T) {
+	s.T().Run("with incorrect checksum", func(t *testing.T) {
 		badChecksum := "2c22e34510bd1d4ad2343cdc54f7165bccf30caef73f39af7dd1db2795a3da48"
 		url := "file://" + planPath + "?checksum=sha256:" + badChecksum
 		_, err := DownloadPlanInfoFromURL(url)
@@ -228,11 +288,10 @@ func (s *DownloaderTestSuite) TestDownloadPlanInfoFromURL() {
 		assert.Contains(t, err.Error(), "Got: " +planChecksum)
 	})
 
-	s.T().Run("plan is empty returns error", func(t *testing.T) {
-		url := "file://" + emptyPlanPath
+	s.T().Run("plan is empty", func(t *testing.T) {
+		url := "file://" + emptyPlanPath + "?checksum=sha256:" + emptyChecksum
 		_, err := DownloadPlanInfoFromURL(url)
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "no content returned")
 	})
 }
-

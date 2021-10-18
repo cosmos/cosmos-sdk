2e7e75bf1dfc17d0f76a6d49732cebc1a572b61a
atheesh <atheesh@vitwit.com>
2021-10-18 17:07:57 +0530

    fix tests

--- x/auth/tx/mode_handler.go
+++ x/auth/tx/mode_handler.go
@@ -13,7 +13,6 @@ var DefaultSignModes = []signingtypes.SignMode{
 	signingtypes.SignMode_SIGN_MODE_DIRECT,
 	signingtypes.SignMode_SIGN_MODE_DIRECT_AUX,
 	signingtypes.SignMode_SIGN_MODE_LEGACY_AMINO_JSON,
-	signingtypes.SignMode_SIGN_MODE_DIRECT_AUX,
 }
 
 // makeSignModeHandler returns the default protobuf SignModeHandler supporting

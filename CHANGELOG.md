85eed1f359040343c6bcdaa825f56de0f45614dd
Roy Crihfield <roy@manteia.ltd>
2021-10-19 19:58:06 +0800

    feat: ADR-040: Implement KV Store with decoupled storage and SMT (#9892)
    ## Description
    
    Resolves: https://github.com/cosmos/cosmos-sdk/issues/10117
    
    Implements a `CommitKVStore` which separates the concerns of state storage and state commitment according to [ADR-040](https://github.com/cosmos/cosmos-sdk/blob/eb7d939f86c6cd7b4218492364cdda3f649f06b5/docs/architecture/adr-040-storage-and-smt-state-commitments.md).
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch (see [PR Targeting](https://github.com/cosmos/cosmos-sdk/blob/master/CONTRIBUTING.md#pr-targeting))
    - [x] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building modules](https://github.com/cosmos/cosmos-sdk/blob/master/docs/building-modules) - n/a
    - [x] included the necessary unit and integration [tests](https://github.com/cosmos/cosmos-sdk/blob/master/CONTRIBUTING.md#testing)
    - [x] added a changelog entry to `CHANGELOG.md`
    - [x] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [x] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed 
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -200,6 +200,7 @@ Ref: https://keepachangelog.com/en/1.0.0/
 * [\#9848](https://github.com/cosmos/cosmos-sdk/pull/9848) ADR-040: Implement BadgerDB backend
 * [\#9851](https://github.com/cosmos/cosmos-sdk/pull/9851) ADR-040: Implement RocksDB backend
 * [\#10308](https://github.com/cosmos/cosmos-sdk/pull/10308) ADR-040: Implement DBConnection.Revert
+* [\#9892](https://github.com/cosmos/cosmos-sdk/pull/9892) ADR-040: KV Store with decoupled storage and state commitment
 
 
 ### Client Breaking Changes
---
+++ store/v2/smt/store_test.go
@@ -0,0 +1,42 @@
+package smt_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	store "github.com/cosmos/cosmos-sdk/store/v2/smt"
+	"github.com/lazyledger/smt"
+)
+
+func TestGetSetHasDelete(t *testing.T) {
+	s := store.NewStore(smt.NewSimpleMap(), smt.NewSimpleMap())
+
+	s.Set([]byte("foo"), []byte("bar"))
+	assert.Equal(t, []byte("bar"), s.Get([]byte("foo")))
+	assert.Equal(t, true, s.Has([]byte("foo")))
+	s.Delete([]byte("foo"))
+	assert.Equal(t, false, s.Has([]byte("foo")))
+
+	assert.Panics(t, func() { s.Get(nil) }, "Get(nil key) should panic")
+	assert.Panics(t, func() { s.Get([]byte{}) }, "Get(empty key) should panic")
+	assert.Panics(t, func() { s.Has(nil) }, "Has(nil key) should panic")
+	assert.Panics(t, func() { s.Has([]byte{}) }, "Has(empty key) should panic")
+	assert.Panics(t, func() { s.Set(nil, []byte("value")) }, "Set(nil key) should panic")
+	assert.Panics(t, func() { s.Set([]byte{}, []byte("value")) }, "Set(empty key) should panic")
+	assert.Panics(t, func() { s.Set([]byte("key"), nil) }, "Set(nil value) should panic")
+}
+
+func TestLoadStore(t *testing.T) {
+	nodes, values := smt.NewSimpleMap(), smt.NewSimpleMap()
+	s := store.NewStore(nodes, values)
+
+	s.Set([]byte{0}, []byte{0})
+	s.Set([]byte{1}, []byte{1})
+	s.Delete([]byte{1})
+	root := s.Root()
+
+	s = store.LoadStore(nodes, values, root)
+	assert.Equal(t, []byte{0}, s.Get([]byte{0}))
+	assert.False(t, s.Has([]byte{1}))
+}

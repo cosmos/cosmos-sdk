25a6e145dc241601c942ffd01f3c3ddf600536d1
Robert Zaremba <robert@zaremba.ch>
2021-10-28 20:11:54 +0200

    fix(cosmovisor): fix version when 'go install ...cosmovisor'

--- cosmovisor/cmd/cosmovisor/cmd/version.go
+++ cosmovisor/cmd/cosmovisor/cmd/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version represents Cosmovisor version value. Set during build
-var Version string
+var Version = "1.0.x"
 
 // VersionArgs is the strings that indicate a cosmovisor version command.
 var VersionArgs = []string{"version", "--version"}

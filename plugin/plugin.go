31b70e863193f4215297e22e4b2b0a77bdca21fd
Aaron Craelius <aaronc@users.noreply.github.com>
2021-10-28 13:33:42 -0400

    add docs

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -7,6 +7,13 @@ type Host interface {
 	// implements the protobuf interface name (cosmos_proto.implements_interface)
 	// is expected to register a golang interface implementation of type golangInterfaceType.
 	Expect(protoInterfaceName string, golangInterfaceType interface{})
-	Register(handlers ...interface{})
-	Resolve(target proto.Message, plugin interface{}) error
+
+	// Register registers plugin handler constructors which are functions taking a plugin target
+	// and returning a handler type. Ex: func(secp256k1.PubKey) crypto.PubKey.
+	Register(handlerConstructors ...interface{})
+
+	// Resolve resolves the handler for the provided target. Ex:
+	//  var pubKey crypto.PubKey
+	//  host.Resolve(&secp256k1.PubKey{}, &pubKey)
+	Resolve(target proto.Message, handler interface{}) error
 }

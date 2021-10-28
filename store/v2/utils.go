8efa2af35069081ac648bb0590b1f5f73558e7d9
Roy Crihfield <roy@manteia.ltd>
2021-10-28 18:55:29 +0800

    missing file

--- store/v2/utils.go
+++ store/v2/utils.go
@@ -1,7 +1,32 @@
 package types
 
 import (
+	"fmt"
 	v1 "github.com/cosmos/cosmos-sdk/store/types"
 )
 
 var PrefixEndBytes = v1.PrefixEndBytes
+
+// // Iterator over all the keys with a certain prefix in ascending order
+// func KVStorePrefixIterator(kvs KVStore, prefix []byte) Iterator {
+// 	return kvs.Iterator(prefix, PrefixEndBytes(prefix))
+// }
+
+// // Iterator over all the keys with a certain prefix in descending order.
+// func KVStoreReversePrefixIterator(kvs KVStore, prefix []byte) Iterator {
+// 	return kvs.ReverseIterator(prefix, PrefixEndBytes(prefix))
+// }
+
+func StoreKeyToType(key StoreKey) (typ StoreType, err error) {
+	switch key.(type) {
+	case *KVStoreKey:
+		typ = StoreTypePersistent
+	case *MemoryStoreKey:
+		typ = StoreTypeMemory
+	case *TransientStoreKey:
+		typ = StoreTypeTransient
+	default:
+		err = fmt.Errorf("unrecognized store key type: %T", key)
+	}
+	return
+}

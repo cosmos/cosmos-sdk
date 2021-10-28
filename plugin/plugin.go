7c50f99278d1aadbd30389f1f4a942dcfa91df76
Aaron Craelius <aaronc@users.noreply.github.com>
2021-10-28 10:17:43 -0400

    add docs

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -3,6 +3,9 @@ package plugin
 import "github.com/gogo/protobuf/proto"
 
 type Host interface {
+	// Expect tells the host to expect that every registered protobuf type which
+	// implements the protobuf interface name (cosmos_proto.implements_interface)
+	// is expected to register a golang interface implementation of type golangInterfaceType.
 	Expect(protoInterfaceName string, golangInterfaceType interface{})
 	Register(handlers ...interface{})
 	Resolve(target proto.Message, plugin interface{}) error

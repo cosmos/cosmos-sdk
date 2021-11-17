d1ea20ef712bf393cb1fa6cee989d588ba5819d0
Assaf Morami <assaf.morami@gmail.com>
2021-11-17 11:48:45 +0200

    move sign-doc outside of tx

--- simapp/simd/cmd/root.go
+++ simapp/simd/cmd/root.go
@@ -162,6 +162,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 		rpc.StatusCommand(),
 		queryCommand(),
 		txCommand(),
+		authcmd.GetSignDocCommand(),
 		keys.Commands(simapp.DefaultNodeHome),
 	)
 
@@ -207,7 +208,6 @@ func txCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(
-		authcmd.GetSignDocCommand(),
 		authcmd.GetSignCommand(),
 		authcmd.GetSignBatchCommand(),
 		authcmd.GetMultiSignCommand(),

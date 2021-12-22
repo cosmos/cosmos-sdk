3b3737e61646e65c94a7877f7a4a4add8306253c
likhita-809 <likhita@vitwit.com>
2021-12-22 13:59:59 +0530

    add genesis validation and related tests

--- x/group/genesis.go
+++ x/group/genesis.go
@@ -2,6 +2,10 @@ package group
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/group/errors"
+	"github.com/cosmos/cosmos-sdk/x/group/internal/math"
 )
 
 // NewGenesisState creates a new genesis state with default values.
@@ -10,6 +14,107 @@ func NewGenesisState() *GenesisState {
 }
 
 func (s GenesisState) Validate() error {
+	for _, f := range s.Groups {
+		groupId := f.GetGroupId()
+		if groupId == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "group's group id")
+		}
+		_, err := sdk.AccAddressFromBech32(f.GetAdmin())
+		if err != nil {
+			return sdkerrors.Wrap(err, "admin")
+		}
+		if _, err := math.NewNonNegativeDecFromString(f.GetTotalWeight()); err != nil {
+			return sdkerrors.Wrap(err, "total weight")
+		}
+		if f.GetVersion() == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "version")
+		}
+	}
+
+	for _, f := range s.GroupAccounts {
+		_, err := sdk.AccAddressFromBech32(f.Admin)
+		if err != nil {
+			return sdkerrors.Wrap(err, "group account admin")
+		}
+		_, err = sdk.AccAddressFromBech32(f.Address)
+		if err != nil {
+			return sdkerrors.Wrap(err, "group account address")
+		}
+		if f.GroupId == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "group account's group id")
+		}
+		if f.Version == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "group account version")
+		}
+		policy := f.GetDecisionPolicy()
+		if policy == nil {
+			return sdkerrors.Wrap(errors.ErrEmpty, "group account policy")
+		}
+		if err := policy.ValidateBasic(); err != nil {
+			return sdkerrors.Wrap(err, "group account policy")
+		}
+	}
+
+	for _, f := range s.GroupMembers {
+		if f.GetGroupId() == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "group member")
+		}
+		err := f.GetMember().ValidateBasic()
+		if err != nil {
+			return sdkerrors.Wrap(err, "group member")
+		}
+	}
+
+	for _, f := range s.Proposals {
+		if f.ProposalId == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "proposal id")
+		}
+		_, err := sdk.AccAddressFromBech32(f.Address)
+		if err != nil {
+			return sdkerrors.Wrap(err, "proposer group account address")
+		}
+		if f.GroupVersion == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "proposal group version")
+		}
+		if f.GroupAccountVersion == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "proposal group account version")
+		}
+		_, err = f.VoteState.GetYesCount()
+		if err != nil {
+			return sdkerrors.Wrap(err, "proposal VoteState yes count")
+		}
+		_, err = f.VoteState.GetNoCount()
+		if err != nil {
+			return sdkerrors.Wrap(err, "proposal VoteState no count")
+		}
+		_, err = f.VoteState.GetAbstainCount()
+		if err != nil {
+			return sdkerrors.Wrap(err, "proposal VoteState abstain count")
+		}
+		_, err = f.VoteState.GetVetoCount()
+		if err != nil {
+			return sdkerrors.Wrap(err, "proposal VoteState veto count")
+		}
+	}
+
+	for _, f := range s.Votes {
+		_, err := sdk.AccAddressFromBech32(f.GetVoter())
+		if err != nil {
+			return sdkerrors.Wrap(err, "voter")
+		}
+		if f.GetProposalId() == 0 {
+			return sdkerrors.Wrap(errors.ErrEmpty, "voter proposal id")
+		}
+		if f.GetChoice() == Choice_CHOICE_UNSPECIFIED {
+			return sdkerrors.Wrap(errors.ErrEmpty, "voter choice")
+		}
+		if _, ok := Choice_name[int32(f.GetChoice())]; !ok {
+			return sdkerrors.Wrap(errors.ErrInvalid, "choice")
+		}
+		if f.GetSubmittedAt().IsZero() {
+			return sdkerrors.Wrap(errors.ErrEmpty, "submitted at")
+		}
+	}
 	return nil
 }
 
---
+++ x/group/genesis_test.go
@@ -0,0 +1,506 @@
+package group
+
+import (
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+var (
+	memberPub  = secp256k1.GenPrivKey().PubKey()
+	accPub     = secp256k1.GenPrivKey().PubKey()
+	accAddr    = sdk.AccAddress(accPub.Address())
+	memberAddr = sdk.AccAddress(memberPub.Address())
+)
+
+func TestGenesisStateValidate(t *testing.T) {
+
+	submittedAt := time.Now().UTC()
+	timeout := submittedAt.Add(time.Second * 1).UTC()
+
+	groupAccount := &GroupAccountInfo{
+		Address:  accAddr.String(),
+		GroupId:  1,
+		Admin:    accAddr.String(),
+		Version:  1,
+		Metadata: []byte("account metadata"),
+	}
+	err := groupAccount.SetDecisionPolicy(&ThresholdDecisionPolicy{
+		Threshold: "1",
+		Timeout:   time.Second,
+	})
+
+	// create another group account to set invalid decision policy for testing
+	groupAccount2 := &GroupAccountInfo{
+		Address:  accAddr.String(),
+		GroupId:  1,
+		Admin:    accAddr.String(),
+		Version:  1,
+		Metadata: []byte("account metadata"),
+	}
+	err = groupAccount2.SetDecisionPolicy(&ThresholdDecisionPolicy{
+		Threshold: "1",
+		Timeout:   0,
+	})
+	require.NoError(t, err)
+
+	proposal := &Proposal{
+		ProposalId:          1,
+		Address:             accAddr.String(),
+		Metadata:            []byte("proposal metadata"),
+		GroupVersion:        1,
+		GroupAccountVersion: 1,
+		Proposers: []string{
+			memberAddr.String(),
+		},
+		SubmittedAt: submittedAt,
+		Status:      ProposalStatusClosed,
+		Result:      ProposalResultAccepted,
+		VoteState: Tally{
+			YesCount:     "1",
+			NoCount:      "0",
+			AbstainCount: "0",
+			VetoCount:    "0",
+		},
+		Timeout:        timeout,
+		ExecutorResult: ProposalExecutorResultSuccess,
+	}
+	err = proposal.SetMsgs([]sdk.Msg{&banktypes.MsgSend{
+		FromAddress: accAddr.String(),
+		ToAddress:   memberAddr.String(),
+		Amount:      sdk.Coins{sdk.NewInt64Coin("test", 100)},
+	}})
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name         string
+		genesisState GenesisState
+		expErr       bool
+	}{
+		{
+			"valid genesisState",
+			GenesisState{
+				GroupSeq:        2,
+				Groups:          []*GroupInfo{{GroupId: 1, Admin: accAddr.String(), Metadata: []byte("1"), Version: 1, TotalWeight: "1"}, {GroupId: 2, Admin: accAddr.String(), Metadata: []byte("2"), Version: 2, TotalWeight: "2"}},
+				GroupMembers:    []*GroupMember{{GroupId: 1, Member: &Member{Address: memberAddr.String(), Weight: "1", Metadata: []byte("member metadata")}}, {GroupId: 2, Member: &Member{Address: memberAddr.String(), Weight: "2", Metadata: []byte("member metadata")}}},
+				GroupAccountSeq: 1,
+				GroupAccounts:   []*GroupAccountInfo{groupAccount},
+				ProposalSeq:     1,
+				Proposals:       []*Proposal{proposal},
+				Votes:           []*Vote{{ProposalId: proposal.ProposalId, Voter: memberAddr.String(), SubmittedAt: submittedAt, Choice: Choice_CHOICE_YES}},
+			},
+			false,
+		},
+		{
+			"empty genesisState",
+			GenesisState{},
+			false,
+		},
+		{
+			"empty group id",
+			GenesisState{
+				Groups: []*GroupInfo{
+					{
+						GroupId:     0,
+						Admin:       accAddr.String(),
+						Metadata:    []byte("1"),
+						Version:     1,
+						TotalWeight: "1",
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group admin",
+			GenesisState{
+				Groups: []*GroupInfo{
+					{
+						GroupId:     1,
+						Admin:       "invalid admin",
+						Metadata:    []byte("1"),
+						Version:     1,
+						TotalWeight: "1",
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group version",
+			GenesisState{
+				Groups: []*GroupInfo{
+					{
+						GroupId:     1,
+						Admin:       accAddr.String(),
+						Metadata:    []byte("1"),
+						Version:     0,
+						TotalWeight: "1",
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group TotalWeight",
+			GenesisState{
+				Groups: []*GroupInfo{
+					{
+						GroupId:     1,
+						Admin:       accAddr.String(),
+						Metadata:    []byte("1"),
+						Version:     1,
+						TotalWeight: "-1",
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account address",
+			GenesisState{
+				GroupAccounts: []*GroupAccountInfo{
+					{
+						Address:  "invalid address",
+						GroupId:  1,
+						Admin:    accAddr.String(),
+						Version:  1,
+						Metadata: []byte("account metadata"),
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account admin",
+			GenesisState{
+				GroupAccounts: []*GroupAccountInfo{
+					{
+						Address:  accAddr.String(),
+						GroupId:  1,
+						Admin:    "invalid admin",
+						Version:  1,
+						Metadata: []byte("account metadata"),
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account's group id",
+			GenesisState{
+				GroupAccounts: []*GroupAccountInfo{
+					{
+						Address:  accAddr.String(),
+						GroupId:  0,
+						Admin:    accAddr.String(),
+						Version:  1,
+						Metadata: []byte("account metadata"),
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account version",
+			GenesisState{
+				GroupAccounts: []*GroupAccountInfo{
+					{
+						Address:  accAddr.String(),
+						GroupId:  1,
+						Admin:    accAddr.String(),
+						Version:  0,
+						Metadata: []byte("account metadata"),
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account policy",
+			GenesisState{
+				GroupAccounts: []*GroupAccountInfo{
+					{
+						Address:        accAddr.String(),
+						GroupId:        1,
+						Admin:          accAddr.String(),
+						Version:        1,
+						Metadata:       []byte("account metadata"),
+						DecisionPolicy: groupAccount2.DecisionPolicy,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group member's group id",
+			GenesisState{
+				GroupMembers: []*GroupMember{
+					{
+						GroupId: 0,
+						Member: &Member{
+							Address: memberAddr.String(),
+							Weight:  "1", Metadata: []byte("member metadata"),
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group member address",
+			GenesisState{
+				GroupMembers: []*GroupMember{
+					{
+						GroupId: 1,
+						Member: &Member{
+							Address: "invalid address",
+							Weight:  "1", Metadata: []byte("member metadata"),
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group member weight",
+			GenesisState{
+				GroupMembers: []*GroupMember{
+					{
+						GroupId: 0,
+						Member: &Member{
+							Address: memberAddr.String(),
+							Weight:  "0", Metadata: []byte("member metadata"),
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid proposal id",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          0,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 1,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account address of proposal",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             "invalid address",
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 1,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group version of proposal",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        0,
+						GroupAccountVersion: 1,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid group account version",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 0,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid VoteState with negative YesCount",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 1,
+						Proposers: []string{
+							memberAddr.String(),
+						},
+						SubmittedAt: submittedAt,
+						Status:      ProposalStatusClosed,
+						Result:      ProposalResultAccepted,
+						VoteState: Tally{
+							YesCount:     "-1",
+							NoCount:      "0",
+							AbstainCount: "0",
+							VetoCount:    "0",
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid VoteState with negative NoCount",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 1,
+						Proposers: []string{
+							memberAddr.String(),
+						},
+						SubmittedAt: submittedAt,
+						Status:      ProposalStatusClosed,
+						Result:      ProposalResultAccepted,
+						VoteState: Tally{
+							YesCount:     "0",
+							NoCount:      "-1",
+							AbstainCount: "0",
+							VetoCount:    "0",
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid VoteState with negative AbstainCount",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 1,
+						Proposers: []string{
+							memberAddr.String(),
+						},
+						SubmittedAt: submittedAt,
+						Status:      ProposalStatusClosed,
+						Result:      ProposalResultAccepted,
+						VoteState: Tally{
+							YesCount:     "0",
+							NoCount:      "0",
+							AbstainCount: "-1",
+							VetoCount:    "0",
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid VoteState with negative VetoCount",
+			GenesisState{
+				Proposals: []*Proposal{
+					{
+						ProposalId:          1,
+						Address:             accAddr.String(),
+						Metadata:            []byte("proposal metadata"),
+						GroupVersion:        1,
+						GroupAccountVersion: 1,
+						Proposers: []string{
+							memberAddr.String(),
+						},
+						SubmittedAt: submittedAt,
+						Status:      ProposalStatusClosed,
+						Result:      ProposalResultAccepted,
+						VoteState: Tally{
+							YesCount:     "0",
+							NoCount:      "0",
+							AbstainCount: "0",
+							VetoCount:    "-1",
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid voter",
+			GenesisState{
+				Votes: []*Vote{
+					{
+						ProposalId:  proposal.ProposalId,
+						Voter:       "invalid voter",
+						SubmittedAt: submittedAt,
+						Choice:      Choice_CHOICE_YES,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid proposal id",
+			GenesisState{
+				Votes: []*Vote{
+					{
+						ProposalId:  0,
+						Voter:       memberAddr.String(),
+						SubmittedAt: submittedAt,
+						Choice:      Choice_CHOICE_YES,
+					},
+				},
+			},
+			true,
+		},
+		{
+			"invalid choice",
+			GenesisState{
+				Votes: []*Vote{
+					{
+						ProposalId:  proposal.ProposalId,
+						Voter:       memberAddr.String(),
+						SubmittedAt: submittedAt,
+						Choice:      Choice_CHOICE_UNSPECIFIED,
+					},
+				},
+			},
+			true,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+
+			err := tc.genesisState.Validate()
+			if tc.expErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}

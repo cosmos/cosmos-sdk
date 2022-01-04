94cdfc68962c91c44afd4c0a9c1f436299bce0b0
Aaron Craelius <aaronc@users.noreply.github.com>
2022-01-04 16:12:36 -0500

    fix(errors): test failures

--- errors/abci_test.go
+++ errors/abci_test.go
@@ -143,7 +143,7 @@ func (s *abciTestSuite) TestABCIInfoStacktrace() {
 		},
 	}
 
-	const thisTestSrc = "github.com/cosmos/cosmos-sdk/types/errors.(*abciTestSuite).TestABCIInfoStacktrace"
+	const thisTestSrc = "github.com/cosmos/cosmos-sdk/errors.(*abciTestSuite).TestABCIInfoStacktrace"
 
 	for testName, tc := range cases {
 		_, _, log := ABCIInfo(tc.err, tc.debug)

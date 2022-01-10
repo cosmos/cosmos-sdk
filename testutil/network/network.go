1a16348afba3c9a30da45023e44cf017e9798820
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-01-10 12:29:57 -0500

    updates

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -80,21 +80,21 @@ type Config struct {
 	TimeoutCommit    time.Duration              // the consensus commitment timeout
 	ChainID          string                     // the network chain-id
 	NumValidators    int                        // the total number of validators to create and bond
-	Mnemonics        []string
-	BondDenom        string           // the staking bond denomination
-	MinGasPrices     string           // the minimum gas prices each validator will accept
-	AccountTokens    sdk.Int          // the amount of unique validator tokens (e.g. 1000node0)
-	StakingTokens    sdk.Int          // the amount of tokens each validator has available to stake
-	BondedTokens     sdk.Int          // the amount of tokens each validator stakes
-	PruningStrategy  string           // the pruning strategy each validator will have
-	EnableTMLogging  bool             // enable Tendermint logging to STDOUT
-	CleanupDir       bool             // remove base temporary directory during cleanup
-	SigningAlgo      string           // signing algorithm for keys
-	KeyringOptions   []keyring.Option // keyring configuration options
-	RPCAddress       string           // RPC listen address (including port)
-	APIAddress       string           // REST API listen address (including port)
-	GRPCAddress      string           // GRPC server listen address (including port)
-	PrintMnemonic    bool             // print the mnemonic of first validator as log output for testing
+	Mnemonics        []string                   // custom user-provided validator mnemonics
+	BondDenom        string                     // the staking bond denomination
+	MinGasPrices     string                     // the minimum gas prices each validator will accept
+	AccountTokens    sdk.Int                    // the amount of unique validator tokens (e.g. 1000node0)
+	StakingTokens    sdk.Int                    // the amount of tokens each validator has available to stake
+	BondedTokens     sdk.Int                    // the amount of tokens each validator stakes
+	PruningStrategy  string                     // the pruning strategy each validator will have
+	EnableTMLogging  bool                       // enable Tendermint logging to STDOUT
+	CleanupDir       bool                       // remove base temporary directory during cleanup
+	SigningAlgo      string                     // signing algorithm for keys
+	KeyringOptions   []keyring.Option           // keyring configuration options
+	RPCAddress       string                     // RPC listen address (including port)
+	APIAddress       string                     // REST API listen address (including port)
+	GRPCAddress      string                     // GRPC server listen address (including port)
+	PrintMnemonic    bool                       // print the mnemonic of first validator as log output for testing
 }
 
 // DefaultConfig returns a sane default configuration suitable for nearly all

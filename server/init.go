6efda6b1420859ad39ce3832fa1538641f0f4226
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-01-10 13:33:58 -0500

    updates

--- server/init.go
+++ server/init.go
@@ -14,12 +14,14 @@ func GenerateCoinKey(algo keyring.SignatureAlgo, cdc codec.Codec) (sdk.AccAddres
 	// generate a private key, with recovery phrase
 	k, secret, err := keyring.NewInMemory(cdc).NewMnemonic("name", keyring.English, sdk.GetConfig().GetFullBIP44Path(), keyring.DefaultBIP39Passphrase, algo)
 	if err != nil {
-		return sdk.AccAddress([]byte{}), "", err
+		return sdk.AccAddress{}, "", err
 	}
+
 	addr, err := k.GetAddress()
 	if err != nil {
 		return nil, "", err
 	}
+
 	return addr, secret, nil
 }
 
@@ -34,19 +36,17 @@ func GenerateSaveCoinKey(keybase keyring.Keyring, keyName string, overwrite bool
 
 	// ensure no overwrite
 	if !overwrite && exists {
-		return sdk.AccAddress([]byte{}), "", fmt.Errorf(
-			"key already exists, overwrite is disabled")
+		return sdk.AccAddress{}, "", fmt.Errorf("key already exists, overwrite is disabled")
 	}
 
-	// generate a private key, with recovery phrase
+	// remove the old key by name if it exists
 	if exists {
-		err = keybase.Delete(keyName)
-		if err != nil {
-			return sdk.AccAddress([]byte{}), "", fmt.Errorf(
-				"failed to overwrite key")
+		if err = keybase.Delete(keyName); err != nil {
+			return sdk.AccAddress{}, "", fmt.Errorf("failed to overwrite key")
 		}
 	}
 
+	// generate a private key, with recovery phrase
 	k, secret, err := keybase.NewMnemonic(keyName, keyring.English, sdk.GetConfig().GetFullBIP44Path(), keyring.DefaultBIP39Passphrase, algo)
 	if err != nil {
 		return sdk.AccAddress([]byte{}), "", err

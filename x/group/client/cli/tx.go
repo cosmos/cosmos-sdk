d0f6ba652c8e43bcec9313898e0ad0d4230f9a50
likhita-809 <likhita@vitwit.com>
2022-01-19 16:02:29 +0530

    fix tests

--- x/group/client/cli/tx.go
+++ x/group/client/cli/tx.go
@@ -298,7 +298,7 @@ func MsgCreateGroupWithPolicyCmd() *cobra.Command {
 			fmt.Sprintf(`Create a group with policy which is an aggregation of member accounts with associated weights and
 an administrator account. Note, the '--from' flag is ignored as it is implied from [admin].
 Members accounts can be given through a members JSON file that contains an array of members.
-If group-policy-as-admin flag is set to true, admin of the newly created group is set with group policy address itself.
+If group-policy-as-admin flag is set to true, admin of the newly created group and group policy is set with group policy address itself.
 
 Example:
 $ %s tx group create-group-with-policy [admin] [group-metadata] [group-policy-metadata] [members-json-file] \
@@ -380,6 +380,7 @@ where members.json contains:
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
+	cmd.Flags().Bool(FlagGroupPolicyAsAdmin, false, "Sets admin of the newly created group and group policy with group policy address itself when true")
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd

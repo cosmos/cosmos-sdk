f0f1f4cd14adc7178899a2a8b187a59affb62d75
likhita-809 <likhita@vitwit.com>
2022-01-19 18:54:18 +0530

    remove changes from events.proto

--- x/group/events.pb.go
+++ x/group/events.pb.go
@@ -115,61 +115,6 @@ func (m *EventUpdateGroup) GetGroupId() uint64 {
 	return 0
 }
 
-// EventCreateGroupWithPolicy is an event emitted when a group with policy is created.
-type EventCreateGroupWithPolicy struct {
-	// group_id is the unique ID of the newly created group with policy.
-	GroupId uint64 `protobuf:"varint,1,opt,name=group_id,json=groupId,proto3" json:"group_id,omitempty"`
-	// group_policy_address is the account address of the newly created group policy.
-	GroupPolicyAddress string `protobuf:"bytes,2,opt,name=group_policy_address,json=groupPolicyAddress,proto3" json:"group_policy_address,omitempty"`
-}
-
-func (m *EventCreateGroupWithPolicy) Reset()         { *m = EventCreateGroupWithPolicy{} }
-func (m *EventCreateGroupWithPolicy) String() string { return proto.CompactTextString(m) }
-func (*EventCreateGroupWithPolicy) ProtoMessage()    {}
-func (*EventCreateGroupWithPolicy) Descriptor() ([]byte, []int) {
-	return fileDescriptor_7879e051fb126fc0, []int{2}
-}
-func (m *EventCreateGroupWithPolicy) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *EventCreateGroupWithPolicy) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_EventCreateGroupWithPolicy.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *EventCreateGroupWithPolicy) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_EventCreateGroupWithPolicy.Merge(m, src)
-}
-func (m *EventCreateGroupWithPolicy) XXX_Size() int {
-	return m.Size()
-}
-func (m *EventCreateGroupWithPolicy) XXX_DiscardUnknown() {
-	xxx_messageInfo_EventCreateGroupWithPolicy.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_EventCreateGroupWithPolicy proto.InternalMessageInfo
-
-func (m *EventCreateGroupWithPolicy) GetGroupId() uint64 {
-	if m != nil {
-		return m.GroupId
-	}
-	return 0
-}
-
-func (m *EventCreateGroupWithPolicy) GetGroupPolicyAddress() string {
-	if m != nil {
-		return m.GroupPolicyAddress
-	}
-	return ""
-}
-
 // EventCreateGroupPolicy is an event emitted when a group policy is created.
 type EventCreateGroupPolicy struct {
 	// address is the account address of the group policy.
@@ -180,7 +125,7 @@ func (m *EventCreateGroupPolicy) Reset()         { *m = EventCreateGroupPolicy{}
 func (m *EventCreateGroupPolicy) String() string { return proto.CompactTextString(m) }
 func (*EventCreateGroupPolicy) ProtoMessage()    {}
 func (*EventCreateGroupPolicy) Descriptor() ([]byte, []int) {
-	return fileDescriptor_7879e051fb126fc0, []int{3}
+	return fileDescriptor_7879e051fb126fc0, []int{2}
 }
 func (m *EventCreateGroupPolicy) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -226,7 +171,7 @@ func (m *EventUpdateGroupPolicy) Reset()         { *m = EventUpdateGroupPolicy{}
 func (m *EventUpdateGroupPolicy) String() string { return proto.CompactTextString(m) }
 func (*EventUpdateGroupPolicy) ProtoMessage()    {}
 func (*EventUpdateGroupPolicy) Descriptor() ([]byte, []int) {
-	return fileDescriptor_7879e051fb126fc0, []int{4}
+	return fileDescriptor_7879e051fb126fc0, []int{3}
 }
 func (m *EventUpdateGroupPolicy) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -272,7 +217,7 @@ func (m *EventCreateProposal) Reset()         { *m = EventCreateProposal{} }
 func (m *EventCreateProposal) String() string { return proto.CompactTextString(m) }
 func (*EventCreateProposal) ProtoMessage()    {}
 func (*EventCreateProposal) Descriptor() ([]byte, []int) {
-	return fileDescriptor_7879e051fb126fc0, []int{5}
+	return fileDescriptor_7879e051fb126fc0, []int{4}
 }
 func (m *EventCreateProposal) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -318,7 +263,7 @@ func (m *EventVote) Reset()         { *m = EventVote{} }
 func (m *EventVote) String() string { return proto.CompactTextString(m) }
 func (*EventVote) ProtoMessage()    {}
 func (*EventVote) Descriptor() ([]byte, []int) {
-	return fileDescriptor_7879e051fb126fc0, []int{6}
+	return fileDescriptor_7879e051fb126fc0, []int{5}
 }
 func (m *EventVote) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -364,7 +309,7 @@ func (m *EventExec) Reset()         { *m = EventExec{} }
 func (m *EventExec) String() string { return proto.CompactTextString(m) }
 func (*EventExec) ProtoMessage()    {}
 func (*EventExec) Descriptor() ([]byte, []int) {
-	return fileDescriptor_7879e051fb126fc0, []int{7}
+	return fileDescriptor_7879e051fb126fc0, []int{6}
 }
 func (m *EventExec) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -403,7 +348,6 @@ func (m *EventExec) GetProposalId() uint64 {
 func init() {
 	proto.RegisterType((*EventCreateGroup)(nil), "cosmos.group.v1beta1.EventCreateGroup")
 	proto.RegisterType((*EventUpdateGroup)(nil), "cosmos.group.v1beta1.EventUpdateGroup")
-	proto.RegisterType((*EventCreateGroupWithPolicy)(nil), "cosmos.group.v1beta1.EventCreateGroupWithPolicy")
 	proto.RegisterType((*EventCreateGroupPolicy)(nil), "cosmos.group.v1beta1.EventCreateGroupPolicy")
 	proto.RegisterType((*EventUpdateGroupPolicy)(nil), "cosmos.group.v1beta1.EventUpdateGroupPolicy")
 	proto.RegisterType((*EventCreateProposal)(nil), "cosmos.group.v1beta1.EventCreateProposal")
@@ -414,28 +358,25 @@ func init() {
 func init() { proto.RegisterFile("cosmos/group/v1beta1/events.proto", fileDescriptor_7879e051fb126fc0) }
 
 var fileDescriptor_7879e051fb126fc0 = []byte{
-	// 321 bytes of a gzipped FileDescriptorProto
+	// 280 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x4c, 0xce, 0x2f, 0xce,
 	0xcd, 0x2f, 0xd6, 0x4f, 0x2f, 0xca, 0x2f, 0x2d, 0xd0, 0x2f, 0x33, 0x4c, 0x4a, 0x2d, 0x49, 0x34,
 	0xd4, 0x4f, 0x2d, 0x4b, 0xcd, 0x2b, 0x29, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x81,
 	0x28, 0xd1, 0x03, 0x2b, 0xd1, 0x83, 0x2a, 0x91, 0x92, 0x84, 0x88, 0xc6, 0x83, 0xd5, 0xe8, 0x43,
 	0x95, 0x80, 0x39, 0x4a, 0xba, 0x5c, 0x02, 0xae, 0x20, 0x03, 0x9c, 0x8b, 0x52, 0x13, 0x4b, 0x52,
 	0xdd, 0x41, 0xda, 0x84, 0x24, 0xb9, 0x38, 0xc0, 0xfa, 0xe3, 0x33, 0x53, 0x24, 0x18, 0x15, 0x18,
-	0x35, 0x58, 0x82, 0xd8, 0xc1, 0x7c, 0xcf, 0x14, 0xb8, 0xf2, 0xd0, 0x82, 0x14, 0x62, 0x94, 0x37,
-	0x33, 0x72, 0x49, 0xa1, 0x1b, 0x1f, 0x9e, 0x59, 0x92, 0x11, 0x90, 0x9f, 0x93, 0x99, 0x5c, 0x89,
-	0x47, 0xa7, 0x90, 0x17, 0x97, 0x08, 0x44, 0xaa, 0x00, 0xac, 0x34, 0x3e, 0x31, 0x25, 0xa5, 0x28,
-	0xb5, 0xb8, 0x58, 0x82, 0x49, 0x81, 0x51, 0x83, 0xd3, 0x49, 0xe2, 0xd2, 0x16, 0x5d, 0x98, 0x57,
-	0x1d, 0x21, 0x32, 0xc1, 0x25, 0x45, 0x99, 0x79, 0xe9, 0x41, 0x42, 0x60, 0x5d, 0x10, 0xf3, 0xa1,
-	0x32, 0x4a, 0x3e, 0x5c, 0x62, 0xe8, 0x8e, 0x80, 0x3a, 0xc0, 0x88, 0x8b, 0x1d, 0x66, 0x30, 0x23,
-	0x01, 0x83, 0x61, 0x0a, 0xe1, 0xa6, 0x21, 0x05, 0x01, 0x05, 0xa6, 0x99, 0x71, 0x09, 0x23, 0xb9,
-	0x2d, 0xa0, 0x28, 0xbf, 0x20, 0xbf, 0x38, 0x31, 0x47, 0x48, 0x9e, 0x8b, 0xbb, 0x00, 0xca, 0x46,
-	0x04, 0x0e, 0x17, 0x4c, 0xc8, 0x33, 0x45, 0x49, 0x87, 0x8b, 0x13, 0xac, 0x2f, 0x2c, 0xbf, 0x24,
-	0x95, 0x78, 0xd5, 0xae, 0x15, 0xa9, 0xc9, 0x04, 0x55, 0x3b, 0xd9, 0x9d, 0x78, 0x24, 0xc7, 0x78,
-	0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7,
-	0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x4a, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e, 0x72, 0x7e,
-	0x2e, 0x34, 0x19, 0x41, 0x29, 0xdd, 0xe2, 0x94, 0x6c, 0xfd, 0x0a, 0x48, 0xca, 0x4c, 0x62, 0x03,
-	0x27, 0x2d, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x8d, 0x39, 0xab, 0x4e, 0xb0, 0x02, 0x00,
-	0x00,
+	0x35, 0x58, 0x82, 0xd8, 0xc1, 0x7c, 0xcf, 0x14, 0xb8, 0xf2, 0xd0, 0x82, 0x14, 0x62, 0x94, 0xfb,
+	0x70, 0x89, 0xa1, 0x9b, 0x1e, 0x90, 0x9f, 0x93, 0x99, 0x5c, 0x29, 0x64, 0xc4, 0xc5, 0x9e, 0x98,
+	0x92, 0x52, 0x94, 0x5a, 0x5c, 0x0c, 0xd6, 0xc3, 0xe9, 0x24, 0x71, 0x69, 0x8b, 0x2e, 0xcc, 0xf5,
+	0x8e, 0x10, 0x99, 0xe0, 0x92, 0xa2, 0xcc, 0xbc, 0xf4, 0x20, 0x98, 0x42, 0xb8, 0x69, 0x48, 0x96,
+	0x53, 0x60, 0x9a, 0x19, 0x97, 0x30, 0x92, 0xdb, 0x02, 0x8a, 0xf2, 0x0b, 0xf2, 0x8b, 0x13, 0x73,
+	0x84, 0xe4, 0xb9, 0xb8, 0x0b, 0xa0, 0x6c, 0x84, 0x87, 0xb8, 0x60, 0x42, 0x9e, 0x29, 0x4a, 0x3a,
+	0x5c, 0x9c, 0x60, 0x7d, 0x61, 0xf9, 0x25, 0xa9, 0xc4, 0xab, 0x76, 0xad, 0x48, 0x4d, 0x26, 0xa8,
+	0xda, 0xc9, 0xee, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c,
+	0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2, 0x54, 0xd2, 0x33,
+	0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xa1, 0x11, 0x08, 0xa5, 0x74, 0x8b, 0x53, 0xb2,
+	0xf5, 0x2b, 0x20, 0x69, 0x22, 0x89, 0x0d, 0x1c, 0xa9, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff,
+	0x78, 0x22, 0x27, 0x30, 0x2a, 0x02, 0x00, 0x00,
 }
 
 func (m *EventCreateGroup) Marshal() (dAtA []byte, err error) {
@@ -486,41 +427,6 @@ func (m *EventUpdateGroup) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if m.GroupId != 0 {
-		i = encodeVarintEvents(dAtA, i, uint64(m.GroupId))
-		i--
-		dAtA[i] = 0x8
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *EventCreateGroupWithPolicy) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *EventCreateGroupWithPolicy) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *EventCreateGroupWithPolicy) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if len(m.GroupPolicyAddress) > 0 {
-		i -= len(m.GroupPolicyAddress)
-		copy(dAtA[i:], m.GroupPolicyAddress)
-		i = encodeVarintEvents(dAtA, i, uint64(len(m.GroupPolicyAddress)))
-		i--
-		dAtA[i] = 0x12
-	}
 	if m.GroupId != 0 {
 		i = encodeVarintEvents(dAtA, i, uint64(m.GroupId))
 		i--
@@ -704,22 +610,6 @@ func (m *EventUpdateGroup) Size() (n int) {
 	_ = l
 	if m.GroupId != 0 {
 		n += 1 + sovEvents(uint64(m.GroupId))
-	}
-	return n
-}
-
-func (m *EventCreateGroupWithPolicy) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.GroupId != 0 {
-		n += 1 + sovEvents(uint64(m.GroupId))
-	}
-	l = len(m.GroupPolicyAddress)
-	if l > 0 {
-		n += 1 + l + sovEvents(uint64(l))
 	}
 	return n
 }
@@ -909,107 +799,6 @@ func (m *EventUpdateGroup) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
-		default:
-			iNdEx = preIndex
-			skippy, err := skipEvents(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthEvents
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *EventCreateGroupWithPolicy) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowEvents
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: EventCreateGroupWithPolicy: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: EventCreateGroupWithPolicy: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field GroupId", wireType)
-			}
-			m.GroupId = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowEvents
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.GroupId |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field GroupPolicyAddress", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowEvents
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthEvents
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthEvents
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.GroupPolicyAddress = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipEvents(dAtA[iNdEx:])

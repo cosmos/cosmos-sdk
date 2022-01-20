55f8618af5dc5a50597b005f61b1d5933ab824c5
likhita-809 <likhita@vitwit.com>
2022-01-20 10:18:44 +0530

    try fix tests

--- x/group/client/testutil/tx.go
+++ x/group/client/testutil/tx.go
@@ -675,7 +675,7 @@ func (s *IntegrationTestSuite) TestTxCreateGroupWithPolicy() {
 
 	var commonFlags = []string{
 		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
-		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
 		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
 	}
 

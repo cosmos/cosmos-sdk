79583ecb1c4202d38a44dd1e7bb13c39d0a1b774
Sai Kumar <sai@vitwit.com>
2022-01-27 10:57:04 +0530

    chore: address the pr comments

--- store/v2/multi/migration.go
+++ store/v2/multi/migration.go
@@ -10,18 +10,37 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// MigrationFromIAVLStoreToSMTStore will migrate the complete state from iavl to smt
-func MigrationFromIAVLStoreToSMTStore(rs *v1Store.Store, rootStore *Store) error {
-	// Collect stores to snapshot (only IAVL stores are supported)
-	type namedStore struct {
-		*iavl.Store
-		name string
+// MigrateV2 will migrate the state from iavl to smt
+func MigrateV2(rs *v1Store.Store, rootStore *Store) error {
+	// make new smt store schema
+	if len(rootStore.schema) != 0 {
+		// schema already exists
+		return sdkerrors.Wrapf(sdkerrors.ErrLogic, "smt store already have schema")
 	}
-	var stores []namedStore
+
+	// set the schema to smt store
+	schemaWriter := prefixdb.NewPrefixWriter(rootStore.stateTxn, schemaPrefix)
+
 	for key := range rs.GetStores() {
 		switch store := rs.GetCommitKVStore(key).(type) {
 		case *iavl.Store:
-			stores = append(stores, namedStore{name: key.Name(), Store: store})
+			rootStore.schema[key.Name()] = types.StoreTypePersistent
+			err := schemaWriter.Set([]byte(key.Name()), []byte{byte(types.StoreTypePersistent)})
+			if err != nil {
+				return sdkerrors.Wrap(err, "error at set the store schema key values")
+			}
+
+			subStore, err := rootStore.getSubstore(key.Name())
+			if err != nil {
+				return err
+			}
+			// iterate all iavl tree node key/values
+			iterator := store.Iterator(nil, nil)
+			for ; iterator.Valid(); iterator.Next() {
+				// set the iavl key,values into smt node
+				subStore.Set(iterator.Key(), iterator.Value())
+			}
+
 		case *transient.Store, *mem.Store:
 			continue
 		default:
@@ -29,36 +48,8 @@ func MigrationFromIAVLStoreToSMTStore(rs *v1Store.Store, rootStore *Store) error
 		}
 	}
 
-	// make new smt store schema
-	if len(rootStore.schema) != 0 {
-		// schema is already exists
-		return sdkerrors.Wrapf(sdkerrors.ErrLogic, "smt store already have schema")
-	}
-
-	// set the schema to smt store
-	schemaWriter := prefixdb.NewPrefixWriter(rootStore.stateTxn, schemaPrefix)
-	for _, store := range stores {
-		rootStore.schema[store.name] = types.StoreTypePersistent
-		err := schemaWriter.Set([]byte(store.name), []byte{byte(types.StoreTypePersistent)})
-		if err != nil {
-			return sdkerrors.Wrap(err, "error at set the store schema key values")
-		}
-	}
-
-	// iterate through all iavl stores
-	for _, store := range stores {
-		subStore, err := rootStore.getSubstore(store.name)
-		if err != nil {
-			return err
-		}
-		// iterate all iavl tree node key/values
-		iterator := store.Iterator(nil, nil)
-		for ; iterator.Valid(); iterator.Next() {
-			// set the iavl key,values into smt node
-			subStore.Set(iterator.Key(), iterator.Value())
-		}
-	}
 	// commit the all key/values from iavl to smt tree (SMT Store)
 	rootStore.Commit()
+
 	return nil
 }

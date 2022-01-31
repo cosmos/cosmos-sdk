65f11013a5487b049d8e782536e1877845a848dc
Aaron Craelius <aaronc@users.noreply.github.com>
2022-01-31 10:57:30 -0500

    proto files

--- types/tx/signing/signing.pb.go
+++ types/tx/signing/signing.pb.go
@@ -25,6 +25,13 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // SignMode represents a signing mode with its own security guarantees.
+//
+// This enum should be considered a registry of all known sign modes
+// in the Cosmos ecosystem. Apps are not expected to support all known
+// sign modes. Apps that would like to support custom  sign modes are
+// encouraged to open a small PR against this file to add a new case
+// to this SignMode enum describing their sign mode so that different
+// apps have a consistent version of this enum.
 type SignMode int32
 
 const (

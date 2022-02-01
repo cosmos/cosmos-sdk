7c57e604a90e38445d495f29a34b10b0d2db8082
fdymylja <frojdi.dymylja@gmail.com>
2022-02-02 00:12:57 +0100

    fix: grpc server uses the sdk codec

--- server/grpc/server.go
+++ server/grpc/server.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/codec"
 	"net"
 	"time"
 
@@ -16,7 +17,10 @@ import (
 
 // StartGRPCServer starts a gRPC server on the given address.
 func StartGRPCServer(clientCtx client.Context, app types.Application, address string) (*grpc.Server, error) {
-	grpcSrv := grpc.NewServer()
+	grpcSrv := grpc.NewServer(
+		grpc.ForceServerCodec(codec.NewProtoCodec(clientCtx.InterfaceRegistry).GRPCCodec()),
+	)
+
 	app.RegisterGRPCServer(grpcSrv)
 	// reflection allows consumers to build dynamic clients that can write
 	// to any cosmos-sdk application without relying on application packages at compile time

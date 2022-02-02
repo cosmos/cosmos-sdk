894969bace71a77d92a84f45db58ee3ca40f4b10
Marko <marbar3778@yahoo.com>
2022-02-02 20:36:47 +0100

    chore: remove & add `since:` (#11094)
    * remove & add since
    
    * add since to gov
    
    * add since

--- api/cosmos/tx/signing/v1beta1/signing.pulsar.go
+++ api/cosmos/tx/signing/v1beta1/signing.pulsar.go
@@ -2705,6 +2705,13 @@ const (
 )
 
 // SignMode represents a signing mode with its own security guarantees.
+//
+// This enum should be considered a registry of all known sign modes
+// in the Cosmos ecosystem. Apps are not expected to support all known
+// sign modes. Apps that would like to support custom  sign modes are
+// encouraged to open a small PR against this file to add a new case
+// to this SignMode enum describing their sign mode so that different
+// apps have a consistent version of this enum.
 type SignMode int32
 
 const (
@@ -2722,6 +2729,8 @@ const (
 	// SignDocDirectAux. As opposed to SIGN_MODE_DIRECT, this sign mode does not
 	// require signers signing over other signers' `signer_info`. It also allows
 	// for adding Tips in transactions.
+	//
+	// Since: cosmos-sdk 0.46
 	SignMode_SIGN_MODE_DIRECT_AUX SignMode = 3
 	// SIGN_MODE_LEGACY_AMINO_JSON is a backwards compatibility mode which uses
 	// Amino JSON and will be removed in the future.

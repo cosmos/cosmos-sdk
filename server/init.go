845be8320c7983bb21307f968225e1b09d2370ca
Amaury <1293565+amaurym@users.noreply.github.com>
2022-02-02 11:02:01 +0100

    revert: "support custom mnemonics in in-process testing network (backport #10922) (#10935)" (#11083)
    This reverts commit 8bb7a968cc8181cb10c8c15f10665d9f14ffe73c.

--- server/init.go
+++ server/init.go
@@ -12,27 +12,16 @@ import (
 // phrase to recover the private key.
 func GenerateCoinKey(algo keyring.SignatureAlgo) (sdk.AccAddress, string, error) {
 	// generate a private key, with recovery phrase
-	info, secret, err := keyring.NewInMemory().NewMnemonic(
-		"name",
-		keyring.English,
-		sdk.GetConfig().GetFullBIP44Path(),
-		keyring.DefaultBIP39Passphrase,
-		algo)
+	info, secret, err := keyring.NewInMemory().NewMnemonic("name", keyring.English, sdk.GetConfig().GetFullBIP44Path(), keyring.DefaultBIP39Passphrase, algo)
 	if err != nil {
-		return sdk.AccAddress{}, "", err
+		return sdk.AccAddress([]byte{}), "", err
 	}
-
 	return sdk.AccAddress(info.GetPubKey().Address()), secret, nil
 }
 
 // GenerateSaveCoinKey returns the address of a public key, along with the secret
 // phrase to recover the private key.
-func GenerateSaveCoinKey(
-	keybase keyring.Keyring,
-	keyName, mnemonic string,
-	overwrite bool,
-	algo keyring.SignatureAlgo,
-) (sdk.AccAddress, string, error) {
+func GenerateSaveCoinKey(keybase keyring.Keyring, keyName string, overwrite bool, algo keyring.SignatureAlgo) (sdk.AccAddress, string, error) {
 	exists := false
 	_, err := keybase.Key(keyName)
 	if err == nil {
@@ -41,41 +30,22 @@ func GenerateSaveCoinKey(
 
 	// ensure no overwrite
 	if !overwrite && exists {
-		return sdk.AccAddress{}, "", fmt.Errorf("key already exists, overwrite is disabled")
+		return sdk.AccAddress([]byte{}), "", fmt.Errorf(
+			"key already exists, overwrite is disabled")
 	}
 
-	// remove the old key by name if it exists
+	// generate a private key, with recovery phrase
 	if exists {
-		if err := keybase.Delete(keyName); err != nil {
-			return sdk.AccAddress{}, "", fmt.Errorf("failed to overwrite key")
+		err = keybase.Delete(keyName)
+		if err != nil {
+			return sdk.AccAddress([]byte{}), "", fmt.Errorf(
+				"failed to overwrite key")
 		}
 	}
 
-	var (
-		info   keyring.Info
-		secret string
-	)
-
-	if mnemonic != "" {
-		secret = mnemonic
-		info, err = keybase.NewAccount(
-			keyName,
-			mnemonic,
-			keyring.DefaultBIP39Passphrase,
-			sdk.GetConfig().GetFullBIP44Path(),
-			algo,
-		)
-	} else {
-		info, secret, err = keybase.NewMnemonic(
-			keyName,
-			keyring.English,
-			sdk.GetConfig().GetFullBIP44Path(),
-			keyring.DefaultBIP39Passphrase,
-			algo,
-		)
-	}
+	info, secret, err := keybase.NewMnemonic(keyName, keyring.English, sdk.GetConfig().GetFullBIP44Path(), keyring.DefaultBIP39Passphrase, algo)
 	if err != nil {
-		return sdk.AccAddress{}, "", err
+		return sdk.AccAddress([]byte{}), "", err
 	}
 
 	return sdk.AccAddress(info.GetPubKey().Address()), secret, nil

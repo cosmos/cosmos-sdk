05420a752f5ce755ebdb3edde7c09ba75c6f5154
Aaron Craelius <aaronc@users.noreply.github.com>
2022-02-04 16:31:25 -0500

    docs

---
+++ types/apicompat/docs.go
@@ -0,0 +1,14 @@
+// Package apicompat contains gogo protobuf generated code for protobuf files
+// which are primarily built into the api module with protocgen-go-pulsar.
+// You should generally prefer using the files in the api module and switch
+// over to the new generated code when possible. If you need to reference
+// any of these newer types from proto files which are still generated
+// with gogo proto, you can use a buf.gen.yaml template with buf managed mode
+// to set the correct proto path:
+//  version: v1
+//  managed:
+//   enabled: true
+//   go_package_prefix:
+//     override:
+//       buf.build/cosmos/cosmos-sdk: github.com/cosmos/cosmos-sdk/types/apicompat
+package apicompat

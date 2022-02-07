b4ea591c6526b5161436e600dd3056061488ae80
Robert Zaremba <robert@zaremba.ch>
2022-02-07 21:45:20 +0100

    update docs

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,15 +2,19 @@
 
 #== Requirements ==
 #
-### https://github.com/grpc-ecosystem/grpc-gateway#installation
+## All protoc dependencies must be installed not in the module scope
+## https://github.com/grpc-ecosystem/grpc-gateway#installation
+# cd ~
 # go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 # go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 # go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.16.0
+# go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
+# go get github.com/regen-network/cosmos-proto@latest # doesn't work in install mode
 #
-## make sure you are outside of module (eg `cd ~`) - the command below doesn't work with `go install`:
-# go get github.com/regen-network/cosmos-proto@latest
-#
-## Install latest buf (v1.0.0-rc11 or later)
+## make sure your `go env GOPATH` is in the `$PATH`
+## Install:
+## + latest buf (v1.0.0-rc11 or later)
+## + protobuf v3
 
 set -eo pipefail
 
@@ -22,7 +26,7 @@ protoc_install_gocosmos() {
   # So the command below issues a warning and we are muting it for now.
   #
   # Installing plugins must be done outside of the module
-  (cd ~; go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@v0.3.1 2> /dev/null)
+  (cd ~; go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@@v0.3.1 2> /dev/null)
 }
 
 protoc_install_gocosmos
--- scripts/protocgen2.sh
+++ scripts/protocgen2.sh
@@ -3,8 +3,8 @@
 set -eo pipefail
 
 protoc_install_gopulsar() {
-  go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest 2>/dev/null
-  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest 2>/dev/null
+  go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
+  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 }
 
 protoc_install_gopulsar

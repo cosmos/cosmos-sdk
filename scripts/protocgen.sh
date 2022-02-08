b41f09f7635947b2ee7e8c2a10704626ba341eff
Robert Zaremba <robert@zaremba.ch>
2022-02-08 12:24:36 +0100

    update docs

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,25 +2,25 @@
 
 #== Requirements ==
 #
+## make sure your `go env GOPATH` is in the `$PATH`
+## Install:
+## + latest buf (v1.0.0-rc11 or later)
+## + protobuf v3
+#
 ## All protoc dependencies must be installed not in the module scope
-## https://github.com/grpc-ecosystem/grpc-gateway#installation
+## currently we must use grpc-gateway v1
 # cd ~
 # go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 # go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 # go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.16.0
 # go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
 # go get github.com/regen-network/cosmos-proto@latest # doesn't work in install mode
-#
-## make sure your `go env GOPATH` is in the `$PATH`
-## Install:
-## + latest buf (v1.0.0-rc11 or later)
-## + protobuf v3
 
 set -eo pipefail
 
 protoc_install_gocosmos() {
   echo "Installing protobuf gocosmos plugin"
-  # we should use go install, but regen-network/cosmos-proto uses contains
+  # we should use go install, but regen-network/cosmos-proto contains
   # replace directives. It must not contain directives that would cause
   # it to be interpreted differently than if it were the main module.
   # So the command below issues a warning and we are muting it for now.

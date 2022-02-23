ca1b1851765f04d51c24be66b316718a26097ed5
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-02-23 15:44:17 -0500

    updates

--- server/start.go
+++ server/start.go
@@ -88,13 +88,8 @@ For '--pruning' the options are as follows:
 
 default: the last 100 states are kept in addition to every 500th state; pruning at 10 block intervals
 nothing: all historic states will be saved, nothing will be deleted (i.e. archiving node)
-<<<<<<< HEAD
-everything: all saved states will be deleted, storing only the current state; pruning at 10 block intervals
-custom: allow pruning options to be manually specified through 'pruning-keep-recent', 'pruning-keep-every', and 'pruning-interval'
-=======
 everything: all saved states will be deleted, storing only the current and previous state; pruning at 10 block intervals
-custom: allow pruning options to be manually specified through 'pruning-keep-recent' and 'pruning-interval'
->>>>>>> 75bcf47f1 (refactor: prune everything (#11177))
+custom: allow pruning options to be manually specified through 'pruning-keep-recent', 'pruning-keep-every', and 'pruning-interval'
 
 Node halting configurations exist in the form of two flags: '--halt-height' and '--halt-time'. During
 the ABCI Commit phase, the node will check if the current block height is greater than or equal to

06d812843da27ae38ee0be51d4806e49a52f404d
atheesh <atheesh@vitwit.com>
2022-03-02 18:45:52 +0530

    fix test

--- x/group/client/testutil/tx.go
+++ x/group/client/testutil/tx.go
@@ -208,7 +208,7 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.votedMember = &group.Member{
 		Address:  val.Address.String(),
 		Weight:   memberWeight,
-		Metadata: []byte(validMetadata),
+		Metadata: validMetadata,
 	}
 }
 

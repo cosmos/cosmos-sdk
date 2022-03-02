090571bba47c714c7acbc3fd51fb0e189d7733b4
atheesh <atheesh@vitwit.com>
2022-03-02 18:05:26 +0530

    fix conflicts

--- x/group/keeper/tally.go
+++ x/group/keeper/tally.go
@@ -8,6 +8,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/group/internal/orm"
 )
 
+// Tally is a function that tallies a proposal by iterating through its votes,
+// and returns the tally result without modifying the proposal or any state.
 func (q Keeper) Tally(ctx sdk.Context, p group.Proposal, groupId uint64) (group.TallyResult, error) {
 	// If proposal has already been tallied and updated, then its status is
 	// closed, in which case we just return the previously stored result.
@@ -34,8 +36,18 @@ func (q Keeper) Tally(ctx sdk.Context, p group.Proposal, groupId uint64) (group.
 		}
 
 		var member group.GroupMember
-		err := q.groupMemberTable.GetOne(ctx.KVStore(q.key), orm.PrimaryKey(&group.GroupMember{GroupId: groupId, Member: &group.Member{Address: vote.Voter}}), &member)
-		if err != nil {
+		err := q.groupMemberTable.GetOne(ctx.KVStore(q.key), orm.PrimaryKey(&group.GroupMember{
+			GroupId: groupId,
+			Member:  &group.Member{Address: vote.Voter},
+		}), &member)
+
+		switch {
+		case sdkerrors.ErrNotFound.Is(err):
+			// If the member left the group after voting, then we simply skip the
+			// vote.
+			continue
+		case err != nil:
+			// For any other errors, we stop and return the error.
 			return group.TallyResult{}, err
 		}
 

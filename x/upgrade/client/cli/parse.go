d2d3ee2ff583f5715cdb6da8b1ffd547d17658a0
Callum Waters <cmwaters19@gmail.com>
2022-03-03 12:36:24 +0100

    remove redundant parse command

--- x/upgrade/client/cli/parse.go
+++ x/upgrade/client/cli/parse.go
@@ -1,50 +1,11 @@
 package cli
 
 import (
-	"encoding/json"
-	"os"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/client/cli"
 	gov "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/spf13/cobra"
 )
-
-// softwareUpgradeProposal defines a software upgrade proposal.
-type softwareUpgradeProposal struct {
-	Plan     json.RawMessage
-	Metadata []byte
-	Deposit  string
-}
-
-func parseSubmitSoftwareUpgradeProposal(cdc codec.Codec, path string) (*types.Plan, []byte, sdk.Coins, error) {
-	var proposal softwareUpgradeProposal
-	var plan types.Plan
-
-	contents, err := os.ReadFile(path)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	err = json.Unmarshal(contents, &proposal)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	err = cdc.UnmarshalJSON(proposal.Plan, &plan)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	deposit, err := sdk.ParseCoinsNormalized(proposal.Deposit)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	return &plan, proposal.Metadata, deposit, nil
-}
 
 func parseArgsToContent(cmd *cobra.Command, name string) (gov.Content, error) {
 	title, err := cmd.Flags().GetString(cli.FlagTitle)

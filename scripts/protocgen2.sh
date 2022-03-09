e5b6f3a6c250df9094fede0f69e0194f5561f871
technicallyty <48813565+tytech3@users.noreply.github.com>
2022-03-09 13:17:49 -0800

    chore: wipe generated code before buf gen

--- scripts/protocgen2.sh
+++ scripts/protocgen2.sh
@@ -9,6 +9,9 @@ protoc_install_gopulsar() {
 
 protoc_install_gopulsar
 
+echo "Cleaning API directory"
+(cd api; find ./ -type f \( -iname \*.pulsar.go -o -iname \*.pb.go -o -iname \*.cosmos_orm.go -o -iname \*.pb.gw.go \) -delete; find . -empty -type d -delete; cd ..)
+
 echo "Generating API module"
 (cd proto; buf generate --template buf.gen.pulsar.yaml)
 

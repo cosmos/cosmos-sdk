5a0836fbb4036c39d66ce265ac17248347603651
blushi <marie.gauthier63@gmail.com>
2022-03-11 11:26:59 +0100

    Update go doc

--- codec/legacy/doc.go
+++ codec/legacy/doc.go
@@ -2,5 +2,5 @@
 // still used in several places within the SDK. This package is intended
 // to be removed at some point in the future when the global Cdc is removed.
 // It also contains a util function RegisterAminoMsg that checks a msg name length
-// and registers the concrete msg type with amino.
+// before registering the concrete msg type with amino.
 package legacy

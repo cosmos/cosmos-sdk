282fb2c27661872f31c4d1c582eb2e9126896e23
marbar3778 <marbar3778@yahoo.com>
2022-03-26 23:02:05 +0100

    revert comment removal

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+#== Requirements ==
+ #
+ ## make sure your `go env GOPATH` is in the `$PATH`
+ ## Install:
+ ## + latest buf (v1.0.0-rc11 or later)
+ ## + protobuf v3
+ #
+ ## All protoc dependencies must be installed not in the module scope
+ ## currently we must use grpc-gateway v1
+ # cd ~
+ # go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+ # go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+ # go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.16.0
+ # go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
+ # go get github.com/regen-network/cosmos-proto@latest # doesn't work in install mode
+
 set -eo pipefail
 
 protoc_install_gocosmos() {

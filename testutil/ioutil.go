5613d2adb7dbb56f98380d13a05760ca710c0a5f
Roman Akhtariev <ackhtariev@gmail.com>
2022-03-30 23:10:45 +0000

    use os.MkdirTemp for creating temp dir

--- testutil/ioutil.go
+++ testutil/ioutil.go
@@ -3,7 +3,6 @@ package testutil
 import (
 	"bytes"
 	"io"
-	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
@@ -77,10 +76,11 @@ func TempFile(t testing.TB) *os.File {
 // GetTempDir returns a writable temporary director for the test to use.
 func GetTempDir(t testing.TB) string {
 	t.Helper()
-	// ioutil.TempDir() is used instead of testing.T.TempDir()
-	// see https://github.com/cosmos/cosmos-sdk/pull/8475 for
+	// os.MkDir() is used instead of testing.T.TempDir()
+	// see https://github.com/cosmos/cosmos-sdk/pull/8475 and
+	// https://github.com/cosmos/cosmos-sdk/pull/10341 for
 	// this change's rationale.
-	tempdir, err := ioutil.TempDir("", "")
+	tempdir, err := os.MkdirTemp("", "")
 	require.NoError(t, err)
 	t.Cleanup(func() { _ = os.RemoveAll(tempdir) })
 	return tempdir

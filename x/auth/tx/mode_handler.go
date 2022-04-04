f17f1e0f9cdb19662b0ea3bcb275e4eae6cb87c6
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-04-04 13:21:51 +0200

    feat: EIP191 sign mode

--- x/auth/tx/mode_handler.go
+++ x/auth/tx/mode_handler.go
@@ -12,6 +12,7 @@ var DefaultSignModes = []signingtypes.SignMode{
 	signingtypes.SignMode_SIGN_MODE_DIRECT,
 	signingtypes.SignMode_SIGN_MODE_DIRECT_AUX,
 	signingtypes.SignMode_SIGN_MODE_LEGACY_AMINO_JSON,
+	signingtypes.SignMode_SIGN_MODE_EIP_191,
 }
 
 // makeSignModeHandler returns the default protobuf SignModeHandler supporting

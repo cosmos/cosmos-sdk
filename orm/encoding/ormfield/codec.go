1486a669b7b3ce0b2eff4ec0b05cfdda2c922ae6
Aaron Craelius <aaron@regen.network>
2022-04-05 15:48:58 -0400

    fix(orm): add additional checks on invalid tables (#11387)
    ## Description
    
    Adds checks for two types of errors in table definitions
    * defining a key on an `optional` field (haven't found a way to test unfortunately because a test case will break the code generator)
    * defining a trivial unique key, essentially a unique key which contains all the fields in the primary key and is redundant
    
    
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch (see [PR Targeting](https://github.com/cosmos/cosmos-sdk/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building modules](https://github.com/cosmos/cosmos-sdk/blob/master/docs/building-modules)
    - [ ] included the necessary unit and integration [tests](https://github.com/cosmos/cosmos-sdk/blob/master/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed 
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- orm/encoding/ormfield/codec.go
+++ orm/encoding/ormfield/codec.go
@@ -55,14 +55,18 @@ var (
 // non-terminal segment of a multi-part key.
 func GetCodec(field protoreflect.FieldDescriptor, nonTerminal bool) (Codec, error) {
 	if field == nil {
-		return nil, ormerrors.UnsupportedKeyField.Wrap("nil field")
+		return nil, ormerrors.InvalidKeyField.Wrap("nil field")
 	}
 	if field.IsList() {
-		return nil, ormerrors.UnsupportedKeyField.Wrapf("repeated field %s", field.FullName())
+		return nil, ormerrors.InvalidKeyField.Wrapf("repeated field %s", field.FullName())
 	}
 
 	if field.ContainingOneof() != nil {
-		return nil, ormerrors.UnsupportedKeyField.Wrapf("oneof field %s", field.FullName())
+		return nil, ormerrors.InvalidKeyField.Wrapf("oneof field %s", field.FullName())
+	}
+
+	if field.HasOptionalKeyword() {
+		return nil, ormerrors.InvalidKeyField.Wrapf("optional field %s", field.FullName())
 	}
 
 	switch field.Kind() {
@@ -102,9 +106,9 @@ func GetCodec(field protoreflect.FieldDescriptor, nonTerminal bool) (Codec, erro
 		case durationFullName:
 			return DurationCodec{}, nil
 		default:
-			return nil, ormerrors.UnsupportedKeyField.Wrapf("%s of type %s", field.FullName(), msgName)
+			return nil, ormerrors.InvalidKeyField.Wrapf("%s of type %s", field.FullName(), msgName)
 		}
 	default:
-		return nil, ormerrors.UnsupportedKeyField.Wrapf("%s of kind %s", field.FullName(), field.Kind())
+		return nil, ormerrors.InvalidKeyField.Wrapf("%s of kind %s", field.FullName(), field.Kind())
 	}
 }

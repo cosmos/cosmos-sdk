bc2431d8ca7d6eeba35a41935406190d3ca61a2d
Amaury <1293565+amaurym@users.noreply.github.com>
2022-04-05 17:50:28 +0200

    Update x/group/client/cli/tx.go
    Co-authored-by: Marie Gauthier <marie.gauthier63@gmail.com>

--- x/group/client/cli/tx.go
+++ x/group/client/cli/tx.go
@@ -468,7 +468,7 @@ func MsgUpdateGroupPolicyAdminCmd() *cobra.Command {
 // MsgUpdateGroupPolicyDecisionPolicyCmd creates a CLI command for Msg/UpdateGroupPolicyDecisionPolicy.
 func MsgUpdateGroupPolicyDecisionPolicyCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-group-decision-policy [admin] [group-policy-account] [decision-policy]",
+		Use:   "update-group-policy-decision-policy [admin] [group-policy-account] [decision-policy]",
 		Short: "Update a group policy's decision policy",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {

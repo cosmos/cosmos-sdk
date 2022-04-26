85aa4a59ff97d5ec6134e545bfbdbba5d22f2b8d
Julien Robert <julien@rbrt.fr>
2022-04-26 23:06:48 +0200

    docs: fix master to main redirect

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,9 @@
 export default ({ router }) => {
   router.addRoutes([
-    { path: '/main/spec/*', redirect: '/main/modules/' },
-    { path: '/main/spec/governance/', redirect: '/main/modules/gov/' },
-    { path: '/v0.41/', redirect: '/v0.42/' },
-    { path: '/v0.43/', redirect: '/v0.44/' },
-    { path: '/master/', redirect: '/main/' },
-  ])
-}
+    { path: "/main/spec/*", redirect: "/modules/" },
+    { path: "/main/spec/governance/", redirect: "/modules/gov/" },
+    { path: "/v0.41/", redirect: "/v0.42/" }, // TODO to fix
+    { path: "/v0.43/", redirect: "/v0.44/" }, // TODO to fix
+    { path: "/master/", redirect: "/" },
+  ]);
+};

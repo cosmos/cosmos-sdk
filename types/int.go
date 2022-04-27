ae3373c0098a0c9240c759be92e9854f8fc48668
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-04-27 09:24:08 -0400

    updates

--- types/int.go
+++ types/int.go
@@ -1,5 +1,19 @@
 package types
 
+import (
+	mathmod "github.com/cosmos/cosmos-sdk/math"
+)
+
+// Type aliases to the SDK's math sub-module
+//
+// Deprecated: Functionality of this package has been moved to it's own module:
+// github.com/cosmos/cosmos-sdk/math
+//
+// Please use the above module instead of this package.
+var (
+	Int = mathmod.Int
+)
+
 func (ip IntProto) String() string {
 	return ip.Int.String()
 }

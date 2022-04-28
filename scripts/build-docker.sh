0d53ac73701d215083e5230d8e6aa2bf612374ec
Jacob Gadikian <jacobgadikian@gmail.com>
2022-04-29 01:05:51 +0700

    Update build-docker.sh

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,6 +2,5 @@
 # use this script from the repository root to build all docker images in the repository in sequence, beginning with the development environment, also located in the repository root
 docker build -t ghcr.io/cosmos/cosmos-sdk:latest -
 docker build --dockerfile contrib/images/proto-builder -t ghcr.io/cosmos/cosmos-sdk/proto-builder:latest -
-docker build --dockerfile contrib/images/simapp -t ghcr.io/cosmos/cosmos-sdk/simapp:latest -
 docker build --dockerfile contrib/images/simd-dlv -t ghcr.io/cosmos/cosmos-sdk/simd-dlv:latest contrib/images/simd-dlv
 docker build --dockerfile contrib/images/simd-env -t ghcr.io/cosmos/cosmos-sdk/simd-env:latest contrib/images/simd-env

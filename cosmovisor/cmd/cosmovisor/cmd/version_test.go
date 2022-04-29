2266cd6703efdb00d5068e83d1ce780dfab98a87
Julien Robert <julien@rbrt.fr>
2022-04-29 16:05:13 +0200

    fix test

--- cosmovisor/cmd/cosmovisor/cmd/version_test.go
+++ cosmovisor/cmd/cosmovisor/cmd/version_test.go
@@ -1,16 +1,21 @@
 package cmd
 
 import (
+	"context"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/cosmos/cosmos-sdk/cosmovisor"
+	"github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestVersionCommand_Error(t *testing.T) {
-	output, err := cli.ExecTestCLICmd(client.Context{}, rootCmd, []string{"version"})
-	require.Error(t, err)
+	logger := cosmovisor.NewLogger()
 
-	require.Contains(t, output.String(), "DAEMON_NAME is not set")
+	rootCmd.SetArgs([]string{"version"})
+	_, out := testutil.ApplyMockIO(rootCmd)
+	ctx := context.WithValue(context.Background(), cosmovisor.LoggerKey, logger)
+
+	require.Error(t, rootCmd.ExecuteContext(ctx))
+	require.Contains(t, out.String(), "DAEMON_NAME is not set")
 }

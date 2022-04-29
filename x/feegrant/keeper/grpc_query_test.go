97eaf50f778df77dd7a4627de9c15ca4f7b5cef5
atheeshp <59333759+atheeshp@users.noreply.github.com>
2022-04-29 16:23:18 +0530

    fix: grants by granter pagination `total` (#11813)
    Closes: #11812
    
    ---
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch (see [PR Targeting](https://github.com/cosmos/cosmos-sdk/blob/main/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building modules](https://github.com/cosmos/cosmos-sdk/blob/main/docs/building-modules)
    - [ ] included the necessary unit and integration [tests](https://github.com/cosmos/cosmos-sdk/blob/main/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)
    
    (cherry picked from commit 8dfc205731c7a35964fa1a4e0e8b99bbb6eb5bd1)

--- x/feegrant/keeper/grpc_query_test.go
+++ x/feegrant/keeper/grpc_query_test.go
@@ -59,7 +59,7 @@ func (suite *KeeperTestSuite) TestFeeAllowance() {
 			},
 			false,
 			func() {
-				grantFeeAllowance(suite)
+				suite.grantFeeAllowance(suite.addrs[0], suite.addrs[1])
 			},
 			func(response *feegrant.QueryAllowanceResponse) {
 				suite.Require().Equal(response.Allowance.Granter, suite.addrs[0].String())
@@ -124,7 +124,7 @@ func (suite *KeeperTestSuite) TestFeeAllowances() {
 			},
 			false,
 			func() {
-				grantFeeAllowance(suite)
+				suite.grantFeeAllowance(suite.addrs[0], suite.addrs[1])
 			},
 			func(resp *feegrant.QueryAllowancesResponse) {
 				suite.Require().Equal(len(resp.Allowances), 1)
@@ -190,7 +190,7 @@ func (suite *KeeperTestSuite) TestFeeAllowancesByGranter() {
 			},
 			false,
 			func() {
-				grantFeeAllowance(suite)
+				suite.grantFeeAllowance(suite.addrs[0], suite.addrs[1])
 			},
 			func(resp *feegrant.QueryAllowancesByGranterResponse) {
 				suite.Require().Equal(len(resp.Allowances), 1)
@@ -214,9 +214,9 @@ func (suite *KeeperTestSuite) TestFeeAllowancesByGranter() {
 	}
 }
 
-func grantFeeAllowance(suite *KeeperTestSuite) {
+func (suite *KeeperTestSuite) grantFeeAllowance(granter, grantee sdk.AccAddress) {
 	exp := suite.sdkCtx.BlockTime().AddDate(1, 0, 0)
-	err := suite.app.FeeGrantKeeper.GrantAllowance(suite.sdkCtx, suite.addrs[0], suite.addrs[1], &feegrant.BasicAllowance{
+	err := suite.app.FeeGrantKeeper.GrantAllowance(suite.sdkCtx, granter, grantee, &feegrant.BasicAllowance{
 		SpendLimit: sdk.NewCoins(sdk.NewInt64Coin("atom", 555)),
 		Expiration: &exp,
 	})

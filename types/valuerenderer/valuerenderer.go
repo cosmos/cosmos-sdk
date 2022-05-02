b17fac73e8290e2165566915c521809fe4be0dbb
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-05-02 15:25:59 +0200

    add coins tests

--- types/valuerenderer/valuerenderer.go
+++ types/valuerenderer/valuerenderer.go
@@ -3,9 +3,13 @@ package valuerenderer
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
+
+const thousandSeparator string = "'"
 
 // ValueRenderer defines an interface to produce formated output for Int,Dec,Coin types as well as parse a string to Coin or Uint.
 type ValueRenderer interface {
@@ -19,27 +23,112 @@ var _ ValueRenderer = DefaultValueRenderer{}
 
 func (r DefaultValueRenderer) Format(ctx context.Context, v interface{}) ([]string, error) {
 	switch v := v.(type) {
-	case int:
-		return r.formatNumber(ctx, strconv.Itoa(v))
-
+	case sdk.Int:
+		r, err := formatInteger(v.String())
+		return []string{r}, err
+	case sdk.Dec:
+		r, err := formatDecimal(v.String())
+		return []string{r}, err
+	case sdk.Coin:
+		var metadata bank.Metadata
+		// TODO get metadata from ctx and state.
+		r, err := formatCoin(v, metadata)
+		return []string{r}, err
 	default:
 		return nil, fmt.Errorf("value renderers cannot format value %s of type %T", v, v)
 	}
 }
 
 func (r DefaultValueRenderer) Parse(context.Context, []string) (interface{}, error) {
-	panic("TODO")
+	panic("implement me")
 }
 
-func (r DefaultValueRenderer) formatNumber(_ context.Context, v string) ([]string, error) {
-	// Remove leading and trailing zeroes.
-	v = strings.Trim(v, "0")
-	// Add `'` delimiter every 3 integral digits
+// formatInteger formats an integer into a value-rendered string. This function
+// operates with string manipulation (instead of manipulating the int or sdk.Int
+// object).
+func formatInteger(v string) (string, error) {
+	if v[0] == '-' {
+		v = v[1:]
+	}
+	if len(v) > 1 {
+		v = strings.TrimLeft(v, "0")
+
+	}
+
 	startOffset := 3
 	for outputIndex := len(v); outputIndex > startOffset; {
 		outputIndex -= 3
-		v = v[:outputIndex] + "," + v[outputIndex:]
+		v = v[:outputIndex] + thousandSeparator + v[outputIndex:]
+	}
+	return v, nil
+}
+
+// formatDecimal formats a decimal into a value-rendered string. This function
+// operates with string manipulation (instead of manipulating the sdk.Dec
+// object).
+func formatDecimal(v string) (string, error) {
+	parts := strings.Split(v, ".")
+	intPart, err := formatInteger(parts[0])
+	if err != nil {
+		return "", err
 	}
 
-	return []string{v}, nil
+	if len(parts) > 2 {
+		return "", fmt.Errorf("invalid decimal %s", v)
+	}
+
+	if len(parts) == 1 {
+		return intPart, nil
+	}
+
+	decPart := strings.TrimRight(parts[1], "0")
+	if len(decPart) == 0 {
+		return intPart, nil
+	}
+
+	return intPart + "." + decPart, nil
+}
+
+// formatDecimal formats a sdk.Coin into a value-rendered string, using the
+// given metadata about the denom.
+func formatCoin(coin sdk.Coin, metadata bank.Metadata) (string, error) {
+	curDenom := coin.Denom
+	dispDenom := metadata.Display
+
+	// Return early if no display denom or display denom is the current coin denom.
+	if dispDenom == "" || curDenom == dispDenom {
+		vr, err := formatInteger(coin.Amount.String())
+		return vr + " " + coin.Denom, err
+	}
+
+	// Find exponents of both denoms.
+	var curExp, dispExp uint32
+	foundCurExp, foundDispExp := false, false
+	for _, unit := range metadata.DenomUnits {
+		if curDenom == unit.Denom {
+			curExp = unit.Exponent
+			foundCurExp = true
+		}
+		if dispDenom == unit.Denom {
+			dispExp = unit.Exponent
+			foundDispExp = true
+		}
+	}
+
+	// If we didn't find either exponent, then we return early.
+	if !foundCurExp || !foundDispExp {
+		vr, err := formatInteger(coin.Amount.String())
+		return vr + " " + coin.Denom, err
+	}
+
+	exponentDiff := int64(curExp) - int64(dispExp)
+	var dispAmount sdk.Dec
+	if exponentDiff > 0 {
+		dispAmount = sdk.NewDecFromInt(coin.Amount).Mul(sdk.NewDec(10).Power(uint64(exponentDiff)))
+	} else {
+		dispAmount = sdk.NewDecFromInt(coin.Amount).Quo(sdk.NewDec(10).Power(uint64(-exponentDiff)))
+	}
+
+	vr, err := formatDecimal(dispAmount.String())
+	return vr + " " + dispDenom, err
 }
--- types/valuerenderer/valuerenderer_test.go
+++ types/valuerenderer/valuerenderer_test.go
@@ -1,18 +1,16 @@
-package valuerenderer_test
+package valuerenderer
 
 import (
-	"context"
 	"encoding/json"
 	"io/ioutil"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/types/valuerenderer"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
 )
 
-func TestIntegers(t *testing.T) {
-	vr := valuerenderer.DefaultValueRenderer{}
-
+func TestFormatInteger(t *testing.T) {
 	type integerTest []string
 	var testcases []integerTest
 	raw, err := ioutil.ReadFile("./testdata/integers.json")
@@ -21,11 +19,59 @@ func TestIntegers(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, tc := range testcases {
-		output, err := vr.Format(context.Background(), tc[0])
+		output, err := formatInteger(tc[0])
 		require.NoError(t, err)
 
-		require.Equal(t, tc[1], output[0])
+		require.Equal(t, tc[1], output)
 	}
+}
 
-	require.False(t, true)
+func TestFormatDecimal(t *testing.T) {
+	type decimalTest []string
+	var testcases []decimalTest
+	raw, err := ioutil.ReadFile("./testdata/decimals.json")
+	require.NoError(t, err)
+	err = json.Unmarshal(raw, &testcases)
+	require.NoError(t, err)
+
+	for _, tc := range testcases {
+		output, err := formatDecimal(tc[0])
+		require.NoError(t, err)
+
+		require.Equal(t, tc[1], output)
+	}
+}
+
+func TestFormatCoin(t *testing.T) {
+	var testcases []coinTest
+	raw, err := ioutil.ReadFile("./testdata/coins.json")
+	require.NoError(t, err)
+	err = json.Unmarshal(raw, &testcases)
+	require.NoError(t, err)
+
+	for _, tc := range testcases {
+		output, err := formatCoin(tc.coin, bank.Metadata{
+			Display:    tc.metadata.Denom,
+			DenomUnits: []*bank.DenomUnit{{Denom: tc.coin.Denom, Exponent: 0}, {Denom: tc.metadata.Denom, Exponent: tc.metadata.Exponent}},
+		})
+		require.NoError(t, err)
+
+		require.Equal(t, tc.expRes, output)
+	}
+}
+
+type coinTestMetadata struct {
+	Denom    string `json:"denom"`
+	Exponent uint32 `json:"exponent"`
+}
+
+type coinTest struct {
+	coin     sdk.Coin
+	metadata coinTestMetadata
+	expRes   string
+}
+
+func (t *coinTest) UnmarshalJSON(b []byte) error {
+	a := []interface{}{&t.coin, &t.metadata, &t.expRes}
+	return json.Unmarshal(b, &a)
 }

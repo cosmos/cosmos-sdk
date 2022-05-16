80ff7d01835742bcd9c13d90f3eadd421250bf47
Daniel Wedul <dwedul@figure.com>
2022-05-16 17:01:35 -0600

    [11859]: Move the v047 store migration stuff into Migrate3to4 directly to prevent a circular dependency between 047 and the keeper. Not using the keeper for that would be a significant pain in the butt.

--- x/bank/keeper/migrations.go
+++ x/bank/keeper/migrations.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v043 "github.com/cosmos/cosmos-sdk/x/bank/migrations/v043"
 	v046 "github.com/cosmos/cosmos-sdk/x/bank/migrations/v046"
-	v047 "github.com/cosmos/cosmos-sdk/x/bank/migrations/v047"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -29,5 +29,12 @@ func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 
 // Migrate3to4 migrates x/bank storage from version 3 to 4.
 func (m Migrator) Migrate3to4(ctx sdk.Context) error {
-	return v047.MigrateStore(ctx, m.keeper)
+	oldParams := m.keeper.GetParams(ctx)
+	m.keeper.SetAllSendEnabled(ctx, oldParams.GetSendEnabled())
+	newParams := banktypes.Params{
+		SendEnabled:        []*banktypes.SendEnabled{},
+		DefaultSendEnabled: oldParams.DefaultSendEnabled,
+	}
+	m.keeper.SetParams(ctx, newParams)
+	return nil
 }

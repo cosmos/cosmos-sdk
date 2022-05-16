a1f61404d0a5b26074d820641132315e907c8790
Daniel Wedul <dwedul@figure.com>
2022-05-16 16:36:44 -0600

    [11859]: Add a function for decoding a --page-key base64 value so that pagination can work as expected.

--- client/utils.go
+++ client/utils.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"encoding/base64"
 	"github.com/spf13/pflag"
 	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
 
@@ -75,4 +76,25 @@ func ReadPageRequest(flagSet *pflag.FlagSet) (*query.PageRequest, error) {
 // JSON RPC and WebSockets
 func NewClientFromNode(nodeURI string) (*rpchttp.HTTP, error) {
 	return rpchttp.New(nodeURI)
+}
+
+// WithPageKeyDecoded returns the provided flagSet with the page-key value base64 decoded (if it exists).
+// This is for when the page-key is provided as a base64 string (e.g. from the CLI).
+// ReadPageRequest expects it to be the raw bytes.
+//
+// Common usage: pageReq, err := client.ReadPageRequest(client.WithPageKeyDecoded(cmd.Flags()))
+func WithPageKeyDecoded(flagSet *pflag.FlagSet) *pflag.FlagSet {
+	encoded, err := flagSet.GetString(flags.FlagPageKey)
+	if err != nil {
+		panic(err.Error())
+	}
+	if len(encoded) > 0 {
+		var raw []byte
+		raw, err = base64.StdEncoding.DecodeString(encoded)
+		if err != nil {
+			panic(err.Error())
+		}
+		_ = flagSet.Set(flags.FlagPageKey, string(raw))
+	}
+	return flagSet
 }

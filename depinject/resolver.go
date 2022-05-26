9cf5e172521d3f01461bb2a4f6368d7855e0c8d0
Facundo Medica <facundomedica@gmail.com>
2022-05-26 10:43:51 -0300

    rename to depinject

---
+++ depinject/resolver.go
@@ -0,0 +1,14 @@
+package depinject
+
+import (
+	"reflect"
+
+	"cosmossdk.io/depinject/internal/graphviz"
+)
+
+type resolver interface {
+	addNode(*simpleProvider, int) error
+	resolve(*container, *moduleKey, Location) (reflect.Value, error)
+	describeLocation() string
+	typeGraphNode() *graphviz.Node
+}

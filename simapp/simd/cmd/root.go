622c32e4238b0c54f4abfb676521374d08465aa9
marbar3778 <marbar3778@yahoo.com>
2022-05-27 14:56:46 +0200

    revert root.go

--- simapp/simd/cmd/root.go
+++ simapp/simd/cmd/root.go
@@ -90,7 +90,7 @@ func initTendermintConfig() *tmcfg.Config {
 	cfg := tmcfg.DefaultConfig()
 
 	// these values put a higher strain on node memory
-	cfg.P2P.UseLegacy = true
+	// cfg.P2P.MaxNumInboundPeers = 100
 	// cfg.P2P.MaxNumOutboundPeers = 40
 
 	return cfg

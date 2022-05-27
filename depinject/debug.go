c17f38d22432e4b8fb088f17921f3356355067ac
Facundo Medica <14063057+facundomedica@users.noreply.github.com>
2022-05-27 14:34:48 -0300

    refactor!: rename container to cosmossdk.io/depinject (#12020)
    * chore: rename container to cosmossdk.io/container
    
    * fix imports
    
    * add replace for container
    
    * remove replace
    
    * add replace
    
    * fix replace
    
    * fix replace
    
    * fix replace
    
    * fix replace
    
    * fix replace
    
    * try to fix replace
    
    * rename to depinject
    
    * rename to depinject
    
    * do not use vanity URL for now
    
    * try fix tests
    
    * try fix tests
    
    * try fix tests
    
    * build -> inject
    
    * build -> inject
    
    * go mod tidy
    
    * fix dep vulnerability
    
    * fix dep vulnerability
    
    * fix Dockerfile for liveness-test
    
    * fix codeql error
    
    * try to solve dependency review
    
    * try to solve dependency review
    
    * go mod tidy
    
    * try to fix tests
    
    * another try

--- depinject/debug.go
+++ depinject/debug.go
@@ -1,4 +1,4 @@
-package container
+package depinject
 
 import (
 	"fmt"
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 	"reflect"
 
-	"github.com/cosmos/cosmos-sdk/container/internal/graphviz"
+	"github.com/cosmos/cosmos-sdk/depinject/internal/graphviz"
 )
 
 // DebugOption is a functional option for running a container that controls

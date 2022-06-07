23ea6a7c6e1c029a85f82d565d6ffbd9c6357011
Matt Kocubinski <mkocubinski@gmail.com>
2022-06-07 15:33:18 -0500

    Add docstrings for errors

--- depinject/errors.go
+++ depinject/errors.go
@@ -7,6 +7,9 @@ import (
 	"github.com/pkg/errors"
 )
 
+// ErrMultipleImplicitInterfaceBindings defines an error condition where an attempt was made to implicitly bind
+// Interface to a concrete type, but the container was unable to come to a resolution because multiple Matches
+// were found.
 type ErrMultipleImplicitInterfaceBindings struct {
 	error
 	Interface reflect.Type
@@ -17,6 +20,8 @@ func (err ErrMultipleImplicitInterfaceBindings) Error() string {
 	return fmt.Sprintf("Multiple implementations found for interface %v: %v", err.Interface, err.Matches)
 }
 
+// ErrExplicitBindingNotFound defines an error condition where an explicit binding of Interface type was marked as a
+// Preference but no provider for the requested type implementation was found in the container.
 type ErrExplicitBindingNotFound struct {
 	Preference preference
 	Interface  reflect.Type

cd331413ab2965cc883e2a13265773d7da9c4d24
Julien Robert <julien@rbrt.fr>
2022-06-08 02:23:38 +0200

    updates

--- types/errors/doc.go
+++ types/errors/doc.go
@@ -1,5 +1,5 @@
-// Package errors provides a shared set of errors for use in the SDK, and provides some helpers for converting
-// errors to ABCI response code.
+// Package errors provides a shared set of errors for use in the SDK,
+// and provides some helpers for converting errors to ABCI response code.
 //
 // New code should generally import cosmossdk.io/errors directly
 // and define a custom set of errors in custom codespace, rather than importing

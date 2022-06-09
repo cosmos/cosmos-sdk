905caf60eac2db583fa2e5609fa78478142c508c
Sai Kumar <sai@vitwit.com>
2022-06-09 16:32:43 +0530

    refactor: increase the consensus version for auth migration

--- x/auth/module.go
+++ x/auth/module.go
@@ -161,7 +161,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 2 }
+func (AppModule) ConsensusVersion() uint64 { return 3 }
 
 // BeginBlock returns the begin blocker for the auth module.
 func (AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}

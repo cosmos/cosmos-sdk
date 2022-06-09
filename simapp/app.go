720ae1096567d33730dba871f76553f21661a25a
Julien Robert <julien@rbrt.fr>
2022-06-09 15:57:58 +0200

    typo

--- simapp/app.go
+++ simapp/app.go
@@ -35,7 +35,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authsims "github.com/cosmos/cosmos-sdk/x/auth/simulation"
-	_ "github.com/cosmos/cosmos-sdk/x/auth/tx" // import tx module for side-effects
+	_ "github.com/cosmos/cosmos-sdk/x/auth/tx/module" // import tx module for side-effects
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/vesting"
 	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"

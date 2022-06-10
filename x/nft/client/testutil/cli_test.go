2ba7fdc336178c28fc39b7280453bad67eedda3b
Julien Robert <julien@rbrt.fr>
2022-06-10 15:50:54 +0200

    feedback

--- x/nft/client/testutil/cli_test.go
+++ x/nft/client/testutil/cli_test.go
@@ -7,15 +7,12 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
-	"cosmossdk.io/core/appconfig"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/cosmos/cosmos-sdk/x/nft/testutil"
 )
 
-//go:embed app.yaml
-var appConfig []byte
-
 func TestIntegrationTestSuite(t *testing.T) {
-	cfg, err := network.DefaultConfigWithAppConfig(appconfig.LoadYAML(appConfig))
+	cfg, err := network.DefaultConfigWithAppConfig(testutil.AppConfig)
 	require.NoError(t, err)
 	cfg.NumValidators = 1
 	suite.Run(t, NewIntegrationTestSuite(cfg))

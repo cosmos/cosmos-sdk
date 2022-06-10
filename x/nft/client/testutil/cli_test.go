06e11ce2ab96d1da0c0eb35fded75b49d7cf584e
Julien Robert <julien@rbrt.fr>
2022-06-10 15:24:48 +0200

    feat: add x/nft app wiring integration tests

--- x/nft/client/testutil/cli_test.go
+++ x/nft/client/testutil/cli_test.go
@@ -1,14 +1,27 @@
 package testutil
 
 import (
+	_ "embed"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
+	"cosmossdk.io/core/appconfig"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 )
 
+//go:embed app.yaml
+var appConfig []byte
+
 func TestIntegrationTestSuite(t *testing.T) {
+	cfg, err := network.DefaultConfigWithAppConfig(appconfig.LoadYAML(appConfig))
+	require.NoError(t, err)
+	cfg.NumValidators = 1
+	suite.Run(t, NewIntegrationTestSuite(cfg))
+}
+
+func TestIntegrationTestSuite_Legacy(t *testing.T) {
 	cfg := network.DefaultConfig()
 	cfg.NumValidators = 1
 	suite.Run(t, NewIntegrationTestSuite(cfg))

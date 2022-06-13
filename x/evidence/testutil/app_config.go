f7db89cffd9963f21b4bffc34b2364e3b00371ba
Julien Robert <julien@rbrt.fr>
2022-06-13 23:45:46 +0200

    feedback

--- x/evidence/testutil/app_config.go
+++ x/evidence/testutil/app_config.go
@@ -4,6 +4,14 @@ import (
 	_ "embed"
 
 	"cosmossdk.io/core/appconfig"
+	_ "github.com/cosmos/cosmos-sdk/x/auth"
+	_ "github.com/cosmos/cosmos-sdk/x/auth/tx/module"
+	_ "github.com/cosmos/cosmos-sdk/x/bank"
+	_ "github.com/cosmos/cosmos-sdk/x/evidence"
+	_ "github.com/cosmos/cosmos-sdk/x/genutil"
+	_ "github.com/cosmos/cosmos-sdk/x/params"
+	_ "github.com/cosmos/cosmos-sdk/x/slashing"
+	_ "github.com/cosmos/cosmos-sdk/x/staking"
 )
 
 //go:embed app.yaml

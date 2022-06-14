197732edf98ca7aeca395c6812201701f14aafa3
atheesh <atheesh@vitwit.com>
2022-06-14 16:42:32 +0530

    wip: update tests

--- x/authz/client/testutil/cli_test.go
+++ x/authz/client/testutil/cli_test.go
@@ -4,15 +4,19 @@
 package testutil
 
 import (
+	_ "embed"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/cosmos/cosmos-sdk/x/authz/testutil"
 )
 
 func TestIntegrationTestSuite(t *testing.T) {
-	cfg := network.DefaultConfig()
+	cfg, err := network.DefaultConfigWithAppConfig(testutil.AppConfig)
+	require.NoError(t, err)
 	cfg.NumValidators = 1
 	suite.Run(t, NewIntegrationTestSuite(cfg))
 }

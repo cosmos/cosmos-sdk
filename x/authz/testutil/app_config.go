128b403ecd41f6108586d0a96b3ccd8828474e42
atheesh <atheesh@vitwit.com>
2022-06-15 18:28:25 +0530

    update tests

--- x/authz/testutil/app_config.go
+++ x/authz/testutil/app_config.go
@@ -3,6 +3,15 @@ package testutil
 import (
 	_ "embed"
 
+	_ "github.com/cosmos/cosmos-sdk/x/auth"
+	_ "github.com/cosmos/cosmos-sdk/x/auth/tx/module"
+	_ "github.com/cosmos/cosmos-sdk/x/authz"
+	_ "github.com/cosmos/cosmos-sdk/x/bank"
+	_ "github.com/cosmos/cosmos-sdk/x/genutil"
+	_ "github.com/cosmos/cosmos-sdk/x/gov"
+	_ "github.com/cosmos/cosmos-sdk/x/params"
+	_ "github.com/cosmos/cosmos-sdk/x/staking"
+
 	"cosmossdk.io/core/appconfig"
 )
 

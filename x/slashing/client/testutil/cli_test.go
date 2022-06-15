e59238d34ab816fc8757c7abb7ce058faf501820
atheesh <atheesh@vitwit.com>
2022-06-15 17:50:32 +0530

    wip: `x/slashing` integration tests

--- x/slashing/client/testutil/cli_test.go
+++ x/slashing/client/testutil/cli_test.go
@@ -1,18 +1,18 @@
-//go:build norace
-// +build norace
-
 package testutil
 
 import (
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/cosmos/cosmos-sdk/x/slashing/testutil"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
 func TestIntegrationTestSuite(t *testing.T) {
-	cfg := network.DefaultConfig()
+	cfg, err := network.DefaultConfigWithAppConfig(testutil.AppConfig)
+	require.NoError(t, err)
 	cfg.NumValidators = 1
 	suite.Run(t, NewIntegrationTestSuite(cfg))
 }

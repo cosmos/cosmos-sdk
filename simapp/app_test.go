db7c6310e24c23d6358a80655b8e7b64e64dee71
romelukaku <gundamaster5@gmail.com>
2022-06-17 18:55:51 +0700

    update TestInitGenesisOnMigration in app_test.go

--- simapp/app_test.go
+++ simapp/app_test.go
@@ -218,7 +218,8 @@ func TestInitGenesisOnMigration(t *testing.T) {
 	mockModule := mocks.NewMockAppModule(mockCtrl)
 	mockDefaultGenesis := json.RawMessage(`{"key": "value"}`)
 	mockModule.EXPECT().DefaultGenesis(gomock.Eq(app.appCodec)).Times(1).Return(mockDefaultGenesis)
-	mockModule.EXPECT().InitGenesis(gomock.Eq(ctx), gomock.Eq(app.appCodec), gomock.Eq(mockDefaultGenesis)).Times(1).Return(nil)
+	mockModule.EXPECT().UnmarshalGenesis(gomock.Eq(app.appCodec), gomock.Eq(mockDefaultGenesis)).Times(1).Return(nil)
+	mockModule.EXPECT().InitGenesis(gomock.Eq(ctx), gomock.Nil()).Times(1).Return(nil)
 	mockModule.EXPECT().ConsensusVersion().Times(1).Return(uint64(0))
 
 	app.ModuleManager.Modules["mock"] = mockModule

eebb1084c6178e5810a42fb654632d2f63f7fac8
likhita-809 <likhita@vitwit.com>
2022-06-22 14:33:46 +0530

    wip: add auth integration tests

--- x/auth/client/testutil/cli_test.go
+++ x/auth/client/testutil/cli_test.go
@@ -7,12 +7,15 @@ import (
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/cosmos/cosmos-sdk/x/auth/testutil"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
 func TestIntegrationTestSuite(t *testing.T) {
-	cfg := network.DefaultConfig()
+	cfg, err := network.DefaultConfigWithAppConfig(testutil.AppConfig)
+	require.NoError(t, err)
 	cfg.NumValidators = 2
 	suite.Run(t, NewIntegrationTestSuite(cfg))
 }

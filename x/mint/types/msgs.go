ad3c88779af099dd8afdc743f0da39b7ad89ad21
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-06-27 13:11:38 +0100

    updates

--- x/mint/types/msgs.go
+++ x/mint/types/msgs.go
@@ -7,6 +7,11 @@ import (
 
 var _ sdk.Msg = &MsgUpdateParams{}
 
+// GetSignBytes implements the LegacyMsg interface.
+func (m MsgUpdateParams) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&m))
+}
+
 // GetSigners returns the expected signers for a MsgUpdateParams message.
 func (m *MsgUpdateParams) GetSigners() []sdk.AccAddress {
 	addr, _ := sdk.AccAddressFromBech32(m.Authority)

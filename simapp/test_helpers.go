d6c8c47978b6a03e97e4c6f49fc6ee80297bc6d5
Adu <foriteration@gmail.com>
2022-06-29 10:08:13 +0800

    directly pass rand to functions as an argument

--- simapp/test_helpers.go
+++ simapp/test_helpers.go
@@ -258,9 +258,8 @@ func SignCheckDeliver(
 	t *testing.T, txCfg client.TxConfig, app *bam.BaseApp, header tmproto.Header, msgs []sdk.Msg,
 	chainID string, accNums, accSeqs []uint64, expSimPass, expPass bool, priv ...cryptotypes.PrivKey,
 ) (sdk.GasInfo, *sdk.Result, error) {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	tx, err := simtestutil.GenSignedMockTx(
-		r,
+		rand.New(rand.NewSource(time.Now().UnixNano())),
 		txCfg,
 		msgs,
 		sdk.Coins{sdk.NewInt64Coin(sdk.DefaultBondDenom, 0)},
@@ -308,11 +307,10 @@ func SignCheckDeliver(
 // every transaction.
 func GenSequenceOfTxs(txGen client.TxConfig, msgs []sdk.Msg, accNums []uint64, initSeqNums []uint64, numToGenerate int, priv ...cryptotypes.PrivKey) ([]sdk.Tx, error) {
 	txs := make([]sdk.Tx, numToGenerate)
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	var err error
 	for i := 0; i < numToGenerate; i++ {
 		txs[i], err = simtestutil.GenSignedMockTx(
-			r,
+			rand.New(rand.NewSource(time.Now().UnixNano())),
 			txGen,
 			msgs,
 			sdk.Coins{sdk.NewInt64Coin(sdk.DefaultBondDenom, 0)},

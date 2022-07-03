d2b72a2c1c78551187a34e0178dd95a9d1f123ac
Robert Zaremba <robert@zaremba.ch>
2022-07-03 09:57:59 +0200

    chore: optimize get last completed upgrade (#12268)
    * feat: improve GetLastCompletedUpgrade
    
    * rename
    
    * use var block

--- x/upgrade/keeper/keeper.go
+++ x/upgrade/keeper/keeper.go
@@ -329,24 +329,20 @@ func (k Keeper) GetLastCompletedUpgrade(ctx sdk.Context) (string, int64) {
 	iter := sdk.KVStoreReversePrefixIterator(ctx.KVStore(k.storeKey), []byte{types.DoneByte})
 	defer iter.Close()
 
-	var upgrades []upgrade
+	var (
+		latest upgrade
+		found  bool
+	)
 	for ; iter.Valid(); iter.Next() {
-		name := parseDoneKey(iter.Key())
-		value := int64(sdk.BigEndianToUint64(iter.Value()))
-
-		upgrades = append(upgrades, upgrade{Name: name, BlockHeight: value})
+		upgradeHeight := int64(sdk.BigEndianToUint64(iter.Value()))
+		if !found || upgradeHeight >= latest.BlockHeight {
+			found = true
+			name := parseDoneKey(iter.Key())
+			latest = upgrade{Name: name, BlockHeight: upgradeHeight}
+		}
 	}
 
-	// sort upgrades in descending order by block height
-	sort.SliceStable(upgrades, func(i, j int) bool {
-		return upgrades[i].BlockHeight > upgrades[j].BlockHeight
-	})
-
-	if len(upgrades) > 0 {
-		return upgrades[0].Name, upgrades[0].BlockHeight
-	}
-
-	return "", 0
+	return latest.Name, latest.BlockHeight
 }
 
 // parseDoneKey - split upgrade name from the done key

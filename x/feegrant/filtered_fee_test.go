2dffb5649bad47eb12c9c9ee14b88b3f199bb7b2
Facundo Medica <facundomedica@gmail.com>
2022-07-06 14:42:56 -0300

    progress

--- x/feegrant/filtered_fee_test.go
+++ x/feegrant/filtered_fee_test.go
@@ -9,33 +9,18 @@ import (
 	ocproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/depinject"
-	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/feegrant"
-	"github.com/cosmos/cosmos-sdk/x/feegrant/keeper"
 	"github.com/cosmos/cosmos-sdk/x/feegrant/testutil"
 	feegranttestutil "github.com/cosmos/cosmos-sdk/x/feegrant/testutil"
-	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 )
 
 func TestFilteredFeeValidAllow(t *testing.T) {
-	var (
-		interfaceRegistry codectypes.InterfaceRegistry
-		bankKeeper        bankkeeper.Keeper
-		stakingKeeper     *stakingkeeper.Keeper
-		feegrantKeeper    keeper.Keeper
-	)
+	key := sdk.NewKVStoreKey(feegrant.StoreKey)
+	testCtx := testutil.DefaultContextWithDB(t, key, sdk.NewTransientStoreKey("transient_test"))
 
-	app, err := simtestutil.Setup(testutil.AppConfig,
-		&feegrantKeeper,
-		&bankKeeper,
-		&stakingKeeper,
-		&interfaceRegistry,
-	)
 	require.NoError(t, err)
 
 	ctx := app.BaseApp.NewContext(false, ocproto.Header{Time: time.Now()})

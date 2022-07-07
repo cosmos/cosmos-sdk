1037c03a301aed14041277b20e366103d1d1cf32
likhita-809 <likhita@vitwit.com>
2022-07-07 14:51:11 +0530

    fix tests

--- x/auth/module.go
+++ x/auth/module.go
@@ -198,7 +198,7 @@ func (AppModule) ProposalContents(simState module.SimulationState) []simtypes.We
 
 // RandomizedParams creates randomized auth param changes for the simulator.
 func (AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
-	return simulation.ParamChanges(r)
+	return []simtypes.ParamChange{}
 }
 
 // RegisterStoreDecoder registers a decoder for auth module's types

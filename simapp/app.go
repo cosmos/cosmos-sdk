03375ec4f07a23f211d6b637b06a9902b5be9a8f
deepto <ddepp98@outlook.com>
2022-07-10 18:25:17 +0530

    fix

--- simapp/app.go
+++ simapp/app.go
@@ -88,9 +88,6 @@ import (
 	upgradeclient "github.com/cosmos/cosmos-sdk/x/upgrade/client"
 	upgradekeeper "github.com/cosmos/cosmos-sdk/x/upgrade/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-
-	// unnamed import of statik for swagger UI support
-	_ "github.com/cosmos/cosmos-sdk/client/docs/statik"
 )
 
 var (

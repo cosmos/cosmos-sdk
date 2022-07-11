befbbc0e76c0823ea8804030aacea578315324d7
atheesh <atheesh@vitwit.com>
2022-07-11 15:41:08 +0530

    fix tests

--- x/crisis/keeper/params.go
+++ x/crisis/keeper/params.go
@@ -2,10 +2,11 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/crisis/types"
 )
 
-// GetConstantFee get's the constant fee from the paramSpace
+// GetConstantFee get's the constant fee from the store
 func (k *Keeper) GetConstantFee(ctx sdk.Context) (constantFee sdk.Coin) {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.ConstantFee)
@@ -16,10 +17,14 @@ func (k *Keeper) GetConstantFee(ctx sdk.Context) (constantFee sdk.Coin) {
 	return
 }
 
-// GetConstantFee set's the constant fee in the paramSpace
+// GetConstantFee set's the constant fee in the store
 func (k *Keeper) SetConstantFee(ctx sdk.Context, constantFee sdk.Coin) error {
 	store := ctx.KVStore(k.storeKey)
 	bz, err := k.cdc.Marshal(&constantFee)
+
+	if !constantFee.IsValid() {
+		return errors.ErrInvalidCoins.Wrap("invalid constant fee")
+	}
 
 	if err != nil {
 		return err

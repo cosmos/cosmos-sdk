cd0ae796a68c17441167a3c0ac67b241c9434a2b
Matt Kocubinski <mkocubinski@gmail.com>
2022-07-11 20:19:30 -0500

    some progress on test refactor

--- baseapp/util_test.go
+++ baseapp/util_test.go
@@ -1,77 +1,62 @@
-package baseapp
+package baseapp_test
 
 import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	tmtypes "github.com/tendermint/tendermint/types"
+
 	runtimev1alpha1 "cosmossdk.io/api/cosmos/app/runtime/v1alpha1"
 	appv1alpha1 "cosmossdk.io/api/cosmos/app/v1alpha1"
 	authmodulev1 "cosmossdk.io/api/cosmos/auth/module/v1"
 	bankmodulev1 "cosmossdk.io/api/cosmos/bank/module/v1"
+	paramsmodulev1 "cosmossdk.io/api/cosmos/params/module/v1"
+	stakingmodulev1 "cosmossdk.io/api/cosmos/staking/module/v1"
 	"cosmossdk.io/core/appconfig"
 	"cosmossdk.io/depinject"
-
-	"github.com/cosmos/cosmos-sdk/snapshots"
-	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/runtime"
+	"github.com/cosmos/cosmos-sdk/testutil/mock"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-// TODO: Can be removed once we move all middleware tests into x/auth/middleware
-// ref: #https://github.com/cosmos/cosmos-sdk/issues/10282
+// GenesisStateWithSingleValidator initializes GenesisState with a single validator and genesis accounts
+// that also act as delegators.
+func GenesisStateWithSingleValidator(t *testing.T, codec codec.Codec, builder *runtime.AppBuilder) map[string]json.RawMessage {
+	t.Helper()
 
-// CheckState is an exported method to be able to access baseapp's
-// checkState in tests.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) CheckState() *state {
-	return app.checkState
-}
+	privVal := mock.NewPV()
+	pubKey, err := privVal.GetPubKey(context.TODO())
+	require.NoError(t, err)
 
-// DeliverState is an exported method to be able to access baseapp's
-// deliverState in tests.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) DeliverState() *state {
-	return app.deliverState
-}
+	// create validator set with single validator
+	validator := tmtypes.NewValidator(pubKey, 1)
+	valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
 
-// CMS is an exported method to be able to access baseapp's cms in tests.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) CMS() types.CommitMultiStore {
-	return app.cms
-}
+	// generate genesis account
+	senderPrivKey := secp256k1.GenPrivKey()
+	acc := authtypes.NewBaseAccount(senderPrivKey.PubKey().Address().Bytes(), senderPrivKey.PubKey(), 0, 0)
+	balances := []banktypes.Balance{
+		{
+			Address: acc.GetAddress().String(),
+			Coins:   sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100000000000000))),
+		},
+	}
 
-// GetSnapshotManager() is an exported method to be able to access baseapp's snapshot
-// manager in tests.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) GetSnapshotManager() *snapshots.Manager {
-	return app.snapshotManager
-}
+	genesisState := builder.DefaultGenesis()
+	// sus
+	genesisState, err = simtestutil.GenesisStateWithValSet(codec, genesisState, valSet, []authtypes.GenesisAccount{acc}, balances...)
+	require.NoError(t, err)
 
-// GetMaximumBlockGas return maximum blocks gas.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) GetMaximumBlockGas(ctx types.Context) uint64 {
-	return app.getMaximumBlockGas(ctx)
-}
-
-// GetName return name.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) GetName() string {
-	return app.name
-}
-
-// CreateQueryContext calls app's createQueryContext.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) CreateQueryContext(height int64, prove bool) (types.Context, error) {
-	return app.createQueryContext(height, prove)
-}
-
-// MinGasPrices returns minGasPrices.
-//
-// This method is only accessible in baseapp tests.
-func (app *BaseApp) MinGasPrices() types.DecCoins {
-	return app.minGasPrices
+	return genesisState
 }
 
 func MakeTestConfig() depinject.Config {
@@ -84,10 +69,14 @@ func MakeTestConfig() depinject.Config {
 					BeginBlockers: []string{
 						"auth",
 						"bank",
+						"staking",
+						"params",
 					},
 					EndBlockers: []string{
 						"auth",
 						"bank",
+						"staking",
+						"params",
 					},
 					OverrideStoreKeys: []*runtimev1alpha1.StoreKeyConfig{
 						{
@@ -98,6 +87,8 @@ func MakeTestConfig() depinject.Config {
 					InitGenesis: []string{
 						"auth",
 						"bank",
+						"staking",
+						"params",
 					},
 				}),
 			},
@@ -107,12 +98,23 @@ func MakeTestConfig() depinject.Config {
 					Bech32Prefix: "cosmos",
 					ModuleAccountPermissions: []*authmodulev1.ModuleAccountPermission{
 						{Account: "fee_collector"},
+						{Account: minttypes.ModuleName, Permissions: []string{authtypes.Minter}},
+						{Account: stakingtypes.BondedPoolName, Permissions: []string{authtypes.Burner, stakingtypes.ModuleName}},
+						{Account: stakingtypes.NotBondedPoolName, Permissions: []string{authtypes.Burner, stakingtypes.ModuleName}},
 					},
 				}),
 			},
 			{
 				Name:   "bank",
 				Config: appconfig.WrapAny(&bankmodulev1.Module{}),
+			},
+			{
+				Name:   "params",
+				Config: appconfig.WrapAny(&paramsmodulev1.Module{}),
+			},
+			{
+				Name:   "staking",
+				Config: appconfig.WrapAny(&stakingmodulev1.Module{}),
 			},
 		},
 	})

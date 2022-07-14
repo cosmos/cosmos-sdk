054520b0a768d92b1ff78c7c291cf6a51e8c75fc
Jeancarlo <jeancarlobarrios@gmail.com>
2022-07-14 07:08:30 -0600

    remove duplicated values

--- x/staking/migrations/v046/keys.go
+++ x/staking/migrations/v046/keys.go
@@ -4,14 +4,3 @@ const (
 	// ModuleName is the name of the module
 	ModuleName = "staking"
 )
-
-var (
-	ParametersKey = []byte{0x51} // prefix for parameters for module x/staking
-
-	KeyUnbondingTime     = []byte("UnbondingTime")
-	KeyMaxValidators     = []byte("MaxValidators")
-	KeyMaxEntries        = []byte("MaxEntries")
-	KeyBondDenom         = []byte("BondDenom")
-	KeyHistoricalEntries = []byte("HistoricalEntries")
-	KeyMinCommissionRate = []byte("MinCommissionRate")
-)

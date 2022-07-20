292a08eaf193ad78611e6daabfb3792776fda670
atheesh <atheesh@vitwit.com>
2022-07-20 19:07:10 +0530

    review

--- api/cosmos/gov/v1/tx.pulsar.go
+++ api/cosmos/gov/v1/tx.pulsar.go
@@ -6265,7 +6265,7 @@ type MsgUpdateParams struct {
 
 	// authority is the address of the governance account.
 	Authority string `protobuf:"bytes,1,opt,name=authority,proto3" json:"authority,omitempty"`
-	// params defines the x/distribution parameters to update.
+	// params defines the x/gov parameters to update.
 	//
 	// NOTE: All parameters must be supplied.
 	Params *Params `protobuf:"bytes,2,opt,name=params,proto3" json:"params,omitempty"`
--- x/gov/types/v1/tx.pb.go
+++ x/gov/types/v1/tx.pb.go
@@ -557,7 +557,7 @@ var xxx_messageInfo_MsgDepositResponse proto.InternalMessageInfo
 type MsgUpdateParams struct {
 	// authority is the address of the governance account.
 	Authority string `protobuf:"bytes,1,opt,name=authority,proto3" json:"authority,omitempty"`
-	// params defines the x/distribution parameters to update.
+	// params defines the x/gov parameters to update.
 	//
 	// NOTE: All parameters must be supplied.
 	Params Params `protobuf:"bytes,2,opt,name=params,proto3" json:"params"`

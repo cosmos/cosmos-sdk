4b9b40b87d5d136f54568b72b748763641f377f7
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-07-25 20:58:41 +0800

    updates

---
+++ x/bank/client/cli/suite_test.go
@@ -0,0 +1,50 @@
+package cli_test
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	testutilmod "github.com/cosmos/cosmos-sdk/types/module/testutil"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+)
+
+type CLITestSuite struct {
+	suite.Suite
+
+	kr      keyring.Keyring
+	encCfg  testutilmod.TestEncodingConfig
+	baseCtx client.Context
+}
+
+func TestMigrateTestSuite(t *testing.T) {
+	suite.Run(t, new(CLITestSuite))
+}
+
+func (s *CLITestSuite) SetupSuite() {
+	s.encCfg = testutilmod.MakeTestEncodingConfig(bank.AppModuleBasic{})
+	s.kr = keyring.NewInMemory(s.encCfg.Codec)
+	s.baseCtx = client.Context{}.
+		WithKeyring(s.kr).
+		WithTxConfig(s.encCfg.TxConfig).
+		WithCodec(s.encCfg.Codec).
+		WithOutput(io.Discard)
+}
+
+func (s *CLITestSuite) createKeyringRecords(num int) []*keyring.Record {
+	records := make([]*keyring.Record, num)
+	for i := range records {
+		record, _, err := s.kr.NewMnemonic(fmt.Sprint("key-%d", i), keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+		s.Require().NoError(err)
+
+		records[i] = record
+	}
+
+	return records
+}

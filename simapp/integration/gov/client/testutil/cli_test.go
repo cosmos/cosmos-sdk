1a826a2b86358e0f80a033fde31f5d17e4c006d0
Matt Kocubinski <mkocubinski@gmail.com>
2022-07-25 11:03:43 -0500

    refactor: move gov cli test runner to simapp

--- simapp/integration/gov/client/testutil/cli_test.go
+++ simapp/integration/gov/client/testutil/cli_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/gov/client/testutil"
 	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 
 	"github.com/stretchr/testify/require"
@@ -16,7 +17,7 @@ import (
 func TestIntegrationTestSuite(t *testing.T) {
 	cfg := network.DefaultConfig(simapp.NewTestNetworkFixture)
 	cfg.NumValidators = 1
-	suite.Run(t, NewIntegrationTestSuite(cfg))
+	suite.Run(t, testutil.NewIntegrationTestSuite(cfg))
 
 	dp := v1.NewDepositParams(sdk.NewCoins(sdk.NewCoin(cfg.BondDenom, v1.DefaultMinDepositTokens)), time.Duration(15)*time.Second)
 	vp := v1.NewVotingParams(time.Duration(5) * time.Second)
@@ -26,5 +27,5 @@ func TestIntegrationTestSuite(t *testing.T) {
 	bz, err := cfg.Codec.MarshalJSON(genesisState)
 	require.NoError(t, err)
 	cfg.GenesisState["gov"] = bz
-	suite.Run(t, NewDepositTestSuite(cfg))
+	suite.Run(t, testutil.NewDepositTestSuite(cfg))
 }

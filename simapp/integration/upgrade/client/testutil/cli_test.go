327c4fc4aa7e9f6f1e1cf4d6d2520c72bd520269
Matt Kocubinski <mkocubinski@gmail.com>
2022-07-25 13:43:07 -0500

    remove simapp usages

--- simapp/integration/upgrade/client/testutil/cli_test.go
+++ simapp/integration/upgrade/client/testutil/cli_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
@@ -13,5 +14,12 @@ import (
 func TestIntegrationTestSuite(t *testing.T) {
 	cfg := network.DefaultConfig(simapp.NewTestNetworkFixture)
 	cfg.NumValidators = 1
-	suite.Run(t, testutil.NewIntegrationTestSuite(cfg))
+
+	app := simapp.Setup(t, false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	app.UpgradeKeeper.SetVersionSetter(app.BaseApp)
+	app.UpgradeKeeper.SetModuleVersionMap(ctx, app.ModuleManager.GetVersionMap())
+
+	suite.Run(t, testutil.NewIntegrationTestSuite(cfg, app.UpgradeKeeper, ctx))
 }

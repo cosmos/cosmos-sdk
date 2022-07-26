7c803630cc1c3a5a52b9d95c084537736f17cbce
evan-forbes <evan.samuel.forbes@gmail.com>
2022-07-26 16:25:00 -0500

    fix: remove evidence from cosmos Block type

--- client/grpc/tmservice/util.go
+++ client/grpc/tmservice/util.go
@@ -32,7 +32,6 @@ func convertBlock(tmblock *tmprototypes.Block) *Block {
 	b.Header = convertHeader(tmblock.Header)
 	b.LastCommit = tmblock.LastCommit
 	b.Data = tmblock.Data
-	b.Evidence = tmblock.Evidence
 
 	return b
 }

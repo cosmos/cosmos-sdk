fdb1974f1de6a019955344ea6727c2853403e96c
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-07-27 13:50:55 +0800

    updates

---
+++ x/bank/client/testutil/cli_helpers.go
@@ -0,0 +1,45 @@
+/*
+Note, these utility functions are remaining in this file because they are needed
+by other legacy CLI test suites. Once those test suites are migrated and refactored
+per https://github.com/cosmos/cosmos-sdk/issues/12696, then this package should
+be entirely removed.
+*/
+package testutil
+
+import (
+	"fmt"
+
+	"github.com/tendermint/tendermint/libs/cli"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/testutil"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bankcli "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
+)
+
+func MsgSendExec(clientCtx client.Context, from, to, amount fmt.Stringer, extraArgs ...string) (testutil.BufferWriter, error) {
+	args := []string{from.String(), to.String(), amount.String()}
+	args = append(args, extraArgs...)
+
+	return clitestutil.ExecTestCLICmd(clientCtx, bankcli.NewSendTxCmd(), args)
+}
+
+func MsgMultiSendExec(clientCtx client.Context, from sdk.AccAddress, to []sdk.AccAddress, amount fmt.Stringer, extraArgs ...string) (testutil.BufferWriter, error) {
+	args := []string{from.String()}
+	for _, addr := range to {
+		args = append(args, addr.String())
+	}
+
+	args = append(args, amount.String())
+	args = append(args, extraArgs...)
+
+	return clitestutil.ExecTestCLICmd(clientCtx, bankcli.NewMultiSendTxCmd(), args)
+}
+
+func QueryBalancesExec(clientCtx client.Context, address fmt.Stringer, extraArgs ...string) (testutil.BufferWriter, error) {
+	args := []string{address.String(), fmt.Sprintf("--%s=json", cli.OutputFlag)}
+	args = append(args, extraArgs...)
+
+	return clitestutil.ExecTestCLICmd(clientCtx, bankcli.GetBalancesCmd(), args)
+}

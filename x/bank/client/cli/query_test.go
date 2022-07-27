a58abf291ee3941ec2265a307fba5102377fcf43
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-07-27 13:27:04 +0800

    updates

--- x/bank/client/cli/query_test.go
+++ x/bank/client/cli/query_test.go
@@ -286,3 +286,77 @@ func (s *CLITestSuite) TestGetCmdQueryTotalSupply() {
 		})
 	}
 }
+
+func (s *CLITestSuite) TestGetCmdQuerySendEnabled() {
+	cmd := cli.GetCmdQuerySendEnabled()
+	cmd.SetOutput(io.Discard)
+
+	testCases := []struct {
+		name         string
+		ctxGen       func() client.Context
+		args         []string
+		expectResult proto.Message
+		expectErr    bool
+	}{
+		{
+			"valid query",
+			func() client.Context {
+				bz, _ := s.encCfg.Codec.Marshal(&types.QuerySendEnabledResponse{
+					SendEnabled: []*types.SendEnabled{},
+				})
+				c := newMockTendermintRPC(abci.ResponseQuery{
+					Value: bz,
+				})
+				return s.baseCtx.WithClient(c)
+			},
+			[]string{
+				fmt.Sprintf("--%s=json", flags.FlagOutput),
+			},
+			&types.QuerySendEnabledResponse{},
+			false,
+		},
+		{
+			"valid query with denoms",
+			func() client.Context {
+				bz, _ := s.encCfg.Codec.Marshal(&types.QuerySendEnabledResponse{
+					SendEnabled: []*types.SendEnabled{},
+				})
+				c := newMockTendermintRPC(abci.ResponseQuery{
+					Value: bz,
+				})
+				return s.baseCtx.WithClient(c)
+			},
+			[]string{
+				"photon",
+				"stake",
+				fmt.Sprintf("--%s=json", flags.FlagOutput),
+			},
+			&types.QuerySendEnabledResponse{},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			var outBuf bytes.Buffer
+
+			clientCtx := tc.ctxGen().WithOutput(&outBuf)
+			ctx := svrcmd.CreateExecuteContext(context.Background())
+
+			cmd.SetContext(ctx)
+			cmd.SetArgs(tc.args)
+
+			s.Require().NoError(client.SetCmdClientContextHandler(clientCtx, cmd))
+
+			err := cmd.Execute()
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(s.encCfg.Codec.UnmarshalJSON(outBuf.Bytes(), tc.expectResult))
+				s.Require().NoError(err)
+			}
+		})
+	}
+}

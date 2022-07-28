353cb25787c7af5e7374d8adab896c6b5fe9b008
atheesh <atheesh@vitwit.com>
2022-07-28 16:35:55 +0530

    review changes

--- x/gov/module.go
+++ x/gov/module.go
@@ -39,7 +39,7 @@ import (
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
-const ConsensusVersion = 3
+const ConsensusVersion = 4
 
 var (
 	_ module.AppModule           = AppModule{}

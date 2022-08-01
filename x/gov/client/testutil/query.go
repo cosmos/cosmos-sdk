bddb5a1921e33e46489717a3c2c4605bccfeebb1
Roman <ackhtariev@gmail.com>
2022-08-01 09:24:40 -0400

    fix e2e gov tests

--- x/gov/client/testutil/query.go
+++ x/gov/client/testutil/query.go
@@ -24,7 +24,7 @@ func (s *IntegrationTestSuite) TestCmdParams() {
 		{
 			"json output",
 			[]string{fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
-			`{"voting_params":{"voting_period":"172800000000000"},"tally_params":{"quorum":"0.334000000000000000","threshold":"0.500000000000000000","veto_threshold":"0.334000000000000000"},"deposit_params":{"min_deposit":[{"denom":"stake","amount":"10000000"}],"max_deposit_period":"172800000000000"}}`,
+			`{"voting_params":{"voting_period":"172800000000000"},"tally_params":{"quorum":"0.334000000000000000","threshold":"0.500000000000000000","veto_threshold":"0.334000000000000000"},"deposit_params":{"min_deposit":[{"denom":"stake","amount":"10000000"}],"max_deposit_period":"172800000000000","min_initial_deposit_ratio":"0.000000000000000000"}}`,
 		},
 		{
 			"text output",
@@ -35,6 +35,7 @@ deposit_params:
   min_deposit:
   - amount: "10000000"
     denom: stake
+  min_initial_deposit_ratio: "0.000000000000000000"
 tally_params:
   quorum: "0.334000000000000000"
   threshold: "0.500000000000000000"
@@ -89,7 +90,7 @@ func (s *IntegrationTestSuite) TestCmdParam() {
 				"deposit",
 				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
 			},
-			`{"min_deposit":[{"denom":"stake","amount":"10000000"}],"max_deposit_period":"172800000000000"}`,
+			`{"min_deposit":[{"denom":"stake","amount":"10000000"}],"max_deposit_period":"172800000000000","min_initial_deposit_ratio":"0.000000000000000000"}`,
 		},
 	}
 

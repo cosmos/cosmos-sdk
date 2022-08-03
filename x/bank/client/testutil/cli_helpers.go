92350388105fe0a2d9db2a7191e43cd4c8b525f4
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-08-03 10:12:57 -0400

    updates

--- x/bank/client/testutil/cli_helpers.go
+++
@@ -1,7 +0,0 @@
-/*
-Note, these utility functions are remaining in this file because they are needed
-by other legacy CLI test suites. Once those test suites are migrated and refactored
-per https://github.com/cosmos/cosmos-sdk/issues/12696, then this package should
-be entirely removed.
-*/
-package testutil

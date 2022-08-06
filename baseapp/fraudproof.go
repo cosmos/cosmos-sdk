b6acaf101a70ee8c59e45abc8181551609ec1568
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-08-06 02:31:00 -0400

    Tidy up code

--- baseapp/fraudproof.go
+++ baseapp/fraudproof.go
@@ -2,17 +2,22 @@ package baseapp
 
 import "github.com/lazyledger/smt"
 
+// Represents a single-round fraudProof
 type FraudProof struct {
+	// The block height to load state of
 	blockHeight int64
 
+	// A map from module name to state witness
 	stateWitness map[string]StateWitness
 }
 
+// State witness with a list of all witness data
 type StateWitness struct {
-	root        []byte
+	// List of witness data
 	WitnessData []WitnessData
 }
 
+// Witness data containing a key/value pair and a SMT proof for said key/value pair
 type WitnessData struct {
 	Key   []byte
 	Value []byte
--- go.mod
+++ go.mod
@@ -65,7 +65,10 @@ require (
 	sigs.k8s.io/yaml v1.3.0
 )
 
-require github.com/cosmos/cosmos-sdk/store/tools/ics23 v0.0.0-00010101000000-000000000000
+require (
+	github.com/chrispappas/golang-generics-set v1.0.1
+	github.com/cosmos/cosmos-sdk/store/tools/ics23 v0.0.0-00010101000000-000000000000
+)
 
 require (
 	4d63.com/gochecknoglobals v0.1.0 // indirect

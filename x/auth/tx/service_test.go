2c5afcfbaedd29f0b9ea44fc500d5d653d2e6ff9
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-08-08 11:54:53 -0400

    updates

--- x/auth/tx/service_test.go
+++ x/auth/tx/service_test.go
@@ -267,7 +267,7 @@ func (s IntegrationTestSuite) TestSimulateTx_GRPCGateway() {
 				s.Require().NoError(err)
 				// Check the result and gas used are correct.
 				s.Require().Len(result.GetResult().MsgResponses, 1)
-				s.Require().Equal(len(result.GetResult().GetEvents()), 13) // See TestSimulateTx_GRPC for the 13 events.
+				s.Require().Equal(len(result.GetResult().GetEvents()), 14) // See TestSimulateTx_GRPC for the 13 events.
 				s.Require().True(result.GetGasInfo().GetGasUsed() > 0)     // Gas used sometimes change, jus
 			}
 		})

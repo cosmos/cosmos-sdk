09321d7ae50c94d341b5a9985023b414bf9db524
yihuang <huang@crypto.com>
2022-08-10 05:12:43 +0800

    fix: Use fixed length hex for pointer at FwdCapabilityKey (backport #11737) (#12818)
    * fix: Use fixed length hex for pointer at FwdCapabilityKey (backport #11737)
    
    * Update CHANGELOG.md
    
    * add comments and unit tests

--- x/capability/types/keys_test.go
+++ x/capability/types/keys_test.go
@@ -2,6 +2,7 @@ package types_test
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,7 +17,12 @@ func TestRevCapabilityKey(t *testing.T) {
 
 func TestFwdCapabilityKey(t *testing.T) {
 	cap := types.NewCapability(23)
-	expected := []byte(fmt.Sprintf("bank/fwd/%p", cap))
+	key := fmt.Sprintf("%#010p", cap)
+	if len(key) > 10 {
+		key = key[len(key)-10:]
+	}
+	require.Equal(t, 10, len(key))
+	expected := []byte(fmt.Sprintf("bank/fwd/0x%s", key))
 	require.Equal(t, expected, types.FwdCapabilityKey("bank", cap))
 }
 
@@ -26,4 +32,22 @@ func TestIndexToKey(t *testing.T) {
 
 func TestIndexFromKey(t *testing.T) {
 	require.Equal(t, uint64(3162), types.IndexFromKey([]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xc, 0x5a}))
+}
+
+// to test the backward compatibiltiy of the new function
+func legacyFwdCapabilityKey(module string, cap *types.Capability) []byte {
+	return []byte(fmt.Sprintf("%s/fwd/%p", module, cap))
+}
+
+func TestFwdCapabilityKeyCompatibility(t *testing.T) {
+	cap := types.NewCapability(24)
+	new := types.FwdCapabilityKey("bank", cap)
+	old := legacyFwdCapabilityKey("bank", cap)
+	if runtime.GOOS == "darwin" && runtime.GOARCH == "arm" {
+		// the legacy version has 1 more byte on mac m1
+		require.Equal(t, len(old), len(new)+1)
+	} else {
+		// otherwise, the new version is identical
+		require.Equal(t, new, old)
+	}
 }

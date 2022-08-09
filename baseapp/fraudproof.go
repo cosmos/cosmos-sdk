3b55ff2bd5f6423dd577a46fb8f36c081b58e34e
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-08-09 15:16:23 -0400

    Refactor verify fraud proof into one method

--- baseapp/fraudproof.go
+++ baseapp/fraudproof.go
@@ -1,6 +1,11 @@
 package baseapp
 
 import (
+	"bytes"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/store/v2alpha1/smt"
 	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
 )
 
@@ -27,4 +32,44 @@ type WitnessData struct {
 	Key   []byte
 	Value []byte
 	proof tmcrypto.ProofOp
+}
+
+func (fraudProof *FraudProof) verifyFraudProof(headerAppHash []byte) (bool, error) {
+	for storeKey, stateWitness := range fraudProof.stateWitness {
+		proofOp := stateWitness.proof
+		proof, err := types.CommitmentOpDecoder(proofOp)
+		if err != nil {
+			return false, err
+		}
+		if !bytes.Equal(proof.GetKey(), []byte(storeKey)) {
+			return false, fmt.Errorf("got storeKey: %s, expected: %s", string(proof.GetKey()), storeKey)
+		}
+		appHash, err := proof.Run([][]byte{stateWitness.rootHash})
+		if err != nil {
+			return false, err
+		}
+		if !bytes.Equal(appHash[0], headerAppHash) {
+			return false, fmt.Errorf("got appHash: %s, expected: %s", string(headerAppHash), string(headerAppHash))
+		}
+
+		// Fraudproof verification on a substore level
+		for _, witness := range stateWitness.WitnessData {
+			proofOp, key, value := witness.proof, witness.Key, witness.Value
+			proof, err := smt.ProofDecoder(proofOp)
+			if err != nil {
+				return false, err
+			}
+			if !bytes.Equal(key, proof.GetKey()) {
+				return false, fmt.Errorf("got key: %s, expected: %s for storeKey: %s", string(key), string(proof.GetKey()), storeKey)
+			}
+			rootHash, err := proof.Run([][]byte{value})
+			if err != nil {
+				return false, err
+			}
+			if !bytes.Equal(rootHash[0], stateWitness.rootHash) {
+				return false, fmt.Errorf("got rootHash: %s, expected: %s for storeKey: %s", string(rootHash[0]), string(stateWitness.rootHash), storeKey)
+			}
+		}
+	}
+	return true, nil
 }

9312079b3afbb0d127bf001fcc49e4d83e9170d1
Jacob Gadikian <jacobgadikian@gmail.com>
2022-08-09 16:52:03 +0700

    golangci-lint run ./... --fix

--- crypto/keys/secp256k1/internal/secp256k1/curve.go
+++ crypto/keys/secp256k1/internal/secp256k1/curve.go
@@ -29,7 +29,7 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// nolint // this nolint lets us use this file in its original and unmodified form.
+//nolint // this nolint lets us use this file in its original and unmodified form.
 package secp256k1
 
 import (

04ff89822e81a9677354b636b97056683b7ac101
Matt Kocubinski <mkocubinski@gmail.com>
2022-08-10 15:43:13 -0500

    remove simapp from slashing test

--- x/slashing/app_test.go
+++ x/slashing/app_test.go
@@ -5,7 +5,11 @@ import (
 	"testing"
 
 	"cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	"github.com/cosmos/cosmos-sdk/testutil/sims"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	"github.com/cosmos/cosmos-sdk/x/slashing/keeper"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -13,11 +17,9 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/slashing/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -30,20 +32,6 @@ var (
 	valAddr = sdk.AccAddress(valKey.PubKey().Address())
 )
 
-func checkValidator(t *testing.T, app *simapp.SimApp, _ sdk.AccAddress, expFound bool) stakingtypes.Validator {
-	ctxCheck := app.BaseApp.NewContext(true, tmproto.Header{})
-	validator, found := app.StakingKeeper.GetValidator(ctxCheck, sdk.ValAddress(addr1))
-	require.Equal(t, expFound, found)
-	return validator
-}
-
-func checkValidatorSigningInfo(t *testing.T, app *simapp.SimApp, addr sdk.ConsAddress, expFound bool) types.ValidatorSigningInfo {
-	ctxCheck := app.BaseApp.NewContext(true, tmproto.Header{})
-	signingInfo, found := app.SlashingKeeper.GetValidatorSigningInfo(ctxCheck, addr)
-	require.Equal(t, expFound, found)
-	return signingInfo
-}
-
 func TestSlashingMsgs(t *testing.T) {
 	genTokens := sdk.TokensFromConsensusPower(42, sdk.DefaultPowerReduction)
 	bondTokens := sdk.TokensFromConsensusPower(10, sdk.DefaultPowerReduction)
@@ -53,16 +41,32 @@ func TestSlashingMsgs(t *testing.T) {
 	acc1 := &authtypes.BaseAccount{
 		Address: addr1.String(),
 	}
-	accs := authtypes.GenesisAccounts{acc1}
-	balances := []banktypes.Balance{
-		{
-			Address: addr1.String(),
-			Coins:   sdk.Coins{genCoin},
-		},
-	}
+	accs := []sims.GenesisAccount{{GenesisAccount: acc1, Coins: sdk.Coins{genCoin}}}
 
-	app := simapp.SetupWithGenesisAccounts(t, accs, balances...)
-	simapp.CheckBalance(t, app, addr1, sdk.Coins{genCoin})
+	startupCfg := sims.DefaultStartUpConfig()
+	startupCfg.GenesisAccounts = accs
+
+	var (
+		stakingKeeper  *stakingkeeper.Keeper
+		bankKeeper     bankkeeper.Keeper
+		slashingKeeper keeper.Keeper
+	)
+
+	app, err := sims.SetupWithConfiguration(configurator.NewAppConfig(
+		configurator.ParamsModule(),
+		configurator.AuthModule(),
+		configurator.StakingModule(),
+		configurator.SlashingModule(),
+		configurator.TxModule(),
+		configurator.BankModule()),
+		startupCfg, &stakingKeeper, &bankKeeper, &slashingKeeper)
+
+	baseApp := app.BaseApp
+
+	ctxCheck := baseApp.NewContext(true, tmproto.Header{})
+	require.True(t, sdk.Coins{genCoin}.IsEqual(bankKeeper.GetAllBalances(ctxCheck, addr1)))
+
+	require.NoError(t, err)
 
 	description := stakingtypes.NewDescription("foo_moniker", "", "", "", "")
 	commission := stakingtypes.NewCommissionRates(math.LegacyZeroDec(), math.LegacyZeroDec(), math.LegacyZeroDec())
@@ -76,18 +80,22 @@ func TestSlashingMsgs(t *testing.T) {
 	txConfig := moduletestutil.MakeTestEncodingConfig().TxConfig
 	_, _, err = sims.SignCheckDeliver(t, txConfig, app.BaseApp, header, []sdk.Msg{createValidatorMsg}, "", []uint64{0}, []uint64{0}, true, true, priv1)
 	require.NoError(t, err)
-	simapp.CheckBalance(t, app, addr1, sdk.Coins{genCoin.Sub(bondCoin)})
+	require.True(t, sdk.Coins{genCoin.Sub(bondCoin)}.IsEqual(bankKeeper.GetAllBalances(ctxCheck, addr1)))
 
 	header = tmproto.Header{Height: app.LastBlockHeight() + 1}
 	app.BeginBlock(abci.RequestBeginBlock{Header: header})
 
-	validator := checkValidator(t, app, addr1, true)
+	ctxCheck = baseApp.NewContext(true, tmproto.Header{})
+	validator, found := stakingKeeper.GetValidator(ctxCheck, sdk.ValAddress(addr1))
+	require.True(t, found)
 	require.Equal(t, sdk.ValAddress(addr1).String(), validator.OperatorAddress)
 	require.Equal(t, stakingtypes.Bonded, validator.Status)
 	require.True(math.IntEq(t, bondTokens, validator.BondedTokens()))
 	unjailMsg := &types.MsgUnjail{ValidatorAddr: sdk.ValAddress(addr1).String()}
 
-	checkValidatorSigningInfo(t, app, sdk.ConsAddress(valAddr), true)
+	ctxCheck = app.BaseApp.NewContext(true, tmproto.Header{})
+	_, found = slashingKeeper.GetValidatorSigningInfo(ctxCheck, sdk.ConsAddress(valAddr))
+	require.True(t, found)
 
 	// unjail should fail with unknown validator
 	header = tmproto.Header{Height: app.LastBlockHeight() + 1}

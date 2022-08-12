6cf1eb4fed454378529afb003eb7a82e08b33866
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-08-11 21:49:05 -0400

    Update documentation

--- baseapp/baseapp_test.go
+++ baseapp/baseapp_test.go
@@ -2285,12 +2285,13 @@ func TestGenerateAndLoadFraudProof(t *testing.T) {
 	/*
 		Tests switch between a baseapp and fraudproof and covers parts of the fraudproof cycle. Steps:
 		1. Initialize a baseapp, B1, with some state, S0
-		2. Make some state transition to state S1 by doing some transactions
-		3. Export that state S1 into a fraudProof data structure (minimal snapshot)
-		4. Load a fresh baseapp, B2, with the contents of fraud proof data structure from (3) so it can begin from state S1.
-		5. Now, pick some set of transactions, txs1, to make some more state transitions.
-		6. Execute txs1 on both B1 and B2 so they go through the same state transitions
-		7. If the state of both B1 and B2 has to converge at a state S2, then we the test passes.
+		2. Make some state transition to state S1 by doing some transactions, commit those transactions, and save
+		resulting SMT root hash
+		3. Make another set of state transitions to state S2 but do not commit
+		4. Generate a fraudproof which should ignore the uncommitted set of transactions, and export S1 into a fraudProof data structure (minimal snapshot)
+		5. Verify the fraudproof and check verification passes (done in light client)
+		6. Load a fresh baseapp, B2, with the contents of fraud proof data structure from (4) so it can begin from state S1.
+		7. Check if the SMT root hashes of the new app with saved SMT root hash
 
 		Note that the appHash from B1 and B2 will not be the same because the subset of storeKeys in both apps is different
 		so we compare subStores instead
@@ -2301,10 +2302,6 @@ func TestGenerateAndLoadFraudProof(t *testing.T) {
 		2. Block with invalid appHash at the end
 		3. Corrupted Fraud Proof: bad SMT format, insufficient key-value pairs inside SMT needed to verify fraud
 		4. Bad block, fraud proof needed, fraud proof works, chain halts (happy case)
-
-		Notes: In the current implementation, all substores might not be SMTs, but we assume they are for the prototype here
-		Try to keep tx as generic as possible so you don't need to care about the messages inside a Tx
-
 	*/
 
 	storeTraceBuf := &bytes.Buffer{}

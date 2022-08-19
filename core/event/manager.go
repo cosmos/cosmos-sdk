7f9b15c06167564a489cccaec56bee82c518f1cb
Aaron Craelius <aaronc@users.noreply.github.com>
2022-08-19 17:56:59 +0200

    updates

--- core/event/manager.go
+++ core/event/manager.go
@@ -13,9 +13,6 @@ import (
 type Service interface {
 	// GetManager returns the event manager for the context or a no-op event manager if one isn't attached.
 	GetManager(context.Context) Manager
-
-	// WithManager returns a new context with the provided event manager attached.
-	WithManager(context.Context, Manager) context.Context
 }
 
 // Manager represents an event manager.

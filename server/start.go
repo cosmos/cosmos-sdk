83b8dc157196a23829aaeeb417527eabf74da0a1
Lior Bondarevski <lior@scrtlabs.com>
2022-08-21 12:59:28 +0300

    Print rellevant log when parsing the config

--- server/start.go
+++ server/start.go
@@ -242,7 +242,11 @@ func startInProcess(ctx *Context, clientCtx client.Context, appCreator types.App
 		return err
 	}
 
-	config := config.GetConfig(ctx.Viper)
+	config, err := config.GetConfig(ctx.Viper)
+	if err != nil {
+		return err
+	}
+
 	if err := config.ValidateBasic(); err != nil {
 		ctx.Logger.Error("WARNING: The minimum-gas-prices config in app.toml is set to the empty string. " +
 			"This defaults to 0 in the current version, but will error in the next version " +

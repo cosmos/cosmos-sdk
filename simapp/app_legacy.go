969d6fc83980df22cfe112a1cebdce3956a37772
Robert Zaremba <robert@zaremba.ch>
2022-08-22 11:40:25 +0200

    update all_legacy

--- simapp/app_legacy.go
+++ simapp/app_legacy.go
@@ -108,7 +108,7 @@ var (
 	// and genesis verification.
 	ModuleBasics = module.NewBasicManager(
 		auth.AppModuleBasic{},
-		genutil.AppModuleBasic{},
+		genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
 		bank.AppModuleBasic{},
 		capability.AppModuleBasic{},
 		staking.AppModuleBasic{},

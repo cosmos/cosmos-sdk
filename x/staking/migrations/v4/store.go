184bfd878b684ef9a0f9f81e44777dd08b5ebd16
Sai Kumar <gskumar967@gmail.com>
2022-08-23 11:11:04 +0530

    chore: address the pr comments ++

--- x/staking/migrations/v4/store.go
+++ x/staking/migrations/v4/store.go
@@ -1,6 +1,8 @@
 package v4
 
 import (
+	"sort"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -21,5 +23,58 @@ func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.Binar
 	bz := cdc.MustMarshal(&legacyParams)
 	store.Set(types.ParamsKey, bz)
 
+	// this will remove the ubdEntries with same creation_height
+	// and create a new ubdEntry with updated balance and initial_balance
+
+	iterator := sdk.KVStorePrefixIterator(store, types.UnbondingDelegationKey)
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		ubd := types.MustUnmarshalUBD(cdc, iterator.Value())
+
+		entriesAtSameCreationHeight := make(map[int64][]types.UnbondingDelegationEntry)
+		for _, ubdEntry := range ubd.Entries {
+			entriesAtSameCreationHeight[ubdEntry.CreationHeight] = append(entriesAtSameCreationHeight[ubdEntry.CreationHeight], ubdEntry)
+		}
+
+		creationHeights := make([]int64, 0, len(entriesAtSameCreationHeight))
+		for k := range entriesAtSameCreationHeight {
+			creationHeights = append(creationHeights, k)
+		}
+
+		sort.Slice(creationHeights, func(i, j int) bool { return creationHeights[i] < creationHeights[j] })
+
+		ubd.Entries = make([]types.UnbondingDelegationEntry, 0, len(creationHeights))
+
+		for _, h := range creationHeights {
+			var ubdEntry types.UnbondingDelegationEntry
+			for _, entry := range entriesAtSameCreationHeight[h] {
+				ubdEntry.Balance = ubdEntry.Balance.Add(entry.Balance)
+				ubdEntry.InitialBalance = ubdEntry.InitialBalance.Add(entry.InitialBalance)
+				ubdEntry.CreationHeight = entry.CreationHeight
+				ubdEntry.CompletionTime = entry.CompletionTime
+			}
+			ubd.Entries = append(ubd.Entries, ubdEntry)
+		}
+
+		// set the new ubd to the store
+		setUBDToStore(ctx, store, cdc, ubd)
+	}
+
 	return nil
+}
+
+func setUBDToStore(ctx sdk.Context, store storetypes.KVStore, cdc codec.BinaryCodec, ubd types.UnbondingDelegation) {
+	delegatorAddress := sdk.MustAccAddressFromBech32(ubd.DelegatorAddress)
+
+	bz := types.MustMarshalUBD(cdc, ubd)
+
+	addr, err := sdk.ValAddressFromBech32(ubd.ValidatorAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	key := types.GetUBDKey(delegatorAddress, addr)
+
+	store.Set(key, bz)
 }

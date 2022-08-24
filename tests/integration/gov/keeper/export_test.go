32b7e8c8c57d58e00bb8ebf3432d89c5b67bb49f
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-08-24 15:02:38 +0200

    Make tests/integration pass

--- tests/integration/gov/keeper/export_test.go
+++
@@ -1,7 +0,0 @@
-package keeper
-
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
-func (k Keeper) ValidateInitialDeposit(ctx sdk.Context, initialDeposit sdk.Coins) error {
-	return k.validateInitialDeposit(ctx, initialDeposit)
-}

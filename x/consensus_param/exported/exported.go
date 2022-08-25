6d03157fbdbc886fe560ef7af4e574274b722358
Jeancarlo <jeancarlobarrios@gmail.com>
2022-08-24 19:39:18 -0600

    add legacy exported + keeper and module dep inject functions

--- x/consensus_param/exported/exported.go
+++ x/consensus_param/exported/exported.go
@@ -5,10 +5,20 @@ import (
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-// ProtocolVersionSetter defines the interface fulfilled by BaseApp
-// which allows setting it's appVersion field.
-type ConsensusParamSetter interface {
-	Get(ctx sdk.Context) (*tmproto.ConsensusParams, error)
-	Has(ctx sdk.Context) bool
-	Set(ctx sdk.Context, cp *tmproto.ConsensusParams)
-}
+type (
+	// Subspace defines an interface that implements the legacy x/params Subspace
+	// type.
+	//
+	// NOTE: This is used solely for migration of x/params managed parameters.
+	Subspace interface {
+		Get(ctx sdk.Context) (*tmproto.ConsensusParams, error)
+	}
+
+	// ConsensusParamSetter ProtocolVersionSetter defines the interface fulfilled by BaseApp
+	// which allows setting its appVersion field.
+	ConsensusParamSetter interface {
+		Get(ctx sdk.Context) (*tmproto.ConsensusParams, error)
+		Has(ctx sdk.Context) bool
+		Set(ctx sdk.Context, cp *tmproto.ConsensusParams)
+	}
+)

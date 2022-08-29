da8a72aa1526120d58ca511d5af1ef7a4f66baad
Riley Edmunds <riley@stridelabs.co>
2022-08-29 01:51:42 -0400

    move case around

--- x/distribution/keeper/allocation.go
+++ x/distribution/keeper/allocation.go
@@ -247,10 +247,6 @@ func (k Keeper) AllocateTokens(
 	// TODO consider parallelizing later, ref https://github.com/cosmos/cosmos-sdk/pull/3099#discussion_r246276376
 	k.Logger(ctx).Info(fmt.Sprintf("\n...\n\n"))
 	adjustedTotalPower := sdk.NewDec(totalPreviousPower).Mul(totalWhitelistedPowerShare).TruncateInt64() // TODO might rounding cause issues later?
-	// If all we have is blacklisted delegations, stop here! | TODO clean up this case
-	if adjustedTotalPower == 0 {
-		return
-	}
 	for _, vote := range bondedVotes {
 
 		validator := k.stakingKeeper.ValidatorByConsAddr(ctx, vote.Validator.Address)
@@ -261,7 +257,7 @@ func (k Keeper) AllocateTokens(
 
 		var powerFraction sdk.Dec
 		// reduce the validator's power if they are tainted
-		if k.StringInSlice(valAddr, taintedVals) {
+		if k.StringInSlice(valAddr, taintedVals) && adjustedTotalPower != 0 { // If all we have is blacklisted delegations, process normally | TODO clean up this case
 			validatorPowerAdjNew := sdk.NewDec(validatorPowerAdj).Mul(taintedValsWhitelistedPowerShare[valAddr]).TruncateInt64()
 			k.Logger(ctx).Info(fmt.Sprintf("...reducing val %s power: %d * %d ===> %d ", valAddr, validatorPowerAdj, taintedValsWhitelistedPowerShare[valAddr], validatorPowerAdjNew))
 			validatorPowerAdj = validatorPowerAdjNew

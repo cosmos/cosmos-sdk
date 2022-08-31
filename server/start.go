cb85e5fac7eb35e1fc68b76fd017c4420543c9c3
Facundo Medica <facundomedica@gmail.com>
2022-08-31 11:44:26 -0700

    replace all usaged of tmos

--- server/start.go
+++ server/start.go
@@ -13,7 +13,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/abci/server"
 	tcmd "github.com/tendermint/tendermint/cmd/tendermint/commands"
-	tmos "github.com/tendermint/tendermint/libs/os"
 	"github.com/tendermint/tendermint/node"
 	"github.com/tendermint/tendermint/p2p"
 	pvm "github.com/tendermint/tendermint/privval"
@@ -229,12 +228,14 @@ func startStandAlone(ctx *Context, appCreator types.AppCreator) error {
 
 	err = svr.Start()
 	if err != nil {
-		tmos.Exit(err.Error())
+		fmt.Printf(err.Error() + "\n")
+		os.Exit(1)
 	}
 
 	defer func() {
 		if err = svr.Stop(); err != nil {
-			tmos.Exit(err.Error())
+			fmt.Printf(err.Error() + "\n")
+			os.Exit(1)
 		}
 	}()
 

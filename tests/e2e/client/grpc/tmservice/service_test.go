b829a1f0be179f5d05a953f3a4f1e04615d70215
Sai Kumar <gskumar967@gmail.com>
2022-09-01 18:16:52 +0530

    fix: fix the tests

--- tests/e2e/client/grpc/tmservice/service_test.go
+++ tests/e2e/client/grpc/tmservice/service_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types"
 	qtypes "github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/cosmos-sdk/version"
+	_ "github.com/cosmos/cosmos-sdk/x/distribution"
 	_ "github.com/cosmos/cosmos-sdk/x/gov"
 )
 
@@ -44,6 +45,7 @@ func (s *IntegrationTestSuite) SetupSuite() {
 		configurator.GenutilModule(),
 		configurator.StakingModule(),
 		configurator.GovModule(),
+		configurator.DistributionModule(),
 		configurator.TxModule())
 
 	cfg, err := network.DefaultConfigWithAppConfig(appConfig)

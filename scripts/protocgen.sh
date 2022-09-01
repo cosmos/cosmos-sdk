371fb52e366e28d747be3343ca7fa4d8f4e19490
Julien Robert <julien@rbrt.fr>
2022-09-01 13:10:53 +0200

    refactor: generate local tendermint types

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
-#== Requirements ==
-#
-## make sure your `go env GOPATH` is in the `$PATH`
-## Install:
-## + latest buf (v1.0.0-rc11 or later)
-## + protobuf v3
-#
-## All protoc dependencies must be installed not in the module scope
-## currently we must use grpc-gateway v1
-# cd ~
-# go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-# go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.16.0
-# go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
-# go get github.com/regen-network/cosmos-proto@latest # doesn't work in install mode
-# go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@v0.3.1
+# How to run manually:
+# docker build --pull --rm -f "contrib/devtools/Dockerfile" -t cosmossdk-proto:latest "contrib/devtools"
+# docker run --rm -v $(pwd):/workspace --workdir /workspace cosmossdk-proto sh ./scripts/protocgen.sh
 
 set -eo pipefail
 
-echo "Generating gogo proto code"
+echo "Generating cosmos gogo proto code"
 cd proto
 proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
@@ -30,18 +17,34 @@ for dir in $proto_dirs; do
   done
 done
 
+echo "Generating types/tendermint gogo proto code"
+proto_dirs=$(find ./tendermint -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep "option go_package" $file &> /dev/null ; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
+
 cd ..
 
-# generate codec/testdata proto code
+echo "Generate codec/testdata proto code"
 (cd testutil/testdata; buf generate)
 
-# generate baseapp test messages
+echo "Generate baseapp test messages"
 (cd baseapp/testutil; buf generate)
 
-# move proto files to the right places
+echo "Move proto files to the right places"
 cp -r github.com/cosmos/cosmos-sdk/* ./
+cp -r github.com/tendermint/tendermint/proto/tendermint/* ./types/tendermint/
+cp -r github.com/tendermint/tendermint/abci* ./types/tendermint
 rm -rf github.com
 
-go mod tidy -compat=1.18
+echo "Rewrite tendermint proto files"
+find ./types/tendermint -type f -exec sed -i 's/github.com\/tendermint\/tendermint\/proto\/tendermint/github.com\/cosmos\/cosmos-sdk\/types\/tendermint/' {} \;
+find ./types/tendermint -type f -exec sed -i 's/github.com\/tendermint\/tendermint\/abci\/types/github.com\/cosmos\/cosmos-sdk\/types\/abci\/types/' {} \;
+
+go mod tidy
 
 ./scripts/protocgen-pulsar.sh

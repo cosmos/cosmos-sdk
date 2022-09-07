871391b5bac51c2eaf83fb8cb037cad3220a4506
faddat <jacobgadikian@gmail.com>
2022-09-07 20:26:18 +0000

    format

--- server/mock/tx.go
+++ server/mock/tx.go
@@ -1,4 +1,4 @@
-//nolint
+// nolint
 package mock
 
 import (
--- types/errors/doc.go
+++ types/errors/doc.go
@@ -11,24 +11,23 @@ of the errors package. If it will be needed my many extensions, please consider
 registering it in the errors package. To create a new error instance use Register
 function. You must provide a unique, non zero error code and a short description, for example:
 
-  var ErrZeroDivision = errors.Register(9241, "zero division")
+	var ErrZeroDivision = errors.Register(9241, "zero division")
 
 When returning an error, you can attach to it an additional context
 information by using Wrap function, for example:
 
-   func safeDiv(val, div int) (int, err) {
-	   if div == 0 {
-		   return 0, errors.Wrapf(ErrZeroDivision, "cannot divide %d", val)
+	   func safeDiv(val, div int) (int, err) {
+		   if div == 0 {
+			   return 0, errors.Wrapf(ErrZeroDivision, "cannot divide %d", val)
+		   }
+		   return val / div, nil
 	   }
-	   return val / div, nil
-   }
 
 The first time an error instance is wrapped a stacktrace is attached as well.
 Stacktrace information can be printed using %+v and %v formats.
 
-  %s  is just the error message
-  %+v is the full stack trace
-  %v  appends a compressed [filename:line] where the error was created
-
+	%s  is just the error message
+	%+v is the full stack trace
+	%v  appends a compressed [filename:line] where the error was created
 */
 package errors

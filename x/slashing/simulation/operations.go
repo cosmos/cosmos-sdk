60015544104fa5cbe73b5c753eb8ee4a949ad643
faddat <jacobgadikian@gmail.com>
2022-09-08 12:15:17 +0000

    linting fixes

--- x/slashing/simulation/operations.go
+++ x/slashing/simulation/operations.go
@@ -18,7 +18,7 @@ import (
 
 // Simulation operation weights constants
 const (
-	OpWeightMsgUnjail = "op_weight_msg_unjail"
+	OpWeightMsgUnjail = "op_weight_msg_unjail" //nolint:gosec
 )
 
 // WeightedOperations returns all the operations from the module with their respective weights

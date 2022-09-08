720f8f7ae081abf18025fc11bcbfc9ffd57d927a
Daniel Wedul <github@wedul.com>
2022-09-08 17:52:29 -0600

    [11859]: Fix import of cosmossdk.io/simapp (was github.com/cosmos/cosmos-sdk/simapp) in x/bank/app_test.go and update other go mod stuff associated with that.

--- x/bank/app_test.go
+++ x/bank/app_test.go
@@ -7,11 +7,11 @@ import (
 	"github.com/stretchr/testify/require"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
+	"cosmossdk.io/simapp"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/runtime"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"

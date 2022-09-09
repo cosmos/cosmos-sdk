11845378f1790d1a3b72c5db978496fd38e4640f
jay tseng <jay.tseng@crypto.com>
2022-09-09 15:55:59 -0400

    remove extra line

--- simapp/simd/cmd/root.go
+++ simapp/simd/cmd/root.go
@@ -176,7 +176,6 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, simapp.DefaultNodeHome,
 			gentxModule.GenTxValidator),
 		genutilcli.MigrateGenesisCmd(),
-
 		genutilcli.GenTxCmd(simapp.ModuleBasics, encodingConfig.TxConfig,
 			banktypes.GenesisBalancesIterator{}, simapp.DefaultNodeHome),
 		genutilcli.ValidateGenesisCmd(simapp.ModuleBasics),

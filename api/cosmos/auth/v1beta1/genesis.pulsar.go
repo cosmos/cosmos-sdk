57852904e3a807616576de2cee3bd7eec2061702
Aaron Craelius <aaronc@users.noreply.github.com>
2022-09-12 09:47:01 -0400

    proto-gen

--- api/cosmos/auth/v1beta1/genesis.pulsar.go
+++ api/cosmos/auth/v1beta1/genesis.pulsar.go
@@ -3,16 +3,15 @@ package authv1beta1
 
 import (
 	fmt "fmt"
-	io "io"
-	reflect "reflect"
-	sync "sync"
-
 	runtime "github.com/cosmos/cosmos-proto/runtime"
 	_ "github.com/cosmos/gogoproto/gogoproto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoiface "google.golang.org/protobuf/runtime/protoiface"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	anypb "google.golang.org/protobuf/types/known/anypb"
+	io "io"
+	reflect "reflect"
+	sync "sync"
 )
 
 var _ protoreflect.List = (*_GenesisState_2_list)(nil)

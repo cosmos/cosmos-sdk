08b0283a30e79de5c9a73d1b1a2f1d623a0eabcc
Jeancarlo <jeancarlobarrios@gmail.com>
2022-09-14 08:20:42 -0600

    added base for select benchmark

--- types/mempool_test.go
+++ types/mempool_test.go
@@ -33,12 +33,42 @@ type ConfigurableBenchmarker struct {
 func TestInsertMemPool(t *testing.T) {
 	mPool := types.NewBTreeMempool(smallSize)
 	ctx := types.NewContext(nil, tmproto.Header{}, false, log.NewNopLogger())
-	tx := SimulateTx(ctx)
+	tx := simulateTx(ctx)
 	err := mPool.Insert(ctx, TxWSize{tx})
 	require.NoError(t, err)
 }
 
-func SimulateTx(ctx types.Context) types.Tx {
+func BenchmarkBtreeMempool_Insert(b *testing.B) {
+	mPool := types.NewBTreeMempool(smallSize)
+	ctx := types.NewContext(nil, tmproto.Header{}, false, log.NewNopLogger())
+	tx := simulateTx(ctx)
+	for i := 0; i < b.N; i++ {
+		mPool.Insert(ctx, TxWSize{tx})
+	}
+}
+
+func BenchmarkBtreeMempool_Select(b *testing.B) {
+	mPool := types.NewBTreeMempool(smallSize)
+	ctx := types.NewContext(nil, tmproto.Header{}, false, log.NewNopLogger())
+	transactions := simulateManyTx(ctx, 1000000)
+	for _, t := range transactions {
+		mPool.Insert(ctx, TxWSize{t})
+	}
+	for i := 0; i < b.N; i++ {
+		mPool.Select(ctx, nil, 10000)
+	}
+}
+
+func simulateManyTx(ctx types.Context, n int) []types.Tx {
+	transactions := make([]types.Tx, n)
+	for i := 0; i < n; i++ {
+		tx := simulateTx(ctx)
+		transactions[i] = tx
+	}
+	return transactions
+}
+
+func simulateTx(ctx types.Context) types.Tx {
 	s := rand.NewSource(1)
 	r := rand.New(s)
 	msg := group.MsgUpdateGroupMembers{

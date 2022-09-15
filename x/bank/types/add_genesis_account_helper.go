4f45d36b77ef0b5a06d7f867809c2c8c2aefae8d
likhita-809 <likhita@vitwit.com>
2022-09-15 12:48:28 +0530

    add genesis account helper

---
+++ x/bank/types/add_genesis_account_helper.go
@@ -0,0 +1,99 @@
+package types
+
+import (
+	"encoding/json"
+	fmt "fmt"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module/testutil"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+)
+
+func AddGenesisAccount(path, moniker, amountStr string, accAddr sdk.AccAddress) error {
+	serverCtx := server.NewDefaultContext()
+	config := serverCtx.Config
+
+	config.SetRoot(path)
+	config.Moniker = moniker
+
+	cfg := testutil.MakeTestEncodingConfig()
+
+	coins, err := sdk.ParseCoinsNormalized(amountStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse coins: %w", err)
+	}
+
+	balances := Balance{Address: accAddr.String(), Coins: coins.Sort()}
+	genAccount := authtypes.NewBaseAccount(accAddr, nil, 0, 0)
+
+	// if err := genAccount.Validate(); err != nil {
+	// 	return fmt.Errorf("failed to validate new genesis account: %w", err)
+	// }
+
+	// TODO: Make the SDK make it far cleaner to add an account to GenesisState
+	genFile := config.GenesisFile()
+	appState, genDoc, err := genutiltypes.GenesisStateFromGenFile(genFile)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal genesis state: %w", err)
+	}
+
+	authGenState := authtypes.GetGenesisStateFromAppState(cfg.Codec, appState)
+
+	accs, err := authtypes.UnpackAccounts(authGenState.Accounts)
+	if err != nil {
+		return fmt.Errorf("failed to get accounts from any: %w", err)
+	}
+
+	bankGenState := GetGenesisStateFromAppState(cfg.Codec, appState)
+	if accs.Contains(accAddr) {
+		genesisB := GetGenesisStateFromAppState(cfg.Codec, appState)
+		for idx, acc := range genesisB.Balances {
+			if acc.Address != accAddr.String() {
+				continue
+			}
+
+			updatedCoins := acc.Coins.Add(coins...)
+			bankGenState.Balances[idx] = Balance{Address: accAddr.String(), Coins: updatedCoins.Sort()}
+			break
+		}
+	} else {
+		// Add the new account to the set of genesis accounts and sanitize the accounts afterwards.
+		accs = append(accs, genAccount)
+		accs = authtypes.SanitizeGenesisAccounts(accs)
+
+		genAccs, err := authtypes.PackAccounts(accs)
+		if err != nil {
+			return fmt.Errorf("failed to convert accounts into any's: %w", err)
+		}
+		authGenState.Accounts = genAccs
+
+		authGenStateBz, err := cfg.Codec.MarshalJSON(&authGenState)
+		if err != nil {
+			return fmt.Errorf("failed to marshal auth genesis state: %w", err)
+		}
+		appState[authtypes.ModuleName] = authGenStateBz
+
+		bankGenState.Balances = append(bankGenState.Balances, balances)
+	}
+
+	bankGenState.Balances = SanitizeGenesisBalances(bankGenState.Balances)
+
+	bankGenState.Supply = bankGenState.Supply.Add(balances.Coins...)
+
+	bankGenStateBz, err := cfg.Codec.MarshalJSON(bankGenState)
+	if err != nil {
+		return fmt.Errorf("failed to marshal bank genesis state: %w", err)
+	}
+	appState[ModuleName] = bankGenStateBz
+
+	appStateJSON, err := json.Marshal(appState)
+	if err != nil {
+		return fmt.Errorf("failed to marshal application genesis state: %w", err)
+	}
+
+	genDoc.AppState = appStateJSON
+	return genutil.ExportGenesisFile(genDoc, genFile)
+}

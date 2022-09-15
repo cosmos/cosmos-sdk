049c2088fbdca374468661028ba0bef71abf4d2a
likhita-809 <likhita@vitwit.com>
2022-09-15 16:41:48 +0530

    wip

--- x/auth/helpers/add_genesis_account_helper.go
+++ x/auth/helpers/add_genesis_account_helper.go
@@ -1,34 +1,67 @@
-package auth
+package helpers
 
 import (
 	"encoding/json"
+	"errors"
 	fmt "fmt"
 
 	"github.com/cosmos/cosmos-sdk/server"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
+	"github.com/cosmos/cosmos-sdk/x/auth"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	authvesting "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 )
 
-func AddGenesisAccount(path, moniker, amountStr string, accAddr sdk.AccAddress) error {
+func AddGenesisAccount(path, moniker, amountStr string, accAddr sdk.AccAddress, appendAcct bool, vestingStart, vestingEnd int64, vestingAmtStr string) error {
 	serverCtx := server.NewDefaultContext()
 	config := serverCtx.Config
 
 	config.SetRoot(path)
 	config.Moniker = moniker
 
-	cfg := testutil.MakeTestEncodingConfig()
+	encCfg := testutil.MakeTestEncodingConfig(auth.AppModuleBasic{})
 
 	coins, err := sdk.ParseCoinsNormalized(amountStr)
 	if err != nil {
 		return fmt.Errorf("failed to parse coins: %w", err)
 	}
 
+	vestingAmt, err := sdk.ParseCoinsNormalized(vestingAmtStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse vesting amount: %w", err)
+	}
+
+	// create concrete account type based on input parameters
+	var genAccount authtypes.GenesisAccount
+
 	balances := banktypes.Balance{Address: accAddr.String(), Coins: coins.Sort()}
-	genAccount := authtypes.NewBaseAccount(accAddr, nil, 0, 0)
+	baseAccount := authtypes.NewBaseAccount(accAddr, nil, 0, 0)
+
+	if !vestingAmt.IsZero() {
+		baseVestingAccount := authvesting.NewBaseVestingAccount(baseAccount, vestingAmt.Sort(), vestingEnd)
+
+		if (balances.Coins.IsZero() && !baseVestingAccount.OriginalVesting.IsZero()) ||
+			baseVestingAccount.OriginalVesting.IsAnyGT(balances.Coins) {
+			return errors.New("vesting amount cannot be greater than total amount")
+		}
+
+		switch {
+		case vestingStart != 0 && vestingEnd != 0:
+			genAccount = authvesting.NewContinuousVestingAccountRaw(baseVestingAccount, vestingStart)
+
+		case vestingEnd != 0:
+			genAccount = authvesting.NewDelayedVestingAccountRaw(baseVestingAccount)
+
+		default:
+			return errors.New("invalid vesting parameters; must supply start and end time or end time")
+		}
+	} else {
+		genAccount = baseAccount
+	}
 
 	if err := genAccount.Validate(); err != nil {
 		return fmt.Errorf("failed to validate new genesis account: %w", err)
@@ -40,16 +73,20 @@ func AddGenesisAccount(path, moniker, amountStr string, accAddr sdk.AccAddress) 
 		return fmt.Errorf("failed to unmarshal genesis state: %w", err)
 	}
 
-	authGenState := authtypes.GetGenesisStateFromAppState(cfg.Codec, appState)
+	authGenState := authtypes.GetGenesisStateFromAppState(encCfg.Codec, appState)
 
 	accs, err := authtypes.UnpackAccounts(authGenState.Accounts)
 	if err != nil {
 		return fmt.Errorf("failed to get accounts from any: %w", err)
 	}
 
-	bankGenState := banktypes.GetGenesisStateFromAppState(cfg.Codec, appState)
+	bankGenState := banktypes.GetGenesisStateFromAppState(encCfg.Codec, appState)
 	if accs.Contains(accAddr) {
-		genesisB := banktypes.GetGenesisStateFromAppState(cfg.Codec, appState)
+		if !appendAcct {
+			return fmt.Errorf(" Account %s already exists\nUse `append` flag to append account at existing address", accAddr)
+		}
+
+		genesisB := banktypes.GetGenesisStateFromAppState(encCfg.Codec, appState)
 		for idx, acc := range genesisB.Balances {
 			if acc.Address != accAddr.String() {
 				continue
@@ -70,7 +107,7 @@ func AddGenesisAccount(path, moniker, amountStr string, accAddr sdk.AccAddress) 
 		}
 		authGenState.Accounts = genAccs
 
-		authGenStateBz, err := cfg.Codec.MarshalJSON(&authGenState)
+		authGenStateBz, err := encCfg.Codec.MarshalJSON(&authGenState)
 		if err != nil {
 			return fmt.Errorf("failed to marshal auth genesis state: %w", err)
 		}
@@ -83,7 +120,7 @@ func AddGenesisAccount(path, moniker, amountStr string, accAddr sdk.AccAddress) 
 
 	bankGenState.Supply = bankGenState.Supply.Add(balances.Coins...)
 
-	bankGenStateBz, err := cfg.Codec.MarshalJSON(bankGenState)
+	bankGenStateBz, err := encCfg.Codec.MarshalJSON(bankGenState)
 	if err != nil {
 		return fmt.Errorf("failed to marshal bank genesis state: %w", err)
 	}

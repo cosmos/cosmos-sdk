bac27127f993d1b9c9b66f66f10911e3cdf3f572
Julien Robert <julien@rbrt.fr>
2022-09-20 13:08:03 +0200

    fix conflicts

--- x/distribution/keeper/keeper.go
+++ x/distribution/keeper/keeper.go
@@ -98,17 +98,6 @@ func (k Keeper) WithdrawDelegationRewards(ctx sdk.Context, delAddr sdk.AccAddres
 		return nil, err
 	}
 
-<<<<<<< HEAD
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			types.EventTypeWithdrawRewards,
-			sdk.NewAttribute(sdk.AttributeKeyAmount, rewards.String()),
-			sdk.NewAttribute(types.AttributeKeyValidator, valAddr.String()),
-		),
-	)
-
-=======
->>>>>>> c1c23a75d (fix: ensure withdraw_rewards events are always emitted on reward withdrawal (#13323))
 	// reinitialize the delegation
 	k.initializeDelegation(ctx, valAddr, delAddr)
 	return rewards, nil

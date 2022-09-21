3e7ea613eeb354c38e3c420525ff4996b2a5b927
h4rpy-eagle <hassaniyaqoti@gmail.com>
2022-09-21 15:36:39 +0430

    feat: add unit tests for the option.go and wrapper.go

--- telemetry/option_test.go
+++ telemetry/option_test.go
@@ -1,39 +1,65 @@
 package telemetry
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+)
 
 func TestOptionWithConfig(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithConfig(Config{})(c)
+	require.NoError(t, err)
 }
 
 func TestOptionFromViper(t *testing.T) {
-	// TODO
+	c := &Config{}
+	v := viper.New()
+	v.SetDefault("telemetry.global-labels", []interface{}{})
+	err := OptionFromViper(v)(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithEnable(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithEnable(true)(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithServiceName(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithServiceName("some_service_name")(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithEnableHostname(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithEnableHostname(true)(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithEnableHostnameLabel(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithEnableHostnameLabel(true)(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithEnableServiceLabel(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithEnableServiceLabel(true)(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithPrometheusRetentionTime(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithPrometheusRetentionTime(1 * time.Second)(c)
+	require.NoError(t, err)
 }
 
 func TestOptionWithGlobalLabels(t *testing.T) {
-	// TODO
+	c := &Config{}
+	err := OptionWithGlobalLabels([][]string{})(c)
+	require.NoError(t, err)
 }
--- telemetry/wrapper_test.go
+++ telemetry/wrapper_test.go
@@ -1,39 +1,83 @@
-package telemetry
+package telemetry_test
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	gometrics "github.com/armon/go-metrics"
+	"github.com/cosmos/cosmos-sdk/telemetry"
+	"github.com/cosmos/cosmos-sdk/testutil/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
 
 func TestNewLabel(t *testing.T) {
-	// TODO
+	var L gometrics.Label = telemetry.NewLabel("some-key", "some-value")
+	require.NotNil(t, L)
 }
 
 func TestWrapperGather(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().Gather(gomock.Any()).Return(telemetry.GatherResponse{}, nil).AnyTimes()
+	telemetry.Default = MM
+	_, err := telemetry.Gather("some-format")
+	require.NoError(t, err)
 }
 
 func TestWrapperModuleMeasureSince(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().ModuleMeasureSince(gomock.Any(), gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.ModuleMeasureSince("some-format", time.Now())
+
 }
 
 func TestWrapperModuleSetGauge(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().ModuleSetGauge(gomock.Any(), gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.ModuleSetGauge("some-format", 1.234)
 }
 
 func TestWrapperIncrCounter(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().IncrCounter(gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.IncrCounter(1.234)
 }
 
 func TestWrapperIncrCounterWithLabels(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().IncrCounterWithLabels(gomock.Any(), gomock.Any(), gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.IncrCounterWithLabels([]string{"some-key"}, 1.234, []gometrics.Label{})
 }
 
 func TestWrapperSetGauge(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().SetGauge(gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.SetGauge(1.234)
 }
 
 func TestWrapperSetGaugeWithLabels(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().SetGaugeWithLabels(gomock.Any(), gomock.Any(), gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.SetGaugeWithLabels([]string{"some-key"}, 1.234, []gometrics.Label{})
 }
 
 func TestWrapperMeasureSince(t *testing.T) {
-	// TODO
+	C := gomock.NewController(t)
+	MM := mock.NewMockMetrics(C)
+	MM.EXPECT().MeasureSince(gomock.Any()).Return().AnyTimes()
+	telemetry.Default = MM
+	telemetry.MeasureSince(time.Now())
 }

dc46fbe9399c95a08193af2137dfb3256acf4233
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-10-04 11:26:45 -0700

    updates

--- simapp/upgrades.go
+++ simapp/upgrades.go
@@ -5,17 +5,17 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/x/group"
-	"github.com/cosmos/cosmos-sdk/x/nft"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-// UpgradeName defines the on-chain upgrade name for the sample simap upgrade from v045 to v046.
+// UpgradeName defines the on-chain upgrade name for the sample SimApp upgrade
+// from v045 to v046.
 //
-// NOTE: This upgrade defines a reference implementation of what an upgrade could look like
-// when an application is migrating from Cosmos SDK version v0.45.x to v0.46.x.
-const UpgradeName = "v045-to-v046"
+// NOTE: This upgrade defines a reference implementation of what an upgrade
+// could look like when an application is migrating from Cosmos SDK version
+// v0.45.x to v0.46.x.
+const UpgradeName = "v046-to-v047"
 
 func (app SimApp) RegisterUpgradeHandlers() {
 	baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
@@ -39,12 +39,7 @@ func (app SimApp) RegisterUpgradeHandlers() {
 	}
 
 	if upgradeInfo.Name == UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{
-				group.ModuleName,
-				nft.ModuleName,
-			},
-		}
+		storeUpgrades := storetypes.StoreUpgrades{}
 
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))

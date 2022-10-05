15078596afc561e1295f6b97240bf188316dfad8
Julien Robert <julien@rbrt.fr>
2022-10-05 23:53:15 +0200

    docs: update `pre.sh`

--- docs/pre.sh
+++ docs/pre.sh
@@ -7,15 +7,6 @@ for D in ../x/*; do
     MODDOC=modules/$(echo $D | awk -F/ '{print $NF}')
     rm -rf $MODDOC
     mkdir -p $MODDOC && cp -r $D/README.md "$_"
-    if [ -f "$MODDOC/README.md" ]; then
-      cd $MODDOC
-      # This ensures that we have multiples pages for the modules documantation
-      # This is easier to read for the user
-      # In order to split pages, we need to add a <!-- order: X --> in the module README.md, for each pages that we want.
-      csplit -k -q README.md '/<!-- order:/' '{*}' --prefix='section_' --suffix-format='%02d.md'
-      mv section_00.md README.md
-      cd ../..
-    fi
   fi
 done
 
@@ -23,10 +14,6 @@ done
 ## TODO to be removed in https://github.com/cosmos/cosmos-sdk/issues/9958
 mkdir -p modules/vesting
 cp -r ../x/auth/vesting/README.md modules/vesting
-cd modules/vesting
-csplit -k -q README.md '/<!-- order:/' '{*}' --prefix='section_' --suffix-format='%02d.md'
-mv section_00.md README.md
-cd ../..
 
 cat ../x/README.md | sed 's/\.\/x/\/modules/g' | sed 's/\.\.\/docs\/building-modules\/README\.md/\/building-modules\/intro\.html/g' > ./modules/README.md
 

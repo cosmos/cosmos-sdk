1782e7b32a0b8d47e977d10c3dde056e7cea1ae9
Aaron Craelius <aaronc@users.noreply.github.com>
2022-10-10 16:05:20 -0400

    address review comments

--- client/v2/cli/flag/value.go
+++ client/v2/cli/flag/value.go
@@ -5,11 +5,17 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
+// Value represents a single pflag.Value value.
 type Value interface {
 	pflag.Value
 	HasValue
 }
 
+// HasValue wraps a reference to a protobuf value.
 type HasValue interface {
-	Get(mutable protoreflect.Value) (protoreflect.Value, error)
+	// Get gets the value of the protobuf value reference and returns that value
+	// or an error. For composite protoreflect.Value types such as messages,
+	// lists and maps, a mutable reference to the value of field obtained with
+	// protoreflect.Message.NewField should be passed as the newFieldValue parameter.
+	Get(newFieldValue protoreflect.Value) (protoreflect.Value, error)
 }

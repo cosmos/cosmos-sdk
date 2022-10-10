a828af3926797466245dcc3aadf073f66971e540
Julien Robert <julien@rbrt.fr>
2022-10-10 15:26:13 +0200

    fix redirect - no regex :(

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -221,17 +221,189 @@ const config = {
             from: ["/", "/master", "/v0.43", "/v0.44"],
             to: "/main",
           },
+          {
+            from: [
+              "/main/modules/auth/01_concepts",
+              "/main/modules/auth/02_state",
+              "/main/modules/auth/03_antehandlers",
+              "/main/modules/auth/04_keepers",
+              "/main/modules/auth/06_params",
+              "/main/modules/auth/07_client",
+            ],
+            to: "/main/modules/auth",
+          },
+          {
+            from: "/main/modules/auth/05_vesting",
+            to: "/main/modules/vesting",
+          },
+          {
+            from: [
+              "/main/modules/authz/01_concepts",
+              "/main/modules/authz/02_state",
+              "/main/modules/authz/03_messages",
+              "/main/modules/authz/04_events",
+              "/main/modules/authz/05_client",
+            ],
+            to: "/main/modules/authz",
+          },
+          {
+            from: [
+              "/main/modules/bank/01_state",
+              "/main/modules/bank/02_keepers",
+              "/main/modules/bank/04_events",
+              "/main/modules/bank/05_params",
+              "/main/modules/bank/06_client",
+            ],
+            to: "/main/modules/bank",
+          },
+          {
+            from: [
+              "/main/modules/capability/01_concepts",
+              "/main/modules/capability/02_state",
+            ],
+            to: "/main/modules/capability",
+          },
+          {
+            from: [
+              "/main/modules/crisis/01_state",
+              "/main/modules/crisis/02_messages",
+              "/main/modules/crisis/03_events",
+              "/main/modules/crisis/04_params",
+              "/main/modules/crisis/05_client",
+            ],
+            to: "/main/modules/crisis",
+          },
+          {
+            from: [
+              "/main/modules/distribution/01_concepts",
+              "/main/modules/distribution/02_state",
+              "/main/modules/distribution/03_begin_block",
+              "/main/modules/distribution/04_messages",
+              "/main/modules/distribution/05_hooks",
+              "/main/modules/distribution/06_events",
+              "/main/modules/distribution/07_params",
+              "/main/modules/distribution/08_client",
+            ],
+            to: "/main/modules/distribution",
+          },
+          {
+            from: [
+              "/main/modules/epoching/01_state",
+              "/main/modules/epoching/03_to_improve",
+            ],
+            to: "/main/modules/epoching",
+          },
+          {
+            from: [
+              "/main/modules/evidence/01_concepts",
+              "/main/modules/evidence/02_state",
+              "/main/modules/evidence/03_messages",
+              "/main/modules/evidence/04_events",
+              "/main/modules/evidence/05_params",
+              "/main/modules/evidence/06_begin_block",
+              "/main/modules/evidence/07_client",
+            ],
+            to: "/main/modules/evidence",
+          },
+          {
+            from: [
+              "/main/modules/feegrant/01_concepts",
+              "/main/modules/feegrant/02_state",
+              "/main/modules/feegrant/03_messages",
+              "/main/modules/feegrant/04_events",
+              "/main/modules/feegrant/05_client",
+            ],
+            to: "/main/modules/feegrant",
+          },
+          {
+            from: [
+              "/main/modules/gov/01_concepts",
+              "/main/modules/gov/02_state",
+              "/main/modules/gov/03_messages",
+              "/main/modules/gov/04_events",
+              "/main/modules/gov/05_future_improvements",
+              "/main/modules/gov/06_params",
+              "/main/modules/gov/07_client",
+              "/main/modules/gov/08_metadata",
+            ],
+            to: "/main/modules/gov",
+          },
+          {
+            from: [
+              "/main/modules/group/01_concepts",
+              "/main/modules/group/02_state",
+              "/main/modules/group/03_messages",
+              "/main/modules/group/04_events",
+              "/main/modules/group/05_client",
+              "/main/modules/group/06_metadata",
+            ],
+            to: "/main/modules/group/",
+          },
+          {
+            from: [
+              "/main/modules/mint/01_concepts",
+              "/main/modules/mint/02_state",
+              "/main/modules/mint/03_begin_block",
+              "/main/modules/mint/04_params",
+              "/main/modules/mint/05_events",
+              "/main/modules/mint/06_client",
+            ],
+            to: "/main/modules/mint/",
+          },
+          {
+            from: [
+              "/main/modules/nft/01_concepts",
+              "/main/modules/nft/02_state",
+              "/main/modules/nft/03_messages",
+              "/main/modules/nft/04_events",
+            ],
+            to: "/main/modules/nft/",
+          },
+          {
+            from: [
+              "/main/modules/params/01_keeper",
+              "/main/modules/params/02_subspace",
+            ],
+            to: "/main/modules/params/",
+          },
+          {
+            from: [
+              "/main/modules/slashing/01_concepts",
+              "/main/modules/slashing/02_state",
+              "/main/modules/slashing/03_messages",
+              "/main/modules/slashing/04_begin_block",
+              "/main/modules/slashing/05_hooks",
+              "/main/modules/slashing/06_events",
+              "/main/modules/slashing/07_tombstone",
+              "/main/modules/slashing/08_params",
+              "/main/modules/slashing/09_client",
+            ],
+            to: "/main/modules/slashing/",
+          },
+          {
+            from: [
+              "/main/modules/staking/01_state",
+              "/main/modules/staking/02_state_transitions",
+              "/main/modules/staking/03_messages",
+              "/main/modules/staking/04_begin_block",
+              "/main/modules/staking/05_end_block",
+              "/main/modules/staking/06_hooks",
+              "/main/modules/staking/07_events",
+              "/main/modules/staking/08_params",
+              "/main/modules/staking/09_client",
+            ],
+            to: "/main/modules/staking/",
+          },
+          {
+            from: [
+              "/main/modules/upgrade/01_concepts",
+              "/main/modules/upgrade/02_state",
+              "/main/modules/upgrade/03_events",
+              "/main/modules/upgrade/04_client",
+            ],
+            to: "/main/modules/upgrade/",
+          },
         ],
-        createRedirects: function(existingPath) {
-          // modules redirections
-          const modulesRegex = /\/main\/modules\/([A-z]+)\/([0-9]+)/g;
-          const modulesMatch = modulesRegex.exec(existingPath);
-          if (modulesMatch) {
-            return ["/main/modules/" + modulesMatch[0]];
-          }
-
-          return undefined;
-        },
       },
     ],
   ],

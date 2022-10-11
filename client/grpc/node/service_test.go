44addfd050af5ebfc5a9b0bef8bcf3d9d6fef8f4
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-10-11 13:05:00 -0400

    updates

--- client/grpc/node/service_test.go
+++ client/grpc/node/service_test.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	context "context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,7 +12,9 @@ import (
 
 func TestServiceServer_Config(t *testing.T) {
 	svr := NewQueryServer(client.Context{})
-	ctx := sdk.Context{}.WithMinGasPrices(sdk.NewDecCoins(sdk.NewInt64DecCoin("stake", 15)))
+	ctx := sdk.Context{}.
+		WithMinGasPrices(sdk.NewDecCoins(sdk.NewInt64DecCoin("stake", 15))).
+		WithContext(context.Background())
 	goCtx := sdk.WrapSDKContext(ctx)
 
 	resp, err := svr.Config(goCtx, &ConfigRequest{})

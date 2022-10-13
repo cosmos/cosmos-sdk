77efba25e16e8f19cef86c26a6cfdc50189d1e24
likhita-809 <likhita@vitwit.com>
2022-10-13 19:12:48 +0530

    add godoc for store helper methods

--- store/store_helper.go
+++ store/store_helper.go
@@ -4,6 +4,7 @@ import (
 	types "github.com/cosmos/cosmos-sdk/store/types"
 )
 
+// GetAndDecode gets and decodes key and returns it. Returns nil if key doesn't exist.
 func GetAndDecode[T any](store types.KVStore, dec func([]byte) (T, error), key []byte) (T, error) {
 	var res T
 	bz := store.Get(key)
@@ -17,6 +18,7 @@ func GetAndDecode[T any](store types.KVStore, dec func([]byte) (T, error), key [
 	return resp, err
 }
 
+// GetAndDecodeWithBool gets and decodes key and returns it. Returns false if key doesn't exist.
 func GetAndDecodeWithBool[T any](store types.KVStore, dec func([]byte) (T, bool), key []byte) (T, bool) {
 	var res T
 	bz := store.Get(key)
@@ -27,24 +29,29 @@ func GetAndDecodeWithBool[T any](store types.KVStore, dec func([]byte) (T, bool)
 	return resp, boolval
 }
 
+// StoreAPI is a wrapper around the store's KVStore to provide more safe key management and better ease-of-use.
 type StoreAPI struct {
 	types.KVStore
 }
 
+// NewStoreAPI returns a new KVStore.
 func NewStoreAPI(store types.KVStore) StoreAPI {
 	return StoreAPI{
 		KVStore: store,
 	}
 }
 
+// Set stores the value under the given key.
 func (store StoreAPI) Set(key []byte, value []byte) {
 	store.KVStore.Set(key, value)
 }
 
+// Get returns the raw bytes stored under the given key. Returns nil when key does not exist.
 func (store StoreAPI) Get(key []byte) []byte {
 	return store.KVStore.Get(key)
 }
 
+// Delete deletes the value stored under the given key, if it exists.
 func (store StoreAPI) Delete(key []byte) {
 	store.KVStore.Delete(key)
 }

fccc192edeec790ddc9a227c147d924620653b1b
Roman <roman@osmosis.team>
2022-10-14 01:01:59 -0500

    chore(backport): feat: add grants by grantee authz query (backport #10944) (#368)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- client/tx/factory.go
+++ client/tx/factory.go
@@ -39,6 +39,8 @@ func NewFactoryCLI(clientCtx client.Context, flagSet *pflag.FlagSet) Factory {
 		signMode = signing.SignMode_SIGN_MODE_DIRECT
 	case flags.SignModeLegacyAminoJSON:
 		signMode = signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON
+	case flags.SignModeEIP191:
+		signMode = signing.SignMode_SIGN_MODE_EIP_191
 	}
 
 	accNum, _ := flagSet.GetUint64(flags.FlagAccountNumber)

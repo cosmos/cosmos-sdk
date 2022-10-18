12eb91402c7631dd40a793b0cc06183cb51561cf
Nick DeLuca <nickdeluca08@gmail.com>
2022-10-18 13:03:19 -0700

    Update x/distribution/keeper/delegation.go
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- x/distribution/keeper/delegation.go
+++ x/distribution/keeper/delegation.go
@@ -196,7 +196,7 @@ func (k Keeper) withdrawDelegationRewards(ctx sdk.Context, val stakingtypes.Vali
 		}
 
 		// Note, we do not call the NewCoins constructor as we do not want the zero
-		// coin removed for event emission
+		// coin removed for event emission.
 		emittedRewards = sdk.Coins{sdk.NewCoin(baseDenom, sdk.ZeroInt())}
 	}
 

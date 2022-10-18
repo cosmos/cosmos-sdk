8667eba85db57555c0d7cc437566199e6b5e9549
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2022-10-18 03:21:57 -0400

    refactor: Amend #13497 (#13577)

--- server/start.go
+++ server/start.go
@@ -334,7 +334,10 @@ func startInProcess(ctx *Context, clientCtx client.Context, appCreator types.App
 
 		app.RegisterTxService(clientCtx)
 		app.RegisterTendermintService(clientCtx)
-		app.RegisterNodeService(clientCtx)
+
+		if a, ok := app.(types.ApplicationQueryService); ok {
+			a.RegisterNodeService(clientCtx)
+		}
 	}
 
 	metrics, err := startTelemetry(config)

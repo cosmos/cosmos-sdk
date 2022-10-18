e2bef952a8d7045036945a038eb87dab8186a1c0
Facundo Medica <facundomedica@gmail.com>
2022-10-18 19:27:08 -0300

    remove change

--- x/gov/keeper/grpc_query_test.go
+++ x/gov/keeper/grpc_query_test.go
@@ -80,7 +80,7 @@ func (suite *KeeperTestSuite) TestGRPCQueryProposal() {
 				suite.Require().NoError(err)
 				actualJSON, err := suite.cdc.MarshalJSON(proposalRes.Proposal)
 				suite.Require().NoError(err)
-				suite.Require().Equal(string(expJSON), string(actualJSON))
+				suite.Require().Equal(expJSON, actualJSON)
 			} else {
 				suite.Require().Error(err)
 				suite.Require().Nil(proposalRes)
@@ -306,7 +306,7 @@ func (suite *KeeperTestSuite) TestGRPCQueryProposals() {
 					actualJSON, err := suite.cdc.MarshalJSON(proposals.GetProposals()[i])
 					suite.Require().NoError(err)
 
-					suite.Require().Equal(string(expJSON), string(actualJSON))
+					suite.Require().Equal(expJSON, actualJSON)
 				}
 
 			} else {

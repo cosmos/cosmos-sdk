1f32225cbebd187d985bba652660b40a92082396
Facundo Medica <facundomedica@gmail.com>
2022-10-19 15:18:39 -0300

    progress

--- x/gov/abci.go
+++ x/gov/abci.go
@@ -66,10 +66,8 @@ func EndBlocker(ctx sdk.Context, keeper *keeper.Keeper) {
 			// Messages may mutate state thus we use a cached context. If one of
 			// the handlers fails, no state mutation is written and the error
 			// message is logged.
-			keeper.PopulateProposalContents(ctx, &proposal)
-			messages, err := proposal.GetMsgs()
-
 			cacheCtx, writeCache := ctx.CacheContext()
+			messages, err := proposal.GetMsgs()
 			if err == nil {
 				for idx, msg = range messages {
 					handler := keeper.Router().Handler(msg)

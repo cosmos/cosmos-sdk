96ea326edeb1c24889c2272b385c2bcd43b9012e
Sai Kumar <17549398+gsk967@users.noreply.github.com>
2022-10-22 23:53:24 +0530

    feat(cli): add module-account cli cmd and grpc get api (#13612)
    (cherry picked from commit ddf5cf0d8d26f8beb504cb2fcc2d4a8b0964261c)
    
    # Conflicts:
    #	CHANGELOG.md
    #	api/cosmos/auth/v1beta1/query.pulsar.go
    #	api/cosmos/auth/v1beta1/query_grpc.pb.go
    #	x/auth/client/testutil/suite.go
    #	x/auth/types/query.pb.go

--- x/auth/client/testutil/suite.go
+++ x/auth/client/testutil/suite.go
@@ -15,6 +15,15 @@ import (
 	"github.com/stretchr/testify/suite"
 	tmcli "github.com/tendermint/tendermint/libs/cli"
 
+<<<<<<< HEAD:x/auth/client/testutil/suite.go
+=======
+	"cosmossdk.io/depinject"
+	"cosmossdk.io/math"
+
+	authtestutil "github.com/cosmos/cosmos-sdk/x/auth/testutil"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
+
+>>>>>>> ddf5cf0d8 (feat(cli): add module-account cli cmd and grpc get api (#13612)):tests/e2e/auth/suite.go
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
@@ -1387,6 +1396,68 @@ func (s *IntegrationTestSuite) TestGetAccountsCmd() {
 	s.Require().NoError(err)
 
 	var res authtypes.QueryAccountsResponse
+	s.Require().NoError(val.ClientCtx.Codec.UnmarshalJSON(out.Bytes(), &res))
+	s.Require().NotEmpty(res.Accounts)
+}
+
+func (s *IntegrationTestSuite) TestQueryModuleAccountByNameCmd() {
+	val := s.network.Validators[0]
+
+	testCases := []struct {
+		name       string
+		moduleName string
+		expectErr  bool
+	}{
+		{
+			"invalid module name",
+			"gover",
+			true,
+		},
+		{
+			"valid module name",
+			"mint",
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		s.Run(tc.name, func() {
+			clientCtx := val.ClientCtx
+
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, authcli.QueryModuleAccountByNameCmd(), []string{
+				tc.moduleName,
+				fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+			})
+			if tc.expectErr {
+				s.Require().Error(err)
+				s.Require().NotEqual("internal", err.Error())
+			} else {
+				var res authtypes.QueryModuleAccountByNameResponse
+				s.Require().NoError(val.ClientCtx.Codec.UnmarshalJSON(out.Bytes(), &res))
+
+				var account types.AccountI
+				err := val.ClientCtx.InterfaceRegistry.UnpackAny(res.Account, &account)
+				s.Require().NoError(err)
+
+				moduleAccount, ok := account.(types.ModuleAccountI)
+				s.Require().True(ok)
+				s.Require().Equal(tc.moduleName, moduleAccount.GetName())
+			}
+		})
+	}
+}
+
+func (s *IntegrationTestSuite) TestQueryModuleAccountsCmd() {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+
+	out, err := clitestutil.ExecTestCLICmd(clientCtx, authcli.QueryModuleAccountsCmd(), []string{
+		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+	})
+	s.Require().NoError(err)
+
+	var res authtypes.QueryModuleAccountsResponse
 	s.Require().NoError(val.ClientCtx.Codec.UnmarshalJSON(out.Bytes(), &res))
 	s.Require().NotEmpty(res.Accounts)
 }

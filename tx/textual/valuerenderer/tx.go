10891fda71cc3ecde9aca05c538d5e002059a879
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-10-25 13:32:09 +0200

    add tx vr

--- tx/textual/valuerenderer/tx.go
+++ tx/textual/valuerenderer/tx.go
@@ -26,7 +26,7 @@ func NewTxValueRenderer(tr *Textual) ValueRenderer {
 
 // Format implements the ValueRenderer interface.
 func (vr txValueRenderer) Format(ctx context.Context, v protoreflect.Value) ([]Screen, error) {
-	// Reify the reflected message as a proto Timestamp
+	// Reify the reflected message as a proto Tx
 	msg := v.Message().Interface()
 	protoTx, ok := msg.(*txv1beta1.Tx)
 	if !ok {
@@ -45,7 +45,7 @@ func (vr txValueRenderer) Format(ctx context.Context, v protoreflect.Value) ([]S
 	if err != nil {
 		return nil, err
 	}
-	screens[2].Text = fmt.Sprintf("Public key: %s", pk)
+	screens[2].Text = "Public key: TODO Any"
 	screens[2].Expert = true
 
 	// Get sdk.Msgs screens, from Tx.Body.Messages (field number 1).
@@ -59,10 +59,127 @@ func (vr txValueRenderer) Format(ctx context.Context, v protoreflect.Value) ([]S
 	// }
 	// screens = append(screens, msgScreens...)
 
-	// Fees
-	vr.Format(ctx, protoTx.AuthInfo.Fee.ProtoReflect().Get(protoTx.AuthInfo.Fee.ProtoReflect().Descriptor().Fields().ByNumber(1)))
+	// TODO BODY MSGS
+	screens, err = vr.appendScreen(screens, ctx, protoTx.AuthInfo.Fee.ProtoReflect(), "amount", "Fees", false)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.AuthInfo.Fee.ProtoReflect(), "payer", "Fee payer", true)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.AuthInfo.Fee.ProtoReflect(), "granter", "Fee granter", true)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.Body.ProtoReflect(), "memo", "Memo", false)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.AuthInfo.Fee.ProtoReflect(), "gas_limit", "Gas limit", true)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.Body.ProtoReflect(), "timeout_height", "Timeout height", true)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.AuthInfo.Tip.ProtoReflect(), "tipper", "Tipper", true)
+	if err != nil {
+		return nil, err
+	}
+	screens, err = vr.appendScreen(screens, ctx, protoTx.AuthInfo.Tip.ProtoReflect(), "amount", "Tip", true)
+	if err != nil {
+		return nil, err
+	}
+	// screens, err = vr.appendScreen(screens, ctx, protoTx.Body.ProtoReflect(), "extension_options", "Body extensions", true)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// screens, err = vr.appendScreen(screens, ctx, protoTx.Body.ProtoReflect(), "non_critical_extension_options options", "Non-critical body extensions", true)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	if len(protoTx.AuthInfo.SignerInfos) > 1 {
+		// Get all signer infos except the signer's one.
+		otherSigners := make([]*txv1beta1.SignerInfo, 0, len(protoTx.AuthInfo.SignerInfos)-1)
+		for _, si := range protoTx.AuthInfo.SignerInfos {
+			if !proto.Equal(si.PublicKey, vr.tr.signerData.PubKey) {
+				otherSigners = append(otherSigners, si)
+			}
+		}
+
+		// Create the same protoTx.AuthInfo.SignerInfos message, but without the signer
+		fd := protoTx.AuthInfo.ProtoReflect().Descriptor().Fields().ByName("signer_infos")
+		newField := protoTx.AuthInfo.ProtoReflect().NewField(fd)
+		r, err := vr.tr.GetValueRenderer(fd)
+		if err != nil {
+			return nil, err
+		}
+		newScreens, err := r.Format(ctx, newField)
+		if err != nil {
+			return nil, err
+		}
+
+		// Manually replace the first screen, to add the "other" word
+		newScreens[0].Text = fmt.Sprintf("This transaction has %d other signers:", len(otherSigners))
+
+		screens = append(screens, newScreens...)
+	}
 
 	return screens, nil
+}
+
+func (vr txValueRenderer) appendScreen(
+	screens []Screen, ctx context.Context,
+	msg protoreflect.Message, fieldName,
+	label string, expert bool,
+) ([]Screen, error) {
+	// Skip if the message is empty
+	if !msg.IsValid() {
+		return screens, nil
+	}
+	fd := msg.Descriptor().Fields().ByName(protoreflect.Name(fieldName))
+	value := msg.Get(fd)
+	// Skip if the value is empty
+	if !value.IsValid() || isValueEmpty(msg, fd, value) {
+		return screens, nil
+	}
+
+	// Get the value-rendered text of the inner Message
+	r, err := vr.tr.GetValueRenderer(fd)
+	if err != nil {
+		return nil, err
+	}
+	new, err := r.Format(ctx, value)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add label and expert fields as needed
+	new[0].Text = fmt.Sprintf("%s: %s", label, new[0].Text)
+	if expert {
+		for i := range new {
+			new[i].Expert = true
+		}
+	}
+
+	screens = append(screens, new...)
+
+	return screens, nil
+}
+
+// isValueEmpty checks if the protoreflect.Value is equal to its empty
+// (default) value.
+// Protobuf only exposes the `Equal` method on messages, so here we create
+// two messages: one empty, and the other empty except for the field with the
+// given Value. We then do proto.Equal on these two messages.
+func isValueEmpty(msg protoreflect.Message, fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+	msgWithValue := msg.New()
+	msgWithValue.Set(fd, v)
+	emptyMsg := msg.New()
+
+	return proto.Equal(emptyMsg.Interface(), msgWithValue.Interface())
 }
 
 // Parse implements the ValueRenderer interface.
--- tx/textual/valuerenderer/tx_test.go
+++ tx/textual/valuerenderer/tx_test.go
@@ -8,13 +8,14 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/reflect/protoreflect"
-	tspb "google.golang.org/protobuf/types/known/timestamppb"
 
 	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
+	_ "cosmossdk.io/api/cosmos/crypto/secp256k1"
 	txv1beta1 "cosmossdk.io/api/cosmos/tx/v1beta1"
 	"cosmossdk.io/tx/signing"
+	"cosmossdk.io/tx/textual/internal/testpb"
 	"cosmossdk.io/tx/textual/valuerenderer"
 )
 
@@ -24,10 +25,10 @@ func EmptyCoinMetadataQuerier(ctx context.Context, denom string) (*bankv1beta1.M
 }
 
 type txJsonTest struct {
-	Proto      *txv1beta1.Tx
-	SignerData signing.SignerData
+	Proto      json.RawMessage
+	SignerData json.RawMessage
 	Error      bool
-	Text       string
+	Screens    []valuerenderer.Screen
 }
 
 func TestTxJsonTestcases(t *testing.T) {
@@ -40,31 +41,45 @@ func TestTxJsonTestcases(t *testing.T) {
 
 	for i, tc := range testcases {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			tr := valuerenderer.NewTextual(EmptyCoinMetadataQuerier, tc.SignerData)
+
+			var testSignerData testpb.SignerData
+			err = protojson.Unmarshal(tc.SignerData, &testSignerData)
+			require.NoError(t, err)
+
+			signerData := signing.SignerData{
+				Address:       testSignerData.Address,
+				ChainID:       testSignerData.ChainId,
+				AccountNumber: testSignerData.AccountNumber,
+				Sequence:      testSignerData.Sequence,
+				PubKey:        testSignerData.PubKey,
+			}
+
+			tr := valuerenderer.NewTextual(EmptyCoinMetadataQuerier, signerData)
 			rend := valuerenderer.NewTxValueRenderer(&tr)
 
 			var screens []valuerenderer.Screen
 			if tc.Proto != nil {
-				screens, err = rend.Format(context.Background(), protoreflect.ValueOf(tc.Proto.ProtoReflect()))
+				var protoTx txv1beta1.Tx
+				protojson.Unmarshal(tc.Proto, &protoTx)
+				screens, err = rend.Format(context.Background(), protoreflect.ValueOf(protoTx.ProtoReflect()))
 				if tc.Error {
 					require.Error(t, err)
 					return
 				}
 				require.NoError(t, err)
-				require.Equal(t, 1, len(screens))
-				require.Equal(t, tc.Text, screens[0].Text)
+				require.Equal(t, tc.Screens, screens)
 			}
 
-			val, err := rend.Parse(context.Background(), screens)
-			if tc.Error {
-				require.Error(t, err)
-				return
-			}
-			require.NoError(t, err)
-			msg := val.Message().Interface()
-			require.IsType(t, &tspb.Timestamp{}, msg)
-			timestamp := msg.(*tspb.Timestamp)
-			require.True(t, proto.Equal(timestamp, tc.Proto))
+			// val, err := rend.Parse(context.Background(), screens)
+			// if tc.Error {
+			// 	require.Error(t, err)
+			// 	return
+			// }
+			// require.NoError(t, err)
+			// msg := val.Message().Interface()
+			// require.IsType(t, &tspb.Timestamp{}, msg)
+			// timestamp := msg.(*tspb.Timestamp)
+			// require.True(t, proto.Equal(timestamp, tc.Proto))
 		})
 	}
 }

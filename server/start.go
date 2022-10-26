cc6690ce9d1f4ec4d96cc374ea528cc40042acb2
William Chong <6198816+williamchong@users.noreply.github.com>
2022-10-26 22:28:29 +0800

    feat(cli): Add iavl-disable-fastnode cmd flag with proper description (#13656)
    (cherry picked from commit c833190a166e92039659f0b35f5401d796841532)
    
    # Conflicts:
    #	server/start.go

--- server/start.go
+++ server/start.go
@@ -47,6 +47,7 @@ const (
 	FlagTrace              = "trace"
 	FlagInvCheckPeriod     = "inv-check-period"
 
+<<<<<<< HEAD
 	FlagPruning           = "pruning"
 	FlagPruningKeepRecent = "pruning-keep-recent"
 	FlagPruningKeepEvery  = "pruning-keep-every"
@@ -55,6 +56,15 @@ const (
 	FlagMinRetainBlocks   = "min-retain-blocks"
 	FlagIAVLCacheSize     = "iavl-cache-size"
 	FlagIAVLFastNode      = "iavl-disable-fastnode"
+=======
+	FlagPruning             = "pruning"
+	FlagPruningKeepRecent   = "pruning-keep-recent"
+	FlagPruningInterval     = "pruning-interval"
+	FlagIndexEvents         = "index-events"
+	FlagMinRetainBlocks     = "min-retain-blocks"
+	FlagIAVLCacheSize       = "iavl-cache-size"
+	FlagDisableIAVLFastNode = "iavl-disable-fastnode"
+>>>>>>> c833190a1 (feat(cli): Add iavl-disable-fastnode cmd flag with proper description (#13656))
 
 	// state sync-related flags
 	FlagStateSyncSnapshotInterval   = "state-sync.snapshot-interval"
@@ -165,7 +175,11 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 	cmd.Flags().Uint64(FlagStateSyncSnapshotInterval, 0, "State sync snapshot interval")
 	cmd.Flags().Uint32(FlagStateSyncSnapshotKeepRecent, 2, "State sync snapshot to keep")
 
+<<<<<<< HEAD
 	cmd.Flags().Bool(FlagIAVLFastNode, true, "Enable fast node for IAVL tree")
+=======
+	cmd.Flags().Bool(FlagDisableIAVLFastNode, false, "Disable fast node for IAVL tree")
+>>>>>>> c833190a1 (feat(cli): Add iavl-disable-fastnode cmd flag with proper description (#13656))
 
 	// add support for all Tendermint-specific command line options
 	tcmd.AddNodeFlags(cmd)

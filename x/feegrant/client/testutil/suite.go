760acf195fdf9770c336e57fa1f960424c74c050
Likhita Polavarapu <78951027+likhita-809@users.noreply.github.com>
2022-10-31 20:23:47 +0530

    fix: --dry-run flag not working when using tx cmd (#13673)

--- x/feegrant/client/testutil/suite.go
+++ x/feegrant/client/testutil/suite.go
@@ -313,7 +313,7 @@ func (s *IntegrationTestSuite) TestNewCmdFeeGrant() {
 	alreadyExistedGrantee := s.addedGrantee
 	clientCtx := val.ClientCtx
 
-	fromAddr, fromName, _, err := client.GetFromFields(clientCtx.Keyring, granter.String(), clientCtx.GenerateOnly)
+	fromAddr, fromName, _, err := client.GetFromFields(clientCtx, clientCtx.Keyring, granter.String())
 	s.Require().Equal(fromAddr, granter)
 	s.Require().NoError(err)
 

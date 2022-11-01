b9e65b6d418e4b7601f63fa01fe96e3d845dd2e9
Aaron Craelius <aaron@regen.network>
2022-11-01 17:34:59 -0400

    feat: add autocli app wiring + remote info support (#13281)

--- x/capability/module.go
+++ x/capability/module.go
@@ -14,6 +14,7 @@ import (
 
 	modulev1 "cosmossdk.io/api/cosmos/capability/module/v1"
 	"cosmossdk.io/depinject"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"

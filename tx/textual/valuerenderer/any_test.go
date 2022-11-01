65ee7f62dc94d4098d3e9a4c4208b3d2198a6df3
Jim Larson <jim@agoric.com>
2022-10-31 17:49:53 -0700

    refactor: use protojson for better test case legibility

--- tx/textual/valuerenderer/any_test.go
+++ tx/textual/valuerenderer/any_test.go
@@ -11,15 +11,14 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
-	"google.golang.org/protobuf/reflect/protoregistry"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
 type anyJsonTest struct {
-	Url     string
-	Msg     map[string]interface{}
+	Proto   map[string]interface{}
 	Screens []valuerenderer.Screen
 }
 
@@ -34,26 +33,18 @@ func TestAny(t *testing.T) {
 	tr := valuerenderer.NewTextual(EmptyCoinMetadataQuerier)
 	for i, tc := range testcases {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			rend := valuerenderer.NewAnyValueRenderer((&tr))
-
-			// Construct the appropriate internal message from its URL
-			internalMsgType, err := protoregistry.GlobalTypes.FindMessageByURL(tc.Url)
+			// Must marshal the proto object back into JSON,
+			// then unmarshal with protojson into an Any.
+			bz, err := json.Marshal(tc.Proto)
 			require.NoError(t, err)
-			internalMsg := internalMsgType.New().Interface()
-
-			// For ease of use, the internal message is shown in the test case as a JSON object,
-			// not a string or (shudder) bytes of a proto encoding. So we read that into Go
-			// as a map, then render to JSON, then parse as the right message type.
-			jsondata, err := json.Marshal(tc.Msg)
+			anyMsg := anypb.Any{}
+			err = protojson.Unmarshal(bz, &anyMsg)
 			require.NoError(t, err)
-			err = json.Unmarshal(jsondata, &internalMsg)
-			require.NoError(t, err)
-
-			// Now embed the internal message in a google.protobuf.Any message.
-			anyMsg, err := anypb.New(internalMsg)
+			internalMsg, err := anyMsg.UnmarshalNew()
 			require.NoError(t, err)
 
 			// Format into screens and check vs expected
+			rend := valuerenderer.NewAnyValueRenderer((&tr))
 			screens, err := rend.Format(context.Background(), protoreflect.ValueOfMessage(anyMsg.ProtoReflect()))
 			require.NoError(t, err)
 			require.Equal(t, tc.Screens, screens)
@@ -67,7 +58,6 @@ func TestAny(t *testing.T) {
 
 			// Check for equality of the internal message of the parsed message,
 			// to avoid sensitivity to the exact proto encoding bytes.
-			require.Equal(t, tc.Url, parsedAny.TypeUrl)
 			parsedInternal, err := parsedAny.UnmarshalNew()
 			require.NoError(t, err)
 			require.True(t, proto.Equal(internalMsg, parsedInternal))

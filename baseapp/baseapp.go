a3bc195f1d9eb347a12d85841846ed7b9ebcc78b
Jeancarlo Barrios <JeancarloBarrios@users.noreply.github.com>
2022-11-07 10:58:56 -0600

    Update baseapp/baseapp.go
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -81,8 +81,8 @@ type BaseApp struct { //nolint: maligned
 	// deliverState is set on InitChain and BeginBlock and set to nil on Commit
 	checkState           *state // for CheckTx
 	deliverState         *state // for DeliverTx
-	processProposalState *state // for PrepareProposal
-	prepareProposalState *state // for ProcessProposal
+	processProposalState *state // for ProcessProposal
+	prepareProposalState *state // for PrepareProposal
 
 	// an inter-block write-through cache provided to the context during deliverState
 	interBlockCache sdk.MultiStorePersistentCache

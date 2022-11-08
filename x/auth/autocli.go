c89c22be615b9d973fefc24179f385633c6efce2
Aaron Craelius <aaron@regen.network>
2022-11-08 10:08:37 -0500

    Update x/auth/autocli.go
    Co-authored-by: Julien Robert <julien@rbrt.fr>

--- x/auth/autocli.go
+++ x/auth/autocli.go
@@ -19,7 +19,7 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 				},
 				{
 					RpcMethod:      "AccountAddressByID",
-					Use:            "address-by-id [id]",
+					Use:            "address-by-id [acc-num]",
 					Short:          "query account address by account ID",
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "id"}},
 				},

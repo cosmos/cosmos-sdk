7108e161ff2cd03e680f2f13a150d535a420b671
Dreamer <745124335@qq.com>
2022-11-15 09:47:18 +0800

    update error code

--- x/nft/errors.go
+++ x/nft/errors.go
@@ -6,10 +6,10 @@ import (
 
 // x/nft module sentinel errors
 var (
-	ErrClassExists    = errors.Register(ModuleName, 2, "nft class already exist")
-	ErrClassNotExists = errors.Register(ModuleName, 3, "nft class does not exist")
-	ErrNFTExists      = errors.Register(ModuleName, 4, "nft already exist")
-	ErrNFTNotExists   = errors.Register(ModuleName, 5, "nft does not exist")
-	ErrEmptyClassID   = errors.Register(ModuleName, 6, "empty class id")
-	ErrEmptyNFTID     = errors.Register(ModuleName, 7, "empty nft id")
+	ErrClassExists    = errors.Register(ModuleName, 3, "nft class already exist")
+	ErrClassNotExists = errors.Register(ModuleName, 4, "nft class does not exist")
+	ErrNFTExists      = errors.Register(ModuleName, 5, "nft already exist")
+	ErrNFTNotExists   = errors.Register(ModuleName, 6, "nft does not exist")
+	ErrEmptyClassID   = errors.Register(ModuleName, 7, "empty class id")
+	ErrEmptyNFTID     = errors.Register(ModuleName, 8, "empty nft id")
 )

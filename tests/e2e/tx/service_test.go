72b1237b1a1c0be27dd53cf3cbf52185df61185e
likhita-809 <likhita@vitwit.com>
2022-11-16 20:44:22 +0530

    add decode amino tests

--- tests/e2e/tx/service_test.go
+++ tests/e2e/tx/service_test.go
@@ -980,6 +980,7 @@ func (s *IntegrationTestSuite) TestTxEncodeAmino_GRPCGateway() {
 func (s IntegrationTestSuite) TestTxDecodeAmino_GRPC() {
 	val := s.network.Validators[0]
 	txBuilder := s.mkTxBuilder()
+
 	encodedTx, err := val.ClientCtx.TxConfig.TxEncoder()(txBuilder.GetTx())
 	s.Require().NoError(err)
 
@@ -1008,6 +1009,42 @@ func (s IntegrationTestSuite) TestTxDecodeAmino_GRPC() {
 			} else {
 				s.Require().NoError(err)
 				s.Require().NotEmpty(res.GetAminoJson())
+			}
+		})
+	}
+}
+
+func (s IntegrationTestSuite) TestTxDecodeAmino_GRPCGateway() {
+	val := s.network.Validators[0]
+	txBuilder := s.mkTxBuilder()
+
+	encodedTx, err := val.ClientCtx.TxConfig.TxEncoder()(txBuilder.GetTx())
+	s.Require().NoError(err)
+
+	invalidTxBytes := append(encodedTx, byte(0o00))
+
+	testCases := []struct {
+		name      string
+		req       *tx.TxDecodeAminoRequest
+		expErr    bool
+		expErrMsg string
+	}{
+		{"empty request", &tx.TxDecodeAminoRequest{}, true, "invalid empty tx bytes"},
+		{"invalid tx bytes", &tx.TxDecodeAminoRequest{AminoBinary: invalidTxBytes}, true, "invalid request"},
+		{"valid request with tx bytes", &tx.TxDecodeAminoRequest{AminoBinary: encodedTx}, false, ""},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			req, err := val.ClientCtx.Codec.MarshalJSON(tc.req)
+			s.Require().NoError(err)
+
+			res, err := testutil.PostRequest(fmt.Sprintf("%s/cosmos/tx/v1beta1/decode/amino", val.APIAddress), "application/json", req)
+			s.Require().NoError(err)
+			if tc.expErr {
+				s.Require().Contains(string(res), tc.expErrMsg)
+			} else {
+				s.Require().NoError(err)
 			}
 		})
 	}

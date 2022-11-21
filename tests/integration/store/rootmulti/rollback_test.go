f08f43ce69f7946494fee3a8dc237ffcdce28bf1
mmsqe <mavis@crypto.com>
2022-11-21 09:17:36 +0800

    test offline rollback

--- tests/integration/store/rootmulti/rollback_test.go
+++ tests/integration/store/rootmulti/rollback_test.go
@@ -14,56 +14,72 @@ import (
 )
 
 func TestRollback(t *testing.T) {
-	db := dbm.NewMemDB()
-	options := simapp.SetupOptions{
-		Logger:  log.NewNopLogger(),
-		DB:      db,
-		AppOpts: simtestutil.NewAppOptionsWithFlagHome(simapp.DefaultNodeHome),
+	testCases := []struct {
+		name            string
+		offlineRollback bool
+	}{
+		{"disable offline rollback", false},
+		{"enable offline rollback", true},
 	}
-	app := simapp.NewSimappWithCustomOptions(t, false, options)
-	app.Commit()
-	ver0 := app.LastBlockHeight()
-	// commit 10 blocks
-	for i := int64(1); i <= 10; i++ {
-		header := tmproto.Header{
-			Height:  ver0 + i,
-			AppHash: app.LastCommitID().Hash,
-		}
-		app.BeginBlock(abci.RequestBeginBlock{Header: header})
-		ctx := app.NewContext(false, header)
-		store := ctx.KVStore(app.GetKey("bank"))
-		store.Set([]byte("key"), []byte(fmt.Sprintf("value%d", i)))
-		app.Commit()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			db := dbm.NewMemDB()
+			options := simapp.SetupOptions{
+				Logger:  log.NewNopLogger(),
+				DB:      db,
+				AppOpts: simtestutil.NewAppOptionsWithFlagHome(simapp.DefaultNodeHome),
+			}
+			app := simapp.NewSimappWithCustomOptions(t, false, options)
+			app.Commit()
+			ver0 := app.LastBlockHeight()
+			// commit 10 blocks
+			for i := int64(1); i <= 10; i++ {
+				header := tmproto.Header{
+					Height:  ver0 + i,
+					AppHash: app.LastCommitID().Hash,
+				}
+				app.BeginBlock(abci.RequestBeginBlock{Header: header})
+				ctx := app.NewContext(false, header)
+				store := ctx.KVStore(app.GetKey("bank"))
+				store.Set([]byte("key"), []byte(fmt.Sprintf("value%d", i)))
+				app.Commit()
+			}
+
+			require.Equal(t, ver0+10, app.LastBlockHeight())
+			store := app.NewContext(true, tmproto.Header{}).KVStore(app.GetKey("bank"))
+			require.Equal(t, []byte("value10"), store.Get([]byte("key")))
+
+			// rollback 5 blocks
+			target := ver0 + 5
+			rs := app.CommitMultiStore()
+			require.NoError(t, rs.RollbackToVersionWithMode(target, tc.offlineRollback))
+			if tc.offlineRollback {
+				// assume loadLatestVersion on restart after offline rollback
+				rs.LoadLatestVersion()
+			}
+			require.Equal(t, target, app.LastBlockHeight())
+
+			// recreate app to have clean check state
+			app = simapp.NewSimApp(options.Logger, options.DB, nil, true, simtestutil.NewAppOptionsWithFlagHome(simapp.DefaultNodeHome))
+			store = app.NewContext(true, tmproto.Header{}).KVStore(app.GetKey("bank"))
+			require.Equal(t, []byte("value5"), store.Get([]byte("key")))
+
+			// commit another 5 blocks with different values
+			for i := int64(6); i <= 10; i++ {
+				header := tmproto.Header{
+					Height:  ver0 + i,
+					AppHash: app.LastCommitID().Hash,
+				}
+				app.BeginBlock(abci.RequestBeginBlock{Header: header})
+				ctx := app.NewContext(false, header)
+				store := ctx.KVStore(app.GetKey("bank"))
+				store.Set([]byte("key"), []byte(fmt.Sprintf("VALUE%d", i)))
+				app.Commit()
+			}
+
+			require.Equal(t, ver0+10, app.LastBlockHeight())
+			store = app.NewContext(true, tmproto.Header{}).KVStore(app.GetKey("bank"))
+			require.Equal(t, []byte("VALUE10"), store.Get([]byte("key")))
+		})
 	}
-
-	require.Equal(t, ver0+10, app.LastBlockHeight())
-	store := app.NewContext(true, tmproto.Header{}).KVStore(app.GetKey("bank"))
-	require.Equal(t, []byte("value10"), store.Get([]byte("key")))
-
-	// rollback 5 blocks
-	target := ver0 + 5
-	require.NoError(t, app.CommitMultiStore().RollbackToVersion(target))
-	require.Equal(t, target, app.LastBlockHeight())
-
-	// recreate app to have clean check state
-	app = simapp.NewSimApp(options.Logger, options.DB, nil, true, simtestutil.NewAppOptionsWithFlagHome(simapp.DefaultNodeHome))
-	store = app.NewContext(true, tmproto.Header{}).KVStore(app.GetKey("bank"))
-	require.Equal(t, []byte("value5"), store.Get([]byte("key")))
-
-	// commit another 5 blocks with different values
-	for i := int64(6); i <= 10; i++ {
-		header := tmproto.Header{
-			Height:  ver0 + i,
-			AppHash: app.LastCommitID().Hash,
-		}
-		app.BeginBlock(abci.RequestBeginBlock{Header: header})
-		ctx := app.NewContext(false, header)
-		store := ctx.KVStore(app.GetKey("bank"))
-		store.Set([]byte("key"), []byte(fmt.Sprintf("VALUE%d", i)))
-		app.Commit()
-	}
-
-	require.Equal(t, ver0+10, app.LastBlockHeight())
-	store = app.NewContext(true, tmproto.Header{}).KVStore(app.GetKey("bank"))
-	require.Equal(t, []byte("VALUE10"), store.Get([]byte("key")))
 }

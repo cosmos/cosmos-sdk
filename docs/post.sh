86fa9cb3abccba9c77eb946767576e329c4ef09f
Julien Robert <julien@rbrt.fr>
2022-11-25 15:49:48 +0100

    updates

--- docs/post.sh
+++ docs/post.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 find docs/modules ! -name '_category_.json' -type f -exec rm -rf {} +
-find docs/pkg ! -name '_category_.json' -type f -exec rm -rf {} +
 rm -rf docs/tooling/01-cosmovisor.md
 rm -rf docs/tooling/02-depinject.md
 rm -rf docs/run-node/04-rosetta.md

c29339a3e1efdca7ba4a7840a1e66bd051f851f0
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2022-11-29 10:12:10 -0500

    Update baseapp/baseapp.go
    Co-authored-by: Julien Robert <julien@rbrt.fr>

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -357,7 +357,11 @@ func (app *BaseApp) Init() error {
 	app.setProcessProposalState(emptyHeader)
 	app.Seal()
 
-	return app.cms.GetPruning().Validate()
+if app.cms == nil {
+	return errors.New("commit multi-store must not be nil")
+}
+
+return app.cms.GetPruning().Validate()
 }
 
 func (app *BaseApp) setMinGasPrices(gasPrices sdk.DecCoins) {

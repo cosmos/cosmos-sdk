caf6e472476e3970717602de63f1187712baddef
Jeancarlo <jeancarlobarrios@gmail.com>
2022-11-30 14:36:42 -0500

    fix inport

--- server/start.go
+++ server/start.go
@@ -28,7 +28,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/server/types"
 	pruningtypes "github.com/cosmos/cosmos-sdk/store/pruning/types"
 	"github.com/cosmos/cosmos-sdk/telemetry"
-	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/mempool"
 )
 

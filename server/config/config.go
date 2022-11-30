efe699bcb1860ae909f6281d898e3ae8d3599e6a
Jeancarlo <jeancarlobarrios@gmail.com>
2022-11-30 14:46:36 -0500

    fix inport

--- server/config/config.go
+++ server/config/config.go
@@ -170,7 +170,7 @@ type StateSyncConfig struct {
 type MempoolConfig struct {
 	// MaxTxs defines the behavior of the mempool. A negative value indicates
 	// the mempool is disabled entirely, zero indicates that the mempool is
-	// unbounded in how many txs it may contain, and a postive value indicates
+	// unbounded in how many txs it may contain, and a positive value indicates
 	// the maximum amount of txs it may contain.
 	MaxTxs int
 }

fb4d964f7ecc34251a439ebbcd24c7e484f18e32
Amaury <1293565+amaurym@users.noreply.github.com>
2022-12-01 16:20:10 +0100

    refactor: don't break `SignModeHandler` API (#14087)
    * refactor: Make SignModeHandler update not API breaking
    
    * changelog
    
    * fix tests
    
    * add comment
    
    * Revert rosetta
    
    * address reviews
    
    * shorter name
    
    * Fix test build

--- x/auth/signing/handler_map.go
+++ x/auth/signing/handler_map.go
@@ -51,11 +51,28 @@ func (h SignModeHandlerMap) Modes() []signing.SignMode {
 	return h.modes
 }
 
-// DefaultMode implements SignModeHandler.GetSignBytes
-func (h SignModeHandlerMap) GetSignBytes(ctx context.Context, mode signing.SignMode, data SignerData, tx sdk.Tx) ([]byte, error) {
+// GetSignBytes implements SignModeHandler.GetSignBytes
+func (h SignModeHandlerMap) GetSignBytes(mode signing.SignMode, data SignerData, tx sdk.Tx) ([]byte, error) {
 	handler, found := h.signModeHandlers[mode]
 	if !found {
 		return nil, fmt.Errorf("can't verify sign mode %s", mode.String())
 	}
-	return handler.GetSignBytes(ctx, mode, data, tx)
+	return handler.GetSignBytes(mode, data, tx)
+}
+
+// GetSignBytesWithContext implements SignModeHandler.GetSignBytesWithContext
+func (h SignModeHandlerMap) GetSignBytesWithContext(ctx context.Context, mode signing.SignMode, data SignerData, tx sdk.Tx) ([]byte, error) {
+	handler, found := h.signModeHandlers[mode]
+	if !found {
+		return nil, fmt.Errorf("can't verify sign mode %s", mode.String())
+	}
+
+	handlerWithContext, ok := handler.(SignModeHandlerWithContext)
+	if ok {
+		return handlerWithContext.GetSignBytesWithContext(ctx, mode, data, tx)
+	}
+
+	// Default to stateless GetSignBytes if the underlying handler does not
+	// implement WithContext.
+	return handler.GetSignBytes(mode, data, tx)
 }

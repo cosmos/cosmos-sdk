9d12a5fce3b38c4c9c05c4b867a5c37106663a7d
atheeshp <59333759+atheeshp@users.noreply.github.com>
2022-12-02 18:56:25 +0530

    feat: implement parse method for Int,Dec,Coin (#13696)
    * wip: parse coins
    
    * review changes
    
    * update tests
    
    * update tests
    
    * Use ValueOfString
    
    * fix tests
    
    * fix coin tests
    
    * FIXES
    
    * Add empty coins test case
    
    * fix tests
    
    * fix tests
    
    * review changes
    
    * fix tests
    
    * fix tests
    
    * revert format.go
    
    * revert `format.go`
    
    * review changes
    
    * fix tests & review changes
    
    * review changes
    
    * review changes
    
    * review changes
    
    * review changes
    
    * review changes
    
    * review changes
    
    * proposal shorter code
    
    * review changes
    
    * conflicts
    
    * review changes
    
    * wip
    
    * ParseRepeated for coins
    
    * Revert listpb
    
    * remove duplicate case
    
    * revert some more
    
    Co-authored-by: Amaury M <1293565+amaurym@users.noreply.github.com>
    Co-authored-by: Marko <marbar3778@yahoo.com>

--- tx/textual/valuerenderer/int.go
+++ tx/textual/valuerenderer/int.go
@@ -2,6 +2,9 @@ package valuerenderer
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"cosmossdk.io/math"
 	"google.golang.org/protobuf/reflect/protoreflect"
@@ -9,11 +12,13 @@ import (
 
 // NewIntValueRenderer returns a ValueRenderer for uint32, uint64, int32 and
 // int64, and sdk.Int scalars.
-func NewIntValueRenderer() ValueRenderer {
-	return intValueRenderer{}
+func NewIntValueRenderer(fd protoreflect.FieldDescriptor) ValueRenderer {
+	return intValueRenderer{fd}
 }
 
-type intValueRenderer struct{}
+type intValueRenderer struct {
+	fd protoreflect.FieldDescriptor
+}
 
 var _ ValueRenderer = intValueRenderer{}
 
@@ -26,5 +31,62 @@ func (vr intValueRenderer) Format(_ context.Context, v protoreflect.Value) ([]Sc
 }
 
 func (vr intValueRenderer) Parse(_ context.Context, screens []Screen) (protoreflect.Value, error) {
-	panic("implement me")
+	if n := len(screens); n != 1 {
+		return nilValue, fmt.Errorf("expected 1 screen, got: %d", n)
+	}
+
+	parsedInt, err := parseInt(screens[0].Text)
+	if err != nil {
+		return nilValue, err
+	}
+
+	switch vr.fd.Kind() {
+	case protoreflect.Uint32Kind:
+		value, err := strconv.ParseUint(parsedInt, 10, 32)
+		if err != nil {
+			return nilValue, err
+		}
+		return protoreflect.ValueOfUint32(uint32(value)), nil //nolint:gosec
+
+	case protoreflect.Uint64Kind:
+		value, err := strconv.ParseUint(parsedInt, 10, 64)
+		if err != nil {
+			return nilValue, err
+		}
+		return protoreflect.ValueOfUint64(value), nil
+
+	case protoreflect.Int32Kind:
+		value, err := strconv.ParseInt(parsedInt, 10, 32)
+		if err != nil {
+			return nilValue, err
+		}
+		return protoreflect.ValueOfInt32(int32(value)), nil //nolint:gosec
+
+	case protoreflect.Int64Kind:
+		value, err := strconv.ParseInt(parsedInt, 10, 64)
+		if err != nil {
+			return nilValue, err
+		}
+		return protoreflect.ValueOfInt64(value), nil
+
+	case protoreflect.StringKind:
+		return protoreflect.ValueOfString(parsedInt), nil
+
+	default:
+		return nilValue, fmt.Errorf("parsing integers into a %s field is not supported", vr.fd.Kind())
+	}
+}
+
+// parseInt parses a value-rendered string into an integer
+func parseInt(v string) (string, error) {
+	sign := ""
+	if v[0] == '-' {
+		sign = "-"
+		v = v[1:]
+	}
+
+	// remove the 1000 separators (ex: 1'000'000 -> 1000000)
+	v = strings.ReplaceAll(v, "'", "")
+
+	return sign + v, nil
 }

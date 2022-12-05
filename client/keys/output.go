7d6025486c3863ef4dc1cf84027f4c31786d9661
Julian Toledano <jtoledanodiaz@gmail.com>
2022-12-05 11:22:33 +0100

    refactor: move key presentation to client/keys

--- client/keys/output.go
+++ client/keys/output.go
@@ -1,13 +1,13 @@
-package keyring
+package keys
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// TODO: Move this file to client/keys
 // Use protobuf interface marshaler rather then generic JSON
 
 // KeyOutput defines a structure wrapping around an Info object used for output
@@ -21,7 +21,7 @@ type KeyOutput struct {
 }
 
 // NewKeyOutput creates a default KeyOutput instance without Mnemonic, Threshold and PubKeys
-func NewKeyOutput(name string, keyType KeyType, a sdk.Address, pk cryptotypes.PubKey) (KeyOutput, error) { //nolint:interfacer
+func NewKeyOutput(name string, keyType keyring.KeyType, a sdk.Address, pk cryptotypes.PubKey) (KeyOutput, error) { //nolint:interfacer
 	apk, err := codectypes.NewAnyWithValue(pk)
 	if err != nil {
 		return KeyOutput{}, err
@@ -39,7 +39,7 @@ func NewKeyOutput(name string, keyType KeyType, a sdk.Address, pk cryptotypes.Pu
 }
 
 // MkConsKeyOutput create a KeyOutput in with "cons" Bech32 prefixes.
-func MkConsKeyOutput(k *Record) (KeyOutput, error) {
+func MkConsKeyOutput(k *keyring.Record) (KeyOutput, error) {
 	pk, err := k.GetPubKey()
 	if err != nil {
 		return KeyOutput{}, err
@@ -49,7 +49,7 @@ func MkConsKeyOutput(k *Record) (KeyOutput, error) {
 }
 
 // MkValKeyOutput create a KeyOutput in with "val" Bech32 prefixes.
-func MkValKeyOutput(k *Record) (KeyOutput, error) {
+func MkValKeyOutput(k *keyring.Record) (KeyOutput, error) {
 	pk, err := k.GetPubKey()
 	if err != nil {
 		return KeyOutput{}, err
@@ -63,7 +63,7 @@ func MkValKeyOutput(k *Record) (KeyOutput, error) {
 // MkAccKeyOutput create a KeyOutput in with "acc" Bech32 prefixes. If the
 // public key is a multisig public key, then the threshold and constituent
 // public keys will be added.
-func MkAccKeyOutput(k *Record) (KeyOutput, error) {
+func MkAccKeyOutput(k *keyring.Record) (KeyOutput, error) {
 	pk, err := k.GetPubKey()
 	if err != nil {
 		return KeyOutput{}, err
@@ -75,7 +75,7 @@ func MkAccKeyOutput(k *Record) (KeyOutput, error) {
 // MkAccKeysOutput returns a slice of KeyOutput objects, each with the "acc"
 // Bech32 prefixes, given a slice of Record objects. It returns an error if any
 // call to MkKeyOutput fails.
-func MkAccKeysOutput(records []*Record) ([]KeyOutput, error) {
+func MkAccKeysOutput(records []*keyring.Record) ([]KeyOutput, error) {
 	kos := make([]KeyOutput, len(records))
 	var err error
 	for i, r := range records {

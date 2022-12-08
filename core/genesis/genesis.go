6ac3d9cd0f08ab23ae22db4c605758e8149aa4a4
Aaron Craelius <aaronc@users.noreply.github.com>
2022-12-08 11:59:07 -0500

    docs

--- core/genesis/genesis.go
+++ core/genesis/genesis.go
@@ -7,35 +7,43 @@ import (
 	"google.golang.org/protobuf/runtime/protoiface"
 )
 
-// Source is a source for reading genesis files in JSON format or reading
-// from the protobuf messages. It may abstract over a single JSON object or JSON
-// in separate files that can be streamed over.
+// Source is a source for genesis data in JSON format. It may abstract over a
+// single JSON object or JSON in separate files that can be streamed over. Those
+// details are left to the implementation. Generally modules should either
+// consume a single JSON blob using the ReadMessage or ReadRawJSON methods OR
+// ideally, read individual fields using the OpenReader method. Using the
+// OpenReader method with array data will allow that data to be streamed which
+// can result in lower memory usage than reading a single large JSON blob into
+// memory.
 type Source interface {
-	// ReadMessage reads the source as a protobuf message.
+	// ReadRawJSON reads the all the genesis data as a single JSON blob.
+	ReadRawJSON() (json.RawMessage, error)
+
+	// ReadMessage reads the all the genesis data and attempts to unmarshal
+	// it as the provided protobuf message.
 	ReadMessage(protoiface.MessageV1) error
 
 	// OpenReader returns an io.ReadCloser for the named field. If there
-	// is no genesis file, this method will return nil. It is
-	// important the caller closes the reader when done with it.
+	// is data for this field, this method will return nil, nil. It is
+	// important that the caller closes the reader when done with it.
+	// It is expected that the reader points to a stream of JSON data.
 	OpenReader(field string) (io.ReadCloser, error)
-
-	// ReadRawJSON reads an encoded JSON data from the source file.
-	ReadRawJSON() (json.RawMessage, error)
 }
 
-// Target is a target for writing genesis files in JSON format or
-// writing the genesis to protobuf messages. It may abstract over a single JSON
-// object or JSON in separate files that can be streamed over.
+// Target is a target for writing genesis data in JSON format or. It may
+// abstract over a single JSON object or JSON in separate files that can be
+// streamed over. Modules should prefer writing fields separately if possible
+// using the OpenWriter method to take advantage of streaming.
 type Target interface {
+	// WriteRawJSON writes the encoded JSON data to the target.
+	WriteRawJSON(json.RawMessage) error
 
-	// WriteMessage writes a protobuf message to the target.
+	// WriteMessage writes the provided protobuf message to the target.
 	WriteMessage(protoiface.MessageV1) error
 
 	// OpenWriter returns an io.WriteCloser for the named field. It is
 	// important the caller closers the writer AND checks the error
-	// when done with it.
+	// when done with it. It is expected that a steam of JSON data is written
+	// to the writer.
 	OpenWriter(field string) (io.WriteCloser, error)
-
-	// WriteRawJSON writes the encoded JSON data to the target file.
-	WriteRawJSON(json.RawMessage) error
 }

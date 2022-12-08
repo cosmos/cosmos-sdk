e0caa71d10614b86de27b95eaa4512347ba6831b
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-12-08 11:40:03 +0100

    fix(gov): Fix v3 votes migrations

--- x/gov/migrations/v3/store_test.go
+++ x/gov/migrations/v3/store_test.go
@@ -41,6 +41,15 @@ func TestMigrateStore(t *testing.T) {
 	store.Set(v1gov.ProposalKey(prop1.ProposalId), prop1Bz)
 	store.Set(v1gov.ProposalKey(prop2.ProposalId), prop2Bz)
 
+	// Vote on prop 1
+	options := []v1beta1.WeightedVoteOption{
+		{Option: v1beta1.OptionNo, Weight: sdk.MustNewDecFromStr("0.5")},
+		{Option: v1beta1.OptionYes, Weight: sdk.MustNewDecFromStr("0.5")},
+	}
+	vote1 := v1beta1.NewVote(1, voter, options)
+	vote1Bz := cdc.MustMarshal(&vote1)
+	store.Set(v1gov.VoteKey(1, voter), vote1Bz)
+
 	// Run migrations.
 	err = v3gov.MigrateStore(ctx, govKey, cdc)
 	require.NoError(t, err)
@@ -54,6 +63,14 @@ func TestMigrateStore(t *testing.T) {
 	err = cdc.Unmarshal(store.Get(v1gov.ProposalKey(prop2.ProposalId)), &newProp2)
 	require.NoError(t, err)
 	compareProps(t, prop2, newProp2)
+
+	var newVote1 v1.Vote
+	err = cdc.Unmarshal(store.Get(v1gov.VoteKey(prop1.ProposalId, voter)), &newVote1)
+	require.NoError(t, err)
+	// Without the votes migration, we would have 500000000000000000 in state,
+	// because of how sdk.Dec stores itself in state.
+	require.Equal(t, "0.500000000000000000", newVote1.Options[0].Weight)
+	require.Equal(t, "0.500000000000000000", newVote1.Options[1].Weight)
 }
 
 func compareProps(t *testing.T, oldProp v1beta1.Proposal, newProp v1.Proposal) {

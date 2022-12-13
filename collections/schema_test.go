62b43dd4f1149adcbbd66d7c915ca1629fefd9d1
Aaron Craelius <aaronc@users.noreply.github.com>
2022-12-13 16:26:46 -0500

    add tests

--- collections/schema_test.go
+++ collections/schema_test.go
@@ -13,3 +13,24 @@ func TestNameRegex(t *testing.T) {
 	require.NotRegexp(t, nameRegex, "_bar")
 	require.NotRegexp(t, nameRegex, "abc-xyz")
 }
+
+func TestAddCollection(t *testing.T) {
+	sk, _ := deps()
+
+	require.Panics(t, func() {
+		schema := NewSchema(sk)
+		schema.addCollection(NewMap(schema, NewPrefix(2), "123", Uint64Key, Uint64Value))
+	}, "invalid name")
+
+	require.Panics(t, func() {
+		schema := NewSchema(sk)
+		schema.addCollection(NewMap(schema, NewPrefix(1), "abc", Uint64Key, Uint64Value))
+		schema.addCollection(NewMap(schema, NewPrefix(1), "def", Uint64Key, Uint64Value))
+	}, "prefix conflict")
+
+	require.Panics(t, func() {
+		schema := NewSchema(sk)
+		schema.addCollection(NewMap(schema, NewPrefix(1), "abc", Uint64Key, Uint64Value))
+		schema.addCollection(NewMap(schema, NewPrefix(2), "abc", Uint64Key, Uint64Value))
+	}, "name conflict")
+}

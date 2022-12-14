c96633a75e650c64377dfa16ad6629ad8bff0ae6
Matt Kocubinski <mkocubinski@gmail.com>
2022-12-14 10:31:02 -0600

    add a clarifying comment

--- x/staking/keeper/keeper.go
+++ x/staking/keeper/keeper.go
@@ -68,16 +68,17 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 }
 
 // Hooks gets the hooks for staking *Keeper {
-func (keeper *Keeper) Hooks() types.StakingHooks {
-	if keeper.hooks == nil {
+func (k *Keeper) Hooks() types.StakingHooks {
+	if k.hooks == nil {
 		// return a no-op implementation if no hooks are set
 		return types.MultiStakingHooks{}
 	}
 
-	return keeper.hooks
+	return k.hooks
 }
 
-// SetHooks Set the validator hooks
+// SetHooks Set the validator hooks.  In contrast to other receivers, this method must take a pointer due to nature
+// of the hooks interface and SDK start up sequence.
 func (k *Keeper) SetHooks(sh types.StakingHooks) {
 	if k.hooks != nil {
 		panic("cannot set validator hooks twice")

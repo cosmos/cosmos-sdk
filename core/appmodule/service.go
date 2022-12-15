16cb185be4f773e3d6851b58e424ab78c4b50452
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2022-12-15 12:11:08 -0500

    Update core/appmodule/service.go
    Co-authored-by: Amaury <1293565+amaurym@users.noreply.github.com>

--- core/appmodule/service.go
+++ core/appmodule/service.go
@@ -8,7 +8,7 @@ import (
 // Service bundles all the core services provided by a compliant runtime
 // implementation into a single services.
 //
-// NOTE: If new core services are provided after core v.10, they shouldn't be added to this
+// NOTE: If new core services are provided after core v1, they shouldn't be added to this
 // interface which would be API breaking, but rather a cosmossdk.io/core/appmodule/v2.Service
 // should be created which extends this Service interface with new services.
 type Service interface {

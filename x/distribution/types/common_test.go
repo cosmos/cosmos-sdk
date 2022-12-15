e213ce46e587ea9bc1f65602d4fb1017f114b766
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-16 04:06:54 +0700

    remove tagging for deadcode in distribution

--- x/distribution/types/common_test.go
+++ x/distribution/types/common_test.go
@@ -5,7 +5,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-//nolint:deadcode,varcheck
 var (
 	delPk1       = ed25519.GenPrivKey().PubKey()
 	delPk2       = ed25519.GenPrivKey().PubKey()

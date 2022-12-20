b1c534f36c3702ea78bd57fb77ad19d00772740b
testinginprod <testinginprod@somewhere.idk>
2022-12-20 12:00:40 +0100

    feat(collections): add sequence

---
+++ collections/sequence.go
@@ -0,0 +1,51 @@
+package collections
+
+import (
+	"context"
+	"errors"
+)
+
+// DefaultSequenceStart defines the default starting number of a sequence.
+const DefaultSequenceStart uint64 = 1
+
+// Sequence builds on top of an Item, and represents
+// an ever-increasing number.
+type Sequence Item[uint64]
+
+// NewSequence instantiates a new sequence given
+// a Schema, a Prefix and humanised name for the sequence.
+func NewSequence(schema Schema, prefix Prefix, name string) Sequence {
+	return (Sequence)(NewItem(schema, prefix, name, Uint64Value))
+}
+
+// Peek returns the current sequence value, if no number
+// is set then the DefaultSequenceStart is returned.
+// Errors on encoding issues.
+func (s Sequence) Peek(ctx context.Context) (uint64, error) {
+	n, err := (Item[uint64])(s).Get(ctx)
+	if err == nil {
+		return n, nil
+	} else {
+		if errors.Is(err, ErrNotFound) {
+			return DefaultSequenceStart, nil
+		} else {
+			return 0, err
+		}
+	}
+}
+
+// Next returns the next sequence number, and sets the next expected sequence.
+// Errors on encoding issues.
+func (s Sequence) Next(ctx context.Context) (uint64, error) {
+	seq, err := s.Peek(ctx)
+	if err != nil {
+		return 0, err
+	}
+	return seq, s.Set(ctx, seq+1)
+}
+
+// Set hard resets the sequence to the provided value.
+// Errors on encoding issues.
+func (s Sequence) Set(ctx context.Context, value uint64) error {
+	return (Item[uint64])(s).Set(ctx, value)
+}

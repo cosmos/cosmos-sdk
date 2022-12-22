3d4a1bd48a26fb1b768b77dd6047201d7b150440
Cal Bera <calbera@berachain.com>
2022-12-22 11:08:09 -0800

    update the event manager modifier on context

--- types/context.go
+++ types/context.go
@@ -243,7 +243,7 @@ func (c Context) WithConsensusParams(params *tmproto.ConsensusParams) Context {
 }
 
 // WithEventManager returns a Context with an updated event manager
-func (c Context) WithEventManager(em *EventManager) Context {
+func (c Context) WithEventManager(em EventManagerI) Context {
 	c.eventManager = em
 	return c
 }

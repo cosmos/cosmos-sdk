b958cc6cd58777430016a040f304396300636b81
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-28 14:17:10 +0700

    remove unused import

--- store/streaming/constructor_test.go
+++ store/streaming/constructor_test.go
@@ -3,7 +3,6 @@ package streaming_test
 import (
 	"testing"
 
-	dbm "github.com/cosmos/cosmos-db"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 
@@ -48,7 +47,6 @@ func TestStreamingServiceConstructor(t *testing.T) {
 }
 
 func TestLoadStreamingServices(t *testing.T) {
-
 	encCdc := types.NewTestCodec()
 	keys := types.NewKVStoreKeys("mockKey1", "mockKey2")
 

adde04358cc1450f6d80196936b4d95dc2600205
marbar3778 <marbar3778@yahoo.com>
2022-12-29 10:58:15 +0100

    remove uneeded constructor

--- store/metrics/telemetry.go
+++ store/metrics/telemetry.go
@@ -27,7 +27,7 @@ func NewMetrics(labels [][]string) Metrics {
 	if numGlobalLables := len(labels); numGlobalLables > 0 {
 		parsedGlobalLabels := make([]metrics.Label, numGlobalLables)
 		for i, gl := range labels {
-			parsedGlobalLabels[i] = NewLabel(gl[0], gl[1])
+			parsedGlobalLabels[i] = metrics.Label{Name: gl[0], Value: gl[1]}
 		}
 
 		gatherer.Labels = parsedGlobalLabels
--- store/metrics/util.go
+++
@@ -1,7 +0,0 @@
-package metrics
-
-import "github.com/armon/go-metrics"
-
-func NewLabel(name, value string) metrics.Label {
-	return metrics.Label{Name: name, Value: value}
-}

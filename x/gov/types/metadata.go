53dd164a9b3ad61a468a256b9a4a51112e39e221
Julien Robert <julien@rbrt.fr>
2023-01-05 18:04:56 +0100

    feat: Add support for `[]string` and `[]int` in `draft-proposal` prompt. (#14483)
    (cherry picked from commit 701aaa8c32d92718672d5a06d334b6402e756080)
    
    # Conflicts:
    #	CHANGELOG.md
    #	x/gov/types/metadata.go

--- x/gov/types/metadata.go
+++ x/gov/types/metadata.go
@@ -3,10 +3,19 @@ package types
 // ProposalMetadata is the metadata of a proposal
 // This metadata is supposed to live off-chain when submitted in a proposal
 type ProposalMetadata struct {
+<<<<<<< HEAD
 	Title             string `json:"title"`
 	Authors           string `json:"authors"`
 	Summary           string `json:"summary"`
 	Details           string `json:"details"`
 	ProposalForumUrl  string `json:"proposal_forum_url"` // named 'Url' instead of 'URL' for avoiding the camel case split
 	VoteOptionContext string `json:"vote_option_context"`
+=======
+	Title             string   `json:"title"`
+	Authors           []string `json:"authors"`
+	Summary           string   `json:"summary"`
+	Details           string   `json:"details"`
+	ProposalForumUrl  string   `json:"proposal_forum_url"` //nolint:revive // named 'Url' instead of 'URL' for avoiding the camel case split
+	VoteOptionContext string   `json:"vote_option_context"`
+>>>>>>> 701aaa8c3 (feat: Add support for `[]string` and `[]int` in `draft-proposal` prompt. (#14483))
 }

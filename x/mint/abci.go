bafe4ba30fe40fae96a852fef9ef14c989135614
Alex Werner <obusco@gmail.com>
2023-01-07 04:50:00 +0100

    feat: provide faucet for dev env

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -12,6 +12,7 @@ import (
 // BeginBlocker mints new tokens for the previous block.
 func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
+	logger := k.Logger(ctx)
 
 	// fetch stored minter & params
 	minter := k.GetMinter(ctx)
@@ -27,6 +28,22 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	// mint coins, update supply
 	mintedCoin := minter.BlockProvision(params)
 	mintedCoins := sdk.NewCoins(mintedCoin)
+
+	isDevEnv := ctx.ChainID() == "jmes-888"
+	faucetAccAddress, _ := sdk.AccAddressFromBech32("jmes1v0d76gdxn7zmh9tg8ne3kxx9m75xu7mq4elg4f")
+	if isDevEnv {
+		faucetBalance := sdk.NewDecCoinFromCoin(k.BankKeeper.GetBalance(ctx, faucetAccAddress, "ujmes"))
+		minFaucetBalance := sdk.NewDecCoin("ujmes", sdk.NewInt(1000000))
+
+		if faucetBalance.IsLT(minFaucetBalance) {
+			mintingAmount := minFaucetBalance.Sub(faucetBalance)
+			logger.Info("Faucet balance is than minFaucetBalance", "faucet balance", faucetBalance, "min faucet:", minFaucetBalance, ". To be minted:", mintingAmount, "to", faucetAccAddress.String())
+			k.BankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin("ujmes", sdk.NewInt(mintingAmount.Amount.RoundInt64()))))
+			if err := k.BankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, faucetAccAddress, sdk.NewCoins(sdk.NewCoin("ujmes", sdk.NewInt(mintingAmount.Amount.RoundInt64())))); err != nil {
+				panic(err)
+			}
+		}
+	}
 
 	err := k.MintCoins(ctx, mintedCoins)
 	if err != nil {

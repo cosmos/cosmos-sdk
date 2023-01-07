3ca39722e177d12d4c2e52bffbab7fd22d9c5d84
Alex Werner <obusco@gmail.com>
2023-01-07 04:46:45 +0100

    feat: added x/bank.GetBalance to x/mint

--- x/mint/types/expected_keepers.go
+++ x/mint/types/expected_keepers.go
@@ -26,4 +26,5 @@ type BankKeeper interface {
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
 }

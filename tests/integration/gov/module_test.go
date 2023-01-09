82d0e9b13ae5d9ba86bfc6af93d9edb61b15ca00
likhita-809 <likhita@vitwit.com>
2023-01-09 12:32:56 +0530

    remove simapp usage from module_test.go in gov tests

--- tests/integration/gov/module_test.go
+++ tests/integration/gov/module_test.go
@@ -3,43 +3,33 @@ package gov_test
 import (
 	"testing"
 
-	dbm "github.com/cosmos/cosmos-db"
-	abcitypes "github.com/tendermint/tendermint/abci/types"
-	tmjson "github.com/tendermint/tendermint/libs/json"
-	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	"gotest.tools/v3/assert"
 
-	"cosmossdk.io/simapp"
-
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
-	db := dbm.NewMemDB()
-
-	appOptions := make(simtestutil.AppOptionsMap, 0)
-	appOptions[flags.FlagHome] = simapp.DefaultNodeHome
-	appOptions[server.FlagInvCheckPeriod] = 5
-
-	app := simapp.NewSimApp(log.NewNopLogger(), db, nil, true, appOptions)
-
-	genesisState := simapp.GenesisStateWithSingleValidator(t, app)
-	stateBytes, err := tmjson.Marshal(genesisState)
+	var accountKeeper authkeeper.AccountKeeper
+	app, err := simtestutil.SetupWithConfiguration(
+		configurator.NewAppConfig(
+			configurator.ParamsModule(),
+			configurator.AuthModule(),
+			configurator.StakingModule(),
+			configurator.BankModule(),
+			configurator.GovModule(),
+			configurator.ConsensusModule(),
+		),
+		simtestutil.DefaultStartUpConfig(),
+		&accountKeeper,
+	)
 	assert.NilError(t, err)
 
-	app.InitChain(
-		abcitypes.RequestInitChain{
-			AppStateBytes: stateBytes,
-			ChainId:       "test-chain-id",
-		},
-	)
-
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
-	acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
+	acc := accountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
 	assert.Assert(t, acc != nil)
 }

22ef32b8b804b5ddff1d54e33141fc1302b5d355
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-01-09 11:04:50 +0200

    tests: fix handler tests to include GasMeter

--- types/handler_test.go
+++ types/handler_test.go
@@ -41,7 +41,6 @@ func TestChainPostDecorators(t *testing.T) {
 	// Create empty context as well as transaction
 	ctx := sdk.Context{}
 	tx := sdk.Tx(nil)
-	res := &sdk.Result{}
 
 	// Create mocks
 	mockCtrl := gomock.NewController(t)
@@ -49,19 +48,19 @@ func TestChainPostDecorators(t *testing.T) {
 	mockPostDecorator2 := mock.NewMockPostDecorator(mockCtrl)
 
 	// Test chaining only one post decorator
-	mockPostDecorator1.EXPECT().PostHandle(gomock.Eq(ctx), gomock.Eq(tx), gomock.Eq(res.MsgResponses), true, gomock.Eq(true), gomock.Any()).Times(1)
-	_, err := sdk.ChainPostDecorators(mockPostDecorator1)(ctx, tx, res.MsgResponses, true, true)
+	mockPostDecorator1.EXPECT().PostHandle(gomock.Eq(ctx), gomock.Eq(tx), gomock.Eq(ctx.GasMeter()), true, gomock.Eq(true), gomock.Any()).Times(1)
+	_, err := sdk.ChainPostDecorators(mockPostDecorator1)(ctx, tx, ctx.GasMeter(), true, true)
 	require.NoError(t, err)
 
 	// Tests chaining multiple post decorators
-	mockPostDecorator1.EXPECT().PostHandle(gomock.Eq(ctx), gomock.Eq(tx), gomock.Eq(res.MsgResponses), true, gomock.Eq(true), gomock.Any()).Times(1)
-	mockPostDecorator2.EXPECT().PostHandle(gomock.Eq(ctx), gomock.Eq(tx), gomock.Eq(res.MsgResponses), true, gomock.Eq(true), gomock.Any()).Times(1)
+	mockPostDecorator1.EXPECT().PostHandle(gomock.Eq(ctx), gomock.Eq(tx), gomock.Eq(ctx.GasMeter()), true, gomock.Eq(true), gomock.Any()).Times(1)
+	mockPostDecorator2.EXPECT().PostHandle(gomock.Eq(ctx), gomock.Eq(tx), gomock.Eq(ctx.GasMeter()), true, gomock.Eq(true), gomock.Any()).Times(1)
 	// NOTE: we can't check that mockAnteDecorator2 is passed as the last argument because
 	// ChainAnteDecorators wraps the decorators into closures, so each decorator is
 	// receiving a closure.
 	_, err = sdk.ChainPostDecorators(
 		mockPostDecorator1,
 		mockPostDecorator2,
-	)(ctx, tx, res.MsgResponses, true, true)
+	)(ctx, tx, ctx.GasMeter(), true, true)
 	require.NoError(t, err)
 }

05192fccfef1b8434b7db2027caf368c9ff2995d
Aaron Craelius <aaronc@users.noreply.github.com>
2023-01-12 12:27:40 -0500

    feat(core): add begin/end block extension interfaces

--- core/appmodule/module.go
+++ core/appmodule/module.go
@@ -1,6 +1,10 @@
 package appmodule
 
-import "cosmossdk.io/depinject"
+import (
+	"context"
+
+	"cosmossdk.io/depinject"
+)
 
 // AppModule is a tag interface for app module implementations to use as a basis
 // for extension interfaces. It provides no functionality itself, but is the
@@ -11,4 +15,24 @@ type AppModule interface {
 
 	// IsAppModule is a dummy method to tag a struct as implementing an AppModule.
 	IsAppModule()
+}
+
+// HasBeginBlocker is the extension interface that modules should implement to run
+// custom logic before transaction processing in a block.
+type HasBeginBlocker interface {
+	AppModule
+
+	// BeginBlock is a method that will be run before transactions are processed in
+	// a block.
+	BeginBlock(context.Context) error
+}
+
+// HasEndBlocker is the extension interface that modules should implement to run
+// custom logic after transaction processing in a block.
+type HasEndBlocker interface {
+	AppModule
+
+	// EndBlock is a method that will be run after transactions are processed in
+	// a block.
+	EndBlock(context.Context) error
 }

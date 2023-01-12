c2427aa1ded2614c7ae6a6e87eff266131395443
0xmuralik <krishna192reddy@gmail.com>
2023-01-12 17:00:50 +0530

    return delegate amount

--- x/staking/keeper/msg_server.go
+++ x/staking/keeper/msg_server.go
@@ -338,10 +338,12 @@ func (k msgServer) Undelegate(goCtx context.Context, msg *types.MsgUndelegate) (
 		)
 	}
 
-	completionTime, err := k.Keeper.Undelegate(ctx, delegatorAddress, addr, shares)
+	completionTime, undelegatedAmt, err := k.Keeper.Undelegate(ctx, delegatorAddress, addr, shares)
 	if err != nil {
 		return nil, err
 	}
+
+	undelegatedCoin := sdk.NewCoin(msg.Amount.Denom, undelegatedAmt)
 
 	if msg.Amount.Amount.IsInt64() {
 		defer func() {
@@ -358,13 +360,14 @@ func (k msgServer) Undelegate(goCtx context.Context, msg *types.MsgUndelegate) (
 		sdk.NewEvent(
 			types.EventTypeUnbond,
 			sdk.NewAttribute(types.AttributeKeyValidator, msg.ValidatorAddress),
-			sdk.NewAttribute(sdk.AttributeKeyAmount, msg.Amount.String()),
+			sdk.NewAttribute(sdk.AttributeKeyAmount, undelegatedCoin.String()),
 			sdk.NewAttribute(types.AttributeKeyCompletionTime, completionTime.Format(time.RFC3339)),
 		),
 	})
 
 	return &types.MsgUndelegateResponse{
 		CompletionTime: completionTime,
+		Amount:         undelegatedCoin,
 	}, nil
 }
 

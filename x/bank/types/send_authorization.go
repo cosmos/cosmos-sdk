4113494335d6da63fd141cbf12fa1665a315a0df
Julien Robert <julien@rbrt.fr>
2023-01-13 17:47:14 +0100

    fix(authz): fix allow list bypassed when whole spend limit used

--- x/bank/types/send_authorization.go
+++ x/bank/types/send_authorization.go
@@ -33,19 +33,9 @@ func (a SendAuthorization) Accept(ctx sdk.Context, msg sdk.Msg) (authz.AcceptRes
 		return authz.AcceptResponse{}, sdkerrors.ErrInvalidType.Wrap("type mismatch")
 	}
 
-	toAddr := mSend.ToAddress
-
-	limitLeft, isNegative := a.SpendLimit.SafeSub(mSend.Amount...)
-	if isNegative {
-		return authz.AcceptResponse{}, sdkerrors.ErrInsufficientFunds.Wrapf("requested amount is more than spend limit")
-	}
-	if limitLeft.IsZero() {
-		return authz.AcceptResponse{Accept: true, Delete: true}, nil
-	}
-
 	isAddrExists := false
+	toAddr := mSend.ToAddress
 	allowedList := a.GetAllowList()
-
 	for _, addr := range allowedList {
 		ctx.GasMeter().ConsumeGas(gasCostPerIteration, "send authorization")
 		if addr == toAddr {
@@ -56,6 +46,14 @@ func (a SendAuthorization) Accept(ctx sdk.Context, msg sdk.Msg) (authz.AcceptRes
 
 	if len(allowedList) > 0 && !isAddrExists {
 		return authz.AcceptResponse{}, sdkerrors.ErrUnauthorized.Wrapf("cannot send to %s address", toAddr)
+	}
+
+	limitLeft, isNegative := a.SpendLimit.SafeSub(mSend.Amount...)
+	if isNegative {
+		return authz.AcceptResponse{}, sdkerrors.ErrInsufficientFunds.Wrapf("requested amount is more than spend limit")
+	}
+	if limitLeft.IsZero() {
+		return authz.AcceptResponse{Accept: true, Delete: true}, nil
 	}
 
 	return authz.AcceptResponse{Accept: true, Delete: false, Updated: &SendAuthorization{SpendLimit: limitLeft, AllowList: allowedList}}, nil

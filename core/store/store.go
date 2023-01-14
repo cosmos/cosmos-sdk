d341b5762e193922276164159ca5f654aa60dcda
marbar3778 <marbar3778@yahoo.com>
2023-01-14 18:48:23 +0100

    fix comments

--- core/store/store.go
+++ core/store/store.go
@@ -4,16 +4,16 @@ import dbm "github.com/cosmos/cosmos-db"
 
 // KVStore describes the basic interface for interacting with key-value stores.
 type KVStore interface {
-	// Get returns nil iff key doesn't exist. Panics on nil key.
+	// Get returns nil iff key doesn't exist. Errors on nil key.
 	Get(key []byte) ([]byte, error)
 
-	// Has checks if a key exists. Panics on nil key.
+	// Has checks if a key exists. Errors on nil key.
 	Has(key []byte) (bool, error)
 
-	// Set sets the key. Panics on nil key or value.
+	// Set sets the key. Errors on nil key or value.
 	Set(key, value []byte) error
 
-	// Delete deletes the key. Panics on nil key.
+	// Delete deletes the key. Errors on nil key.
 	Delete(key []byte) error
 
 	// Iterator iterates over a domain of keys in ascending order. End is exclusive.
@@ -22,14 +22,14 @@ type KVStore interface {
 	// To iterate over entire domain, use store.Iterator(nil, nil)
 	// CONTRACT: No writes may happen within a domain while an iterator exists over it.
 	// Exceptionally allowed for cachekv.Store, safe to write in the modules.
-	Iterator(start, end []byte) (Iterator, error)
+	Iterator(start, end []byte) Iterator
 
 	// ReverseIterator iterates over a domain of keys in descending order. End is exclusive.
 	// Start must be less than end, or the Iterator is invalid.
 	// Iterator must be closed by caller.
 	// CONTRACT: No writes may happen within a domain while an iterator exists over it.
 	// Exceptionally allowed for cachekv.Store, safe to write in the modules.
-	ReverseIterator(start, end []byte) (Iterator, error)
+	ReverseIterator(start, end []byte) Iterator
 }
 
 // Iterator is an alias db's Iterator for convenience.

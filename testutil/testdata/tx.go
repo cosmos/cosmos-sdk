ab2000197199502f88bd5a95a82ea7eaeeb2a345
Amaury <1293565+amaurym@users.noreply.github.com>
2023-01-19 11:12:47 +0100

    Revert

--- testutil/testdata/tx.go
+++ testutil/testdata/tx.go
@@ -108,5 +108,5 @@ func (msg *TestMsg) ValidateBasic() error {
 
 var _ sdk.Msg = &MsgCreateDog{}
 
-func (msg *MsgCreateDog) GetSigners() []sdk.AccAddress { return nil }
+func (msg *MsgCreateDog) GetSigners() []sdk.AccAddress { return []sdk.AccAddress }
 func (msg *MsgCreateDog) ValidateBasic() error         { return nil }

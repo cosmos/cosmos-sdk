bbbbc6ad68d70f3805c5163a7c95871dcb04699a
marbar3778 <marbar3778@yahoo.com>
2023-01-20 18:13:53 +0100

    lint

--- types/query/collections_pagination_test.go
+++ types/query/collections_pagination_test.go
@@ -162,27 +162,22 @@ func (t testStore) Get(key []byte) ([]byte, error) {
 
 func (t testStore) Has(key []byte) (bool, error) {
 	return t.db.Has(key)
-
 }
 
 func (t testStore) Set(key, value []byte) error {
 	return t.db.Set(key, value)
-
 }
 
 func (t testStore) Delete(key []byte) error {
 	return t.db.Delete(key)
-
 }
 
 func (t testStore) Iterator(start, end []byte) (store.Iterator, error) {
 	return t.db.Iterator(start, end)
-
 }
 
 func (t testStore) ReverseIterator(start, end []byte) (store.Iterator, error) {
 	return t.db.ReverseIterator(start, end)
-
 }
 
 var _ store.KVStore = testStore{}

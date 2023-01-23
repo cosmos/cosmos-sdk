2fb43bc931925dc5f29beda550b45b332e498494
Aaron Craelius <aaronc@users.noreply.github.com>
2023-01-23 11:07:59 -0500

    add EmitProtoEventNonConsensus and godocs

--- core/event/manager.go
+++ core/event/manager.go
@@ -12,10 +12,24 @@ import (
 // build an app via depinject.
 type Service interface {
 	// EmitProtoEvent emits events represented as a protobuf message (as described in ADR 032).
+	//
+	// Callers SHOULD assume that these events may be included in consensus. These events
+	// MUST be emitted deterministically and adding, removing or changing these events SHOULD
+	// be considered state-machine breaking.
 	EmitProtoEvent(ctx context.Context, event protoiface.MessageV1) error
 
 	// EmitKVEvent emits an event based on an event and kv-pair attributes.
+	//
+	// These events will not be part of consensus and adding, removing or changing these events is
+	// not a state-machine breaking change.
 	EmitKVEvent(ctx context.Context, eventType string, attrs ...KVEventAttribute) error
+
+	// EmitProtoEventNonConsensus emits events represented as a protobuf message (as described in ADR 032), without
+	// including it in blockchain consensus.
+	//
+	// These events will not be part of consensus and adding, removing or changing events is
+	// not a state-machine breaking change.
+	EmitProtoEventNonConsensus(ctx context.Context, event protoiface.MessageV1) error
 }
 
 // KVEventAttribute is a kv-pair event attribute.

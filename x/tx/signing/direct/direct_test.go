705fc3f669e49b7d4dd5d11b922e2dd2a614a79d
Aaron Craelius <aaronc@users.noreply.github.com>
2023-01-23 17:44:19 -0500

    working test

--- x/tx/signing/direct/direct_test.go
+++ x/tx/signing/direct/direct_test.go
@@ -24,7 +24,9 @@ func TestDirectModeHandler(t *testing.T) {
 	msg, err := any.New(&bankv1beta1.MsgSend{})
 	require.NoError(t, err)
 
-	pk, err := any.New(&secp256k1.PubKey{})
+	pk, err := any.New(&secp256k1.PubKey{
+		Key: make([]byte, 256),
+	})
 	require.NoError(t, err)
 
 	accSeq := uint64(2) // Arbitrary account sequence
@@ -56,10 +58,13 @@ func TestDirectModeHandler(t *testing.T) {
 
 	directHandler := direct.SignModeHandler{}
 
+	chainId := "test-chain"
+	accNum := uint64(1)
+
 	signingData := signing.SignerData{
 		Address:       "",
-		ChainId:       "test-chain",
-		AccountNumber: 1,
+		ChainId:       chainId,
+		AccountNumber: accNum,
 		PubKey:        pk,
 	}
 
@@ -85,53 +90,14 @@ func TestDirectModeHandler(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, signBytes)
 
-	//authInfo := &txv1beta1.AuthInfo{
-	//	Fee:         fee,
-	//	SignerInfos: signerInfo,
-	//}
-	//
-	//authInfoBytes := marshaler.MustMarshal(authInfo)
-	//
-	//anys := make([]*codectypes.Any, len(msgs))
-	//
-	//for i, msg := range msgs {
-	//	var err error
-	//	anys[i], err = codectypes.NewAnyWithValue(msg)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//}
-	//
-	//txBody := &txtypes.TxBody{
-	//	Memo:     memo,
-	//	Messages: anys,
-	//}
-	//bodyBytes := marshaler.MustMarshal(txBody)
-	//
-	//t.Log("verify GetSignBytes with generating sign bytes by marshaling SignDoc")
-	//signDoc := txtypes.SignDoc{
-	//	AccountNumber: 1,
-	//	AuthInfoBytes: authInfoBytes,
-	//	BodyBytes:     bodyBytes,
-	//	ChainId:       "test-chain",
-	//}
-	//
-	//expectedSignBytes, err := signDoc.Marshal()
-	//require.NoError(t, err)
-	//require.Equal(t, expectedSignBytes, signBytes)
-	//
-	//t.Log("verify that setting signature doesn't change sign bytes")
-	//sigData.Signature, err = privKey.Sign(signBytes)
-	//require.NoError(t, err)
-	//err = txBuilder.SetSignatures(sig)
-	//require.NoError(t, err)
-	//signBytes, err = modeHandler.GetSignBytes(signingtypes.SignMode_SIGN_MODE_DIRECT, signingData, txBuilder.GetTx())
-	//require.NoError(t, err)
-	//require.Equal(t, expectedSignBytes, signBytes)
-	//
-	//t.Log("verify GetSignBytes with false txBody data")
-	//signDoc.BodyBytes = []byte("dfafdasfds")
-	//expectedSignBytes, err = signDoc.Marshal()
-	//require.NoError(t, err)
-	//require.NotEqual(t, expectedSignBytes, signBytes)
+	signBytes2, err := proto.Marshal(&txv1beta1.SignDoc{
+		BodyBytes:     txData.TxRaw.BodyBytes,
+		AuthInfoBytes: txData.TxRaw.AuthInfoBytes,
+		ChainId:       chainId,
+		AccountNumber: accNum,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, signBytes2)
+
+	require.Equal(t, signBytes2, signBytes)
 }

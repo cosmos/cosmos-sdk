8aeb72f33fcbbcc63c0cc14f46ac3dd32f1c581c
Facundo Medica <facundomedica@gmail.com>
2023-01-26 11:17:10 -0300

    progress

---
+++ types/module/mock_appmodule_test.go
@@ -0,0 +1,12 @@
+package module_test
+
+import (
+	"cosmossdk.io/core/appmodule"
+)
+
+// CoreAppModule is solely here for the purpose of generating
+// mocks to be used in module tests.
+type CoreAppModule interface {
+	appmodule.AppModule
+	appmodule.HasGenesis
+}

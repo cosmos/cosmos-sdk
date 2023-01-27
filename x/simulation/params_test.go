d3c319418514601a86c68552e8d76d54daa52a03
Julien Robert <julien@rbrt.fr>
2023-01-27 21:44:39 +0100

    fix: add simulation tests for new param change (#14728)

--- x/simulation/params_test.go
+++ x/simulation/params_test.go
@@ -12,13 +12,13 @@ import (
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 )
 
-func TestParamChange(t *testing.T) {
+func TestLegacyParamChange(t *testing.T) {
 	subspace, key := "theSubspace", "key"
 	f := func(r *rand.Rand) string {
 		return "theResult"
 	}
 
-	pChange := NewSimParamChange(subspace, key, f)
+	pChange := NewSimLegacyParamChange(subspace, key, f)
 
 	require.Equal(t, subspace, pChange.Subspace())
 	require.Equal(t, key, pChange.Key())
@@ -30,7 +30,7 @@ func TestNewWeightedProposalContent(t *testing.T) {
 	key := "theKey"
 	weight := 1
 	content := &testContent{}
-	f := func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) simtypes.Content {
+	f := func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) simtypes.Content { //nolint:staticcheck
 		return content
 	}
 

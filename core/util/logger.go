e6642ff13f1eec76fa7a895e2660d781abab8102
marbar3778 <marbar3778@yahoo.com>
2023-01-29 21:24:00 +0100

    logger interface

---
+++ core/util/logger.go
@@ -0,0 +1,9 @@
+package util
+
+type Logger interface {
+	Debug(msg string, keyvals ...interface{})
+	Info(msg string, keyvals ...interface{})
+	Error(msg string, keyvals ...interface{})
+
+	With(keyvals ...interface{}) Logger
+}

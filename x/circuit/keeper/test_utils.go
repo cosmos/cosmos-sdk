3cf68506d910c8069604ee62f4627aff94cd4c02
marbar3778 <marbar3778@yahoo.com>
2023-01-30 19:11:47 +0100

    fix build

--- x/circuit/keeper/test_utils.go
+++ x/circuit/keeper/test_utils.go
@@ -4,13 +4,8 @@ import (
 	"context"
 	"testing"
 
-	dbm "github.com/cosmos/cosmos-db"
-	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/tendermint/tendermint/libs/log"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	"gotest.tools/v3/assert"
 )
 
 type baseFixture struct {
@@ -26,14 +21,6 @@ type baseFixture struct {
 
 func setupBase(t *testing.T) *baseFixture {
 	s := &baseFixture{t: t}
-	db := dbm.NewMemDB()
-	cms := store.NewCommitMultiStore(db, log.NewNopLogger(), nil)
-	s.storeKey = storetypes.NewKVStoreKey("test")
-	cms.MountStoreWithDB(s.storeKey, storetypes.StoreTypeIAVL, db)
-	assert.NilError(t, cms.LoadLatestVersion())
-
-	s.sdkCtx = sdk.NewContext(cms, tmproto.Header{}, false, log.NewNopLogger())
-	s.ctx = sdk.WrapSDKContext(s.sdkCtx)
 
 	return s
 }

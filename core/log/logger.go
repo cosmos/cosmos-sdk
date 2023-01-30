e630a537bc43f2e69aeaee46805d4e6d640db6cb
marbar3778 <marbar3778@yahoo.com>
2023-01-30 15:07:51 +0100

    logger to service

--- core/log/logger.go
+++ core/log/logger.go
@@ -1,9 +1,10 @@
 package log
 
-type Logger interface {
+// Service is the interface that wraps the basic logging methods.
+type Service interface {
 	Debug(msg string, keyvals ...interface{})
 	Info(msg string, keyvals ...interface{})
 	Error(msg string, keyvals ...interface{})
 
-	With(keyvals ...interface{}) Logger
+	With(keyvals ...interface{}) Service
 }

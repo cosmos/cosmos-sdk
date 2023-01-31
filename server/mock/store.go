9f310d1c2b57ad51017d9ad79a44645556e519f3
HuangYi <huang@crypto.com>
2023-01-31 09:22:42 +0800

    fix test

--- server/mock/store.go
+++ server/mock/store.go
@@ -138,7 +138,7 @@ func (ms multiStore) SetIAVLDisableFastNode(disable bool) {
 	panic("not implemented")
 }
 
-func (ms multiStore) SetLazyLoading(bool) int64 {
+func (ms multiStore) SetLazyLoading(bool) {
 	panic("not implemented")
 }
 

612dfb3b33adcb7755ffa00d2b8a5843699e0a5e
Matt Kocubinski <mkocubinski@gmail.com>
2023-02-11 19:07:33 -0700

    refactor, cleanup

---
+++ x/tx/aminojson/encoder.go
@@ -0,0 +1,86 @@
+package aminojson
+
+import (
+	authapi "cosmossdk.io/api/cosmos/auth/v1beta1"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"io"
+)
+
+// nullSliceAsEmptyEncoder replicates the behavior at:
+// https://github.com/cosmos/cosmos-sdk/blob/be9bd7a8c1b41b115d58f4e76ee358e18a52c0af/types/coin.go#L199-L205
+func nullSliceAsEmptyEncoder(aj AminoJSON, v protoreflect.Value, w io.Writer) error {
+	switch list := v.Interface().(type) {
+	case protoreflect.List:
+		if list.Len() == 0 {
+			_, err := w.Write([]byte("[]"))
+			return err
+		}
+		return aj.marshalList(list, w)
+	default:
+		return fmt.Errorf("unsupported type %T", list)
+	}
+}
+
+func emptyStringEncoder(_ AminoJSON, _ protoreflect.Value, w io.Writer) error {
+	_, err := w.Write([]byte(`""`))
+	return err
+}
+
+func jsonDefaultEncoder(_ AminoJSON, v protoreflect.Value, w io.Writer) error {
+	switch val := v.Interface().(type) {
+	case string, bool, int32, uint32, uint64, int64, protoreflect.EnumNumber:
+		return jsonMarshal(w, val)
+	default:
+		return fmt.Errorf("unsupported type %T", val)
+	}
+}
+
+func keyFieldEncoder(msg protoreflect.Message, w io.Writer) error {
+	keyField := msg.Descriptor().Fields().ByName("key")
+	if keyField == nil {
+		return errors.New(`message encoder for key_field: no field named "key" found`)
+	}
+
+	bz := msg.Get(keyField).Bytes()
+
+	if len(bz) == 0 {
+		_, err := fmt.Fprint(w, "null")
+		return err
+	}
+
+	_, err := fmt.Fprintf(w, `"%s"`, base64.StdEncoding.EncodeToString(bz))
+	return err
+
+}
+
+// moduleAccountEncoder replicates the behavior in
+// https://github.com/cosmos/cosmos-sdk/blob/41a3dfeced2953beba3a7d11ec798d17ee19f506/x/auth/types/account.go#L230-L254
+func moduleAccountEncoder(msg protoreflect.Message, w io.Writer) error {
+	ma := msg.Interface().(*authapi.ModuleAccount)
+	pretty := moduleAccountPretty{
+		PubKey:      "",
+		Name:        ma.Name,
+		Permissions: ma.Permissions,
+	}
+	if ma.BaseAccount != nil {
+		pretty.Address = ma.BaseAccount.Address
+		pretty.AccountNumber = ma.BaseAccount.AccountNumber
+		pretty.Sequence = ma.BaseAccount.Sequence
+	} else {
+		pretty.Address = ""
+		pretty.AccountNumber = 0
+		pretty.Sequence = 0
+	}
+
+	bz, err := json.Marshal(pretty)
+	//bz, err := json.Marshal(pretty)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(bz)
+	return err
+}
--- x/tx/aminojson/json_marshal.go
+++ x/tx/aminojson/json_marshal.go
@@ -15,7 +15,6 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 
 	"cosmossdk.io/api/amino"
-	authapi "cosmossdk.io/api/cosmos/auth/v1beta1"
 )
 
 type MessageEncoder func(protoreflect.Message, io.Writer) error
@@ -36,50 +35,13 @@ func NewAminoJSON() AminoJSON {
 			},
 		},
 		messageEncoders: map[string]MessageEncoder{
-			"key_field": func(msg protoreflect.Message, w io.Writer) error {
-				keyField := msg.Descriptor().Fields().ByName("key")
-				if keyField == nil {
-					return errors.New(`message encoder for key_field: no field named "key" found`)
-				}
-
-				bz := msg.Get(keyField).Bytes()
-
-				if len(bz) == 0 {
-					_, err := fmt.Fprint(w, "null")
-					return err
-				}
-
-				_, err := fmt.Fprintf(w, `"%s"`, base64.StdEncoding.EncodeToString(bz))
-				return err
-			},
+			"key_field":      keyFieldEncoder,
 			"module_account": moduleAccountEncoder,
 		},
 		fieldEncoders: map[string]FieldEncoder{
-			"empty_string": func(_ AminoJSON, v protoreflect.Value, writer io.Writer) error {
-				_, err := writer.Write([]byte(`""`))
-				return err
-			},
-			"json_default": func(_ AminoJSON, v protoreflect.Value, writer io.Writer) error {
-				switch val := v.Interface().(type) {
-				case string, bool, int32, uint32, uint64, int64, protoreflect.EnumNumber:
-					return jsonMarshal(writer, val)
-				default:
-					return fmt.Errorf("unsupported type %T", val)
-				}
-			},
-			// created to replicate: https://github.com/cosmos/cosmos-sdk/blob/be9bd7a8c1b41b115d58f4e76ee358e18a52c0af/types/coin.go#L199-L205
-			"null_slice_as_empty": func(aj AminoJSON, v protoreflect.Value, writer io.Writer) error {
-				switch list := v.Interface().(type) {
-				case protoreflect.List:
-					if list.Len() == 0 {
-						_, err := writer.Write([]byte("[]"))
-						return err
-					}
-					return aj.marshalList(list, writer)
-				default:
-					return fmt.Errorf("unsupported type %T", list)
-				}
-			},
+			"empty_string":        emptyStringEncoder,
+			"json_default":        jsonDefaultEncoder,
+			"null_slice_as_empty": nullSliceAsEmptyEncoder,
 		},
 	}
 	return aj
@@ -451,34 +413,6 @@ type moduleAccountPretty struct {
 type typeWrapper struct {
 	Type  string `json:"type"`
 	Value any    `json:"value"`
-}
-
-// moduleAccountEncoder replicates the behavior in
-// https://github.com/cosmos/cosmos-sdk/blob/41a3dfeced2953beba3a7d11ec798d17ee19f506/x/auth/types/account.go#L230-L254
-func moduleAccountEncoder(msg protoreflect.Message, w io.Writer) error {
-	ma := msg.Interface().(*authapi.ModuleAccount)
-	pretty := moduleAccountPretty{
-		PubKey:      "",
-		Name:        ma.Name,
-		Permissions: ma.Permissions,
-	}
-	if ma.BaseAccount != nil {
-		pretty.Address = ma.BaseAccount.Address
-		pretty.AccountNumber = ma.BaseAccount.AccountNumber
-		pretty.Sequence = ma.BaseAccount.Sequence
-	} else {
-		pretty.Address = ""
-		pretty.AccountNumber = 0
-		pretty.Sequence = 0
-	}
-
-	bz, err := json.Marshal(pretty)
-	//bz, err := json.Marshal(pretty)
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(bz)
-	return err
 }
 
 const (

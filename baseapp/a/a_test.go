7ae46063ac1adc2ef95e9181338f500bee2889ce
Amaury <1293565+amaurym@users.noreply.github.com>
2023-02-13 15:49:10 +0100

    Fail test

--- baseapp/a/a_test.go
+++ baseapp/a/a_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/gogoproto/proto"
 	"github.com/stretchr/testify/require"
 
-	// _ "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	_ "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	_ "github.com/cosmos/gogoproto/types" // Import gogo types
 )
 

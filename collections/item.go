a9a9482c860f892f1712f2f1adb3f98868c7a1ab
testinginprod <98415576+testinginprod@users.noreply.github.com>
2023-02-19 14:06:53 +0100

    refactor(collections): move codecs to separate package and add int keys. (#15001)

--- collections/item.go
+++ collections/item.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+
+	"cosmossdk.io/collections/codec"
 )
 
 // Item is a type declaration based on Map
@@ -17,7 +19,7 @@ func NewItem[V any](
 	schema *SchemaBuilder,
 	prefix Prefix,
 	name string,
-	valueCodec ValueCodec[V],
+	valueCodec codec.ValueCodec[V],
 ) Item[V] {
 	item := (Item[V])(NewMap[noKey](schema, prefix, name, noKey{}, valueCodec))
 	return item

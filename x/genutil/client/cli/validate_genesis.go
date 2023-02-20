9f83600fb9ff55cdcc4e547c2fdc059dd408fbbe
Julien Robert <julien@rbrt.fr>
2023-02-20 12:02:14 +0100

    better docs

--- x/genutil/client/cli/validate_genesis.go
+++ x/genutil/client/cli/validate_genesis.go
@@ -20,7 +20,7 @@ func ValidateGenesisCmd(mbm module.BasicManager) *cobra.Command {
 		Use:     "validate [file]",
 		Aliases: []string{"validate-genesis"},
 		Args:    cobra.RangeArgs(0, 1),
-		Short:   "validates the genesis file at the default location or at the location passed as an arg",
+		Short:   "Validates the genesis file at the default location or at the location passed as an arg",
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			clientCtx := client.GetClientContextFromCmd(cmd)

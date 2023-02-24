81a7d2d051cf60ff09def5fcc5af6079aa3de0b4
Jeancarlo <jeancarlobarrios@gmail.com>
2023-02-23 18:49:26 -0600

    t

--- client/v2/autocli/msg_test.go
+++ client/v2/autocli/msg_test.go
@@ -95,49 +95,49 @@ var testCmdMsgDesc = &autocliv1.ServiceCommandDescriptor{
 	},
 }
 
-func testMsgBuildError(t *testing.T, args ...string) *testClientConn {
-	server := grpc.NewServer()
-	testpb.RegisterMsgServer(server, &testMessageServer{})
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-	assert.NilError(t, err)
-	go func() {
-		err := server.Serve(listener)
-		if err != nil {
-			panic(err)
-		}
-	}()
-	defer server.GracefulStop()
-	clientConn, err := grpc.Dial(listener.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	assert.NilError(t, err)
-	defer func() {
-		err := clientConn.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	conn := &testClientConn{
-		ClientConn: clientConn,
-		t:          t,
-		out:        &bytes.Buffer{},
-	}
-	b := &Builder{
-		GetClientConn: func(*cobra.Command) (grpc.ClientConnInterface, error) {
-			return conn, nil
-		},
-	}
-	buildModuleMsgCommand := func(moduleName string, cmdDescriptor *autocliv1.ServiceCommandDescriptor) (*cobra.Command, error) {
-		cmd := topLevelCmd(moduleName, fmt.Sprintf("Transations commands for the %s module", moduleName))
-
-		err := b.AddMsgServiceCommands(cmd, cmdDescriptor)
-		return cmd, err
-	}
-	cmd, err := buildModuleMsgCommand("test", testCmdMsgDesc)
-	assert.NilError(t, err)
-	cmd.SetArgs(args)
-	cmd.SetOut(conn.out)
-	return conn
-}
+//func testMsgBuildError(t *testing.T, args ...string) *testClientConn {
+//	server := grpc.NewServer()
+//	testpb.RegisterMsgServer(server, &testMessageServer{})
+//	listener, err := net.Listen("tcp", "127.0.0.1:0")
+//	assert.NilError(t, err)
+//	go func() {
+//		err := server.Serve(listener)
+//		if err != nil {
+//			panic(err)
+//		}
+//	}()
+//	defer server.GracefulStop()
+//	clientConn, err := grpc.Dial(listener.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+//	assert.NilError(t, err)
+//	defer func() {
+//		err := clientConn.Close()
+//		if err != nil {
+//			panic(err)
+//		}
+//	}()
+//
+//	conn := &testClientConn{
+//		ClientConn: clientConn,
+//		t:          t,
+//		out:        &bytes.Buffer{},
+//	}
+//	b := &Builder{
+//		GetClientConn: func(*cobra.Command) (grpc.ClientConnInterface, error) {
+//			return conn, nil
+//		},
+//	}
+//	buildModuleMsgCommand := func(moduleName string, cmdDescriptor *autocliv1.ServiceCommandDescriptor) (*cobra.Command, error) {
+//		cmd := topLevelCmd(moduleName, fmt.Sprintf("Transations commands for the %s module", moduleName))
+//
+//		err := b.AddMsgServiceCommands(cmd, cmdDescriptor)
+//		return cmd, err
+//	}
+//	cmd, err := buildModuleMsgCommand("test", testCmdMsgDesc)
+//	assert.NilError(t, err)
+//	cmd.SetArgs(args)
+//	cmd.SetOut(conn.out)
+//	return conn
+//}
 
 func testMsgExec(t *testing.T, args ...string) *testClientConn {
 	server := grpc.NewServer()

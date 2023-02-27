4a47ac0f633b7fb772c338fad50298f5f7afdc57
Julien Robert <julien@rbrt.fr>
2023-02-27 23:50:03 +0100

    feat: add filtering to logger

--- log/level.go
+++ log/level.go
@@ -1,5 +1,7 @@
 package log
 
+import "errors"
+
 const defaultLogLevelKey = "*"
 
 // ParseLogLevel parses complex log level
@@ -8,6 +10,12 @@ const defaultLogLevelKey = "*"
 //
 // Example:
 // ParseLogLevel("consensus:debug,mempool:debug,*:error", "info")
+// 
+// This function attemps to keep the same behavior as the CometBFT ParseLogLevel
 func ParseLogLevel(lvl string, defaultLogLevelValue string) (func(key, level string) bool, error) {
+	if lvl == "" {
+		return nil, errors.New("empty log level")
+	}
+
 	return func(key, level string) bool { return true }, nil
 }

3535ee51c7284e3a2c8afb1aaf02f3c315d96034
Julien Robert <julien@rbrt.fr>
2023-02-27 21:48:52 +0100

    updates

--- log/logger.go
+++ log/logger.go
@@ -19,23 +19,23 @@ var ContextKey struct{}
 type Logger interface {
 	// Info takes a message and a set of key/value pairs and logs with level INFO.
 	// The key of the tuple must be a string.
-	Info(msg string, keyVals ...interface{})
+	Info(msg string, keyVals ...any)
 
 	// Error takes a message and a set of key/value pairs and logs with level DEBUG.
 	// The key of the tuple must be a string.
-	Error(msg string, keyVals ...interface{})
+	Error(msg string, keyVals ...any)
 
 	// Debug takes a message and a set of key/value pairs and logs with level ERR.
 	// The key of the tuple must be a string.
-	Debug(msg string, keyVals ...interface{})
+	Debug(msg string, keyVals ...any)
 
 	// With returns a new wrapped logger with additional context provided by a set
-	With(keyVals ...interface{}) Logger
+	With(keyVals ...any) Logger
 
 	// Impl returns the underlying logger implementation
 	// It is used to access the full functionalities of the underlying logger
 	// Advanced users can type cast the returned value to the actual logger
-	Impl() interface{}
+	Impl() any
 }
 
 type zeroLogWrapper struct {

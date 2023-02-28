652c7b95ac0a0a5d68c2611155a1600853cf6d57
Julien Robert <julien@rbrt.fr>
2023-03-01 00:18:40 +0100

    chore: clean-up of  #15151

--- x/gov/migrations/v5/store.go
+++ x/gov/migrations/v5/store.go
@@ -24,9 +24,6 @@ func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.Binar
 	params.ExpeditedMinDeposit = defaultParams.ExpeditedMinDeposit
 	params.ExpeditedVotingPeriod = defaultParams.ExpeditedVotingPeriod
 	params.ExpeditedThreshold = defaultParams.ExpeditedThreshold
-	params.BurnProposalDepositPrevote = defaultParams.BurnProposalDepositPrevote
-	params.BurnVoteQuorum = defaultParams.BurnVoteQuorum
-	params.BurnVoteVeto = defaultParams.BurnVoteVeto
 
 	bz, err := cdc.Marshal(&params)
 	if err != nil {

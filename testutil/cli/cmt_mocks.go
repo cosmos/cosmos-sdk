80fac8a4124462acd31b2466ba0a5a6c7c4ce4c2
chixiaowen <chixiaowen@zhigui.com>
2023-02-28 12:59:39 +0800

    fix: add ABCIQueryWithOptions to ensure ci

--- testutil/cli/cmt_mocks.go
+++ testutil/cli/cmt_mocks.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	abci "github.com/cometbft/cometbft/abci/types"
+	cmtbytes "github.com/cometbft/cometbft/libs/bytes"
+	rpcclient "github.com/cometbft/cometbft/rpc/client"
 	rpcclientmock "github.com/cometbft/cometbft/rpc/client/mock"
 	coretypes "github.com/cometbft/cometbft/rpc/core/types"
 	cmttypes "github.com/cometbft/cometbft/types"
@@ -29,11 +31,11 @@ func (MockCometRPC) BroadcastTxSync(context.Context, cmttypes.Tx) (*coretypes.Re
 	return &coretypes.ResultBroadcastTx{Code: 0}, nil
 }
 
-//func (m MockCometRPC) ABCIQueryWithOptions(
-//	_ context.Context,
-//	_ string,
-//	_ cmtbytes.HexBytes,
-//	_ rpcclient.ABCIQueryOptions,
-//) (*coretypes.ResultABCIQuery, error) {
-//	return &coretypes.ResultABCIQuery{Response: m.responseQuery}, nil
-//}
+func (m MockCometRPC) ABCIQueryWithOptions(
+	_ context.Context,
+	_ string,
+	_ cmtbytes.HexBytes,
+	_ rpcclient.ABCIQueryOptions,
+) (*coretypes.ResultABCIQuery, error) {
+	return &coretypes.ResultABCIQuery{Response: m.responseQuery}, nil
+}

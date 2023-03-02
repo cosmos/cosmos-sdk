d618c29cb20d6438be6f512c3d2be9147d9edd88
Julien Robert <julien@rbrt.fr>
2023-03-02 12:49:19 +0100

    updates

--- docs/build-all.sh
+++ docs/build-all.sh
@@ -16,8 +16,8 @@ echo "building docusaurus main docs"
 (git clean -fdx && git reset --hard && git checkout $COMMIT)
 mv ~/versioned_docs ~/versioned_sidebars .
 npm ci && npm run build
-mv swagger ~/output/swagger
 mv build ~/output
+mv swagger ~/output/swagger
 while read -r branch path_prefix; do
     echo "building vuepress $branch docs"
     (git clean -fdx && git reset --hard && git checkout $branch && npm install && VUEPRESS_BASE="/$path_prefix/" npm run build)

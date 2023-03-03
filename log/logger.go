70e4bc24c22b85d3f9654cfc4f930fa93fbc097f
Julien Robert <julien@rbrt.fr>
2023-03-03 15:47:57 +0100

    punctuation

--- log/logger.go
+++ log/logger.go
@@ -7,15 +7,15 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// Defines commons keys for logging
+// Defines commons keys for logging.
 const ModuleKey = "module"
 
-// ContextKey is used to store the logger in the context
+// ContextKey is used to store the logger in the context.
 var ContextKey struct{}
 
-// Logger is the Cosmos SDK logger interface
-// It maintains as much backward compatibility with the CometBFT logger as possible
-// All functionalities of the logger are available through the Impl() method
+// Logger is the Cosmos SDK logger interface.
+// It maintains as much backward compatibility with the CometBFT logger as possible.
+// All functionalities of the logger are available through the Impl() method.
 type Logger interface {
 	// Info takes a message and a set of key/value pairs and logs with level INFO.
 	// The key of the tuple must be a string.
@@ -29,12 +29,12 @@ type Logger interface {
 	// The key of the tuple must be a string.
 	Debug(msg string, keyVals ...any)
 
-	// With returns a new wrapped logger with additional context provided by a set
+	// With returns a new wrapped logger with additional context provided by a set.
 	With(keyVals ...any) Logger
 
-	// Impl returns the underlying logger implementation
-	// It is used to access the full functionalities of the underlying logger
-	// Advanced users can type cast the returned value to the actual logger
+	// Impl returns the underlying logger implementation.
+	// It is used to access the full functionalities of the underlying logger.
+	// Advanced users can type cast the returned value to the actual logger.
 	Impl() any
 }
 
@@ -98,8 +98,8 @@ func (l zeroLogWrapper) Impl() interface{} {
 }
 
 // FilterKeys returns a new logger that filters out all key/value pairs that do not match the filter.
-// This functions assumes that the logger is a zerolog.Logger, which is the case for the logger returned by log.NewLogger()
-// NOTE: filtering has a performance impact on the logger
+// This functions assumes that the logger is a zerolog.Logger, which is the case for the logger returned by log.NewLogger().
+// NOTE: filtering has a performance impact on the logger.
 func FilterKeys(logger Logger, filter FilterFunc) Logger {
 	zl, ok := logger.Impl().(*zerolog.Logger)
 	if !ok {

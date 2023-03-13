c9398f22881db362ee04c67fa40728ceaebfc6bb
Julien Robert <julien@rbrt.fr>
2023-03-13 15:21:40 +0100

    change to sdterr

--- simapp/simd/main.go
+++ simapp/simd/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	rootCmd := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, "", simapp.DefaultNodeHome); err != nil {
-		log.NewLogger(rootCmd.OutOrStdout()).Error("failure when running app", "err", err)
+		log.NewLogger(rootCmd.OutOrStderr()).Error("failure when running app", "err", err)
 		os.Exit(1)
 	}
 }

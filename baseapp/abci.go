25d9f499dac95bcad6f9eae0adaecc3023234c1c
Facundo Medica <facundomedica@gmail.com>
2023-03-15 10:06:58 -0300

    feat: Add VoteInfos to context in BeginBlock

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -196,7 +196,8 @@ func (app *BaseApp) BeginBlock(req abci.RequestBeginBlock) (res abci.ResponseBeg
 	app.deliverState.ctx = app.deliverState.ctx.
 		WithBlockGasMeter(gasMeter).
 		WithHeaderHash(req.Hash).
-		WithConsensusParams(app.GetConsensusParams(app.deliverState.ctx))
+		WithConsensusParams(app.GetConsensusParams(app.deliverState.ctx)).
+		WithVoteInfos(req.LastCommitInfo.Votes)
 
 	if app.checkState != nil {
 		app.checkState.ctx = app.checkState.ctx.

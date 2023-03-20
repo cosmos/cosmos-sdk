4b888d8f2ff8cad9879fa62ccbf606fe0d58e5d6
Julien Robert <julien@rbrt.fr>
2023-03-20 14:32:35 +0100

    gogo proto ok.

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -6,21 +6,26 @@
 
 set -e
 
+pwd=$(pwd)
+
 echo "Generating gogo proto code"
-cd proto
-proto_dirs=$(find ./cosmos ./amino -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
-    # gogo proto files SHOULD ONLY be generated if this is false
-    # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
-    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$'; then
-      buf generate --template buf.gen.gogo.yaml $file
-    fi
+for i in `find . -name buf.gen.swagger.yaml -exec realpath {} \; 2> /dev/null`; do
+  dir=$(dirname $i)
+  cd $dir
+  proto_dirs=$(find $dir -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+  for dir in $proto_dirs; do
+    for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+      # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
+      # gogo proto files SHOULD ONLY be generated if this is false
+      # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
+      if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$'; then
+        buf generate --template buf.gen.gogo.yaml $file
+      fi
+    done
   done
 done
 
-cd ..
+cd $pwd
 
 # generate codec/testdata proto code
 (cd testutil/testdata; buf generate)
@@ -29,9 +34,11 @@ cd ..
 (cd baseapp/testutil; buf generate)
 
 # move proto files to the right places
-cp -r github.com/cosmos/cosmos-sdk/* ./
-cp -r cosmossdk.io/** ./
-rm -rf github.com cosmossdk.io
+cp -r github.com/cosmos/cosmos-sdk/* ./ && rm -rf github.com
+for dir in `find ./ -name cosmossdk.io -not -path '*/\.*' 2> /dev/null`; do
+  cp -r $dir/** $pwd
+  rm -r $dir
+done
 
 go mod tidy
 

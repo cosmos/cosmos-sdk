c97c226b679cc4795f370a3d58b5616f69bb8e19
Julien Robert <julien@rbrt.fr>
2023-03-20 14:14:48 +0100

    swagger ok.

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -2,18 +2,26 @@
 
 set -eo pipefail
 
+pwd=$(pwd)
+
 mkdir -p ./tmp-swagger-gen
-cd proto
-proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  # generate swagger files (filter query files)
-  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
-  if [[ ! -z "$query_file" ]]; then
-    buf generate --template buf.gen.swagger.yaml $query_file
-  fi
+for i in `find . -name buf.gen.swagger.yaml -exec realpath {} \; 2> /dev/null`
+do
+  dir=$(dirname $i)
+  cd $dir
+  proto_dirs=$(find $dir -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+  for dir in $proto_dirs; do
+    echo "Generating swagger files for $dir"
+    # generate swagger files (filter query files)
+    query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+    if [[ ! -z "$query_file" ]]; then
+      buf generate --template buf.gen.swagger.yaml $query_file
+    fi
+  done
 done
 
-cd ..
+cd $pwd
+
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging

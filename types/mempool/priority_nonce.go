bc328a1586758952a271cb8b1e94ae90e18ef0ef
Julien Robert <julien@rbrt.fr>
2023-03-20 22:16:04 +0100

    fix conflicts

--- types/mempool/priority_nonce.go
+++ types/mempool/priority_nonce.go
@@ -306,14 +306,10 @@ func (i *PriorityNonceIterator) Tx() sdk.Tx {
 //
 // The maxBytes parameter defines the maximum number of bytes of transactions to
 // return.
-<<<<<<< HEAD
-func (mp *PriorityNonceMempool) Select(_ context.Context, _ [][]byte) Iterator {
-=======
 //
 // NOTE: It is not safe to use this iterator while removing transactions from
 // the underlying mempool.
-func (mp *PriorityNonceMempool[C]) Select(_ context.Context, _ [][]byte) Iterator {
->>>>>>> 0d20a1456 (docs: update mempool Select godoc (#15486))
+func (mp *PriorityNonceMempool) Select(_ context.Context, _ [][]byte) Iterator {
 	if mp.priorityIndex.Len() == 0 {
 		return nil
 	}

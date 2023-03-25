1e6f2251823989677f7c96197c6c20ae2140d3be
marbar3778 <marbar3778@yahoo.com>
2023-03-25 12:53:38 +0100

    add status and extra data to config grpc requests

--- client/grpc/node/query.pb.go
+++ client/grpc/node/query.pb.go
@@ -68,6 +68,9 @@ var xxx_messageInfo_ConfigRequest proto.InternalMessageInfo
 // ConfigResponse defines the response structure for the Config gRPC query.
 type ConfigResponse struct {
 	MinimumGasPrice string `protobuf:"bytes,1,opt,name=minimum_gas_price,json=minimumGasPrice,proto3" json:"minimum_gas_price,omitempty"`
+	// pruning settings
+	PruningKeepRecent string `protobuf:"bytes,2,opt,name=pruning_keep_recent,json=pruningKeepRecent,proto3" json:"pruning_keep_recent,omitempty"`
+	PruningInterval   string `protobuf:"bytes,3,opt,name=pruning_interval,json=pruningInterval,proto3" json:"pruning_interval,omitempty"`
 }
 
 func (m *ConfigResponse) Reset()         { *m = ConfigResponse{} }
@@ -110,9 +113,107 @@ func (m *ConfigResponse) GetMinimumGasPrice() string {
 	return ""
 }
 
+func (m *ConfigResponse) GetPruningKeepRecent() string {
+	if m != nil {
+		return m.PruningKeepRecent
+	}
+	return ""
+}
+
+func (m *ConfigResponse) GetPruningInterval() string {
+	if m != nil {
+		return m.PruningInterval
+	}
+	return ""
+}
+
+// StateRequest defines the request structure for the status of a node.
+type StatusRequest struct {
+}
+
+func (m *StatusRequest) Reset()         { *m = StatusRequest{} }
+func (m *StatusRequest) String() string { return proto.CompactTextString(m) }
+func (*StatusRequest) ProtoMessage()    {}
+func (*StatusRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8324226a07064341, []int{2}
+}
+func (m *StatusRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *StatusRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_StatusRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *StatusRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StatusRequest.Merge(m, src)
+}
+func (m *StatusRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *StatusRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_StatusRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StatusRequest proto.InternalMessageInfo
+
+// StateResponse defines the response structure for the status of a node.
+type StatusResponse struct {
+	EarliestVersion string `protobuf:"bytes,1,opt,name=earliest_version,json=earliestVersion,proto3" json:"earliest_version,omitempty"`
+}
+
+func (m *StatusResponse) Reset()         { *m = StatusResponse{} }
+func (m *StatusResponse) String() string { return proto.CompactTextString(m) }
+func (*StatusResponse) ProtoMessage()    {}
+func (*StatusResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8324226a07064341, []int{3}
+}
+func (m *StatusResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *StatusResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_StatusResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *StatusResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StatusResponse.Merge(m, src)
+}
+func (m *StatusResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *StatusResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_StatusResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StatusResponse proto.InternalMessageInfo
+
+func (m *StatusResponse) GetEarliestVersion() string {
+	if m != nil {
+		return m.EarliestVersion
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*ConfigRequest)(nil), "cosmos.base.node.v1beta1.ConfigRequest")
 	proto.RegisterType((*ConfigResponse)(nil), "cosmos.base.node.v1beta1.ConfigResponse")
+	proto.RegisterType((*StatusRequest)(nil), "cosmos.base.node.v1beta1.StatusRequest")
+	proto.RegisterType((*StatusResponse)(nil), "cosmos.base.node.v1beta1.StatusResponse")
 }
 
 func init() {
@@ -120,25 +221,31 @@ func init() {
 }
 
 var fileDescriptor_8324226a07064341 = []byte{
-	// 282 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x90, 0xb1, 0x4b, 0xc3, 0x40,
-	0x18, 0xc5, 0x73, 0x0e, 0x15, 0x0f, 0xb4, 0x98, 0xa9, 0x14, 0x39, 0x4a, 0x10, 0x0c, 0x42, 0xef,
-	0xa8, 0xae, 0x4e, 0x3a, 0x74, 0x95, 0xba, 0xb9, 0x94, 0xcb, 0xf5, 0xf3, 0x3c, 0x6c, 0xee, 0x4b,
-	0x73, 0x97, 0x82, 0xab, 0xe0, 0xae, 0xf8, 0x4f, 0x39, 0x16, 0x5c, 0x1c, 0x25, 0xf1, 0x0f, 0x91,
-	0x24, 0xed, 0xe0, 0x50, 0x3a, 0x1d, 0xbc, 0xfb, 0xbd, 0xf7, 0x3d, 0x1e, 0x3d, 0x55, 0xe8, 0x52,
-	0x74, 0x22, 0x91, 0x0e, 0x84, 0xc5, 0x19, 0x88, 0xe5, 0x28, 0x01, 0x2f, 0x47, 0x62, 0x51, 0x40,
-	0xfe, 0xcc, 0xb3, 0x1c, 0x3d, 0x86, 0xbd, 0x96, 0xe2, 0x35, 0xc5, 0x6b, 0x8a, 0xaf, 0xa9, 0xfe,
-	0x89, 0x46, 0xd4, 0x73, 0x10, 0x32, 0x33, 0x42, 0x5a, 0x8b, 0x5e, 0x7a, 0x83, 0xd6, 0xb5, 0xbe,
-	0xa8, 0x4b, 0x0f, 0x6f, 0xd0, 0x3e, 0x18, 0x3d, 0x81, 0x45, 0x01, 0xce, 0x47, 0x57, 0xf4, 0x68,
-	0x23, 0xb8, 0x0c, 0xad, 0x83, 0xf0, 0x9c, 0x1e, 0xa7, 0xc6, 0x9a, 0xb4, 0x48, 0xa7, 0x5a, 0xba,
-	0x69, 0x96, 0x1b, 0x05, 0x3d, 0x32, 0x20, 0xf1, 0xc1, 0xa4, 0xbb, 0xfe, 0x18, 0x4b, 0x77, 0x5b,
-	0xcb, 0x17, 0xef, 0x84, 0xee, 0xdf, 0x41, 0xbe, 0x34, 0x0a, 0xc2, 0x57, 0x42, 0x3b, 0x6d, 0x54,
-	0x78, 0xc6, 0xb7, 0xd5, 0xe3, 0xff, 0xae, 0xf7, 0xe3, 0xdd, 0x60, 0xdb, 0x2a, 0x8a, 0x5f, 0xbe,
-	0x7e, 0x3f, 0xf6, 0xa2, 0x70, 0x20, 0xb6, 0xee, 0xa3, 0x1a, 0xc7, 0xf5, 0xf8, 0xb3, 0x64, 0x64,
-	0x55, 0x32, 0xf2, 0x53, 0x32, 0xf2, 0x56, 0xb1, 0x60, 0x55, 0xb1, 0xe0, 0xbb, 0x62, 0xc1, 0xfd,
-	0x50, 0x1b, 0xff, 0x58, 0x24, 0x5c, 0x61, 0xba, 0x49, 0x69, 0x9f, 0xa1, 0x9b, 0x3d, 0x09, 0x35,
-	0x37, 0x60, 0xbd, 0xd0, 0x79, 0xa6, 0x9a, 0xdc, 0xa4, 0xd3, 0x4c, 0x76, 0xf9, 0x17, 0x00, 0x00,
-	0xff, 0xff, 0x7d, 0x46, 0xb4, 0x93, 0x92, 0x01, 0x00, 0x00,
+	// 370 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x91, 0xbf, 0x6b, 0xe3, 0x30,
+	0x14, 0xc7, 0xa3, 0x1c, 0xe4, 0x38, 0xc1, 0xe5, 0x87, 0x6f, 0x31, 0xe1, 0x30, 0xc1, 0x1c, 0x5c,
+	0x72, 0x10, 0x89, 0xdc, 0x8d, 0xb7, 0xb5, 0x43, 0x28, 0x5d, 0x4a, 0x02, 0x1d, 0xba, 0x18, 0xd9,
+	0x79, 0x75, 0x45, 0x6c, 0x49, 0x91, 0x64, 0x43, 0xd7, 0x42, 0xf7, 0x96, 0x0e, 0xfd, 0x97, 0x3a,
+	0x06, 0xba, 0x74, 0x2c, 0x49, 0xff, 0x90, 0xe2, 0x5f, 0x43, 0x87, 0xd0, 0x49, 0xf0, 0x79, 0x9f,
+	0xf7, 0xf8, 0xea, 0x3d, 0xfc, 0x2b, 0x92, 0x26, 0x95, 0x86, 0x86, 0xcc, 0x00, 0x15, 0x72, 0x05,
+	0x34, 0x9f, 0x85, 0x60, 0xd9, 0x8c, 0x6e, 0x32, 0xd0, 0xd7, 0x44, 0x69, 0x69, 0xa5, 0xe3, 0x56,
+	0x16, 0x29, 0x2c, 0x52, 0x58, 0xa4, 0xb6, 0x86, 0x3f, 0x63, 0x29, 0xe3, 0x04, 0x28, 0x53, 0x9c,
+	0x32, 0x21, 0xa4, 0x65, 0x96, 0x4b, 0x61, 0xaa, 0x3e, 0xbf, 0x87, 0xbf, 0x1f, 0x4b, 0x71, 0xc9,
+	0xe3, 0x05, 0x6c, 0x32, 0x30, 0xd6, 0x7f, 0x44, 0xb8, 0xdb, 0x10, 0xa3, 0xa4, 0x30, 0xe0, 0xfc,
+	0xc1, 0x83, 0x94, 0x0b, 0x9e, 0x66, 0x69, 0x10, 0x33, 0x13, 0x28, 0xcd, 0x23, 0x70, 0xd1, 0x08,
+	0x8d, 0xbf, 0x2d, 0x7a, 0x75, 0x61, 0xce, 0xcc, 0x59, 0x81, 0x1d, 0x82, 0x7f, 0x28, 0x9d, 0x09,
+	0x2e, 0xe2, 0x60, 0x0d, 0xa0, 0x02, 0x0d, 0x11, 0x08, 0xeb, 0xb6, 0x4b, 0x7b, 0x50, 0x97, 0x4e,
+	0x01, 0xd4, 0xa2, 0x2c, 0x38, 0x13, 0xdc, 0x6f, 0x7c, 0x2e, 0x2c, 0xe8, 0x9c, 0x25, 0xee, 0x97,
+	0x6a, 0x74, 0xcd, 0x4f, 0x6a, 0x5c, 0x44, 0x5d, 0x5a, 0x66, 0x33, 0xd3, 0x44, 0xfd, 0x8f, 0xbb,
+	0x0d, 0xa8, 0x93, 0x4e, 0x70, 0x1f, 0x98, 0x4e, 0x38, 0x18, 0x1b, 0xe4, 0xa0, 0x0d, 0x97, 0xa2,
+	0x09, 0xda, 0xf0, 0xf3, 0x0a, 0xff, 0xbd, 0x47, 0xf8, 0xeb, 0x12, 0x74, 0x5e, 0x84, 0xbe, 0x45,
+	0xb8, 0x53, 0xfd, 0xd9, 0xf9, 0x4d, 0x0e, 0x2d, 0x92, 0x7c, 0xd8, 0xd3, 0x70, 0xfc, 0xb9, 0x58,
+	0x85, 0xf2, 0xc7, 0x37, 0xcf, 0x6f, 0x0f, 0x6d, 0xdf, 0x19, 0xd1, 0x83, 0x97, 0x8c, 0xca, 0x8e,
+	0xa3, 0xf9, 0xd3, 0xce, 0x43, 0xdb, 0x9d, 0x87, 0x5e, 0x77, 0x1e, 0xba, 0xdb, 0x7b, 0xad, 0xed,
+	0xde, 0x6b, 0xbd, 0xec, 0xbd, 0xd6, 0xc5, 0x34, 0xe6, 0xf6, 0x2a, 0x0b, 0x49, 0x24, 0xd3, 0x66,
+	0x4a, 0xf5, 0x4c, 0xcd, 0x6a, 0x4d, 0xa3, 0x84, 0x83, 0xb0, 0x34, 0xd6, 0x2a, 0x2a, 0xe7, 0x86,
+	0x9d, 0xf2, 0xb8, 0xff, 0xde, 0x03, 0x00, 0x00, 0xff, 0xff, 0x44, 0x74, 0xe0, 0x19, 0x3c, 0x02,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -266,10 +373,77 @@ func (m *ConfigResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.PruningInterval) > 0 {
+		i -= len(m.PruningInterval)
+		copy(dAtA[i:], m.PruningInterval)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.PruningInterval)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.PruningKeepRecent) > 0 {
+		i -= len(m.PruningKeepRecent)
+		copy(dAtA[i:], m.PruningKeepRecent)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.PruningKeepRecent)))
+		i--
+		dAtA[i] = 0x12
+	}
 	if len(m.MinimumGasPrice) > 0 {
 		i -= len(m.MinimumGasPrice)
 		copy(dAtA[i:], m.MinimumGasPrice)
 		i = encodeVarintQuery(dAtA, i, uint64(len(m.MinimumGasPrice)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *StatusRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *StatusRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *StatusRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *StatusResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *StatusResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *StatusResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.EarliestVersion) > 0 {
+		i -= len(m.EarliestVersion)
+		copy(dAtA[i:], m.EarliestVersion)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.EarliestVersion)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -303,6 +477,36 @@ func (m *ConfigResponse) Size() (n int) {
 	var l int
 	_ = l
 	l = len(m.MinimumGasPrice)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	l = len(m.PruningKeepRecent)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	l = len(m.PruningInterval)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func (m *StatusRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *StatusResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.EarliestVersion)
 	if l > 0 {
 		n += 1 + l + sovQuery(uint64(l))
 	}
@@ -425,6 +629,202 @@ func (m *ConfigResponse) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.MinimumGasPrice = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PruningKeepRecent", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PruningKeepRecent = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PruningInterval", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PruningInterval = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *StatusRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: StatusRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: StatusRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *StatusResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: StatusResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: StatusResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field EarliestVersion", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.EarliestVersion = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex

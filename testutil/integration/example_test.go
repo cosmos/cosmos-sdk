463ad91f36e9b3c46d81d2afdc4b48772aaa9442
Julien Robert <julien@rbrt.fr>
2023-03-27 18:57:58 +0200

    updates

--- testutil/integration/example_test.go
+++ testutil/integration/example_test.go
@@ -15,35 +15,32 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/mint"
 	mintkeeper "github.com/cosmos/cosmos-sdk/x/mint/keeper"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	_ "github.com/cosmos/cosmos-sdk/x/params"
 )
 
 func TestIntegrationTestExample(t *testing.T) {
 	// in this example we are testing the integration of the following modules:
 	// - mint, which directly depends on auth, bank and staking
 
-	cdc := moduletestutil.MakeTestEncodingConfig(auth.AppModuleBasic{}, mint.AppModuleBasic{})
+	encodingCfg := moduletestutil.MakeTestEncodingConfig(auth.AppModuleBasic{}, mint.AppModuleBasic{})
 	keys := storetypes.NewKVStoreKeys(authtypes.StoreKey, minttypes.StoreKey)
 	authority := authtypes.NewModuleAddress("gov").String()
 
 	accountKeeper := authkeeper.NewAccountKeeper(
-		cdc.Codec,
+		encodingCfg.Codec,
 		keys[authtypes.StoreKey],
 		authtypes.ProtoBaseAccount,
-		map[string][]string{
-			minttypes.ModuleName: {authtypes.Minter},
-		},
+		map[string][]string{minttypes.ModuleName: {authtypes.Minter}},
 		"cosmos",
 		authority,
 	)
 
 	// subspace is nil because we don't test params (which is legacy anyway)
-	authModule := auth.NewAppModule(cdc.Codec, accountKeeper, authsims.RandomGenesisAccounts, nil)
+	authModule := auth.NewAppModule(encodingCfg.Codec, accountKeeper, authsims.RandomGenesisAccounts, nil)
 
 	// here bankkeeper and staking keeper is nil because we are not testing them
 	// subspace is nil because we don't test params (which is legacy anyway)
-	mintKeeper := mintkeeper.NewKeeper(cdc.Codec, keys[minttypes.StoreKey], nil, accountKeeper, nil, authtypes.FeeCollectorName, authority)
-	mintModule := mint.NewAppModule(cdc.Codec, mintKeeper, accountKeeper, nil, nil)
+	mintKeeper := mintkeeper.NewKeeper(encodingCfg.Codec, keys[minttypes.StoreKey], nil, accountKeeper, nil, authtypes.FeeCollectorName, authority)
+	mintModule := mint.NewAppModule(encodingCfg.Codec, mintKeeper, accountKeeper, nil, nil)
 
 	integrationApp := integration.NewIntegrationApp(t, keys, authModule, mintModule)
 
@@ -62,6 +59,6 @@ func TestIntegrationTestExample(t *testing.T) {
 
 	// we now check the result
 	resp := minttypes.MsgUpdateParamsResponse{}
-	err = cdc.Codec.Unmarshal(result.Value, &resp)
+	err = encodingCfg.Codec.Unmarshal(result.Value, &resp)
 	assert.NilError(t, err)
 }

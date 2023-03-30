8634d5a4b9316e8858bed7223b92cbfe33411250
Jacob Gadikian <jacobgadikian@gmail.com>
2023-03-31 00:05:10 +0700

    remove references to the interfacer linter

--- x/authz/msgs.go
+++ x/authz/msgs.go
@@ -29,7 +29,7 @@ var (
 
 // NewMsgGrant creates a new MsgGrant
 //
-//nolint:interfacer
+
 func NewMsgGrant(granter, grantee sdk.AccAddress, a Authorization, expiration *time.Time) (*MsgGrant, error) {
 	m := &MsgGrant{
 		Granter: granter.String(),
@@ -110,7 +110,7 @@ func (msg MsgGrant) UnpackInterfaces(unpacker cdctypes.AnyUnpacker) error {
 
 // NewMsgRevoke creates a new MsgRevoke
 //
-//nolint:interfacer
+
 func NewMsgRevoke(granter, grantee sdk.AccAddress, msgTypeURL string) MsgRevoke {
 	return MsgRevoke{
 		Granter:    granter.String(),
@@ -154,7 +154,7 @@ func (msg MsgRevoke) GetSignBytes() []byte {
 
 // NewMsgExec creates a new MsgExecAuthorized
 //
-//nolint:interfacer
+
 func NewMsgExec(grantee sdk.AccAddress, msgs []sdk.Msg) MsgExec {
 	msgsAny := make([]*cdctypes.Any, len(msgs))
 	for i, msg := range msgs {
--- x/staking/types/delegation.go
+++ x/staking/types/delegation.go
@@ -15,7 +15,7 @@ var _ DelegationI = Delegation{}
 
 // NewDelegation creates a new delegation object
 //
-//nolint:interfacer
+
 func NewDelegation(delegatorAddr sdk.AccAddress, validatorAddr sdk.ValAddress, shares sdk.Dec) Delegation {
 	return Delegation{
 		DelegatorAddress: delegatorAddr.String(),
@@ -116,7 +116,7 @@ func UnmarshalUBDE(cdc codec.BinaryCodec, value []byte) (ubd UnbondingDelegation
 
 // NewUnbondingDelegation - create a new unbonding delegation object
 //
-//nolint:interfacer
+
 func NewUnbondingDelegation(
 	delegatorAddr sdk.AccAddress, validatorAddr sdk.ValAddress,
 	creationHeight int64, minTime time.Time, balance math.Int, id uint64,
@@ -213,7 +213,6 @@ func (e RedelegationEntry) OnHold() bool {
 	return e.UnbondingOnHoldRefCount > 0
 }
 
-//nolint:interfacer
 func NewRedelegation(
 	delegatorAddr sdk.AccAddress, validatorSrcAddr, validatorDstAddr sdk.ValAddress,
 	creationHeight int64, minTime time.Time, balance math.Int, sharesDst sdk.Dec, id uint64,
@@ -312,7 +311,7 @@ func (d DelegationResponses) String() (out string) {
 
 // NewRedelegationResponse crates a new RedelegationEntryResponse instance.
 //
-//nolint:interfacer
+
 func NewRedelegationResponse(
 	delegatorAddr sdk.AccAddress, validatorSrc, validatorDst sdk.ValAddress, entries []RedelegationEntryResponse,
 ) RedelegationResponse {

7b74c1dec76f4c77f6d628d6c533bb73a3c2af50
Jacob Gadikian <jacobgadikian@gmail.com>
2023-03-30 10:30:17 +0700

    cosmovisor linted

--- tools/cosmovisor/buffer_test.go
+++ tools/cosmovisor/buffer_test.go
@@ -6,28 +6,28 @@ import (
 )
 
 // buffer is a thread safe bytes buffer
-type buffer struct {
+type Buffer struct {
 	b bytes.Buffer
 	m sync.Mutex
 }
 
-func NewBuffer() *buffer {
-	return &buffer{}
+func NewBuffer() *Buffer {
+	return &Buffer{}
 }
 
-func (b *buffer) Write(bz []byte) (int, error) {
+func (b *Buffer) Write(bz []byte) (int, error) {
 	b.m.Lock()
 	defer b.m.Unlock()
 	return b.b.Write(bz)
 }
 
-func (b *buffer) String() string {
+func (b *Buffer) String() string {
 	b.m.Lock()
 	defer b.m.Unlock()
 	return b.b.String()
 }
 
-func (b *buffer) Reset() {
+func (b *Buffer) Reset() {
 	b.m.Lock()
 	defer b.m.Unlock()
 	b.b.Reset()

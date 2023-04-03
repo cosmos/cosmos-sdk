c710b9c3087529d6444d412d9526a1590653f049
MalteHerrmann <malte@evmos.org>
2023-04-03 08:36:38 +0200

    update testing script

--- config-test.sh
+++ config-test.sh
@@ -1,23 +1,49 @@
-printf "\n-------------------\n $ make build\n"
+BIN=./build/simd
+
+printf "\n-------------------\nBuilding binary\n"
+printf " $ make build\n"
 make build || exit 1
 
-printf "\n-------------------\n $ ./build/simd config\n"
-./build/simd config
+echo "Running version:"
+$BIN version
 
-printf "\n-------------------\n $ ./build/simd config home $HOME/.tmp-simd\n"
-./build/simd config home $HOME/.tmp-simd
+printf "\n-----------------\nShowing the current home directory\n"
+printf " $ ./build/simd config home\n"
+echo "Showing home directory"
+$BIN config home
 
-printf "\n-------------------\n $ ./build/simd config keyring-backend test\n"
-./build/simd config keyring-backend test
+printf "\n-----------------\nShow node in original home\n"
+printf " $ ./build/simd config client get node\n"
+$BIN config get client node
 
-printf "\n-------------------\n $ ./build/simd config node mynodeurl:26657\n"
-./build/simd config node "mynodeurl:26657"
+printf "\n-----------------\nSetting a new home directory (1st option)\n"
+printf " $ ./build/simd config home $HOME/.tmp-simd\n"
+$BIN config home $HOME/.tmp-simd
 
-printf "\n-------------------\n $ ./build/simd config\n"
-./build/simd config
+printf "\n-----------------\nSetting a new home directory (2nd option)\n"
+printf " $ ./build/simd config set home $HOME/.tmp-simd\n"
+$BIN config home $HOME/.tmp-simd
 
-printf "\n-------------------\n $ ./build/simd config home $HOME/.simapp\n"
-./build/simd config home $HOME/.simapp
+printf "\n-----------------\nAdjusting chain-id in new home\n"
+printf " $ ./build/simd config set client chain-id test\n"
+$BIN config set client chain-id test
+#
+printf "\n-----------------\nAdjusting node in new home\n"
+printf " $ ./build/simd config set client node mynodeurl:26657\n"
+$BIN config set client node "mynodeurl:26657"
 
-printf "\n-------------------\n $ ./build/simd config\n"
-./build/simd config
+printf "\n-----------------\nShow node in new home\n"
+printf " $ ./build/simd config get client node\n"
+$BIN config get client node
+
+printf "\n-----------------\nSet home back to original path\n"
+printf " $ ./build/simd config home $HOME/.simapp\n"
+$BIN config home $HOME/.simapp
+
+printf "\n-----------------\nGet current home\n"
+printf " $ ./build/simd config home\n"
+$BIN config home
+
+printf "\n-----------------\nShow node in original home\n"
+printf " $ ./build/simd config client get node\n"
+$BIN config get client node

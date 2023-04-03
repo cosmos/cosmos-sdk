e602b0158ee154fdd79f96f72440e2140b15715c
Julien Robert <julien@rbrt.fr>
2023-04-03 17:10:54 +0200

    updates

--- testutil/compare.go
+++ testutil/compare.go
@@ -10,5 +10,6 @@ import (
 
 // ProtoDeepEqual is a helper function that uses the protocmp package to compare two protobuf messages.
 func ProtoDeepEqual(t *testing.T, p1, p2 interface{}) {
+	t.Helper()
 	require.Empty(t, cmp.Diff(p1, p2, protocmp.Transform()))
 }

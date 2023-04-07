9b3d494b3e7474806af813644f98800a95c871a9
Facundo Medica <facundomedica@gmail.com>
2023-04-07 13:15:30 -0300

    update beginblock in baseapp

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -193,7 +193,8 @@ func (app *BaseApp) BeginBlock(req abci.RequestBeginBlock) (res abci.ResponseBeg
 		WithBlockGasMeter(gasMeter).
 		WithHeaderHash(req.Hash).
 		WithConsensusParams(app.GetConsensusParams(app.deliverState.ctx)).
-		WithVoteInfos(req.LastCommitInfo.GetVotes())
+		WithVoteInfos(req.LastCommitInfo.GetVotes()).
+		WithMisbehaviors(req.GetByzantineValidators())
 
 	if app.checkState != nil {
 		app.checkState.ctx = app.checkState.ctx.

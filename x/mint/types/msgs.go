d018f39a93a9b446e0466c9270cc2a862f94d12c
Julien Robert <julien@rbrt.fr>
2023-04-08 18:14:03 +0200

    refactor(mint): move `ValidateBasic` logic to `msgServer`

--- x/mint/types/msgs.go
+++ x/mint/types/msgs.go
@@ -1,8 +1,6 @@
 package types
 
 import (
-	"cosmossdk.io/errors"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
 )
@@ -21,17 +19,4 @@ func (m MsgUpdateParams) GetSignBytes() []byte {
 func (m MsgUpdateParams) GetSigners() []sdk.AccAddress {
 	addr, _ := sdk.AccAddressFromBech32(m.Authority)
 	return []sdk.AccAddress{addr}
-}
-
-// ValidateBasic does a sanity check on the provided data.
-func (m MsgUpdateParams) ValidateBasic() error {
-	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
-		return errors.Wrap(err, "invalid authority address")
-	}
-
-	if err := m.Params.Validate(); err != nil {
-		return err
-	}
-
-	return nil
 }

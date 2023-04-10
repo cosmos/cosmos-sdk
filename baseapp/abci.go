f894f571487461e01d10b29cfc294de184d92344
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-04-10 14:07:03 -0400

    updates

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -586,7 +586,7 @@ func (app *BaseApp) ExtendVote(_ context.Context, req *abci.RequestExtendVote) (
 				"hash", fmt.Sprintf("%X", req.Hash),
 				"panic", err,
 			)
-			err = fmt.Errorf("recovered panic in ExtendVote: %w", err)
+			err = fmt.Errorf("recovered application panic in ExtendVote: %w", err)
 		}
 	}()
 
@@ -623,7 +623,7 @@ func (app *BaseApp) VerifyVoteExtension(_ context.Context, req *abci.RequestVeri
 				"validator", fmt.Sprintf("%X", req.ValidatorAddress),
 				"panic", err,
 			)
-			err = fmt.Errorf("recovered panic in VerifyVoteExtension: %w", err)
+			err = fmt.Errorf("recovered application panic in VerifyVoteExtension: %w", err)
 		}
 	}()
 

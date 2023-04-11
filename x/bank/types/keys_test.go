7b2fb6f891ff65f18606fb97975d99ffa292b6f6
testinginprod <testinginprod@somewhere.idk>
2023-04-11 13:15:24 +0200

    add test for balance vlaue codec

---
+++ x/bank/types/keys_test.go
@@ -0,0 +1,25 @@
+package types
+
+import (
+	"cosmossdk.io/collections/colltest"
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestBalanceValueCodec(t *testing.T) {
+	c := NewBalanceCompatValueCodec()
+	t.Run("value codec implementation", func(t *testing.T) {
+		colltest.TestValueCodec(t, c, math.NewInt(100))
+	})
+
+	t.Run("legacy coin", func(t *testing.T) {
+		coin := sdk.NewInt64Coin("coin", 1000)
+		b, err := coin.Marshal()
+		require.NoError(t, err)
+		amt, err := c.Decode(b)
+		require.NoError(t, err)
+		require.Equal(t, coin.Amount, amt)
+	})
+}

28d1e5db73ebfbc8129e18bd50e1768a365dcd34
Julien Robert <julien@rbrt.fr>
2023-04-11 18:49:34 +0200

    updates

--- x/slashing/keeper/params.go
+++ x/slashing/keeper/params.go
@@ -49,6 +49,7 @@ func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 }
 
 // SetParams sets the x/slashing module parameters.
+// NOTE: This method performs no validation of the parameters.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) error {
 	store := ctx.KVStore(k.storeKey)
 	bz := k.cdc.MustMarshal(&params)

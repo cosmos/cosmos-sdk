0019cb97c0ab70e61a2ad59888899c14c5381d69
Matt Kocubinski <mkocubinski@gmail.com>
2023-04-12 09:52:35 -0500

    incorporate changes from #15713

--- x/auth/signing/verify.go
+++ x/auth/signing/verify.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	txsigning "cosmossdk.io/x/tx/signing"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/crypto/types/multisig"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -42,6 +43,44 @@ func VerifySignature(ctx context.Context, pubKey cryptotypes.PubKey, signerData 
 		return nil
 	default:
 		return fmt.Errorf("unexpected SignatureData %T", sigData)
+	}
+}
+
+// VerifySignatureV2 verifies a transaction signature contained in SignatureData abstracting over different signing
+// modes. It differs from VerifySignature in that it uses the new txsigning.TxData interface in x/tx.
+func VerifySignatureV2(
+	ctx context.Context,
+	pubKey cryptotypes.PubKey,
+	signerData txsigning.SignerData,
+	signatureData signing.SignatureData,
+	handler txsigning.SignModeHandler,
+	txData txsigning.TxData,
+) error {
+	switch data := signatureData.(type) {
+	case *signing.SingleSignatureData:
+		signBytes, err := handler.GetSignBytes(ctx, signerData, txData)
+		if err != nil {
+			return err
+		}
+		if !pubKey.VerifySignature(signBytes, data.Signature) {
+			return fmt.Errorf("unable to verify single signer signature")
+		}
+		return nil
+
+	case *signing.MultiSignatureData:
+		multiPK, ok := pubKey.(multisig.PubKey)
+		if !ok {
+			return fmt.Errorf("expected %T, got %T", (multisig.PubKey)(nil), pubKey)
+		}
+		err := multiPK.VerifyMultisignature(func(mode signing.SignMode) ([]byte, error) {
+			return handler.GetSignBytes(ctx, signerData, txData)
+		}, data)
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return fmt.Errorf("unexpected SignatureData %T", signatureData)
 	}
 }
 

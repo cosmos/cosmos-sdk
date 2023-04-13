7ea12e4961876375336fca976e76731ae81cd77a
babadro <babadro@gmail.com>
2023-04-13 06:21:53 +0200

    lint fix

--- x/staking/migrations/v2/keys.go
+++ x/staking/migrations/v2/keys.go
@@ -7,9 +7,7 @@ const (
 	ModuleName = "staking"
 )
 
-var (
-	HistoricalInfoKey = []byte{0x50} // prefix for the historical info
-)
+var HistoricalInfoKey = []byte{0x50} // prefix for the historical info
 
 // GetHistoricalInfoKey returns a key prefix for indexing HistoricalInfo objects.
 func GetHistoricalInfoKey(height int64) []byte {

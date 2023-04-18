4b8527452d3f264250d79f7c80eba7c9194519ea
marbar3778 <marbar3778@yahoo.com>
2023-04-18 20:29:09 +0200

    fixxie

--- core/header/doc.go
+++ core/header/doc.go
@@ -1,5 +1,6 @@
 /*
-Package header defines the Header Service interface and Info types.
+Package header defines a generalized Header type that all consensus & networking layers must provide.
+
 If modules need access to the current block header information, like height, hash, time, or chain ID
 they should use the Header Service interface.
 */

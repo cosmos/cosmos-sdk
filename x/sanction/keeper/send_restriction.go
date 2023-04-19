9fdc5465226249d4273a52d162d236bdca54648b
Daniel Wedul <github@wedul.com>
2023-04-19 13:08:44 -0600

    Update the quarantine module to use a SendRestriction.

--- x/sanction/keeper/send_restriction.go
+++ x/sanction/keeper/send_restriction.go
@@ -7,9 +7,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/sanction/errors"
 )
 
-var _ banktypes.SendRestrictionFn = (*Keeper)(nil).SendRestrictionFn
+var _ banktypes.SendRestrictionFn = Keeper{}.SendRestrictionFn
 
-func (k *Keeper) SendRestrictionFn(ctx types.Context, _, toAddr types.AccAddress, _ types.Coins) (types.AccAddress, error) {
+func (k Keeper) SendRestrictionFn(ctx types.Context, _, toAddr types.AccAddress, _ types.Coins) (types.AccAddress, error) {
 	if !sanction.HasSanctionBypass(ctx) && k.IsSanctionedAddr(ctx, toAddr) {
 		return nil, errors.ErrSanctionedAccount.Wrap(toAddr.String())
 	}

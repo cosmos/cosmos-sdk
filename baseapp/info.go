e9332419337dce58bdcffaa9e16fc3e5f65445ef
Aaron Craelius <aaronc@users.noreply.github.com>
2023-04-19 12:22:12 -0400

    delete code

--- baseapp/info.go
+++ baseapp/info.go
@@ -8,65 +8,6 @@ import (
 	"cosmossdk.io/core/comet"
 )
 
-//func NewHeaderInfo(height int64, hash []byte, t time.Time, chainID string) header.Info {
-//	return header.Info{
-//		Height:  height,
-//		Hash:    hash,
-//		Time:    t,
-//		ChainID: chainID,
-//	}
-//}
-//
-//func NewCometInfo(bg abci.RequestBeginBlock) comet.BlockInfo {
-//	return comet.BlockInfo{
-//		Evidence:          FromABCIEvidence(bg.ByzantineValidators),
-//		ValidatorsHash:    bg.Hash,
-//		ProposerAddress:   bg.Header.ProposerAddress,
-//		DecidedLastCommit: FromABCICommitInfo(bg.LastCommitInfo),
-//	}
-//}
-//
-//// Utils
-//
-//// FromABCIEvidence converts a CometBFT concrete Evidence type to
-//// SDK Evidence.
-//func FromABCIEvidence(e []abci.Misbehavior) []comet.Misbehavior {
-//	misbehavior := make([]comet.Misbehavior, len(e))
-//
-//	for i, ev := range e {
-//		misbehavior[i] = comet.Misbehavior{
-//			Type:   comet.MisbehaviorType(ev.Type),
-//			Height: ev.Height,
-//			Validator: comet.Validator{
-//				Address: ev.Validator.Address,
-//				Power:   ev.Validator.Power,
-//			},
-//			Time:             ev.Time,
-//			TotalVotingPower: ev.TotalVotingPower,
-//		}
-//	}
-//
-//	return misbehavior
-//}
-//
-//func FromABCICommitInfo(ci abci.CommitInfo) comet.CommitInfo {
-//	votes := make([]*comet.VoteInfo, len(ci.Votes))
-//	for i, v := range ci.Votes {
-//		votes[i] = &comet.VoteInfo{
-//			Validator: comet.Validator{
-//				Address: v.Validator.Address,
-//				Power:   v.Validator.Power,
-//			},
-//			SignedLastBlock: v.SignedLastBlock,
-//		}
-//	}
-//
-//	return comet.CommitInfo{
-//		Round: ci.Round,
-//		Votes: votes,
-//	}
-//}
-
 type reqBeginBlockInfo struct {
 	*abci.RequestBeginBlock
 }

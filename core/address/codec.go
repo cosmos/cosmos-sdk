7d830309f3b1acd16054c67dce69f9771945b8c2
Aaron Craelius <aaronc@users.noreply.github.com>
2023-04-20 12:30:18 -0400

    fix validate another way

--- core/address/codec.go
+++ core/address/codec.go
@@ -2,11 +2,8 @@ package address
 
 // Codec defines an interface to convert addresses from and to string/bytes.
 type Codec interface {
-	// StringToBytes decodes a string address (in bech32 format for example) to
-	// its binary representation. An empty string returns nil and not an error.
+	// StringToBytes decodes text to bytes
 	StringToBytes(text string) ([]byte, error)
-
-	// BytesToString encodes a binary address to its string representation (bech32
-	// for example). An empty byte array returns an empty string and not an error.
+	// BytesToString encodes bytes to text
 	BytesToString(bz []byte) (string, error)
 }

e1b049c8349d227bb8fe2cb5ffa8ad906c926573
Daniel Wedul <github@wedul.com>
2023-04-20 11:56:48 -0600

    Remove the quarantine and sanction expected keepers and mocks from the bank module.

--- x/bank/types/expected_keepers.go
+++ x/bank/types/expected_keepers.go
@@ -26,15 +26,3 @@ type AccountKeeper interface {
 	GetModuleAccount(ctx sdk.Context, moduleName string) types.ModuleAccountI
 	SetModuleAccount(ctx sdk.Context, macc types.ModuleAccountI)
 }
-
-// QuarantineKeeper defines the quarantine functionality needed from within the bank module.
-type QuarantineKeeper interface {
-	IsQuarantinedAddr(ctx sdk.Context, toAddr sdk.AccAddress) bool
-	IsAutoAccept(ctx sdk.Context, toAddr sdk.AccAddress, fromAddrs ...sdk.AccAddress) bool
-	GetFundsHolder() sdk.AccAddress
-	AddQuarantinedCoins(ctx sdk.Context, coins sdk.Coins, toAddr sdk.AccAddress, fromAddrs ...sdk.AccAddress) error
-}
-
-type SanctionKeeper interface {
-	IsSanctionedAddr(ctx sdk.Context, addr sdk.AccAddress) bool
-}

cdaa222770484af77583ef93495a6abfdbed7440
MalteHerrmann <malte@evmos.org>
2023-04-22 10:12:27 +0200

    update cobra home command description

--- tools/confix/cmd/home.go
+++ tools/confix/cmd/home.go
@@ -10,10 +10,9 @@ import (
 
 func HomeCommand() *cobra.Command {
 	return &cobra.Command{
-		Use: "home",
-		// TODO: Check usage output - currently not describing case for setting new value
+		Use:   "home [new-home]",
 		Short: "Read/Change the home directory. No home directory is set when using the tool standalone.",
-		Long:  "Read/Change the home directory. Given no argument, it outputs the string being used as the home path. Given one argument, the home directory is written to the config file. No home directory is set when using the tool standalone.",
+		Long:  "Read/Change the home directory. Given no argument, it outputs the string being used as the home path. Given one argument, the new home directory is set. No home directory is set when using the tool standalone.",
 		Args:  cobra.RangeArgs(0, 1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)

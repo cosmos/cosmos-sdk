d4ddd9147ad7d621c90ef26e2f06f0a6e20cf2e5
mmsqe <mavis@crypto.com>
2023-04-29 21:54:26 +0800

    add test

--- types/module/testutil/codec.go
+++ types/module/testutil/codec.go
@@ -47,3 +47,32 @@ func MakeTestTxConfig() client.TxConfig {
 	cdc := codec.NewProtoCodec(interfaceRegistry)
 	return tx.NewTxConfig(cdc, tx.DefaultSignModes)
 }
+
+type TestBuilderTxConfig struct {
+	client.TxConfig
+	TxBuilder *TestTxBuilder
+}
+
+func MakeBuilderTestTxConfig() TestBuilderTxConfig {
+	return TestBuilderTxConfig{
+		TxConfig: MakeTestTxConfig(),
+	}
+}
+
+func (cfg TestBuilderTxConfig) NewTxBuilder() client.TxBuilder {
+	if cfg.TxBuilder == nil {
+		cfg.TxBuilder = &TestTxBuilder{
+			TxBuilder: cfg.TxConfig.NewTxBuilder(),
+		}
+	}
+	return cfg.TxBuilder
+}
+
+type TestTxBuilder struct {
+	client.TxBuilder
+	ExtOptions []*types.Any
+}
+
+func (b *TestTxBuilder) SetExtensionOptions(extOpts ...*types.Any) {
+	b.ExtOptions = extOpts
+}

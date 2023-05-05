ec384c561dc8b488fcfa02c7a379291399452045
Matt Kocubinski <mkocubinski@gmail.com>
2023-05-05 16:41:01 -0500

    aminojson.Encoder now takes EncoderOptions as an arugment. aminojson.marhsalAny now uses a fileResolver and dynamicpb instead of a typeResolver

--- x/tx/signing/aminojson/any.go
+++ x/tx/signing/aminojson/any.go
@@ -4,34 +4,34 @@ import (
 	"fmt"
 	"io"
 
+	"google.golang.org/protobuf/types/dynamicpb"
 	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
-	"google.golang.org/protobuf/reflect/protoregistry"
 )
 
 func (enc Encoder) marshalAny(message protoreflect.Message, writer io.Writer) error {
 	anyMsg := message.Interface().(*anypb.Any)
-	resolver := protoregistry.GlobalTypes
 
-	typ, err := resolver.FindMessageByURL(anyMsg.TypeUrl)
+	desc, err := enc.fileResolver.FindDescriptorByName(protoreflect.FullName(anyMsg.TypeUrl[1:]))
 	if err != nil {
 		return errors.Wrapf(err, "can't resolve type URL %s", anyMsg.TypeUrl)
 	}
 
-	valueMsg := typ.New()
-	err = proto.Unmarshal(anyMsg.Value, valueMsg.Interface())
-	if err != nil {
-		return err
-	}
-
-	_, named := getMessageAminoName(valueMsg)
+	_, named := getMessageAminoName(desc.Options())
 	if !named {
 		return fmt.Errorf("message %s is packed into an any field, so requires an amino.name annotation",
 			anyMsg.TypeUrl)
 	}
 
-	return enc.beginMarshal(valueMsg, writer)
+	valueMsg := dynamicpb.NewMessageType(desc.(protoreflect.MessageDescriptor)).New().Interface()
+	err = proto.Unmarshal(anyMsg.Value, valueMsg)
+	if err != nil {
+		return err
+	}
+	protoMessage := valueMsg.ProtoReflect()
+
+	return enc.beginMarshal(protoMessage, writer)
 }

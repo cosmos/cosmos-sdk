d4b6d718f7e543cac3e6c4b5fe54f00c5df72fd8
yihuang <huang@crypto.com>
2023-05-10 10:07:27 +0800

    Update server/cmt_cmds.go

--- server/cmt_cmds.go
+++ server/cmt_cmds.go
@@ -263,9 +263,6 @@ func BootstrapStateCmd(appCreator types.AppCreator) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "bootstrap-state",
 		Short: "Bootstrap CometBFT state at an arbitrary block height using a light client",
-		Long: `
-Bootstrap CometBFT state at an arbitrary block height using a light client
-`,
 
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {

25df90d0fc6d41ffcf57d219ac5d0b4556c14a66
Marko <marbar3778@yahoo.com>
2023-05-10 12:25:07 +0200

    feat!: bootstrap comet cmd for local state sync  (#16061)
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
    Co-authored-by: HuangYi <huang@crypto.com>

--- server/util.go
+++ server/util.go
@@ -320,6 +320,7 @@ func AddCommands(rootCmd *cobra.Command, defaultNodeHome string, appCreator type
 		VersionCmd(),
 		cmtcmd.ResetAllCmd,
 		cmtcmd.ResetStateCmd,
+		BootstrapStateCmd(appCreator),
 	)
 
 	startCmd := StartCmd(appCreator, defaultNodeHome)

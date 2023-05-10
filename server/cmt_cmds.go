a4e2be952e827370770be49630340a72f1000882
marbar3778 <marbar3778@yahoo.com>
2023-05-10 11:36:04 +0200

    fix linting

--- server/cmt_cmds.go
+++ server/cmt_cmds.go
@@ -263,7 +263,7 @@ func BootstrapStateCmd(appCreator types.AppCreator) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "bootstrap-state",
 		Short: "Bootstrap CometBFT state at an arbitrary block height using a light client",
-		Args: cobra.NoArgs,
+		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			serverCtx := GetServerContextFromCmd(cmd)
 			logger := log.NewLogger(cmd.OutOrStdout())

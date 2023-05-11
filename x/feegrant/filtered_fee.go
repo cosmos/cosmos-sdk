d3370f231aaf4876649b4d88993b7ff3a3ff676d
Assaf Morami <assaf.morami@gmail.com>
2023-05-11 17:35:12 +0300

    fix(feegrant): infinite feegrant bug (#16097)

--- x/feegrant/filtered_fee.go
+++ x/feegrant/filtered_fee.go
@@ -63,7 +63,17 @@ func (a *AllowedMsgAllowance) Accept(ctx sdk.Context, fee sdk.Coins, msgs []sdk.
 		return false, err
 	}
 
-	return allowance.Accept(ctx, fee, msgs)
+	remove, err := allowance.Accept(ctx, fee, msgs)
+	if err != nil {
+		return false, err
+	}
+
+	a.Allowance, err = types.NewAnyWithValue(allowance.(proto.Message))
+	if err != nil {
+		return false, err
+	}
+
+	return remove, nil
 }
 
 func (a *AllowedMsgAllowance) allowedMsgsToMap(ctx sdk.Context) map[string]bool {

69e6204056fdc4fda896cb88867a4ebf49b4f5dc
marbar3778 <marbar3778@yahoo.com>
2023-05-12 14:47:41 +0200

    fix build

--- x/consensus/keeper/keeper.go
+++ x/consensus/keeper/keeper.go
@@ -81,13 +81,5 @@ func (k Keeper) UpdateParams(ctx context.Context, msg *types.MsgUpdateParams) (*
 		return nil, err
 	}
 
-	if err := k.event.EventManager(ctx).EmitKV(
-		ctx,
-		"update_consensus_params",
-		event.Attribute{Key: "authority", Value: req.Authority},
-		event.Attribute{Key: "parameters", Value: consensusParams.String()}); err != nil {
-		return nil, err
-	}
-
 	return &types.MsgUpdateParamsResponse{}, nil
 }

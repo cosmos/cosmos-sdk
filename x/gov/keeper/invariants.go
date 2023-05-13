730116e56b58d85040cae002a32989aa0b023673
unknown unknown <unknown@unknown>
2023-05-13 20:58:47 +0200

    fmt

--- x/gov/keeper/invariants.go
+++ x/gov/keeper/invariants.go
@@ -1,9 +1,10 @@
 package keeper
 
 import (
-	"cosmossdk.io/collections"
 	"errors"
 	"fmt"
+
+	"cosmossdk.io/collections"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"

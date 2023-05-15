5d3601f2e78893adc20b4ef18a8eaa194c9c8b4b
unknown unknown <unknown@unknown>
2023-05-15 21:11:34 +0200

    lint

--- x/gov/types/keys_test.go
+++ x/gov/types/keys_test.go
@@ -5,12 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
-
-var addr = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
 
 func TestProposalKeys(t *testing.T) {
 	// key proposal

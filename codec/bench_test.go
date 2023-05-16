9d16ef253ba5529be21548b03a44c43c808f5285
Aaron Craelius <aaronc@users.noreply.github.com>
2023-05-16 10:23:30 -0400

    add dynamicpb bench

--- codec/bench_test.go
+++ codec/bench_test.go
@@ -5,6 +5,8 @@ import (
 
 	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
 	"github.com/stretchr/testify/require"
+	protov2 "google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/dynamicpb"
 
 	codectestutil "github.com/cosmos/cosmos-sdk/codec/testutil"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
@@ -30,6 +32,7 @@ func BenchmarkProtoreflectGetSigners(b *testing.B) {
 	cdc := codectestutil.CodecOptions{}.NewCodec()
 	signingCtx := cdc.InterfaceRegistry().SigningContext()
 	_, _, addr := testdata.KeyTestPubAddr()
+	// use a pulsar message
 	msg := &bankv1beta1.MsgSend{
 		FromAddress: addr.String(),
 		ToAddress:   "",
@@ -61,6 +64,31 @@ func BenchmarkProtoreflectGetSignersWithUnmarshal(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_, _, err := cdc.GetMsgAnySigners(a)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkProtoreflectGetSignersDynamicpb(b *testing.B) {
+	cdc := codectestutil.CodecOptions{}.NewCodec()
+	signingCtx := cdc.InterfaceRegistry().SigningContext()
+	_, _, addr := testdata.KeyTestPubAddr()
+	msg := &bankv1beta1.MsgSend{
+		FromAddress: addr.String(),
+		ToAddress:   "",
+		Amount:      nil,
+	}
+	bz, err := protov2.Marshal(msg)
+	require.NoError(b, err)
+
+	dynamicmsg := dynamicpb.NewMessage(msg.ProtoReflect().Descriptor())
+	err = protov2.Unmarshal(bz, dynamicmsg)
+	require.NoError(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := signingCtx.GetSigners(dynamicmsg)
 		if err != nil {
 			panic(err)
 		}

e32138240fff11619cf74cfee4bfa0773d4a2f50
Jeancarlo <jeancarlobarrios@gmail.com>
2023-05-16 12:17:45 -0600

    feat(evidence): add autocli config

---
+++ x/evidence/autocli.go
@@ -0,0 +1,56 @@
+package evidence
+
+import (
+	"fmt"
+	"strings"
+
+	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+	evidencev1beta1 "cosmossdk.io/api/cosmos/evidence/v1beta1"
+	"cosmossdk.io/x/evidence/types"
+	"github.com/cosmos/cosmos-sdk/version"
+)
+
+func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+	return &autocliv1.ModuleOptions{
+		Query: &autocliv1.ServiceCommandDescriptor{
+			Service: evidencev1beta1.Query_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "Evidence",
+					Use:       "evidence [evidence-hash]",
+					Short:     "Query for evidence by hash or for all (paginated) submitted evidence",
+					Long: strings.TrimSpace(
+						fmt.Sprintf(`Query for specific submitted evidence by hash or query for all (paginated) evidence:
+
+Example:
+$ %s query %s DF0C23E8634E480F84B9D5674A7CDC9816466DEC28A3358F73260F68D28D7660
+$ %s query %s --page=2 --limit=50
+`,
+							version.AppName, types.ModuleName, version.AppName, types.ModuleName,
+						),
+					),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "hash"},
+					},
+				},
+				{
+					RpcMethod: "AllEvidence",
+					//	This is done with custom logic of zero params are passed it queries all evidence if not just by hash
+				},
+			},
+		},
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service: evidencev1beta1.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "SubmitEvidence",
+					Use:       "submit [evidence]",
+					Short:     "Submit arbitrary evidence of misbehavior",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "evidence"},
+					},
+				},
+			},
+		},
+	}
+}

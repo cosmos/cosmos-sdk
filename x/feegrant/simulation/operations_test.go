0fbdf96c9084a514dc96f64e3717fe82d949f56c
marbar3778 <marbar3778@yahoo.com>
2023-05-18 22:52:05 +0200

    fix lint

--- x/feegrant/simulation/operations_test.go
+++ x/feegrant/simulation/operations_test.go
@@ -19,7 +19,7 @@ import (
 	_ "github.com/cosmos/cosmos-sdk/x/mint"
 	_ "github.com/cosmos/cosmos-sdk/x/params"
 	_ "github.com/cosmos/cosmos-sdk/x/staking"
-	"github.com/golang/protobuf/proto"
+	"github.com/cosmos/gogoproto/proto"
 
 	"cosmossdk.io/x/feegrant"
 	"cosmossdk.io/x/feegrant/keeper"

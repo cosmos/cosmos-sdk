9fa63e55414b5cacae02add9373b8c0f6978b4d9
Jacob Gadikian <jacobgadikian@gmail.com>
2023-05-23 05:25:48 +0700

    //nolint:gosec in keyring.go

--- crypto/keyring/keyring.go
+++ crypto/keyring/keyring.go
@@ -749,7 +749,7 @@ func newRealPrompt(dir string, buf io.Reader) func(string) (string, error) {
 				continue
 			}
 
-			if err := os.WriteFile(dir+"/keyhash", passwordHash, 0o555); err != nil {
+			if err := os.WriteFile(dir+"/keyhash", passwordHash, 0o555); err != nil { //nolint:gosec // this fails when 600 or less permissions are used
 				return "", err
 			}
 

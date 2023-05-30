ddb13130fdff72ae15182445069efc594de30e06
unknown unknown <unknown@unknown>
2023-05-30 11:25:43 +0200

    remove EvidenceKey, RouterKey

--- x/evidence/types/keys.go
+++ x/evidence/types/keys.go
@@ -8,19 +8,9 @@ const (
 
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
-
-	// RouterKey defines the module's message routing key
-	RouterKey = ModuleName
 )
 
 // KVStore key prefixes
 var (
 	KeyPrefixEvidence = collections.NewPrefix(0)
 )
-
-func EvidenceKey(hash []byte) (key []byte) {
-	key = make([]byte, len(KeyPrefixEvidence)+len(hash))
-	copy(key, KeyPrefixEvidence)
-	copy(key[len(KeyPrefixEvidence):], hash)
-	return
-}

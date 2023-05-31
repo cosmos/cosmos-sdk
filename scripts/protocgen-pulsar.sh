d44e7b8cd72c4eaab3203e8625864418e55c5304
Julien Robert <julien@rbrt.fr>
2023-05-31 02:08:41 +0200

    build: optimize proto-builder

--- scripts/protocgen-pulsar.sh
+++ scripts/protocgen-pulsar.sh
@@ -2,13 +2,6 @@
 
 set -eo pipefail
 
-protoc_install_gopulsar() {
-  go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
-  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-}
-
-protoc_install_gopulsar
-
 echo "Cleaning API directory"
 (cd api; find ./ -type f \( -iname \*.pulsar.go -o -iname \*.pb.go -o -iname \*.cosmos_orm.go -o -iname \*.pb.gw.go \) -delete; find . -empty -type d -delete; cd ..)
 

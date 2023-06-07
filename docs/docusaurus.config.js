715198b98873cbeb5e122b8b23515dccd6b8f2da
Julien Robert <julien@rbrt.fr>
2023-06-07 22:56:59 +0200

    chore: configure release/v0.50.x branch

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -43,6 +43,10 @@ const config = {
               path: "main",
               banner: "unreleased",
             },
+            "v0.50": {
+              path: "v0.50",
+              label: "v0.50",
+            },
             "v0.47": {
               path: "v0.47",
               label: "v0.47",

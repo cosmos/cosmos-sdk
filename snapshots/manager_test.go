673ee3a545189f8408f58fd7d338256491e98189
HuangYi <huang@crypto.com>
2023-06-19 09:20:23 +0800

    fix test

--- snapshots/manager_test.go
+++ snapshots/manager_test.go
@@ -4,13 +4,14 @@ import (
 	"errors"
 	"testing"
 
-	db "github.com/cosmos/cosmos-db"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/snapshots"
 	"github.com/cosmos/cosmos-sdk/snapshots/types"
+	"github.com/cosmos/cosmos-sdk/testutil"
 )
 
 var opts = types.NewSnapshotOptions(1500, 2)
@@ -239,7 +240,7 @@ func TestManager_Restore(t *testing.T) {
 
 func TestManager_TakeError(t *testing.T) {
 	snapshotter := &mockErrorSnapshotter{}
-	store, err := snapshots.NewStore(db.NewMemDB(), GetTempDir(t))
+	store, err := snapshots.NewStore(dbm.NewMemDB(), testutil.GetTempDir(t))
 	require.NoError(t, err)
 	manager := snapshots.NewManager(store, opts, snapshotter, nil, log.NewNopLogger())
 

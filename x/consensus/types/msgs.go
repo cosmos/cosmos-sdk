1d93a452184147deda54626749f6dbc50dbf491b
Facundo Medica <facundomedica@gmail.com>
2023-06-27 12:37:42 +0200

    progress

--- x/consensus/types/msgs.go
+++ x/consensus/types/msgs.go
@@ -17,7 +17,7 @@ func (msg MsgUpdateParams) GetSigners() []sdk.AccAddress {
 }
 
 func (msg MsgUpdateParams) ToProtoConsensusParams() cmtproto.ConsensusParams {
-	return cmtproto.ConsensusParams{
+	cp := cmtproto.ConsensusParams{
 		Block: &cmtproto.BlockParams{
 			MaxBytes: msg.Block.MaxBytes,
 			MaxGas:   msg.Block.MaxGas,
@@ -32,4 +32,12 @@ func (msg MsgUpdateParams) ToProtoConsensusParams() cmtproto.ConsensusParams {
 		},
 		Version: cmttypes.DefaultConsensusParams().ToProto().Version, // Version is stored in x/upgrade
 	}
+
+	if msg.Abci != nil {
+		cp.Abci = &cmtproto.ABCIParams{
+			VoteExtensionsEnableHeight: msg.Abci.VoteExtensionsEnableHeight,
+		}
+	}
+
+	return cp
 }

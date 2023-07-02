77b364b80f7c2b7f35f1ec3d9d6e346b2bb64c01
Roman <roman@osmosis.team>
2023-07-02 19:46:00 -0400

    chore: state breaking barberry fix (#478)

--- x/auth/vesting/msg_server.go
+++ x/auth/vesting/msg_server.go
@@ -128,15 +128,13 @@ func (s msgServer) CreateClawbackVestingAccount(goCtx context.Context, msg *type
 		lockupCoins = lockupCoins.Add(period.Amount...)
 	}
 
-	if ctx.IsCheckTx() {
-		if err := lockupCoins.Validate(); err != nil {
-			return nil, sdkerrors.Wrapf(
-				sdkerrors.ErrInvalidCoins, "lockup coins %s are invalid: %v", lockupCoins, err)
-		}
-		if err := vestingCoins.Validate(); err != nil {
-			return nil, sdkerrors.Wrapf(
-				sdkerrors.ErrInvalidCoins, "vesting coins %s are invalid: %v", vestingCoins, err)
-		}
+	if err := lockupCoins.Validate(); err != nil {
+		return nil, sdkerrors.Wrapf(
+			sdkerrors.ErrInvalidCoins, "lockup coins %s are invalid: %v", lockupCoins, err)
+	}
+	if err := vestingCoins.Validate(); err != nil {
+		return nil, sdkerrors.Wrapf(
+			sdkerrors.ErrInvalidCoins, "vesting coins %s are invalid: %v", vestingCoins, err)
 	}
 
 	// if lockup absent, default to an instant unlock schedule

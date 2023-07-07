a063614885da1422ac39f06bbcab92abab4595f2
sampocs <sam.pochyly@gmail.com>
2023-07-07 13:09:35 -0400

    fixed random gen state unit tests

--- x/staking/simulation/operations_test.go
+++ x/staking/simulation/operations_test.go
@@ -48,6 +48,7 @@ func TestWeightedOperations(t *testing.T) {
 		{simappparams.DefaultWeightMsgUndelegate, types.ModuleName, types.TypeMsgUndelegate},
 		{simappparams.DefaultWeightMsgBeginRedelegate, types.ModuleName, types.TypeMsgBeginRedelegate},
 		{simappparams.DefaultWeightMsgCancelUnbondingDelegation, types.ModuleName, types.TypeMsgCancelUnbondingDelegation},
+		{simappparams.DefaultWeightMsgValidatorBond, types.ModuleName, types.TypeMsgValidatorBond},
 		{simappparams.DefaultWeightMsgTokenizeShares, types.ModuleName, types.TypeMsgTokenizeShares},
 		{simappparams.DefaultWeightMsgRedeemTokensforShares, types.ModuleName, types.TypeMsgRedeemTokensForShares},
 		{simappparams.DefaultWeightMsgTransferTokenizeShareRecord, types.ModuleName, types.TypeMsgTransferTokenizeShareRecord},

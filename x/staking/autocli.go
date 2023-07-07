cbcf71784e3bfa8aa8e57fee317de344a5560159
Julien Robert <julien@rbrt.fr>
2023-07-07 17:00:22 +0200

    simplify

--- x/staking/autocli.go
+++ x/staking/autocli.go
@@ -19,14 +19,12 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					RpcMethod: "Validators",
 					Short:     "Query for all validators",
 					Long:      "Query details about all validators on a network.",
-					Example:   fmt.Sprintf("$ %s query staking validators", version.AppName),
 				},
 				{
 					RpcMethod: "Validator",
 					Use:       "validator [validator-addr]",
 					Short:     "Query a validator",
 					Long:      "Query details about an individual validator.",
-					Example:   fmt.Sprintf("$ %s query staking validator [validator-adr]", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "validator_addr"},
 					},
@@ -36,7 +34,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "delegations-to [validator-addr]",
 					Short:     "Query all delegations made to one validator",
 					Long:      "Query delegations on an individual validator.",
-					Example:   fmt.Sprintf("$ %s query staking delegations-to [validator-adr]", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{
 							ProtoField: "validator_addr",
@@ -57,9 +54,7 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					RpcMethod: "Delegation",
 					Use:       "delegation [delegator-addr] [validator-addr]",
 					Short:     "Query a delegation based on address and validator address",
-					Example: fmt.Sprintf(`%s query staking delegation [delegator-address] [validator-address]`,
-						version.AppName),
-					Long: "Query delegations for an individual delegator on an individual validator",
+					Long:      "Query delegations for an individual delegator on an individual validator",
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "delegator_addr"},
 						{ProtoField: "validator_addr"},
@@ -70,7 +65,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "unbonding-delegation [delegator-addr] [validator-addr]",
 					Short:     "Query an unbonding-delegation record based on delegator and validator address",
 					Long:      "Query unbonding delegations for an individual delegator on an individual validator.",
-					Example:   fmt.Sprintf("$ %s query staking unbonding-delegation [delegator-adr] [validator-adr]", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "delegator_addr"},
 						{ProtoField: "validator_addr"},
@@ -81,9 +75,25 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "delegations [delegator-addr]",
 					Short:     "Query all delegations made by one delegator",
 					Long:      "Query delegations for an individual delegator on all validators.",
-					Example:   fmt.Sprintf("$ %s query staking delegations [validator-addr]", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "delegator_addr"},
+					},
+				},
+				{
+					RpcMethod: "DelegatorValidators",
+					Use:       "delegator-validators [delegator-addr]",
+					Short:     "Query all validators info for given delegator address",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "delegator_addr"},
+					},
+				},
+				{
+					RpcMethod: "DelegatorValidator",
+					Use:       "delegator-validator [delegator-addr] [validator-addr]",
+					Short:     "Query validator info for given delegator validator pair",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "delegator_addr"},
+						{ProtoField: "validator_addr"},
 					},
 				},
 				{
@@ -91,7 +101,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "unbonding-delegations [delegator-addr]",
 					Short:     "Query all unbonding-delegations records for one delegator",
 					Long:      "Query unbonding delegations for an individual delegator.",
-					Example:   fmt.Sprintf("$ %s query staking unbonding-delegations [delegator-addr]", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "delegator_addr"},
 					},
@@ -101,7 +110,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "redelegation [delegator-addr] [src-validator-addr] [dst-validator-addr]",
 					Short:     "Query a redelegation record based on delegator and a source and destination validator address",
 					Long:      "Query a redelegation record for an individual delegator between a source and destination validator.",
-					Example:   fmt.Sprintf("$ %s query staking redelegation [delegator-addr] [src-validator-addr] [dst-validator-addr]", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "delegator_addr"},
 						{ProtoField: "src_validator_addr"},
@@ -123,14 +131,12 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "pool",
 					Short:     "Query the current staking pool values",
 					Long:      "Query values for amounts stored in the staking pool.",
-					Example:   fmt.Sprintf("$ %s query staking pool", version.AppName),
 				},
 				{
 					RpcMethod: "Params",
 					Use:       "params",
 					Short:     "Query the current staking parameters information",
 					Long:      "Query values set as staking parameters.",
-					Example:   fmt.Sprintf("$ %s query staking params", version.AppName),
 				},
 			},
 		},

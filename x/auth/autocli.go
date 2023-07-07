ab253b100dc896562b6bf4ecd372ce18b607ee67
Julien Robert <julien@rbrt.fr>
2023-07-07 16:46:12 +0200

    updates

--- x/auth/autocli.go
+++ x/auth/autocli.go
@@ -3,8 +3,11 @@ package auth
 import (
 	"fmt"
 
+	// the following packages are not registered by default, so we import them here to ensure they are registered
 	authv1beta1 "cosmossdk.io/api/cosmos/auth/v1beta1"
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+	_ "cosmossdk.io/api/cosmos/crypto/secp256k1"
+	_ "cosmossdk.io/api/cosmos/crypto/secp256r1"
 
 	"github.com/cosmos/cosmos-sdk/version"
 )

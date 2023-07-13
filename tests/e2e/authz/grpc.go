1145da9616f875d5005ee354cb04f94b476a3e2e
Julien Robert <julien@rbrt.fr>
2023-07-13 16:45:56 +0200

    feat(authz): autocli query support (#16986)

--- tests/e2e/authz/grpc.go
+++ tests/e2e/authz/grpc.go
@@ -200,7 +200,7 @@ func (s *E2ETestSuite) TestQueryGranterGrantsGRPC() {
 			fmt.Sprintf("%s/cosmos/authz/v1beta1/grants/granter/%s", val.APIAddress, val.Address.String()),
 			false,
 			"",
-			7,
+			6,
 		},
 	}
 	for _, tc := range testCases {

a7ae54982705134e6ab8ee7c1a866a4b03d95884
Julien Robert <julien@rbrt.fr>
2023-07-14 13:10:38 +0200

    updates

--- x/slashing/autocli.go
+++ x/slashing/autocli.go
@@ -20,9 +20,9 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 				},
 				{
 					RpcMethod: "SigningInfo",
-					Use:       "signing-info [validator-conspub]",
+					Use:       "signing-info [validator-conspub/address]",
 					Short:     "Query a validator's signing information",
-					Long:      "Query a validator's signing information, the validator consensus public key can be got with '<appd> comet show-validator'",
+					Long:      "Query a validator's signing information, with a pubkey ('<appd> comet show-validator') or a validator consensus address",
 					Example:   fmt.Sprintf(`%s query slashing signing-info '{"@type":"/cosmos.crypto.ed25519.PubKey","key":"OauFcTKbN5Lx3fJL689cikXBqe+hcp6Y+x0rYUdR9Jk="}'`, version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "cons_address"},

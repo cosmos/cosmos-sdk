daef98ed94bc94ae661e66981473265f2fbd8789
Julien Robert <julien@rbrt.fr>
2023-07-20 17:52:02 +0200

    updates

--- scripts/go-lint-all.bash
+++ scripts/go-lint-all.bash
@@ -9,12 +9,16 @@ lint_module() {
   local root="$1"
   shift
   cd "$(dirname "$root")" &&
-    echo "linting $(grep "^module" go.mod) [$(date -Iseconds -u)]" &&
+    echo "linting $root [$(date -Iseconds -u)]" &&
     golangci-lint run ./... -c "${REPO_ROOT}/.golangci.yml" "$@"
 }
 export -f lint_module
 
-lint_files() {
+# if LINT_DIFF env is set, only lint the files in the current commit otherwise lint all files
+if [[ -z "${LINT_DIFF:-}" ]]; then
+  find "${REPO_ROOT}" -type f -name go.mod -print0 |
+    xargs -0 -I{} bash -c 'lint_module "$@"' _ {} "$@"
+else
   if [[ -z $GIT_DIFF ]]; then
     GIT_DIFF="$(git diff --name-only --diff-filter=d | grep \.go$ | grep -v \.pb\.go$)"
   fi
@@ -24,18 +28,5 @@ lint_files() {
     exit 0
   fi
 
-  for f in $GIT_DIFF; do
-    local dir_name="$(dirname "$REPO_ROOT/$f")"
-    echo "linting ${dir_name} [$(date -Iseconds -u)]"
-    golangci-lint run "${dir_name}" -c "${REPO_ROOT}/.golangci.yml" "$@"
-  done
-}
-export -f lint_files
-
-# if LINT_DIFF env is set, only lint the files in the current commit otherwise lint all files
-if [[ -z "${LINT_DIFF:-}" ]]; then
-  find "${REPO_ROOT}" -type f -name go.mod -print0 |
-    xargs -0 -I{} bash -c 'lint_module "$@"' _ {} "$@"
-else
-  lint_files "$@"
+  dirname $GIT_DIFF | uniq | xargs -I{} bash -c 'cd {} && golangci-lint run ./... -c "${REPO_ROOT}/.golangci.yml" "$@"'
 fi

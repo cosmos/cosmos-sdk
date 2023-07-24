b12938f6c0582bf66d478e80ab39b4ee9cffc3bd
Julien Robert <julien@rbrt.fr>
2023-07-24 14:51:38 +0200

    docs, lint and test

--- x/group/keeper/msg_server.go
+++ x/group/keeper/msg_server.go
@@ -1064,8 +1064,8 @@ func (k Keeper) assertMetadataLength(metadata, description string) error {
 
 // assertSummaryLength returns an error if given summary length
 // is greater than a pre-defined 40*MaxMetadataLen.
-func (keeper Keeper) assertSummaryLength(summary string) error {
-	if summary != "" && uint64(len(summary)) > 40*keeper.config.MaxMetadataLen {
+func (k Keeper) assertSummaryLength(summary string) error {
+	if summary != "" && uint64(len(summary)) > 40*k.config.MaxMetadataLen {
 		return errorsmod.Wrapf(errors.ErrMaxLimit, "proposal summary is too long")
 	}
 	return nil

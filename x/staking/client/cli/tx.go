425e2db24e9de26da75ce5f047253ba96b662c7f
marbar3778 <marbar3778@yahoo.com>
2023-07-24 18:32:07 +0200

    change cmd line examples

--- x/staking/client/cli/tx.go
+++ x/staking/client/cli/tx.go
@@ -189,7 +189,7 @@ func NewDelegateCmd(valAddrCodec, ac address.Codec) *cobra.Command {
 			fmt.Sprintf(`Delegate an amount of liquid coins to a validator from your wallet.
 
 Example:
-$ %s tx staking delegate cosmoss1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 1000stake --from mykey
+$ %s tx staking delegate cosmosvalopers1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 1000stake --from mykey
 `,
 				version.AppName,
 			),
@@ -235,7 +235,7 @@ func NewRedelegateCmd(valAddrCodec, ac address.Codec) *cobra.Command {
 			fmt.Sprintf(`Redelegate an amount of illiquid staking tokens from one validator to another.
 
 Example:
-$ %s tx staking redelegate cosmoss1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj cosmoss1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 100stake --from mykey
+$ %s tx staking redelegate cosmosvalopers1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj cosmoss1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 100stake --from mykey
 `,
 				version.AppName,
 			),

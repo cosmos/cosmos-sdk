8d4731073685998b623897ea41cd91291e5fc177
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-07-25 11:33:08 -0400

    updates

---
+++ store/database.go
@@ -0,0 +1,28 @@
+package store
+
+// Reader wraps the Has and Get method of a backing data store.
+type Reader interface {
+	// Has retrieves if a key is present in the key-value data store.
+	//
+	// Note: <key> is safe to modify and read after calling Has.
+	Has(key []byte) (bool, error)
+
+	// Get retrieves the given key if it's present in the key-value data store.
+	//
+	// Note: <key> is safe to modify and read after calling Get.
+	// The returned byte slice is safe to read, but cannot be modified.
+	Get(key []byte) ([]byte, error)
+}
+
+// Writer wraps the Set method of a backing data store.
+type Writer interface {
+	// Set inserts the given value into the key-value data store.
+	//
+	// Note: <key, value> are safe to modify and read after calling Set.
+	Set(key, value []byte) error
+
+	// Delete removes the key from the backing key-value data store.
+	//
+	// Note: <key> is safe to modify and read after calling Delete.
+	Delete(key []byte) error
+}

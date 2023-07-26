73c9bc70ba354c31208a1949159e9809f6dbffff
Facundo Medica <facundomedica@gmail.com>
2023-07-26 12:49:17 +0200

    fix

--- tests/integration/distribution/keeper/grpc_query_test.go
+++ tests/integration/distribution/keeper/grpc_query_test.go
@@ -238,7 +238,7 @@ func TestGRPCValidatorSlashes(t *testing.T) {
 	for i, slash := range slashes {
 		err := f.distrKeeper.ValidatorSlashEvents.Set(
 			f.sdkCtx,
-			collections.Join3(f.valAddr, uint64(i+2), 0),
+			collections.Join3(f.valAddr, uint64(i+2), uint64(0)),
 			slash,
 		)
 		assert.NilError(t, err)

b93c4af22c7652aea8cd7c41d1a863a7dbab223a
Facundo Medica <facundomedica@gmail.com>
2023-07-26 14:50:10 +0200

    add test

---
+++ testutil/collections_test.go
@@ -0,0 +1,55 @@
+package testutil_test
+
+import (
+	"testing"
+
+	"cosmossdk.io/store/types"
+	storetypes "cosmossdk.io/store/types"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/testutil"
+)
+
+func TestDiffCollectionsMigration(t *testing.T) {
+	key := storetypes.NewKVStoreKey("test")
+	ctx := testutil.DefaultContext(key, types.NewTransientStoreKey("transient"))
+
+	// First try with some invalid hash
+	err := testutil.DiffCollectionsMigration(
+		ctx,
+		key,
+		5,
+		func(i int64) {
+			ctx.KVStore(key).Set([]byte{byte(i)}, []byte{byte(i)})
+		},
+		"abcdef0123456789",
+	)
+	require.Error(t, err)
+
+	// Now reset and try with the correct hash
+	ctx = testutil.DefaultContext(key, types.NewTransientStoreKey("transient"))
+	err = testutil.DiffCollectionsMigration(
+		ctx,
+		key,
+		5,
+		func(i int64) {
+			ctx.KVStore(key).Set([]byte{byte(i)}, []byte{byte(i)})
+		},
+		"79541ed9da9c16cb7a1d43d5a3d5f6ee31a873c85a6cb4334fb99e021ee0e556",
+	)
+	require.NoError(t, err)
+
+	// Change the data a little and it will result in an error
+	ctx = testutil.DefaultContext(key, types.NewTransientStoreKey("transient"))
+	err = testutil.DiffCollectionsMigration(
+		ctx,
+		key,
+		5,
+		func(i int64) {
+			ctx.KVStore(key).Set([]byte{byte(i)}, []byte{byte(i + 1)})
+		},
+		"79541ed9da9c16cb7a1d43d5a3d5f6ee31a873c85a6cb4334fb99e021ee0e556",
+	)
+	require.Error(t, err)
+}

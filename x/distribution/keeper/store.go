42397d02d34c88b7bd4847354ec3e041b835e419
Facundo Medica <facundomedica@gmail.com>
2023-07-26 12:24:51 +0200

    more suggestions

--- x/distribution/keeper/store.go
+++ x/distribution/keeper/store.go
@@ -23,36 +23,24 @@ func (k Keeper) GetDelegatorWithdrawAddr(ctx context.Context, delAddr sdk.AccAdd
 // iterate over slash events between heights, inclusive
 func (k Keeper) IterateValidatorSlashEventsBetween(ctx context.Context, val sdk.ValAddress, startingHeight, endingHeight uint64,
 	handler func(height uint64, event types.ValidatorSlashEvent) (stop bool),
-) {
+) error {
 	rng := new(collections.Range[collections.Triple[sdk.ValAddress, uint64, uint64]]).
 		StartInclusive(collections.Join3(val, startingHeight, uint64(0))).
 		EndExclusive(collections.Join3(val, endingHeight+1, uint64(math.MaxUint64)))
 
-	iter, err := k.ValidatorSlashEvents.Iterate(
-		ctx,
-		rng,
-	)
-
-	if errors.Is(err, collections.ErrInvalidIterator) {
-		return
-	}
-
-	defer iter.Close()
-	for ; iter.Valid(); iter.Next() {
-		event, err := iter.Value()
-		if err != nil {
-			panic(err)
-		}
-		k, err := iter.Key()
-		if err != nil {
-			panic(err)
-		}
-
+	err := k.ValidatorSlashEvents.Walk(ctx, rng, func(k collections.Triple[sdk.ValAddress, uint64, uint64], ev types.ValidatorSlashEvent) (stop bool, err error) {
 		height := k.K2()
-		if handler(height, event) {
-			break
+		if handler(height, ev) {
+			return true, nil
 		}
+		return false, nil
+	})
+
+	if err != nil && !errors.Is(err, collections.ErrInvalidIterator) {
+		return err
 	}
+
+	return nil
 }
 
 // delete slash events for a particular validator

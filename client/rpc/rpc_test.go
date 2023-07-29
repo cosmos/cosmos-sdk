dfdcf0fa8f14dd6d1dc4538ac7fd7cbb4fa74674
zakir <80246097+zakir-code@users.noreply.github.com>
2023-07-29 18:39:43 +0800

    revert: status cmd cli test

--- client/rpc/rpc_test.go
+++ client/rpc/rpc_test.go
@@ -11,7 +11,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/rpc"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
@@ -49,7 +48,7 @@ func (s *IntegrationTestSuite) TestStatusCommand() {
 	val0 := s.network.Validators[0]
 	cmd := rpc.StatusCommand()
 
-	out, err := clitestutil.ExecTestCLICmd(val0.ClientCtx, cmd, []string{fmt.Sprintf("--%s=json", flags.FlagOutput)})
+	out, err := clitestutil.ExecTestCLICmd(val0.ClientCtx, cmd, []string{})
 	s.Require().NoError(err)
 
 	// Make sure the output has the validator moniker.

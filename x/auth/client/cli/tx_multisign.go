63b1890ad2652091c0f074767a3b18e4db3ef7a6
zakir-code <80246097+zakir-code@users.noreply.github.com>
2023-07-30 07:53:27 +0800

    refactor: hide `--output` flags that don't work (#17188)
    Co-authored-by: Julien Robert <julien@rbrt.fr>
    (cherry picked from commit 0b5e7ed0af567a5be49f0dd0d7a0f2062042c9fd)
    
    # Conflicts:
    #	x/auth/client/cli/tx_multisign.go

--- x/auth/client/cli/tx_multisign.go
+++ x/auth/client/cli/tx_multisign.go
@@ -65,6 +65,7 @@ The SIGN_MODE_DIRECT sign mode is not supported.'
 	cmd.Flags().String(flags.FlagOutputDocument, "", "The document is written to the given file instead of STDOUT")
 	cmd.Flags().Bool(flagAmino, false, "Generate Amino-encoded JSON suitable for submitting to the txs REST endpoint")
 	flags.AddTxFlagsToCmd(cmd)
+	_ = cmd.Flags().MarkHidden(flags.FlagOutput)
 
 	return cmd
 }
@@ -188,6 +189,7 @@ func makeMultiSignCmd() func(cmd *cobra.Command, args []string) (err error) {
 			}
 		}
 
+<<<<<<< HEAD
 		outputDoc, _ := cmd.Flags().GetString(flags.FlagOutputDocument)
 		if outputDoc == "" {
 			cmd.Printf("%s\n", json)
@@ -195,20 +197,23 @@ func makeMultiSignCmd() func(cmd *cobra.Command, args []string) (err error) {
 		}
 
 		fp, err := os.OpenFile(outputDoc, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o644)
+=======
+		closeFunc, err := setOutputFile(cmd)
+>>>>>>> 0b5e7ed0a (refactor: hide `--output` flags that don't work (#17188))
 		if err != nil {
 			return err
 		}
 
-		defer func() {
-			err2 := fp.Close()
-			if err == nil {
-				err = err2
-			}
-		}()
+		defer closeFunc()
 
+<<<<<<< HEAD
 		err = clientCtx.PrintBytes(json)
 
 		return
+=======
+		cmd.Printf("%s\n", json)
+		return nil
+>>>>>>> 0b5e7ed0a (refactor: hide `--output` flags that don't work (#17188))
 	}
 }
 
@@ -243,6 +248,7 @@ The SIGN_MODE_DIRECT sign mode is not supported.'
 	)
 	cmd.Flags().String(flags.FlagOutputDocument, "", "The document is written to the given file instead of STDOUT")
 	flags.AddTxFlagsToCmd(cmd)
+	_ = cmd.Flags().MarkHidden(flags.FlagOutput) // signing makes sense to output only json
 
 	return cmd
 }

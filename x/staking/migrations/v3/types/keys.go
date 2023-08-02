018ecc805cc0d231b2e1d2f0fb27854ce5bafd57
sampocs <sam.pochyly@gmail.com>
2023-08-02 15:16:31 -0500

    added legacy types

---
+++ x/staking/migrations/v3/types/keys.go
@@ -0,0 +1,36 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+)
+
+const (
+	// ModuleName is the name of the staking module
+	ModuleName = "staking"
+
+	// StoreKey is the string store representation
+	StoreKey = ModuleName
+)
+
+var (
+	ValidatorsKey = []byte{0x21} // prefix for each key to a validator
+	DelegationKey = []byte{0x31} // key for a delegation
+)
+
+// GetValidatorKey creates the key for the validator with address
+// VALUE: staking/Validator
+func GetValidatorKey(operatorAddr sdk.ValAddress) []byte {
+	return append(ValidatorsKey, address.MustLengthPrefix(operatorAddr)...)
+}
+
+// GetDelegationKey creates the key for delegator bond with validator
+// VALUE: staking/Delegation
+func GetDelegationKey(delAddr sdk.AccAddress, valAddr sdk.ValAddress) []byte {
+	return append(GetDelegationsKey(delAddr), address.MustLengthPrefix(valAddr)...)
+}
+
+// GetDelegationsKey creates the prefix for a delegator for all validators
+func GetDelegationsKey(delAddr sdk.AccAddress) []byte {
+	return append(DelegationKey, address.MustLengthPrefix(delAddr)...)
+}

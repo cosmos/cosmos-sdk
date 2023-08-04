b75a00e94da5b65e2caa69981dc5e63bcf17f969
Riley Edmunds <riley@stridelabs.co>
2023-08-04 12:31:38 -0400

    chore: add migrateUBDEntries to migration

--- x/staking/migrations/v3/store.go
+++ x/staking/migrations/v3/store.go
@@ -1,8 +1,13 @@
 package v3
 
 import (
+	"sort"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/cosmos/cosmos-sdk/x/staking/exported"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
@@ -53,6 +58,64 @@ func MigrateDelegations(ctx sdk.Context, k keeper) {
 		delegation.ValidatorBond = false
 		k.SetDelegation(ctx, delegation)
 	}
+}
+
+// migrateUBDEntries will remove the ubdEntries with same creation_height
+// and create a new ubdEntry with updated balance and initial_balance
+func migrateUBDEntries(ctx sdk.Context, store storetypes.KVStore, cdc codec.BinaryCodec, legacySubspace exported.Subspace) error {
+	iterator := sdk.KVStorePrefixIterator(store, types.UnbondingDelegationKey)
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		ubd := types.MustUnmarshalUBD(cdc, iterator.Value())
+
+		entriesAtSameCreationHeight := make(map[int64][]types.UnbondingDelegationEntry)
+		for _, ubdEntry := range ubd.Entries {
+			entriesAtSameCreationHeight[ubdEntry.CreationHeight] = append(entriesAtSameCreationHeight[ubdEntry.CreationHeight], ubdEntry)
+		}
+
+		creationHeights := make([]int64, 0, len(entriesAtSameCreationHeight))
+		for k := range entriesAtSameCreationHeight {
+			creationHeights = append(creationHeights, k)
+		}
+
+		sort.Slice(creationHeights, func(i, j int) bool { return creationHeights[i] < creationHeights[j] })
+
+		ubd.Entries = make([]types.UnbondingDelegationEntry, 0, len(creationHeights))
+
+		for _, h := range creationHeights {
+			ubdEntry := types.UnbondingDelegationEntry{
+				Balance:        sdk.ZeroInt(),
+				InitialBalance: sdk.ZeroInt(),
+			}
+			for _, entry := range entriesAtSameCreationHeight[h] {
+				ubdEntry.Balance = ubdEntry.Balance.Add(entry.Balance)
+				ubdEntry.InitialBalance = ubdEntry.InitialBalance.Add(entry.InitialBalance)
+				ubdEntry.CreationHeight = entry.CreationHeight
+				ubdEntry.CompletionTime = entry.CompletionTime
+			}
+			ubd.Entries = append(ubd.Entries, ubdEntry)
+		}
+
+		// set the new ubd to the store
+		setUBDToStore(ctx, store, cdc, ubd)
+	}
+	return nil
+}
+
+func setUBDToStore(ctx sdk.Context, store storetypes.KVStore, cdc codec.BinaryCodec, ubd types.UnbondingDelegation) {
+	delegatorAddress := sdk.MustAccAddressFromBech32(ubd.DelegatorAddress)
+
+	bz := types.MustMarshalUBD(cdc, ubd)
+
+	addr, err := sdk.ValAddressFromBech32(ubd.ValidatorAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	key := types.GetUBDKey(delegatorAddress, addr)
+
+	store.Set(key, bz)
 }
 
 // Peforms the in-place store migration for adding LSM support to v0.45.16-ics, including:

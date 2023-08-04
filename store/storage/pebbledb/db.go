b86b52e8454c2840610a43982f0cb40302da90a8
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-08-04 11:25:45 -0700

    updates

--- store/storage/pebbledb/db.go
+++ store/storage/pebbledb/db.go
@@ -1,9 +1,24 @@
 package pebbledb
 
 import (
+	"encoding/binary"
 	"fmt"
 
+	"cosmossdk.io/store/v2"
 	"github.com/cockroachdb/pebble"
+)
+
+const (
+	VersionSize = 8
+
+	StorePrefixTpl   = "s/k:%s/"
+	latestVersionKey = "s/latest"
+)
+
+var (
+	_ store.VersionedDatabase = (*Database)(nil)
+
+	defaultWriteOpts = pebble.Sync
 )
 
 type Database struct {
@@ -22,4 +37,33 @@ func New(dataDir string) (*Database, error) {
 	return &Database{
 		storage: db,
 	}, nil
+}
+
+func NewWithDB(storage *pebble.DB) *Database {
+	return &Database{
+		storage: storage,
+	}
+}
+
+func (db *Database) Close() error {
+	return db.storage.Close()
+}
+
+func (db *Database) SetLatestVersion(version uint64) error {
+	var ts [VersionSize]byte
+	binary.LittleEndian.PutUint64(ts[:], version)
+	return db.storage.Set([]byte(latestVersionKey), ts[:], defaultWriteOpts)
+}
+
+func (db *Database) GetLatestVersion() (uint64, error) {
+	bz, closer, err := db.storage.Get([]byte(latestVersionKey))
+	if err != nil {
+		return 0, err
+	}
+
+	if len(bz) == 0 {
+		return 0, nil
+	}
+
+	return binary.LittleEndian.Uint64(bz), closer.Close()
 }

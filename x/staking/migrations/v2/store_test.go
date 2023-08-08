b255839e764b5f67a07684a50f08ae8f6e87f382
likhita-809 <likhita@vitwit.com>
2023-08-08 13:21:11 +0530

    nit

--- x/staking/migrations/v2/store_test.go
+++ x/staking/migrations/v2/store_test.go
@@ -9,7 +9,7 @@ import (
 
 	storetypes "cosmossdk.io/store/types"
 
-	sdktestuil "github.com/cosmos/cosmos-sdk/testutil"
+	sdktestutil "github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v1 "github.com/cosmos/cosmos-sdk/x/staking/migrations/v1"
@@ -21,7 +21,7 @@ import (
 func TestStoreMigration(t *testing.T) {
 	stakingKey := storetypes.NewKVStoreKey("staking")
 	tStakingKey := storetypes.NewTransientStoreKey("transient_test")
-	ctx := sdktestuil.DefaultContext(stakingKey, tStakingKey)
+	ctx := sdktestutil.DefaultContext(stakingKey, tStakingKey)
 	store := ctx.KVStore(stakingKey)
 
 	_, pk1, addr1 := testdata.KeyTestPubAddr()

900413824164373043fffd8c4aea76af4b8fb34e
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-08-08 12:13:48 -0700

    updates

--- store/storage/pebbledb/comparator.go
+++ store/storage/pebbledb/comparator.go
@@ -1,29 +1,151 @@
 package pebbledb
 
-import "github.com/cockroachdb/pebble"
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/cockroachdb/pebble"
+)
 
 // MVCCComparer returns a PebbleDB Comparer with encoding and decoding routines
 // for MVCC control, used to compare and store versioned data.
-var MVCCComparer = &pebble.Comparer{}
+var MVCCComparer = &pebble.Comparer{
+	Name:    "ss_pebbledb_comparator",
+	Compare: MVCCKeyCompare,
+	// Equal: func(a, b []byte) bool {
+	// 	return mvccCompare(a, b) == 0
+	// },
+	// AbbreviatedKey: func(k []byte) uint64 {
+	// 	key, _, ok := mvccSplitKey(k)
+	// 	if !ok {
+	// 		return 0
+	// 	}
+	// 	return pebble.DefaultComparer.AbbreviatedKey(key)
+	// },
 
-// mvccEncode encodes a key for MVCC control. Note, adapted from PebbleDB.
+	// Separator: func(dst, a, b []byte) []byte {
+	// 	aKey, _, ok := mvccSplitKey(a)
+	// 	if !ok {
+	// 		return append(dst, a...)
+	// 	}
+	// 	bKey, _, ok := mvccSplitKey(b)
+	// 	if !ok {
+	// 		return append(dst, a...)
+	// 	}
+	// 	// If the keys are the same just return a.
+	// 	if bytes.Equal(aKey, bKey) {
+	// 		return append(dst, a...)
+	// 	}
+	// 	n := len(dst)
+	// 	// MVCC key comparison uses bytes.Compare on the roachpb.Key, which is the same semantics as
+	// 	// pebble.DefaultComparer, so reuse the latter's Separator implementation.
+	// 	dst = pebble.DefaultComparer.Separator(dst, aKey, bKey)
+	// 	// Did it pick a separator different than aKey -- if it did not we can't do better than a.
+	// 	buf := dst[n:]
+	// 	if bytes.Equal(aKey, buf) {
+	// 		return append(dst[:n], a...)
+	// 	}
+	// 	// The separator is > aKey, so we only need to add the timestamp sentinel.
+	// 	return append(dst, 0)
+	// },
+
+	// Successor: func(dst, a []byte) []byte {
+	// 	aKey, _, ok := mvccSplitKey(a)
+	// 	if !ok {
+	// 		return append(dst, a...)
+	// 	}
+	// 	n := len(dst)
+	// 	// MVCC key comparison uses bytes.Compare on the roachpb.Key, which is the same semantics as
+	// 	// pebble.DefaultComparer, so reuse the latter's Successor implementation.
+	// 	dst = pebble.DefaultComparer.Successor(dst, aKey)
+	// 	// Did it pick a successor different than aKey -- if it did not we can't do better than a.
+	// 	buf := dst[n:]
+	// 	if bytes.Equal(aKey, buf) {
+	// 		return append(dst[:n], a...)
+	// 	}
+	// 	// The successor is > aKey, so we only need to add the timestamp sentinel.
+	// 	return append(dst, 0)
+	// },
+
+	// Split: func(k []byte) int {
+	// 	key, _, ok := mvccSplitKey(k)
+	// 	if !ok {
+	// 		return len(k)
+	// 	}
+	// 	// This matches the behavior of libroach/KeyPrefix. RocksDB requires that
+	// 	// keys generated via a SliceTransform be comparable with normal encoded
+	// 	// MVCC keys. Encoded MVCC keys have a suffix indicating the number of
+	// 	// bytes of timestamp data. MVCC keys without a timestamp have a suffix of
+	// 	// 0. We're careful in EncodeKey to make sure that the user-key always has
+	// 	// a trailing 0. If there is no timestamp this falls out naturally. If
+	// 	// there is a timestamp we prepend a 0 to the encoded timestamp data.
+	// 	return len(key) + 1
+	// },
+}
+
+// MVCCKeyCompare compares PebbleDB versioned store keys, which are MVCC timestamps.
+// The result will be 0 if a == b, -1 if a < b, and +1 if a > b.
+func MVCCKeyCompare(a, b []byte) int {
+	keyA, tsA, okA := SplitMVCCKey(a)
+	keyB, tsB, okB := SplitMVCCKey(b)
+
+	if !okA || !okB {
+		return bytes.Compare(a, b)
+	}
+
+	// compare the "user key" part of the key
+	if res := bytes.Compare(keyA, keyB); res != 0 {
+		return res
+	}
+
+	if len(tsA) == 0 {
+		if len(tsB) == 0 {
+			return 0
+		}
+
+		return -1
+	} else if len(tsB) == 0 {
+		return 1
+	}
+
+	return bytes.Compare(tsA, tsB)
+}
+
+func SplitMVCCKey(mvccKey []byte) (key []byte, ts []byte, ok bool) {
+	if len(mvccKey) == 0 {
+		return nil, nil, false
+	}
+
+	tsLen := int(mvccKey[len(mvccKey)-1])
+	keyPartEnd := len(mvccKey) - 1 - tsLen
+	if keyPartEnd < 0 {
+		return nil, nil, false
+	}
+
+	key = mvccKey[:keyPartEnd]
+	if tsLen > 0 {
+		ts = mvccKey[keyPartEnd+1 : len(mvccKey)-1]
+	}
+
+	return key, ts, true
+}
+
+// MVCCEncode encodes a key for MVCC control. Note, adapted from PebbleDB.
 //
 // <key>\x00[<version>]<#version-bytes>
-func mvccEncode(key []byte, version uint64) (dst []byte) {
+func MVCCEncode(key []byte, version uint64) (dst []byte) {
 	dst = append(dst, key...)
 	dst = append(dst, 0)
 
 	if version != 0 {
 		extra := byte(1 + 8)
-		dst = encodeUint64Ascending(dst, version)
+
+		var versionBz [VersionSize]byte
+		binary.LittleEndian.PutUint64(versionBz[:], version)
+
+		dst = append(dst, versionBz[:]...)
 		dst = append(dst, extra)
 	}
 
 	return dst
-}
-
-func encodeUint64Ascending(b []byte, v uint64) []byte {
-	return append(b,
-		byte(v>>56), byte(v>>48), byte(v>>40), byte(v>>32),
-		byte(v>>24), byte(v>>16), byte(v>>8), byte(v))
 }
---
+++ store/storage/pebbledb/comparator_test.go
@@ -0,0 +1,56 @@
+package pebbledb
+
+import (
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMVCCKey(t *testing.T) {
+	for i := uint64(1); i < 1001; i++ {
+		keyA := MVCCEncode([]byte("key001"), i)
+
+		splitKeyA, splitVersionA, ok := SplitMVCCKey(keyA)
+		require.True(t, ok)
+		require.Equal(t, i, binary.LittleEndian.Uint64(splitVersionA))
+		require.Equal(t, []byte("key001"), splitKeyA)
+	}
+}
+
+func TestMVCCKeyCompare(t *testing.T) {
+	testCases := []struct {
+		keyA     []byte
+		keyB     []byte
+		expected int
+	}{
+		{
+			// same key, same version
+			keyA:     MVCCEncode([]byte("key001"), 1),
+			keyB:     MVCCEncode([]byte("key001"), 1),
+			expected: 0,
+		},
+		{
+			// same key, different version
+			keyA:     MVCCEncode([]byte("key001"), 1),
+			keyB:     MVCCEncode([]byte("key001"), 2),
+			expected: -1,
+		},
+		{
+			// same key, different version (inverse)
+			keyA:     MVCCEncode([]byte("key001"), 2),
+			keyB:     MVCCEncode([]byte("key001"), 1),
+			expected: 1,
+		},
+		{
+			// different key, same version
+			keyA:     MVCCEncode([]byte("key001"), 1),
+			keyB:     MVCCEncode([]byte("key009"), 1),
+			expected: -1,
+		},
+	}
+
+	for _, tc := range testCases {
+		require.Equalf(t, tc.expected, MVCCKeyCompare(tc.keyA, tc.keyB), "keyA: %s, keyB: %s", tc.keyA, tc.keyB)
+	}
+}

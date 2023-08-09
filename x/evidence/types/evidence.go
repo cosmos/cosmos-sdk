1cf15b352836f6ac1500bf151e24ea36ef6d9eb8
marbar3778 <marbar3778@yahoo.com>
2023-08-09 18:39:25 +0200

    remove todo

--- x/evidence/types/evidence.go
+++ x/evidence/types/evidence.go
@@ -76,9 +76,8 @@ func (e Equivocation) GetTotalPower() int64 { return 0 }
 
 // FromABCIEvidence converts a CometBFT concrete Evidence type to
 // SDK Evidence using Equivocation as the concrete type.
-func FromABCIEvidence(e comet.Evidence) *Equivocation {
-	bech32PrefixConsAddr := sdk.GetConfig().GetBech32ConsensusAddrPrefix() // TODO: remove this
-	consAddr, err := sdk.Bech32ifyAddressBytes(bech32PrefixConsAddr, e.Validator().Address())
+func FromABCIEvidence(e comet.Evidence, conAc address.Codec) *Equivocation {
+	consAddr, err := conAc.BytesToString(e.Validator().Address())
 	if err != nil {
 		panic(err)
 	}

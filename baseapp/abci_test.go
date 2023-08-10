1991f40f3c4a2d536c84a9c4ce0badc210b1c67c
Callum Waters <cmwaters19@gmail.com>
2023-08-10 18:49:16 +0200

    lint

--- baseapp/abci_test.go
+++ baseapp/abci_test.go
@@ -47,7 +47,8 @@ import (
 func TestABCI_Info(t *testing.T) {
 	suite := NewBaseAppSuite(t)
 	ctx := suite.baseApp.NewContext(true)
-	suite.baseApp.StoreConsensusParams(ctx, cmttypes.DefaultConsensusParams().ToProto())
+	err := suite.baseApp.StoreConsensusParams(ctx, cmttypes.DefaultConsensusParams().ToProto())
+	require.NoError(t, err)
 
 	reqInfo := abci.RequestInfo{}
 	res, err := suite.baseApp.Info(&reqInfo)
@@ -61,8 +62,10 @@ func TestABCI_Info(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, appVersion, res.AppVersion)
 
-	suite.baseApp.FinalizeBlock(&abci.RequestFinalizeBlock{Height: 1})
-	suite.baseApp.Commit()
+	_, err = suite.baseApp.FinalizeBlock(&abci.RequestFinalizeBlock{Height: 1})
+	require.NoError(t, err)
+	_, err = suite.baseApp.Commit()
+	require.NoError(t, err)
 	require.NoError(t, suite.baseApp.SetAppVersion(ctx, 1))
 	res, err = suite.baseApp.Info(&reqInfo)
 	require.NoError(t, err)

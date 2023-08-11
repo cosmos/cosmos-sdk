d2b93e050d8a9c6f3ee529eae8af99d5cd95d64b
yihuang <huang@crypto.com>
2023-08-11 15:19:02 +0800

    feat: support build with rocksdb backend in CI (#17346)

--- scripts/go-mod-tidy-all.sh
+++ scripts/go-mod-tidy-all.sh
@@ -7,3 +7,12 @@ for modfile in $(find . -name go.mod); do
  DIR=$(dirname $modfile)
  (cd $DIR; go mod tidy)
 done
+
+if ! command -v gomod2nix &> /dev/null
+then
+    echo "gomod2nix could not be found in PATH"
+    echo "run \"go install github.com/nix-community/gomod2nix@latest\" to install"
+    exit
+fi
+# update gomod2nix.toml for simapp
+cd simapp; gomod2nix

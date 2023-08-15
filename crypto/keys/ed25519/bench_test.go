c32ca6599023ee3d68deb4ebd0946a028947cca0
bizk <santiago.yanzon1999@gmail.com>
2023-08-15 13:51:41 -0300

    added bench test for ed25519 key

---
+++ crypto/keys/ed25519/bench_test.go
@@ -0,0 +1,29 @@
+package ed25519
+
+import (
+	"io"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/internal/benchmarking"
+	"github.com/cosmos/cosmos-sdk/crypto/types"
+)
+
+func BenchmarkKeyGeneration(b *testing.B) {
+	b.ReportAllocs()
+	benchmarkKeygenWrapper := func(reader io.Reader) types.PrivKey {
+		return genPrivKey(reader)
+	}k
+	benchmarking.BenchmarkKeyGeneration(b, benchmarkKeygenWrapper)
+}
+
+func BenchmarkSigning(b *testing.B) {
+	b.ReportAllocs()
+	priv := GenPrivKey()
+	benchmarking.BenchmarkSigning(b, priv)
+}
+
+func BenchmarkVerification(b *testing.B) {
+	b.ReportAllocs()
+	priv := GenPrivKey()
+	benchmarking.BenchmarkVerification(b, priv)
+}

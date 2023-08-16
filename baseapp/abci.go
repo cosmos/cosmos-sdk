cebee4d898b9ba06415be70cd2f26b3b598bad18
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2023-08-16 20:07:23 +0800

    chore: add AppHash to prepare proposal

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -400,6 +400,7 @@ func (app *BaseApp) PrepareProposal(req *abci.RequestPrepareProposal) (resp *abc
 		Time:               req.Time,
 		ProposerAddress:    req.ProposerAddress,
 		NextValidatorsHash: req.NextValidatorsHash,
+		AppHash:            app.LastCommitID().Hash,
 	}
 	app.setState(execModePrepareProposal, header)
 

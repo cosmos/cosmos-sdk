e85b12d145b8331211947ace05b87d5b865862dd
Julian Toledano <jtoledanodiaz@gmail.com>
2023-08-17 11:23:26 +0200

    add: import hex keys

--- client/keys/import.go
+++ client/keys/import.go
@@ -38,3 +38,19 @@ func ImportKeyCommand() *cobra.Command {
 		},
 	}
 }
+
+func ImportKeyHexCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "import-hex <name> <hex> <key-type>",
+		Short: "Import private keys into the local keybase",
+		Long:  "Import hex encoded private key into the local keybase.",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			return clientCtx.Keyring.ImportPrivKeyHex(args[0], args[1], args[2])
+		},
+	}
+}

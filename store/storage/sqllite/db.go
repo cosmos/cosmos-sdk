c9dbb06b026bcff059c1b1578a0c403615e30662
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-08-16 20:47:51 -0700

    updates

--- store/storage/sqllite/db.go
+++ store/storage/sqllite/db.go
@@ -5,27 +5,46 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"cosmossdk.io/store/v2"
 	_ "modernc.org/sqlite"
 )
 
 const (
 	driverName = "sqlite"
+	dbName     = "ss.db"
 )
 
-var _ store.VersionedDatabase = (*Database)(nil)
+// var _ store.VersionedDatabase = (*Database)(nil)
 
 type Database struct {
 	storage *sql.DB
 }
 
 func New(dataDir string) (*Database, error) {
-	db, err := sql.Open(driverName, filepath.Join(dataDir, "ss.db"))
+	db, err := sql.Open(driverName, filepath.Join(dataDir, dbName))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open sqlite DB: %w", err)
+	}
+
+	sqlStmt := `
+	create table if not exists state_storage (
+		id integer not null primary key, 
+		store_key varchar not null,
+		key varchar not null,
+		value varchar not null,
+		height integer unsigned not null
+	);
+	create unique index if not exists idx_store_key_height on state_storage (store_key, key, height);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to exec SQL statement: %w", err)
 	}
 
 	return &Database{
 		storage: db,
 	}, nil
+}
+
+func (db *Database) Close() error {
+	return db.storage.Close()
 }

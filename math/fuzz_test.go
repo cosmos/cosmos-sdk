4ba4636fe34119c04489a59105fc69fcdc2baaa1
Julien Robert <julien@rbrt.fr>
2023-08-21 15:51:08 +0200

    remove fuzz

--- math/fuzz_test.go
+++ math/fuzz_test.go
@@ -4,33 +4,6 @@ import (
 	"testing"
 )
 
-func FuzzNewIntFromStringSize(f *testing.F) {
-	if testing.Short() {
-		f.Skip("running in -short mode")
-	}
-
-	// TODO add more test cases
-	f.Add("0")
-	f.Add("1")
-	f.Add("123456789")
-	f.Add("-123456789")
-	f.Add("9999999999999999000")
-	f.Add("-123456789123456789123456789")
-	f.Add("1000000000000000000000000000")
-	f.Add("-1000000000000000000000000000")
-	f.Add("123456789123456789123456789")
-	f.Add("-123456789123456789123456789")
-	f.Add("0x123456789abcdef")
-	f.Add("-0x123456789abcdef")
-
-	f.Fuzz(func(t *testing.T, input string) {
-		i, _ := NewIntFromString(input)
-		if i.Size() != len(input) {
-			t.Fatalf("input %s: i.Size()=%d, len(input)=%d", input, i.Size(), len(input))
-		}
-	})
-}
-
 func FuzzLegacyNewDecFromStr(f *testing.F) {
 	if testing.Short() {
 		f.Skip("running in -short mode")

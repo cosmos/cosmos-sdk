afe429f7eacd6c28836adbb8c9886051e4739c75
HuangYi <huang@crypto.com>
2023-08-21 14:24:31 +0800

    merged

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:yihuang/nixpkgs/master-1";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
     gomod2nix = {
       url = "github:nix-community/gomod2nix";

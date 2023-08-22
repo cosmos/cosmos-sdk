49d1a9439c4bc325754cde2c568386df1c0b9d90
Phuc Ta <vinhphuctadang@gmail.com>
2023-08-22 15:16:27 +0700

    fix: init globalCdc so that at least chain won't panik when globalcdc is nil

--- x/authz/codec/cdc.go
+++ x/authz/codec/cdc.go
@@ -2,6 +2,7 @@ package codec
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -16,4 +17,5 @@ func init() {
 	cryptocodec.RegisterCrypto(Amino)
 	codec.RegisterEvidences(Amino)
 	sdk.RegisterLegacyAminoCodec(Amino)
+	GlobalCdc = codec.NewProtoCodec(types.NewInterfaceRegistry())
 }

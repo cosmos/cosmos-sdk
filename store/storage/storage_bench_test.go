d2e6c6db0b454d837fd4130ed2726d84cf663c8f
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-08-22 12:41:51 -0700

    updates

--- store/storage/storage_bench_test.go
+++ store/storage/storage_bench_test.go
@@ -89,3 +89,87 @@ func BenchmarkSet_MultiStore(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkBatch_SingleStore(b *testing.B) {
+	for ty, fn := range backends {
+		db, err := fn(b.TempDir())
+		require.NoError(b, err)
+		defer func() {
+			_ = db.Close()
+		}()
+
+		b.Run(fmt.Sprintf("backend_%s", ty), func(b *testing.B) {
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+
+				batch, err := db.NewBatch(uint64(b.N + 1))
+				require.NoError(b, err)
+
+				for j := 0; j < 1000; j++ {
+					b.StopTimer()
+					key := make([]byte, 128)
+					val := make([]byte, 128)
+
+					_, err = rng.Read(key)
+					require.NoError(b, err)
+					_, err = rng.Read(val)
+					require.NoError(b, err)
+
+					b.StartTimer()
+					err = batch.Set(storeKey1, key, val)
+					require.NoError(b, err)
+					b.StopTimer()
+				}
+
+				b.StartTimer()
+				err = batch.Write()
+				require.NoError(b, err)
+			}
+		})
+	}
+}
+
+func BenchmarkBatch_MultiStore(b *testing.B) {
+	for ty, fn := range backends {
+		db, err := fn(b.TempDir())
+		require.NoError(b, err)
+		defer func() {
+			_ = db.Close()
+		}()
+
+		b.Run(fmt.Sprintf("backend_%s", ty), func(b *testing.B) {
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+
+				batch, err := db.NewBatch(uint64(b.N + 1))
+				require.NoError(b, err)
+
+				for j := 0; j < 1000; j++ {
+					b.StopTimer()
+					key := make([]byte, 128)
+					val := make([]byte, 128)
+
+					_, err = rng.Read(key)
+					require.NoError(b, err)
+					_, err = rng.Read(val)
+					require.NoError(b, err)
+
+					sk := storeKeys[rng.Intn(len(storeKeys))]
+
+					b.StartTimer()
+					err = batch.Set(sk, key, val)
+					require.NoError(b, err)
+					b.StopTimer()
+				}
+
+				b.StartTimer()
+				err = batch.Write()
+				require.NoError(b, err)
+			}
+		})
+	}
+}

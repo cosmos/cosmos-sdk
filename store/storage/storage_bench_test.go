41bea8be3d52f30ceda2e7608e629d90a4b7e6d0
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-08-23 19:46:23 -0700

    updates

--- store/storage/storage_bench_test.go
+++ store/storage/storage_bench_test.go
@@ -4,8 +4,10 @@
 package storage
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
+	"sort"
 	"testing"
 
 	"cosmossdk.io/store/v2"
@@ -17,13 +19,13 @@ import (
 
 var (
 	backends = map[string]func(dataDir string) (store.VersionedDatabase, error){
-		"rocksdb": func(dataDir string) (store.VersionedDatabase, error) {
+		"rocksdb_versiondb_opts": func(dataDir string) (store.VersionedDatabase, error) {
 			return rocksdb.New(dataDir)
 		},
-		"pebbledb": func(dataDir string) (store.VersionedDatabase, error) {
+		"pebbledb_default_opts": func(dataDir string) (store.VersionedDatabase, error) {
 			return pebbledb.New(dataDir)
 		},
-		"btree(sqlite)": func(dataDir string) (store.VersionedDatabase, error) {
+		"btree_sqlite": func(dataDir string) (store.VersionedDatabase, error) {
 			return sqlite.New(dataDir)
 		},
 	}
@@ -83,8 +85,6 @@ func BenchmarkGet(b *testing.B) {
 			_ = db.Close()
 		}()
 
-		b.StopTimer()
-
 		batch, err := db.NewBatch(1)
 		require.NoError(b, err)
 
@@ -140,12 +140,73 @@ func BenchmarkBatch(b *testing.B) {
 					b.StartTimer()
 					err = batch.Set(storeKey1, key, val)
 					require.NoError(b, err)
-					b.StopTimer()
 				}
 
-				b.StartTimer()
 				err = batch.Write()
 				require.NoError(b, err)
+			}
+		})
+	}
+}
+
+func BenchmarkIterate(b *testing.B) {
+	numKeyVals := 1_000_000
+	keys := make([][]byte, numKeyVals)
+	vals := make([][]byte, numKeyVals)
+	for i := 0; i < numKeyVals; i++ {
+		key := make([]byte, 128)
+		val := make([]byte, 128)
+
+		_, err := rng.Read(key)
+		require.NoError(b, err)
+		_, err = rng.Read(val)
+		require.NoError(b, err)
+
+		keys[i] = key
+		vals[i] = val
+
+	}
+
+	for ty, fn := range backends {
+		db, err := fn(b.TempDir())
+		require.NoError(b, err)
+		defer func() {
+			_ = db.Close()
+		}()
+
+		b.StopTimer()
+
+		batch, err := db.NewBatch(1)
+		require.NoError(b, err)
+
+		for i := 0; i < numKeyVals; i++ {
+			err = batch.Set(storeKey1, keys[i], vals[i])
+			require.NoError(b, err)
+		}
+
+		require.NoError(b, batch.Write())
+
+		sort.Slice(keys, func(i, j int) bool {
+			return bytes.Compare(keys[i], keys[j]) < 0
+		})
+
+		b.Run(fmt.Sprintf("backend_%s", ty), func(b *testing.B) {
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+
+				itr, err := db.NewIterator(storeKey1, 1, keys[0], nil)
+				require.NoError(b, err)
+
+				b.StartTimer()
+
+				for ; itr.Valid(); itr.Next() {
+					_ = itr.Key()
+					_ = itr.Value()
+				}
+
+				require.NoError(b, itr.Error())
 			}
 		})
 	}

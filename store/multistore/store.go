54dd27aa47f6291828c172bd2b98bcf835f762b3
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-08-29 16:50:36 -0700

    updates

--- store/multistore/store.go
+++ store/multistore/store.go
@@ -3,12 +3,18 @@ package multistore
 import (
 	"cosmossdk.io/store/v2"
 	"cosmossdk.io/store/v2/commitment"
+	ics23 "github.com/cosmos/ics23/go"
 )
 
-// TODO: Move this to Core package.
+// MultiStore defines an abstraction layer containing a State Storage (SS) engine
+// and one or more State Commitment (SC) engines.
+//
+// TODO: Move this type to the Core package.
 type MultiStore interface {
+	GetProof(storeKey string, version uint64, key []byte) (*ics23.CommitmentProof, error)
+	LoadVersion(version uint64) error
 	WorkingHash() []byte
-	Commit() error
+	Commit() ([]byte, error)
 }
 
 type Store struct {

c6c65c12d039ab05e42e78b47c0e5240aff42168
Muffet <samanthalricotta@gmail.com>
2023-09-01 15:13:16 +0300

    update to pre

--- docs/pre.sh
+++ docs/pre.sh
@@ -5,7 +5,7 @@ for D in ../x/*; do
   if [ -d "${D}" ]; then
     MODDOC=docs/build/modules/$(basename "$D")
     rm -rf $MODDOC
-    mkdir -p $MODDOC && cp -r $D/README.md "$MODDOC/$(basename "$D").md"
+    mkdir -p $MODDOC && cp -r $D/README.md "$_"
   fi
 done
 
@@ -15,7 +15,7 @@ cp ../x/auth/vesting/README.md ./docs/build/modules/auth/1-vesting.md
 cp ../x/auth/tx/README.md ./docs/build/modules/auth/2-tx.md
 
 ## Add modules page list
-cat ../x/README.md | sed 's/\.\.\/docs\/build\/building-modules\/README\.md/\/building-modules\/intro\.html/g' > ./docs/build/modules/"$MODDOC/$(basename "$D").md"
+cat ../x/README.md | sed 's/\.\.\/docs\/build\/building-modules\/README\.md/\/building-modules\/intro\.html/g' > ./docs/modules/README.md
 
 ## Add tooling documentation
 cp ../tools/cosmovisor/README.md ./docs/build/tooling/01-cosmovisor.md

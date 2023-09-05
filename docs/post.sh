92b1dbcd23d59a45317ba2ab60889593946cb78e
samricotta <37125168+samricotta@users.noreply.github.com>
2023-09-05 10:56:01 +0300

    refactor: docs path updates (#17459)
    Co-authored-by: Julien Robert <julien@rbrt.fr>
    Co-authored-by: Marko <marko@baricevic.me>
    Co-authored-by: marbar3778 <marbar3778@yahoo.com>
    (cherry picked from commit f198ea9445fe1c3a6355eabf90242f26382550a4)
    
    # Conflicts:
    #	.github/workflows/deploy-docs.yml
    #	UPGRADING.md
    #	docs/docs/build/building-modules/01-module-manager.md
    #	docs/docs/build/building-modules/06-keeper.md
    #	docs/docs/develop/advanced/00-baseapp.md
    #	docs/rfc/README.md

--- docs/post.sh
+++ docs/post.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-find docs/modules ! -name '_category_.json' -type f -exec rm -rf {} +
-rm -rf docs/tooling/01-cosmovisor.md
-rm -rf docs/tooling/02-confix.md
-rm -rf docs/tooling/03-hubl.md
-rm -rf docs/packages/01-depinject.md
-rm -rf docs/packages/02-collections.md
-rm -rf docs/packages/03-orm.md
-rm -rf docs/core/17-autocli.md
-rm -rf docs/run-node/04-rosetta.md
-rm -rf docs/architecture
-rm -rf docs/spec
-rm -rf docs/rfc
-rm -rf docs/migrations/02-upgrading.md
+find docs/build/modules ! -name '_category_.json' -type f -exec rm -rf {} +
+rm -rf docs/build/tooling/01-cosmovisor.md
+rm -rf docs/build/tooling/02-confix.md
+rm -rf docs/build/tooling/03-hubl.md
+rm -rf docs/build/packages/01-depinject.md
+rm -rf docs/build/packages/02-collections.md
+rm -rf docs/build/packages/03-orm.md
+rm -rf docs/develop/advaced-concepts/17-autocli.md
+rm -rf docs/user/run-node/04-rosetta.md
+rm -rf docs/build/architecture
+rm -rf docs/build/spec
+rm -rf docs/build/rfc
+rm -rf  docs/develop/advanced/17-autocli.md
+rm -rf docs/build/migrations/02-upgrading.md
 rm -rf versioned_docs versioned_sidebars versions.json

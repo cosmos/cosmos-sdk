c99bafb83af66d99d318d0530eee63b65c2b5374
likhita-809 <likhita@vitwit.com>
2023-09-08 15:53:13 +0530

    fix lint

--- x/pool/module.go
+++ x/pool/module.go
@@ -3,14 +3,14 @@ package pool
 import (
 	"context"
 
+	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
+
 	modulev1 "cosmossdk.io/api/cosmos/pool/module/v1"
 	"cosmossdk.io/core/appmodule"
 	storetypes "cosmossdk.io/core/store"
 	"cosmossdk.io/depinject"
 	"cosmossdk.io/x/pool/keeper"
 	"cosmossdk.io/x/pool/types"
-
-	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"

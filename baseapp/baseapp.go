c79964c5379772b2900ee2b737d4e14ccf3ba8f6
mmsqe <mavis@crypto.com>
2023-09-11 15:08:43 +0800

    chore: log panic recovery in runTx (backport: #17556) (#17671)
    Co-authored-by: Cal Bera <calbera@berachain.com>

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -639,6 +639,7 @@ func (app *BaseApp) runTx(mode runTxMode, txBytes []byte) (gInfo sdk.GasInfo, re
 		if r := recover(); r != nil {
 			recoveryMW := newOutOfGasRecoveryMiddleware(gasWanted, ctx, app.runTxRecoveryMiddleware)
 			err, result = processRecovery(r, recoveryMW), nil
+			ctx.Logger().Error("panic recovered in runTx", "err", err)
 		}
 
 		gInfo = sdk.GasInfo{GasWanted: gasWanted, GasUsed: ctx.GasMeter().GasConsumed()}

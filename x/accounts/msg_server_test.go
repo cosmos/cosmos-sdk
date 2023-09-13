c946e14211f109461a885c3bddc1c817639fcc2c
testinginprod <98415576+testinginprod@users.noreply.github.com>
2023-09-13 20:39:18 +0200

    feat(accounts): allow accounts to query and execute modules (#17685)
    Co-authored-by: unknown unknown <unknown@unknown>

--- x/accounts/msg_server_test.go
+++ x/accounts/msg_server_test.go
@@ -1,6 +1,7 @@
 package accounts
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,6 +10,7 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
+	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
 	"cosmossdk.io/x/accounts/internal/implementation"
 	v1 "cosmossdk.io/x/accounts/v1"
 )
@@ -17,6 +19,11 @@ func TestMsgServer(t *testing.T) {
 	k, ctx := newKeeper(t, map[string]implementation.Account{
 		"test": TestAccount{},
 	})
+	k.queryModuleFunc = func(ctx context.Context, msg proto.Message) (proto.Message, error) {
+		_, ok := msg.(*bankv1beta1.QueryBalanceRequest)
+		require.True(t, ok)
+		return &bankv1beta1.QueryBalanceResponse{}, nil
+	}
 
 	s := NewMsgServer(k)
 

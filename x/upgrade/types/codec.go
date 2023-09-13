70d739df8592817710b736d7676c09b035eb2c54
marbar3778 <marbar3778@yahoo.com>
2023-09-13 19:27:10 +0200

    remove content type

--- x/upgrade/types/codec.go
+++ x/upgrade/types/codec.go
@@ -6,7 +6,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 )
 
 // RegisterLegacyAminoCodec registers concrete types on the LegacyAmino codec
@@ -20,11 +19,6 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 
 // RegisterInterfaces registers the interfaces types with the Interface Registry.
 func RegisterInterfaces(registry types.InterfaceRegistry) {
-	registry.RegisterImplementations(
-		(*govtypes.Content)(nil),
-		&SoftwareUpgradeProposal{},
-		&CancelSoftwareUpgradeProposal{},
-	)
 
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgSoftwareUpgrade{},

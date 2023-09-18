23e48a1dec6010b8e2359f9f11dd839a90579143
atheesh <atheesh@vitwit.com>
2023-09-18 17:28:33 +0530

    fix tests

--- tests/integration/distribution/keeper/msg_server_test.go
+++ tests/integration/distribution/keeper/msg_server_test.go
@@ -100,6 +100,7 @@ func initFixture(tb testing.TB) *fixture {
 	)
 
 	stakingKeeper := stakingkeeper.NewKeeper(cdc, runtime.NewKVStoreService(keys[stakingtypes.StoreKey]), accountKeeper, bankKeeper, authority.String(), addresscodec.NewBech32Codec(sdk.Bech32PrefixValAddr), addresscodec.NewBech32Codec(sdk.Bech32PrefixConsAddr))
+	stakingKeeper.Params.Set(newCtx, stakingtypes.DefaultParams())
 
 	distrKeeper := distrkeeper.NewKeeper(
 		cdc, runtime.NewKVStoreService(keys[distrtypes.StoreKey]), accountKeeper, bankKeeper, stakingKeeper, distrtypes.ModuleName, authority.String(),

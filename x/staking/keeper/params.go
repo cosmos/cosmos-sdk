02f1544033ad7a6a072d69781ed9284ecf712487
atheesh <atheesh@vitwit.com>
2023-09-18 20:18:14 +0530

    review

--- x/staking/keeper/params.go
+++ x/staking/keeper/params.go
@@ -54,32 +54,3 @@ func (k Keeper) MinCommissionRate(ctx context.Context) (math.LegacyDec, error) {
 	params, err := k.Params.Get(ctx)
 	return params.MinCommissionRate, err
 }
-
-// // SetParams sets the x/staking module parameters.
-// // CONTRACT: This method performs no validation of the parameters.
-// func (k Keeper) SetParams(ctx context.Context, params types.Params) error {
-// 	store := k.storeService.OpenKVStore(ctx)
-// 	bz, err := k.cdc.Marshal(&params)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return store.Set(types.ParamsKey, bz)
-// }
-
-// // GetParams gets the x/staking module parameters.
-// func (k Keeper) GetParams(ctx context.Context) (params types.Params, err error) {
-// 	params, err := k.Params.Get(ctx)
-
-// 	store := k.storeService.OpenKVStore(ctx)
-// 	bz, err := store.Get(types.ParamsKey)
-// 	if err != nil {
-// 		return params, err
-// 	}
-
-// 	if bz == nil {
-// 		return params, nil
-// 	}
-
-// 	err = k.cdc.Unmarshal(bz, &params)
-// 	return params, err
-// }

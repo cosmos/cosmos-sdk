7e842754e7ed76197d3a629b144407dc7025798d
Julien Robert <julien@rbrt.fr>
2023-09-21 18:24:51 +0200

    wip

--- client/v2/autocli/flag/coin.go
+++ client/v2/autocli/flag/coin.go
@@ -2,17 +2,19 @@ package flag
 
 import (
 	"context"
+	"strings"
 
 	"google.golang.org/protobuf/reflect/protoreflect"
 
 	basev1beta1 "cosmossdk.io/api/cosmos/base/v1beta1"
+	ip "cosmossdk.io/client/v2/internal/proto"
 	"cosmossdk.io/core/coins"
 )
 
 type coinType struct{}
 
 type coinValue struct {
-	value *basev1beta1.Coin
+	value []*basev1beta1.Coin
 }
 
 func (c coinType) NewValue(context.Context, *Builder) Value {
@@ -28,19 +30,37 @@ func (c *coinValue) Get(protoreflect.Value) (protoreflect.Value, error) {
 	if c.value == nil {
 		return protoreflect.Value{}, nil
 	}
-	return protoreflect.ValueOfMessage(c.value.ProtoReflect()), nil
+
+	if len(c.value) == 1 {
+		return protoreflect.ValueOfMessage(c.value[0].ProtoReflect()), nil
+	}
+
+	return protoreflect.ValueOfList(ip.NewGenericList(c.value)), nil
 }
 
 func (c *coinValue) String() string {
-	return c.value.String()
+	if len(c.value) == 1 {
+		return c.value[0].String()
+	}
+
+	var result string
+	for _, coin := range c.value {
+		result += coin.String() + ","
+	}
+
+	return result
 }
 
 func (c *coinValue) Set(stringValue string) error {
-	coin, err := coins.ParseCoin(stringValue)
-	if err != nil {
-		return err
+	result := strings.Split(stringValue, ",")
+	for _, coin := range result {
+		coin, err := coins.ParseCoin(coin)
+		if err != nil {
+			return err
+		}
+		c.value = append(c.value, coin)
 	}
-	c.value = coin
+
 	return nil
 }
 

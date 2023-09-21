4ed3551720d984df26b3ace0f5f0905bd68696d8
marbar3778 <marbar3778@yahoo.com>
2023-09-21 10:51:59 +0200

    bring back no ops

--- x/bank/keeper/migrations.go
+++ x/bank/keeper/migrations.go
@@ -1,5 +1,7 @@
 package keeper
 
+import "context"
+
 // Migrator is a struct for handling in-place store migrations.
 type Migrator struct {
 	keeper BaseKeeper
@@ -8,4 +10,19 @@ type Migrator struct {
 // NewMigrator returns a new Migrator.
 func NewMigrator(keeper BaseKeeper) Migrator {
 	return Migrator{keeper: keeper}
+}
+
+// Migrate1to2 migrates from version 1 to 2.
+func (m Migrator) Migrate1to2(ctx context.Context) error {
+	return nil
+}
+
+// Migrate2to3 migrates x/bank storage from version 2 to 3.
+func (m Migrator) Migrate2to3(ctx context.Context) error {
+	return nil
+}
+
+// Migrate3to4 migrates x/bank storage from version 3 to 4.
+func (m Migrator) Migrate3to4(ctx context.Context) error {
+	return nil
 }

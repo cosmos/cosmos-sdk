f9aa3c0784d6b298a31d0df5a8af66651c03ce60
Julien Robert <julien@rbrt.fr>
2023-09-25 09:18:02 +0200

    wip

--- client/v2/autocli/flag/coin.go
+++ client/v2/autocli/flag/coin.go
@@ -7,14 +7,13 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 
 	basev1beta1 "cosmossdk.io/api/cosmos/base/v1beta1"
-	ip "cosmossdk.io/client/v2/internal/proto"
 	"cosmossdk.io/core/coins"
 )
 
 type coinType struct{}
 
 type coinValue struct {
-	value []*basev1beta1.Coin
+	values []*basev1beta1.Coin
 }
 
 func (c coinType) NewValue(context.Context, *Builder) Value {
@@ -26,25 +25,26 @@ func (c coinType) DefaultValue() string {
 	return stringCoin
 }
 
-func (c *coinValue) Get(protoreflect.Value) (protoreflect.Value, error) {
-	if c.value == nil {
+func (c *coinValue) Get(mutable protoreflect.Value) (protoreflect.Value, error) {
+	if c.values == nil {
 		return protoreflect.Value{}, nil
 	}
 
-	if len(c.value) == 1 {
-		return protoreflect.ValueOfMessage(c.value[0].ProtoReflect()), nil
+	list := mutable.List()
+	for _, value := range c.values {
+		list.Append(protoreflect.ValueOfMessage(value.ProtoReflect()))
 	}
 
-	return protoreflect.ValueOfList(ip.NewGenericList(c.value)), nil
+	return mutable, nil
 }
 
 func (c *coinValue) String() string {
-	if len(c.value) == 1 {
-		return c.value[0].String()
+	if len(c.values) == 1 {
+		return c.values[0].String()
 	}
 
 	var result string
-	for _, coin := range c.value {
+	for _, coin := range c.values {
 		result += coin.String() + ","
 	}
 
@@ -58,7 +58,7 @@ func (c *coinValue) Set(stringValue string) error {
 		if err != nil {
 			return err
 		}
-		c.value = append(c.value, coin)
+		c.values = append(c.values, coin)
 	}
 
 	return nil

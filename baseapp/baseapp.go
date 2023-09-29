30cbbf82bb4c0d152884f83c2490055114020621
Michele Meloni <cleaversdev@gmail.com>
2023-09-29 21:17:41 +0200

    feat(stream): add block timestamp in events channel struct

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -744,7 +744,10 @@ func (app *BaseApp) beginBlock(req *abci.RequestFinalizeBlock) (sdk.BeginBlock, 
 				abci.EventAttribute{Key: "mode", Value: "BeginBlock"},
 			)
 		}
-		app.AddStreamEvents(app.finalizeBlockState.ctx.BlockHeight(), resp.Events, true)
+
+		ctx := app.finalizeBlockState.ctx
+		app.AddStreamEvents(ctx.BlockHeight(), ctx.BlockTime(), resp.Events, true)
+
 		resp.Events = sdk.MarkEventsToIndex(resp.Events, app.indexEvents)
 	}
 
@@ -777,7 +780,8 @@ func (app *BaseApp) deliverTx(tx []byte) *abci.ExecTxResult {
 		return resp
 	}
 
-	app.AddStreamEvents(app.checkState.Context().BlockHeight(), result.Events, false)
+	ctx := app.checkState.Context()
+	app.AddStreamEvents(ctx.BlockHeight(), ctx.BlockTime(), result.Events, false)
 
 	resp = &abci.ExecTxResult{
 		GasWanted: int64(gInfo.GasWanted),
@@ -809,7 +813,9 @@ func (app *BaseApp) endBlock(ctx context.Context) (sdk.EndBlock, error) {
 			)
 		}
 
-		app.AddStreamEvents(app.finalizeBlockState.ctx.BlockHeight(), eb.Events, true)
+		ctx := app.finalizeBlockState.ctx
+		app.AddStreamEvents(ctx.BlockHeight(), ctx.BlockTime(), eb.Events, true)
+
 		eb.Events = sdk.MarkEventsToIndex(eb.Events, app.indexEvents)
 		endblock = eb
 	}
--- baseapp/chain_stream.go
+++ baseapp/chain_stream.go
@@ -1,19 +1,24 @@
 package baseapp
 
-import abci "github.com/cometbft/cometbft/abci/types"
+import (
+	abci "github.com/cometbft/cometbft/abci/types"
+	"time"
+)
 
 type StreamEvents struct {
-	Events []abci.Event
-	Height uint64
-	Flush  bool
+	Events    []abci.Event
+	Height    uint64
+	BlockTime time.Time
+	Flush     bool
 }
 
-func (app *BaseApp) AddStreamEvents(height int64, events []abci.Event, flush bool) {
+func (app *BaseApp) AddStreamEvents(height int64, blockTime time.Time, events []abci.Event, flush bool) {
 	if app.EnableStreamer {
 		app.StreamEvents <- StreamEvents{
-			Events: events,
-			Height: uint64(height),
-			Flush:  flush,
+			Events:    events,
+			Height:    uint64(height),
+			BlockTime: blockTime,
+			Flush:     flush,
 		}
 	}
 }

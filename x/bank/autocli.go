56879ff3685ac687b04c5029cde6746ed9f1f443
atheesh <atheesh@vitwit.com>
2023-10-05 17:10:13 +0530

    review changes

--- x/bank/autocli.go
+++ x/bank/autocli.go
@@ -93,18 +93,18 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
 				{
 					RpcMethod: "Send",
-					Use:       "send [to_address] [amount] --from [from]",
+					Use:       "send [from_key_or_address] [to_address] [amount]",
 					Short:     "Send funds from one account to another.",
 					Long: `Send funds from one account to another.
 			Note, the '--from' flag is ignored as it is implied from [from_key_or].
 			When using '--dry-run' a key name cannot be used, only a bech32 address.`,
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "to_address"}, {ProtoField: "amount"}},
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "from_address"}, {ProtoField: "to_address"}, {ProtoField: "amount"}},
 				},
 				{
 					RpcMethod:      "Burn",
-					Use:            "burn [amount] --from [from]",
+					Use:            "burn [from_key_or_address] [amount]",
 					Short:          "Burns the amount specified from the given account.",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "amount"}},
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "from_address"}, {ProtoField: "amount"}},
 				},
 				{
 					RpcMethod: "UpdateParams",

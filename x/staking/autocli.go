cbd866a13fe10cebcbfd5fac9cc347ab809b38bc
atheesh <atheesh@vitwit.com>
2023-10-06 15:18:06 +0530

    feat(x/staking): autocli tx support

--- x/staking/autocli.go
+++ x/staking/autocli.go
@@ -7,6 +7,7 @@ import (
 	_ "cosmossdk.io/api/cosmos/crypto/ed25519" // register to that it shows up in protoregistry.GlobalTypes
 	stakingv1beta "cosmossdk.io/api/cosmos/staking/v1beta1"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 )
 
@@ -142,6 +143,58 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 		},
 		Tx: &autocliv1.ServiceCommandDescriptor{
 			Service: stakingv1beta.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod:      "Delegate",
+					Use:            "delegate [validator-addr] [amount] --from [delegator_address]",
+					Short:          "Delegate liquid tokens to a validator",
+					Long:           "Delegate an amount of liquid coins to a validator from your wallet.",
+					Example:        fmt.Sprintf("%s tx staking delegate cosmosvalopers1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 1000stake --from mykey", version.AppName),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "validator_address"}, {ProtoField: "amount"}},
+				},
+				{
+					RpcMethod: "BeginRedelegate",
+					Use:       "redelegate [src-validator-addr] [dst-validator-addr] [amount] --from [delegator]",
+					Alias:     []string{"redelegate"},
+					Short:     "Generate multisig signatures for transactions generated offline",
+					Long: `Sign transactions created with the --generate-only flag that require multisig signatures.
+
+Read one or more signatures from one or more [signature] file, generate a multisig signature compliant to the
+multisig key [name], and attach the key name to the transaction read from [file].
+
+If --signature-only flag is on, output a JSON representation
+of only the generated signature.
+
+If the --offline flag is on, the client will not reach out to an external node.
+Account number or sequence number lookups are not performed so you must
+set these parameters manually.
+
+The current multisig implementation defaults to amino-json sign mode.
+The SIGN_MODE_DIRECT sign mode is not supported.`,
+					Example:        fmt.Sprintf(`%s tx multisign transaction.json k1k2k3 k1sig.json k2sig.json k3sig.json`, version.AppName),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "validator_src_address"}, {ProtoField: "validator_dst_address"}, {ProtoField: "amount"}},
+				},
+				{
+					RpcMethod:      "Undelegate",
+					Use:            "unbond [validator-addr] [amount] --from [delegator_address]",
+					Short:          "Unbond shares from a validator",
+					Long:           "Unbond an amount of bonded shares from a validator.",
+					Example:        fmt.Sprintf(`%s tx staking unbond %s1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj 100stake --from mykey`, version.AppName, sdk.GetConfig().GetBech32ValidatorAddrPrefix()),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "validator_address"}, {ProtoField: "amount"}},
+				},
+				{
+					RpcMethod:      "CancelUnbondingDelegation",
+					Use:            "cancel-unbond [validator-addr] [amount] [creation-height]",
+					Short:          "Cancel unbonding delegation and delegate back to the validator",
+					Example:        fmt.Sprintf(`%s tx staking cancel-unbond %s1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj 100stake 2 --from mykey`, version.AppName, sdk.GetConfig().GetBech32ValidatorAddrPrefix()),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "validator_address"}, {ProtoField: "amount"}, {ProtoField: "creation_height"}},
+				},
+				{
+					RpcMethod: "UpdateParams",
+					Skip:      true, // skipped because authority gated
+				},
+			},
+			EnhanceCustomCommand: true,
 		},
 	}
 }

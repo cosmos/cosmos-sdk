b86d9c44f8a8b05b63e9089de71c43830892cb67
Julien Robert <julien@rbrt.fr>
2023-10-06 10:07:40 +0200

    feat(x/crisis): add autocli options for tx (#17965)
    (cherry picked from commit 12b45f3db608442325311c8392f5d333f30c54be)
    
    # Conflicts:
    #	x/crisis/client/cli/tx_test.go
    #	x/crisis/module.go

--- x/crisis/module.go
+++ x/crisis/module.go
@@ -25,8 +25,11 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+<<<<<<< HEAD
 	"github.com/cosmos/cosmos-sdk/x/crisis/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/crisis/exported"
+=======
+>>>>>>> 12b45f3db (feat(x/crisis): add autocli options for tx (#17965))
 	"github.com/cosmos/cosmos-sdk/x/crisis/keeper"
 	"github.com/cosmos/cosmos-sdk/x/crisis/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
@@ -80,11 +83,6 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncod
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the crisis module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(_ client.Context, _ *gwruntime.ServeMux) {}
-
-// GetTxCmd returns the root tx command for the crisis module.
-func (b AppModuleBasic) GetTxCmd() *cobra.Command {
-	return cli.NewTxCmd()
-}
 
 // RegisterInterfaces registers interfaces and implementations of the crisis
 // module.

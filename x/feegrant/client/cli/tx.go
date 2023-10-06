b7fae0950eb371470a87ff11aa096c83d39e87f4
Julien Robert <julien@rbrt.fr>
2023-10-06 11:32:45 +0200

    add comment + client/v2 alias handling

--- x/feegrant/client/cli/tx.go
+++ x/feegrant/client/cli/tx.go
@@ -45,6 +45,7 @@ func GetTxCmd(ac address.Codec) *cobra.Command {
 }
 
 // NewCmdFeeGrant returns a CLI command handler to create a MsgGrantAllowance transaction.
+// This command is more powerful than AutoCLI generated command as it allows a better input validation.
 func NewCmdFeeGrant(ac address.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "grant [granter_key_or_address] [grantee]",

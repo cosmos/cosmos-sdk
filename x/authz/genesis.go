f24ebe9ac92bcfec0e25f985991d7da54b4e822d
marbar3778 <marbar3778@yahoo.com>
2023-10-10 15:10:17 +0200

    remove dead code

--- x/authz/genesis.go
+++ x/authz/genesis.go
@@ -11,11 +11,6 @@ func NewGenesisState(entries []GrantAuthorization) *GenesisState {
 	}
 }
 
-// ValidateGenesis check the given genesis state has no integrity issues
-func ValidateGenesis(data GenesisState) error {
-	return nil
-}
-
 // DefaultGenesisState - Return a default genesis state
 func DefaultGenesisState() *GenesisState {
 	return &GenesisState{}

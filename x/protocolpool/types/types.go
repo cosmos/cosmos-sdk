8782ff8fae7951aff47b8fca576f18b1ff987fca
likhita-809 <likhita@vitwit.com>
2023-10-13 20:00:23 +0530

    delete unused distrinfo struct

--- x/protocolpool/types/types.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
-// DistributionInfo represents the information about distributed funds for a recipient.
-type DistributionInfo struct {
-	Address sdk.AccAddress `json:"address"`
-	Amount  sdk.Coin       `json:"amount"`
-}

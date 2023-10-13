6b0675f0c2161d84d99828129b73bcdba4d5fbaa
likhita-809 <likhita@vitwit.com>
2023-10-13 19:57:10 +0530

    add grpc query implementation

--- x/protocolpool/keeper/grpc_query.go
+++ x/protocolpool/keeper/grpc_query.go
@@ -2,8 +2,12 @@ package keeper
 
 import (
 	"context"
+	"errors"
 
+	"cosmossdk.io/collections"
 	"cosmossdk.io/x/protocolpool/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -26,4 +30,22 @@ func (k Querier) CommunityPool(ctx context.Context, req *types.QueryCommunityPoo
 	}
 	decCoins := sdk.NewDecCoinsFromCoins(amount...)
 	return &types.QueryCommunityPoolResponse{Pool: decCoins}, nil
+}
+
+func (k Querier) UnclaimedBudget(ctx context.Context, req *types.QueryUnclaimedBudgetRequest) (*types.QueryUnclaimedBudgetResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	address, err := k.Keeper.authKeeper.AddressCodec().StringToBytes(req.Address)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid address: %s", err.Error())
+	}
+	budget, err := k.Keeper.BudgetProposal.Get(ctx, address)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil, status.Errorf(codes.NotFound, "no budget proposal found for address: %s", req.Address)
+		}
+		return nil, err
+	}
+	return &types.QueryUnclaimedBudgetResponse{budget.TotalBudget}, nil
 }

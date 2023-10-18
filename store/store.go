6417aa036f9ea820fc4fa735c8b8e946d16465c1
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-10-18 15:08:04 -0700

    update godocs

--- store/store.go
+++ store/store.go
@@ -6,8 +6,6 @@ import (
 	ics23 "github.com/cosmos/ics23/go"
 )
 
-// TODO: Move relevant types to the 'core' package.
-
 // StoreType defines a type of KVStore.
 type StoreType int
 
@@ -21,13 +19,30 @@ const (
 // RootStore defines an abstraction layer containing a State Storage (SS) engine
 // and one or more State Commitment (SC) engines.
 type RootStore interface {
+	// GetSCStore should return the SC backend for the given store key. A RootStore
+	// implementation may choose to ignore the store key in cases where only a single
+	// SC backend is used.
 	GetSCStore(storeKey string) Tree
+	// MountSCStore should mount the given SC backend for the given store key. For
+	// implementations that utilize a single SC backend, this method may be optional
+	// or a no-op.
 	MountSCStore(storeKey string, sc Tree) error
+	// GetKVStore returns the KVStore for the given store key. If an implementation
+	// chooses to have a single SS backend, the store key may be ignored.
 	GetKVStore(storeKey string) KVStore
+	// GetBranchedKVStore returns the KVStore for the given store key. If an
+	// implementation chooses to have a single SS backend, the store key may be
+	// ignored.
 	GetBranchedKVStore(storeKey string) BranchedKVStore
 
+	// GetProof returns a proof for the given key, version (height), and store key
+	// tuple. See the CommitmentProof type for the concrete supported proof types.
 	GetProof(storeKey string, version uint64, key []byte) (*ics23.CommitmentProof, error)
 
+	// Query(*RequestQuery) (*ResponseQuery, error)
+
+	// Branch should branch the entire RootStore, i.e. a copy of the original RootStore
+	// except with all internal KV store(s) branched.
 	Branch() BranchedRootStore
 
 	SetTracingContext(tc TraceContext)
@@ -41,12 +56,6 @@ type RootStore interface {
 	WorkingHash() ([]byte, error)
 	SetCommitHeader(h CommitHeader)
 	Commit() ([]byte, error)
-
-	// TODO:
-	//
-	// - Queries
-	//
-	// Ref: https://github.com/cosmos/cosmos-sdk/issues/17314
 
 	io.Closer
 }

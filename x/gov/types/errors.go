9484f4883aca73911368932c56a0dbeadef31020
Julien Robert <julien@rbrt.fr>
2023-10-20 18:05:12 +0200

    feat(x/gov): limit deposited coins to accepted proposal denom (#18189)
    (cherry picked from commit 2fbc547ce9a8cf19548ddbeeb3b7723672d595ca)
    
    # Conflicts:
    #	CHANGELOG.md

--- x/gov/types/errors.go
+++ x/gov/types/errors.go
@@ -24,4 +24,5 @@ var (
 	ErrVotingPeriodEnded       = errors.Register(ModuleName, 20, "voting period already ended")
 	ErrInvalidProposal         = errors.Register(ModuleName, 21, "invalid proposal")
 	ErrSummaryTooLong          = errors.Register(ModuleName, 22, "summary too long")
+	ErrInvalidDepositDenom     = errors.Register(ModuleName, 23, "invalid deposit denom")
 )

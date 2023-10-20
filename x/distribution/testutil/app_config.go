fc441dc640adf8b390407228983df06620af49b9
marbar3778 <marbar3778@yahoo.com>
2023-10-20 12:41:02 +0200

    fix dist test

--- x/distribution/testutil/app_config.go
+++ x/distribution/testutil/app_config.go
@@ -2,6 +2,7 @@ package testutil
 
 import (
 	_ "cosmossdk.io/x/mint" // import as blank for app wiring
+	_ "cosmossdk.io/x/protocolpool"
 
 	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	_ "github.com/cosmos/cosmos-sdk/x/auth"           // import as blank for app wiring

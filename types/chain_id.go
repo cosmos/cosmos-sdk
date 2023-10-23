9c220f59d7d5e648163d8dce0e34442572c747d4
HuangYi <huang@crypto.com>
2023-10-23 15:41:12 +0800

    mention encoding/json/v2

--- types/chain_id.go
+++ types/chain_id.go
@@ -10,6 +10,8 @@ import (
 const ChainIDFieldName = "chain_id"
 
 // ParseChainIDFromGenesis parses the chain-id from the genesis file using constant memory.
+//
+// TODO consider [encoding/json/v2](https://github.com/golang/go/discussions/63397) when it's ready.
 func ParseChainIDFromGenesis(reader io.Reader) (string, error) {
 	decoder := jstream.NewDecoder(reader, 1).EmitKV()
 	var chain_id string

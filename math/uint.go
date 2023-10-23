442c3c5e4baf83171e78190d750e9e04e0b0f62c
Facundo Medica <14063057+facundomedica@users.noreply.github.com>
2023-10-23 15:21:13 +0200

    fix: RelativePow now returns 1 when 0^0 (#18211)

--- math/uint.go
+++ math/uint.go
@@ -243,7 +243,7 @@ func checkNewUint(i *big.Int) (Uint, error) {
 func RelativePow(x, n, b Uint) (z Uint) {
 	if x.IsZero() {
 		if n.IsZero() {
-			z = b // 0^0 = 1
+			z = OneUint() // 0^0 = 1
 			return z
 		}
 		z = ZeroUint() // otherwise 0^a = 0

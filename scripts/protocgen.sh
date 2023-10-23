83dc679ea77d997ff9db9ad1687938742ea1d46a
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-10-23 14:34:25 -0700

    updates

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -11,13 +11,13 @@ set -e
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./cosmos ./amino -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./cosmos ./amino -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq | grep -v '^./cosmos/store/')
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
     # gogo proto files SHOULD ONLY be generated if this is false
     # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
-    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$' && [[ ! "$file" =~ ./cosmos/store/* ]]; then
+    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*cosmossdk.io/api' "$file" | grep -q ':0$'; then
       buf generate --template buf.gen.gogo.yaml $file
     fi
   done

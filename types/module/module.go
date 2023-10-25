e4c21081ccae569bb653b3833494210a2ca783ea
Julien Robert <julien@rbrt.fr>
2023-10-25 14:31:37 +0200

    refactor: modules should implement appmodule.AppModule (#18252)
    (cherry picked from commit 797ae0fe99cb3762aee960788627a199e7feb6bb)

--- types/module/module.go
+++ types/module/module.go
@@ -199,6 +199,8 @@ type HasABCIGenesis interface {
 // its functionality has been moved to extension interfaces.
 // Deprecated: use appmodule.AppModule with a combination of extension interfaes interfaces instead.
 type AppModule interface {
+	appmodule.AppModule
+
 	AppModuleBasic
 }
 
@@ -288,6 +290,10 @@ func NewManager(modules ...AppModule) *Manager {
 	modulesStr := make([]string, 0, len(modules))
 	preBlockModulesStr := make([]string, 0)
 	for _, module := range modules {
+		if _, ok := module.(appmodule.AppModule); !ok {
+			panic(fmt.Sprintf("module %s does not implement appmodule.AppModule", module.Name()))
+		}
+
 		moduleMap[module.Name()] = module
 		modulesStr = append(modulesStr, module.Name())
 		if _, ok := module.(appmodule.HasPreBlocker); ok {

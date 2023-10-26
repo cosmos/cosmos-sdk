5086c5f7055abc8960668b534078f8c6dff2ea6a
atheesh <atheesh@vitwit.com>
2023-10-26 18:39:33 +0530

    fix lint

--- x/staking/keeper/msg_server.go
+++ x/staking/keeper/msg_server.go
@@ -11,8 +11,8 @@ import (
 
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
-
 	distrtypes "cosmossdk.io/x/distribution/types"
+
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"

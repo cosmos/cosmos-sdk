187903cd037be58bde0972f054e95ea452120589
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-11-01 14:49:36 -0700

    add doc.go

---
+++ store/internal/doc.go
@@ -0,0 +1,6 @@
+/*
+Package internal contains remnants from the store v1 implementation, with non
+essential components removed. What remains is required for the store v2 to handle
+commitment of state, specifically Merkle proofs of the root hash.
+*/
+package internal

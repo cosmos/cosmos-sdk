fd2accb27949a27c3f3c9d87bffc1688a8146f55
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-11-11 20:07:27 -0300

    updates

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -1,11 +1,12 @@
 package rootmulti
 
-import "cosmossdk.io/store/v2"
+import (
+	"io"
 
-var (
-	_ store.RootStore            = (*Store)(nil)
-	_ store.UpgradeableRootStore = (*Store)(nil)
+	"cosmossdk.io/store/v2"
 )
+
+var _ store.UpgradeableRootStore = (*Store)(nil)
 
 // Store defines a multi-tree implementation variant of a RootStore .It contains
 // a single State Storage (SS) backend and a State Commitment (SC) backend per module,
@@ -13,3 +14,71 @@ var (
 // v1 RootMultiStore and support existing application that DO NOT wish to migrate
 // to the SDK's default single tree RootStore variant.
 type Store struct{}
+
+func (s *Store) GetSCStore(storeKey string) store.Committer {
+	panic("not implemented!")
+}
+
+func (s *Store) MountSCStore(storeKey string, sc store.Committer) error {
+	panic("not implemented!")
+}
+
+func (s *Store) GetKVStore(storeKey string) store.KVStore {
+	panic("not implemented!")
+}
+
+func (s *Store) GetBranchedKVStore(storeKey string) store.BranchedKVStore {
+	panic("not implemented!")
+}
+
+func (s *Store) Query(storeKey string, version uint64, key []byte, prove bool) (store.QueryResult, error) {
+	panic("not implemented!")
+}
+
+func (s *Store) Branch() store.BranchedRootStore {
+	panic("not implemented!")
+}
+
+func (s *Store) SetTracingContext(tc store.TraceContext) {
+	panic("not implemented!")
+}
+
+func (s *Store) SetTracer(w io.Writer) {
+	panic("not implemented!")
+}
+
+func (s *Store) TracingEnabled() bool {
+	panic("not implemented!")
+}
+
+func (s *Store) LoadVersion(version uint64) error {
+	panic("not implemented!")
+}
+
+func (s *Store) LoadLatestVersion() error {
+	panic("not implemented!")
+}
+
+func (s *Store) LoadVersionAndUpgrade(version uint64, upgrades *store.StoreUpgrades) error {
+	panic("not implemented!")
+}
+
+func (s *Store) GetLatestVersion() (uint64, error) {
+	panic("not implemented!")
+}
+
+func (s *Store) SetCommitHeader(h store.CommitHeader) {
+	panic("not implemented!")
+}
+
+func (s *Store) WorkingHash() ([]byte, error) {
+	panic("not implemented!")
+}
+
+func (s *Store) Commit() ([]byte, error) {
+	panic("not implemented!")
+}
+
+func (s *Store) Close() error {
+	panic("not implemented!")
+}

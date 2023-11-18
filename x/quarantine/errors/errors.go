146fa717cdd9e1e0a8ec4c14e3789d1a10fd9ebe
Daniel Wedul <github@wedul.com>
2023-11-17 17:46:06 -0700

    [1752]: Delete the x/quarantine dir.

--- x/quarantine/errors/errors.go
+++
@@ -1,8 +0,0 @@
-package errors
-
-import "cosmossdk.io/errors"
-
-// quarantineCodespace is the codespace for all errors defined in quarantine package
-const quarantineCodespace = "quarantineold"
-
-var ErrInvalidValue = errors.Register(quarantineCodespace, 2, "invalid value")

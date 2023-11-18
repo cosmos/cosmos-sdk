6aada296daeb4b24896607a838881a5b4b48fcd3
Daniel Wedul <github@wedul.com>
2023-11-17 17:50:22 -0700

    [1752]: Ignore the quarantine and sanction protos when generating code.

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -21,7 +21,7 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq | grep -v -e quarantine -e sanction)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then

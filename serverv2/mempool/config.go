7fe5493cdd3e8c1de96d42953a30a704bef2ff2e
marbar3778 <marbar3778@yahoo.com>
2023-11-20 15:55:33 +0100

    add toml template

--- serverv2/mempool/config.go
+++ serverv2/mempool/config.go
@@ -1,5 +1,20 @@
 package mempool
 
+const ConfigTemplateMempool = `
+###############################################################################
+###                         Mempool                                         ###
+###############################################################################
+
+[mempool]
+# Setting max-txs to 0 will allow for a unbounded amount of transactions in the mempool.
+# Setting max_txs to negative 1 (-1) will disable transactions from being inserted into the mempool.
+# Setting max_txs to a positive number (> 0) will limit the number of transactions in the mempool, by the specified amount.
+#
+# Note, this configuration only applies to SDK built-in app-side mempool
+# implementations.
+max-txs = {{ .Mempool.MaxTxs }}
+`
+
 // Config defines the configurations for the SDK built-in app-side mempool
 // implementations.
 type Config struct {

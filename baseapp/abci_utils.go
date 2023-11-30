d52bd920dd80703ce3042bcda8dd11970b92bbf7
leven <112051166+lx-xiang@users.noreply.github.com>
2023-11-30 23:57:24 +0800

    fix(baseapp): Use CometBFT's `ComputeProtoSizeForTxs` in `defaultTxSelector.SelectTxForProposal` (#18551)

--- baseapp/abci_utils.go
+++ baseapp/abci_utils.go
@@ -10,6 +10,7 @@ import (
 	cryptoenc "github.com/cometbft/cometbft/crypto/encoding"
 	cmtprotocrypto "github.com/cometbft/cometbft/proto/tendermint/crypto"
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	cmttypes "github.com/cometbft/cometbft/types"
 	protoio "github.com/cosmos/gogoproto/io"
 	"github.com/cosmos/gogoproto/proto"
 
@@ -368,7 +369,7 @@ func (ts *defaultTxSelector) Clear() {
 }
 
 func (ts *defaultTxSelector) SelectTxForProposal(_ context.Context, maxTxBytes, maxBlockGas uint64, memTx sdk.Tx, txBz []byte) bool {
-	txSize := uint64(len(txBz))
+	txSize := uint64(cmttypes.ComputeProtoSizeForTxs([]cmttypes.Tx{txBz}))
 
 	var txGasLimit uint64
 	if memTx != nil {

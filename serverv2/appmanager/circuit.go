4295cbfbf9f3506c4c52541e0e9af307b5a96fd4
marbar3778 <marbar3778@yahoo.com>
2023-12-06 14:51:27 +0100

    app manager

---
+++ serverv2/appmanager/circuit.go
@@ -0,0 +1,8 @@
+package appmanager
+
+import "context"
+
+// CircuitBreaker is an interface that defines the methods for a circuit breaker.
+type CircuitBreaker interface {
+	IsAllowed(ctx context.Context, typeURL string) (bool, error)
+}

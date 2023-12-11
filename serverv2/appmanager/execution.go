cddc37afc5930a93d477d0812afb84791822a15c
marbar3778 <marbar3778@yahoo.com>
2023-12-11 15:59:45 +0100

    pull out execmsg from exectx

--- serverv2/appmanager/execution.go
+++ serverv2/appmanager/execution.go
@@ -11,6 +11,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/serverv2/core/transaction"
 )
 
+// ExecTx executes a transaction. It returns the result of the transaction execution and an error if the state transition fails.
+// This is meant to be called in three different contexts:
+// 1. DeliverTx: the transaction is executed and the state transition is persisted
+// 2. SimulateTx: the transaction is executed and the state transition is not persisted
+// 3. Prepare/Process: the transaction is executed and the state transition is not persisted, but cached
 func ExecTx[T transaction.Tx](ctx context.Context, logger log.Logger, tx T) (appmanager.TxResult, error) {
 	// gInfo sdk.GasInfo, result *sdk.Result, anteEvents []event.Event,
 
@@ -27,6 +32,9 @@ func ExecTx[T transaction.Tx](ctx context.Context, logger log.Logger, tx T) (app
 
 // ExecMsg executes a single message. It returns the result of the message execution and an error if the state transition fails,
 // this is used for the internal router for modules to communicate with each other
+// This is meant to be called in two different contexts:
+// 1. ExternalMsg: the message being executed is from a transaction
+// 2. InternalMsg: the message being executed is from another module
 func ExecMsg(ctx context.Context, logger log.Logger, msg proto.Message) (appmanager.TxResult, error) {
 	_, err := PreExec(ctx, msg)
 	if err != nil {

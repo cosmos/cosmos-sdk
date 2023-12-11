dbdb7bc4679964a439780d4d80bdb84538073189
marbar3778 <marbar3778@yahoo.com>
2023-12-11 15:57:00 +0100

    pull out execmsg from exectx

--- serverv2/appmanager/execution.go
+++ serverv2/appmanager/execution.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"cosmossdk.io/log"
+	"github.com/cosmos/gogoproto/proto"
 
 	"github.com/cosmos/cosmos-sdk/serverv2/core/appmanager"
 	"github.com/cosmos/cosmos-sdk/serverv2/core/event"
@@ -21,6 +22,32 @@ func ExecTx[T transaction.Tx](ctx context.Context, logger log.Logger, tx T) (app
 
 	// postExecution Hook
 
+	panic("implement me")
+}
+
+// ExecMsg executes a single message. It returns the result of the message execution and an error if the state transition fails,
+// this is used for the internal router for modules to communicate with each other
+func ExecMsg(ctx context.Context, logger log.Logger, msg proto.Message) (appmanager.TxResult, error) {
+	_, err := PreExec(ctx, msg)
+	if err != nil {
+		return appmanager.TxResult{}, err
+	}
+
+	// handler for the message
+
+	_, err = PostExec(ctx, msg)
+	if err != nil {
+		return appmanager.TxResult{}, err
+	}
+
+	panic("implement me")
+}
+
+func PreExec(ctx context.Context, msg proto.Message) (appmanager.TxResult, error) {
+	panic("implement me")
+}
+
+func PostExec(ctx context.Context, msg proto.Message) (appmanager.TxResult, error) {
 	panic("implement me")
 }
 

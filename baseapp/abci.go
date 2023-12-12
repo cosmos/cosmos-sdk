c9e19659dde560ed95753c1bce6bcc92eaf3bf87
Eric <eric.warehime@gmail.com>
2023-12-12 14:00:32 -0800

    Zero gas meter at the end of BeginBlock

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -206,6 +206,11 @@ func (app *BaseApp) BeginBlock(req abci.RequestBeginBlock) (res abci.ResponseBeg
 		}
 	}
 
+	// Reset the gas meter so that the AnteHandlers aren't required to
+	gasMeter = app.getBlockGasMeter(app.deliverState.ctx)
+	app.deliverState.ctx = app.deliverState.ctx.WithBlockGasMeter(gasMeter)
+	app.checkState.ctx = app.checkState.ctx.WithBlockGasMeter(gasMeter)
+
 	return res
 }
 

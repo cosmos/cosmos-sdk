5871d65c5d20093c61caa7ef4a3312c4126f7161
Halimao <1065621723@qq.com>
2023-12-14 10:52:40 +0800

    fix when passphrase input is empty

--- client/keys/add.go
+++ client/keys/add.go
@@ -313,7 +313,7 @@ func runAddCmd(ctx client.Context, cmd *cobra.Command, args []string, inBuf *buf
 
 	// override bip39 passphrase
 	if interactive {
-		bip39Passphrase, err = input.GetPassword(
+		bip39Passphrase, err = input.GetSecretString(
 			"Enter your bip39 passphrase. This is combined with the mnemonic to derive the seed. "+
 				"Most users should just hit enter to use the default, \"\"\n", inBuf)
 		if err != nil {
@@ -322,7 +322,7 @@ func runAddCmd(ctx client.Context, cmd *cobra.Command, args []string, inBuf *buf
 
 		// if they use one, make them re-enter it
 		if len(bip39Passphrase) != 0 {
-			p2, err := input.GetPassword("Repeat the passphrase:\n", inBuf)
+			p2, err := input.GetSecretString("Repeat the passphrase:\n", inBuf)
 			if err != nil {
 				return err
 			}

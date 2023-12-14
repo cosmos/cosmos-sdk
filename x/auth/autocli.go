6968642da94e436666bfe119557a6651759e3f95
Halimao <1065621723@qq.com>
2023-12-14 17:16:00 +0800

    revert import remove

--- x/auth/autocli.go
+++ x/auth/autocli.go
@@ -6,6 +6,7 @@ import (
 	authv1beta1 "cosmossdk.io/api/cosmos/auth/v1beta1"
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
 	_ "cosmossdk.io/api/cosmos/crypto/secp256k1" // register to that it shows up in protoregistry.GlobalTypes
+	_ "cosmossdk.io/api/cosmos/crypto/secp256r1" // register to that it shows up in protoregistry.GlobalTypes
 
 	"github.com/cosmos/cosmos-sdk/version"
 )

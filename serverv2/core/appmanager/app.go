d1b9865db31a072846383f9d205194184fd867eb
Marko <marbar3778@yahoo.com>
2023-12-19 10:46:19 -0800

    refactor: define STF in app manager  (#18763)
    Co-authored-by: unknown unknown <unknown@unknown>

--- serverv2/core/appmanager/app.go
+++ serverv2/core/appmanager/app.go
@@ -4,17 +4,20 @@ import (
 	"context"
 	"time"
 
+	"google.golang.org/protobuf/proto"
+
 	"github.com/cosmos/cosmos-sdk/serverv2/core/event"
 	"github.com/cosmos/cosmos-sdk/serverv2/core/transaction"
-	"github.com/cosmos/cosmos-sdk/serverv2/core/validator"
 )
+
+type Type = proto.Message
 
 type App[T transaction.Tx] interface {
 	ChainID() string
 	AppVersion() (uint64, error)
 
 	InitChain(context.Context, RequestInitChain) (ResponseInitChain, error)
-	DeliverBlock(context.Context, RequestDeliverBlock[T]) (ResponseDeliverBlock, error)
+	DeliverBlock(context.Context, BlockRequest) (BlockResponse, error)
 
 	Query(context.Context, *QueryRequest) (*QueryResponse, error)
 }
@@ -30,38 +33,39 @@ type QueryResponse struct {
 	Value  []byte
 }
 
-type RequestDeliverBlock[T transaction.Tx] struct {
-	Height int64
-	Time   time.Time
-	Hash   []byte
-
-	Txs []T
+type BlockRequest struct {
+	Height            uint64
+	Time              time.Time
+	Hash              []byte
+	Txs               [][]byte
+	ConsensusMessages []Type //
 }
 
-type ResponseDeliverBlock struct {
+type BlockResponse struct {
 	Apphash          []byte
-	ValidatorUpdates []validator.Update
+	ValidatorUpdates []ValidatorUpdate
+	BeginBlockEvents []event.Event
 	TxResults        []TxResult
-	Events           []event.Event
+	EndBlockEvents   []event.Event
 }
 
 type RequestInitChain struct {
 	Time          time.Time
 	ChainId       string
-	Validators    []validator.Update
+	Validators    []ValidatorUpdate
 	AppStateBytes []byte
 	InitialHeight int64
 }
 
 type ResponseInitChain struct {
-	Validators []validator.Update
+	Validators []ValidatorUpdate
 	AppHash    []byte
 }
 
 type TxResult struct {
-	GasWanted int64
-	GasUsed   int64
-	Log       string
-	Data      string
-	Events    event.Event
+	Events  []event.Event
+	GasUsed uint64
+
+	Resp  Type
+	Error error
 }

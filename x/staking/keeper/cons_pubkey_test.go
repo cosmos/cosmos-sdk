c74278304835f271c860b332481f2dd8b7e423d0
atheesh <atheesh@vitwit.com>
2023-12-21 14:50:47 +0530

    fix lint

--- x/staking/keeper/cons_pubkey_test.go
+++ x/staking/keeper/cons_pubkey_test.go
@@ -93,7 +93,7 @@ func (s *KeeperTestSuite) TestConsPubKeyRotationHistory() {
 	s.Require().Len(historyObjects, 1)
 }
 
-func (s *KeeperTestSuite) TestValidatorIdentifier1() {
+func (s *KeeperTestSuite) TestValidatorIdentifier() {
 	stakingKeeper, ctx, accountKeeper, bankKeeper := s.stakingKeeper, s.ctx, s.accountKeeper, s.bankKeeper
 
 	msgServer := stakingkeeper.NewMsgServerImpl(stakingKeeper)
@@ -145,35 +145,11 @@ func (s *KeeperTestSuite) TestValidatorIdentifier1() {
 
 	oldPk2, err := stakingKeeper.ValidatorIdentifier(ctx, sdk.ConsAddress(PKs[494].Address()))
 	s.Require().NoError(err)
-	stakingKeeper.BlockValidatorUpdates(ctx)
+	_, err = stakingKeeper.BlockValidatorUpdates(ctx)
+	s.Require().NoError(err)
 
 	s.Require().Equal(oldPk2.Bytes(), initialConsAddr)
 }
-
-// func (s *KeeperTestSuite) TestValidatorIdentifier() {
-
-// 	stakingKeeper, ctx := s.stakingKeeper, s.ctx
-// 	consAddr1 := sdk.ConsAddress(([]byte("addr1_______________")))
-// 	consAddr2 := sdk.ConsAddress(([]byte("addr2_______________")))
-// 	consAddr3 := sdk.ConsAddress(([]byte("addr3_______________")))
-// 	s.Require().NoError(stakingKeeper.NewToOldConsKeyMap.Set(ctx, consAddr2, consAddr1))
-// 	s.Require().NoError(stakingKeeper.NewToOldConsKeyMap.Set(ctx, consAddr3, consAddr1))
-
-// 	// ValidatorIdentifier returns the same key if there is no key map found
-// 	addr, err := stakingKeeper.ValidatorIdentifier(ctx, consAddr1)
-// 	s.Require().NoError(err)
-// 	s.Require().Equal(consAddr1, addr)
-
-// 	// ValidatorIdentifier should return the consAddr1 here
-// 	addr, err = stakingKeeper.ValidatorIdentifier(ctx, consAddr2)
-// 	s.Require().NoError(err)
-// 	s.Require().Equal(consAddr1, addr)
-
-// 	// ValidatorIdentifier should return the consAddr1 here as well cause it's the original key
-// 	addr, err = stakingKeeper.ValidatorIdentifier(ctx, consAddr3)
-// 	s.Require().NoError(err)
-// 	s.Require().Equal(consAddr1, addr)
-// }
 
 func (s *KeeperTestSuite) setValidators(n int) {
 	stakingKeeper, ctx := s.stakingKeeper, s.ctx

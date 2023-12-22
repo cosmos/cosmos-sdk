971aa3eee95d5250b6ecc94b19256097d414e432
marbar3778 <marbar3778@yahoo.com>
2023-12-22 13:24:54 -0800

    add prepare skeleton

--- server/v2/appmanager/service.go
+++ server/v2/appmanager/service.go
@@ -1,6 +1,7 @@
 package appmanager
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"sync/atomic"
@@ -11,6 +12,17 @@ import (
 	"cosmossdk.io/server/v2/core/store"
 	"cosmossdk.io/server/v2/core/transaction"
 )
+
+type Tx struct {
+	Tx         []byte // transaction bytes
+	Identifier string // transaction Identifier
+}
+
+type TxPool interface {
+	GetTxs(context.Context, uint32) ([]Tx, error)
+}
+
+type PrepareHandler func(ctx context.Context, txs []Tx) ([]Tx, []store.ChangeSet, error)
 
 type AppManagerBuilder[T transaction.Tx] struct {
 	InitGenesis map[string]func(ctx context.Context, moduleGenesisBytes []byte) error
@@ -52,11 +64,42 @@ type AppManager[T transaction.Tx] struct {
 
 	db store.Store
 
+	txpool TxPool
+
 	lastBlockHeight *atomic.Uint64
 
 	initGenesis func(ctx context.Context, genesisBytes []byte) error
 
 	stf *stf.STF[T]
+
+	cachedState         []store.ChangeSet
+	cachedTx            []Tx
+	cachedBlockResponse *appmanager.BlockResponse
+}
+
+// BuildBlock builds a block when requested by consensus. It will take in a list of transactions and return a list of transactions
+func (a AppManager[T]) BuildBlock(ctx context.Context, txs []Tx, totalSize uint32) ([]Tx, error) {
+
+	txs, err := a.txpool.GetTxs(ctx, totalSize)
+	if err != nil {
+		return nil, err
+	}
+
+	// run txs through handler
+	bsr, changeSets, err := a.PrepareBlock(ctx, txs)
+	if err != nil {
+		return nil, err
+	}
+
+	// cache the changes and txs to avoid execution later on
+	if changeSets != nil && bsr != nil {
+		a.cachedState = changeSets
+		a.cachedBlockResponse = bsr
+		a.cachedTx = txs
+		return txs, nil
+	}
+
+	return txs, nil
 }
 
 func (a AppManager[T]) DeliverBlock(ctx context.Context, block appmanager.BlockRequest) (*appmanager.BlockResponse, Hash, error) {
@@ -65,15 +108,41 @@ func (a AppManager[T]) DeliverBlock(ctx context.Context, block appmanager.BlockR
 		return nil, nil, fmt.Errorf("unable to create new state for height %d: %w", block.Height, err)
 	}
 
+	// if we cached values, avoid rexecuting
+	if a.cachedState != nil && a.cachedTx != nil {
+		diff := false
+		for _, txs := range a.cachedTx {
+			// compare txs to make sure they are whats in the cache, if not do normal execution
+			for _, tx := range block.Txs {
+				if !bytes.Equal(txs.Tx, tx) {
+					// if txs dont match break and continue with normal execution
+					// this means that a tx was added to the block which we did not optimistically execute
+					diff = true
+					break
+				}
+			}
+		}
+
+		if !diff {
+			stateRoot, err := a.db.CommitState(a.cachedState)
+			if err != nil {
+				return nil, nil, fmt.Errorf("commit failed: %w", err)
+			}
+			return a.cachedBlockResponse, stateRoot, nil
+		}
+	}
+
 	blockResponse, newState, err := a.stf.DeliverBlock(ctx, block, currentState)
 	if err != nil {
 		return nil, nil, fmt.Errorf("block delivery failed: %w", err)
 	}
+
 	// apply new state to store
 	newStateChanges, err := newState.ChangeSets()
 	if err != nil {
 		return nil, nil, fmt.Errorf("change set: %w", err)
 	}
+
 	stateRoot, err := a.db.CommitState(newStateChanges)
 	if err != nil {
 		return nil, nil, fmt.Errorf("commit failed: %w", err)

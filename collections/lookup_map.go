6342c2dfa0e749bcb5db9b72f79b822a5a841cd8
likhita-809 <likhita@vitwit.com>
2024-01-08 16:09:25 +0530

    wip: attempt 2

--- collections/lookup_map.go
+++ collections/lookup_map.go
@@ -2,127 +2,58 @@ package collections
 
 import (
 	"context"
-	"fmt"
 
 	"cosmossdk.io/collections/codec"
-	"cosmossdk.io/core/store"
 )
 
 // LookupMap represents a map that is not iterable.
-type LookupMap[K, V any] struct {
-	kc codec.KeyCodec[K]
-	vc codec.ValueCodec[V]
-
-	// store accessor
-	sa     func(context.Context) store.KVStore
-	prefix []byte
-	name   string
-}
+type LookupMap[K any] Map[K, NoValue]
 
 // NewLookupMap creates a new LookupMap.
-func NewLookupMap[K, V any](
+func NewLookupMap[K any](
 	schemaBuilder *SchemaBuilder,
 	prefix Prefix,
 	name string,
 	keyCodec codec.KeyCodec[K],
-	valueCodec codec.ValueCodec[V],
-) LookupMap[K, V] {
-	m := LookupMap[K, V]{
-		kc:     keyCodec,
-		vc:     valueCodec,
-		sa:     schemaBuilder.schema.storeAccessor,
-		prefix: prefix.Bytes(),
-		name:   name,
-	}
-	schemaBuilder.addCollection(lookupMapImpl[K, V]{m})
+) LookupMap[K] {
+	m := LookupMap[K](NewMap[K](schemaBuilder, prefix, name, keyCodec, noValueCodec))
 	return m
 }
 
-func (m LookupMap[K, V]) GetName() string {
+// GetName returns the name of the collection.
+func (m LookupMap[K]) GetName() string {
 	return m.name
 }
 
-func (m LookupMap[K, V]) GetPrefix() []byte {
+// GetPrefix returns the prefix of the collection.
+func (m LookupMap[K]) GetPrefix() []byte {
 	return m.prefix
 }
 
-// Set maps the provided value to the provided key in the store.
-// Errors with ErrEncoding if key or value encoding fails.
-func (m LookupMap[K, V]) Set(ctx context.Context, key K, value V) error {
-	bytesKey, err := EncodeKeyWithPrefix(m.prefix, m.kc, key)
-	if err != nil {
-		return err
-	}
-
-	valueBytes, err := m.vc.Encode(value)
-	if err != nil {
-		return fmt.Errorf("%w: value encode: %s", ErrEncoding, err) // TODO: use multi err wrapping in go1.20: https://github.com/golang/go/issues/53435
-	}
-
-	kvStore := m.sa(ctx)
-	return kvStore.Set(bytesKey, valueBytes)
+// Set adds the key to the LookupMap.
+// Errors on encoding problems.
+func (m LookupMap[K]) Set(ctx context.Context, key K) error {
+	// return m.(LookupMap[K]).Set(ctx, key)
+	return (Map[K, NoValue])(m).Set(ctx, key, NoValue{})
 }
 
-// Get returns the value associated with the provided key,
-// errors with ErrNotFound if the key does not exist, or
-// with ErrEncoding if the key or value decoding fails.
-func (m LookupMap[K, V]) Get(ctx context.Context, key K) (v V, err error) {
-	bytesKey, err := EncodeKeyWithPrefix(m.prefix, m.kc, key)
-	if err != nil {
-		return v, err
-	}
-
-	kvStore := m.sa(ctx)
-	valueBytes, err := kvStore.Get(bytesKey)
-	if err != nil {
-		return v, err
-	}
-	if valueBytes == nil {
-		return v, fmt.Errorf("%w: key '%s' of type %s", ErrNotFound, m.kc.Stringify(key), m.vc.ValueType())
-	}
-
-	v, err = m.vc.Decode(valueBytes)
-	if err != nil {
-		return v, fmt.Errorf("%w: value decode: %s", ErrEncoding, err) // TODO: use multi err wrapping in go1.20: https://github.com/golang/go/issues/53435
-	}
-	return v, nil
+// Has returns if the key is present in the LookupMap.
+// An error is returned only in case of encoding problems.
+func (m LookupMap[K]) Has(ctx context.Context, key K) (bool, error) {
+	return (Map[K, NoValue])(m).Has(ctx, key)
 }
 
-// Has reports whether the key is present in storage or not.
-// Errors with ErrEncoding if key encoding fails.
-func (m LookupMap[K, V]) Has(ctx context.Context, key K) (bool, error) {
-	bytesKey, err := EncodeKeyWithPrefix(m.prefix, m.kc, key)
-	if err != nil {
-		return false, err
-	}
-	kvStore := m.sa(ctx)
-	return kvStore.Has(bytesKey)
-}
-
-// Remove removes the key from the storage.
-// Errors with ErrEncoding if key encoding fails.
-// If the key does not exist then this is a no-op.
-func (m LookupMap[K, V]) Remove(ctx context.Context, key K) error {
-	bytesKey, err := EncodeKeyWithPrefix(m.prefix, m.kc, key)
-	if err != nil {
-		return err
-	}
-	kvStore := m.sa(ctx)
-	return kvStore.Delete(bytesKey)
+// Remove removes the key for the LookupMap. An error is returned in case of
+// encoding error, it won't report through the error if the key was
+// removed or not.
+func (m LookupMap[K]) Remove(ctx context.Context, key K) error {
+	return (Map[K, NoValue])(m).Remove(ctx, key)
 }
 
 // KeyCodec returns the Map's KeyCodec.
-func (m LookupMap[K, V]) KeyCodec() codec.KeyCodec[K] { return m.kc }
+func (m LookupMap[K]) KeyCodec() codec.KeyCodec[K] { return (Map[K, NoValue])(m).KeyCodec() }
 
 // ValueCodec returns the Map's ValueCodec.
-func (m LookupMap[K, V]) ValueCodec() codec.ValueCodec[V] { return m.vc }
-
-// // ProveExistence proves the existence of a key in the collection.
-// func (m LookupMap[K, V]) ProveExistence(ctx context.Context, key K) error {
-// 	// Implementation...
-// }
-
-// // ProveNonExistence proves the non-existence of a key in the collection.
-// func (m LookupMap[K, V]) ProveNonExistence(ctx context.Context, key K) error {
-// 	// Implementation...
-// }
+func (m LookupMap[K]) ValueCodec() codec.ValueCodec[NoValue] {
+	return (Map[K, NoValue])(m).ValueCodec()
+}

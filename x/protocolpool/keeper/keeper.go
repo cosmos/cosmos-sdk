483fe82544fce9c8c6f0d9534e1dd1035c08c3f7
likhita-809 <likhita@vitwit.com>
2024-01-11 16:36:46 +0530

    fix lint

--- x/protocolpool/keeper/keeper.go
+++ x/protocolpool/keeper/keeper.go
@@ -42,7 +42,7 @@ type Keeper struct {
 }
 
 func NewKeeper(cdc codec.BinaryCodec, storeService storetypes.KVStoreService,
-	ak types.AccountKeeper, bk types.BankKeeper, sk types.StakingKeeper, authority string, streamMAcc string,
+	ak types.AccountKeeper, bk types.BankKeeper, sk types.StakingKeeper, authority, streamMAcc string,
 ) Keeper {
 	// ensure pool module account is set
 	if addr := ak.GetModuleAddress(types.ModuleName); addr == nil {

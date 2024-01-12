024c0b7ebb0a0c553ee4012317fdf8f82c1d1ccb
atheesh <atheesh@vitwit.com>
2024-01-12 13:06:38 +0530

    add cli tests

--- tests/e2e/staking/suite.go
+++ tests/e2e/staking/suite.go
@@ -3,16 +3,19 @@ package testutil
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 
-	"github.com/cometbft/cometbft/proto/tendermint/crypto"
 	"github.com/cometbft/cometbft/rpc/client/http"
 	"github.com/stretchr/testify/suite"
 
 	"cosmossdk.io/math"
 	banktypes "cosmossdk.io/x/bank/types"
+	"cosmossdk.io/x/staking/client/cli"
 	stakingtypes "cosmossdk.io/x/staking/types"
 
+	"github.com/cometbft/cometbft/proto/tendermint/crypto"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
@@ -120,4 +123,100 @@ func (s *E2ETestSuite) TestBlockResults() {
 
 		return nil
 	}, 10)
+}
+
+func (s *E2ETestSuite) TestConsKeyRotation() {
+	require := s.Require()
+	val := s.network.GetValidators()[0]
+	cmd := cli.NewRotateConsensusKeyCmd()
+
+	validPubkey := `{"@type":"/cosmos.crypto.ed25519.PubKey","key":"oWg2ISpLF405Jcm2vXV+2v4fnjodh6aafuIdeoW+rUw="}`
+	invalidPubkey := `{"@type":"/ed25519.PubKey","key":"wrong"}`
+
+	// Create new account in the keyring.
+	k, _, err := val.GetClientCtx().Keyring.NewMnemonic("NewDelegator", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+	require.NoError(err)
+	pub, err := k.GetPubKey()
+	require.NoError(err)
+	newAddr := sdk.AccAddress(pub.Address())
+
+	msgSend := &banktypes.MsgSend{
+		FromAddress: val.GetAddress().String(),
+		ToAddress:   newAddr.String(),
+		Amount:      sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, math.NewInt(200))),
+	}
+
+	// Send some funds to the new account.
+	_, err = clitestutil.SubmitTestTx(
+		val.GetClientCtx(),
+		msgSend,
+		val.GetAddress(),
+		clitestutil.TestTxConfig{},
+	)
+	require.NoError(err)
+	require.NoError(s.network.WaitForNextBlock())
+
+	testCases := []struct {
+		name      string
+		args      []string
+		expErrMsg string
+		code      uint32
+	}{
+		{
+			"not a validator",
+			[]string{
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, newAddr.String()),
+				fmt.Sprintf("--%s=%s", cli.FlagPubKey, validPubkey),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10)).String()),
+			},
+			"",
+			3,
+		},
+		{
+			"valid pubkey",
+			[]string{
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.GetAddress()),
+				fmt.Sprintf("--%s=%s", cli.FlagPubKey, validPubkey),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10)).String()),
+			},
+			"",
+			0,
+		},
+		{
+			"invalid pubkey",
+			[]string{
+				fmt.Sprintf("--%s=%s", flags.FlagFrom, val.GetAddress()),
+				fmt.Sprintf("--%s=%s", cli.FlagPubKey, invalidPubkey),
+				fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+				fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+				fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10)).String()),
+			},
+			"unable to resolve type URL",
+			0,
+		},
+	}
+
+	clientCtx := val.GetClientCtx()
+	for _, tc := range testCases {
+		tc := tc
+
+		s.Run(tc.name, func() {
+			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)
+			if tc.expErrMsg != "" {
+				require.Error(err)
+				require.Contains(err.Error(), tc.expErrMsg)
+			} else {
+				require.NoError(err, out.String())
+				resp := &sdk.TxResponse{}
+				require.NoError(clientCtx.Codec.UnmarshalJSON(out.Bytes(), resp))
+
+				_, err := clitestutil.GetTxResponse(s.network, val.GetClientCtx(), resp.TxHash)
+				require.NoError(err)
+			}
+		})
+	}
 }

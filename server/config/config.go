8b3db7c8af401b3a6fdf22a7a0c2d6f71d5e58a0
Adam Tucker <adam@osmosis.team>
2024-01-14 14:52:46 -0600

    feat: config option to log gRPC queries (#503)
    * config option to log gRPC queries
    
    * clarify gRPC log comment
    
    * create var

--- server/config/config.go
+++ server/config/config.go
@@ -29,6 +29,10 @@ const (
 	// DefaultGRPCMaxSendMsgSize defines the default gRPC max message size in
 	// bytes the server can send.
 	DefaultGRPCMaxSendMsgSize = math.MaxInt32
+
+	// DefaultLogQueries defines the default value for the log_queries parameter.
+	// Should be set to false unless debugging.
+	DefaultLogQueries = false
 )
 
 // BaseConfig defines the server's basic configuration
@@ -139,6 +143,9 @@ type GRPCConfig struct {
 	// MaxSendMsgSize defines the max message size in bytes the server can send.
 	// The default value is math.MaxInt32.
 	MaxSendMsgSize int `mapstructure:"max-send-msg-size"`
+
+	// LogQueries logs every gRPC query to the console as an info log.
+	LogQueries bool `mapstructure:"log-queries"`
 }
 
 // GRPCWebConfig defines configuration for the gRPC-web server.
@@ -248,6 +255,7 @@ func DefaultConfig() *Config {
 			Address:        DefaultGRPCAddress,
 			MaxRecvMsgSize: DefaultGRPCMaxRecvMsgSize,
 			MaxSendMsgSize: DefaultGRPCMaxSendMsgSize,
+			LogQueries:     DefaultLogQueries,
 		},
 		GRPCWeb: GRPCWebConfig{
 			Enable: true,

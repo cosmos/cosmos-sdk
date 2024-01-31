17d5f73db4abb1cf3a88f37d9ce615479b943352
Dusan Maksimovic <94966669+dusan-maksimovic@users.noreply.github.com>
2024-01-31 12:17:36 +0100

    fix(math): Backport of pull requests #17352 and #18214 to feature/v.0.47.x-ics-lsm branch (#19293)

--- math/uint.go
+++ math/uint.go
@@ -235,7 +235,7 @@ func checkNewUint(i *big.Int) (Uint, error) {
 	if err := UintOverflow(i); err != nil {
 		return Uint{}, err
 	}
-	return Uint{i}, nil
+	return Uint{new(big.Int).Set(i)}, nil
 }
 
 // RelativePow raises x to the power of n, where x (and the result, z) are scaled by factor b

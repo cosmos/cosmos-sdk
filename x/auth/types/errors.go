a5e4320d4c48b828f5089b4fc1e44d7a98b8a155
marbar3778 <marbar3778@yahoo.com>
2024-02-05 17:53:10 +0100

    cleanup

--- x/auth/types/errors.go
+++
@@ -1,8 +0,0 @@
-package types
-
-import "errors"
-
-var (
-	// ErrAccountNotFound defines the error that is thrown when the account is not found
-	ErrAccountNotFound = errors.New("account not found")
-)

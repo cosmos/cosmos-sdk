93666bfc5a397c472a8e86363882c2f95824bae3
Cool Developer <cool199966@outlook.com>
2024-02-20 12:45:17 -0500

    test

--- store/changeset_test.go
+++ store/changeset_test.go
@@ -26,22 +26,33 @@ func TestChangesetMarshal(t *testing.T) {
 			encodedBytes: []byte{0x1, 0x8, 0x73, 0x74, 0x6f, 0x72, 0x65, 0x6b, 0x65, 0x79, 0x1, 0x3, 0x6b, 0x65, 0x79, 0x5, 0x76, 0x61, 0x6c, 0x75, 0x65},
 		},
 		{
-			name:         "two stores",
-			changeset:    &Changeset{Pairs: map[string]KVPairs{"storekey1": {{Key: []byte("key1"), Value: []byte("value1"), StoreKey: "storekey1"}}, "storekey2": {{Key: []byte("key2"), Value: []byte("value2"), StoreKey: "storekey2"}, {Key: []byte("key3"), Value: []byte("value3"), StoreKey: "storekey2"}}}},
-			encodedSize:  1 + 1 + 9 + 1 + 1 + 4 + 1 + 6 + 1 + 9 + 1 + 1 + 4 + 1 + 6 + 1 + 4 + 1 + 6,
-			encodedBytes: []byte{0x2, 0x9, 0x73, 0x74, 0x6f, 0x72, 0x65, 0x6b, 0x65, 0x79, 0x31, 0x1, 0x4, 0x6b, 0x65, 0x79, 0x31, 0x6, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x31, 0x9, 0x73, 0x74, 0x6f, 0x72, 0x65, 0x6b, 0x65, 0x79, 0x32, 0x2, 0x4, 0x6b, 0x65, 0x79, 0x32, 0x6, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x32, 0x4, 0x6b, 0x65, 0x79, 0x33, 0x6, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x33},
+			name:        "two stores",
+			changeset:   &Changeset{Pairs: map[string]KVPairs{"storekey1": {{Key: []byte("key1"), Value: []byte("value1"), StoreKey: "storekey1"}}, "storekey2": {{Key: []byte("key2"), Value: []byte("value2"), StoreKey: "storekey2"}, {Key: []byte("key3"), Value: []byte("value3"), StoreKey: "storekey2"}}}},
+			encodedSize: 1 + 1 + 9 + 1 + 1 + 4 + 1 + 6 + 1 + 9 + 1 + 1 + 4 + 1 + 6 + 1 + 4 + 1 + 6,
+			// encodedBytes: it is not deterministic,
 		},
 	}
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
+			// check the encoded size
 			require.Equal(t, tc.changeset.encodedSize(), tc.encodedSize, "encoded size mismatch")
+			// check the encoded bytes
 			encodedBytes, err := tc.changeset.Marshal()
 			require.NoError(t, err, "marshal error")
-			require.Equal(t, encodedBytes, tc.encodedBytes, "encoded bytes mismatch")
+			if len(tc.encodedBytes) != 0 {
+				require.Equal(t, encodedBytes, tc.encodedBytes, "encoded bytes mismatch")
+			}
+			// check the unmarshaled changeset
 			cs := NewChangeset()
-			require.NoError(t, cs.Unmarshal(tc.encodedBytes), "unmarshal error")
-			require.Equal(t, cs, tc.changeset, "unmarshaled changeset mismatch")
+			require.NoError(t, cs.Unmarshal(encodedBytes), "unmarshal error")
+			require.Equal(t, len(tc.changeset.Pairs), len(cs.Pairs), "unmarshaled changeset store size mismatch")
+			for storeKey, pairs := range tc.changeset.Pairs {
+				require.Equal(t, len(pairs), len(cs.Pairs[storeKey]), "unmarshaled changeset pairs size mismatch")
+				for i, pair := range pairs {
+					require.Equal(t, pair, cs.Pairs[storeKey][i], "unmarshaled changeset pair mismatch")
+				}
+			}
 		})
 	}
 }

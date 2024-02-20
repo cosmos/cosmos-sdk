52106a66cba972676113464eacfd4aa703b1a82d
Marko <marbar3778@yahoo.com>
2024-02-20 16:03:22 +0100

    refactor(feegrant): migrate to use env var (#19450)

--- x/feegrant/module/abci_test.go
+++ x/feegrant/module/abci_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"cosmossdk.io/core/header"
+	"cosmossdk.io/log"
 	sdkmath "cosmossdk.io/math"
 	storetypes "cosmossdk.io/store/types"
 	authtypes "cosmossdk.io/x/auth/types"
@@ -47,7 +48,9 @@ func TestFeegrantPruning(t *testing.T) {
 	ac := address.NewBech32Codec("cosmos")
 	accountKeeper.EXPECT().AddressCodec().Return(ac).AnyTimes()
 
-	feegrantKeeper := keeper.NewKeeper(encCfg.Codec, runtime.NewKVStoreService(key), accountKeeper)
+	env := runtime.NewEnvironment(runtime.NewKVStoreService(key), log.NewNopLogger())
+
+	feegrantKeeper := keeper.NewKeeper(env, encCfg.Codec, accountKeeper)
 
 	err := feegrantKeeper.GrantAllowance(
 		testCtx.Ctx,

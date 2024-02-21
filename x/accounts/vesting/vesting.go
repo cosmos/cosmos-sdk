7b5a93d47eb382dfb269f887e87fe1480d7e9b10
son trinh <trinhleson2000@gmail.com>
2024-02-21 15:30:27 +0700

    minor
    Co-authored-by: Facundo Medica <14063057+facundomedica@users.noreply.github.com>

--- x/accounts/vesting/vesting.go
+++ x/accounts/vesting/vesting.go
@@ -388,11 +388,7 @@ func (bva BaseVesting) IterateCoinEntries(
 	err := entries.Walk(ctx, nil, func(key string, value math.Int) (stop bool, err error) {
 		return cb(key, value)
 	})
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 // LockedCoinsFromVesting returns all the coins that are not spendable (i.e. locked)

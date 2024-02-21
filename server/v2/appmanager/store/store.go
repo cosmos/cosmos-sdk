bf0356baacc084f8a4c7660329843596a72982b8
marbar3778 <marbar3778@yahoo.com>
2024-02-21 18:20:10 +0100

    rename ss to db

--- server/v2/appmanager/store/store.go
+++ server/v2/appmanager/store/store.go
@@ -9,32 +9,32 @@ import (
 
 var _ Store = (*Storage[Database])(nil)
 
-type Storage[SS Database] struct {
-	ss SS
+type Storage[DB Database] struct {
+	ss DB
 
 	// we use latest to keep track of the last height. Reading using atomics is way
-	// cheaper than reading it from SS or SC everytime, it is also implementation
+	// cheaper than reading it from DB or SC everytime, it is also implementation
 	// dependent.
 	latest *atomic.Uint64
 }
 
-func (s Storage[SS]) StateLatest() (uint64, store.ReaderMap, error) {
+func (s Storage[DB]) StateLatest() (uint64, store.ReaderMap, error) {
 	latest := s.latest.Load()
-	return latest, actorsState[SS]{latest, s.ss}, nil
+	return latest, actorsState[DB]{latest, s.ss}, nil
 }
 
-func (s Storage[SS]) StateAt(version uint64) (store.ReaderMap, error) {
-	return actorsState[SS]{version, s.ss}, nil
+func (s Storage[DB]) StateAt(version uint64) (store.ReaderMap, error) {
+	return actorsState[DB]{version, s.ss}, nil
 }
 
-func New[SS Database](ss SS) (Storage[SS], error) {
+func New[DB Database](ss DB) (Storage[DB], error) {
 	// sanity checks.
 	ssVersion, err := ss.GetLatestVersion()
 	if err != nil {
-		return Storage[SS]{}, err
+		return Storage[DB]{}, err
 	}
 
-	s := Storage[SS]{
+	s := Storage[DB]{
 		ss:     ss,
 		latest: new(atomic.Uint64),
 	}
@@ -42,33 +42,33 @@ func New[SS Database](ss SS) (Storage[SS], error) {
 	return s, nil
 }
 
-type actorsState[SS Database] struct {
+type actorsState[DB Database] struct {
 	version uint64
-	ss      SS
+	ss      DB
 }
 
-func (a actorsState[SS]) GetReader(address []byte) (store.Reader, error) {
-	return state[SS]{
+func (a actorsState[DB]) GetReader(address []byte) (store.Reader, error) {
+	return state[DB]{
 		version:  a.version,
 		storeKey: string(address),
 		ss:       a.ss,
 	}, nil
 }
 
-type state[SS Database] struct {
+type state[DB Database] struct {
 	version  uint64
 	storeKey string
-	ss       SS
+	ss       DB
 }
 
-func (s state[SS]) Has(key []byte) (bool, error) { return s.ss.Has(s.storeKey, s.version, key) }
+func (s state[DB]) Has(key []byte) (bool, error) { return s.ss.Has(s.storeKey, s.version, key) }
 
-func (s state[SS]) Get(bytes []byte) ([]byte, error) { return s.ss.Get(s.storeKey, s.version, bytes) }
+func (s state[DB]) Get(bytes []byte) ([]byte, error) { return s.ss.Get(s.storeKey, s.version, bytes) }
 
-func (s state[SS]) Iterator(start, end []byte) (corestore.Iterator, error) {
+func (s state[DB]) Iterator(start, end []byte) (corestore.Iterator, error) {
 	return s.ss.Iterator(s.storeKey, s.version, start, end)
 }
 
-func (s state[SS]) ReverseIterator(start, end []byte) (corestore.Iterator, error) {
+func (s state[DB]) ReverseIterator(start, end []byte) (corestore.Iterator, error) {
 	return s.ss.ReverseIterator(s.storeKey, s.version, start, end)
 }

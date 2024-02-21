61976a8053169f005f33a262bc3a684bf187d31b
marbar3778 <marbar3778@yahoo.com>
2024-02-21 18:39:41 +0100

    rename ss to db

--- server/v2/appmanager/store/store.go
+++ server/v2/appmanager/store/store.go
@@ -10,7 +10,7 @@ import (
 var _ Store = (*Storage[Database])(nil)
 
 type Storage[DB Database] struct {
-	ss DB
+	db DB
 
 	// we use latest to keep track of the last height. Reading using atomics is way
 	// cheaper than reading it from DB or SC everytime, it is also implementation
@@ -20,55 +20,55 @@ type Storage[DB Database] struct {
 
 func (s Storage[DB]) StateLatest() (uint64, store.ReaderMap, error) {
 	latest := s.latest.Load()
-	return latest, actorsState[DB]{latest, s.ss}, nil
+	return latest, actorsState[DB]{latest, s.db}, nil
 }
 
 func (s Storage[DB]) StateAt(version uint64) (store.ReaderMap, error) {
-	return actorsState[DB]{version, s.ss}, nil
+	return actorsState[DB]{version, s.db}, nil
 }
 
-func New[DB Database](ss DB) (Storage[DB], error) {
+func New[DB Database](db DB) (Storage[DB], error) {
 	// sanity checks.
-	ssVersion, err := ss.GetLatestVersion()
+	dbVersion, err := db.GetLatestVersion()
 	if err != nil {
 		return Storage[DB]{}, err
 	}
 
 	s := Storage[DB]{
-		ss:     ss,
+		db:     db,
 		latest: new(atomic.Uint64),
 	}
-	s.latest.Store(ssVersion)
+	s.latest.Store(dbVersion)
 	return s, nil
 }
 
 type actorsState[DB Database] struct {
 	version uint64
-	ss      DB
+	db      DB
 }
 
 func (a actorsState[DB]) GetReader(address []byte) (store.Reader, error) {
 	return state[DB]{
 		version:  a.version,
 		storeKey: string(address),
-		ss:       a.ss,
+		db:       a.db,
 	}, nil
 }
 
 type state[DB Database] struct {
 	version  uint64
 	storeKey string
-	ss       DB
+	db       DB
 }
 
-func (s state[DB]) Has(key []byte) (bool, error) { return s.ss.Has(s.storeKey, s.version, key) }
+func (s state[DB]) Has(key []byte) (bool, error) { return s.db.Has(s.storeKey, s.version, key) }
 
-func (s state[DB]) Get(bytes []byte) ([]byte, error) { return s.ss.Get(s.storeKey, s.version, bytes) }
+func (s state[DB]) Get(bytes []byte) ([]byte, error) { return s.db.Get(s.storeKey, s.version, bytes) }
 
 func (s state[DB]) Iterator(start, end []byte) (corestore.Iterator, error) {
-	return s.ss.Iterator(s.storeKey, s.version, start, end)
+	return s.db.Iterator(s.storeKey, s.version, start, end)
 }
 
 func (s state[DB]) ReverseIterator(start, end []byte) (corestore.Iterator, error) {
-	return s.ss.ReverseIterator(s.storeKey, s.version, start, end)
+	return s.db.ReverseIterator(s.storeKey, s.version, start, end)
 }

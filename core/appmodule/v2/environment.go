cfd426fdb332fcbd96db33da51cc53085e75a989
Julien Robert <julien@rbrt.fr>
2024-03-04 11:25:43 +0100

    feat(runtime): message router service (#19571)

--- core/appmodule/v2/environment.go
+++ core/appmodule/v2/environment.go
@@ -5,18 +5,21 @@ import (
 	"cosmossdk.io/core/event"
 	"cosmossdk.io/core/gas"
 	"cosmossdk.io/core/header"
+	"cosmossdk.io/core/router"
 	"cosmossdk.io/core/store"
 	"cosmossdk.io/log"
 )
 
 // Environment is used to get all services to their respective module
 type Environment struct {
-	BranchService   branch.Service
-	EventService    event.Service
-	GasService      gas.Service
-	HeaderService   header.Service
+	Logger log.Logger
+
+	BranchService branch.Service
+	EventService  event.Service
+	GasService    gas.Service
+	HeaderService header.Service
+	RouterService router.Service
+
 	KVStoreService  store.KVStoreService
 	MemStoreService store.MemoryStoreService
-	DataBaseService store.DatabaseService
-	Logger          log.Logger
 }

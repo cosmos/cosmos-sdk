8c1299176ca8718fe777556b7146341cb9131ca4
Julien Robert <julien@rbrt.fr>
2024-03-04 15:26:18 +0100

    refactor(x/authz)!: use router service (#19637)
    Co-authored-by: Likhita Polavarapu <78951027+likhita-809@users.noreply.github.com>

--- core/router/service.go
+++ core/router/service.go
@@ -16,7 +16,7 @@ type Service interface {
 // Router is the interface that wraps the basic methods for a router.
 type Router interface {
 	// CanInvoke returns an error if the given request cannot be invoked.
-	CanInvoke(ctx context.Context, req protoiface.MessageV1) error
+	CanInvoke(ctx context.Context, typeURL string) error
 	// InvokeTyped execute a message or query. It should be used when the called knows the type of the response.
 	InvokeTyped(ctx context.Context, req, res protoiface.MessageV1) error
 	// InvokeUntyped execute a message or query. It should be used when the called doesn't know the type of the response.

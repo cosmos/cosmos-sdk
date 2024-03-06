5692da35effca068bc428d65fd5c493d089f2c6b
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-03-06 09:04:57 -0800

    updates

---
+++ store/db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+
+	"cosmossdk.io/store/v2"
+)
+
+type RawDBType string
+
+const (
+	DBTypeGoLevelDB RawDBType = "goleveldb"
+	DBTypeRocksDB             = "rocksdb"
+	DBTypePebbleDB            = "pebbledb"
+	DBTypePrefixDB            = "prefixdb"
+)
+
+func NewRawDB(dbType RawDBType, name, dataDir string, opts store.DBOptions) (store.RawDB, error) {
+	switch dbType {
+	case DBTypeGoLevelDB:
+		return NewGoLevelDB(name, dataDir, opts)
+
+	case DBTypeRocksDB:
+		return NewRocksDB(dataDir)
+
+	case DBTypePebbleDB:
+		return NewPebbleDB(dataDir)
+	}
+
+	return nil, fmt.Errorf("unsupported db type: %s", dbType)
+}

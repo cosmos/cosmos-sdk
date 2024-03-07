70d64cb840234e854c04c0697cb8ee22dd6916b5
Julien Robert <julien@rbrt.fr>
2024-03-07 17:05:40 +0100

    build

--- x/auth/signing/adapter_test.go
+++ x/auth/signing/adapter_test.go
@@ -6,11 +6,10 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	authsign "cosmossdk.io/x/auth/signing"
-
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsign "github.com/cosmos/cosmos-sdk/x/auth/signing"
 )
 
 func TestGetSignBytesAdapterNoPublicKey(t *testing.T) {

82bd39bac55ce5a6ab2e07fcc3442de33752abaf
Matt Kocubinski <mkocubinski@gmail.com>
2024-03-13 20:59:50 -0500

    import runtime for side-effects in tests

--- crypto/keys/multisig/multisig_test.go
+++ crypto/keys/multisig/multisig_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/crypto/types/multisig"
+	_ "github.com/cosmos/cosmos-sdk/runtime"
 	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 )

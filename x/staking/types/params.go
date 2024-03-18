4b9c6918c18a64ea34b0aca2ac88e05df480668b
ocnc <anocpublic@gmail.com>
2024-03-18 19:06:04 -0400

    allow zero unbonding time

--- x/staking/types/params.go
+++ x/staking/types/params.go
@@ -128,7 +128,7 @@ func validateUnbondingTime(i interface{}) error {
 		return fmt.Errorf("invalid parameter type: %T", i)
 	}
 
-	if v <= 0 {
+	if v < 0 {
 		return fmt.Errorf("unbonding time must be positive: %d", v)
 	}
 

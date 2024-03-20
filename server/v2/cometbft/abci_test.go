e1fda05f25723e00418cb0daf6608ea12c573a27
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-03-20 16:36:17 +0700

    set up test

---
+++ server/v2/cometbft/abci_test.go
@@ -0,0 +1,114 @@
+package cometbft
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	appmodulev2 "cosmossdk.io/core/appmodule/v2"
+	"cosmossdk.io/core/transaction"
+	am "cosmossdk.io/server/v2/appmanager"
+	ammstore "cosmossdk.io/server/v2/appmanager/store"
+	"cosmossdk.io/server/v2/cometbft/mempool"
+	"cosmossdk.io/server/v2/core/store"
+	"cosmossdk.io/server/v2/stf"
+	"cosmossdk.io/server/v2/stf/branch"
+	"cosmossdk.io/server/v2/stf/mock"
+	"cosmossdk.io/store/v2/storage/pebbledb"
+	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+func TestConsensus(t *testing.T) {
+	mockTx := mock.Tx{
+		Sender:   []byte("sender"),
+		Msg:      wrapperspb.Bool(true), // msg does not matter at all because our handler does nothing.
+		GasLimit: 100_000,
+	}
+
+	s := stf.NewSTF(
+		func(ctx context.Context, msg transaction.Type) (msgResp transaction.Type, err error) {
+			err = kvSet(t, ctx, "exec")
+			return nil, err
+		},
+		func(ctx context.Context, msg transaction.Type) (msgResp transaction.Type, err error) {
+			err = kvSet(t, ctx, "query")
+			return nil, err
+		},
+		func(ctx context.Context, txs []mock.Tx) error { return nil },
+		func(ctx context.Context) error {
+			return kvSet(t, ctx, "begin-block")
+		},
+		func(ctx context.Context) error {
+			return kvSet(t, ctx, "end-block")
+		},
+		func(ctx context.Context, tx mock.Tx) error {
+			return kvSet(t, ctx, "validate")
+		},
+		func(ctx context.Context) ([]appmodulev2.ValidatorUpdate, error) { return nil, nil },
+		func(ctx context.Context, tx mock.Tx, success bool) error {
+			return kvSet(t, ctx, "post-tx-exec")
+		},
+		branch.DefaultNewWriterMap,
+	)
+
+	storageDB, err := pebbledb.New(t.TempDir())
+	require.NoError(t, err)
+	ss, _ := ammstore.New(storageDB)
+
+	b := am.Builder[mock.Tx]{
+		STF:                s,
+		DB:                 ss,
+		ValidateTxGasLimit: 100_000,
+		QueryGasLimit:      100_000,
+		SimulationGasLimit: 100_000,
+	}
+
+	am, err := b.Build()
+	require.NoError(t, err)
+
+	mockStore := NewMockStore()
+
+	c := NewConsensus[mock.Tx](am, mempool.NoOpMempool[mock.Tx]{}, mockStore, Config{})
+
+	c.txCodec = mock.TxCodec{}
+	t.Run("Check tx", func(t *testing.T) {
+		res, err := c.CheckTx(context.Background(), &abci.RequestCheckTx{
+			Tx: mockTx.Bytes(),
+			Type: 0,
+		})
+		fmt.Println("res", res)
+		require.Error(t, err)
+	})
+
+}
+
+var actorName = []byte("cookies")
+
+func kvSet(t *testing.T, ctx context.Context, v string) error {
+	t.Helper()
+	executionCtx, ok := ctx.(*stf.ExecutionContext)
+	require.True(t, ok)
+	state, err := executionCtx.State.GetWriter(actorName)
+	require.NoError(t, err)
+	return state.Set([]byte(v), []byte(v))
+}
+
+func stateHas(t *testing.T, accountState store.ReaderMap, key string) {
+	t.Helper()
+	state, err := accountState.GetReader(actorName)
+	require.NoError(t, err)
+	has, err := state.Has([]byte(key))
+	require.NoError(t, err)
+	require.Truef(t, has, "state did not have key: %s", key)
+}
+
+func stateNotHas(t *testing.T, accountState store.ReaderMap, key string) {
+	t.Helper()
+	state, err := accountState.GetReader(actorName)
+	require.NoError(t, err)
+	has, err := state.Has([]byte(key))
+	require.NoError(t, err)
+	require.Falsef(t, has, "state was not supposed to have key: %s", key)
+}

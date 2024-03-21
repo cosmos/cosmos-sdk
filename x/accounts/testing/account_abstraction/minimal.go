fdb9a8c88d3471fdc5515290405b99e6b7b71f33
testinginprod <frojdi@faulttolerance.net>
2024-03-21 12:46:48 +0100

    gogo

--- x/accounts/testing/account_abstraction/minimal.go
+++ x/accounts/testing/account_abstraction/minimal.go
@@ -11,8 +11,6 @@ import (
 	"cosmossdk.io/x/accounts/accountstd"
 	account_abstractionv1 "cosmossdk.io/x/accounts/interfaces/account_abstraction/v1"
 	rotationv1 "cosmossdk.io/x/accounts/testing/rotation/v1"
-
-	"github.com/cosmos/cosmos-sdk/codec"
 )
 
 var (
@@ -24,7 +22,6 @@ var _ accountstd.Interface = (*MinimalAbstractedAccount)(nil)
 
 func NewMinimalAbstractedAccount(d accountstd.Dependencies) (MinimalAbstractedAccount, error) {
 	return MinimalAbstractedAccount{
-		PubKey:   collections.NewItem(d.SchemaBuilder, PubKeyPrefix, "pubkey", codec.CollValue[secp256k1.PubKey](d.LegacyStateCodec)),
 		Sequence: collections.NewSequence(d.SchemaBuilder, SequencePrefix, "sequence"),
 		Env:      d.Environment,
 	}, nil
@@ -39,7 +36,7 @@ type MinimalAbstractedAccount struct {
 }
 
 func (a MinimalAbstractedAccount) Init(ctx context.Context, msg *rotationv1.MsgInit) (*rotationv1.MsgInitResponse, error) {
-	return nil, a.PubKey.Set(ctx, secp256k1.PubKey{Key: msg.PubKeyBytes})
+	return nil, nil
 }
 
 func (a MinimalAbstractedAccount) RotatePubKey(ctx context.Context, msg *rotationv1.MsgRotatePubKey) (*rotationv1.MsgRotatePubKeyResponse, error) {

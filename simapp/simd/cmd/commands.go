69d1c28ac2312b486478a974a9553d69b7eb5211
Antonio Pitasi <antonio@pitasi.dev>
2024-03-27 17:53:56 +0100

    feat(client): replace `event-query-tx-for` with `wait-tx` (#19870)
    (cherry picked from commit 8488733fdcdfa4e534c7a15c8a377a13141d2fb7)

--- simapp/simd/cmd/commands.go
+++ simapp/simd/cmd/commands.go
@@ -159,7 +159,7 @@ func queryCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(
-		rpc.QueryEventForTxCmd(),
+		rpc.WaitTxCmd(),
 		server.QueryBlockCmd(),
 		authcmd.QueryTxsByEventsCmd(),
 		server.QueryBlocksCmd(),

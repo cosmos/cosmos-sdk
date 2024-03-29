a76e4f886c2c039c95f3710ec484d41a2dd84961
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2024-03-29 15:46:39 +0100

    enhanced telemetry tests

--- telemetry/wrapper_test.go
+++ telemetry/wrapper_test.go
@@ -1,35 +1,54 @@
 package telemetry
 
 import (
+	"sync"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestNow(t *testing.T) {
-	initTelemetry(true)
-
-	currentTime := time.Now()
-	telemetryTime := Now()
-
-	assert.NotEqual(t, time.Time{}, telemetryTime, "Now() should not return zero time when telemetry is enabled")
-	assert.WithinDuration(t, currentTime, telemetryTime, time.Second, "Now() should be close to current time")
-
-	initTelemetry(false)
-
-	telemetryTime = Now()
-	assert.Equal(t, time.Time{}, telemetryTime, "Now() should return zero time when telemetry is disabled")
+// TelemetrySuite is a struct that holds the setup for the telemetry tests.
+// It includes a mutex to ensure that tests that depend on the global state
+// do not run in parallel, which can cause race conditions and unpredictable results.
+type TelemetrySuite struct {
+	suite.Suite
+	mu sync.Mutex
 }
 
-func TestIsTelemetryEnabled(t *testing.T) {
+// SetupTest is called before each test to reset the global state to a known disabled state.
+// This ensures each test starts with the telemetry disabled
+func (suite *TelemetrySuite) SetupTest() {
+	initTelemetry(false)
+}
+
+// TestNow tests the Now function when telemetry is enabled and disabled.
+func (suite *TelemetrySuite) TestNow() {
+	suite.mu.Lock()
+	defer suite.mu.Unlock()
+
 	initTelemetry(true)
-	if !isTelemetryEnabled() {
-		t.Errorf("isTelemetryEnabled() should return true when globalTelemetryEnabled is set to true")
-	}
+	telemetryTime := Now()
+	suite.NotEqual(time.Time{}, telemetryTime, "Now() should not return zero time when telemetry is enabled")
 
 	initTelemetry(false)
-	if isTelemetryEnabled() {
-		t.Errorf("isTelemetryEnabled() should return false when globalTelemetryEnabled is set to false")
-	}
+	telemetryTime = Now()
+	suite.Equal(time.Time{}, telemetryTime, "Now() should return zero time when telemetry is disabled")
+}
+
+// TestIsTelemetryEnabled tests the isTelemetryEnabled function.
+func (suite *TelemetrySuite) TestIsTelemetryEnabled() {
+	suite.mu.Lock()
+	defer suite.mu.Unlock()
+
+	initTelemetry(true)
+	suite.True(isTelemetryEnabled(), "isTelemetryEnabled() should return true when globalTelemetryEnabled is set to true")
+
+	initTelemetry(false)
+	suite.False(isTelemetryEnabled(), "isTelemetryEnabled() should return false when globalTelemetryEnabled is set to false")
+}
+
+// TestTelemetrySuite initiates the test suite.
+func TestTelemetrySuite(t *testing.T) {
+	suite.Run(t, new(TelemetrySuite))
 }

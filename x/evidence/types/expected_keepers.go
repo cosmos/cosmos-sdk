a9da47604182fb2db2a05d82faa231bd3ae46ba5
Julien Robert <julien@rbrt.fr>
2024-04-19 16:45:07 +0200

    refactor(x/evidence): cleanup api

--- x/evidence/types/expected_keepers.go
+++ x/evidence/types/expected_keepers.go
@@ -12,37 +12,28 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-type (
-	// StakingKeeper defines the staking module interface contract needed by the
-	// evidence module.
-	StakingKeeper interface {
-		ConsensusAddressCodec() address.Codec
-		ValidatorByConsAddr(context.Context, sdk.ConsAddress) (sdk.ValidatorI, error)
-	}
+// StakingKeeper defines the staking module interface contract needed by the
+// evidence module.
+type StakingKeeper interface {
+	ConsensusAddressCodec() address.Codec
+	ValidatorByConsAddr(context.Context, sdk.ConsAddress) (sdk.ValidatorI, error)
+}
 
-	// SlashingKeeper defines the slashing module interface contract needed by the
-	// evidence module.
-	SlashingKeeper interface {
-		GetPubkey(context.Context, cryptotypes.Address) (cryptotypes.PubKey, error)
-		IsTombstoned(context.Context, sdk.ConsAddress) bool
-		HasValidatorSigningInfo(context.Context, sdk.ConsAddress) bool
-		Tombstone(context.Context, sdk.ConsAddress) error
-		Slash(context.Context, sdk.ConsAddress, math.LegacyDec, int64, int64) error
-		SlashWithInfractionReason(context.Context, sdk.ConsAddress, math.LegacyDec, int64, int64, st.Infraction) error
-		SlashFractionDoubleSign(context.Context) (math.LegacyDec, error)
-		Jail(context.Context, sdk.ConsAddress) error
-		JailUntil(context.Context, sdk.ConsAddress, time.Time) error
-	}
+// SlashingKeeper defines the slashing module interface contract needed by the
+// evidence module.
+type SlashingKeeper interface {
+	GetPubkey(context.Context, cryptotypes.Address) (cryptotypes.PubKey, error)
+	IsTombstoned(context.Context, sdk.ConsAddress) bool
+	HasValidatorSigningInfo(context.Context, sdk.ConsAddress) bool
+	Tombstone(context.Context, sdk.ConsAddress) error
+	Slash(context.Context, sdk.ConsAddress, math.LegacyDec, int64, int64) error
+	SlashWithInfractionReason(context.Context, sdk.ConsAddress, math.LegacyDec, int64, int64, st.Infraction) error
+	SlashFractionDoubleSign(context.Context) (math.LegacyDec, error)
+	Jail(context.Context, sdk.ConsAddress) error
+	JailUntil(context.Context, sdk.ConsAddress, time.Time) error
+}
 
-	// AccountKeeper define the account keeper interface contracted needed by the evidence module
-	AccountKeeper interface {
-		SetAccount(ctx context.Context, acc sdk.AccountI)
-	}
-
-	// BankKeeper define the account keeper interface contracted needed by the evidence module
-	BankKeeper interface {
-		MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
-		SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-		GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
-	}
-)
+// AccountKeeper define the account keeper interface contracted needed by the evidence module
+type AccountKeeper interface {
+	SetAccount(ctx context.Context, acc sdk.AccountI)
+}

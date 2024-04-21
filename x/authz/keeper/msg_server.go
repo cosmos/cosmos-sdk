fcb9d84edbec2e3bfa5d357bb100dbf569d06e07
Julien Robert <julien@rbrt.fr>
2024-04-21 21:27:56 +0200

    fix(x/authz,x/feegrant): check blocked address (#20102)

--- x/authz/keeper/msg_server.go
+++ x/authz/keeper/msg_server.go
@@ -38,6 +38,10 @@ func (k Keeper) Grant(goCtx context.Context, msg *authz.MsgGrant) (*authz.MsgGra
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	granteeAcc := k.authKeeper.GetAccount(ctx, grantee)
 	if granteeAcc == nil {
+		if k.bankKeeper.BlockedAddr(grantee) {
+			return nil, sdkerrors.ErrUnauthorized.Wrapf("%s is not allowed to receive funds", grantee)
+		}
+
 		granteeAcc = k.authKeeper.NewAccountWithAddress(ctx, grantee)
 		k.authKeeper.SetAccount(ctx, granteeAcc)
 	}

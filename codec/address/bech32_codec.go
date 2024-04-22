36ed3fe264a10a086be93045758edeca42d74adc
Julian Toledano <jtoledanodiaz@gmail.com>
2024-04-22 10:24:15 +0200

    add: address codec cache

--- codec/address/bech32_codec.go
+++ codec/address/bech32_codec.go
@@ -2,7 +2,12 @@ package address
 
 import (
 	"errors"
+	"github.com/cosmos/cosmos-sdk/internal/conv"
 	"strings"
+	"sync"
+	"sync/atomic"
+
+	"github.com/hashicorp/golang-lru/simplelru"
 
 	"cosmossdk.io/core/address"
 	errorsmod "cosmossdk.io/errors"
@@ -12,14 +17,78 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+const (
+	// TODO: ideally sdk.GetBech32PrefixValAddr("") should be used but currently there's a cyclical import.
+	// 	Once globals are deleted the cyclical import won't happen.
+	prefixValAddr  = "valoper"
+	prefixConsAddr = "valcons"
+)
+
+// cache variables
+var (
+	accAddrMu     sync.Mutex
+	accAddrCache  *simplelru.LRU
+	consAddrMu    sync.Mutex
+	consAddrCache *simplelru.LRU
+	valAddrMu     sync.Mutex
+	valAddrCache  *simplelru.LRU
+
+	isCachingEnabled atomic.Bool
+)
+
+func init() {
+	var err error
+	isCachingEnabled.Store(true)
+
+	// in total the cache size is 61k entries. Key is 32 bytes and value is around 50-70 bytes.
+	// That will make around 92 * 61k * 2 (LRU) bytes ~ 11 MB
+	if accAddrCache, err = simplelru.NewLRU(60000, nil); err != nil {
+		panic(err)
+	}
+	if consAddrCache, err = simplelru.NewLRU(500, nil); err != nil {
+		panic(err)
+	}
+	if valAddrCache, err = simplelru.NewLRU(500, nil); err != nil {
+		panic(err)
+	}
+}
+
 type Bech32Codec struct {
 	Bech32Prefix string
 }
 
-var _ address.Codec = &Bech32Codec{}
+type cachedBech32Codec struct {
+	codec Bech32Codec
+	cache *simplelru.LRU
+	mu    *sync.Mutex
+}
+
+var (
+	_ address.Codec = &Bech32Codec{}
+	_ address.Codec = &cachedBech32Codec{}
+)
 
 func NewBech32Codec(prefix string) address.Codec {
-	return Bech32Codec{prefix}
+	ac := Bech32Codec{prefix}
+	if !isCachingEnabled.Load() {
+		return ac
+	}
+
+	lru := accAddrCache
+	mu := &accAddrMu
+	if strings.Contains(prefix, prefixValAddr) {
+		lru = valAddrCache
+		mu = &valAddrMu
+	} else if strings.Contains(prefix, prefixConsAddr) {
+		lru = consAddrCache
+		mu = &consAddrMu
+	}
+
+	return cachedBech32Codec{
+		codec: ac,
+		cache: lru,
+		mu:    mu,
+	}
 }
 
 // StringToBytes encodes text to bytes
@@ -60,4 +129,39 @@ func (bc Bech32Codec) BytesToString(bz []byte) (string, error) {
 	}
 
 	return text, nil
+}
+
+func (cbc cachedBech32Codec) BytesToString(bz []byte) (string, error) {
+	key := conv.UnsafeBytesToStr(bz)
+	cbc.mu.Lock()
+	defer cbc.mu.Unlock()
+
+	if addr, ok := cbc.cache.Get(key); ok {
+		return addr.(string), nil
+	}
+
+	addr, err := cbc.codec.BytesToString(bz)
+	if err != nil {
+		return "", err
+	}
+	cbc.cache.Add(key, addr)
+
+	return addr, nil
+}
+
+func (cbc cachedBech32Codec) StringToBytes(text string) ([]byte, error) {
+	cbc.mu.Lock()
+	defer cbc.mu.Unlock()
+
+	if addr, ok := cbc.cache.Get(text); ok {
+		return addr.([]byte), nil
+	}
+
+	addr, err := cbc.codec.StringToBytes(text)
+	if err != nil {
+		return nil, err
+	}
+	cbc.cache.Add(text, addr)
+
+	return addr, nil
 }
---
+++ codec/address/bech32_codec_test.go
@@ -0,0 +1,61 @@
+package address
+
+import (
+	"github.com/cosmos/cosmos-sdk/internal/conv"
+	"github.com/hashicorp/golang-lru/simplelru"
+	"gotest.tools/v3/assert"
+	"testing"
+)
+
+func TestNewBech32Codec(t *testing.T) {
+	tests := []struct {
+		name    string
+		prefix  string
+		lru     *simplelru.LRU
+		address string
+	}{
+		{
+			name:    "create accounts cached bech32 codec",
+			prefix:  "cosmos",
+			lru:     accAddrCache,
+			address: "cosmos1p8s0p6gqc6c9gt77lgr2qqujz49huhu6a80smx",
+		},
+		{
+			name:    "create validator cached bech32 codec",
+			prefix:  "cosmosvaloper",
+			lru:     valAddrCache,
+			address: "cosmosvaloper1sjllsnramtg3ewxqwwrwjxfgc4n4ef9u2lcnj0",
+		},
+		{
+			name:    "create consensus cached bech32 codec",
+			prefix:  "cosmosvalcons",
+			lru:     consAddrCache,
+			address: "cosmosvalcons1ntk8eualewuprz0gamh8hnvcem2nrcdsgz563h",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.lru.Len(), 0)
+			ac := NewBech32Codec(tt.prefix)
+			cached, ok := ac.(cachedBech32Codec)
+			assert.Assert(t, ok)
+			assert.Equal(t, cached.cache, tt.lru)
+
+			addr, err := ac.StringToBytes(tt.address)
+			assert.NilError(t, err)
+			assert.Equal(t, tt.lru.Len(), 1)
+
+			cachedAddr, ok := tt.lru.Get(tt.address)
+			assert.Assert(t, ok)
+			assert.DeepEqual(t, addr, cachedAddr)
+
+			accAddr, err := ac.BytesToString(addr)
+			assert.NilError(t, err)
+			assert.Equal(t, tt.lru.Len(), 2)
+
+			cachedStrAddr, ok := tt.lru.Get(conv.UnsafeBytesToStr(addr))
+			assert.Assert(t, ok)
+			assert.DeepEqual(t, accAddr, cachedStrAddr)
+		})
+	}
+}

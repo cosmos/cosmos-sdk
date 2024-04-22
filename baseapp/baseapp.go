605b996a970942f62a0dc2bd37c85b0774e46436
marbar3778 <marbar3778@yahoo.com>
2024-04-22 19:24:30 +0200

    remove txs from mempool when antehandler fails in recheck

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -911,6 +911,12 @@ func (app *BaseApp) runTx(mode execMode, txBytes []byte) (gInfo sdk.GasInfo, res
 		gasWanted = ctx.GasMeter().Limit()
 
 		if err != nil {
+			if mode == execModeReCheck {
+				// if the ante handler fails on recheck, we want to remove the tx from the mempool
+				if err := app.mempool.Remove(tx); err != nil {
+					return gInfo, nil, anteEvents, errors.Join(err, err)
+				}
+			}
 			return gInfo, nil, nil, err
 		}
 

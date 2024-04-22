a6ce4f08dd0fb4b72174c505fca87a7d8a2779c6
Reece Williams <31943163+Reecepbcups@users.noreply.github.com>
2024-04-22 13:53:37 -0500

    Update x/circuit/ante/circuit.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- x/circuit/ante/circuit.go
+++ x/circuit/ante/circuit.go
@@ -25,7 +25,7 @@ func NewCircuitBreakerDecorator(ck CircuitBreaker) CircuitBreakerDecorator {
 }
 
 // If you copy this as reference and your app has the authz module enabled, you must either:
-// - recessively check for nested authz.Exec messages in this function.
+// - recursively check for nested authz.Exec messages in this function.
 // - or error early if a nested authz grant is found.
 // The circuit AnteHandler handles this with baseapp's service router: https://github.com/cosmos/cosmos-sdk/issues/18632.
 func (cbd CircuitBreakerDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {

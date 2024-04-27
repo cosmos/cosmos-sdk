e3687b541db49adab74a37c684f4fe84530764ec
Emil Georgiev <emil.georgiev@mail.schwarz>
2024-04-27 07:06:11 +0300

    add test cases for unpack

--- types/tx/msgs_test.go
+++ types/tx/msgs_test.go
@@ -1,12 +1,14 @@
 package tx_test
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	"testing"
 
 	"github.com/cosmos/gogoproto/proto"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/tx"
 )
@@ -38,7 +40,7 @@ func Test_SetMsg(t *testing.T) {
 			}
 
 			expected := mustAny(tc.msg)
-			require.Equal(t, actual, expected)
+			require.Equal(t, expected, actual)
 			require.Nil(t, err)
 		})
 	}
@@ -86,11 +88,105 @@ func Test_SetMsgs(t *testing.T) {
 				expected[i] = a
 			}
 
-			require.Equal(t, actual, expected)
+			require.Equal(t, expected, actual)
 			require.Nil(t, err)
 		})
 	}
 }
+
+func Test_GetMsgs(t *testing.T) {
+	sdkMsgs := []sdk.Msg{&DummyProtoMessage1{}, &DummyProtoMessage2{}}
+	anyMsg, err := tx.SetMsgs(sdkMsgs)
+	require.Nil(t, err)
+
+	cases := []struct {
+		name     string
+		msgs     []*types.Any
+		expected []sdk.Msg
+		expErr   bool
+	}{
+		{
+			name:     "GetMsgs from a nil slice of Any messages",
+			msgs:     nil,
+			expected: []sdk.Msg{},
+			expErr:   false,
+		},
+		{
+			name:     "GetMsgs from empty slice of Any messages",
+			msgs:     []*types.Any{},
+			expected: []sdk.Msg{},
+			expErr:   false,
+		},
+		{
+			name:     "GetMsgs from a slice with valid Any messages",
+			msgs:     anyMsg,
+			expected: sdkMsgs,
+			expErr:   false,
+		},
+		{
+			name:   "GetMsgs from a slice that contains uncashed Any message",
+			msgs:   []*types.Any{{}},
+			expErr: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := tx.GetMsgs(tc.msgs, "dummy")
+			if tc.expErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.Equal(t, tc.expected, actual)
+			require.Nil(t, err)
+		})
+	}
+}
+
+func TestTx_UnpackInterfaces(t *testing.T) {
+	unpacker := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+
+	sdkMsgs := []sdk.Msg{&DummyProtoMessage1{}, &DummyProtoMessage2{}}
+	anyMsg, err := tx.SetMsgs(sdkMsgs)
+	require.Nil(t, err)
+
+	cases := []struct {
+		name   string
+		msgs   []*types.Any
+		expErr bool
+	}{
+		{
+			name:   "Unpack nil slice messages",
+			msgs:   nil,
+			expErr: false,
+		},
+		{
+			name:   "Unpack empty slice of messages",
+			msgs:   []*types.Any{},
+			expErr: false,
+		},
+		{
+			name:   "Unpack valid messages",
+			msgs:   anyMsg,
+			expErr: false,
+		},
+		{
+			name:   "Unpack uncashed message",
+			msgs:   []*types.Any{{TypeUrl: "uncached"}},
+			expErr: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			err = tx.UnpackInterfaces(unpacker, tc.msgs)
+			require.Equal(t, tc.expErr, err != nil)
+		})
+	}
+}
+
+// in get cached value there are an error
 
 func mustAny(msg proto.Message) *types.Any {
 	a, err := types.NewAnyWithValue(msg)

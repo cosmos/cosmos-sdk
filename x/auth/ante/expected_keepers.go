aa7d4aa66becc3aa87e72abdf8804eff20dc7cee
marbar3778 <marbar3778@yahoo.com>
2024-04-30 17:02:53 +0200

    mocks

--- x/auth/ante/expected_keepers.go
+++ x/auth/ante/expected_keepers.go
@@ -30,4 +30,5 @@ type FeegrantKeeper interface {
 
 type ConsensusKeeper interface {
 	Params(context.Context, *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error)
+	GetCometInfo(context.Context, *consensustypes.QueryCometInfoRequest) (*consensustypes.QueryCometInfoResponse, error)
 }

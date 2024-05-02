4d939ad411bff629ace1a12df9ce8205faa664e0
marbar3778 <marbar3778@yahoo.com>
2024-05-02 19:16:49 +0200

    remove deadcode

--- client/grpc/cmtservice/types.go
+++ client/grpc/cmtservice/types.go
@@ -14,20 +14,3 @@ func (req *ABCIQueryRequest) ToABCIRequestQuery() *abci.RequestQuery {
 		Prove:  req.Prove,
 	}
 }
-
-// FromABCIResponseQuery converts an ABCI ResponseQuery type to a gRPC
-// ABCIQueryResponse type.
-func FromABCIResponseQuery(res *abci.ResponseQuery) *ABCIQueryResponse {
-
-	return &ABCIQueryResponse{
-		Code:      res.Code,
-		Log:       res.Log,
-		Info:      res.Info,
-		Index:     res.Index,
-		Key:       res.Key,
-		Value:     res.Value,
-		ProofOps:  res.ProofOps,
-		Height:    res.Height,
-		Codespace: res.Codespace,
-	}
-}

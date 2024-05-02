54ef00993bedf8284475e3c01b896899512c6ca2
Facundo <facundomedica@gmail.com>
2024-05-02 16:27:29 +0200

    add rechecktx test

--- baseapp/baseapp_test.go
+++ baseapp/baseapp_test.go
@@ -32,7 +32,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/mempool"
 )
 
 var (
@@ -81,12 +80,6 @@ func NewBaseAppSuite(t *testing.T, opts ...func(*baseapp.BaseApp)) *BaseAppSuite
 	app.SetParamStore(paramStore{db: dbm.NewMemDB()})
 	app.SetTxDecoder(txConfig.TxDecoder())
 	app.SetTxEncoder(txConfig.TxEncoder())
-	mem := mempool.NewPriorityMempool[int64](mempool.PriorityNonceMempoolConfig[int64]{
-		TxPriority:      mempool.NewDefaultTxPriority(),
-		MaxTx:           0,
-		SignerExtractor: mempool.NewDefaultSignerExtractionAdapter(),
-	})
-	app.SetMempool(mem)
 
 	// mount stores and seal
 	require.Nil(t, app.LoadLatestVersion())

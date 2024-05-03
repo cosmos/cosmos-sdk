1bae25eb339d20c35b2f3ef4572fa9cd38de3303
marbar3778 <marbar3778@yahoo.com>
2024-05-03 11:30:10 +0200

    bring back store v1 to main

---
+++ store/pruning/export_test.go
@@ -0,0 +1,8 @@
+package pruning
+
+var (
+	PruneSnapshotHeightsKey = pruneSnapshotHeightsKey
+
+	Int64SliceToBytes          = int64SliceToBytes
+	LoadPruningSnapshotHeights = loadPruningSnapshotHeights
+)

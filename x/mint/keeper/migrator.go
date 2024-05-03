23dc78527e741cc1139005ae7ee7d321f5bef0da
Likhita Polavarapu <78951027+likhita-809@users.noreply.github.com>
2024-05-03 15:56:18 +0530

    feat(x/mint): Add max supply param  (#19895)
    Co-authored-by: Marko <marko@baricevic.me>
    
    remove cherry-pick commet errors
    
    fix test
    
    fix v3 migrate
    
    simplify the condition
    
    cleanup logic
    
    Problem: minted coin could be negative
    
    Solution:
    - stop minting if total supply already exceeds max supply

--- x/mint/keeper/migrator.go
+++ x/mint/keeper/migrator.go
@@ -1,7 +1,9 @@
 package keeper
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/mint/exported"
+	v3 "github.com/cosmos/cosmos-sdk/x/mint/migrations/v3"
 )
 
 // Migrator is a struct for handling in-place state migrations.
@@ -16,4 +18,10 @@ func NewMigrator(k Keeper, ss exported.Subspace) Migrator {
 		keeper:         k,
 		legacySubspace: ss,
 	}
+}
+
+// Migrate2to3 migrates the x/mint module state from the consensus version 2 to
+// version 3.
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	return v3.Migrate(ctx, m.keeper.storeService.OpenKVStore(ctx), m.keeper.cdc, m.keeper.Params)
 }

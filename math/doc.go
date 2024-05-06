e5e0f6563682ad6d72c5f0f8f8154392aff701d9
samricotta <samanthalricotta@gmail.com>
2024-05-06 11:18:05 +0300

    Update doc.go
    Co-Authored-By: Aaron Craelius <aaronc@users.noreply.github.com>

--- math/doc.go
+++ math/doc.go
@@ -2,5 +2,11 @@
 Package math implements custom Cosmos SDK math types used for arithmetic
 operations. Signed and unsigned integer types utilize Golang's standard library
 big integers types, having a maximum bit length of 256 bits.
+
+This code is based on the implementation found in the Regen Ledger project:
+https://github.com/regen-network/regen-ledger/tree/main/types/math
+
+Co-authors and contributors include:
+Regen Network
 */
 package math

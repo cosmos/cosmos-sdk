7ba9f5865cb20e8a7af7b1baae79e5d2f29eb829
marbar3778 <marbar3778@yahoo.com>
2024-05-07 20:40:21 +0200

    changes from team review

--- server/v2/appmanager/appmanager_builder.go
+++ server/v2/appmanager/appmanager_builder.go
@@ -10,16 +10,24 @@ import (
 	"cosmossdk.io/server/v2/stf"
 )
 
+// Builder is a struct that represents the application builder for managing transactions.
+// It contains various fields and methods for initializing the application and handling transactions.
 type Builder[T transaction.Tx] struct {
-	STF *stf.STF[T]
-	DB  store.Store
-	ValidateTxGasLimit,
-	QueryGasLimit,
+	STF *stf.STF[T] // The state transition function for processing transactions.
+	DB  store.Store // The database for storing application data.
+
+	// Gas limits for validating, querying, and simulating transactions.
+	ValidateTxGasLimit uint64
+	QueryGasLimit      uint64
 	SimulationGasLimit uint64
 
+	// InitGenesis is a function that initializes the application state from a genesis file.
+	// It takes a context, a source reader for the genesis file, and a transaction handler function.
 	InitGenesis func(ctx context.Context, src io.Reader, txHandler func(json.RawMessage) error) error
 }
 
+// Build creates a new instance of AppManager with the provided configuration and returns it.
+// It initializes the AppManager with the given database, export state, import state, initGenesis function, and state transition function.
 func (b Builder[T]) Build() (*AppManager[T], error) {
 	return &AppManager[T]{
 		config: Config{

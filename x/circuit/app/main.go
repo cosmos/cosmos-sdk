c02409744f5749fc86a5f483b2b158d3e040f722
testinginprod <frojdi@faulttolerance.net>
2024-05-08 18:50:17 +0200

    checkpoint

--- x/circuit/app/main.go
+++ x/circuit/app/main.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"context"
+	"time"
+
+	appmanager "cosmossdk.io/core/app"
+)
+
 var app = NewStf()
 
-func main() {
+//go:wasm-module stf
+//export add
+func execute_block() {
+	_, _, _ = app.DeliverBlock(context.Background(), &appmanager.BlockRequest[Tx]{
+		Height:            0,
+		Time:              time.Time{},
+		Hash:              nil,
+		ChainId:           "",
+		AppHash:           nil,
+		Txs:               nil,
+		ConsensusMessages: nil,
+	},
+		nil, // TODO: state
+	)
 }
+
+// main is required for the `wasi` target, even if it isn't used.
+func main() {}

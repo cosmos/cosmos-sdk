cc538f8a1b49acd51a1180f9e005bc68e1ff8708
Facundo <facundomedica@gmail.com>
2024-05-08 16:26:14 +0200

    brb

--- tests/e2e/accounts/multisig/account_test.go
+++ tests/e2e/accounts/multisig/account_test.go
@@ -4,14 +4,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
 	basev1beta1 "cosmossdk.io/api/cosmos/base/v1beta1"
 	"cosmossdk.io/core/header"
 	"cosmossdk.io/math"
-	multisigaccount "cosmossdk.io/x/accounts/defaults/multisig"
 	v1 "cosmossdk.io/x/accounts/defaults/multisig/v1"
 	accountsv1 "cosmossdk.io/x/accounts/v1"
 
@@ -28,41 +26,22 @@ func TestE2ETestSuite(t *testing.T) {
 // then adds 2 more members and changes the config to require 2/3 majority (also through a proposal).
 // Finally it creates a proposal that won't pass.
 func (s *E2ETestSuite) TestFullMultisig() {
-	t := s.T()
-	app := setupApp(t)
-	currentTime := time.Now()
-	ctx := sdk.NewContext(app.CommitMultiStore(), false, app.Logger()).WithHeaderInfo(header.Info{
-		Time: currentTime,
+	ctx := sdk.NewContext(s.app.CommitMultiStore(), false, s.app.Logger()).WithHeaderInfo(header.Info{
+		Time: time.Now(),
 	})
-	member0, err := app.AuthKeeper.AddressCodec().BytesToString(members[0])
-	require.NoError(t, err)
-	member1, err := app.AuthKeeper.AddressCodec().BytesToString(members[1])
-	require.NoError(t, err)
-	member2, err := app.AuthKeeper.AddressCodec().BytesToString(members[2])
-	require.NoError(t, err)
 
-	s.fundAccount(app, ctx, members[0], sdk.Coins{sdk.NewCoin("stake", math.NewInt(1000000))})
+	s.fundAccount(ctx, s.members[0], sdk.Coins{sdk.NewCoin("stake", math.NewInt(1000000))})
 	randAcc := sdk.AccAddress(secp256k1.GenPrivKey().PubKey().Address())
+	addr, err := s.app.AuthKeeper.AddressCodec().BytesToString(randAcc)
+	s.NoError(err)
 
-	_, accountAddr, err := app.AccountsKeeper.Init(ctx, multisigaccount.MULTISIG_ACCOUNT, members[0], &v1.MsgInit{
-		Members: []*v1.Member{{Address: member0, Weight: 100}},
-		Config: &v1.Config{
-			Threshold:      100,
-			Quorum:         100,
-			VotingPeriod:   120,
-			Revote:         false,
-			EarlyExecution: true,
-		},
-	}, sdk.Coins{sdk.NewCoin("stake", math.NewInt(1000))})
-	require.NoError(t, err)
+	initialMembers := map[string]uint64{
+		s.membersAddr[0]: 100,
+	}
+	accountAddr, accAddrStr := s.initAccount(ctx, s.members[0], initialMembers)
 
-	addr, err := app.AuthKeeper.AddressCodec().BytesToString(randAcc)
-	require.NoError(t, err)
-
-	accAddrStr := sdk.AccAddress(accountAddr).String()
-
-	balance := app.BankKeeper.GetBalance(ctx, randAcc, "stake")
-	require.True(t, balance.Amount.Equal(math.NewInt(0)))
+	balance := s.app.BankKeeper.GetBalance(ctx, randAcc, "stake")
+	s.Equal(math.NewInt(0), balance.Amount)
 
 	// do a simple bank send
 	msg := &bankv1beta1.MsgSend{
@@ -76,7 +55,7 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		},
 	}
 	anyMsg, err := codectypes.NewAnyWithValue(msg)
-	require.NoError(t, err)
+	s.NoError(err)
 
 	propReq := &v1.MsgCreateProposal{
 		Proposal: &v1.Proposal{
@@ -85,8 +64,8 @@ func (s *E2ETestSuite) TestFullMultisig() {
 			Messages: []*codectypes.Any{anyMsg},
 		},
 	}
-	err = s.executeTx(ctx, propReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, propReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	// now we vote for it
 	voteReq := &v1.MsgVote{
@@ -94,60 +73,60 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		Vote:       v1.VoteOption_VOTE_OPTION_YES,
 	}
 
-	err = s.executeTx(ctx, voteReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, voteReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	// now we execute it
 	execReq := &v1.MsgExecuteProposal{
 		ProposalId: 0,
 	}
 
-	err = s.executeTx(ctx, execReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, execReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	for _, v := range ctx.EventManager().Events() {
 		if v.Type == "proposal_tally" {
 			status, found := v.GetAttribute("status")
-			require.True(t, found)
-			require.Equal(t, v1.ProposalStatus_PROPOSAL_STATUS_PASSED.String(), status.Value)
+			s.True(found)
+			s.Equal(v1.ProposalStatus_PROPOSAL_STATUS_PASSED.String(), status.Value)
 
 			yesVotes, found := v.GetAttribute("yes_votes")
-			require.True(t, found)
-			require.Equal(t, "100", yesVotes.Value)
+			s.True(found)
+			s.Equal("100", yesVotes.Value)
 
 			noVotes, found := v.GetAttribute("no_votes")
-			require.True(t, found)
-			require.Equal(t, "0", noVotes.Value)
+			s.True(found)
+			s.Equal("0", noVotes.Value)
 
 			propID, found := v.GetAttribute("proposal_id")
-			require.True(t, found)
-			require.Equal(t, "0", propID.Value)
+			s.True(found)
+			s.Equal("0", propID.Value)
 
 			execErr, found := v.GetAttribute("exec_err")
-			require.True(t, found)
-			require.Equal(t, "<nil>", execErr.Value)
+			s.True(found)
+			s.Equal("<nil>", execErr.Value)
 
 			rejectErr, found := v.GetAttribute("reject_err")
-			require.True(t, found)
-			require.Equal(t, "<nil>", rejectErr.Value)
+			s.True(found)
+			s.Equal("<nil>", rejectErr.Value)
 		}
 	}
-	balance = app.BankKeeper.GetBalance(ctx, randAcc, "stake")
-	require.Equal(t, int64(100), balance.Amount.Int64())
+	balance = s.app.BankKeeper.GetBalance(ctx, randAcc, "stake")
+	s.Equal(int64(100), balance.Amount.Int64())
 
-	err = s.executeTx(ctx, execReq, app, accountAddr, members[0])
-	require.Error(t, err)
+	err = s.executeTx(ctx, execReq, accountAddr, s.members[0])
+	s.Error(err)
 
 	// Add 2 members and pass the proposal
 	// create proposal
 	updateMsg := &v1.MsgUpdateConfig{
 		UpdateMembers: []*v1.Member{
 			{
-				Address: member1,
+				Address: s.membersAddr[1],
 				Weight:  100,
 			},
 			{
-				Address: member2,
+				Address: s.membersAddr[2],
 				Weight:  100,
 			},
 		},
@@ -175,8 +154,8 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		},
 	}
 
-	err = s.executeTx(ctx, proposal, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, proposal, accountAddr, s.members[0])
+	s.NoError(err)
 
 	// vote
 	voteReq = &v1.MsgVote{
@@ -184,28 +163,28 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		Vote:       v1.VoteOption_VOTE_OPTION_YES,
 	}
 
-	err = s.executeTx(ctx, voteReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, voteReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	execReq = &v1.MsgExecuteProposal{
 		ProposalId: 1,
 	}
-	err = s.executeTx(ctx, execReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, execReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	// get members
-	res, err := s.queryAcc(ctx, &v1.QueryConfig{}, app, accountAddr)
-	require.NoError(t, err)
+	res, err := s.queryAcc(ctx, &v1.QueryConfig{}, accountAddr)
+	s.NoError(err)
 	resp := res.(*v1.QueryConfigResponse)
-	require.Len(t, resp.Members, 3)
-	require.Equal(t, int64(200), resp.Config.Threshold)
+	s.Len(resp.Members, 3)
+	s.Equal(int64(200), resp.Config.Threshold)
 
 	// Try to remove a member, but it doesn't reach passing threshold
 	// create proposal
 	updateMsg = &v1.MsgUpdateConfig{
 		UpdateMembers: []*v1.Member{
 			{
-				Address: member1,
+				Address: s.membersAddr[1],
 				Weight:  0,
 			},
 		},
@@ -233,8 +212,8 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		},
 	}
 
-	err = s.executeTx(ctx, proposal, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, proposal, accountAddr, s.members[0])
+	s.NoError(err)
 
 	// vote
 	voteReq = &v1.MsgVote{
@@ -242,8 +221,8 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		Vote:       v1.VoteOption_VOTE_OPTION_NO,
 	}
 
-	err = s.executeTx(ctx, voteReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, voteReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	execReq = &v1.MsgExecuteProposal{
 		ProposalId: 2,
@@ -251,8 +230,8 @@ func (s *E2ETestSuite) TestFullMultisig() {
 
 	// need to wait until voting period is over because we disabled early execution on the last
 	// config update
-	err = s.executeTx(ctx, execReq, app, accountAddr, members[0])
-	require.ErrorContains(t, err, "voting period has not ended yet, and early execution is not enabled")
+	err = s.executeTx(ctx, execReq, accountAddr, s.members[0])
+	s.ErrorContains(err, "voting period has not ended yet, and early execution is not enabled")
 
 	// vote with member 1
 	voteReq = &v1.MsgVote{
@@ -260,48 +239,48 @@ func (s *E2ETestSuite) TestFullMultisig() {
 		Vote:       v1.VoteOption_VOTE_OPTION_NO,
 	}
 
-	err = s.executeTx(ctx, voteReq, app, accountAddr, members[1])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, voteReq, accountAddr, s.members[1])
+	s.NoError(err)
 
 	// need to wait until voting period is over because we disabled early execution on the last
 	// config update
-	err = s.executeTx(ctx, execReq, app, accountAddr, members[0])
-	require.ErrorContains(t, err, "voting period has not ended yet, and early execution is not enabled")
+	err = s.executeTx(ctx, execReq, accountAddr, s.members[0])
+	s.ErrorContains(err, "voting period has not ended yet, and early execution is not enabled")
 
 	headerInfo := ctx.HeaderInfo()
 	headerInfo.Time = headerInfo.Time.Add(time.Second * 121)
 	ctx = ctx.WithHeaderInfo(headerInfo)
 
 	// now it should work, but the proposal will fail
-	err = s.executeTx(ctx, execReq, app, accountAddr, members[0])
-	require.NoError(t, err)
+	err = s.executeTx(ctx, execReq, accountAddr, s.members[0])
+	s.NoError(err)
 
 	for _, v := range ctx.EventManager().Events() {
 		if v.Type == "proposal_tally" {
 			propID, found := v.GetAttribute("proposal_id")
-			require.True(t, found)
+			s.True(found)
 
 			if propID.Value == "2" {
 				status, found := v.GetAttribute("status")
-				require.True(t, found)
-				require.Equal(t, v1.ProposalStatus_PROPOSAL_STATUS_REJECTED.String(), status.Value)
+				s.True(found)
+				s.Equal(v1.ProposalStatus_PROPOSAL_STATUS_REJECTED.String(), status.Value)
 
 				// exec_err is nil because the proposal didn't execute
 				execErr, found := v.GetAttribute("exec_err")
-				require.True(t, found)
-				require.Equal(t, "<nil>", execErr.Value)
+				s.True(found)
+				s.Equal("<nil>", execErr.Value)
 
 				rejectErr, found := v.GetAttribute("reject_err")
-				require.True(t, found)
-				require.Equal(t, "threshold not reached", rejectErr.Value)
+				s.True(found)
+				s.Equal("threshold not reached", rejectErr.Value)
 			}
 		}
 	}
 
 	// get members
-	res, err = s.queryAcc(ctx, &v1.QueryConfig{}, app, accountAddr)
-	require.NoError(t, err)
+	res, err = s.queryAcc(ctx, &v1.QueryConfig{}, accountAddr)
+	s.NoError(err)
 	resp = res.(*v1.QueryConfigResponse)
-	require.Len(t, resp.Members, 3)
-	require.Equal(t, int64(200), resp.Config.Threshold)
+	s.Len(resp.Members, 3)
+	s.Equal(int64(200), resp.Config.Threshold)
 }

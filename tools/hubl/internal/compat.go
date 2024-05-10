d744a9a6128dde502f032a810f69fa240aca1fae
kien6034 <trinhtrungkientb99@gmail.com>
2024-05-10 18:34:02 +0700

    fix: typo

--- tools/hubl/internal/compat.go
+++ tools/hubl/internal/compat.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -15,7 +16,6 @@ import (
 
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
 	reflectionv1beta1 "cosmossdk.io/api/cosmos/base/reflection/v1beta1"
-	"cosmossdk.io/errors"
 )
 
 // loadFileDescriptorsGRPCReflection attempts to load the file descriptor set using gRPC reflection when cosmos.reflection.v1
@@ -48,7 +48,7 @@ func loadFileDescriptorsGRPCReflection(ctx context.Context, client *grpc.ClientC
 	go func() {
 		for {
 			in, err := reflectClient.Recv()
-			if errors.IsOf(err, io.EOF) {
+			if errors.Is(err, io.EOF) {
 				// read done.
 				close(waitc)
 				return
@@ -173,7 +173,7 @@ func addMissingFileDescriptors(ctx context.Context, client *grpc.ClientConn, fdM
 	go func() {
 		for {
 			in, err := reflectClient.Recv()
-			if errors.IsOf(err, io.EOF) {
+			if errors.Is(err, io.EOF) {
 				// read done.
 				close(waitc)
 				return

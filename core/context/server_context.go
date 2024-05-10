f8a7bca7538e4c1840bb8a9caf5df976f161406a
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-05-10 18:20:37 +0700

    clean core/context

--- core/context/server_context.go
+++ core/context/server_context.go
@@ -1,26 +1,6 @@
 package context
 
-import (
-	cmtcfg "github.com/cometbft/cometbft/config"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-
-	"cosmossdk.io/log"
-)
-
-const ServerContextKey ContextKey = "server.context"
-
-type ServerContext interface {
-	GetLogger() log.Logger
-	GetViper() *viper.Viper
-	GetConfig() *cmtcfg.Config
-	SetRoot(string)
-}
-
-func GetServerContextFromCmd(cmd *cobra.Command) ServerContext {
-	if v := cmd.Context().Value(ServerContextKey); v != nil {
-		serverCtxPtr := v.(ServerContext)
-		return serverCtxPtr
-	}
-	return nil
-}
+var (
+	LoggerContextKey = struct{}{}
+	ViperContextKey = struct{}{}
+) 

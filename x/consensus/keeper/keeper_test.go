40b2dff8b5d59d17e3537fb2bb7e5e474d20d67d
Sergio Mena <sergio@informal.systems>
2024-05-10 16:16:56 +0200

    Re-fixed lint

--- x/consensus/keeper/keeper_test.go
+++ x/consensus/keeper/keeper_test.go
@@ -127,7 +127,7 @@ func (s *KeeperTestSuite) TestGRPCQueryConsensusParams() {
 					Validator: modifiedConsensusParams.Validator,
 					Evidence:  modifiedConsensusParams.Evidence,
 					Synchrony: modifiedConsensusParams.Synchrony,
-					Abci: &cmtproto.ABCIParams{ //nolint: staticcheck
+					Abci: &cmtproto.ABCIParams{ //nolint: staticcheck // testing backwards compatibility
 						VoteExtensionsEnableHeight: 1234,
 					},
 				}
@@ -313,7 +313,7 @@ func (s *KeeperTestSuite) TestUpdateParams() {
 				Block:     defaultConsensusParams.Block,
 				Validator: defaultConsensusParams.Validator,
 				Evidence:  defaultConsensusParams.Evidence,
-				Abci: &cmtproto.ABCIParams{ //nolint: staticcheck
+				Abci: &cmtproto.ABCIParams{ //nolint: staticcheck // testing backwards compatibility
 					VoteExtensionsEnableHeight: 90,
 				},
 			},
@@ -327,7 +327,7 @@ func (s *KeeperTestSuite) TestUpdateParams() {
 				Block:     defaultConsensusParams.Block,
 				Validator: defaultConsensusParams.Validator,
 				Evidence:  defaultConsensusParams.Evidence,
-				Abci: &cmtproto.ABCIParams{ //nolint: staticcheck
+				Abci: &cmtproto.ABCIParams{ //nolint: staticcheck // testing backwards compatibility
 					VoteExtensionsEnableHeight: 3000,
 				},
 				Feature: &cmtproto.FeatureParams{

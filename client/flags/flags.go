0a16ffaa2a82d756dee8873bd34e12d3d8294642
Albert Chon <albert@injectivelabs.org>
2024-05-13 16:30:27 +0100

    chore: update DefaultKeyringBackend to file

--- client/flags/flags.go
+++ client/flags/flags.go
@@ -19,7 +19,7 @@ const (
 	GasFlagAuto          = "auto"
 
 	// DefaultKeyringBackend
-	DefaultKeyringBackend = keyring.BackendOS
+	DefaultKeyringBackend = keyring.BackendFile
 
 	// BroadcastSync defines a tx broadcasting mode where the client waits for
 	// a CheckTx execution response only.

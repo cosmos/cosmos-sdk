68c18080a29974ee4816202ab2c368149eab671d
marbar3778 <marbar3778@yahoo.com>
2024-05-15 12:35:01 +0200

    linting

--- baseapp/abci_utils.go
+++ baseapp/abci_utils.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"slices"
+
 	"github.com/cockroachdb/errors"
 	abci "github.com/cometbft/cometbft/abci/types"
 	cryptoenc "github.com/cometbft/cometbft/crypto/encoding"
@@ -11,7 +13,6 @@ import (
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	protoio "github.com/cosmos/gogoproto/io"
 	"github.com/cosmos/gogoproto/proto"
-	"slices"
 
 	"cosmossdk.io/core/comet"
 

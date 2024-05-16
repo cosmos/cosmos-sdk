53925ef5fd2e840a1a12043688dad8367c2c95b9
Aaron Craelius <aaronc@users.noreply.github.com>
2024-05-16 05:55:08 -0400

    refactor!: turn MsgsV2 into ReflectMessages to make it less confusing (#19839)
    Co-authored-by: marbar3778 <marbar3778@yahoo.com>
    Co-authored-by: sontrinh16 <trinhleson2000@gmail.com>
    Co-authored-by: Marko <marko@baricevic.me>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- types/tx_msg.go
+++ types/tx_msg.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	protov2 "google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 
 	coretransaction "cosmossdk.io/core/transaction"
 
@@ -53,8 +53,13 @@ type (
 	Tx interface {
 		HasMsgs
 
-		// GetMsgsV2 gets the transaction's messages as google.golang.org/protobuf/proto.Message's.
-		GetMsgsV2() ([]protov2.Message, error)
+		// GetReflectMessages gets a reflected version of the transaction's messages
+		// that can be used by dynamic APIs. These messages should not be used for actual
+		// processing as they cannot be guaranteed to be what handlers are expecting, but
+		// they can be used for dynamically reading specific fields from the message such
+		// as signers or validation data. Message processors should ALWAYS use the messages
+		// returned by GetMsgs.
+		GetReflectMessages() ([]protoreflect.Message, error)
 	}
 
 	// FeeTx defines the interface to be implemented by Tx to use the FeeDecorators

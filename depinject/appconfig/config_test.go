4fcd3621b0f4918f2e231328c46a18cc30f1db15
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-05-27 19:25:45 +0700

    go mod tidy & tests

--- depinject/appconfig/config_test.go
+++ depinject/appconfig/config_test.go
@@ -35,7 +35,7 @@ modules:
   config:
     "@type": testpb.ModuleFoo
 `))
-	expectContainerErrorContains(t, opt, `unable to resolve`)
+	expectContainerErrorContains(t, opt, `unknown message type`)
 
 	opt = appconfig.LoadYAML([]byte(`
 modules:
@@ -132,15 +132,15 @@ func TestComposeGogoTypes(t *testing.T) {
 modules:
 - name: a
   config:
-    "@type": testpb.ModuleFoo
+    "@type": type.googleapis.com/testpb.ModuleFoo
 `))
-	expectContainerErrorContains(t, opt, `unable to resolve`)
+	expectContainerErrorContains(t, opt, `unknown message type`)
 
 	opt = appconfig.LoadYAML([]byte(`
 modules:
 - name: a
   config:
-    "@type": cosmos.app.v1alpha1.Config # this is not actually a module config type!
+    "@type": type.googleapis.com/cosmos.app.v1alpha1.Config # this is not actually a module config type!
 `))
 	expectContainerErrorContains(t, opt, "does not have the option cosmos.app.v1alpha1.module")
 	expectContainerErrorContains(t, opt, "registered modules are")
@@ -150,7 +150,7 @@ modules:
 modules:
 - name: a
   config:
-    "@type": testpb.TestGogoUnregisteredModule
+    "@type": type.googleapis.com/testpb.TestGogoUnregisteredModule
 `))
 	expectContainerErrorContains(t, opt, "did you forget to import cosmossdk.io/core/internal/testpb")
 	expectContainerErrorContains(t, opt, "registered modules are")
@@ -161,13 +161,13 @@ modules:
 modules:
 - name: runtime
   config:
-   "@type": testpb.TestGogoRuntimeModule
+   "@type": type.googleapis.com/testpb.TestGogoRuntimeModule
 - name: a
   config:
-   "@type": testpb.TestGogoModuleA
+   "@type": type.googleapis.com/testpb.TestGogoModuleA
 - name: b
   config:
-   "@type": testpb.TestGogoModuleB
+   "@type": type.googleapis.com/testpb.TestGogoModuleB
 `))
 	assert.NilError(t, depinject.Inject(opt, &app))
 	buf := &bytes.Buffer{}
@@ -190,7 +190,7 @@ golang_bindings:
 modules:
   - name: a
     config:
-      "@type": testpb.TestGogoModuleA
+      "@type": type.googleapis.com/testpb.TestGogoModuleA
     golang_bindings:
       - interfaceType: interfaceType/package.name 
         implementation: implementationType/package.name
@@ -205,7 +205,7 @@ modules:
 modules:
 - name: a
   config:
-   "@type": testpb.TestNoGoImportModule
+   "@type": type.googleapis.com/testpb.TestNoGoImportModule
 `))
 	expectContainerErrorContains(t, opt, "module should have the option cosmos.app.v1alpha1.module")
 
@@ -215,7 +215,7 @@ modules:
 modules:
 - name: a
   config:
-   "@type": testpb.TestNoGoImportModule
+   "@type": type.googleapis.com/testpb.TestNoGoImportModule
 `))
 	expectContainerErrorContains(t, opt, "module should have ModuleDescriptor.go_import specified")
 }
--- depinject/go.mod
+++ depinject/go.mod
@@ -9,21 +9,24 @@ require (
 	github.com/cosmos/cosmos-proto v1.0.0-beta.5
 	github.com/cosmos/gogoproto v1.4.12
 	github.com/stretchr/testify v1.9.0
-	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
+	golang.org/x/exp v0.0.0-20240404231335-c0f41cb1a7a0
 	google.golang.org/protobuf v1.34.1
 	gotest.tools/v3 v3.5.1
 	sigs.k8s.io/yaml v1.4.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240513163218-0867130af1f8 // indirect
 	google.golang.org/grpc v1.64.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 

866da86fa4237aed7d09869726fbd26db9be7ebf
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-05-27 19:23:33 +0700

    helper

---
+++ api/cosmos/app/v1alpha1/types.go
@@ -0,0 +1,28 @@
+package appv1alpha1
+
+import "fmt"
+
+func (x *ModuleConfig) GetTypeUrl() (string, error) {
+	if x != nil {
+		if x.Config != nil {
+			return x.Config.TypeUrl, nil
+		} else {
+			return "", fmt.Errorf("module %q is missing a config object", x.Name)
+		}
+	}
+	return "", fmt.Errorf("Module is nil")
+}
+
+func (x *ModuleConfig) GetGolangBindingsStrings() ([]string, []string) {
+	if x != nil {
+		interfaceTypes := make([]string, len(x.GolangBindings))
+		imple := make([]string, len(x.GolangBindings))
+
+		for i, binding := range x.GolangBindings {
+			interfaceTypes[i] = binding.InterfaceType
+			imple[i] = binding.Implementation
+		}
+		return interfaceTypes, imple
+	}
+	return nil, nil
+}

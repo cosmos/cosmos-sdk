442dae0822b40e804930af382399ecdc8076f95a
sontrinh16 <trinhleson2000@gmail.com>
2024-06-17 16:34:24 +0700

    add docs

--- x/authz/keeper/msg_server.go
+++ x/authz/keeper/msg_server.go
@@ -43,6 +43,9 @@ func (k Keeper) Grant(ctx context.Context, msg *authz.MsgGrant) (*authz.MsgGrant
 	if err := k.MsgRouterService.CanInvoke(ctx, t); err != nil {
 		return nil, sdkerrors.ErrInvalidType.Wrapf("%s doesn't exist", t)
 	}
+
+	// Disable granting other accounts with grant permission.
+	// Preventing user from accidentally authorizing their entire account to a different account.
 	if t == sdk.MsgTypeURL(&authz.MsgGrant{}) {
 		return nil, sdkerrors.ErrInvalidType.Wrap("authz msgGrant is not allowed")
 	}

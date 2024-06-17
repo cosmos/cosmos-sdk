cosmos/cosmos-sdk

commit 05ff7a7cb72ef8399cb4de76bd1d7aab42debaf3

feat(crypto): add blst (#20296)
Co-authored-by: itsdevbear <itsdevbear@berachain.com>
Co-authored-by: Akhil Kumar P <36399231+akhilkumarpilli@users.noreply.github.com>

x/genutil/utils.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	cfg "github.com/cometbft/cometbft/config"
+	cmtcrypto "github.com/cometbft/cometbft/crypto"
+	cmtbls12381 "github.com/cometbft/cometbft/crypto/bls12381"
 	tmed25519 "github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/cometbft/cometbft/p2p"
 	"github.com/cometbft/cometbft/privval"
@@ -44,13 +46,13 @@ func ExportGenesisFileWithTime(genFile, chainID string, validators []cmttypes.Ge
 }
 
 // InitializeNodeValidatorFiles creates private validator and p2p configuration files.
-func InitializeNodeValidatorFiles(config *cfg.Config) (nodeID string, valPubKey cryptotypes.PubKey, err error) {
-	return InitializeNodeValidatorFilesFromMnemonic(config, "")
+func InitializeNodeValidatorFiles(config *cfg.Config, keyType string) (nodeID string, valPubKey cryptotypes.PubKey, err error) {
+	return InitializeNodeValidatorFilesFromMnemonic(config, "", keyType)
 }
 
 // InitializeNodeValidatorFilesFromMnemonic creates private validator and p2p configuration files using the given mnemonic.
 // If no valid mnemonic is given, a random one will be used instead.
-func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic string) (nodeID string, valPubKey cryptotypes.PubKey, err error) {
+func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic, keyType string) (nodeID string, valPubKey cryptotypes.PubKey, err error) {
 	if len(mnemonic) > 0 && !bip39.IsMnemonicValid(mnemonic) {
 		return "", nil, fmt.Errorf("invalid mnemonic")
 	}
@@ -71,14 +73,37 @@ func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic strin
 		return "", nil, fmt.Errorf("could not create directory %q: %w", filepath.Dir(pvStateFile), err)
 	}
 
-	var filePV *privval.FilePV
+	var (
+		filePV  *privval.FilePV
+		privKey cmtcrypto.PrivKey
+	)
+
 	if len(mnemonic) == 0 {
-		filePV = privval.LoadOrGenFilePV(pvKeyFile, pvStateFile)
+		switch keyType {
+		case "ed25519":
+			privKey = tmed25519.GenPrivKey()
+		case "bls12_381":
+			privKey, err = cmtbls12381.GenPrivKey()
+			if err != nil {
+				return "", nil, err
+			}
+		default:
+			privKey = tmed25519.GenPrivKey()
+		}
 	} else {
-		privKey := tmed25519.GenPrivKeyFromSecret([]byte(mnemonic))
-		filePV = privval.NewFilePV(privKey, pvKeyFile, pvStateFile)
-		filePV.Save()
+		switch keyType {
+		case "ed25519":
+			privKey = tmed25519.GenPrivKeyFromSecret([]byte(mnemonic))
+		case "bls12_381":
+			// TODO: need to add support for getting from mnemonic in Comet.
+			return "", nil, fmt.Errorf("BLS key type does not support mnemonic")
+		default:
+			privKey = tmed25519.GenPrivKeyFromSecret([]byte(mnemonic))
+		}
 	}
+
+	filePV = privval.NewFilePV(privKey, pvKeyFile, pvStateFile)
+	filePV.Save()
 
 	tmValPubKey, err := filePV.GetPubKey()
 	if err != nil {

Makefile
@@ -67,6 +67,12 @@ ifeq (boltdb,$(findstring boltdb,$(COSMOS_BUILD_OPTIONS)))
   build_tags += boltdb
 endif
 
+# handle blst
+ifeq (blst,$(findstring blst,$(COSMOS_BUILD_OPTIONS)))
+  CGO_ENABLED=1
+  build_tags += blst
+endif
+
 whitespace :=
 whitespace += $(whitespace)
 comma := ,

crypto/codec/amino.go
@@ -4,7 +4,7 @@ import (
 	"github.com/cometbft/cometbft/crypto/sr25519"
 
 	"cosmossdk.io/core/legacy"
-
+	bls12_381 "github.com/cosmos/cosmos-sdk/crypto/keys/bls12_381"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	kmultisig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
@@ -21,6 +21,7 @@ func RegisterCrypto(cdc legacy.Amino) {
 		ed25519.PubKeyName)
 	cdc.RegisterConcrete(&secp256k1.PubKey{},
 		secp256k1.PubKeyName)
+	cdc.RegisterConcrete(&bls12_381.PubKey{}, bls12_381.PubKeyName)
 	cdc.RegisterConcrete(&kmultisig.LegacyAminoPubKey{},
 		kmultisig.PubKeyAminoRoute)
 
@@ -31,4 +32,5 @@ func RegisterCrypto(cdc legacy.Amino) {
 		ed25519.PrivKeyName)
 	cdc.RegisterConcrete(&secp256k1.PrivKey{},
 		secp256k1.PrivKeyName)
+	cdc.RegisterConcrete(&bls12_381.PrivKey{}, bls12_381.PrivKeyName)
 }

crypto/codec/cmt.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cometbft/cometbft/crypto/encoding"
 
 	"cosmossdk.io/errors"
-
+	bls12_381 "github.com/cosmos/cosmos-sdk/crypto/keys/bls12_381"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
@@ -23,6 +23,10 @@ func FromCmtProtoPublicKey(protoPk cmtprotocrypto.PublicKey) (cryptotypes.PubKey
 	case *cmtprotocrypto.PublicKey_Secp256K1:
 		return &secp256k1.PubKey{
 			Key: protoPk.Secp256K1,
+		}, nil
+	case *cmtprotocrypto.PublicKey_Bls12381:
+		return &bls12_381.PubKey{
+			Key: protoPk.Bls12381,
 		}, nil
 	default:
 		return nil, errors.Wrapf(sdkerrors.ErrInvalidType, "cannot convert %v from Tendermint public key", protoPk)
@@ -42,6 +46,12 @@ func ToCmtProtoPublicKey(pk cryptotypes.PubKey) (cmtprotocrypto.PublicKey, error
 		return cmtprotocrypto.PublicKey{
 			Sum: &cmtprotocrypto.PublicKey_Secp256K1{
 				Secp256K1: pk.Key,
+			},
+		}, nil
+	case *bls12_381.PubKey:
+		return cmtprotocrypto.PublicKey{
+			Sum: &cmtprotocrypto.PublicKey_Bls12381{
+				Bls12381: pk.Key,
 			},
 		}, nil
 	default:

crypto/codec/proto.go
@@ -3,6 +3,7 @@ package codec
 import (
 	"cosmossdk.io/core/registry"
 
+	bls12_381 "github.com/cosmos/cosmos-sdk/crypto/keys/bls12_381"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
@@ -16,11 +17,13 @@ func RegisterInterfaces(registry registry.InterfaceRegistrar) {
 	registry.RegisterInterface("cosmos.crypto.PubKey", pk)
 	registry.RegisterImplementations(pk, &ed25519.PubKey{})
 	registry.RegisterImplementations(pk, &secp256k1.PubKey{})
+	registry.RegisterImplementations(pk, &bls12_381.PubKey{})
 	registry.RegisterImplementations(pk, &multisig.LegacyAminoPubKey{})
 
 	var priv *cryptotypes.PrivKey
 	registry.RegisterInterface("cosmos.crypto.PrivKey", priv)
 	registry.RegisterImplementations(priv, &secp256k1.PrivKey{})
 	registry.RegisterImplementations(priv, &ed25519.PrivKey{})
+	registry.RegisterImplementations(priv, &bls12_381.PrivKey{})
 	secp256r1.RegisterInterfaces(registry)
 }

crypto/hd/algo.go
@@ -19,6 +19,9 @@ const (
 	// Ed25519Type represents the Ed25519Type signature system.
 	// It is currently not supported for end-user keys (wallets/ledgers).
 	Ed25519Type = PubKeyType("ed25519")
+	// Ed25519Type represents the Ed25519Type signature system.
+	// It is currently not supported for end-user keys (wallets/ledgers).
+	Bls12_381Type = PubKeyType("bls12_381")
 	// Sr25519Type represents the Sr25519Type signature system.
 	Sr25519Type = PubKeyType("sr25519")
 )

crypto/keys/bls12_381/const.go
@@ -1,0 +1,20 @@
+package bls12_381
+
+const (
+	PrivKeyName = "cometbft/PrivKeyBls12_381"
+	PubKeyName  = "cometbft/PubKeyBls12_381"
+	// PubKeySize is the size, in bytes, of public keys as used in this package.
+	PubKeySize = 32
+	// PrivKeySize is the size, in bytes, of private keys as used in this package.
+	PrivKeySize = 64
+	// SignatureLength defines the byte length of a BLS signature.
+	SignatureLength = 96
+	// SeedSize is the size, in bytes, of private key seeds. These are the
+	// private key representations used by RFC 8032.
+	SeedSize = 32
+
+	// MaxMsgLen defines the maximum length of the message bytes as passed to Sign.
+	MaxMsgLen = 32
+
+	KeyType = "bls12381"
+)

crypto/keys/bls12_381/key.go
@@ -1,0 +1,131 @@
+package bls12_381
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"github.com/cometbft/cometbft/crypto"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+)
+
+// ===============================================================================================
+// Private Key
+// ===============================================================================================
+
+// PrivKey is a wrapper around the Ethereum BLS12-381 private key type. This
+// wrapper conforms to crypto.Pubkey to allow for the use of the Ethereum
+// BLS12-381 private key type.
+
+var (
+	_ cryptotypes.PrivKey  = &PrivKey{}
+	_ codec.AminoMarshaler = &PrivKey{}
+)
+
+// NewPrivateKeyFromBytes build a new key from the given bytes.
+func NewPrivateKeyFromBytes(bz []byte) (PrivKey, error) {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// GenPrivKey generates a new key.
+func GenPrivKey() (PrivKey, error) {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// Bytes returns the byte representation of the Key.
+func (privKey PrivKey) Bytes() []byte {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// PubKey returns the private key's public key. If the privkey is not valid
+// it returns a nil value.
+func (privKey PrivKey) PubKey() cryptotypes.PubKey {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// Equals returns true if two keys are equal and false otherwise.
+func (privKey PrivKey) Equals(other cryptotypes.LedgerPrivKey) bool {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// Type returns the type.
+func (PrivKey) Type() string {
+	return KeyType
+}
+
+// Sign signs the given byte array. If msg is larger than
+// MaxMsgLen, SHA256 sum will be signed instead of the raw bytes.
+func (privKey PrivKey) Sign(msg []byte) ([]byte, error) {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// MarshalAmino overrides Amino binary marshaling.
+func (privKey PrivKey) MarshalAmino() ([]byte, error) {
+	return privKey.Key, nil
+}
+
+// UnmarshalAmino overrides Amino binary marshaling.
+func (privKey *PrivKey) UnmarshalAmino(bz []byte) error {
+	if len(bz) != PrivKeySize {
+		return errors.New("invalid privkey size")
+	}
+	privKey.Key = bz
+
+	return nil
+}
+
+// MarshalAminoJSON overrides Amino JSON marshaling.
+func (privKey PrivKey) MarshalAminoJSON() ([]byte, error) {
+	// When we marshal to Amino JSON, we don't marshal the "key" field itself,
+	// just its contents (i.e. the key bytes).
+	return privKey.MarshalAmino()
+}
+
+// UnmarshalAminoJSON overrides Amino JSON marshaling.
+func (privKey *PrivKey) UnmarshalAminoJSON(bz []byte) error {
+	return privKey.UnmarshalAmino(bz)
+}
+
+// ===============================================================================================
+// Public Key
+// ===============================================================================================
+
+// Pubkey is a wrapper around the Ethereum BLS12-381 public key type. This
+// wrapper conforms to crypto.Pubkey to allow for the use of the Ethereum
+// BLS12-381 public key type.
+
+var _ cryptotypes.PubKey = &PubKey{}
+
+// Address returns the address of the key.
+//
+// The function will panic if the public key is invalid.
+func (pubKey PubKey) Address() crypto.Address {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// VerifySignature verifies the given signature.
+func (pubKey PubKey) VerifySignature(msg, sig []byte) bool {
+	panic("not implemented, build flags are required to use bls12_381 keys")
+}
+
+// Bytes returns the byte format.
+func (pubKey PubKey) Bytes() []byte {
+	return pubKey.Key
+}
+
+// Type returns the key's type.
+func (PubKey) Type() string {
+	return KeyType
+}
+
+// Equals returns true if the other's type is the same and their bytes are deeply equal.
+func (pubKey PubKey) Equals(other cryptotypes.PubKey) bool {
+	return pubKey.Type() == other.Type() && bytes.Equal(pubKey.Bytes(), other.Bytes())
+}
+
+// String returns Hex representation of a pubkey with it's type
+func (pubKey PubKey) String() string {
+	return fmt.Sprintf("PubKeyBLS12_381{%X}", pubKey.Key)
+}

commit 314327eb2c0eb6701303d6bf4cffbc1de5f58dd4

fix(x/genutil): fix logic error introduced in #20296

x/genutil/utils.go
@@ -33,7 +33,9 @@ func ExportGenesisFile(genesis *types.AppGenesis, genFile string) error {
 
 // ExportGenesisFileWithTime creates and writes the genesis configuration to disk.
 // An error is returned if building or writing the configuration to file fails.
-func ExportGenesisFileWithTime(genFile, chainID string, validators []cmttypes.GenesisValidator, appState json.RawMessage, genTime time.Time) error {
+func ExportGenesisFileWithTime(
+	genFile, chainID string, validators []cmttypes.GenesisValidator, appState json.RawMessage, genTime time.Time,
+) error {
 	appGenesis := types.NewAppGenesisWithVersion(chainID, appState)
 	appGenesis.GenesisTime = genTime
 	appGenesis.Consensus.Validators = validators
@@ -46,13 +48,17 @@ func ExportGenesisFileWithTime(genFile, chainID string, validators []cmttypes.Ge
 }
 
 // InitializeNodeValidatorFiles creates private validator and p2p configuration files.
-func InitializeNodeValidatorFiles(config *cfg.Config, keyType string) (nodeID string, valPubKey cryptotypes.PubKey, err error) {
+func InitializeNodeValidatorFiles(config *cfg.Config, keyType string) (
+	nodeID string, valPubKey cryptotypes.PubKey, err error,
+) {
 	return InitializeNodeValidatorFilesFromMnemonic(config, "", keyType)
 }
 
 // InitializeNodeValidatorFilesFromMnemonic creates private validator and p2p configuration files using the given mnemonic.
 // If no valid mnemonic is given, a random one will be used instead.
-func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic, keyType string) (nodeID string, valPubKey cryptotypes.PubKey, err error) {
+func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic, keyType string) (
+	nodeID string, valPubKey cryptotypes.PubKey, err error,
+) {
 	if len(mnemonic) > 0 && !bip39.IsMnemonicValid(mnemonic) {
 		return "", nil, fmt.Errorf("invalid mnemonic")
 	}
@@ -81,14 +87,15 @@ func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic, keyT
 	if len(mnemonic) == 0 {
 		switch keyType {
 		case "ed25519":
-			privKey = tmed25519.GenPrivKey()
+			filePV = loadOrGenFilePV(tmed25519.GenPrivKey(), pvKeyFile, pvStateFile)
 		case "bls12_381":
 			privKey, err = cmtbls12381.GenPrivKey()
 			if err != nil {
 				return "", nil, err
 			}
+			filePV = loadOrGenFilePV(privKey, pvKeyFile, pvStateFile)
 		default:
-			privKey = tmed25519.GenPrivKey()
+			filePV = loadOrGenFilePV(tmed25519.GenPrivKey(), pvKeyFile, pvStateFile)
 		}
 	} else {
 		switch keyType {
@@ -100,10 +107,9 @@ func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic, keyT
 		default:
 			privKey = tmed25519.GenPrivKeyFromSecret([]byte(mnemonic))
 		}
+		filePV = privval.NewFilePV(privKey, pvKeyFile, pvStateFile)
+		filePV.Save()
 	}
-
-	filePV = privval.NewFilePV(privKey, pvKeyFile, pvStateFile)
-	filePV.Save()
 
 	tmValPubKey, err := filePV.GetPubKey()
 	if err != nil {
@@ -116,4 +122,20 @@ func InitializeNodeValidatorFilesFromMnemonic(config *cfg.Config, mnemonic, keyT
 	}
 
 	return nodeID, valPubKey, nil
+}
+
+// loadOrGenFilePV loads a FilePV from the given filePaths
+// or else generates a new one and saves it to the filePaths.
+func loadOrGenFilePV(privKey cmtcrypto.PrivKey, keyFilePath, stateFilePath string) *privval.FilePV {
+	_, err := os.Stat(keyFilePath)
+	exists := !os.IsNotExist(err)
+
+	var pv *privval.FilePV
+	if exists {
+		pv = privval.LoadFilePV(keyFilePath, stateFilePath)
+	} else {
+		pv = privval.NewFilePV(privKey, keyFilePath, stateFilePath)
+		pv.Save()
+	}
+	return pv
 }
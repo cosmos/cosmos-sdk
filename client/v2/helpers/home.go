743f1d3e4f59769e829f40e1dfb308df323a9c87
Julien Robert <julien@rbrt.fr>
2024-06-25 12:57:06 +0200

    updates

--- client/v2/helpers/home.go
+++ client/v2/helpers/home.go
@@ -6,24 +6,24 @@ import (
 	"strings"
 )
 
-// GetNodeHomeDirectory gets the home directory of the node (where the config is located)
-// It parses the home flag if set or an environment variable if set
-// Otherwise, it returns the default home directory
+// GetNodeHomeDirectory gets the home directory of the node (where the config is located).
+// It parses the home flag if set or an environment variable if set (and ignores name).
+// Otherwise, it returns the default home directory given its name.
 func GetNodeHomeDirectory(name string) (string, error) {
 	// get the home directory from the flag
 	args := os.Args
 	for i := 0; i < len(args); i++ {
 		if args[i] == "--home" && i+1 < len(args) {
-			return filepath.Join(args[i+1], name), nil
+			return filepath.Join(args[i+1]), nil
 		} else if strings.HasPrefix(args[i], "--home=") {
-			return filepath.Join(args[i][7:], name), nil
+			return filepath.Join(args[i][7:]), nil
 		}
 	}
 
 	// get the home directory from the environment variable
 	homeDir := os.Getenv("HOME")
 	if homeDir != "" {
-		return filepath.Join(homeDir, name), nil
+		return filepath.Join(homeDir), nil
 	}
 
 	// return the default home directory

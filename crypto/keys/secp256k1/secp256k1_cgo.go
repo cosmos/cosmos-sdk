89bc5cb5448fcbb6c486921461d039362ca182a3
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2024-06-26 09:17:27 -0300

    Use sha256 from cosmos/crypto

--- crypto/keys/secp256k1/secp256k1_cgo.go
+++ crypto/keys/secp256k1/secp256k1_cgo.go
@@ -5,6 +5,7 @@ package secp256k1
 
 import (
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1/internal/secp256k1"
+	"github.com/cosmos/crypto/hash/sha256"
 )
 
 // Sign creates an ECDSA signature on curve Secp256k1, using SHA256 on the msg.
@@ -21,5 +22,5 @@ func (privKey *PrivKey) Sign(msg []byte) ([]byte, error) {
 // VerifySignature validates the signature.
 // The msg will be hashed prior to signature verification.
 func (pubKey *PubKey) VerifySignature(msg, sigStr []byte) bool {
-	return secp256k1.VerifySignature(pubKey.Bytes(), crypto.Sha256(msg), sigStr)
+	return secp256k1.VerifySignature(pubKey.Bytes(), sha256.Sum(msg), sigStr)
 }

4cd29a5c9cecae7a40e9614fab61b08bf8e0ea1c
yihuang <huang@crypto.com>
2024-06-26 12:00:48 +0800

    feat(mempool): respect gas wanted returned by ante handler
    * Problem: mempool don't respect gas wanted returned by ante handler
    
    Solution:
    - support custom gas wanted in mempool
    
    * Update CHANGELOG.md
    
    Signed-off-by: yihuang <huang@crypto.com>
    
    * cleanup
    
    * cleanup
    
    * fix priorityIndex
    
    * fix process proposal
    
    * fix lint
    
    * fix lint
    
    ---------
    
    Signed-off-by: yihuang <huang@crypto.com>
    
    Problem: mempool.GasTx interface is not reused (#536)
    
    * Problem: redundant mutex for InsertWithGasWanted
    
    cfg of PriorityNonceMempool remains unchanged once assigned, so no lock is required
    
    * make mocks
    
    * cleanup
    
    * keep order of check MaxTx

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -62,6 +62,7 @@ Ref: https://keepachangelog.com/en/1.0.0/
 * (baseapp) [#24655](https://github.com/cosmos/cosmos-sdk/pull/24655) Add mutex locks for `state` and make `lastCommitInfo` atomic to prevent race conditions between `Commit` and `CreateQueryContext`.
 * (proto) [#24161](https://github.com/cosmos/cosmos-sdk/pull/24161) Remove unnecessary annotations from `x/staking` authz proto.
 * (x/bank) [#24660](https://github.com/cosmos/cosmos-sdk/pull/24660) Improve performance of the `GetAllBalances` and `GetAccountsBalances` keeper methods.
+* (mempool)[#25338](https://github.com/cosmos/cosmos-sdk/pull/25338) Respect gas wanted returned by ante handler for mempool.
 
 ### Bug Fixes
 
--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -873,7 +873,7 @@ func (app *BaseApp) runTx(mode sdk.ExecMode, txBytes []byte, tx sdk.Tx) (gInfo s
 
 	switch mode {
 	case execModeCheck:
-		err = app.mempool.Insert(ctx, tx)
+		err = app.mempool.InsertWithGasWanted(ctx, tx, gasWanted)
 		if err != nil {
 			return gInfo, nil, anteEvents, err
 		}
@@ -1067,18 +1067,18 @@ func (app *BaseApp) PrepareProposalVerifyTx(tx sdk.Tx) ([]byte, error) {
 // ProcessProposal state internally will be discarded. <nil, err> will be
 // returned if the transaction cannot be decoded. <Tx, nil> will be returned if
 // the transaction is valid, otherwise <Tx, err> will be returned.
-func (app *BaseApp) ProcessProposalVerifyTx(txBz []byte) (sdk.Tx, error) {
+func (app *BaseApp) ProcessProposalVerifyTx(txBz []byte) (sdk.Tx, uint64, error) {
 	tx, err := app.txDecoder(txBz)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 
-	_, _, _, err = app.runTx(execModeProcessProposal, txBz, tx)
+	gInfo, _, _, err := app.runTx(execModeProcessProposal, txBz, tx)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 
-	return tx, nil
+	return tx, gInfo.GasWanted, nil
 }
 
 func (app *BaseApp) TxDecode(txBytes []byte) (sdk.Tx, error) {

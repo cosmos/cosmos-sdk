e0173b92730a7324bafe1c29b2db33534d47da7d
ocnc2 <169075746+ocnc2@users.noreply.github.com>
2024-06-26 15:37:35 -0400

    fix(server/v2): make CometBFTServer component adhere to HasStartFlags interface

--- server/v2/cometbft/server.go
+++ server/v2/cometbft/server.go
@@ -196,8 +196,8 @@ func getGenDocProvider(cfg *cmtcfg.Config) func() (node.ChecksummedGenesisDoc, e
 	}
 }
 
-func (s *CometBFTServer[T]) StartCmdFlags() pflag.FlagSet {
-	flags := *pflag.NewFlagSet("cometbft", pflag.ExitOnError)
+func (s *CometBFTServer[T]) StartCmdFlags() *pflag.FlagSet {
+	flags := pflag.NewFlagSet("cometbft", pflag.ExitOnError)
 	flags.Bool(flagWithComet, true, "Run abci app embedded in-process with CometBFT")
 	flags.String(flagAddress, "tcp://127.0.0.1:26658", "Listen address")
 	flags.String(flagTransport, "socket", "Transport protocol: socket, grpc")

92bb34ca0bd1310539dd6932e50c2420f051315f
Matt Kocubinski <mkocubinski@gmail.com>
2024-06-27 16:02:10 -0500

    json encoding

--- x/auth/tx/encoder.go
+++ x/auth/tx/encoder.go
@@ -2,11 +2,12 @@ package tx
 
 import (
 	"fmt"
-
-	"google.golang.org/protobuf/encoding/protojson"
+	gogoproto "github.com/cosmos/gogoproto/proto"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
+	"google.golang.org/protobuf/proto"
 )
 
 // DefaultTxEncoder returns a default protobuf TxEncoder using the provided Marshaler
@@ -22,16 +23,20 @@ func DefaultTxEncoder() sdk.TxEncoder {
 
 // DefaultJSONTxEncoder returns a default protobuf JSON TxEncoder using the provided Marshaler.
 func DefaultJSONTxEncoder(cdc codec.Codec) sdk.TxEncoder {
-	jsonMarshaler := protojson.MarshalOptions{
-		Indent:         "",
-		UseProtoNames:  true,
-		UseEnumNumbers: false,
-	}
 	return func(tx sdk.Tx) ([]byte, error) {
 		gogoWrapper, ok := tx.(*gogoTxWrapper)
 		if !ok {
 			return nil, fmt.Errorf("unexpected tx type: %T", tx)
 		}
-		return jsonMarshaler.Marshal(gogoWrapper.Tx)
+		bz, err := proto.Marshal(gogoWrapper.Tx)
+		if err != nil {
+			return nil, err
+		}
+		v1Tx := &sdktx.Tx{}
+		err = gogoproto.Unmarshal(bz, v1Tx)
+		if err != nil {
+			return nil, err
+		}
+		return cdc.MarshalJSON(v1Tx)
 	}
 }

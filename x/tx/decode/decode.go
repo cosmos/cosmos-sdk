2e44712f7db3d68a50b17a9d9246c262cc6a9444
akhilkumarpilli <akhilkumar7947@gmail.com>
2024-07-01 17:01:16 +0530

    feat(tx): add cache to tx decoder

--- x/tx/decode/decode.go
+++ x/tx/decode/decode.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	gogoproto "github.com/cosmos/gogoproto/proto"
+	lru "github.com/hashicorp/golang-lru"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/dynamicpb"
@@ -16,6 +17,11 @@ import (
 	"cosmossdk.io/core/transaction"
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/x/tx/signing"
+)
+
+const (
+	// DefaultTxCacheSize is the default capacity of TxCache
+	DefaultTxCacheSize uint = 1000
 )
 
 // DecodedTx contains the decoded transaction, its signers, and other flags.
@@ -39,16 +45,24 @@ type gogoProtoCodec interface {
 	Unmarshal([]byte, gogoproto.Message) error
 }
 
+// cacheEntry wraps a decoded transaction and an error
+type cacheEntry struct {
+	decodedTx *DecodedTx
+	err       error
+}
+
 // Decoder contains the dependencies required for decoding transactions.
 type Decoder struct {
 	signingCtx *signing.Context
 	codec      gogoProtoCodec
+	txCache    *lru.Cache
 }
 
 // Options are options for creating a Decoder.
 type Options struct {
 	SigningContext *signing.Context
 	ProtoCodec     gogoProtoCodec
+	TxCacheSize    uint
 }
 
 // NewDecoder creates a new Decoder for decoding transactions.
@@ -59,18 +73,42 @@ func NewDecoder(options Options) (*Decoder, error) {
 	if options.ProtoCodec == nil {
 		return nil, errors.New("proto codec is required for unmarshalling gogoproto messages")
 	}
+
+	txCacheSize := options.TxCacheSize
+	if options.TxCacheSize == 0 {
+		txCacheSize = DefaultTxCacheSize
+	}
+
+	// Create a new LRU tx decoder cache
+	txCache, err := lru.New(int(txCacheSize))
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize tx decoder cache: %w", err)
+	}
+
 	return &Decoder{
 		signingCtx: options.SigningContext,
 		codec:      options.ProtoCodec,
+		txCache:    txCache,
 	}, nil
 }
 
 // Decode decodes raw protobuf encoded transaction bytes into a DecodedTx.
 func (d *Decoder) Decode(txBytes []byte) (*DecodedTx, error) {
+	// generate cache key using txBytes
+	cacheKey := string(txBytes)
+
+	// check if the tx is already present the cache
+	if entry, found := d.txCache.Get(cacheKey); found {
+		c, ok := entry.(cacheEntry)
+		if ok {
+			return c.decodedTx, c.err
+		}
+	}
+
 	// Make sure txBytes follow ADR-027.
 	err := rejectNonADR027TxRaw(txBytes)
 	if err != nil {
-		return nil, errorsmod.Wrap(ErrTxDecode, err.Error())
+		return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, err.Error()))
 	}
 
 	var raw v1beta1.TxRaw
@@ -79,12 +117,12 @@ func (d *Decoder) Decode(txBytes []byte) (*DecodedTx, error) {
 	fileResolver := d.signingCtx.FileResolver()
 	err = RejectUnknownFieldsStrict(txBytes, raw.ProtoReflect().Descriptor(), fileResolver)
 	if err != nil {
-		return nil, errorsmod.Wrap(ErrTxDecode, err.Error())
+		return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, err.Error()))
 	}
 
 	err = proto.Unmarshal(txBytes, &raw)
 	if err != nil {
-		return nil, err
+		return d.returnDecodeError(cacheKey, err)
 	}
 
 	var body v1beta1.TxBody
@@ -92,12 +130,12 @@ func (d *Decoder) Decode(txBytes []byte) (*DecodedTx, error) {
 	// allow non-critical unknown fields in TxBody
 	txBodyHasUnknownNonCriticals, err := RejectUnknownFields(raw.BodyBytes, body.ProtoReflect().Descriptor(), true, fileResolver)
 	if err != nil {
-		return nil, errorsmod.Wrap(ErrTxDecode, err.Error())
+		return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, err.Error()))
 	}
 
 	err = proto.Unmarshal(raw.BodyBytes, &body)
 	if err != nil {
-		return nil, errorsmod.Wrap(ErrTxDecode, err.Error())
+		return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, err.Error()))
 	}
 
 	var authInfo v1beta1.AuthInfo
@@ -105,12 +143,12 @@ func (d *Decoder) Decode(txBytes []byte) (*DecodedTx, error) {
 	// reject all unknown proto fields in AuthInfo
 	err = RejectUnknownFieldsStrict(raw.AuthInfoBytes, authInfo.ProtoReflect().Descriptor(), fileResolver)
 	if err != nil {
-		return nil, errorsmod.Wrap(ErrTxDecode, err.Error())
+		return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, err.Error()))
 	}
 
 	err = proto.Unmarshal(raw.AuthInfoBytes, &authInfo)
 	if err != nil {
-		return nil, errorsmod.Wrap(ErrTxDecode, err.Error())
+		return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, err.Error()))
 	}
 
 	theTx := &v1beta1.Tx{
@@ -131,31 +169,31 @@ func (d *Decoder) Decode(txBytes []byte) (*DecodedTx, error) {
 		// unmarshal into dynamic message
 		msgDesc, err := fileResolver.FindDescriptorByName(protoreflect.FullName(typeURL))
 		if err != nil {
-			return nil, fmt.Errorf("protoFiles does not have descriptor %s: %w", anyMsg.TypeUrl, err)
+			return d.returnDecodeError(cacheKey, fmt.Errorf("protoFiles does not have descriptor %s: %w", anyMsg.TypeUrl, err))
 		}
 		dynamicMsg := dynamicpb.NewMessageType(msgDesc.(protoreflect.MessageDescriptor)).New().Interface()
 		err = anyMsg.UnmarshalTo(dynamicMsg)
 		if err != nil {
-			return nil, err
+			return d.returnDecodeError(cacheKey, err)
 		}
 		dynamicMsgs = append(dynamicMsgs, dynamicMsg)
 
 		// unmarshal into gogoproto message
 		gogoType := gogoproto.MessageType(typeURL)
 		if gogoType == nil {
-			return nil, fmt.Errorf("cannot find type: %s", anyMsg.TypeUrl)
+			return d.returnDecodeError(cacheKey, fmt.Errorf("cannot find type: %s", anyMsg.TypeUrl))
 		}
 		msg := reflect.New(gogoType.Elem()).Interface().(gogoproto.Message)
 		err = d.codec.Unmarshal(anyMsg.Value, msg)
 		if err != nil {
-			return nil, err
+			return d.returnDecodeError(cacheKey, err)
 		}
 		msgs = append(msgs, msg)
 
 		// fetch signers with dynamic message
 		ss, signerErr := d.signingCtx.GetSigners(dynamicMsg)
 		if signerErr != nil {
-			return nil, errorsmod.Wrap(ErrTxDecode, signerErr.Error())
+			return d.returnDecodeError(cacheKey, errorsmod.Wrap(ErrTxDecode, signerErr.Error()))
 		}
 		for _, s := range ss {
 			_, seen := seenSigners[string(s)]
@@ -167,14 +205,25 @@ func (d *Decoder) Decode(txBytes []byte) (*DecodedTx, error) {
 		}
 	}
 
-	return &DecodedTx{
+	decodedTx := &DecodedTx{
 		Messages:                     msgs,
 		DynamicMessages:              dynamicMsgs,
 		Tx:                           theTx,
 		TxRaw:                        &raw,
 		TxBodyHasUnknownNonCriticals: txBodyHasUnknownNonCriticals,
 		Signers:                      signers,
-	}, nil
+	}
+
+	// store the decoded tx in the tx decoder cache
+	d.txCache.Add(cacheKey, &cacheEntry{decodedTx: decodedTx, err: err})
+
+	return decodedTx, nil
+}
+
+func (d *Decoder) returnDecodeError(cacheKey string, err error) (*DecodedTx, error) {
+	// store the error in the tx decoder cache
+	d.txCache.Add(cacheKey, &cacheEntry{decodedTx: nil, err: err})
+	return nil, err
 }
 
 // Hash implements the interface for the Tx interface.

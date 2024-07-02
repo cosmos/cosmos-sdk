6343cd201c7c705d299db84354ec4f7512328d1a
Aaron Craelius <aaronc@users.noreply.github.com>
2024-07-02 19:37:19 +0200

    cleanup

--- schema/decoder.go
+++ schema/decoder.go
@@ -1,7 +1,5 @@
 package schema
 
-import "context"
-
 // HasModuleCodec is an interface that modules can implement to provide a ModuleCodec.
 // Usually these modules would also implement appmodule.AppModule, but that is not included
 // to keep this package free of any dependencies.
@@ -18,32 +16,10 @@ type ModuleCodec struct {
 	// KVDecoder is a function that decodes a key-value pair into an ObjectUpdate.
 	// If it is nil, the module doesn't support state decoding directly.
 	KVDecoder KVDecoder
-
-	// ApplyUpdate is a function that applies an ObjectUpdate to the module's state for the given context.
-	// If it is nil, the module doesn't support applying logical updates. If this function is provided
-	// then it can be used as a genesis import path.
-	ApplyUpdate ApplyUpdate
 }
 
-// KVDecoder is a function that decodes a key-value pair into one or more ObjectUpdate's.
-// If the KV-pair doesn't represent object updates, the function should return nil as the first
-// and no error. The error result  should only be non-nil when the decoder expected
-// to parse a valid update and was unable to. In the case of an error, the decoder may return
-// a non-nil value for the first return value, which can indicate which parts of the update
-// were decodable to aid debugging.
-type KVDecoder = func(KVPairUpdate) ([]ObjectUpdate, error)
-
-type KVPairUpdate struct {
-	// Key is the key of the key-value pair.
-	Key []byte
-
-	// Value is the value of the key-value pair. It should be ignored when Delete is true.
-	Value []byte
-
-	// Delete is a flag that indicates that the key-value pair was deleted. If it is false,
-	// then it is assumed that this has been a set operation.
-	Delete bool
-}
-
-// ApplyUpdate is a function that applies an ObjectUpdate to the module's state for the given context.
-type ApplyUpdate = func(context.Context, ObjectUpdate) error
+// KVDecoder is a function that decodes a key-value pair into an ObjectUpdate.
+// If the KV-pair doesn't represent an object update, the function should return false
+// as the second return value. Error should only be non-nil when the decoder expected
+// to parse a valid update and was unable to.
+type KVDecoder = func(key, value []byte) (ObjectUpdate, bool, error)

402eb4deb4b3552656d1ea7660e2ae3bb17ccfed
marbar3778 <marbar3778@yahoo.com>
2024-07-05 10:58:25 +0200

    address comments

--- x/staking/types/params.go
+++ x/staking/types/params.go
@@ -24,12 +24,6 @@ const (
 
 	// Default maximum entries in a UBD/RED pair
 	DefaultMaxEntries uint32 = 7
-
-	// DefaultHistorical entries is 10000. Apps that don't use IBC can ignore this
-	// value by not adding the staking module to the application module manager's
-	// SetOrderBeginBlockers.
-	// Deprecated: HistoricalEntries is deprecated
-	DefaultHistoricalEntries uint32 = 10000
 )
 
 var (

8418694ff5e5d2d9ed910880084aa789a1cf0d4b
testinginprod <frojdi@faulttolerance.net>
2024-07-10 12:44:45 +0200

    rev

--- server/grpc/gogoreflection/serverreflection.go
+++ server/grpc/gogoreflection/serverreflection.go
@@ -42,7 +42,7 @@ import (
 	"sort"
 	"sync"
 
-	// nolint: staticcheck // keep this import for backward compatibility
+	//nolint: staticcheck // keep this import for backward compatibility
 	"github.com/golang/protobuf/proto"
 	dpb "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"google.golang.org/grpc"

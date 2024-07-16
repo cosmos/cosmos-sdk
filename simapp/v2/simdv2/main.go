6708818470826923b96ff7fb6ef55729d8c4269e
Julien Robert <julien@rbrt.fr>
2024-07-16 16:52:29 +0200

    fix(client/v2): use (PREFIX)_HOME instead of NODE_HOME (#20964)

--- simapp/v2/simdv2/main.go
+++ simapp/v2/simdv2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	clientv2helpers "cosmossdk.io/client/v2/helpers"
 	"cosmossdk.io/core/transaction"
 	serverv2 "cosmossdk.io/server/v2"
 	"cosmossdk.io/simapp/v2"
@@ -12,7 +13,7 @@ import (
 
 func main() {
 	rootCmd := cmd.NewRootCmd[transaction.Tx]()
-	if err := serverv2.Execute(rootCmd, "", simapp.DefaultNodeHome); err != nil {
+	if err := serverv2.Execute(rootCmd, clientv2helpers.EnvPrefix, simapp.DefaultNodeHome); err != nil {
 		fmt.Fprintln(rootCmd.OutOrStderr(), err)
 		os.Exit(1)
 	}

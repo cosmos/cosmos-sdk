ad50d8d19837bc0a148b5bf56f56579c096c8691
marbar3778 <marbar3778@yahoo.com>
2024-07-16 12:54:18 +0200

    turn simapp v2 tests into a systemtest

---
+++ tests/systemtests/init_test.go
@@ -0,0 +1,26 @@
+package systemtests
+
+import (
+	"testing"
+	"time"
+)
+
+func TestWaitForBlock(t *testing.T) {
+	// Scenario:
+	// Spin up a chain and wait for height 5
+
+	sut.ResetChain(t)
+
+	cli := NewCLIWrapper(t, sut, verbose)
+
+	// add genesis account with some tokens
+	account1Addr := cli.AddKey("account1")
+	sut.ModifyGenesisCLI(t,
+		[]string{"genesis", "add-genesis-account", account1Addr, "10000000stake"},
+	)
+
+	sut.StartChain(t)
+
+	// query validator address to delegate tokens
+	sut.AwaitBlockHeight(t, 5, time.Second*30)
+}

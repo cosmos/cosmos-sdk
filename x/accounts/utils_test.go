a9d2029e4e7e879992afe1df919ae20b5d211d12
Halimao <1065621723@qq.com>
2024-07-19 16:44:51 +0800

    fix: use `core/testing`

--- x/accounts/utils_test.go
+++ x/accounts/utils_test.go
@@ -14,7 +14,6 @@ import (
 	"cosmossdk.io/core/event"
 	coretesting "cosmossdk.io/core/testing"
 	coretransaction "cosmossdk.io/core/transaction"
-	"cosmossdk.io/log"
 	"cosmossdk.io/x/accounts/internal/implementation"
 	"cosmossdk.io/x/tx/signing"
 

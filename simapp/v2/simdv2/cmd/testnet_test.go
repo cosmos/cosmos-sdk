40bc307c13538f367afb4c82248982fecba1f720
akhilkumarpilli <akhilkumar7947@gmail.com>
2024-07-22 12:09:26 +0530

    fix lint

--- simapp/v2/simdv2/cmd/testnet_test.go
+++ simapp/v2/simdv2/cmd/testnet_test.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"cosmossdk.io/core/transaction"
 	"cosmossdk.io/simapp/v2"
 	"cosmossdk.io/simapp/v2/simdv2/cmd"
+
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/stretchr/testify/require"
 )
 
 func TestInitTestFilesCmd(t *testing.T) {

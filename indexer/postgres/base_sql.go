1024a1d6842f96612e5d7e94507615dbd73ed99e
Aaron Craelius <aaronc@users.noreply.github.com>
2024-07-25 22:36:20 +0200

    feat(indexer/postgres): block, tx and event schemas

--- indexer/postgres/base_sql.go
+++ indexer/postgres/base_sql.go
@@ -5,4 +5,29 @@ const BaseSQL = `
 CREATE OR REPLACE FUNCTION nanos_to_timestamptz(nanos bigint) RETURNS timestamptz AS $$
     SELECT to_timestamp(nanos / 1000000000) + (nanos / 1000000000) * INTERVAL '1 microsecond'
 $$ LANGUAGE SQL IMMUTABLE;
+
+CREATE TABLE IF NOT EXISTS block
+(
+    number BIGINT NOT NULL PRIMARY KEY,
+    header JSONB  NULL
+);
+
+CREATE TABLE IF NOT EXISTS tx
+(
+    id             BIGSERIAL PRIMARY KEY,
+    block_number   BIGINT NOT NULL REFERENCES block (number),
+    index_in_block BIGINT NOT NULL,
+    data           JSONB  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS event
+(
+    id           BIGSERIAL PRIMARY KEY,
+    block_number BIGINT NOT NULL REFERENCES block (number),
+    tx_idx       BIGINT NOT NULL REFERENCES tx (index_in_block),
+    msg_idx      BIGINT NOT NULL,
+    event_idx    BIGINT NOT NULL,
+    type         TEXT   NOT NULL,
+    data         JSONB  NOT NULL
+);
 `

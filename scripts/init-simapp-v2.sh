2fc5247f9b9da0da624b0bb9d5c3540c9f6ff503
Julien Robert <julien@rbrt.fr>
2024-07-28 09:28:05 +0200

    ci: make init-simapp v1 / v2 consistent (#21082)
    (cherry picked from commit 73a9396a0a2e4e2e62e2a0cf7b256e4c7a2989a4)

--- scripts/init-simapp-v2.sh
+++ scripts/init-simapp-v2.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-SIMD_BIN=${SIMD_BIN:=$(which simd 2>/dev/null)}
+SIMD_BIN=${SIMD_BIN:=$(which simdv2 2>/dev/null)}
 
-if [ -z "$SIMD_BIN" ]; then echo "SIMD_BIN is not set. Make sure to run 'make install' before"; exit 1; fi
+if [ -z "$SIMD_BIN" ]; then echo "SIMD_BIN is not set. Make sure to run 'COSMOS_BUILD_OPTIONS=v2 make install' before"; exit 1; fi
+echo "using $SIMD_BIN"
 SIMD_HOME=$($SIMD_BIN config home)
 if [ -d "$SIMD_HOME" ]; then rm -rv $SIMD_HOME; fi
-$SIMD_BIN config set client chain-id demo
+$SIMD_BIN config set client chain-id simapp-v2-chain
 $SIMD_BIN config set client keyring-backend test
 $SIMD_BIN config set client keyring-default-keyname alice
 $SIMD_BIN config set app api.enable true
 $SIMD_BIN keys add alice --indiscreet
 $SIMD_BIN keys add bob --indiscreet
-$SIMD_BIN init test --chain-id demo
+$SIMD_BIN init simapp-v2-node --chain-id simapp-v2-chain
 # to change the voting_period
 jq '.app_state.gov.params.voting_period = "600s"' $SIMD_HOME/config/genesis.json > temp.json && mv temp.json $SIMD_HOME/config/genesis.json
 jq '.app_state.gov.params.expedited_voting_period = "300s"' $SIMD_HOME/config/genesis.json > temp.json && mv temp.json $SIMD_HOME/config/genesis.json

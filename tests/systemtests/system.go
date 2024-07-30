9d4f4612f76b67ee3e4a25d317308a964f0314b7
marbar3778 <marbar3778@yahoo.com>
2024-07-30 18:08:01 +0200

    use context for comet info

--- tests/systemtests/system.go
+++ tests/systemtests/system.go
@@ -154,7 +154,7 @@ func (s *SystemUnderTest) StartChain(t *testing.T, xargs ...string) {
 	t.Helper()
 	s.Log("Start chain\n")
 	s.ChainStarted = true
-	s.startNodesAsync(t, append([]string{"start", "--trace", "--log_level=info", "--log_no_color"}, xargs...)...)
+	s.startNodesAsync(t, append([]string{"start", "--log_level=info", "--log_no_color"}, xargs...)...)
 
 	s.AwaitNodeUp(t, s.rpcAddr)
 

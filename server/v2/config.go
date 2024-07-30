c3cf0a7a044c778a3757f956b02219428e2e9afd
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-07-30 18:38:42 +0700

    read client.toml & tight condition

--- server/v2/config.go
+++ server/v2/config.go
@@ -2,7 +2,6 @@ package serverv2
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
@@ -23,6 +22,12 @@ func ReadConfig(configPath string) (*viper.Viper, error) {
 		return nil, fmt.Errorf("failed to merge configuration: %w", err)
 	}
 
+	// Read client.toml to get chain-id
+	v.SetConfigName("client")
+	if err := v.MergeInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to merge configuration: %w", err)
+	}
+
 	v.WatchConfig()
 
 	return v, nil
@@ -34,8 +39,9 @@ func ReadConfig(configPath string) (*viper.Viper, error) {
 func UnmarshalSubConfig(v *viper.Viper, subName string, target any) error {
 	var sub any
 	for k, val := range v.AllSettings() {
-		if strings.HasPrefix(k, subName) {
+		if k == subName {
 			sub = val
+			break
 		}
 	}
 

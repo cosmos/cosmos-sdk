4cedbdeff60769514a9754c061018ae211d86381
Julien Robert <julien@rbrt.fr>
2024-08-07 09:51:17 +0200

    revert

--- x/upgrade/types/storeloader.go
+++ x/upgrade/types/storeloader.go
@@ -1,15 +1,15 @@
 package types
 
 import (
-	corestore "cosmossdk.io/core/store"
+	storetypes "cosmossdk.io/store/types"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 )
 
 // UpgradeStoreLoader is used to prepare baseapp with a fixed StoreLoader
 // pattern. This is useful for custom upgrade loading logic.
-func UpgradeStoreLoader(upgradeHeight int64, storeUpgrades corestore.StoreUpgrades) baseapp.StoreLoader {
-	return func(ms corestore.CommitMultiStore) error {
+func UpgradeStoreLoader(upgradeHeight int64, storeUpgrades *storetypes.StoreUpgrades) baseapp.StoreLoader {
+	return func(ms storetypes.CommitMultiStore) error {
 		if upgradeHeight == ms.LastCommitID().Version+1 {
 			// Check if the current commit version and upgrade height matches
 			if len(storeUpgrades.Renamed) > 0 || len(storeUpgrades.Deleted) > 0 || len(storeUpgrades.Added) > 0 {

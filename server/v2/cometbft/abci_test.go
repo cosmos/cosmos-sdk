07f4fa3267c1b2344f3d29be683298a245313146
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-08-09 15:14:38 +0700

    check tx & prepare proposal

--- server/v2/cometbft/abci_test.go
+++ server/v2/cometbft/abci_test.go
@@ -15,6 +15,7 @@ import (
 	"cosmossdk.io/server/v2/cometbft/mempool"
 	cometmock "cosmossdk.io/server/v2/cometbft/mock"
 	"cosmossdk.io/server/v2/cometbft/types"
+	"cosmossdk.io/server/v2/cometbft/handlers"
 	"cosmossdk.io/server/v2/stf"
 	"cosmossdk.io/server/v2/stf/branch"
 	"cosmossdk.io/server/v2/stf/mock"
@@ -37,11 +38,16 @@ var (
 			MaxGas: 5000000,
 		},
 	}
-	anyMsg, _ = gogotypes.MarshalAny(&gogotypes.BoolValue{Value: true})
 	mockTx = mock.Tx{
 		Sender:   []byte("sender"),
-		Msg:      anyMsg, // msg does not matter at all because our handler does nothing.
-		GasLimit: 100_000,                                 // NO GAS!
+		Msg:      &gogotypes.BoolValue{Value: true},
+		GasLimit: 100_000,                                 
+	}
+
+	invalidMockTx = mock.Tx{
+		Sender:   []byte("sender"),
+		Msg:      &gogotypes.BoolValue{Value: true},
+		GasLimit: 0,                                 
 	}
 )
 
@@ -112,7 +118,7 @@ func addQueryHandlerToSTF[T any, PT interface {
 }
 
 func TestConsensus_InitChain_Without_UpdateParam(t *testing.T) {
-	c, _ := setUpConsensus(t)
+	c, _ := setUpConsensus(t, 100_000)
 	mockStore := c.store
 	_, err := c.InitChain(context.Background(), &abciproto.InitChainRequest{
 		Time:          time.Now(),
@@ -137,7 +143,7 @@ func TestConsensus_InitChain_Without_UpdateParam(t *testing.T) {
 }
 
 func TestConsensus_InitChain_With_UpdateParam(t *testing.T) {
-	c, s := setUpConsensus(t)
+	c, s := setUpConsensus(t, 100_000)
 	mockStore := c.store
 	addMsgHandlerToSTF(t, s, func(ctx context.Context, msg *consensustypes.MsgUpdateParams) (*consensustypes.MsgUpdateParams, error) {
 		kvSet(t, ctx, "exec")
@@ -170,7 +176,7 @@ func TestConsensus_InitChain_With_UpdateParam(t *testing.T) {
 }
 
 func TestConsensus_InitChain_Invalid_Height(t *testing.T) {
-	c, _ := setUpConsensus(t)
+	c, _ := setUpConsensus(t, 100_000)
 	mockStore := c.store
 	_, err := c.InitChain(context.Background(), &abciproto.InitChainRequest{
 		Time:          time.Now(),
@@ -191,7 +197,7 @@ func TestConsensus_InitChain_Invalid_Height(t *testing.T) {
 }
 
 func TestConsensus_FinalizeBlock_Invalid_Height(t *testing.T) {
-	c, _ := setUpConsensus(t)
+	c, _ := setUpConsensus(t, 100_000)
 	_, err := c.InitChain(context.Background(), &abciproto.InitChainRequest{
 		Time:          time.Now(),
 		ChainId:       "test",
@@ -214,7 +220,7 @@ func TestConsensus_FinalizeBlock_Invalid_Height(t *testing.T) {
 }
 
 func TestConsensus_FinalizeBlock_NoTxs(t *testing.T) {
-	c, s := setUpConsensus(t)
+	c, s := setUpConsensus(t, 100_000)
 	mockStore := c.store
 
 	addQueryHandlerToSTF(t, s, func(ctx context.Context, q *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error) {
@@ -254,8 +260,52 @@ func TestConsensus_FinalizeBlock_NoTxs(t *testing.T) {
 	require.Equal(t, int64(endBlock), c.lastCommittedHeight.Load())
 }
 
+func TestConsensus_FinalizeBlock_MultiTxs_OutOfGas(t *testing.T) {
+	c, s := setUpConsensus(t, 100_000)
+
+	addQueryHandlerToSTF(t, s, func(ctx context.Context, q *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error) {
+		cParams := DefaulConsensusParams
+		kvSet(t, ctx, "query")
+		return &consensustypes.QueryParamsResponse{
+			Params: cParams,
+		}, nil
+	})
+	addMsgHandlerToSTF(t, s, func(ctx context.Context, msg *gogotypes.BoolValue) (*gogotypes.BoolValue, error) {
+		kvSet(t, ctx, "exec")
+		return nil, nil
+	})
+
+	_, err := c.InitChain(context.Background(), &abciproto.InitChainRequest{
+		Time:          time.Now(),
+		ChainId:       "test",
+		InitialHeight: 1,
+	})
+	require.NoError(t, err)
+
+	_, err = c.FinalizeBlock(context.Background(), &abciproto.FinalizeBlockRequest{
+		Time:   time.Now(),
+		Height: 1,
+	})
+	require.NoError(t, err)
+
+	endBlock := 10
+	for i := 2; i <= endBlock; i++ {
+		res, err := c.FinalizeBlock(context.Background(), &abciproto.FinalizeBlockRequest{
+			Time:   time.Now(),
+			Height: int64(i),
+			Hash:   sum[:],
+			Txs:    [][]byte{invalidMockTx.Bytes()},
+		})
+		fmt.Println("res", res, err)
+		require.NoError(t, err)
+		require.NotEqual(t, res.TxResults[0].Code, 0)
+		require.Contains(t, res.TxResults[0].Log, "out of gas")
+	}
+	require.Equal(t, int64(endBlock), c.lastCommittedHeight.Load())
+}
+
 func TestConsensus_FinalizeBlock_MultiTxs(t *testing.T) {
-	c, s := setUpConsensus(t)
+	c, s := setUpConsensus(t, 100_000)
 	mockStore := c.store
 
 	addQueryHandlerToSTF(t, s, func(ctx context.Context, q *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error) {
@@ -265,7 +315,7 @@ func TestConsensus_FinalizeBlock_MultiTxs(t *testing.T) {
 			Params: cParams,
 		}, nil
 	})
-	addMsgHandlerToSTF(t, s, func(ctx context.Context, msg *gogotypes.Any) (*gogotypes.Any, error) {
+	addMsgHandlerToSTF(t, s, func(ctx context.Context, msg *gogotypes.BoolValue) (*gogotypes.BoolValue, error) {
 		kvSet(t, ctx, "exec")
 		return nil, nil
 	})
@@ -299,8 +349,53 @@ func TestConsensus_FinalizeBlock_MultiTxs(t *testing.T) {
 	require.Equal(t, int64(endBlock), c.lastCommittedHeight.Load())
 }
 
+func TestConsensus_CheckTx(t *testing.T) {
+	c, s := setUpConsensus(t, 0)
+
+	addMsgHandlerToSTF(t, s, func(ctx context.Context, msg *gogotypes.BoolValue) (*gogotypes.BoolValue, error) {
+		kvSet(t, ctx, "exec")
+		return nil, nil
+	})
+
+	_, err := c.InitChain(context.Background(), &abciproto.InitChainRequest{
+		Time:          time.Now(),
+		ChainId:       "test",
+		InitialHeight: 1,
+	})
+	require.NoError(t, err)
+
+	// empty byte
+	_, err = c.CheckTx(context.Background(), &abciproto.CheckTxRequest{
+		Tx: []byte{},
+	})
+	require.Error(t, err)
+
+	// out of gas
+	res, err := c.CheckTx(context.Background(), &abciproto.CheckTxRequest{
+		Tx: mock.Tx{
+			Sender:   []byte("sender"),
+			Msg:      &gogotypes.BoolValue{Value: true},
+			GasLimit: 100_000,
+		}.Bytes(),
+	})
+	require.NoError(t, err)
+	require.Contains(t, res.Log, "out of gas")
+	require.NotEqual(t, res.Code, 0)
+
+	c, _ = setUpConsensus(t, 100_000)
+	res, err = c.CheckTx(context.Background(), &abciproto.CheckTxRequest{
+		Tx: mock.Tx{
+			Sender:   []byte("sender"),
+			Msg:      &gogotypes.BoolValue{Value: true},
+			GasLimit: 100_000,
+		}.Bytes(),
+	})
+	require.NoError(t, err)
+	require.NotEqual(t, res.GasUsed, 0)
+}
+
 func TestConsensus_ExtendVote(t *testing.T) {
-	c, s := setUpConsensus(t)
+	c, s := setUpConsensus(t, 100_000)
 
 	addQueryHandlerToSTF(t, s, func(ctx context.Context, q *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error) {
 		cParams := &v1.ConsensusParams{
@@ -365,7 +460,7 @@ func TestConsensus_ExtendVote(t *testing.T) {
 }
 
 func TestConsensus_VerifyVoteExtension(t *testing.T) {
-	c, s := setUpConsensus(t)
+	c, s := setUpConsensus(t, 100_000)
 
 	addQueryHandlerToSTF(t, s, func(ctx context.Context, q *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error) {
 		cParams := &v1.ConsensusParams{
@@ -429,7 +524,104 @@ func TestConsensus_VerifyVoteExtension(t *testing.T) {
 	require.Equal(t, res.Status, abciproto.VERIFY_VOTE_EXTENSION_STATUS_ACCEPT)
 }
 
-func setUpConsensus(t *testing.T) (*Consensus[mock.Tx], *stf.STF[mock.Tx]) {
+func TestConsensus_PrepareProposal(t *testing.T) {
+	c, _ := setUpConsensus(t, 100_000)
+
+	// Invalid height
+	_, err := c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 0,
+	})
+	require.Error(t, err)
+
+	// empty handler
+	_, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+	})
+	require.Error(t, err)
+
+	// NoOp handler
+	c.prepareProposalHandler = DefaultServerOptions[mock.Tx]().PrepareProposalHandler
+	_, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		Txs: [][]byte{mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+}
+
+func TestConsensus_PrepareProposal_With_Handler_NoOpMempool(t *testing.T) {
+	c, s := setUpConsensus(t, 100_000)
+
+	addQueryHandlerToSTF(t, s, func(ctx context.Context, q *consensustypes.QueryParamsRequest) (*consensustypes.QueryParamsResponse, error) {
+		cParams := &v1.ConsensusParams{
+			Block: &v1.BlockParams{
+				MaxGas: 300_000,
+			},
+		}
+		return &consensustypes.QueryParamsResponse{
+			Params: cParams,
+		}, nil
+	})
+
+	c.prepareProposalHandler = handlers.NewDefaultProposalHandler(c.mempool).PrepareHandler()
+	
+	// zero MaxTxBytes
+	res, err := c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		MaxTxBytes: 0,
+		Txs: [][]byte{mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(res.Txs), 0)
+
+	// have tx exeed MaxTxBytes
+	// each mock tx has 128 bytes, should select 2 txs
+	res, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		MaxTxBytes: 300,
+		Txs: [][]byte{mockTx.Bytes(), mockTx.Bytes(), mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(res.Txs), 2)
+
+	// reach MaxTxBytes
+	res, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		MaxTxBytes: 256,
+		Txs: [][]byte{mockTx.Bytes(), mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(res.Txs), 2)
+
+	// Over gas, under MaxTxBytes
+	// 300_000 gas limit, should only take 3 txs
+	res, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		MaxTxBytes: 1000,
+		Txs: [][]byte{mockTx.Bytes(), mockTx.Bytes(), mockTx.Bytes(), mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(res.Txs), 3)
+
+	// Reach max gas
+	res, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		MaxTxBytes: 1000,
+		Txs: [][]byte{mockTx.Bytes(), mockTx.Bytes(), mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(res.Txs), 3)
+
+	// have a bad encoding tx
+	res, err = c.PrepareProposal(context.Background(), &abciproto.PrepareProposalRequest{
+		Height: 1,
+		MaxTxBytes: 1000,
+		Txs: [][]byte{mockTx.Bytes(), append(mockTx.Bytes(), []byte("bad")...), mockTx.Bytes()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, len(res.Txs), 2)
+}
+
+func setUpConsensus(t *testing.T, gasLimit uint64) (*Consensus[mock.Tx], *stf.STF[mock.Tx]) {
 	s, err := stf.NewSTF(
 		log.NewNopLogger().With("module", "stf"),
 		stf.NewMsgRouterBuilder(),
@@ -459,9 +651,9 @@ func setUpConsensus(t *testing.T) (*Consensus[mock.Tx], *stf.STF[mock.Tx]) {
 	b := am.Builder[mock.Tx]{
 		STF:                s,
 		DB:                 mockStore,
-		ValidateTxGasLimit: 100_000,
-		QueryGasLimit:      100_000,
-		SimulationGasLimit: 100_000,
+		ValidateTxGasLimit: gasLimit,
+		QueryGasLimit:      gasLimit,
+		SimulationGasLimit: gasLimit,
 		InitGenesis: func(ctx context.Context, src io.Reader, txHandler func(json.RawMessage) error) error {
 			return kvSet(t, ctx, "init-chain")
 		},

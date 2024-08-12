2091ae700d9b4ac80682ebdfa1f0da0d2d942906
sontrinh16 <trinhleson2000@gmail.com>
2024-08-12 13:28:47 +0700

    add validate basic test

--- x/authz/authorization_grant_test.go
+++ x/authz/authorization_grant_test.go
@@ -5,17 +5,11 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+
+	any "github.com/cosmos/gogoproto/types/any"
 )
 
 // TODO: remove and use: robert/expect-error
-func expecError(r *require.Assertions, expected string, received error) {
-	if expected == "" {
-		r.NoError(received)
-	} else {
-		r.Error(received)
-		r.Contains(received.Error(), expected)
-	}
-}
 
 func TestNewGrant(t *testing.T) {
 	a := NewGenericAuthorization("some-type")
@@ -37,6 +31,33 @@ func TestNewGrant(t *testing.T) {
 		tc := tc
 		t.Run(tc.title, func(t *testing.T) {
 			_, err := NewGrant(tc.blockTime, tc.a, tc.expire)
+			expecError(require.New(t), tc.err, err)
+		})
+	}
+}
+
+func TestValidateBasic(t *testing.T) {
+	validAuthz, err := any.NewAnyWithCacheWithValue(NewGenericAuthorization("some-type"))
+	require.NoError(t, err)
+	invalidAuthz, err := any.NewAnyWithCacheWithValue(&Grant{})
+	require.NoError(t, err)
+	tcs := []struct {
+		title         string
+		authorization *any.Any
+		err           string
+	}{
+		{"valid grant", validAuthz, ""},
+		{"invalid authorization", invalidAuthz, "invalid type"},
+		{"empty authorization", nil, "authorization is nil"},
+	}
+
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			grant := Grant{
+				Authorization: tc.authorization,
+			}
+			err := grant.ValidateBasic()
 			expecError(require.New(t), tc.err, err)
 		})
 	}
--- x/authz/keeper/keeper.go
+++ x/authz/keeper/keeper.go
@@ -58,7 +58,7 @@ func (k Keeper) getGrant(ctx context.Context, skey []byte) (grant authz.Grant, f
 	return grant, true
 }
 
-func (k Keeper) update(ctx context.Context, grantee, granter sdk.AccAddress, updated authz.Authorization) error {
+func (k Keeper) updateGrant(ctx context.Context, grantee, granter sdk.AccAddress, updated authz.Authorization) error {
 	skey := grantStoreKey(grantee, granter, updated.MsgTypeURL())
 	grant, found := k.getGrant(ctx, skey)
 	if !found {
@@ -133,7 +133,7 @@ func (k Keeper) DispatchActions(ctx context.Context, grantee sdk.AccAddress, msg
 				if !ok {
 					return nil, fmt.Errorf("expected authz.Authorization but got %T", resp.Updated)
 				}
-				err = k.update(ctx, grantee, granter, updated)
+				err = k.updateGrant(ctx, grantee, granter, updated)
 			}
 			if err != nil {
 				return nil, err

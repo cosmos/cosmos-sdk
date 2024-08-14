b20f0a76bee0f05f7f8209f0d90cd98683d44d18
Aaron Craelius <aaronc@users.noreply.github.com>
2024-08-14 15:45:06 -0400

    docs

--- schema/appdata/async.go
+++ schema/appdata/async.go
@@ -19,11 +19,8 @@ type AsyncListenerOptions struct {
 	DoneWaitGroup *sync.WaitGroup
 }
 
-// AsyncListenerMux returns a listener that forwards received events to all the provided listeners asynchronously
-// with each listener processing in a separate go routine. All callbacks in the returned listener will return nil
-// except for Commit which will return an error or nil once all listeners have processed the commit. The context
-// is used to signal that the listeners should stop listening and return. bufferSize is the size of the buffer for the
-// channels used to send events to the listeners.
+// AsyncListenerMux is a convenience function that calls AsyncListener for each listener
+// with the provided options and combines them using ListenerMux.
 func AsyncListenerMux(opts AsyncListenerOptions, listeners ...Listener) Listener {
 	asyncListeners := make([]Listener, len(listeners))
 	for i, l := range listeners {
@@ -34,12 +31,10 @@ func AsyncListenerMux(opts AsyncListenerOptions, listeners ...Listener) Listener
 
 // AsyncListener returns a listener that forwards received events to the provided listener listening in asynchronously
 // in a separate go routine. The listener that is returned will return nil for all methods including Commit and
-// an error or nil will only be returned in commitChan once the sender has sent commit and the receiving listener has
-// processed it. Thus commitChan can be used as a synchronization and error checking mechanism. The go routine
+// an error or nil will only be returned when the callback returned by Commit is called.
+// Thus Commit() can be used as a synchronization and error checking mechanism. The go routine
 // that is being used for listening will exit when context.Done() returns and no more events will be received by the listener.
 // bufferSize is the size of the buffer for the channel that is used to send events to the listener.
-// Instead of using AsyncListener directly, it is recommended to use AsyncListenerMux which does coordination directly
-// via its Commit callback.
 func AsyncListener(opts AsyncListenerOptions, listener Listener) Listener {
 	commitChan := make(chan error)
 	packetChan := make(chan Packet, opts.BufferSize)

a68cc6aea8519e39041ee582e7e4bd5190da6434
Randy Grok <randygrok@gmail.com>
2024-08-19 21:22:57 +0200

    add context key to get execution context from context

--- server/v2/stf/util.go
+++ server/v2/stf/util.go
@@ -5,10 +5,17 @@ import (
 	"fmt"
 )
 
+var theExecutionContext = 0x1
+
 // getExecutionCtxFromContext tries to get the execution context from the given go context.
 func getExecutionCtxFromContext(ctx context.Context) (*executionContext, error) {
 	if ec, ok := ctx.(*executionContext); ok {
 		return ec, nil
+	}
+
+	value, ok := ctx.Value(theExecutionContext).(*executionContext)
+	if ok {
+		return value, nil
 	}
 
 	return nil, fmt.Errorf("failed to get executionContext from context")

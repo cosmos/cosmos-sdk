7c85b981c82c2d3b2bfc57ac6ecc34974683790d
Facundo Medica <14063057+facundomedica@users.noreply.github.com>
2024-08-26 11:40:54 +0200

    fix: reimplement sig verification for app v2 (#21386)

--- x/auth/tx/config/depinject.go
+++ x/auth/tx/config/depinject.go
@@ -15,6 +15,8 @@ import (
 	txconfigv1 "cosmossdk.io/api/cosmos/tx/config/v1"
 	"cosmossdk.io/core/address"
 	"cosmossdk.io/core/appmodule"
+	appmodulev2 "cosmossdk.io/core/appmodule/v2"
+	"cosmossdk.io/core/transaction"
 	"cosmossdk.io/depinject"
 	"cosmossdk.io/depinject/appconfig"
 	"cosmossdk.io/x/auth/ante"
@@ -49,12 +51,13 @@ type ModuleInputs struct {
 	ProtoFileResolver     txsigning.ProtoFileResolver
 	Environment           appmodule.Environment
 	// BankKeeper is the expected bank keeper to be passed to AnteHandlers
-	BankKeeper             authtypes.BankKeeper               `optional:"true"`
-	MetadataBankKeeper     BankKeeper                         `optional:"true"`
-	AccountKeeper          ante.AccountKeeper                 `optional:"true"`
-	FeeGrantKeeper         ante.FeegrantKeeper                `optional:"true"`
-	CustomSignModeHandlers func() []txsigning.SignModeHandler `optional:"true"`
-	CustomGetSigners       []txsigning.CustomGetSigner        `optional:"true"`
+	BankKeeper               authtypes.BankKeeper               `optional:"true"`
+	MetadataBankKeeper       BankKeeper                         `optional:"true"`
+	AccountKeeper            ante.AccountKeeper                 `optional:"true"`
+	FeeGrantKeeper           ante.FeegrantKeeper                `optional:"true"`
+	AccountAbstractionKeeper ante.AccountAbstractionKeeper      `optional:"true"`
+	CustomSignModeHandlers   func() []txsigning.SignModeHandler `optional:"true"`
+	CustomGetSigners         []txsigning.CustomGetSigner        `optional:"true"`
 }
 
 type ModuleOutputs struct {
@@ -63,6 +66,7 @@ type ModuleOutputs struct {
 	TxConfig        client.TxConfig
 	TxConfigOptions tx.ConfigOptions
 	BaseAppOption   runtime.BaseAppOption // This is only useful for chains using baseapp. Server/v2 chains use TxValidator.
+	Module          appmodule.AppModule
 }
 
 func ProvideProtoRegistry() txsigning.ProtoFileResolver {
@@ -140,7 +144,15 @@ func ProvideModule(in ModuleInputs) ModuleOutputs {
 		app.SetTxEncoder(txConfig.TxEncoder())
 	}
 
-	return ModuleOutputs{TxConfig: txConfig, TxConfigOptions: txConfigOptions, BaseAppOption: baseAppOption}
+	svd := ante.NewSigVerificationDecorator(
+		in.AccountKeeper,
+		txConfig.SignModeHandler(),
+		ante.DefaultSigVerificationGasConsumer,
+		in.AccountAbstractionKeeper,
+	)
+	appModule := AppModule{svd}
+
+	return ModuleOutputs{TxConfig: txConfig, TxConfigOptions: txConfigOptions, BaseAppOption: baseAppOption, Module: appModule}
 }
 
 func newAnteHandler(txConfig client.TxConfig, in ModuleInputs) (sdk.AnteHandler, error) {
@@ -220,3 +232,23 @@ func metadataExists(err error) error {
 
 	return err
 }
+
+var (
+	_ appmodulev2.AppModule                      = AppModule{}
+	_ appmodulev2.HasTxValidator[transaction.Tx] = AppModule{}
+)
+
+type AppModule struct {
+	sigVerification ante.SigVerificationDecorator
+}
+
+// TxValidator implements appmodule.HasTxValidator.
+func (a AppModule) TxValidator(ctx context.Context, tx transaction.Tx) error {
+	return a.sigVerification.ValidateTx(ctx, tx)
+}
+
+// IsAppModule implements appmodule.AppModule.
+func (a AppModule) IsAppModule() {}
+
+// IsOnePerModuleType implements appmodule.AppModule.
+func (a AppModule) IsOnePerModuleType() {}

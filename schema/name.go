099aecfc8c158b4c3651fcb256fda5b512e49639
Aaron Craelius <aaronc@users.noreply.github.com>
2024-08-27 18:16:59 -0400

    revert name format change

--- schema/name.go
+++ schema/name.go
@@ -4,8 +4,8 @@ import "regexp"
 
 // NameFormat is the regular expression that a name must match.
 // A name must start with a letter or underscore and can only contain letters, numbers, and underscores.
-// A name must be at least one character long and can be at most 63 characters long.
-const NameFormat = `^[a-zA-Z_][a-zA-Z0-9_]{0,62}$`
+// A name must be at least one character long and can be at most 64 characters long.
+const NameFormat = `^[a-zA-Z_][a-zA-Z0-9_]{0,63}$`
 
 var nameRegex = regexp.MustCompile(NameFormat)
 

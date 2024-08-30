e2059e4012a31941bcfacc6eda6cbe9c2fd64029
marbar3778 <marbar3778@yahoo.com>
2024-08-30 13:47:01 +0200

    ++

--- x/consensus/exported/exported.go
+++ x/consensus/exported/exported.go
@@ -3,13 +3,13 @@ package exported
 import (
 	"context"
 
-	"cosmossdk.io/x/consensus/types"
+	cmtproto "github.com/cometbft/cometbft/api/cometbft/types/v1"
 )
 
 // ConsensusParamSetter defines the interface fulfilled by BaseApp's
 // ParamStore which allows setting its appVersion field.
 type ConsensusParamSetter interface {
-	Get(ctx context.Context) (types.ConsensusParams, error)
+	Get(ctx context.Context) (cmtproto.ConsensusParams, error)
 	Has(ctx context.Context) (bool, error)
-	Set(ctx context.Context, cp types.ConsensusParams) error
+	Set(ctx context.Context, cp cmtproto.ConsensusParams) error
 }

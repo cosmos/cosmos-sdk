4333d15614e3de9609672f51ac5a88350e112384
marbar3778 <marbar3778@yahoo.com>
2024-08-30 13:38:15 +0200

    demo of removing comet from consenuss

--- x/consensus/exported/exported.go
+++ x/consensus/exported/exported.go
@@ -3,13 +3,13 @@ package exported
 import (
 	"context"
 
-	cmtproto "github.com/cometbft/cometbft/api/cometbft/types/v1"
+	"cosmossdk.io/x/consensus/types"
 )
 
 // ConsensusParamSetter defines the interface fulfilled by BaseApp's
 // ParamStore which allows setting its appVersion field.
 type ConsensusParamSetter interface {
-	Get(ctx context.Context) (cmtproto.ConsensusParams, error)
+	Get(ctx context.Context) (types.ConsensusParams, error)
 	Has(ctx context.Context) (bool, error)
-	Set(ctx context.Context, cp cmtproto.ConsensusParams) error
+	Set(ctx context.Context, cp types.ConsensusParams) error
 }
--- x/consensus/types/msgs.go
+++ x/consensus/types/msgs.go
@@ -16,7 +16,7 @@ func (msg MsgUpdateParams) ToProtoConsensusParams() (cmtproto.ConsensusParams, e
 		return cmtproto.ConsensusParams{}, errors.New("all parameters must be present")
 	}
 
-	if msg.Abci != nil && msg.Feature != nil && msg.Feature.VoteExtensionsEnableHeight != nil {
+	if msg.Feature != nil && msg.Feature.VoteExtensionsEnableHeight != nil {
 		return cmtproto.ConsensusParams{}, errors.New("abci in sections Feature and (deprecated) ABCI cannot be used simultaneously")
 	}
 
@@ -38,14 +38,12 @@ func (msg MsgUpdateParams) ToProtoConsensusParams() (cmtproto.ConsensusParams, e
 		Synchrony: msg.Synchrony,
 	}
 
-	if msg.Abci != nil {
-		if cp.Feature == nil {
-			cp.Feature = &cmtproto.FeatureParams{}
-		}
+	if cp.Feature == nil {
+		cp.Feature = &cmtproto.FeatureParams{}
+	}
 
-		cp.Feature.VoteExtensionsEnableHeight = &types.Int64Value{
-			Value: msg.Abci.VoteExtensionsEnableHeight,
-		}
+	cp.Feature.VoteExtensionsEnableHeight = &types.Int64Value{
+		Value: msg.Abci.VoteExtensionsEnableHeight,
 	}
 
 	return cp, nil

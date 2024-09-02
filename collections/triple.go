bc0f0607bb11b8e1588430bd38041f183061626b
Oren <oren@lavanet.xyz>
2024-09-02 17:29:27 +0300

    changing reverse iterator option to be of Option type and not bool

--- collections/triple.go
+++ collections/triple.go
@@ -273,58 +273,69 @@ func (t tripleKeyCodec[K1, K2, K3]) SizeNonTerminal(key Triple[K1, K2, K3]) int 
 	return size
 }
 
-// NewPrefixUntilTripleRange defines a collection query which ranges until the provided Pair prefix.
-// Unstable: this API might change in the future. Use the first boolean option to get a reverse iterator (reverse=true).
-func NewPrefixUntilTripleRange[K1, K2, K3 any](k1 K1, opts ...bool) Ranger[Triple[K1, K2, K3]] {
-	key := TriplePrefix[K1, K2, K3](k1)
-	order := OrderAscending
-	if len(opts) > 0 {
-		reverse := opts[0]
-		if reverse {
-			order = OrderDescending
-		}
-	}
+// defaultConfig has all the options disabled, except Color and TimeFormat
+var defaultConfig = Config{
+	Order: OrderAscending,
+}
 
-	return &Range[Triple[K1, K2, K3]]{
-		end:   RangeKeyPrefixEnd(key),
-		order: order,
+// Config defines configuration for the Triple collection iterator.
+type Config struct {
+	Order Order
+}
+
+type Option func(*Config)
+
+// OrderOption sets the order for the iterator.
+func OrderOption(order Order) Option {
+	return func(cfg *Config) {
+		cfg.Order = order
 	}
 }
 
-// NewPrefixedTripleRange provides a Range for all keys prefixed with the given
-// first part of the Triple key. Use the first boolean option to get a reverse iterator (reverse=true).
-func NewPrefixedTripleRange[K1, K2, K3 any](k1 K1, opts ...bool) Ranger[Triple[K1, K2, K3]] {
-	key := TriplePrefix[K1, K2, K3](k1)
-	order := OrderAscending
-	if len(opts) > 0 {
-		reverse := opts[0]
-		if reverse {
-			order = OrderDescending
-		}
+// NewPrefixUntilTripleRange defines a collection query which ranges until the provided Pair prefix.
+// Unstable: this API might change in the future.
+func NewPrefixUntilTripleRange[K1, K2, K3 any](k1 K1, options ...Option) Ranger[Triple[K1, K2, K3]] {
+	cfg := defaultConfig
+	for _, opt := range options {
+		opt(&cfg)
 	}
+
+	key := TriplePrefix[K1, K2, K3](k1)
+
+	return &Range[Triple[K1, K2, K3]]{
+		end:   RangeKeyPrefixEnd(key),
+		order: cfg.Order,
+	}
+}
+
+// NewPrefixedTripleRange provides a Range for all keys prefixed with the given first part of the Triple key.
+func NewPrefixedTripleRange[K1, K2, K3 any](k1 K1, options ...Option) Ranger[Triple[K1, K2, K3]] {
+	cfg := defaultConfig
+	for _, opt := range options {
+		opt(&cfg)
+	}
+
+	key := TriplePrefix[K1, K2, K3](k1)
 
 	return &Range[Triple[K1, K2, K3]]{
 		start: RangeKeyExact(key),
 		end:   RangeKeyPrefixEnd(key),
-		order: order,
+		order: cfg.Order,
 	}
 }
 
 // NewSuperPrefixedTripleRange provides a Range for all keys prefixed with the given
-// first and second parts of the Triple key. Use the first boolean option to get a reverse iterator (reverse=true).
-func NewSuperPrefixedTripleRange[K1, K2, K3 any](k1 K1, k2 K2, opts ...bool) Ranger[Triple[K1, K2, K3]] {
+// first and second parts of the Triple key.
+func NewSuperPrefixedTripleRange[K1, K2, K3 any](k1 K1, k2 K2, options ...Option) Ranger[Triple[K1, K2, K3]] {
 	key := TripleSuperPrefix[K1, K2, K3](k1, k2)
-	order := OrderAscending
-	if len(opts) > 0 {
-		reverse := opts[0]
-		if reverse {
-			order = OrderDescending
-		}
+	cfg := defaultConfig
+	for _, opt := range options {
+		opt(&cfg)
 	}
 
 	return &Range[Triple[K1, K2, K3]]{
 		start: RangeKeyExact(key),
 		end:   RangeKeyPrefixEnd(key),
-		order: order,
+		order: cfg.Order,
 	}
 }

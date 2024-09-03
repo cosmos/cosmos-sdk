33665d581934ec07a2ff088ef92d17eb2b4ffe59
Facundo <facundomedica@gmail.com>
2024-09-03 15:35:12 +0200

    double-check this, not sure

--- types/mempool/sender_nonce.go
+++ types/mempool/sender_nonce.go
@@ -146,6 +146,15 @@ func (snm *SenderNonceMempool) Insert(_ context.Context, tx sdk.Tx) error {
 		snm.senders[sender] = senderTxs
 	}
 
+	// if it's an unordered tx, we use the gas instead of the nonce
+	if unordered, ok := tx.(sdk.TxWithUnordered); ok && unordered.GetUnordered() {
+		gasLimit, err := unordered.GetGasLimit()
+		nonce = gasLimit
+		if err != nil {
+			return err
+		}
+	}
+
 	senderTxs.Set(nonce, tx)
 
 	key := txKey{nonce: nonce, address: sender}
@@ -212,6 +221,15 @@ func (snm *SenderNonceMempool) Remove(tx sdk.Tx) error {
 	sig := sigs[0]
 	sender := sdk.AccAddress(sig.PubKey.Address()).String()
 	nonce := sig.Sequence
+
+	// if it's an unordered tx, we use the gas instead of the nonce
+	if unordered, ok := tx.(sdk.TxWithUnordered); ok && unordered.GetUnordered() {
+		gasLimit, err := unordered.GetGasLimit()
+		nonce = gasLimit
+		if err != nil {
+			return err
+		}
+	}
 
 	senderTxs, found := snm.senders[sender]
 	if !found {

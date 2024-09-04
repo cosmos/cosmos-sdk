d9fe1e4e87c18a91192ed6552d5f7b38c0003ac5
Julien Robert <julien@rbrt.fr>
2024-09-04 16:39:46 +0200

    updates

--- tests/systemtests/unordered_tx_test.go
+++ tests/systemtests/unordered_tx_test.go
@@ -12,6 +12,8 @@ import (
 )
 
 func TestUnorderedTXDuplicate(t *testing.T) {
+	t.Skip("The unordered tx handling is not wired in v2")
+
 	// scenario: test unordered tx duplicate
 	// given a running chain with a tx in the unordered tx pool
 	// when a new tx with the same hash is broadcasted

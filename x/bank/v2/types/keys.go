f472be14a31ee90bca003868c85d4c008533db57
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-09-09 20:32:30 +0700

    add store key

--- x/bank/v2/types/keys.go
+++ x/bank/v2/types/keys.go
@@ -12,6 +12,9 @@ const (
 	// ModuleName is the name of the module
 	ModuleName = "bankv2"
 
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
 	// GovModuleName duplicates the gov module's name to avoid a dependency with x/gov.
 	GovModuleName = "gov"
 

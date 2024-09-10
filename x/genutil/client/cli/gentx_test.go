ae68ee1a34706e447d2b76f0a6a1d375188910d9
sontrinh16 <trinhleson2000@gmail.com>
2024-09-10 15:27:56 +0700

    minor

--- x/genutil/client/cli/gentx_test.go
+++ x/genutil/client/cli/gentx_test.go
@@ -55,9 +55,7 @@ func (s *CLITestSuite) SetupSuite() {
 
 	ctxGen := func() client.Context {
 		bz, _ := s.encCfg.Codec.Marshal(&sdk.TxResponse{})
-		c := clitestutil.NewMockCometRPC(abci.QueryResponse{
-			Value: bz,
-		})
+		c := clitestutil.NewMockCometRPCWithResponseQueryValue(bz)
 		return s.baseCtx.WithClient(c)
 	}
 	s.clientCtx = ctxGen()

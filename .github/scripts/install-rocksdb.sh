c57156ce1f0f64519b0c2bff2874cdb1d6b261e4
auricom <27022259+auricom@users.noreply.github.com>
2024-09-11 15:05:14 +0200

    ci: centralized job for rocksdb libaries cache

--- .github/scripts/install-rocksdb.sh
+++ .github/scripts/install-rocksdb.sh
@@ -6,9 +6,6 @@ if [ -z "$ROCKSDB_VERSION" ]; then
   exit 1
 fi
 
-# Update and install dependencies
-sudo apt update && sudo apt-get install -y libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev build-essential
-
 # Clone RocksDB repository
 git clone https://github.com/facebook/rocksdb.git /home/runner/rocksdb
 cd /home/runner/rocksdb || exit 1

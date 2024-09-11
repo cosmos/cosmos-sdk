9edd09b4b79855269f56f915d2cf191a4d5c1589
akhilkumarpilli <akhilkumar7947@gmail.com>
2024-09-11 12:38:13 +0530

    add bank grpc query tests

--- tests/systemtests/bank_test.go
+++ tests/systemtests/bank_test.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"testing"
 
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+
+	"github.com/cosmos/cosmos-sdk/testutil"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func TestBankSendTxCmd(t *testing.T) {
@@ -30,9 +33,9 @@ func TestBankSendTxCmd(t *testing.T) {
 	sut.StartChain(t)
 
 	// query accounts balances
-	balance1 := cli.QueryBalance(account1Addr, "stake")
+	balance1 := cli.QueryBalance(account1Addr, denom)
 	assert.Equal(t, int64(initialAmount), balance1)
-	balance2 := cli.QueryBalance(account2Addr, "stake")
+	balance2 := cli.QueryBalance(account2Addr, denom)
 	assert.Equal(t, int64(initialAmount), balance2)
 
 	bankSendCmdArgs := []string{"tx", "bank", "send", account1Addr, account2Addr, "1000stake"}
@@ -209,7 +212,6 @@ func TestBankMultiSendTxCmd(t *testing.T) {
 		tc := tc
 
 		t.Run(tc.name, func(t *testing.T) {
-
 			if tc.expectErr {
 				assertErr := func(xt assert.TestingT, gotErr error, gotOutputs ...interface{}) bool {
 					assert.Len(t, gotOutputs, 1)
@@ -239,14 +241,154 @@ func TestBankGRPCQueries(t *testing.T) {
 
 	sut.ResetChain(t)
 	cli := NewCLIWrapper(t, sut, verbose)
+
+	// update denom metadata in bank genesis
+	atomDenomMetadata := `{"description":"The native staking token of the Cosmos Hub.","denom_units":[{"denom":"uatom","exponent":0,"aliases":["microatom"]},{"denom":"atom","exponent":6,"aliases":["ATOM"]}],"base":"uatom","display":"atom","name":"Cosmos Hub Atom","symbol":"ATOM","uri":"","uri_hash":""}`
+	ethDenomMetadata := `{"description":"Ethereum mainnet token","denom_units":[{"denom":"wei","exponent":0,"aliases":[]},{"denom":"eth","exponent":6,"aliases":["ETH"]}],"base":"wei","display":"eth","name":"Ethereum","symbol":"ETH","uri":"","uri_hash":""}`
+
+	bankDenomMetadata := fmt.Sprintf("[%s,%s]", atomDenomMetadata, ethDenomMetadata)
+
+	sut.ModifyGenesisJSON(t, func(genesis []byte) []byte {
+		state, err := sjson.SetRawBytes(genesis, "app_state.bank.denom_metadata", []byte(bankDenomMetadata))
+		assert.NoError(t, err)
+		return state
+	})
+
 	// add genesis account with some tokens
 	account1Addr := cli.AddKey("account1")
-	account2Addr := cli.AddKey("account2")
-	require.NotEqual(t, account1Addr, account2Addr)
+	newDenom := "newdenom"
+	initialAmount := "10000000"
 	sut.ModifyGenesisCLI(t,
-		[]string{"genesis", "add-genesis-account", account1Addr, "10000000stake"},
+		[]string{"genesis", "add-genesis-account", account1Addr, "10000000stake," + initialAmount + newDenom},
 	)
 
+	// start chain
 	sut.StartChain(t)
+	baseurl := fmt.Sprintf("http://localhost:%d", apiPortStart)
 
+	// test supply grpc endpoint
+	supplyUrl := baseurl + "/cosmos/bank/v1beta1/supply"
+
+	defaultExpSupplyOutput := `{"supply":[{"denom":"newdenom","amount":"10000000"},{"denom":"stake","amount":"2010000191"},{"denom":"testtoken","amount":"4000000000"}],"pagination":{"next_key":null,"total":"3"}}`
+	specificDenomOutput := fmt.Sprintf(`{"denom":"%s","amount":"%s"}`, newDenom, initialAmount)
+	bogusDenomOutput := `{"denom":"foobar","amount":"0"}`
+
+	blockHeightHeader := "x-cosmos-block-height"
+	blockHeight := sut.CurrentHeight()
+
+	supplyTestCases := []struct {
+		name    string
+		url     string
+		headers map[string]string
+		expOut  string
+	}{
+		{
+			"test GRPC total supply",
+			supplyUrl,
+			map[string]string{
+				blockHeightHeader: fmt.Sprintf("%d", blockHeight),
+			},
+			defaultExpSupplyOutput,
+		},
+		{
+			"test GRPC total supply of a specific denom",
+			supplyUrl + "/by_denom?denom=" + newDenom,
+			map[string]string{},
+			specificDenomOutput,
+		},
+		{
+			"error when querying supply with height greater than block height",
+			supplyUrl,
+			map[string]string{
+				blockHeightHeader: fmt.Sprintf("%d", blockHeight+5),
+			},
+			"invalid height",
+		},
+		{
+			"test GRPC total supply of a bogus denom",
+			supplyUrl + "/by_denom?denom=foobar",
+			map[string]string{},
+			bogusDenomOutput,
+		},
+	}
+
+	for _, tc := range supplyTestCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+			assert.NoError(t, err)
+			assert.Contains(t, string(resp), tc.expOut)
+		})
+	}
+
+	// test denom metadata endpoint
+	denomMetadataUrl := baseurl + "/cosmos/bank/v1beta1/denoms_metadata"
+	dmTestCases := []struct {
+		name   string
+		url    string
+		expOut string
+	}{
+		{
+			"test GRPC client metadata",
+			denomMetadataUrl,
+			bankDenomMetadata,
+		},
+		{
+			"test GRPC client metadata of a specific denom",
+			denomMetadataUrl + "/uatom",
+			atomDenomMetadata,
+		},
+		{
+			"test GRPC client metadata of a bogus denom",
+			denomMetadataUrl + "/foobar",
+			`{"code":5,"message":"client metadata for denom foobar","details":[]}`,
+		},
+	}
+
+	for _, tc := range dmTestCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := testutil.GetRequest(tc.url)
+			assert.NoError(t, err)
+			assert.Contains(t, string(resp), tc.expOut)
+		})
+	}
+
+	// test bank balances endpoint
+	balanceUrl := baseurl + "/cosmos/bank/v1beta1/balances/"
+	allBalancesOutput := `{"balances":[` + specificDenomOutput + `,{"denom":"stake","amount":"10000000"}],"pagination":{"next_key":null,"total":"2"}}`
+
+	balanceTestCases := []struct {
+		name   string
+		url    string
+		expOut string
+	}{
+		{
+			"test GRPC total account balance",
+			balanceUrl + account1Addr,
+			allBalancesOutput,
+		},
+		{
+			"test GRPC account balance of a specific denom",
+			fmt.Sprintf("%s%s/by_denom?denom=%s", balanceUrl, account1Addr, newDenom),
+			specificDenomOutput,
+		},
+		{
+			"test GRPC account balance of a bogus denom",
+			fmt.Sprintf("%s%s/by_denom?denom=foobar", balanceUrl, account1Addr),
+			bogusDenomOutput,
+		},
+	}
+
+	for _, tc := range balanceTestCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := testutil.GetRequest(tc.url)
+			assert.NoError(t, err)
+			assert.Contains(t, string(resp), tc.expOut)
+		})
+	}
 }

4f5deceff6860084e16754d40b8defd7add73a21
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-09-11 22:00:22 +0700

    unused

--- x/bank/v2/types/expected_keepers.go
+++ x/bank/v2/types/expected_keepers.go
@@ -9,7 +9,6 @@ import (
 // AuthKeeper defines the account contract that must be fulfilled when
 // creating a x/bank keeper.
 type AuthKeeper interface {
-	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
 	GetModuleAccount(ctx context.Context, moduleName string) sdk.ModuleAccountI
 	GetModuleAddress(moduleName string) sdk.AccAddress
 }

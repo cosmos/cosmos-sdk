99c2a5c301b3f39c066980bd12dfe8388f90487d
Matt Kocubinski <mkocubinski@gmail.com>
2024-09-11 11:27:42 -0500

    add comment

--- core/server/config.go
+++ core/server/config.go
@@ -1,5 +1,7 @@
 package server
 
+// DynamicConfig defines an interface for configuration that can be dynamically
+// fetched at runtime by an arbitrary key.
 type DynamicConfig interface {
 	Get(string) any
 	GetString(string) string

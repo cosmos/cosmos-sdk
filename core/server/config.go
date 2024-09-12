93078d72e16565d43312a6e7be8a9100681897c8
Julien Robert <julien@rbrt.fr>
2024-09-12 10:29:23 +0200

    remove core

--- core/server/config.go
+++
@@ -1,8 +0,0 @@
-package server
-
-// DynamicConfig defines an interface for configuration that can be dynamically
-// fetched at runtime by an arbitrary key.
-type DynamicConfig interface {
-	Get(string) any
-	GetString(string) string
-}

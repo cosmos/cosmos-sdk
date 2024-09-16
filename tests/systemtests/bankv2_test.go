36d8752646a25bfa5e7cbb7c7176707850a3a171
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2024-09-16 19:23:35 +0700

    test valid case

---
+++ tests/systemtests/bankv2_test.go
@@ -0,0 +1,41 @@
+//go:build system_test
+
+package systemtests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+)
+
+func TestBankV2SendTxCmd(t *testing.T) {
+	// scenario: test bank send command
+	// given a running chain
+
+	sut.ResetChain(t)
+	cli := NewCLIWrapper(t, sut, verbose)
+
+	// get validator address
+	valAddr := gjson.Get(cli.Keys("keys", "list"), "1.address").String()
+	require.NotEmpty(t, valAddr)
+
+	// add new key
+	receiverAddr := cli.AddKey("account1")
+	denom := "stake"
+	sut.StartChain(t)
+
+	// query validator balance and make sure it has enough balance
+	var transferAmount int64 = 1000
+	valBalance := cli.QueryBalance(valAddr, denom)
+	require.Greater(t, valBalance, transferAmount, "not enough balance found with validator")
+
+	bankSendCmdArgs := []string{"tx", "bankv2", "send", valAddr, receiverAddr, fmt.Sprintf("%d%s", transferAmount, denom)}
+
+	// test valid transaction
+	rsp := cli.Run(append(bankSendCmdArgs, "--fees=1stake")...)
+	txResult, found := cli.AwaitTxCommitted(rsp)
+	require.True(t, found)
+	RequireTxSuccess(t, txResult)
+}

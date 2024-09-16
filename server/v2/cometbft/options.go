741842f6ad9f036a8e64e0d797139c1cd7b6b743
Julien Robert <julien@rbrt.fr>
2024-09-16 10:19:01 +0200

    feat(server/v2/cometbft): wire mempool config

--- server/v2/cometbft/options.go
+++ server/v2/cometbft/options.go
@@ -9,14 +9,15 @@ import (
 )
 
 // ServerOptions defines the options for the CometBFT server.
+// Options are func that are able to take CometBFT app.toml section config and config.toml config.
 type ServerOptions[T transaction.Tx] struct {
-	Mempool                    mempool.Mempool[T]
+	Mempool                    func(cfg map[string]any) mempool.Mempool[T]
 	PrepareProposalHandler     handlers.PrepareHandler[T]
 	ProcessProposalHandler     handlers.ProcessHandler[T]
 	VerifyVoteExtensionHandler handlers.VerifyVoteExtensionhandler
 	ExtendVoteHandler          handlers.ExtendVoteHandler
 
-	SnapshotOptions snapshots.SnapshotOptions
+	SnapshotOptions func(cfg map[string]any) snapshots.SnapshotOptions
 
 	AddrPeerFilter types.PeerFilter // filter peers by address and port
 	IdPeerFilter   types.PeerFilter // filter peers by node ID
@@ -26,12 +27,12 @@ type ServerOptions[T transaction.Tx] struct {
 // It defaults to a NoOpMempool and NoOp handlers.
 func DefaultServerOptions[T transaction.Tx]() ServerOptions[T] {
 	return ServerOptions[T]{
-		Mempool:                    mempool.NoOpMempool[T]{},
+		Mempool:                    func(cfg map[string]any) mempool.Mempool[T] { return mempool.NoOpMempool[T]{} },
 		PrepareProposalHandler:     handlers.NoOpPrepareProposal[T](),
 		ProcessProposalHandler:     handlers.NoOpProcessProposal[T](),
 		VerifyVoteExtensionHandler: handlers.NoOpVerifyVoteExtensionHandler(),
 		ExtendVoteHandler:          handlers.NoOpExtendVote(),
-		SnapshotOptions:            snapshots.NewSnapshotOptions(0, 0),
+		SnapshotOptions:            func(cfg map[string]any) snapshots.SnapshotOptions { return snapshots.NewSnapshotOptions(0, 0) },
 		AddrPeerFilter:             nil,
 		IdPeerFilter:               nil,
 	}

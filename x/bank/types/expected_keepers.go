e8e767ab79c862fbe704ddc219545fd096381b32
Mantrachain Development Support <gcp-security-admins+github-user-mantrachain@mantra.finance>
2024-09-16 20:23:18 +0900

    add neutron bank (#3)

--- x/bank/types/expected_keepers.go
+++ x/bank/types/expected_keepers.go
@@ -33,3 +33,15 @@ type AccountKeeper interface {
 	SetModuleAccount(ctx context.Context, macc sdk.ModuleAccountI)
 	GetModulePermissions() map[string]types.PermissionsForAddress
 }
+
+// Event Hooks
+// These can be utilized to communicate between a bank keeper and another
+// keeper which must take particular actions when sends happen.
+// The second keeper must implement this interface, which then the
+// bank keeper can call.
+
+// BankHooks event hooks for bank sends
+type BankHooks interface {
+	TrackBeforeSend(ctx context.Context, from, to sdk.AccAddress, amount sdk.Coins)       // Must be before any send is executed
+	BlockBeforeSend(ctx context.Context, from, to sdk.AccAddress, amount sdk.Coins) error // Must be before any send is executed
+}
---
+++ x/bank/types/hooks.go
@@ -0,0 +1,33 @@
+package types
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// MultiBankHooks combine multiple bank hooks, all hook functions are run in array sequence
+type MultiBankHooks []BankHooks
+
+// NewMultiBankHooks takes a list of BankHooks and returns a MultiBankHooks
+func NewMultiBankHooks(hooks ...BankHooks) MultiBankHooks {
+	return hooks
+}
+
+// TrackBeforeSend runs the TrackBeforeSend hooks in order for each BankHook in a MultiBankHooks struct
+func (h MultiBankHooks) TrackBeforeSend(ctx context.Context, from, to sdk.AccAddress, amount sdk.Coins) {
+	for i := range h {
+		h[i].TrackBeforeSend(ctx, from, to, amount)
+	}
+}
+
+// BlockBeforeSend runs the BlockBeforeSend hooks in order for each BankHook in a MultiBankHooks struct
+func (h MultiBankHooks) BlockBeforeSend(ctx context.Context, from, to sdk.AccAddress, amount sdk.Coins) error {
+	for i := range h {
+		err := h[i].BlockBeforeSend(ctx, from, to, amount)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}

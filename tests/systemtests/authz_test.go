5a85f1102b3ead9395b1925e8c5be860c47a3728
akhilkumarpilli <akhilkumar7947@gmail.com>
2024-09-17 16:57:16 +0530

    wip: tests

--- tests/systemtests/authz_test.go
+++ tests/systemtests/authz_test.go
@@ -1,15 +1,19 @@
 package systemtests
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tidwall/gjson"
 )
 
-func TestExecAuthorizationCmd(t *testing.T) {
-	// scenario: test authz exec command
+// const (
+// 	msgVoteTypeURL:= `/cosmos.gov.v1.MsgVote`
+// )
+
+func TestAuthzGrantTxCmd(t *testing.T) {
+	// scenario: test authz grant command
 	// given a running chain
 
 	sut.ResetChain(t)
@@ -19,20 +23,54 @@ func TestExecAuthorizationCmd(t *testing.T) {
 	valAddr := gjson.Get(cli.Keys("keys", "list"), "1.address").String()
 	require.NotEmpty(t, valAddr)
 
-	// add few more keys
+	// add one more key
 	account1Addr := cli.AddKey("account1")
-	account2Addr := cli.AddKey("account2")
-	require.NotEqual(t, account1Addr, account2Addr)
-
-	denom := "stake"
-	var initialAmount int64 = 10000000
-	initialBalance := fmt.Sprintf("%d%s", initialAmount, denom)
-	sut.ModifyGenesisCLI(t,
-		[]string{"genesis", "add-genesis-account", account1Addr, initialBalance},
-	)
 	sut.StartChain(t)
 
-	// query accounts balances
-	account1Bal := cli.QueryBalance(account1Addr, denom)
-	require.Equal(t, initialAmount, account1Bal)
+	grantCmdArgs := []string{"tx", "authz", "grant", account1Addr, "--from", valAddr}
+
+	// test grant command
+	testCases := []struct {
+		name      string
+		cmdArgs   []string
+		expectErr bool
+		expErrMsg string
+	}{
+		{
+			"not enough arguments",
+			[]string{},
+			true,
+			"accepts 2 arg(s), received 1",
+		},
+		{
+			"invalid authorization type",
+			[]string{"spend"},
+			true,
+			"invalid authorization type",
+		},
+		{
+			"send authorization without spend-limit",
+			[]string{"send"},
+			true,
+			"spend-limit should be greater than zero",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cmd := append(grantCmdArgs, tc.cmdArgs...)
+			if tc.expectErr {
+				assertErr := func(_ assert.TestingT, gotErr error, gotOutputs ...interface{}) bool {
+					require.Len(t, gotOutputs, 1)
+					output := gotOutputs[0].(string)
+					require.Contains(t, output, tc.expErrMsg)
+					return false
+				}
+				_ = cli.WithRunErrorMatcher(assertErr).Run(cmd...)
+			} else {
+				rsp := cli.RunAndWait(cmd...)
+				RequireTxSuccess(t, rsp)
+			}
+		})
+	}
 }

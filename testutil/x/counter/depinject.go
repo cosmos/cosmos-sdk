2a33f42bfbed05792aaa4ef1577bcccc79330055
Eric Mokaya <emagembe@gmail.com>
2024-09-25 01:30:06 +0300

    feat(testutil): add hooks and change AppModule.Keeper to pointer

--- testutil/x/counter/depinject.go
+++ testutil/x/counter/depinject.go
@@ -1,6 +1,10 @@
 package counter
 
 import (
+	"fmt"
+	"maps"
+	"slices"
+
 	"cosmossdk.io/core/appmodule"
 	"cosmossdk.io/depinject"
 	"cosmossdk.io/depinject/appconfig"
@@ -17,6 +21,7 @@ func (am AppModule) IsOnePerModuleType() {}
 func init() {
 	appconfig.RegisterModule(
 		&types.Module{},
+		appconfig.Invoke(InvokeSetHooks),
 		appconfig.Provide(ProvideModule),
 	)
 }
@@ -31,7 +36,7 @@ type ModuleInputs struct {
 type ModuleOutputs struct {
 	depinject.Out
 
-	Keeper keeper.Keeper
+	Keeper *keeper.Keeper
 	Module appmodule.AppModule
 }
 
@@ -43,4 +48,25 @@ func ProvideModule(in ModuleInputs) ModuleOutputs {
 		Keeper: k,
 		Module: m,
 	}
+}
+
+func InvokeSetHooks(keeper *keeper.Keeper, counterHooks map[string]types.CounterHooksWrapper) error {
+	if keeper == nil || counterHooks == nil {
+		return nil
+	}
+
+	// Default ordering is lexical by module name.
+	// Explicit ordering can be added to the module config if required.
+	modNames := slices.Sorted(maps.Keys(counterHooks))
+	var multiHooks types.MultiCounterHooks
+	for _, modName := range modNames {
+		hook, ok := counterHooks[modName]
+		if !ok {
+			return fmt.Errorf("can't find hooks for module %s", modName)
+		}
+		multiHooks = append(multiHooks, hook)
+	}
+
+	keeper.SetHooks(multiHooks)
+	return nil
 }

2d997402219f4d0ca27d78d90db1c48435a10116
akhilkumarpilli <akhilkumar7947@gmail.com>
2024-09-25 17:32:41 +0530

    add grpc request with headers method

--- tests/systemtests/rest_cli.go
+++ tests/systemtests/rest_cli.go
@@ -27,3 +27,24 @@ func RunGRPCQueries(t *testing.T, testCases []GRPCTestCase) {
 		})
 	}
 }
+
+type GRPCTestCaseWithHeaders struct {
+	name    string
+	url     string
+	headers map[string]string
+	expOut  string
+}
+
+// RunGRPCQueriesWithHeaders runs given grpc testcases by making requests with headers and
+// checking response with expected output
+func RunGRPCQueriesWithHeaders(t *testing.T, testCases []GRPCTestCaseWithHeaders) {
+	t.Helper()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := testutil.GetRequestWithHeaders(tc.url, tc.headers)
+			require.NoError(t, err)
+			require.JSONEq(t, tc.expOut, string(resp))
+		})
+	}
+}

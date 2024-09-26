d258cffbd79ffa3bfc785cd0702d8f2e8ff4743b
Bastien Rigaud <bastienrigaud@mbp-de-bastien.home>
2024-09-26 15:24:50 +0200

    fix: stdout

--- tools/cosmovisor/cmd/cosmovisor/show_upgrade.go
+++ tools/cosmovisor/cmd/cosmovisor/show_upgrade.go
@@ -12,14 +12,19 @@ func NewShowUpgradeInfoCmd() *cobra.Command {
 		Use:          "show-upgrade-info <path to executable>",
 		Short:        "Show upgrade-info.json into stdout.",
 		SilenceUsage: true,
+		Args:         cobra.ExactArgs(1),
 		RunE:         showUpgradeInfoCmd,
 	}
 
 	return showUpgradeInfo
 }
 
-func showUpgradeInfoCmd(cmd *cobra.Command, path string) error {
-	data, err := os.ReadFile(path)
+func showUpgradeInfoCmd(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("expected exactly one argument")
+	}
+
+	data, err := os.ReadFile(args[0])
 	if err != nil {
 		return fmt.Errorf("failed to read upgrade-info.json: %w", err)
 	}

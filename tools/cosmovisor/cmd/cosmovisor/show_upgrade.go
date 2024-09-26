aa7f8b1172bd05ca4846b06440bd07c22ba17317
Bastien Rigaud <bastienrigaud@mbp-de-bastien.home>
2024-09-26 16:01:39 +0200

    fix: feedbacks

--- tools/cosmovisor/cmd/cosmovisor/show_upgrade.go
+++ tools/cosmovisor/cmd/cosmovisor/show_upgrade.go
@@ -9,27 +9,28 @@ import (
 
 func NewShowUpgradeInfoCmd() *cobra.Command {
 	showUpgradeInfo := &cobra.Command{
-		Use:          "show-upgrade-info <path to executable>",
+		Use:          "show-upgrade-info",
 		Short:        "Show upgrade-info.json into stdout.",
 		SilenceUsage: true,
-		Args:         cobra.ExactArgs(1),
 		RunE:         showUpgradeInfoCmd,
 	}
 
 	return showUpgradeInfo
 }
 
-func showUpgradeInfoCmd(cmd *cobra.Command, args []string) error {
-	if len(args) != 1 {
-		return fmt.Errorf("expected exactly one argument")
-	}
-
-	data, err := os.ReadFile(args[0])
+func showUpgradeInfoCmd(cmd *cobra.Command) error {
+	data, err := os.ReadFile(cfg.UpgradeInfoFilePath())
 	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("upgrade-info.json not found at %s: %w", args[0], err)
+		}
 		return fmt.Errorf("failed to read upgrade-info.json: %w", err)
 	}
 
-	fmt.Println(string(data))
+	_, err = fmt.Fprintln(cmd.OutOrStdout(), string(data))
+	if err != nil {
+		return fmt.Errorf("failed to write output: %w", err)
+	}
 
 	return nil
 }

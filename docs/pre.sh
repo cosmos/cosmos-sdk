ec2e1f701624f8af32d73c307e6490e641c87775
Julien Robert <julien@rbrt.fr>
2024-10-07 22:08:23 +0200

    docs: prepare 0.52 docs deployment

--- docs/pre.sh
+++ docs/pre.sh
@@ -26,15 +26,16 @@ cp -r ../x/auth/tx/README.md ./build/modules/auth/2-tx.md
 cat ../x/README.md | sed 's/\.\.\/\/build\/building-modules\/README\.md/\/building-modules\/intro\.html/g' > ./build/modules/README.md
 
 ## Add tooling documentation
-cp ../tools/cosmovisor/README.md ./build/tooling/01-cosmovisor.md
-cp ../tools/confix/README.md ./build/tooling/02-confix.md
-cp ../tools/hubl/README.md ./build/tooling/03-hubl.md
+wget -O ./build/tooling/01-cosmovisor.md https://raw.githubusercontent.com/cosmos/cosmos-sdk/main/tools/cosmovisor/README.md
+wget -O ./build/tooling/02-confix.md https://raw.githubusercontent.com/cosmos/cosmos-sdk/main/tools/confix/README.md
+wget -O ./build/tooling/03-hubl.md https://raw.githubusercontent.com/cosmos/cosmos-sdk/main/tools/hubl/README.md
 
 ## Add package documentation
 cp ../client/v2/README.md ./learn/advanced/17-autocli.md
-cp ../depinject/README.md ./build/packages/01-depinject.md
-cp ../collections/README.md ./build/packages/02-collections.md
-cp ../orm/README.md ./build/packages/03-orm.md
+
+wget -O ./build/packages/01-depinject.md https://raw.githubusercontent.com/cosmos/cosmos-sdk/main/depinject/README.md
+wget -O ./build/packages/02-collections.md https://raw.githubusercontent.com/cosmos/cosmos-sdk/main/tools/collections/README.md
+wget -O ./build/packages/03-orm.md https://raw.githubusercontent.com/cosmos/cosmos-sdk/main/tools/orm/README.md
 
 ## Add architecture documentation
 cp -r ./architecture ./build

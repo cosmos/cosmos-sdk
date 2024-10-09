fae85e0ec41016a027321715f30525d630479f75
Julien Robert <julien@rbrt.fr>
2024-10-09 18:56:09 +0200

    docs: fix pre.sh (#22194)

--- docs/post.sh
+++ docs/post.sh
@@ -7,7 +7,6 @@ rm -rf build/tooling/03-hubl.md
 rm -rf build/packages/01-depinject.md
 rm -rf build/packages/02-collections.md
 rm -rf build/packages/03-orm.md
-rm -rf learn/advaced-concepts/17-autocli.md
 rm -rf build/architecture
 rm -rf build/spec
 rm -rf build/rfc

441141a779ff44c4772f71416f389408a83941f1
Matt Kocubinski <mkocubinski@gmail.com>
2024-10-10 09:20:24 -0500

    lint fix

--- simapp/v2/simdv2/cmd/root_di.go
+++ simapp/v2/simdv2/cmd/root_di.go
@@ -14,6 +14,7 @@ import (
 	"cosmossdk.io/log"
 	"cosmossdk.io/runtime/v2"
 	"cosmossdk.io/simapp/v2"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/config"
 	nodeservice "github.com/cosmos/cosmos-sdk/client/grpc/node"

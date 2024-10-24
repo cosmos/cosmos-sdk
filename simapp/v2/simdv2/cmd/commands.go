63a234ed4f6e69fc09a02e1380c4031944188b0b
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-10-24 04:32:38 -0400

    refactor(simdv2): allow non-comet server components (#22351)
    (cherry picked from commit 6e6255df1f559826761d9f462d5b44fac81dde1e)

--- simapp/v2/simdv2/cmd/commands.go
+++ simapp/v2/simdv2/cmd/commands.go
@@ -16,7 +16,6 @@ import (
 	serverv2 "cosmossdk.io/server/v2"
 	"cosmossdk.io/server/v2/api/grpc"
 	"cosmossdk.io/server/v2/api/telemetry"
-	"cosmossdk.io/server/v2/cometbft"
 	serverstore "cosmossdk.io/server/v2/store"
 	"cosmossdk.io/simapp/v2"
 	confixcmd "cosmossdk.io/tools/confix/cmd"
@@ -42,8 +41,8 @@ func newApp[T transaction.Tx](logger log.Logger, viper *viper.Viper) serverv2.Ap
 
 func initRootCmd[T transaction.Tx](
 	rootCmd *cobra.Command,
-	txConfig client.TxConfig,
 	moduleManager *runtimev2.MM[T],
+	consensusComponent serverv2.ServerComponent[T],
 ) {
 	cfg := sdk.GetConfig()
 	cfg.Seal()
@@ -69,11 +68,7 @@ func initRootCmd[T transaction.Tx](
 		rootCmd,
 		newApp,
 		initServerConfig(),
-		cometbft.New(
-			&genericTxDecoder[T]{txConfig},
-			initCometOptions[T](),
-			initCometConfig(),
-		),
+		consensusComponent,
 		grpc.New[T](),
 		serverstore.New[T](),
 		telemetry.New[T](),

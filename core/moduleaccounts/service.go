3628d4741fb9fc5a399c071a83b139199da62dca
Facundo <facundomedica@gmail.com>
2024-10-25 20:01:24 -0300

    remove extra methods from service

--- core/moduleaccounts/service.go
+++ core/moduleaccounts/service.go
@@ -1,20 +1,9 @@
 package moduleaccounts
 
-import (
-	"context"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 type Service interface {
 	Register(name string, perms []string) error
 	Address(name string) []byte         // TODO: should we return an empty byte slice if it wasn't registered or should we just register it?
 	IsModuleAccount(addr []byte) string // Needed in burn coins and ante handler
-	// AllAccounts() map[string][]byte
-
-	// TODO: @facu, remove these two methods
-	GetModuleAddress(moduleName string) sdk.AccAddress                // TODO: @facu tmp, so I don't have to modify a bunch of things in sims right now
-	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI // TODO: same as above
 }
 
 type ServiceWithPerms interface {

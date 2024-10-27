e37d71ad246cb47a0f00956692720bc25deeaa1e
Akhil Kumar P <36399231+akhilkumarpilli@users.noreply.github.com>
2024-10-27 19:45:02 +0530

    test: migrate remaining e2e tests to integration tests (#22364)
    Co-authored-by: Julien Robert <julien@rbrt.fr>
    Co-authored-by: Marko <marko@baricevic.me>

--- tests/integration/accounts/lockup/utils.go
+++ tests/integration/accounts/lockup/utils.go
@@ -20,23 +20,23 @@ var (
 	accOwner  = sdk.AccAddress(ownerAddr)
 )
 
-type E2ETestSuite struct {
+type IntegrationTestSuite struct {
 	suite.Suite
 
 	app *simapp.SimApp
 }
 
-func NewE2ETestSuite() *E2ETestSuite {
-	return &E2ETestSuite{}
+func NewIntegrationTestSuite() *IntegrationTestSuite {
+	return &IntegrationTestSuite{}
 }
 
-func (s *E2ETestSuite) SetupSuite() {
-	s.T().Log("setting up e2e test suite")
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
 	s.app = setupApp(s.T())
 }
 
-func (s *E2ETestSuite) TearDownSuite() {
-	s.T().Log("tearing down e2e test suite")
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
 }
 
 func setupApp(t *testing.T) *simapp.SimApp {
@@ -45,21 +45,21 @@ func setupApp(t *testing.T) *simapp.SimApp {
 	return app
 }
 
-func (s *E2ETestSuite) executeTx(ctx sdk.Context, msg sdk.Msg, app *simapp.SimApp, accAddr, sender []byte) error {
+func (s *IntegrationTestSuite) executeTx(ctx sdk.Context, msg sdk.Msg, app *simapp.SimApp, accAddr, sender []byte) error {
 	_, err := app.AccountsKeeper.Execute(ctx, accAddr, sender, msg, nil)
 	return err
 }
 
-func (s *E2ETestSuite) queryAcc(ctx sdk.Context, req sdk.Msg, app *simapp.SimApp, accAddr []byte) (transaction.Msg, error) {
+func (s *IntegrationTestSuite) queryAcc(ctx sdk.Context, req sdk.Msg, app *simapp.SimApp, accAddr []byte) (transaction.Msg, error) {
 	resp, err := app.AccountsKeeper.Query(ctx, accAddr, req)
 	return resp, err
 }
 
-func (s *E2ETestSuite) fundAccount(app *simapp.SimApp, ctx sdk.Context, addr sdk.AccAddress, amt sdk.Coins) {
+func (s *IntegrationTestSuite) fundAccount(app *simapp.SimApp, ctx sdk.Context, addr sdk.AccAddress, amt sdk.Coins) {
 	require.NoError(s.T(), testutil.FundAccount(ctx, app.BankKeeper, addr, amt))
 }
 
-func (s *E2ETestSuite) queryLockupAccInfo(ctx sdk.Context, app *simapp.SimApp, accAddr []byte) *types.QueryLockupAccountInfoResponse {
+func (s *IntegrationTestSuite) queryLockupAccInfo(ctx sdk.Context, app *simapp.SimApp, accAddr []byte) *types.QueryLockupAccountInfoResponse {
 	req := &types.QueryLockupAccountInfoRequest{}
 	resp, err := s.queryAcc(ctx, req, app, accAddr)
 	require.NoError(s.T(), err)

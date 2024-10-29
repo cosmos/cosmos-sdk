d95d5fb1b17192ad46e332af762a64607921505d
Hieu Vu <hieuvubk@gmail.com>
2024-10-29 16:11:14 +0700

    update proto

--- x/bank/v2/types/errors.go
+++ x/bank/v2/types/errors.go
@@ -10,10 +10,11 @@ import (
 
 // x/tokenfactory module sentinel errors
 var (
-	ErrDenomExists     = errorsmod.Register(ModuleName, 2, "attempting to create a denom that already exists (has bank metadata)")
-	ErrUnauthorized    = errorsmod.Register(ModuleName, 3, "unauthorized account")
-	ErrInvalidDenom    = errorsmod.Register(ModuleName, 4, "invalid denom")
-	ErrInvalidCreator  = errorsmod.Register(ModuleName, 5, "invalid creator")
-	ErrSubdenomTooLong = errorsmod.Register(ModuleName, 8, fmt.Sprintf("subdenom too long, max length is %d bytes", MaxSubdenomLength))
-	ErrCreatorTooLong  = errorsmod.Register(ModuleName, 9, fmt.Sprintf("creator too long, max length is %d bytes", MaxCreatorLength))
+	ErrDenomExists       = errorsmod.Register(ModuleName, 2, "attempting to create a denom that already exists (has bank metadata)")
+	ErrUnauthorized      = errorsmod.Register(ModuleName, 3, "unauthorized account")
+	ErrInvalidDenom      = errorsmod.Register(ModuleName, 4, "invalid denom")
+	ErrInvalidCreator    = errorsmod.Register(ModuleName, 5, "invalid creator")
+	ErrSubdenomTooLong   = errorsmod.Register(ModuleName, 8, fmt.Sprintf("subdenom too long, max length is %d bytes", MaxSubdenomLength))
+	ErrCreatorTooLong    = errorsmod.Register(ModuleName, 9, fmt.Sprintf("creator too long, max length is %d bytes", MaxCreatorLength))
+	ErrDenomDoesNotExist = errorsmod.Register(ModuleName, 10, "denom does not exist")
 )

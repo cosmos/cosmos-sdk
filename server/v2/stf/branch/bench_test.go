40d491e4546285cffe87e77c88c7e3f26ff576a0
testinginprod <frojdi@faulttolerance.net>
2024-10-31 11:58:41 +0100

    more refactors

--- server/v2/stf/branch/bench_test.go
+++ server/v2/stf/branch/bench_test.go
@@ -1,167 +1,86 @@
 package branch
 
 import (
+	"fmt"
 	"testing"
 
 	"cosmossdk.io/core/store"
 	coretesting "cosmossdk.io/core/testing"
 )
 
-func Benchmark_CacheStack1_Set(b *testing.B) {
-	bs := makeBranchStack(b, 1)
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		err := bs.Set([]byte{0}, []byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
+var (
+	stackSizes   = []int{1, 10, 100}
+	elemsInStack = 10
+)
+
+func Benchmark_CacheStack_Set(b *testing.B) {
+	for _, stackSize := range stackSizes {
+		b.Run(fmt.Sprintf("StackSize%d", stackSize), func(b *testing.B) {
+			bs := makeBranchStack(b, stackSize)
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				err := bs.Set([]byte{0}, []byte{0})
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
 	}
 }
 
-func Benchmark_CacheStack10_Set(b *testing.B) {
-	bs := makeBranchStack(b, 10)
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		err := bs.Set([]byte{0}, []byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
+func Benchmark_Get(b *testing.B) {
+	for _, stackSize := range stackSizes {
+		b.Run(fmt.Sprintf("StackSize%d", stackSize), func(b *testing.B) {
+			bs := makeBranchStack(b, stackSize)
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				_, err := bs.Get([]byte{0})
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
 	}
 }
 
-func Benchmark_CacheStack100_Set(b *testing.B) {
-	bs := makeBranchStack(b, 100)
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		err := bs.Set([]byte{0}, []byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func Benchmark_CacheStack1_Get(b *testing.B) {
-	bs := makeBranchStack(b, 1)
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		_, err := bs.Get([]byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func Benchmark_CacheStack10_Get(b *testing.B) {
-	bs := makeBranchStack(b, 10)
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		_, err := bs.Get([]byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func Benchmark_CacheStack100_Get(b *testing.B) {
-	bs := makeBranchStack(b, 100)
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		_, err := bs.Get([]byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func Benchmark_CacheStack1_Iterate(b *testing.B) {
-	bs := makeBranchStack(b, 1)
+func Benchmark_Iterate(b *testing.B) {
 	var keySink, valueSink any
 
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		iter, err := bs.Iterator([]byte{0}, []byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-		for iter.Valid() {
-			iter.Next()
-			keySink = iter.Key()
-			valueSink = iter.Value()
-		}
+	for _, stackSize := range stackSizes {
+		b.Run(fmt.Sprintf("StackSize%d", stackSize), func(b *testing.B) {
+			bs := makeBranchStack(b, stackSize)
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				iter, err := bs.Iterator([]byte{0}, []byte{0})
+				if err != nil {
+					b.Fatal(err)
+				}
+				for iter.Valid() {
+					iter.Next()
+					keySink = iter.Key()
+					valueSink = iter.Value()
+				}
+			}
+		})
 	}
 
 	_ = keySink
 	_ = valueSink
 }
 
-func Benchmark_CacheStack10_Iterate(b *testing.B) {
-	bs := makeBranchStack(b, 10)
-	var keySink, valueSink any
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		iter, err := bs.Iterator([]byte{0}, []byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-		for iter.Valid() {
-			iter.Next()
-			keySink = iter.Key()
-			valueSink = iter.Value()
-		}
-	}
-
-	_ = keySink
-	_ = valueSink
-}
-
-func Benchmark_CacheStack100_Iterate(b *testing.B) {
-	bs := makeBranchStack(b, 100)
-	var keySink, valueSink any
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	// test sets
-	for i := 0; i < b.N; i++ {
-		iter, err := bs.Iterator([]byte{0}, []byte{0})
-		if err != nil {
-			b.Fatal(err)
-		}
-		for iter.Valid() {
-			iter.Next()
-			keySink = iter.Key()
-			valueSink = iter.Value()
-		}
-	}
-
-	_ = keySink
-	_ = valueSink
-}
-
+// makeBranchStack creates a branch stack of the given size and initializes it with unique key-value pairs.
 func makeBranchStack(b *testing.B, stackSize int) Store[store.KVStore] {
-	const elems = 10
 	parent := coretesting.NewMemKV()
 	branch := NewStore[store.KVStore](parent)
 	for i := 1; i < stackSize; i++ {
 		branch = NewStore[store.KVStore](branch)
-		for j := 0; j < elems; j++ {
-			key, value := []byte{byte(j)}, []byte{byte(j)}
+		for j := 0; j < elemsInStack; j++ {
+			// create unique keys by including the branch index.
+			key := []byte{byte(i), byte(j)}
+			value := []byte{byte(j)}
 			err := branch.Set(key, value)
 			if err != nil {
 				b.Fatal(err)

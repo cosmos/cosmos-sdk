046101ed739d6009ec7e08c5ef8aaf6f0195c704
GnaD13 <cpt.meoz@gmail.com>
2024-11-07 01:59:09 +0700

    add logic to check if msgURL is empty

--- x/circuit/keeper/msg_server.go
+++ x/circuit/keeper/msg_server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"slices"
 	"strings"
 
 	"cosmossdk.io/collections"
@@ -137,6 +138,7 @@ func (srv msgServer) TripCircuitBreaker(ctx context.Context, msg *types.MsgTripC
 // have been paused using TripCircuitBreaker.
 func (srv msgServer) ResetCircuitBreaker(ctx context.Context, msg *types.MsgResetCircuitBreaker) (*types.MsgResetCircuitBreakerResponse, error) {
 	keeper := srv.Keeper
+	msgTypeUrls := msg.MsgTypeUrls
 	address, err := srv.addressCodec.StringToBytes(msg.Authority)
 	if err != nil {
 		return nil, err
@@ -148,7 +150,29 @@ func (srv msgServer) ResetCircuitBreaker(ctx context.Context, msg *types.MsgRese
 		return nil, err
 	}
 
-	for _, msgTypeURL := range msg.MsgTypeUrls {
+	// check if msgURL is empty
+	if len(msgTypeUrls) == 0 {
+		switch {
+		case perms.Level == types.Permissions_LEVEL_SUPER_ADMIN || perms.Level == types.Permissions_LEVEL_ALL_MSGS || bytes.Equal(address, srv.GetAuthority()):
+			// if the sender is a super admin or the module authority, will remove all disabled msgs
+			srv.DisableList.Walk(ctx, nil, func(msgUrl string) (stop bool, err error) {
+				msgTypeUrls = append(msgTypeUrls, msgUrl)
+				return false, nil
+			})
+
+		case perms.Level == types.Permissions_LEVEL_SOME_MSGS:
+			// if the sender has permission for some messages, will remove all disabled msgs that in the perms.LimitTypeUrls
+			srv.DisableList.Walk(ctx, nil, func(msgUrl string) (stop bool, err error) {
+				if slices.Contains(perms.LimitTypeUrls, msgUrl) {
+					msgTypeUrls = append(msgTypeUrls, msgUrl)
+				}
+				return false, nil
+			})
+		default:
+		}
+	}
+
+	for _, msgTypeURL := range msgTypeUrls {
 		// check if the message is in the list of allowed messages
 		isAllowed, err := srv.IsAllowed(ctx, msgTypeURL)
 		if err != nil {
@@ -176,7 +200,7 @@ func (srv msgServer) ResetCircuitBreaker(ctx context.Context, msg *types.MsgRese
 		}
 	}
 
-	urls := strings.Join(msg.GetMsgTypeUrls(), ",")
+	urls := strings.Join(msgTypeUrls, ",")
 
 	if err = srv.Keeper.EventService.EventManager(ctx).EmitKV(
 		"reset_circuit_breaker",

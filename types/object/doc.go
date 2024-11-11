1a79d31b308e6b4baf435e05b70ebfb1454385d4
marbar3778 <marbar3778@yahoo.com>
2024-11-11 15:40:51 +0100

    tt4

--- types/object/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Object Package defines an implementation of a generic object type that can be used to store any type of data.
-
-The object store stores decoded values in a cache that much be populated and cleared on each block.
-*/
-package object

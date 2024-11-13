1a8f7b5e2267e55ccd5863a0cc55575b42d0c790
Julian Toledano <jtoledanodiaz@gmail.com>
2024-11-13 11:56:56 +0100

    undo config

--- client/config/config.go
+++ client/config/config.go
@@ -2,14 +2,17 @@ package config
 
 import (
 	"crypto/tls"
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
-	"cosmossdk.io/client/v2/autocli/config"
-
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 )
 
 // DefaultConfig returns default config for the client.toml
@@ -24,17 +27,25 @@ func DefaultConfig() *Config {
 	}
 }
 
-type (
-	// ClientConfig is an alias for Config for backward compatibility
-	// Deprecated: use Config instead which avoid name stuttering
-	ClientConfig config.Config
+// ClientConfig is an alias for Config for backward compatibility
+// Deprecated: use Config instead which avoid name stuttering
+type ClientConfig Config
 
-	Config = config.Config
+type Config struct {
+	ChainID               string     `mapstructure:"chain-id" json:"chain-id"`
+	KeyringBackend        string     `mapstructure:"keyring-backend" json:"keyring-backend"`
+	KeyringDefaultKeyName string     `mapstructure:"keyring-default-keyname" json:"keyring-default-keyname"`
+	Output                string     `mapstructure:"output" json:"output"`
+	Node                  string     `mapstructure:"node" json:"node"`
+	BroadcastMode         string     `mapstructure:"broadcast-mode" json:"broadcast-mode"`
+	GRPC                  GRPCConfig `mapstructure:",squash"`
+}
 
-	GRPCConfig = config.GRPCConfig
-)
-
-var DefaultClientConfigTemplate = config.DefaultClientConfigTemplate
+// GRPCConfig holds the gRPC client configuration.
+type GRPCConfig struct {
+	Address  string `mapstructure:"grpc-address"  json:"grpc-address"`
+	Insecure bool   `mapstructure:"grpc-insecure"  json:"grpc-insecure"`
+}
 
 // ReadFromClientConfig reads values from client.toml file and updates them in client.Context
 // It uses CreateClientConfig internally with no custom template and custom config.
@@ -48,43 +59,80 @@ func ReadFromClientConfig(ctx client.Context) (client.Context, error) {
 // It takes a customClientTemplate and customConfig as input that can be used to overwrite the default config and enhance the client.toml file.
 // The custom template/config must be both provided or be "" and nil.
 func CreateClientConfig(ctx client.Context, customClientTemplate string, customConfig interface{}) (client.Context, error) {
-	conf, err := config.CreateClientConfig(ctx.HomeDir, ctx.ChainID, ctx.Viper, customClientTemplate, customConfig)
+	configPath := filepath.Join(ctx.HomeDir, "config")
+	configFilePath := filepath.Join(configPath, "client.toml")
+
+	// when client.toml does not exist create and init with default values
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+		if err := os.MkdirAll(configPath, os.ModePerm); err != nil {
+			return ctx, fmt.Errorf("couldn't make client config: %w", err)
+		}
+
+		if (customClientTemplate != "" && customConfig == nil) || (customClientTemplate == "" && customConfig != nil) {
+			return ctx, errors.New("customClientTemplate and customConfig should be both nil or not nil")
+		}
+
+		if customClientTemplate != "" {
+			if err := setConfigTemplate(customClientTemplate); err != nil {
+				return ctx, fmt.Errorf("couldn't set client config template: %w", err)
+			}
+
+			if ctx.ChainID != "" {
+				// chain-id will be written to the client.toml while initiating the chain.
+				ctx.Viper.Set(flags.FlagChainID, ctx.ChainID)
+			}
+
+			if err = ctx.Viper.Unmarshal(&customConfig); err != nil {
+				return ctx, fmt.Errorf("failed to parse custom client config: %w", err)
+			}
+
+			if err := writeConfigFile(configFilePath, customConfig); err != nil {
+				return ctx, fmt.Errorf("could not write client config to the file: %w", err)
+			}
+
+		} else {
+			conf := DefaultConfig()
+			if ctx.ChainID != "" {
+				// chain-id will be written to the client.toml while initiating the chain.
+				conf.ChainID = ctx.ChainID
+			}
+
+			if err := writeConfigFile(configFilePath, conf); err != nil {
+				return ctx, fmt.Errorf("could not write client config to the file: %w", err)
+			}
+		}
+	}
+
+	conf, err := getClientConfig(configPath, ctx.Viper)
 	if err != nil {
 		return ctx, fmt.Errorf("couldn't get client config: %w", err)
 	}
-
 	// we need to update KeyringDir field on client.Context first cause it is used in NewKeyringFromBackend
 	ctx = ctx.WithOutputFormat(conf.Output).
 		WithChainID(conf.ChainID).
 		WithKeyringDir(ctx.HomeDir).
 		WithKeyringDefaultKeyName(conf.KeyringDefaultKeyName)
-
 	keyring, err := client.NewKeyringFromBackend(ctx, conf.KeyringBackend)
 	if err != nil {
 		return ctx, fmt.Errorf("couldn't get keyring: %w", err)
 	}
-
 	// https://github.com/cosmos/cosmos-sdk/issues/8986
 	client, err := client.NewClientFromNode(conf.Node)
 	if err != nil {
 		return ctx, fmt.Errorf("couldn't get client from nodeURI: %w", err)
 	}
-
 	ctx = ctx.
 		WithNodeURI(conf.Node).
 		WithBroadcastMode(conf.BroadcastMode).
 		WithClient(client).
 		WithKeyring(keyring)
-
 	if conf.GRPC.Address != "" {
 		grpcClient, err := getGRPCClient(conf.GRPC)
 		if err != nil {
 			return ctx, fmt.Errorf("couldn't get grpc client: %w", err)
 		}
-
 		ctx = ctx.WithGRPCClient(grpcClient)
 	}
-
 	return ctx, nil
 }
 
@@ -95,16 +143,13 @@ func getGRPCClient(grpcConfig GRPCConfig) (*grpc.ClientConn, error) {
 	transport := grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
 		MinVersion: tls.VersionTLS12,
 	}))
-
 	if grpcConfig.Insecure {
 		transport = grpc.WithTransportCredentials(insecure.NewCredentials())
 	}
-
 	dialOptions := []grpc.DialOption{transport}
 	grpcClient, err := grpc.NewClient(grpcConfig.Address, dialOptions...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to dial gRPC server at %s: %w", grpcConfig.Address, err)
 	}
-
 	return grpcClient, nil
 }
---
+++ client/config/toml.go
@@ -0,0 +1,92 @@
+package config
+
+import (
+	"bytes"
+	"os"
+	"text/template"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	DefaultClientConfigTemplate = `# This is a TOML config file.
+# For more information, see https://github.com/toml-lang/toml
+
+###############################################################################
+###                           Client Configuration                          ###
+###############################################################################
+
+# The network chain ID
+chain-id = "{{ .ChainID }}"
+# The keyring's backend, where the keys are stored (os|file|kwallet|pass|test|memory)
+keyring-backend = "{{ .KeyringBackend }}"
+# Default key name, if set, defines the default key to use for signing transaction when the --from flag is not specified
+keyring-default-keyname = "{{ .KeyringDefaultKeyName }}"
+# CLI output format (text|json)
+output = "{{ .Output }}"
+# <host>:<port> to CometBFT RPC interface for this chain
+node = "{{ .Node }}"
+# Transaction broadcasting mode (sync|async)
+broadcast-mode = "{{ .BroadcastMode }}"
+
+# gRPC server endpoint to which the client will connect.
+# It can be overwritten by the --grpc-addr flag in each command.
+grpc-address = "{{ .GRPC.Address }}"
+
+# Allow the gRPC client to connect over insecure channels.
+# It can be overwritten by the --grpc-insecure flag in each command.
+grpc-insecure = {{ .GRPC.Insecure }}
+`
+)
+
+var configTemplate *template.Template
+
+func init() {
+	var err error
+
+	tmpl := template.New("clientConfigFileTemplate")
+	if configTemplate, err = tmpl.Parse(DefaultClientConfigTemplate); err != nil {
+		panic(err)
+	}
+}
+
+// setConfigTemplate sets the custom app config template for
+// the application
+func setConfigTemplate(customTemplate string) error {
+	tmpl := template.New("clientConfigFileTemplate")
+	var err error
+	if configTemplate, err = tmpl.Parse(customTemplate); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// writeConfigFile renders config using the template and writes it to
+// configFilePath.
+func writeConfigFile(configFilePath string, config interface{}) error {
+	var buffer bytes.Buffer
+	if err := configTemplate.Execute(&buffer, config); err != nil {
+		return err
+	}
+
+	return os.WriteFile(configFilePath, buffer.Bytes(), 0o600)
+}
+
+// getClientConfig reads values from client.toml file and unmarshalls them into ClientConfig
+func getClientConfig(configPath string, v *viper.Viper) (*Config, error) {
+	v.AddConfigPath(configPath)
+	v.SetConfigName("client")
+	v.SetConfigType("toml")
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	conf := DefaultConfig()
+	if err := v.Unmarshal(conf); err != nil {
+		return nil, err
+	}
+
+	return conf, nil
+}

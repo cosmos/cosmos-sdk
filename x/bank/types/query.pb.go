fef7b7fa09cc5dcf6cb0bf7250b440d1d8043b1c
JoowonYun <joowon@delightlabs.io>
2024-11-25 16:30:19 +0900

    fix: re-order rest proiority

--- x/bank/types/query.pb.go
+++ x/bank/types/query.pb.go
@@ -1394,7 +1394,7 @@ func init() {
 func init() { proto.RegisterFile("cosmos/bank/v1beta1/query.proto", fileDescriptor_9c6fc1939682df13) }
 
 var fileDescriptor_9c6fc1939682df13 = []byte{
-	// 1439 bytes of a gzipped FileDescriptorProto
+	// 1435 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xdc, 0x59, 0x5f, 0x68, 0x1c, 0xd5,
 	0x17, 0xce, 0x6d, 0x69, 0x9a, 0x9c, 0x4d, 0x7f, 0x3f, 0x7a, 0x93, 0xda, 0x74, 0x62, 0x77, 0xe3,
 	0xb4, 0x34, 0xe9, 0x92, 0xcc, 0x64, 0x77, 0x63, 0x53, 0x63, 0x5a, 0xc8, 0xb6, 0xc6, 0x07, 0x95,
@@ -1469,22 +1469,22 @@ var fileDescriptor_9c6fc1939682df13 = []byte{
 	0xa8, 0xd3, 0x1e, 0xea, 0xfd, 0x78, 0x42, 0x8e, 0x9a, 0xc1, 0xb8, 0x86, 0x60, 0x48, 0x58, 0x68,
 	0x1c, 0xf1, 0xbe, 0x75, 0x99, 0x72, 0x25, 0x1d, 0x67, 0x2a, 0x47, 0x93, 0xf1, 0xd0, 0x1c, 0xc2,
 	0x07, 0x23, 0xd0, 0x78, 0x6a, 0xbd, 0x8f, 0x60, 0x90, 0xf9, 0x66, 0x3c, 0xd5, 0x3b, 0x53, 0xc0,
-	0xa4, 0x2b, 0xd3, 0xfd, 0x27, 0xc6, 0x97, 0x87, 0x39, 0x74, 0x7c, 0x13, 0xc1, 0xae, 0x80, 0x5f,
-	0xc3, 0x5a, 0xef, 0x2c, 0x32, 0xbf, 0xaa, 0xe8, 0xb1, 0xe7, 0x73, 0x70, 0x4b, 0x1e, 0xb8, 0x0c,
-	0xd6, 0xa5, 0xe0, 0xd8, 0xd9, 0x53, 0x14, 0x46, 0x4f, 0xbf, 0x4c, 0x07, 0x8e, 0xa5, 0xd3, 0x57,
-	0xf0, 0x03, 0x04, 0x4a, 0x6f, 0x83, 0x89, 0x5f, 0x8c, 0x89, 0x46, 0x66, 0x6b, 0x95, 0xa5, 0xcd,
-	0x05, 0x73, 0x5e, 0xcb, 0x1e, 0xaf, 0x23, 0x78, 0x3e, 0x0e, 0xaf, 0x62, 0xa9, 0x55, 0xa4, 0xc7,
-	0x4f, 0x91, 0x30, 0xf4, 0x9f, 0x21, 0xf8, 0x5f, 0xf0, 0x12, 0x83, 0xfb, 0xc9, 0xdb, 0x7d, 0xab,
-	0x52, 0xe6, 0xe2, 0x07, 0xc4, 0x2f, 0xdf, 0x2e, 0xe0, 0xf8, 0x0e, 0x82, 0x84, 0xcf, 0x0c, 0x45,
-	0xbd, 0xec, 0x61, 0x73, 0x1e, 0xf5, 0xb2, 0x4b, 0x8c, 0xab, 0xfa, 0x6a, 0x64, 0x67, 0xd6, 0xf0,
-	0x4c, 0x6f, 0xc8, 0xdc, 0x8d, 0xf9, 0x0b, 0xe8, 0x7b, 0x04, 0x38, 0xec, 0xe3, 0x70, 0x2e, 0x16,
-	0xa6, 0xa0, 0x1b, 0x55, 0xe6, 0x37, 0x16, 0xe4, 0xe3, 0x13, 0x76, 0x67, 0x1e, 0xa3, 0x19, 0x9c,
-	0xee, 0xcb, 0xa8, 0x53, 0x3a, 0xf8, 0x4b, 0x04, 0x09, 0x9f, 0xfd, 0x89, 0xda, 0x8a, 0xb0, 0xed,
-	0x8b, 0xda, 0x0a, 0x89, 0x27, 0x53, 0x97, 0x23, 0x0f, 0x90, 0x03, 0xf8, 0x39, 0x79, 0xf3, 0xf3,
-	0xd9, 0xb8, 0x7c, 0xee, 0xee, 0x7a, 0x12, 0xdd, 0x5b, 0x4f, 0xa2, 0xdf, 0xd6, 0x93, 0xe8, 0xa3,
-	0x87, 0xc9, 0x81, 0x7b, 0x0f, 0x93, 0x03, 0x3f, 0x3d, 0x4c, 0x0e, 0x9c, 0xe3, 0xff, 0x18, 0x20,
-	0xe6, 0x79, 0xad, 0xea, 0xe8, 0xef, 0xb0, 0x35, 0xe8, 0x8f, 0x37, 0xa5, 0x41, 0xfa, 0x7b, 0x7f,
-	0xee, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x05, 0x3a, 0x25, 0x40, 0x12, 0x19, 0x00, 0x00,
+	0xa4, 0x2b, 0xd3, 0xfd, 0x27, 0xc6, 0x97, 0x87, 0x39, 0x74, 0xfc, 0x19, 0x82, 0xff, 0x05, 0x6d,
+	0x33, 0xd6, 0x7b, 0xa7, 0x91, 0xfa, 0x78, 0x65, 0x2e, 0x7e, 0x40, 0x7c, 0xc1, 0xd8, 0xf1, 0x53,
+	0x14, 0x5e, 0x0f, 0xdf, 0x44, 0xb0, 0x2b, 0x60, 0x2c, 0xb1, 0xd6, 0x27, 0x6d, 0x37, 0x4c, 0x3d,
+	0xf6, 0x7c, 0x8e, 0x72, 0xc9, 0x43, 0x99, 0xc1, 0x7a, 0x1c, 0x94, 0xfa, 0x65, 0x3a, 0x70, 0x2c,
+	0x9d, 0xbe, 0x82, 0x1f, 0x20, 0x50, 0x7a, 0x3b, 0x61, 0xfc, 0x62, 0x4c, 0x34, 0x32, 0xff, 0xad,
+	0x2c, 0x6d, 0x2e, 0x98, 0xf3, 0x5a, 0xf6, 0x78, 0x1d, 0xc1, 0xf3, 0x71, 0x78, 0x15, 0x4b, 0xad,
+	0x22, 0x3d, 0x27, 0x8b, 0x84, 0xa1, 0xbf, 0x83, 0x20, 0xe1, 0x33, 0x43, 0x51, 0x2f, 0x7b, 0xd8,
+	0x9c, 0x47, 0xbd, 0xec, 0x12, 0xe3, 0xaa, 0xbe, 0x1a, 0xd9, 0x99, 0x35, 0x3c, 0xd3, 0x9b, 0x02,
+	0x77, 0x63, 0xfe, 0x7d, 0xf9, 0x1e, 0x01, 0x0e, 0xfb, 0x38, 0x9c, 0x8b, 0x85, 0x29, 0xe8, 0x46,
+	0x95, 0xf9, 0x8d, 0x05, 0xf9, 0xf8, 0x84, 0xdd, 0x99, 0xc7, 0x68, 0x06, 0xa7, 0xfb, 0x32, 0xea,
+	0xec, 0x08, 0xfe, 0x12, 0x41, 0xc2, 0x67, 0x7f, 0xa2, 0xb6, 0x22, 0x6c, 0xfb, 0xa2, 0xb6, 0x42,
+	0xe2, 0xc9, 0xd4, 0xe5, 0xc8, 0x03, 0xe4, 0x00, 0x7e, 0x4e, 0xde, 0xfc, 0x7c, 0x36, 0x2e, 0x9f,
+	0xbb, 0xbb, 0x9e, 0x44, 0xf7, 0xd6, 0x93, 0xe8, 0xb7, 0xf5, 0x24, 0xfa, 0xe8, 0x61, 0x72, 0xe0,
+	0xde, 0xc3, 0xe4, 0xc0, 0x4f, 0x0f, 0x93, 0x03, 0xe7, 0xf8, 0x3f, 0x06, 0x88, 0x79, 0x5e, 0xab,
+	0x3a, 0xfa, 0x3b, 0x6c, 0x0d, 0xfa, 0xe3, 0x4d, 0x69, 0x90, 0xfe, 0xde, 0x9f, 0xfb, 0x27, 0x00,
+	0x00, 0xff, 0xff, 0x59, 0xd2, 0xc2, 0x0f, 0x12, 0x19, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -1529,13 +1529,13 @@ type QueryClient interface {
 	SupplyOf(ctx context.Context, in *QuerySupplyOfRequest, opts ...grpc.CallOption) (*QuerySupplyOfResponse, error)
 	// Params queries the parameters of x/bank module.
 	Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error)
+	// DenomsMetadata queries the client metadata for all registered coin
+	// denominations.
+	DenomsMetadata(ctx context.Context, in *QueryDenomsMetadataRequest, opts ...grpc.CallOption) (*QueryDenomsMetadataResponse, error)
 	// DenomMetadata queries the client metadata of a given coin denomination.
 	DenomMetadata(ctx context.Context, in *QueryDenomMetadataRequest, opts ...grpc.CallOption) (*QueryDenomMetadataResponse, error)
 	// DenomMetadataByQueryString queries the client metadata of a given coin denomination.
 	DenomMetadataByQueryString(ctx context.Context, in *QueryDenomMetadataByQueryStringRequest, opts ...grpc.CallOption) (*QueryDenomMetadataByQueryStringResponse, error)
-	// DenomsMetadata queries the client metadata for all registered coin
-	// denominations.
-	DenomsMetadata(ctx context.Context, in *QueryDenomsMetadataRequest, opts ...grpc.CallOption) (*QueryDenomsMetadataResponse, error)
 	// DenomOwners queries for all account addresses that own a particular token
 	// denomination.
 	//
@@ -1624,6 +1624,15 @@ func (c *queryClient) Params(ctx context.Context, in *QueryParamsRequest, opts .
 	return out, nil
 }
 
+func (c *queryClient) DenomsMetadata(ctx context.Context, in *QueryDenomsMetadataRequest, opts ...grpc.CallOption) (*QueryDenomsMetadataResponse, error) {
+	out := new(QueryDenomsMetadataResponse)
+	err := c.cc.Invoke(ctx, "/cosmos.bank.v1beta1.Query/DenomsMetadata", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *queryClient) DenomMetadata(ctx context.Context, in *QueryDenomMetadataRequest, opts ...grpc.CallOption) (*QueryDenomMetadataResponse, error) {
 	out := new(QueryDenomMetadataResponse)
 	err := c.cc.Invoke(ctx, "/cosmos.bank.v1beta1.Query/DenomMetadata", in, out, opts...)
@@ -1636,15 +1645,6 @@ func (c *queryClient) DenomMetadata(ctx context.Context, in *QueryDenomMetadataR
 func (c *queryClient) DenomMetadataByQueryString(ctx context.Context, in *QueryDenomMetadataByQueryStringRequest, opts ...grpc.CallOption) (*QueryDenomMetadataByQueryStringResponse, error) {
 	out := new(QueryDenomMetadataByQueryStringResponse)
 	err := c.cc.Invoke(ctx, "/cosmos.bank.v1beta1.Query/DenomMetadataByQueryString", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *queryClient) DenomsMetadata(ctx context.Context, in *QueryDenomsMetadataRequest, opts ...grpc.CallOption) (*QueryDenomsMetadataResponse, error) {
-	out := new(QueryDenomsMetadataResponse)
-	err := c.cc.Invoke(ctx, "/cosmos.bank.v1beta1.Query/DenomsMetadata", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -1710,13 +1710,13 @@ type QueryServer interface {
 	SupplyOf(context.Context, *QuerySupplyOfRequest) (*QuerySupplyOfResponse, error)
 	// Params queries the parameters of x/bank module.
 	Params(context.Context, *QueryParamsRequest) (*QueryParamsResponse, error)
+	// DenomsMetadata queries the client metadata for all registered coin
+	// denominations.
+	DenomsMetadata(context.Context, *QueryDenomsMetadataRequest) (*QueryDenomsMetadataResponse, error)
 	// DenomMetadata queries the client metadata of a given coin denomination.
 	DenomMetadata(context.Context, *QueryDenomMetadataRequest) (*QueryDenomMetadataResponse, error)
 	// DenomMetadataByQueryString queries the client metadata of a given coin denomination.
 	DenomMetadataByQueryString(context.Context, *QueryDenomMetadataByQueryStringRequest) (*QueryDenomMetadataByQueryStringResponse, error)
-	// DenomsMetadata queries the client metadata for all registered coin
-	// denominations.
-	DenomsMetadata(context.Context, *QueryDenomsMetadataRequest) (*QueryDenomsMetadataResponse, error)
 	// DenomOwners queries for all account addresses that own a particular token
 	// denomination.
 	//
@@ -1759,14 +1759,14 @@ func (*UnimplementedQueryServer) SupplyOf(ctx context.Context, req *QuerySupplyO
 func (*UnimplementedQueryServer) Params(ctx context.Context, req *QueryParamsRequest) (*QueryParamsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Params not implemented")
 }
+func (*UnimplementedQueryServer) DenomsMetadata(ctx context.Context, req *QueryDenomsMetadataRequest) (*QueryDenomsMetadataResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DenomsMetadata not implemented")
+}
 func (*UnimplementedQueryServer) DenomMetadata(ctx context.Context, req *QueryDenomMetadataRequest) (*QueryDenomMetadataResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DenomMetadata not implemented")
 }
 func (*UnimplementedQueryServer) DenomMetadataByQueryString(ctx context.Context, req *QueryDenomMetadataByQueryStringRequest) (*QueryDenomMetadataByQueryStringResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DenomMetadataByQueryString not implemented")
-}
-func (*UnimplementedQueryServer) DenomsMetadata(ctx context.Context, req *QueryDenomsMetadataRequest) (*QueryDenomsMetadataResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DenomsMetadata not implemented")
 }
 func (*UnimplementedQueryServer) DenomOwners(ctx context.Context, req *QueryDenomOwnersRequest) (*QueryDenomOwnersResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DenomOwners not implemented")
@@ -1908,6 +1908,24 @@ func _Query_Params_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_DenomsMetadata_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryDenomsMetadataRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).DenomsMetadata(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/cosmos.bank.v1beta1.Query/DenomsMetadata",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).DenomsMetadata(ctx, req.(*QueryDenomsMetadataRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _Query_DenomMetadata_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(QueryDenomMetadataRequest)
 	if err := dec(in); err != nil {
@@ -1940,24 +1958,6 @@ func _Query_DenomMetadataByQueryString_Handler(srv interface{}, ctx context.Cont
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(QueryServer).DenomMetadataByQueryString(ctx, req.(*QueryDenomMetadataByQueryStringRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Query_DenomsMetadata_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(QueryDenomsMetadataRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(QueryServer).DenomsMetadata(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/cosmos.bank.v1beta1.Query/DenomsMetadata",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(QueryServer).DenomsMetadata(ctx, req.(*QueryDenomsMetadataRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -2050,16 +2050,16 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 			Handler:    _Query_Params_Handler,
 		},
 		{
+			MethodName: "DenomsMetadata",
+			Handler:    _Query_DenomsMetadata_Handler,
+		},
+		{
 			MethodName: "DenomMetadata",
 			Handler:    _Query_DenomMetadata_Handler,
 		},
 		{
 			MethodName: "DenomMetadataByQueryString",
 			Handler:    _Query_DenomMetadataByQueryString_Handler,
-		},
-		{
-			MethodName: "DenomsMetadata",
-			Handler:    _Query_DenomsMetadata_Handler,
 		},
 		{
 			MethodName: "DenomOwners",

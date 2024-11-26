14d98d2771244c6e23375022f46277657a4776c9
Alexander Peters <alpe@users.noreply.github.com>
2024-11-26 15:46:54 +0100

    refactor(systemtests): Extract system test framework (#22578)
    Co-authored-by: marbar3778 <marbar3778@yahoo.com>

--- tests/systemtests/unordered_tx_test.go
+++ tests/systemtests/unordered_tx_test.go
@@ -9,31 +9,35 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	systest "cosmossdk.io/systemtests"
 )
 
 func TestUnorderedTXDuplicate(t *testing.T) {
-	t.Skip("The unordered tx handling is not wired in v2")
-
+	if systest.IsV2() {
+		t.Skip("The unordered tx handling is not wired in v2")
+		return
+	}
 	// scenario: test unordered tx duplicate
 	// given a running chain with a tx in the unordered tx pool
 	// when a new tx with the same hash is broadcasted
 	// then the new tx should be rejected
 
-	sut.ResetChain(t)
-	cli := NewCLIWrapper(t, sut, verbose)
+	systest.Sut.ResetChain(t)
+	cli := systest.NewCLIWrapper(t, systest.Sut, systest.Verbose)
 	// add genesis account with some tokens
 	account1Addr := cli.AddKey("account1")
 	account2Addr := cli.AddKey("account2")
-	sut.ModifyGenesisCLI(t,
+	systest.Sut.ModifyGenesisCLI(t,
 		[]string{"genesis", "add-genesis-account", account1Addr, "10000000stake"},
 	)
 
-	sut.StartChain(t)
+	systest.Sut.StartChain(t)
 
 	timeoutTimestamp := time.Now().Add(time.Minute)
 	// send tokens
 	rsp1 := cli.Run("tx", "bank", "send", account1Addr, account2Addr, "5000stake", "--from="+account1Addr, "--fees=1stake", fmt.Sprintf("--timeout-timestamp=%v", timeoutTimestamp.Unix()), "--unordered", "--sequence=1", "--note=1")
-	RequireTxSuccess(t, rsp1)
+	systest.RequireTxSuccess(t, rsp1)
 
 	assertDuplicateErr := func(xt assert.TestingT, gotErr error, gotOutputs ...interface{}) bool {
 		require.Len(t, gotOutputs, 1)
@@ -41,9 +45,9 @@ func TestUnorderedTXDuplicate(t *testing.T) {
 		return false // always abort
 	}
 	rsp2 := cli.WithRunErrorMatcher(assertDuplicateErr).Run("tx", "bank", "send", account1Addr, account2Addr, "5000stake", "--from="+account1Addr, "--fees=1stake", fmt.Sprintf("--timeout-timestamp=%v", timeoutTimestamp.Unix()), "--unordered", "--sequence=1")
-	RequireTxFailure(t, rsp2)
+	systest.RequireTxFailure(t, rsp2)
 
 	require.Eventually(t, func() bool {
 		return cli.QueryBalance(account2Addr, "stake") == 5000
-	}, time.Minute, time.Microsecond*500, "TX was not executed before timeout")
+	}, 10*systest.Sut.BlockTime(), 200*time.Millisecond, "TX was not executed before timeout")
 }

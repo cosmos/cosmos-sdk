f153426ab2c0caa1e644218800483c620be41ccd
Marko <marko@baricevic.me>
2024-11-26 15:25:08 +0100

    chore: move generic txdecoder  (#22653)

--- simapp/v2/simdv2/cmd/commands.go
+++ simapp/v2/simdv2/cmd/commands.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"errors"
 	"io"
 
 	"github.com/spf13/cobra"
@@ -108,7 +107,7 @@ func InitRootCmd[T transaction.Tx](
 			simApp.Store(),
 			simApp.App.AppManager,
 			simApp.AppCodec(),
-			&genericTxDecoder[T]{deps.TxConfig},
+			&client.DefaultTxDecoder[T]{TxConfig: deps.TxConfig},
 			simApp.App.QueryHandlers(),
 			simApp.App.SchemaDecoderResolver(),
 			initCometOptions[T](),
@@ -242,44 +241,4 @@ func RootCommandPersistentPreRun(clientCtx client.Context) func(*cobra.Command, 
 
 		return nil
 	}
-}
-
-var _ transaction.Codec[transaction.Tx] = &genericTxDecoder[transaction.Tx]{}
-
-type genericTxDecoder[T transaction.Tx] struct {
-	txConfig client.TxConfig
-}
-
-// Decode implements transaction.Codec.
-func (t *genericTxDecoder[T]) Decode(bz []byte) (T, error) {
-	var out T
-	tx, err := t.txConfig.TxDecoder()(bz)
-	if err != nil {
-		return out, err
-	}
-
-	var ok bool
-	out, ok = tx.(T)
-	if !ok {
-		return out, errors.New("unexpected Tx type")
-	}
-
-	return out, nil
-}
-
-// DecodeJSON implements transaction.Codec.
-func (t *genericTxDecoder[T]) DecodeJSON(bz []byte) (T, error) {
-	var out T
-	tx, err := t.txConfig.TxJSONDecoder()(bz)
-	if err != nil {
-		return out, err
-	}
-
-	var ok bool
-	out, ok = tx.(T)
-	if !ok {
-		return out, errors.New("unexpected Tx type")
-	}
-
-	return out, nil
 }

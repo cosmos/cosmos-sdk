ebc6473fdc8eee8684ef657129c9300343831c6a
Hieu Vu <hieuvubk@gmail.com>
2024-12-02 15:24:27 +0700

    default logic

--- x/accounts/defaults/asset/default.go
+++ x/accounts/defaults/asset/default.go
@@ -7,14 +7,65 @@ import (
 	v1 "cosmossdk.io/x/accounts/defaults/asset/v1"
 )
 
-func DefaultTransfer(aa v1.AssetAccountI) func(ctx context.Context, from, to []byte, amount math.Int) error {
+func DefaultTransfer(aa v1.AssetAccountI) func(ctx context.Context, from, to []byte, amount math.Int) ([][]byte,error) {
+	return func(ctx context.Context, from, to []byte, amount math.Int) ([][]byte,error) {
+		err := aa.SubUnlockedCoins(ctx, from, amount)
+		if err != nil {
+			return nil, err
+		}
+
+		err = aa.AddCoins(ctx, to, amount)
+		if err != nil {
+			return nil, err
+		}
+
+		return [][]byte{from, to}, nil
+	}
+}
+
+func DefaultMint(aa v1.AssetAccountI) func(ctx context.Context, to []byte, amount math.Int) ([][]byte,error) {
+	return func(ctx context.Context, to []byte, amount math.Int) ([][]byte,error) {
+		err := aa.AddCoins(ctx, to, amount)
+		if err != nil {
+			return nil, err
+		}
+
+		return [][]byte{to}, nil
+	}
+}
+
+func DefaultBurn(aa v1.AssetAccountI) func(ctx context.Context, from []byte, amount math.Int) ([][]byte,error) {
+	return func(ctx context.Context, from []byte, amount math.Int) ([][]byte,error) {
+		err := aa.SubUnlockedCoins(ctx, from, amount)
+		if err != nil {
+			return nil, err
+		}
+
+		return [][]byte{from}, nil
+	}
+}
+
+func CustomTransfer(aa v1.AssetAccountI) func(ctx context.Context, from, to []byte, amount math.Int) error {
 	return func(ctx context.Context, from, to []byte, amount math.Int) error {
 		err := aa.SubUnlockedCoins(ctx, from, amount)
 		if err != nil {
 			return err
 		}
 
-		err = aa.AddCoins(ctx, to, amount)
+		fee := math.LegacyNewDecWithPrec(10, 2) // 10%
+		feeAmount := math.LegacyNewDecFromInt(amount).Mul(fee).TruncateInt()
+		transferAmount := amount.Sub(feeAmount)
+		owner, err := aa.GetOwner(ctx)
+		if err != nil {
+			return err
+		}
+
+		err = aa.AddCoins(ctx, to, transferAmount)
+		if err != nil {
+			return err
+		}
+
+		err = aa.AddCoins(ctx, owner, feeAmount)
 		if err != nil {
 			return err
 		}

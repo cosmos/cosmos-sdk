8f91e5b1313c3839e49b0525aec6949b3f86122d
Hieu Vu <hieuvubk@gmail.com>
2024-12-02 15:25:17 +0700

    test

--- x/bank/v2/keeper/keeper_test.go
+++ x/bank/v2/keeper/keeper_test.go
@@ -19,7 +19,9 @@ import (
 	accountskeeper "cosmossdk.io/x/accounts"
 	"cosmossdk.io/x/accounts/accountstd"
 	asset "cosmossdk.io/x/accounts/defaults/asset"
+	assetmock "cosmossdk.io/x/accounts/defaults/asset/mock"
 	assetv1 "cosmossdk.io/x/accounts/defaults/asset/v1"
+
 	banktestutil "cosmossdk.io/x/bank/v2/testutil"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectestutil "github.com/cosmos/cosmos-sdk/codec/testutil"
@@ -32,8 +34,9 @@ import (
 )
 
 const (
-	fooDenom = "foo"
-	barDenom = "bar"
+	fooDenom   = "foo"   // use default asset account logic
+	barDenom   = "bar"   // use custom asset account logic
+	stakeDenom = "stake" // use sdk logic
 )
 
 var (
@@ -55,6 +58,10 @@ func newFooCoin(amt int64) sdk.Coin {
 
 func newBarCoin(amt int64) sdk.Coin {
 	return sdk.NewInt64Coin(barDenom, amt)
+}
+
+func newStakeCoin(amt int64) sdk.Coin {
+	return sdk.NewInt64Coin(stakeDenom, amt)
 }
 
 type KeeperTestSuite struct {
@@ -94,7 +101,7 @@ func (suite *KeeperTestSuite) SetupTest() {
 		encCfg.Codec,
 		accountsKeeper,
 	)
-	
+
 	// Init x/account for foo & bar coin
 
 	msgInitFoo := assetv1.MsgInitAssetAccount{
@@ -102,7 +109,7 @@ func (suite *KeeperTestSuite) SetupTest() {
 		Denom: fooDenom,
 		InitBalance: []assetv1.Balance{
 			{
-				Addr: accAddrs[0],
+				Addr:   accAddrs[0],
 				Amount: math.NewInt(100),
 			},
 		},
@@ -111,18 +118,23 @@ func (suite *KeeperTestSuite) SetupTest() {
 	msgInitFooWraper := &assetv1.MsgInitAssetAccountWrapper{
 		MsgInitAssetAccount: msgInitFoo,
 		TransferFunc:        asset.DefaultTransfer,
+		MintFunc:            asset.DefaultMint,
+		BurnFunc:            asset.DefaultBurn,
 	}
 	_, fooAddr, err := accountsKeeper.Init(suite.ctx, asset.Type, authority, msgInitFooWraper, sdk.NewCoins())
 	suite.Require().NoError(err)
 	suite.Require().NoError(suite.bankKeeper.SetAssetAccount(suite.ctx, fooDenom, fooAddr))
-	
 
+	// Init bar with custom send, mint, burn logic
+	// - send: receiver take 90%, owner take 10% (gov)
+	// - mint: receiver take 90%, owner take 10%
+	// - burn: just burn 90%
 	msgInitBar := assetv1.MsgInitAssetAccount{
 		Owner: authority.String(),
 		Denom: barDenom,
 		InitBalance: []assetv1.Balance{
 			{
-				Addr: accAddrs[0],
+				Addr:   accAddrs[0],
 				Amount: math.NewInt(100),
 			},
 		},
@@ -130,41 +142,114 @@ func (suite *KeeperTestSuite) SetupTest() {
 
 	msgInitBarWraper := &assetv1.MsgInitAssetAccountWrapper{
 		MsgInitAssetAccount: msgInitBar,
-		TransferFunc:        asset.DefaultTransfer,
+		TransferFunc:        assetmock.CustomTransfer,
+		MintFunc:            assetmock.CustomMint,
+		BurnFunc:            assetmock.CustomBurn,
 	}
 	_, barAddr, err := accountsKeeper.Init(suite.ctx, asset.Type, authority, msgInitBarWraper, sdk.NewCoins())
 	suite.Require().NoError(err)
 	suite.Require().NoError(suite.bankKeeper.SetAssetAccount(suite.ctx, barDenom, barAddr))
 }
 
-func (suite *KeeperTestSuite) TestSendCoins_Acount_To_Account() {
+func (suite *KeeperTestSuite) TestSendCoins() {
 	fmt.Println("check")
 	ctx := suite.ctx
 	require := suite.Require()
-	sendAmt := sdk.NewCoins(newFooCoin(10), newBarCoin(10))
+	sendAmt := sdk.NewCoins(newFooCoin(10), newBarCoin(10), newStakeCoin(10))
 
 	handler := keeper.NewHandlers(&suite.bankKeeper)
 
 	// Set balances for acc0 and then try send to acc1
-	require.NoError(banktestutil.FundAccount(ctx, suite.bankKeeper, accAddrs[0], newFooCoin(100)))
-	require.NoError(banktestutil.FundAccount(ctx, suite.bankKeeper, accAddrs[0], newBarCoin(100)))
+	require.NoError(banktestutil.FundAccount(ctx, suite.bankKeeper, accAddrs[0], newStakeCoin(100)))
 	rsp, err := handler.MsgSend(ctx, &banktypes.MsgSend{
 		FromAddress: accAddrs[0].String(),
-		ToAddress: accAddrs[1].String(),
-		Amount: sendAmt,
+		ToAddress:   accAddrs[1].String(),
+		Amount:      sendAmt,
 	})
 	require.NoError(err)
 	fmt.Println("Send", rsp, err)
 
-	// Check balances
+	// Check acc0 balances
 	acc0FooBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[0], fooDenom)
 	require.Equal(acc0FooBalance.Amount, math.NewInt(90))
 	acc0BarBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[0], barDenom)
 	require.Equal(acc0BarBalance.Amount, math.NewInt(90))
+	fmt.Println("acc0BarBalance", acc0BarBalance)
+	acc0StakeBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[0], stakeDenom)
+	require.Equal(acc0StakeBalance.Amount, math.NewInt(90))
+	fmt.Println("acc0BarBalance", acc0BarBalance)
+
+	// Check acc1 balances
 	acc1FooBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[1], fooDenom)
+	fmt.Println("acc1 foo balance", acc1FooBalance)
 	require.Equal(acc1FooBalance.Amount, math.NewInt(10))
 	acc1BarBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[1], barDenom)
-	require.Equal(acc1BarBalance.Amount, math.NewInt(10))
+	require.Equal(acc1BarBalance.Amount, math.NewInt(9))
+	acc1StakeBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[1], stakeDenom)
+	require.Equal(acc1StakeBalance.Amount, math.NewInt(10))
+
+	// Check gov balance
+	govBarBalance := suite.bankKeeper.GetBalance(ctx, authtypes.NewModuleAddress("gov"), barDenom)
+	require.Equal(govBarBalance.Amount, math.NewInt(1))
+}
+
+func (suite *KeeperTestSuite) TestMintCoins() {
+	fmt.Println("check")
+	ctx := suite.ctx
+	require := suite.Require()
+	mintAmt := sdk.NewCoins(newFooCoin(10), newBarCoin(10), newStakeCoin(10))
+
+	handler := keeper.NewHandlers(&suite.bankKeeper)
+
+	rsp, err := handler.MsgMint(ctx, &banktypes.MsgMint{
+		Authority: authtypes.NewModuleAddress("gov").String(),
+		ToAddress: accAddrs[1].String(),
+		Amount:    mintAmt,
+	})
+	require.NoError(err)
+	fmt.Println("Send", rsp, err)
+
+	// Check acc1 balances
+	acc1FooBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[1], fooDenom)
+	fmt.Println("acc1 foo balance", acc1FooBalance)
+	require.Equal(acc1FooBalance.Amount, math.NewInt(10))
+	acc1BarBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[1], barDenom)
+	require.Equal(acc1BarBalance.Amount, math.NewInt(9))
+	acc1StakeBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[1], stakeDenom)
+	require.Equal(acc1StakeBalance.Amount, math.NewInt(10))
+
+	// Check gov balance
+	govBarBalance := suite.bankKeeper.GetBalance(ctx, authtypes.NewModuleAddress("gov"), barDenom)
+	require.Equal(govBarBalance.Amount, math.NewInt(1))
+}
+
+func (suite *KeeperTestSuite) TestBurnCoins() {
+	fmt.Println("check")
+	ctx := suite.ctx
+	require := suite.Require()
+	burnAmount := sdk.NewCoins(newFooCoin(10), newBarCoin(10), newStakeCoin(10))
+
+	handler := keeper.NewHandlers(&suite.bankKeeper)
+
+	// Set balances for acc0 and then try burn
+	require.NoError(banktestutil.FundAccount(ctx, suite.bankKeeper, accAddrs[0], newStakeCoin(100)))
+	rsp, err := handler.MsgBurn(ctx, &banktypes.MsgBurn{
+		Authority:       authtypes.NewModuleAddress("gov").String(),
+		BurnFromAddress: accAddrs[0].String(),
+		Amount:          burnAmount,
+	})
+	require.NoError(err)
+	fmt.Println("Send", rsp, err)
+
+	// Check acc0 balances
+	acc1FooBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[0], fooDenom)
+	fmt.Println("acc1 foo balance", acc1FooBalance)
+	require.Equal(acc1FooBalance.Amount, math.NewInt(90))
+	acc1BarBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[0], barDenom)
+	require.Equal(acc1BarBalance.Amount, math.NewInt(91))
+	acc1StakeBalance := suite.bankKeeper.GetBalance(ctx, accAddrs[0], stakeDenom)
+	require.Equal(acc1StakeBalance.Amount, math.NewInt(90))
+
 }
 
 // func (suite *KeeperTestSuite) TestSendCoins_Acount_To_Module() {

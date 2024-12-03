4f611a22753832964c43576af6bf9ec28e7342b1
Julien Robert <julien@rbrt.fr>
2024-12-03 15:07:48 +0100

    readbility

--- server/v2/appmanager/appmanager.go
+++ server/v2/appmanager/appmanager.go
@@ -142,15 +142,15 @@ func (a appManager[T]) InitGenesis(
 		return nil, nil, fmt.Errorf("failed to apply block zero state changes to genesis state: %w", err)
 	}
 
-	// override validator updates with the ones from the genesis state
+	// override validator updates with the ones from the init genesis state
 	// this triggers only when x/staking or another module that returns validator updates in InitGenesis
 	// otherwise, genutil validator updates takes precedence (returned from executing the genesis txs (as it implements appmodule.GenesisDecoder) in the end block)
-	if len(valUpdates) > 0 && len(blockResponse.ValidatorUpdates) == 0 {
-		blockResponse.ValidatorUpdates = valUpdates
-	}
-
 	if len(valUpdates) > 0 && len(blockResponse.ValidatorUpdates) > 0 {
 		return nil, nil, errors.New("validator updates returned from InitGenesis and genesis transactions, only one can be used")
+	}
+
+	if len(valUpdates) > 0 {
+		blockResponse.ValidatorUpdates = valUpdates
 	}
 
 	return blockResponse, genesisState, err

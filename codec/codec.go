fb50a9b571053483d0c96424d90863007f8e9721
planetBoy <140164174+Guayaba221@users.noreply.github.com>
2024-12-15 11:43:00 +0100

    Update codec.go

--- codec/codec.go
+++ codec/codec.go
@@ -88,7 +88,7 @@ type (
 		// MarshalInterfaceJSON is a helper method which will wrap `i` into `Any` for correct
 		// JSON interface (de)serialization.
 		MarshalInterfaceJSON(i proto.Message) ([]byte, error)
-		// UnmarshalInterfaceJSON is a helper method which will parse JSON enoded data
+		// UnmarshalInterfaceJSON is a helper method which will parse JSON encoded data
 		// into `Any` and unpack any into the `ptr`. It fails if the target interface type
 		// is not registered in codec, or is not compatible with the serialized data
 		UnmarshalInterfaceJSON(bz []byte, ptr interface{}) error

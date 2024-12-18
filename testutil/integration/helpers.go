ca559989da43557315d7520b107ce630aeee767e
Julien Robert <julien@rbrt.fr>
2024-12-18 09:47:12 +0100

    refactor: limit breaking changes for next version (#22972)

--- testutil/integration/helpers.go
+++ testutil/integration/helpers.go
@@ -1,22 +1,9 @@
 package integration
 
 import (
-	coretesting "cosmossdk.io/core/testing"
-	"cosmossdk.io/log"
-	"cosmossdk.io/store"
-	"cosmossdk.io/store/metrics"
-	storetypes "cosmossdk.io/store/types"
+	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 )
 
 // CreateMultiStore is a helper for setting up multiple stores for provided modules.
-func CreateMultiStore(keys map[string]*storetypes.KVStoreKey, logger log.Logger) storetypes.CommitMultiStore {
-	db := coretesting.NewMemDB()
-	cms := store.NewCommitMultiStore(db, logger, metrics.NewNoOpMetrics())
-
-	for key := range keys {
-		cms.MountStoreWithDB(keys[key], storetypes.StoreTypeIAVL, db)
-	}
-
-	_ = cms.LoadLatestVersion()
-	return cms
-}
+// Deprecated: use github.com/cosmos/cosmos-sdk/types/module/testutil.CreateMultiStore instead.
+var CreateMultiStore = moduletestutil.CreateMultiStore

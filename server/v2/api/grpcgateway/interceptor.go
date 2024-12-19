dadd86df8377b8a9caae524a0e632b5a73d62501
tyler <48813565+technicallyty@users.noreply.github.com>
2024-12-19 12:41:06 -0800

    comments

--- server/v2/api/grpcgateway/interceptor.go
+++ server/v2/api/grpcgateway/interceptor.go
@@ -57,53 +57,58 @@ func newGatewayInterceptor[T transaction.Tx](logger log.Logger, gateway *runtime
 func (g *gatewayInterceptor[T]) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	g.logger.Debug("received grpc-gateway request", "request_uri", request.RequestURI)
 	match := matchURL(request.URL, g.customEndpointMapping)
-	if match != nil {
-		g.logger.Debug("matched request", "query_input", match.QueryInputName)
-		_, out := runtime.MarshalerForRequest(g.gateway, request)
-		var msg gogoproto.Message
-		var err error
-
-		switch request.Method {
-		case http.MethodPost:
-			msg, err = createMessageFromJSON(match, request)
-		case http.MethodGet:
-			msg, err = createMessage(match)
-		default:
-			runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, status.Error(codes.Unimplemented, "HTTP method must be POST or GET"))
-			return
-		}
-		if err != nil {
-			runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, err)
-			return
-		}
-
-		var height uint64
-		heightStr := request.Header.Get(GRPCBlockHeightHeader)
-		if heightStr != "" {
-			height, err = strconv.ParseUint(heightStr, 10, 64)
-			if err != nil {
-				err = status.Errorf(codes.InvalidArgument, "invalid height: %s", heightStr)
-				runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, err)
-				return
-			}
-		}
-
-		query, err := g.appManager.Query(request.Context(), height, msg)
-		if err != nil {
-			if errors.Is(err, stf.ErrNoHandler) {
-				g.gateway.ServeHTTP(writer, request)
-				return
-			}
-			runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, err)
-			return
-		}
-		runtime.ForwardResponseMessage(request.Context(), g.gateway, out, writer, request, query)
+	if match == nil {
+		// no match cases fall back to gateway mux.
+		g.gateway.ServeHTTP(writer, request)
 		return
 	}
-	g.gateway.ServeHTTP(writer, request)
+	g.logger.Debug("matched request", "query_input", match.QueryInputName)
+	_, out := runtime.MarshalerForRequest(g.gateway, request)
+	var msg gogoproto.Message
+	var err error
+
+	switch request.Method {
+	case http.MethodPost:
+		msg, err = createMessageFromJSON(match, request)
+	case http.MethodGet:
+		msg, err = createMessage(match)
+	default:
+		runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, status.Error(codes.Unimplemented, "HTTP method must be POST or GET"))
+		return
+	}
+	if err != nil {
+		runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, err)
+		return
+	}
+
+	// extract block height header
+	var height uint64
+	heightStr := request.Header.Get(GRPCBlockHeightHeader)
+	if heightStr != "" {
+		height, err = strconv.ParseUint(heightStr, 10, 64)
+		if err != nil {
+			err = status.Errorf(codes.InvalidArgument, "invalid height: %s", heightStr)
+			runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, err)
+			return
+		}
+	}
+
+	query, err := g.appManager.Query(request.Context(), height, msg)
+	if err != nil {
+		// if we couldn't find a handler for this request, just fall back to the gateway mux.
+		if errors.Is(err, stf.ErrNoHandler) {
+			g.gateway.ServeHTTP(writer, request)
+		} else {
+			// for all other errors, we just return the error.
+			runtime.DefaultHTTPProtoErrorHandler(request.Context(), g.gateway, out, writer, request, err)
+		}
+		return
+	}
+	// for no errors, we forward the response.
+	runtime.ForwardResponseMessage(request.Context(), g.gateway, out, writer, request, query)
 }
 
-// getHTTPGetAnnotationMapping returns a mapping of proto query input type full name to its RPC method's HTTP GET annotation.
+// getHTTPGetAnnotationMapping returns a mapping of RPC Method HTTP GET annotation to the RPC Handler's Request Input type full name.
 //
 // example: "/cosmos/auth/v1beta1/account_info/{address}":"cosmos.auth.v1beta1.Query.AccountInfo"
 func getHTTPGetAnnotationMapping() (map[string]string, error) {
@@ -115,19 +120,16 @@ func getHTTPGetAnnotationMapping() (map[string]string, error) {
 	httpGets := make(map[string]string)
 	protoFiles.RangeFiles(func(fd protoreflect.FileDescriptor) bool {
 		for i := 0; i < fd.Services().Len(); i++ {
-			// Get the service descriptor
-			sd := fd.Services().Get(i)
+			serviceDesc := fd.Services().Get(i)
+			for j := 0; j < serviceDesc.Methods().Len(); j++ {
+				methodDesc := serviceDesc.Methods().Get(j)
 
-			for j := 0; j < sd.Methods().Len(); j++ {
-				// Get the method descriptor
-				md := sd.Methods().Get(j)
-
-				httpOption := proto.GetExtension(md.Options(), annotations.E_Http)
-				if httpOption == nil {
+				httpAnnotation := proto.GetExtension(methodDesc.Options(), annotations.E_Http)
+				if httpAnnotation == nil {
 					continue
 				}
 
-				httpRule, ok := httpOption.(*annotations.HttpRule)
+				httpRule, ok := httpAnnotation.(*annotations.HttpRule)
 				if !ok || httpRule == nil {
 					continue
 				}
@@ -135,7 +137,7 @@ func getHTTPGetAnnotationMapping() (map[string]string, error) {
 					continue
 				}
 
-				httpGets[httpRule.GetGet()] = string(md.Input().FullName())
+				httpGets[httpRule.GetGet()] = string(methodDesc.Input().FullName())
 			}
 		}
 		return true

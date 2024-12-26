d0c20bc255f33da27a7aae3187f8fd6ae68d274a
crStiv <cryptostiv7@gmail.com>
2024-12-26 01:32:55 +0100

    Update config.go

--- server/v2/api/swagger/config.go
+++ server/v2/api/swagger/config.go
@@ -1,27 +1,23 @@
 package swagger
 
-import "github.com/cosmos/cosmos-sdk/server/v2/config"
+import (
+    "cosmossdk.io/core/server"
+)
 
-// Config represents Swagger configuration options
+const ServerName = "swagger"
+
 type Config struct {
-    // Enable enables the Swagger UI endpoint
-    Enable bool `mapstructure:"enable"`
-    // Path is the URL path where Swagger UI will be served
-    Path string `mapstructure:"path"`
+    Enable  bool   `toml:"enable" mapstructure:"enable"`
+    Address string `toml:"address" mapstructure:"address"`
+    Path    string `toml:"path" mapstructure:"path"`
 }
 
-// DefaultConfig returns default configuration for Swagger
-func DefaultConfig() Config {
-    return Config{
-        Enable: false,
-        Path:   "/swagger",
+func DefaultConfig() *Config {
+    return &Config{
+        Enable:  true,
+        Address: "localhost:8080",
+        Path:    "/swagger/",
     }
 }
 
-// Validate validates the configuration
-func (c Config) Validate() error {
-    if c.Path == "" {
-        return fmt.Errorf("swagger path cannot be empty")
-    }
-    return nil
-} 
+type CfgOption func(*Config)

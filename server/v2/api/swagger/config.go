ac5982aef33f692f9f19c7c65d976b1eb1ae090e
crStiv <cryptostiv7@gmail.com>
2024-12-26 01:39:41 +0100

    Update config.go

--- server/v2/api/swagger/config.go
+++ server/v2/api/swagger/config.go
@@ -1,17 +1,21 @@
 package swagger
 
 import (
+    "fmt"
+
     "cosmossdk.io/core/server"
 )
 
 const ServerName = "swagger"
 
+// Config defines the configuration for the Swagger UI server
 type Config struct {
     Enable  bool   `toml:"enable" mapstructure:"enable"`
     Address string `toml:"address" mapstructure:"address"`
     Path    string `toml:"path" mapstructure:"path"`
 }
 
+// DefaultConfig returns the default configuration
 func DefaultConfig() *Config {
     return &Config{
         Enable:  true,
@@ -20,4 +24,13 @@ func DefaultConfig() *Config {
     }
 }
 
+// Validate checks the configuration
+func (c Config) Validate() error {
+    if c.Path == "" {
+        return fmt.Errorf("swagger path cannot be empty")
+    }
+    return nil
+}
+
+// CfgOption defines a function for configuring the settings
 type CfgOption func(*Config)

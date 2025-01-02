ae4e0dec32570ef61b022f9890b756b36ae487c7
mmsqe <mavis@crypto.com>
2025-01-02 22:09:54 +0800

    lint

--- x/auth/tx/builder_test.go
+++ x/auth/tx/builder_test.go
@@ -3,9 +3,10 @@ package tx
 import (
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/types/tx"
 	any "github.com/cosmos/gogoproto/types/any"
 	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/types/tx"
 )
 
 func TestIntoV2SignerInfo(t *testing.T) {

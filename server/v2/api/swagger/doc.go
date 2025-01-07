8ccb884141b5e405e20f564ce634149d3b2851d7
crStiv <cryptostiv7@gmail.com>
2025-01-07 03:26:08 +0100

    Update doc.go

--- server/v2/api/swagger/doc.go
+++ server/v2/api/swagger/doc.go
@@ -7,20 +7,10 @@ Example usage in commands.go:
 	swaggerServer, err := swaggerv2.New[T](
 		logger.With(log.ModuleKey, "swagger"),
 		deps.GlobalConfig,
-		swaggerv2.CfgOption(func(cfg *swaggerv2.Config) {
-			cfg.SwaggerUI = docs.SwaggerUI
-		}),
-	)
-
-	// Add server to your application
-	return serverv2.AddCommands[T](
-		// ...other servers...,
-		swaggerServer,
 	)
 
 Configuration options:
-  - enable: Enable/disable the Swagger server (default: true)
-  - address: Server address (default: localhost:8080)
-  - path: UI endpoint path (default: /swagger/)
+  - enable: Enable/disable the Swagger UI server (default: true)
+  - address: Server address (default: localhost:8090)
 */
 package swagger 

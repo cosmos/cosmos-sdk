6ee02602102cc012419f09315ef2aad6b89d9d86
marbar3778 <marbar3778@yahoo.com>
2025-01-08 11:34:09 +0100

    store fix

--- store/v2/commitment/store_test_suite.go
+++ store/v2/commitment/store_test_suite.go
@@ -176,7 +176,7 @@ func (s *CommitStoreTestSuite) TestStore_LoadVersion() {
 	for i := uint64(1); i <= latestVersion; i++ {
 		commitInfo, _ := targetStore.GetCommitInfo(i)
 		s.Require().NotNil(commitInfo)
-		s.Require().Equal(i, commitInfo.Version)
+		s.Require().Equal(i, uint64(commitInfo.Version))
 	}
 
 	// rollback to a previous version

80b14226847149be1837fdc80df9b06d2327128e
DevOrbitlabs <hoa@notional.ventures>
2025-01-10 00:52:05 +0700

    chore(x/auth): use cosmossdk.io/core/codec instead of github.com/cosmos/cosmos-sdk/codec (#23273)

--- x/auth/tx/builder.go
+++ x/auth/tx/builder.go
@@ -14,10 +14,10 @@ import (
 	signingv1beta1 "cosmossdk.io/api/cosmos/tx/signing/v1beta1"
 	txv1beta1 "cosmossdk.io/api/cosmos/tx/v1beta1"
 	"cosmossdk.io/core/address"
+	"cosmossdk.io/core/codec"
 	"cosmossdk.io/x/tx/decode"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"

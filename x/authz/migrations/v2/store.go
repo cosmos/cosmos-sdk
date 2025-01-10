ecd7a383893dc24a74d654cdccaa04eb0c9cef3c
Khanh Hoa <hoa@notional.ventures>
2025-01-10 10:49:26 +0700

    update

--- x/authz/migrations/v2/store.go
+++ x/authz/migrations/v2/store.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/core/codec"
 	"cosmossdk.io/store/prefix"
 	"cosmossdk.io/x/authz"
 	"cosmossdk.io/x/authz/internal/conv"
 
-	"cosmossdk.io/core/codec"
 	"github.com/cosmos/cosmos-sdk/runtime"
 )
 

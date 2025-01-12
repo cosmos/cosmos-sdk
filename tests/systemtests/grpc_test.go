95ebdb9669d0f7d122fa84a2465d0f3e66fb00be
Julien Robert <julien@rbrt.fr>
2025-01-12 15:00:26 +0100

    add test query

--- tests/systemtests/grpc_test.go
+++ tests/systemtests/grpc_test.go
@@ -3,10 +3,12 @@
 package systemtests
 
 import (
+	"bytes"
 	"context"
+	"os"
 	"testing"
 
-	"github.com/fullstorydev/grpcurl"
+	"github.com/fullstorydev/grpcurl" //nolint:staticcheck: input in grpcurl
 	"github.com/jhump/protoreflect/grpcreflect"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
@@ -28,4 +30,55 @@ func TestGRPCReflection(t *testing.T) {
 	require.NoError(t, err)
 	require.Greater(t, len(services), 0)
 	require.Contains(t, services, "cosmos.staking.v1beta1.Query")
+}
+
+func TestGRPCQuery(t *testing.T) {
+	systest.Sut.ResetChain(t)
+	systest.Sut.StartChain(t)
+
+	ctx := context.Background()
+	grpcClient, err := grpc.NewClient("localhost:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err)
+
+	descSource := grpcurl.DescriptorSourceFromServer(ctx, grpcreflect.NewClientAuto(ctx, grpcClient))
+
+	rf, formatter, err := grpcurl.RequestParserAndFormatter(grpcurl.FormatText, descSource, os.Stdin, grpcurl.FormatOptions{})
+	require.NoError(t, err)
+
+	buf := &bytes.Buffer{}
+	h := &grpcurl.DefaultEventHandler{
+		Out:            buf,
+		Formatter:      formatter,
+		VerbosityLevel: 0,
+	}
+
+	err = grpcurl.InvokeRPC(ctx, descSource, grpcClient, "cosmos.staking.v1beta1.Query/Params", nil, h, rf.Next)
+	require.NoError(t, err)
+	require.Contains(t, buf.String(), "max_validators")
+}
+
+func TestGRPCQueryAutoCLIOptions(t *testing.T) {
+	t.Skip() // TODO(@julienrbrt): re-add autocli query in v2 in follow-up
+
+	systest.Sut.ResetChain(t)
+	systest.Sut.StartChain(t)
+
+	ctx := context.Background()
+	grpcClient, err := grpc.NewClient("localhost:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err)
+
+	descSource := grpcurl.DescriptorSourceFromServer(ctx, grpcreflect.NewClientAuto(ctx, grpcClient))
+
+	rf, formatter, err := grpcurl.RequestParserAndFormatter(grpcurl.FormatText, descSource, os.Stdin, grpcurl.FormatOptions{})
+	require.NoError(t, err)
+
+	buf := &bytes.Buffer{}
+	h := &grpcurl.DefaultEventHandler{
+		Out:            buf,
+		Formatter:      formatter,
+		VerbosityLevel: 0,
+	}
+
+	err = grpcurl.InvokeRPC(ctx, descSource, grpcClient, "cosmos.autocli.v1.Query/AppOptions", nil, h, rf.Next)
+	require.NoError(t, err)
 }

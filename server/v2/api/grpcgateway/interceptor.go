c103c74561a4e713678245800b01a9b530c0aded
tyler <48813565+technicallyty@users.noreply.github.com>
2025-01-12 16:15:07 -0800

    dont include that in the main mapping though

--- server/v2/api/grpcgateway/interceptor.go
+++ server/v2/api/grpcgateway/interceptor.go
@@ -237,23 +237,30 @@ func createRegexMapping(logger log.Logger, annotationMapping map[string]string) 
 	for annotation, queryInputName := range annotationMapping {
 		pattern, wildcardNames := patternToRegex(annotation)
 		if len(wildcardNames) == 0 {
+			if otherAnnotation, ok := annotationMapping[annotation]; ok {
+				// TODO: eventually we want this to error, but there is currently a duplicate in the protobuf.
+				// see: https://github.com/cosmos/cosmos-sdk/issues/23281
+				logger.Warn("duplicate HTTP annotation found", "annotation1", annotation, "annotation2", otherAnnotation, "query_input_name", queryInputName)
+			}
 			simpleMatchers[annotation] = queryMetadata{
 				queryInputProtoName: queryInputName,
 				wildcardKeyNames:    nil,
 			}
 			seenPatterns[annotation] = annotation
-		}
-		reg := regexp.MustCompile(pattern)
-		if otherAnnotation, ok := seenPatterns[pattern]; !ok {
-			seenPatterns[pattern] = annotation
 		} else {
-			// TODO: eventually we want this to error, but there is currently a duplicate in the protobuf.
-			// see: https://github.com/cosmos/cosmos-sdk/issues/23281
-			logger.Warn("duplicate HTTP annotation found", "annotation1", annotation, "annotation2", otherAnnotation, "query_input_name", queryInputName)
-		}
-		wildcardMatchers[reg] = queryMetadata{
-			queryInputProtoName: queryInputName,
-			wildcardKeyNames:    wildcardNames,
+			reg := regexp.MustCompile(pattern)
+			if otherAnnotation, ok := seenPatterns[pattern]; ok {
+
+				// TODO: eventually we want this to error, but there is currently a duplicate in the protobuf.
+				// see: https://github.com/cosmos/cosmos-sdk/issues/23281
+				logger.Warn("duplicate HTTP annotation found", "annotation1", annotation, "annotation2", otherAnnotation, "query_input_name", queryInputName)
+			}
+			wildcardMatchers[reg] = queryMetadata{
+				queryInputProtoName: queryInputName,
+				wildcardKeyNames:    wildcardNames,
+			}
+			seenPatterns[pattern] = annotation
+
 		}
 	}
 	return wildcardMatchers, simpleMatchers

8b85a1cc917808026d16f02753a7b5466fb1a100
tyler <48813565+technicallyty@users.noreply.github.com>
2025-01-13 09:22:39 -0800

    fix error check output

--- tests/systemtests/distribution_test.go
+++ tests/systemtests/distribution_test.go
@@ -179,7 +179,7 @@ func TestDistrValidatorGRPCQueries(t *testing.T) {
 
 	// test validator slashes grpc endpoint
 	slashURL := baseurl + `/cosmos/distribution/v1beta1/validators/%s/slashes`
-	invalidHeightOutput := `"code":"NUMBER", "details":[]interface {}{}, "message":"strconv.ParseUint: parsing \"NUMBER\": invalid syntax"`
+	invalidHeightOutput := `{"code":"NUMBER", "details":[]interface {}{}, "message":"strconv.ParseUint: parsing \"NUMBER\": invalid syntax"}`
 
 	slashTestCases := []systest.RestTestCase{
 		{

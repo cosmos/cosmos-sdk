aac283b48d937d73d345a6bdbe92d2fd571d3a96
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-01-15 11:09:06 -0800

    fix doc.go

--- server/v2/api/grpcgateway/doc.go
+++ server/v2/api/grpcgateway/doc.go
@@ -1,7 +1,6 @@
-// Package grpcgateway provides a custom http mux that utilizes the global gogoproto registry to match
-// to create dynamic query handlers.
+// Package grpcgateway utilizes the global gogoproto registry to create dynamic query handlers on net/http's mux router.
 //
 // Header `x-cosmos-block-height` allows you to specify a height for the query.
 //
-// Requests that do not have a dynamic handler will be routed to the canonical gRPC gateway mux.
+// Requests that do not have a dynamic handler registered will be routed to the canonical gRPC gateway mux.
 package grpcgateway

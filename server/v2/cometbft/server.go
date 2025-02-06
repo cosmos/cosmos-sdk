e3a2d6b3cf22d65b23256c80965cba4300ceeb4f
wonderfan <fanjiahe2000@163.com>
2025-02-06 03:26:00 +0000

    fix: add missing rollback command

--- server/v2/cometbft/server.go
+++ server/v2/cometbft/server.go
@@ -353,6 +353,7 @@ func (s *CometBFTServer[T]) CLICommands() serverv2.CLIConfig {
 			s.BootstrapStateCmd(),
 			cmtcmd.ResetAllCmd,
 			cmtcmd.ResetStateCmd,
+			cmtcmd.RollbackStateCmd,
 		},
 		Queries: []*cobra.Command{
 			QueryBlockCmd(),

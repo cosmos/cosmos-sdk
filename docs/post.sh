4f9882b5523c89ac74f6a260818dc72788bfb2d9
Alex | Interchain Labs <alex@interchainlabs.io>
2025-02-12 14:13:20 -0500

    chore: bump `internal` to `main` (#23681)

--- docs/post.sh
+++ docs/post.sh
@@ -6,7 +6,6 @@ rm -rf docs/build/tooling/02-confix.md
 rm -rf docs/build/tooling/03-hubl.md
 rm -rf docs/build/packages/01-depinject.md
 rm -rf docs/build/packages/02-collections.md
-rm -rf docs/build/packages/03-orm.md
 rm -rf docs/learn/advaced-concepts/17-autocli.md
 rm -rf docs/user/run-node/04-rosetta.md
 rm -rf docs/build/architecture

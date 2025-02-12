f5015cb60415dcb083fc05770d9aadd243c1e274
Avory <avorycorelli@gmail.com>
2025-02-12 22:11:36 +0200

    Update config.go

--- store/v2/commitment/iavl/config.go
+++ store/v2/commitment/iavl/config.go
@@ -1,15 +1,28 @@
+// Package iavl implements the IAVL tree commitment store.
 package iavl
 
-// Config is the configuration for the IAVL tree.
+// Config contains the configuration for the IAVL tree.
+// It provides options to customize the behavior of the tree.
 type Config struct {
-	CacheSize              int  `mapstructure:"cache-size" toml:"cache-size" comment:"CacheSize set the size of the iavl tree cache."`
-	SkipFastStorageUpgrade bool `mapstructure:"skip-fast-storage-upgrade" toml:"skip-fast-storage-upgrade" comment:"If true, the tree will work like no fast storage and always not upgrade fast storage."`
+	// CacheSize is the size of the cache in memory.
+	// A larger cache size can improve performance at the cost of memory usage.
+	CacheSize int `mapstructure:"cache-size"`
+
+	// SkipFastStorageUpgrade determines whether to skip the fast storage upgrade.
+	// If true, the tree will work like no fast storage and always not upgrade fast storage.
+	SkipFastStorageUpgrade bool `mapstructure:"skip-fast-storage-upgrade"`
+
+	// EnableHistoricalQueries enables querying historical data from the old tree.
+	// When enabled, queries for versions before the migration height will use the old tree.
+	EnableHistoricalQueries bool `mapstructure:"enable-historical-queries"`
 }
 
-// DefaultConfig returns the default configuration for the IAVL tree.
+// DefaultConfig returns the default configuration.
+// It provides sensible defaults for all configuration options.
 func DefaultConfig() *Config {
 	return &Config{
-		CacheSize:              1000,
+		CacheSize:              1000000,
 		SkipFastStorageUpgrade: false,
+		EnableHistoricalQueries: false,
 	}
 }

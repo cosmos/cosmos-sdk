1b45d90439e50a170d5d678e6cb058876edf15df
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-02-13 10:41:43 -0800

    depinject v53

--- depinject/invoke_test.go
+++ depinject/invoke_test.go
@@ -3,32 +3,14 @@ package depinject_test
 import (
 	"testing"
 
-	"github.com/regen-network/gocuke"
-	"gotest.tools/v3/assert"
+	"github.com/stretchr/testify/require"
 
 	"cosmossdk.io/depinject"
 )
 
-func TestInvoke(t *testing.T) {
-	gocuke.NewRunner(t, &InvokeSuite{}).
-		Path("features/invoke.feature").
-		Step("an int provider returning 5", (*InvokeSuite).AnIntProviderReturning5).
-		Step(`a string pointer provider pointing to "foo"`, (*InvokeSuite).AStringPointerProviderPointingToFoo).
-		Run()
-}
-
 type InvokeSuite struct {
-	gocuke.TestingT
-	configs []depinject.Config
-	i       int
-	sp      *string
-}
-
-func (s *InvokeSuite) AnInvokerRequestingAnIntAndStringPointer() {
-	s.configs = append(s.configs,
-		depinject.Supply(s),
-		depinject.Invoke((*InvokeSuite).IntStringPointerInvoker),
-	)
+	i  int
+	sp *string
 }
 
 func (s *InvokeSuite) IntStringPointerInvoker(i int, sp *string) {
@@ -36,53 +18,67 @@ func (s *InvokeSuite) IntStringPointerInvoker(i int, sp *string) {
 	s.sp = sp
 }
 
-func (s *InvokeSuite) TheContainerIsBuilt() {
-	assert.NilError(s, depinject.Inject(depinject.Configs(s.configs...)))
-}
-
-func (s *InvokeSuite) TheInvokerWillGetTheIntParameterSetTo(a int64) {
-	assert.Equal(s, int(a), s.i)
-}
-
-func (s *InvokeSuite) TheInvokerWillGetTheStringPointerParameterSetToNil() {
-	if s.sp != nil {
-		s.Fatalf("expected a nil string pointer, got %s", *s.sp)
-	}
+func ProvideLenModuleKey(key depinject.ModuleKey) int {
+	return len(key.Name())
 }
 
 func IntProvider5() int { return 5 }
-
-func (s *InvokeSuite) AnIntProviderReturning5() {
-	s.configs = append(s.configs, depinject.Provide(IntProvider5))
-}
 
 func StringPtrProviderFoo() *string {
 	x := "foo"
 	return &x
 }
 
-func (s *InvokeSuite) AStringPointerProviderPointingToFoo() {
-	s.configs = append(s.configs, depinject.Provide(StringPtrProviderFoo))
-}
+func TestInvokerNoResolvableDependencies(t *testing.T) {
+	t.Parallel()
 
-func (s *InvokeSuite) TheInvokerWillGetTheStringPointerParameterSetTo(a string) {
-	if s.sp == nil {
-		s.Fatalf("expected a non-nil string pointer")
-	}
-	assert.Equal(s, a, *s.sp)
-}
-
-func (s *InvokeSuite) AnInvokerRequestingAnIntAndStringPointerRunInModule(a string) {
-	s.configs = append(s.configs,
-		depinject.Supply(s),
-		depinject.InvokeInModule(a, (*InvokeSuite).IntStringPointerInvoker),
+	invokerSuite := &InvokeSuite{}
+	configs := depinject.Configs(
+		depinject.Supply(invokerSuite),
+		depinject.Invoke((*InvokeSuite).IntStringPointerInvoker),
 	)
+
+	err := depinject.Inject(configs)
+	require.NoError(t, err)
+
+	// invokers get called even if their dependencies can't be resolved
+	// values are still zeroed
+	require.Equal(t, 0, invokerSuite.i)
+	require.Equal(t, (*string)(nil), invokerSuite.sp)
 }
 
-func ProvideLenModuleKey(key depinject.ModuleKey) int {
-	return len(key.Name())
+func TestInvokerProvidedDependencies(t *testing.T) {
+	t.Parallel()
+
+	invokerSuite := &InvokeSuite{}
+	configs := depinject.Configs(
+		depinject.Supply(invokerSuite),
+		depinject.Provide(IntProvider5, StringPtrProviderFoo),
+		depinject.Invoke((*InvokeSuite).IntStringPointerInvoker),
+	)
+
+	err := depinject.Inject(configs)
+	require.NoError(t, err)
+
+	require.Equal(t, 5, invokerSuite.i)
+	require.Equal(t, "foo", *invokerSuite.sp)
 }
 
-func (s *InvokeSuite) AModulescopedIntProviderWhichReturnsTheLengthOfTheModuleName() {
-	s.configs = append(s.configs, depinject.Provide(ProvideLenModuleKey))
+func TestInvokerScopedDependencies(t *testing.T) {
+	t.Parallel()
+
+	moduleName := "test"
+
+	invokerSuite := &InvokeSuite{}
+	configs := depinject.Configs(
+		depinject.Supply(invokerSuite),
+		depinject.Provide(ProvideLenModuleKey),
+		depinject.InvokeInModule(moduleName, (*InvokeSuite).IntStringPointerInvoker),
+	)
+
+	err := depinject.Inject(configs)
+	require.NoError(t, err)
+
+	require.Equal(t, len(moduleName), invokerSuite.i)
+	require.Equal(t, (*string)(nil), invokerSuite.sp)
 }

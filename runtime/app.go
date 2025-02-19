17f2bf9777a190a065ed613d8f08d9f4566f68fd
aljo242 <alex@interchainlabs.io>
2025-02-19 11:37:44 -0500

    cleanup

--- runtime/app.go
+++ runtime/app.go
@@ -115,6 +115,13 @@ func (a *App) Load(loadLatest bool) error {
 		a.ModuleManager.SetOrderExportGenesis(a.config.InitGenesis...)
 	}
 
+	if len(a.config.PreBlockers) != 0 {
+		a.ModuleManager.SetOrderPreBlockers(a.config.PreBlockers...)
+		if a.BaseApp.PreBlocker() == nil {
+			a.SetPreBlocker(a.PreBlocker)
+		}
+	}
+
 	if len(a.config.BeginBlockers) != 0 {
 		a.ModuleManager.SetOrderBeginBlockers(a.config.BeginBlockers...)
 		a.SetBeginBlocker(a.BeginBlocker)

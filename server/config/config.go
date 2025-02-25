3221719c3d51ce5083fa3098d77fc0e621dd2569
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-02-25 07:36:57 -0800

    foo

--- server/config/config.go
+++ server/config/config.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"math"
 
-	"github.com/spf13/viper"
-
 	pruningtypes "cosmossdk.io/store/pruning/types"
+	"github.com/spf13/viper"
 
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"

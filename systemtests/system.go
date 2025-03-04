396b2e4d69c04c87d796f4efe7181793c156713f
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-03-04 11:53:00 -0800

    use combined output

--- systemtests/system.go
+++ systemtests/system.go
@@ -579,7 +579,7 @@ func RunShellCmd(cmd string, args ...string) (string, error) {
 		args...,
 	)
 	c.Dir = WorkDir
-	out, err := c.Output()
+	out, err := c.CombinedOutput()
 	if err != nil {
 		return string(out), fmt.Errorf("run `%s %s`: out: %s: %w", cmd, strings.Join(args, " "), string(out), err)
 	}

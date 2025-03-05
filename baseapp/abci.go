37c9fc742cc92f25bdd5fb588030838eafe83ac2
mmsqe <mavis@crypto.com>
2025-03-06 01:59:07 +0800

    fix(baseapp): ensure finalize block response is not empty (#23879)
    (cherry picked from commit 05df209fa4dbed87fc7be6006f79ef701262755c)
    
    # Conflicts:
    #	CHANGELOG.md

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -855,6 +855,9 @@ func (app *BaseApp) internalFinalizeBlock(ctx context.Context, req *abci.Request
 // where they adhere to the sdk.Tx interface.
 func (app *BaseApp) FinalizeBlock(req *abci.RequestFinalizeBlock) (res *abci.ResponseFinalizeBlock, err error) {
 	defer func() {
+		if res == nil {
+			return
+		}
 		// call the streaming service hooks with the FinalizeBlock messages
 		for _, streamingListener := range app.streamingManager.ABCIListeners {
 			if err := streamingListener.ListenFinalizeBlock(app.finalizeBlockState.Context(), *req, *res); err != nil {

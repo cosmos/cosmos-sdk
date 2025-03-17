082dd48253a1ac77c9aea74c986fbaba39f6bddf
aljo242 <alex@interchainlabs.io>
2025-03-17 17:17:45 -0400

    check and fix

--- x/protocolpool/keeper/grpc_query_test.go
+++ x/protocolpool/keeper/grpc_query_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"go.uber.org/mock/gomock"
 	"time"
 
 	"cosmossdk.io/math"
@@ -157,6 +158,52 @@ func (suite *KeeperTestSuite) TestParams() {
 				tc.preRun()
 			}
 			resp, err := suite.queryServer.Params(suite.ctx, tc.req)
+			if tc.expErr {
+				suite.Require().Error(err)
+				suite.Require().Contains(err.Error(), tc.expErrMsg)
+			} else {
+				suite.Require().NoError(err)
+				suite.Require().Equal(tc.resp, resp)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestCommunityPool() {
+	testCases := []struct {
+		name      string
+		preRun    func()
+		req       *types.QueryCommunityPoolRequest
+		expErr    bool
+		expErrMsg string
+		resp      *types.QueryCommunityPoolResponse
+	}{
+		{
+			name:      "nil request	- error",
+			req:       nil,
+			expErr:    true,
+			expErrMsg: "",
+		},
+		{
+			name: "valid",
+			preRun: func() {
+				suite.authKeeper.EXPECT().GetModuleAccount(gomock.Any(), types.ModuleName).Return(poolAcc).Times(1)
+				suite.bankKeeper.EXPECT().GetAllBalances(gomock.Any(), poolAcc.GetAddress()).Return(sdk.NewCoins(fooCoin)).Times(1)
+			},
+			req: &types.QueryCommunityPoolRequest{},
+			resp: &types.QueryCommunityPoolResponse{
+				Pool: sdk.NewCoins(fooCoin),
+			},
+			expErr:    false,
+			expErrMsg: "",
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			if tc.preRun != nil {
+				tc.preRun()
+			}
+			resp, err := suite.queryServer.CommunityPool(suite.ctx, tc.req)
 			if tc.expErr {
 				suite.Require().Error(err)
 				suite.Require().Contains(err.Error(), tc.expErrMsg)

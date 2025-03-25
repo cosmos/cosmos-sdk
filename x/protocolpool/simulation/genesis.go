312474ae75473b08c129345e62c4701358d43223
aljo242 <alex@interchainlabs.io>
2025-03-25 15:55:45 -0400

    simplify

--- x/protocolpool/simulation/genesis.go
+++ x/protocolpool/simulation/genesis.go
@@ -1,8 +1,6 @@
 package simulation
 
 import (
-	"encoding/json"
-	"fmt"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -30,10 +28,5 @@ func RandomizedGenState(simState *module.SimulationState) {
 	genesis := types.DefaultGenesisState()
 	genesis.Params = params
 
-	bz, err := json.MarshalIndent(genesis, "", " ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Selected randomly generated protocolpool parameters:\n%s\n", bz)
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(genesis)
 }

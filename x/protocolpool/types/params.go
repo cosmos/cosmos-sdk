a0c82e6e223695e5e694941a352dcb74705b4342
aljo242 <alex@interchainlabs.io>
2025-03-26 18:24:21 -0400

    duplicate check

--- x/protocolpool/types/params.go
+++ x/protocolpool/types/params.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"errors"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -13,15 +14,22 @@ func DefaultParams() Params {
 	}
 }
 
-func (params *Params) Validate() error {
-	for _, d := range params.EnabledDistributionDenoms {
+func (p *Params) Validate() error {
+	seenDenoms := make(map[string]struct{})
+
+	for _, d := range p.EnabledDistributionDenoms {
 		err := sdk.ValidateDenom(d)
 		if err != nil {
 			return err
 		}
+
+		if _, seen := seenDenoms[d]; seen {
+			return fmt.Errorf("duplicate enabled distribution denom %s", d)
+		}
+		seenDenoms[d] = struct{}{}
 	}
 
-	if params.DistributionFrequency == 0 {
+	if p.DistributionFrequency == 0 {
 		return errors.New("DistributionFrequency must be greater than 0")
 	}
 

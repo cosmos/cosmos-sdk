f32116137465449d28c10ea079ab3e0ad2094fc9
Hoang Do <hoangdv2429@gmail.com>
2025-03-26 10:03:37 +0700

    Update baseapp/options.go
    Co-authored-by: Tyler <48813565+technicallyty@users.noreply.github.com>

--- baseapp/options.go
+++ baseapp/options.go
@@ -346,7 +346,7 @@ func (app *BaseApp) SetPrepareProposal(handler sdk.PrepareProposalHandler) {
 // SetCheckTx sets the checkTx function for the BaseApp.
 func (app *BaseApp) SetCheckTxHandler(handler sdk.CheckTxHandler) {
 	if app.sealed {
-		panic("SetCheckTx() on sealed BaseApp")
+		panic("SetCheckTxHandler() on sealed BaseApp")
 	}
 
 	app.checkTxHandler = handler

594fedee8bbe7ae3631c8b22cfd68b1b5214823b
Michael FIG <mfig@agoric.com>
2025-03-26 18:41:35 -0600

    test(crypto): fix `Example*` function names

--- crypto/ledger/encode_test.go
+++ crypto/ledger/encode_test.go
@@ -30,7 +30,7 @@ func checkAminoJSON(t *testing.T, src interface{}, dst interface{}, isNil bool) 
 
 // nolint: govet
 func ExamplePrintRegisteredTypes() {
-	_ = cdc.PrintTypes(os.Stdout)
+	_ = PrintRegisteredTypes(os.Stdout)
 	// | Type | Name | Prefix | Length | Notes |
 	// | ---- | ---- | ------ | ----- | ------ |
 	// | PrivKeyLedgerSecp256k1 | tendermint/PrivKeyLedgerSecp256k1 | 0x10CAB393 | variable |  |

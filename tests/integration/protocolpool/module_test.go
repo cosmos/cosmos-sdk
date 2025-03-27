f89b39a0f686bc2478167112ec18c797e999c736
Alex | Interchain Labs <alex@interchainlabs.io>
2025-03-27 15:10:34 -0400

    test: `x/protocolpool` e2e testing (#24064)

--- tests/integration/protocolpool/module_test.go
+++ tests/integration/protocolpool/module_test.go
@@ -9,15 +9,25 @@ import (
 	"cosmossdk.io/log"
 
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	"github.com/cosmos/cosmos-sdk/x/protocolpool/testutil"
+	"github.com/cosmos/cosmos-sdk/x/protocolpool/types"
 )
 
 func TestCreateTestModule(t *testing.T) {
-	_, err := simtestutil.SetupAtGenesis(
+	app, err := simtestutil.SetupAtGenesis(
 		depinject.Configs(
 			testutil.AppConfig,
 			depinject.Supply(log.NewNopLogger()),
 		),
 	)
 	assert.NilError(t, err)
+
+	gotModule, found := app.ModuleManager.Modules[types.ModuleName]
+	assert.Assert(t, found)
+	assert.Assert(t, gotModule != nil)
+
+	gotModule, found = app.ModuleManager.Modules[distrtypes.ModuleName]
+	assert.Assert(t, found)
+	assert.Assert(t, gotModule != nil)
 }

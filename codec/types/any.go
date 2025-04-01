3be27970367245a6a55fe25fd3688914a21a8466
Aaron Craelius <aaronc@users.noreply.github.com>
2025-04-01 19:44:41 -0400

    add alias docs

--- codec/types/any.go
+++ codec/types/any.go
@@ -4,8 +4,11 @@ import (
 	gogoany "github.com/cosmos/gogoproto/types/any"
 )
 
+// Any is an alias for github.com/cosmos/gogoproto/types/any.Any
 type Any = gogoany.Any
 
+// NewAnyWithValue is an alias for github.com/cosmos/gogoproto/types/any.NewAnyWithCacheWithValue
 var NewAnyWithValue = gogoany.NewAnyWithCacheWithValue
 
+// UnsafePackAny is an alias for github.com/cosmos/gogoproto/types/any.UnsafePackAnyWithCache
 var UnsafePackAny = gogoany.UnsafePackAnyWithCache

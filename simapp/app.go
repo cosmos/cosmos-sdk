92006c8707b23d9a7c276880ecaa7d321816ac60
aljo242 <alex@interchainlabs.io>
2025-04-08 16:12:35 -0400

    ok

--- simapp/app.go
+++ simapp/app.go
@@ -470,7 +470,7 @@ func NewSimApp(
 
 	app.GovKeeper = *govKeeper.SetHooks(
 		govtypes.NewMultiGovHooks(
-			// register the governance hooks
+		// register the governance hooks
 		),
 	)
 
@@ -500,7 +500,7 @@ func NewSimApp(
 
 	app.EpochsKeeper.SetHooks(
 		epochstypes.NewMultiEpochHooks(
-			// insert epoch hooks receivers here
+		// insert epoch hooks receivers here
 		),
 	)
 

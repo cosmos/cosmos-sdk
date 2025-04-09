30db76893619281a08b1ebbb54396d04cf9a3874
Julien Robert <julien@rbrt.fr>
2025-04-09 17:53:59 +0200

    fix (should be backported)

--- x/gov/client/cli/util.go
+++ x/gov/client/cli/util.go
@@ -131,7 +131,7 @@ func AddGovPropFlagsToCmd(cmd *cobra.Command) {
 	cmd.Flags().String(FlagMetadata, "", "The metadata to include with the governance proposal")
 	cmd.Flags().String(FlagTitle, "", "The title to put on the governance proposal")
 	cmd.Flags().String(FlagSummary, "", "The summary to include with the governance proposal")
-	cmd.Flags().Bool(FlagExpedited, false, "Whether to expedite the governance proposal")
+	// cmd.Flags().Bool(FlagExpedited, false, "Whether to expedite the governance proposal") // cannot be enabled because of IBC redefining this flag in `upgrade-channels` command.
 }
 
 // ReadGovPropCmdFlags parses a MsgSubmitProposal from the provided context and flags.

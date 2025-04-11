2827fbe18d29651b9c0ac478aeb17c5159ed9391
Eric Warehime <eric.warehime@gmail.com>
2025-04-11 11:53:25 -0700

    StreamingManager already included in baseapp

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -428,11 +428,6 @@ func (app *BaseApp) Mempool() mempool.Mempool {
 	return app.mempool
 }
 
-// StreamingManager returns the StreamingManager of the app.
-func (app *BaseApp) StreamingManager() storetypes.StreamingManager {
-	return app.streamingManager
-}
-
 // Init initializes the app. It seals the app, preventing any
 // further modifications. In addition, it validates the app against
 // the earlier provided settings. Returns an error if validation fails.

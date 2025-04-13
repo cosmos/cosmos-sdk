949bbe82b6e5428966ed7e42620abde07959e75f
Thomas Nguy <thomas.nguy@crypto.com>
2025-04-13 18:01:26 +0900

    some cleanup

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -352,7 +352,7 @@ func (app *BaseApp) CheckTx(req *abci.RequestCheckTx) (*abci.ResponseCheckTx, er
 	}
 
 	if app.checkTxHandler == nil {
-		gInfo, result, anteEvents, err := app.runTx(mode, req.Tx, nil)
+		gInfo, result, anteEvents, err := app.RunTx(mode, req.Tx, nil, nil, nil)
 		if err != nil {
 			return sdkerrors.ResponseCheckTxWithEvents(err, gInfo.GasWanted, gInfo.GasUsed, anteEvents, app.trace), nil
 		}
@@ -368,7 +368,7 @@ func (app *BaseApp) CheckTx(req *abci.RequestCheckTx) (*abci.ResponseCheckTx, er
 
 	// Create wrapper to avoid users overriding the execution mode
 	runTx := func(txBytes []byte, tx sdk.Tx) (gInfo sdk.GasInfo, result *sdk.Result, anteEvents []abci.Event, err error) {
-		return app.runTx(mode, txBytes, tx)
+		return app.RunTx(mode, txBytes, tx, nil, nil)
 	}
 
 	return app.checkTxHandler(runTx, req)
@@ -833,43 +833,15 @@ func (app *BaseApp) internalFinalizeBlock(ctx context.Context, req *abci.Request
 }
 
 func (app *BaseApp) executeTxsWithExecutor(ctx context.Context, txs [][]byte) ([]*abci.ExecTxResult, error) {
-	if app.txExecutor != nil {
-		return app.txExecutor(ctx, txs, app.finalizeBlockState.ms, func(i int, memTx sdk.Tx, ms storetypes.MultiStore, incarnationCache map[string]any) *abci.ExecTxResult {
-			return app.deliverTxWithMultiStore(txs[i], ms, incarnationCache)
-		})
+	if app.txExecutor == nil {
+		app.txExecutor = DefaultExecutor{
+			ctx:       ctx,
+			txDecoder: app.txDecoder,
+			deliverTx: func(txs []byte) *abci.ExecTxResult { return app.deliverTx(txs, nil, nil) },
+		}
 	}
 
-	// Fallback to the default execution logic
-	txResults := make([]*abci.ExecTxResult, 0, len(txs))
-	for _, rawTx := range txs {
-		var response *abci.ExecTxResult
-
-		if _, err := app.txDecoder(rawTx); err == nil {
-			response = app.deliverTx(rawTx)
-		} else {
-			// In the case where a transaction included in a block proposal is malformed,
-			// we still want to return a default response to comet. This is because comet
-			// expects a response for each transaction included in a block proposal.
-			response = sdkerrors.ResponseExecTxResultWithEvents(
-				sdkerrors.ErrTxDecode,
-				0,
-				0,
-				nil,
-				false,
-			)
-		}
-
-		// check after every tx if we should abort
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		default:
-			// continue
-		}
-
-		txResults = append(txResults, response)
-	}
-	return txResults, nil
+	return app.txExecutor.run(txs)
 }
 
 // FinalizeBlock will execute the block proposal provided by RequestFinalizeBlock.

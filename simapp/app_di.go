a82c6b0cbb43d44bfac53633941aec0432394fdf
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-04-27 13:27:55 -0700

    use module config rather than supplied option

--- simapp/app_di.go
+++ simapp/app_di.go
@@ -139,7 +139,6 @@ func NewSimApp(
 				// with the prefix defined in the auth module configuration.
 				//
 				// func() address.Codec { return <- custom address codec type -> }
-				[]authkeeper.InitOption{authkeeper.WithUnorderedTransactions(true)},
 				//
 				// STAKING
 				//

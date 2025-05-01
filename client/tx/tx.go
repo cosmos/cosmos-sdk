245a5faf02029e79065b663a5f24f69348315dee
Takeshi Arabiki <takeshi.arabiki@gmail.com>
2025-05-02 07:50:18 +0900

    fix: Handle context cancellation (#24551)
    Co-authored-by: Alex | Interchain Labs <alex@interchainlabs.io>

--- client/tx/tx.go
+++ client/tx/tx.go
@@ -126,7 +126,7 @@ func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
 		}
 	}
 
-	if err = Sign(clientCtx.CmdContext, txf, clientCtx.FromName, tx, true); err != nil {
+	if err = Sign(clientCtx.GetCmdContextWithFallback(), txf, clientCtx.FromName, tx, true); err != nil {
 		return err
 	}
 

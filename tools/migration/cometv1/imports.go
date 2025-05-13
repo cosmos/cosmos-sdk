206b7be89dbc8b8fd3835b18f4b809dae3ecf1f6
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-05-13 11:44:01 -0700

    add missing import

--- tools/migration/cometv1/imports.go
+++ tools/migration/cometv1/imports.go
@@ -6,4 +6,5 @@ var importReplacements = []migration.ImportReplacement{
 	{Old: "github.com/cometbft/cometbft/proto/tendermint/types", New: "github.com/cometbft/cometbft/api/cometbft/types/v1"},
 	{Old: "github.com/cometbft/cometbft/proto/tendermint/crypto", New: "github.com/cometbft/cometbft/api/cometbft/crypto/v1"},
 	{Old: "github.com/cometbft/cometbft/proto/tendermint/state", New: "github.com/cometbft/cometbft/api/cometbft/state/v1"},
+	{Old: "github.com/cometbft/cometbft/proto/tendermint/version", New: "github.com/cometbft/cometbft/api/cometbft/version/v1"},
 }

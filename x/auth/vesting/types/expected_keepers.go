5c3ce7c840321aa8ea7836a102f7713e3439bf92
Jeancarlo Barrios <JeancarloBarrios@users.noreply.github.com>
2025-05-15 15:51:26 -0600

    fix: vesting expected keepers for new apus

--- x/auth/vesting/types/expected_keepers.go
+++ x/auth/vesting/types/expected_keepers.go
@@ -3,42 +3,42 @@ package types
 import (
 	context "context"
 
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // AccountKeeper defines the expected interface contract the vesting module
 // requires for storing accounts.
 type AccountKeeper interface {
-	GetAccount(sdk.Context, sdk.AccAddress) authtypes.AccountI
-	SetAccount(sdk.Context, authtypes.AccountI)
+	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI
+	SetAccount(context.Context, sdk.AccountI)
 }
 
 // BankKeeper defines the expected interface contract the vesting module requires
 // for creating vesting accounts with funds.
 type BankKeeper interface {
-	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
-	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
-	IsSendEnabledCoins(ctx sdk.Context, coins ...sdk.Coin) error
-	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
-	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
+	GetBalance(ctx context.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	IsSendEnabledCoins(ctx context.Context, coins ...sdk.Coin) error
+	SendCoins(ctx context.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 	BlockedAddr(addr sdk.AccAddress) bool
 }
 
 // StakingKeeper defines the expected interface contract the vesting module
 // requires for finding and changing the delegated tokens, used in clawback.
 type StakingKeeper interface {
-	BondDenom(ctx sdk.Context) string
-	GetDelegatorBonded(ctx sdk.Context, delegator sdk.AccAddress) sdk.Int
-	GetDelegatorDelegations(ctx sdk.Context, delegator sdk.AccAddress,
+	BondDenom(ctx context.Context) string
+	GetDelegatorBonded(ctx context.Context, delegator sdk.AccAddress) math.Int
+	GetDelegatorDelegations(ctx context.Context, delegator sdk.AccAddress,
 		maxRetrieve uint16) []stakingtypes.Delegation
-	GetDelegatorUnbonding(ctx sdk.Context, delegator sdk.AccAddress) sdk.Int
-	GetUnbondingDelegations(ctx sdk.Context, delegator sdk.AccAddress,
+	GetDelegatorUnbonding(ctx context.Context, delegator sdk.AccAddress) math.Int
+	GetUnbondingDelegations(ctx context.Context, delegator sdk.AccAddress,
 		maxRetrieve uint16) []stakingtypes.UnbondingDelegation
-	GetValidator(ctx sdk.Context, valAddr sdk.ValAddress) (stakingtypes.Validator, bool)
-	TransferUnbonding(ctx sdk.Context, fromAddr, toAddr sdk.AccAddress, valAddr sdk.ValAddress, wantAmt sdk.Int) sdk.Int
-	TransferDelegation(ctx sdk.Context, fromAddr, toAddr sdk.AccAddress, valAddr sdk.ValAddress, wantShares sdk.Dec) sdk.Dec
+	GetValidator(ctx context.Context, valAddr sdk.ValAddress) (stakingtypes.Validator, bool)
+	TransferUnbonding(ctx context.Context, fromAddr, toAddr sdk.AccAddress, valAddr sdk.ValAddress, wantAmt math.Int) math.Int
+	TransferDelegation(ctx context.Context, fromAddr, toAddr sdk.AccAddress, valAddr sdk.ValAddress, wantShares math.LegacyDec) math.LegacyDec
 }
 
 // DistributionHooks is the expected interface for distribution module hooks.
@@ -48,8 +48,8 @@ type DistributionHooks interface {
 	// keeper will call this before each reward withdrawal.
 	// If multiple distribution hooks are set, then any of them may
 	// disallow the withdraw address.
-	AllowWithdrawAddr(ctx sdk.Context, delAddr sdk.AccAddress) bool
+	AllowWithdrawAddr(ctx context.Context, delAddr sdk.AccAddress) bool
 
 	// AfterDelegationReward is called after the reward has been transferred the address.
-	AfterDelegationReward(ctx sdk.Context, delAddr, withdrawAddr sdk.AccAddress, reward sdk.Coins)
+	AfterDelegationReward(ctx context.Context, delAddr, withdrawAddr sdk.AccAddress, reward sdk.Coins)
 }

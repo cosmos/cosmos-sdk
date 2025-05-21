2277ffb65869e0e4d1763ebbec12d0f6c87cac6b
Aaron Craelius <aaronc@users.noreply.github.com>
2025-05-21 14:53:21 -0400

    data watcher tests

---
+++ tools/cosmovisor/data_watcher.go
@@ -0,0 +1,55 @@
+package cosmovisor
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type DataWatcher[T any] struct {
+	outChan chan T
+	errChan chan error
+}
+
+func NewDataWatcher[T any](ctx context.Context, watcher Watcher[[]byte]) *DataWatcher[T] {
+	outChan := make(chan T, 1)
+	errChan := make(chan error, 1)
+	go func() {
+		defer close(outChan)
+		defer close(errChan)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case contents, ok := <-watcher.Updated():
+				if !ok {
+					return
+				}
+				var data T
+				err := json.Unmarshal(contents, &data)
+				// ignore errors because failing JSON unmarshal probably just means the file is incomplete
+				if err == nil {
+					outChan <- data
+				}
+			case err, ok := <-watcher.Errors():
+				if !ok {
+					return
+				}
+				errChan <- err
+
+			}
+		}
+	}()
+	return &DataWatcher[T]{
+		outChan: outChan,
+		errChan: errChan,
+	}
+}
+
+func (d DataWatcher[T]) Updated() <-chan T {
+	return d.outChan
+
+}
+
+func (d DataWatcher[T]) Errors() <-chan error {
+	return d.errChan
+}

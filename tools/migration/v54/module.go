27d58bc8dee88ea743b4fbf269acb3176b21cb37
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-05-22 14:06:09 -0700

    store version

--- tools/migration/v54/module.go
+++ tools/migration/v54/module.go
@@ -3,6 +3,6 @@ package main
 import migration "github.com/cosmos/cosmos-sdk/tools/migrate"
 
 var moduleUpdates = migration.GoModUpdate{
-	"github.com/cosmos/cosmos-sdk": "f7601e5",
-	"cosmossdk.io/store":           "f7601e5",
+	"github.com/cosmos/cosmos-sdk": "v0.53.0-rc.4.0.20250522154455-f7601e5b81c2",
+	"cosmossdk.io/store":           "v1.10.0-rc.1.0.20250522154455-f7601e5b81c2",
 }

8ea7effaa1eff1a7649b753c2344b61955bd6412
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-05-22 14:00:16 -0700

    store too

--- tools/migration/v54/module.go
+++ tools/migration/v54/module.go
@@ -4,4 +4,5 @@ import migration "github.com/cosmos/cosmos-sdk/tools/migrate"
 
 var moduleUpdates = migration.GoModUpdate{
 	"github.com/cosmos/cosmos-sdk": "f7601e5",
+	"cosmossdk.io/store":           "f7601e5",
 }

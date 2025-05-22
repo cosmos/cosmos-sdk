dd89a2615140eef42bfc232d553b3afc70920728
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-05-22 14:56:37 -0700

    more go mods

--- tools/migration/v54/module.go
+++ tools/migration/v54/module.go
@@ -5,4 +5,7 @@ import migration "github.com/cosmos/cosmos-sdk/tools/migrate"
 var moduleUpdates = migration.GoModUpdate{
 	"github.com/cosmos/cosmos-sdk": "v0.53.0-rc.4.0.20250522154455-f7601e5b81c2",
 	"cosmossdk.io/store":           "v1.10.0-rc.1.0.20250522154455-f7601e5b81c2",
+	"cosmossdk.io/x/upgrade":       "v0.2.0-rc.2",
+	"github.com/cometbft/cometbft": "v1.0.1",
+	"google.golang.org/grpc":       "v1.72.1",
 }

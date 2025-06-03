39a9da3740b0d91180a108c14fc3d96404c61f0d
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-06-03 09:40:54 -0700

    cometv2 v54 migration

--- tools/migration/v54/imports.go
+++ tools/migration/v54/imports.go
@@ -3,9 +3,9 @@ package main
 import migration "github.com/cosmos/cosmos-sdk/tools/migrate"
 
 var importReplacements = []migration.ImportReplacement{
-	{Old: "github.com/cometbft/cometbft/proto/tendermint/types", New: "github.com/cometbft/cometbft/api/cometbft/types/v1"},
+	{Old: "github.com/cometbft/cometbft/proto/tendermint/types", New: "github.com/cometbft/cometbft/api/cometbft/types/v2"},
 	{Old: "github.com/cometbft/cometbft/proto/tendermint/crypto", New: "github.com/cometbft/cometbft/api/cometbft/crypto/v1"},
-	{Old: "github.com/cometbft/cometbft/proto/tendermint/state", New: "github.com/cometbft/cometbft/api/cometbft/state/v1"},
+	{Old: "github.com/cometbft/cometbft/proto/tendermint/state", New: "github.com/cometbft/cometbft/api/cometbft/state/v2"},
 	{Old: "github.com/cometbft/cometbft/proto/tendermint/version", New: "github.com/cometbft/cometbft/api/cometbft/version/v1"},
 	{Old: "github.com/cometbft/cometbft/proto/tendermint/p2p", New: "github.com/cometbft/cometbft/api/cometbft/p2p/v1"},
 	{Old: "cosmossdk.io/x/feegrant", New: "github.com/cosmos/cosmos-sdk/x/feegrant", AllPackages: true},
--- tools/migration/v54/module.go
+++ tools/migration/v54/module.go
@@ -3,12 +3,12 @@ package main
 import migration "github.com/cosmos/cosmos-sdk/tools/migrate"
 
 var moduleUpdates = migration.GoModUpdate{
-	"github.com/cosmos/cosmos-sdk": "v0.53.0-rc.4.0.20250522154455-f7601e5b81c2",
-	"cosmossdk.io/store":           "v1.10.0-rc.1.0.20250522154455-f7601e5b81c2",
-	"github.com/cometbft/cometbft": "v1.0.1",
-	"google.golang.org/grpc":       "v1.72.1",
-	"cosmossdk.io/x/tx":            "v1.1.1-0.20250515174933-df4c1c3edf16",
-	"cosmossdk.io/client/v2":       "v2.0.0-beta.8",
+	"github.com/cosmos/cosmos-sdk": "v0.54.0-alpha.0.0.20250602195229-601ab15623c5",
+	"cosmossdk.io/store":           "v1.10.0-rc.1.0.20250602195229-601ab15623c5",
+	// "github.com/cometbft/cometbft": "v1.0.1",
+	"google.golang.org/grpc": "v1.72.1",
+	"cosmossdk.io/x/tx":      "v1.2.0-alpha.0.0.20250602195229-601ab15623c5",
+	"cosmossdk.io/client/v2": "v2.0.0-beta.10.0.20250602195229-601ab15623c5",
 }
 
 var removals = []string{

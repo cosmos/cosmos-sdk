291e00fdd9983cd40456f8f5dc4d148ff8594580
Aaron Craelius <aaronc@users.noreply.github.com>
2025-06-03 17:51:52 -0400

    fixes from testing

--- tools/cosmovisor/cmd/cosmovisor/mockchain_test.go
+++ tools/cosmovisor/cmd/cosmovisor/mockchain_test.go
@@ -20,11 +20,12 @@ type MockChainSetup struct {
 }
 
 func mockNodeWrapper(args string) string {
-	return fmt.Sprintf(`
-#!/usr/bin/env bash
+	return fmt.Sprintf(
+		`#!/usr/bin/env bash
 set -e
 
-exec mock_node %s "$@"
+echo "$@"
+exec mock_node %s "$@" 
 `, args)
 }
 
@@ -40,9 +41,9 @@ func (m MockChainSetup) Setup(t *testing.T) (string, string) {
 	// create genesis wrapper
 	genDir := filepath.Join(cosmovisorDir, "genesis", "bin")
 	require.NoError(t, os.MkdirAll(genDir, 0o755))
+	mockdPath := filepath.Join(genDir, "mockd")
 	require.NoError(t,
-		os.WriteFile(filepath.Join(genDir, "mockd"),
-			[]byte(mockNodeWrapper(m.Genesis)), 0o755),
+		os.WriteFile(mockdPath, []byte(mockNodeWrapper(m.Genesis)), 0o755),
 	)
 	// create upgrade wrappers
 	for name, args := range m.Upgrades {
@@ -70,7 +71,7 @@ func (m MockChainSetup) Setup(t *testing.T) (string, string) {
 
 func TestMockChain(t *testing.T) {
 	mockchainDir, cfgFile := MockChainSetup{
-		Genesis: "--block-time 1s --upgrade-plan '{\"name\":\"gov1\",\"height\":14}'",
+		Genesis: "--block-time 1s --upgrade-plan '{\"name\":\"gov1\",\"height\":15}'",
 		Upgrades: map[string]string{
 			"gov1":    "--halt-height 20 --block-time 1s --upgrade-plan '{\"name\":\"gov2\",\"height\":30}'",
 			"manual1": "--block-time 1s --upgrade-plan '{\"name\":\"gov1\",\"height\":15}'",
@@ -82,5 +83,7 @@ func TestMockChain(t *testing.T) {
 
 	rootCmd := NewRootCmd()
 	rootCmd.SetArgs([]string{"run", "--home", mockchainDir, "--cosmovisor-config", cfgFile})
+	rootCmd.SetOut(os.Stdout)
+	rootCmd.SetErr(os.Stderr)
 	require.NoError(t, rootCmd.ExecuteContext(context.Background()))
 }

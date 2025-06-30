ef8d1b4012a5a6f10a24c23d966cd140689c62b8
Aaron Craelius <aaronc@users.noreply.github.com>
2025-06-30 12:03:48 +0200

    refactor process runner to better handle cases where process could have already exited

--- tools/cosmovisor/internal/process.go
+++ tools/cosmovisor/internal/process.go
@@ -2,59 +2,67 @@ package internal
 
 import (
 	"os/exec"
-	"sync"
 	"syscall"
 	"time"
 )
 
 type ProcessRunner struct {
 	cmd  *exec.Cmd
-	done chan error
-	wg   *sync.WaitGroup
+	done chan error // closed exactly once, after Wait returns
 }
 
-func RunProcess(cmd *exec.Cmd) *ProcessRunner {
+func RunProcess(cmd *exec.Cmd) (*ProcessRunner, error) {
+	// start the process before returning a ProcessRunner
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
 	done := make(chan error, 1)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		err := cmd.Run()
-		done <- err
+		// wait on the process to complete in a separate go routine
+		done <- cmd.Wait()
 		close(done)
 	}()
-	return &ProcessRunner{
-		cmd:  cmd,
-		done: done,
-		wg:   wg,
-	}
+	return &ProcessRunner{cmd: cmd, done: done}, nil
 }
 
-func (pr *ProcessRunner) Done() chan error {
+// Done returns the error that the process returned when it exited.
+func (pr *ProcessRunner) Done() <-chan error {
 	return pr.done
 }
 
-func (pr *ProcessRunner) Shutdown(shutdownGrace time.Duration) error {
+// Shutdown attempts to gracefully shut down the process by sending a SIGTERM signal.
+// If the process does not exit within the specified grace period, it will be forcefully killed.
+// An error will only be returned if there was an error shutting down the process.
+// To get the error that the process itself returned, use Done().
+func (pr *ProcessRunner) Shutdown(grace time.Duration) error {
+	// check if already finished
+	select {
+	case <-pr.done:
+		// already finished, nothing to do
+		return nil
+	default:
+		// not finished yet, proceed with shutdown
+	}
+
 	proc := pr.cmd.Process
 	if proc == nil {
-		return nil // process already exited
+		// this should only be true if the process has already exited
+		<-pr.done // make sure Wait() has returned
+		return nil
 	}
-	// TODO make sure we don't kill a process that is not running
+
+	// signal shutdown
 	if err := proc.Signal(syscall.SIGTERM); err != nil {
 		return err
 	}
-	done := make(chan struct{})
-	go func() {
-		pr.wg.Wait()
-		close(done)
-	}()
-	go func() {
-		select {
-		case <-done:
-		case <-time.After(shutdownGrace):
-			// TODO handle the error
-			proc.Kill()
-		}
-	}()
+
+	// wait for graceful exit or force-kill after timeout
+	select {
+	case <-pr.done:
+	case <-time.After(grace):
+		_ = proc.Kill()
+		<-pr.done
+	}
 	return nil
 }

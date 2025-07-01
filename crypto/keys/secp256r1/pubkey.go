98b29b7cc6f86a63cb23af5cae9b7d3160662db0
Shreyans T <shreyans.tatiya@gmail.com>
2025-07-02 00:32:32 +0530

    refactor(secp256r1): simplify test code and improve readability in pubkey_internal_test.go

--- crypto/keys/secp256r1/pubkey.go
+++ crypto/keys/secp256r1/pubkey.go
@@ -6,75 +6,59 @@ import (
 	cmtcrypto "github.com/cometbft/cometbft/v2/crypto"
 	"github.com/cosmos/gogoproto/proto"
 
-	errorsmod "cosmossdk.io/errors"
-
 	ecdsa "github.com/cosmos/cosmos-sdk/crypto/keys/internal/ecdsa"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // customProtobufType is here to make sure that ecdsaPK and ecdsaSK implement the
-
 // gogoproto customtype interface.
-
 type customProtobufType interface {
 	Marshal() ([]byte, error)
-
 	MarshalTo(data []byte) (n int, err error)
-
 	Unmarshal(data []byte) error
-
 	Size() int
 
 	MarshalJSON() ([]byte, error)
-
 	UnmarshalJSON(data []byte) error
 }
 
 var _ customProtobufType = (*ecdsaPK)(nil)
 
 // String implements proto.Message interface.
-
 func (m *PubKey) String() string {
 	return m.Key.String(name)
 }
 
 // Bytes implements SDK PubKey interface.
-
 func (m *PubKey) Bytes() []byte {
 	if m == nil {
 		return nil
 	}
-
 	return m.Key.Bytes()
 }
 
 // Equals implements SDK PubKey interface.
-
 func (m *PubKey) Equals(other cryptotypes.PubKey) bool {
 	pk2, ok := other.(*PubKey)
-
 	if !ok {
 		return false
 	}
-
 	return m.Key.Equal(&pk2.Key.PublicKey)
 }
 
 // Address implements SDK PubKey interface.
-
 func (m *PubKey) Address() cmtcrypto.Address {
 	return m.Key.Address(proto.MessageName(m))
 }
 
 // Type returns key type name. Implements SDK PubKey interface.
-
 func (m *PubKey) Type() string {
 	return name
 }
 
 // VerifySignature implements SDK PubKey interface.
-
 func (m *PubKey) VerifySignature(msg, sig []byte) bool {
 	return m.Key.VerifySignature(msg, sig)
 }
@@ -84,24 +68,19 @@ type ecdsaPK struct {
 }
 
 // Marshal implements customProtobufType.
-
 func (pk ecdsaPK) Marshal() ([]byte, error) {
 	return pk.Bytes(), nil
 }
 
 // MarshalJSON implements customProtobufType.
-
 func (pk ecdsaPK) MarshalJSON() ([]byte, error) {
 	b64 := base64.StdEncoding.EncodeToString(pk.Bytes())
-
 	return []byte("\"" + b64 + "\""), nil
 }
 
 // UnmarshalJSON implements customProtobufType.
-
 func (pk *ecdsaPK) UnmarshalJSON(data []byte) error {
 	// the string is quoted so we need to remove them
-
 	bz, err := base64.StdEncoding.DecodeString(string(data[1 : len(data)-1]))
 	if err != nil {
 		return err
@@ -111,38 +90,29 @@ func (pk *ecdsaPK) UnmarshalJSON(data []byte) error {
 }
 
 // Size implements proto.Marshaler interface
-
 func (pk *ecdsaPK) Size() int {
 	if pk == nil {
 		return 0
 	}
-
 	return pubKeySize
 }
 
 // Unmarshal implements proto.Marshaler interface
-
 func (pk *ecdsaPK) Unmarshal(bz []byte) error {
 	return pk.PubKey.Unmarshal(bz, secp256r1, pubKeySize)
 }
 
-// NewPubKeyFromBytes creates a PubKey from bytes.
-
+// NewPubKeyFromBytes creates a secp256r1 PubKey from bytes.
 func NewPubKeyFromBytes(bytes []byte) (*PubKey, error) {
 	if len(bytes) != pubKeySize {
 		return nil, errorsmod.Wrapf(errors.ErrInvalidPubKey,
-
 			"wrong secp256r1 pubkey size, expecting %d bytes, got %d",
-
 			pubKeySize, len(bytes))
 	}
-
 	pk := &ecdsaPK{}
-
 	err := pk.Unmarshal(bytes)
 	if err != nil {
 		return nil, errorsmod.Wrap(errors.ErrInvalidPubKey, err.Error())
 	}
-
 	return &PubKey{Key: pk}, nil
 }
--- crypto/keys/secp256r1/pubkey_internal_test.go
+++ crypto/keys/secp256r1/pubkey_internal_test.go
@@ -19,19 +19,14 @@ func TestPKSuite(t *testing.T) {
 
 type CommonSuite struct {
 	suite.Suite
-
 	pk *PubKey // cryptotypes.PubKey
-
 	sk cryptotypes.PrivKey
 }
 
 func (suite *CommonSuite) SetupSuite() {
 	sk, err := GenPrivKey()
-
 	suite.Require().NoError(err)
-
 	suite.sk = sk
-
 	suite.pk = sk.PubKey().(*PubKey)
 }
 
@@ -41,9 +36,7 @@ func (suite *PKSuite) TestString() {
 	require := suite.Require()
 
 	pkStr := suite.pk.String()
-
 	prefix := "secp256r1{"
-
 	require.Equal(prefix, pkStr[:len(prefix)])
 }
 
@@ -53,11 +46,8 @@ func (suite *PKSuite) TestType() {
 
 func (suite *PKSuite) TestBytes() {
 	bz := suite.pk.Bytes()
-
 	suite.Len(bz, fieldSize+1)
-
 	var pk *PubKey
-
 	suite.Nil(pk.Bytes())
 }
 
@@ -65,19 +55,13 @@ func (suite *PKSuite) TestEquals() {
 	require := suite.Require()
 
 	skOther, err := GenPrivKey()
-
 	require.NoError(err)
-
 	pkOther := skOther.PubKey()
-
 	pkOther2 := &PubKey{&ecdsaPK{skOther.Secret.PubKey()}}
 
 	require.False(suite.pk.Equals(pkOther))
-
 	require.True(pkOther.Equals(pkOther2))
-
 	require.True(pkOther2.Equals(pkOther))
-
 	require.True(pkOther.Equals(pkOther), "Equals must be reflexive") //nolint:gocritic // false positive
 }
 
@@ -87,77 +71,48 @@ func (suite *PKSuite) TestMarshalProto() {
 	/**** test structure marshaling ****/
 
 	var pk PubKey
-
 	bz, err := proto.Marshal(suite.pk)
-
 	require.NoError(err)
-
 	require.NoError(proto.Unmarshal(bz, &pk))
-
 	require.True(pk.Equals(suite.pk))
 
 	/**** test structure marshaling with codec ****/
 
 	pk = PubKey{}
-
 	emptyRegistry := types.NewInterfaceRegistry()
-
 	emptyCodec := codec.NewProtoCodec(emptyRegistry)
-
 	registry := types.NewInterfaceRegistry()
-
 	RegisterInterfaces(registry)
-
 	pubkeyCodec := codec.NewProtoCodec(registry)
 
 	bz, err = emptyCodec.Marshal(suite.pk)
-
 	require.NoError(err)
-
 	require.NoError(emptyCodec.Unmarshal(bz, &pk))
-
 	require.True(pk.Equals(suite.pk))
 
 	const bufSize = 100
-
 	bz2 := make([]byte, bufSize)
-
 	pkCpy := suite.pk
-
 	_, err = pkCpy.MarshalTo(bz2)
-
 	require.NoError(err)
-
 	require.Len(bz2, bufSize)
-
 	require.Equal(bz, bz2[:pk.Size()])
 
 	bz2 = make([]byte, bufSize)
-
 	_, err = pkCpy.MarshalToSizedBuffer(bz2)
-
 	require.NoError(err)
-
 	require.Len(bz2, bufSize)
-
 	require.Equal(bz, bz2[(bufSize-pk.Size()):])
 
 	/**** test interface marshaling ****/
-
 	bz, err = pubkeyCodec.MarshalInterface(suite.pk)
-
 	require.NoError(err)
-
 	var pkI cryptotypes.PubKey
-
 	err = emptyCodec.UnmarshalInterface(bz, &pkI)
-
 	require.EqualError(err, "no registered implementations of type types.PubKey")
 
 	RegisterInterfaces(emptyRegistry)
-
 	require.NoError(emptyCodec.UnmarshalInterface(bz, &pkI))
-
 	require.True(pkI.Equals(suite.pk))
 
 	require.Error(emptyCodec.UnmarshalInterface(bz, nil), "nil should fail")
@@ -165,13 +120,10 @@ func (suite *PKSuite) TestMarshalProto() {
 
 func (suite *PKSuite) TestSize() {
 	require := suite.Require()
-
 	var pk ecdsaPK
-
 	require.Equal(pk.Size(), len(suite.pk.Bytes()))
 
 	var nilPk *ecdsaPK
-
 	require.Equal(0, nilPk.Size(), "nil value must have zero size")
 }
 
@@ -179,13 +131,10 @@ func (suite *PKSuite) TestJson() {
 	require := suite.Require()
 
 	bz, err := suite.pk.Key.MarshalJSON()
-
 	require.NoError(err)
 
 	pk := &ecdsaPK{}
-
 	require.NoError(pk.UnmarshalJSON(bz))
-
 	require.Equal(suite.pk.Key, pk)
 }
 
@@ -193,30 +142,20 @@ func (suite *PKSuite) TestNewPubKeyFromBytes() {
 	require := suite.Require()
 
 	originalBytes := suite.pk.Bytes()
-
 	newPk, err := NewPubKeyFromBytes(originalBytes)
-
 	require.NoError(err)
-
 	require.NotNil(newPk)
-
 	require.True(newPk.Equals(suite.pk))
-
 	require.Equal(originalBytes, newPk.Bytes())
 
 	_, err = NewPubKeyFromBytes([]byte{1, 2, 3})
-
 	require.Error(err)
 
 	_, err = NewPubKeyFromBytes(nil)
-
 	require.Error(err)
 
 	invalidBytes := make([]byte, pubKeySize)
-
 	invalidBytes[0] = 0x04
-
 	_, err = NewPubKeyFromBytes(invalidBytes)
-
 	require.Error(err)
 }

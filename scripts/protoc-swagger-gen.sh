7db9b6387cd1e30712bcdd27528c00dfb255c2b2
MozirDmitriy <dmitriymozir@gmail.com>
2025-07-14 21:46:06 +0300

    chore: clean up protoc-swagger-gen.sh by removing unused tmp-swagger-gen and redundant find options

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -2,9 +2,8 @@
 
 set -eo pipefail
 
-mkdir -p ./tmp-swagger-gen
 cd proto
-proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./cosmos -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
@@ -18,6 +17,3 @@ cd ..
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
 swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
-
-# clean swagger files
-rm -rf ./tmp-swagger-gen

1826ed72bc69c1070c4567ffa6c02ed5a2ff5c01
jay.tseng <jaytseng106@gmail.com>
2025-07-23 20:50:25 -0400

    fix v3 migrate

--- x/mint/keeper/migrator.go
+++ x/mint/keeper/migrator.go
@@ -1,12 +1,10 @@
 package keeper
 
 import (
-	"context"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/mint/exported"
 	v2 "github.com/cosmos/cosmos-sdk/x/mint/migrations/v2"
-	"github.com/cosmos/cosmos-sdk/x/mint/types"
+	v3 "github.com/cosmos/cosmos-sdk/x/mint/migrations/v3"
 )
 
 // Migrator is a struct for handling in-place state migrations.
@@ -33,21 +31,6 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 
 // Migrate2to3 migrates the x/mint module state from the consensus version 2 to
 // version 3.
-func (m Migrator) Migrate2to3(ctx context.Context) error {
-	params, err := m.keeper.Params.Get(ctx)
-	if err != nil {
-		return err
-	}
-
-	// Initialize the new MaxSupply parameter with the default value
-	defaultParams := types.DefaultParams()
-	params.MaxSupply = defaultParams.MaxSupply
-
-	// Set the updated params
-	err = m.keeper.Params.Set(ctx, params)
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	return v3.Migrate(ctx, m.keeper.storeService.OpenKVStore(ctx), m.keeper.cdc, m.keeper.Params)
 }
--- x/mint/module.go
+++ x/mint/module.go
@@ -133,6 +133,10 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
 		panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", types.ModuleName, err))
 	}
+
+	if err := cfg.RegisterMigration(types.ModuleName, 2, m.Migrate2to3); err != nil {
+		panic(fmt.Sprintf("failed to migrate x/%s from version 2 to 3: %v", types.ModuleName, err))
+	}
 }
 
 // InitGenesis performs genesis initialization for the mint module. It returns

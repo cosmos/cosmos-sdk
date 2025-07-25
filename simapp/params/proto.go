ca0c177bcabb7d8e69f52a63d5fcd7a9ccb97aca
bigbear <155267841+aso20455@users.noreply.github.com>
2025-07-25 21:02:07 +0300

    docs: fix spelling issues  (#25009)

--- simapp/params/proto.go
+++ simapp/params/proto.go
@@ -11,7 +11,7 @@ import (
 
 // MakeTestEncodingConfig creates an EncodingConfig for a non-amino based test configuration.
 // This function should be used only internally (in the SDK).
-// App user should'nt create new codecs - use the app.AppCodec instead.
+// App user shouldn't create new codecs - use the app.AppCodec instead.
 // [DEPRECATED]
 func MakeTestEncodingConfig() EncodingConfig {
 	cdc := codec.NewLegacyAmino()

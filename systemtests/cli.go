170a0e89ac889160744292163d2facf8c8faf416
crStiv <cryptostiv7@gmail.com>
2025-07-28 21:03:30 +0200

    Update cli.go

--- systemtests/cli.go
+++ systemtests/cli.go
@@ -344,9 +344,9 @@ func (c CLIWrapper) AddKey(name string) string {
 }
 
 // AddKeyFromSeed recovers the key from given seed and add it to default keyring. Returns address
-func (c CLIWrapper) AddKeyFromSeed(name, mnemoic string) string {
+func (c CLIWrapper) AddKeyFromSeed(name, mnemonic string) string {
 	cmd := c.WithKeyringFlags("keys", "add", name, "--recover")
-	out, _ := c.runWithInput(cmd, strings.NewReader(mnemoic))
+	out, _ := c.runWithInput(cmd, strings.NewReader(mnemonic))
 	addr := gjson.Get(out, "address").String()
 	require.NotEmpty(c.t, addr, "got %q", out)
 	return addr

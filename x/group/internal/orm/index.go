4ace257a4de228d7b3b0744e5499fb3585919708
strmfos <155266597+strmfos@users.noreply.github.com>
2025-07-30 18:03:29 +0200

    docs: fix typos in comments / code  (#25047)
    Co-authored-by: Alex | Interchain Labs <alex@interchainlabs.io>

--- x/group/internal/orm/index.go
+++ x/group/internal/orm/index.go
@@ -145,7 +145,7 @@ func (i MultiKeyIndex) PrefixScan(store types.KVStore, startI, endI any) (Iterat
 }
 
 // ReversePrefixScan returns an Iterator over a domain of keys in descending order. End is exclusive.
-// Start is an MultiKeyIndex key or prefix. It must be less than end, or the Iterator is invalid  and error is returned.
+// Start is an MultiKeyIndex key or prefix. It must be less than end, or the Iterator is invalid and error is returned.
 // Iterator must be closed by caller.
 // To iterate over entire domain, use PrefixScan(nil, nil)
 //

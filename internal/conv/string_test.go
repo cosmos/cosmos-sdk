021122c22eabad461c497ae6412e915cc261c294
AJoX <shuraev2707@gmail.com>
2025-08-10 11:26:04 +0200

    Update string_test.go

--- internal/conv/string_test.go
+++ internal/conv/string_test.go
@@ -20,7 +20,7 @@ func unsafeConvertStr() []byte {
 }
 
 func (s *StringSuite) TestUnsafeStrToBytes() {
-	// we convert in other function to trigger GC. We want to check that
+	// we convert in another function to trigger GC. We want to check that
 	// the underlying array in []bytes is accessible after GC will finish swapping.
 	for range 5 {
 		b := unsafeConvertStr()

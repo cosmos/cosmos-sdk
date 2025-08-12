964f6cf4e1ef0b86d874ea4ccc812d3c9cdf401e
yyhrnk <yaroslavnerest@gmail.com>
2025-08-12 13:38:50 +0300

    Update app.go

--- runtime/app.go
+++ runtime/app.go
@@ -92,7 +92,7 @@ func (a *App) RegisterModules(modules ...module.AppModule) error {
 
 // RegisterStores registers the provided store keys.
 // This method should only be used for registering extra stores
-// which is necessary for modules that not registered using the app config.
+// which is necessary for modules that are not registered using the app config.
 // To be used in combination of RegisterModules.
 func (a *App) RegisterStores(keys ...storetypes.StoreKey) error {
 	a.storeKeys = append(a.storeKeys, keys...)

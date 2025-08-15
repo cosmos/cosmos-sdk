2a5fc7809885708d116f9bd132e4d1246c585013
menzudilat <158516585+menzudilat@users.noreply.github.com>
2025-08-15 15:45:57 +0200

    Update genesis.go

--- simapp/genesis.go
+++ simapp/genesis.go
@@ -5,7 +5,7 @@ import (
 )
 
 // GenesisState of the blockchain is represented here as a map of raw json
-// messages key'd by a identifier string.
+// messages key'd by an identifier string.
 // The identifier is used to determine which module genesis information belongs
 // to so it may be appropriately routed during init chain.
 // Within this application default genesis information is retrieved from

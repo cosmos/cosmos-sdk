21aefdfe0e1ceaf3378ebaf6aa917f22099c6c62
Sebastian <159052459+sewqasreedas@users.noreply.github.com>
2025-08-16 08:23:20 +0200

    Update hash.go

--- types/address/hash.go
+++ types/address/hash.go
@@ -1,32 +1,93 @@
-package bech32
+package address
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
+	"sort"
 
-	"github.com/cosmos/btcutil/bech32"
+	"github.com/cometbft/cometbft/v2/crypto"
+
+	"cosmossdk.io/errors"
+
+	"github.com/cosmos/cosmos-sdk/internal/conv"
 )
 
-// ConvertAndEncode converts from a base256 encoded byte string to a base32 encoded byte string and then to bech32.
-func ConvertAndEncode(hrp string, data []byte) (string, error) {
-	converted, err := bech32.ConvertBits(data, 8, 5, true)
-	if err != nil {
-		return "", fmt.Errorf("encoding bech32 failed: %w", err)
-	}
+// Len is the length of base addresses
+const Len = sha256.Size
 
-	return bech32.Encode(hrp, converted)
+// Addressable represents any type from which we can derive an address.
+type Addressable interface {
+	Address() []byte
 }
 
-// DecodeAndConvert decodes a bech32 encoded string and converts to base256 encoded bytes.
-func DecodeAndConvert(bech string) (string, []byte, error) {
-	hrp, data, err := bech32.Decode(bech, 1023)
-	if err != nil {
-		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
+// Hash creates a new address from address type and key.
+// The functions should only be used by new types defining their own address function
+// (eg public keys).
+func Hash(typ string, key []byte) []byte {
+	hasher := sha256.New()
+	_, err := hasher.Write(conv.UnsafeStrToBytes(typ))
+	// the error is always nil, it's here only to satisfy the io.Writer interface
+	errors.AssertNil(err)
+	th := hasher.Sum(nil)
+
+	hasher.Reset()
+	_, err = hasher.Write(th)
+	errors.AssertNil(err)
+	_, err = hasher.Write(key)
+	errors.AssertNil(err)
+	return hasher.Sum(nil)
+}
+
+// Compose creates a new address based on sub addresses.
+func Compose(typ string, subAddresses []Addressable) ([]byte, error) {
+	as := make([][]byte, len(subAddresses))
+	totalLen := 0
+	var err error
+	for i := range subAddresses {
+		a := subAddresses[i].Address()
+		as[i], err = LengthPrefix(a)
+		if err != nil {
+			return nil, fmt.Errorf("not compatible sub-address=%v at index=%d [%w]", a, i, err)
+		}
+		totalLen += len(as[i])
 	}
 
-	converted, err := bech32.ConvertBits(data, 5, 8, false)
-	if err != nil {
-		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
+	sort.Slice(as, func(i, j int) bool { return bytes.Compare(as[i], as[j]) <= 0 })
+	key := make([]byte, totalLen)
+	offset := 0
+	for i := range as {
+		copy(key[offset:], as[i])
+		offset += len(as[i])
 	}
+	return Hash(typ, key), nil
+}
 
-	return hrp, converted, nil
+// Module is a specialized version of a composed address for modules. Each module account
+// is constructed from a module name and a sequence of derivation keys (at least one
+// derivation key must be provided). The derivation keys must be unique
+// in the module scope, and is usually constructed from some object id. Example, let's
+// a x/dao module, and a new DAO object, its address would be:
+//
+//	address.Module(dao.ModuleName, newDAO.ID)
+func Module(moduleName string, derivationKeys ...[]byte) []byte {
+	mKey := []byte(moduleName)
+	if len(derivationKeys) == 0 { // fallback to the "traditional" ModuleAddress
+		return crypto.AddressHash(mKey)
+	}
+	// need to append zero byte to avoid potential clash between the module name and the first
+	// derivation key
+	mKey = append(mKey, 0)
+	addr := Hash("module", append(mKey, derivationKeys[0]...))
+	for _, k := range derivationKeys[1:] {
+		addr = Derive(addr, k)
+	}
+	return addr
+}
+
+// Derive derives a new address from the main `address` and a derivation `key`.
+// This function is used to create sub accounts. To create a module accounts use the
+// `Module` function.
+func Derive(address, key []byte) []byte {
+	return Hash(conv.UnsafeBytesToStr(address), key)
 }

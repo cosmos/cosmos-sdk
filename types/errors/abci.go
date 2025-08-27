8b0016a8b4c3b9598cb110fbdf9cb9704a167a65
Ragnar <rodiondenmark@gmail.com>
2025-08-27 10:05:10 +0200

    Update abci.go

--- types/errors/abci.go
+++ types/errors/abci.go
@@ -1,10 +1,21 @@
 package errors
 
 import (
+	"math"
+
 	abci "github.com/cometbft/cometbft/v2/abci/types"
 
 	errorsmod "cosmossdk.io/errors"
 )
+
+// safeInt64FromUint64 converts uint64 to int64 with overflow checking.
+// If the value is too large to fit in int64, it returns math.MaxInt64.
+func safeInt64FromUint64(val uint64) int64 {
+	if val > math.MaxInt64 {
+		return math.MaxInt64
+	}
+	return int64(val)
+}
 
 // ResponseCheckTxWithEvents returns an ABCI ResponseCheckTx object with fields filled in
 // from the given error, gas values and events.
@@ -14,8 +25,8 @@ func ResponseCheckTxWithEvents(err error, gw, gu uint64, events []abci.Event, de
 		Codespace: space,
 		Code:      code,
 		Log:       log,
-		GasWanted: int64(gw),
-		GasUsed:   int64(gu),
+		GasWanted: safeInt64FromUint64(gw),
+		GasUsed:   safeInt64FromUint64(gu),
 		Events:    events,
 	}
 }
@@ -28,8 +39,8 @@ func ResponseExecTxResultWithEvents(err error, gw, gu uint64, events []abci.Even
 		Codespace: space,
 		Code:      code,
 		Log:       log,
-		GasWanted: int64(gw),
-		GasUsed:   int64(gu),
+		GasWanted: safeInt64FromUint64(gw),
+		GasUsed:   safeInt64FromUint64(gu),
 		Events:    events,
 	}
 }

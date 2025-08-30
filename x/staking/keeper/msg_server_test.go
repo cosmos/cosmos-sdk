90d6c06cd139c581d0877857e1a3d334df7117f3
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2025-08-30 21:05:02 +0700

    Update msg_server_test.go

--- x/staking/keeper/msg_server_test.go
+++ x/staking/keeper/msg_server_test.go
@@ -695,7 +695,7 @@ func (s *KeeperTestSuite) TestMsgBeginRedelegate() {
 				require.NoError(err)
 				events := ctx.EventManager().Events()
 				delegator, found := events.GetAttributes("delegator")
-				s.Require().Equal(delegator, tc.input.DelegatorAddress)
+				s.Require().Equal(delegator[0].Value, tc.input.DelegatorAddress)
 				s.Require().Equal(true, found)
 			}
 		})

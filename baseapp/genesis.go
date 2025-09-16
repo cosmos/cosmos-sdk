61d46ffc54874b3292c5c191a419ee98aeb83ad2
forkfury <forkfury@gmail.com>
2025-09-16 17:08:04 +0200

    Update genesis.go

--- baseapp/genesis.go
+++ baseapp/genesis.go
@@ -8,13 +8,17 @@ import (
 	"cosmossdk.io/core/genesis"
 )
 
+// Ensure BaseApp implements the genesis.TxHandler interface
 var _ genesis.TxHandler = (*BaseApp)(nil)
 
-// ExecuteGenesisTx implements genesis.GenesisState from
-// cosmossdk.io/core/genesis to set initial state in genesis
+// ExecuteGenesisTx implements the genesis.TxHandler interface from
+// cosmossdk.io/core/genesis. It executes a transaction during genesis
+// to set up the initial state of the blockchain.
 func (ba *BaseApp) ExecuteGenesisTx(tx []byte) error {
+	// Execute the transaction using the deliverTx method
 	res := ba.deliverTx(tx)
 
+	// Check if the transaction execution was successful
 	if res.Code != types.CodeTypeOK {
 		return errors.New(res.Log)
 	}

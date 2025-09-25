61e402c525fc65c156fdb62e180b26e8c281f012
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-09-25 12:02:16 -0400

    resolve lint errors

--- x/staking/keeper/grpc_query_test.go
+++ x/staking/keeper/grpc_query_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"cosmossdk.io/math"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking/testutil"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"

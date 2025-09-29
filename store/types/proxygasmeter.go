0ddeccf46a6dfc57d561456a9d9e2e92cea74da4
yihuang <yi.codeplayer@gmail.com>
2025-09-29 09:03:49 +0800

    reverse order

--- store/types/proxygasmeter.go
+++ store/types/proxygasmeter.go
@@ -1,24 +1,21 @@
 package types
 
-import (
-	"fmt"
-)
+import "fmt"
 
 var _ GasMeter = &ProxyGasMeter{}
 
 // ProxyGasMeter is like a basicGasMeter, but delegates the gas changes (refund and consume) to the parent GasMeter in
-// realtime, so it won't risk losing gas accounting in face of error return or panics.
+// realtime, ensuring accurate gas accounting even during panics.
 type ProxyGasMeter struct {
 	GasMeter
 
 	parent GasMeter
 }
 
-// NewProxyGasMeter returns a new ProxyGasMeter which is like a basic gas meter with minimum of new limit and remaining gas
-// of the parent gas meter, it also delegate the gas consumption to parent gas meter in real time, so it won't risk
-// losing gas accounting in face of panics or other unexpected errors.
+// NewProxyGasMeter creates a ProxyGasMeter that wraps a parent gas meter, inheriting the minimum of the new limit and the parent's remaining gas.
+// It delegates consumption to the parent in real time, ensuring accurate gas accounting even during panics.
 //
-// If limit is greater than or equal to the remaining gas, no wrapping is needed and the original gas meter is returned.
+// Returns the parent directly if the new limit is greater than or equal to its remaining gas.
 func NewProxyGasMeter(gasMeter GasMeter, limit Gas) GasMeter {
 	limit = min(limit, gasMeter.GasRemaining())
 	return &ProxyGasMeter{
@@ -29,8 +26,8 @@ func NewProxyGasMeter(gasMeter GasMeter, limit Gas) GasMeter {
 
 // RefundGas will also refund gas to parent gas meter.
 func (pgm ProxyGasMeter) RefundGas(amount Gas, descriptor string) {
-	pgm.GasMeter.RefundGas(amount, descriptor)
 	pgm.parent.RefundGas(amount, descriptor)
+	pgm.GasMeter.RefundGas(amount, descriptor)
 }
 
 // ConsumeGas will also consume gas from parent gas meter.
@@ -40,8 +37,8 @@ func (pgm ProxyGasMeter) RefundGas(amount Gas, descriptor string) {
 // is the gas is always charged before the work is done, so when out-of-gas panic happens,
 // the actual work is not done yet, so we don't need to consume the gas in parent gas meter.
 func (pgm ProxyGasMeter) ConsumeGas(amount Gas, descriptor string) {
-	pgm.GasMeter.ConsumeGas(amount, descriptor)
 	pgm.parent.ConsumeGas(amount, descriptor)
+	pgm.GasMeter.ConsumeGas(amount, descriptor)
 }
 
 func (pgm ProxyGasMeter) String() string {

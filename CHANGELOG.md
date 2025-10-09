d8cd354e541a7c77f25f888ec298ae1345bf4230
mmsqe <tqd0800210105@gmail.com>
2025-10-09 16:33:15 +0800

    feat: add SetDERConversion to reset skipDERConversion
    add Ledger app name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -55,6 +55,7 @@ Ref: https://keepachangelog.com/en/1.0.0/
 * (server) [#24720](https://github.com/cosmos/cosmos-sdk/pull/24720) add `verbose_log_level` flag for configuring the log level when switching to verbose logging mode during sensitive operations (such as chain upgrades).
 * (crypto) [#24861](https://github.com/cosmos/cosmos-sdk/pull/24861) add `PubKeyFromCometTypeAndBytes` helper function to convert from `comet/v2` PubKeys to the `cryptotypes.Pubkey` interface.
 * (abci_utils) [#25008](https://github.com/cosmos/cosmos-sdk/pull/25008) add the ability to assign a custom signer extraction adapter in `DefaultProposalHandler`.
+* (crypto/ledger) [#25435](https://github.com/cosmos/cosmos-sdk/pull/25435) Add SetDERConversion to reset skipDERConversion and App name for ledger.
 
 ### Improvements
 
--- crypto/ledger/ledger_secp256k1.go
+++ crypto/ledger/ledger_secp256k1.go
@@ -17,6 +17,9 @@ import (
 // options stores the Ledger Options that can be used to customize Ledger usage
 var options Options
 
+// AppName defines the Ledger app used for signing. Cosmos SDK uses the Cosmos app
+const AppName = "Cosmos"
+
 type (
 	// discoverLedgerFn defines a Ledger discovery function that returns a
 	// connected device or an error upon failure. Its allows a method to avoid CGO
@@ -66,7 +69,7 @@ func initOptionsDefault() {
 	options.createPubkey = func(key []byte) types.PubKey {
 		return &secp256k1.PubKey{Key: key}
 	}
-	options.appName = "Cosmos"
+	options.appName = AppName
 	options.skipDERConversion = false
 }
 
@@ -88,6 +91,11 @@ func SetAppName(appName string) {
 // SetSkipDERConversion sets the DER Conversion requirement to true (false by default)
 func SetSkipDERConversion() {
 	options.skipDERConversion = true
+}
+
+// Set the skip DER Conversion requirement to default false
+func SetDERConversion() {
+	options.skipDERConversion = false
 }
 
 // NewPrivKeySecp256k1Unsafe will generate a new key and store the public key for later use.

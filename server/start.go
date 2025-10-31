c922688e2d08cb9458d6db457280c71af696d945
Aaron Craelius <aaronc@users.noreply.github.com>
2025-10-31 14:30:38 -0400

    add telemetry shutdown hook

--- server/start.go
+++ server/start.go
@@ -607,6 +607,12 @@ func startApp(svrCtx *Context, appCreator types.AppCreator, opts StartCmdOptions
 
 	cleanupFn = func() {
 		traceCleanupFn()
+
+		shutdownCtx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		if err := telemetry.Shutdown(shutdownCtx); err != nil {
+			svrCtx.Logger.Error("failed to shutdown telemetry", "error", err)
+		}
+
 		if localErr := app.Close(); localErr != nil {
 			svrCtx.Logger.Error(localErr.Error())
 		}

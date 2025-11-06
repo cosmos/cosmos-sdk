e2a5098e3d3b12047fa34405a14f6069dcff3b38
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-11-06 10:52:25 -0800

    remove this

--- server/config/toml.go
+++ server/config/toml.go
@@ -87,52 +87,6 @@ iavl-disable-fastnode = {{ .BaseConfig.IAVLDisableFastNode }}
 app-db-backend = "{{ .BaseConfig.AppDBBackend }}"
 
 ###############################################################################
-###                         Telemetry Configuration                         ###
-###############################################################################
-
-[telemetry]
-
-# Prefixed with keys to separate services.
-service-name = "{{ .Telemetry.ServiceName }}"
-
-# Enabled enables the application telemetry functionality. When enabled,
-# an in-memory sink is also enabled by default. Operators may also enable
-# other sinks such as Prometheus.
-enabled = {{ .Telemetry.Enabled }}
-
-# Enable prefixing gauge values with hostname.
-enable-hostname = {{ .Telemetry.EnableHostname }}
-
-# Enable adding hostname to labels.
-enable-hostname-label = {{ .Telemetry.EnableHostnameLabel }}
-
-# Enable adding service to labels.
-enable-service-label = {{ .Telemetry.EnableServiceLabel }}
-
-# PrometheusRetentionTime, when positive, enables a Prometheus metrics sink.
-prometheus-retention-time = {{ .Telemetry.PrometheusRetentionTime }}
-
-# GlobalLabels defines a global set of name/value label tuples applied to all
-# metrics emitted using the wrapper functions defined in telemetry package.
-#
-# Example:
-# [["chain_id", "cosmoshub-1"]]
-global-labels = [{{ range $k, $v := .Telemetry.GlobalLabels }}
-  ["{{index $v 0 }}", "{{ index $v 1}}"],{{ end }}
-]
-
-# MetricsSink defines the type of metrics sink to use.
-metrics-sink = "{{ .Telemetry.MetricsSink }}"
-
-# StatsdAddr defines the address of a statsd server to send metrics to.
-# Only utilized if MetricsSink is set to "statsd" or "dogstatsd".
-statsd-addr = "{{ .Telemetry.StatsdAddr }}"
-
-# DatadogHostname defines the hostname to use when emitting metrics to
-# Datadog. Only utilized if MetricsSink is set to "dogstatsd".
-datadog-hostname = "{{ .Telemetry.DatadogHostname }}"
-
-###############################################################################
 ###                           API Configuration                             ###
 ###############################################################################
 

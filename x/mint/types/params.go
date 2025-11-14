b413e009b0f2e7ee117d620419b5794111e77d41
aljo242 <alex@cosmoslabs.io>
2025-11-14 13:49:58 -0500

    validation

--- x/mint/types/params.go
+++ x/mint/types/params.go
@@ -3,15 +3,16 @@ package types
 import (
 	"errors"
 	"fmt"
+	"math"
 	"strings"
 
-	"cosmossdk.io/math"
+	sdkmath "cosmossdk.io/math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // NewParams returns Params instance with the given values.
-func NewParams(mintDenom string, inflationRateChange, inflationMax, inflationMin, goalBonded math.LegacyDec, blocksPerYear uint64) Params {
+func NewParams(mintDenom string, inflationRateChange, inflationMax, inflationMin, goalBonded sdkmath.LegacyDec, blocksPerYear uint64) Params {
 	return Params{
 		MintDenom:           mintDenom,
 		InflationRateChange: inflationRateChange,
@@ -26,10 +27,10 @@ func NewParams(mintDenom string, inflationRateChange, inflationMax, inflationMin
 func DefaultParams() Params {
 	return Params{
 		MintDenom:           sdk.DefaultBondDenom,
-		InflationRateChange: math.LegacyNewDecWithPrec(13, 2),
-		InflationMax:        math.LegacyNewDecWithPrec(20, 2),
-		InflationMin:        math.LegacyNewDecWithPrec(7, 2),
-		GoalBonded:          math.LegacyNewDecWithPrec(67, 2),
+		InflationRateChange: sdkmath.LegacyNewDecWithPrec(13, 2),
+		InflationMax:        sdkmath.LegacyNewDecWithPrec(20, 2),
+		InflationMin:        sdkmath.LegacyNewDecWithPrec(7, 2),
+		GoalBonded:          sdkmath.LegacyNewDecWithPrec(67, 2),
 		BlocksPerYear:       uint64(60 * 60 * 8766 / 5), // assuming 5 second block times
 	}
 }
@@ -75,56 +76,56 @@ func validateMintDenom(denom string) error {
 	return nil
 }
 
-func validateInflationRateChange(rateChange math.LegacyDec) error {
+func validateInflationRateChange(rateChange sdkmath.LegacyDec) error {
 	if rateChange.IsNil() {
 		return fmt.Errorf("inflation rate change cannot be nil: %s", rateChange)
 	}
 	if rateChange.IsNegative() {
 		return fmt.Errorf("inflation rate change cannot be negative: %s", rateChange)
 	}
-	if rateChange.GT(math.LegacyOneDec()) {
+	if rateChange.GT(sdkmath.LegacyOneDec()) {
 		return fmt.Errorf("inflation rate change too large: %s", rateChange)
 	}
 
 	return nil
 }
 
-func validateInflationMax(inflationMax math.LegacyDec) error {
+func validateInflationMax(inflationMax sdkmath.LegacyDec) error {
 	if inflationMax.IsNil() {
 		return fmt.Errorf("max inflation cannot be nil: %s", inflationMax)
 	}
 	if inflationMax.IsNegative() {
 		return fmt.Errorf("max inflation cannot be negative: %s", inflationMax)
 	}
-	if inflationMax.GT(math.LegacyOneDec()) {
+	if inflationMax.GT(sdkmath.LegacyOneDec()) {
 		return fmt.Errorf("max inflation too large: %s", inflationMax)
 	}
 
 	return nil
 }
 
-func validateInflationMin(inflationMin math.LegacyDec) error {
+func validateInflationMin(inflationMin sdkmath.LegacyDec) error {
 	if inflationMin.IsNil() {
 		return fmt.Errorf("min inflation cannot be nil: %s", inflationMin)
 	}
 	if inflationMin.IsNegative() {
 		return fmt.Errorf("min inflation cannot be negative: %s", inflationMin)
 	}
-	if inflationMin.GT(math.LegacyOneDec()) {
+	if inflationMin.GT(sdkmath.LegacyOneDec()) {
 		return fmt.Errorf("min inflation too large: %s", inflationMin)
 	}
 
 	return nil
 }
 
-func validateGoalBonded(goalBonded math.LegacyDec) error {
+func validateGoalBonded(goalBonded sdkmath.LegacyDec) error {
 	if goalBonded.IsNil() {
 		return fmt.Errorf("goal bonded cannot be nil: %s", goalBonded)
 	}
 	if goalBonded.IsNegative() || goalBonded.IsZero() {
 		return fmt.Errorf("goal bonded must be positive: %s", goalBonded)
 	}
-	if goalBonded.GT(math.LegacyOneDec()) {
+	if goalBonded.GT(sdkmath.LegacyOneDec()) {
 		return fmt.Errorf("goal bonded too large: %s", goalBonded)
 	}
 
@@ -134,6 +135,10 @@ func validateGoalBonded(goalBonded math.LegacyDec) error {
 func validateBlocksPerYear(blocksPerYear uint64) error {
 	if blocksPerYear == 0 {
 		return fmt.Errorf("blocks per year must be positive: %d", blocksPerYear)
+	}
+
+	if blocksPerYear > math.MaxInt64 {
+		return fmt.Errorf("blocks per year too large: %d, maximum value is: %d", blocksPerYear, math.MaxInt64)
 	}
 
 	return nil

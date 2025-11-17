f077fc878d231095442dbff32c7caa3e4b13c923
aljo242 <alex@cosmoslabs.io>
2025-11-17 15:28:02 -0500

    lint

--- tests/e2e/gov/tx.go
+++ tests/e2e/gov/tx.go
@@ -260,7 +260,7 @@ func (s *E2ETestSuite) TestNewCmdSubmitLegacyProposal() {
 
 	for _, tc := range testCases {
 		s.Run(tc.name, func() {
-			cmd := cli.NewCmdSubmitProposal()
+			cmd := cli.NewCmdSubmitLegacyProposal() // nolint:staticcheck // legacy testing
 			clientCtx := val.ClientCtx
 
 			out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, tc.args)

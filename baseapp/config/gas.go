0779939db963f9175b4d9c2d426a19b1501900d5
Tyyare <65265263+codeform670@users.noreply.github.com>
2025-11-25 14:02:31 +0300

    Update gas.go

--- baseapp/config/gas.go
+++ baseapp/config/gas.go
@@ -1,12 +1,15 @@
-package config
+// package config
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
+// GasConfig defines the configuration parameters related to gas limits and pricing.
 type GasConfig struct {
-	// queryGasLimit defines the maximum gas for queries; unbounded if 0.
-	QueryGasLimit uint64
+	// QueryGasLimit defines the maximum gas allowed for a query.
+	// It is unbounded if the value is set to 0.
+	QueryGasLimit uint64 
 
-	// The minimum gas prices a validator is willing to accept for processing a
-	// transaction. This is mainly used for DoS and spam prevention.
+	// MinGasPrices represents the minimum gas prices that a validator is willing
+	// to accept for processing a transaction. This is primarily used for
+	// denial-of-service (DoS) and spam prevention.
 	MinGasPrices sdk.DecCoins
 }

b5c902e45d9bf30e76eb9d62e96e831cc766823c
Vlad <vladjdk@gmail.com>
2025-11-27 15:47:28 -0500

    lints

--- x/gov/keeper/genesis_test.go
+++ x/gov/keeper/genesis_test.go
@@ -1,8 +1,9 @@
 package keeper_test
 
 import (
-	sdkmath "cosmossdk.io/math"
 	"go.uber.org/mock/gomock"
+
+	sdkmath "cosmossdk.io/math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/gov/keeper"

e8cfa9300d0fd2c1b32e0a7caec6349923cbc9ab
Aaron Craelius <aaronc@users.noreply.github.com>
2025-12-03 17:20:34 -0500

    WIP on kv data design

--- iavl/internal/kvdata.go
+++ iavl/internal/kvdata.go
@@ -1,17 +1,40 @@
 package internal
 
-// KVDataEntryType represents the type of entry in the KV data file.
-type KVDataEntryType byte
+// KVEntryType represents the type of entry in the KV data file.
+type KVEntryType byte
 
 const (
-	// KVDataEntryTypeWALStart is the first entry in an uncompacted KV data file which indicates that this KV data file
+	// KVEntryWALStart is the first entry in an uncompacted KV data file which indicates that this KV data file
 	// can be used for WAL replay restoration. It must immediately be followed by the varint-encoded version number
 	// corresponding to the first version in this changeset.
-	KVDataEntryTypeWALStart KVDataEntryType = iota
-	// KVDataEntryTypeWALSet indicates a set operation for a key-value pair.
-	KVDataEntryTypeWALSet = iota
-	KVDataEntryTypeWALDelete
-	KVDataEntryTypeWALCommit
-	KVDataEntryTypeExtraK
-	KVDataEntryTypeExtraKV
+	KVEntryWALStart KVEntryType = 0x0
+
+	// KVEntryWALSet indicates a set operation for a key-value pair.
+	// This must be followed by:
+	// - 16-bit little-endian length-prefixed key or 32-bit offset to cached key if KVFlagCachedKey is set
+	// - varint length-prefixed value
+	KVEntryWALSet KVEntryType = 0x1
+
+	// KVEntryWALDelete indicates a delete operation for a key.
+	// This must be followed by:
+	// - 16-bit little-endian length-prefixed key or 32-bit offset to cached key if KVFlagCachedKey is set
+	KVEntryWALDelete KVEntryType = 0x2
+
+	// KVEntryWALCommit indicates the end of a batch of operations for a specific version.
+	// This must be followed by a varint-encoded version number.
+	KVEntryWALCommit KVEntryType = 0x3
+
+	// KVEntryKeyData indicates a key entry in the KV data file.
+	// This must be followed by:
+	// - 16-bit little-endian length-prefixed key
+	KVEntryKeyData KVEntryType = 0x4
+
+	// KVEntryKeyValueData indicates a value entry in the KV data file.
+	// This must be followed by:
+	// - 16-bit little-endian length-prefixed key or 32-bit offset to cached key if KVFlagCachedKey is set
+	// - varint length-prefixed value
+	KVEntryKeyValueData KVEntryType = 0x5
+
+	// KVFlagCachedKey indicates that the key is stored as a 32-bit offset to a cached key entry.
+	KVFlagCachedKey KVEntryType = 0x80
 )
--- iavl/internal/kvdata_writer.go
+++ iavl/internal/kvdata_writer.go
@@ -9,48 +9,37 @@ import (
 
 type KVDataWriter struct {
 	*FileWriter
+	keyCache map[string]uint32
 }
 
 func NewKVDataWriter(file *os.File) *KVDataWriter {
 	fw := NewFileWriter(file)
 	return &KVDataWriter{
 		FileWriter: fw,
+		keyCache:   make(map[string]uint32),
 	}
 }
 
-func (kvs *KVDataWriter) WriteK(key []byte) (offset uint64, err error) {
-	_, err = kvs.Write([]byte{KVDataEntryTypeExtraK})
+func (kvs *KVDataWriter) StartWAL(version uint64) error {
+	err := kvs.writeType(KVEntryWALStart)
 	if err != nil {
-		return offset, err
+		return err
 	}
-
-	return kvs.writeLenPrefixedBytes(key)
-}
-
-func (kvs *KVDataWriter) WriteKV(key, value []byte) (offset uint32, err error) {
-	_, err = kvs.Write([]byte{KVDataEntryTypeExtraKV})
-	if err != nil {
-		return offset, err
-	}
-
-	offset, err = kvs.writeLenPrefixedBytes(key)
-	if err != nil {
-		return 0, err
-	}
-	_, err = kvs.writeLenPrefixedBytes(value)
-	return offset, err
+	return kvs.writeVarUint(version)
 }
 
 func (kvs *KVDataWriter) WriteUpdates(updates []KVUpdate) error {
 	for _, update := range updates {
 		if deleteKey := update.DeleteKey; deleteKey != nil {
-			_, err := kvs.Write([]byte{KVDataEntryTypeDelete})
+			cachedOffset, cached := kvs.keyCache[string(deleteKey)]
+			err := kvs.writeType(KVEntryWALDelete | KVFlagCachedKey)
 			if err != nil {
 				return err
 			}
-			_, err = kvs.writeLenPrefixedBytes(deleteKey)
-			if err != nil {
-				return err
+
+			if cached {
+			} else {
+
 			}
 		} else if memNode := update.SetNode; memNode != nil {
 			_, err := kvs.Write([]byte{KVDataEntryTypeSet})
@@ -83,6 +72,29 @@ func (kvs *KVDataWriter) WriteCommit(version uint32) error {
 	return kvs.writeLEU32(version)
 }
 
+func (kvs *KVDataWriter) WriteK(key []byte) (offset uint64, err error) {
+	_, err = kvs.Write([]byte{KVDataEntryTypeExtraK})
+	if err != nil {
+		return offset, err
+	}
+
+	return kvs.writeLenPrefixedBytes(key)
+}
+
+func (kvs *KVDataWriter) WriteKV(key, value []byte) (offset uint32, err error) {
+	_, err = kvs.Write([]byte{KVDataEntryTypeExtraKV})
+	if err != nil {
+		return offset, err
+	}
+
+	offset, err = kvs.writeLenPrefixedBytes(key)
+	if err != nil {
+		return 0, err
+	}
+	_, err = kvs.writeLenPrefixedBytes(value)
+	return offset, err
+}
+
 func (kvs *KVDataWriter) writeLenPrefixedBytes(key []byte) (offset uint64, err error) {
 	lenKey := len(key)
 	if lenKey > math.MaxUint32 {
@@ -106,7 +118,44 @@ func (kvs *KVDataWriter) writeLenPrefixedBytes(key []byte) (offset uint64, err e
 	return offset, nil
 }
 
-func (kvs *KVDataWriter) writeLEU32(x uint32) error {
+func (kvs *KVDataWriter) writeKey(typ KVEntryType, key []byte) error {
+	cachedOffset, cached := kvs.keyCache[string(key)]
+	if cached {
+		typ |= KVFlagCachedKey
+	}
+	if err := kvs.writeType(typ); err != nil {
+		return err
+	}
+	if cached {
+		return kvs.writeLEU32(uint32(cachedOffset))
+	} else {
+
+	}
+}
+
+func (kvs *KVDataWriter) writeType(x KVEntryType) error {
+	_, err := kvs.Write([]byte{byte(x)})
+	return err
+}
+
+func (kvs *KVDataWriter) writeVarUint(x uint64) error {
+	var buf [binary.MaxVarintLen64]byte
+	n := binary.PutUvarint(buf[:], x)
+	_, err := kvs.Write(buf[0:n])
+	return err
+}
+
+func (kvs *KVDataWriter) writeLEU16(x uint64) error {
+	if x > math.MaxUint16 {
+		return fmt.Errorf("value overflows uint16: %d", x)
+	}
+}
+
+func (kvs *KVDataWriter) writeLEU32(x uint64) error {
+	if x > math.MaxUint32 {
+		return fmt.Errorf("value overflows uint32: %d", x)
+	}
+
 	var buf [4]byte
 	binary.LittleEndian.PutUint32(buf[:], x)
 	_, err := kvs.Write(buf[:])

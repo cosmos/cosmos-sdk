db113dc596044a2dd7db2a6879c11e3e6883dcc6
Aaron Craelius <aaronc@users.noreply.github.com>
2025-12-05 13:47:36 -0500

    document FileWriter

--- iavl/internal/file_writer.go
+++ iavl/internal/file_writer.go
@@ -7,23 +7,29 @@ import (
 	"os"
 )
 
+// FileWriter is a buffered writer that tracks the number of bytes written.
 type FileWriter struct {
 	writer  *bufio.Writer
 	written int
 }
 
+// NewFileWriter creates a new FileWriter.
+// Currently, it uses a buffer size of 512kb.
+// If we want to make that configurable, we can add a constructor with a buffer size parameter in the future.
 func NewFileWriter(file *os.File) *FileWriter {
 	return &FileWriter{
 		writer: bufio.NewWriterSize(file, 512*1024 /* 512kb */), // TODO: maybe we can have this as a config option?
 	}
 }
 
+// Write writes data to the underlying buffered writer and updates the written byte count.
 func (f *FileWriter) Write(p []byte) (n int, err error) {
 	n, err = f.writer.Write(p)
 	f.written += n
 	return n, err
 }
 
+// Flush flushes the underlying buffered writer.
 func (f *FileWriter) Flush() error {
 	if err := f.writer.Flush(); err != nil {
 		return fmt.Errorf("failed to flush writer: %w", err)
@@ -31,6 +37,7 @@ func (f *FileWriter) Flush() error {
 	return nil
 }
 
+// Size returns the total number of bytes written so far.
 func (f *FileWriter) Size() int {
 	return f.written
 }

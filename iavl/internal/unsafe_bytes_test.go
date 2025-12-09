65db861716daab3774cf6e836e4263c764f01ef0
Aaron Craelius <aaronc@users.noreply.github.com>
2025-12-08 19:18:47 -0500

    add UnsafeBytes tests

---
+++ iavl/internal/unsafe_bytes_test.go
@@ -0,0 +1,29 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnsafeBytes(t *testing.T) {
+	hello := []byte("hello")
+	unsafe := WrapUnsafeBytes(hello)
+	require.False(t, unsafe.IsNil())
+	require.Equal(t, hello, unsafe.UnsafeBytes())
+	safeCopy := unsafe.SafeCopy()
+	require.Equal(t, hello, safeCopy)
+	require.NotSame(t, &hello[0], &safeCopy[0]) // different underlying array
+
+	safe := WrapSafeBytes(hello)
+	require.False(t, safe.IsNil())
+	require.Equal(t, hello, safe.UnsafeBytes())
+	safeCopy2 := safe.SafeCopy()
+	require.Equal(t, hello, safeCopy2)
+	require.Same(t, &hello[0], &safeCopy2[0]) // same underlying array
+
+	nilUnsafe := WrapUnsafeBytes(nil)
+	require.True(t, nilUnsafe.IsNil())
+	require.Nil(t, nilUnsafe.UnsafeBytes())
+	require.Nil(t, nilUnsafe.SafeCopy())
+}

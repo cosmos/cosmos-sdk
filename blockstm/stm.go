301ed2e2e445089787804068cdf6b260cacd5992
Eric Warehime <eric.warehime@gmail.com>
2025-12-19 19:50:47 -0800

    Add stm, oe metrics

--- blockstm/stm.go
+++ blockstm/stm.go
@@ -67,8 +67,9 @@ func ExecuteBlockWithEstimates(
 		return errors.New("scheduler did not complete")
 	}
 
-	telemetry.SetGauge(float32(scheduler.executedTxns.Load()), TelemetrySubsystem, KeyExecutedTxs)   //nolint:staticcheck // TODO: switch to OpenTelemetry
-	telemetry.SetGauge(float32(scheduler.validatedTxns.Load()), TelemetrySubsystem, KeyValidatedTxs) //nolint:staticcheck // TODO: switch to OpenTelemetry
+	telemetry.IncrCounter(float32(scheduler.executedTxns.Load()), TelemetrySubsystem, KeyExecutedTxs)   //nolint:staticcheck // TODO: switch to OpenTelemetry
+	telemetry.IncrCounter(float32(scheduler.validatedTxns.Load()), TelemetrySubsystem, KeyValidatedTxs) //nolint:staticcheck // TODO: switch to OpenTelemetry
+	telemetry.IncrCounter(float32(scheduler.decreaseCnt.Load()), TelemetrySubsystem, KeyDecreaseCount)
 
 	// Write the snapshot into the storage
 	mvMemory.WriteSnapshot(storage)

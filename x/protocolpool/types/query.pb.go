f66f49f00b5c3bd69c93596f5e825d108e5c3946
sashaodessa <140454972+sashaodessa@users.noreply.github.com>
2026-01-07 12:58:38 +0100

    Update query.pb.go

--- x/protocolpool/types/query.pb.go
+++ x/protocolpool/types/query.pb.go
@@ -448,7 +448,7 @@ const _ = grpc.SupportPackageIsVersion4
 type QueryClient interface {
 	// CommunityPool queries the community pool coins.
 	CommunityPool(ctx context.Context, in *QueryCommunityPoolRequest, opts ...grpc.CallOption) (*QueryCommunityPoolResponse, error)
-	// ContinuousFund queries a continuous fund by the recipient is is associated with.
+	// ContinuousFund queries a continuous fund by the recipient it is associated with.
 	ContinuousFund(ctx context.Context, in *QueryContinuousFundRequest, opts ...grpc.CallOption) (*QueryContinuousFundResponse, error)
 	// ContinuousFunds queries all continuous funds in the store.
 	ContinuousFunds(ctx context.Context, in *QueryContinuousFundsRequest, opts ...grpc.CallOption) (*QueryContinuousFundsResponse, error)
@@ -504,7 +504,7 @@ func (c *queryClient) Params(ctx context.Context, in *QueryParamsRequest, opts .
 type QueryServer interface {
 	// CommunityPool queries the community pool coins.
 	CommunityPool(context.Context, *QueryCommunityPoolRequest) (*QueryCommunityPoolResponse, error)
-	// ContinuousFund queries a continuous fund by the recipient is is associated with.
+	// ContinuousFund queries a continuous fund by the recipient it is associated with.
 	ContinuousFund(context.Context, *QueryContinuousFundRequest) (*QueryContinuousFundResponse, error)
 	// ContinuousFunds queries all continuous funds in the store.
 	ContinuousFunds(context.Context, *QueryContinuousFundsRequest) (*QueryContinuousFundsResponse, error)

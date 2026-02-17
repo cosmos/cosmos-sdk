315f1df9d9dee0f1c7a678c5587cc505a1d5250b
Eric Warehime <eric.warehime@gmail.com>
2026-02-17 10:13:06 -0800

    Add stub for iavlx

--- iavlx/commit_multi_tree.go
+++ iavlx/commit_multi_tree.go
@@ -36,6 +36,11 @@ type CommitMultiTree struct {
 	workingHash       []byte
 }
 
+func (db *CommitMultiTree) EarliestVersion() int64 {
+	// TODO implement me
+	return 0
+}
+
 // GetObjKVStore returns a mounted ObjKVStore for a given StoreKey.
 func (db *CommitMultiTree) GetObjKVStore(key storetypes.StoreKey) storetypes.ObjKVStore {
 	treeIdx, ok := db.treesByKey[key]
@@ -152,8 +157,10 @@ func (db *CommitMultiTree) Commit() storetypes.CommitID {
 	return commitId
 }
 
-const commitInfoSubPath = "commit_info"
-const latestFilename = "latest"
+const (
+	commitInfoSubPath = "commit_info"
+	latestFilename    = "latest"
+)
 
 // saveCommitInfo saves the CommitInfo for a given version to a <version>.ci file,
 // and updates the latest.version file to point to the latest version.

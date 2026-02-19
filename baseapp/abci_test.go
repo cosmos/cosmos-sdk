33b4189ab7623ca7be564c351fe3f4db933d0d4a
Tyler <48813565+technicallyty@users.noreply.github.com>
2026-02-18 16:38:02 -0800

    put test in abci_test.go instead

--- baseapp/abci_test.go
+++ baseapp/abci_test.go
@@ -15,11 +15,13 @@ import (
 	"testing"
 	"time"
 
+	"cosmossdk.io/store/rootmulti"
 	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cometbft/cometbft/crypto/secp256k1"
 	cmtprotocrypto "github.com/cometbft/cometbft/proto/tendermint/crypto"
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	dbm "github.com/cosmos/cosmos-db"
+	iavlx "github.com/cosmos/cosmos-sdk/iavl"
 	protoio "github.com/cosmos/gogoproto/io"
 	"github.com/cosmos/gogoproto/jsonpb"
 	"github.com/cosmos/gogoproto/proto"
@@ -553,6 +555,72 @@ func TestABCI_GRPCQuery(t *testing.T) {
 	var res testdata.SayHelloResponse
 	require.NoError(t, res.Unmarshal(resQuery.Value))
 	require.Equal(t, "Hello foo!", res.Greeting)
+}
+
+func TestABCI_QueryProof(t *testing.T) {
+	key, value := []byte("hello"), []byte("goodbye")
+
+	var cms *iavlx.CommitMultiTree
+	setCMSOpt := func(bapp *baseapp.BaseApp) {
+		var err error
+		cms, err = iavlx.LoadCommitMultiTree(t.TempDir(), iavlx.Options{})
+		require.NoError(t, err)
+		bapp.SetCMS(cms)
+	}
+	anteOpt := func(bapp *baseapp.BaseApp) {
+		bapp.SetAnteHandler(func(ctx sdk.Context, tx sdk.Tx, simulate bool) (newCtx sdk.Context, err error) {
+			ctx.KVStore(capKey1).Set(key, value)
+			return ctx, nil
+		})
+	}
+
+	suite := NewBaseAppSuite(t, setCMSOpt, anteOpt)
+	t.Cleanup(func() {
+		if cms != nil {
+			_ = cms.Close()
+		}
+	})
+
+	baseapptestutil.RegisterCounterServer(suite.baseApp.MsgServiceRouter(), CounterServerImplGasMeterOnly{})
+	_, err := suite.baseApp.InitChain(&abci.RequestInitChain{
+		ConsensusParams: &cmtproto.ConsensusParams{},
+	})
+	require.NoError(t, err)
+
+	tx := newTxCounter(t, suite.txConfig, 0, 0)
+	bz, err := suite.txConfig.TxEncoder()(tx)
+	require.NoError(t, err)
+
+	_, err = suite.baseApp.FinalizeBlock(&abci.RequestFinalizeBlock{
+		Height: 1,
+		Txs:    [][]byte{bz},
+	})
+	require.NoError(t, err)
+	_, err = suite.baseApp.Commit()
+	require.NoError(t, err)
+
+	_, err = suite.baseApp.FinalizeBlock(&abci.RequestFinalizeBlock{
+		Height: 2,
+		Txs:    [][]byte{bz},
+	})
+	require.NoError(t, err)
+	_, err = suite.baseApp.Commit()
+	require.NoError(t, err)
+
+	res, err := suite.baseApp.Query(context.TODO(), &abci.RequestQuery{
+		Path:   "/store/key1/key",
+		Data:   key,
+		Height: 2,
+		Prove:  true,
+	})
+	require.NoError(t, err)
+	require.Equal(t, value, res.Value)
+	require.NotNil(t, res.ProofOps)
+	require.Len(t, res.ProofOps.Ops, 2)
+	require.Empty(t, res.Log)
+
+	prt := rootmulti.DefaultProofRuntime()
+	require.NoError(t, prt.VerifyValue(res.ProofOps, suite.baseApp.LastCommitID().Hash, "/key1/hello", value))
 }
 
 func TestABCI_P2PQuery(t *testing.T) {

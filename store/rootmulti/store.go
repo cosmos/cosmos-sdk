ef3b8a9d1d57260a4abaa76f9fee2bc05d044d7b
Aaron Craelius <aaronc@users.noreply.github.com>
2026-03-05 13:54:56 -0500

    gci

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -12,15 +12,8 @@ import (
 	"sync"
 	"sync/atomic"
 
-	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	dbm "github.com/cosmos/cosmos-db"
-	protoio "github.com/cosmos/gogoproto/io"
-	gogotypes "github.com/cosmos/gogoproto/types"
-	iavltree "github.com/cosmos/iavl"
-
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/log/v2"
-
 	"cosmossdk.io/store/cachemulti"
 	"cosmossdk.io/store/dbadapter"
 	"cosmossdk.io/store/iavl"
@@ -33,6 +26,11 @@ import (
 	"cosmossdk.io/store/tracekv"
 	"cosmossdk.io/store/transient"
 	"cosmossdk.io/store/types"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	dbm "github.com/cosmos/cosmos-db"
+	protoio "github.com/cosmos/gogoproto/io"
+	gogotypes "github.com/cosmos/gogoproto/types"
+	iavltree "github.com/cosmos/iavl"
 )
 
 const (

a7d4046425869305fd3de749d1dc192a06eca6e7
Aaron Craelius <aaronc@users.noreply.github.com>
2026-03-10 13:02:44 -0400

    add tracekv.MultiStore

---
+++ store/tracekv/multistore.go
@@ -0,0 +1,67 @@
+package tracekv
+
+import (
+	"io"
+
+	"cosmossdk.io/store/types"
+)
+
+var (
+	_ types.MultiStore      = (*MultiStore)(nil)
+	_ types.CacheMultiStore = (*MultiStore)(nil)
+)
+
+// MultiStore wraps a types.MultiStore and traces all KVStore operations.
+type MultiStore struct {
+	parent  types.MultiStore
+	writer  io.Writer
+	context types.TraceContext
+}
+
+// NewMultiStore returns a new tracing MultiStore wrapper.
+func NewMultiStore(parent types.MultiStore, writer io.Writer, tc types.TraceContext) *MultiStore {
+	return &MultiStore{parent: parent, writer: writer, context: tc}
+}
+
+func (ms *MultiStore) GetStoreType() types.StoreType {
+	return ms.parent.GetStoreType()
+}
+
+func (ms *MultiStore) CacheWrap() types.CacheWrap {
+	return ms.CacheMultiStore().(types.CacheWrap)
+}
+
+func (ms *MultiStore) CacheMultiStore() types.CacheMultiStore {
+	return NewMultiStore(ms.parent.CacheMultiStore(), ms.writer, ms.context)
+}
+
+func (ms *MultiStore) CacheMultiStoreWithVersion(version int64) (types.CacheMultiStore, error) {
+	cms, err := ms.parent.CacheMultiStoreWithVersion(version)
+	if err != nil {
+		return nil, err
+	}
+	return NewMultiStore(cms, ms.writer, ms.context), nil
+}
+
+func (ms *MultiStore) GetStore(key types.StoreKey) types.Store {
+	return ms.parent.GetStore(key)
+}
+
+func (ms *MultiStore) GetKVStore(key types.StoreKey) types.KVStore {
+	return NewStore(ms.parent.GetKVStore(key), ms.writer, ms.context)
+}
+
+func (ms *MultiStore) GetObjKVStore(key types.StoreKey) types.ObjKVStore {
+	return ms.parent.GetObjKVStore(key)
+}
+
+func (ms *MultiStore) LatestVersion() int64 {
+	return ms.parent.LatestVersion()
+}
+
+// Write implements CacheMultiStore. It delegates to the parent if it supports Write.
+func (ms *MultiStore) Write() {
+	if cms, ok := ms.parent.(types.CacheMultiStore); ok {
+		cms.Write()
+	}
+}

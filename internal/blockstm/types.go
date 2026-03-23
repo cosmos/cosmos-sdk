5e778566786951d8c40e14a6f312e655b8f192fa
Eric Warehime <eric.warehime@gmail.com>
2026-03-23 17:22:32 -0500

    Remove old metric vars

--- internal/blockstm/types.go
+++ internal/blockstm/types.go
@@ -6,37 +6,6 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/v2/types"
 )
 
-const (
-	TelemetrySubsystem = "blockstm"
-	KeyExecutedTxs     = "executed_txs"
-	KeyTryExecuteTime  = "try_execute_time"
-	KeyValidatedTxs    = "validated_txs"
-	KeyDecreaseCount   = "decrease_count"
-	KeyExecutionRatio  = "execution_ratio"
-
-	// MVData Metrics
-
-	KeyMVDataRead        = "mvdata_read"
-	KeyMVDataConsolidate = "mvdata_consolidate"
-
-	// MVView Metrics
-
-	KeyMVViewReadWriteSet    = "mvview_read_writeset"
-	KeyMVViewReadMVData      = "mvview_read_mvdata"
-	KeyMVViewReadStorage     = "mvview_read_storage"
-	KeyMVViewWrite           = "mvview_write"
-	KeyMVViewDelete          = "mvview_delete"
-	KeyMVViewIteratorKeys    = "mvview_iterator_keys_read"
-	KeyMVViewIteratorKeysCnt = "mvview_iterator_keys_read_count"
-	KeyMVViewEstimateWait    = "mvview_estimate_wait"
-
-	// Executor/Transaction Metrics
-
-	KeyTxReadCount        = "tx_read_count"
-	KeyTxWriteCount       = "tx_write_count"
-	KeyTxNewLocationWrite = "tx_new_location_write"
-)
-
 type (
 	TxnIndex    int
 	Incarnation uint

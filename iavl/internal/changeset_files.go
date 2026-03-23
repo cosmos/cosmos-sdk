48fb209e9dfeda8eabd3ab56ad8a8f55b69fff5c
Aaron Craelius <aaronc@users.noreply.github.com>
2026-03-23 14:08:11 -0400

    merge changes from PR branch

--- iavl/internal/changeset_files.go
+++ iavl/internal/changeset_files.go
@@ -10,13 +10,24 @@ import (
 )
 
 // ChangesetFiles encapsulates management of changeset files.
-// This type is shared between the Changeset and ChangesetWriter types.
+//
+// A changeset contains the following files:
+// - wal.log: the WAL entries for all versions in the changeset
+// - kv.dat: key-value data which isn't in the WAL
+// - checkpoints.dat: checkpoint metadata
+// - branches.dat: the actual BranchLayout structs for checkpoints
+// - leaves.dat: the actual LeafLayout structs for checkpoints
+// - orphans.dat: a log of orphan node IDs and versions they were orphaned at for pruning
+//
+// Changeset are identified by their directory in two formats:
+// - original, uncompacted changesets: {startVersion}/
+// - compacted changesets: {startVersion}-{endVersion}.{compactedAt}/
 type ChangesetFiles struct {
 	dir          string
 	treeDir      string
 	startVersion uint32
 	endVersion   uint32 // 0 if original changeset
-	compactedAt  uint32
+	compactedAt  uint32 // 0 if original changeset
 
 	walFile         *os.File
 	kvDataFile      *os.File
@@ -32,6 +43,11 @@ type ChangesetFiles struct {
 // If compactedAt is 0, the changeset is considered original and uncompacted.
 // If compactedAt is greater than 0, the changeset is considered compacted and will be suffixed with -tmp
 // until MarkReady is called.
+//
+// If the directory already exists, it will be used if and only if all the changeset files within it are empty,
+// otherwise an error will be returned to avoid data corruption.
+// Sometimes during normal operation, a new changeset is created but not written to, this is okay,
+// and we want to continue using this directory, but only if it is truly unused (i.e. all files are empty).
 func CreateChangesetFiles(treeDir string, startVersion, compactedAt uint32) (*ChangesetFiles, error) {
 	// ensure absolute path
 	var err error
@@ -64,6 +80,7 @@ func CreateChangesetFiles(treeDir string, startVersion, compactedAt uint32) (*Ch
 
 	err = cr.open(writeModeFlags)
 	if err != nil {
+		cr.Close() // close any files that were opened before returning error
 		return nil, fmt.Errorf("failed to open changeset files: %w", err)
 	}
 
@@ -76,6 +93,10 @@ func CreateChangesetFiles(treeDir string, startVersion, compactedAt uint32) (*Ch
 				cr.Close() // close files before returning error
 				return nil, fmt.Errorf("changeset dir already exists and is not empty: %s", dir)
 			}
+			if err != nil {
+				cr.Close() // close files before returning error
+				return nil, fmt.Errorf("failed to stat file %s: %w", f.Name(), err)
+			}
 		}
 	}
 
@@ -85,8 +106,8 @@ func CreateChangesetFiles(treeDir string, startVersion, compactedAt uint32) (*Ch
 const writeModeFlags = os.O_RDWR | os.O_CREATE | os.O_APPEND
 
 // OpenChangesetFiles opens an existing changeset directory and files.
-// All files are opened in readonly mode, except for orphans.dat and info.dat which are opened in read-write mode
-// to track orphan data and statistics.
+// All files are opened in readonly mode, except for orphans.dat which is opened in read-write mode
+// to track orphan data.
 func OpenChangesetFiles(dirName string) (*ChangesetFiles, error) {
 	startVersion, endVersion, compactedAt, valid := ParseChangesetDirName(filepath.Base(dirName))
 	if !valid {
@@ -110,12 +131,15 @@ func OpenChangesetFiles(dirName string) (*ChangesetFiles, error) {
 
 	err = cr.open(os.O_RDONLY)
 	if err != nil {
+		cr.Close() // close any files that were opened before returning error
 		return nil, fmt.Errorf("failed to open changeset files: %w", err)
 	}
 
 	return cr, nil
 }
 
+// open opens all changeset files with the specified mode, except for orphans.dat which is always opened with write
+// permissions to allow tracking of orphan data.
 func (cr *ChangesetFiles) open(mode int) error {
 	var err error
 
@@ -162,6 +186,10 @@ func (cr *ChangesetFiles) open(mode int) error {
 // and end version plus compacted at version (if these are available).
 // If the directory name is invalid, valid will be false.
 // If a changeset is original and uncompacted, endVersion and compactedAt will be 0.
+//
+// Valid directory name formats are:
+// - original, uncompacted changesets: {startVersion}/
+// - compacted changesets: {startVersion}-{endVersion}.{compactedAt}/
 func ParseChangesetDirName(dirName string) (startVersion, endVersion, compactedAt uint32, valid bool) {
 	var err error
 	var v uint64
@@ -258,12 +286,17 @@ func (cr *ChangesetFiles) CompactedAtVersion() uint32 {
 }
 
 // EndVersion returns the end version of the changeset if it is known, or 0.
-// For original changesets, this will always return 0.
-// For sealed, compacted changesets, this will return the accurate end version of the changeset.
+// For original changesets, this will always return 0 and the end version must be determined by
+// reading to the end of the WAL.
+// For sealed, compacted changesets, this will always return the accurate end version of the changeset.
 func (cr *ChangesetFiles) EndVersion() uint32 {
 	return cr.endVersion
 }
 
+// MarkReadyAndClose marks a compacted changeset as ready by renaming the directory to remove the -tmp suffix,
+// and then closes all files.
+// It is an error to call this method on an original, uncompacted changeset (i.e. one without the -tmp suffix)
+// since these changesets are already considered ready.
 func (cr *ChangesetFiles) MarkReadyAndClose(endVersion uint32) (finalDir string, err error) {
 	tmpDir := cr.dir
 	if !strings.HasSuffix(tmpDir, "-tmp") {
@@ -274,12 +307,12 @@ func (cr *ChangesetFiles) MarkReadyAndClose(endVersion uint32) (finalDir string,
 	if err != nil {
 		return "", fmt.Errorf("failed to rename changeset directory from %s to %s: %w", tmpDir, finalDir, err)
 	}
+	cr.dir = finalDir
+	cr.endVersion = endVersion
 	err = cr.Close()
 	if err != nil {
 		return "", fmt.Errorf("failed to close changeset files: %w", err)
 	}
-	cr.dir = finalDir
-	cr.endVersion = endVersion
 	return finalDir, nil
 }
 
@@ -287,14 +320,15 @@ func (cr *ChangesetFiles) allFiles() []*os.File {
 	return []*os.File{
 		cr.walFile,
 		cr.kvDataFile,
-		cr.branchesFile,
 		cr.leavesFile,
+		cr.branchesFile,
 		cr.checkpointsFile,
 		cr.orphansFile,
 	}
 }
 
 // Close closes all changeset files.
+// Calls to Close are idempotent and will not return an error if the files are already closed.
 func (cr *ChangesetFiles) Close() error {
 	if cr.closed {
 		return nil

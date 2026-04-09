55639d476d9c575f3c95b964a28c8f596cbc8abc
Vlad <vlad@cosmoslabs.io>
2026-04-09 10:47:55 -0400

    fix the simapp export

--- enterprise/poa/simapp/export.go
+++ enterprise/poa/simapp/export.go
@@ -16,9 +16,14 @@ package simapp
 
 import (
 	"encoding/json"
+	"fmt"
 
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	"github.com/cometbft/cometbft/types"
+
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 )
@@ -41,10 +46,36 @@ func (app *SimApp) ExportAppStateAndValidators(forZeroHeight bool, jailAllowedAd
 		return servertypes.ExportedApp{}, err
 	}
 
+	validators, err := app.POAKeeper.GetAllValidators(ctx)
+	if err != nil {
+		return servertypes.ExportedApp{}, fmt.Errorf("failed to get validators from POA module: %w", err)
+	}
+
+	genesisValidators := make([]types.GenesisValidator, 0, len(validators))
+	for _, v := range validators {
+		if v.Power == 0 {
+			continue
+		}
+		var pk cryptotypes.PubKey
+		if err := app.InterfaceRegistry().UnpackAny(v.PubKey, &pk); err != nil {
+			return servertypes.ExportedApp{}, fmt.Errorf("failed to unpack validator pubkey: %w", err)
+		}
+		cmtPk, err := cryptocodec.ToCmtPubKeyInterface(pk)
+		if err != nil {
+			return servertypes.ExportedApp{}, fmt.Errorf("failed to convert validator pubkey: %w", err)
+		}
+		genesisValidators = append(genesisValidators, types.GenesisValidator{
+			Address: sdk.ConsAddress(cmtPk.Address()).Bytes(),
+			PubKey:  cmtPk,
+			Power:   v.Power,
+			Name:    v.Metadata.Moniker,
+		})
+	}
+
 	return servertypes.ExportedApp{
 		AppState:        appState,
-		Validators:      []types.GenesisValidator{}, // TODO(zrbecker): Get validators from POA module
+		Validators:      genesisValidators,
 		Height:          height,
 		ConsensusParams: app.GetConsensusParams(ctx),
-	}, err
+	}, nil
 }

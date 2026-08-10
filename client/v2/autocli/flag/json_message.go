188433d5e2be92e76e315e3c86ad2f908d29be0d
Jason Colapietro <55137770+JasonColapietro@users.noreply.github.com>
2026-08-10 15:03:58 -0400

    fix(client/v2): encode LegacyDec JSON arguments

--- client/v2/autocli/flag/json_message.go
+++ client/v2/autocli/flag/json_message.go
@@ -2,6 +2,7 @@ package flag
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -12,6 +13,7 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 
 	"cosmossdk.io/client/v2/internal/util"
+	"cosmossdk.io/math"
 )
 
 var isJSONFileRegex = regexp.MustCompile(`\.json$`)
@@ -73,9 +75,136 @@ func (j *jsonMessageFlagValue) Set(s string) error {
 	} else {
 		messageBytes = []byte(s)
 	}
+	messageBytes, err := normalizeLegacyDecJSON(
+		j.messageType.Descriptor(),
+		messageBytes,
+	)
+	if err != nil {
+		return err
+	}
 	return j.jsonUnmarshalOptions.Unmarshal(messageBytes, j.message)
 }
 
 func (j *jsonMessageFlagValue) Type() string {
 	return fmt.Sprintf("%s (json)", j.messageType.Descriptor().FullName())
+}
+
+func normalizeLegacyDecJSON(
+	descriptor protoreflect.MessageDescriptor,
+	messageBytes []byte,
+) ([]byte, error) {
+	var value map[string]json.RawMessage
+	if err := json.Unmarshal(messageBytes, &value); err != nil {
+		return nil, err
+	}
+
+	if err := normalizeLegacyDecFields(descriptor, value); err != nil {
+		return nil, err
+	}
+
+	return json.Marshal(value)
+}
+
+func normalizeLegacyDecFields(
+	descriptor protoreflect.MessageDescriptor,
+	value map[string]json.RawMessage,
+) error {
+	fields := descriptor.Fields()
+	for i := 0; i < fields.Len(); i++ {
+		field := fields.Get(i)
+		fieldValue, ok := value[field.JSONName()]
+		if !ok {
+			fieldValue, ok = value[string(field.Name())]
+		}
+		if !ok {
+			continue
+		}
+
+		normalized, err := normalizeLegacyDecField(field, fieldValue)
+		if err != nil {
+			return fmt.Errorf("invalid %s: %w", field.FullName(), err)
+		}
+		if _, hasJSONName := value[field.JSONName()]; hasJSONName {
+			value[field.JSONName()] = normalized
+		} else {
+			value[string(field.Name())] = normalized
+		}
+	}
+
+	return nil
+}
+
+func normalizeLegacyDecField(
+	field protoreflect.FieldDescriptor,
+	value json.RawMessage,
+) (json.RawMessage, error) {
+	if scalar, ok := GetScalarType(field); ok && scalar == DecScalarType {
+		if field.IsList() {
+			var values []json.RawMessage
+			if err := json.Unmarshal(value, &values); err != nil {
+				return nil, err
+			}
+			for i, item := range values {
+				normalized, err := normalizeLegacyDecValue(item)
+				if err != nil {
+					return nil, err
+				}
+				values[i] = normalized
+			}
+			return json.Marshal(values)
+		}
+
+		return normalizeLegacyDecValue(value)
+	}
+
+	if field.Kind() != protoreflect.MessageKind {
+		return value, nil
+	}
+
+	if field.IsMap() {
+		var values map[string]json.RawMessage
+		if err := json.Unmarshal(value, &values); err != nil {
+			return nil, err
+		}
+		for key, item := range values {
+			normalized, err := normalizeLegacyDecField(field.MapValue(), item)
+			if err != nil {
+				return nil, err
+			}
+			values[key] = normalized
+		}
+		return json.Marshal(values)
+	}
+
+	if field.IsList() {
+		var values []json.RawMessage
+		if err := json.Unmarshal(value, &values); err != nil {
+			return nil, err
+		}
+		for i, item := range values {
+			normalized, err := normalizeLegacyDecJSON(field.Message(), item)
+			if err != nil {
+				return nil, err
+			}
+			values[i] = normalized
+		}
+		return json.Marshal(values)
+	}
+
+	return normalizeLegacyDecJSON(field.Message(), value)
+}
+
+func normalizeLegacyDecValue(value json.RawMessage) (json.RawMessage, error) {
+	if len(value) == 0 || value[0] != '"' {
+		return value, nil
+	}
+	var text string
+	if err := json.Unmarshal(value, &text); err != nil {
+		return nil, err
+	}
+	dec, err := math.LegacyNewDecFromStr(text)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(dec.BigInt().String())
 }

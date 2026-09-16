b4e60baa9e17bde880d68c990e771588d6b34600
Diana Borbe <diana@cosmoslabs.io>
2026-09-16 16:00:34 +0200

    Replace `golang.org/x/crypto/openpgp/armor` with `github.com/ProtonMail/go-crypto/openpgp/armor`

--- crypto/armor.go
+++ crypto/armor.go
@@ -7,10 +7,10 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ProtonMail/go-crypto/openpgp/armor"
 	"github.com/cometbft/cometbft/crypto"
 	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/chacha20poly1305"
-	"golang.org/x/crypto/openpgp/armor" //nolint:staticcheck //TODO: remove this dependency
 
 	errorsmod "cosmossdk.io/errors"
 
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	cosmossdk.io/log/v2 v2.1.0
 	cosmossdk.io/math v1.5.3
 	github.com/99designs/keyring v1.2.1
+	github.com/ProtonMail/go-crypto v1.1.6
 	github.com/RoaringBitmap/roaring/v2 v2.25.0
 	github.com/bgentry/speakeasy v0.2.0
 	github.com/bits-and-blooms/bitset v1.24.5
